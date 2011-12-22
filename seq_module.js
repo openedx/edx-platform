fd2dbd82942c4be10cb6936a52dfcc133b2e3ccc
Piotr Mitros <pmitros@mit.edu>
2011-12-21 21:21:12 -0500

    UI Cleanup. Much nicer now

--- seq_module.js
+++ seq_module.js
@@ -7,6 +7,13 @@ var ${ id }contents=["",
  ""
        ];
 
+var ${ id }types=["",
+ %for t in items:
+ "${t[1]['type']}" , 
+ %endfor
+ ""
+       ];
+
 var ${ id }init_functions=["",
  %for t in items:
 	       function(){ ${t[1]['init_js']} }, 
@@ -24,16 +31,23 @@ var ${ id }loc = -1;
 function ${ id }goto(i) {
     if (${ id }loc!=-1)
 	${ id }destroy_functions[ ${ id }loc ]();
-    $('#content').html(${ id }contents[i]);
+    $('#seq_content').html(${ id }contents[i]);
     ${ id }init_functions[i]()
-	       $('#tt_'+${ id }loc).attr("style", "background-color:grey");
+	       //$('#tt_'+${ id }loc).attr("style", "background-color:gray");
+	       $('#tt_'+${ id }loc).removeClass();
+	       $('#tt_'+${ id }loc).addClass("seq_"+${ id }types[${ id }loc]+"_visited");
     ${ id }loc=i;
-    $('#tt_'+i).attr("style", "background-color:red");
+    //$('#tt_'+i).attr("style", "background-color:red");
+    $('#tt_'+i).removeClass();
+    $('#tt_'+i).addClass("seq_active");
+
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
 
 function ${ id }setup_click(i) {
         $('#tt_'+i).click(function(eo) { ${ id }goto(i);});
+	$('#tt_'+i).addClass("seq_"+${ id }types[i]+"_inactive");
+
 }
 
 function ${ id }next() { 
