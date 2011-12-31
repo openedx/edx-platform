84c81dcaf08a449920e6f1cfe30a3b6be3333861
Piotr Mitros <pmitros@mit.edu>
2011-12-31 11:39:14 -0500

    Ugly big video player

--- video_init.js
+++ video_init.js
@@ -3,7 +3,7 @@ var streams=${ streams }
 var params = { allowScriptAccess: "always", bgcolor: "#cccccc" };
 var atts = { id: "myytplayer" };
 swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&playerapiid=ytplayer", 
-		   "ytapiplayer", "400", "300", "8", null, null, params, atts);
+		   "ytapiplayer", "600", "450", "8", null, null, params, atts);
 var captions=0;
 $("#slider").slider({slide:function(event,ui){seek_slide('slide',event.originalEvent,ui.value);},
                      stop:function(event,ui){seek_slide('stop',event.originalEvent,ui.value);}});
