754f3224a563cde935fd5b1e24406c930e84ae08
Piotr Mitros <pmitros@mit.edu>
2012-01-02 08:00:24 -0500

    GET -> POST

--- js/video_player.js
+++ js/video_player.js
@@ -1,11 +1,28 @@
 // Things to abstract out to another file
 
-function postJSON(url, data, callback, csrf) {
-  $.ajax({
-    url: url,
-    dataType: 'json',
-    data: data,
-    success: callback
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+	var cookies = document.cookie.split(';');
+	for (var i = 0; i < cookies.length; i++) {
+	    var cookie = jQuery.trim(cookies[i]);
+	    // Does this cookie string begin with the name we want?
+	    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+		cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+		break;
+	    }
+	}
+    }
+    return cookieValue;
+}
+
+function postJSON(url, data, callback) {
+    $.ajax({type:'POST',
+	    url: url,
+		dataType: 'json',
+		data: data,
+		success: callback,
+		headers : {'X-CSRFToken':getCookie('csrftoken')}
   });
 }
 
