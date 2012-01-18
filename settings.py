9619bf20ea52ace34fb7365626bba1bea25c9dcb
David Ormsbee <dormsbee@mitx.mit.edu>
2012-01-18 15:00:46 -0500

    moved logging level to INFO, replaced tabs with spaces in settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 if 'COURSEWARE_ENABLED' not in locals():
-   COURSEWARE_ENABLED = True
+    COURSEWARE_ENABLED = True
 if 'ASKBOT_ENABLED' not in locals():
-   ASKBOT_ENABLED = True
+    ASKBOT_ENABLED = True
 if not COURSEWARE_ENABLED:
-   ASKBOT_ENABLED = False
+    ASKBOT_ENABLED = False
 
 # Defaults to be overridden
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -25,7 +25,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-   ('Piotr Mitros', 'pmitros@csail.mit.edu'),
+    ('Piotr Mitros', 'pmitros@csail.mit.edu'),
 )
 
 MANAGERS = ADMINS
@@ -74,14 +74,14 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-   'django.middleware.common.CommonMiddleware',
-   'django.contrib.sessions.middleware.SessionMiddleware',
-   'django.middleware.csrf.CsrfViewMiddleware',
-   'django.contrib.auth.middleware.AuthenticationMiddleware',
-   'django.contrib.messages.middleware.MessageMiddleware',
-   'track.middleware.TrackMiddleware',
-   'djangomako.middleware.MakoMiddleware',
-   #'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'track.middleware.TrackMiddleware',
+    'djangomako.middleware.MakoMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'mitx.urls'
@@ -125,7 +125,7 @@ LOGGING = {
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
-            'level': 'ERROR',
+            'level': 'INFO',
             'propagate': True,
         },
     }
@@ -141,104 +141,104 @@ MAXLOG = 500
 execfile("../settings.py")
 
 if PERFSTATS :
-   MIDDLEWARE_CLASSES = ( 'perfstats.middleware.ProfileMiddleware',) + MIDDLEWARE_CLASSES
+    MIDDLEWARE_CLASSES = ( 'perfstats.middleware.ProfileMiddleware',) + MIDDLEWARE_CLASSES
 
 if 'TRACK_DIR' not in locals():
-   TRACK_DIR = BASE_DIR+'/track_dir/'
+    TRACK_DIR = BASE_DIR+'/track_dir/'
 if 'ASKBOT_EXTRA_SKINS_DIR' not in locals():
-   ASKBOT_EXTRA_SKINS_DIR = BASE_DIR+'/askbot/skins'
+    ASKBOT_EXTRA_SKINS_DIR = BASE_DIR+'/askbot/skins'
 if 'ASKBOT_DIR' not in locals():
-   ASKBOT_DIR = BASE_DIR
+    ASKBOT_DIR = BASE_DIR
 if 'STATIC_ROOT' not in locals():
-   STATIC_ROOT = BASE_DIR+'/staticroot/'
+    STATIC_ROOT = BASE_DIR+'/staticroot/'
 if 'DATA_DIR' not in locals():
-   DATA_DIR = BASE_DIR+'/data/'
+    DATA_DIR = BASE_DIR+'/data/'
 if 'TEXTBOOK_DIR' not in locals():
-   TEXTBOOK_DIR = BASE_DIR+'/textbook/'
+    TEXTBOOK_DIR = BASE_DIR+'/textbook/'
 
 if 'TEMPLATE_DIRS' not in locals():
-   TEMPLATE_DIRS = (
-      BASE_DIR+'/templates/',
-      DATA_DIR+'/templates',
-      TEXTBOOK_DIR,
-   )
+    TEMPLATE_DIRS = (
+        BASE_DIR+'/templates/',
+        DATA_DIR+'/templates',
+        TEXTBOOK_DIR,
+    )
 
 if 'STATICFILES_DIRS' not in locals():
-   STATICFILES_DIRS = (
-      BASE_DIR+'/3rdParty/static',
-      BASE_DIR+'/static'
-   )
+    STATICFILES_DIRS = (
+        BASE_DIR+'/3rdParty/static',
+        BASE_DIR+'/static'
+    )
 
 if ASKBOT_ENABLED:
-   import sys
-   sys.path.append(ASKBOT_DIR)
-   import os
-   import askbot
-   import site
-   site.addsitedir(os.path.join(os.path.dirname(askbot.__file__), 'deps'))
-   TEMPLATE_LOADERS = TEMPLATE_LOADERS + ('askbot.skins.loaders.filesystem_load_template_source',)
+    import sys
+    sys.path.append(ASKBOT_DIR)
+    import os
+    import askbot
+    import site
+    site.addsitedir(os.path.join(os.path.dirname(askbot.__file__), 'deps'))
+    TEMPLATE_LOADERS = TEMPLATE_LOADERS + ('askbot.skins.loaders.filesystem_load_template_source',)
 
-   MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
-	   'askbot.middleware.anon_user.ConnectToSessionMessagesMiddleware',
-	   'askbot.middleware.pagesize.QuestionsPageSizeMiddleware',
-	   'askbot.middleware.cancel.CancelActionMiddleware',
-	   'django.middleware.transaction.TransactionMiddleware',
-	   'askbot.middleware.view_log.ViewLogMiddleware',
-	   'askbot.middleware.spaceless.SpacelessMiddleware',
-	   'askbot.middleware.forum_mode.ForumModeMiddleware',
-	   )
-
-   FILE_UPLOAD_TEMP_DIR = os.path.join(
-	   os.path.dirname(__file__),
-	   'tmp'
-	   ).replace('\\','/')
-   FILE_UPLOAD_HANDLERS = (
-	   'django.core.files.uploadhandler.MemoryFileUploadHandler',
-	   'django.core.files.uploadhandler.TemporaryFileUploadHandler',
-	   )
-   ASKBOT_ALLOWED_UPLOAD_FILE_TYPES = ('.jpg', '.jpeg', '.gif', '.bmp', '.png', '.tiff')
-   ASKBOT_MAX_UPLOAD_FILE_SIZE = 1024 * 1024 #result in bytes
-   ASKBOT_FILE_UPLOAD_DIR = os.path.join(os.path.dirname(__file__), 'askbot', 'upfiles')
-   DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-   
-   PROJECT_ROOT = os.path.dirname(__file__)
-   
-   TEMPLATE_CONTEXT_PROCESSORS = (
-	   'django.core.context_processors.request',
-	   'askbot.context.application_settings',
-	   #'django.core.context_processors.i18n',
-	   'askbot.user_messages.context_processors.user_messages',#must be before auth
-	   'django.core.context_processors.auth', #this is required for admin
-	   'django.core.context_processors.csrf', #necessary for csrf protection
-	   )
-   
-   INSTALLED_APPS = INSTALLED_APPS + (
-	   'django.contrib.sitemaps',
-	   'django.contrib.admin',
-	   'south',
-	   'askbot.deps.livesettings',
-	   'askbot',
-	   #'keyedcache', # TODO: Main askbot tree has this installed, but we get intermittent errors if we include it. 
-	   'robots',
-	   'django_countries',
-	   'djcelery',
-	   'djkombu',
-	   'followit',
-	   )
-   
-   CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
-   ASKBOT_URL = 'discussion/'
-   LOGIN_REDIRECT_URL = '/'
-   LOGIN_URL = '/'
-   
-   ASKBOT_UPLOADED_FILES_URL = '%s%s' % (ASKBOT_URL, 'upfiles/')
-   ALLOW_UNICODE_SLUGS = False
-   ASKBOT_USE_STACKEXCHANGE_URLS = False #mimic url scheme of stackexchange
-   ASKBOT_CSS_DEVEL = True
-   
-   #Celery Settings
-   BROKER_TRANSPORT = "djkombu.transport.DatabaseTransport"
-   CELERY_ALWAYS_EAGER = True
-   
-   import djcelery
-   djcelery.setup_loader()
+    MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+      'askbot.middleware.anon_user.ConnectToSessionMessagesMiddleware',
+      'askbot.middleware.pagesize.QuestionsPageSizeMiddleware',
+      'askbot.middleware.cancel.CancelActionMiddleware',
+      'django.middleware.transaction.TransactionMiddleware',
+      'askbot.middleware.view_log.ViewLogMiddleware',
+      'askbot.middleware.spaceless.SpacelessMiddleware',
+      'askbot.middleware.forum_mode.ForumModeMiddleware',
+      )
+ 
+    FILE_UPLOAD_TEMP_DIR = os.path.join(
+      os.path.dirname(__file__),
+      'tmp'
+      ).replace('\\','/')
+    FILE_UPLOAD_HANDLERS = (
+      'django.core.files.uploadhandler.MemoryFileUploadHandler',
+      'django.core.files.uploadhandler.TemporaryFileUploadHandler',
+      )
+    ASKBOT_ALLOWED_UPLOAD_FILE_TYPES = ('.jpg', '.jpeg', '.gif', '.bmp', '.png', '.tiff')
+    ASKBOT_MAX_UPLOAD_FILE_SIZE = 1024 * 1024 #result in bytes
+    MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'askbot', 'upfiles')
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+    
+    PROJECT_ROOT = os.path.dirname(__file__)
+    
+    TEMPLATE_CONTEXT_PROCESSORS = (
+      'django.core.context_processors.request',
+      'askbot.context.application_settings',
+      #'django.core.context_processors.i18n',
+      'askbot.user_messages.context_processors.user_messages',#must be before auth
+      'django.core.context_processors.auth', #this is required for admin
+      'django.core.context_processors.csrf', #necessary for csrf protection
+      )
+    
+    INSTALLED_APPS = INSTALLED_APPS + (
+      'django.contrib.sitemaps',
+      'django.contrib.admin',
+      'south',
+      'askbot.deps.livesettings',
+      'askbot',
+      #'keyedcache', # TODO: Main askbot tree has this installed, but we get intermittent errors if we include it. 
+      'robots',
+      'django_countries',
+      'djcelery',
+      'djkombu',
+      'followit',
+      )
+    
+    CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+    ASKBOT_URL = 'discussion/'
+    LOGIN_REDIRECT_URL = '/'
+    LOGIN_URL = '/'
+    
+    MEDIA_URL = '/%s%s' % (ASKBOT_URL, 'upfiles/')
+    ALLOW_UNICODE_SLUGS = False
+    ASKBOT_USE_STACKEXCHANGE_URLS = False #mimic url scheme of stackexchange
+    ASKBOT_CSS_DEVEL = True
+    
+    #Celery Settings
+    BROKER_TRANSPORT = "djkombu.transport.DatabaseTransport"
+    CELERY_ALWAYS_EAGER = True
+    
+    import djcelery
+    djcelery.setup_loader()
