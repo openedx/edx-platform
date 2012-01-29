abef62a23a112b4afdb02cfeaacb67e6e09a8c21
Piotr Mitros <pmitros@mit.edu>
2012-01-28 21:27:54 -0500

    Syncing between machines

--- settings_new_askbot.py
+++ settings_new_askbot.py
@@ -107,6 +107,7 @@ INSTALLED_APPS = (
     'track',
     'circuit',
     'perfstats',
+    'util',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
