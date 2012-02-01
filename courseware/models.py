b0451083501f3999c1f64a9d7b8f92f58c234df0
David Ormsbee <dormsbee@mitx.mit.edu>
2012-02-01 12:28:50 -0500

    Add indexes to fields in courseware.StudentModule

--- courseware/models.py
+++ courseware/models.py
@@ -9,8 +9,8 @@ class StudentModule(models.Model):
                     ('html','html'),
                     )
     ## These three are the key for the object
-    module_type = models.CharField(max_length=32, choices=MODULE_TYPES, default='problem')
-    module_id = models.CharField(max_length=255) # Filename for homeworks, etc. 
+    module_type = models.CharField(max_length=32, choices=MODULE_TYPES, default='problem', db_index=True)
+    module_id = models.CharField(max_length=255, db_index=True) # Filename for homeworks, etc. 
     student = models.ForeignKey(User)
     class Meta:
         unique_together = (('student', 'module_id', 'module_type'),)
@@ -19,7 +19,7 @@ class StudentModule(models.Model):
     state = models.TextField(null=True, blank=True)
 
     ## Grade, and are we done? 
-    grade = models.FloatField(null=True, blank=True)
+    grade = models.FloatField(null=True, blank=True, db_index=True)
     #max_grade = models.FloatField(null=True, blank=True)
     
     # DONE_TYPES = (('done','DONE'),          # Finished
@@ -27,8 +27,8 @@ class StudentModule(models.Model):
     #               ('na','NA'))              # Not applicable (e.g. vertical)
     # done = models.CharField(max_length=16, choices=DONE_TYPES)
 
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    modified = models.DateTimeField(auto_now=True, db_index=True)
 
     def __unicode__(self):
         return self.module_type+'/'+self.student.username+"/"+self.module_id+'/'+str(self.state)[:20]
