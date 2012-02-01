5f7d25587f0a94c6907a6932397148e63b98d1fe
Ernie Park <eipark@mit.edu>
2012-02-01 14:05:12 -0500

    open close sidebar implement js adding removing closed class on accordion
    --HG--
    branch : close_sidebar

--- accordion_init.js
+++ accordion_init.js
@@ -1,10 +1,19 @@
 $("#accordion").accordion({
   active: ${ active_chapter },
-  autoHeight: false
+  header: 'h3',
+  autoHeight: false,
+});
+
+$("#open_close_accordion").click(function(){
+  if ($("#accordion").hasClass("closed")){
+    $("#accordion").removeClass("closed");
+  } else {
+    $("#accordion").addClass("closed");
+  }
 });
 
 $('.ui-accordion').bind('accordionchange', function(event, ui) {
    var event_data = {'newheader':ui.newHeader.text(),
-		     'oldheader':ui.oldHeader.text()};
+         'oldheader':ui.oldHeader.text()};
    log_event('accordion', event_data);
 });
