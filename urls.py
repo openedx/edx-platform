2eedce0139bdd261228a1943193c3f66f8fc4f30
David Ormsbee <dormsbee@mitx.mit.edu>
2012-03-21 17:54:58 -0400

    Allow gunicorn_django to serve static files when DEBUG=True

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 import django.contrib.auth.views
 
 # Uncomment the next two lines to enable the admin:
@@ -33,37 +35,40 @@ urlpatterns = ('',
 )
 
 if settings.PERFSTATS:
-   urlpatterns=urlpatterns + (url(r'^reprofile$','perfstats.views.end_profile'),)
+    urlpatterns += (url(r'^reprofile$','perfstats.views.end_profile'),)
 
 if settings.COURSEWARE_ENABLED:
-    urlpatterns=urlpatterns + (url(r'^courseware/$', 'courseware.views.index', name="courseware"),
-    url(r'^wiki/', include('simplewiki.urls')),
-    url(r'^courseware/(?P<course>[^/]*)/(?P<chapter>[^/]*)/(?P<section>[^/]*)/$', 'courseware.views.index', name="courseware_section"),
-    url(r'^courseware/(?P<course>[^/]*)/(?P<chapter>[^/]*)/$', 'courseware.views.index', name="courseware_chapter"),
-    url(r'^courseware/(?P<course>[^/]*)/$', 'courseware.views.index', name="courseware_course"),
-    url(r'^section/(?P<section>[^/]*)/$', 'courseware.views.render_section'),
-    url(r'^modx/(?P<module>[^/]*)/(?P<id>[^/]*)/(?P<dispatch>[^/]*)$', 'courseware.views.modx_dispatch'), #reset_problem'),
-    url(r'^profile$', 'courseware.views.profile'),
-    url(r'^profile/(?P<student_id>[^/]*)/$', 'courseware.views.profile'),
-    url(r'^change_setting$', 'student.views.change_setting'),
-    url(r'^s/(?P<template>[^/]*)$', 'static_template_view.views.auth_index'),
-    url(r'^book/(?P<page>[^/]*)$', 'staticbook.views.index'), 
-    url(r'^book-shifted/(?P<page>[^/]*)$', 'staticbook.views.index_shifted'), 
-    url(r'^book*$', 'staticbook.views.index'), 
-    #    url(r'^course_info/$', 'student.views.courseinfo'),
-    #    url(r'^show_circuit/(?P<circuit>[^/]*)$', 'circuit.views.show_circuit'),
-    url(r'^edit_circuit/(?P<circuit>[^/]*)$', 'circuit.views.edit_circuit'),
-    url(r'^save_circuit/(?P<circuit>[^/]*)$', 'circuit.views.save_circuit'),
-    url(r'^calculate$', 'util.views.calculate'),
-)
+    urlpatterns += (
+        url(r'^courseware/$', 'courseware.views.index', name="courseware"),
+        url(r'^wiki/', include('simplewiki.urls')),
+        url(r'^courseware/(?P<course>[^/]*)/(?P<chapter>[^/]*)/(?P<section>[^/]*)/$', 'courseware.views.index', name="courseware_section"),
+        url(r'^courseware/(?P<course>[^/]*)/(?P<chapter>[^/]*)/$', 'courseware.views.index', name="courseware_chapter"),
+        url(r'^courseware/(?P<course>[^/]*)/$', 'courseware.views.index', name="courseware_course"),
+        url(r'^section/(?P<section>[^/]*)/$', 'courseware.views.render_section'),
+        url(r'^modx/(?P<module>[^/]*)/(?P<id>[^/]*)/(?P<dispatch>[^/]*)$', 'courseware.views.modx_dispatch'), #reset_problem'),
+        url(r'^profile$', 'courseware.views.profile'),
+        url(r'^profile/(?P<student_id>[^/]*)/$', 'courseware.views.profile'),
+        url(r'^change_setting$', 'student.views.change_setting'),
+        url(r'^s/(?P<template>[^/]*)$', 'static_template_view.views.auth_index'),
+        url(r'^book/(?P<page>[^/]*)$', 'staticbook.views.index'), 
+        url(r'^book-shifted/(?P<page>[^/]*)$', 'staticbook.views.index_shifted'), 
+        url(r'^book*$', 'staticbook.views.index'), 
+        #    url(r'^course_info/$', 'student.views.courseinfo'),
+        #    url(r'^show_circuit/(?P<circuit>[^/]*)$', 'circuit.views.show_circuit'),
+        url(r'^edit_circuit/(?P<circuit>[^/]*)$', 'circuit.views.edit_circuit'),
+        url(r'^save_circuit/(?P<circuit>[^/]*)$', 'circuit.views.save_circuit'),
+        url(r'^calculate$', 'util.views.calculate'),
+    )
 
 if settings.ASKBOT_ENABLED:
-   urlpatterns=urlpatterns + (url(r'^%s' % settings.ASKBOT_URL, include('askbot.urls')), \
-                                 url(r'^admin/', include(admin.site.urls)), \
-                                 url(r'^settings/', include('askbot.deps.livesettings.urls')), \
-                                 url(r'^followit/', include('followit.urls')), \
-#                                 url(r'^robots.txt$', include('robots.urls')),
+    urlpatterns += (url(r'^%s' % settings.ASKBOT_URL, include('askbot.urls')), \
+                    url(r'^admin/', include(admin.site.urls)), \
+                    url(r'^settings/', include('askbot.deps.livesettings.urls')), \
+                    url(r'^followit/', include('followit.urls')), \
+#                       url(r'^robots.txt$', include('robots.urls')),
                               )
-
-urlpatterns = patterns(*urlpatterns)
+if settings.DEBUG:
+    urlpatterns = patterns(*urlpatterns) + staticfiles_urlpatterns()
+else:
+    urlpatterns = patterns(*urlpatterns)
 
