6d3d83e3aabd902b9d5a277fea35621613a4cbb6
David Ormsbee <dormsbee@mitx.mit.edu>
2012-03-23 10:55:37 -0400

    Turned on settings and profiling in django_toolbar for dev settings

--- settings2/dev.py
+++ settings2/dev.py
@@ -51,13 +51,13 @@ INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_PANELS = (
    'debug_toolbar.panels.version.VersionDebugPanel',
    'debug_toolbar.panels.timer.TimerDebugPanel',
-#  'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+   'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
    'debug_toolbar.panels.headers.HeaderDebugPanel',
    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
    'debug_toolbar.panels.sql.SQLDebugPanel',
    'debug_toolbar.panels.signals.SignalDebugPanel',
    'debug_toolbar.panels.logger.LoggingPanel',
-#  'debug_toolbar.panels.profiling.ProfilingDebugPanel', # Lots of overhead
+   'debug_toolbar.panels.profiling.ProfilingDebugPanel', # Lots of overhead
 )
 
 ############################ FILE UPLOADS (ASKBOT) #############################
