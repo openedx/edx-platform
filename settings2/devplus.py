14a8030988ef15bd70fccbfb80aed122e1005647
David Ormsbee <dormsbee@mitx.mit.edu>
2012-03-23 12:15:27 -0400

    document assumptions for devplus env

--- settings2/devplus.py
+++ settings2/devplus.py
@@ -3,17 +3,26 @@ This config file tries to mimic the production environment more closely than the
 normal dev.py. It assumes you're running a local instance of MySQL 5.1 and that
 you're running memcached. You'll want to use this to test caching and database
 migrations.
+
+Assumptions:
+* MySQL 5.1 (version important -- askbot breaks on 5.5)
+
+Dir structure:
+/envroot/
+        /mitx # The location of this repo
+        /log  # Where we're going to write log files
+
 """
 from dev import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'wwc',  # Or path to database file if using sqlite3.
-        'USER': 'root', # Not used with sqlite3.
-        'PASSWORD': '', # Not used with sqlite3.
-        'HOST': '127.0.0.1', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wwc',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -26,7 +35,7 @@ CACHES = {
        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
        'LOCATION': '127.0.0.1:11211',
        'KEY_PREFIX' : 'general',
-       'VERSION' : 3,
+       'VERSION' : 5,
    }
 }
 
