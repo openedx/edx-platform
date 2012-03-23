942bfd233dce13dac34c3a9410bd5c5d9710a261
David Ormsbee <dormsbee@mitx.mit.edu>
2012-03-23 12:24:01 -0400

    fixed typo in devplus env

--- settings2/devplus.py
+++ settings2/devplus.py
@@ -30,7 +30,7 @@ CACHES = {
    'default': {
        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
        'LOCATION': '127.0.0.1:11211',
-   }
+   },
    'general': {
        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
        'LOCATION': '127.0.0.1:11211',
