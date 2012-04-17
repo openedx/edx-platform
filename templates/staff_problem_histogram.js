7f069ac2ce790b991f3642726cab9ea2c4101596
Bridger Maxwell <bridgeyman@gmail.com>
2012-04-17 00:40:29 -0400

    Made sure that histogram graph doesn't show up for modules without scores. Modified look of histogram graph.

--- templates/staff_problem_histogram.js
+++ templates/staff_problem_histogram.js
@@ -9,6 +9,7 @@ xticks = []
 yticks = []
 maxx = 1
 maxy = 1.5
+#Here we change the y-axis (count) to be logorithmic. We also define better ticks
 for score, count in histogram:
     score = score or 0 #Sometimes score is None. This fixes that
         
@@ -30,9 +31,9 @@ $.plot($("#histogram_${module_id}"), [{
             align: 'center',
             lineWidth: 0, 
             fill: 1.0 },
-    color: "#600101",
+    color: "#b72121",
   }],
   {
-    xaxis: {min: -1, max: ${maxx}, ticks: ${ json.dumps(xticks) }},
+    xaxis: {min: -1, max: ${maxx}, ticks: ${ json.dumps(xticks) }, tickLength: 0},
     yaxis: {min: 0.0, max: ${maxy}, ticks: ${ json.dumps(yticks) }, labelWidth: 50},
   });
