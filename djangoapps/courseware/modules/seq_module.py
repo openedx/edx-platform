303d9ede0714a19a15a5ead77d9e2c7274b61784
Prem Sichanugrist <psichanugrist@thoughtbot.com>
2012-04-30 16:23:45 -0400

    Rewrite Sequence module to use unobtrusive JS

--- djangoapps/courseware/modules/seq_module.py
+++ djangoapps/courseware/modules/seq_module.py
@@ -28,14 +28,6 @@ class Module(XModule):
         self.render()
         return self.content
 
-    def get_init_js(self):
-        self.render()
-        return self.init_js
-
-    def get_destroy_js(self):
-        self.render()
-        return self.destroy_js
-
     def handle_ajax(self, dispatch, get):
         if dispatch=='goto_position':
             self.position = int(get['position'])
@@ -45,45 +37,40 @@ class Module(XModule):
     def render(self):
         if self.rendered:
             return
-        def j(m): 
+        def j(m):
             ''' jsonify contents so it can be embedded in a js array
             We also need to split </script> tags so they don't break
             mid-string'''
-            if 'init_js' not in m: m['init_js']=""
-            if 'type' not in m: m['init_js']=""
-            content=json.dumps(m['content']) 
-            content=content.replace('</script>', '<"+"/script>') 
+            content=json.dumps(m['content'])
+            content=content.replace('</script>', '<"+"/script>')
 
-            return {'content':content, 
-                    "destroy_js":m['destroy_js'], 
-                    'init_js':m['init_js'], 
+            return {'content':content,
                     'type': m['type']}
 
 
         ## Returns a set of all types of all sub-children
         child_classes = [set([i.tag for i in e.iter()]) for e in self.xmltree]
 
-        self.titles = json.dumps(["\n".join([i.get("name").strip() for i in e.iter() if i.get("name") != None]) \
-                           for e in self.xmltree])
+        titles = ["\n".join([i.get("name").strip() for i in e.iter() if i.get("name") != None]) \
+                           for e in self.xmltree]
 
         self.contents = [j(self.render_function(e)) \
                              for e in self.xmltree]
 
-        print self.titles
+        for i in range(len(self.contents)):
+          self.contents[i]['title'] = titles[i]
 
         for (content, element_class) in zip(self.contents, child_classes):
             new_class = 'other'
             for c in class_priority:
-                if c in element_class: 
+                if c in element_class:
                     new_class = c
             content['type'] = new_class
-     
-        js=""
 
         params={'items':self.contents,
                 'id':self.item_id,
                 'position': self.position,
-                'titles':self.titles, 
+                'titles':titles,
                 'tag':self.xmltree.tag}
 
         # TODO/BUG: Destroy JavaScript should only be called for the active view
@@ -94,16 +81,11 @@ class Module(XModule):
         destroy_js="".join([e['destroy_js'] for e in self.contents if 'destroy_js' in e])
 
         if self.xmltree.tag in ['sequential', 'videosequence']:
-            self.init_js=js+render_to_string('seq_module.js',params)
-            self.destroy_js=destroy_js
             self.content=render_to_string('seq_module.html',params)
         if self.xmltree.tag == 'tab':
             params['id'] = 'tab'
-            self.init_js=js+render_to_string('tab_module.js',params)
-            self.destroy_js=destroy_js
             self.content=render_to_string('tab_module.html',params)
         self.rendered = True
-        
 
     def __init__(self, system, xml, item_id, state=None):
         XModule.__init__(self, system, xml, item_id, state)
---
+++ templates/coffee/src/modules/sequence.coffee
@@ -0,0 +1,70 @@
+class window.Sequence
+  constructor: (@id, @elements, @tag, position) ->
+    @buildNavigation()
+    @bind()
+    @render position
+
+  bind: ->
+    $('#sequence-list a').click @goto
+
+  buildNavigation: ->
+    $.each @elements, (index, item) ->
+      link = $('<a>').attr class: "seq_#{item.type}_inactive", 'data-element': index + 1
+      title = $('<p>').html(item.title)
+      list_item = $('<li>').append(link.append(title))
+      $('#sequence-list').append list_item
+
+  toggleArrows: ->
+    $('.sequence-nav-buttons a').unbind('click')
+
+    if @position == 1
+      $('.sequence-nav-buttons .prev a').addClass('disabled')
+    else
+      $('.sequence-nav-buttons .prev a').removeClass('disabled').click(@previous)
+
+    if @position == @elements.length
+      $('.sequence-nav-buttons .next a').addClass('disabled')
+    else
+      $('.sequence-nav-buttons .next a').removeClass('disabled').click(@next)
+
+  render: (new_position) ->
+    if @position != undefined
+      @mark_visited @position
+      $.post "/modx/#{@tag}/#{@id}/goto_position", position: new_position
+
+    if @position != new_position
+      @mark_active new_position
+      $('#seq_content').html eval(@elements[new_position - 1].content)
+
+      MathJax.Hub.Queue(["Typeset",MathJax.Hub])
+      @position = new_position
+      @toggleArrows()
+
+  goto: (e) =>
+    e.preventDefault()
+    new_position = $(e.srcElement).data('element')
+    log_event("seq_goto", old: @position, new: new_position, id: @id)
+    @render new_position
+
+  next: (e) =>
+    e.preventDefault()
+    new_position = @position + 1
+    log_event("seq_next", old: @position, new: new_position, id: @id)
+    @render new_position
+
+  previous: (e) =>
+    e.preventDefault()
+    new_position = @position - 1
+    log_event("seq_prev", old: @position, new: new_position, id: @id)
+    @render new_position
+
+  link_for: (position) ->
+    $("#sequence-list a[data-element=#{position}]")
+
+  mark_visited: (position) ->
+    @link_for(position).attr class: "seq_#{@elements[position - 1].type}_visited"
+
+  mark_active: (position) ->
+    @link_for(position).attr class: "seq_#{@elements[position - 1].type}_active"
+
+
