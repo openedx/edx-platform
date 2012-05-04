2b7b9ada60a417f2158c2fad3f427cb9673ba48c
Calen Pennington <calen.pennington@gmail.com>
2012-05-04 10:20:25 -0400

    Make local file uploads work in dev environment

--- envs/dev.py
+++ envs/dev.py
@@ -73,7 +73,8 @@ DEBUG_TOOLBAR_PANELS = (
 ############################ FILE UPLOADS (ASKBOT) #############################
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 MEDIA_ROOT = ENV_ROOT / "uploads"
-MEDIA_URL = "/discussion/upfiles/"
+MEDIA_URL = "/static/uploads/"
+STATICFILES_DIRS.append(("uploads", MEDIA_ROOT))
 FILE_UPLOAD_TEMP_DIR = ENV_ROOT / "uploads"
 FILE_UPLOAD_HANDLERS = (
     'django.core.files.uploadhandler.MemoryFileUploadHandler',
