c214d0dc9960a8fea1769f950bf12dea16a55aa7
Bridger Maxwell <bridgeyman@gmail.com>
2012-05-17 15:47:09 -0400

    Added UUID generation to certificates. Now, each student/course will have a unique ID for verifying the certificate.

--- djangoapps/certificates/models.py
+++ djangoapps/certificates/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+'''
+When a certificate is generated, a unique ID is generated so that 
+the certificate can be verified later. The ID is a UUID4, so that
+it can't be easily guessed and so that it is unique. Even though
+we save these generated certificates (for later verification), we
+also record the UUID so that if we regenerate the certificate it
+will have the same UUID.
+
+Certificates are created for a student and an offering of a course.
+
+'''
+
+class GeneratedCertificate(models.Model):
+    user = models.ForeignKey(User, db_index=True)
+    certificate_id = models.CharField(max_length=32)
+    
+    # TODO: Maybe we store the grade when the certificate was
+    # generated, so we can tell the user if their grade improves?
+    # Can a grade improve?
--- djangoapps/certificates/views.py
+++ djangoapps/certificates/views.py
@@ -1,10 +1,17 @@
 import json
+import logging
+import uuid
 
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponse
 from mitxmako.shortcuts import render_to_response
 
 import courseware.grades as grades
+from certificates.models import GeneratedCertificate
+
+
+log = logging.getLogger("mitx.certificates")
+
 
 @login_required
 def certificate_request(request):
@@ -33,9 +40,27 @@ def certificate_request(request):
             error += 'You have not earned a grade in this course. '
             
     if len(error) == 0:
+        generate_certificate(request.user, grade, destination_email)
+        
         # TODO: Send the certificate email
         return HttpResponse(json.dumps({'success':True,
                                         'value': 'A certificate is being generated and will be sent. ' }))
     else:
         return HttpResponse(json.dumps({'success':False,
                                         'error': error }))
+
+
+# TODO: This method should be executed as an asynchronous task
+def generate_certificate(user, grade, destination_email):
+    generated_certificate = None
+    
+    try:
+        generated_certificate = GeneratedCertificate.objects.get(user = user)
+    except GeneratedCertificate.DoesNotExist:
+        generated_certificate = GeneratedCertificate(user = user, certificate_id = uuid.uuid4().hex)
+        generated_certificate.save()
+        
+    certificate_id = generated_certificate.certificate_id
+    
+    log.debug("Generating certificate for " + str(user.username) + " with ID: " + certificate_id)
+    
