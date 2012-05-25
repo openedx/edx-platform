34a5b8b6cd68b88c30a1978eff0760a370ae34ab
Calen Pennington <calen.pennington@gmail.com>
2012-05-25 10:52:04 -0400

    Url encode memcache keys, rather than writing a new encoding scheme. Also use fasthash, rather than md5

--- lib/util/memcache.py
+++ lib/util/memcache.py
@@ -2,17 +2,19 @@
 This module provides a KEY_FUNCTION suitable for use with a memcache backend
 so that we can cache any keys, not just ones that memcache would ordinarily accept
 """
-from django.utils.hashcompat import md5_constructor
 from django.utils.encoding import smart_str
-import string
+import hashlib
+import urllib
+
+def fasthash(string):
+    m = hashlib.new("md4")
+    m.update(string)
+    return "id"+m.hexdigest()
 
 def safe_key(key, key_prefix, version):
-    safe_key = smart_str(key)
-    for char in safe_key:
-        if ord(char) < 33 or ord(char) == 127:
-            safe_key = safe_key.replace(char, '_')
+    safe_key = urllib.quote_plus(smart_str(key))
 
     if len(safe_key) > 250:
-        safe_key = md5_constructor(safe_key)
+        safe_key = fasthash(safe_key)
 
     return ":".join([key_prefix, str(version), safe_key])
