9a6919351723b3d14b17aebc3d7042222a82bc35
Bridger Maxwell <bridgeyman@gmail.com>
2012-06-01 10:50:16 -0400

    Changed certificate request form wording. Removed email address field.

--- djangoapps/certificates/views.py
+++ djangoapps/certificates/views.py
@@ -20,16 +20,22 @@ def certificate_request(request):
     if request.method != "POST" or not settings.END_COURSE_ENABLED:
         raise Http404
     
-    verification_checked = request.POST.get('cert_request_verify', 'false')
-    destination_email = request.POST.get('cert_request_email', '')
+    honor_code_verify = request.POST.get('cert_request_honor_code_verify', 'false')
+    name_verify = request.POST.get('cert_request_name_verify', 'false')
+    id_verify = request.POST.get('cert_request_id_verify', 'false')
     error = ''
     
-    if verification_checked != 'true':
-        error += 'You must verify that you have followed the honor code to receive a certificate. '
-    try:
-        validate_email(destination_email)
-    except ValidationError:
-        error += 'Please provide a valid email address to send the certificate. '
+    
+    if honor_code_verify != 'true':
+        error += 'Please verify that you have followed the honor code to receive a certificate. '
+    
+    if name_verify != 'true':
+        error += 'Please verify that your name is correct to receive a certificate. '
+    
+    if id_verify != 'true':
+        error += 'Please certify that you understand the unique ID on the certificate. '
+        
+        
     grade = None
     if len(error) == 0:
         student_gradesheet = grades.grade_sheet(request.user)
@@ -40,18 +46,17 @@ def certificate_request(request):
             error += 'You have not earned a grade in this course. '
             
     if len(error) == 0:
-        generate_certificate(request.user, grade, destination_email)
+        generate_certificate(request.user, grade)
         
         # TODO: Send the certificate email
-        return HttpResponse(json.dumps({'success':True,
-                                        'value': 'A certificate is being generated and will be sent. ' }))
+        return HttpResponse(json.dumps({'success':True}))
     else:
         return HttpResponse(json.dumps({'success':False,
                                         'error': error }))
 
 
 # This method should only be called if the user has a grade and has requested a certificate
-def generate_certificate(user, grade, destination_email):
+def generate_certificate(user, grade):
     # Make sure to see the comments in models.GeneratedCertificate to read about the valid
     # states for a GeneratedCertificate object
     generated_certificate = None
