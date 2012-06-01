6964fbd65564c98afb0f92867801c6e29504b162
ichuang <ichuang@gmail.com>
2012-06-01 14:15:48 -0400

    untabify lib/dogfood/check.py

--- lib/dogfood/check.py
+++ lib/dogfood/check.py
@@ -21,7 +21,7 @@ def check_problem_code(ans,the_lcp,correct_answers,false_answers):
     returns dict {'ok':is_ok,'msg': message with iframe}
     """
     pfn = "dog%s" % randomid
-    pfn += the_lcp.problem_id.replace('filename','')	# add problem ID to dogfood problem name
+    pfn += the_lcp.problem_id.replace('filename','')    # add problem ID to dogfood problem name
     update_problem(pfn,ans,filestore=the_lcp.system.filestore)
     msg = '<hr width="100%"/>'
     msg += '<iframe src="%s/dogfood/filename%s" width="95%%" frameborder="1">No iframe support!</iframe>' % (settings.MITX_ROOT_URL,pfn)
@@ -31,16 +31,16 @@ def check_problem_code(ans,the_lcp,correct_answers,false_answers):
     try:
         # check correctness
         fp = the_lcp.system.filestore.open('problems/%s.xml' % pfn)
-	test_lcp = lcp.LoncapaProblem(fp, '1', system=the_lcp.system)
+        test_lcp = lcp.LoncapaProblem(fp, '1', system=the_lcp.system)
     
-	if not (test_lcp.grade_answers(correct_answers)['1_2_1']=='correct'):
+        if not (test_lcp.grade_answers(correct_answers)['1_2_1']=='correct'):
             is_ok = False
-	if (test_lcp.grade_answers(false_answers)['1_2_1']=='correct'):
+        if (test_lcp.grade_answers(false_answers)['1_2_1']=='correct'):
             is_ok = False
     except Exception,err:
         is_ok = False
         msg += "<p>Error: %s</p>" % str(err).replace('<','&#60;')
-	msg += "<p><pre>%s</pre></p>" % traceback.format_exc().replace('<','&#60;')
+        msg += "<p><pre>%s</pre></p>" % traceback.format_exc().replace('<','&#60;')
         
     ret = {'ok':is_ok,
            'msg': msg,
