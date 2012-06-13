490d35b888c244ea9b81c69574a8143be2b61176
Calen Pennington <calen.pennington@gmail.com>
2012-06-13 11:54:17 -0400

    Add templates directly from cms_proto/ui_prototype

---
+++ cms/static/sass/bourbon/lib/bourbon.rb
@@ -0,0 +1,19 @@
+require "bourbon/generator"
+
+module Bourbon
+  if defined?(Rails)
+    class Engine < ::Rails::Engine
+      require 'bourbon/engine'
+    end
+
+    module Rails
+      class Railtie < ::Rails::Railtie
+        rake_tasks do
+          load "tasks/install.rake"
+        end
+      end
+    end
+  end
+end
+
+require File.join(File.dirname(__FILE__), "/bourbon/sass_extensions")
