dc2daefb39a3083faf4ad238fbf6f3d7bfb87aa3
John Jarvis <jrjarvis@mitx.mit.edu>
2012-06-15 15:00:23 -0400

    moved find_ungenerated to the certificates app, added help text

--- djangoapps/certificates/management/commands/find_ungenerated.py
+++ djangoapps/certificates/management/commands/find_ungenerated.py
@@ -2,6 +2,16 @@ from django.utils.simplejson import dumps
 from django.core.management.base import BaseCommand, CommandError
 from certificates.models import GeneratedCertificate
 class Command(BaseCommand):
+
+    help =  """
+    This command finds all GeneratedCertificate objects that do not have a 
+    certificate generated. These come into being when a user requests a 
+    certificate, or when grade_all_students is called (for pre-generating 
+    certificates).
+    
+    It returns a json formatted list of users and their user ids
+    """
+
     def handle(self, *args, **options):
         users = GeneratedCertificate.objects.filter(
                 download_url = None )
