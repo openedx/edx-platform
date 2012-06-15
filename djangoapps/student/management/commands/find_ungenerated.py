84b8d4be3114ff47af228839c2cc8bf05883ca85
John Jarvis <jrjarvis@mitx.mit.edu>
2012-06-15 09:56:37 -0400

    switching to dot notation for GeneratedCertificate objects

--- djangoapps/student/management/commands/find_ungenerated.py
+++ djangoapps/student/management/commands/find_ungenerated.py
@@ -4,7 +4,7 @@ from certificates.models import GeneratedCertificate
 class Command(BaseCommand):
     def handle(self, *args, **options):
         users = GeneratedCertificate.objects.filter(
-                download_url = None ).values()
-        user_output = [{'user_id':user['user_id'], 'name':user['name']}
+                download_url = None )
+        user_output = [{'user_id':user.user_id, 'name':user.name}
                 for user in users]
         self.stdout.write(dumps(user_output) + "\n")
