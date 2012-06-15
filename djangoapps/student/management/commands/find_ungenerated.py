fffdc741af43eeb9b32e0151e4f6bb91f7655b02
John Jarvis <jrjarvis@mitx.mit.edu>
2012-06-15 09:25:05 -0400

    variable cleanup, fix stdout problem and simplejson

--- djangoapps/student/management/commands/find_ungenerated.py
+++ djangoapps/student/management/commands/find_ungenerated.py
@@ -1,10 +1,10 @@
-import sys
-
+from django.utils.simplejson import dumps
 from django.core.management.base import BaseCommand, CommandError
 from certificates.models import GeneratedCertificate
-import simplejson
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        l = GeneratedCertificate.objects.filter( download_url = None ).values()
-        sl = [{'user_id':x['user_id'], 'name':x['name']} for x in l]
-        sys.stdout.write(simplejson.dumps(sl) + "\n")
+        users = GeneratedCertificate.objects.filter(
+                download_url = None ).values()
+        user_output = [{'user_id':user['user_id'], 'name':user['name']}
+                for user in users]
+        self.stdout.write(dumps(user_output) + "\n")
