bea7935d740b87b3fec3c719c0c4a34c9e708f8e
Calen Pennington <calen.pennington@gmail.com>
2012-06-18 13:41:53 -0400

    Delay initializition of the MongoKeyStore until required

--- cms/djangoapps/contentstore/views.py
+++ cms/djangoapps/contentstore/views.py
@@ -7,6 +7,6 @@ def index(request):
     # FIXME (cpennington): These need to be read in from the active user
     org = 'mit.edu'
     course = '6002xs12'
-    course = keystore.get_item(['i4x', org, course, 'Course', None])
+    course = keystore().get_item(['i4x', org, course, 'Course', None])
     weeks = course.get_children()
     return render_to_response('index.html', {'weeks': weeks})
