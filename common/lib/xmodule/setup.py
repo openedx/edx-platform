da71515bfad4a1b6dd1eeaa221533467439a2184
Calen Pennington <calen.pennington@gmail.com>
2012-06-18 11:09:11 -0400

    Add categories and XModuleDescriptors for all module types that are used at the top level of a course

--- common/lib/xmodule/setup.py
+++ common/lib/xmodule/setup.py
@@ -7,8 +7,15 @@ setup(
     install_requires=['distribute'],
     entry_points={
         'xmodule.v1': [
-            "Course = seq_module:CourseModuleDescriptor",
-            "Chapter = seq_module:ChapterModuleDescriptor",
+            "Course = seq_module:SectionDescriptor",
+            "Week = seq_module:WeekDescriptor",
+            "Section = seq_module:SectionDescriptor",
+            "LectureSequence = seq_module:SectionDescriptor",
+            "Lab = seq_module:SectionDescriptor",
+            "Homework = seq_module:SectionDescriptor",
+            "TutorialIndex = seq_module:SectionDescriptor",
+            "Exam = seq_module:SectionDescriptor",
+            "VideoSegment = video_module:VideoSegmentDescriptor",
         ]
     }
 )
