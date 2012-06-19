3d69f443efd919f8b0e06d37be6c108796294cd0
Calen Pennington <calen.pennington@gmail.com>
2012-06-19 11:27:56 -0400

    Remove unused urls

--- cms/urls.py
+++ cms/urls.py
@@ -5,7 +5,5 @@ from django.conf.urls.defaults import patterns, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^(?P<org>[^/]+)/(?P<course>[^/]+)/calendar/', 'contentstore.views.calendar', name='calendar'),
-    url(r'^accounts/login/', 'instructor.views.do_login', name='login'),
     url(r'^$', 'contentstore.views.index', name='index'),
 )
