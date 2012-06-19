519d99a453e2ae9c9258f957e21ae9a21ba6f55b
Calen Pennington <calen.pennington@gmail.com>
2012-06-19 11:25:29 -0400

    Search for course specifically in navigation view

--- cms/djangoapps/contentstore/views.py
+++ cms/djangoapps/contentstore/views.py
@@ -7,6 +7,7 @@ def index(request):
     # TODO (cpennington): These need to be read in from the active user
     org = 'mit.edu'
     course = '6002xs12'
-    course = keystore().get_item(['i4x', org, course, 'Course', None])
+    name = '6.002 Spring 2012'
+    course = keystore().get_item(['i4x', org, course, 'Course', name])
     weeks = course.get_children()
     return render_to_response('index.html', {'weeks': weeks})
