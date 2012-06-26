59d5d146601f40cb8a387628ea96b179befe7036
John Jarvis <jrjarvis@mitx.mit.edu>
2012-06-26 19:34:53 -0400

    adding edx4edx_aws for edx4edx aws instance

---
+++ lms/envs/edx4edx_aws.py
@@ -0,0 +1,22 @@
+# Settings for edx4edx production instance
+from .aws import *
+
+COURSE_NAME = "edx4edx"
+COURSE_NUMBER = "edX.01"
+COURSE_TITLE = "edx4edx: edX Author Course"
+
+### Dark code. Should be enabled in local settings for devel. 
+
+ENABLE_MULTICOURSE = True # set to False to disable multicourse display (see lib.util.views.mitxhome)
+QUICKEDIT = False
+
+###
+
+COURSE_DEFAULT = 'edx4edx'
+COURSE_SETTINGS =  {'edx4edx': {'number' : 'edX.01',
+                                    'title'  : 'edx4edx: edX Author Course',
+                                    'xmlpath': '/edx4edx/',
+                                    'github_url': 'https://github.com/MITx/edx4edx',
+                                    'active' : True,
+                                    },
+                    }
