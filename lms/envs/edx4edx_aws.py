071bb70dbaab2bef234d4cd72963ae27c9c2d6b6
John Jarvis <jrjarvis@mitx.mit.edu>
2012-06-28 09:10:48 -0400

    settings YUI_COMPRESSOR to None for js

--- lms/envs/edx4edx_aws.py
+++ lms/envs/edx4edx_aws.py
@@ -5,8 +5,9 @@ COURSE_NAME = "edx4edx"
 COURSE_NUMBER = "edX.01"
 COURSE_TITLE = "edx4edx: edX Author Course"
 
+PIPELINE_CSS_COMPRESSOR = None
+PIPELINE_JS_COMPRESSOR = None
 ### Dark code. Should be enabled in local settings for devel. 
-
 ENABLE_MULTICOURSE = True # set to False to disable multicourse display (see lib.util.views.mitxhome)
 QUICKEDIT = True
 
