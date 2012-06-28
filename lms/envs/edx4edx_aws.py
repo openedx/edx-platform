5c398b25dabb806e78398a131a506777d7f28c9d
John Jarvis <jrjarvis@mitx.mit.edu>
2012-06-28 11:15:03 -0400

    updating edx4edx_aws config

--- lms/envs/edx4edx_aws.py
+++ lms/envs/edx4edx_aws.py
@@ -1,16 +1,16 @@
 # Settings for edx4edx production instance
 from .aws import *
-
 COURSE_NAME = "edx4edx"
 COURSE_NUMBER = "edX.01"
 COURSE_TITLE = "edx4edx: edX Author Course"
+EDX4EDX_ROOT = ENV_ROOT / "data/edx4edx"
 
 ### Dark code. Should be enabled in local settings for devel. 
-
-ENABLE_MULTICOURSE = True # set to False to disable multicourse display (see lib.util.views.mitxhome)
 QUICKEDIT = True
-
+ENABLE_MULTICOURSE = True # set to False to disable multicourse display (see lib.util.views.mitxhome)
 ###
+PIPELINE_CSS_COMPRESSOR = None
+PIPELINE_JS_COMPRESSOR = None
 
 COURSE_DEFAULT = 'edx4edx'
 COURSE_SETTINGS =  {'edx4edx': {'number' : 'edX.01',
@@ -20,3 +20,17 @@ COURSE_SETTINGS =  {'edx4edx': {'number' : 'edX.01',
                                     'active' : True,
                                     },
                     }
+
+STATICFILES_DIRS = [
+    PROJECT_ROOT / "static",
+    ASKBOT_ROOT / "askbot" / "skins",
+    ("edx4edx", EDX4EDX_ROOT / "html"),
+    ("circuits", DATA_DIR / "images"),
+    ("handouts", DATA_DIR / "handouts"),
+    ("subs", DATA_DIR / "subs"),
+
+# This is how you would use the textbook images locally
+#    ("book", ENV_ROOT / "book_images")
+]
+
+MAKO_TEMPLATES['course'] = [DATA_DIR, EDX4EDX_ROOT ]
