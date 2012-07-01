23128ced67c5f38389dbedc6285d18d135836613
ichuang <ichuang@gmail.com>
2012-07-01 12:51:14 -0400

    courseware views now goes to default chapter and section if unprovided   - uses multicourse settings (default_chapter and default_section)   - it would be nice to use course_settings instead, but that is not     currently compatible with multicourse

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -165,7 +165,8 @@ def get_course(request, course):
         if not settings.ENABLE_MULTICOURSE:
             course = "edx4edx"
         elif 'coursename' in request.session:
-            course = request.session['coursename']
+            # use multicourse_settings, so that settings.COURSE_TITLE is set properly
+            course = multicourse_settings.get_coursename_from_request(request)
         else:
             course = settings.COURSE_DEFAULT
     return course
@@ -286,6 +287,17 @@ def index(request, course=None, chapter=None, section=None,
 
     # keep track of current course being viewed in django's request.session
     request.session['coursename'] = course
+
+    # get default chapter & section from multicourse settings, if not provided
+    if chapter is None:
+        defchapter = multicourse_settings.get_course_default_chapter(course)
+        defsection = multicourse_settings.get_course_default_section(course)
+        if defchapter and defsection:
+            # jump there using redirect, so the user gets the right URL in their browser
+            return redirect('%s/courseware/%s/%s/%s/' % (settings.MITX_ROOT_URL,
+                                                         get_course(request, course),
+                                                         defchapter,
+                                                         defsection))
 
     chapter = clean(chapter)
     section = clean(section)
