a0f550396cd94ce9cc014ef09a14cd4b3003597d
Calen Pennington <calen.pennington@gmail.com>
2012-07-02 13:19:27 -0400

    Make xmodule fit the typical python installation setup

--- common/lib/xmodule/setup.py
+++ common/lib/xmodule/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="XModule",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     install_requires=['distribute'],
     package_data={
         '': ['js/*']
