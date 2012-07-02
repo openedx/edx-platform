4359a2544e3e6d0a302f4cfb08d9f2f89f157800
Galen Frechette <galen@thoughtbot.com>
2012-07-02 11:27:42 -0400

    wip course info/profile page

--- lms/static/js/toggle_login_modal.js
+++ lms/static/js/toggle_login_modal.js
@@ -16,5 +16,14 @@ $(document).ready(function () {
     $('.modal.signup-modal').removeClass("visible");
     $('.modal-overlay').removeClass("visible");
   });
+
+  $('.hero').click(function() {
+    $('.modal.video-modal').addClass("visible");
+    $('.modal-overlay').addClass("visible");
+  });
+  $('div.close-modal').click(function() {
+    $('.modal.video-modal').removeClass("visible");
+    $('.modal-overlay').removeClass("visible");
+  });
 });
 
