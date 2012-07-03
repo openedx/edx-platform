9b134aadf4b1f8878eb80df2a88b1abeefd6302e
Calen Pennington <calen.pennington@gmail.com>
2012-07-03 16:04:18 -0400

    Swallow attempts to create a new item where one already exists

--- cms/djangoapps/contentstore/__init__.py
+++ cms/djangoapps/contentstore/__init__.py
@@ -1,5 +1,8 @@
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.xml import XMLModuleStore
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def import_from_xml(org, course, data_dir):
@@ -9,7 +12,14 @@ def import_from_xml(org, course, data_dir):
     """
     module_store = XMLModuleStore(org, course, data_dir, 'xmodule.raw_module.RawDescriptor', eager=True)
     for module in module_store.modules.itervalues():
-        modulestore().create_item(module.location)
+
+        # TODO (cpennington): This forces import to overrite the same items.
+        # This should in the future create new revisions of the items on import
+        try:
+            modulestore().create_item(module.location)
+        except:
+            log.exception('Item already exists at %s' % module.location.url())
+            pass
         if 'data' in module.definition:
             modulestore().update_item(module.location, module.definition['data'])
         if 'children' in module.definition:
