56d873097ce1b7bd68e323b89304da4687e91eff
David Ormsbee <dave@mitx.mit.edu>
2012-07-10 14:12:32 -0400

    add pyparsing to requirements for capa lib

--- common/lib/capa/setup.py
+++ common/lib/capa/setup.py
@@ -4,5 +4,5 @@ setup(
     name="capa",
     version="0.1",
     packages=find_packages(exclude=["tests"]),
-    install_requires=['distribute'],
+    install_requires=['distribute', 'pyparsing'],
 )
