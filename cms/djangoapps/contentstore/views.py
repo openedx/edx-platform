c176a7fe925f390148ad1b925033e091e1b33745
Victor Shnayder <victor@mitx.mit.edu>
2012-07-15 14:01:01 -0400

    Add account creation and login to CMS
    * connect up views from student app
    * Add initial @login_required decorators on the protected views
    * Add tests for some of the basic functionality

--- cms/djangoapps/contentstore/views.py
+++ cms/djangoapps/contentstore/views.py
@@ -2,6 +2,7 @@ from util.json_request import expect_json
 import json
 
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from django_future.csrf import ensure_csrf_cookie
 from django.core.urlresolvers import reverse
@@ -13,7 +14,7 @@ from github_sync import export_to_github
 from mitxmako.shortcuts import render_to_response
 from xmodule.modulestore.django import modulestore
 
-
+@login_required
 @ensure_csrf_cookie
 def index(request):
     courses = modulestore().get_items(['i4x', None, None, 'course', None])
@@ -36,13 +37,23 @@ def signup(request):
     return render_to_response('signup.html', {'csrf': csrf_token }) 
 
 @ensure_csrf_cookie
+def login_page(request):
+    """
+    Display the login form.
+    """
+    csrf_token = csrf(request)['csrf_token']
+    return render_to_response('login.html', {'csrf': csrf_token }) 
+    
+
+@login_required
+@ensure_csrf_cookie
 def course_index(request, org, course, name):
     # TODO (cpennington): These need to be read in from the active user
     course = modulestore().get_item(['i4x', org, course, 'course', name])
     weeks = course.get_children()
     return render_to_response('course_index.html', {'weeks': weeks})
 
-
+@login_required
 def edit_item(request):
     item_id = request.GET['id']
     item = modulestore().get_item(item_id)
@@ -54,6 +65,7 @@ def edit_item(request):
     })
 
 
+@login_required
 @expect_json
 def save_item(request):
     item_id = request.POST['id']
