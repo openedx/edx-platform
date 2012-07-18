77512cb6ebe48e73278fdff2293a71a806d01b04
Victor Shnayder <victor@mitx.mit.edu>
2012-07-18 16:14:34 -0400

    add options to dev_mongo env

--- lms/envs/dev_mongo.py
+++ lms/envs/dev_mongo.py
@@ -3,6 +3,8 @@ This config file runs the dev environment, but with mongo as the datastore
 """
 from .dev import *
 
+GITHUB_REPO_ROOT = ENV_ROOT / "data"
+
 MODULESTORE = {
     'default': {
         'ENGINE': 'xmodule.modulestore.mongo.MongoModuleStore',
@@ -11,6 +13,7 @@ MODULESTORE = {
             'host': 'localhost',
             'db': 'xmodule',
             'collection': 'modulestore',
+            'fs_root': GITHUB_REPO_ROOT,
         }
     }
 }
