124a26d2d466951b310d264f3600e8800d3a1a69
Calen Pennington <calen.pennington@gmail.com>
2012-07-20 11:21:34 -0400

    Eagerly load all modules when reading the course from xml, so that users who are already on a problem page can check their answers

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -144,6 +144,7 @@ MODULESTORE = {
         'OPTIONS': {
             'data_dir': DATA_DIR,
             'default_class': 'xmodule.hidden_module.HiddenDescriptor',
+            'eager': True,
         }
     }
 }
