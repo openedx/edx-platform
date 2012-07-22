29361ec02cb9d60c444e52defe121cc66d72e79f
Rocky Duan <dementrock@gmail.com>
2012-07-22 19:05:32 -0400

    forum view for discussions

--- common/lib/xmodule/xmodule/discussion_module.py
+++ common/lib/xmodule/xmodule/discussion_module.py
@@ -16,13 +16,14 @@ class DiscussionModule(XModule):
             'parse': dateutil.parser.parse,
             'commentable_id': self.discussion_id,
         }
-        return self.system.render_template('discussion.html', context)
+        return self.system.render_template('discussion/inline.html', context)
 
     def __init__(self, system, location, definition, instance_state=None, shared_state=None, **kwargs):
         XModule.__init__(self, system, location, definition, instance_state, shared_state, **kwargs)
         xml_data = etree.fromstring(definition['data'])
         self.discussion_id = xml_data.attrib['id']
         self.title = xml_data.attrib['for']
+        self.category = xml_data.attrib['category']
 
 class DiscussionDescriptor(RawDescriptor):
     module_class = DiscussionModule
