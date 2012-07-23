214416a6be575681b898d773e6674f18896f60cb
Rocky Duan <dementrock@gmail.com>
2012-07-23 16:20:45 -0400

    moved django_comment_client into edx

---
+++ lms/djangoapps/django_comment_client/forum/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import url, patterns
+import django_comment_client.forum.views
+
+urlpatterns = patterns('django_comment_client.forum.views',
+    url(r'search$', 'search', name='search'),
+    url(r'threads/(?P<thread_id>\w+)$', 'single_thread', name='single_thread'),
+    url(r'(?P<course_id>[\w\.\-]+)/(?P<discussion_id>\w+)$', 'forum_form_discussion', name='forum_form_discussion'),
+)
