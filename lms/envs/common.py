66f79251dc2ee01a483b4c1d970f2c23552401bd
Calen Pennington <calen.pennington@gmail.com>
2012-07-24 13:22:50 -0400

    Add feature flag for disabling course start dates

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -41,6 +41,7 @@ MITX_FEATURES = {
     'DISPLAY_HISTOGRAMS_TO_STAFF' : True,
     'REROUTE_ACTIVATION_EMAIL' : False,		# nonempty string = address for all activation emails 
     'DEBUG_LEVEL' : 0,				# 0 = lowest level, least verbose, 255 = max level, most verbose
+    'DISABLE_START_DATES': False,  # When True, all courses will be active, regardless of start date
 }
 
 # Used for A/B testing
