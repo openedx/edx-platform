0431b54cd2412e67c74a94d9716929532fab8fab
David Ormsbee <dave@mitx.mit.edu>
2012-07-23 23:54:35 -0400

    Use settings.SITE_NAME instead of hardcoded edx.org

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -403,7 +403,7 @@ def password_reset(request):
         form.save( use_https = request.is_secure(),
                    from_email = settings.DEFAULT_FROM_EMAIL,
                    request = request,
-                   domain_override = "edx.org" )
+                   domain_override = settings.SITE_NAME )
         return HttpResponse(json.dumps({'success':True,
                                         'value': render_to_string('registration/password_reset_done.html', {})}))
     else:
