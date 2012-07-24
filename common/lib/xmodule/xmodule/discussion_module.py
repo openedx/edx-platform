e6d437b66edf3a990f31bf4e944c55df67feae91
Rocky Duan <dementrock@gmail.com>
2012-07-24 16:49:53 -0400

    get user_id in discussion module

--- common/lib/xmodule/xmodule/discussion_module.py
+++ common/lib/xmodule/xmodule/discussion_module.py
@@ -8,6 +8,8 @@ import dateutil
 from dateutil.tz import tzlocal
 from datehelper import time_ago_in_words
 
+import json
+
 class DiscussionModule(XModule):
     def get_html(self):
         context = {
@@ -16,15 +18,20 @@ class DiscussionModule(XModule):
             'parse': dateutil.parser.parse,
             'discussion_id': self.discussion_id,
             'search_bar': '',
+            'user_info': comment_client.get_user_info(self.user_id, raw=True),
         }
         return self.system.render_template('discussion/inline.html', context)
 
     def __init__(self, system, location, definition, instance_state=None, shared_state=None, **kwargs):
         XModule.__init__(self, system, location, definition, instance_state, shared_state, **kwargs)
+
+        if isinstance(instance_state, str):
+            instance_state = json.loads(instance_state)
         xml_data = etree.fromstring(definition['data'])
         self.discussion_id = xml_data.attrib['id']
         self.title = xml_data.attrib['for']
         self.category = xml_data.attrib['category']
+        self.user_id = instance_state['user_id']
 
 class DiscussionDescriptor(RawDescriptor):
     module_class = DiscussionModule
