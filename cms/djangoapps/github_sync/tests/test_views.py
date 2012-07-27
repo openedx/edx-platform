a3453cd3189a6fc0195b02c8cb4801aed76ed596
Calen Pennington <calen.pennington@gmail.com>
2012-07-27 12:36:30 -0400

    Add some more tests of github_sync

--- cms/djangoapps/github_sync/tests/test_views.py
+++ cms/djangoapps/github_sync/tests/test_views.py
@@ -1,52 +1,43 @@
 import json
 from django.test.client import Client
 from django.test import TestCase
-from mock import patch, Mock
+from mock import patch
 from override_settings import override_settings
-from django.conf import settings
+from github_sync import load_repo_settings
 
 
-@override_settings(REPOS={'repo': {'path': 'path', 'branch': 'branch'}})
+@override_settings(REPOS={'repo': {'branch': 'branch', 'origin': 'origin'}})
 class PostReceiveTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
-    @patch('github_sync.views.export_to_github')
-    @patch('github_sync.views.import_from_github')
-    def test_non_branch(self, import_from_github, export_to_github):
+    @patch('github_sync.views.sync_with_github')
+    def test_non_branch(self, sync_with_github):
         self.client.post('/github_service_hook', {'payload': json.dumps({
             'ref': 'refs/tags/foo'})
         })
-        self.assertFalse(import_from_github.called)
-        self.assertFalse(export_to_github.called)
+        self.assertFalse(sync_with_github.called)
 
-    @patch('github_sync.views.export_to_github')
-    @patch('github_sync.views.import_from_github')
-    def test_non_watched_repo(self, import_from_github, export_to_github):
+    @patch('github_sync.views.sync_with_github')
+    def test_non_watched_repo(self, sync_with_github):
         self.client.post('/github_service_hook', {'payload': json.dumps({
             'ref': 'refs/heads/branch',
             'repository': {'name': 'bad_repo'}})
         })
-        self.assertFalse(import_from_github.called)
-        self.assertFalse(export_to_github.called)
+        self.assertFalse(sync_with_github.called)
 
-    @patch('github_sync.views.export_to_github')
-    @patch('github_sync.views.import_from_github')
-    def test_non_tracked_branch(self, import_from_github, export_to_github):
+    @patch('github_sync.views.sync_with_github')
+    def test_non_tracked_branch(self, sync_with_github):
         self.client.post('/github_service_hook', {'payload': json.dumps({
             'ref': 'refs/heads/non_branch',
             'repository': {'name': 'repo'}})
         })
-        self.assertFalse(import_from_github.called)
-        self.assertFalse(export_to_github.called)
+        self.assertFalse(sync_with_github.called)
 
-    @patch('github_sync.views.export_to_github')
-    @patch('github_sync.views.import_from_github', return_value=(Mock(), Mock()))
-    def test_tracked_branch(self, import_from_github, export_to_github):
+    @patch('github_sync.views.sync_with_github')
+    def test_tracked_branch(self, sync_with_github):
         self.client.post('/github_service_hook', {'payload': json.dumps({
             'ref': 'refs/heads/branch',
             'repository': {'name': 'repo'}})
         })
-        import_from_github.assert_called_with(settings.REPOS['repo'])
-        mock_revision, mock_course = import_from_github.return_value
-        export_to_github.assert_called_with(mock_course, 'path', "Changes from cms import of revision %s" % mock_revision)
+        sync_with_github.assert_called_with(load_repo_settings('repo'))
