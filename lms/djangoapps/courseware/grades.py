987b9c11a94372f6d4c888755a1ce5ff5a036e30
Victor Shnayder <victor@mitx.mit.edu>
2012-08-02 14:05:42 -0400

    Use url_name for chapters and sections in lms views
    * got rid of the hackish conversions between ' ' and '_'
    * use url_name and display_name where appropriate
    * update templates to match.

--- lms/djangoapps/courseware/grades.py
+++ lms/djangoapps/courseware/grades.py
@@ -12,18 +12,23 @@ _log = logging.getLogger("mitx.courseware")
 
 def grade_sheet(student, course, grader, student_module_cache):
     """
-    This pulls a summary of all problems in the course. It returns a dictionary with two datastructures:
+    This pulls a summary of all problems in the course. It returns a dictionary
+    with two datastructures:
 
-    - courseware_summary is a summary of all sections with problems in the course. It is organized as an array of chapters,
-    each containing an array of sections, each containing an array of scores. This contains information for graded and ungraded
-    problems, and is good for displaying a course summary with due dates, etc.
+    - courseware_summary is a summary of all sections with problems in the
+    course. It is organized as an array of chapters, each containing an array of
+    sections, each containing an array of scores. This contains information for
+    graded and ungraded problems, and is good for displaying a course summary
+    with due dates, etc.
 
-    - grade_summary is the output from the course grader. More information on the format is in the docstring for CourseGrader.
+    - grade_summary is the output from the course grader. More information on
+      the format is in the docstring for CourseGrader.
 
     Arguments:
         student: A User object for the student to grade
         course: An XModule containing the course to grade
-        student_module_cache: A StudentModuleCache initialized with all instance_modules for the student
+        student_module_cache: A StudentModuleCache initialized with all
+             instance_modules for the student
     """
     totaled_scores = {}
     chapters = []
@@ -51,12 +56,16 @@ def grade_sheet(student, course, grader, student_module_cache):
                         correct = total
 
                 if not total > 0:
-                    #We simply cannot grade a problem that is 12/0, because we might need it as a percentage
+                    #We simply cannot grade a problem that is 12/0, because we
+                    #might need it as a percentage
                     graded = False
 
-                scores.append(Score(correct, total, graded, module.metadata.get('display_name')))
+                scores.append(Score(correct, total, graded,
+                                    module.metadata.get('display_name')))
 
-            section_total, graded_total = graders.aggregate_scores(scores, s.metadata.get('display_name'))
+            section_total, graded_total = graders.aggregate_scores(
+                scores, s.metadata.get('display_name'))
+
             #Add the graded total to totaled_scores
             format = s.metadata.get('format', "")
             if format and graded_total.possible > 0:
@@ -65,7 +74,8 @@ def grade_sheet(student, course, grader, student_module_cache):
                 totaled_scores[format] = format_scores
 
             sections.append({
-                'section': s.metadata.get('display_name'),
+                'display_name': s.metadata.get('display_name'),
+                'url_name': s.metadata.get('url_name'),
                 'scores': scores,
                 'section_total': section_total,
                 'format': format,
@@ -74,7 +84,8 @@ def grade_sheet(student, course, grader, student_module_cache):
             })
 
         chapters.append({'course': course.metadata.get('display_name'),
-                         'chapter': c.metadata.get('display_name'),
+                         'display_name': c.metadata.get('display_name'),
+                         'url_name': c.metadata.get('url_name'),
                          'sections': sections})
 
     grade_summary = grader.grade(totaled_scores)
