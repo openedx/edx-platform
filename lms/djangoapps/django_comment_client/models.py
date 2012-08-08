b2a048e6fc7a05ceb3600fa07420d55d416dd5d9
Mike Chen <ccp0101@gmail.com>
2012-08-08 10:42:29 -0400

    limit permissions of a role to courses and fix bugs

--- lms/djangoapps/django_comment_client/models.py
+++ lms/djangoapps/django_comment_client/models.py
@@ -1,36 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
+import logging
 
 
 class Role(models.Model):
-    name = models.CharField(max_length=30, null=False, blank=False, primary_key=True)
+    name = models.CharField(max_length=30, null=False, blank=False)
     users = models.ManyToManyField(User, related_name="roles")
+    course_id = models.CharField(max_length=255, blank=True, db_index=True)
 
     def __unicode__(self):
-        return self.name
-
-    @staticmethod
-    def register(name):
-        return Role.objects.get_or_create(name=name)[0]
-
-    def register_permissions(self, permissions):
-        for p in permissions:
-            if not self.permissions.filter(name=p):
-                self.permissions.add(Permission.register(p))
+        return self.name + " for " + (self.course_id if self.course_id else "all courses")
 
     def inherit_permissions(self, role):
-        self.register_permissions(map(lambda p: p.name, role.permissions.all()))
+        if role.course_id and role.course_id != self.course_id:
+            logging.warning("%s cannot inheret permissions from %s due to course_id inconsistency" % 
+                            (self, role))
+        for per in role.permissions.all():
+            self.add_permission(per)
+
+    def add_permission(self, permission):
+        self.permissions.add(Permission.objects.get_or_create(name=permission)[0])
+
+    def has_permission(self, permission):
+        return self.permissions.filter(name=permission).exists()
 
 
 class Permission(models.Model):
     name = models.CharField(max_length=30, null=False, blank=False, primary_key=True)
-    users = models.ManyToManyField(User, related_name="permissions")
     roles = models.ManyToManyField(Role, related_name="permissions")
 
     def __unicode__(self):
         return self.name
-
-    @staticmethod
-    def register(name):
-        return Permission.objects.get_or_create(name=name)[0]
-
