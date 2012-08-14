dd92f51f027b1c772496f6b588d36d0c662c1475
Tom Giannattasio <tom@mitx.mit.edu>
2012-08-14 11:38:02 -0400

    added fluid widths

--- lms/static/js/jquery.gradebook.js
+++ lms/static/js/jquery.gradebook.js
@@ -9,7 +9,7 @@ var Gradebook = function($element) {
 	var $leftShadow = $('<div class="left-shadow"></div>');
 	var $rightShadow = $('<div class="right-shadow"></div>');
 	var tableHeight = $gradeTable.height();
-	var maxScroll = $gradeTable.width() - $element.find('.grades').width();
+	var maxScroll = $gradeTable.width() - $grades.width();
 	var $body = $('body');
 	var mouseOrigin;
 	var tableOrigin;
@@ -17,6 +17,7 @@ var Gradebook = function($element) {
 	var startDrag = function(e) {
 		mouseOrigin = e.pageX;
 		tableOrigin = $gradeTable.position().left;
+		$body.css('-webkit-user-select', 'none');
 		$body.bind('mousemove', moveDrag);
 		$body.bind('mouseup', stopDrag);
 	};
@@ -25,14 +26,13 @@ var Gradebook = function($element) {
 		var offset = e.pageX - mouseOrigin;
 		var targetLeft = clamp(tableOrigin + offset, -maxScroll, 0);
 
-		$gradeTable.css({
-			'left': targetLeft + 'px'
-		});
+		updateHorizontalPosition(targetLeft);
 
 		setShadows(targetLeft);
 	};
 
 	var stopDrag = function(e) {
+		$body.css('-webkit-user-select', 'auto');
 		$body.unbind('mousemove', moveDrag);
 		$body.unbind('mouseup', stopDrag);
 	};
@@ -40,15 +40,11 @@ var Gradebook = function($element) {
 	var setShadows = function(left) {
 		var padding = 30;
 
-		if(left > -padding) {
-			var percent = -left / padding;
-			$leftShadow.css('opacity', percent);
-		}
-
-		if(left < -maxScroll + padding) {
-			var percent = (maxScroll + left) / padding;
-			$rightShadow.css('opacity', percent);
-		}
+		var leftPercent = clamp(-left / padding, 0, 1);
+		$leftShadow.css('opacity', leftPercent);
+	
+		var rightPercent = clamp((maxScroll + left) / padding, 0, 1);
+		$rightShadow.css('opacity', rightPercent);
 	};
 
 	var clamp = function(val, min, max) {
@@ -57,10 +53,24 @@ var Gradebook = function($element) {
 	    return val;
 	};
 
+	var updateWidths = function(e) {
+		maxScroll = $gradeTable.width() - $grades.width();
+		var targetLeft = clamp($gradeTable.position().left, -maxScroll, 0);
+		updateHorizontalPosition(targetLeft);
+		setShadows(targetLeft);
+	}
+
+	var updateHorizontalPosition = function(left) {
+		$gradeTable.css({
+			'left': left + 'px'
+		});
+	}
+
 	$leftShadow.css('height', tableHeight + 'px');
 	$rightShadow.css('height', tableHeight + 'px');
 	$grades.append($leftShadow).append($rightShadow);
 	setShadows(0);
 	$grades.css('height', tableHeight);
 	$gradeTable.bind('mousedown', startDrag);
+	$(window).bind('resize', updateWidths);
 }
