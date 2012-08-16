4b408f7cd37bd4f3dc08e201534e376b4c053654
kimth <kimt@mit.edu>
2012-08-16 11:27:29 -0400

    Xqueue URL is https not http

--- lms/envs/dev.py
+++ lms/envs/dev.py
@@ -54,7 +54,7 @@ CACHES = {
 }
 
 XQUEUE_INTERFACE = {
-    "url": "http://sandbox-xqueue.edx.org",
+    "url": "https://sandbox-xqueue.edx.org",
     "django_auth": {
         "username": "lms",
         "password": "48b5c838c17eede6355ac7"
