d08b4057388470ba53e155f209682e1f7f762886
Mark L. Chang <mark.chang@gmail.com>
2012-08-20 14:34:04 -0400

    first cut integrating lettuce + selenium into django

---
+++ lms/djangoapps/terrain.py
@@ -0,0 +1,11 @@
+from lettuce import before, after, world
+from selenium import webdriver
+import lettuce_webdriver.webdriver
+
+@before.all
+def setup_browser():
+  world.browser = webdriver.Firefox()
+
+@after.all
+def teardown_browser(total):
+  world.browser.quit()
