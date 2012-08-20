01fafd3400dd7368f4b36849b2c5dceed0d86019
kimth <kimt@mit.edu>
2012-08-20 12:40:34 -0400

    book_url doesn't come from settings

--- lms/djangoapps/staticbook/views.py
+++ lms/djangoapps/staticbook/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from mitxmako.shortcuts import render_to_response
 
@@ -13,8 +14,10 @@ def index(request, course_id, book_index, page=0):
     textbook = course.textbooks[int(book_index)]
     table_of_contents = textbook.table_of_contents
 
+    book_url = settings.BOOK_URL    
+
     return render_to_response('staticbook.html',
-                              {'page': int(page), 'course': course,
+                              {'page': int(page), 'course': course, 'book_url': book_url,
                                'table_of_contents': table_of_contents,
                                'staff_access': staff_access})
 
