26c06afc2eb611d02f1c21b4834b038f0b7603ca
Victor Shnayder <victor@mitx.mit.edu>
2012-08-21 16:09:48 -0400

    comment out debug messages

--- common/lib/xmodule/xmodule/html_module.py
+++ common/lib/xmodule/xmodule/html_module.py
@@ -86,7 +86,7 @@ class HtmlDescriptor(XmlDescriptor, EditingDescriptor):
             # online and has imported all current (fall 2012) courses from xml
             if not system.resources_fs.exists(filepath):
                 candidates = cls.backcompat_paths(filepath)
-                log.debug("candidates = {0}".format(candidates))
+                #log.debug("candidates = {0}".format(candidates))
                 for candidate in candidates:
                     if system.resources_fs.exists(candidate):
                         filepath = candidate
