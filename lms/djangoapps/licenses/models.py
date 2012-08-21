ed88708d716123acac5d8f9ca7304b79d363852a
Carlos Andrés Rocha <rocha@edx.org>
2012-08-21 19:04:11 -0400

    Renamed models and commands for importing serial numbers.

--- lms/djangoapps/licenses/models.py
+++ lms/djangoapps/licenses/models.py
@@ -1,19 +1,20 @@
 """
 """
-
 from django.db import models
-
 from student.models import User
 
 
-class Software(models.Model):
+class CourseSoftware(models.Model):
     name = models.CharField(max_length=255)
     full_name = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
     course_id = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return u'{0} for {1}'.format(self.name, self.course_id)
 
-class StudentLicense(models.Model):
-    software = models.ForeignKey(Software, db_index=True)
+
+class UserLicense(models.Model):
+    software = models.ForeignKey(CourseSoftware, db_index=True)
+    user = models.ForeignKey(User, null=True)
     serial = models.CharField(max_length=255)
-    user = models.ForeignKey(User, null=True, blank=True)
