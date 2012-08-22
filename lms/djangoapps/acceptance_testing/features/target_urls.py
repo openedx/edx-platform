3c450ac271ce9299405d72b5cbcbdba853104ed9
Mark L. Chang <mark.chang@gmail.com>
2012-08-22 09:42:30 -0400

    starting target url abstraction

---
+++ lms/djangoapps/acceptance_testing/features/target_urls.py
@@ -0,0 +1,5 @@
+from lettuce import * #before, world
+from selenium import *
+
+@step(u'I load edx_dev')
+def i_load_edx_dev(step):
--- lms/djangoapps/terrain.py
+++ lms/djangoapps/terrain.py
@@ -1,10 +1,15 @@
 from lettuce import before, after, world
 from selenium import webdriver
 import lettuce_webdriver.webdriver
+from os import getenv
 
 @before.all
 def setup_browser():
   world.browser = webdriver.Firefox()
+  world.dev_url = getenv("EDX_DEV_URL")
+  world.sandbox_url = getenv("EDX_SANDBOX_URL")
+  world.stage_url = getenv("EDX_STAGE_URL")
+  world.production_url = getenv("EDX_PRODUCTION_URL")
 
 @after.all
 def teardown_browser(total):
