4b8da88ac1ac2fe6b166ac2d63baf40741d9307f
Calen Pennington <calen.pennington@gmail.com>
2012-08-23 09:16:40 -0400

    Make heartbeat url wait for courses to be loaded

--- lms/djangoapps/heartbeat/views.py
+++ lms/djangoapps/heartbeat/views.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 from django.http import HttpResponse
+from xmodule.modulestore.django import modulestore
 
 
 def heartbeat(request):
@@ -8,6 +9,7 @@ def heartbeat(request):
     Simple view that a loadbalancer can check to verify that the app is up
     """
     output = {
-        'date': datetime.now().isoformat()
+        'date': datetime.now().isoformat(),
+        'courses': [course.location for course in modulestore().get_courses()],
     }
     return HttpResponse(json.dumps(output, indent=4))
