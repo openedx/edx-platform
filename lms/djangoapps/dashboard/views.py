0fc5c0bffebe2683961630c9592bf9bb8d602dd2
John Hess <mgojohn@gmail.com>
2012-08-27 19:05:00 -0400

    updated admin_dashboard to show count of unique students

--- lms/djangoapps/dashboard/views.py
+++ lms/djangoapps/dashboard/views.py
@@ -21,11 +21,16 @@ def dashboard(request):
     if not request.user.is_staff:
         raise Http404
 
-    query = "select count(user_id) as students, course_id from student_courseenrollment group by course_id order by students desc"
+    queries=[]
+    queries.append("select count(user_id) as students, course_id from student_courseenrollment group by course_id order by students desc;")
+    queries.append("select count(distinct user_id) as unique_students from student_courseenrollment;")
 
     from django.db import connection
     cursor = connection.cursor()
-    cursor.execute(query)
-    results = dictfetchall(cursor)
+    results =[]
+
+    for query in queries:
+        cursor.execute(query)
+        results.append(dictfetchall(cursor))
 
     return HttpResponse(json.dumps(results, indent=4))
