1d2aec0db5e48282dc61dba08a86780daaa6ae7b
Arjun Singh <arjun810@gmail.com>
2012-08-28 11:32:47 -0700

    Move compilation of course coffee into dev only; requires that all compiled js files be in course data on prod (this was already the case).

--- lms/envs/dev.py
+++ lms/envs/dev.py
@@ -154,3 +154,24 @@ FILE_UPLOAD_HANDLERS = (
 ########################### PIPELINE #################################
 
 PIPELINE_SASS_ARGUMENTS = '-r {proj_dir}/static/sass/bourbon/lib/bourbon.rb'.format(proj_dir=PROJECT_ROOT)
+
+# Compile all coffee files in course data directories if they are out of date.
+# TODO: Remove this once we move data into Mongo. This is only temporary while
+# course data directories are still in use. Note that since this only happens in
+# dev, the js files must be present in the course directories in prod, (i.e. you
+# must commit them).
+if os.path.isdir(DATA_DIR):
+    for course_dir in os.listdir(DATA_DIR):
+        js_dir = DATA_DIR / course_dir / "js"
+        if not os.path.isdir(js_dir):
+            continue
+        for filename in os.listdir(js_dir):
+            if filename.endswith('coffee'):
+                new_filename = os.path.splitext(filename)[0] + ".js"
+                if os.path.exists(js_dir / new_filename):
+                    coffee_timestamp = os.stat(js_dir / filename).st_mtime
+                    js_timestamp     = os.stat(js_dir / new_filename).st_mtime
+                    if coffee_timestamp <= js_timestamp:
+                        continue
+                os.system("rm %s" % (js_dir / new_filename))
+                os.system("coffee -c %s" % (js_dir / filename))
