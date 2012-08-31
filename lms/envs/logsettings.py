cef089f722e2e540c4b0b38fcee8958542ec4e81
John Jarvis <jrjarvis@mitx.mit.edu>
2012-08-31 09:38:13 -0400

    tracking log filename now determined by puppet, formatting cleanup

--- lms/envs/logsettings.py
+++ lms/envs/logsettings.py
@@ -4,6 +4,7 @@ import platform
 import sys
 from logging.handlers import SysLogHandler
 
+
 def get_logger_config(log_dir,
                       logging_env="no_env",
                       tracking_filename=None,
@@ -18,94 +19,91 @@ def get_logger_config(log_dir,
     settings are extended."""
 
     # Revert to INFO if an invalid string is passed in
-    if local_loglevel not in ['DEBUG','INFO','WARNING','ERROR','CRITICAL']:
+    if local_loglevel not in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
         local_loglevel = 'INFO'
 
-    # If we're given an explicit place to put tracking logs, we do that (say for
-    # debugging). However, logging is not safe for multiple processes hitting
-    # the same file. So if it's left blank, we dynamically create the filename
+    # If we're given an explicit place to put tracking logs,
+    # we do that (say for debugging). However, logging is not
+    # safe for multiple processes hitting the same file.
+    # So if it's left blank, we dynamically create the filename
     # based on the PID of this worker process.
-    if tracking_filename:
-        tracking_file_loc = os.path.join(log_dir, tracking_filename)
-    else:
-        pid = os.getpid() # So we can log which process is creating the log
-        tracking_file_loc = os.path.join(log_dir, "tracking_{0}.log".format(pid))
 
     hostname = platform.node().split(".")[0]
     syslog_format = ("[%(name)s][env:{logging_env}] %(levelname)s [{hostname} " +
                      " %(process)d] [%(filename)s:%(lineno)d] - %(message)s").format(
                         logging_env=logging_env, hostname=hostname)
 
-    handlers = ['console'] if debug else ['console', 'syslogger-remote', 'syslogger-local', 'newrelic']
+    handlers = ['console'] if debug else ['console', 'syslogger-remote',
+                                            'syslogger-local', 'newrelic']
 
     return {
         'version': 1,
         'disable_existing_loggers': False,
-        'formatters' : {
-            'standard' : {
-                'format' : '%(asctime)s %(levelname)s %(process)d [%(name)s] %(filename)s:%(lineno)d - %(message)s',
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s %(levelname)s %(process)d [%(name)s] %(filename)s:%(lineno)d - %(message)s',
             },
-            'syslog_format' : { 'format' : syslog_format },
-            'raw' : { 'format' : '%(message)s' },
+            'syslog_format': {'format': syslog_format ,
+            'raw': {'format': '%(message)s'},
         },
-        'handlers' : {
-            'console' : {
-                'level' : 'DEBUG' if debug else 'INFO',
-                'class' : 'logging.StreamHandler',
-                'formatter' : 'standard',
-                'stream' : sys.stdout,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG' if debug else 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard',
+                'stream': sys.stdout,
             },
-            'syslogger-remote' : {
-                'level' : 'INFO',
-                'class' : 'logging.handlers.SysLogHandler',
-                'address' : syslog_addr,
-                'formatter' : 'syslog_format',
+            'syslogger-remote': {
+                'level': 'INFO',
+                'class': 'logging.handlers.SysLogHandler',
+                'address': syslog_addr,
+                'formatter': 'syslog_format',
             },
-            'syslogger-local' : {
-                'level' : local_loglevel,
-                'class' : 'logging.handlers.SysLogHandler',
-                'address' : '/dev/log',
-                'formatter' : 'syslog_format',
+            'syslogger-local': {
+                'level': local_loglevel,
+                'class': 'logging.handlers.SysLogHandler',
+                'address': '/dev/log',
+                'formatter': 'syslog_format',
                 'facility': SysLogHandler.LOG_LOCAL0,
             },
-            'tracking' : {
-                'level' : 'DEBUG',
-                'class' : 'logging.handlers.SysLogHandler',
-                'address' : '/dev/log',
-                'facility' : SysLogHandler.LOG_LOCAL1,
-                'formatter' : 'raw',
+            'tracking': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.SysLogHandler',
+                'address': '/dev/log',
+                'facility': SysLogHandler.LOG_LOCAL1,
+                'formatter': 'raw',
             },
-            'newrelic' : {
+            'newrelic': {
                 'level': 'ERROR',
                 'class': 'newrelic_logging.NewRelicHandler',
                 'formatter': 'raw',
             }
         },
-        'loggers' : {
-            'django' : {
-                'handlers' : handlers,
-                'propagate' : True,
-                'level' : 'INFO'
+        'loggers': {
+            'django': {
+                'handlers': handlers,
+                'propagate': True,
+                'level': 'INFO'
             },
-            'tracking' : {
-                'handlers' : ['tracking'],
-                'level' : 'DEBUG',
-                'propagate' : False,
+            'tracking': {
+                'handlers': ['tracking'],
+                'level': 'DEBUG',
+                'propagate': False,
             },
-            '' : {
-                'handlers' : handlers,
-                'level' : 'DEBUG',
-                'propagate' : False
+            '': {
+                'handlers': handlers,
+                'level': 'DEBUG',
+                'propagate': False
             },
-            'mitx' : {
-                'handlers' : handlers,
-                'level' : 'DEBUG',
-                'propagate' : False
+            'mitx': {
+                'handlers': handlers,
+                'level': 'DEBUG',
+                'propagate': False
             },
-            'keyedcache' : {
-                'handlers' : handlers,
-                'level' : 'DEBUG',
-                'propagate' : False
+            'keyedcache': {
+                'handlers': handlers,
+                'level': 'DEBUG',
+                'propagate': False
             },
         }
     }
