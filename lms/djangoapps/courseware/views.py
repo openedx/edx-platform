67f7c38afd86730b4c7e712c050ff69ad8d1b70a
David Ormsbee <dave@edx.org>
2012-08-31 15:20:13 -0400

    add comments explaining prefetch need

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -327,6 +327,9 @@ def progress(request, course_id, student_id=None):
 
     # NOTE: To make sure impersonation by instructor works, use
     # student instead of request.user in the rest of the function.
+
+    # The pre-fetching of groups is done to make auth checks not require an 
+    # additional DB lookup (this kills the Progress page in particular).
     student = User.objects.prefetch_related("groups").get(id=student.id)
 
     student_module_cache = StudentModuleCache.cache_for_descriptor_descendents(
