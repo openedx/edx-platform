34ddb0147861ad68382777f861b3a41580b385ac
Victor Shnayder <victor@mitx.mit.edu>
2012-09-03 12:05:41 -0400

    initial forum scripts

---
+++ common/test/load/multimech/forum/test_scripts/forum_search.py
@@ -0,0 +1,44 @@
+import random
+import time
+import requests
+import random
+
+# NOTE: Script relies on this user being signed up for cs188, and cs188 having started.
+email = 'victor+test@edx.org'
+password = 'abc123'
+
+machine = 'load-test-001.m.edx.org'
+protocol = 'http://'
+
+auth = ('anant', 'agarwal')
+
+def url(path):
+    """
+    path should be something like '/', '/courses'
+    """
+    return ''.join([protocol, machine, path])
+
+class Transaction(object):
+    def __init__(self):
+        # Load / to get csrf cookie
+        s = requests.session(auth=auth)
+        r = s.get(url('/'))
+        # Need to set the header as well as the cookie.  Why? No one knows.
+        headers = {'X-CSRFToken' : s.cookies['csrftoken']}
+        # login
+        r = s.post(url('/login'), data={'email' : email, 'password': password}, headers=headers)
+        print r.text
+        self.session = s
+
+    def run(self):
+        s = self.session
+
+        r = s.get(url('/courses/BerkeleyX/CS188/fa12/discussion/forum/'
+                      '?sort_key=activity&sort_order=desc&page=1&text=um'))
+        assert r.status_code == requests.codes.ok
+
+if __name__ == '__main__':
+    trans = Transaction()
+    print "running..."
+    trans.run()
+    print "run done"
