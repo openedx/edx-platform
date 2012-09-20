9ec38176e01639f3087eec4b85af67ef24c8e2f7
Bridger Maxwell <bridgeyman@gmail.com>
2012-09-20 16:56:09 -0400

    Small cleanup tweaks in grading.

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -22,7 +22,7 @@ from django.views.decorators.cache import cache_control
 from courseware import grades
 from courseware.access import has_access
 from courseware.courses import (get_course_with_access, get_courses_by_university)
-from models import StudentModuleCache
+from courseware.models import StudentModuleCache
 from module_render import toc_for_course, get_module, get_instance_module
 from student.models import UserProfile
 
@@ -457,7 +457,7 @@ def progress(request, course_id, student_id=None):
         course_id, student, course)
 
     courseware_summary = grades.progress_summary(student, request, course,
-                                                 course.grader, student_module_cache)
+                                                 student_module_cache)
     grade_summary = grades.grade(student, request, course, student_module_cache)
     
     if courseware_summary is None:
