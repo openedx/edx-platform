6bd6dbb08d5c6a5b1bc2f5a5f2a2d001208cbf5e
Arjun Singh <arjun810@gmail.com>
2012-09-21 05:42:51 -0700

    Better fix for trailing slash

--- lms/djangoapps/django_comment_client/urls.py
+++ lms/djangoapps/django_comment_client/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import url, patterns, include
 
 urlpatterns = patterns('',
-    url(r'forum/', include('django_comment_client.forum.urls')),
+    url(r'forum/?', include('django_comment_client.forum.urls')),
     url(r'', include('django_comment_client.base.urls')),
 )
