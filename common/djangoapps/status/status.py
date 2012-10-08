7e1ca6e3d001ff3623956d81a76e3120c13c8d00
Victor Shnayder <victor@mitx.mit.edu>
2012-10-08 14:06:58 -0400

    Pass course_id instead of course to get_site_status_msg
    * makes testing easier -- don't need course objects

--- common/djangoapps/status/status.py
+++ common/djangoapps/status/status.py
@@ -10,7 +10,7 @@ import sys
 
 log = logging.getLogger(__name__)
 
-def get_site_status_msg(course):
+def get_site_status_msg(course_id):
     """
     Look for a file settings.STATUS_MESSAGE_PATH.  If found, read it,
     parse as json, and do the following:
@@ -33,9 +33,9 @@ def get_site_status_msg(course):
 
         status_dict = json.loads(content)
         msg = status_dict.get('global', None)
-        if course and course.id in status_dict:
+        if course_id in status_dict:
             msg = msg + "<br>" if msg else ''
-            msg += status_dict[course.id]
+            msg += status_dict[course_id]
 
         return msg
     except:
