9e02b3ce34e86f2dcac8b71b269d404bc967325a
Victor Shnayder <victor@mitx.mit.edu>
2012-10-08 15:52:18 -0400

    Move tests around:
    - move progress tests into test_progress.py
    - move grader tests into test_graders.py
    - move the capa tests into capa/tests/

---
+++ common/lib/capa/capa/tests/__init__.py
@@ -0,0 +1,19 @@
+import fs
+import fs.osfs
+import os
+
+from mock import Mock
+
+test_system = Mock(
+    ajax_url='courses/course_id/modx/a_location',
+    track_function=Mock(),
+    get_module=Mock(),
+    render_template=Mock(),
+    replace_urls=Mock(),
+    user=Mock(),
+    filestore=fs.osfs.OSFS(os.path.dirname(os.path.realpath(__file__))+"/test_files"),
+    debug=True,
+    xqueue={'interface':None, 'callback_url':'/', 'default_queuename': 'testqueue', 'waittime': 10},
+    node_path=os.environ.get("NODE_PATH", "/usr/local/lib/node_modules"),
+    anonymous_student_id = 'student'
+)
