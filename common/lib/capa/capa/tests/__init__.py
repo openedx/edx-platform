ecc2909d906c801ecbfbd887a02cdbe0c4b2e7f5
Victor Shnayder <victor@mitx.mit.edu>
2012-10-08 16:44:54 -0400

    Move more tests around, start prep for inputtype refactor

--- common/lib/capa/capa/tests/__init__.py
+++ common/lib/capa/capa/tests/__init__.py
@@ -4,6 +4,8 @@ import os
 
 from mock import Mock
 
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+
 test_system = Mock(
     ajax_url='courses/course_id/modx/a_location',
     track_function=Mock(),
@@ -11,7 +13,7 @@ test_system = Mock(
     render_template=Mock(),
     replace_urls=Mock(),
     user=Mock(),
-    filestore=fs.osfs.OSFS(os.path.dirname(os.path.realpath(__file__))+"/test_files"),
+    filestore=fs.osfs.OSFS(os.path.join(TEST_DIR, "test_files")),
     debug=True,
     xqueue={'interface':None, 'callback_url':'/', 'default_queuename': 'testqueue', 'waittime': 10},
     node_path=os.environ.get("NODE_PATH", "/usr/local/lib/node_modules"),
