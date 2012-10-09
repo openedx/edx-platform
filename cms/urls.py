eb2fadb639478ce6c349075a884cf29fc569ec07
Tom Giannattasio <tom@mitx.mit.edu>
2012-10-09 14:45:56 -0400

    added static page templates; urls will probably need some reworking, but i wanted to get the templates in there

--- cms/urls.py
+++ cms/urls.py
@@ -29,6 +29,8 @@ urlpatterns = ('',
     url(r'^(?P<org>[^/]+)/(?P<course>[^/]+)/course/(?P<name>[^/]+)/remove_user$',
         'contentstore.views.remove_user', name='remove_user'),
     url(r'^assets/(?P<location>.*?)$', 'contentstore.views.asset_index', name='asset_index'),
+    url(r'^pages/(?P<org>[^/]+)/(?P<course>[^/]+)/course/(?P<coursename>[^/]+)$', 'contentstore.views.static_pages', name='static_pages'),
+    url(r'^edit_static/(?P<org>[^/]+)/(?P<course>[^/]+)/course/(?P<coursename>[^/]+)$', 'contentstore.views.edit_static', name='edit_static'),
 
     # temporary landing page for a course
     url(r'^landing/(?P<org>[^/]+)/(?P<course>[^/]+)/course/(?P<coursename>[^/]+)$', 'contentstore.views.landing', name='landing')
