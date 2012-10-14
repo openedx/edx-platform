09d7d6d6881f97eb85154ce111337f34d6f9f5ea
ichuang <ichuang@mit.edu>
2012-10-14 16:40:04 -0400

    add AUTH_USE_MIT_CERTIFICATES feature flag to CMS, and @ssl_login_shortcut

--- cms/envs/dev_ike.py
+++ cms/envs/dev_ike.py
@@ -1,8 +1,21 @@
 # dev environment for ichuang/mit
 
+# FORCE_SCRIPT_NAME = '/cms'
+
 from .common import *
 from logsettings import get_logger_config
 from .dev import *
 import socket
 
-#MITX_FEATURES['USE_DJANGO_PIPELINE']=False      # don't recompile scss
+MITX_FEATURES['AUTH_USE_MIT_CERTIFICATES'] = True
+
+MITX_FEATURES['USE_DJANGO_PIPELINE']=False      # don't recompile scss
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')	# django 1.4 for nginx ssl proxy
+
+MITX_ROOT_URL = 'https://qisx.mit.edu:442'
+#MITX_ROOT_URL = 'cms'
+
+LOGIN_REDIRECT_URL = MITX_ROOT_URL + '/login'
+LOGIN_URL = MITX_ROOT_URL + '/login'
+
