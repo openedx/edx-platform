f846f4c2adaa0ce013ed7dcacba7feed60b64cc1
ichuang <ichuang@mit.edu>
2012-10-13 22:33:41 -0400

    add import filter capability to html & problem editing; provides latex2edx interface

---
+++ cms/envs/dev_ike.py
@@ -0,0 +1,8 @@
+# dev environment for ichuang/mit
+
+from .common import *
+from logsettings import get_logger_config
+from .dev import *
+import socket
+
+#MITX_FEATURES['USE_DJANGO_PIPELINE']=False      # don't recompile scss
