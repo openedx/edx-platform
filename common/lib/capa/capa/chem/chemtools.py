a176d294e75958028e67812a1b12c5d552e5082a
Александр <kryklia@gmail.com>
2012-10-16 19:42:04 +0300

    continue vsepr grading

--- common/lib/capa/capa/chem/chemtools.py
+++ common/lib/capa/capa/chem/chemtools.py
@@ -1,6 +1,12 @@
 from collections import OrderedDict
 
 
+def vsepr_parse_user_answer(user_input):
+    d = OrderedDict(eval(user_input))
+    d['atoms'] = OrderedDict(sorted(d['atoms'].items()))
+    return d
+
+
 def vsepr_build_correct_answer(geometry, atoms):
     correct_answer = OrderedDict()
     correct_answer['geometry'] = geometry
@@ -9,6 +15,8 @@ def vsepr_build_correct_answer(geometry, atoms):
 
 
 def vsepr_grade(user_input, correct_answer):
+    print user_input, type(user_input)
+    print correct_answer, type(correct_answer)
     if user_input['geometry'] != correct_answer['geometry']:
         return False
     if user_input['atoms'].values() != correct_answer['atoms'].values():
