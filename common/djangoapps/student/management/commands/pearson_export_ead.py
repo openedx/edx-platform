7a84bed916b035f11a4923031c6fb920cf698406
David Ormsbee <dave@edx.org>
2012-10-26 15:50:32 -0400

    hackish EAD export support

--- common/djangoapps/student/management/commands/pearson_export_ead.py
+++ common/djangoapps/student/management/commands/pearson_export_ead.py
@@ -7,67 +7,73 @@ from django.core.management.base import BaseCommand, CommandError
 
 from student.models import TestCenterUser
 
-class Command(BaseCommand):
-    CSV_TO_MODEL_FIELDS = OrderedDict([
-        ("ClientCandidateID", "client_candidate_id"),
-        ("FirstName", "first_name"),
-        ("LastName", "last_name"),
-        ("MiddleName", "middle_name"),
-        ("Suffix", "suffix"),
-        ("Salutation", "salutation"),
-        ("Email", "email"),
-        # Skipping optional fields Username and Password
-        ("Address1", "address_1"),
-        ("Address2", "address_2"),
-        ("Address3", "address_3"),
-        ("City", "city"),
-        ("State", "state"),
-        ("PostalCode", "postal_code"),
-        ("Country", "country"),
-        ("Phone", "phone"),
-        ("Extension", "extension"),
-        ("PhoneCountryCode", "phone_country_code"),
-        ("FAX", "fax"),
-        ("FAXCountryCode", "fax_country_code"),
-        ("CompanyName", "company_name"),
-        # Skipping optional field CustomQuestion
-        ("LastUpdate", "user_updated_at"), # in UTC, so same as what we store
-    ])
+def generate_id():
+    return "{:012}".format(uuid.uuid4().int % (10**12))
 
+class Command(BaseCommand):
     args = '<output_file>'
     help = """
     Export user information from TestCenterUser model into a tab delimited
     text file with a format that Pearson expects.
     """
+    FIELDS = [
+        'AuthorizationTransactionType',
+        'AuthorizationID',
+        'ClientAuthorizationID',
+        'ClientCandidateID',
+        'ExamAuthorizationCount',
+        'ExamSeriesCode',
+        'EligibilityApptDateFirst',
+        'EligibilityApptDateLast',
+        'LastUpdate',
+    ]
+    
     def handle(self, *args, **kwargs):
         if len(args) < 1:
             print Command.help
             return
 
-        self.reset_sample_data()
+        # self.reset_sample_data()
 
         with open(args[0], "wb") as outfile:
             writer = csv.DictWriter(outfile,
-                                    Command.CSV_TO_MODEL_FIELDS,
+                                    Command.FIELDS,
                                     delimiter="\t",
                                     quoting=csv.QUOTE_MINIMAL,
                                     extrasaction='ignore')
             writer.writeheader()
-            for tcu in TestCenterUser.objects.order_by('id'):
-                record = dict((csv_field, getattr(tcu, model_field))
-                              for csv_field, model_field
-                              in Command.CSV_TO_MODEL_FIELDS.items())
-                record["LastUpdate"] = record["LastUpdate"].strftime("%Y/%m/%d %H:%M:%S")
+            for tcu in TestCenterUser.objects.order_by('id')[:5]:
+                record = defaultdict(
+                    lambda: "",
+                    AuthorizationTransactionType="Add",
+                    ClientAuthorizationID=generate_id(),
+                    ClientCandidateID=tcu.client_candidate_id,
+                    ExamAuthorizationCount="1",
+                    ExamSeriesCode="MIT 6.002-001",
+                    EligibilityApptDateFirst="2012/12/15",
+                    EligibilityApptDateLast="2012/12/30",
+                    LastUpdate=datetime.utcnow().strftime("%Y/%m/%d %H:%M:%S")
+                )
                 writer.writerow(record)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def reset_sample_data(self):
         def make_sample(**kwargs):
             data = dict((model_field, kwargs.get(model_field, ""))
                         for model_field in Command.CSV_TO_MODEL_FIELDS.values())
             return TestCenterUser(**data)
-        
-        def generate_id():
-            return "edX{:012}".format(uuid.uuid4().int % (10**12))
         
         # TestCenterUser.objects.all().delete()
         
