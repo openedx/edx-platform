76f0cf4a861da84796e448477c98496de7843b3c
Victor Shnayder <victor@mitx.mit.edu>
2012-10-30 18:23:07 -0400

    remove (almost) all references to askbot.

--- lms/urls.py
+++ lms/urls.py
@@ -1,13 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from django.conf.urls.static import static
 import django.contrib.auth.views
-
-# Uncomment the next two lines to enable the admin:
-if settings.DEBUG:
-    from django.contrib import admin
-    admin.autodiscover()
 
 urlpatterns = ('',
     url(r'^$', 'branding.views.index', name="root"), # Main marketing page, or redirect to courseware
@@ -246,20 +240,9 @@ if settings.QUICKEDIT:
     urlpatterns += (url(r'^quickedit/(?P<id>[^/]*)$', 'dogfood.views.quickedit'),)
     urlpatterns += (url(r'^dogfood/(?P<id>[^/]*)$', 'dogfood.views.df_capa_problem'),)
 
-if settings.ASKBOT_ENABLED:
-    urlpatterns += (url(r'^%s' % settings.ASKBOT_URL, include('askbot.urls')), \
-                    url(r'^settings/', include('askbot.deps.livesettings.urls')), \
-                    url(r'^followit/', include('followit.urls')), \
-#                       url(r'^robots.txt$', include('robots.urls')),
-                              )
-
-
-
 if settings.DEBUG:
-    ## Jasmine and admin
-    urlpatterns=urlpatterns + (url(r'^_jasmine/', include('django_jasmine.urls')),
-                    url(r'^admin/', include(admin.site.urls)),
-                    )
+    ## Jasmine
+    urlpatterns=urlpatterns + (url(r'^_jasmine/', include('django_jasmine.urls')),)
 
 if settings.MITX_FEATURES.get('AUTH_USE_OPENID'):
     urlpatterns += (
