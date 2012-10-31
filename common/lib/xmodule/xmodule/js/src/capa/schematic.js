2b5e55b6623af972b7b403a4e82d6771759ce2c8
Don Mitchell <dmitchell@edx.org>
2012-10-31 17:29:19 -0400

    Use try/catch to detect undefined globals for canvas

--- common/lib/xmodule/xmodule/js/src/capa/schematic.js
+++ common/lib/xmodule/xmodule/js/src/capa/schematic.js
@@ -3339,23 +3339,28 @@ schematic = (function() {
 	}
 
 	// add method to canvas to compute relative coords for event
-	if (HTMLCanvasElement) HTMLCanvasElement.prototype.relMouseCoords = function(event){
-	    // run up the DOM tree to figure out coords for top,left of canvas
-	    var totalOffsetX = 0;
-	    var totalOffsetY = 0;
-	    var currentElement = this;
-	    do {
-		totalOffsetX += currentElement.offsetLeft;
-		totalOffsetY += currentElement.offsetTop;
-	    }
-	    while (currentElement = currentElement.offsetParent);
-
-	    // now compute relative position of click within the canvas
-	    this.mouse_x = event.pageX - totalOffsetX;
-	    this.mouse_y = event.pageY - totalOffsetY;
-
-	    this.page_x = event.pageX;
-	    this.page_y = event.pageY;
+	try {
+		if (HTMLCanvasElement)
+		 HTMLCanvasElement.prototype.relMouseCoords = function(event){
+		    // run up the DOM tree to figure out coords for top,left of canvas
+		    var totalOffsetX = 0;
+		    var totalOffsetY = 0;
+		    var currentElement = this;
+		    do {
+			totalOffsetX += currentElement.offsetLeft;
+			totalOffsetY += currentElement.offsetTop;
+		    }
+		    while (currentElement = currentElement.offsetParent);
+	
+		    // now compute relative position of click within the canvas
+		    this.mouse_x = event.pageX - totalOffsetX;
+		    this.mouse_y = event.pageY - totalOffsetY;
+	
+		    this.page_x = event.pageX;
+		    this.page_y = event.pageY;
+		 }
+	}
+	catch (err) { // ignore
 	}
 
 	///////////////////////////////////////////////////////////////////////////////
@@ -4091,48 +4096,52 @@ schematic = (function() {
 
 	// add dashed lines!
 	// from http://davidowens.wordpress.com/2010/09/07/html-5-canvas-and-dashed-lines/
-	CanvasRenderingContext2D.prototype.dashedLineTo = function(fromX, fromY, toX, toY, pattern) {
-	    // Our growth rate for our line can be one of the following:
-	    //   (+,+), (+,-), (-,+), (-,-)
-	    // Because of this, our algorithm needs to understand if the x-coord and
-	    // y-coord should be getting smaller or larger and properly cap the values
-	    // based on (x,y).
-	    var lt = function (a, b) { return a <= b; };
-	    var gt = function (a, b) { return a >= b; };
-	    var capmin = function (a, b) { return Math.min(a, b); };
-	    var capmax = function (a, b) { return Math.max(a, b); };
-
-	    var checkX = { thereYet: gt, cap: capmin };
-	    var checkY = { thereYet: gt, cap: capmin };
-
-	    if (fromY - toY > 0) {
-		checkY.thereYet = lt;
-		checkY.cap = capmax;
-	    }
-	    if (fromX - toX > 0) {
-		checkX.thereYet = lt;
-		checkX.cap = capmax;
-	    }
-
-	    this.moveTo(fromX, fromY);
-	    var offsetX = fromX;
-	    var offsetY = fromY;
-	    var idx = 0, dash = true;
-	    while (!(checkX.thereYet(offsetX, toX) && checkY.thereYet(offsetY, toY))) {
-		var ang = Math.atan2(toY - fromY, toX - fromX);
-		var len = pattern[idx];
-
-		offsetX = checkX.cap(toX, offsetX + (Math.cos(ang) * len));
-		offsetY = checkY.cap(toY, offsetY + (Math.sin(ang) * len));
-
-		if (dash) this.lineTo(offsetX, offsetY);
-		else this.moveTo(offsetX, offsetY);
-
-		idx = (idx + 1) % pattern.length;
-		dash = !dash;
-	    }
-	};
-
+	try {
+		if (CanvasRenderingContext2D)
+			CanvasRenderingContext2D.prototype.dashedLineTo = function(fromX, fromY, toX, toY, pattern) {
+			    // Our growth rate for our line can be one of the following:
+			    //   (+,+), (+,-), (-,+), (-,-)
+			    // Because of this, our algorithm needs to understand if the x-coord and
+			    // y-coord should be getting smaller or larger and properly cap the values
+			    // based on (x,y).
+			    var lt = function (a, b) { return a <= b; };
+			    var gt = function (a, b) { return a >= b; };
+			    var capmin = function (a, b) { return Math.min(a, b); };
+			    var capmax = function (a, b) { return Math.max(a, b); };
+		
+			    var checkX = { thereYet: gt, cap: capmin };
+			    var checkY = { thereYet: gt, cap: capmin };
+		
+			    if (fromY - toY > 0) {
+				checkY.thereYet = lt;
+				checkY.cap = capmax;
+			    }
+			    if (fromX - toX > 0) {
+				checkX.thereYet = lt;
+				checkX.cap = capmax;
+			    }
+		
+			    this.moveTo(fromX, fromY);
+			    var offsetX = fromX;
+			    var offsetY = fromY;
+			    var idx = 0, dash = true;
+			    while (!(checkX.thereYet(offsetX, toX) && checkY.thereYet(offsetY, toY))) {
+				var ang = Math.atan2(toY - fromY, toX - fromX);
+				var len = pattern[idx];
+		
+				offsetX = checkX.cap(toX, offsetX + (Math.cos(ang) * len));
+				offsetY = checkY.cap(toY, offsetY + (Math.sin(ang) * len));
+		
+				if (dash) this.lineTo(offsetX, offsetY);
+				else this.moveTo(offsetX, offsetY);
+		
+				idx = (idx + 1) % pattern.length;
+				dash = !dash;
+			    }
+			};
+	}
+	catch (err) { //noop
+	}
 	// given a range of values, return a new range [vmin',vmax'] where the limits
 	// have been chosen "nicely".  Taken from matplotlib.ticker.LinearLocator
 	function view_limits(vmin,vmax) {
