6b28b70e51293637c006f8bd5dddeda5b57db621
Calen Pennington <calen.pennington@gmail.com>
2012-11-06 16:12:19 -0500

    Add jenkins files for builds

---
+++ jenkins/quality.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Reset the submodule, in case it changed
+git submodule foreach 'git reset --hard HEAD'
+
+# Set the IO encoding to UTF-8 so that askbot will start
+export PYTHONIOENCODING=UTF-8
+
+rake clobber
+rake pep8 || echo "pep8 failed, continuing"
+rake pylint || echo "pylint failed, continuing"
