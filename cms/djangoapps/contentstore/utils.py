c3c6a7af7e635568970456d9a812c1d3dd3f386f
Chris Dodge <cdodge@edx.org>
2012-11-08 13:29:46 -0500

    address feedback from cale

--- cms/djangoapps/contentstore/utils.py
+++ cms/djangoapps/contentstore/utils.py
@@ -33,31 +33,6 @@ def get_course_location_for_item(location):
 
     return location
 
-def get_course_for_item(location):
-    '''
-    cdodge: for a given Xmodule, return the course that it belongs to
-    NOTE: This makes a lot of assumptions about the format of the course location
-    Also we have to assert that this module maps to only one course item - it'll throw an
-    assert if not
-    '''
-    item_loc = Location(location)
-
-    # @hack! We need to find the course location however, we don't
-    # know the 'name' parameter in this context, so we have
-    # to assume there's only one item in this query even though we are not specifying a name
-    course_search_location = ['i4x', item_loc.org, item_loc.course, 'course', None]
-    courses = modulestore().get_items(course_search_location)
-
-    # make sure we found exactly one match on this above course search
-    found_cnt = len(courses)
-    if found_cnt == 0:
-        raise BaseException('Could not find course at {0}'.format(course_search_location))
-
-    if found_cnt > 1:
-        raise BaseException('Found more than one course at {0}. There should only be one!!! Dump = {1}'.format(course_search_location, courses))
-
-    return courses[0]
-
 
 def get_lms_link_for_item(location, preview=False):
     location = Location(location)
