6d45445aef1521b27ba09721c466927336a47807
Chris Dodge <cdodge@edx.org>
2012-11-14 11:14:30 -0500

    assume that users are putting caption information in /static/subs. We'll enforce this on the xlint utility

--- common/lib/xmodule/xmodule/video_module.py
+++ common/lib/xmodule/xmodule/video_module.py
@@ -97,11 +97,11 @@ class VideoModule(XModule):
 
     def get_html(self):
         if isinstance(modulestore(), MongoModuleStore) :
-            caption_asset_path = StaticContent.get_base_url_path_for_course_assets(self.location)
+            caption_asset_path = StaticContent.get_base_url_path_for_course_assets(self.location) + '/subs_'
         else:
             # VS[compat]
             # cdodge: filesystem static content support.
-            caption_asset_path = "/static/{0}/subs".format(self.metadata['data_dir'])
+            caption_asset_path = "/static/{0}/subs/".format(self.metadata['data_dir'])
 
         return self.system.render_template('video.html', {
             'streams': self.video_list(),
