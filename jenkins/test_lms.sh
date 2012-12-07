5a3cdc11cd2aaf7bb2602b260fac6168e06f9534
Calen Pennington <calen.pennington@gmail.com>
2012-12-07 10:41:24 -0500

    Only mark github commit status when full test suite passes

--- jenkins/test_lms.sh
+++ jenkins/test_lms.sh
@@ -3,12 +3,6 @@
 set -e
 set -x
 
-source jenkins/base.sh
-
-github_mark_failed_on_exit
-github_status state:pending "is running"
-
-
 # Reset the submodule, in case it changed
 git submodule foreach 'git reset --hard HEAD'
 
@@ -31,5 +25,3 @@ rake coverage:xml coverage:html
 
 [ $TESTS_FAILED == '0' ]
 rake autodeploy_properties
-
-github_status state:success "passed"
