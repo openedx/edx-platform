e75f63e957e64f73d968016436cb0c887f14ed07
Don Mitchell <dmitchell@edx.org>
2012-12-12 17:13:55 -0500

    More merge repairs. Hopefully done.

--- cms/static/js/template_loader.js
+++ cms/static/js/template_loader.js
@@ -5,7 +5,7 @@
 	if (typeof window.templateLoader == 'function') return;
 	
     var templateLoader = {
-      templateVersion: "0.0.8",
+      templateVersion: "0.0.9",
       templates: {},
       loadRemoteTemplate: function(templateName, filename, callback) {
         if (!this.templates[templateName]) {
