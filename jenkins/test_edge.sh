c4e86140c19e9c5c5f95af02d8a1e442f76289fb
Christina Roberts <christina@edxubuntu.(none)>
2012-12-13 09:26:46 -0500

    Try running xmodule Jasmine tests on Jenkins (again!).

--- jenkins/test_edge.sh
+++ jenkins/test_edge.sh
@@ -23,6 +23,7 @@ rake test_common/lib/capa || TESTS_FAILED=1
 rake test_common/lib/xmodule || TESTS_FAILED=1
 rake phantomjs_jasmine_lms || true
 rake phantomjs_jasmine_cms || true
+rake phantomjs_jasmine_common/lib/xmodule || true
 rake coverage:xml coverage:html
 
 [ $TESTS_FAILED == '0' ]
