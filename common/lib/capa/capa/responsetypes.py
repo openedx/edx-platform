cc78ebe329781cbd1a3445800f8d2f697be30ae2
Alexander Kryklia <kryklia@gmail.com>
2012-12-21 12:43:58 +0200

    rename tag

--- common/lib/capa/capa/responsetypes.py
+++ common/lib/capa/capa/responsetypes.py
@@ -871,7 +871,7 @@ def sympy_check2():
 
     allowed_inputfields = ['textline', 'textbox', 'crystallography',
                             'chemicalequationinput', 'vsepr_input',
-                            'drag_and_drop']
+                            'drag_and_drop_input']
 
     def setup_response(self):
         xml = self.xml
