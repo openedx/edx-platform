b39c2d5e32a2ff8e8452a2b0c73a315945fe23cf
ichuang <ichuang@mit.edu>
2013-01-03 00:49:57 +0000

    turn on tracking (helps with debugging)

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -179,6 +179,9 @@ LANGUAGE_CODE = 'en'            # http://www.i18nguy.com/unicode/language-identi
 USE_I18N = True
 USE_L10N = True
 
+# Tracking
+TRACK_MAX_EVENT = 10000
+
 # Messages
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
@@ -282,6 +285,9 @@ INSTALLED_APPS = (
     'auth',
     'student',  # misleading name due to sharing with lms
 
+    # tracking
+    'track',
+    
     # For asset pipelining
     'pipeline',
     'staticfiles',
