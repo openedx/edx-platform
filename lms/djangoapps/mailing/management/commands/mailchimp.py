8ab4a378db2d514704b4bd0ce2fd1c1ab52353fd
Carlos Andrés Rocha <rocha@edx.org>
2013-01-11 18:04:17 -0500

    Fix incomplete results when retrieving list members
    Also speedup queries significantly

--- lms/djangoapps/mailing/management/commands/mailchimp.py
+++ lms/djangoapps/mailing/management/commands/mailchimp.py
@@ -1,16 +1,17 @@
 import logging
+import itertools
 from optparse import make_option
+from collections import namedtuple
+
 
 from django.core.management.base import BaseCommand, CommandError
 
 from mailsnake import MailSnake
 
 from student.models import UserProfile, unique_id_for_user
-# from xmodule.course_module import CourseDescriptor
-# from xmodule.modulestore.django import modulestore
 
 
-BATCH_SIZE = 2000
+BATCH_SIZE = 5000
 
 log = logging.getLogger('edx.mailchimp')
 
@@ -23,9 +24,11 @@ class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
         make_option('--key', action='store', help='mailchimp api key'),
-        make_option('--list', action='store', dest='list_id', help='mailchimp list id'),
-        make_option('--course', action='store', dest='course_id', help='xmodule course_id'),
-        )
+        make_option('--list', action='store', dest='list_id',
+                    help='mailchimp list id'),
+        make_option('--course', action='store', dest='course_id',
+                    help='xmodule course_id'),
+    )
 
     def parse_options(self, options):
         if not options['key']:
@@ -48,15 +51,18 @@ class Command(BaseCommand):
 
         subscribed = get_subscribed(mailchimp, list_id)
         unsubscribed = get_unsubscribed(mailchimp, list_id)
+        cleaned = get_cleaned(mailchimp, list_id)
+        non_subscribed = unsubscribed.union(cleaned)
 
         enrolled = get_enrolled_students(course_id)
-        active = [s for s in enrolled if s.user.email not in unsubscribed]
+        active = enrolled.all().exclude(user__email__in=non_subscribed)
 
         data = get_student_data(course_id, active)
-        update_merge_tags(mailchimp, list_id, data)
-        update_grade_data(mailchimp, list_id, data)
 
-        enrolled_emails = set(s.user.email for s in enrolled)
+        update_merge_tags(mailchimp, list_id, data)
+        update_data(mailchimp, list_id, data)
+
+        enrolled_emails = set(enrolled.values_list('user__email', flat=True))
         non_enrolled_emails = list(subscribed.difference(enrolled_emails))
 
         unsubscribe(mailchimp, list_id, non_enrolled_emails)
@@ -77,56 +83,78 @@ def connect_mailchimp(key, list_id, course_id):
     log.debug('list name: %s' % list_name)
 
     # check that we are connecting to the correct list
-    parts = course_id.replace('_', ' ').replace('/',' ').split()
+    parts = course_id.replace('_', ' ').replace('/', ' ').split()
     count = sum(1 for p in parts if p in list_name)
     if count != 4:
-        log.debug(course_id)
-        log.debug(list_name)
+        log.info(course_id)
+        log.info(list_name)
         raise CommandError('course_id does not match list name')
 
     return mailchimp
 
 
 def get_student_data(course_id, students):
-    # store = modulestore()
-    # course_loc = CourseDescriptor.id_to_location(course_id)
-    # course = store.get_instance(course_id, course_loc)
+    # To speed the query, we won't retrieve the full User object, only
+    # two of its values. The namedtuple simulates the User object.
+    FakeUser = namedtuple('Fake', 'id username')
 
-    grades = []
-    for student in students:
-        student_email = student.user.email
-        entry = {'EMAIL': student_email,
-                 'FULLNAME': student.name.title()}
+    def make(v):
+        e = {'EMAIL': v['user__email'],
+             'FULLNAME': v['name'].title()}
 
-        entry['UNIQUE_ID'] = unique_id_for_user(student.user)
+        e['UNIQUE_ID'] = unique_id_for_user(FakeUser(v['user_id'],
+                                                     v['user__username']))
+        return e
 
-        # student_grade = grade(student.user, None, course)
-        # for g in student_grade['section_breakdown']:
-        #     name = g['label'].upper()
-        #     entry[name] = g['percent']
+    fields = 'user__email', 'name', 'user_id', 'user__username'
+    values = students.values(*fields)
 
-        grades.append(entry)
-
-    return grades
+    return [make(s) for s in values]
 
 
 def get_enrolled_students(course_id):
-    students = UserProfile.objects.filter(user__courseenrollment__course_id=course_id)
+    objects = UserProfile.objects
+    students = objects.filter(user__courseenrollment__course_id=course_id)
     return students
 
 
-def get_unsubscribed(mailchimp, list_id):
-    unsubscribed = mailchimp.listMembers(id=list_id, status='unsubscribed')
-    return set(d['email'] for d in unsubscribed.get('data',[]))
-
-
 def get_subscribed(mailchimp, list_id):
-    unsubscribed = mailchimp.listMembers(id=list_id, status='subscribed')
-    return set(d['email'] for d in unsubscribed.get('data',[]))
+    return get_members(mailchimp, list_id, 'subscribed')
+
+
+def get_unsubscribed(mailchimp, list_id):
+    return get_members(mailchimp, list_id, 'unsubscribed')
+
+
+def get_cleaned(mailchimp, list_id):
+    return get_members(mailchimp, list_id, 'cleaned')
+
+
+def get_members(mailchimp, list_id, status):
+    mc_get_members = mailchimp.listMembers
+    members = set()
+
+    for page in itertools.count():
+        response = mc_get_members(id=list_id,
+                                  status=status,
+                                  start=page,
+                                  limit=BATCH_SIZE)
+        data = response.get('data', [])
+
+        if not data:
+            break
+
+        members.update(d['email'] for d in data)
+
+    return members
 
 
 def unsubscribe(mailchimp, list_id, emails):
-    result = mailchimp.listBatchUnsubscribe(id=list_id, emails=emails, send_goodbye=False, delete_member=False)
+    batch_unsubscribe = mailchimp.listBatchUnsubscribe
+    result = batch_unsubscribe(id=list_id,
+                               emails=emails,
+                               send_goodbye=False,
+                               delete_member=False)
     log.debug(result)
 
 
@@ -138,32 +166,35 @@ def update_merge_tags(mailchimp, list_id, data):
     mc_vars = mailchimp.listMergeVars(id=list_id)
     mc_names = set(v['name'] for v in mc_vars)
 
+    mc_merge = mailchimp.listMergeVarAdd
+
     for name in names:
         tag = name_to_tag(name)
 
         # verify FULLNAME is present
         tags = [v['tag'] for v in mc_vars]
         if 'FULLNAME' not in tags:
-            result = mailchimp.listMergeVarAdd(id=list_id,
-                                               tag='FULLNAME',
-                                               name='Full Name',
-                                               options={'field_type':'text',
-                                                        'public': False})
+            result = mc_merge(id=list_id,
+                              tag='FULLNAME',
+                              name='Full Name',
+                              options={'field_type': 'text',
+                                       'public': False})
             log.debug(result)
 
         # add extra tags if not present
         if name not in mc_names and tag not in ['EMAIL', 'FULLNAME']:
             ftype = FIELD_TYPES.get(name, 'number')
-            result = mailchimp.listMergeVarAdd(id=list_id,
-                                               tag=tag,
-                                               name=name,
-                                               options={'field_type': ftype,
-                                                        'public': False})
+            result = mc_merge(id=list_id,
+                              tag=tag,
+                              name=name,
+                              options={'field_type': ftype,
+                                       'public': False})
             log.debug(result)
 
 
-def update_grade_data(mailchimp, list_id, data):
-    formated_data = list({name_to_tag(k):v for k, v in e.iteritems()} for e in data)
+def update_data(mailchimp, list_id, data):
+    format_entry = lambda e: {name_to_tag(k): v for k, v in e.iteritems()}
+    formated_data = list(format_entry(e) for e in data)
 
     # send the updates in batches of a fixed size
     for batch in batches(formated_data, BATCH_SIZE):
@@ -175,7 +206,7 @@ def update_grade_data(mailchimp, list_id, data):
 
 
 def name_to_tag(name):
-    return (name[:10] if len(name) > 10 else name).replace(' ','_').strip()
+    return (name[:10] if len(name) > 10 else name).replace(' ', '_').strip()
 
 
 def batches(iterable, size):
