fc09bbd0b4aa93a6c564db5da92c7af5bc95bef3
Alexander Kryklia <kryklia@gmail.com>
2013-01-11 16:11:13 +0200

    added funciton call

--- common/lib/xmodule/xmodule/js/src/graphical_slider_tool/state.js
+++ common/lib/xmodule/xmodule/js/src/graphical_slider_tool/state.js
@@ -238,7 +238,7 @@ define('State', ['logme'], function (logme) {
 
             for (c1 = 0; c1 < dynamicEl.length; c1++) {
                 if (dynamicEl[c1].el === null) {
-                    continue;
+                    dynamicEl[c1].func.apply(window, allParameterValues);
                 }
 
                 if (
