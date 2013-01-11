5cc88ec1adbc04b4a6459c8acd90209bb2e5adc9
ichuang <ichuang@mit.edu>
2013-01-10 23:25:29 -0500

    example course_id for compute_grades management command

--- lms/djangoapps/instructor/management/commands/compute_grades.py
+++ lms/djangoapps/instructor/management/commands/compute_grades.py
@@ -19,15 +19,17 @@ class Command(BaseCommand):
     help = "Compute grades for all students in a course, and store result in DB.\n"
     help += "Usage: compute_grades course_id_or_dir \n"
     help += "   course_id_or_dir: either course_id or course_dir\n"
+    help += 'Example course_id: MITx/8.01rq_MW/Classical_Mechanics_Reading_Questions_Fall_2012_MW_Section'
 
     def handle(self, *args, **options):
 
         print "args = ", args
 
-        course_id = 'MITx/8.01rq_MW/Classical_Mechanics_Reading_Questions_Fall_2012_MW_Section'
-
         if len(args)>0:
             course_id = args[0]
+        else:
+            print self.help
+            return
 
         try:
             course = get_course_by_id(course_id)
