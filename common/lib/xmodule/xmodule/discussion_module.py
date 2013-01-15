27b57bb48b636ec29b9e592f4be89731fd7d3dd9
Calen Pennington <calen.pennington@gmail.com>
2013-01-15 08:47:08 -0500

    Don't update settings based on data from content for discussion modules during init

--- common/lib/xmodule/xmodule/discussion_module.py
+++ common/lib/xmodule/xmodule/discussion_module.py
@@ -17,21 +17,12 @@ class DiscussionModule(XModule):
     discussion_target = String(scope=Scope.settings)
     sort_key = String(scope=Scope.settings)
 
-    data = String(help="XML definition of inline discussion", scope=Scope.content)
-
     def get_html(self):
         context = {
             'discussion_id': self.discussion_id,
         }
         return self.system.render_template('discussion/_discussion_module.html', context)
 
-    def __init__(self, *args, **kwargs):
-        XModule.__init__(self, *args, **kwargs)
-
-        xml_data = etree.fromstring(self.data)
-        self.discussion_id = xml_data.attrib['id']
-        self.title = xml_data.attrib['for']
-        self.discussion_category = xml_data.attrib['discussion_category']
 
 class DiscussionDescriptor(RawDescriptor):
     module_class = DiscussionModule
