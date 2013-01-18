bd6d1531266507bc76ed2cc7f6c029a37bfb225a
Diana Huang <dkh@edx.org>
2013-01-18 15:18:13 -0500

    Fix failing self-assessment unit tests

--- common/lib/xmodule/xmodule/tests/test_self_assessment.py
+++ common/lib/xmodule/xmodule/tests/test_self_assessment.py
@@ -4,8 +4,11 @@ import unittest
 
 from xmodule.self_assessment_module import SelfAssessmentModule
 from xmodule.modulestore import Location
+from lxml import etree
+import logging
 
 from . import test_system
+log = logging.getLogger("mitx.courseware")
 
 class SelfAssessmentTest(unittest.TestCase):
 
@@ -26,22 +29,40 @@ class SelfAssessmentTest(unittest.TestCase):
         state = json.dumps({'student_answers': ["Answer 1", "answer 2", "answer 3"],
                             'scores': [0, 1],
                             'hints': ['o hai'],
-                            'state': SelfAssessmentModule.ASSESSING,
+                            'state': SelfAssessmentModule.INITIAL,
                             'attempts': 2})
+
+        rubric = '''<rubric><rubric>
+            <category>
+			<description>Response Quality</description>
+			<option>The response is not a satisfactory answer to the question.  It either fails to address the question or does so in a limited way, with no evidence of higher-order thinking.</option>
+		    </category>
+             </rubric></rubric>'''
+
+        prompt = etree.XML("<prompt>Text</prompt>")
+        static_data = {
+                'max_attempts': 10,
+                'rubric': etree.XML(rubric),
+                'prompt': prompt,
+                'max_score': 1
+                }
 
         module = SelfAssessmentModule(test_system, self.location,
                                       self.definition, self.descriptor,
-                                      state, {}, metadata=self.metadata)
+                                      static_data, state, metadata=self.metadata)
 
         self.assertEqual(module.get_score()['score'], 0)
 
-        self.assertTrue('answer 3' in module.get_html())
-        self.assertFalse('answer 2' in module.get_html())
+        html = module.get_html(test_system)
+        log.debug("rendered html: {0}".format(html))
 
-        module.save_assessment({'assessment': '0'})
-        self.assertEqual(module.state, module.REQUEST_HINT)
 
-        module.save_hint({'hint': 'hint for ans 3'})
+        module.save_answer({'student_answer': "I am an answer"}, test_system)
+        self.assertEqual(module.state, module.ASSESSING)
+
+        module.save_assessment({'assessment': '0'}, test_system)
+        self.assertEqual(module.state, module.POST_ASSESSMENT)
+        module.save_hint({'hint': 'this is a hint'}, test_system)
         self.assertEqual(module.state, module.DONE)
 
         d = module.reset({})
@@ -49,6 +70,6 @@ class SelfAssessmentTest(unittest.TestCase):
         self.assertEqual(module.state, module.INITIAL)
 
         # if we now assess as right, skip the REQUEST_HINT state
-        module.save_answer({'student_answer': 'answer 4'})
-        module.save_assessment({'assessment': '1'})
+        module.save_answer({'student_answer': 'answer 4'}, test_system)
+        module.save_assessment({'assessment': '1'}, test_system)
         self.assertEqual(module.state, module.DONE)
