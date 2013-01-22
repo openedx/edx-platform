f4574c75db61af0db9a99d571b7569ad11a60d4e
Vik Paruchuri <vik@edx.org>
2013-01-22 10:54:14 -0500

    Proper config dictionary generation for controller query

--- lms/djangoapps/open_ended_grading/open_ended_util.py
+++ lms/djangoapps/open_ended_grading/open_ended_util.py
@@ -1,7 +1,12 @@
 from django.conf import settings
+import logging
+
+log=logging.getLogger(__name__)
 
 def get_controller_url():
-    peer_grading_url = settings.PEER_GRADING_INTERFACE
+    peer_grading_url = settings.PEER_GRADING_INTERFACE['url']
     split_url = peer_grading_url.split("/")
     controller_url = "http://" + split_url[2] + "/grading_controller"
-    return controller_url
+    controller_settings=settings.PEER_GRADING_INTERFACE
+    controller_settings['url'] = controller_url
+    return controller_settings
