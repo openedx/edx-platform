29c4e6c0660336b6b70ad41b75bd97f6e9615bce
Jay Zoldak <jzoldak@edx.org>
2013-01-23 16:03:25 -0500

    Do not run the lms jasmine tests on jenkins for now, until we fix them.

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -43,7 +43,9 @@ rake test_cms[false] || TESTS_FAILED=1
 rake test_lms[false] || TESTS_FAILED=1
 rake test_common/lib/capa || TESTS_FAILED=1
 rake test_common/lib/xmodule || TESTS_FAILED=1
-rake phantomjs_jasmine_lms || true
+# Don't run the lms jasmine tests for now because 
+# they mostly all fail anyhow
+# rake phantomjs_jasmine_lms || true
 rake phantomjs_jasmine_cms || TESTS_FAILED=1
 rake phantomjs_jasmine_common/lib/xmodule || TESTS_FAILED=1
 
