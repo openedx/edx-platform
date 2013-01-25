00776e55345f63723ea7c87ac7ea6c15af6a3da6
Alexander Kryklia <kryklia@gmail.com>
2013-01-25 14:15:27 +0200

    updated poll module status

--- common/lib/xmodule/xmodule/js/src/poll/poll_main.js
+++ common/lib/xmodule/xmodule/js/src/poll/poll_main.js
@@ -1,62 +1,147 @@
-// Wrapper for RequireJS. It will make the standard requirejs(), require(), and
-// define() functions from Require JS available inside the anonymous function.
-(function(requirejs, require, define) {
+(function (requirejs, require, define) { define('PollMain', ['logme'], function (logme) { return {
 
-  // Even though it is not explicitly in this module, we have to specify
-  // 'GeneralMethods' as a dependency. It expands some of the core JS objects
-  // with additional useful methods that are used in other modules.
-  define("PollMain", ["logme"], function(logme) {
 
-    function PollMain(element) {
-      logme(element);
-      // alert("PollJSConstructor");
+// class @PollModule
+//   constructor: (element) ->
+//     @el = element
+//     @ajaxUrl = @$('.container').data('url')
+//     @$('.upvote').on('click', () => $.postWithPrefix(@url('upvote'), @handleVote))
+//     @$('.downvote').on('click', () => $.postWithPrefix(@url('downvote'), @handleVote))
 
-      this.reinitialize(element);
+//   $: (selector) -> $(selector, @el)
+
+//   url: (target) -> "#{@ajaxUrl}/#{target}"
+
+//   handleVote: (response) =>
+//     @$('.container').replaceWith(response.results)
+
+'submitAnswer': function (event, pollObj, vote_type) {
+
+    logme('We are sending the following data: ' + vote_type);
+
+    // Send the data to the server as an AJAX request. Attach a callback that will
+    // be fired on server's response.
+    $.postWithPrefix(
+        pollObj.ajax_url + '/submit_answer',  vote_type,
+        function (response) {
+            var color;
+
+            logme('The following response was received: ' + JSON.stringify(response));
+
+            // Show the answer from server.
+            pollObj.graph_answer.show();
+
+            // Show the next poll in series, and disable the current poll's submit and radio buttons.
+            if (pollObj.nextPollObj !== null) {
+                pollObj.nextPollObj.element.show();
+            }
+
+            function disableClick (event) {
+                event.preventDefault();
+                return false;
+            }
+            pollObj.upvote.click(disableClick);
+            pollObj.downvote.click(disableClick);
+
+            jQuery.plot(
+                pollObj.graph_answer,
+                [
+                    [[1, response.upvotes / (response.upvotes + response.downvotes)   ]],
+                    [[2, response.downvotes / (response.upvotes + response.downvotes) ]]
+                ],
+                {
+                    'xaxis': {
+                        'min': 0,
+                        'max': 3,
+                        'tickFormatter': function formatter(val, axis) {
+                            var valStr;
+
+                            valStr = val.toFixed(axis.tickDecimals);
+
+                            if (valStr === '1.0') {
+                                return 'Yes / Upvote';
+                            } else if (valStr === '2.0') {
+                                return 'No / Downvote';
+                            } else { return ''; }
+                        }
+                    },
+                    'yaxis': {
+                        'min': 0,
+                        'max': 100,
+                        'tickFormatter': function formatter(val, axis) {
+                            return val.toFixed(axis.tickDecimals) + ' %';
+                        }
+                    },
+                    'lines':  {  'show': false  },
+                    'points': {  'show': false  },
+                    'bars': {
+                        'show': true,
+                        'align': 'center',
+                        'barWidth': 0.5
+                    }
+                }
+            );
+        }
+    );
+},
+
+'initialize': function (element) {
+    var _this, prevPollObj;
+
+    if (element.attr('poll_main_processed') === 'true') {
+        // This element was already processed once.
+        return;
     }
 
-    PollMain.prototype.submit_answer = function (event, _this) {
-      // alert('answer');
-      _this.status_item = $(event.target).parent().parent().find(
-                                           'input:checked').val();
-      _this.data = {
-        'answer': _this.status_item
-      };
-      logme( "" + _this.ajax_url + "/submit_answer");
-      $.postWithPrefix(
-        "" + _this.ajax_url + "/submit_answer",
-        _this.data,
-        function(response) {
-          if (response.success) {
-            // alert("Success");
-            _this.element.find(".hidden").attr('style',
-               'display: block !important; visibility: visible; border: 1px solid green;');
-          } else {
-            // alert("No success");
-            _this.element.find(".hidden").attr('style',
-               'display: block !important; visibility: visible; border: 1px solid red;');
-          }
+    // Make sure that next time we will not process this element a second time.
+    element.attr('poll_main_processed', 'true');
+
+    // Access PollMain instance inside inner functions created by $.each() iterator.
+    _this = this;
+
+    // Helper object which will help create a chain of poll objects.
+    // Initially there is no previous poll object, so we initialize this reference to null.
+    prevPollObj = null;
+
+    element.children('.polls').each(function (index, value) {
+        var pollObj;
+
+        // Poll object with poll configuration and properties.
+        pollObj = {
+            'element': $(value), // Current poll DOM element (jQuery object).
+            'id': $(value).prop('id'), // ID of DOM element with current poll.
+            'pollId': element.prop('id'), // ID of DOM element which contains all polls.
+            'ajax_url': element.data('ajax-url'),
+            'upvote': $(value).find('.upvote'),
+            'downvote': $(value).find('.downvote'),
+            'vote_blocks': $(value).find('.vote_blocks'),
+            'graph_answer': $(value).find('.graph_answer')
+        };
+
+        pollObj.graph_answer.css({
+                'width': 400,
+                'height': 400,
+                'margin-left': 'auto',
+                'margin-right': 'auto',
+                'margin-bottom': 15
+            });
+
+        // Set up a reference to current poll object in previous poll object.
+        // Reference to next poll object is initialized to null.
+        if (prevPollObj !== null) {
+            prevPollObj.nextPollObj = pollObj;
         }
-      );
-    };
+        prevPollObj = pollObj;
+        pollObj.nextPollObj = null;
 
-    PollMain.prototype.reinitialize = function (element) {
-      var _this = this;
-      // alert('Reinit');
-      this.element = element;
-      this.id = element.data("id");
-      this.ajax_url = element.data("ajax-url");
-      this.state = element.data("state");
-      this.answer_button = element.find(".submit-button");
-      this.answer_button.click(function (event) {
-        // logme('123');
-        _this.submit_answer(event, _this);
-      });
-    };
-    return PollMain;
-  });
-// End of wrapper for RequireJS. As you can see, we are passing
-// namespaced Require JS variables to an anonymous function. Within
-// it, you can use the standard requirejs(), require(), and define()
-// functions as if they were in the global namespace.
-}(RequireJS.requirejs, RequireJS.require, RequireJS.define));
+        // Attach a handler to the submit button, which will pass the current poll object.
+        pollObj.upvote.click(function (event) {
+            _this.submitAnswer(event, pollObj, 'upvote');
+        });
+        pollObj.downvote.click(function (event) {
+            _this.submitAnswer(event, pollObj,'downvote');
+        });
+    });
+}
 
+}; }); }(RequireJS.requirejs, RequireJS.require, RequireJS.define));
--- common/lib/xmodule/xmodule/poll_module.py
+++ common/lib/xmodule/xmodule/poll_module.py
@@ -1,24 +1,58 @@
+"""
+Poll module is ungraded xmodule used by students to
+to do set of polls.
+
+Poll module contains a nummber of 2 - steps basic sequences. Every sequence
+has data from all previous sequences. Selection of sequences can be in
+control block.
+
+Basic sequence:
+
+0. Control block
+    a) get data from any previous sequence
+    b) if block
+    c) link to sequence
+
+
+1. First. - must be, always visible
+If student does not yet anwered - Question
+If stundent have not answered - Question with statistics (yes/no)
+
+2. Second - optional, if student does not yet answered on 1st - hidden
+If student answers first time - show plot with statistics from
+answer from other users.
+
+"""
+
 import json
 import logging
 
-from lxml import etree
-from pkg_resources import resource_string, resource_listdir
+from lxml import html, etree
+from pkg_resources import resource_string
 
 from xmodule.x_module import XModule
-from xmodule.raw_module import RawDescriptor
-from xblock.core import Integer, Scope, Boolean
+from xmodule.stringify import stringify_children
+from xmodule.mako_module import MakoModuleDescriptor
+from xmodule.xml_module import XmlDescriptor
+from xblock.core import Integer, Scope  # , Boolean
 
 log = logging.getLogger(__name__)
 
 
 class PollModule(XModule):
-
-    js = {'coffee': [resource_string(__name__, 'js/src/poll/display.coffee')]}
-    js_module_name = "PollModule"
+    ''' Poll Module
+    '''
+    js = {
+      'coffee': [resource_string(__name__, 'js/src/javascript_loader.coffee')],
+      'js': [resource_string(__name__, 'js/src/poll/poll.js'),
+             resource_string(__name__, 'js/src/poll/poll_main.js')]
+         }
+    css = {'scss': [resource_string(__name__, 'css/poll/display.scss')]}
+    js_module_name = "Poll"
 
     upvotes = Integer(help="Number of upvotes this poll has recieved", scope=Scope.content, default=0)
     downvotes = Integer(help="Number of downvotes this poll has recieved", scope=Scope.content, default=0)
-    voted = Boolean(help="Whether this student has voted on the poll", scope=Scope.student_state, default=False)
+    # voted = Boolean(help="Whether this student has voted on the poll", scope=Scope.student_state, default=False)
 
     def handle_ajax(self, dispatch, get):
         '''
@@ -26,29 +60,124 @@ class PollModule(XModule):
         TODO (vshnayder): This is not being called right now, so the position
         is not being saved.
         '''
-        if self.voted:
-            return json.dumps({'error': 'Already Voted!'})
+        # if self.voted:
+            # return json.dumps({'error': 'Already Voted!'})
+        if None:
+            pass
         elif dispatch == 'upvote':
             self.upvotes += 1
-            self.voted = True
-            return json.dumps({'results': self.get_html()})
+            # self.voted = True
+            return json.dumps({'upvotes': self.upvotes, 'downvotes': self.downvote})
         elif dispatch == 'downvote':
             self.downvotes += 1
-            self.voted = True
-            return json.dumps({'results': self.get_html()})
+            # self.voted = True
+            return json.dumps({'upvotes': self.upvotes, 'downvotes': self.downvote})
 
         return json.dumps({'error': 'Unknown Command!'})
 
     def get_html(self):
-        return self.system.render_template('poll.html', {
-            'upvotes': self.upvotes,
-            'downvotes': self.downvotes,
-            'voted': self.voted,
-            'ajax_url': self.system.ajax_url,
-        })
+        """ Renders parameters to template. """
+
+        self.html_id = self.location.html_id()
+        self.html_class = self.location.category
+        self.poll_units_list = []
+
+        params = {
+                  'element_html': self.definition['render'],
+                  'element_id': self.html_id,
+                  'element_class': self.html_class,
+                  'ajax_url': self.system.ajax_url,
+                  'poll_units': self.parse_sequence(self.definition['sequence']),
+                  }
+        self.content = self.system.render_template(
+                        'poll.html', params)
+        return self.content
+
+    def get_poll_unit_html(self, i, question):
+        """ """
+        return """
+<div id="poll_unit_{poll_number}" class="polls"> {question}
+  <div class="vote_and_submit">
+    <div id="vote_block-1" class="vote">
+      <ul>
+        <li>
+          <input type="radio" id="vote_{poll_number}" name="vote_{poll_number}" value="Yes" />
+            <label for="vote_{poll_number}">Yes</label>
+          </li>
+          <li>
+            <input type="radio" id="vote_{poll_number}" name="vote_{poll_number}" value="No" />
+            <label for="vote_{poll_number}">No</label>
+          </li>
+        </ul>
+      </div>
+
+    <div class="vote_submit_button">
+        <input type="button" value="Cast Your vote" class=".submit-button"/>
+    </div>
+  </div>
+
+  <div class="graph_answer"></div>
+</div>""".format(poll_number=i, question=question)
+
+    def parse_sequence(self, html_string):
+        """    substitute sequence     """
+        xml = html.fromstring(html_string)
+        poll_units = xml.xpath('//unit')
+        for i, poll_unit in enumerate(poll_units):
+            # import ipdb; ipdb.set_trace()
+            poll_unit_question = stringify_children(poll_unit.xpath('//question')[0])
+            poll_unit_html = html.fromstring(self.get_poll_unit_html(i,
+                                             poll_unit_question))
+
+            poll_unit_controls = (poll_unit.get('plot', "no"),
+                             poll_unit.get('next_yes', "end"),
+                             poll_unit.get('next_no', "end"))
+            self.poll_units_list.append((poll_unit_html,
+                                   poll_unit_controls))
+        # import ipdb; ipdb.set_trace()
 
 
-class PollDescriptor(RawDescriptor):
+class PollDescriptor(MakoModuleDescriptor, XmlDescriptor):
     module_class = PollModule
+    template_dir_name = 'poll'
     stores_state = True
-    template_dir_name = "poll"
+
+    @classmethod
+    def definition_from_xml(cls, xml_object, system):
+        """
+        Pull out the data into dictionary.
+
+        Args:
+            xml_object: xml from file.
+
+        Returns:
+            dict
+        """
+        # check for presense of required tags in xml
+        expected_children_level_0 = ['render', 'sequence']
+        for child in expected_children_level_0:
+            if len(xml_object.xpath(child)) != 1:
+                raise ValueError("Poll definition must include \
+                    exactly one '{0}' tag".format(child))
+
+        def parse(k):
+            """Assumes that xml_object has child k"""
+            return stringify_children(xml_object.xpath(k)[0])
+        return {
+                    'render': parse('render'),
+                    'sequence': parse('sequence')
+                }
+
+    def definition_to_xml(self, resource_fs):
+        '''Return an xml element representing this definition.'''
+        xml_object = etree.Element('graphical_slider_tool')
+
+        def add_child(k):
+            child_str = '<{tag}>{body}</{tag}>'.format(tag=k, body=self.definition[k])
+            child_node = etree.fromstring(child_str)
+            xml_object.append(child_node)
+
+        for child in ['render', 'sequence']:
+            add_child(child)
+
+        return
