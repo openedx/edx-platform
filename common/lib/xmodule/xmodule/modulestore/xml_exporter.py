cc09a90245786e016f2d70ae15089b0043ab6196
Chris Dodge <cdodge@edx.org>
2013-01-30 13:50:30 -0500

    DRY things up a bit

--- common/lib/xmodule/xmodule/modulestore/xml_exporter.py
+++ common/lib/xmodule/xmodule/modulestore/xml_exporter.py
@@ -18,24 +18,21 @@ def export_to_xml(modulestore, contentstore, course_location, root_dir, course_d
   contentstore.export_all_for_course(course_location, root_dir + '/' + course_dir + '/static/')
 
   # export the static tabs
-  static_tabs_query_loc = Location('i4x', course_location.org, course_location.course, 'static_tab', None)
-  static_tabs = modulestore.get_items(static_tabs_query_loc)
+  export_extra_content(export_fs, modulestore, course_location, 'static_tab', 'tabs', '.html')
 
-  if len(static_tabs) > 0:
-    tab_dir = export_fs.makeopendir('tabs')
-    for tab in static_tabs:
-      with tab_dir.open(tab.location.name + '.html', 'w') as tab_file:
-        tab_file.write(tab.definition['data'].encode('utf8'))
+  # export the custom tags
+  export_extra_content(export_fs, modulestore, course_location, 'custom_tag_template', 'custom_tags')
 
-  # export custom tags
-  custom_tags_query_loc = Location('i4x', course_location.org, course_location.course, 'custom_tag_template', None)
-  custom_tags = modulestore.get_items(custom_tags_query_loc)
 
-  if len(custom_tags) > 0:
-    tab_dir = export_fs.makeopendir('custom_tags')
-    for tag in custom_tags:
-      with tab_dir.open(tag.location.name, 'w') as tag_file:
-        tag_file.write(tag.definition['data'].encode('utf8')) 
+def export_extra_content(export_fs, modulestore, course_location, category_type, dirname, file_suffix = ''):
+  query_loc = Location('i4x', course_location.org, course_location.course, category_type, None)
+  items = modulestore.get_items(query_loc)
+
+  if len(items) > 0:
+    item_dir = export_fs.makeopendir(dirname)
+    for item in items:
+      with item_dir.open(item.location.name + file_suffix, 'w') as item_file:
+        item_file.write(item.definition['data'].encode('utf8'))
 
 
 
