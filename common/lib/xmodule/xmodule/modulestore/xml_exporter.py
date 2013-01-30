c676046341d8858f49c21e7229b1163a471256c7
Chris Dodge <cdodge@edx.org>
2013-01-30 13:25:17 -0500

    adding exporting of tabs and custom_tags. Also added unit tests

--- common/lib/xmodule/xmodule/modulestore/xml_exporter.py
+++ common/lib/xmodule/xmodule/modulestore/xml_exporter.py
@@ -17,4 +17,26 @@ def export_to_xml(modulestore, contentstore, course_location, root_dir, course_d
   # export the static assets
   contentstore.export_all_for_course(course_location, root_dir + '/' + course_dir + '/static/')
 
+  # export the static tabs
+  static_tabs_query_loc = Location('i4x', course_location.org, course_location.course, 'static_tab', None)
+  static_tabs = modulestore.get_items(static_tabs_query_loc)
+
+  if len(static_tabs) > 0:
+    tab_dir = export_fs.makeopendir('tabs')
+    for tab in static_tabs:
+      with tab_dir.open(tab.location.name + '.html', 'w') as tab_file:
+        tab_file.write(tab.definition['data'].encode('utf8'))
+
+  # export custom tags
+  custom_tags_query_loc = Location('i4x', course_location.org, course_location.course, 'custom_tag_template', None)
+  custom_tags = modulestore.get_items(custom_tags_query_loc)
+
+  if len(custom_tags) > 0:
+    tab_dir = export_fs.makeopendir('custom_tags')
+    for tag in custom_tags:
+      with tab_dir.open(tag.location.name, 'w') as tag_file:
+        tag_file.write(tag.definition['data'].encode('utf8')) 
+
+
+
   
