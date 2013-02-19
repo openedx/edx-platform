c1c050a6b6fee7dfc92c8d6df7e9e304e6d5f31a
Victor Shnayder <victor@mitx.mit.edu>
2013-02-19 17:35:40 -0500

    WIP on an inputtype for heroesX annotation
    - has a text area for saved but (currently) ungraded comments
    - has a set of tags with weights

---
+++ common/static/js/capa/annotationinput.js
@@ -0,0 +1,11 @@
+(function () {
+    var update = function() {
+             alert("o hi");
+    };
+
+    var inputs = $('.annotation-input input');
+    // update on load
+    inputs.each(update); 
+    // and on every change
+    inputs.bind("input", update);
+}).call(this);
