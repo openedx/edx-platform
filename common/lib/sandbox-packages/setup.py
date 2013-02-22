df17c0c7dde48e46ecf403e0412c5cf09087f689
Ned Batchelder <ned@nedbatchelder.com>
2013-02-22 06:27:36 -0500

    Move symmath into capa so that it's available where needed

--- common/lib/sandbox-packages/setup.py
+++ common/lib/sandbox-packages/setup.py
@@ -4,14 +4,11 @@ setup(
     name="sandbox-packages",
     version="0.1",
     packages=[
-        "symmath",
         "verifiers",
     ],
     py_modules=[
         "eia",
     ],
     install_requires=[
-        # symmath needs:
-        "sympy", "requests", "lxml",
     ],
 )
