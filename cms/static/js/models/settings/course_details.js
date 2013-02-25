bb0ad61f81b07779759f9a6fde4e15f8c1afe950
Don Mitchell <dmitchell@edx.org>
2013-02-25 12:13:50 -0500

    Somehow the saves changed to just sets w/ don't send data to the server.

--- cms/static/js/models/settings/course_details.js
+++ cms/static/js/models/settings/course_details.js
@@ -68,10 +68,10 @@ CMS.Models.Settings.CourseDetails = Backbone.Model.extend({
     save_videosource: function(newsource) {
         // newsource either is <video youtube="speed:key, *"/> or just the "speed:key, *" string
         // returns the videosource for the preview which iss the key whose speed is closest to 1
-        if (_.isEmpty(newsource) && !_.isEmpty(this.get('intro_video'))) this.set({'intro_video': null}); 
+        if (_.isEmpty(newsource) && !_.isEmpty(this.get('intro_video'))) this.save({'intro_video': null}); 
         // TODO remove all whitespace w/in string
         else {
-            if (this.get('intro_video') !== newsource) this.set('intro_video', newsource);
+            if (this.get('intro_video') !== newsource) this.save('intro_video', newsource);
         }
 
         return this.videosourceSample();
