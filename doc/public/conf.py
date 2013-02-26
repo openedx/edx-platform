eeddf89c34a85063ccff79bdfea7e3e5245e294c
David Ormsbee <dave@edx.org>
2013-02-26 12:21:19 -0500

    doc config changes to make things compatible with GitHub pages

--- doc/public/conf.py
+++ doc/public/conf.py
@@ -19,13 +19,12 @@ import sys, os
 #sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration -----------------------------------------------------
-
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
