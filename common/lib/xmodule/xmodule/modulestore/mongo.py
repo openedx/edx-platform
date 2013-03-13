cf4ea53d015b04f1fea146a974f9d049338776e7
Calen Pennington <calen.pennington@gmail.com>
2013-03-13 09:34:27 -0400

    Small fixes to mongo metadata translations

--- common/lib/xmodule/xmodule/modulestore/mongo.py
+++ common/lib/xmodule/xmodule/modulestore/mongo.py
@@ -153,9 +153,10 @@ class CachingDescriptorSystem(MakoDescriptorSystem):
                 )
                 definition = json_data.get('definition', {})
                 metadata = json_data.get('metadata', {})
-                for old_name, new_name in class_.metadata_tranlations.items():
-                    metadata[new_name] = metadata[old_name]
-                    del metadata[old_name]
+                for old_name, new_name in class_.metadata_translations.items():
+                    if old_name in metadata:
+                        metadata[new_name] = metadata[old_name]
+                        del metadata[old_name]
 
                 kvs = MongoKeyValueStore(
                     definition.get('data', {}),
