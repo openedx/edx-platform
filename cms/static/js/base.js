25acab497e05ab6d9883726d0cba2ec5146fa6ae
Brian Talbot <btalbot@edx.org>
2013-03-27 01:33:04 -0400

    studio - corrected JQ selector for smoothscrolling in-page links

--- cms/static/js/base.js
+++ cms/static/js/base.js
@@ -81,7 +81,7 @@ $(document).ready(function () {
     });
 
     // general link management - smooth scrolling page links
-    $('a[rel*="view"][href|="#"]').bind('click', smoothScrollLink);
+    $('a[rel*="view"][href^="#"]').bind('click', smoothScrollLink);
 
     // tender feedback window scrolling
     $('a.show-tender').bind('click', smoothScrollTop);
