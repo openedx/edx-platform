43f6f0ce8f9d704d9a4c970778ad8f02bec05c85
Mark L. Chang <mark.chang@gmail.com>
2013-04-03 00:26:12 -0400

    put survey behind MITX_FEATURES flag

--- cms/envs/dev.py
+++ cms/envs/dev.py
@@ -143,3 +143,6 @@ DEBUG_TOOLBAR_CONFIG = {
 # To see stacktraces for MongoDB queries, set this to True.
 # Stacktraces slow down page loads drastically (for pages with lots of queries).
 DEBUG_TOOLBAR_MONGO_STACKTRACES = False
+
+# disable NPS survey in dev mode
+MITX_FEATURES['STUDIO_NPS_SURVEY'] = False
