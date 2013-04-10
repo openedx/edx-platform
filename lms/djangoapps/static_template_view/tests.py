4b68e0282f4936a03f0d7705f9b16c31ce2d2cab
Sef Kloninger <sef@kloninger.com>
2013-04-10 02:16:35 -0400

    press releases: use generic matching rule
    This way instead of having to explicitly add releases to the urls.py
    file, we just add the relase to the static_templates/press_releases
    directory and the right thing happens.
    
    Getting rid of the current "feature" where /pressrelease redirects to
    the current press release.  It was broken, looks like it's unused.
    
    Limitation: release file name must match slug, but with underscores and
    all lower case.
    
    Why bother? I wanted to do something small to learn the edX toolchain.

--- lms/djangoapps/static_template_view/tests.py
+++ lms/djangoapps/static_template_view/tests.py
@@ -1,16 +1,53 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_render(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Render a normal page, like jobs
         """
-        self.assertEqual(1 + 1, 2)
+        response = self.client.get("/jobs")
+        self.assertEquals(response.status_code, 200)
+
+
+    def test_render_press_release(self):
+        """
+        Render press releases from generic URL match
+        """
+        # since I had to remap files, pedantically test all press releases
+        # published to date. Decent positive test while we're at it.
+        all_releases = ["/press/mit-and-harvard-announce-edx",
+                "/press/uc-berkeley-joins-edx",
+                "/press/edX-announces-proctored-exam-testing",
+                "/press/elsevier-collaborates-with-edx",
+                "/press/ut-joins-edx",
+                "/press/cengage-to-provide-book-content",
+                "/press/gates-foundation-announcement",
+                "/press/wellesley-college-joins-edx",
+                "/press/georgetown-joins-edx",
+                "/press/spring-courses",
+                "/press/lewin-course-announcement",
+                "/press/bostonx-announcement",
+                "/press/eric-lander-secret-of-life",
+                "/press/edx-expands-internationally",
+                "/press/xblock_announcement",
+                "/press/stanford-to-work-with-edx",
+            ]
+
+        for rel in all_releases:
+            response = self.client.get(rel)
+            self.assertNotContains(response, "PAGE NOT FOUND", status_code=200)
+
+        # should work with caps
+        response = self.client.get("/press/STANFORD-to-work-with-edx")
+        self.assertContains(response, "Stanford", status_code=200)
+
+        # negative test
+        response = self.client.get("/press/this-shouldnt-work")
+        self.assertEqual(response.status_code, 404)
+
