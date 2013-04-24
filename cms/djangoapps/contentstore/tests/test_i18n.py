dd71ae818a6bbc83992b94cccb7ed9adf9d1632f
Steve Strassmann <steve@strassmann.com>
2013-04-24 11:33:47 -0400

    ModuleStoreTestCase moved to new location

--- cms/djangoapps/contentstore/tests/test_i18n.py
+++ cms/djangoapps/contentstore/tests/test_i18n.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.test.client import Client
 
-from .utils import ModuleStoreTestCase
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 
 class InternationalizationTest(ModuleStoreTestCase):
     """
