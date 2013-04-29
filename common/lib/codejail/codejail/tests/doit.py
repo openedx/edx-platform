09fbbe7bfa2c16a33074b72de9e5fcf2eeb02de1
Ned Batchelder <ned@nedbatchelder.com>
2013-04-29 17:31:19 -0400

    Codejail is in its own repo now.

--- common/lib/codejail/codejail/tests/doit.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-print "This is doit.py!"
-print "My args are %r" % (sys.argv,)
--- common/lib/codejail/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="codejail",
-    version="0.1",
-    packages=['codejail'],
-)
