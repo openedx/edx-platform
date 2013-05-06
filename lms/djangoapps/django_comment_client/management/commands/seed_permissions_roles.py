c20dd7d90373227402f8b75356455c7d693ffa47
Chris Dodge <cdodge@edx.org>
2013-05-06 16:40:25 -0400

    forgot to remove enough stuff from the django admin path

--- lms/djangoapps/django_comment_client/management/commands/seed_permissions_roles.py
+++ lms/djangoapps/django_comment_client/management/commands/seed_permissions_roles.py
@@ -13,9 +13,4 @@ class Command(BaseCommand):
             raise CommandError("Too many arguments")
         course_id = args[0]
 
-        administrator_role = Role.objects.get_or_create(name="Administrator", course_id=course_id)[0]
-        moderator_role = Role.objects.get_or_create(name="Moderator", course_id=course_id)[0]
-        community_ta_role = Role.objects.get_or_create(name="Community TA", course_id=course_id)[0]
-        student_role = Role.objects.get_or_create(name="Student", course_id=course_id)[0]
-
         seed_permissions_roles(course_id)
