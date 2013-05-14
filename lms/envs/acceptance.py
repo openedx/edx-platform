36fb8f91e063b234c0b71cd323e2b4cc5595fe39
Will Daly <will@edx.org>
2013-05-14 12:45:38 -0400

    Acceptance tests now use the same mongo db as the unit test suite. The LMS/CMS acceptance tests both use the collection acceptance_modules

--- lms/envs/acceptance.py
+++ lms/envs/acceptance.py
@@ -13,7 +13,7 @@ modulestore_options = {
     'default_class': 'xmodule.raw_module.RawDescriptor',
     'host': 'localhost',
     'db': 'test_xmodule',
-    'collection': 'test_modulestore',
+    'collection': 'acceptance_modulestore',
     'fs_root': TEST_ROOT / "data",
     'render_template': 'mitxmako.shortcuts.render_to_string',
 }
