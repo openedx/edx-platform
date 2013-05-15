b2b6ca5f7ba5acd89bb8e2e0b4704db6d1edc2fa
Calen Pennington <calen.pennington@gmail.com>
2013-05-15 13:10:52 -0400

    Record junit reports of jasmine tests

--- cms/envs/jasmine.py
+++ cms/envs/jasmine.py
@@ -36,8 +36,13 @@ PIPELINE_JS['spec'] = {
 }
 
 JASMINE_TEST_DIRECTORY = PROJECT_ROOT + '/static/coffee'
+JASMINE_REPORT_DIR = os.environ.get('JASMINE_REPORT_DIR', 'reports/cms/jasmine')
+
+TEMPLATE_CONTEXT_PROCESSORS += ('settings_context_processor.context_processors.settings',)
+TEMPLATE_VISIBLE_SETTINGS = ('JASMINE_REPORT_DIR', )
 
 STATICFILES_DIRS.append(REPO_ROOT/'node_modules/phantom-jasmine/lib')
+STATICFILES_DIRS.append(REPO_ROOT/'node_modules/jasmine-reporters/src')
 
 # Remove the localization middleware class because it requires the test database
 # to be sync'd and migrated in order to run the jasmine tests interactively
@@ -45,4 +50,4 @@ STATICFILES_DIRS.append(REPO_ROOT/'node_modules/phantom-jasmine/lib')
 MIDDLEWARE_CLASSES = tuple(e for e in MIDDLEWARE_CLASSES \
                            if e != 'django.middleware.locale.LocaleMiddleware')
 
-INSTALLED_APPS += ('django_jasmine', )
+INSTALLED_APPS += ('django_jasmine', 'settings_context_processor')
--- lms/envs/jasmine.py
+++ lms/envs/jasmine.py
@@ -36,7 +36,12 @@ PIPELINE_JS['spec'] = {
 }
 
 JASMINE_TEST_DIRECTORY = PROJECT_ROOT + '/static/coffee'
+JASMINE_REPORT_DIR = os.environ.get('JASMINE_REPORT_DIR', 'reports/lms/jasmine')
+
+TEMPLATE_CONTEXT_PROCESSORS += ('settings_context_processor.context_processors.settings',)
+TEMPLATE_VISIBLE_SETTINGS = ('JASMINE_REPORT_DIR', )
 
 STATICFILES_DIRS.append(REPO_ROOT/'node_modules/phantom-jasmine/lib')
+STATICFILES_DIRS.append(REPO_ROOT/'node_modules/jasmine-reporters/src')
 
-INSTALLED_APPS += ('django_jasmine', )
+INSTALLED_APPS += ('django_jasmine', 'settings_context_processor')
