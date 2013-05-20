0e3756449046f18081de3b7b967d5b9642917de4
e0d <edward@indeterminate.org>
2013-05-20 12:01:25 -0400

    pep8 fixes

--- lms/djangoapps/notes/models.py
+++ lms/djangoapps/notes/models.py
@@ -21,9 +21,9 @@ class Note(models.Model):
     updated = models.DateTimeField(auto_now=True, db_index=True)
 
     def clean(self, json_body):
-        '''
+        """
         Cleans the note object or raises a ValidationError.
-        '''
+        """
         if json_body is None:
             raise ValidationError('Note must have a body.')
 
@@ -53,16 +53,16 @@ class Note(models.Model):
             self.tags = ",".join(tags)
 
     def get_absolute_url(self):
-        '''
+        """
         Returns the absolute url for the note object.
-        '''
+        """
         kwargs = {'course_id': self.course_id, 'note_id': str(self.pk)}
         return reverse('notes_api_note', kwargs=kwargs)
 
     def as_dict(self):
-        '''
+        """
         Returns the note object as a dictionary.
-        '''
+        """
         return {
             'id': self.pk,
             'user_id': self.user.pk,
