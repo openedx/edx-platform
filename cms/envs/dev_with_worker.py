cfe220a746b7faff79a4cbd92afdae7b961fd727
Calen Pennington <calen.pennington@gmail.com>
2013-05-21 13:07:04 -0400

    Add exceptions for unused imports in settings files

--- cms/envs/dev_with_worker.py
+++ cms/envs/dev_with_worker.py
@@ -8,6 +8,10 @@ The worker can be executed using:
 django_admin.py celery worker
 """
 
+# We intentionally define lots of variables that aren't used, and
+# want to import all variables from base settings files
+# pylint: disable=W0401, W0614
+
 from dev import *
 
 ################################# CELERY ######################################
