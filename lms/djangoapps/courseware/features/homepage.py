7fe206a9d2244c6741c27a62e897c1ea6bcd33e9
JonahStanley <Jonah_Stanley@brown.edu>
2013-05-30 14:23:49 -0400

    Sped up some Lettuce Tests

--- lms/djangoapps/courseware/features/homepage.py
+++ lms/djangoapps/courseware/features/homepage.py
@@ -2,11 +2,22 @@
 #pylint: disable=W0621
 
 from lettuce import world, step
-from nose.tools import assert_in
+from nose.tools import assert_in, assert_equals
 
 
-@step('I should see "([^"]*)" in the Partners section$')
-def i_should_see_partner(step, partner):
+@step(u'I should see the following Partners in the Partners section')
+def i_should_see_partner(step):
     partners = world.browser.find_by_css(".partner .name span")
     names = set(span.text for span in partners)
-    assert_in(partner, names)
+    for partner in step.hashes:
+        assert_in(partner['Partner'], names)
+
+
+@step(u'I should see the following links and ids')
+def should_see_a_link_called(step):
+    for link_id_pair in step.hashes:
+        link_id = link_id_pair['id']
+        text = link_id_pair['Link']
+        link = world.browser.find_by_id(link_id)
+        assert len(link) > 0
+        assert_equals(link.text, text)
