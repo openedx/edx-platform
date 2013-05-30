5e218389c9f4df6d4a56fa0d282083a55ce7d99f
Chris Dodge <cdodge@edx.org>
2013-05-30 14:27:50 -0400

    add ability to do failure testing. If a post passes in a '?fail_at=<num_bytes>' then the middleware will throw an exception

--- cms/djangoapps/contentstore/debug_file_uploader.py
+++ cms/djangoapps/contentstore/debug_file_uploader.py
@@ -3,8 +3,17 @@ import time
 
 
 class DebugFileUploader(FileUploadHandler):
+    def __init__(self, request=None):
+        super(DebugFileUploader, self).__init__(request)
+        self.count = 0
+
     def receive_data_chunk(self, raw_data, start):
         time.sleep(1)
+        self.count = self.count + len(raw_data)
+        fail_at = self.request.GET.get('fail_at', None)
+        if fail_at and self.count > fail_at:
+            raise Exception('Triggered fail')
+
         return raw_data
 
     def file_complete(self, file_size):
