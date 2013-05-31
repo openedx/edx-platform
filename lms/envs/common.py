6354afd57f126e49a5f6c777c5b60fd5e17876dc
Nate Hardison <natehardison@gmail.com>
2013-05-31 15:24:33 -0700

    Use a setting for the favicon path
    The `FAVICON_PATH` setting determines the location of the favicon for
    the site. It's automatically adjusted when a theme is enabled,
    establishing the convention that themes will place their favicon in
    `static/images/favicon.ico`.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -330,6 +330,8 @@ STATICFILES_DIRS = [
     PROJECT_ROOT / "static",
 ]
 
+FAVICON_PATH = 'images/favicon.ico'
+
 # Locale/Internationalization
 TIME_ZONE = 'America/New_York'   # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 LANGUAGE_CODE = 'en'   # http://www.i18nguy.com/unicode/language-identifiers.html
