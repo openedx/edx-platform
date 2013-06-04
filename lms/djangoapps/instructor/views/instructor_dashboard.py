8a3af8f6a40a9c9c5b508c5f0d27551cb3a46e1a
Miles Steele <msteele@edx.org>
2013-06-04 12:08:54 -0400

    add more info to course info section of instructor dash 2

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -31,6 +31,13 @@ def instructor_dashboard_2(request, course_id):
     instructor_access = has_access(request.user, course, 'instructor')   # an instructor can manage staff lists
     forum_admin_access = has_forum_access(request.user, course_id, FORUM_ROLE_ADMINISTRATOR)
 
+    section_data = {
+        'course_info':   _section_course_info(request, course_id),
+        'enrollment':    _section_enrollment(course_id),
+        'student_admin': _section_student_admin(course_id),
+        'data_download': _section_data_download(course_id),
+    }
+
     context = {
         'course': course,
         'staff_access': True,
@@ -42,6 +49,37 @@ def instructor_dashboard_2(request, course_id):
         'mitx_version': getattr(settings, 'MITX_VERSION_STRING', ''),
         'offline_grade_log': offline_grades_available(course_id),
         'cohorts_ajax_url': reverse('cohorts', kwargs={'course_id': course_id}),
+        'section_data': section_data
     }
 
     return render_to_response('courseware/instructor_dashboard_2.html', context)
+
+
+def _section_course_info(request, course_id):
+    course = get_course_with_access(request.user, course_id, 'staff', depth=None)
+
+    section_data = {}
+    section_data['course_id'] = course_id
+    section_data['display_name'] = course.display_name
+    section_data['has_started'] = course.has_started()
+    section_data['has_ended'] = course.has_ended()
+    section_data['grade_cutoffs'] = "[" + reduce(lambda memo, (letter, score): "{}: {}, ".format(letter, score) + memo , course.grade_cutoffs.items(), "")[:-2] + "]"
+    return section_data
+
+
+def _section_enrollment(course_id):
+    section_data = {}
+    section_data['placeholder'] = "Enrollment content."
+    return section_data
+
+
+def _section_student_admin(course_id):
+    section_data = {}
+    section_data['placeholder'] = "Student Admin content."
+    return section_data
+
+
+def _section_data_download(course_id):
+    section_data = {}
+    section_data['placeholder'] = "Data Download content."
+    return section_data
