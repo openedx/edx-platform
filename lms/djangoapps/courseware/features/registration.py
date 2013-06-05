584c11dc59f17c1acf50c8d9cc216473b8b09a79
Don Mitchell <dmitchell@edx.org>
2013-06-05 14:39:44 -0400

    Squelch pep8/pylint warnings

--- lms/djangoapps/courseware/features/registration.py
+++ lms/djangoapps/courseware/features/registration.py
@@ -7,10 +7,9 @@ from common import TEST_COURSE_ORG, TEST_COURSE_NAME
 
 
 @step('I register for the course "([^"]*)"$')
-def i_register_for_the_course(step, course):
+def i_register_for_the_course(_step, course):
     cleaned_name = TEST_COURSE_NAME.replace(' ', '_')
     url = django_url('courses/%s/%s/%s/about' % (TEST_COURSE_ORG, course, cleaned_name))
-    from nose.tools import set_trace; set_trace()
     world.browser.visit(url)
 
     intro_section = world.browser.find_by_css('section.intro')
@@ -21,19 +20,19 @@ def i_register_for_the_course(step, course):
 
 
 @step(u'I should see the course numbered "([^"]*)" in my dashboard$')
-def i_should_see_that_course_in_my_dashboard(step, course):
+def i_should_see_that_course_in_my_dashboard(_step, course):
     course_link_css = 'section.my-courses a[href*="%s"]' % course
     assert world.is_css_present(course_link_css)
 
 
 @step(u'I should NOT see the course numbered "([^"]*)" in my dashboard$')
-def i_should_not_see_that_course_in_my_dashboard(step, course):
+def i_should_not_see_that_course_in_my_dashboard(_step, course):
     course_link_css = 'section.my-courses a[href*="%s"]' % course
     assert not world.is_css_present(course_link_css)
 
 
 @step(u'I unregister for the course numbered "([^"]*)"')
-def i_unregister_for_that_course(step, course):
+def i_unregister_for_that_course(_step, course):
     unregister_css = 'section.info a[href*="#unenroll-modal"][data-course-number*="%s"]' % course
     world.css_click(unregister_css)
     button_css = 'section#unenroll-modal input[value="Unregister"]'
