c715edf57a6b76c127939e4fc2759cf1ab045cd3
cahrens <christina@edx.org>
2013-06-06 10:55:41 -0400

    Strip off https://

--- cms/djangoapps/contentstore/tests/test_utils.py
+++ cms/djangoapps/contentstore/tests/test_utils.py
@@ -24,6 +24,24 @@ class LMSLinksTestCase(TestCase):
         with mock.patch.dict('django.conf.settings.MITX_FEATURES', {'ENABLE_MKTG_SITE': False}):
             self.assertEquals(self.get_about_page_link(), "//localhost:8000/courses/mitX/101/test/about")
 
+    @override_settings(MKTG_URLS={'ROOT': 'http://www.dummy'})
+    def about_page_marketing_site_remove_http_test(self):
+        """ Get URL for about page, marketing root present, remove http://. """
+        with mock.patch.dict('django.conf.settings.MITX_FEATURES', {'ENABLE_MKTG_SITE': True}):
+            self.assertEquals(self.get_about_page_link(), "//www.dummy/courses/mitX/101/test/about")
+
+    @override_settings(MKTG_URLS={'ROOT': 'https://www.dummy'})
+    def about_page_marketing_site_remove_https_test(self):
+        """ Get URL for about page, marketing root present, remove https://. """
+        with mock.patch.dict('django.conf.settings.MITX_FEATURES', {'ENABLE_MKTG_SITE': True}):
+            self.assertEquals(self.get_about_page_link(), "//www.dummy/courses/mitX/101/test/about")
+
+    @override_settings(MKTG_URLS={'ROOT': 'www.dummyhttps://x'})
+    def about_page_marketing_site_https__edge_test(self):
+        """ Get URL for about page, only remove https:// at the beginning of the string. """
+        with mock.patch.dict('django.conf.settings.MITX_FEATURES', {'ENABLE_MKTG_SITE': True}):
+            self.assertEquals(self.get_about_page_link(), "//www.dummyhttps://x/courses/mitX/101/test/about")
+
     @override_settings(MKTG_URLS={})
     def about_page_marketing_urls_not_set_test(self):
         """ Error case. ENABLE_MKTG_SITE is True, but there is either no MKTG_URLS, or no MKTG_URLS Root property. """
