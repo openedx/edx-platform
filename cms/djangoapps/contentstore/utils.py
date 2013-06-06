9220a4f45940484a31edf61fb599f75d062ac9e4
cahrens <christina@edx.org>
2013-06-06 10:29:10 -0400

    Bullet-proofing for MKTG_URLS.

--- cms/djangoapps/contentstore/utils.py
+++ cms/djangoapps/contentstore/utils.py
@@ -4,6 +4,9 @@ from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
 from django.core.urlresolvers import reverse
 import copy
+import logging
+
+log = logging.getLogger(__name__)
 
 DIRECT_ONLY_CATEGORIES = ['course', 'chapter', 'sequential', 'about', 'static_tab', 'course_info']
 
@@ -108,9 +111,18 @@ def get_lms_link_for_about_page(location):
     Returns the url to the course about page from the location tuple.
     """
     if settings.MITX_FEATURES.get('ENABLE_MKTG_SITE', False):
-        # Root will be "www.edx.org". The complete URL will still not be exactly correct,
-        # but redirects exist from www.edx.org to get to the drupal course about page URL.
-        about_base = settings.MKTG_URLS.get('ROOT')
+        if not hasattr(settings, 'MKTG_URLS'):
+            log.exception("ENABLE_MKTG_SITE is True, but MKTG_URLS is not defined.")
+            about_base = None
+        else:
+            marketing_urls = settings.MKTG_URLS
+            if marketing_urls.get('ROOT', None) is None:
+                log.exception('There is no ROOT defined in MKTG_URLS')
+                about_base = None
+            else:
+                # Root will be "www.edx.org". The complete URL will still not be exactly correct,
+                # but redirects exist from www.edx.org to get to the drupal course about page URL.
+                about_base = marketing_urls.get('ROOT')
     elif settings.LMS_BASE is not None:
         about_base = settings.LMS_BASE
     else:
