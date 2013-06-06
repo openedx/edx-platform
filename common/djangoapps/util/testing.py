6c24694a7ce16eea38d6af31a414aef70fad928a
Calen Pennington <cale@edx.org>
2013-06-06 12:48:59 -0400

    Fix tests that vary urls.py
    Create a mixin class that can be used for tests that customize urls.py
    to force django to reload it, so that they don't break other tests.

---
+++ common/djangoapps/util/testing.py
@@ -0,0 +1,34 @@
+import sys
+
+from django.conf import settings
+from django.core.urlresolvers import clear_url_caches
+
+
+class UrlResetMixin(object):
+    """Mixin to reset urls.py before and after a test
+
+    Django memoizes the function that reads the urls module (whatever module
+    urlconf names). The module itself is also stored by python in sys.modules.
+    To fully reload it, we need to reload the python module, and also clear django's
+    cache of the parsed urls.
+
+    However, the order in which we do this doesn't matter, because neither one will
+    get reloaded until the next request
+
+    Doing this is expensive, so it should only be added to tests that modify settings
+    that affect the contents of urls.py
+    """
+
+    def _reset_urls(self, urlconf=None):
+        if urlconf is None:
+            urlconf = settings.ROOT_URLCONF
+
+        if urlconf in sys.modules:
+            reload(sys.modules[urlconf])
+        clear_url_caches()
+
+    def setUp(self):
+        """Reset django default urlconf before tests and after tests"""
+        super(UrlResetMixin, self).setUp()
+        self._reset_urls()
+        self.addCleanup(self._reset_urls)
