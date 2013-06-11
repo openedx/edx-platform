1602f592b4b22ec79cad85baa9e7f831048b77a9
Chris Dodge <cdodge@edx.org>
2013-06-11 14:32:33 -0400

    use better API

--- cms/static/js/base.js
+++ cms/static/js/base.js
@@ -411,12 +411,12 @@ function showFileSelectionMenu(e) {
 }
 
 function startUpload(e) {
-    file_input = $('.file-input').val().replace('C:\\fakepath\\', '');
-    if (file_input === '')
+    files = $('.file-input').get(0).files;
+    if (files.length === 0)
         return;
 
     $('.upload-modal h1').html(gettext('Uploading…'));
-    $('.upload-modal .file-name').html(file_input);
+    $('.upload-modal .file-name').html(files[0].name);
     $('.upload-modal .file-chooser').ajaxSubmit({
         beforeSend: resetUploadBar,
         uploadProgress: showUploadFeedback,
