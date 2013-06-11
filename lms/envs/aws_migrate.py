1511ec841a30a7a8adbec2d700502a1e685f6103
e0d <edward@indeterminate.org>
2013-06-11 15:00:39 -0400

    Better error handling.

--- lms/envs/aws_migrate.py
+++ lms/envs/aws_migrate.py
@@ -4,5 +4,9 @@ import os
 USER = os.environ.get('DB_MIGRATION_USER', 'root')
 PASSWORD = os.environ.get('DB_MIGRATION_PASS', None)
 
+if not PASSWORD:
+   raise ImproperlyConfigured("No database password was provided for running "
+                              "migrations.  This is fatal.")
+
 DATABASES['default']['USER'] = USER
 DATABASES['default']['PASSWORD'] = PASSWORD
