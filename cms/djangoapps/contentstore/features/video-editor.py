52bf82ac17a0994458503beda135eb41d8be3e4c
Peter Fogg <pfogg@edx.org>
2013-06-12 11:29:14 -0400

    Set Youtube ID defaults to the empty string.

--- cms/djangoapps/contentstore/features/video-editor.py
+++ cms/djangoapps/contentstore/features/video-editor.py
@@ -6,11 +6,11 @@ from lettuce import world, step
 
 @step('I see the correct settings and default values$')
 def i_see_the_correct_settings_and_values(step):
-    world.verify_all_setting_entries([['.75x', 'JMD_ifUUfsU', False],
-                                      ['1.25x', 'AKqURZnYqpk', False],
-                                      ['1.5x', 'DYpADpL7jAY', False],
+    world.verify_all_setting_entries([['.75x', '', False],
+                                      ['1.25x', '', False],
+                                      ['1.5x', '', False],
                                       ['Display Name', 'default', True],
-                                      ['Normal Speed', 'OEoXaMPEzfM', False],
+                                      ['Normal Speed', '', False],
                                       ['Show Captions', 'True', False],
                                       ['Source', '', False],
                                       ['Track', '', False]])
