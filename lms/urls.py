6b05b2e75f49d07dcf3dc927d5bb0a3cb183252f
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-06-13 17:31:31 -0400

    Search working, basic functionality and styling in place, as well as csrf protection, snippeting, results highlighting, and spell correction

--- lms/urls.py
+++ lms/urls.py
@@ -127,6 +127,9 @@ urlpatterns += (
     # Search
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Search working, basic functionality and styling in place, as well as csrf protection, snippeting, results highlighting, and spell correction
     url(r'^search$', 'search.views.search')
 =======
     url(r'^search$', 'search.views.search'),
@@ -134,6 +137,11 @@ urlpatterns += (
     url(r'^search$', 'search.views.search')
 >>>>>>> Made prettier, added requirements, and included main edX page
 
+<<<<<<< HEAD
+=======
+=======
+    url(r'^search$', 'search.views.search'),
+>>>>>>> Search working, basic functionality and styling in place, as well as csrf protection, snippeting, results highlighting, and spell correction
 >>>>>>> Search working, basic functionality and styling in place, as well as csrf protection, snippeting, results highlighting, and spell correction
 )
 
