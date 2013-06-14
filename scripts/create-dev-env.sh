c5853efb3611bce0cb4252defbf663e197f59e0f
Akshay Jagadeesh <akjags@gmail.com>
2013-06-14 11:06:03 -0700

    Adding option to have mongo automatically started up

--- scripts/create-dev-env.sh
+++ scripts/create-dev-env.sh
@@ -504,8 +504,10 @@ rake django-admin[migrate]
 
 case `uname -s` in Darwin)
         output "This next step needs Mongo to run"
-        output "Ensure that Mongo is running, then press Enter, or press control-C to quit"
+        output "If you'd like Mongo automatically started up, press m"
+        output "Otherwise start up Mongo, then press enter to continue. Or to quit, press control-C."
         read dummy
+        if [ "$dummy" = "m" ]; then mongod; fi
         ;;
 esac
 
