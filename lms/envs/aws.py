ffeabae069cd88e136b6bbfa66d3e5a9d6becd4d
John Jarvis <jarv@edx.org>
2013-06-17 09:07:02 -0400

    moving settings into the auth tokens section

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -178,7 +178,12 @@ COURSES_WITH_UNSAFE_CODE = ENV_TOKENS.get("COURSES_WITH_UNSAFE_CODE", [])
 with open(ENV_ROOT / CONFIG_PREFIX + "auth.json") as auth_file:
     AUTH_TOKENS = json.load(auth_file)
 
-SEGMENT_IO_LMS_KEY = AUTH_TOKENS.get('SEGMENT_IO_LMS_KEY', None)
+############### Mixed Related(Secure/Not-Secure) Items ##########
+# If segment.io key specified, load it and turn on segment IO if the feature flag is set
+SEGMENT_IO_LMS_KEY = AUTH_TOKENS.get('SEGMENT_IO_LMS_KEY')
+if SEGMENT_IO_LMS_KEY:
+    MITX_FEATURES['SEGMENT_IO_LMS'] = ENV_TOKENS.get('SEGMENT_IO_LMS', False)
+
 
 SECRET_KEY = AUTH_TOKENS['SECRET_KEY']
 
