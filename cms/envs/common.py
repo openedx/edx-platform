89ecc772888c988e3c6f22aed301e0f430931c99
Chris Dodge <cdodge@edx.org>
2013-06-18 11:15:05 -0400

    remove assets.js from the PIPELINE_JS since it seems to cause that file to be loaded twice

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -238,8 +238,7 @@ PIPELINE_JS = {
         ) + ['js/hesitate.js', 'js/base.js',
              'js/models/feedback.js', 'js/views/feedback.js',
              'js/models/section.js', 'js/views/section.js',
-             'js/models/metadata_model.js', 'js/views/metadata_editor_view.js',
-             'js/views/assets.js'],
+             'js/models/metadata_model.js', 'js/views/metadata_editor_view.js'],
         'output_filename': 'js/cms-application.js',
         'test_order': 0
     },
