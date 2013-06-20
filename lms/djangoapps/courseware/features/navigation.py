1e51bd7314d3580f0d3d0b122c6b6f71c3cc20e9
JonahStanley <Jonah_Stanley@brown.edu>
2013-06-20 10:58:11 -0400

    Fixed flakey navigation tests by changing css
    Now only check for the css that appears when the accordion is done

--- lms/djangoapps/courseware/features/navigation.py
+++ lms/djangoapps/courseware/features/navigation.py
@@ -83,9 +83,9 @@ def click_on_section(step, section):
     world.css_click(section_css)
 
     subid = "ui-accordion-accordion-panel-" + str(int(section) - 1)
-    subsection_css = 'ul[id="%s"]> li > a' % subid
+    subsection_css = 'ul.ui-accordion-content-active[id=\'%s\'][aria-expanded=\'true\']> li > a' % subid
     #for some reason needed to do it in two steps
-    world.css_find(subsection_css).click()
+    world.css_click(subsection_css)
 
 
 @step(u'I click on subsection "([^"]*)"$')
