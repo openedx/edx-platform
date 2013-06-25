bf4bef971da8e4401f59533a20afae06c08ac4f7
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-06-25 15:57:44 -0400

    Refactored some code and added full elasticsearch module test suite

--- common/djangoapps/search/tests/test_es.py
+++ common/djangoapps/search/tests/test_es.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 import requests
 from ..es_requests import ElasticDatabase
+import json
+import time
+import os
 
 
 class EsTest(TestCase):
@@ -8,28 +11,93 @@ class EsTest(TestCase):
     def setUp(self, es_instance="http://localhost:9200"):
         # Making sure that there is actually a running es_instance before testing
         database_request = requests.get(es_instance)
-        print database_request._content
         self.assertEqual(database_request.status_code, 200)
         self.elastic_search = ElasticDatabase(es_instance, "common/djangoapps/search/tests/test_settings.json")
-        self.assertEqual(isinstance(self.elastic_search, ElasticDatabase), True)
-        index_request = self.elastic_search.setup_index("test-index")
-        print index_request._content
-        self.assertEqual(index_request.status_code, 200)
-        type_request = self.elastic_search.setup_type("test-index", "test_type",
+        type_request = self.elastic_search.setup_type("test-index", "test-type",
                                                       "common/djangoapps/search/tests/test_mapping.json")
-        print type_request._content
-        self.assertEqual(type_request.status_code, 200)
+        time.sleep(1)  # Without sleep, tests will run without setUp finishing.
+        self.assertEqual(type_request.status_code, 201)
+        self.current_path = os.path.dirname(os.path.abspath(__file__))
 
     def test_index_creation(self):
         settings = self.elastic_search.get_index_settings("test-index")["test-index"]["settings"]
-        self.assertEqual(self.elastic_search.has_index("test-index"), True)
-        self.assertEqual(settings["index.number_of_replicas"], "5")
-        self.assertEqual(settings["index.number_of_shards"], "10")
+        self.assertTrue(self.elastic_search.has_index("test-index"))
+        self.assertFalse(self.elastic_search.has_index("fake-index"))
+        self.assertEqual(settings["index.number_of_replicas"], "1")
+        self.assertEqual(settings["index.number_of_shards"], "5")
+
+    def test_type_existence(self):
+        self.assertTrue(self.elastic_search.has_type("test-index", "test-type"))
+        self.assertFalse(self.elastic_search.has_type("test-index", "fake-type"))
+        self.assertFalse(self.elastic_search.has_type("fake-index", "test-type"))
+        self.assertFalse(self.elastic_search.has_type("fake-index", "fake-type"))
 
     def test_type_creation(self):
-        check = self.elastic_search.get_type_mapping("test-index", "test-type")
-        print check
-        self.assertEqual(True, False)
+        type_mapping = self.elastic_search.get_type_mapping("test-index", "test-type")
+        self.assertTrue("properties" in type_mapping["test-type"].keys())
+        self.assertTrue("test-float" in type_mapping["test-type"]["properties"]["mappings"]["properties"].keys())
+        self.assertTrue("test-string" in type_mapping["test-type"]["properties"]["mappings"]["properties"].keys())
+
+    def test_directory_search(self):
+        relative_paths = ["/testdir/malformed.srt.sjson", "/testdir/transcript.srt.sjson",
+                          "/testdir/nested/goal.srt.sjson"]
+        absolute_paths = set([self.current_path+item for item in relative_paths])
+        paths = self.elastic_search.os_walk_transcript(os.walk(self.current_path))
+        flat_paths = set([item for sublist in paths for item in sublist])
+        self.assertTrue(flat_paths == absolute_paths)
+
+    def test_alternate_directory_search(self):
+        relative_paths = ["/testdir/extension.foo", "/testdir/nested/extension2.foo"]
+        absolute_paths = set([self.current_path+item for item in relative_paths])
+        paths = self.elastic_search.os_walk_transcript(os.walk(self.current_path), file_ending=".foo")
+        flat_paths = set([item for sublist in paths for item in sublist])
+        self.assertTrue(flat_paths == absolute_paths)
+        success = False
+        try:
+            self.elastic_search.os_walk_transcript(os.walk(self.current_path), ".fake").next()
+        except StopIteration:
+            success = True
+        except:
+            success = False
+        self.assertTrue(success)
+
+    def test_data_indexing(self):
+        responses = self.elastic_search.index_directory_transcripts(self.current_path, "test-index",
+                                                                    "test-type", silent=True)
+        successes = [json.loads(response)["ok"] for response in responses]
+        correct_indices = [json.loads(response)["_index"] == "test-index" for response in responses]
+        correct_types = [json.loads(response)["_type"] == "test-type" for response in responses]
+        self.assertTrue(all(successes))
+        self.assertTrue(all(correct_indices))
+        self.assertTrue(all(correct_types))
+
+    def test_data_failure(self):
+        success = False
+        path = self.current_path + "/testdir/malformed.srt.sjson"
+        try:
+            self.elastic_search.index_transcript("test-index", "test-type", path)
+        except ValueError:
+            success = True
+        except:
+            success = False
+        self.assertTrue(success)
+
+    def test_data_read(self):
+        relative_paths = ["/testdir/malformed.srt.sjson", "/testdir/transcript.srt.sjson"]
+        bad_response = self.elastic_search.index_transcript("test-index", "test-type",
+                                                            self.current_path+relative_paths[0],
+                                                            silent=True, id_="1")
+        good_response = self.elastic_search.index_transcript("test-index", "test-type",
+                                                             self.current_path+relative_paths[1],
+                                                             silent=False, id_="2")
+        self.assertEqual(bad_response.status_code, 201)
+        self.assertEqual(good_response.status_code, 201)
+        bad_object = self.elastic_search.get_data("test-index", "test-type", "1")
+        good_object = self.elastic_search.get_data("test-index", "test-type", "2")
+        self.assertEqual(bad_object.status_code, 200)
+        self.assertEqual(good_object.status_code, 200)
+        self.assertEqual(json.loads(bad_object._content)["_source"]["searchable_text"], "INVALID JSON")
+        self.assertEqual(json.loads(good_object._content)["_source"]["searchable_text"], "Success!")
 
     def tearDown(self):
         self.elastic_search.delete_type("test-index", "test-type")
