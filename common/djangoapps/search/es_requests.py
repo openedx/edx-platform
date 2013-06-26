2e4764c9268ddff9b78e08150734da02f8f42454
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-06-26 18:47:31 -0400

    Made prettier, added requirements, and included main edX page

--- common/djangoapps/search/es_requests.py
+++ common/djangoapps/search/es_requests.py
@@ -443,8 +443,8 @@ Set to 50k by default"""
                 dictionary.write(word+"\n")
 
 
-#url = "http://localhost:9200"
-#settings_file = "settings.json"
+url = "http://localhost:9200"
+settings_file = "settings.json"
 
 mongo = MongoIndexer()
 
