e32e443e73aff572b00fbbed506e2ae67f102f0a
Felix Sun <fephsun@gmail.com>
2013-06-26 15:53:40 -0400

    Fixed numerous code-formatting issues and pep8 violations.
    Began enforcing one-vote-per-person.  This can be disabled with debug="True" in the <crowdsource_hinter> tag.
    
    Started tests of the hint manager.

--- lms/djangoapps/instructor/hint_manager.py
+++ lms/djangoapps/instructor/hint_manager.py
@@ -1,28 +1,17 @@
-'''
+"""
 Views for hint management.
-'''
 
-from collections import defaultdict
-import csv
+Along with the crowdsource_hinter xmodule, this code is still
+experimental, and should not be used in new courses, yet.
+"""
+
 import json
-import logging
-from markupsafe import escape
-import os
 import re
-import requests
-from requests.status_codes import codes
-import urllib
-from collections import OrderedDict
 
-from StringIO import StringIO
-
-from django.conf import settings
-from django.contrib.auth.models import User, Group
 from django.http import HttpResponse, Http404
 from django_future.csrf import ensure_csrf_cookie
-from django.views.decorators.cache import cache_control
+
 from mitxmako.shortcuts import render_to_response, render_to_string
-from django.core.urlresolvers import reverse
 
 from courseware.courses import get_course_with_access
 from courseware.models import XModuleContentField
@@ -43,7 +32,9 @@ def hint_manager(request, course_id):
     field = request.POST['field']
     if not (field == 'mod_queue' or field == 'hints'):
         # Invalid field.  (Don't let users continue - they may overwrite other db's)
-        return 
+        out = 'Error in hint manager - an invalid field was accessed.'
+        return HttpResponse(out)
+
     if request.POST['op'] == 'delete hints':
         delete_hints(request, course_id, field)
     if request.POST['op'] == 'switch fields':
@@ -58,12 +49,23 @@ def hint_manager(request, course_id):
     return HttpResponse(json.dumps({'success': True, 'contents': rendered_html}))
 
 
-
 def get_hints(request, course_id, field):
-    # field indicates the database entry that we are modifying.
-    # Right now, the options are 'hints' or 'mod_queue'.
-    # DON'T TRUST field attributes that come from ajax.  Use an if statement
-    # to make sure the field is valid before plugging into functions.
+    """
+    Load all of the hints submitted to the course.
+
+    Args:
+    request -- Django request object.
+    course_id -- The course id, like 'Me/19.002/test_course'
+    field -- Either 'hints' or 'mod_queue'; specifies which set of hints to load.
+
+    Keys in returned dict:
+        - field: Same as input
+        - other_field: 'mod_queue' if field == 'hints'; and vice-versa.
+        - field_label, other_field_label: English name for the above.
+        - all_hints: A list of [answer, pk dict] pairs, representing all hints.
+          Sorted by answer.
+        - id_to_name: A dictionary mapping problem id to problem name.
+    """
 
     if field == 'mod_queue':
         other_field = 'hints'
@@ -76,47 +78,60 @@ def get_hints(request, course_id, field):
     chopped_id = '/'.join(course_id.split('/')[:-1])
     chopped_id = re.escape(chopped_id)
     all_hints = XModuleContentField.objects.filter(field_name=field, definition_id__regex=chopped_id)
+    # big_out_dict[problem id] = [[answer, {pk: [hint, votes]}], sorted by answer]
     big_out_dict = {}
-    name_dict = {}
-    for problem in all_hints:
-        loc = Location(problem.definition_id)
+    # name_dict[problem id] = Display name of problem
+    id_to_name = {}
+
+    for hints_by_problem in all_hints:
+        loc = Location(hints_by_problem.definition_id)
         try:
             descriptor = modulestore().get_items(loc)[0]
         except IndexError:
             # Sometimes, the problem is no longer in the course.  Just
             # don't include said problem.
             continue
-        name_dict[problem.definition_id] = descriptor.get_children()[0].display_name
+        id_to_name[hints_by_problem.definition_id] = descriptor.get_children()[0].display_name
         # Answer list contains (answer, dict_of_hints) tuples.
 
         def answer_sorter(thing):
-            '''
+            """
             thing is a tuple, where thing[0] contains an answer, and thing[1] contains
-            a dict of hints.  This function returns an index based on thing[0], which 
+            a dict of hints.  This function returns an index based on thing[0], which
             is used as a key to sort the list of things.
-            '''
+            """
             try:
                 return float(thing[0])
             except ValueError:
                 # Put all non-numerical answers first.
                 return float('-inf')
 
-        answer_list = sorted(json.loads(problem.value).items(), key=answer_sorter)
-        big_out_dict[problem.definition_id] = answer_list
+        answer_list = sorted(json.loads(hints_by_problem.value).items(), key=answer_sorter)
+        big_out_dict[hints_by_problem.definition_id] = answer_list
 
     render_dict = {'field': field,
                    'other_field': other_field,
                    'field_label': field_label,
                    'other_field_label': other_field_label,
                    'all_hints': big_out_dict,
-                   'id_to_name': name_dict}
+                   'id_to_name': id_to_name}
     return render_dict
 
+
 def delete_hints(request, course_id, field):
-    '''
-    Deletes the hints specified by the [problem_defn_id, answer, pk] tuples in the numbered
-    fields of request.POST.
-    '''
+    """
+    Deletes the hints specified.
+
+    request.POST contains some fields keyed by integers.  Each such field contains a
+    [problem_defn_id, answer, pk] tuple.  These tuples specify the hints to be deleted.
+
+    Example request.POST:
+    {'op': 'delete_hints',
+     'field': 'mod_queue',
+      1: ['problem_whatever', '42.0', 3],
+      2: ['problem_whatever', '32.5', 12]}
+    """
+
     for key in request.POST:
         if key == 'op' or key == 'field':
             continue
@@ -129,31 +144,37 @@ def delete_hints(request, course_id, field):
         this_problem.value = json.dumps(problem_dict)
         this_problem.save()
 
+
 def change_votes(request, course_id, field):
-    '''
-    Updates the number of votes.  The numbered fields of request.POST contain
-    [problem_id, answer, pk, new_votes] tuples.
+    """
+    Updates the number of votes.
+
+    The numbered fields of request.POST contain [problem_id, answer, pk, new_votes] tuples.
     - Very similar to delete_hints.  Is there a way to merge them?  Nah, too complicated.
-    '''
+    """
+
     for key in request.POST:
         if key == 'op' or key == 'field':
             continue
         problem_id, answer, pk, new_votes = request.POST.getlist(key)
         this_problem = XModuleContentField.objects.get(field_name=field, definition_id=problem_id)
         problem_dict = json.loads(this_problem.value)
+        # problem_dict[answer][pk] points to a [hint_text, #votes] pair.
         problem_dict[answer][pk][1] = new_votes
         this_problem.value = json.dumps(problem_dict)
         this_problem.save()
 
+
 def add_hint(request, course_id, field):
-    '''
-    Add a new hint.  POST:
+    """
+    Add a new hint.  request.POST:
     op
     field
     problem - The problem id
     answer - The answer to which a hint will be added
     hint - The text of the hint
-    '''
+    """
+
     problem_id = request.POST['problem']
     answer = request.POST['answer']
     hint_text = request.POST['hint']
@@ -171,13 +192,15 @@ def add_hint(request, course_id, field):
     this_problem.value = json.dumps(problem_dict)
     this_problem.save()
 
+
 def approve(request, course_id, field):
-    '''
+    """
     Approve a list of hints, moving them from the mod_queue to the real
     hint list.  POST:
     op, field
     (some number) -> [problem, answer, pk]
-    '''
+    """
+
     for key in request.POST:
         if key == 'op' or key == 'field':
             continue
@@ -198,28 +221,3 @@ def approve(request, course_id, field):
         problem_dict[answer][pk] = hint_to_move
         problem_in_hints.value = json.dumps(problem_dict)
         problem_in_hints.save()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
