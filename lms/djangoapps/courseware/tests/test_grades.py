af5a41acebee1845df605c5d37aadc8c6f84f8ea
Felix Sun <fephsun@gmail.com>
2013-07-09 12:49:50 -0400

    Rebase fix.

--- lms/djangoapps/courseware/tests/test_grades.py
+++ lms/djangoapps/courseware/tests/test_grades.py
@@ -3,13 +3,13 @@ This is the start of a test for grades.
 It is very incomplete - we're only testing one function
 right now.
 """
-
-from courseware.model_data import LmsKeyValueStore
-from courseware.grades import yield_dynamic_descriptor_descendents
-from courseware import grades
 from mock import MagicMock
 
 import unittest
+import courseware.grades as grades
+import courseware.module_render as module_render
+from courseware.model_data import LmsKeyValueStore
+
 
 
 class FakeChildFactory(object):
@@ -38,6 +38,7 @@ class TestGrades(unittest.TestCase):
         Make sure that yield_dynamic_descriptor_descendents instantiates
         modules to get children.
         """
+
         child_a = FakeChildFactory.create('a')
         child_b = FakeChildFactory.create('b')
         child_c = FakeChildFactory.create('c')
@@ -60,17 +61,119 @@ class TestGrades(unittest.TestCase):
                 return None
 
         # Test with dynamic children
-        dynamic_children = list(yield_dynamic_descriptor_descendents(fake_parent, fake_module_creator))
+        dynamic_children = list(grades.yield_dynamic_descriptor_descendents(fake_parent, fake_module_creator))
         self.assertTrue(child_a in dynamic_children)
         self.assertTrue(child_b in dynamic_children)
         self.assertTrue(child_c not in dynamic_children)
 
         # Test without dynamic children
         fake_parent.has_dynamic_children = lambda: False
-        regular_children = list(yield_dynamic_descriptor_descendents(fake_parent, fake_module_creator))
+        regular_children = list(grades.yield_dynamic_descriptor_descendents(fake_parent, fake_module_creator))
         self.assertTrue(child_a in regular_children)
         self.assertTrue(child_b in regular_children)
         self.assertTrue(child_c in regular_children)
+
+    def test_compute_graded_total(self):
+        """
+        Tests grading for a single section.
+        compute_graded_total(section, student, course_id, m_d_c, request)
+        - section['section_descriptor'] returns the descriptor for this section.
+        - student needs an id
+        - All other paramenters are touched through helper functions only.
+
+        Functions to mock:
+        grades.should_grade_section(descriptor, m_d_c, student_id) -> True/False
+        .module_render.get_module_for_descriptor(student, request, descriptor, m_d_c, course_id) -> Xmodule
+        grades.yield_dynamic_descriptor_descendents(descriptor, create_module) -> iter through descriptors
+        grades.get_score(course_id, student, descriptor, create_module, m_d_c) -> (correct, total)
+        grades.find_attempted(descriptor, m_d_c, student_id) -> True/False
+        """
+
+        section = {'section_descriptor': MagicMock(), 'xmoduledescriptors': MagicMock()}
+        student = MagicMock()
+        student.id = 'my id'
+        course_id = 'course id'
+        m_d_c = MagicMock()  # We will never query the mdc directly.
+        request = MagicMock()   # Same as above.
+
+        # Monkey patching!
+        def fake_should_grade_section(descriptor, m_d_c, student_id):
+            """ Always grade :) """
+            return True
+        grades.find_should_grade_section = fake_should_grade_section
+
+        def fake_get_module_for_descriptor(student, request, descriptor, m_d_c, course_id):
+            """Don't even return anything; this output is not directly used."""
+            return None
+        module_render.get_module_for_descriptor = fake_get_module_for_descriptor
+
+        def fake_yield_dynamic_descriptor_descendents(descriptor, create_module):
+            """
+            Return a bunch of fake descriptors, in iterator form.  Makes 4 descriptors:
+            0: not graded.
+            1: graded, but total is 0.
+            2, 3: normal
+            """
+            for i in xrange(4):
+                out = MagicMock()
+                out.display_name_with_default = str(i)
+                out.lms = MagicMock()
+                out.lms.graded = False if (i == 0) else True
+                yield out
+        grades.yield_dynamic_descriptor_descendents = fake_yield_dynamic_descriptor_descendents
+
+        def fake_get_score(course_id, student, descriptor, create_module, m_d_c):
+            """
+            Returns a score, based on the descriptor passed in.
+            0: None / None
+            1: 5 / 0
+            2: 2 / 4
+            3: 0 / 5
+            """
+            number = int(descriptor.display_name_with_default)
+            if number == 0:
+                return (None, None)
+            elif number == 1:
+                return (5, 0)
+            elif number == 2:
+                return (2, 4)
+            elif number == 3:
+                return (0, 5)
+            else:
+                raise Exception('get_score called with unexpected input.')
+        grades.get_score = fake_get_score
+
+        def fake_find_attempted(descriptor, m_d_c, student_id):
+            """Always return True. """
+            return True
+        grades.find_attempted = fake_find_attempted
+
+        # Actually do the test.
+        graded_total, raw_scores = grades.compute_graded_total(section, student, course_id, m_d_c, request)
+        # Reset all of the monkey patching.
+        # Do this before assertions, because if an assertion fails, the remaining code is not run.
+        reload(grades)
+        reload(module_render)
+
+        self.assertTrue(graded_total.earned == 2)
+        self.assertTrue(graded_total.possible == 9)
+        self.assertTrue(graded_total.graded)
+
+        print len(raw_scores)
+        self.assertTrue(len(raw_scores) == 3)
+
+    def test_grade(self):
+        """
+        Test the grade function.
+        grade(student, request, course, model_data_cache=None, keep_raw_scores=False, include_in_progress_grade=True)
+        student - not used directly.
+        request - not used directly.
+        course:
+            .grading_context['graded_sections']
+            .id
+            .
+        """
+        pass
 
 
 class TestFindShouldGradeSection(unittest.TestCase):
@@ -179,3 +282,7 @@ class TestFindAttempted(unittest.TestCase):
         fake_module = self.fake_module(True)
         result = grades.find_attempted(fake_module, self.fake_model_data_cache, 3.0)
         self.assertTrue(result)
+
+
+
+
