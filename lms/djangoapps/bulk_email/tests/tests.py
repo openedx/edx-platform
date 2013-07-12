0c8564d98fef469528d8ef90926d1b2d9fc3ef2a
Kevin Luo <kevkluo@gmail.com>
2013-07-12 15:42:40 -0700

    Update course email view tests
      -Use factories for setUp
     -Use patch.dict for hacking email settings flag

--- lms/djangoapps/bulk_email/tests/tests.py
+++ lms/djangoapps/bulk_email/tests/tests.py
@@ -5,48 +5,32 @@ Based on (and depends on) unit tests for courseware.
 """
 
 from django.test.utils import override_settings
-
-# Need access to internal func to put users in the right group
-from django.contrib.auth.models import Group
-
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-from courseware.access import _course_staff_group_name
-from courseware.tests.tests import LoginEnrollmentTestCase, TEST_DATA_XML_MODULESTORE, get_user
-from xmodule.modulestore.django import modulestore
-import xmodule.modulestore.django
+from courseware.tests.tests import TEST_DATA_MONGO_MODULESTORE
+from student.tests.factories import AdminFactory
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+from xmodule.modulestore.tests.factories import CourseFactory
 
-@override_settings(MODULESTORE=TEST_DATA_XML_MODULESTORE)
-class TestInstructorDashboardEmailView(LoginEnrollmentTestCase):
+from mock import patch
+
+@override_settings(MODULESTORE=TEST_DATA_MONGO_MODULESTORE)
+class TestInstructorDashboardEmailView(ModuleStoreTestCase):
     '''
     Check for email view displayed with flag
     '''
-
     def setUp(self):
-        xmodule.modulestore.django._MODULESTORES = {}
-        
-        self.toy = modulestore().get_course("edX/toy/2012_Fall")
+        self.course = CourseFactory.create()
         
         # Create instructor account
-        self.instructor = 'view@test.com'
-        self.password = 'foo'
-        self.create_account('u1', self.instructor, self.password)
-        self.activate_user(self.instructor)
+        instructor = AdminFactory.create()
+        self.client.login(username=instructor.username, password="test")
 
-        group_name = _course_staff_group_name(self.toy.location)
-        g = Group.objects.create(name=group_name)
-        g.user_set.add(get_user(self.instructor))
-
-        self.logout()
-        self.login(self.instructor, self.password)
-        self.enroll(self.toy)
-
+    @patch.dict(settings.MITX_FEATURES, {'ENABLE_INSTRUCTOR_EMAIL':True})
     def test_email_flag_true(self):
-        oldEmailFlag = settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL']
-        settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL'] = True
         response = self.client.get(reverse('instructor_dashboard',
-                                   kwargs={'course_id': self.toy.id}))
+                                   kwargs={'course_id': self.course.id}))
         email_link = '<a href="#" onclick="goto(\'Email\')" class="None">Email</a>'
         self.assertTrue(email_link in response.content)
 
@@ -54,21 +38,15 @@ class TestInstructorDashboardEmailView(LoginEnrollmentTestCase):
         session['idash_mode'] = 'Email'
         session.save()
         response = self.client.get(reverse('instructor_dashboard',
-                                   kwargs={'course_id': self.toy.id}))
+                                   kwargs={'course_id': self.course.id}))
         selected_email_link = '<a href="#" onclick="goto(\'Email\')" class="selectedmode">Email</a>'
         self.assertTrue(selected_email_link in response.content)
         send_to_label = '<label for="id_to">Send to:</label>'
         self.assertTrue(send_to_label in response.content)
 
-        del self.client.session['idash_mode']
-        settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL'] = oldEmailFlag
-
+    @patch.dict(settings.MITX_FEATURES, {'ENABLE_INSTRUCTOR_EMAIL':False})
     def test_email_flag_false(self):
-        oldEmailFlag = settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL']
-        settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL'] = False
         response = self.client.get(reverse('instructor_dashboard',
-                                   kwargs={'course_id': self.toy.id}))
+                                   kwargs={'course_id': self.course.id}))
         email_link = '<a href="#" onclick="goto(\'Email\')" class="None">Email</a>'
         self.assertFalse(email_link in response.content)
-        
-        settings.MITX_FEATURES['ENABLE_INSTRUCTOR_EMAIL'] = oldEmailFlag
