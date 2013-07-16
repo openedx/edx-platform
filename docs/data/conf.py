5f9c395afe0cd86df13b6fbca9e8fb6c0fd4bc61
Julian Arni <julian@edx.org>
2013-07-16 13:37:50 -0400

    Use source dir for data docs

--- docs/data/conf.py
+++ docs/data/conf.py
@@ -9,6 +9,7 @@ from docs.shared.conf import *
 # Add any paths that contain templates here, relative to this directory.
 templates_path.append('source/_templates')
 
+sys.path.insert(0, os.path.abspath('.'))
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
