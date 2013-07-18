5c1de0c32f9e041ac14ad581a6c8c9e9075dca2c
Chris Dodge <cdodge@edx.org>
2013-07-18 17:01:25 -0400

    change from_xml class method on video class to make sure we honor the fact that the xml string could actually be a pointer to another file on disk which actually contains the attributes

--- lms/djangoapps/courseware/tests/test_module_render.py
+++ lms/djangoapps/courseware/tests/test_module_render.py
@@ -188,9 +188,9 @@ class TestTOC(TestCase):
                         'format': u'Lecture Sequence', 'due': None, 'active': False},
                        {'url_name': 'Welcome', 'display_name': u'Welcome', 'graded': True,
                         'format': '', 'due': None, 'active': False},
-                       {'url_name': 'video_123456789012', 'display_name': 'video 123456789012', 'graded': True,
+                       {'url_name': 'video_123456789012', 'display_name': 'Test Video', 'graded': True,
                         'format': '', 'due': None, 'active': False},
-                       {'url_name': 'video_4f66f493ac8f', 'display_name': 'video 4f66f493ac8f', 'graded': True,
+                       {'url_name': 'video_4f66f493ac8f', 'display_name': 'Video Title', 'graded': True,
                         'format': '', 'due': None, 'active': False}],
                       'url_name': 'Overview', 'display_name': u'Overview'},
                      {'active': False, 'sections':
@@ -199,6 +199,7 @@ class TestTOC(TestCase):
                       'url_name': 'secret:magic', 'display_name': 'secret:magic'}])
 
         actual = render.toc_for_course(self.portal_user, request, self.toy_course, chapter, None, model_data_cache)
+        print actual
         assert reduce(lambda x, y: x and (y in actual), expected, True)
 
     def test_toc_toy_from_section(self):
@@ -215,9 +216,9 @@ class TestTOC(TestCase):
                         'format': u'Lecture Sequence', 'due': None, 'active': False},
                        {'url_name': 'Welcome', 'display_name': u'Welcome', 'graded': True,
                         'format': '', 'due': None, 'active': True},
-                       {'url_name': 'video_123456789012', 'display_name': 'video 123456789012', 'graded': True,
+                       {'url_name': 'video_123456789012', 'display_name': 'Test Video', 'graded': True,
                         'format': '', 'due': None, 'active': False},
-                       {'url_name': 'video_4f66f493ac8f', 'display_name': 'video 4f66f493ac8f', 'graded': True,
+                       {'url_name': 'video_4f66f493ac8f', 'display_name': 'Video Title', 'graded': True,
                         'format': '', 'due': None, 'active': False}],
                       'url_name': 'Overview', 'display_name': u'Overview'},
                      {'active': False, 'sections':
