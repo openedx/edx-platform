9e5d64b8dfb0f775e851edcda7144f8eb7a8563c
Will Daly <will@edx.org>
2013-08-01 09:29:58 -0400

    Removed check for AUTOMATIC_AUTH_FOR_LOAD_TESTING because it's already implied that you're using load testing settings.

--- lms/envs/load_test.py
+++ lms/envs/load_test.py
@@ -3,9 +3,8 @@
 from .aws import *
 
 # Disable CSRF for load testing
-if MITX_FEATURES.get('AUTOMATIC_AUTH_FOR_LOAD_TESTING'):
-    exclude_csrf = lambda elem: not elem in \
-                   ['django.core.context_processors.csrf',
-                    'django.middleware.csrf.CsrfViewMiddleware']
-    TEMPLATE_CONTEXT_PROCESSORS = filter(exclude_csrf, TEMPLATE_CONTEXT_PROCESSORS)
-    MIDDLEWARE_CLASSES = filter(exclude_csrf, MIDDLEWARE_CLASSES)
+exclude_csrf = lambda elem: not elem in \
+               ['django.core.context_processors.csrf',
+                'django.middleware.csrf.CsrfViewMiddleware']
+TEMPLATE_CONTEXT_PROCESSORS = filter(exclude_csrf, TEMPLATE_CONTEXT_PROCESSORS)
+MIDDLEWARE_CLASSES = filter(exclude_csrf, MIDDLEWARE_CLASSES)
