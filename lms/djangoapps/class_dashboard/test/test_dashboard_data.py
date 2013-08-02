59df7008257171d7c3b0993775e964c85a589e8e
Kristin Stephens <ksteph@cs.berkeley.edu>
2013-08-02 14:41:42 -0700

    Latest metrics tab
    Merge of latest metrics tab code into edx-west/release
    Note that the metrics tab is in the legacy dash only
    for now.

--- lms/djangoapps/class_dashboard/test/test_dashboard_data.py
+++ lms/djangoapps/class_dashboard/test/test_dashboard_data.py
@@ -1,30 +1,30 @@
-
-
 from mock import Mock, patch
 import json
 
 from django.test.utils import override_settings
 from django.test import TestCase
 from django.core import management
+from django.core.urlresolvers import reverse
 
 from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from courseware.tests.tests import TEST_DATA_MONGO_MODULESTORE
 from courseware.tests.factories import StudentModuleFactory
-from student.tests.factories import UserFactory, CourseEnrollmentFactory
+from student.tests.factories import UserFactory, CourseEnrollmentFactory, AdminFactory
 from courseware.models import StudentModule
 from capa.tests.response_xml_factory import StringResponseXMLFactory
 from xmodule.modulestore import Location
-from queryable_student_module.management.commands import populate_studentmoduleexpand
 
 from xmodule.course_module import CourseDescriptor
-
-from class_dashboard.dashboard_data import get_problem_grade_distribution, get_problem_attempt_distrib, get_sequential_open_distrib, \
-                                           get_last_populate, get_problem_set_grade_distribution, get_d3_problem_grade_distribution, \
-                                           get_d3_problem_attempt_distribution, get_d3_sequential_open_distribution, \
-                                           get_d3_section_grade_distribution, get_section_display_name, get_array_section_has_problem
+from class_dashboard.dashboard_data import (get_problem_grade_distribution, get_sequential_open_distrib,
+                                            get_problem_set_grade_distribution, get_d3_problem_grade_distribution,
+                                            get_d3_sequential_open_distribution, get_d3_section_grade_distribution,
+                                            get_section_display_name, get_array_section_has_problem
+                                            )
+from class_dashboard.views import has_instructor_access_for_class
 
 USER_COUNT = 11
+
 
 @override_settings(MODULESTORE=TEST_DATA_MONGO_MODULESTORE)
 class TestGetProblemGradeDistribution(ModuleStoreTestCase):
@@ -33,15 +33,14 @@ class TestGetProblemGradeDistribution(ModuleStoreTestCase):
       - simple test, make sure output correct
       - test when a problem has two max_grade's, should just take the larger value
     """
-    
+
     def setUp(self):
-        
-        self.command = 'populate_studentmoduleexpand'
-        self.script_id = "studentmoduleexpand"
+
+        self.instructor = AdminFactory.create()
+        self.client.login(username=self.instructor.username, password='test')
         self.attempts = 3
-        
         self.course = CourseFactory.create()
-        
+
         section = ItemFactory.create(
             parent_location=self.course.location,
             category="chapter",
@@ -50,20 +49,19 @@ class TestGetProblemGradeDistribution(ModuleStoreTestCase):
         sub_section = ItemFactory.create(
             parent_location=section.location,
             category="sequential",
-           # metadata={'graded': True, 'format': 'Homework'}
         )
-    
+
         unit = ItemFactory.create(
             parent_location=sub_section.location,
             category="vertical",
             metadata={'graded': True, 'format': 'Homework'}
         )
-        
+
         self.users = [UserFactory.create() for _ in xrange(USER_COUNT)]
-    
+
         for user in self.users:
             CourseEnrollmentFactory.create(user=user, course_id=self.course.id)
-    
+
         for i in xrange(USER_COUNT - 1):
             category = "problem"
             item = ItemFactory.create(
@@ -72,11 +70,11 @@ class TestGetProblemGradeDistribution(ModuleStoreTestCase):
                 data=StringResponseXMLFactory().build_xml(answer='foo'),
                 metadata={'rerandomize': 'always'}
             )
-    
+
             for j, user in enumerate(self.users):
                 StudentModuleFactory.create(
                     grade=1 if i < j else 0,
-                    max_grade=1 if i< j else 0.5,
+                    max_grade=1 if i < j else 0.5,
                     student=user,
                     course_id=self.course.id,
                     module_state_key=Location(item.location).url(),
@@ -98,36 +96,13 @@ class TestGetProblemGradeDistribution(ModuleStoreTestCase):
             max_grade = prob_grade_distrib[problem]['max_grade']
             self.assertEquals(1, max_grade)
 
-
-    def test_get_problem_attempt_distribution(self):
- 
-        # Call command
-        management.call_command(self.command, self.course.id)
-        prob_attempts_distrib = get_problem_attempt_distrib(self.course.id)
-        
-        for problem in prob_attempts_distrib:
-            num_attempts = prob_attempts_distrib[problem][self.attempts -1]
-            self.assertEquals(USER_COUNT, num_attempts)
-
-
     def test_get_sequential_open_distibution(self):
-        
+
         sequential_open_distrib = get_sequential_open_distrib(self.course.id)
-        
+
         for problem in sequential_open_distrib:
             num_students = sequential_open_distrib[problem]
             self.assertEquals(USER_COUNT, num_students)
- 
-
-    def test_get_last_populate(self):
-        
-        timestamp = get_last_populate(self.course.id, self.script_id)
-        self.assertEquals(timestamp, None)
-        
-        management.call_command(self.command, self.course.id)
-        timestamp = get_last_populate(self.course.id, self.script_id)
-        self.assertNotEquals(timestamp, None)
-
 
     def test_get_problemset_grade_distrib(self):
 
@@ -144,10 +119,8 @@ class TestGetProblemGradeDistribution(ModuleStoreTestCase):
                 sum_attempts += item[1]
             self.assertEquals(USER_COUNT, sum_attempts)
 
+    def test_get_d3_problem_grade_distrib(self):
 
-  #  @patch('class_dashboard.dashboard_data.get_problem_grade_distribution')
-    def test_get_d3_problem_grade_distrib(self): #, mock_get_data):
-        
         d3_data = get_d3_problem_grade_distribution(self.course.id)
         for data in d3_data:
             for stack_data in data['data']:
@@ -156,54 +129,51 @@ class TestGetProblemGradeDistribution(ModuleStoreTestCase):
                     sum_values += problem['value']
                 self.assertEquals(USER_COUNT, sum_values)
 
-
-    def test_get_d3_problem_attempt_distrib(self):
-        
-        # Call command
-        management.call_command(self.command, self.course.id)
-        d3_data = get_d3_problem_attempt_distribution(self.course.id)
-        
-        for data in d3_data:
-            for stack_data in data['data']:
-                sum_values = 0
-                for problem in stack_data['stackData']:
-                    sum_values += problem['value']
-                self.assertEquals(USER_COUNT, sum_values)   
-
-
     def test_get_d3_sequential_open_distrib(self):
-        
+
         d3_data = get_d3_sequential_open_distribution(self.course.id)
-        
+
         for data in d3_data:
             for stack_data in data['data']:
                 for problem in stack_data['stackData']:
                     value = problem['value']
-                self.assertEquals(0, value)   
-
+                self.assertEquals(0, value)
 
     def test_get_d3_section_grade_distrib(self):
 
-
         d3_data = get_d3_section_grade_distribution(self.course.id, 0)
-        
+
         for stack_data in d3_data:
             sum_values = 0
             for problem in stack_data['stackData']:
                 sum_values += problem['value']
             self.assertEquals(USER_COUNT, sum_values)
 
-
     def test_get_section_display_name(self):
-        
+
         section_display_name = get_section_display_name(self.course.id)
-        
         self.assertMultiLineEqual(section_display_name[0], 'test factory section')
 
-
     def test_get_array_section_has_problem(self):
-        
+
         b_section_has_problem = get_array_section_has_problem(self.course.id)
-        
-        print b_section_has_problem
         self.assertEquals(b_section_has_problem[0], True)
+
+    def test_dashboard(self):
+         
+        url = reverse('instructor_dashboard', kwargs={'course_id': self.course.id})
+        response = self.client.post(
+            url,
+            {
+                'idash_mode': 'Metrics'
+            }
+        )
+        self.assertContains(response, '<h2>Course Statistics At A Glance</h2>')
+        
+    def test_has_instructor_access_for_class(self): #user, course_id):
+        """
+        Test for instructor access
+        """
+        ret_val = has_instructor_access_for_class(self.instructor, self.course.id)
+        self.assertEquals(ret_val, True)
+        
