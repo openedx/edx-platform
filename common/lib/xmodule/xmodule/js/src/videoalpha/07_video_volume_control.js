f301231051b8aeb2ba87b962b25375b75c0a3a05
Valera Rozuvan <valera@edx.org>
2013-08-08 14:23:10 +0300

    Enabled tabbing to volume slider.

--- common/lib/xmodule/xmodule/js/src/videoalpha/07_video_volume_control.js
+++ common/lib/xmodule/xmodule/js/src/videoalpha/07_video_volume_control.js
@@ -61,10 +61,8 @@ function () {
             slide: state.videoVolumeControl.onChange
         });
 
-        // Make sure that we can't focus the actual volume slider while Tabing.
-        state.videoVolumeControl.volumeSliderEl.find('a').each(function (index, value) {
-            $(value).attr('tabindex', '-1');
-        });
+        // Make sure that we can focus the actual volume slider while Tabing.
+        state.videoVolumeControl.volumeSliderEl.find('a').attr('tabindex', '0');
 
         state.videoVolumeControl.el.toggleClass('muted', state.videoVolumeControl.currentVolume === 0);
     }
@@ -88,7 +86,11 @@ function () {
         });
 
         state.videoVolumeControl.buttonEl.on('blur', function() {
-            $(this).parent().removeClass('open');
+            state.videoVolumeControl.volumeSliderEl.find('a').focus();
+        });
+
+        state.videoVolumeControl.volumeSliderEl.find('a').on('blur', function () {
+            state.videoVolumeControl.el.removeClass('open');
         });
     }
 
