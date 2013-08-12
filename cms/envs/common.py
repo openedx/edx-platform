0231e64f3606ccc17eaba0e7f9983163b8eb7612
Chris Dodge <cdodge@edx.org>
2013-08-12 13:41:05 -0400

    also define TECH_SUPPORT_EMAIL in cms.envs.common config file

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -184,6 +184,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'registration@edx.org'
 DEFAULT_FEEDBACK_EMAIL = 'feedback@edx.org'
 SERVER_EMAIL = 'devops@edx.org'
+TECH_SUPPORT_EMAIL = 'technical@edx.org'
 ADMINS = ()
 MANAGERS = ADMINS
 
