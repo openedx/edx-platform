d140ffd868c2b0cf4401c191f5c5f05e1635ef77
Jason Bau <jbau@stanford.edu>
2013-08-20 21:36:32 -0700

    Start of tests for CyberSource processor

--- lms/djangoapps/shoppingcart/processors/__init__.py
+++ lms/djangoapps/shoppingcart/processors/__init__.py
@@ -3,11 +3,7 @@ from django.conf import settings
 ### Now code that determines, using settings, which actual processor implementation we're using.
 processor_name = settings.CC_PROCESSOR.keys()[0]
 module = __import__('shoppingcart.processors.' + processor_name,
-                    fromlist=['sign',
-                              'verify',
-                              'render_purchase_form_html'
-                              'payment_accepted',
-                              'record_purchase',
+                    fromlist=['render_purchase_form_html'
                               'process_postpay_callback',
                               ])
 
@@ -33,38 +29,4 @@ def process_postpay_callback(*args, **kwargs):
     return a helpful-enough error message in error_html.
     """
     return module.process_postpay_callback(*args, **kwargs)
-
-def sign(*args, **kwargs):
-    """
-    Given a dict (or OrderedDict) of parameters to send to the
-    credit card processor, signs them in the manner expected by
-    the processor
-
-    Returns a dict containing the signature
-    """
-    return module.sign(*args, **kwargs)
-
-def verify(*args, **kwargs):
-    """
-    Given a dict (or OrderedDict) of parameters to returned by the
-    credit card processor, verifies them in the manner specified by
-    the processor
-
-    Returns a boolean
-    """
-    return module.sign(*args, **kwargs)
-
-def payment_accepted(*args, **kwargs):
-    """
-    Given params returned by the CC processor, check that processor has accepted the payment
-    Returns a dict of {accepted:bool, amt_charged:float, currency:str, order:Order}
-    """
-    return module.payment_accepted(*args, **kwargs)
-
-def record_purchase(*args, **kwargs):
-    """
-    Given params returned by the CC processor, record that the purchase has occurred in
-    the database and also run callbacks
-    """
-    return module.record_purchase(*args, **kwargs)
 
