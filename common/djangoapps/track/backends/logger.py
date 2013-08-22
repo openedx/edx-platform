cd2029becfd70205fc164e599bc4279df84e647a
Carlos Andrés Rocha <rocha@edx.org>
2013-08-22 11:05:28 -0400

    wip - todos

--- common/djangoapps/track/backends/logger.py
+++ common/djangoapps/track/backends/logger.py
@@ -7,15 +7,25 @@ from django.conf import settings
 
 from track.backends.base import BaseBackend
 
-
 log = logging.getLogger('track.backends.logger')
 
 
 class LoggerBackend(BaseBackend):
     def __init__(self, **options):
         super(LoggerBackend, self).__init__(**options)
-        self.output = logging.getLogger('tracking')
+
+        # TODO: the configuration of the logger backend should come
+        # from the constructor options. Currently is being done using
+        # the Django settings for a logger named 'tracking'.
+
+        self.output_log = logging.getLogger('tracking')
 
     def send(self, event):
         event_str = json.dumps(event)
-        self.output.info(event_str[:settings.TRACK_MAX_EVENT])
+
+        # TODO: remove trucation of the serialized event, either at a
+        # higher level during the emittion of the event, or by
+        # providing warnings when the events exceed certain size.
+        event_str = event_str[:settings.TRACK_MAX_EVENT]
+
+        self.output_log.info(event_str)
