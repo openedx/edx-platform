83f8b033ea02d368dc898ae06b4968512fed12b4
ichuang <ichuang@mit.edu>
2013-08-26 20:23:11 -0400

    fix common/lib/xmodule/xmodule/modulestore/mongo/base.py

--- common/lib/xmodule/xmodule/modulestore/mongo/base.py
+++ common/lib/xmodule/xmodule/modulestore/mongo/base.py
@@ -313,7 +313,7 @@ class MongoModuleStore(ModuleStoreBase):
         query = {'_id.org': location.org,
                  '_id.course': location.course,
                  '_id.category': {'$in': ['course', 'chapter', 'sequential', 'vertical', 'videosequence',
-                                          'wrapper', 'problemset', 'conditional', 'randomize', 'proctor']}
+                                          'wrapper', 'problemset', 'conditional', 'randomize']}
                  }
         # we just want the Location, children, and inheritable metadata
         record_filter = {'_id': 1, 'definition.children': 1}
