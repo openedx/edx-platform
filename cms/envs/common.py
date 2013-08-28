b34ac22a3b3b79a393d035e0480a43ec6ac644e1
Pavel Yushchenko <pavelyushchenko@gmail.com>
2013-08-28 13:25:06 +0400

    new register forms

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -199,9 +199,9 @@ STATICFILES_DIRS = [
 
 # Locale/Internationalization
 TIME_ZONE = 'America/New_York'  # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-LANGUAGE_CODE = 'en'  # http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'ru'  # http://www.i18nguy.com/unicode/language-identifiers.html
 
-USE_I18N = False
+USE_I18N = True
 USE_L10N = True
 
 # Localization strings (e.g. django.po) are under this directory
