729d74f12941caac62cbb6c0db8f7167686905b7
Valera Rozuvan <valera@edx.org>
2013-08-29 11:13:50 +0300

    Fixing issues found by Anton during review of PR 804.
    Changed use of IDs to classes. Simplified lti.js - now simple one function.
    Updated tests.

--- common/lib/xmodule/xmodule/js/spec/lti/constructor.js
+++ common/lib/xmodule/xmodule/js/spec/lti/constructor.js
@@ -8,26 +8,20 @@
 
 (function () {
     describe('LTI', function () {
-        var documentReady = false,
-            element, errorMessage, frame,
+        var element, errorMessage, frame,
             editSettings = false;
 
         // This function will be executed before each of the it() specs
         // in this suite.
         beforeEach(function () {
-            $(document).ready(function () {
-                documentReady = true;
-            });
-
-            spyOn(LTI, 'init').andCallThrough();
             spyOn($.fn, 'submit').andCallThrough();
 
             loadFixtures('lti.html');
 
             element = $('#lti_id');
-            errorMessage = element.find('error_message');
-            form = element.find('form#ltiLaunchForm');
-            frame = element.find('iframe#ltiLaunchFrame');
+            errorMessage = element.find('.error_message');
+            form = element.find('.ltiLaunchForm');
+            frame = element.find('.ltiLaunchFrame');
 
             // First part of the tests will be running without the settings
             // filled in. Once we reach the describe() spec
@@ -36,32 +30,14 @@
             //
             // the variable `editSettings` will be changed to `true`.
             if (editSettings) {
-                form.attr('action', 'http://google.com/');
+                form.attr('action', 'http://www.example.com/');
             }
 
             LTI(element);
         });
 
         describe('constructor', function () {
-            it('init() is called after document is ready', function () {
-                waitsFor(
-                    function () {
-                        return documentReady;
-                    },
-                    'The document is ready',
-                    1000
-                );
-
-                runs(function () {
-                    expect(LTI.init).toHaveBeenCalled();
-                });
-            });
-
             describe('before settings were filled in', function () {
-                it('init() is called with element', function () {
-                    expect(LTI.init).toHaveBeenCalledWith(element);
-                });
-
                 it(
                     'when URL setting is empty error message is shown',
                     function () {
@@ -70,7 +46,7 @@
                 });
 
                 it('when URL setting is empty iframe is hidden', function () {
-                    expect(frame.css('display')).toBe('none');
+                    expect(frame).toHaveClass('hidden');
                 });
             });
 
@@ -95,15 +71,7 @@
                 });
 
                 it('when URL setting is filled iframe is shown', function () {
-                    expect(frame.css('display')).not.toBe('none');
-                });
-
-                it(
-                    'when URL setting is filled iframe is resized',
-                    function () {
-
-                    expect(frame.width()).toBe(form.parent().width());
-                    expect(frame.height()).toBe(800);
+                    expect(frame).not.toHaveClass('hidden');
                 });
             });
         });
