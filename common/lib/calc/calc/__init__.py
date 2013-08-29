ac90679544aa0f0bd78d0b60e21719413725f3dd
Adam Palay <adam@edx.org>
2013-08-29 15:26:05 -0400

    restructure calc

---
+++ common/lib/calc/calc/__init__.py
@@ -0,0 +1,1 @@
+from calc import *
--- common/lib/calc/setup.py
+++ common/lib/calc/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="calc",
     version="0.1.1",
-    py_modules=["calc"],
+    packages=["calc"],
     install_requires=[
         "pyparsing==1.5.6",
         "numpy",
