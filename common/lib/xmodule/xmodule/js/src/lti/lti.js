215b223688bd7f3fbc6e9d72bf13a5d3b131e9f5
Valera Rozuvan <valera@edx.org>
2013-08-29 11:13:50 +0300

    Fixing issues found by Anton during review of PR 804.
    Changed use of IDs to classes. Simplified lti.js - now simple one function.
    Updated tests.

--- common/lib/xmodule/xmodule/js/src/lti/lti.js
+++ common/lib/xmodule/xmodule/js/src/lti/lti.js
@@ -1,21 +1,9 @@
 window.LTI = (function () {
-    var LTI;
-
-    // Function LTI()
-    //
-    // The LTI module constructor. It will be called by XModule for any
-    // LTI module DIV that is found on the page.
-    LTI = function (element) {
-        $(document).ready(function () {
-            LTI.init(element);
-        });
-    }
-
-    // Function init()
+    // Function initialize(element)
     //
     // Initialize the LTI iframe.
-    LTI.init = function (element) {
-        var form, frame;
+    function initialize(element) {
+        var form;
 
         // In cms (Studio) the element is already a jQuery object. In lms it is
         // a DOM object.
@@ -24,26 +12,15 @@ window.LTI = (function () {
         // function. This will make it a jQuery object if it isn't already so.
         element = $(element);
 
-        form = element.find('#ltiLaunchForm');
-        frame = element.find('#ltiLaunchFrame');
+        form = element.find('.ltiLaunchForm');
 
         // If the Form's action attribute is set (i.e. we can perform a normal
-        // submit), then we submit the form and make it big enough so that the
-        // received response can fit in it. Hide the error message, if shown.
+        // submit), then we submit the form and make the frame shown.
         if (form.attr('action')) {
             form.submit();
-
-            element.find('.error_message').addClass('hidden');
-            frame.show();
-            frame.width('100%').height(800);
-        }
-
-        // If no action URL was specified, we show an error message.
-        else {
-            frame.hide();
-            element.find('.error_message').removeClass('hidden');
+            element.find('.lti').addClass('rendered')
         }
     }
 
-    return LTI;
+    return initialize;
 }());
