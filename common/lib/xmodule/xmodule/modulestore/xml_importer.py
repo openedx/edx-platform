beb5f2f846aba47b2d1b822b5c523858df06f1d7
Julian Arni <julian@edx.org>
2013-09-04 13:33:43 -0400

    Fix unicode errors on exceptions and logging

--- common/lib/xmodule/xmodule/modulestore/xml_importer.py
+++ common/lib/xmodule/xmodule/modulestore/xml_importer.py
@@ -31,7 +31,7 @@ def import_static_content(modules, course_loc, course_data_path, static_content_
             try:
                 content_path = os.path.join(dirname, filename)
                 if verbose:
-                    log.debug('importing static content {0}...'.format(content_path.encode("utf-8")))
+                    log.debug('importing static content %s...', content_path.encode("utf-8"))
 
                 fullname_with_subpath = content_path.replace(static_dir, '')  # strip away leading path from the name
                 if fullname_with_subpath.startswith('/'):
