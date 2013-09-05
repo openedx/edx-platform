f6d9c077f0568ea522a08576b0f8ec384dfbaeeb
Alexander Kryklia <kryklia@gmail.com>
2013-09-05 16:43:14 +0300

    Improve LTI module
    Add simplifyed template.
    Update lti integration test.
    Add getting oauth credentials from course settings.
    Add user id transferring to provider.
    Improve documentation of lti module.

--- lms/djangoapps/courseware/tests/test_lti.py
+++ lms/djangoapps/courseware/tests/test_lti.py
@@ -1,53 +1,69 @@
-"""LTI test"""
+"""LTI integration tests"""
 
 import requests
 from . import BaseTestXmodule
+from collections import OrderedDict
 
 
 class TestLTI(BaseTestXmodule):
-    """Integration test for word cloud xmodule."""
+    """
+    Integration test for lti xmodule.
+    """
     CATEGORY = "lti"
 
     def setUp(self):
+        """
+        Mock oauth1 signing of requests library for testing.
+        """
         super(TestLTI, self).setUp()
         mocked_noonce = u'135685044251684026041377608307'
         mocked_timestamp = u'1234567890'
-        mocked_signed_signature = u'my_signature%3D'
+        mocked_signature_after_sign = u'my_signature%3D'
         mocked_decoded_signature = u'my_signature='
 
         self.correct_headers = {
+            u'oauth_callback': u'about:blank',
+            u'lis_outcome_service_url': '',
+            u'lis_result_sourcedid': '',
+            u'launch_presentation_return_url': '',
+            u'lti_message_type': u'basic-lti-launch-request',
+            u'lti_version': 'LTI-1p0',
+
             u'oauth_nonce': mocked_noonce,
             u'oauth_timestamp': mocked_timestamp,
             u'oauth_consumer_key': u'',
             u'oauth_signature_method': u'HMAC-SHA1',
             u'oauth_version': u'1.0',
-            u'oauth_signature': mocked_decoded_signature}
+            u'user_id': self.runtime.anonymous_student_id,
+            u'role': u'student',
+            u'oauth_signature': mocked_decoded_signature
+        }
 
         saved_sign = requests.auth.Client.sign
 
         def mocked_sign(self, *args, **kwargs):
             """Mocked oauth1 sign function"""
-            # self is <oauthlib.oauth1.rfc5849.Client object at 0x107456e90> here:
+            # self is <oauthlib.oauth1.rfc5849.Client object> here:
             _, headers, _ = saved_sign(self, *args, **kwargs)
             # we should replace noonce, timestamp and signed_signature in headers:
             old = headers[u'Authorization']
-            new = old[:19] + mocked_noonce + old[49:69] + mocked_timestamp + \
-                old[79:179] + mocked_signed_signature + old[-1]
-            headers[u'Authorization'] = new
+            old_parsed = OrderedDict([param.strip().replace('"', '').split('=') for param in old.split(',')])
+            old_parsed[u'OAuth oauth_nonce'] = mocked_noonce
+            old_parsed[u'oauth_timestamp'] = mocked_timestamp
+            old_parsed[u'oauth_signature'] = mocked_signature_after_sign
+            headers[u'Authorization'] = ', '.join([k+'="'+v+'"' for k, v in old_parsed.items()])
             return None, headers, None
 
         requests.auth.Client.sign = mocked_sign
 
     def test_lti_constructor(self):
         """Make sure that all parameters extracted """
-        fragment = self.runtime.render(self.item_module, None, 'student_view')
+        self.runtime.render_template = lambda template, context: context
+        generated_context = self.item_module.get_html()
         expected_context = {
+            'input_fields': self.correct_headers,
             'element_class': self.item_module.location.category,
             'element_id': self.item_module.location.html_id(),
-            'lti_url': '',  # default value
+            'launch_url': '',  # default value
         }
-        self.correct_headers.update(expected_context)
-        self.assertEqual(
-            fragment.content,
-            self.runtime.render_template('lti.html', self.correct_headers)
-        )
+        self.assertDictEqual(generated_context, expected_context)
