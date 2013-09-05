9e1813041a91246f772111f73deac5c85c0a462b
Vik Paruchuri <vik@edx.org>
2013-09-05 11:52:05 -0400

    Fix nopathtoitem error

--- lms/djangoapps/open_ended_grading/utils.py
+++ lms/djangoapps/open_ended_grading/utils.py
@@ -1,6 +1,6 @@
 from xmodule.modulestore import search
 from xmodule.modulestore.django import modulestore
-from xmodule.modulestore.exceptions import ItemNotFoundError
+from xmodule.modulestore.exceptions import ItemNotFoundError, NoPathToItem
 
 def does_location_exist(course_id, location):
     """
@@ -12,5 +12,8 @@ def does_location_exist(course_id, location):
         search.path_to_location(modulestore(), course_id, location)
         return True
     except ItemNotFoundError:
-        #If the problem cannot be found at the location received from the grading controller server, it has been deleted by the course author.
+        # If the problem cannot be found at the location received from the grading controller server, it has been deleted by the course author.
+        return False
+    except NoPathToItem:
+        # If the problem can be found, but there is no path to it, then it is a draft.
         return False
