2d0af87d71399d8a3d51eda0fd4a51b1f34a8541
Alexander Kryklia <kryklia@gmail.com>
2013-09-06 17:22:46 +0300

    Add comma in last item of list.

--- cms/djangoapps/contentstore/views/component.py
+++ cms/djangoapps/contentstore/views/component.py
@@ -52,7 +52,7 @@ ADVANCED_COMPONENT_TYPES = [
     'annotatable',
     'word_cloud',
     'graphical_slider_tool',
-    'lti'
+    'lti',
 ] + OPEN_ENDED_COMPONENT_TYPES + NOTE_COMPONENT_TYPES
 ADVANCED_COMPONENT_CATEGORY = 'advanced'
 ADVANCED_COMPONENT_POLICY_KEY = 'advanced_modules'
