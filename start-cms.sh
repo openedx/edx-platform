42ebc3e486eef1eaf371b2b0f68c973a04cedb80
Hassan Benjamin Shirani <hshirani@zenfactory.org>
2013-09-18 01:23:53 -0500

    - file system cleanup

--- start-cms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. /etc/bash_completion.d/virtualenvwrapper; 
-workon edx-platform
-cd ~/edx-platform
-rake cms[dev,0.0.0.0:8001];
--- start-lms.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-. /etc/bash_completion.d/virtualenvwrapper; workon edx-platform; cd ~/edx-platform; rake lms[cms.dev,0.0.0.0:8000];
