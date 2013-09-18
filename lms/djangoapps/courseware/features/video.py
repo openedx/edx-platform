66bd8127e68102f61742d6ddfe76ad3435d97f23
jmclaus <jmc@edx.org>
2013-09-18 23:48:04 +0200

    Acceptance tests now pass

--- lms/djangoapps/courseware/features/video.py
+++ lms/djangoapps/courseware/features/video.py
@@ -17,7 +17,7 @@ HTML5_SOURCES_INCORRECT = [
 ]
 
 @step('when I view the (.*) it does not have autoplay enabled$')
-def does_autoplay_video(_step, video_type):
+def does_not_autoplay(_step, video_type):
     assert(world.css_find('.%s' % video_type)[0]['data-autoplay'] == 'False')
 
 
