66a78dbe1f1cd8cf817d9471a2cdb1be7a3d7fb9
Pavel Yushchenko <pavelyushchenko@gmail.com>
2013-10-16 13:59:12 +0400

    #openended: added content disposition to S3 uploads and uuid as key

--- common/lib/xmodule/xmodule/open_ended_grading_classes/openendedchild.py
+++ common/lib/xmodule/xmodule/open_ended_grading_classes/openendedchild.py
@@ -47,6 +47,7 @@ def upload_to_s3(file_to_upload, keyname, s3_interface):
     k.key = keyname
     k.set_metadata('filename', file_to_upload.name)
     k.set_contents_from_file(file_to_upload)
+    k.content_disposition = "filename=" + file_to_upload.name
 
     k.set_acl("public-read")
     public_url = k.generate_url(60 * 60 * 24 * 365)   # URL timeout in seconds.
@@ -418,9 +419,7 @@ class OpenEndedChild(object):
         @return: A URL corresponding to the uploaded object.
         """
 
-        file_key = file_data.name + datetime.now(UTC).strftime(
-            xqueue_interface.dateformat
-        )
+        file_key = str(uuid.uuid4())
 
         file_data.seek(0)
         s3_public_url = upload_to_s3(
