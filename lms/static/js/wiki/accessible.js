99321fc4d57999b79f31ed1bc024a125c9f6c89a
Julia Hansbrough <julia@edx.org>
2013-10-29 21:25:10 +0000

    Fixed alt-tab, alt-shift-tab, shift-tab

--- lms/static/js/wiki/accessible.js
+++ lms/static/js/wiki/accessible.js
@@ -2,9 +2,25 @@
    users to "tab through" elements on a page.  Including this file and setting keyMap to 
    "accessible" removes the "tab" from CodeMirror's default KeyMap to remedy this problem */
 
-var keyMap = CodeMirror.keyMap.accessible = {
-  "Left": "goCharLeft", "Right": "goCharRight", "Up": "goLineUp", "Down": "goLineDown",
-  "End": "goLineEnd", "Home": "goLineStartSmart", "PageUp": "goPageUp", "PageDown": "goPageDown",
-  "Delete": "delCharRight", "Backspace": "delCharLeft", "Shift-Tab": "indentAuto",
-  "Enter": "newlineAndIndent", "Insert": "toggleOverwrite"
-};
+(function() {
+  var keyMap = CodeMirror.keyMap.accessible = {
+  	"Left": "goCharLeft", 
+  	"Right": "goCharRight", 
+  	"Up": "goLineUp", 
+  	"Down": "goLineDown",
+    "End": "goLineEnd", 
+    "Home": "goLineStartSmart", 
+    "PageUp": "goPageUp", 
+    "PageDown": "goPageDown",
+    "Delete": "delCharAfter", 
+    "Backspace": "delCharBefore", 
+    "Shift-Backspace": "delCharBefore",
+    "Alt-Tab": "insertTab",
+    "Alt-Shift-Tab": "insertTab",
+    "Tab": false,
+    "Shift-Tab": false, 
+    "Enter": "newlineAndIndent", 
+    "Insert": "toggleOverwrite", 
+    fallthrough: "default"
+  };
+})();
