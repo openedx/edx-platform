d4953a245b38bf683695adcedbbffce166647edf
root <root@agava-8761.ejudge.ru>
2013-10-29 18:03:32 +0400

    fixes

--- lms/djangoapps/bulk_email/tasks.py
+++ lms/djangoapps/bulk_email/tasks.py
@@ -431,7 +431,7 @@ def _get_source_address(course_id, course_title):
     invalid_chars = re.compile(r"[^\w.-]")
     course_num = invalid_chars.sub('_', course_num)
 
-    from_addr = '"{0}" <{1}-{2}>'.format(course_title_no_quotes, course_num, settings.BULK_EMAIL_DEFAULT_FROM_EMAIL)
+    from_addr = u'"{0}" <{1}-{2}>'.format(course_title_no_quotes, course_num, settings.BULK_EMAIL_DEFAULT_FROM_EMAIL)
     return from_addr
 
 
