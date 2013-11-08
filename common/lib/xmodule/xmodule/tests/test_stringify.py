80d352460a650d1d027c6949fc7fdb76203133eb
David Baumgold <david@davidbaumgold.com>
2013-11-08 10:40:10 -0500

    Fix some wonky tests around stringify

--- common/lib/xmodule/xmodule/tests/test_stringify.py
+++ common/lib/xmodule/xmodule/tests/test_stringify.py
@@ -4,11 +4,11 @@ from xmodule.stringify import stringify_children
 
 
 def test_stringify():
-    text = 'Hi <div x="foo">there <span>Bruce</span><b>!</b></div>'
-    html = '''<html a="b" foo="bar">{0}</html>'''.format(text)
+    expected = 'Hi <div x="foo">there <span>Bruce</span><b>!</b></div>'
+    html = '''<html a="b" foo="bar">{0}</html>'''.format(expected)
     xml = etree.fromstring(html)
-    out = stringify_children(xml)
-    assert_equals(out, text)
+    actual = stringify_children(xml)
+    assert_equals(actual, expected)
 
 
 def test_stringify_again():
@@ -21,21 +21,14 @@ def test_stringify_again():
   which means linear except for an offset.
   </html>
 """
-
-    html = """<html>A voltage source is non-linear!
-  <div align="center">
-
+    expected = r"""A voltage source is non-linear!
+<div align="center">
+    <img src="/static/images/circuits/voltage-source.png"/>
+    \(V=V_C\)
   </div>
   But it is <a href="http://mathworld.wolfram.com/AffineFunction.html">affine</a>,
   which means linear except for an offset.
-  </html>
   """
     xml = etree.fromstring(html)
-    out = stringify_children(xml)
-
-    print "output:"
-    print out
-
-    # Tracking strange content repeating bug
-    # Should appear once
-    assert_equals(out.count("But it is "), 1)
+    actual = stringify_children(xml)
+    assert_equals(actual, expected)
