5a014ad44bce608db41d1cd2b1b292698be57bea
Giulio Gratta <giulio@giuliogratta.com>
2013-11-20 14:59:33 -0800

    Simplified HTML template and expanded JS to set up image modal on load.

--- common/lib/xmodule/xmodule/js/src/html/imageModal.js
+++ common/lib/xmodule/xmodule/js/src/html/imageModal.js
@@ -1,7 +1,40 @@
 $(function() {
-  $(".imageModal-link").toggle(function() {
-    $(".imageModal", this).show();
+  $("a.zoomable").each(function() {
+    var smallImageObject = $(this).children();
+    var largeImageSRC = $(this).attr('href');
+    
+    // if contents of zoomable link is image and large image link exists: setup modal
+    if (smallImageObject.is('img') && largeImageSRC) {
+      var smallImageHTML = $(this).html();
+      var largeImageHTML = '<img alt="" src="' + largeImageSRC + '" />';
+      var imageModalHTML =
+        '<div class="imageModal-link">' +
+          smallImageHTML +
+          '<i class="icon-resize-full icon-3x"></i></div>' +
+        '<div class="imageModal"><div class="imageModal-content">' +
+          largeImageHTML +
+          '<i class="icon-remove icon-3x"></div></div>';
+      $(this).replaceWith(imageModalHTML);
+    }
+  });
+  
+  $(".imageModal-link").click(function() {
+    $(this).siblings(".imageModal").show();
+  });
+  
+  var imageModalContentHover = false;
+  $(".imageModal-content").hover(function() {
+    imageModalContentHover = true;
   }, function() {
-    $(".imageModal", this).hide();
+    imageModalContentHover = false;
+  });
+  
+  $(".imageModal").click(function() {
+    if (!imageModalContentHover){
+      $(this).hide();
+    }
+  });
+  $(".imageModal-content i.icon-remove").click(function() {
+    $(this).closest(".imageModal").hide();
   });
 });
