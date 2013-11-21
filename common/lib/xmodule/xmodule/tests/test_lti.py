b8a42da889bbeb1425104377dcdb109e86d509f9
Oleg Marshev <oleh.marshev@gmail.com>
2013-11-21 18:12:11 +0200

    Add test.

--- common/lib/xmodule/xmodule/tests/test_lti.py
+++ common/lib/xmodule/xmodule/tests/test_lti.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 """Test for LTI Xmodule functional logic."""
+from mock import Mock
+import textwrap
 from xmodule.lti_module import LTIModuleDescriptor
 from . import LogicTest
 
 
 class LTIModuleTest(LogicTest):
-    """Logic tests for Poll Xmodule."""
+    """Logic tests for LTI module."""
     descriptor_class = LTIModuleDescriptor
 
     def test_handle_ajax(self):
@@ -26,3 +28,46 @@ class LTIModuleTest(LogicTest):
             self.assertEqual(response['status_code'], 200)
             if dispatch == 'read':
                 self.assertDictEqual(response['content']['value'], {'score': 0.5, 'total': 1.0})
+
+    def test_custom_handler(self):
+        self.system.get_real_user = Mock()
+        self.system.publish = Mock()
+
+        dispatch = 'replaceresult'
+        
+        xml_template = textwrap.dedent("""
+            <?xml version = "1.0" encoding = "UTF-8"?>
+                <imsx_POXEnvelopeRequest xmlns = "some_link (may be not required)">
+                  <imsx_POXHeader>
+                    <imsx_POXRequestHeaderInfo>
+                      <imsx_version>V1.0</imsx_version>
+                      <imsx_messageIdentifier>528243ba5241b</imsx_messageIdentifier>
+                    </imsx_POXRequestHeaderInfo>
+                  </imsx_POXHeader>
+                  <imsx_POXBody>
+                    <replaceResultRequest>
+                      <resultRecord>
+                        <sourcedGUID>
+                          <sourcedId>feb-123-456-2929::28883</sourcedId>
+                        </sourcedGUID>
+                        <result>
+                          <resultScore>
+                            <language>en-us</language>
+                            <textString>{score}</textString>
+                          </resultScore>
+                        </result>
+                      </resultRecord>
+                    </replaceResultRequest>
+                  </imsx_POXBody>
+                </imsx_POXEnvelopeRequest>
+        """)
+        
+        
+        mock_request = Mock()
+        good_values = {'score': '0.5'}
+        mock_request.body = xml_template.format(**good_values)
+
+        result = self.xmodule.custom_handler(mock_request, dispatch)
+        self.assertTrue(result.status_code == 200)
+
+
