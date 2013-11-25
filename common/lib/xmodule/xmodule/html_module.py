b65c19f0861a83a7b5aa85ca366548395feb6db6
Giulio Gratta <giulio@giuliogratta.com>
2013-11-25 11:33:52 -0800

    Added preliminary drag script.
    (currently uses Jquery UI draggable, but will be converted to draggabilly...)

--- common/lib/xmodule/xmodule/html_module.py
+++ common/lib/xmodule/xmodule/html_module.py
@@ -45,7 +45,8 @@ class HtmlModule(HtmlFields, XModule):
             resource_string(__name__, 'js/src/html/display.coffee')
         ],
         'js': [
-            resource_string(__name__, 'js/src/html/imageModal.js')
+            resource_string(__name__, 'js/src/html/imageModal.js'),
+            resource_string(__name__, 'js/common_static/js/vendor/draggabilly.pkgd.js')
         ]
     }
     js_module_name = "HTMLModule"
