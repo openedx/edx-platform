da51a52c8fba58ae86800fdc6cc3d0593a0b4ad6
Oleg Marshev <oleh.marshev@gmail.com>
2013-11-25 18:58:17 +0200

    Add unittests.

--- common/lib/xmodule/xmodule/tests/test_lti.py
+++ common/lib/xmodule/xmodule/tests/test_lti.py
@@ -14,7 +14,7 @@ class LTIModuleTest(LogicTest):
 
     xml_template = textwrap.dedent("""
             <?xml version = "1.0" encoding = "UTF-8"?>
-                <imsx_POXEnvelopeRequest xmlns = "some_link (may be not required)">
+                <imsx_POXEnvelopeRequest xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
                   <imsx_POXHeader>
                     <imsx_POXRequestHeaderInfo>
                       <imsx_version>V1.0</imsx_version>
@@ -39,153 +39,134 @@ class LTIModuleTest(LogicTest):
                 </imsx_POXEnvelopeRequest>
         """)
 
-    def test_handle_ajax(self):
-        # Make sure that ajax request works correctly.
-
-        good_requests = {'set': {'score': 5}, 'read': {}, 'delete': {}}
-        bad_requests = {'set': {}, 'unknown_dispatch': {}}
-
-        for dispatch, data in bad_requests.items():
-            response = self.ajax_request(dispatch, data)
-            if dispatch == 'set':
-                self.assertEqual(response['status_code'], 400)
-            else:
-                self.assertEqual(response['status_code'], 404)
-
-        for dispatch, data in good_requests.items():
-            response = self.ajax_request(dispatch, data)
-            self.assertEqual(response['status_code'], 200)
-            if dispatch == 'read':
-                self.assertDictEqual(response['content']['value'], {'score': 0.5, 'total': 1.0})
-
-    def test_valid_xml(self):
+    def test_authorization_header_not_present(self):
         """
-        Valid XML returned from Tool Provider.
+        Request has no Authorization header.
+        This is an unknown service request, i.e., it is not a part of the original service specification.
         """
         self.system.get_real_user = Mock()
+        self.xmodule.get_client_key_secret = Mock(return_value=('key', 'secret'))
+
         self.system.publish = Mock()
 
-        dispatch = 'replaceresult'
-               
-        mock_request = Mock()
-        
+        from webob.request import Request
+        environ = {'wsgi.url_scheme': 'http', 'REQUEST_METHOD': 'POST'}
+
+        incorrect_request = Request(environ)
+
         good_value = {'grade': '0.5'}
-        mock_request.body = self.xml_template.format(**good_value)
-        result = self.xmodule.custom_handler(mock_request, dispatch)
+        incorrect_request.body = self.xml_template.format(**good_value)
+        dispatch = 'test'
+        response = self.xmodule.grade_handler(incorrect_request, dispatch)
 
-        parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
-        root = etree.fromstring(result.body.strip(), parser=parser)
-        namespaces = {'def': root.nsmap.values()[0]}
+        code_major = self.get_code_major(response)
 
-        code_major = root.xpath("//def:imsx_codeMajor", namespaces=namespaces)[0].text
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(code_major, 'unsupported', code_major)
 
-        self.assertEqual(code_major, 'success', code_major)
+    def test_authorization_header_empty(self):
+        """
+        Request Authorization header has no value.
+        This is an unknown service request, i.e., it is not a part of the original service specification.
+        """
+        self.system.get_real_user = Mock()
+        self.xmodule.get_client_key_secret = Mock(return_value=('key', 'secret'))
 
+        self.system.publish = Mock()
+
+        from webob.request import Request
+        environ = {'wsgi.url_scheme': 'http', 'REQUEST_METHOD': 'POST'}
+
+        incorrect_request = Request(environ)
+        incorrect_request.authorization = "bad authorization header"
+
+        good_value = {'grade': '0.5'}
+        incorrect_request.body = self.xml_template.format(**good_value)
+        dispatch = 'test'
+        response = self.xmodule.grade_handler(incorrect_request, dispatch)
+
+        code_major = self.get_code_major(response)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(code_major, 'unsupported', code_major)
 
     def test_bad_grade_range(self):
         """
         Grade returned from Tool Provider is outside the range 0.0-1.0.
         """
+        self.xmodule.verify_oauth_body_sign = Mock()
         self.system.get_real_user = Mock()
-        self.system.publish = Mock()
 
-        dispatch = 'replaceresult'
-               
-        mock_request = Mock()
-        
-        bad_value = {'grade': '100'}
-        mock_request.body = self.xml_template.format(**bad_value)
-        result = self.xmodule.custom_handler(mock_request, dispatch)
+        from webob.request import Request
+        environ = {'wsgi.url_scheme': 'http', 'REQUEST_METHOD': 'POST'}
 
-        parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
-        root = etree.fromstring(result.body.strip(), parser=parser)
-        namespaces = {'def': root.nsmap.values()[0]}
-        code_major = root.xpath("//def:imsx_codeMajor", namespaces=namespaces)[0].text
+        incorrect_request = Request(environ)
 
+        #grade not in range
+        bad_value = {'grade': '10'}
+        incorrect_request.body = self.xml_template.format(**bad_value)
+        dispatch = 'test'
+
+        response = self.xmodule.grade_handler(incorrect_request, dispatch)
+
+        code_major = self.get_code_major(response)
+
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(code_major, 'failure', code_major)
 
     def test_bad_grade_decimal(self):
         """
         Grade returned from Tool Provider doesn't use a period as the decimal point.
         """
+        self.xmodule.verify_oauth_body_sign = Mock()
         self.system.get_real_user = Mock()
-        self.system.publish = Mock()
 
-        dispatch = 'replaceresult'
-       
-        mock_request = Mock()
+        from webob.request import Request
+        environ = {'wsgi.url_scheme': 'http', 'REQUEST_METHOD': 'POST'}
+
+        incorrect_request = Request(environ)
+
         bad_value = {'grade': '0,5'}
-        mock_request.body = self.xml_template.format(**bad_value)
-        result = self.xmodule.custom_handler(mock_request, dispatch)
+        
+        incorrect_request.body = self.xml_template.format(**bad_value)
+        dispatch = 'test'
+        response = self.xmodule.grade_handler(incorrect_request, dispatch)
 
-        parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
-        root = etree.fromstring(result.body.strip(), parser=parser)
-        namespaces = {'def': root.nsmap.values()[0]}
-        code_major = root.xpath("//def:imsx_codeMajor", namespaces=namespaces)[0].text
+        code_major = self.get_code_major(response)
 
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(code_major, 'failure', code_major)
 
-    def test_incomplete_response_body(self):
-        """
-        Response body from Tool Provider doesn't contain messageIdentifier and sourcedId and textString.
-        """
-        self.system.get_real_user = Mock()
-        self.system.publish = Mock()
-
-        dispatch = 'replaceresult'
-
-        xml_template = textwrap.dedent("""
-            <?xml version = "1.0" encoding = "UTF-8"?>
-                <imsx_POXEnvelopeRequest xmlns = "some_link (may be not required)">
-                  <imsx_POXHeader>
-                    <imsx_POXRequestHeaderInfo>
-                      <imsx_version>V1.0</imsx_version>
-                      "here must be messageIdentifier"
-                    </imsx_POXRequestHeaderInfo>
-                  </imsx_POXHeader>
-                  <imsx_POXBody>
-                    <replaceResultRequest>
-                      <resultRecord>
-                        <sourcedGUID>
-                          "here must be sourcedId"
-                        </sourcedGUID>
-                        <result>
-                          <resultScore>
-                            <language>en-us</language>
-                            "here must be textString"
-                          </resultScore>
-                        </result>
-                      </resultRecord>
-                    </replaceResultRequest>
-                  </imsx_POXBody>
-                </imsx_POXEnvelopeRequest>
-        """)
-
-        mock_request = Mock()
-        mock_request.body = xml_template
-        result = self.xmodule.custom_handler(mock_request, dispatch)
-
+    def get_code_major(self, response):
         parser = etree.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
-        root = etree.fromstring(result.body.strip(), parser=parser)
+        root = etree.fromstring(response.body.strip(), parser=parser)
         namespaces = {'def': root.nsmap.values()[0]}
         code_major = root.xpath("//def:imsx_codeMajor", namespaces=namespaces)[0].text
+        return code_major
 
+    @unittest.skip("not implemented")
+    def test_good_request(self):
+        
+        self.system.get_real_user = Mock()
+        self.xmodule.get_client_key_secret = Mock(return_value=('test_key', 'test_secret'))
+
+        self.system.publish = Mock()
+
+        from webob.request import Request
+        environ = {'wsgi.url_scheme': 'http', 'REQUEST_METHOD': 'POST'}
+
+        incorrect_request = Request(environ)
+        incorrect_request.authorization = "bad authorization header"
+
+        good_value = {'grade': '0.5'}
+        incorrect_request.body = self.xml_template.format(**good_value)
+        dispatch = 'test'
+        response = self.xmodule.grade_handler(incorrect_request, dispatch)
+
+        code_major = self.get_code_major(response)
+
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(code_major, 'unsupported', code_major)
-
-    @unittest.skip("skipped because not completed")
-    def test_authorization_header_not_present(self):
-        """
-        Authorization header not provided in request.
-        """
-        mock_request = Mock()
-        mock_request.META = None
-
-        mock_course = Mock()
-        import ipdb; ipdb.set_trace()
-        descriptor = LTIModuleDescriptor()
-
-        result = descriptor.authenticate(mock_request, mock_course)
-
 
 
 
