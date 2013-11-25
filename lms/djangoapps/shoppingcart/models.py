cc187a318316cc904b8fb1f1c8743e7695f6233a
Julia Hansbrough <julia@edx.org>
2013-11-25 21:52:12 +0000

    Added refund datetime info to Orders, OrderItems
    Finance would like to have info about the date any given refund is requested.
    
    This commit adds a refund_requested_time field to OrderItem and a refund_time to Order.
    
    When a user *requests* a refund, the refund_requested_time field on OrderItem is updated; when a user actually *receives* their refund, the refund_time on Order is updated.
    
    Note that only the refund_requested_teim field is actually used right now; later on, when we're able to actually automate the process of actually giving users refunds, we will start usings Order's refund_time field.

--- lms/djangoapps/shoppingcart/models.py
+++ lms/djangoapps/shoppingcart/models.py
@@ -214,6 +214,7 @@ class OrderItem(models.Model):
     refund_requested_time = models.DateTimeField(null=True)
     # general purpose field, not user-visible.  Used for reporting
     report_comments = models.TextField(default="")
+    refund_requested_time = models.DateTimeField(null=True)
 
     @property
     def line_cost(self):
