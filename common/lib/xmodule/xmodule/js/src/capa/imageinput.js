418d6fa9e18f34cbc5cd3fa669a27f0cfb1b20c5
polesye <s2pak.anton@gmail.com>
2013-12-11 15:20:05 +0200

    BLD-413: Add rounding of coordinates.

--- common/lib/xmodule/xmodule/js/src/capa/imageinput.js
+++ common/lib/xmodule/xmodule/js/src/capa/imageinput.js
@@ -11,10 +11,10 @@ window.image_input_click = function(id,event){
     iidiv = document.getElementById("imageinput_"+id);
     pos_x = event.offsetX?(event.offsetX):event.pageX-iidiv.offsetLeft;
     pos_y = event.offsetY?(event.offsetY):event.pageY-iidiv.offsetTop;
-    result = "[" + pos_x + "," + pos_y + "]";
+    result = "[" + Math.round(pos_x) + "," + Math.round(pos_y) + "]";
     cx = (pos_x-15) +"px";
-    cy = (pos_y-15)  +"px" ;
-    // alert(result);
+    cy = (pos_y-15)  +"px";
+
     document.getElementById("cross_"+id).style.left = cx;
     document.getElementById("cross_"+id).style.top = cy;
     document.getElementById("cross_"+id).style.visibility = "visible" ;
