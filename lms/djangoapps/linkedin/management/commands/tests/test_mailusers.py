fc17ed26d4f518c78db54ba967c0ae9c35f4724e
Chris Rossi <chris@archimedeanco.com>
2013-12-13 10:40:01 -0500

    Properly handle already emailed courses.

--- lms/djangoapps/linkedin/management/commands/tests/test_mailusers.py
+++ lms/djangoapps/linkedin/management/commands/tests/test_mailusers.py
@@ -1,6 +1,7 @@
 """
 Test email scripts.
 """
+import json
 import mock
 import unittest
 
@@ -17,11 +18,18 @@ class MailusersTests(unittest.TestCase):
     @mock.patch('linkedin.management.commands.mailusers.LinkedIn')
     def test_mail_users(self, linkedin, certificates, send_email):
         """
-        Test "happy path" for emailing users.
+        Test emailing users.
         """
         fut = mailusers.Command().handle
-        fred = mock.Mock(user=mock.Mock(certificates=[1, 2]))
-        barney = mock.Mock(user=mock.Mock(certificates=[3]))
+        cert1 = mock.Mock(course_id=1)
+        cert2 = mock.Mock(course_id=2)
+        cert3 = mock.Mock(course_id=3)
+        fred = mock.Mock(
+            emailed_courses="[]",
+            user=mock.Mock(certificates=[cert1, cert2]))
+        barney = mock.Mock(
+            emailed_courses="[]",
+            user=mock.Mock(certificates=[cert3]))
         linkedin.objects.filter.return_value = [fred, barney]
 
         def filter_user(user):
@@ -34,20 +42,29 @@ class MailusersTests(unittest.TestCase):
         fut()
         self.assertEqual(
             send_email.call_args_list,
-            [((fred.user, 1),),
-             ((fred.user, 2),),
-             ((barney.user, 3),)])
+            [((fred.user, cert1),),
+             ((fred.user, cert2),),
+             ((barney.user, cert3),)])
+        self.assertEqual(json.loads(fred.emailed_courses), [1, 2])
+        self.assertEqual(json.loads(barney.emailed_courses), [3])
 
     @mock.patch('linkedin.management.commands.mailusers.send_grandfather_email')
     @mock.patch('linkedin.management.commands.mailusers.GeneratedCertificate')
     @mock.patch('linkedin.management.commands.mailusers.LinkedIn')
     def test_mail_users_grandfather(self, linkedin, certificates, send_email):
         """
-        Test "happy path" for sending grandfather emails.
+        Test sending grandfather emails.
         """
         fut = mailusers.Command().handle
-        fred = mock.Mock(user=mock.Mock(certificates=[1, 2]))
-        barney = mock.Mock(user=mock.Mock(certificates=[3]))
+        cert1 = mock.Mock(course_id=1)
+        cert2 = mock.Mock(course_id=2)
+        cert3 = mock.Mock(course_id=3)
+        fred = mock.Mock(
+            emailed_courses="[]",
+            user=mock.Mock(certificates=[cert1, cert2]))
+        barney = mock.Mock(
+            emailed_courses="[]",
+            user=mock.Mock(certificates=[cert3]))
         linkedin.objects.filter.return_value = [fred, barney]
 
         def filter_user(user):
@@ -60,5 +77,43 @@ class MailusersTests(unittest.TestCase):
         fut(grandfather=True)
         self.assertEqual(
             send_email.call_args_list,
-            [((fred.user, [1, 2]),),
-             ((barney.user, [3]),)])
+            [((fred.user, [cert1, cert2]),),
+             ((barney.user, [cert3]),)])
+        self.assertEqual(json.loads(fred.emailed_courses), [1, 2])
+        self.assertEqual(json.loads(barney.emailed_courses), [3])
+
+    @mock.patch('linkedin.management.commands.mailusers.send_email')
+    @mock.patch('linkedin.management.commands.mailusers.GeneratedCertificate')
+    @mock.patch('linkedin.management.commands.mailusers.LinkedIn')
+    def test_mail_users_only_new_courses(self, linkedin, certificates,
+                                         send_email):
+        """
+        Test emailing users, making sure they are only emailed about new
+        certificates.
+        """
+        fut = mailusers.Command().handle
+        cert1 = mock.Mock(course_id=1)
+        cert2 = mock.Mock(course_id=2)
+        cert3 = mock.Mock(course_id=3)
+        fred = mock.Mock(
+            emailed_courses="[1]",
+            user=mock.Mock(certificates=[cert1, cert2]))
+        barney = mock.Mock(
+            emailed_courses="[]",
+            user=mock.Mock(certificates=[cert3]))
+        linkedin.objects.filter.return_value = [fred, barney]
+
+        def filter_user(user):
+            "Mock querying the database."
+            queryset = mock.Mock()
+            queryset.filter.return_value = user.certificates
+            return queryset
+
+        certificates.objects.filter = filter_user
+        fut()
+        self.assertEqual(
+            send_email.call_args_list,
+            [((fred.user, cert2),),
+             ((barney.user, cert3),)])
+        self.assertEqual(json.loads(fred.emailed_courses), [1, 2])
+        self.assertEqual(json.loads(barney.emailed_courses), [3])
