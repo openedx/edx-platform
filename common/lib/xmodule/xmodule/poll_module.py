06fadcdc0c639d32f15c472e9e5eb48b5ba5f3a5
Calen Pennington <cale@edx.org>
2013-12-18 16:18:03 -0500

    Make conditional module and poll modules a little easier to understand

--- common/lib/xmodule/xmodule/poll_module.py
+++ common/lib/xmodule/xmodule/poll_module.py
@@ -32,7 +32,9 @@ class PollFields(object):
     poll_answer = String(help="Student answer", scope=Scope.user_state, default='')
     poll_answers = Dict(help="All possible answers for the poll fro other students", scope=Scope.user_state_summary)
 
+    # List of answers, in the form {'id': 'some id', 'text': 'the answer text'}
     answers = List(help="Poll answers from xml", scope=Scope.content, default=[])
+
     question = String(help="Poll question", scope=Scope.content, default='')
 
 
