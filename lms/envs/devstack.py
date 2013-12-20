2508673e59ffb76a3c441e9d055e0832319df7c1
Oleg Marshev <oleh.marshev@gmail.com>
2013-12-20 17:04:24 +0200

    Refactor stub implementation of LTI Provider. BLD-601.

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -7,6 +7,7 @@ from .aws import * # pylint: disable=wildcard-import, unused-wildcard-import
 DEBUG = True
 USE_I18N = True
 TEMPLATE_DEBUG = True
+SITE_NAME = 'localhost:8000'
 # By default don't use a worker, execute tasks as if they were local functions
 CELERY_ALWAYS_EAGER = True
 
