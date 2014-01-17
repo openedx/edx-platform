981a7f947972cc166873a03809c08c5e48cebee4
Adam Palay <adam@edx.org>
2014-01-17 14:13:46 -0500

    Revert django-debug-toolbar related updates on master, set version to 0.9.4 (STUD-1201)
    Revert "Specific django-toolbar version (stable released) and update the panels in dev envs"
    
    This reverts commit a465b082da3e45604f7b4ccb210ce2826e79e72d.
    
    Revert "Updated settings for devstack django debug toolbar"
    
    This reverts commit 30199e8a615c54640116a6c223ba90e32a6967d2.

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -35,14 +35,14 @@ MIDDLEWARE_CLASSES += ('django_comment_client.utils.QueryCountDebugMiddleware',
 INTERNAL_IPS = ('127.0.0.1',)
 
 DEBUG_TOOLBAR_PANELS = (
-   'debug_toolbar.panels.versions.VersionsPanel',
-   'debug_toolbar.panels.timer.TimerPanel',
-   'debug_toolbar.panels.settings.SettingsPanel',
-   'debug_toolbar.panels.headers.HeadersPanel',
-   'debug_toolbar.panels.request.RequestPanel',
-   'debug_toolbar.panels.sql.SQLPanel',
-   'debug_toolbar.panels.signals.SignalsPanel',
-   'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 DEBUG_TOOLBAR_CONFIG = {
