ebe07491e552797c7c499b66eeda8fb1fff0a203
lduarte1991 <lduarte1991@gmail.com>
2014-01-17 00:01:43 -0500

    First set of fixes from the pull request
    This does not include some of the testing files. The textannotation and
    videoannotation test files are not ready. waiting for an answer on the
    issue.

--- common/lib/xmodule/xmodule/tests/test_textannotation.py
+++ common/lib/xmodule/xmodule/tests/test_textannotation.py
@@ -9,66 +9,50 @@ from xblock.field_data import DictFieldData
 from xblock.fields import ScopeIds
 
 from xmodule.textannotation_module import TextAnnotationModule
+from xmodule.modulestore import Location
 
 from . import get_test_system
 
-
 class TextAnnotationModuleTestCase(unittest.TestCase):
-    ''' text Annotation Module Test Case '''
     sample_xml = '''
         <annotatable>
             <instructions><p>Test Instructions.</p></instructions>
             <p>
                 One Fish. Two Fish.
                 Red Fish. Blue Fish.
-
+                
                 Oh the places you'll go!
             </p>
         </annotatable>
     '''
-
     def setUp(self):
-        """
-            Makes sure that the Module is declared and mocked with the sample xml above.
-        """
         self.mod = TextAnnotationModule(
             Mock(),
             get_test_system(),
-            DictFieldData({'data': self.sample_xml}),
+            DictFieldData({'data':self.sample_xml}),
             ScopeIds(None, None, None, None)
         )
-
+    
     def test_render_content(self):
-        """
-        Tests to make sure the sample xml is rendered and that it forms a valid xmltree
-        that does not contain a display_name.
-        """
-        content = self.mod._render_content()  # pylint: disable=W0212
+        content = self.mod._render_content()
         self.assertIsNotNone(content)
-        element = etree.fromstring(content)
-        self.assertIsNotNone(element)
-        self.assertFalse('display_name' in element.attrib, "Display Name should have been deleted from Content")
-
+        el = etree.fromstring(content)
+        self.assertIsNotNone(el)
+        self.assertFalse('display_name' in el.attrib, "Display Name should have been deleted from Content")
+    
     def test_extract_instructions(self):
-        """
-        Tests to make sure that the instructions are correctly pulled from the sample xml above.
-        It also makes sure that if no instructions exist, that it does in fact return nothing.
-        """
         xmltree = etree.fromstring(self.sample_xml)
 
         expected_xml = u"<div><p>Test Instructions.</p></div>"
-        actual_xml = self.mod._extract_instructions(xmltree)  # pylint: disable=W0212
+        actual_xml = self.mod._extract_instructions(xmltree)
         self.assertIsNotNone(actual_xml)
         self.assertEqual(expected_xml.strip(), actual_xml.strip())
 
         xmltree = etree.fromstring('<annotatable>foo</annotatable>')
-        actual = self.mod._extract_instructions(xmltree)  # pylint: disable=W0212
+        actual = self.mod._extract_instructions(xmltree)
         self.assertIsNone(actual)
-
+    
     def test_get_html(self):
-        """
-        Tests the function that passes in all the information in the context that will be used in templates/textannotation.html
-        """
         context = self.mod.get_html()
-        for key in ['display_name', 'tag', 'source', 'instructions_html', 'content_html', 'annotation_storage']:
+        for key in ['display_name', 'tag','source','element_id','instructions_html','content_html','annotation_storage']:
             self.assertIn(key, context)
