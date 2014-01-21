5c655669770af9b93a66f6f931bb3127bc567413
Mark Hoeber <hoeber@edx.org>
2014-01-21 09:15:12 -0500

    Fixed conf file
    Removed extra RTD theme assignment

--- docs/data/source/conf.py
+++ docs/data/source/conf.py
@@ -11,9 +11,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 
