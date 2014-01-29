e7c5e62da700f51e69662689758ffebf70fa1494
David Baumgold <david@davidbaumgold.com>
2014-01-29 15:28:40 -0500

    Read from doc url mapping file at load time, rather than once per request

--- cms/djangoapps/contentstore/context_processors.py
+++ cms/djangoapps/contentstore/context_processors.py
@@ -1,12 +1,12 @@
 import ConfigParser
 from django.conf import settings
 
+config_file = open(settings.REPO_ROOT / "docs" / "config.ini")
+config = ConfigParser.ConfigParser()
+config.readfp(config_file)
+
 
 def doc_url(request):
-    config_file = open(settings.REPO_ROOT / "docs" / "config.ini")
-    config = ConfigParser.ConfigParser()
-    config.readfp(config_file)
-
     # in the future, we will detect the locale; for now, we will
     # hardcode en_us, since we only have English documentation
     locale = "en_us"
