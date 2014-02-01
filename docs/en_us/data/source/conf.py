390c93c63720b9cc5cd491e0a33ae49bdcefb7f0
Mark Hoeber <hoeber@edx.org>
2014-02-01 09:57:20 -0500

    removed imports

--- docs/en_us/data/source/conf.py
+++ docs/en_us/data/source/conf.py
@@ -28,12 +28,12 @@ sys.path.insert(0, os.path.abspath('.'))
 master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path.append('source/_templates')
+#templates_path.append('source/_templates')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path.append('source/_static')
+#html_static_path.append('source/_static')
 
 project = u'edX Data Documentation'
 copyright = u'2014, edX'
