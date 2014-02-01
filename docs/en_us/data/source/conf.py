2cb2e2c52a8c161ebe2ec99892f3f25544d8af25
Mark Hoeber <hoeber@edx.org>
2014-02-01 09:30:35 -0500

    Added combineopenended statuses
    cleaned up config file as well

--- docs/en_us/data/source/conf.py
+++ docs/en_us/data/source/conf.py
@@ -3,10 +3,23 @@
 
 import sys, os
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-sys.path.append(os.path.abspath('../../../../'))
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+
+
+
+
+
+
 sys.path.append(os.path.abspath('../../../'))
+sys.path.append(os.path.abspath('../../'))
 
 from docs.shared.conf import *
 
@@ -23,7 +36,7 @@ templates_path.append('source/_templates')
 html_static_path.append('source/_static')
 
 project = u'edX Data Documentation'
-copyright = u'2013, edX Documentation Team'
+copyright = u'2013, edX'
 
 # The short X.Y version.
 version = ''
