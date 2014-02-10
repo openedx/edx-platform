2f67d0aab4282cb1c0f6a9f3740a8e4699d3e5a1
Ned Batchelder <ned@edx.org>
2014-02-10 15:13:11 -0500

    Remove tabs.

--- common/lib/xmodule/xmodule/util/django.py
+++ common/lib/xmodule/xmodule/util/django.py
@@ -1,6 +1,6 @@
 """
 Exposes Django utilities for consumption in the xmodule library
-NOTE: This file should only be imported into 'django-safe' code, i.e. known that this code runs int the Django 
+NOTE: This file should only be imported into 'django-safe' code, i.e. known that this code runs int the Django
 runtime environment with the djangoapps in common configured to load
 """
 
@@ -9,12 +9,12 @@ from crum import get_current_request
 
 
 def get_current_request_hostname():
-	"""
-	This method will return the hostname that was used in the current Django request
-	"""
-	hostname = None
-	request = get_current_request()
-	if request:
-		hostname = request.META.get('HTTP_HOST')
+    """
+    This method will return the hostname that was used in the current Django request
+    """
+    hostname = None
+    request = get_current_request()
+    if request:
+        hostname = request.META.get('HTTP_HOST')
 
-	return hostname
+    return hostname
