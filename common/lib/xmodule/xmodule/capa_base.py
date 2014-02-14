704e03d8cb94245383a530ff92f5fa35e12a65fe
Nick Parlante <nick.parlante@cs.stanford.edu>
2014-02-13 21:19:18 -0800

    Rename Check/Submit button for Stanford branch

--- common/lib/xmodule/xmodule/capa_base.py
+++ common/lib/xmodule/xmodule/capa_base.py
@@ -394,8 +394,8 @@ class CapaMixin(CapaFields):
         # The logic flow is a little odd so that _('xxx') strings can be found for
         # translation while also running _() just once for each string.
         _ = self.runtime.service(self, "i18n").ugettext
-        check = _('Check')
-        final_check = _('Final Check')
+        check = _('Submit')
+        final_check = _('Final Submit')
 
         # Apply customizations if present
         if 'custom_check' in self.text_customization:
