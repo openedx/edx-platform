f61a0f907bcc929d5919a9123cbbb3b17f92067e
John Cox <johncox@google.com>
2014-02-21 14:11:13 -0800

    Address review comments and simplify tests.

--- common/djangoapps/auth/tests/test_settings.py
+++ common/djangoapps/auth/tests/test_settings.py
@@ -2,47 +2,17 @@
 Tests settings across configurations.
 """
 
-import functools
+import unittest
 
 from django.conf import settings
 from django.test import TestCase
 
-
-def with_feature(key):
-    """Decorator that runs `test_fn` iff settings.FEATURES has `key`.
-
-    @with_feature('foo')
-    def test_bar(...):
-
-    means test_bar will execute if settings.FEATURES['foo'] is present, whether
-    settings.FEATURES.get('foo') is truthy or falsy. This lets us declare our
-    constraint in the signature rather that with boilerplate in each test body.
-
-    `key` is a string matching a key in settings.FEATURES.
-
-    Returns a wrapped version of `test_fn`.
-    """
-
-    # Silence missing docstring complaints for inner functions.
-    # pylint: disable-msg=missing-docstring
-    def guarded_test_fn(test_fn):
-
-        # Test method names must start with test_ for test reporting and
-        # discovery, so use wraps rather than returning a naked function.
-        @functools.wraps(test_fn)
-        def guard(*args, **kwargs):
-            if key in settings.FEATURES:
-                test_fn(*args, **kwargs)
-
-        return guard
-
-    return guarded_test_fn
+_AUTH_FEATURES_KEY = 'ENABLE_THIRD_PARTY_AUTH'
 
 
 class TestCommonSettings(TestCase):
     """Tests settings from lms/envs/common.py."""
 
-    @with_feature('ENABLE_THIRD_PARTY_AUTH')
+    @unittest.skipUnless(_AUTH_FEATURES_KEY in settings.FEATURES, '%s not present' % _AUTH_FEATURES_KEY)
     def test_enable_third_party_auth_is_disabled(self):
-        self.assertIs(
-            False, settings.FEATURES.get('ENABLE_THIRD_PARTY_AUTH'))
+        self.assertIs(False, settings.FEATURES.get(_AUTH_FEATURES_KEY))
