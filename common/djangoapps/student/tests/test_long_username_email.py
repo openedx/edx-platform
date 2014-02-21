874d7360c1441d20dc1927576fd3a41aa88d3ec2
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2014-02-21 19:00:52 +0500

    Added validation for username and email max length. LMS-1479

---
+++ common/djangoapps/student/tests/test_long_username_email.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import json
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+
+class TestLongUsernameEmail(TestCase):
+
+    def setUp(self):
+        self.url = reverse('create_account')
+        self.url_params = {
+            'username': 'username',
+            'email': 'foo_bar' + '@bar.com',
+            'name': 'foo bar',
+            'password': '123',
+            'terms_of_service': 'true',
+            'honor_code': 'true',
+        }
+
+    def test_long_username(self):
+        """
+        Test username cannot be more than 30 characters long.
+        """
+
+        self.url_params['username'] = 'username' * 4
+        response = self.client.post(self.url, self.url_params)
+
+        # Status code should be 400.
+        self.assertEqual(response.status_code, 400)
+
+        obj = json.loads(response.content)
+        self.assertEqual(
+            obj['value'],
+            "Username cannot be more than 30 characters long",
+        )
+
+    def test_long_email(self):
+        """
+        Test email cannot be more than 75 characters long.
+        """
+
+        self.url_params['email'] = '{0}@bar.com'.format('foo_bar' * 15)
+        response = self.client.post(self.url, self.url_params)
+
+        # Status code should be 400.
+        self.assertEqual(response.status_code, 400)
+
+        obj = json.loads(response.content)
+        self.assertEqual(
+            obj['value'],
+            "Email cannot be more than 75 characters long",
+        )
