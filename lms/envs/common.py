2735b6ef8991b4b6caafb379cc6937d34954537d
jazinheira <jva357@gmail.com>
2014-02-27 11:37:01 -0500

    Enabled LMS Chat
    ENABLE_CHAT in lms/envs/common.py set to True.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -182,7 +182,7 @@ FEATURES = {
 
     # Toggle to enable chat availability (configured on a per-course
     # basis in Studio)
-    'ENABLE_CHAT': False,
+    'ENABLE_CHAT': True,
 
     # Allow users to enroll with methods other than just honor code certificates
     'MULTIPLE_ENROLLMENT_ROLES': False,
