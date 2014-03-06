aa25f906c0e8e15e9082145af10977ded712bb64
Andy Armstrong <andya@edx.org>
2014-03-06 00:29:16 -0500

    Fix split_test xblock so that it can render in Studio
    STUD-1403

--- common/test/acceptance/pages/studio/container.py
+++ common/test/acceptance/pages/studio/container.py
@@ -71,4 +71,4 @@ class XBlockWrapper(PageObject):
 
     @property
     def preview_selector(self):
-        return self._bounded_selector('.xblock-student_view')
+        return self._bounded_selector('.xblock-studio_preview_view')
