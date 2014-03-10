9f2b8bb0ede9a7d0991b631fd903729595f21dd4
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-03-10 16:48:41 +0100

    Disable cache busting

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -874,7 +874,7 @@ X_FRAME_OPTIONS = 'ALLOW'
 
 ############################### Pipeline #######################################
 
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
 
 from rooted_paths import rooted_glob
 
