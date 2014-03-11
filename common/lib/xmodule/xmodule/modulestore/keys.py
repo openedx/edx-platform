99968526134d0b87f81c6f2dd6f5b579e3764048
Calen Pennington <cale@edx.org>
2014-03-11 12:05:50 -0400

    Add base classes for opaque keys to be used by LMS, Studio, and the modulestores

---
+++ common/lib/xmodule/xmodule/modulestore/keys.py
@@ -0,0 +1,58 @@
+from abc import ABCMeta, abstractmethod
+
+from opaque_keys import OpaqueKey
+
+
+class CourseKey(OpaqueKey):
+    KEY_TYPE = 'course_key'
+
+    @abstractmethod
+    def org(self):
+        """
+        The organization that this course belongs to.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def run(self):
+        """
+        The run identifier for this course.
+        """
+        raise NotImplementedError()
+
+
+class DefinitionKey(OpaqueKey):
+    """
+    An :class:`opaque_keys.OpaqueKey` identifying an XBlock definition.
+    """
+    KEY_TYPE = 'definition_key'
+
+    @abstractmethod
+    def block_type(self):
+        """
+        The XBlock type of this definition.
+        """
+        raise NotImplementedError()
+
+
+class UsageKey(OpaqueKey):
+    """
+    An :class:`opaque_keys.OpaqueKey` identifying an XBlock usage.
+    """
+    KEY_TYPE = 'usage_key'
+
+    @abstractmethod
+    def course_key(self):
+        """
+        Return the :class:`CourseKey` for the course containing this usage.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def map_into_course(self, course_key):
+        """
+        Return a new :class:`UsageKey` representing this usage inside the
+        course identified by the supplied :class:`CourseKey`.
+        """
+        raise NotImplementedError()
+
