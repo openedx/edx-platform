2a20b3f67e67d49ddb83f427bbf3912f4f804cd3
Dave St.Germain <dstgermain@edx.org>
2014-03-13 13:44:53 -0400

    This fixes LMS-2387 by removing a resize call that prevented scrolling. It also enables syntax highlighting for matlab input fields, which wasn't previously enabled.

--- common/lib/capa/capa/inputtypes.py
+++ common/lib/capa/capa/inputtypes.py
@@ -832,6 +832,8 @@ class MatlabInput(CodeInput):
             'queue_len': str(self.queue_len),
             'queue_msg': self.queue_msg,
             'button_enabled': self.button_enabled(),
+            'matlab_editor_js': '{static_url}js/vendor/CodeMirror/addons/octave.js'.format(
+                static_url=self.capa_system.STATIC_URL),
         }
         return extra_context
 
