0953583d38f1cb44ad3bc67b10de1870977c9cf3
cahrens <christina@edx.org>
2014-03-24 09:41:45 -0400

    Change code editor icon to say HTML.

--- common/static/js/vendor/tiny_mce/plugins/codemirror/plugin.js
+++ common/static/js/vendor/tiny_mce/plugins/codemirror/plugin.js
@@ -38,9 +38,11 @@ tinymce.PluginManager.add('codemirror', function(editor, url) {
 	};
 
 	// Add a button to the button bar
+    // EDX changed to show "HTML" on toolbar button
 	editor.addButton('code', {
 		title: 'Edit HTML',
-		icon: 'code',
+        text: 'HTML',
+		icon: false,
 		onclick: showSourceEditor
 	});
 
