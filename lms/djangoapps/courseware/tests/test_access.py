d37fb9cc54694d0471ae629ea5f9f22b0694a6c8
Don Mitchell <dmitchell@edx.org>
2014-04-02 15:50:16 -0400

    test_access squash me

--- lms/djangoapps/courseware/tests/test_access.py
+++ lms/djangoapps/courseware/tests/test_access.py
@@ -11,6 +11,7 @@ from student.tests.factories import AnonymousUserFactory
 from xmodule.modulestore import Location
 from courseware.tests.tests import TEST_DATA_MIXED_MODULESTORE
 import pytz
+from xmodule.modulestore.keys import CourseKey
 
 
 # pylint: disable=protected-access
@@ -21,65 +22,66 @@ class AccessTestCase(TestCase):
     """
 
     def setUp(self):
-        self.course = Location('i4x://edX/toy/course/2012_Fall')
+        course_key = CourseKey.from_string('edX/toy/2012_Fall')
+        self.course = course_key.make_usage_key('course', course_key.run)
         self.anonymous_user = AnonymousUserFactory()
         self.student = UserFactory()
         self.global_staff = UserFactory(is_staff=True)
-        self.course_staff = StaffFactory(course=self.course.course_id)
-        self.course_instructor = InstructorFactory(course=self.course.course_id)
+        self.course_staff = StaffFactory(course=self.course.course_key)
+        self.course_instructor = InstructorFactory(course=self.course.course_key)
 
     def test__has_access_to_location(self):
         self.assertFalse(access._has_access_to_location(
-            None, 'staff', self.course, self.course.course_id
+            None, 'staff', self.course, self.course.course_key
         ))
 
         self.assertFalse(access._has_access_to_location(
-            self.anonymous_user, 'staff', self.course, self.course.course_id
+            self.anonymous_user, 'staff', self.course, self.course.course_key
         ))
         self.assertFalse(access._has_access_to_location(
-            self.anonymous_user, 'instructor', self.course, self.course.course_id
+            self.anonymous_user, 'instructor', self.course, self.course.course_key
         ))
 
         self.assertTrue(access._has_access_to_location(
-            self.global_staff, 'staff', self.course, self.course.course_id
+            self.global_staff, 'staff', self.course, self.course.course_key
         ))
         self.assertTrue(access._has_access_to_location(
-            self.global_staff, 'instructor', self.course, self.course.course_id
+            self.global_staff, 'instructor', self.course, self.course.course_key
         ))
 
         # A user has staff access if they are in the staff group
         self.assertTrue(access._has_access_to_location(
-            self.course_staff, 'staff', self.course, self.course.course_id
+            self.course_staff, 'staff', self.course, self.course.course_key
         ))
         self.assertFalse(access._has_access_to_location(
-            self.course_staff, 'instructor', self.course, self.course.course_id
+            self.course_staff, 'instructor', self.course, self.course.course_key
         ))
 
         # A user has staff and instructor access if they are in the instructor group
         self.assertTrue(access._has_access_to_location(
-            self.course_instructor, 'staff', self.course, self.course.course_id
+            self.course_instructor, 'staff', self.course, self.course.course_key
         ))
         self.assertTrue(access._has_access_to_location(
-            self.course_instructor, 'instructor', self.course, self.course.course_id
+            self.course_instructor, 'instructor', self.course, self.course.course_key
         ))
 
         # A user does not have staff or instructor access if they are
         # not in either the staff or the the instructor group
         self.assertFalse(access._has_access_to_location(
-            self.student, 'staff', self.course, self.course.course_id
+            self.student, 'staff', self.course, self.course.course_key
         ))
         self.assertFalse(access._has_access_to_location(
-            self.student, 'instructor', self.course, self.course.course_id
+            self.student, 'instructor', self.course, self.course.course_key
         ))
 
     def test__has_access_string(self):
         u = Mock(is_staff=True)
-        self.assertFalse(access._has_access_string(u, 'staff', 'not_global', self.course.course_id))
+        self.assertFalse(access._has_access_string(u, 'staff', 'not_global', self.course.course_key))
 
         u._has_global_staff_access.return_value = True
-        self.assertTrue(access._has_access_string(u, 'staff', 'global', self.course.course_id))
+        self.assertTrue(access._has_access_string(u, 'staff', 'global', self.course.course_key))
 
-        self.assertRaises(ValueError, access._has_access_string, u, 'not_staff', 'global', self.course.course_id)
+        self.assertRaises(ValueError, access._has_access_string, u, 'not_staff', 'global', self.course.course_key)
 
     def test__has_access_descriptor(self):
         # TODO: override DISABLE_START_DATES and test the start date branch of the method
@@ -106,9 +108,12 @@ class AccessTestCase(TestCase):
         u = Mock(email='test@edx.org', is_staff=False)
         u.is_authenticated.return_value = True
 
-        c = Mock(enrollment_start=tomorrow, enrollment_end=tomorrow, id='edX/test/2012_Fall', enrollment_domain='')
+        c = Mock(
+            enrollment_start=tomorrow, enrollment_end=tomorrow,
+            id=CourseKey.from_string('edX/test/2012_Fall'), enrollment_domain=''
+        )
 
-        allowed = CourseEnrollmentAllowedFactory(email=u.email, course_id=c.id)
+        CourseEnrollmentAllowedFactory(email=u.email, course_id=c.id)
 
         self.assertTrue(access._has_access_course_desc(u, 'enroll', c))
 
@@ -116,7 +121,10 @@ class AccessTestCase(TestCase):
         u = Mock(email='test@edx.org', is_staff=True)
         u.is_authenticated.return_value = True
 
-        c = Mock(enrollment_start=tomorrow, enrollment_end=tomorrow, id='edX/test/Whenever', enrollment_domain='')
+        c = Mock(
+            enrollment_start=tomorrow, enrollment_end=tomorrow,
+            id=CourseKey.from_string('edX/test/Whenever'), enrollment_domain='',
+        )
         self.assertTrue(access._has_access_course_desc(u, 'enroll', c))
 
         # TODO:
@@ -132,42 +140,42 @@ class UserRoleTestCase(TestCase):
     Tests for user roles.
     """
     def setUp(self):
-        self.course = Location('i4x://edX/toy/course/2012_Fall')
+        self.course_key = CourseKey.from_string('edX/toy/2012_Fall')
         self.anonymous_user = AnonymousUserFactory()
         self.student = UserFactory()
         self.global_staff = UserFactory(is_staff=True)
-        self.course_staff = StaffFactory(course=self.course.course_id)
-        self.course_instructor = InstructorFactory(course=self.course.course_id)
+        self.course_staff = StaffFactory(course=self.course_key)
+        self.course_instructor = InstructorFactory(course=self.course_key)
 
     def test_user_role_staff(self):
         """Ensure that user role is student for staff masqueraded as student."""
         self.assertEqual(
             'staff',
-            access.get_user_role(self.course_staff, self.course.course_id)
+            access.get_user_role(self.course_staff, self.course_key)
         )
         # Masquerade staff
         self.course_staff.masquerade_as_student = True
         self.assertEqual(
             'student',
-            access.get_user_role(self.course_staff, self.course.course_id)
+            access.get_user_role(self.course_staff, self.course_key)
         )
 
     def test_user_role_instructor(self):
         """Ensure that user role is student for instructor masqueraded as student."""
         self.assertEqual(
             'instructor',
-            access.get_user_role(self.course_instructor, self.course.course_id)
+            access.get_user_role(self.course_instructor, self.course_key)
         )
         # Masquerade instructor
         self.course_instructor.masquerade_as_student = True
         self.assertEqual(
             'student',
-            access.get_user_role(self.course_instructor, self.course.course_id)
+            access.get_user_role(self.course_instructor, self.course_key)
         )
 
     def test_user_role_anonymous(self):
         """Ensure that user role is student for anonymous user."""
         self.assertEqual(
             'student',
-            access.get_user_role(self.anonymous_user, self.course.course_id)
+            access.get_user_role(self.anonymous_user, self.course_key)
         )
