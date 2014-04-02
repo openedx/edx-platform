f0df029682447b5ec52757be53c64e88c8cd24b4
Valera Rozuvan <valera@edx.org>
2014-04-02 14:33:56 +0000

    Refactoring i18n.js file.

--- common/static/js/test/i18n.js
+++ common/static/js/test/i18n.js
@@ -1,10 +1,27 @@
-window.gettext = function(s){return s;};
-window.ngettext = function(singular, plural, num){ return num == 1 ? singular : plural }
+/*jslint
+    indent: 4, white: false
+*/
 
-function interpolate(fmt, obj, named) {
-    if (named) {
-        return fmt.replace(/%\(\w+\)s/g, function(match){return String(obj[match.slice(2,-2)])});
-    } else {
-        return fmt.replace(/%s/g, function(match){return String(obj.shift())});
-    }
-}
+(function () {
+    'use strict';
+
+    this.gettext = function (s) {
+        return s;
+    };
+
+    this.ngettext = function (singular, plural, num) {
+        return num === 1 ? singular : plural;
+    };
+
+    this.interpolate = function (fmt, obj, named) {
+        if (named) {
+            return fmt.replace(/%\(\w+\)s/g, function (match) {
+                return String(obj[match.slice(2, -2)]);
+            });
+        }
+
+        return fmt.replace(/%s/g, function () {
+            return String(obj.shift());
+        });
+    };
+}).call(this);
