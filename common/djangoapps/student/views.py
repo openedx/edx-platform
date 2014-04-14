048d877e356d3afb002aeed82196501a66901ae3
Pavel Yushchenko <pavelyushchenko@gmail.com>
2014-04-14 15:01:57 +0400

    hotfix

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -1472,7 +1472,7 @@ def reactivation_email_for_user(user):
             "error": _('No inactive user with this e-mail exists'),
         })  # TODO: this should be status code 400  # pylint: disable=fixme
 
-    d = {'name': user.profile.name,
+    context = {'name': user.profile.name,
          'login': user.email,
          'password': u'******',
          'key': reg.activation_key}
