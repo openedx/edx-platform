8ba16446e58d0dcb46d2f761746c2bc20594ee53
Andy Armstrong <andya@edx.org>
2014-04-15 19:06:08 -0400

    fixup! Make Studio support opaque-keys

--- cms/djangoapps/contentstore/views/tests/test_container.py
+++ cms/djangoapps/contentstore/views/tests/test_container.py
@@ -6,6 +6,7 @@ from contentstore.tests.utils import CourseTestCase
 from contentstore.utils import compute_publish_state, PublishState
 from contentstore.views.helpers import xblock_studio_url
 from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.keys import CourseKey
 from xmodule.modulestore.tests.factories import ItemFactory
 
 
@@ -28,19 +29,19 @@ class ContainerViewTestCase(CourseTestCase):
                                         category="video", display_name="My Video")
 
     def test_container_html(self):
-        branch_name = "MITx.999.Robot_Super_Course/branch/draft/block"
         self._test_html_content(
             self.child_vertical,
-            branch_name=branch_name,
             expected_section_tag=(
                 '<section class="wrapper-xblock level-page is-hidden" '
-                'data-locator="{branch_name}/Child_Vertical">'.format(branch_name=branch_name)
+                'data-locator="{child_location}">'.format(
+                    child_location=unicode(self.child_vertical.location),
+                )
             ),
             expected_breadcrumbs=(
-                r'<a href="/unit/{branch_name}/Unit"\s*'
+                r'<a href="/unit/{unit_location}"\s*'
                 r'class="navigation-link navigation-parent">Unit</a>\s*'
                 r'<a href="#" class="navigation-link navigation-current">Child Vertical</a>'
-            ).format(branch_name=branch_name)
+            ).format(unit_location=(unicode(self.vertical.location).replace("+", "\\+")))
         )
 
     def test_container_on_container_html(self):
@@ -57,39 +58,46 @@ class ContainerViewTestCase(CourseTestCase):
             category="html", display_name="Child HTML"
         )
         draft_xblock_with_child = modulestore('draft').convert_to_draft(published_xblock_with_child.location)
-        branch_name = "MITx.999.Robot_Super_Course/branch/draft/block"
         self._test_html_content(
             published_xblock_with_child,
-            branch_name=branch_name,
             expected_section_tag=(
                 '<section class="wrapper-xblock level-page is-hidden" '
-                'data-locator="{branch_name}/Wrapper">'.format(branch_name=branch_name)
+                'data-locator="{child_location}">'.format(
+                    child_location=unicode(published_xblock_with_child.location),
+                )
             ),
             expected_breadcrumbs=(
-                r'<a href="/unit/{branch_name}/Unit"\s*'
+                r'<a href="/unit/{unit_location}"\s*'
                 r'class="navigation-link navigation-parent">Unit</a>\s*'
-                r'<a href="/container/{branch_name}/Child_Vertical"\s*'
+                r'<a href="/container/{wrapper_location}"\s*'
                 r'class="navigation-link navigation-parent">Child Vertical</a>\s*'
                 r'<a href="#" class="navigation-link navigation-current">Wrapper</a>'
-            ).format(branch_name=branch_name)
+            ).format(
+                unit_location=unicode(self.vertical.location).replace("+", "\\+"),
+                wrapper_location=unicode(published_xblock_with_child.location).replace("+", "\\+"),
+            )
         )
         self._test_html_content(
             draft_xblock_with_child,
-            branch_name=branch_name,
             expected_section_tag=(
                 '<section class="wrapper-xblock level-page is-hidden" '
-                'data-locator="{branch_name}/Wrapper">'.format(branch_name=branch_name)
+                'data-locator="location:{child_location}">'.format(
+                    child_location=unicode(draft_xblock_with_child.location),
+                )
             ),
             expected_breadcrumbs=(
-                r'<a href="/unit/{branch_name}/Unit"\s*'
+                r'<a href="/unit/{unit_location}"\s*'
                 r'class="navigation-link navigation-parent">Unit</a>\s*'
-                r'<a href="/container/{branch_name}/Child_Vertical"\s*'
+                r'<a href="/container/{wrapper_location}"\s*'
                 r'class="navigation-link navigation-parent">Child Vertical</a>\s*'
-                r'<a href="#" class="navigation-link navigation-current">Wrapper</a>'
-            ).format(branch_name=branch_name)
+                r'<a href="#" class="location:navigation-link navigation-current">Wrapper</a>'
+            ).format(
+                unit_location=unicode(self.vertical.location).replace("+", "\\+"),
+                wrapper_location=unicode(draft_xblock_with_child.location).replace("+", "\\+"),
+            )
         )
 
-    def _test_html_content(self, xblock, branch_name, expected_section_tag, expected_breadcrumbs):
+    def _test_html_content(self, xblock, expected_section_tag, expected_breadcrumbs):
         """
         Get the HTML for a container page and verify the section tag is correct
         and the breadcrumbs trail is correct.
@@ -103,12 +111,11 @@ class ContainerViewTestCase(CourseTestCase):
         # Verify the navigation link at the top of the page is correct.
         self.assertRegexpMatches(html, expected_breadcrumbs)
         # Verify the link that allows users to change publish status.
-        expected_message = None
         if publish_state == PublishState.public:
-            expected_message = 'you need to edit unit <a href="/unit/{branch_name}/Unit">Unit</a> as a draft.'
+            expected_message = 'you need to edit unit <a href="/unit/{unit_location}">Unit</a> as a draft.'
         else:
-            expected_message = 'your changes will be published with unit <a href="/unit/{branch_name}/Unit">Unit</a>.'
+            expected_message = 'your changes will be published with unit <a href="/unit/{unit_location}">Unit</a>.'
         expected_unit_link = expected_message.format(
-            branch_name=branch_name
+            unit_location=unicode(self.vertical.location)
         )
         self.assertIn(expected_unit_link, html)
