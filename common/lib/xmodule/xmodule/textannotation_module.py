949c3035f9bf44d6f8c4414f0b9a6e470f4f837b
lduarte1991 <lduarte1991@gmail.com>
2014-04-25 18:53:29 -0400

    Removed diacritic mention and explained token

--- common/lib/xmodule/xmodule/textannotation_module.py
+++ common/lib/xmodule/xmodule/textannotation_module.py
@@ -45,12 +45,6 @@ class AnnotatableFields(object):
     )
     annotation_storage_url = String(help="Location of Annotation backend", scope=Scope.settings, default="http://your_annotation_storage.com", display_name="Url for Annotation Storage")
     annotation_token_secret = String(help="Secret string for annotation storage", scope=Scope.settings, default="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", display_name="Secret Token String for Annotation")
-    diacritics = String(
-         display_name="Diacritic Marks",
-         help= "Add diacritic marks to be added to a text using the comma-separated form, i.e. markname;urltomark;baseline,markname2;urltomark2;baseline2",
-         scope=Scope.settings,
-         default='',
-    )
 
 
 class TextAnnotationModule(AnnotatableFields, XModule):
