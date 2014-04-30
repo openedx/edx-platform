586700aa12cc1d18876846c57e3d08e543ba4feb
Calen Pennington <cale@edx.org>
2014-04-30 10:17:35 -0400

    Make course ids and usage ids opaque to LMS and Studio [partial commit]
    This commit adds custom django fields for CourseKeys and UsageKeys.
    
    These keys are now objects with a limited interface, and the particular
    internal representation is managed by the data storage layer (the
    modulestore).
    
    For the LMS, there should be no outward-facing changes to the system.
    The keys are, for now, a change to internal representation only. For
    Studio, the new serialized form of the keys is used in urls, to allow
    for further migration in the future.
    
    Co-Author: Andy Armstrong <andya@edx.org>
    Co-Author: Christina Roberts <christina@edx.org>
    Co-Author: David Baumgold <db@edx.org>
    Co-Author: Diana Huang <dkh@edx.org>
    Co-Author: Don Mitchell <dmitchell@edx.org>
    Co-Author: Julia Hansbrough <julia@edx.org>
    Co-Author: Nimisha Asthagiri <nasthagiri@edx.org>
    Co-Author: Sarina Canelake <sarina@edx.org>
    
    [LMS-2370]

---
+++ common/djangoapps/xmodule_django/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from xmodule.modulestore.locations import SlashSeparatedCourseKey, Location
+from types import NoneType
+
+
+class CourseKeyField(models.CharField):
+    description = "A SlashSeparatedCourseKey object, saved to the DB in the form of a string"
+
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        super(CourseKeyField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        assert isinstance(value, (NoneType, basestring, SlashSeparatedCourseKey))
+        if not value:
+            # handle empty string for models being created w/o fields populated
+            return None
+        if isinstance(value, basestring):
+            return SlashSeparatedCourseKey.from_deprecated_string(value)
+        else:
+            return value
+
+    def get_prep_value(self, value):
+        assert isinstance(value, (NoneType, SlashSeparatedCourseKey))
+        return value.to_deprecated_string() if value else ''
+
+    def validate(self, value, model_instance):
+        # The default django CharField validator tries to call len() on SlashSeparatedCourseKey,
+        # so we write custom validation that allows us to use SlashSeparatedCourseKeys
+        assert isinstance(value, (NoneType, basestring, SlashSeparatedCourseKey))
+
+    def run_validators(self, value):
+        # The default django CharField validator tries to call len() on SlashSeparatedCourseKey,
+        # so we write custom validation that allows us to use SlashSeparatedCourseKeys
+        if isinstance(value, SlashSeparatedCourseKey):
+            assert len(value.to_deprecated_string()) <= self.max_length
+
+
+class LocationKeyField(models.CharField):
+    description = "A Location object, saved to the DB in the form of a string"
+
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        super(LocationKeyField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        assert isinstance(value, (NoneType, basestring, Location))
+        if not value:
+            return None
+        if isinstance(value, basestring):
+            return Location.from_deprecated_string(value)
+        else:
+            return value
+
+    def get_prep_value(self, value):
+        assert isinstance(value, Location)
+        return value.to_deprecated_string() if value else ''
+
+    def validate(self, value, model_instance):
+        # The default django CharField validator tries to call len() on Locations,
+        # so we write custom validation that allows us to use Locations
+        assert isinstance(value, (NoneType, basestring, Location))
+
+    def run_validators(self, value):
+        # The default django CharField validator tries to call len() on Locations,
+        # so we write custom validation that allows us to use Locations
+        if isinstance(value, Location):
+            assert len(value.to_deprecated_string()) <= self.max_length
