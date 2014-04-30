974e66b74f5f7ac6f325f0fc49e572193b8d6739
Calen Pennington <cale@edx.org>
2014-04-30 10:17:48 -0400

    Make course ids and usage ids opaque to LMS and Studio [partial commit]
    This commit updates miscellaneous files in common.
    
    These keys are now objects with a limited interface, and the particular
    internal representation is managed by the data storage layer (the
    modulestore).
    
    For the LMS, there should be no outward-facing changes to the system.
    The keys are, for now, a change to internal representation only. For
    Studio, the new serialized form of the keys is used in urls, to allow
    for further migration in the future.
    
    Co-Author: Andy Armstrong <andya@edx.org>
    Co-Author: Christina Roberts <christina@edx.org>
    Co-Author: David Baumgold <db@edx.org>
    Co-Author: Diana Huang <dkh@edx.org>
    Co-Author: Don Mitchell <dmitchell@edx.org>
    Co-Author: Julia Hansbrough <julia@edx.org>
    Co-Author: Nimisha Asthagiri <nasthagiri@edx.org>
    Co-Author: Sarina Canelake <sarina@edx.org>
    
    [LMS-2370]

--- common/test/acceptance/pages/studio/course_page.py
+++ common/test/acceptance/pages/studio/course_page.py
@@ -34,8 +34,5 @@ class CoursePage(PageObject):
         """
         Construct a URL to the page within the course.
         """
-        return "/".join([
-            BASE_URL, self.url_path,
-            "{course_org}.{course_num}.{course_run}".format(**self.course_info),
-            "branch", "draft", "block", self.course_info['course_run']
-        ])
+        course_key = "slashes:{course_org}+{course_num}+{course_run}".format(**self.course_info)
+        return "/".join([BASE_URL, self.url_path, course_key])
