acd9d7d904e3d918bfa5f4107eae28e2e429787d
cahrens <christina@edx.org>
2014-05-05 11:36:04 -0400

    These files are not being used.

--- cms/static/js/models/course_relative.js
+++
@@ -1,9 +0,0 @@
-define(["backbone"], function(Backbone) {
-    var CourseRelative = Backbone.Model.extend({
-        defaults: {
-            course_location : null, // must never be null, but here to doc the field
-            idx : null  // the index making it unique in the containing collection (no implied sort)
-        }
-    });
-    return CourseRelative;
-});
