cce63f6ecd6af36dbb3355916708d6a8bb018965
Jason Bau <jbau@stanford.edu>
2014-05-07 14:51:08 -0700

    Revert "Fix randomization bug for multiple questions per problem"
    This reverts commit 0c583d9edbc2d1f75ec2fead2b3346b49ae13113.

--- common/lib/capa/capa/capa_problem.py
+++ common/lib/capa/capa/capa_problem.py
@@ -179,12 +179,9 @@ class LoncapaProblem(object):
         self.inputs = {}
 
         # Run response late_transforms last (see MultipleChoiceResponse)
-        # Sort the responses to be in *_1 *_2 ... order.
-        responses = self.responders.values()
-        responses = sorted(responses, key = lambda resp: int(resp.id[resp.id.rindex('_')+1:]) )
-        for response in responses:
+        for response in self.responders.values():
             if hasattr(response, 'late_transforms'):
-                response.late_transforms(self)
+                response.late_transforms()
 
         self.extracted_tree = self._extract_html(self.tree)
 
