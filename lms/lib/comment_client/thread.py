299046a86f94658efa63189698b1e4b1875f4793
Greg Price <gprice@edx.org>
2014-05-19 17:37:26 -0400

    Add logging of search quality information
    For now, this only includes course id, search terms, and result count.
    Information is logged both to the standard logger for near-term analysis
    and the event tracker for the longer term. Result count logging requires
    commit 02466b1 of cs_comments_service (otherwise it will be null).

--- lms/lib/comment_client/thread.py
+++ lms/lib/comment_client/thread.py
@@ -1,8 +1,12 @@
+import logging
+
+from eventtracking import tracker
 from .utils import merge_dict, strip_blank, strip_none, extract, perform_request
 from .utils import CommentClientRequestError
 import models
 import settings
 
+log = logging.getLogger(__name__)
 
 class Thread(models.Model):
 
@@ -54,6 +58,26 @@ class Thread(models.Model):
             metric_action='thread.search',
             paged_results=True
         )
+        if query_params.get('text'):
+            search_query = query_params['text']
+            course_id = query_params['course_id']
+            total_results = response.get('total_results')
+            # Record search result metric to allow search quality analysis.
+            # course_id is already included in the context for the event tracker
+            tracker.emit(
+                'edx.forum.searched',
+                {
+                    'query': search_query,
+                    'total_results': total_results,
+                }
+            )
+            log.info(
+                'forum_text_search query="{search_query}" course_id={course_id} total_results={total_results}'.format(
+                    search_query=search_query,
+                    course_id=course_id,
+                    total_results=total_results
+                )
+            )
         return response.get('collection', []), response.get('page', 1), response.get('num_pages', 1)
 
     @classmethod
