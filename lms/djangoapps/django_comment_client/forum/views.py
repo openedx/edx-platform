2d23059617d20a85a66ea5bdab8d75680c5e8888
Alan Boudreault <boudreault.alan@gmail.com>
2014-05-27 21:40:25 -0400

    Apply jsa changes to get the lms using the discussion_app resources

--- lms/djangoapps/django_comment_client/forum/views.py
+++ lms/djangoapps/django_comment_client/forum/views.py
@@ -28,6 +28,8 @@ INLINE_THREADS_PER_PAGE = 20
 PAGES_NEARBY_DELTA = 2
 log = logging.getLogger("edx.discussions")
 
+from edxmako import add_lookup, lookup_template
+add_lookup('main', 'templates', package='discussion_app')
 
 def _attr_safe_json(obj):
     """
@@ -304,7 +306,11 @@ def single_thread(request, course_id, discussion_id, thread_id):
             'user_cohort': user_cohort,
             'sort_preference': cc_user.default_sort_key,
             'category_map': course_settings["category_map"],
-            'course_settings': _attr_safe_json(course_settings)
+            'course_settings': _attr_safe_json(course_settings),
+            'has_permission_to_create_thread': cached_has_permission(request.user, "create_thread", course_id),
+            'has_permission_to_create_comment': cached_has_permission(request.user, "create_comment", course_id),
+            'has_permission_to_create_subcomment': cached_has_permission(request.user, "create_subcomment", course_id),
+            'has_permission_to_openclose_thread': cached_has_permission(request.user, "openclose_thread", course_id)
         }
         return render_to_response('discussion/index.html', context)
 
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -37,6 +37,8 @@ from xmodule.modulestore.modulestore_settings import update_module_store_setting
 
 from lms.lib.xblock.mixin import LmsBlockMixin
 
+from discussion_app.views import get_js_urls as discussion_get_js_urls
+
 ################################### FEATURES ###################################
 # The display name of the platform to be used in templates/emails/etc.
 PLATFORM_NAME = "Your Platform Name Here"
@@ -915,7 +917,7 @@ main_vendor_js = [
     'js/vendor/URI.min.js',
 ]
 
-discussion_js = sorted(rooted_glob(COMMON_ROOT / 'static', 'coffee/src/discussion/**/*.js'))
+discussion_js = discussion_get_js_urls()
 staff_grading_js = sorted(rooted_glob(PROJECT_ROOT / 'static', 'coffee/src/staff_grading/**/*.js'))
 open_ended_js = sorted(rooted_glob(PROJECT_ROOT / 'static', 'coffee/src/open_ended/**/*.js'))
 notes_js = sorted(rooted_glob(PROJECT_ROOT / 'static', 'coffee/src/notes/**/*.js'))
@@ -1287,6 +1289,7 @@ INSTALLED_APPS = (
     'django_comment_client',
     'django_comment_common',
     'notes',
+    'discussion_app',
 
     # Splash screen
     'splash',
