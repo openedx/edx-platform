55f0d0f9d1794730d9f9d7df3453c9f01b748c7b
Muhammad Ammar <mammar@gmail.com>
2014-06-02 09:40:55 +0000

    Check YouTube for Bok-Choy

--- common/test/acceptance/tests/video/test_video_module.py
+++ common/test/acceptance/tests/video/test_video_module.py
@@ -5,15 +5,16 @@ Acceptance tests for Video.
 """
 
 import json
+from unittest import skipIf
 import requests
-from ..helpers import UniqueCourseTest
+from box.test.flaky import flaky
+from ..helpers import UniqueCourseTest, is_youtube_available
 from ...pages.lms.video.video import VideoPage
 from ...pages.lms.tab_nav import TabNavPage
 from ...pages.lms.course_nav import CourseNavPage
 from ...pages.lms.auto_auth import AutoAuthPage
 from ...pages.lms.course_info import CourseInfoPage
 from ...fixtures.course import CourseFixture, XBlockFixtureDesc
-from box.test.flaky import flaky
 
 
 VIDEO_SOURCE_PORT = 8777
@@ -39,6 +40,7 @@ class YouTubeConfigError(Exception):
 
 
 @flaky
+@skipIf(is_youtube_available() is False, 'YouTube is not available!')
 class VideoBaseTest(UniqueCourseTest):
     """
     Base class for tests of the Video Player
