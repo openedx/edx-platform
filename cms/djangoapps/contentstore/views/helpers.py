895e4f6362c27aa00d2269468912511a246c1f0d
Alan Boudreault <boudreault.alan@gmail.com>
2014-06-02 12:08:14 -0400

    XBlock Discussion changes
    Conflicts:
    	cms/djangoapps/contentstore/views/helpers.py
    	common/static/coffee/src/discussion/content.coffee
    	common/static/coffee/src/discussion/discussion_filter.coffee
    	common/static/coffee/src/discussion/discussion_module_view.coffee
    	common/static/coffee/src/discussion/discussion_router.coffee
    	common/static/coffee/src/discussion/views/discussion_content_view.coffee
    	common/static/coffee/src/discussion/views/discussion_thread_profile_view.coffee
    	common/static/coffee/src/discussion/views/discussion_thread_show_view.coffee
    	common/static/coffee/src/discussion/views/discussion_thread_view.coffee
    	common/static/coffee/src/discussion/views/new_post_view.coffee
    	common/static/coffee/src/discussion/views/response_comment_show_view.coffee
    	common/static/coffee/src/discussion/views/thread_response_show_view.coffee
    	common/static/coffee/src/discussion/views/thread_response_view.coffee
    	lms/djangoapps/django_comment_client/forum/views.py
    	lms/static/sass/discussion/_discussion.scss
    	lms/templates/discussion/_discussion_module.html
    	lms/templates/discussion/_filter_dropdown.html
    	lms/templates/discussion/_js_head_dependencies.html
    	lms/templates/discussion/_thread_list_template.html
    	lms/templates/discussion/_underscore_templates.html
    	lms/templates/discussion/index.html
    	lms/templates/discussion/mustache/_inline_discussion.mustache
    	lms/templates/discussion/mustache/_pagination.mustache
    	lms/templates/discussion/mustache/_profile_thread.mustache

--- cms/djangoapps/contentstore/views/helpers.py
+++ cms/djangoapps/contentstore/views/helpers.py
@@ -19,6 +19,10 @@ from contentstore.utils import reverse_course_url, reverse_library_url, reverse_
 __all__ = ['edge', 'event', 'landing']
 
 
+# Add Discussion templates
+add_lookup('lms.main', 'templates', package='discussion_app')
+
+
 # points to the temporary course landing page with log in and sign up
 def landing(request, org, course, coursename):
     return render_to_response('temp-course-landing.html', {})
