ae1d872742a3e4dee03dad14f9a0f6a93c33041b
lduarte1991 <lduarte1991@gmail.com>
2014-06-09 14:22:29 -0400

    Annotation Tools: Removing opaque_keys reference

--- lms/djangoapps/notes/views.py
+++ lms/djangoapps/notes/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from edxmako.shortcuts import render_to_response
-from opaque_keys.edx.locations import SlashSeparatedCourseKey
 from courseware.courses import get_course_with_access
 from notes.models import Note
 from notes.utils import notes_enabled_for_course
