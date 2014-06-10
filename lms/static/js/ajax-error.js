11c2bb19af0b20c83ffa97f0ce8ec7aa7b6132f0
polesye <s2pak.anton@gmail.com>
2014-06-10 15:09:08 +0300

    Add url redirect to.

--- lms/static/js/ajax-error.js
+++ lms/static/js/ajax-error.js
@@ -1,6 +1,6 @@
 $(document).ajaxError(function (event, jXHR) {
     if (jXHR.status === 403) {
         alert(gettext('You\'re logged out. Redirecting on login page.'));
-        window.location = '/accounts/login';
+        window.location.href = '/accounts/login?next=' + window.location.href;
     }
 });
