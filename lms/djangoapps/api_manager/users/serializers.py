94fe4584ff6eee8021cc41713f6e59f6d97a9630
Zia Fazal <zia.fazal@arbisoft.com>
2014-06-18 14:32:13 +0500

    added organisation to users api
    added id, name and created fields
    
    Added display_name to basic serializer

--- lms/djangoapps/api_manager/users/serializers.py
+++ lms/djangoapps/api_manager/users/serializers.py
@@ -2,12 +2,15 @@
 
 from api_manager.models import APIUser
 from rest_framework import serializers
+from api_manager.organizations.serializers import BasicOrganizationSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
     """ Serializer for User model interactions """
+    organizations = BasicOrganizationSerializer(many=True, required=False)
+
     class Meta:
         """ Serializer/field specification """
         model = APIUser
-        fields = ("id", "email", "username", "first_name", "last_name")
+        fields = ("id", "email", "username", "first_name", "last_name", "organizations")
         read_only_fields = ("id", "email", "username")
