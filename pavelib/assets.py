245ed42ddbff8f8e16402f2938a4d365155485b0
Christine Lytwynec <clytwynec@edx.org>
2014-06-18 16:30:13 -0400

    stop update_assets commands from being called extra times

--- pavelib/assets.py
+++ pavelib/assets.py
@@ -190,7 +190,10 @@ def watch_assets(options):
 
 
 @task
-@needs('pavelib.prereqs.install_asset_prereqs')
+@needs(
+    'pavelib.prereqs.install_ruby_prereqs',
+    'pavelib.prereqs.install_node_prereqs',
+)
 @consume_args
 def update_assets(args):
     """
