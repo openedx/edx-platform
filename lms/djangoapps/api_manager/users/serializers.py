df7e1ab98f16980c5ce38f65d9e16a2a8caab652
Zia Fazal <zia.fazal@arbisoft.com>
2014-06-25 16:31:40 +0500

    added created field to users api

--- lms/djangoapps/api_manager/users/serializers.py
+++ lms/djangoapps/api_manager/users/serializers.py
@@ -8,11 +8,12 @@ from api_manager.organizations.serializers import BasicOrganizationSerializer
 class UserSerializer(serializers.ModelSerializer):
     """ Serializer for User model interactions """
     organizations = BasicOrganizationSerializer(many=True, required=False)
+    created = serializers.DateTimeField(source='date_joined', required=False)
 
     class Meta:
         """ Serializer/field specification """
         model = APIUser
-        fields = ("id", "email", "username", "first_name", "last_name", "organizations")
+        fields = ("id", "email", "username", "first_name", "last_name", "created", "organizations")
         read_only_fields = ("id", "email", "username")
 
 
