70e5044f479e0bfe6182c3cc2edc613cc77a7728
zubair-arbi <zubair.afzal@arbisoft.com>
2014-06-30 18:03:24 +0500

    update export_all_courses management command to count and list all failed export courses

--- cms/djangoapps/contentstore/management/commands/export_all_courses.py
+++ cms/djangoapps/contentstore/management/commands/export_all_courses.py
@@ -1,5 +1,5 @@
 """
-Script for exporting all courseware from Mongo to a directory
+Script for exporting all courseware from Mongo to a directory and listing the courses which failed to export
 """
 from django.core.management.base import BaseCommand, CommandError
 from xmodule.modulestore.xml_exporter import export_to_xml
@@ -8,35 +8,52 @@ from xmodule.contentstore.django import contentstore
 
 
 class Command(BaseCommand):
-    """Export all courses from mongo to the specified data directory"""
-    help = 'Export all courses from mongo to the specified data directory'
+    """
+    Export all courses from mongo to the specified data directory and list the courses which failed to export
+    """
+    help = 'Export all courses from mongo to the specified data directory and list the courses which failed to export'
 
     def handle(self, *args, **options):
-        "Execute the command"
+        """
+        Execute the command
+        """
         if len(args) != 1:
             raise CommandError("export requires one argument: <output path>")
 
         output_path = args[0]
+        courses, failed_export_courses = export_courses_to_output_path(output_path)
 
-        cs = contentstore()
-        ms = modulestore()
-        root_dir = output_path
-        courses = ms.get_courses()
+        print("=" * 80)
+        print(u"=" * 30 + u"> Export summary")
+        print(u"Total number of courses to export: {0}".format(len(courses)))
+        print(u"Total number of courses which failed to export: {0}".format(len(failed_export_courses)))
+        print(u"List of export failed courses ids:")
+        print(u"\n".join(failed_export_courses))
+        print("=" * 80)
 
-        print("%d courses to export:" % len(courses))
-        cids = [x.id for x in courses]
-        print(cids)
 
-        for course_id in cids:
+def export_courses_to_output_path(output_path):
+    """
+    Export all courses to target directory and return the list of courses which failed to export
+    """
+    content_store = contentstore()
+    module_store = modulestore()
+    root_dir = output_path
+    courses = module_store.get_courses()
 
-            print("-"*77)
-            print("Exporting course id = {0} to {1}".format(course_id, output_path))
+    course_ids = [x.id for x in courses]
+    failed_export_courses = []
 
-            if 1:
-                try:
-                    course_dir = course_id.replace('/', '...')
-                    export_to_xml(ms, cs, course_id, root_dir, course_dir)
-                except Exception as err:
-                    print("="*30 + "> Oops, failed to export %s" % course_id)
-                    print("Error:")
-                    print(err)
+    for course_id in course_ids:
+        print(u"-" * 80)
+        print(u"Exporting course id = {0} to {1}".format(course_id, output_path))
+        try:
+            course_dir = course_id.to_deprecated_string().replace('/', '...')
+            export_to_xml(module_store, content_store, course_id, root_dir, course_dir)
+        except Exception as err:  # pylint: disable=broad-except
+            failed_export_courses.append(unicode(course_id))
+            print(u"=" * 30 + u"> Oops, failed to export {0}".format(course_id))
+            print(u"Error:")
+            print(err)
+
+    return courses, failed_export_courses
