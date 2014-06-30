a3d52557a54a738ef8ba02c44394c261b9992265
marcore <marco.re@polimi.it>
2014-06-30 19:16:31 +0200

    Fix filter with new opaque key, allowing user to set COURSE_LISTING as old way path

--- lms/djangoapps/branding/__init__.py
+++ lms/djangoapps/branding/__init__.py
@@ -2,8 +2,8 @@ from xmodule.modulestore.django import modulestore
 from xmodule.course_module import CourseDescriptor
 from django.conf import settings
 
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
 from microsite_configuration import microsite
-
 
 def get_visible_courses():
     """
@@ -22,7 +22,7 @@ def get_visible_courses():
 
     # this is legacy format which is outside of the microsite feature -- also handle dev case, which should not filter
     if hasattr(settings, 'COURSE_LISTINGS') and subdomain in settings.COURSE_LISTINGS and not settings.DEBUG:
-        filtered_visible_ids = frozenset(settings.COURSE_LISTINGS[subdomain])
+        filtered_visible_ids = frozenset([SlashSeparatedCourseKey.from_deprecated_string(c) for c in settings.COURSE_LISTINGS[subdomain]])
 
     filtered_by_org = microsite.get_value('course_org_filter')
 
