e9d8620d63102c0129a3b74677c802248f98227e
Nimisha Asthagiri <nasthagiri@edx.org>
2014-07-02 23:47:12 -0400

    removed unneeded import.

--- common/djangoapps/contentserver/tests/test.py
+++ common/djangoapps/contentserver/tests/test.py
@@ -15,7 +15,7 @@ from student.models import CourseEnrollment
 
 from xmodule.contentstore.django import contentstore, _CONTENTSTORE
 from xmodule.modulestore.django import modulestore
-from opaque_keys.edx.locations import SlashSeparatedCourseKey, AssetLocation
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.xml_importer import import_from_xml
 
