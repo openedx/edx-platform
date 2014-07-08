ff2822cf48f280654438070546322af6853e43e3
Sarina Canelake <sarina@edx.org>
2014-07-08 12:06:05 -0400

    Fix cms tests

--- common/lib/xmodule/xmodule/modulestore/tests/persistent_factories.py
+++ common/lib/xmodule/xmodule/modulestore/tests/persistent_factories.py
@@ -33,14 +33,14 @@ class PersistentCourseFactory(SplitFactory):
 
     # pylint: disable=W0613
     @classmethod
-    def _create(cls, target_class, offering='999', org='testX', user_id=ModuleStoreEnum.UserID.test,
+    def _create(cls, target_class, course='999', run='run', org='testX', user_id=ModuleStoreEnum.UserID.test,
                 master_branch=ModuleStoreEnum.BranchName.draft, **kwargs):
 
         modulestore = kwargs.pop('modulestore')
         root_block_id = kwargs.pop('root_block_id', 'course')
         # Write the data to the mongo datastore
         new_course = modulestore.create_course(
-            org, offering, user_id, fields=kwargs,
+            org, course, run, user_id, fields=kwargs,
             master_branch=master_branch, root_block_id=root_block_id
         )
 
