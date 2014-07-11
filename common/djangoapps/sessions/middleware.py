2b8c30737e280755d6b71cf850567aa4ec494805
Usman Khalid <2200617@gmail.com>
2014-07-11 19:13:18 +0500

    Added logging for initial state and save events of sessions.
    LMS-6630.

--- common/djangoapps/sessions/middleware.py
+++ common/djangoapps/sessions/middleware.py
@@ -2,26 +2,35 @@
 Session Middlewares.
 """
 
+import json
 import logging
 import types
 
 from django.contrib.sessions.middleware import SessionMiddleware as DjangoSessionMiddleware
 from django.conf import settings
 
-AUDIT_LOG = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 def patch_session_object(session):
     """Patch session object to do some additional logging."""
 
     original_create = session.create
+    original_save = session.save
     original_delete = session.delete
 
     def create(self):
         """Logs and then calls original create method."""
         original_create()
-        AUDIT_LOG.info("SessionEngine:Created SessionKey:{0} SessionExpiry:{1}".format(
-            self.session_key, self.get_expiry_date())
+        log.info(u"SessionEngine:Created SessionKey:{0} SessionExpiry:{1} State:\"{2}\"".format(
+            self.session_key, self.get_expiry_date(), json.dumps(self._session))
+        )
+
+    def save(self, must_create=False):
+        """Logs and then calls original save method."""
+        original_save(must_create=must_create)
+        log.info(u"SessionEngine:Saved SessionKey:{0} SessionExpiry:{1} State:\"{2}\"".format(
+            self.session_key, self.get_expiry_date(), json.dumps(self._session))
         )
 
     def delete(self, session_key=None):
@@ -30,11 +39,12 @@ def patch_session_object(session):
 
         if session_key is None:
             session_key = self.session_key
-        AUDIT_LOG.info("SessionEngine:Deleted SessionKey:{0} SessionExpiry:{1}".format(
-            self.session_key, self.get_expiry_date())
+        log.info(u"SessionEngine:Deleted SessionKey:{0} SessionExpiry:{1} State:\"{2}\"".format(
+            self.session_key, self.get_expiry_date(), json.dumps(self._session))
         )
 
     session.create = types.MethodType(create, session)
+    session.save = types.MethodType(save, session)
     session.delete = types.MethodType(delete, session)
 
 
@@ -52,3 +62,6 @@ class SessionMiddleware(DjangoSessionMiddleware):
 
         if settings.FEATURES.get("ENABLE_VERBOSE_LOGGING_FOR_SESSIONS", False):
             patch_session_object(request.session)
+            log.info(u"SessionEngine:InitialState SessionKey:{0} SessionExpiry:{1} State:\"{2}\"".format(
+                request.session.session_key, request.session.get_expiry_date(), json.dumps(request.session._session))
+            )
