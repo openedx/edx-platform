4cf704696a82e5ecd95b82c3d5a54a10dba24be1
cahrens <christina@edx.org>
2014-07-22 09:42:07 -0400

    More fallout from rebase.

--- common/lib/xmodule/xmodule/modulestore/mongo/base.py
+++ common/lib/xmodule/xmodule/modulestore/mongo/base.py
@@ -1024,7 +1024,8 @@ class MongoModuleStore(ModuleStoreDraftAndPublished, ModuleStoreWriteBase):
         xblock = self.create_xmodule(location, **kwargs)
         xblock = self.update_item(xblock, user_id, allow_not_found=True)
 
-        return xblock
+        # Retrieve the item from the store so that it has edit info attributes
+        return self.get_item(xblock.location)
 
     def create_child(self, user_id, parent_usage_key, block_type, block_id=None, **kwargs):
         """
