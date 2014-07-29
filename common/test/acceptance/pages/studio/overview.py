bb9fae5e0cd0b8ecb402a724ddbad82ca4e03490
Ben McMorran <ben.mcmorran@gmail.com>
2014-07-29 13:08:04 -0400

    Add tests for course outline warning messages
    Fix failing bokchoy tests

--- common/test/acceptance/pages/studio/overview.py
+++ common/test/acceptance/pages/studio/overview.py
@@ -18,6 +18,7 @@ class CourseOutlineItem(object):
     NAME_SELECTOR = '.xblock-title .xblock-field-value'
     NAME_INPUT_SELECTOR = '.xblock-field-input'
     NAME_FIELD_WRAPPER_SELECTOR = '.xblock-title .wrapper-xblock-field'
+    STATUS_MESSAGE_SELECTOR = '> div[class$="status"] .status-message'
 
     def __repr__(self):
         # CourseOutlineItem is also used as a mixin for CourseOutlinePage, which doesn't have a locator
@@ -29,11 +30,16 @@ class CourseOutlineItem(object):
         """
         Returns `selector`, but limited to this particular `CourseOutlineItem` context
         """
-        return '{}[data-locator="{}"] {}'.format(
-            self.BODY_SELECTOR,
-            self.locator,
-            selector
-        )
+        # If the item doesn't have a body selector or locator, then it can't be bounded
+        # This happens in the context of the CourseOutlinePage
+        if self.BODY_SELECTOR and hasattr(self, 'locator'):
+            return '{}[data-locator="{}"] {}'.format(
+                self.BODY_SELECTOR,
+                self.locator,
+                selector
+            )
+        else:
+            return selector
 
     @property
     def name(self):
@@ -45,6 +51,20 @@ class CourseOutlineItem(object):
             return name_element.text[0]
         else:
             return None
+
+    @property
+    def has_status_message(self):
+        """
+        Returns True if the item has a status message, False otherwise.
+        """
+        return self.q(css=self._bounded_selector(self.STATUS_MESSAGE_SELECTOR)).first.visible
+
+    @property
+    def status_message(self):
+        """
+        Returns the status message of this item.
+        """
+        return self.q(css=self._bounded_selector(self.STATUS_MESSAGE_SELECTOR)).text[0]
 
     def edit_name(self):
         """
@@ -106,7 +126,7 @@ class CourseOutlineContainer(CourseOutlineItem):
         """
         if not child_class:
             child_class = self.CHILD_CLASS
-        return self.q(css=child_class.BODY_SELECTOR).map(
+        return self.q(css=self._bounded_selector(child_class.BODY_SELECTOR)).map(
             lambda el: child_class(self.browser, el.get_attribute('data-locator'))).results
 
     def child_at(self, index, child_class=None):
