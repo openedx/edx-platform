f9125553f9d2688f56d153b01f710a5d1f5d22d7
njdup <njdupoux1994@gmail.com>
2014-07-29 11:51:35 -0700

    Added default value to fall back on for time zone

--- common/lib/xmodule/xmodule/modulestore/django.py
+++ common/lib/xmodule/xmodule/modulestore/django.py
@@ -137,7 +137,7 @@ class ModuleI18nService(object):
         # right there.  If you are reading this comment after April 1, 2014,
         # then Cale was a liar.
         from util.date_utils import strftime_localized
-        kwargs['coerce_tz'] = settings.TIME_ZONE_DISPLAYED_FOR_DEADLINES
+        kwargs['coerce_tz'] = getattr(settings, 'TIME_ZONE_DISPLAYED_FOR_DEADLINES', None)
         return strftime_localized(*args, **kwargs)
 
 
