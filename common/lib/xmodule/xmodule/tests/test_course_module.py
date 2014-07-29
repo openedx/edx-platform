8a3373cc92ba9d137f6938934686a9978bc18bfe
njdup <njdupoux1994@gmail.com>
2014-07-29 14:22:30 -0700

    Push to rerun tests

--- common/lib/xmodule/xmodule/tests/test_course_module.py
+++ common/lib/xmodule/xmodule/tests/test_course_module.py
@@ -282,6 +282,7 @@ class IsNewCourseTestCase(unittest.TestCase):
         descriptor = get_dummy_course(start='2012-12-31T12:00')
         assert(descriptor.is_newish is True)
 
+
     # def test_end_date_text(self):
     #     # No end date set, returns empty string.
     #     d = get_dummy_course('2012-12-02T12:00')
