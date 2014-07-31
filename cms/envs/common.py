13846e074df33779834b544b8d66f01c21a55fa6
Jason Zhu <fmyzjs@gmail.com>
2014-07-31 15:20:28 +0800

    Support ipv6 in geoinfo and Embargo

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -129,7 +129,7 @@ sys.path.append(COMMON_ROOT / 'lib')
 
 # For geolocation ip database
 GEOIP_PATH = REPO_ROOT / "common/static/data/geoip/GeoIP.dat"
-
+GEOIPV6_PATH = REPO_ROOT / "common/static/data/geoip/GeoIPv6.dat"
 
 ############################# WEB CONFIGURATION #############################
 # This is where we stick our compiled template files.
