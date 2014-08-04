5ae9079d9ef426fd42b1e8b906bacac709eaecc1
cahrens <christina@edx.org>
2014-08-03 22:09:15 -0400

    Store the expanded locators on the page.
    STUD-2038

--- cms/static/js/views/pages/course_outline.js
+++ cms/static/js/views/pages/course_outline.js
@@ -4,7 +4,9 @@
 define(["jquery", "underscore", "gettext", "js/views/pages/base_page", "js/views/utils/xblock_utils",
         "js/views/course_outline"],
     function ($, _, gettext, BasePage, XBlockViewUtils, CourseOutlineView) {
-        var CourseOutlinePage = BasePage.extend({
+        var expandedLocators, CourseOutlinePage;
+
+        CourseOutlinePage = BasePage.extend({
             // takes XBlockInfo as a model
 
             events: {
@@ -36,12 +38,32 @@ define(["jquery", "underscore", "gettext", "js/views/pages/base_page", "js/views
             },
 
             renderPage: function() {
+                var setInitialExpandState = function (xblockInfo, expandedLocators) {
+                    if (xblockInfo.isCourse() || xblockInfo.isChapter()) {
+                        expandedLocators.add(xblockInfo.get('id'));
+                    }
+                };
+
                 this.setCollapseExpandVisibility();
+                this.expandedLocators = expandedLocators;
+                this.expandedLocators.clear();
+                if (this.model.get('child_info')) {
+                    _.each(this.model.get('child_info').children, function (childXBlockInfo) {
+                       setInitialExpandState(childXBlockInfo, this.expandedLocators);
+                    }, this);
+                }
+                setInitialExpandState(this.model, this.expandedLocators);
+
+                if (this.initialState && this.initialState.expanded_locators) {
+                    this.expandedLocators.addAll(this.initialState.expanded_locators);
+                }
+
                 this.outlineView = new CourseOutlineView({
                     el: this.$('.outline'),
                     model: this.model,
                     isRoot: true,
-                    initialState: this.initialState
+                    initialState: this.initialState,
+                    expandedLocators: this.expandedLocators
                 });
                 this.outlineView.render();
                 this.outlineView.setViewState(this.initialState || {});
@@ -65,8 +87,67 @@ define(["jquery", "underscore", "gettext", "js/views/pages/base_page", "js/views
                         element.removeClass('is-collapsed');
                     }
                 });
+                if (this.model.get('child_info')) {
+                    _.each(this.model.get('child_info').children, function (childXBlockInfo) {
+                        if (collapse) {
+                            this.expandedLocators.remove(childXBlockInfo.get('id'));
+                        }
+                        else {
+                            this.expandedLocators.add(childXBlockInfo.get('id'));
+                        }
+                    }, this);
+                }
             }
         });
+
+        /**
+         * Represents the set of locators that should be expanded for the page.
+         */
+        expandedLocators = {
+            locators: [],
+
+            /**
+             * Add the locator to the set if it is not already present.
+             */
+            add: function (locator) {
+                if (!this.contains(locator)) {
+                   this.locators.push(locator);
+                }
+            },
+
+            /**
+             * Accepts an array of locators and adds them all to the set if not already present.
+             */
+            addAll: function(locators) {
+                _.each(locators, function(locator) {
+                    this.add(locator);
+                }, this);
+            },
+
+            /**
+             * Remove the locator from the set if it is present.
+             */
+            remove: function (locator) {
+                var index = this.locators.indexOf(locator);
+                if (index >= 0) {
+                    this.locators.splice(index, 1);
+                }
+            },
+
+            /**
+             * Returns true iff the locator is present in the set.
+             */
+            contains: function (locator) {
+                return this.locators.indexOf(locator) >= 0;
+            },
+
+            /**
+             * Clears all expanded locators from the set.
+             */
+            clear: function () {
+                this.locators = [];
+            }
+        };
 
         return CourseOutlinePage;
     }); // end define();
