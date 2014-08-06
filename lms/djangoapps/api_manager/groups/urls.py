111eaebb3882d60fd4401f64d00a6c04069a09dd
Matt Drayer <mattdrayer@edx.org>
2014-08-06 19:33:09 -0400

    squashit

--- lms/djangoapps/api_manager/groups/urls.py
+++ lms/djangoapps/api_manager/groups/urls.py
@@ -8,7 +8,7 @@ from api_manager.groups import views as groups_views
 urlpatterns = patterns(
     '',
     url(r'/*$^', groups_views.GroupsList.as_view()),
-    url(r'^(?P<group_id>[0-9]+)/courses/(?P<course_id>[a-zA-Z0-9_+\/:]+)$', groups_views.GroupsCoursesDetail.as_view()),
+    url(r'^(?P<group_id>[0-9]+)/courses/(?P<course_id>[a-zA-Z0-9_+\/:-]+)$', groups_views.GroupsCoursesDetail.as_view()),
     url(r'^(?P<group_id>[0-9]+)/courses/*$', groups_views.GroupsCoursesList.as_view()),
     url(r'^(?P<group_id>[0-9]+)/organizations/*$', groups_views.GroupsOrganizationsList.as_view()),
     url(r'^(?P<group_id>[0-9]+)/workgroups/*$', groups_views.GroupsWorkgroupsList.as_view()),
