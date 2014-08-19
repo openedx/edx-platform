79461722dedbf9974c992d2090bac1b49fbc3a45
Renzo Lucioni <renzo@renzolucioni.com>
2014-08-19 12:52:57 -0400

    Re-use GA cookie when sending server-side events to Segment.io

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -756,6 +756,7 @@ class CourseEnrollment(models.Model):
                 tracker.emit(event_name, data)
 
                 if settings.FEATURES.get('SEGMENT_IO_LMS') and settings.SEGMENT_IO_LMS_KEY:
+                    tracking_context = tracker.get_tracker().resolve_context()
                     analytics.track(self.user_id, event_name, {
                         'category': 'conversion',
                         'label': self.course_id.to_deprecated_string(),
@@ -763,7 +764,12 @@ class CourseEnrollment(models.Model):
                         'course': self.course_id.course,
                         'run': self.course_id.run,
                         'mode': self.mode,
+                    }, context={
+                        'Google Analytics': {
+                            'clientId': tracking_context.get('client_id')
+                        }
                     })
+
         except:  # pylint: disable=bare-except
             if event_name and self.course_id:
                 log.exception('Unable to emit event %s for user %s and course %s', event_name, self.user.username, self.course_id)
