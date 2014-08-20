bbcad06ef3c5e194170cf4247c07cbca13ca58e1
Matt Drayer <mattdrayer@edx.org>
2014-08-20 17:06:27 -0400

    mattdrayer/api-course-exists-check: Refactored course lookup logic

--- lms/djangoapps/api_manager/courseware_access.py
+++ lms/djangoapps/api_manager/courseware_access.py
@@ -10,64 +10,32 @@ from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
 
 
-def get_course(request, user, course_id, depth=0):
+def get_course(request, user, course_id, depth=0, load_content=False):
     """
     Utility method to obtain course components
     """
     course_descriptor = None
-    course_key = None
     course_content = None
-    try:
-        course_key = CourseKey.from_string(course_id)
-    except InvalidKeyError:
-        try:
-            course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
-        except InvalidKeyError:
-            pass
+    course_key = get_course_key(course_id)
     if course_key:
-        try:
-            course_descriptor = courses.get_course(course_key, depth)
-        except ValueError:
-            pass
-    if course_descriptor:
-        field_data_cache = FieldDataCache([course_descriptor], course_key, user)
-        course_content = module_render.get_module_for_descriptor(
-            user,
-            request,
-            course_descriptor,
-            field_data_cache,
-            course_key)
+        course_descriptor = get_course_descriptor(course_key, depth)
+        if course_descriptor and load_content:
+            course_content = get_course_content(request, user, course_key, course_descriptor)
     return course_descriptor, course_key, course_content
 
 
-def get_course_child(request, user, course_key, content_id):
+def get_course_child(request, user, course_key, content_id, load_content=False):
     """
     Return a course xmodule/xblock to the caller
     """
-    content_descriptor = None
-    content_key = None
-    content = None
-    try:
-        content_key = UsageKey.from_string(content_id)
-    except InvalidKeyError:
-        try:
-            content_key = Location.from_deprecated_string(content_id)
-        except (InvalidLocationError, InvalidKeyError):
-            pass
-    if content_key:
-        try:
-            content_descriptor = modulestore().get_item(content_key)
-        except ItemNotFoundError:
-            pass
-        if content_descriptor:
-            field_data_cache = FieldDataCache([content_descriptor], course_key, user)
-            content = module_render.get_module_for_descriptor(
-                user,
-                request,
-                content_descriptor,
-                field_data_cache,
-                course_key)
-    return content_descriptor, content_key, content
+    child_descriptor = None
+    child_content = None
+    child_key = get_course_child_key(content_id)
+    if child_key:
+        child_descriptor = get_course_child_descriptor(child_key)
+        if child_descriptor and load_content:
+            child_content = get_course_child_content(request, user, course_key, child_descriptor)
+    return child_descriptor, child_key, child_content
 
 
 def get_course_total_score(course_summary):
@@ -92,3 +60,72 @@ def get_course_leaf_nodes(course_key, detached_categories):
         nodes.extend([unit.location for unit in vertical.get_children()
                       if getattr(unit, 'category') not in detached_categories])
     return nodes
+
+
+def get_course_key(course_id):
+    try:
+        course_key = CourseKey.from_string(course_id)
+    except InvalidKeyError:
+        try:
+            course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
+        except InvalidKeyError:
+            course_key = None
+    return course_key
+
+
+def get_course_descriptor(course_key, depth):
+    try:
+        course_descriptor = courses.get_course(course_key, depth)
+    except ValueError:
+        course_descriptor = None
+    return course_descriptor
+
+
+def get_course_content(request, user, course_key, course_descriptor):
+    field_data_cache = FieldDataCache([course_descriptor], course_key, user)
+    course_content = module_render.get_module_for_descriptor(
+        user,
+        request,
+        course_descriptor,
+        field_data_cache,
+        course_key)
+    return course_content
+
+
+def course_exists(request, user, course_id):
+    course_key = get_course_key(course_id)
+    if not course_key:
+        return False
+    if not modulestore().has_course(course_key):
+        return False
+    return True
+
+
+def get_course_child_key(content_id):
+    try:
+        content_key = UsageKey.from_string(content_id)
+    except InvalidKeyError:
+        try:
+            content_key = Location.from_deprecated_string(content_id)
+        except (InvalidLocationError, InvalidKeyError):
+            content_key = None
+    return content_key
+
+
+def get_course_child_descriptor(child_key):
+    try:
+        content_descriptor = modulestore().get_item(child_key)
+    except ItemNotFoundError:
+        content_descriptor = None
+    return content_descriptor
+
+
+def get_course_child_content(request, user, course_key, child_descriptor):
+    field_data_cache = FieldDataCache([child_descriptor], course_key, user)
+    child_content = module_render.get_module_for_descriptor(
+        user,
+        request,
+        child_descriptor,
+        field_data_cache,
+        course_key)
+    return child_content
