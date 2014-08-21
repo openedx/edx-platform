180837603eeefcb9a1779c247516640a32f7b29b
Dave St.Germain <dstgermain@edx.org>
2014-08-21 14:23:29 -0400

    Add subtitle data to video API

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1362,8 +1362,8 @@ INSTALLED_APPS = (
     'edx_jsme',    # Molecular Structure
 
     # Country list
-    'django_countries'
-
+    'django_countries',
+    
     # edX Public API
     'public_api',
     'public_api.users',
