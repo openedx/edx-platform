e4d464fc5af0bf754abc93021e394dc2b5b83b93
polesye <s2pak.anton@gmail.com>
2014-08-28 10:15:55 +0300

    Video link obfuscation. [BLD-1230]

--- common/lib/xmodule/xmodule/js/src/video/10_main.js
+++ common/lib/xmodule/xmodule/js/src/video/10_main.js
@@ -43,7 +43,9 @@
             'video/06_video_progress_slider.js',
             'video/07_video_volume_control.js',
             'video/08_video_speed_control.js',
-            'video/09_video_caption.js'
+            'video/09_video_caption.js',
+            'video/10_commands.js',
+            'video/095_video_context_menu.js'
         ],
         function (
             initialize,
@@ -54,7 +56,9 @@
             VideoProgressSlider,
             VideoVolumeControl,
             VideoSpeedControl,
-            VideoCaption
+            VideoCaption,
+            VideoCommands,
+            VideoContextMenu
         ) {
             var youtubeXhr = null,
                 oldVideo = window.Video;
@@ -87,7 +91,9 @@
                     VideoProgressSlider,
                     VideoVolumeControl,
                     VideoSpeedControl,
-                    VideoCaption
+                    VideoCaption,
+                    VideoCommands,
+                    VideoContextMenu
                 ];
 
                 state.youtubeXhr = youtubeXhr;
