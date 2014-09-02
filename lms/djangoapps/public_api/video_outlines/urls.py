48ddbd2778474136ea8869a2987e7c0e1e1bc029
David Ormsbee <dave@edx.org>
2014-09-01 21:52:51 -0400

    Update to make the mobile REST API support multi-language transcripts.
    This is still a work in progress as the course_id code is a total
    hack. I got clarification on how opaque keys should be used for this
    but haven't had time to fit it in yet.

--- lms/djangoapps/public_api/video_outlines/urls.py
+++ lms/djangoapps/public_api/video_outlines/urls.py
@@ -7,12 +7,17 @@ from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf import settings
 
-from .views import VideoSummaryList
+from .views import VideoSummaryList, VideoTranscripts
 
 urlpatterns = patterns('public_api.video_outlines.views',
 #    url(r'^{}'.format(settings.COURSE_ID_PATTERN), VideoSummaryList.as_view(), name='video-summary-list'),
 
-    url(r'^(?P<course_id>[^/]*)$', VideoSummaryList.as_view(), name='video-summary-list'),
+    url(r'^courses/(?P<course_id>[^/]*)$', VideoSummaryList.as_view(), name='video-summary-list'),
+    url(
+        r'^transcripts/(?P<course_id>[^/]*)/(?P<block_id>[^/]*)/(?P<lang>[^/]*)$',
+        VideoTranscripts.as_view(),
+        name='video-transcripts-detail'
+    ),
 
 #    url(
 #        r'^(?P<username>\w+)/course_enrollments/$',
