1429a7bf5f23142a998d965a48dc8fcf83d27ef1
Dave St.Germain <dstgermain@edx.org>
2014-09-03 16:28:23 -0400

    Show only the courses that have mobile_available set to True.

---
+++ lms/djangoapps/public_api/__init__.py
@@ -0,0 +1,12 @@
+from xmodule.modulestore.django import modulestore
+from rest_framework.exceptions import PermissionDenied
+
+def get_mobile_course(course_id):
+    """
+    Return only courses that have mobile_available set to True
+    """
+    course = modulestore().get_course(course_id)
+
+    if not course.mobile_available:
+        raise PermissionDenied(detail="Course not available on mobile.")
+    return course
