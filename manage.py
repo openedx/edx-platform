1ee78d4bced5f2403c97c6a9d4a5e9b001cdc88c
Usman Khalid <2200617@gmail.com>
2014-09-12 19:51:25 +0500

    Moved imports to manage.py and wsgi.py.

--- manage.py
+++ manage.py
@@ -11,6 +11,10 @@ by passing the --settings flag, you can specify what environment specific settin
 Any arguments not understood by this manage.py will be passed to django-admin.py
 """
 
+# Patch the xml libs before anything else.
+from safe_lxml import defuse_xml_libs
+defuse_xml_libs()
+
 import os
 import sys
 import importlib
