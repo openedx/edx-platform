50cdd2b087dc01392319d5c82241731fb4de0ae1
Chris Dodge <cdodge@edx.org>
2014-09-15 12:43:54 -0400

    only redirect to dashboard if user has course enrollments, otherwise remain on the course catalog page (aka homepage or root)
    update lettuce test to match new expected behavior
    
    add Jason's feedback
    
    only apply new redirect logic for microsites
    
    switch to use a feature flag to determine whether to support redirect behavior

--- lms/djangoapps/courseware/tests/test_microsites.py
+++ lms/djangoapps/courseware/tests/test_microsites.py
@@ -5,7 +5,6 @@ from django.core.urlresolvers import reverse
 from django.test.utils import override_settings
 
 from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
-
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 
 from helpers import LoginEnrollmentTestCase
@@ -44,17 +43,13 @@ class TestMicrosites(ModuleStoreTestCase, LoginEnrollmentTestCase):
 
         self.course_outside_microsite = CourseFactory.create(display_name='Robot_Course_Outside_Microsite', org='FooX')
 
-    def create_student_accounts(self):
-        """
-        Build out the test accounts we'll use in these tests
-        """
+    def setup_users(self):
         # Create student accounts and activate them.
         for i in range(len(self.STUDENT_INFO)):
             email, password = self.STUDENT_INFO[i]
             username = 'u{0}'.format(i)
             self.create_account(username, email, password)
             self.activate_user(email)
-
 
     @override_settings(SITE_NAME=MICROSITE_TEST_HOSTNAME)
     def test_microsite_anonymous_homepage_content(self):
@@ -89,7 +84,6 @@ class TestMicrosites(ModuleStoreTestCase, LoginEnrollmentTestCase):
         # assert that the edX partners tag line is not in the HTML
         self.assertNotContains(resp, 'Explore free courses from')
 
-
     def test_not_microsite_anonymous_homepage_content(self):
         """
         Make sure we see the right content on the homepage if we are not in a microsite
@@ -114,16 +108,39 @@ class TestMicrosites(ModuleStoreTestCase, LoginEnrollmentTestCase):
         # assert that footer template has been properly overriden on homepage
         self.assertNotContains(resp, 'This is a Test Microsite footer')
 
+    def test_no_redirect_on_homepage_when_no_enrollments(self):
+        """
+        Verify that a user going to homepage will not redirect if he/she has no course enrollments
+        """
+        self.setup_users()
+
+        email, password = self.STUDENT_INFO[0]
+        self.login(email, password)
+        resp = self.client.get(reverse('root'), HTTP_HOST=MICROSITE_TEST_HOSTNAME)
+        self.assertEquals(resp.status_code, 200)
+
+    def test_redirect_on_homepage_when_has_enrollments(self):
+        """
+        Verify that a user going to homepage will redirect to dashboard if he/she has
+        a course enrollment
+        """
+        self.setup_users()
+
+        email, password = self.STUDENT_INFO[0]
+        self.login(email, password)
+        self.enroll(self.course, True)
+
+        resp = self.client.get(reverse('root'), HTTP_HOST=MICROSITE_TEST_HOSTNAME)
+        self.assertEquals(resp.status_code, 302)
 
     def test_microsite_course_enrollment(self):
         """
         Enroll user in a course scoped in a Microsite and one course outside of a Microsite
         and make sure that they are only visible in the right Dashboards
         """
+        self.setup_users()
 
-        self.create_student_accounts()
-
-        email, password = self.STUDENT_INFO[0]
+        email, password = self.STUDENT_INFO[1]
         self.login(email, password)
         self.enroll(self.course, True)
         self.enroll(self.course_outside_microsite, True)
