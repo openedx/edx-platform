0d64cd0aac1239be2d48a9e03e90a8f8a6c54cef
David Ormsbee <dave@edx.org>
2014-09-14 20:33:54 -0400

    Cleanup in mobile video URLs

--- lms/djangoapps/public_api/urls.py
+++ lms/djangoapps/public_api/urls.py
@@ -7,11 +7,6 @@ from users.views import my_user_info, password_reset
 urlpatterns = patterns('',
     url(r'^users/', include('public_api.users.urls')),
     url(r'^my_user_info', my_user_info),
-    url(r'^password_reset', password_reset),
-
     url(r'^video_outlines/', include('public_api.video_outlines.urls')),
-
     url(r'^course_info/', include('public_api.course_info.urls')),
-
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
