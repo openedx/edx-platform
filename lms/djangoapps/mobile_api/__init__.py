d80f3e21fee4bda72657f6b5dc809400eedcc5cc
Dave St.Germain <dstgermain@edx.org>
2014-09-15 14:06:20 -0400

    Staff should be able to view courses which aren't available yet

--- lms/djangoapps/mobile_api/__init__.py
+++ lms/djangoapps/mobile_api/__init__.py
@@ -1,12 +1,27 @@
 from xmodule.modulestore.django import modulestore
 from rest_framework.exceptions import PermissionDenied
+from courseware.access import has_access
 
-def get_mobile_course(course_id):
+
+def get_mobile_course(course_id, user=None):
     """
     Return only courses that have mobile_available set to True
     """
     course = modulestore().get_course(course_id, depth=None)
 
-    if not course.mobile_available:
+    if not course.mobile_available or not has_access(user, 'staff', course):
         raise PermissionDenied(detail="Course not available on mobile.")
     return course
+
+
+def mobile_course_enrollments(enrollments, user):
+    """
+    Return enrollments only if courses are mobile_available (or if the user has staff access)
+    enrollments is a list of CourseEnrollments.
+    """
+    for enr in enrollments:
+        course = enr.course
+        if course.mobile_available:
+            yield enr
+        elif has_access(user, 'staff', course):
+            yield enr
