4e528140f81072f9da3b30b921f62b6918db49bc
Jason Zhu <fmyzjs@gmail.com>
2014-09-19 10:35:13 +0800

    When s3fs cannot be accessed replaced with osfs

--- cms/envs/aws.py
+++ cms/envs/aws.py
@@ -215,14 +215,7 @@ with open(CONFIG_ROOT / CONFIG_PREFIX + "auth.json") as auth_file:
     AUTH_TOKENS = json.load(auth_file)
 
 ############### XBlock filesystem field config ##########
-if 'XBLOCK_FS_STORAGE_BUCKET' in ENV_TOKENS:
-    DJFS = {
-        'type' : 's3fs',
-        'bucket' : ENV_TOKENS.get('XBLOCK_FS_STORAGE_BUCKET', None),
-        'prefix' : ENV_TOKENS.get('XBLOCK_FS_STORAGE_PREFIX', '/xblock-storage/'),
-        'aws_access_key_id' : AUTH_TOKENS.get('AWS_ACCESS_KEY_ID', None),
-        'aws_secret_access_key' : AUTH_TOKENS.get('AWS_SECRET_ACCESS_KEY', None)
-    }
+DJFS = AUTH_TOKENS.get('DJFS', None)
 
 EMAIL_HOST_USER = AUTH_TOKENS.get('EMAIL_HOST_USER', EMAIL_HOST_USER)
 EMAIL_HOST_PASSWORD = AUTH_TOKENS.get('EMAIL_HOST_PASSWORD', EMAIL_HOST_PASSWORD)
