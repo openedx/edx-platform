e8e29753719dd63b5c08b0683535b213f27a81b8
Martyn James <mjames@edx.org>
2014-09-23 15:08:07 -0400

    Added new assistant role

--- common/djangoapps/student/roles.py
+++ common/djangoapps/student/roles.py
@@ -272,6 +272,12 @@ class CourseBetaTesterRole(CourseRole):
     def __init__(self, *args, **kwargs):
         super(CourseBetaTesterRole, self).__init__(self.ROLE, *args, **kwargs)
 
+class CourseAssistantRole(CourseRole):
+    """A course assistant"""
+    ROLE = 'assistant'
+
+    def __init__(self, *args, **kwargs):
+        super(CourseAssistantRole, self).__init__(self.ROLE, *args, **kwargs)
 
 class CourseAssistantRole(CourseRole):
     """A course assistant"""
