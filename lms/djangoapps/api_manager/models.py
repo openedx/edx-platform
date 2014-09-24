9bd816ea6dce0737f450731ed14b0f63b280e9d9
Zia Fazal <zia.fazal@arbisoft.com>
2014-09-24 12:27:18 +0500

    ziafazal/api-add-company-logo-to-organization-model: added logo_url field

--- lms/djangoapps/api_manager/models.py
+++ lms/djangoapps/api_manager/models.py
@@ -144,6 +144,7 @@ class Organization(TimeStampedModel):
     contact_name = models.CharField(max_length=255, null=True, blank=True)
     contact_email = models.EmailField(max_length=255, null=True, blank=True)
     contact_phone = models.CharField(max_length=50, null=True, blank=True)
+    logo_url = models.CharField(max_length=255, blank=True, null=True)
     workgroups = models.ManyToManyField(Workgroup, related_name="organizations")
     users = models.ManyToManyField(User, related_name="organizations")
     groups = models.ManyToManyField(Group, related_name="organizations")
