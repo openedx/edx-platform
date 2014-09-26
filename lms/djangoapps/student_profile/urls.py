254a0ce6c848573c1bcfe1205cb582dd25ec05ee
Renzo Lucioni <renzo@renzolucioni.com>
2014-09-26 17:31:46 -0400

    Allow changing of language preference from profile page

--- lms/djangoapps/student_profile/urls.py
+++ lms/djangoapps/student_profile/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns(
     'student_profile.views',
     url(r'^$', 'index', name='profile_index'),
     url(r'^name_change$', 'name_change_handler', name='name_change'),
+    url(r'^language_change$', 'language_change_handler', name='language_change'),
 )
