74824a140a0100b6557feb96f50859b18c9d341c
Jesse Zoldak <zoldak@edx.org>
2014-09-26 12:02:12 -0400

    Quiet stevedore.extension logging in bok-choy tests

--- common/test/acceptance/tests/__init__.py
+++ common/test/acceptance/tests/__init__.py
@@ -4,6 +4,7 @@ import logging
 LOG_OVERRIDES = [
     ('requests.packages.urllib3.connectionpool', logging.ERROR),
     ('django.db.backends', logging.ERROR),
+    ('stevedore.extension', logging.ERROR),
 ]
 
 for log_name, log_level in LOG_OVERRIDES:
