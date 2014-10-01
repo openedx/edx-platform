c828897a6eeacb0091f855c487b83bda5c695db1
David Baumgold <david@davidbaumgold.com>
2014-10-01 11:13:00 -0400

    Change DISABLE_RESET_EMAIL_TEST=False to TEST_EMAIL_RESET=True
    It's more readable/understandable this way

--- cms/envs/test.py
+++ cms/envs/test.py
@@ -190,8 +190,9 @@ SEGMENT_IO_KEY = '***REMOVED***'
 
 FEATURES['ENABLE_SERVICE_STATUS'] = True
 
-# This is to disable a test under the common directory that will not pass when run under CMS
-FEATURES['DISABLE_RESET_EMAIL_TEST'] = True
+# This allows you to is to disable tests under the common directory
+# that will not pass when run under CMS
+FEATURES['TEST_EMAIL_RESET'] = False
 
 # Toggles embargo on for testing
 FEATURES['EMBARGO'] = True
