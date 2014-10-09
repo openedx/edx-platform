ca60bbd1b7f19c19ac028761e0517db82fb431e4
Filip Jukic <filip@jukic.me>
2014-10-09 12:04:59 +0200

    add docker config files

---
+++ cms/envs/docker.py
@@ -0,0 +1,13 @@
+import os
+from .aws import *
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.mandrillapp.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "nate@appsembler.com"
+EMAIL_HOST_PASSWORD = os.environ.get("MANDRILL_API_KEY", "")
+LMS_BASE = os.environ.get("EDX_LMS_BASE", "")
+PREVIEW_LMS_BASE = os.environ.get("EDX_PREVIEW_LMS_BASE", "")
+CMS_BASE = os.environ.get("EDX_CMS_BASE", "")
+TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
