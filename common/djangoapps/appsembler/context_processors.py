315b1dee5195acd9279af054bce468f72816f215
Filip Jukic <filip@jukic.me>
2014-10-09 18:54:31 +0200

    use Intercom's secure mode

--- common/djangoapps/appsembler/context_processors.py
+++ common/djangoapps/appsembler/context_processors.py
@@ -1,9 +1,17 @@
+import hmac
+import hashlib
 import os
 
 
 def intercom(request):
+    email = os.environ.get("INTERCOM_USER_EMAIL")
+    user_hash = hmac.new(
+        os.environ.get("INTERCOM_API_SECRET"),
+        email,
+        digestmod=hashlib.sha256).hexdigest()
     data = {
-        'intercom_user_email': os.environ.get("INTERCOM_USER_EMAIL"),
+        'intercom_user_email': email,
+        'intercom_user_hash': user_hash,
         'intercom_app_id': os.environ.get("INTERCOM_APP_ID"),
     }
     return data
