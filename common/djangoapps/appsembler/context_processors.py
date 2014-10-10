4bd4c870084076aa97b96eb4cdc5746a3379fd0c
Filip Jukic <filip@jukic.me>
2014-10-10 15:26:36 +0200

    default missing value for intercom should be an empty string instead of None

--- common/djangoapps/appsembler/context_processors.py
+++ common/djangoapps/appsembler/context_processors.py
@@ -4,14 +4,14 @@ import os
 
 
 def intercom(request):
-    email = os.environ.get("INTERCOM_USER_EMAIL")
+    email = os.environ.get("INTERCOM_USER_EMAIL", "")
     user_hash = hmac.new(
-        os.environ.get("INTERCOM_APP_SECRET"),
+        os.environ.get("INTERCOM_APP_SECRET", ""),
         email,
         digestmod=hashlib.sha256).hexdigest()
     data = {
         'intercom_user_email': email,
         'intercom_user_hash': user_hash,
-        'intercom_app_id': os.environ.get("INTERCOM_APP_ID"),
+        'intercom_app_id': os.environ.get("INTERCOM_APP_ID", ""),
     }
     return data
