e42ae1d9c388fa884f4416ac7d788106e8e8a185
Filip Jukic <filip@jukic.me>
2014-10-10 16:09:46 +0200

    allow all xblocks (needed to enable recommender xblock)

--- lms/envs/docker.py
+++ lms/envs/docker.py
@@ -7,7 +7,10 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "nate@appsembler.com"
 EMAIL_HOST_PASSWORD = os.environ.get("MANDRILL_API_KEY", "")
+
 LMS_BASE = os.environ.get("EDX_LMS_BASE", "")
 PREVIEW_LMS_BASE = os.environ.get("EDX_PREVIEW_LMS_BASE", "")
 CMS_BASE = os.environ.get("EDX_CMS_BASE", "")
+
+# Allows putting Intercom env variables passed from Docker into templates
 TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
