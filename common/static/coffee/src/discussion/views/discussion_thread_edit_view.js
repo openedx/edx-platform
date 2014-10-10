openedx/edx-platform

commit c2716d470c97bfd01658b815d2f3725628b186fa

TNL-172: Change type of a previously posted post. IMPORTANT: this commit depends on commit 774887b in cs_comments_service.

common/static/coffee/src/discussion/views/discussion_thread_edit_view.js
@@ -16,16 +16,22 @@
                 this.container = options.container || $('.thread-content-wrapper');
                 this.mode = options.mode || 'inline';
                 this.course_settings = options.course_settings;
+                this.threadType = this.model.get('thread_type');
                 this.topicId = options.topicId;
                 _.bindAll(this);
                 return this;
             },
 
             render: function() {
+                var threadTypeTemplate,
+                    formId = _.uniqueId("form-");
                 this.template = _.template($('#thread-edit-template').html());
                 this.$el.html(this.template(this.model.toJSON())).appendTo(this.container);
                 this.submitBtn = this.$('.post-update');
                 if (this.isTabMode()) {
+                    threadTypeTemplate = _.template($("#thread-type-template").html());
+                    this.addField(threadTypeTemplate({form_id: formId}));
+                    this.$("#" + formId + "-post-type-" + this.threadType).attr('checked', true);
                     this.topicView = new DiscussionTopicMenuView({
                         topicId: this.topicId,
                         course_settings: this.course_settings
@@ -47,6 +53,7 @@
 
             save: function() {
                 var title = this.$('.edit-post-title').val(),
+                    threadType = this.$(".post-type-input:checked").val(),
                     body = this.$('.edit-post-body textarea').val(),
                     commentableId = this.isTabMode() ? this.topicView.getCurrentTopicId() : null;
 
@@ -59,6 +66,7 @@
                     async: false, // @TODO when the rest of the stuff below is made to work properly..
                     data: {
                         title: title,
+                        thread_type: threadType,
                         body: body,
                         commentable_id: commentableId
                     },
@@ -74,12 +82,17 @@
                         this.$('.wmd-preview p').html('');
                         if (this.isTabMode()) {
                             _.extend(newAttrs, {
+                                thread_type: threadType,
                                 commentable_id: commentableId,
                                 courseware_title: this.topicView.getFullTopicName()
                             });
                         }
                         this.model.set(newAttrs).unset('abbreviatedBody');
                         this.trigger('thread:updated');
+                        if (this.threadType !== threadType) {
+                            this.model.trigger('thread:thread_type_updated');
+                            this.trigger('comment:endorse');
+                        }
                     }.bind(this)
                 });
             },

common/static/coffee/src/discussion/views/new_post_view.coffee
@@ -17,6 +17,8 @@ if Backbone?
           })
           @$el.html(_.template($("#new-post-template").html(), context))
           if @isTabMode()
+              threadTypeTemplate = _.template($("#thread-type-template").html());
+              @addField(threadTypeTemplate({form_id: _.uniqueId("form-")}));
               @topicView = new DiscussionTopicMenuView {
                   topicId:  @topicId
                   course_settings: @course_settings

common/static/coffee/spec/discussion/view/new_post_view_spec.coffee
@@ -115,7 +115,7 @@ describe "NewPostView", ->
         eventSpy = jasmine.createSpy('eventSpy')
         view.listenTo(view, "newPost:cancel", eventSpy)
         view.$(".post-errors").html("<li class='post-error'>Title can't be empty</li>")
-        view.$("#tab-post-type-discussion").click()
+        view.$("label[for$='post-type-discussion']").click()
         view.$(".js-post-title").val("Test Title")
         view.$(".js-post-body textarea").val("Test body")
         view.$(".wmd-preview p").html("Test body")
@@ -127,13 +127,14 @@ describe "NewPostView", ->
         view.$(".cancel").click()
         expect(eventSpy).toHaveBeenCalled()
         expect(view.$(".post-errors").html()).toEqual("");
-        expect($("##{mode}-post-type-question").prop("checked")).toBe(true)
-        expect($("##{mode}-post-type-discussion").prop("checked")).toBe(false)
+        if mode == "tab"
+          expect($("input[id$='post-type-question']")).toBeChecked()
+          expect($("input[id$='post-type-discussion']")).not.toBeChecked()
         expect(view.$(".js-post-title").val()).toEqual("");
         expect(view.$(".js-post-body textarea").val()).toEqual("");
-        expect(view.$(".js-follow").prop("checked")).toBe(true)
-        expect(view.$(".js-anon").prop("checked")).toBe(false)
-        expect(view.$(".js-anon-peers").prop("checked")).toBe(false)
+        expect(view.$(".js-follow")).toBeChecked()
+        expect(view.$(".js-anon")).not.toBeChecked()
+        expect(view.$(".js-anon-peers")).not.toBeChecked()
         if mode == "tab"
           expect(view.$(".js-selected-topic").text()).toEqual("General")
 

common/static/coffee/spec/discussion/discussion_spec_helper.coffee
@@ -298,26 +298,6 @@ browser and pasting the output.  When that file changes, this one should be rege
 <script aria-hidden="true" type="text/template" id="new-post-template">
     <form class="forum-new-post-form">
         <ul class="post-errors" style="display: none"></ul>
-        <div class="post-field">
-            <div class="field-label">
-                <span class="field-label-text">
-                    Post type:
-                </span><fieldset class="field-input">
-                    <input type="radio" name="<%= form_id %>-post-type" class="post-type-input" id="<%= form_id %>-post-type-question" value="question" checked>
-                    <label for="<%= form_id %>-post-type-question" class="post-type-label">
-                        <i class="icon icon-question"></i>
-                        Question
-                    </label>
-                    <input type="radio" name="<%= form_id %>-post-type" class="post-type-input" id="<%= form_id %>-post-type-discussion" value="discussion">
-                    <label for="<%= form_id %>-post-type-discussion" class="post-type-label">
-                        <i class="icon icon-comments"></i>
-                        Discussion
-                    </label>
-                </fieldset>
-            </div><span class="field-help">
-                Questions raise issues that need answers. Discussions share ideas and start conversations.
-            </span>
-        </div>
         <div class="forum-new-post-form-wrapper"></div>
         <% if (cohort_options) { %>
         <div class="post-field">
@@ -367,6 +347,29 @@ browser and pasting the output.  When that file changes, this one should be rege
             <a href="#" class="cancel">Cancel</a>
         </div>
     </form>
+</script>
+
+<script aria-hidden="true" type="text/template" id="thread-type-template">
+    <div class="post-field">
+        <div class="field-label">
+            <span class="field-label-text">
+                "Post type:"
+            </span><fieldset class="field-input">
+                <input type="radio" name="<%= form_id %>-post-type" class="post-type-input" id="<%= form_id %>-post-type-question" value="question" checked>
+                <label for="<%= form_id %>-post-type-question" class="post-type-label">
+                    <i class="icon icon-question"></i>
+                    "Question"
+                </label>
+                <input type="radio" name="<%= form_id %>-post-type" class="post-type-input" id="<%= form_id %>-post-type-discussion" value="discussion">
+                <label for="<%= form_id %>-post-type-discussion" class="post-type-label">
+                    <i class="icon icon-comments"></i>
+                    "Discussion"
+                </label>
+            </fieldset>
+        </div><span class="field-help">
+            "Questions raise issues that need answers. Discussions share ideas and start conversations."
+        </span>
+    </div>
 </script>
 
 <script aria-hidden="true" type="text/template" id="new-post-menu-entry-template">

common/static/coffee/spec/discussion/view/discussion_thread_edit_view_spec.js
@@ -26,6 +26,7 @@
                     model: this.thread,
                     mode: 'tab',
                     topicId: 'dummy_id',
+                    threadType: 'question',
                     course_settings: this.course_settings
                 }, options);
                 this.view = new DiscussionThreadEditView(options);
@@ -37,6 +38,7 @@
             var view;
             spyOn($, 'ajax').andCallFake(function(params) {
                 expect(params.url.path()).toEqual(DiscussionUtil.urlFor('update_thread', 'dummy_id'));
+                expect(params.data.thread_type).toBe('discussion');
                 expect(params.data.commentable_id).toBe('topic');
                 expect(params.data.title).toBe('new_title');
                 params.success();
@@ -45,11 +47,13 @@
             this.createEditView();
             this.view.$el.find('a.topic-title').first().click(); // set new topic
             this.view.$('.edit-post-title').val('new_title'); // set new title
+            this.view.$("label[for$='post-type-discussion']").click(); // set new thread type
             this.view.$('.post-update').click();
             expect($.ajax).toHaveBeenCalled();
 
             expect(this.thread.get('title')).toBe('new_title');
             expect(this.thread.get('commentable_id')).toBe('topic');
+            expect(this.thread.get('thread_type')).toBe('discussion');
             expect(this.thread.get('courseware_title')).toBe('Topic');
 
             expect(this.view.$('.edit-post-title')).toHaveValue('');

common/static/coffee/src/discussion/discussion_router.coffee
@@ -46,6 +46,9 @@ if Backbone?
       @thread.set("unread_comments_count", 0)
       @thread.set("read", true)
       @setActiveThread()
+      @showMain()
+
+    showMain: =>
       if(@main)
         @main.cleanup()
         @main.undelegateEvents()
@@ -63,6 +66,7 @@ if Backbone?
       @main.render()
       @main.on "thread:responses:rendered", =>
         @nav.updateSidebar()
+      @thread.on "thread:thread_type_updated", @showMain
 
     navigateToThread: (thread_id) =>
       thread = @discussion.get(thread_id)

common/static/coffee/src/discussion/views/discussion_thread_view.coffee
@@ -275,6 +275,7 @@ if Backbone?
         topicId: @model.get('commentable_id')
       )
       @editView.bind "thread:updated thread:cancel_edit", @closeEditView
+      @editView.bind "comment:endorse", @endorseThread
 
     renderSubView: (view) ->
       view.setElement(@$('.thread-content-wrapper'))

lms/djangoapps/django_comment_client/base/tests.py
@@ -868,7 +868,7 @@ class UpdateThreadUnicodeTestCase(ModuleStoreTestCase, UnicodeTestMixin, MockReq
             "user_id": str(self.student.id),
             "closed": False,
         })
-        request = RequestFactory().post("dummy_url", {"body": text, "title": text, "commentable_id": "test_commentable"})
+        request = RequestFactory().post("dummy_url", {"body": text, "title": text, "thread_type": "question", "commentable_id": "test_commentable"})
         request.user = self.student
         request.view_name = "update_thread"
         response = views.update_thread(request, course_id=self.course.id.to_deprecated_string(), thread_id="dummy_thread_id")
@@ -877,6 +877,7 @@ class UpdateThreadUnicodeTestCase(ModuleStoreTestCase, UnicodeTestMixin, MockReq
         self.assertTrue(mock_request.called)
         self.assertEqual(mock_request.call_args[1]["data"]["body"], text)
         self.assertEqual(mock_request.call_args[1]["data"]["title"], text)
+        self.assertEqual(mock_request.call_args[1]["data"]["thread_type"], "question")
         self.assertEqual(mock_request.call_args[1]["data"]["commentable_id"], "test_commentable")
 
 

commit 7292003610c34c6eee79c041bde62ba26aeed703

Fixed errors in discussion XModule, caused by c2716d470c97bfd01658b815d2f3725628b186fa: 1. Enabled `thread_type` selector for inline discussions 2. Fixed missing `commentable_id` (Javascript null was encoded as string 'null', not null object)

common/static/coffee/src/discussion/views/discussion_thread_edit_view.js
@@ -28,9 +28,9 @@
                 this.template = _.template($('#thread-edit-template').html());
                 this.$el.html(this.template(this.model.toJSON())).appendTo(this.container);
                 this.submitBtn = this.$('.post-update');
+                threadTypeTemplate = _.template($("#thread-type-template").html());
+                this.addField(threadTypeTemplate({form_id: formId}));
                 if (this.isTabMode()) {
-                    threadTypeTemplate = _.template($("#thread-type-template").html());
-                    this.addField(threadTypeTemplate({form_id: formId}));
                     this.$("#" + formId + "-post-type-" + this.threadType).attr('checked', true);
                     this.topicView = new DiscussionTopicMenuView({
                         topicId: this.topicId,
@@ -57,6 +57,16 @@
                     body = this.$('.edit-post-body textarea').val(),
                     commentableId = this.isTabMode() ? this.topicView.getCurrentTopicId() : null;
 
+                var post_data = {
+                    title: title,
+                    thread_type: threadType,
+                    body: body
+                };
+
+                if (this.isTabMode()){
+                    post_data.commentable_id = commentableId;
+                }
+
                 return DiscussionUtil.safeAjax({
                     $elem: this.submitBtn,
                     $loading: this.submitBtn,
@@ -64,12 +74,7 @@
                     type: 'POST',
                     dataType: 'json',
                     async: false, // @TODO when the rest of the stuff below is made to work properly..
-                    data: {
-                        title: title,
-                        thread_type: threadType,
-                        body: body,
-                        commentable_id: commentableId
-                    },
+                    data: post_data,
                     error: DiscussionUtil.formErrorHandler(this.$('.post-errors')),
                     success: function() {
                         var newAttrs = {

common/static/coffee/src/discussion/views/new_post_view.coffee
@@ -16,9 +16,9 @@ if Backbone?
               form_id: @mode + (if @topicId then "-" + @topicId else "")
           })
           @$el.html(_.template($("#new-post-template").html(), context))
+          threadTypeTemplate = _.template($("#thread-type-template").html());
+          @addField(threadTypeTemplate({form_id: _.uniqueId("form-")}));
           if @isTabMode()
-              threadTypeTemplate = _.template($("#thread-type-template").html());
-              @addField(threadTypeTemplate({form_id: _.uniqueId("form-")}));
               @topicView = new DiscussionTopicMenuView {
                   topicId:  @topicId
                   course_settings: @course_settings

common/static/coffee/spec/discussion/view/new_post_view_spec.coffee
@@ -127,9 +127,8 @@ describe "NewPostView", ->
         view.$(".cancel").click()
         expect(eventSpy).toHaveBeenCalled()
         expect(view.$(".post-errors").html()).toEqual("");
-        if mode == "tab"
-          expect($("input[id$='post-type-question']")).toBeChecked()
-          expect($("input[id$='post-type-discussion']")).not.toBeChecked()
+        expect($("input[id$='post-type-question']")).toBeChecked()
+        expect($("input[id$='post-type-discussion']")).not.toBeChecked()
         expect(view.$(".js-post-title").val()).toEqual("");
         expect(view.$(".js-post-body textarea").val()).toEqual("");
         expect(view.$(".js-follow")).toBeChecked()