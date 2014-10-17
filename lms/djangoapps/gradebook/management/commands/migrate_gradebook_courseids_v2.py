6aeb6d47eb8dfb005a9fbfb8889416f4f1a662ae
Matt Drayer <mattdrayer@edx.org>
2014-10-17 12:24:20 -0400

    Addressed data migration issues

--- lms/djangoapps/gradebook/management/commands/migrate_gradebook_courseids_v2.py
+++ lms/djangoapps/gradebook/management/commands/migrate_gradebook_courseids_v2.py
@@ -11,6 +11,26 @@ from gradebook import models
 log = logging.getLogger(__name__)
 
 
+def _migrate_course_id(old_course_id):
+    course_id = old_course_id.replace("slashes:", "")
+    course_id = course_id.replace("course-v1:", "")
+    course_id = course_id.replace("+", "/")
+    return course_id
+
+
+def _migrate_content_id(old_content_id):
+    if "slashes:" in old_content_id or "course-v1:" in old_content_id:
+        new_content_id = self._migrate_course_id(old_content_id)
+    else:
+        content_id = old_content_id.replace("location:", "")
+        content_components = content_id.split('+')
+        new_content_id = "i4x:/"
+        for x in range(0, len(content_components)):
+            if x != 2:
+                new_content_id = "{}/{}".format(new_content_id, content_components[x])
+    return new_content_id
+
+
 class Command(BaseCommand):
     """
     Migrates legacy course/content identifiers across several models to the new format
@@ -21,19 +41,16 @@ class Command(BaseCommand):
         log.warning('Migrating Student Gradebook Entries...')
         gradebook_entries = models.StudentGradebook.objects.all()
         for gbe in gradebook_entries:
-            current_course_id = unicode(gbe.course_id)
-            oldstyle_course_id = current_course_id.replace("slashes:", "")
-            oldstyle_course_id = oldstyle_course_id.replace("+", "/")
-            gbe.course_id = CourseKey.from_string(oldstyle_course_id)
+            course_id = _migrate_course_id(unicode(gbe.course_id))
+            print course_id
+            gbe.course_id = CourseKey.from_string(course_id)
             gbe.save()
         log.warning('Complete!')
 
         log.warning('Migrating Student Gradebook History Entries...')
         history_entries = models.StudentGradebookHistory.objects.all()
         for he in history_entries:
-            current_course_id = unicode(he.course_id)
-            oldstyle_course_id = current_course_id.replace("slashes:", "")
-            oldstyle_course_id = oldstyle_course_id.replace("+", "/")
-            he.course_id = oldstyle_course_id
+            course_id = _migrate_course_id(unicode(he.course_id))
+            he.course_id = CourseKey.from_string(course_id)
             he.save()
         log.warning('Complete!')
--- lms/djangoapps/gradebook/management/commands/tests/test_migrate_gradebook_courseids_v2.py
+++ lms/djangoapps/gradebook/management/commands/tests/test_migrate_gradebook_courseids_v2.py
@@ -20,43 +20,15 @@ class MigrateCourseIdsTests(ModuleStoreTestCase):
 
     def setUp(self):
 
-        self.course = CourseFactory.create(
-            start=datetime(2014, 6, 16, 14, 30),
-            end=datetime(2015, 1, 16)
-        )
-        self.test_data = '<html>{}</html>'.format(str(uuid.uuid4()))
+        self.bad_style_course_id = "slashes:old+style+id"
+        self.good_style_course_id = "old/style/id"
+        self.bad_style_content_id = "location:old+style+id+chapter+1234567890"
+        self.good_style_content_id = "i4x://old/style/chapter/1234567890"
 
-        self.chapter = ItemFactory.create(
-            category="chapter",
-            parent_location=self.course.location,
-            data=self.test_data,
-            due=datetime(2014, 5, 16, 14, 30),
-            display_name="Overview"
-        )
-
-        self.old_style_course_id = self.course.id.to_deprecated_string()
-        self.new_style_course_id = unicode(self.course.id)
-        self.old_style_content_id = self.chapter.location.to_deprecated_string()
-        self.new_style_content_id = unicode(self.chapter.location)
-
-        self.course2 = CourseFactory.create(
-            org='TEST',
-            start=datetime(2014, 6, 16, 14, 30),
-            end=datetime(2015, 1, 16)
-        )
-        self.chapter2 = ItemFactory.create(
-            category="chapter",
-            parent_location=self.course2.location,
-            data=self.test_data,
-            due=datetime(2014, 5, 16, 14, 30),
-            display_name="Overview"
-        )
-
-        self.old_style_course_id2 = self.course2.id.to_deprecated_string()
-        self.new_style_course_id2 = unicode(self.course2.id)
-        self.old_style_content_id2 = self.chapter2.location.to_deprecated_string()
-        self.new_style_content_id2 = unicode(self.chapter2.location)
-
+        self.bad_style_course_id2 = "course-v1:old2+style2+id2"
+        self.good_style_course_id2 = "old2/style2/id2"
+        self.bad_style_content_id2 = "location:old2+style2+id2+chapter2+1234567890"
+        self.good_style_content_id2 = "i4x://old2/style2/chapter2/1234567890"
 
     def test_migrate_courseids(self):
         """
@@ -64,10 +36,10 @@ class MigrateCourseIdsTests(ModuleStoreTestCase):
         """
         # Set up the data to be migrated
         user = User.objects.create(email='testuser@edx.org', username='testuser', password='testpassword', is_active=True)
-        gradebook_entry = gradebook_models.StudentGradebook.objects.create(user=user, course_id=self.new_style_course_id, grade=0.85, proforma_grade=0.74)
+        gradebook_entry = gradebook_models.StudentGradebook.objects.create(user=user, course_id=self.bad_style_course_id, grade=0.85, proforma_grade=0.74)
 
         user2 = User.objects.create(email='testuser2@edx.org', username='testuser2', password='testpassword2', is_active=True)
-        gradebook_entry2 = gradebook_models.StudentGradebook.objects.create(user=user2, course_id=self.new_style_course_id2, grade=0.95, proforma_grade=0.64)
+        gradebook_entry2 = gradebook_models.StudentGradebook.objects.create(user=user2, course_id=self.bad_style_course_id2, grade=0.95, proforma_grade=0.64)
 
 
         # Run the data migration
@@ -77,13 +49,13 @@ class MigrateCourseIdsTests(ModuleStoreTestCase):
         # Confirm that the data has been properly migrated
         updated_gradebook_entries = gradebook_models.StudentGradebook.objects.get(id=gradebook_entry.id)
         updated_gradebook_entry = gradebook_models.StudentGradebook.objects.get(id=gradebook_entry2.id)
-        self.assertEqual(unicode(updated_gradebook_entry.course_id), self.old_style_course_id2)
+        self.assertEqual(unicode(updated_gradebook_entry.course_id), self.good_style_course_id2)
         print "Student Gradebook Data Migration Passed"
 
         updated_history_entries = gradebook_models.StudentGradebookHistory.objects.filter(user=user.id)
         for entry in updated_history_entries:
-            self.assertEqual(unicode(entry.course_id), self.old_style_course_id)
+            self.assertEqual(unicode(entry.course_id), self.good_style_course_id)
         updated_history_entries = gradebook_models.StudentGradebookHistory.objects.filter(user=user2.id)
         for entry in updated_history_entries:
-            self.assertEqual(unicode(entry.course_id), self.old_style_course_id2)
+            self.assertEqual(unicode(entry.course_id), self.good_style_course_id2)
         print "Student Gradebook History Data Migration Passed"
