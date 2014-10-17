aabaf23e584286007ef41ce0136962e36432b9bb
Matt Drayer <mattdrayer@edx.org>
2014-10-17 12:24:20 -0400

    Addressed data migration issues

--- lms/djangoapps/api_manager/management/commands/migrate_courseids_v2.py
+++ lms/djangoapps/api_manager/management/commands/migrate_courseids_v2.py
@@ -10,6 +10,26 @@ from api_manager import models as api_models
 log = logging.getLogger(__name__)
 
 
+def _migrate_course_id(old_course_id):
+    course_id = old_course_id.replace("slashes:", "")
+    course_id = course_id.replace("course-v1:", "")
+    course_id = course_id.replace("+", "/")
+    return course_id
+
+
+def _migrate_content_id(old_content_id):
+    if "slashes:" in old_content_id or "course-v1:" in old_content_id:
+        new_content_id = self._migrate_course_id(old_content_id)
+    else:
+        content_id = old_content_id.replace("location:", "")
+        content_components = content_id.split('+')
+        new_content_id = "i4x:/"
+        for x in range(0, len(content_components)):
+            if x != 2:
+                new_content_id = "{}/{}".format(new_content_id, content_components[x])
+    return new_content_id
+
+
 class Command(BaseCommand):
     """
     Migrates legacy course/content identifiers across several models to the new format
@@ -20,45 +40,24 @@ class Command(BaseCommand):
         log.warning('Migrating Course Groups...')
         course_groups = api_models.CourseGroupRelationship.objects.all()
         for cg in course_groups:
-            current_course_id = cg.course_id
-            oldstyle_course_id = current_course_id.replace("slashes:", "")
-            oldstyle_course_id = oldstyle_course_id.replace("+", "/")
-            cg.course_id = oldstyle_course_id
+            cg.course_id = _migrate_course_id(cg.course_id)
             cg.save()
         log.warning('Complete!')
 
         log.warning('Migrating Course Content Groups...')
         course_content_groups = api_models.CourseContentGroupRelationship.objects.all()
         for ccg in course_content_groups:
-            current_course_id = ccg.course_id
-            oldstyle_course_id = current_course_id.replace("slashes:", "")
-            oldstyle_course_id = oldstyle_course_id.replace("+", "/")
-            ccg.course_id = oldstyle_course_id
-
-            current_content_id = ccg.content_id
-            oldstyle_content_id = current_content_id.replace("slashes:", "")
-            oldstyle_content_id = oldstyle_content_id.replace("+", "/")
-            ccg.content_id = oldstyle_content_id
+            ccg.course_id = _migrate_course_id(ccg.course_id)
+            ccg.content_id = _migrate_content_id(ccg.content_id)
             ccg.save()
         log.warning('Complete!')
 
         log.warning('Migrating Course Module Completions...')
         course_module_completions = api_models.CourseModuleCompletion.objects.all()
         for cmc in course_module_completions:
-            current_course_id = cmc.course_id
-            oldstyle_course_id = current_course_id.replace("slashes:", "")
-            oldstyle_course_id = oldstyle_course_id.replace("+", "/")
-            cmc.course_id = oldstyle_course_id
-
-            current_content_id = cmc.content_id
-            oldstyle_content_id = current_content_id.replace("slashes:", "")
-            oldstyle_content_id = oldstyle_content_id.replace("+", "/")
-            cmc.content_id = oldstyle_content_id
-
+            cmc.course_id = _migrate_course_id(cmc.course_id)
+            cmc.content_id = _migrate_content_id(cmc.content_id)
             if cmc.stage is not None:
-                current_stage = cmc.stage
-                oldstyle_stage = current_stage.replace("slashes:", "")
-                oldstyle_stage = oldstyle_stage.replace("+", "/")
-                cmc.stage = oldstyle_stage
-                cmc.save()
+                cmc.stage = _migrate_content_id(cmc.stage)
+            cmc.save()
         log.warning('Complete!')
--- lms/djangoapps/projects/management/commands/tests/test_migrate_project_courseids_v2.py
+++ lms/djangoapps/projects/management/commands/tests/test_migrate_project_courseids_v2.py
@@ -20,42 +20,15 @@ class MigrateCourseIdsTests(TestCase):
 
     def setUp(self):
 
-        self.course = CourseFactory.create(
-            start=datetime(2014, 6, 16, 14, 30),
-            end=datetime(2015, 1, 16)
-        )
-        self.test_data = '<html>{}</html>'.format(str(uuid.uuid4()))
+        self.bad_style_course_id = "slashes:old+style+id"
+        self.good_style_course_id = "old/style/id"
+        self.bad_style_content_id = "location:old+style+id+chapter+1234567890"
+        self.good_style_content_id = "i4x://old/style/chapter/1234567890"
 
-        self.chapter = ItemFactory.create(
-            category="chapter",
-            parent_location=self.course.location,
-            data=self.test_data,
-            due=datetime(2014, 5, 16, 14, 30),
-            display_name="Overview"
-        )
-
-        self.old_style_course_id = self.course.id.to_deprecated_string()
-        self.new_style_course_id = unicode(self.course.id)
-        self.old_style_content_id = self.chapter.location.to_deprecated_string()
-        self.new_style_content_id = unicode(self.chapter.location)
-
-        self.course2 = CourseFactory.create(
-            org='TEST',
-            start=datetime(2014, 6, 16, 14, 30),
-            end=datetime(2015, 1, 16)
-        )
-        self.chapter2 = ItemFactory.create(
-            category="chapter",
-            parent_location=self.course2.location,
-            data=self.test_data,
-            due=datetime(2014, 5, 16, 14, 30),
-            display_name="Overview"
-        )
-
-        self.old_style_course_id2 = self.course2.id.to_deprecated_string()
-        self.new_style_course_id2 = unicode(self.course2.id)
-        self.old_style_content_id2 = self.chapter2.location.to_deprecated_string()
-        self.new_style_content_id2 = unicode(self.chapter2.location)
+        self.bad_style_course_id2 = "course-v1:old2+style2+id2"
+        self.good_style_course_id2 = "old2/style2/id2"
+        self.bad_style_content_id2 = "location:old2+style2+id2+chapter2+1234567890"
+        self.good_style_content_id2 = "i4x://old2/style2/chapter2/1234567890"
 
 
     def test_migrate_project_courseids_v2(self):
@@ -65,21 +38,21 @@ class MigrateCourseIdsTests(TestCase):
         # Set up the data to be migrated
         user = User.objects.create(email='testuser@edx.org', username='testuser', password='testpassword', is_active=True)
         reviewer = User.objects.create(email='testreviewer@edx.org', username='testreviewer', password='testpassword', is_active=True)
-        project = Project.objects.create(course_id=self.old_style_course_id, content_id=self.old_style_content_id)
+        project = Project.objects.create(course_id=self.bad_style_course_id, content_id=self.bad_style_content_id)
         workgroup = Workgroup.objects.create(name='Test Workgroup', project=project)
-        workgroup_review = WorkgroupReview.objects.create(workgroup=workgroup, content_id=self.old_style_content_id)
-        workgroup_peer_review = WorkgroupPeerReview.objects.create(workgroup=workgroup, user=user, reviewer=reviewer, question="Question?", answer="Answer!", content_id=self.new_style_content_id)
+        workgroup_review = WorkgroupReview.objects.create(workgroup=workgroup, content_id=self.bad_style_content_id)
+        workgroup_peer_review = WorkgroupPeerReview.objects.create(workgroup=workgroup, user=user, reviewer=reviewer, question="Question?", answer="Answer!", content_id=self.bad_style_content_id)
         workgroup_submission = WorkgroupSubmission.objects.create(workgroup=workgroup, user=user)
-        workgroup_submission_review = WorkgroupSubmissionReview.objects.create(submission=workgroup_submission, content_id=self.old_style_content_id)
+        workgroup_submission_review = WorkgroupSubmissionReview.objects.create(submission=workgroup_submission, content_id=self.bad_style_content_id)
 
         user2 = User.objects.create(email='testuser2@edx.org', username='testuser2', password='testpassword2', is_active=True)
         reviewer2 = User.objects.create(email='testreviewer2@edx.org', username='testreviewer2', password='testpassword', is_active=True)
-        project2 = Project.objects.create(course_id=self.new_style_course_id2, content_id=self.new_style_content_id2)
+        project2 = Project.objects.create(course_id=self.bad_style_course_id2, content_id=self.bad_style_content_id2)
         workgroup2 = Workgroup.objects.create(name='Test Workgroup2', project=project2)
-        workgroup_review2 = WorkgroupReview.objects.create(workgroup=workgroup2, content_id=self.new_style_content_id2)
-        workgroup_peer_review2 = WorkgroupPeerReview.objects.create(workgroup=workgroup2, user=user2, reviewer=reviewer2, question="Question?", answer="Answer!", content_id=self.new_style_content_id2)
+        workgroup_review2 = WorkgroupReview.objects.create(workgroup=workgroup2, content_id=self.bad_style_content_id2)
+        workgroup_peer_review2 = WorkgroupPeerReview.objects.create(workgroup=workgroup2, user=user2, reviewer=reviewer2, question="Question?", answer="Answer!", content_id=self.bad_style_content_id2)
         workgroup_submission2 = WorkgroupSubmission.objects.create(workgroup=workgroup2, user=user2)
-        workgroup_submission_review2 = WorkgroupSubmissionReview.objects.create(submission=workgroup_submission2, content_id=self.new_style_content_id2)
+        workgroup_submission_review2 = WorkgroupSubmissionReview.objects.create(submission=workgroup_submission2, content_id=self.bad_style_content_id2)
 
 
         # Run the data migration
@@ -88,27 +61,27 @@ class MigrateCourseIdsTests(TestCase):
 
         # Confirm that the data has been properly migrated
         updated_project = Project.objects.get(id=project.id)
-        self.assertEqual(updated_project.course_id, self.old_style_course_id)
-        self.assertEqual(updated_project.content_id, self.old_style_content_id)
+        self.assertEqual(updated_project.course_id, self.good_style_course_id)
+        self.assertEqual(updated_project.content_id, self.good_style_content_id)
         updated_project = Project.objects.get(id=project2.id)
-        self.assertEqual(updated_project.course_id, self.old_style_course_id2)
-        self.assertEqual(updated_project.content_id, self.old_style_content_id2)
+        self.assertEqual(updated_project.course_id, self.good_style_course_id2)
+        self.assertEqual(updated_project.content_id, self.good_style_content_id2)
         print "Project Data Migration Passed"
 
         updated_workgroup_review = WorkgroupReview.objects.get(id=workgroup_review.id)
-        self.assertEqual(updated_workgroup_review.content_id, self.old_style_content_id)
+        self.assertEqual(updated_workgroup_review.content_id, self.good_style_content_id)
         updated_workgroup_review = WorkgroupReview.objects.get(id=workgroup_review2.id)
-        self.assertEqual(updated_workgroup_review.content_id, self.old_style_content_id2)
+        self.assertEqual(updated_workgroup_review.content_id, self.good_style_content_id2)
         print "Workgroup Review Data Migration Passed"
 
         updated_workgroup_peer_review = WorkgroupPeerReview.objects.get(id=workgroup_peer_review.id)
-        self.assertEqual(updated_workgroup_peer_review.content_id, self.old_style_content_id)
+        self.assertEqual(updated_workgroup_peer_review.content_id, self.good_style_content_id)
         updated_workgroup_peer_review = WorkgroupPeerReview.objects.get(id=workgroup_peer_review2.id)
-        self.assertEqual(updated_workgroup_peer_review.content_id, self.old_style_content_id2)
+        self.assertEqual(updated_workgroup_peer_review.content_id, self.good_style_content_id2)
         print "Workgroup Peer Review Data Migration Passed"
 
         updated_workgroup_submission_review = WorkgroupSubmissionReview.objects.get(id=workgroup_submission_review.id)
-        self.assertEqual(updated_workgroup_submission_review.content_id, self.old_style_content_id)
+        self.assertEqual(updated_workgroup_submission_review.content_id, self.good_style_content_id)
         updated_workgroup_submission_review = WorkgroupSubmissionReview.objects.get(id=workgroup_submission_review2.id)
-        self.assertEqual(updated_workgroup_submission_review.content_id, self.old_style_content_id2)
+        self.assertEqual(updated_workgroup_submission_review.content_id, self.good_style_content_id2)
         print "Workgroup Submission Review Data Migration Passed"
