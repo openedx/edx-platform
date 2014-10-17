3ab6860bdb95c49927e278d252f42a95bb5311d9
Will Daly <will@edx.org>
2014-10-17 16:16:30 -0400

    Fix unit test failures due to duplicate url names
    Reset the correct URLs module to fix test failures.
    
    Fix third party auth to reconfigure the registry on test cleanup.

--- lms/djangoapps/student_account/test/test_views.py
+++ lms/djangoapps/student_account/test/test_views.py
@@ -53,7 +53,7 @@ class StudentAccountViewTest(UrlResetMixin, TestCase):
 
     @patch.dict(settings.FEATURES, {'ENABLE_NEW_DASHBOARD': True})
     def setUp(self):
-        super(StudentAccountViewTest, self).setUp()
+        super(StudentAccountViewTest, self).setUp("student_account.urls")
 
         # Create/activate a new account
         activation_key = account_api.create_account(self.USERNAME, self.OLD_PASSWORD, self.OLD_EMAIL)
@@ -68,8 +68,8 @@ class StudentAccountViewTest(UrlResetMixin, TestCase):
         self.assertContains(response, "Student Account")
 
     @ddt.data(
-        ("login", "login"),
-        ("register", "register"),
+        ("account_login", "login"),
+        ("account_register", "register"),
     )
     @ddt.unpack
     def test_login_and_registration_form(self, url_name, initial_mode):
@@ -77,7 +77,7 @@ class StudentAccountViewTest(UrlResetMixin, TestCase):
         expected_data = u"data-initial-mode=\"{mode}\"".format(mode=initial_mode)
         self.assertContains(response, expected_data)
 
-    @ddt.data("login", "register")
+    @ddt.data("account_login", "account_register")
     def test_login_and_registration_third_party_auth_urls(self, url_name):
         response = self.client.get(reverse(url_name))
 
