2e4525838394664e0647f679aaeb402d1fed7d7a
Matt Drayer <mattdrayer@edx.org>
2014-10-17 12:24:20 -0400

    Addressed data migration issues

--- lms/djangoapps/api_manager/management/commands/tests/test_migrate_courseids_v2.py
+++ lms/djangoapps/api_manager/management/commands/tests/test_migrate_courseids_v2.py
@@ -20,42 +20,15 @@ class MigrateCourseIdsTests(ModuleStoreTestCase):
 
     def setUp(self):
 
-        self.course = CourseFactory.create(
-            start=datetime(2014, 6, 16, 14, 30),
-            end=datetime(2015, 1, 16)
-        )
-        self.test_data = '<html>{}</html>'.format(str(uuid.uuid4()))
+        self.bad_style_course_id = "slashes:old+style+id"
+        self.good_style_course_id = "old/style/id"
+        self.bad_style_content_id = "location:old+style+id+chapter+1234567890"
+        self.good_style_content_id = "i4x://old/style/chapter/1234567890"
 
-        self.chapter = ItemFactory.create(
-            category="chapter",
-            parent_location=self.course.location,
-            data=self.test_data,
-            due=datetime(2014, 5, 16, 14, 30),
-            display_name="Overview"
-        )
-
-        self.old_style_course_id = self.course.id.to_deprecated_string()
-        self.new_style_course_id = unicode(self.course.id)
-        self.old_style_content_id = self.chapter.location.to_deprecated_string()
-        self.new_style_content_id = unicode(self.chapter.location)
-
-        self.course2 = CourseFactory.create(
-            org='TEST',
-            start=datetime(2014, 6, 16, 14, 30),
-            end=datetime(2015, 1, 16)
-        )
-        self.chapter2 = ItemFactory.create(
-            category="chapter",
-            parent_location=self.course2.location,
-            data=self.test_data,
-            due=datetime(2014, 5, 16, 14, 30),
-            display_name="Overview"
-        )
-
-        self.old_style_course_id2 = self.course2.id.to_deprecated_string()
-        self.new_style_course_id2 = unicode(self.course2.id)
-        self.old_style_content_id2 = self.chapter2.location.to_deprecated_string()
-        self.new_style_content_id2 = unicode(self.chapter2.location)
+        self.bad_style_course_id2 = "course-v1:old2+style2+id2"
+        self.good_style_course_id2 = "old2/style2/id2"
+        self.bad_style_content_id2 = "location:old2+style2+id2+chapter2+1234567890"
+        self.good_style_content_id2 = "i4x://old2/style2/chapter2/1234567890"
 
 
     def test_migrate_courseids_v2(self):
@@ -66,16 +39,16 @@ class MigrateCourseIdsTests(ModuleStoreTestCase):
         user = User.objects.create(email='testuser@edx.org', username='testuser', password='testpassword', is_active=True)
         group = Group.objects.create(name='Test Group')
         group_profile = api_models.GroupProfile.objects.create(group=group)
-        course_group = api_models.CourseGroupRelationship.objects.create(course_id=self.old_style_course_id, group=group)
-        course_content_group = api_models.CourseContentGroupRelationship.objects.create(course_id=self.old_style_course_id, content_id=self.old_style_content_id, group_profile=group_profile)
-        course_module_completion = api_models.CourseModuleCompletion.objects.create(user=user, course_id=self.old_style_course_id, content_id=self.old_style_content_id)
+        course_group = api_models.CourseGroupRelationship.objects.create(course_id=self.bad_style_course_id, group=group)
+        course_content_group = api_models.CourseContentGroupRelationship.objects.create(course_id=self.bad_style_course_id, content_id=self.bad_style_content_id, group_profile=group_profile)
+        course_module_completion = api_models.CourseModuleCompletion.objects.create(user=user, course_id=self.bad_style_course_id, content_id=self.bad_style_content_id)
 
         user2 = User.objects.create(email='testuser2@edx.org', username='testuser2', password='testpassword2', is_active=True)
         group2 = Group.objects.create(name='Test Group2')
         group_profile2 = api_models.GroupProfile.objects.create(group=group2)
-        course_group2 = api_models.CourseGroupRelationship.objects.create(course_id=self.new_style_course_id2, group=group2)
-        course_content_group2 = api_models.CourseContentGroupRelationship.objects.create(course_id=self.new_style_course_id2, content_id=self.new_style_content_id2, group_profile=group_profile2)
-        course_module_completion2 = api_models.CourseModuleCompletion.objects.create(user=user2, course_id=self.new_style_course_id2, content_id=self.new_style_content_id2)
+        course_group2 = api_models.CourseGroupRelationship.objects.create(course_id=self.bad_style_course_id2, group=group2)
+        course_content_group2 = api_models.CourseContentGroupRelationship.objects.create(course_id=self.bad_style_course_id2, content_id=self.bad_style_content_id2, group_profile=group_profile2)
+        course_module_completion2 = api_models.CourseModuleCompletion.objects.create(user=user2, course_id=self.bad_style_course_id2, content_id=self.bad_style_content_id2, stage=self.bad_style_content_id2)
 
 
         # Run the data migration
@@ -84,23 +57,24 @@ class MigrateCourseIdsTests(ModuleStoreTestCase):
 
         # Confirm that the data has been properly migrated
         updated_course_group = api_models.CourseGroupRelationship.objects.get(id=course_group.id)
-        self.assertEqual(updated_course_group.course_id, self.old_style_course_id)
+        self.assertEqual(updated_course_group.course_id, self.good_style_course_id)
         updated_course_group = api_models.CourseGroupRelationship.objects.get(id=course_group2.id)
-        self.assertEqual(updated_course_group.course_id, self.old_style_course_id2)
+        self.assertEqual(updated_course_group.course_id, self.good_style_course_id2)
         print "Course Group Data Migration Passed"
 
         updated_course_content_group = api_models.CourseContentGroupRelationship.objects.get(id=course_content_group.id)
-        self.assertEqual(updated_course_content_group.course_id, self.old_style_course_id)
-        self.assertEqual(updated_course_content_group.content_id, self.old_style_content_id)
+        self.assertEqual(updated_course_content_group.course_id, self.good_style_course_id)
+        self.assertEqual(updated_course_content_group.content_id, self.good_style_content_id)
         updated_course_content_group = api_models.CourseContentGroupRelationship.objects.get(id=course_content_group2.id)
-        self.assertEqual(updated_course_content_group.course_id, self.old_style_course_id2)
-        self.assertEqual(updated_course_content_group.content_id, self.old_style_content_id2)
+        self.assertEqual(updated_course_content_group.course_id, self.good_style_course_id2)
+        self.assertEqual(updated_course_content_group.content_id, self.good_style_content_id2)
         print "Course Content Group Data Migration Passed"
 
         updated_course_module_completion = api_models.CourseModuleCompletion.objects.get(id=course_module_completion.id)
-        self.assertEqual(updated_course_module_completion.course_id, self.old_style_course_id)
-        self.assertEqual(updated_course_module_completion.content_id, self.old_style_content_id)
+        self.assertEqual(updated_course_module_completion.course_id, self.good_style_course_id)
+        self.assertEqual(updated_course_module_completion.content_id, self.good_style_content_id)
         updated_course_module_completion = api_models.CourseModuleCompletion.objects.get(id=course_module_completion2.id)
-        self.assertEqual(updated_course_module_completion.course_id, self.old_style_course_id2)
-        self.assertEqual(updated_course_module_completion.content_id, self.old_style_content_id2)
+        self.assertEqual(updated_course_module_completion.course_id, self.good_style_course_id2)
+        self.assertEqual(updated_course_module_completion.content_id, self.good_style_content_id2)
+        self.assertEqual(updated_course_module_completion.stage, self.good_style_content_id2)
         print "Course Module Completion Data Migration Passed"
