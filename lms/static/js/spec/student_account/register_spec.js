f9ecd8849aac62b43c0a616b146675427b5d8c47
Renzo Lucioni <renzo@renzolucioni.com>
2014-10-23 17:17:00 -0400

    Fix logistration Jasmine tests
    Includes several bug fixes caught during test writing, general clean-up, and adds js-url, a lightweight URL parser

--- lms/static/js/spec/student_account/register_spec.js
+++ lms/static/js/spec/student_account/register_spec.js
@@ -1,35 +1,296 @@
-define(['js/common_helpers/template_helpers', 'js/student_account/views/RegisterView'],
-    function(TemplateHelpers) {
-        describe("edx.student.account.RegisterView", function() {
+define([
+    'jquery',
+    'underscore',
+    'js/common_helpers/template_helpers',
+    'js/common_helpers/ajax_helpers',
+    'js/student_account/models/RegisterModel',
+    'js/student_account/views/RegisterView'
+], function($, _, TemplateHelpers, AjaxHelpers, RegisterModel, RegisterView) {
+        describe('edx.student.account.RegisterView', function() {
             'use strict';
 
+            var model = null,
+                view = null,
+                requests = null,
+                PLATFORM_NAME = 'edX',
+                USER_DATA = {
+                    email: 'xsy@edx.org',
+                    name: 'Xsy M. Education',
+                    username: 'Xsy',
+                    password: 'xsyisawesome',
+                    level_of_education: 'p',
+                    gender: 'm',
+                    year_of_birth: 2014,
+                    mailing_address: '141 Portland',
+                    goals: 'To boldly learn what no letter of the alphabet has learned before',
+                    terms_of_service: true
+                },
+                THIRD_PARTY_AUTH = {
+                    currentProvider: null,
+                    providers: [
+                        {
+                            name: 'Google',
+                            iconClass: 'icon-google-plus',
+                            loginUrl: '/auth/login/google-oauth2/?auth_entry=account_login',
+                            registerUrl: '/auth/login/google-oauth2/?auth_entry=account_register'
+                        },
+                        {
+                            name: 'Facebook',
+                            iconClass: 'icon-facebook',
+                            loginUrl: '/auth/login/facebook/?auth_entry=account_login',
+                            registerUrl: '/auth/login/facebook/?auth_entry=account_register'
+                        }
+                    ]
+                },
+                FORM_DESCRIPTION = {
+                    method: 'post',
+                    submit_url: '/user_api/v1/account/registration/',
+                    fields: [
+                        {
+                            name: 'email',
+                            label: 'Email',
+                            defaultValue: '',
+                            type: 'email',
+                            required: true,
+                            placeholder: 'place@holder.org',
+                            instructions: 'Enter your email.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'name',
+                            label: 'Full Name',
+                            defaultValue: '',
+                            type: 'text',
+                            required: true,
+                            instructions: 'Enter your username.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'username',
+                            label: 'Username',
+                            defaultValue: '',
+                            type: 'text',
+                            required: true,
+                            instructions: 'Enter your username.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'password',
+                            label: 'Password',
+                            defaultValue: '',
+                            type: 'password',
+                            required: true,
+                            instructions: 'Enter your password.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'level_of_education',
+                            label: 'Highest Level of Education Completed',
+                            defaultValue: '',
+                            type: 'select',
+                            options: [
+                                {value: "", name: "--"},
+                                {value: "p", name: "Doctorate"},
+                                {value: "m", name: "Master's or professional degree"},
+                                {value: "b", name: "Bachelor's degree"},
+                            ],
+                            required: false,
+                            instructions: 'Select your education level.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'gender',
+                            label: 'Gender',
+                            defaultValue: '',
+                            type: 'select',
+                            options: [
+                                {value: "", name: "--"},
+                                {value: "m", name: "Male"},
+                                {value: "f", name: "Female"},
+                                {value: "o", name: "Other"},
+                            ],
+                            required: false,
+                            instructions: 'Select your gender.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'year_of_birth',
+                            label: 'Year of Birth',
+                            defaultValue: '',
+                            type: 'select',
+                            options: [
+                                {value: "", name: "--"},
+                                {value: 1900, name: "1900"},
+                                {value: 1950, name: "1950"},
+                                {value: 2014, name: "2014"},
+                            ],
+                            required: false,
+                            instructions: 'Select your year of birth.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'mailing_address',
+                            label: 'Mailing Address',
+                            defaultValue: '',
+                            type: 'textarea',
+                            required: false,
+                            instructions: 'Enter your mailing address.',
+                            restrictions: {}
+                        },
+                        {
+                            name: 'goals',
+                            label: 'Goals',
+                            defaultValue: '',
+                            type: 'textarea',
+                            required: false,
+                            instructions: "If you'd like, tell us why you're interested in edX.",
+                            restrictions: {}
+                        },
+                        {
+                            name: 'terms_of_service',
+                            label: 'Terms of Service',
+                            defaultValue: '',
+                            type: 'checkbox',
+                            required: true,
+                            instructions: "Agree to the terms of service.",
+                            restrictions: {}
+                        }
+                    ]
+                };
+
+            var createRegisterView = function(that) {
+                // Initialize the register model
+                model = new RegisterModel({ url: FORM_DESCRIPTION.submit_url });
+
+                // Initialize the register view
+                view = new RegisterView({
+                    fields: FORM_DESCRIPTION.fields,
+                    model: model,
+                    thirdPartyAuth: THIRD_PARTY_AUTH,
+                    platformName: PLATFORM_NAME
+                });
+
+                // Spy on AJAX requests
+                requests = AjaxHelpers.requests(that);
+
+                // Mock out redirection logic
+                spyOn(view, 'redirect').andCallFake(function() {
+                    return true;
+                });
+            };
+
+            var submitForm = function(validationSuccess) {
+                // Simulate manual entry of registration form data
+                $('#register-email').val(USER_DATA.email);
+                $('#register-name').val(USER_DATA.name);
+                $('#register-username').val(USER_DATA.username);
+                $('#register-password').val(USER_DATA.password);
+                $('#register-level_of_education').val(USER_DATA.level_of_education);
+                $('#register-gender').val(USER_DATA.gender);
+                $('#register-year_of_birth').val(USER_DATA.year_of_birth);
+                $('#register-mailing_address').val(USER_DATA.mailing_address);
+                $('#register-goals').val(USER_DATA.goals);
+
+                // Check the terms of service checkbox
+                $('#register-terms_of_service').prop('checked', USER_DATA.terms_of_service);
+
+                // Create a fake click event
+                var clickEvent = $.Event('click');
+
+                // If validationSuccess isn't passed, we avoid
+                // spying on `view.validate` twice
+                if ( !_.isUndefined(validationSuccess) ) {
+                    // Force validation to return as expected
+                    spyOn(view, 'validate').andReturn({
+                        isValid: validationSuccess,
+                        message: 'Submission was validated.'
+                    });
+                }
+
+                // Submit the email address
+                view.submitForm(clickEvent);
+            };
+
             beforeEach(function() {
-                setFixtures("<div></div>");
-                TemplateHelpers.installTemplate("templates/student_account/register");
+                setFixtures('<div id="register-form"></div>');
+                TemplateHelpers.installTemplate('templates/student_account/register');
+                TemplateHelpers.installTemplate('templates/student_account/form_field');
             });
 
-            it("registers a new user", function() {
-                // TODO
+            it('registers a new user', function() {
+                createRegisterView(this);
+
+                // Submit the form, with successful validation
+                submitForm(true);
+
+                // Verify that the client contacts the server with the expected data
+                AjaxHelpers.expectRequest(
+                    requests, 'POST', FORM_DESCRIPTION.submit_url, $.param(
+                        $.extend({url: FORM_DESCRIPTION.submit_url}, USER_DATA)
+                    )
+                );
+
+                // Respond with status code 200
+                AjaxHelpers.respondWithJson(requests, {});
+
+                // Verify that the user is redirected to the dashboard
+                expect(view.redirect).toHaveBeenCalledWith('/dashboard');
             });
 
-            it("displays third party auth registration buttons", function() {
-                // TODO
+            it('displays third-party auth registration buttons', function() {
+                createRegisterView(this);
+
+                // Verify that Google and Facebook registration buttons are displayed
+                expect($('.button-Google')).toBeVisible();
+                expect($('.button-Facebook')).toBeVisible();
             });
 
-            it("validates form fields", function() {
-                // TODO
+            it('validates registration form fields', function() {
+                createRegisterView(this);
+
+                // Submit the form, with successful validation
+                submitForm(true);
+
+                // Verify that validation of form fields occurred
+                expect(view.validate).toHaveBeenCalledWith($('#register-email')[0]);
+                expect(view.validate).toHaveBeenCalledWith($('#register-name')[0]);
+                expect(view.validate).toHaveBeenCalledWith($('#register-username')[0]);
+                expect(view.validate).toHaveBeenCalledWith($('#register-password')[0]);
+
+                // Verify that no submission errors are visible
+                expect(view.$errors).toHaveClass('hidden');
             });
 
-            it("displays registration errors", function() {
-                // TODO
+            it('displays registration form validation errors', function() {
+                createRegisterView(this);
+
+                // Submit the form, with failed validation
+                submitForm(false);
+
+                // Verify that submission errors are visible
+                expect(view.$errors).not.toHaveClass('hidden');
             });
 
-            it("displays an error if the form definition could not be loaded", function() {
-                // TODO
-            });
+            it('displays an error if the server returns an error while registering', function() {
+                createRegisterView(this);
 
-            it("displays an error if the server could not be contacted while registering", function() {
-                // TODO
+                // Submit the form, with successful validation
+                submitForm(true);
+
+                // Simulate an error from the LMS servers
+                AjaxHelpers.respondWithError(requests);
+
+                // Expect that an error is displayed
+                expect(view.$errors).not.toHaveClass('hidden');
+
+                // If we try again and succeed, the error should go away
+                submitForm();
+
+                // This time, respond with status code 200
+                AjaxHelpers.respondWithJson(requests, {});
+
+                // Expect that the error is hidden
+                expect(view.$errors).toHaveClass('hidden');
             });
         });
     }
--- lms/static/js/student_account/enrollment_interface.js
+++ lms/static/js/student_account/enrollment_interface.js
@@ -8,71 +8,80 @@ var edx = edx || {};
 
     edx.student.account.EnrollmentInterface = {
         courseUrl: '/enrollment/v0/course/',
+
         studentUrl: '/enrollment/v0/student',
+
         trackSelectionUrl: '/course_modes/choose/',
+
         headers: {
             'X-CSRFToken': $.cookie('csrftoken')
         },
         
-        studentInformation: function(course_key) {
+        studentInformation: function(courseKey) {
             // retrieve student enrollment information
         },
 
-        courseInformation: function(course_key) {
+        courseInformation: function(courseKey) {
             // retrieve course information from the enrollment API
         },
 
-        modeInArray: function(mode_slug, course_modes) {
-            // finds whether or not a particular course mode slug exists
-            // in an array of course modes
-            var result = _.find(course_modes, function(mode){ return mode.slug === mode_slug; });
-            return result != undefined;
+        modeInArray: function(modeObjects, targetMode) {
+            // Check if a given course mode slug exists in an array of mode objects
+            var result = _.find(modeObjects, function(mode) {
+                return mode.slug === targetMode; 
+            });
+
+            /* _.find returns the first value which passes the provided truth test,
+            /* or undefined if no values pass the test
+             */
+            return !_.isUndefined(result);
         },
 
-        enroll: function(course_key, forward_url){
+        enroll: function(courseKey, forwardUrl){
             var me = this;
             // attempt to enroll a student in a course
             $.ajax({
-                url: this.courseUrl + course_key,
+                url: this.courseUrl + courseKey,
                 type: 'POST',
                 data: {},
                 headers: this.headers
             }).done(function(data){
-                me.postEnrollmentHandler(course_key, data, forward_url);
+                me.postEnrollmentHandler(courseKey, data, forwardUrl);
             }
             ).fail(function(data, textStatus) {
-                me.enrollmentFailureHandler(course_key, data, forward_url);
+                me.enrollmentFailureHandler(courseKey, data, forwardUrl);
             });
         },
 
-        enrollmentFailureHandler: function(course_key, data, forward_url) {
+        enrollmentFailureHandler: function(courseKey, data, forwardUrl) {
             // handle failures to enroll via the API
             if(data.status == 400) {
-                // This status code probably means we don't have permissions to register for this course.
-                // look at the contents of the response
+                /* This status code probably means we don't have permissions to register
+                /* for this course; look at the contents of the response
+                 */
                 var course = $.parseJSON(data.responseText);
                 // see if it's a professional ed course
-                if('course_modes' in course && this.modeInArray('professional', course.course_modes)) {
+                if( 'course_modes' in course && this.modeInArray(course.course_modes, 'professional') ) {
                     // forward appropriately
-                    forward_url = this.trackSelectionUrl + course_key;
+                    forwardUrl = this.trackSelectionUrl + courseKey;
                 }
             }
             // TODO: if we have a paid registration mode, add item to the cart and send them along
 
-            // TODO: we should figure out how to handle errors here eventually
-            window.location.href = forward_url;
+            // TODO: we should figure out how to handle errors here
+            window.location.href = forwardUrl;
         },
 
-        postEnrollmentHandler: function(course_key, data, forward_url) {
+        postEnrollmentHandler: function(courseKey, data, forwardUrl) {
             // Determine whether or not the course needs to be redirected to
             // a particular page.
             var course = data.course,
                 course_modes = course.course_modes;
             
             // send the user to the track selection page, because it will do the right thing
-            forward_url = this.trackSelectionUrl + course_key;
+            forwardUrl = this.trackSelectionUrl + courseKey;
 
-            window.location.href = forward_url;
+            window.location.href = forwardUrl;
         }
     };
 })(jQuery, _, gettext);
