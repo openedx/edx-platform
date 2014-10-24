eb562c6530d32a71cb2d9b3a22b73725ba472abb
Calen Pennington <cale@edx.org>
2014-10-24 10:57:47 -0400

    fixup! Short-circuit when binding an XModuleDescriptor as an XModule, if it has already been bound for the same user

--- lms/djangoapps/courseware/module_render.py
+++ lms/djangoapps/courseware/module_render.py
@@ -597,7 +597,7 @@ def get_module_for_descriptor_internal(user, descriptor, field_data_cache, cours
         if not has_access(user, 'load', descriptor, course_id):
             return None
 
-    if descriptor._bound_student == user: # pylint: disable=protected-access
+    if descriptor._bound_student == user:  # pylint: disable=protected-access
         return descriptor
 
     (system, student_data) = get_module_system_for_user(
