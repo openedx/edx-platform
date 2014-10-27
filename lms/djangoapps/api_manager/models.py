27c83fd0bd25149ed9a24570ef49302f81d6c467
Zia Fazal <zia.fazal@arbisoft.com>
2014-10-27 19:46:00 +0500

    merging c9de387... initial changes
    completed tests and added type filter
    
    removed organizations app
    
    migration to delete tables

--- lms/djangoapps/api_manager/models.py
+++ lms/djangoapps/api_manager/models.py
@@ -8,7 +8,6 @@ from django.conf import settings
 
 from model_utils.models import TimeStampedModel
 from .utils import is_int
-from projects.models import Workgroup
 
 
 class GroupRelationship(TimeStampedModel):
@@ -132,22 +131,6 @@ class CourseContentGroupRelationship(TimeStampedModel):
         Mapping model to enable grouping of course content such as chapters
         """
         unique_together = ("course_id", "content_id", "group_profile")
-
-
-class Organization(TimeStampedModel):
-    """
-    Main table representing the Organization concept.  Organizations are
-    primarily a collection of Users.
-    """
-    name = models.CharField(max_length=255)
-    display_name = models.CharField(max_length=255, null=True, blank=True)
-    contact_name = models.CharField(max_length=255, null=True, blank=True)
-    contact_email = models.EmailField(max_length=255, null=True, blank=True)
-    contact_phone = models.CharField(max_length=50, null=True, blank=True)
-    logo_url = models.CharField(max_length=255, blank=True, null=True)
-    workgroups = models.ManyToManyField(Workgroup, related_name="organizations")
-    users = models.ManyToManyField(User, related_name="organizations")
-    groups = models.ManyToManyField(Group, related_name="organizations")
 
 
 class CourseModuleCompletion(TimeStampedModel):
