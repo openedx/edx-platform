578c04f62f27d8e185734bfe9d27f98cca104727
Will Daly <will@edx.org>
2014-10-28 15:02:40 -0400

    Put logistration page behind a feature flag

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -288,6 +288,9 @@ FEATURES = {
     # Enable the new dashboard, account, and profile pages
     'ENABLE_NEW_DASHBOARD': False,
 
+    # Enable the combined login/registration form
+    'ENABLE_COMBINED_LOGIN_REGISTRATION': False,
+
     # Show a section in the membership tab of the instructor dashboard
     # to allow an upload of a CSV file that contains a list of new accounts to create
     # and register for course.
