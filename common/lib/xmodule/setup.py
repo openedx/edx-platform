8df1c47f07b4175b02a457592aae33ebb0b3218a
Braden MacDonald <braden@opencraft.com>
2014-10-28 23:10:40 -0700

    Library Content XBlock

--- common/lib/xmodule/setup.py
+++ common/lib/xmodule/setup.py
@@ -12,6 +12,7 @@ XMODULES = [
     "html = xmodule.html_module:HtmlDescriptor",
     "image = xmodule.backcompat_module:TranslateCustomTagDescriptor",
     "library = xmodule.library_module:LibraryDescriptor",
+    "library_content = xmodule.library_content_module:LibraryContentDescriptor",
     "error = xmodule.error_module:ErrorDescriptor",
     "peergrading = xmodule.peer_grading_module:PeerGradingDescriptor",
     "poll_question = xmodule.poll_module:PollDescriptor",
