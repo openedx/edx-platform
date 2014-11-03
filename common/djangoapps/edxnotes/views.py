b26631c8eb2cf7d112d66576bba13f9c8e0b8222
Tim Babych <tim.babych@gmail.com>
2014-11-03 15:36:20 +0200

    fixed docstring in for edxnotes/views.py

--- common/djangoapps/edxnotes/views.py
+++ common/djangoapps/edxnotes/views.py
@@ -1,5 +1,5 @@
 """
-Views for accessing language preferences
+Views for accessing student notes
 """
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
