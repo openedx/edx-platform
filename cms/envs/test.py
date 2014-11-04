4e575a2add588aba19b118240cddc64d8c68c960
Oleg Marshev <oleh.marshev@gmail.com>
2014-11-04 15:02:18 +0200

    Add platform-specific feature flag.

--- cms/envs/test.py
+++ cms/envs/test.py
@@ -224,3 +224,7 @@ FEATURES['USE_MICROSITES'] = True
 # For consistency in user-experience, keep the value of this setting in sync with
 # the one in lms/envs/test.py
 FEATURES['ENABLE_DISCUSSION_SERVICE'] = False
+
+EDXNOTES_INTERFACE = {
+    'url': 'http://localhost:8042/',
+}
