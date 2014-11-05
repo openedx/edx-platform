8eaffd79242b50d4a82fd399e481feacd71c6208
polesye <s2pak.anton@gmail.com>
2014-11-05 15:55:47 +0200

    TNL-660: Toggle single note visibility.

--- lms/static/js/edxnotes/views/shim.js
+++ lms/static/js/edxnotes/views/shim.js
@@ -4,6 +4,25 @@
         var _t = Annotator._t;
 
         /**
+         * We currently run JQuery 1.7.2 in Jasmine tests and LMS.
+         * AnnotatorJS 1.2.9. uses two calls to addBack (in the two functions
+         * 'isAnnotator' and 'onHighlightMouseover') which was only defined in
+         * JQuery 1.8.0. In LMS, it works without throwing an error because
+         * JQuery.UI 1.10.0 adds support to jQuery<1.8 by augmenting '$.fn' with
+         * that missing function. It is not the case for all Jasmine unit tests,
+         * so we add it here if necessary.
+         **/
+        if (!$.fn.addBack) {
+            $.fn.addBack = function(selector) {
+                return this.add(selector === null ?
+                        this.prevObject : this.prevObject.filter(selector)
+                );
+            };
+        }
+
+        Annotator.frozenSrc = null;
+
+        /**
          * Modifies Annotator.highlightRange to add a "tabindex=0" attribute
          * to the <span class="annotator-hl"> markup that encloses the note.
          * These are then focusable via the TAB key.
@@ -14,6 +33,25 @@
                 return results;
             },
             Annotator.prototype.highlightRange
+        );
+
+        /**
+         * Modifies Annotator.destroy to unbind click.edxnotes:freeze from the
+         * document and reset isFrozen to default value, false.
+         **/
+        Annotator.prototype.destroy = _.compose(
+            Annotator.prototype.destroy,
+            function () {
+                // We are destroying the instance that has the popup visible, revert to default,
+                // unfreeze all instances and set their isFrozen to false
+                if (this === Annotator.frozenSrc) {
+                    _.invoke(Annotator._instances, 'unfreeze');
+                } else {
+                    // Unfreeze only this instance and unbound associated 'click.edxnotes:freeze' handler
+                    $(document).off('click.edxnotes:freeze'+this.uid);
+                    this.isFrozen = false;
+                }
+            }
         );
 
         /**
@@ -35,5 +73,61 @@
               '</span>',
             '</li>'
         ].join('');
+
+        $.extend(true, Annotator.prototype, {
+            events: {
+                '.annotator-hl click': 'onHighlightClick',
+                '.annotator-viewer click': 'onNoteClick'
+            },
+
+            isFrozen: false,
+            uid: _.uniqueId(),
+
+            onHighlightClick: function (event) {
+                Annotator.Util.preventEventDefault(event);
+
+                if (!this.isFrozen) {
+                    event.stopPropagation();
+                    this.onHighlightMouseover.call(this, event);
+                }
+                Annotator.frozenSrc = this;
+                _.invoke(Annotator._instances, 'freeze');
+            },
+
+            onNoteClick: function (event) {
+                event.stopPropagation();
+                Annotator.Util.preventEventDefault(event);
+                if (!$(event.target).is('.annotator-delete')) {
+                    Annotator.frozenSrc = this;
+                    _.invoke(Annotator._instances, 'freeze');
+                }
+            },
+
+            freeze: function() {
+                if (!this.isFrozen) {
+                    // Remove default events
+                    this.removeEvents();
+                    this.viewer.element.unbind('mouseover mouseout');
+                    this.uid = _.uniqueId();
+                    $(document).on('click.edxnotes:freeze'+this.uid, this.unfreeze.bind(this));
+                    this.isFrozen = true;
+                }
+            },
+
+            unfreeze: function() {
+                if (this.isFrozen) {
+                    // Add default events
+                    this.addEvents();
+                    this.viewer.element.bind({
+                        'mouseover': this.clearViewerHideTimer,
+                        'mouseout':  this.startViewerHideTimer
+                    });
+                    this.viewer.hide();
+                    $(document).off('click.edxnotes:freeze'+this.uid);
+                    this.isFrozen = false;
+                    Annotator.frozenSrc = null;
+                }
+            }
+        });
     });
 }).call(this, define || RequireJS.define, jQuery, _);
