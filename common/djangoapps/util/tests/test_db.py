3f947434a44a9e7b6d601fc8ec18e755f4b19e8e
Usman Khalid <2200617@gmail.com>
2014-11-06 19:00:37 +0500

    Added tests.

--- common/djangoapps/util/tests/test_db.py
+++ common/djangoapps/util/tests/test_db.py
@@ -1,6 +1,7 @@
 """Tests for util.db module."""
 
 import ddt
+import sys
 import threading
 import time
 import unittest
@@ -99,3 +100,129 @@ class TransactionIsolationLevelsTestCase(TransactionTestCase):
             with commit_on_success():
                 with commit_on_success():
                     commit_on_success_with_read_committed(do_nothing)()
+
+    @ddt.data(
+        (commit_on_success_with_read_committed, IntegrityError, False, True),
+    )
+    @ddt.unpack
+    def test_concurrent_requests1(self, transaction_decorator, exception_class, created_in_1, created_in_2):
+        """
+        Test that when isolation level is set to READ COMMITTED get_or_create()
+        for the same row in concurrent requests does not raise an IntegrityError.
+        """
+
+        if connection.vendor != 'mysql':
+            raise unittest.SkipTest('Only works on MySQL.')
+
+        class RequestThread(threading.Thread):
+            """ A thread which runs a dummy view."""
+            def __init__(self, delay, **kwargs):
+                super(RequestThread, self).__init__(**kwargs)
+                self.delay = delay
+                self.status = {}
+
+            @transaction_decorator
+            def run(self):
+                """A dummy view."""
+                created = False
+                try:
+                    if self.delay > 0:
+                        time.sleep(self.delay)
+
+                    try:
+                        User.objects.get(username='student', email='student@edx.org')
+                    except User.DoesNotExist:
+                        u = User(username='student', email='student@edx.org')
+                        u.save(force_insert=True)
+                        created = True
+
+                except Exception as exception:  # pylint: disable=broad-except
+                    self.status['exception'] = exception
+                else:
+                    self.status['created'] = created
+
+                assert len(connection.transaction_state) == 1
+                time.sleep(5)
+
+        thread1 = RequestThread(delay=1)
+        thread2 = RequestThread(delay=0)
+
+        thread1.start()
+        thread2.start()
+        thread2.join()
+        thread1.join()
+
+        self.assertIsInstance(thread1.status.get('exception'), exception_class)
+
+        self.assertIsNone(thread2.status.get('exception'))
+        self.assertEqual(thread2.status.get('created'), created_in_2)
+
+
+    @ddt.data(
+        (commit_on_success_with_read_committed, type(None), False, True),
+    )
+    @ddt.unpack
+    def test_concurrent_requests2(self, transaction_decorator, exception_class, created_in_1, created_in_2):
+        """
+        Test that when isolation level is set to READ COMMITTED get_or_create()
+        for the same row in concurrent requests does not raise an IntegrityError.
+        """
+
+        if connection.vendor != 'mysql':
+            raise unittest.SkipTest('Only works on MySQL.')
+
+        class RequestThread(threading.Thread):
+            """ A thread which runs a dummy view."""
+            def __init__(self, delay, **kwargs):
+                super(RequestThread, self).__init__(**kwargs)
+                self.delay = delay
+                self.status = {}
+
+            @transaction_decorator
+            def run(self):
+                """A dummy view."""
+                created = False
+                try:
+                    if self.delay > 0:
+                        time.sleep(self.delay)
+
+                    try:
+                        User.objects.get(username='student', email='student@edx.org')
+                    except User.DoesNotExist:
+                        try:
+                            u = User(username='student', email='student@edx.org')
+                            u.save(force_insert=True)
+                            created = True
+                        except IntegrityError, e:
+                            exc_info = sys.exc_info()
+                            try:
+                                User.objects.get(username='student', email='student@edx.org')
+                            except User.DoesNotExist:
+                                # Re-raise the IntegrityError with its original traceback.
+                                raise exc_info[1], None, exc_info[2]
+                        else:
+                            pass
+
+                except Exception as exception:  # pylint: disable=broad-except
+                    self.status['exception'] = exception
+                else:
+                    self.status['created'] = created
+
+                assert len(connection.transaction_state) == 1
+                time.sleep(5)
+
+
+        thread1 = RequestThread(delay=1)
+        thread2 = RequestThread(delay=0)
+
+        thread1.start()
+        thread2.start()
+        thread2.join()
+        thread1.join()
+
+        self.assertIsInstance(thread1.status.get('exception'), exception_class)
+        self.assertEqual(thread1.status.get('created'), created_in_1)
+
+        self.assertIsNone(thread2.status.get('exception'))
+        self.assertEqual(thread2.status.get('created'), created_in_2)
+
