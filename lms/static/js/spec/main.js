d59cdb8ac2f4b4b8a527bb82a5e1916a1e578a4a
polesye <s2pak.anton@gmail.com>
2014-11-18 14:51:37 +0200

    Fix conflicts.

--- lms/static/js/spec/main.js
+++ lms/static/js/spec/main.js
@@ -389,7 +389,6 @@
         'lms/include/js/spec/staff_debug_actions_spec.js',
         'lms/include/js/spec/views/notification_spec.js',
         'lms/include/js/spec/dashboard/donation.js',
-<<<<<<< HEAD
         'lms/include/js/spec/student_account/account_spec.js',
         'lms/include/js/spec/student_account/access_spec.js',
         'lms/include/js/spec/student_account/login_spec.js',
@@ -399,12 +398,10 @@
         'lms/include/js/spec/student_account/emailoptin_spec.js',
         'lms/include/js/spec/student_account/shoppingcart_spec.js',
         'lms/include/js/spec/student_profile/profile_spec.js'
-=======
         'lms/include/js/spec/student_account/account.js',
         'lms/include/js/spec/student_profile/profile.js',
         'lms/include/js/spec/edxnotes/logger_spec.js',
         'lms/include/js/spec/edxnotes/notes_spec.js'
->>>>>>> TNL-655: Add/Edit/Remove notes.
     ]);
 
 }).call(this, requirejs, define);
