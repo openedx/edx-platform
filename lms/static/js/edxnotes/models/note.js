3293ec12b2b0be63ba77ca457448a8f64d238ec3
polesye <s2pak.anton@gmail.com>
2014-11-19 16:33:28 +0200

    Fix sorting and unit name.

--- lms/static/js/edxnotes/models/note.js
+++ lms/static/js/edxnotes/models/note.js
@@ -16,45 +16,6 @@
                     'url': null
                 },
                 'ranges': []
-            },
-
-            timeFormat: 'hh:mmtt', // For example: 12:59PM
-            dateFormat: 'MMMM dd, yyyy', // For example: November 11, 2014
-
-            parse: function (attributes) {
-                if (attributes.updated) {
-                    attributes.updated = new Date(attributes.updated);
-                }
-                if (attributes.created) {
-                    attributes.created = new Date(attributes.created);
-                }
-                return attributes;
-            },
-
-            /**
-             * Returns date string in the following format:
-             * `November 11, 2014 at 12:59PM`.
-             * @param {Date} datetime Date used to convert to date string.
-             * @return {String}
-             */
-            getDateTime: function (datetime) {
-                var datetimeString = gettext('%(date)s at %(time)s');
-
-                return interpolate(datetimeString, {
-                    date: datetime.toString(this.dateFormat),
-                    time: datetime.toString(this.timeFormat)
-                }, true);
-            },
-
-            /**
-             * Returns context for the views.
-             * @return {Object}
-             */
-            toContext: function () {
-                return $.extend(true, {}, this.attributes, {
-                    created: this.getDateTime(this.get('created')),
-                    updated: this.getDateTime(this.get('updated'))
-                });
             }
         });
 
