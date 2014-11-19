6187565f3f5eb95f1519e4a30cd76e90b6cc7595
polesye <s2pak.anton@gmail.com>
2014-11-19 16:33:28 +0200

    Fix sorting and unit name.

--- lms/djangoapps/edxnotes/tests.py
+++ lms/djangoapps/edxnotes/tests.py
@@ -4,6 +4,7 @@ Tests for the EdxNotes app.
 import json
 from mock import patch, MagicMock
 from unittest import skipUnless
+from datetime import datetime
 from edxnotes.decorators import edxnotes
 from django.conf import settings
 from django.test import TestCase
@@ -99,6 +100,14 @@ class EdxNotesHelpersTest(TestCase):
         self.html_module_2 = ItemFactory.create(category='html', parent_location=self.vertical.location)
         self.user = UserFactory.create(username="Bob", email="bob@example.com", password="edx")
         self.client.login(username=self.user.username, password="edx")
+        self.datetimes = [
+            datetime(2014, 11, 19, 8, 5, 16, 00000).isoformat(),
+            datetime(2014, 11, 19, 8, 6, 16, 00000).isoformat(),
+        ]
+        self.datetimes_str = [
+            "Nov 19, 2014 at 08:05 UTC",
+            "Nov 19, 2014 at 08:06 UTC",
+        ]
 
     def _get_jump_to_url(self, vertical):
         """
@@ -109,29 +118,38 @@ class EdxNotesHelpersTest(TestCase):
             'location': vertical.location.to_deprecated_string(),
         })
 
-    def _get_dummy_model(self, module):
+    def _get_dummy_model(self, module, fields_to_update=None):
         """
         Returns dummy model for the `module`.
         """
-        return {
+        dummy_model = {
             u'quote': u'quote text',
             u'text': u'text',
             u'usage_id': unicode(module.location),
+            u'updated': self.datetimes[0],
         }
+        if fields_to_update:
+            dummy_model.update(fields_to_update)
+        return dummy_model
 
-    def _get_dummy_note(self, module, vertical):
+    def _get_dummy_note(self, module, vertical, fields_to_update=None):
         """
         Returns dummy note for the `module`.
         """
-        return {
+        dummy_note = {
             u'quote': u'quote text',
             u'text': u'text',
             u'unit': {
                 u'url': self._get_jump_to_url(vertical),
-                u'display_name': module.display_name_with_default,
+                u'display_name': vertical.display_name_with_default,
             },
             u'usage_id': unicode(module.location),
+            u'updated': self.datetimes_str[0],
         }
+
+        if fields_to_update:
+            dummy_note.update(fields_to_update)
+        return dummy_note
 
     def test_edxnotes_not_enabled(self):
         """
@@ -177,12 +195,13 @@ class EdxNotesHelpersTest(TestCase):
         """
         mock_get.return_value.content = json.dumps([
             self._get_dummy_model(self.html_module_1),
-            self._get_dummy_model(self.html_module_2),
+            self._get_dummy_model(self.html_module_2, fields_to_update={"updated": self.datetimes[1]}),
         ])
+
         self.assertItemsEqual([
+            self._get_dummy_note(self.html_module_2, self.vertical, fields_to_update={"updated": self.datetimes_str[1]}),
             self._get_dummy_note(self.html_module_1, self.vertical),
-            self._get_dummy_note(self.html_module_2, self.vertical),
-        ], helpers.get_notes(self.user, self.course))
+        ], json.loads(helpers.get_notes(self.user, self.course)))
 
     @patch("edxnotes.helpers.requests.get")
     def test_get_notes_json_error(self, mock_get):
@@ -190,7 +209,7 @@ class EdxNotesHelpersTest(TestCase):
         Tests the result if incorrect json us received.
         """
         mock_get.return_value.content = "Error"
-        self.assertItemsEqual([], helpers.get_notes(self.user, self.course))
+        self.assertIsNone(helpers.get_notes(self.user, self.course))
 
     @patch("edxnotes.helpers.requests.get")
     def test_get_notes_empty_collection(self, mock_get):
@@ -198,7 +217,7 @@ class EdxNotesHelpersTest(TestCase):
         Tests the result if an empty collection is received.
         """
         mock_get.return_value.content = json.dumps([])
-        self.assertItemsEqual([], helpers.get_notes(self.user, self.course))
+        self.assertIsNone(helpers.get_notes(self.user, self.course))
 
     @patch("edxnotes.helpers.requests.get")
     def test_get_notes_no_item(self, mock_get):
@@ -214,7 +233,7 @@ class EdxNotesHelpersTest(TestCase):
 
         self.assertItemsEqual([
             self._get_dummy_note(self.html_module_1, self.vertical),
-        ], helpers.get_notes(self.user, self.course))
+        ], json.loads(helpers.get_notes(self.user, self.course)))
 
     @patch("edxnotes.helpers.requests.get")
     def test_get_notes_has_access(self, mock_get):
@@ -230,7 +249,7 @@ class EdxNotesHelpersTest(TestCase):
         ])
         self.assertItemsEqual([
             self._get_dummy_note(self.html_module_1, self.vertical),
-        ], helpers.get_notes(self.user, self.course))
+        ], json.loads(helpers.get_notes(self.user, self.course)))
 
     def test_get_parent(self):
         """
@@ -255,24 +274,31 @@ class EdxNotesHelpersTest(TestCase):
         store.get_item.side_effect = ItemNotFoundError
         self.assertEqual(helpers.get_parent(store, self.html_module_1.location), None)
 
-    def test_get_parent_url(self):
+    def test_get_parent_context(self):
         """
-        Tests `test_get_parent_url` method for the successful result.
+        Tests `test_get_parent_context` method for the successful result.
         """
-        self.assertEqual(
-            helpers.get_parent_url(self.course, modulestore(), self.html_module_1.location),
-            self._get_jump_to_url(self.vertical)
+        self.assertDictEqual(
+            helpers.get_parent_context(self.course, modulestore(), self.html_module_1.location),
+            {
+                u"url": self._get_jump_to_url(self.vertical),
+                u"display_name": self.vertical.display_name_with_default,
+            }
+
         )
 
     # pylint: disable=unused-argument
     @patch('edxnotes.helpers.get_parent', return_value=None)
-    def test_get_parent_url_no_parent(self, mock_get_parent):
+    def test_get_parent_context_no_parent(self, mock_get_parent):
         """
         Tests the result if parent module is not found.
         """
         self.assertEqual(
-            helpers.get_parent_url(self.course, modulestore(), self.html_module_1.location),
-            None
+            helpers.get_parent_context(self.course, modulestore(), self.html_module_1.location),
+            {
+                u"url": None,
+                u"display_name": None,
+            }
         )
 
 
