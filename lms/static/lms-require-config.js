f5e649966309a1840e81bf424a82936b4eee24e1
AlasdairSwan <alasdair_swan@hotmail.com>
2014-11-20 11:30:53 -0500

    UPdated config

--- lms/static/lms-require-config.js
+++ lms/static/lms-require-config.js
@@ -1,15 +1,21 @@
 require.config({
-    baseUrl: "",
+    baseUrl: '',
     waitSeconds: 60,
     paths: {
-        jquery:         '../../common/static/js/vendor/jquery.min',
-        underscore:     '../../common/static/js/vendor/underscore-min',
-        backbone:       '../../common/static/js/vendor/backbone-min'
+        'backbone':             '../../common/static/js/vendor/backbone-min',
+        'gettext':              '/i18n',
+        'jquery':               '../../common/static/js/vendor/jquery.min',
+        'underscore':           '../../common/static/js/vendor/underscore-min'
+        'underscore.string':    '../../common/static/js/vendor/underscore.string.min'
     },
     shim: {
         backbone: {
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
+        },
+
+        gettext: {
+            exports: 'gettext'
         },
 
         underscore: {
