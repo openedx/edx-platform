bf12b12dc5cda121def3eea54c001bd629734484
AlasdairSwan <alasdair_swan@hotmail.com>
2014-11-20 14:45:00 -0500

    Requirification process continues

--- lms/static/js/student_account/shoppingcart.js
+++ lms/static/js/student_account/shoppingcart.js
@@ -2,48 +2,48 @@
 * Use the shopping cart to purchase courses.
 */
 
-var edx = edx || {};
+define([
+        'jquery'
+    ],
+    function( $ ) {
+        'use strict';
 
-(function($) {
-    'use strict';
+        var ShoppingCartInterface = {
 
-    edx.student = edx.student || {};
-    edx.student.account = edx.student.account || {};
+            urls: {
+                viewCart: "/shoppingcart/",
+                addCourse: "/shoppingcart/add/course/"
+            },
 
-    edx.student.account.ShoppingCartInterface = {
+            headers: {
+                'X-CSRFToken': $.cookie('csrftoken')
+            },
 
-        urls: {
-            viewCart: "/shoppingcart/",
-            addCourse: "/shoppingcart/add/course/"
-        },
+            /**
+             * Add a course to a cart, then redirect to the view cart page.
+             * @param {string} courseId The slash-separated course ID to add to the cart.
+             */
+            addCourseToCart: function( courseId ) {
+                $.ajax({
+                    url: this.urls.addCourse + courseId + "/",
+                    type: 'POST',
+                    data: {},
+                    headers: this.headers,
+                    context: this
+                }).always(function() {
+                    this.redirect( this.urls.viewCart );
+                });
+            },
 
-        headers: {
-            'X-CSRFToken': $.cookie('csrftoken')
-        },
+            /**
+             * Redirect to a URL.  Mainly useful for mocking out in tests.
+             * @param  {string} url The URL to redirect to.
+             */
+            redirect: function( url ) {
+                window.location.href = url;
+            }
+        };
 
-        /**
-         * Add a course to a cart, then redirect to the view cart page.
-         * @param {string} courseId The slash-separated course ID to add to the cart.
-         */
-        addCourseToCart: function( courseId ) {
-            $.ajax({
-                url: this.urls.addCourse + courseId + "/",
-                type: 'POST',
-                data: {},
-                headers: this.headers,
-                context: this
-            }).always(function() {
-                this.redirect( this.urls.viewCart );
-            });
-        },
-
-        /**
-         * Redirect to a URL.  Mainly useful for mocking out in tests.
-         * @param  {string} url The URL to redirect to.
-         */
-        redirect: function( url ) {
-            window.location.href = url;
-        }
-    };
-
-})(jQuery);
+        return ShoppingCartInterface;
+    }
+);
