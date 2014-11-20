eded83b519a6d1a73a11e8e0d5d42a9e4b327c5a
Bertrand Marron <bertrand.marron@ionisx.com>
2014-11-20 10:49:56 -0500

    Add grunt and update package.json

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        watch: {
+        },
+
+        sass: {
+            lms: {
+                // TODO
+            }
+        }
+    });
+
+    grunt.registerTask('default', [
+    ]);
+};
