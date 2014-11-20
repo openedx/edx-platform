f948f542ead63861e3b70c04ec63313ebb0322e6
Bertrand Marron <bertrand.marron@ionisx.com>
2014-11-20 16:58:56 -0500

    Install bi-app using bower
    Remove from both applications static files.

--- common/lib/xmodule/xmodule/static_content.py
+++ common/lib/xmodule/xmodule/static_content.py
@@ -98,6 +98,7 @@ def _write_styles(selector, output_root, classes):
     module_styles_lines = []
     module_styles_lines.append("@import 'bourbon/dist/bourbon';")
     module_styles_lines.append("@import 'bourbon/dist/addons/button';")
+    module_styles_lines.append("@import 'bi-app/bi-app-ltr';")
     module_styles_lines.append("@import 'assets/anims';")
     for class_, fragment_names in css_imports.items():
         module_styles_lines.append("""{selector}.xmodule_{class_} {{""".format(
