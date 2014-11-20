9da54f5c6eaa98280d0a19a687d88685668f7517
AlasdairSwan <alasdair_swan@hotmail.com>
2014-11-20 12:03:01 -0500

    updated vendor paths

--- lms/static/lms-require-config.js
+++ lms/static/lms-require-config.js
@@ -2,11 +2,11 @@ require.config({
     baseUrl: '',
     waitSeconds: 60,
     paths: {
-        'backbone':             '../../common/static/js/vendor/backbone-min',
+        'backbone':             'js/vendor/backbone-min',
         'gettext':              '/i18n',
-        'jquery':               '../../common/static/js/vendor/jquery.min',
-        'underscore':           '../../common/static/js/vendor/underscore-min',
-        'underscore.string':    '../../common/static/js/vendor/underscore.string.min'
+        'jquery':               'js/vendor/jquery.min',
+        'underscore':           'js/vendor/underscore-min',
+        'underscore.string':    'js/vendor/underscore.string.min'
     },
     shim: {
         backbone: {
