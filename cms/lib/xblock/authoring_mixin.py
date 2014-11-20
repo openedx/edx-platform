926374f598568fb211bb4a575a8e33776b523c84
stv <stv@stanford.edu>
2014-11-20 11:06:11 -0500

    Stub implementation for XBlock Studio editing

---
+++ cms/lib/xblock/authoring_mixin.py
@@ -0,0 +1,33 @@
+"""
+TODO: something smart here
+"""
+
+from xblock.core import XBlockMixin
+
+
+class AuthoringMixin(XBlockMixin):
+    """
+    TODO:
+    """
+
+    editor_tabs = [
+        # list things here
+    ]
+
+    def save_editor(self, context=None):
+        """
+        TODO:
+        """
+        pass
+
+    def settings_tab_view(self, context=None):
+        """
+        TODO:
+        """
+        pass
+
+    def xml_tab_view(self, context=None):
+        """
+        TODO:
+        """
+        pass
