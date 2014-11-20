13642196443c7321cf99c0ae206397fdf17e0cb6
AlasdairSwan <alasdair_swan@hotmail.com>
2014-11-20 13:36:32 -0500

    Requireified models

--- lms/static/js/student_account/models/RegisterModel.js
+++ lms/static/js/student_account/models/RegisterModel.js
@@ -1,64 +1,64 @@
-var edx = edx || {};
+define([
+        'backbone',
+        'jquery'
+    ],
+    function( Backbone, $ ) {
+        'use strict';
 
-(function($, Backbone) {
-    'use strict';
+        return Backbone.Model.extend({
 
-    edx.student = edx.student || {};
-    edx.student.account = edx.student.account || {};
+            defaults: {
+                email: '',
+                name: '',
+                username: '',
+                password: '',
+                level_of_education: '',
+                gender: '',
+                year_of_birth: '',
+                mailing_address: '',
+                goals: '',
+            },
 
-    edx.student.account.RegisterModel = Backbone.Model.extend({
+            ajaxType: '',
 
-        defaults: {
-            email: '',
-            name: '',
-            username: '',
-            password: '',
-            level_of_education: '',
-            gender: '',
-            year_of_birth: '',
-            mailing_address: '',
-            goals: '',
-        },
+            urlRoot: '',
 
-        ajaxType: '',
+            initialize: function( attributes, options ) {
+                this.ajaxType = options.method;
+                this.urlRoot = options.url;
+            },
 
-        urlRoot: '',
+            sync: function(method, model) {
+                var headers = { 'X-CSRFToken': $.cookie('csrftoken') },
+                    data = {},
+                    analytics,
+                    courseId = $.url( '?course_id' );
 
-        initialize: function( attributes, options ) {
-            this.ajaxType = options.method;
-            this.urlRoot = options.url;
-        },
+                // If there is a course ID in the query string param,
+                // send that to the server as well so it can be included
+                // in analytics events.
+                if ( courseId ) {
+                    analytics = JSON.stringify({
+                        enroll_course_id: decodeURIComponent( courseId )
+                    });
+                }
 
-        sync: function(method, model) {
-            var headers = { 'X-CSRFToken': $.cookie('csrftoken') },
-                data = {},
-                analytics,
-                courseId = $.url( '?course_id' );
+                // Include all form fields and analytics info in the data sent to the server
+                $.extend( data, model.attributes, { analytics: analytics });
 
-            // If there is a course ID in the query string param,
-            // send that to the server as well so it can be included
-            // in analytics events.
-            if ( courseId ) {
-                analytics = JSON.stringify({
-                    enroll_course_id: decodeURIComponent( courseId )
+                $.ajax({
+                    url: model.urlRoot,
+                    type: model.ajaxType,
+                    data: data,
+                    headers: headers,
+                    success: function() {
+                        model.trigger('sync');
+                    },
+                    error: function( error ) {
+                        model.trigger('error', error);
+                    }
                 });
             }
-
-            // Include all form fields and analytics info in the data sent to the server
-            $.extend( data, model.attributes, { analytics: analytics });
-
-            $.ajax({
-                url: model.urlRoot,
-                type: model.ajaxType,
-                data: data,
-                headers: headers,
-                success: function() {
-                    model.trigger('sync');
-                },
-                error: function( error ) {
-                    model.trigger('error', error);
-                }
-            });
-        }
-    });
-})(jQuery, Backbone);
+        });
+    }
+);
