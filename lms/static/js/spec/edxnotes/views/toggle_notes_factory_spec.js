a9d67a21216ac492a0f4aed73b25cae1f45f8c33
Jean-Michel Claus <jmc@edx.org>
2014-11-22 14:50:41 +0100

    TNL-661: Toggle all notes

---
+++ lms/static/js/spec/edxnotes/views/toggle_notes_factory_spec.js
@@ -0,0 +1,84 @@
+define([
+    'jquery', 'annotator', 'js/common_helpers/ajax_helpers', 'js/edxnotes/views/visibility_decorator',
+    'js/edxnotes/views/toggle_notes_factory', 'js/spec/edxnotes/custom_matchers', 'js/spec/edxnotes/base64',
+    'jasmine-jquery'
+], function(
+    $, Annotator, AjaxHelpers, VisibilityDecorator, ToggleNotesFactory, customMatchers, base64
+) {
+    'use strict';
+    describe('EdxNotes ToggleNotesFactory', function() {
+        var params = {
+            endpoint: '/test_endpoint',
+            user: 'a user',
+            usageId : 'an usage',
+            courseId: 'a course',
+            token: base64.makeToken(),
+            tokenUrl: '/test_token_url'
+        };
+
+        beforeEach(function() {
+            customMatchers(this);
+            loadFixtures(
+                'js/fixtures/edxnotes/edxnotes_wrapper.html',
+                'js/fixtures/edxnotes/toggle_notes.html'
+            );
+            VisibilityDecorator.factory(
+                document.getElementById('edx-notes-wrapper-123'), params, true
+            );
+            VisibilityDecorator.factory(
+                document.getElementById('edx-notes-wrapper-456'), params, true
+            );
+            this.toggleNotes = ToggleNotesFactory(true, '/test_url');
+            this.button = $('.action-toggle-notes');
+            this.icon = this.button.find('.checkbox-icon');
+        });
+
+        afterEach(function () {
+            VisibilityDecorator._setVisibility(null);
+            _.invoke(Annotator._instances, 'destroy');
+        });
+
+        it('can toggle notes', function() {
+            var requests = AjaxHelpers.requests(this);
+
+            expect(this.button).not.toHaveClass('is-disabled');
+            expect(this.icon).toHaveClass('icon-check');
+            expect(this.icon).not.toHaveClass('icon-check-empty');
+
+            this.button.click();
+            expect(this.icon).toHaveClass('icon-check-empty');
+            expect(this.icon).not.toHaveClass('icon-check');
+            expect(Annotator._instances).toHaveLength(0);
+
+            AjaxHelpers.expectJsonRequest(requests, 'PUT', '/test_url', {
+                'visibility': false
+            });
+            AjaxHelpers.respondWithJson(requests, {});
+
+            this.button.click();
+            expect(this.icon).toHaveClass('icon-check');
+            expect(this.icon).not.toHaveClass('icon-check-empty');
+            expect(Annotator._instances).toHaveLength(2);
+
+            AjaxHelpers.expectJsonRequest(requests, 'PUT', '/test_url', {
+                'visibility': true
+            });
+            AjaxHelpers.respondWithJson(requests, {});
+        });
+
+        it('can handle errors', function() {
+            var requests = AjaxHelpers.requests(this),
+                errorContainer = $('.edx-notes-visibility-error');
+
+            this.button.click();
+            AjaxHelpers.respondWithError(requests);
+            expect(errorContainer).toContainText(
+                'Cannot save your state. This may be happening because of an error with our server or your internet connection. Try refreshing the page or making sure you are online.'
+            );
+
+            this.button.click();
+            AjaxHelpers.respondWithJson(requests, {});
+            expect(errorContainer).toBeEmpty();
+        });
+    });
+});
