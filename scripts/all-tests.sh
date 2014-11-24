75eca92a028b6ae60e53407762efbed41f37364b
stv <stv@stanford.edu>
2014-11-24 17:06:06 -0500

    Lower PyLint threshold to 4600 violations

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -56,7 +56,7 @@ set -e
 ###############################################################################
 
 # Violations thresholds for failing the build
-PYLINT_THRESHOLD=4725
+PYLINT_THRESHOLD=4600
 PEP8_THRESHOLD=150
 
 source $HOME/jenkins_env
