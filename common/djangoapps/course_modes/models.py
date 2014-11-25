464dfcfabc1671f1fe7a0e5e706ee1c623e356a8
Will Daly <will@edx.org>
2014-11-25 07:20:04 -0500

    Show student verification status on the dashboard.

--- common/djangoapps/course_modes/models.py
+++ common/djangoapps/course_modes/models.py
@@ -5,7 +5,7 @@ import pytz
 from datetime import datetime
 
 from django.db import models
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from django.utils.translation import ugettext_lazy as _
 from django.db.models import Q
 
@@ -67,6 +67,70 @@ class CourseMode(models.Model):
         unique_together = ('course_id', 'mode_slug', 'currency')
 
     @classmethod
+    def all_modes_for_courses(cls, course_id_list):
+        """Find all modes for a list of course IDs, including expired modes.
+
+        Courses that do not have a course mode will be given a default mode.
+
+        Arguments:
+            course_id_list (list): List of `CourseKey`s
+
+        Returns:
+            dict mapping `CourseKey` to lists of `Mode`
+
+        """
+        modes_by_course = defaultdict(list)
+        for mode in cls.objects.filter(course_id__in=course_id_list):
+            modes_by_course[mode.course_id].append(
+                Mode(
+                    mode.mode_slug,
+                    mode.mode_display_name,
+                    mode.min_price,
+                    mode.suggested_prices,
+                    mode.currency,
+                    mode.expiration_datetime,
+                    mode.description
+                )
+            )
+
+        # Assign default modes if nothing available in the database
+        missing_courses = set(course_id_list) - set(modes_by_course.keys())
+        for course_id in missing_courses:
+            modes_by_course[course_id] = [cls.DEFAULT_MODE]
+
+        return modes_by_course
+
+    @classmethod
+    def all_and_unexpired_modes_for_courses(cls, course_id_list):
+        """Retrieve course modes for a list of courses.
+
+        To reduce the number of database queries, this function
+        loads *all* course modes, then creates a second list
+        of unexpired course modes.
+
+        Arguments:
+            course_id_list (list of `CourseKey`): List of courses for which
+                to retrieve course modes.
+
+        Returns:
+            Tuple of `(all_course_modes, unexpired_course_modes)`, where
+            the first is a list of *all* `Mode`s (including expired ones),
+            and the second is a list of only unexpired `Mode`s.
+
+        """
+        now = datetime.now(pytz.UTC)
+        all_modes = cls.all_modes_for_courses(course_id_list)
+        unexpired_modes = {
+            course_id: [
+                mode for mode in modes
+                if mode.expiration_datetime is None or mode.expiration_datetime >= now
+            ]
+            for course_id, modes in all_modes.iteritems()
+        }
+
+        return (all_modes, unexpired_modes)
+
+    @classmethod
     def modes_for_course(cls, course_id):
         """
         Returns a list of the non-expired modes for a given course id
@@ -91,23 +155,48 @@ class CourseMode(models.Model):
         return modes
 
     @classmethod
-    def modes_for_course_dict(cls, course_id):
+    def modes_for_course_dict(cls, course_id, modes=None):
+        """Returns the non-expired modes for a particular course.
+
+        Arguments:
+            course_id (CourseKey): Search for course modes for this course.
+
+        Keyword Arguments:
+            modes (list of `Mode`): If provided, search through this list
+                of course modes.  This can be used to avoid an additional
+                database query if you have already loaded the modes list.
+
+        Returns:
+            dict: Keys are mode slugs, values are lists of `Mode` namedtuples.
+
         """
-        Returns the non-expired modes for a particular course as a
-        dictionary with the mode slug as the key
-        """
-        return {mode.slug: mode for mode in cls.modes_for_course(course_id)}
+        if modes is None:
+            modes = cls.modes_for_course(course_id)
+        return {mode.slug: mode for mode in modes}
 
     @classmethod
-    def mode_for_course(cls, course_id, mode_slug):
-        """
-        Returns the mode for the course corresponding to mode_slug.
+    def mode_for_course(cls, course_id, mode_slug, modes=None):
+        """Returns the mode for the course corresponding to mode_slug.
 
         Returns only non-expired modes.
 
         If this particular mode is not set for the course, returns None
+
+        Arguments:
+            course_id (CourseKey): Search for course modes for this course.
+            mode_slug (str): Search for modes with this slug.
+
+        Keyword Arguments:
+            modes (list of `Mode`): If provided, search through this list
+                of course modes.  This can be used to avoid an additional
+                database query if you have already loaded the modes list.
+
+        Returns:
+            Mode
+
         """
-        modes = cls.modes_for_course(course_id)
+        if modes is None:
+            modes = cls.modes_for_course(course_id)
 
         matched = [m for m in modes if m.slug == mode_slug]
         if matched:
@@ -116,15 +205,28 @@ class CourseMode(models.Model):
             return None
 
     @classmethod
-    def verified_mode_for_course(cls, course_id):
-        """
-        Since we have two separate modes that can go through the verify flow,
+    def verified_mode_for_course(cls, course_id, modes=None):
+        """Find a verified mode for a particular course.
+
+        Since we have multiple modes that can go through the verify flow,
         we want to be able to select the 'correct' verified mode for a given course.
 
         Currently, we prefer to return the professional mode over the verified one
         if both exist for the given course.
+
+        Arguments:
+            course_id (CourseKey): Search for course modes for this course.
+
+        Keyword Arguments:
+            modes (list of `Mode`): If provided, search through this list
+                of course modes.  This can be used to avoid an additional
+                database query if you have already loaded the modes list.
+
+        Returns:
+            Mode or None
+
         """
-        modes_dict = cls.modes_for_course_dict(course_id)
+        modes_dict = cls.modes_for_course_dict(course_id, modes=modes)
         verified_mode = modes_dict.get('verified', None)
         professional_mode = modes_dict.get('professional', None)
         # we prefer professional over verify
--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -1,5 +1,7 @@
 """Helpers for the student app. """
 import time
+from datetime import datetime
+from pytz import UTC
 from django.utils.http import cookie_date
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -9,6 +11,7 @@ from third_party_auth import (  # pylint: disable=W0611
     pipeline, provider,
     is_enabled as third_party_auth_enabled
 )
+from verify_student.models import SoftwareSecurePhotoVerification  # pylint: disable=F0401
 
 
 def auth_pipeline_urls(auth_entry, redirect_url=None, course_id=None):
@@ -111,3 +114,118 @@ def set_logged_in_cookie(request, response):
 def is_logged_in_cookie_set(request):
     """Check whether the request has the logged in cookie set. """
     return settings.EDXMKTG_COOKIE_NAME in request.COOKIES
+
+
+# Enumeration of per-course verification statuses
+# we display on the student dashboard.
+VERIFY_STATUS_NEED_TO_VERIFY = "verify_need_to_verify"
+VERIFY_STATUS_SUBMITTED = "verify_submitted"
+VERIFY_STATUS_APPROVED = "verify_approved"
+VERIFY_STATUS_MISSED_DEADLINE = "verify_missed_deadline"
+
+
+def check_verify_status_by_course(user, course_enrollment_pairs, all_course_modes):
+    """Determine the per-course verification statuses for a given user.
+
+    The possible statuses are:
+        * VERIFY_STATUS_NEED_TO_VERIFY: The student has not yet submitted photos for verification.
+        * VERIFY_STATUS_SUBMITTED: The student has submitted photos for verification,
+          but has have not yet been approved.
+        * VERIFY_STATUS_APPROVED: The student has been successfully verified.
+        * VERIFY_STATUS_MISSED_DEADLINE: The student did not submit photos within the course's deadline.
+
+    It is is also possible that a course does NOT have a verification status if:
+        * The user is not enrolled in a verified mode, meaning that the user didn't pay.
+        * The course does not offer a verified mode.
+        * The user submitted photos but an error occurred while verifying them.
+        * The user submitted photos but the verification was denied.
+
+    In the last two cases, we rely on messages in the sidebar rather than displaying
+    messages for each course.
+
+    Arguments:
+        user (User): The currently logged-in user.
+        course_enrollment_pairs (list): The courses the user is enrolled in.
+            The list should contain tuples of `(Course, CourseEnrollment)`.
+        all_course_modes (list): List of all course modes for the student's enrolled courses,
+            including modes that have expired.
+
+    Returns:
+        dict: Mapping of course keys verification status dictionaries.
+            If no verification status is applicable to a course, it will not
+            be included in the dictionary.
+            The dictionaries have these keys:
+                * status (str): One of the enumerated status codes.
+                * days_until_deadline (int): Number of days until the verification deadline.
+                * verification_good_until (str): Date string for the verification expiration date.
+    """
+
+    status_by_course = {}
+
+    # Retrieve all verifications for the user, sorted in descending
+    # order by submission datetime
+    verifications = SoftwareSecurePhotoVerification.objects.filter(user=user)
+
+    for course, enrollment in course_enrollment_pairs:
+
+        # Get the verified mode (if any) for this course
+        # We pass in the course modes we have already loaded to avoid
+        # another database hit, as well as to ensure that expired
+        # course modes are included in the search.
+        verified_mode = CourseMode.verified_mode_for_course(
+            course.id,
+            modes=all_course_modes[course.id]
+        )
+
+        # If no verified mode has ever been offered, or the user hasn't enrolled
+        # as verified, then the course won't display state related to its
+        # verification status.
+        if verified_mode is not None and enrollment.mode in CourseMode.VERIFIED_MODES:
+            deadline = verified_mode.expiration_datetime
+            relevant_verification = SoftwareSecurePhotoVerification.verification_for_datetime(deadline, verifications)
+
+            # By default, don't show any status related to verification
+            status = None
+
+            # Check whether the user was approved or is awaiting approval
+            if relevant_verification is not None:
+                if relevant_verification.status == "approved":
+                    status = VERIFY_STATUS_APPROVED
+                elif relevant_verification.status == "submitted":
+                    status = VERIFY_STATUS_SUBMITTED
+
+            # If the user didn't submit at all, then tell them they need to verify
+            # If the deadline has already passed, then tell them they missed it.
+            # If they submitted but something went wrong (error or denied),
+            # then don't show any messaging next to the course, since we already
+            # show messages related to this on the left sidebar.
+            submitted = (
+                relevant_verification is not None and
+                relevant_verification.status not in ["created", "ready"]
+            )
+            if status is None and not submitted:
+                if deadline is None or deadline > datetime.now(UTC):
+                    status = VERIFY_STATUS_NEED_TO_VERIFY
+                else:
+                    status = VERIFY_STATUS_MISSED_DEADLINE
+
+            # Set the status for the course only if we're displaying some kind of message
+            # Otherwise, leave the course out of the dictionary.
+            if status is not None:
+                days_until_deadline = None
+                verification_good_until = None
+
+                now = datetime.now(UTC)
+                if deadline is not None and deadline > now:
+                    days_until_deadline = (deadline - now).days
+
+                if relevant_verification is not None:
+                    verification_good_until = relevant_verification.expiration_datetime.strftime("%m/%d/%Y")
+
+                status_by_course[course.id] = {
+                    'status': status,
+                    'days_until_deadline': days_until_deadline,
+                    'verification_good_until': verification_good_until
+                }
+
+    return status_by_course
