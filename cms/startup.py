befd9ec177dcc099b0bd2bed913e6b9077cf01c4
Sarina Canelake <sarina@edx.org>
2014-11-30 18:07:06 -0500

    s/pylint: disable=W0104/pylint: disable=pointless-statement/

--- cms/startup.py
+++ cms/startup.py
@@ -6,7 +6,7 @@ import logging
 from django.conf import settings
 
 # Force settings to run so that the python path is modified
-settings.INSTALLED_APPS  # pylint: disable=W0104
+settings.INSTALLED_APPS  # pylint: disable=pointless-statement
 
 from django_startup import autostartup
 from edx_notifications import startup
