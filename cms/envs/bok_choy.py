b8c16d5ed78b9d27b565e0a4ffee1f805f771dbf
Sarina Canelake <sarina@edx.org>
2014-11-30 00:20:22 -0500

    s/pylint: disable=E1120/pylint: disable=no-value-for-parameter/

--- cms/envs/bok_choy.py
+++ cms/envs/bok_choy.py
@@ -13,7 +13,7 @@ from path import path
 # This is a convenience for ensuring (a) that we can consistently find the files
 # and (b) that the files are the same in Jenkins as in local dev.
 os.environ['SERVICE_VARIANT'] = 'bok_choy'
-os.environ['CONFIG_ROOT'] = path(__file__).abspath().dirname()  # pylint: disable=E1120
+os.environ['CONFIG_ROOT'] = path(__file__).abspath().dirname()  # pylint: disable=no-value-for-parameter
 
 from .aws import *  # pylint: disable=W0401, W0614
 
@@ -23,7 +23,7 @@ from .aws import *  # pylint: disable=W0401, W0614
 INSTALLED_APPS += ('django_extensions',)
 
 # Redirect to the test_root folder within the repo
-TEST_ROOT = CONFIG_ROOT.dirname().dirname() / "test_root"  # pylint: disable=E1120
+TEST_ROOT = CONFIG_ROOT.dirname().dirname() / "test_root"  # pylint: disable=no-value-for-parameter
 GITHUB_REPO_ROOT = (TEST_ROOT / "data").abspath()
 LOG_DIR = (TEST_ROOT / "log").abspath()
 
@@ -31,7 +31,7 @@ LOG_DIR = (TEST_ROOT / "log").abspath()
 update_module_store_settings(
     MODULESTORE,
     module_store_options={
-        'fs_root': (TEST_ROOT / "data").abspath(),  # pylint: disable=E1120
+        'fs_root': (TEST_ROOT / "data").abspath(),  # pylint: disable=no-value-for-parameter
     },
     xml_store_options={
         'data_dir': (TEST_ROOT / "data").abspath(),
