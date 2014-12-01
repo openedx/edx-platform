861e91d9af41099e47c057421e4d9a163d04f58c
Tim Babych <tim.babych@gmail.com>
2014-12-01 20:36:13 +0200

    TNL-782 use JWT ID-Token for authentication annotation requests

--- lms/djangoapps/edxnotes/decorators.py
+++ lms/djangoapps/edxnotes/decorators.py
@@ -1,14 +1,15 @@
 """
 Decorators related to edXNotes.
 """
+from django.conf import settings
 from edxnotes.helpers import (
     get_endpoint,
-    get_token,
+    get_id_token,
+    get_token_url,
     generate_uid,
     is_feature_enabled,
 )
 from edxmako.shortcuts import render_to_string
-from django.conf import settings
 
 
 def edxnotes(cls):
@@ -21,7 +22,7 @@ def edxnotes(cls):
         """
         Returns raw html for the component.
         """
-        is_studio = getattr(self.system, 'is_author_mode', False)
+        is_studio = getattr(self.system, "is_author_mode", False)
         course = self.descriptor.runtime.modulestore.get_course(self.runtime.course_id)
 
         # Must be disabled in Studio or depends on the feature flag/advanced
@@ -29,16 +30,17 @@ def edxnotes(cls):
         if is_studio or not is_feature_enabled(course):
             return original_get_html(self, *args, **kwargs)
         else:
-            return render_to_string('edxnotes_wrapper.html', {
-                'content': original_get_html(self, *args, **kwargs),
-                'uid': generate_uid(),
-                'params': {
+            return render_to_string("edxnotes_wrapper.html", {
+                "content": original_get_html(self, *args, **kwargs),
+                "uid": generate_uid(),
+                "params": {
                     # Use camelCase to name keys.
-                    'usageId': unicode(self.scope_ids.usage_id).encode('utf-8'),
-                    'courseId': unicode(self.runtime.course_id).encode('utf-8'),
-                    'token': get_token(self.runtime.get_real_user(self.runtime.anonymous_student_id)),
-                    'endpoint': get_endpoint(),
-                    'debug': settings.DEBUG,
+                    "usageId": unicode(self.scope_ids.usage_id).encode("utf-8"),
+                    "courseId": unicode(self.runtime.course_id).encode("utf-8"),
+                    "token": get_id_token(self.runtime.get_real_user(self.runtime.anonymous_student_id)),
+                    "tokenUrl": get_token_url(self.runtime.course_id),
+                    "endpoint": get_endpoint(),
+                    "debug": settings.DEBUG,
                 },
             })
 
