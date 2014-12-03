b89113e114b19c1459772767c31759ed42c6a8e3
christopher lee <clee@edx.org>
2014-12-03 16:24:27 -0500

    Revert "Make static URLs absolute in handouts, updates, and about for mobile API"
    This reverts commit fb6579abfdb6fc9fb8bc9233554ff5c6e9215659.

--- lms/djangoapps/mobile_api/course_info/views.py
+++ lms/djangoapps/mobile_api/course_info/views.py
@@ -10,7 +10,6 @@ from courseware.courses import get_course_about_section, get_course_info_section
 from opaque_keys.edx.keys import CourseKey
 
 from xmodule.modulestore.django import modulestore
-from static_replace import make_static_urls_absolute, replace_static_urls
 
 
 class CourseUpdatesList(generics.ListAPIView):
@@ -29,7 +28,8 @@ class CourseUpdatesList(generics.ListAPIView):
 
             * date: The date of the course update.
 
-            * content: The content, as an HTML string, of the course update.
+            * content: The content, as a string, of the course update. HTML tags
+              are not included in the string.
 
             * status: Whether the update is visible or not.
 
@@ -42,21 +42,10 @@ class CourseUpdatesList(generics.ListAPIView):
         course_id = CourseKey.from_string(kwargs['course_id'])
         course = modulestore().get_course(course_id)
         course_updates_module = get_course_info_section_module(request, course, 'updates')
-        update_items = reversed(getattr(course_updates_module, 'items', []))
-
         updates_to_show = [
-            update for update in update_items
+            update for update in reversed(getattr(course_updates_module, 'items', []))
             if update.get("status") != "deleted"
         ]
-
-        for item in updates_to_show:
-            content = item['content']
-            content = replace_static_urls(
-                content,
-                course_id=course_id,
-                static_asset_path=course.static_asset_path)
-            item['content'] = make_static_urls_absolute(request, content)
-
         return Response(updates_to_show)
 
 
@@ -82,13 +71,7 @@ class CourseHandoutsList(generics.ListAPIView):
         course = modulestore().get_course(course_id)
         course_handouts_module = get_course_info_section_module(request, course, 'handouts')
         if course_handouts_module:
-            handouts_html = course_handouts_module.data
-            handouts_html = replace_static_urls(
-                handouts_html,
-                course_id=course_id,
-                static_asset_path=course.static_asset_path)
-            handouts_html = make_static_urls_absolute(self.request, handouts_html)
-            return Response({'handouts_html': handouts_html})
+            return Response({'handouts_html': course_handouts_module.data})
         else:
             # course_handouts_module could be None if there are no handouts
             # (such as while running tests)
@@ -121,8 +104,6 @@ class CourseAboutDetail(generics.RetrieveAPIView):
         #
         # This can also return None, so check for that before calling strip()
         about_section_html = get_course_about_section(course, "overview")
-        about_section_html = make_static_urls_absolute(self.request, about_section_html)
-
         return Response(
             {"overview": about_section_html.strip() if about_section_html else ""}
         )
