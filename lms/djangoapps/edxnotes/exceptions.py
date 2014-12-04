81c12a75cbe18d18a8da05909fa180262c1bc259
polesye <s2pak.anton@gmail.com>
2014-12-04 17:56:56 +0200

    Add more graceful error message.

--- lms/djangoapps/edxnotes/exceptions.py
+++ lms/djangoapps/edxnotes/exceptions.py
@@ -8,3 +8,10 @@ class EdxNotesParseError(Exception):
     An exception that is raised whenever we have issues with data parsing.
     """
     pass
+
+
+class EdxNotesServiceUnavailable(Exception):
+    """
+    An exception that is raised whenever EdxNotes service is unavailable.
+    """
+    pass
