18cb55d99b19d10578907e8cf4a58e44fe38bcde
Jesse Zoldak <zoldak@edx.org>
2014-12-05 12:19:58 -0500

    Do not create a modulestore unnecessarily in tests

--- common/lib/xmodule/xmodule/modulestore/tests/django_utils.py
+++ common/lib/xmodule/xmodule/modulestore/tests/django_utils.py
@@ -7,6 +7,8 @@ import pytz
 from tempfile import mkdtemp
 from uuid import uuid4
 
+from mock import patch
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import TestCase
@@ -276,10 +278,14 @@ class ModuleStoreTestCase(TestCase):
         return updated_course
 
     @staticmethod
-    def drop_mongo_collections():
+    @patch('xmodule.modulestore.django.create_modulestore_instance')
+    def drop_mongo_collections(mock_create):
         """
         If using a Mongo-backed modulestore & contentstore, drop the collections.
         """
+        # Do not create the modulestore if it does not exist.
+        mock_create.return_value = None
+
         module_store = modulestore()
         if hasattr(module_store, '_drop_database'):
             module_store._drop_database()  # pylint: disable=protected-access
@@ -302,7 +308,6 @@ class ModuleStoreTestCase(TestCase):
         """
         Flush the ModuleStore.
         """
-
         # Flush the Mongo modulestore
         self.drop_mongo_collections()
 
