e012a26e4f262012570e00d4af354bcb933a045a
polesye <s2pak.anton@gmail.com>
2014-12-11 13:50:51 +0200

    Display course structure tab.

---
+++ lms/static/js/edxnotes/utils/template.js
@@ -0,0 +1,22 @@
+;(function (define, undefined) {
+'use strict';
+define(['jquery', 'underscore'], function($, _) {
+    /**
+     * Loads the named template from the page, or logs an error if it fails.
+     * @param name The name of the template.
+     * @return The loaded template.
+     */
+    var loadTemplate = function(name) {
+        var templateSelector = '#' + name + '-tpl',
+            templateText = $(templateSelector).text();
+        if (!templateText) {
+            console.error('Failed to load ' + name + ' template');
+        }
+        return _.template(templateText);
+    };
+
+    return {
+        loadTemplate: loadTemplate
+    };
+});
+}).call(this, define || RequireJS.define);
--- lms/static/js/edxnotes/views/tabs/course_structure.js
+++ lms/static/js/edxnotes/views/tabs/course_structure.js
@@ -1,32 +1,108 @@
 ;(function (define, undefined) {
 'use strict';
 define([
-    'gettext', 'js/edxnotes/views/subview', 'js/edxnotes/views/tab_view'
-], function (gettext, SubView, TabView) {
-    var CourseStructureView = TabView.extend({
-        SubViewConstructor: SubView.extend({
-            id: 'edx-notes-page-course-structure',
-            templateName: 'course-structure-item',
+    'jquery', 'gettext', 'backbone', 'js/edxnotes/utils/template',
+    'js/edxnotes/views/note_item', 'js/edxnotes/views/tab_view'
+], function ($, gettext, Backbone, templateUtils, NoteItemView, TabView) {
+    var NoteGroupView, CourseStructureView;
+
+    NoteGroupView = Backbone.View.extend({
+        tagName: 'section',
+        className: 'note-group',
+        id: function () {
+            return 'note-group-' + _.uniqueId();
+        },
+
+        initialize: function () {
+            this.template = templateUtils.loadTemplate('note-group');
+            this.children = [];
+        },
+
+        render: function () {
+            this.$el.html(this.template({
+                chapterName: this.getChapterName(),
+                sectionName: this.getSectionName()
+            }));
+
+            _.each(this.children, function (child) {
+                this.$el.append(child);
+            }, this);
+
+            return this;
+        },
+
+        getChapterName: function () {
+            return this.options.chapter.display_name || '';
+        },
+
+        getSectionName: function () {
+            return this.options.section.display_name || '';
+        },
+
+        addChild: function (child) {
+            this.children.push(child);
+        }
+    });
+
+    CourseStructureView = TabView.extend({
+        SubViewConstructor: Backbone.View.extend({
+            className: 'tab-panel',
+            id: 'structure-panel',
 
             render: function () {
-                this.$el.html(this.template({
-                    structure: this.collection.getSortedByCourseStructure()
-                }));
+                var courseStructure = this.collection.getCourseStructure();
+
+                this.$el.append(this.getTitle());
+                _.each(courseStructure.chapters, function (chapter) {
+                    _.each(chapter.children, function (location) {
+                        var section = courseStructure.sections[location],
+                            group;
+
+                        if (section) {
+                            group = this.getGroup(chapter, section);
+                            _.each(section.children, function (location) {
+                                var unit = courseStructure.units[location];
+                                if (unit) {
+                                    group.addChild(this.getNotes(unit))
+                                }
+                            }, this);
+                            group.render().$el.appendTo(this.$el);
+                        }
+                    }, this);
+                }, this);
 
                 return this;
+            },
+
+            getNotes: function (models, id) {
+                var container = document.createDocumentFragment();
+                _.each(models, function (model) {
+                    var item = new NoteItemView({model: model});
+                    container.appendChild(item.render().el);
+                });
+
+                return container;
+            },
+
+            getGroup: function (chapter, section) {
+                return  new NoteGroupView({
+                    chapter: chapter,
+                    section: section
+                });
+            },
+
+            getTitle: function () {
+                return $('<h2></h2>', {
+                    'class': 'sr',
+                    'text': gettext('Course Strucutre')
+                }).get(0);
             }
         }),
 
-        getSubView: function () {
-            var collection = this.getCollection();
-            return new this.SubViewConstructor({
-                collection: collection
-            });
-        },
-
         tabInfo: {
             name: gettext('Course Strucutre'),
-            class_name: 'tab-recent-activity'
+            class_name: 'view-course-structure',
+            icon: 'icon-list-ul'
         }
     });
 
