05395590b84b36f44977f3cca9b03a993fec07ae
Akihito Yokose <ayokose.gacco@gmail.com>
2014-12-12 16:42:22 +0900

    EDX-455: Modify course_id for OpaqueKeys (EDX-687)

--- cms/djangoapps/contentstore/management/commands/check_draft.py
+++ cms/djangoapps/contentstore/management/commands/check_draft.py
@@ -3,8 +3,10 @@ from prettytable import PrettyTable
 
 from django.core.management.base import BaseCommand, CommandError, make_option
 
+from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.django import modulestore
-from xmodule.modulestore import Location
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.locator import CourseLocator
 
 
 log = logging.getLogger(__name__)
@@ -15,7 +17,7 @@ class Command(BaseCommand):
     Usage: python manage.py cms --settings=aws check_draft --active_only edX/DemoX/Demo_Course
 
     Args:
-        course_id: 'org/course/run'
+        course_id: 'org/course/run' or 'course-v1:org+course+run'
     """
     help = """Usage: check_draft [--active_only] [<course_id>]"""
 
@@ -34,25 +36,20 @@ class Command(BaseCommand):
         course_id = args[0] if len(args) > 0 else None
         if course_id:
             try:
-                Location.parse_course_id(course_id)
-            except ValueError:
-                raise CommandError("The course_id is not of the right format. It should be like 'org/course/run'")
+                course_id = CourseLocator.from_string(course_id)
+            except InvalidKeyError:
+                raise CommandError("The course_id is not of the right format. It should be like 'org/course/run' or 'course-v1:org+course+run'")
 
         # Check options: active_only
         active_only = options['active_only']
 
         # Result
-        output = PrettyTable(['Course ID', 'Course Name', 'Chapter Name', 'Sequential Name', 'Vertical Name', 'Draft?'])
+        output = PrettyTable(['Course ID', 'Course Name', 'Chapter Name', 'Sequential Name', 'Vertical Name', 'Draft?', 'Changed?'])
         output.align = 'l'
 
         # Find courses
-        tag = 'i4x'
         if course_id:
-            course_dict = Location.parse_course_id(course_id)
-            org = course_dict['org']
-            course = course_dict['course']
-            name = course_dict['name']
-            course_items = modulestore().get_items(Location(tag, org, course, 'course', name))
+            course_items = modulestore().get_items(course_id, qualifiers={'category': 'course'})
             if not course_items:
                 raise CommandError("The specified course does not exist.")
         else:
@@ -63,19 +60,28 @@ class Command(BaseCommand):
             if active_only and course_item.has_ended():
                 continue
             # Find chapter items
-            chapter_items = [modulestore().get_item(Location(item_id)) for item_id in course_item.children]
+            chapter_items = [modulestore().get_item(item.location) for item in course_item.get_children()]
             chapter_items = sorted(chapter_items, key=lambda item: item.start)
             for chapter_item in chapter_items:
                 # Find sequential items
-                sequential_items = [modulestore().get_item(Location(item_id)) for item_id in chapter_item.children]
+                sequential_items = [modulestore().get_item(item.location) for item in chapter_item.get_children()]
                 sequential_items = sorted(sequential_items, key=lambda item: item.start)
                 for sequential_item in sequential_items:
+                    #print "sequential_item.location=%s" % sequential_item.location
+                    #print "sequential_item.published?=%s" % modulestore().has_item(sequential_item.location, revision=ModuleStoreEnum.RevisionOption.published_only)
+                    #print "sequential_item.changed?=%s" % modulestore().has_changes(sequential_item)
                     # Find vertical items
-                    vertical_items = [modulestore().get_item(Location(item_id)) for item_id in sequential_item.children]
+                    vertical_items = [modulestore().get_item(item.location) for item in sequential_item.get_children()]
                     vertical_items = sorted(vertical_items, key=lambda item: item.start)
                     for vertical_item in vertical_items:
-                        if vertical_item.is_draft:
-                            output.add_row([course_item.id, course_item.display_name, chapter_item.display_name, sequential_item.display_name, vertical_item.display_name, vertical_item.is_draft])
+                        #print "vertical_item.location=%s" % vertical_item.location
+                        #print "vertical_item.published?=%s" % modulestore().has_item(vertical_item.location, revision=ModuleStoreEnum.RevisionOption.published_only)
+                        #print "vertical_item.changed?=%s" % modulestore().has_changes(vertical_item)
+                        is_draft = vertical_item.is_draft
+                        # Note: cribbed from cms/djangoapps/contentstore/views/tests/test_item.py
+                        has_changes = modulestore().has_changes(vertical_item)
+                        if is_draft or has_changes:
+                            output.add_row([course_item.id, course_item.display_name, chapter_item.display_name, sequential_item.display_name, vertical_item.display_name, is_draft, has_changes])
 
         # Print result
         print output
