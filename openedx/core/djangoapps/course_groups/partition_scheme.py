4f521de8d4bd79b27976c94fe1c0a789111802a6
cahrens <christina@edx.org>
2014-12-15 14:10:27 -0500

    Method got lost in merge.

--- openedx/core/djangoapps/course_groups/partition_scheme.py
+++ openedx/core/djangoapps/course_groups/partition_scheme.py
@@ -84,3 +84,17 @@ class CohortPartitionScheme(object):
             )
             # fail silently
             return None
+
+
+def get_cohorted_user_partition(course_key):
+    """
+    Returns the first user partition from the specified course which uses the CohortPartitionScheme,
+    or None if one is not found. Note that it is currently recommended that each course have only
+    one cohorted user partition.
+    """
+    course = courses.get_course_by_id(course_key)
+    for user_partition in course.user_partitions:
+        if user_partition.scheme == CohortPartitionScheme:
+            return user_partition
+
+    return None
