9c98dfa6a7c38ba2084d54043a64a3ddb6ef696a
Don Mitchell <dmitchell@edx.org>
2014-12-17 11:57:00 -0500

    Add configuration to studio for asides

--- cms/djangoapps/contentstore/views/tests/test_preview.py
+++ cms/djangoapps/contentstore/views/tests/test_preview.py
@@ -1,18 +1,20 @@
 """
 Tests for contentstore.views.preview.py
 """
+import re
+
 from django.test import TestCase
 from django.test.client import RequestFactory
 
+from xblock.core import XBlockAside
 from student.tests.factories import UserFactory
 
 from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
 
 from contentstore.views.preview import get_preview_fragment
 from xmodule.modulestore import ModuleStoreEnum
-from xblock.core import XBlockAside
 from xmodule.modulestore.tests.test_asides import AsideTestType
-import re
+from cms.djangoapps.xblock_config.models import StudioConfig
 
 
 class GetPreviewHtmlTestCase(TestCase):
@@ -22,7 +24,6 @@ class GetPreviewHtmlTestCase(TestCase):
     Note that there are other existing test cases in test_contentstore that indirectly execute
     get_preview_fragment via the xblock RESTful API.
     """
-
     @XBlockAside.register_temp_plugin(AsideTestType, 'test_aside')
     def test_preview_fragment(self):
         """
@@ -37,6 +38,10 @@ class GetPreviewHtmlTestCase(TestCase):
             category="html",
             data={'data': "<html>foobar</html>"}
         )
+
+        config = StudioConfig.current()
+        config.enabled = True
+        config.save()
 
         request = RequestFactory().get('/dummy-url')
         request.user = UserFactory()
@@ -60,3 +65,36 @@ class GetPreviewHtmlTestCase(TestCase):
         self.assertRegexpMatches(html, "Aside rendered")
         # Now ensure the acid_aside is not in the result
         self.assertNotRegexpMatches(html, r"data-block-type=[\"\']acid_aside[\"\']")
+
+    @XBlockAside.register_temp_plugin(AsideTestType, 'test_aside')
+    def test_preview_no_asides(self):
+        """
+        Test for calling get_preview_html.
+
+        This test used to be specifically about Locators (ensuring that they did not
+        get translated to Locations). The test now has questionable value.
+        """
+        course = CourseFactory.create(default_store=ModuleStoreEnum.Type.split)
+        html = ItemFactory.create(
+            parent_location=course.location,
+            category="html",
+            data={'data': "<html>foobar</html>"}
+        )
+
+        config = StudioConfig.current()
+        config.enabled = False
+        config.save()
+
+        request = RequestFactory().get('/dummy-url')
+        request.user = UserFactory()
+        request.session = {}
+
+        # Call get_preview_fragment directly.
+        context = {
+            'reorderable_items': set(),
+            'read_only': True
+        }
+        html = get_preview_fragment(request, html, context).content
+
+        self.assertNotRegexpMatches(html, r"data-block-type=[\"\']test_aside[\"\']")
+        self.assertNotRegexpMatches(html, "Aside rendered")
--- cms/envs/common.py
+++ cms/envs/common.py
@@ -581,6 +581,7 @@ INSTALLED_APPS = (
     'course_creators',
     'student',  # misleading name due to sharing with lms
     'openedx.core.djangoapps.course_groups',  # not used in cms (yet), but tests run
+    'xblock_config',
 
     # Tracking
     'track',
