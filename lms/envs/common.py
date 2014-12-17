ba9bf1f6a24881f698da9311263b2e5e19a1db6c
Davorin Sego <davorin.sego@gmail.com>
2014-12-17 17:14:17 +0100

    remove sinon from search dependencies

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1009,7 +1009,6 @@ courseware_js = (
 # Courseware Search
 # TODO: add conditional
 courseware_js += (
-    'js/vendor/sinon-1.7.1.js',
     'js/search/models/result.js',
     'js/search/collections/collection.js',
     'js/search/views/form.js',
