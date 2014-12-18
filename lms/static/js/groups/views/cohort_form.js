682ea711e64b247a2878a29a7931f5b1c882ad6a
Andy Armstrong <andya@edx.org>
2014-12-18 17:23:58 -0500

    Assign cohorts to groups in the instructor dashboard
    TNL-653

---
+++ lms/static/js/groups/views/cohort_form.js
@@ -0,0 +1,104 @@
+var edx = edx || {};
+
+(function($, _, Backbone, gettext, interpolate_text, CohortModel, NotificationModel, NotificationView) {
+    'use strict';
+
+    edx.groups = edx.groups || {};
+
+    edx.groups.CohortFormView = Backbone.View.extend({
+        events : {
+            'change .field-radio input': 'onRadioButtonChange',
+            'click .tab-content-settings .action-save': 'saveSettings',
+            'submit .cohort-management-group-add-form': 'addStudents'
+        },
+
+        initialize: function(options) {
+            this.template = _.template($('#cohort-form-tpl').text());
+            this.cohortUserPartitionId = options.cohortUserPartitionId;
+            this.contentGroups = options.contentGroups;
+        },
+
+        showNotification: function(options, beforeElement) {
+            var model = new NotificationModel(options);
+            this.removeNotification();
+            this.notification = new NotificationView({
+                model: model
+            });
+            this.notification.render();
+            if (!beforeElement) {
+                beforeElement = this.$('.cohort-management-group');
+            }
+            beforeElement.before(this.notification.$el);
+        },
+
+        removeNotification: function() {
+            if (this.notification) {
+                this.notification.remove();
+            }
+        },
+
+        render: function() {
+            this.$el.html(this.template({
+                cohort: this.model,
+                contentGroups: this.contentGroups
+            }));
+            return this;
+        },
+
+        onRadioButtonChange: function(event) {
+            var target = $(event.currentTarget),
+                groupsEnabled = target.val() === 'yes';
+            this.$('.input-cohort-group-association').toggleClass('is-disabled', !groupsEnabled);
+        },
+
+        getSelectedGroupId: function() {
+            if (!this.$('.radio-yes').prop('checked')) {
+                return null;
+            }
+            return parseInt(this.$('.input-cohort-group-association').val());
+        },
+
+        saveForm: function() {
+            var self = this,
+                cohort = this.model,
+                saveOperation = $.Deferred(),
+                cohortName, groupId, showAddError;
+            this.removeNotification();
+            showAddError = function(message) {
+                self.showNotification(
+                    {type: 'error', title: message},
+                    self.$('.cohort-management-create-form-name label')
+                );
+            };
+            cohortName = this.$('.cohort-name').val().trim();
+            if (cohortName.length > 0) {
+                groupId = this.getSelectedGroupId();
+                cohort.save(
+                    {name: cohortName, user_partition_id: this.cohortUserPartitionId, group_id: groupId}
+                ).done(function(result) {
+                    if (!result.error) {
+                        cohort.id = result.id;
+                        saveOperation.resolve();
+                    } else {
+                        showAddError(result.error);
+                        saveOperation.reject();
+                    }
+                }).fail(function(result) {
+                    var errorMessage = null;
+                    try {
+                        var jsonResponse = JSON.parse(result.responseText);
+                        errorMessage = jsonResponse.error;
+                    } catch(e) {
+                        errorMessage = gettext("We've encountered an error. Please refresh your browser and then try again.");
+                    }
+                    showAddError(errorMessage);
+                    saveOperation.reject();
+                });
+            } else {
+                showAddError(gettext('Please enter a name for your new cohort group.'));
+                saveOperation.reject();
+            }
+            return saveOperation.promise();
+        }
+    });
+}).call(this, $, _, Backbone, gettext, interpolate_text, edx.groups.CohortModel, NotificationModel, NotificationView);
