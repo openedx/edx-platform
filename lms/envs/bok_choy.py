21cd7a6715d5f7a1b7d627d84b07e441797c8252
Calen Pennington <cale@edx.org>
2014-12-19 17:04:30 -0500

    Allow auto_auth on the LMS in bok_choy tests

--- lms/envs/bok_choy.py
+++ lms/envs/bok_choy.py
@@ -128,6 +128,9 @@ FEATURES['ENABLE_TEAMS'] = True
 # Enable custom content licensing
 FEATURES['LICENSING'] = True
 
+# Use the auto_auth workflow for creating users and logging them in
+FEATURES['AUTOMATIC_AUTH_FOR_TESTING'] = True
+
 ########################### Entrance Exams #################################
 FEATURES['MILESTONES_APP'] = True
 FEATURES['ENTRANCE_EXAMS'] = True
