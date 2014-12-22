34e9a7d404437bf81d1111d38166201921e15d82
Greg Price <gprice@edx.org>
2014-12-22 14:04:03 -0500

    Add download button to Studio video upload page
    The button downloads the CSV data containing all video URLs.

--- cms/static/js/factories/videos_index.js
+++ cms/static/js/factories/videos_index.js
@@ -2,7 +2,14 @@ define(
     ["jquery", "backbone", "js/views/active_video_upload_list", "js/views/previous_video_upload_list"],
     function ($, Backbone, ActiveVideoUploadListView, PreviousVideoUploadListView) {
         "use strict";
-        var VideosIndexFactory = function($contentWrapper, postUrl, concurrentUploadLimit, uploadButton, previousUploads) {
+        var VideosIndexFactory = function(
+            $contentWrapper,
+            postUrl,
+            encodingsDownloadUrl,
+            concurrentUploadLimit,
+            uploadButton,
+            previousUploads
+        ) {
             var activeView = new ActiveVideoUploadListView({
                 postUrl: postUrl,
                 concurrentUploadLimit: concurrentUploadLimit,
@@ -10,7 +17,10 @@ define(
             });
             $contentWrapper.append(activeView.render().$el);
             var previousCollection = new Backbone.Collection(previousUploads);
-            var previousView = new PreviousVideoUploadListView({collection: previousCollection});
+            var previousView = new PreviousVideoUploadListView({
+                collection: previousCollection,
+                encodingsDownloadUrl: encodingsDownloadUrl
+            });
             $contentWrapper.append(previousView.render().$el);
         };
 
