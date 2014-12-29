357fa7be25c237ece6fa8d4333cf892fe503bcfb
Will Daly <will.e.daly@gmail.com>
2014-12-29 10:16:14 -0800

    Update JavaScript includes for verified / payment pages

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1083,8 +1083,6 @@ verify_student_js = [
     'js/verify_student/models/verification_model.js',
     'js/verify_student/views/error_view.js',
     'js/verify_student/views/webcam_photo_view.js',
-    'js/verify_student/views/progress_view.js',
-    'js/verify_student/views/requirements_view.js',
     'js/verify_student/views/step_view.js',
     'js/verify_student/views/intro_step_view.js',
     'js/verify_student/views/make_payment_step_view.js',
