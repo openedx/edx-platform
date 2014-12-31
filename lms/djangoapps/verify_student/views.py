7aced567e02c9cf53f41e2dd86c489e6a95b3977
Will Daly <will@edx.org>
2014-12-31 12:03:17 -0500

    Set attempting_upgrade session flag in the new payment flow

--- lms/djangoapps/verify_student/views.py
+++ lms/djangoapps/verify_student/views.py
@@ -450,6 +450,10 @@ class PayAndVerifyView(View):
             'donation_for_course', {}
         ).get(unicode(course_key), '')
 
+        # Remember whether the user is upgrading
+        # so we can fire an analytics event upon payment.
+        request.session['attempting_upgrade'] = (message == self.UPGRADE_MSG)
+
         # Render the top-level page
         context = {
             'contribution_amount': contribution_amount,
