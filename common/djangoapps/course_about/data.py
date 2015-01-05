5479116f93ff0bc38eeede30f3c5fbfb41af7c03
arbisoft <aamirkhan206>
2015-01-05 17:44:01 +0500

    ECOM-335 Caching the api result

--- common/djangoapps/course_about/data.py
+++ common/djangoapps/course_about/data.py
@@ -10,12 +10,14 @@ from course_about.serializers import serialize_content
 from course_about.errors import CourseNotFoundError
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
+from util.memcache import safe_key
+from django.core.cache import get_cache
+
 
 log = logging.getLogger(__name__)
 
 ABOUT_ATTRIBUTES = [
     'effort',
-    'video'
 ]
 
 
@@ -30,16 +32,23 @@ def get_course_about_details(course_id):  # pylint: disable=unused-argument
         CourseNotFoundError
     """
     try:
+        cache = get_cache("default")
         course_key = CourseKey.from_string(course_id)
-        course_descriptor = modulestore().get_course(course_key)
-        if course_descriptor is None:
-            raise CourseNotFoundError("course not found")
+        course_version = _get_course_version(course_key)
+        cache_key = safe_key(course_id, "course_info_api_", course_version)
+        course_info = cache.get(cache_key)
+        if not course_info:
+            course_descriptor = modulestore().get_course(course_key, remove_version=False, remove_branch=False)
+            if course_descriptor is None:
+                raise CourseNotFoundError("course not found")
+            about_descriptor = {}
+            for attribute in ABOUT_ATTRIBUTES:
+                about_descriptor[attribute] = _fetch_course_detail(course_key, attribute)
+            course_info = serialize_content(course_descriptor=course_descriptor, about_descriptor=about_descriptor)
+            cache.set(cache_key, course_info)
     except InvalidKeyError as err:
         raise CourseNotFoundError(err.message)
-    about_descriptor = {}
-    for attribute in ABOUT_ATTRIBUTES:
-        about_descriptor[attribute] = _fetch_course_detail(course_key, attribute)
-    return serialize_content(course_descriptor=course_descriptor, about_descriptor=about_descriptor)
+    return course_info
 
 
 def _fetch_course_detail(course_key, attribute):
@@ -52,3 +61,12 @@ def _fetch_course_detail(course_key, attribute):
     except ItemNotFoundError:
         value = None
     return value
+
+
+def _get_course_version(course_key):
+    """
+    Return the version of course
+    """
+    #TODO User proper way to return the version of key
+    version_guide = course_key.version_guid
+    return version_guide if version_guide else 100
