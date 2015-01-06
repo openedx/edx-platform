260962e06cb18b66b55d64d7d859e674cb424bcf
David Baumgold <david@davidbaumgold.com>
2015-01-06 16:07:57 -0500

    Use "Studio" name by default (instead of "edX Studio")

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -41,12 +41,8 @@ import dealer.git
 from xmodule.modulestore.edit_info import EditInfoMixin
 
 ############################ FEATURE CONFIGURATION #############################
+STUDIO_NAME = "Studio"
 STUDIO_SHORT_NAME = "Studio"
-STUDIO_NAME = "{platform_name} {short_name}".format(
-    platform_name=PLATFORM_NAME, short_name=STUDIO_SHORT_NAME,
-)
-# Note that if you redefine STUDIO_SHORT_NAME in another settings file,
-# you'll also need to redefine STUDIO_NAME -- it won't automatically inherit.
 FEATURES = {
     'USE_DJANGO_PIPELINE': True,
 
