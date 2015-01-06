f1632306209125ddce2dc08c65c07fa810c33819
Jonathan Piacenti <kelketek@gmail.com>
2015-01-06 20:36:18 +0000

    Refactored shared elements of create factories into base class.

--- cms/static/js/views/utils/create_library_utils.js
+++ cms/static/js/views/utils/create_library_utils.js
@@ -1,66 +1,17 @@
 /**
  * Provides utilities for validating libraries during creation.
  */
-define(["jquery", "underscore", "gettext", "js/views/utils/view_utils"],
-    function ($, _, gettext, ViewUtils) {
+define(["jquery", "gettext", "js/views/utils/view_utils", "js/views/utils/create_utils_base"],
+    function ($, gettext, ViewUtils, CreateUtilsFactory) {
         "use strict";
         return function (selectors, classes) {
-            var toggleSaveButton, validateTotalKeyLength, setFieldInErr,
-                hasInvalidRequiredFields, create, validateFilledFields, configureHandlers;
-
-            var validateRequiredField = ViewUtils.validateRequiredField;
-            var validateURLItemEncoding = ViewUtils.validateURLItemEncoding;
-
-            var keyLengthViolationMessage = gettext("The combined length of the organization and library code fields cannot be more than <%=limit%> characters.");
-
+            var keyLengthViolationMessage = gettext("The combined length of the organization and library code fields cannot be more than <%=limit%> characters.")
             var keyFieldSelectors = [selectors.org, selectors.number];
             var nonEmptyCheckFieldSelectors = [selectors.name, selectors.org, selectors.number];
 
-            toggleSaveButton = function (is_enabled) {
-                var is_disabled = !is_enabled;
-                $(selectors.save).toggleClass(classes.disabled, is_disabled).attr('aria-disabled', is_disabled);
-            };
+            CreateUtilsFactory.call(this, selectors, classes, keyLengthViolationMessage, keyFieldSelectors, nonEmptyCheckFieldSelectors);
 
-            // Ensure that key fields passes checkTotalKeyLengthViolations check
-            validateTotalKeyLength = function () {
-                ViewUtils.checkTotalKeyLengthViolations(
-                    selectors, classes,
-                    keyFieldSelectors,
-                    keyLengthViolationMessage
-                );
-            };
-
-            setFieldInErr = function (element, message) {
-                if (message) {
-                    element.addClass(classes.error);
-                    element.children(selectors.tipError).addClass(classes.showing).removeClass(classes.hiding).text(message);
-                    toggleSaveButton(false);
-                }
-                else {
-                    element.removeClass(classes.error);
-                    element.children(selectors.tipError).addClass(classes.hiding).removeClass(classes.showing);
-                    // One "error" div is always present, but hidden or shown
-                    if ($(selectors.error).length === 1) {
-                        toggleSaveButton(true);
-                    }
-                }
-            };
-
-            // One final check for empty values
-            hasInvalidRequiredFields = function () {
-                return _.reduce(
-                    nonEmptyCheckFieldSelectors,
-                    function (acc, element) {
-                        var $element = $(element);
-                        var error = validateRequiredField($element.val());
-                        setFieldInErr($element.parent(), error);
-                        return error ? true : acc;
-                    },
-                    false
-                );
-            };
-
-            create = function (libraryInfo, errorHandler) {
+            this.create = function (libraryInfo, errorHandler) {
                 $.postJSON(
                     '/library/',
                     libraryInfo
@@ -78,60 +29,6 @@ define(["jquery", "underscore", "gettext", "js/views/utils/view_utils"],
                     }
                     errorHandler(reason);
                 });
-            };
-
-            // Ensure that all fields are not empty
-            validateFilledFields = function () {
-                return _.reduce(
-                    nonEmptyCheckFieldSelectors,
-                    function (acc, element) {
-                        var $element = $(element);
-                        return $element.val().length !== 0 ? acc : false;
-                    },
-                    true
-                );
-            };
-
-            // Handle validation asynchronously
-            configureHandlers = function () {
-                _.each(
-                    keyFieldSelectors,
-                    function (element) {
-                        var $element = $(element);
-                        $element.on('keyup', function (event) {
-                            // Don't bother showing "required field" error when
-                            // the user tabs into a new field; this is distracting
-                            // and unnecessary
-                            if (event.keyCode === $.ui.keyCode.TAB) {
-                                return;
-                            }
-                            var error = validateURLItemEncoding($element.val(), $(selectors.allowUnicode).val() === 'True');
-                            setFieldInErr($element.parent(), error);
-                            validateTotalKeyLength();
-                            if (!validateFilledFields()) {
-                                toggleSaveButton(false);
-                            }
-                        });
-                    }
-                );
-                var $name = $(selectors.name);
-                $name.on('keyup', function () {
-                    var error = validateRequiredField($name.val());
-                    setFieldInErr($name.parent(), error);
-                    validateTotalKeyLength();
-                    if (!validateFilledFields()) {
-                        toggleSaveButton(false);
-                    }
-                });
-            };
-
-            return {
-                validateTotalKeyLength: validateTotalKeyLength,
-                setFieldInErr: setFieldInErr,
-                hasInvalidRequiredFields: hasInvalidRequiredFields,
-                create: create,
-                validateFilledFields: validateFilledFields,
-                configureHandlers: configureHandlers
-            };
+            }
         };
     });
