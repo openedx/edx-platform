61e02fab8796a6616544da65b960f4e7dd491d39
Sarina Canelake <sarina@edx.org>
2015-01-06 14:04:40 -0500

    Hide Honor Code mentions if site does not have an Honor Code

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1582,7 +1582,7 @@ MKTG_URL_LINK_MAP = {
     'COURSES': 'courses',
     'ROOT': 'root',
     'TOS': 'tos',
-    'HONOR': 'honor',
+    'HONOR': 'honor',  # If your site does not have an honor code, simply delete this line.
     'PRIVACY': 'privacy',
     'PRESS': 'press',
     'BLOG': 'blog',
