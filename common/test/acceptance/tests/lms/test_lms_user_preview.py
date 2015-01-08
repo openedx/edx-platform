67583b04262014d5e06fd84160905da843a0b0a3
Andy Armstrong <andya@edx.org>
2015-01-08 15:26:10 -0500

    Miscellaneous text updates

--- common/test/acceptance/tests/lms/test_lms_user_preview.py
+++ common/test/acceptance/tests/lms/test_lms_user_preview.py
@@ -229,7 +229,7 @@ class StaffDebugTest(CourseWithoutContentGroupsTest):
 
 class CourseWithContentGroupsTest(StaffViewTest):
     """
-    Verifies that changing the "previewing as" selector works properly for content groups.
+    Verifies that changing the "View this course as" selector works properly for content groups.
     """
 
     def setUp(self):
