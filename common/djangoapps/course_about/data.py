3f52cbda11de9f13060ed913353c7aad0a0f89be
Awais <awais.qureshi@arbisoft.com>
2015-01-09 23:53:15 +0500

    ECOM-335 remove version from cache key.

--- common/djangoapps/course_about/data.py
+++ common/djangoapps/course_about/data.py
@@ -27,8 +27,7 @@ def get_course_about_details(course_id):  # pylint: disable=unused-argument
     Return course information for a given course id.
     First its checks the default cache for given course id if its exists then returns
     the course otherwise it get the course from module store and set the cache.
-    if course has version then use that for making cache-key.
-    if version is None then set the expiry timeout for cache key.
+    By default cache expiry set to 5 minutes.
 
     Args:
         course_id(str) : The course id to retrieve course information for.
@@ -42,26 +41,24 @@ def get_course_about_details(course_id):  # pylint: disable=unused-argument
     try:
         cache = get_cache("default")
         course_key = CourseKey.from_string(course_id)
-        course_version = _get_course_version(course_key)
-        key_prefix = getattr(settings, 'COURSE_INFO_API_CACHE_PREFIX', '')
-        if course_version:
-            cache_key = safe_key(course_id, key_prefix, course_version)
-        else:
-            cache_key = safe_key(course_id, key_prefix, '')
+        key_prefix = getattr(settings, 'COURSE_INFO_API_CACHE_PREFIX', 'course_info_api_')
+        cache_key = safe_key(course_id, key_prefix,'')
         course_info = cache.get(cache_key)
-        if not course_info:
-            course_descriptor = modulestore().get_course(course_key, remove_version=False, remove_branch=False)
+
+        if course_info is None:
+            course_descriptor = modulestore().get_course(course_key)
+
             if course_descriptor is None:
                 raise CourseNotFoundError("course not found")
+
             about_descriptor = {}
             for attribute in ABOUT_ATTRIBUTES:
                 about_descriptor[attribute] = _fetch_course_detail(course_key, attribute)
+
             course_info = serialize_content(course_descriptor=course_descriptor, about_descriptor=about_descriptor)
-            if course_version:
-                cache.set(cache_key, course_info, None)
-            else:
-                time_out = getattr(settings, 'COURSE_INFO_API_CACHE_TIME_OUT', 300)
-                cache.set(cache_key, course_info, time_out)
+
+            time_out = getattr(settings, 'COURSE_INFO_API_CACHE_TIME_OUT', 300)
+            cache.set(cache_key, course_info, time_out)
     except InvalidKeyError as err:
         raise CourseNotFoundError(err.message)
     return course_info
@@ -77,11 +74,3 @@ def _fetch_course_detail(course_key, attribute):
     except ItemNotFoundError:
         value = None
     return value
-
-
-def _get_course_version(course_key):
-    """
-    Return the version of course
-    """
-    #TODO User proper way to return the version of key
-    return course_key.version_guid
