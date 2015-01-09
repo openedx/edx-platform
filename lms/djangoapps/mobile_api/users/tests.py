e77bcab571497087017924a9e8d3b88f52290a85
Nimisha Asthagiri <nasthagiri@edx.org>
2015-01-09 10:15:33 -0500

    MA-345 Mobile Enrollment API: add course' Facebook URL

--- lms/djangoapps/mobile_api/users/tests.py
+++ lms/djangoapps/mobile_api/users/tests.py
@@ -5,7 +5,6 @@ import datetime
 from django.utils import timezone
 
 from xmodule.modulestore.tests.factories import ItemFactory, CourseFactory
-from xmodule.modulestore.django import modulestore
 from student.models import CourseEnrollment
 from certificates.models import CertificateStatuses
 from certificates.tests.factories import GeneratedCertificateFactory
@@ -106,6 +105,23 @@ class TestUserEnrollmentApi(MobileAPITestCase, MobileAuthUserTestMixin, MobileEn
         response = self.api_response()
         certificate_data = response.data[0]['certificate']  # pylint: disable=no-member
         self.assertEquals(certificate_data['url'], certificate_url)
+
+    def test_no_facebook_url(self):
+        self.login_and_enroll()
+
+        response = self.api_response()
+        course_data = response.data[0]['course']  # pylint: disable=no-member
+        self.assertIsNone(course_data['social_urls']['facebook'])
+
+    def test_facebook_url(self):
+        self.login_and_enroll()
+
+        self.course.facebook_url = "http://facebook.com/test_group_page"
+        self.store.update_item(self.course, self.user.id)
+
+        response = self.api_response()
+        course_data = response.data[0]['course']  # pylint: disable=no-member
+        self.assertEquals(course_data['social_urls']['facebook'], self.course.facebook_url)
 
 
 class CourseStatusAPITestCase(MobileAPITestCase):
@@ -280,7 +296,7 @@ class TestCourseEnrollmentSerializer(MobileAPITestCase):
 
         self.course.display_coursenumber = "overridden_number"
         self.course.display_organization = "overridden_org"
-        modulestore().update_item(self.course, self.user.id)
+        self.store.update_item(self.course, self.user.id)
 
         serialized = CourseEnrollmentSerializer(CourseEnrollment.enrollments_for_user(self.user)[0]).data  # pylint: disable=no-member
         self.assertEqual(serialized['course']['number'], self.course.display_coursenumber)
