8b132b4429dc3960fabc844edd3537c76fea1fb1
polesye <s2pak.anton@gmail.com>
2015-01-12 14:31:28 +0200

    Add feature flag explicitly to the test.

--- cms/djangoapps/contentstore/tests/test_course_settings.py
+++ cms/djangoapps/contentstore/tests/test_course_settings.py
@@ -785,6 +785,7 @@ class CourseMetadataEditingTest(CourseTestCase):
         course = modulestore().get_course(self.course.id)
         self.assertNotIn(EXTRA_TAB_PANELS.get("open_ended"), course.tabs)
 
+    @patch.dict(settings.FEATURES, {'ENABLE_EDXNOTES': True})
     def test_course_settings_munge_tabs(self):
         """
         Test that adding and removing specific course settings adds and removes tabs.
