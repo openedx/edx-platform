7bdaa411a0995e9d70c8e90a2b81fd7e9d5a0eee
Tomasz Zieliński <tomasz.zielinski@pyconsultant.eu>
2015-01-15 13:45:37 +0100

    Added simple error handling

--- common/djangoapps/appsembler/models.py
+++ common/djangoapps/appsembler/models.py
@@ -4,7 +4,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-import intercom
+from intercom import intercom
+import requests
 
 from xmodule.modulestore.django import modulestore
 from student.models import CourseEnrollment
@@ -21,18 +22,25 @@ intercom.Intercom.api_key = os.environ.get("INTERCOM_APP_SECRET", "")
 INTERCOM_USER_EMAIL = os.environ.get("INTERCOM_USER_EMAIL", "")
 
 
+def intercom_update(custom_data):
+    try:
+        intercom.Intercom.update_user(email=INTERCOM_USER_EMAIL, custom_data=custom_data)
+    except (intercom.IntercomError, requests.exceptions.RequestException):
+        pass
+
+
 def update_user_statistics():
-    intercom_user = intercom.User.find(email=INTERCOM_USER_EMAIL)
-    intercom_user.custom_data = {
+    custom_data = {
         'Users - total': User.objects.count(),
         'Users - active': User.objects.filter(is_active=True).count(),
+        # TODO: Filter out inactive users?
         'Users - students': User.objects.filter(
             courseenrollment__is_active=True).distinct().count(),
     }
     # if CourseCreator:
-    #     intercom_user.custom_data['Users - course creation rights granted'] = User.objects.filter(
+    #     custom_data['Users - course creation rights granted'] = User.objects.filter(
     #         coursecreator__state=CourseCreator.GRANTED).count()
-    intercom_user.save()
+    intercom_update(custom_data=custom_data)
 
 
 update_user_statistics()
@@ -45,8 +53,7 @@ def user_save_callback(sender, instance, created, raw, **kwargs):
 
 
 def update_course_statistics():
-    intercom_user = intercom.User.find(email=INTERCOM_USER_EMAIL)
-    intercom_user.custom_data = {
+    custom_data = {
         # TODO: Figure out whether we need to filter out broken items or not
         #       https://github.com/edx/edx-platform/blob/d4de932c2b46dbe1ad6439731b4312fb36813d6d/cms/djangoapps/contentstore/views/course.py#L281
         'Courses - total': len(modulestore().get_courses()),
@@ -55,7 +62,7 @@ def update_course_statistics():
         #       https://github.com/appsembler/edx-platform/blob/d4de932c2b46dbe1ad6439731b4312fb36813d6d/cms/djangoapps/contentstore/views/course.py#L295
         'Courses - reruns': CourseRerunState.objects.count(),
     }
-    intercom_user.save()
+    intercom_update(custom_data=custom_data)
 
 
 update_course_statistics()
