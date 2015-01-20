32e5907b969477536f9b1f54e073f39b211408f1
Tomasz Zieliński <tomasz.zielinski@pyconsultant.eu>
2015-01-20 18:20:42 +0100

    Added the intercom context processor to settings

--- lms/envs/appsembler.py
+++ lms/envs/appsembler.py
@@ -3,3 +3,5 @@ from .aws import *
 GOOGLE_TAG_MANAGER_ID = os.environ.get("GOOGLE_TAG_MANAGER_ID", None)
 
 INSTALLED_APPS += ('appsembler',)
+
+TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
