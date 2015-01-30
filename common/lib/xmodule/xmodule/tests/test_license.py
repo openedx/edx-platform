0432cf0ae54bb4c0cae1f13d4eb518cc63b19524
Sarina Canelake <sarina@edx.org>
2015-01-29 22:56:56 -0500

    Start refactoring license test

--- common/lib/xmodule/xmodule/tests/test_license.py
+++ common/lib/xmodule/xmodule/tests/test_license.py
@@ -4,24 +4,25 @@ import unittest
 from xmodule.license import parse_license, License, ARRLicense, CCLicense
 
 
-def assert_equal_license(self, lic_1, lic_2):
-    """
-    Asserts that lic_1 and lic_2 are both licenses of the same
-    type, with the same versions as applicable.
-    """
-    # Assert that lic_1 and lic_2 are both License types,
-    # otherwise further checks will fail
-    self.assertTrue(isinstance(lic_1, License))
-    self.assertTrue(isinstance(lic_2, License))
-    # Assert licenses are of the same type
-    self.assertEqual(lic_1.kind, lic_2.kind)
-    # If they're not of the ARR type license, they're versioned, so
-    # assert that their versions are also equal
-    if not lic_1.kind == "ARR":
-        self.assertEqual(lic_1.version, lic_2.version)
+class BaseLicenseTest(unittest.TestCase):
+    """Base test class for License tests"""
+    def assert_equal_license(self, lic_1, lic_2):
+        """
+        Asserts that lic_1 and lic_2 are both licenses of the same
+        type, with the same versions as applicable.
+        """
+        # Assert that lic_1 and lic_2 are both License types,
+        # otherwise further checks will fail
+        self.assertTrue(isinstance(lic_1, License))
+        self.assertTrue(isinstance(lic_2, License))
+        # Assert licenses are of the same type
+        self.assertEqual(lic_1.kind, lic_2.kind)
+        # If they're not of the ARR type license, they're versioned, so
+        # assert that their versions are also equal
+        if not lic_1.kind == "ARR":
+            self.assertEqual(lic_1.version, lic_2.version)
 
-
-class LicenseTest(unittest.TestCase):
+class LicenseTest(BaseLicenseTest):
     """Tests for License class."""
 
     def setUp(self):
@@ -43,11 +44,11 @@ class LicenseTest(unittest.TestCase):
             License().to_json(self.invalid_dict)
 
     def test_from_json(self):
-        assert_equal_license(self, License().from_json(None), self.default_license)
-        assert_equal_license(self, License().from_json(""), self.default_license)
+        self.assert_equal_license(License().from_json(None), self.default_license)
+        self.assert_equal_license(License().from_json(""), self.default_license)
 
 
-class ARRLicenseTest(unittest.TestCase):
+class ARRLicenseTest(BaseLicenseTest):
     """Tests for All Rights Reserved License class."""
 
     def setUp(self):
@@ -57,10 +58,10 @@ class ARRLicenseTest(unittest.TestCase):
         self.assertEqual(self.arr_license.html, "&copy;<span class='license-text'>All rights reserved</span>")
 
     def test_from_json(self):
-        assert_equal_license(self, License().from_json("ARR"), self.arr_license)
+        self.assert_equal_license(License().from_json("ARR"), self.arr_license)
 
 
-class CCLicenseTest(unittest.TestCase):
+class CCLicenseTest(BaseLicenseTest):
     """Tests for Creative Commons License class."""
 
     def setUp(self):
@@ -77,75 +78,79 @@ class CCLicenseTest(unittest.TestCase):
 
     def test_html(self):
         # URL should be correct
-        self.assertTrue("creativecommons.org/licenses/by-nc-sa/4.0/" in self.cc_by_nc_sa_license.html)
+        self.assertIn(
+            "creativecommons.org/licenses/by-nc-sa/4.0/",
+            self.cc_by_nc_sa_license.html,
+            msg="Expected to see license URL, but received " + self.cc_by_nc_sa_license.html
+        )
 
         # CC icon should be there
-        self.assertTrue("class='icon-cc" in self.cc0_license.html)
+        self.assertIn("class='icon-cc", self.cc0_license.html)
 
         # ZERO icon should be there
-        self.assertTrue("icon-cc-zero" in self.cc0_license.html)
+        self.assertIn("icon-cc-zero", self.cc0_license.html)
 
         # BY icon should be there
-        self.assertTrue("icon-cc-by" in self.cc_by_license.html)
-        self.assertTrue("icon-cc-by" in self.cc_by_sa_license.html)
-        self.assertTrue("icon-cc-by" in self.cc_by_nd_license.html)
-        self.assertTrue("icon-cc-by" in self.cc_by_nc_license.html)
-        self.assertTrue("icon-cc-by" in self.cc_by_nc_sa_license.html)
-        self.assertTrue("icon-cc-by" in self.cc_by_nc_nd_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_sa_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_nd_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_nc_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_nc_sa_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_nc_nd_license.html)
 
         # NC icon should be there
-        self.assertTrue("icon-cc-nc" in self.cc_by_nc_license.html)
-        self.assertTrue("icon-cc-nc" in self.cc_by_nc_nd_license.html)
+        self.assertIn("icon-cc-nc", self.cc_by_nc_license.html)
+        self.assertIn("icon-cc-nc", self.cc_by_nc_nd_license.html)
 
         # SA icon should be there
-        self.assertTrue("icon-cc-sa" in self.cc_by_sa_license.html)
-        self.assertTrue("icon-cc-sa" in self.cc_by_nc_sa_license.html)
+        self.assertIn("icon-cc-sa", self.cc_by_sa_license.html)
+        self.assertIn("icon-cc-sa", self.cc_by_nc_sa_license.html)
 
         # ND icon should be there
-        self.assertTrue("icon-cc-by" in self.cc_by_nd_license.html)
-        self.assertTrue("icon-cc-by" in self.cc_by_nc_nd_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_nd_license.html)
+        self.assertIn("icon-cc-by", self.cc_by_nc_nd_license.html)
 
     def test_from_json(self):
-        assert_equal_license(self, License().from_json("CC0"), self.cc0_license)
-        assert_equal_license(self, License().from_json("CC-BY"), self.cc_by_license)
-        assert_equal_license(self, License().from_json("CC-BY-SA"), self.cc_by_sa_license)
-        assert_equal_license(self, License().from_json("CC-BY-ND"), self.cc_by_nd_license)
-        assert_equal_license(self, License().from_json("CC-BY-NC"), self.cc_by_nc_license)
-        assert_equal_license(self, License().from_json("CC-BY-NC-SA"), self.cc_by_nc_sa_license)
-        assert_equal_license(self, License().from_json("CC-BY-NC-ND"), self.cc_by_nc_nd_license)
+        self.assert_equal_license(License().from_json("CC0"), self.cc0_license)
+        self.assert_equal_license(License().from_json("CC-BY"), self.cc_by_license)
+        self.assert_equal_license(License().from_json("CC-BY-SA"), self.cc_by_sa_license)
+        self.assert_equal_license(License().from_json("CC-BY-ND"), self.cc_by_nd_license)
+        self.assert_equal_license(License().from_json("CC-BY-NC"), self.cc_by_nc_license)
+        self.assert_equal_license(License().from_json("CC-BY-NC-SA"), self.cc_by_nc_sa_license)
+        self.assert_equal_license(License().from_json("CC-BY-NC-ND"), self.cc_by_nc_nd_license)
 
     def test_description(self):
         # BY text should be there
-        self.assertTrue("Attribution" in self.cc_by_license.description)
-        self.assertTrue("Attribution" in self.cc_by_sa_license.description)
-        self.assertTrue("Attribution" in self.cc_by_nd_license.description)
-        self.assertTrue("Attribution" in self.cc_by_nc_license.description)
-        self.assertTrue("Attribution" in self.cc_by_nc_sa_license.description)
-        self.assertTrue("Attribution" in self.cc_by_nc_nd_license.description)
+        self.assertIn("Attribution", self.cc_by_license.description)
+        self.assertIn("Attribution", self.cc_by_sa_license.description)
+        self.assertIn("Attribution", self.cc_by_nd_license.description)
+        self.assertIn("Attribution", self.cc_by_nc_license.description)
+        self.assertIn("Attribution", self.cc_by_nc_sa_license.description)
+        self.assertIn("Attribution", self.cc_by_nc_nd_license.description)
 
         # NC text should be there
-        self.assertTrue("NonCommercial" in self.cc_by_nc_license.description)
-        self.assertTrue("NonCommercial" in self.cc_by_nc_nd_license.description)
+        self.assertIn("NonCommercial", self.cc_by_nc_license.description)
+        self.assertIn("NonCommercial", self.cc_by_nc_nd_license.description)
 
         # SA text should be there
-        self.assertTrue("ShareAlike" in self.cc_by_sa_license.description)
-        self.assertTrue("ShareAlike" in self.cc_by_nc_sa_license.description)
+        self.assertIn("ShareAlike", self.cc_by_sa_license.description)
+        self.assertIn("ShareAlike", self.cc_by_nc_sa_license.description)
 
         # ND text should be there
-        self.assertTrue("NonDerivatives" in self.cc_by_nd_license.description)
-        self.assertTrue("NonDerivatives" in self.cc_by_nc_nd_license.description)
+        self.assertIn("NonDerivatives", self.cc_by_nd_license.description)
+        self.assertIn("NonDerivatives", self.cc_by_nc_nd_license.description)
 
 
-class ParseLicenseTest(unittest.TestCase):
+class ParseLicenseTest(BaseLicenseTest):
     """Tests for license parser."""
 
     def test_parse_license(self):
-        assert_equal_license(self, parse_license(None), ARRLicense())
-        assert_equal_license(self, parse_license(""), ARRLicense())
-        assert_equal_license(self, parse_license("ARR"), ARRLicense())
-        assert_equal_license(self, parse_license("CC0"), CCLicense("CC0", "4.0"))
-        assert_equal_license(self, parse_license("CC-BY"), CCLicense("CC-BY", "4.0"))
-        assert_equal_license(self, parse_license({"license": "CC-BY", "version": "4.0"}), CCLicense("CC-BY", "4.0"))
-        assert_equal_license(self, parse_license({"kind": "CC-BY", "version": "4.0"}), CCLicense("CC-BY", "4.0"))
+        self.assert_equal_license(parse_license(None), ARRLicense())
+        self.assert_equal_license(parse_license(""), ARRLicense())
+        self.assert_equal_license(parse_license("ARR"), ARRLicense())
+        self.assert_equal_license(parse_license("CC0"), CCLicense("CC0", "4.0"))
+        self.assert_equal_license(parse_license("CC-BY"), CCLicense("CC-BY", "4.0"))
+        self.assert_equal_license(parse_license({"license": "CC-BY", "version": "4.0"}), CCLicense("CC-BY", "4.0"))
+        self.assert_equal_license(parse_license({"kind": "CC-BY", "version": "4.0"}), CCLicense("CC-BY", "4.0"))
         with self.assertRaises(ValueError):
             parse_license({"kind": "CC-BY"})
