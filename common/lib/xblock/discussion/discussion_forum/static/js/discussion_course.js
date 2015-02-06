5a3e5b836d903d04100cfdf3020b09db12639726
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-02-06 11:33:36 +0300

    Tweaked discussion xblocks to get rid of local overrides

--- common/lib/xblock/discussion/discussion_forum/static/js/discussion_course.js
+++ common/lib/xblock/discussion/discussion_forum/static/js/discussion_course.js
@@ -1,17 +1,14 @@
 var $$course_id = "{{course_id}}";
 
 function DiscussionCourseBlock(runtime, element) {
-  var el = $(element).find('section.discussion');
+    var el = $(element).find('section.discussion');
 
-  var testUrl = runtime.handlerUrl(element, 'test');
-  if (testUrl.match(/^(http|https):\/\//)) {
-    var hostname = testUrl.match(/^(.*:\/\/[a-z0-9:\-.]+)\//)[1];
-    DiscussionUtil.setBaseUrl(hostname);
-  }
+    var testUrl = runtime.handlerUrl(element, 'test');
+    if (testUrl.match(/^(http|https):\/\//)) {
+        var hostname = testUrl.match(/^(.*:\/\/[a-z0-9:\-.]+)\//)[1];
+        DiscussionUtil.setBaseUrl(hostname);
+    }
 
-  if (runtime.local_overrides && runtime.local_overrides.discussion) {
-      runtime.local_overrides.discussion(element, DiscussionUtil);
-  }
-
-  DiscussionApp.start(el);
+    DiscussionUtil.force_async = true;
+    DiscussionApp.start(el);
 }
--- common/lib/xblock/discussion/discussion_forum/static/js/discussion_inline.js
+++ common/lib/xblock/discussion/discussion_forum/static/js/discussion_inline.js
@@ -1,17 +1,14 @@
 var $$course_id = "{{course_id}}";
 
 function DiscussionInlineBlock(runtime, element) {
-  var el = $(element).find('.discussion-module');
+    var el = $(element).find('.discussion-module');
 
-  var testUrl = runtime.handlerUrl(element, 'test');
-  if (testUrl.match(/^(http|https):\/\//)) {
-    var hostname = testUrl.match(/^(.*:\/\/[a-z0-9:\-.]+)\//)[1];
-    DiscussionUtil.setBaseUrl(hostname);
-  }
+    var testUrl = runtime.handlerUrl(element, 'test');
+    if (testUrl.match(/^(http|https):\/\//)) {
+        var hostname = testUrl.match(/^(.*:\/\/[a-z0-9:\-.]+)\//)[1];
+        DiscussionUtil.setBaseUrl(hostname);
+    }
 
-  if (runtime.local_overrides && runtime.local_overrides.discussion) {
-      runtime.local_overrides.discussion(element, DiscussionUtil);
-  }
-
-  new DiscussionModuleView({ el: el });
+    DiscussionUtil.force_async = true;
+    new DiscussionModuleView({ el: el });
 }
