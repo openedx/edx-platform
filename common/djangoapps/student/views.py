12f89b3c2d7a5212eb63f36fadf1b8fd3aaeb933
Greg Price <gprice@edx.org>
2015-02-09 12:13:35 -0500

    Make registration endpoint CSRF exempt

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -1370,7 +1370,7 @@ def _do_create_account(post_vars, extended_profile=None):
     return (user, profile, registration)
 
 
-@ensure_csrf_cookie
+@csrf_exempt
 def create_account(request, post_override=None):  # pylint: disable-msg=too-many-statements
     """
     JSON call to create new edX account.
