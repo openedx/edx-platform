2e3b2786888bdb9b1e51d2ab08315607f56fe0ac
Giulio Gratta <giulio@giuliogratta.com>
2015-02-09 16:17:47 -0800

    Making edX changes to pdf viewer/analytics html

--- lms/static/js/pdf-analytics.js
+++ lms/static/js/pdf-analytics.js
@@ -13,10 +13,10 @@ $(function() {
 
   $(window).bind("pagechange", function(event) {
     // log every page render
-    var page = event.originalEvent.pageNumber;
-    var old_page = PDFView.previousPageNumber;
+    var page = PDFViewerApplication.page;
+    var old_page = event.originalEvent.previousPageNumber;
     // pagechange is called many times per viewing.
-    if (PDFView.previousPageNumber !== page || first_page) {
+    if (old_page !== page || first_page) {
       first_page = false;
       if ((event.timeStamp - scroll.timeStamp) < 50) {
         sendLog("page.scrolled", {"page": page, "direction": scroll.direction});
@@ -28,12 +28,12 @@ $(function() {
 
   $('#viewerContainer').bind('DOMMouseScroll mousewheel', function(event) {
     scroll.timeStamp = event.timeStamp;
-    scroll.direction = PDFView.pageViewScroll.down ? "down" : "up";
+    scroll.direction = PDFViewerApplication.pdfViewer.scroll.down ? "down" : "up";
   });
 });
 
 $('#viewThumbnail,#sidebarToggle').on('click', function() {
-    sendLog("thumbnails.toggled", {"page": PDFView.page});
+    sendLog("thumbnails.toggled", {"page": PDFViewerApplication.page});
   });
 
 $('#thumbnailView a').live('click', function(){
@@ -41,19 +41,19 @@ $('#thumbnailView a').live('click', function(){
 });
 
 $('#viewOutline').on('click', function() {
-    sendLog("outline.toggled", {"page": PDFView.page});
+    sendLog("outline.toggled", {"page": PDFViewerApplication.page});
   });
 
 $('#previous').on('click', function() {
-    sendLog("page.navigatednext", {"type": "prevpage", "new": PDFView.page - 1}, "book");
+    sendLog("page.navigatednext", {"type": "prevpage", "new": PDFViewerApplication.page - 1}, "book");
   });
 
 $('#next').on('click', function() {
-    sendLog("page.navigatednext", {"type": "nextpage", "new": PDFView.page + 1}, "book");
+    sendLog("page.navigatednext", {"type": "nextpage", "new": PDFViewerApplication.page + 1}, "book");
   });
 
 $('#zoomIn,#zoomOut').on('click', function() {
-    sendLog("zoom.buttons.changed", {"direction": $(this).attr("id") == "zoomIn" ? "in" : "out", "page": PDFView.page});
+    sendLog("zoom.buttons.changed", {"direction": $(this).attr("id") == "zoomIn" ? "in" : "out", "page": PDFViewerApplication.page});
   });
 
 $('#pageNumber').on('change', function() {
@@ -61,16 +61,16 @@ $('#pageNumber').on('change', function() {
   });
 
 var old_amount = 1;
-$(window).bind('scalechange', function(evt) {
-  var amount = evt.originalEvent.scale;
+$(window).bind('scalechange', function(event) {
+  var amount = event.originalEvent.scale;
   if (amount !== old_amount) {
-    sendLog("display.scaled", {"amount": amount, "page": PDFView.page});
+    sendLog("display.scaled", {"amount": amount, "page": PDFViewerApplication.page});
     old_amount = amount;
   }
 });
 
 $('#scaleSelect').on('change', function() {
-  sendLog("zoom.menu.changed", {"amount": $("#scaleSelect").val(), "page": PDFView.page});
+  sendLog("zoom.menu.changed", {"amount": $("#scaleSelect").val(), "page": PDFViewerApplication.page});
 });
 
 var search_event = null;
@@ -81,7 +81,7 @@ $(window).bind("find findhighlightallchange findagain findcasesensitivitychange"
   search_event = setTimeout(function(){
     var message = event.originalEvent.detail;
     message.status = $('#findMsg').text();
-    message.page = PDFView.page;
+    message.page = PDFViewerApplication.page;
     var event_name = "search";
     switch (event.type) {
       case "find":
