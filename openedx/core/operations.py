c4a63a5eff5a467a522199c649d57dc519647bcd
stv <stv@stanford.edu>
2015-02-15 16:24:38 -0800

    Fix PEP8: E302 expected 2 blank lines

--- openedx/core/operations.py
+++ openedx/core/operations.py
@@ -10,6 +10,7 @@ def dump_memory(signum, frame):
     """Dump memory stats for the current process to a temp directory. Uses the meliae output format."""
     scanner.dump_all_objects('{}/meliae.{}.{}.dump'.format(tempfile.gettempdir(), datetime.now().isoformat(), os.getpid()))
 
+
 def install_memory_dumper(dump_signal=signal.SIGPROF):
     """
     Install a signal handler on `signal` to dump memory stats for the current process.
