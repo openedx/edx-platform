4368726740157a0e9838b67fde23b727a499f39c
stv <stv@stanford.edu>
2015-02-16 07:54:01 -0800

    Fix Pylint: C0202(bad-classmethod-argument)

--- lms/djangoapps/instructor_task/subtasks.py
+++ lms/djangoapps/instructor_task/subtasks.py
@@ -165,7 +165,7 @@ class SubtaskStatus(object):
         self.state = state if state is not None else QUEUING
 
     @classmethod
-    def from_dict(self, d):
+    def from_dict(cls, d):
         """Construct a SubtaskStatus object from a dict representation."""
         options = dict(d)
         task_id = options['task_id']
@@ -173,9 +173,9 @@ class SubtaskStatus(object):
         return SubtaskStatus.create(task_id, **options)
 
     @classmethod
-    def create(self, task_id, **options):
+    def create(cls, task_id, **options):
         """Construct a SubtaskStatus object."""
-        return self(task_id, **options)
+        return cls(task_id, **options)
 
     def to_dict(self):
         """
