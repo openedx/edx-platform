ef420ffd7c5339ca3e9a8dd5a042b7978817b669
stv <stv@stanford.edu>
2015-02-15 16:31:35 -0800

    Fix PEP8: E127 continuation line over-indented
    for visual indent

--- pavelib/paver_tests/test_paver_bok_choy_cmds.py
+++ pavelib/paver_tests/test_paver_bok_choy_cmds.py
@@ -14,15 +14,20 @@ class TestPaverBokChoyCmd(unittest.TestCase):
     def _expected_command(self, expected_text_append, expected_default_store=None):
         if expected_text_append:
             expected_text_append = "/" + expected_text_append
-        expected_statement = ("DEFAULT_STORE={default_store} SCREENSHOT_DIR='{repo_dir}/test_root/log' "
-                                       "BOK_CHOY_HAR_DIR='{repo_dir}/test_root/log/hars' "
-                                       "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' "
-                                       "nosetests {repo_dir}/common/test/acceptance/tests{exp_text} "
-                                       "--with-xunit "
-                                       "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
-                                       "--verbosity=2 ".format(default_store=expected_default_store,
-                                                               repo_dir=REPO_DIR,
-                                                               exp_text=expected_text_append))
+        expected_statement = (
+            "DEFAULT_STORE={default_store} "
+            "SCREENSHOT_DIR='{repo_dir}/test_root/log' "
+            "BOK_CHOY_HAR_DIR='{repo_dir}/test_root/log/hars' "
+            "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' "
+            "nosetests {repo_dir}/common/test/acceptance/tests{exp_text} "
+            "--with-xunit "
+            "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
+            "--verbosity=2 "
+        ).format(
+            default_store=expected_default_store,
+            repo_dir=REPO_DIR,
+            exp_text=expected_text_append,
+        )
         return expected_statement
 
     def test_default_bokchoy(self):
