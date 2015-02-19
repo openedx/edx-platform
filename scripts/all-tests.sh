35b6e15d9fa9bb55533d1386347d86d40f44f764
Christine Lytwynec <clytwynec@edx.org>
2015-02-19 12:24:46 -0500

    add --with-flaky in jenkins script

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -95,17 +95,17 @@ END
     "unit")
         case "$SHARD" in
             "lms")
-                paver test_system -s lms
+                paver test_system -s lms --extra_args="--with-flaky"
                 paver coverage
                 ;;
             "cms-js-commonlib")
-                paver test_system -s cms
+                paver test_system -s cms --extra_args="--with-flaky"
                 paver test_js --coverage --skip_clean
-                paver test_lib --skip_clean
+                paver test_lib --skip_clean --extra_args="--with-flaky"
                 paver coverage
                 ;;
             *)
-                paver test
+                paver test --extra_args="--with-flaky"
                 paver coverage
                 ;;
         esac
@@ -162,19 +162,19 @@ END
                 ;;
 
             "1")
-                paver test_bokchoy --extra_args="-a shard_1"
+                paver test_bokchoy --extra_args="-a shard_1 --with-flaky"
                 ;;
 
             "2")
-                paver test_bokchoy --extra_args="-a 'shard_2'"
+                paver test_bokchoy --extra_args="-a 'shard_2' --with-flaky"
                 ;;
 
             "3")
-                paver test_bokchoy --extra_args="-a 'shard_3'"
+                paver test_bokchoy --extra_args="-a 'shard_3' --with-flaky"
                 ;;
 
             "4")
-                paver test_bokchoy --extra_args="-a shard_1=False,shard_2=False,shard_3=False"
+                paver test_bokchoy --extra_args="-a shard_1=False,shard_2=False,shard_3=False --with-flaky"
                 ;;
 
             # Default case because if we later define another bok-choy shard on Jenkins
