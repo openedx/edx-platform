90dd881871bdd638260166b562ea31a0d8a25d80
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-02-19 12:55:23 +0500

    renaming the topicView.js
    TNL-1256

--- lms/static/js/discussion_topics/views/TopicsView.js
+++ lms/static/js/discussion_topics/views/TopicsView.js
@@ -9,15 +9,14 @@ var edx = edx || {};
     edx.discussions = edx.discussions || {};
 
     edx.discussions.DiscussionTopicsView = Backbone.View.extend({
-        //events : {
-        //    'click .toggle-cohort-management-discussions': 'showDiscussionTopics'
-        //},
+        events : {
+            'check .discussion-topic': 'toggleTopicCheck'
+        },
 
         initialize: function(options) {
             //var model = this.model;
 
             this.template = _.template($('#cohort-discussion-topics-tpl').text());
-            //this.selectorTemplate = _.template($('#cohort-selector-tpl').text());
             this.context = options.context;
 
             //model.on('sync', this.onSync, this);
@@ -36,35 +35,11 @@ var edx = edx || {};
             }));
             return this;
         },
-
-        showDiscussionTopics: function(event) {
+        toggleTopicCheck: function (event) {
             event.preventDefault();
-
-            $(event.currentTarget).addClass(hiddenClass);
-            var topicsElement = this.$('.discussion-topics').removeClass(hiddenClass);
-
-            this.template('cohort-discussion-topics').render();
-            //if (!this.fileUploaderView) {
-            //    this.fileUploaderView = new FileUploaderView({
-            //        el: uploadElement,
-            //        title: gettext("Assign students to cohorts by uploading a CSV file."),
-            //        inputLabel: gettext("Choose a .csv file"),
-            //        inputTip: gettext("Only properly formatted .csv files will be accepted."),
-            //        submitButtonText: gettext("Upload File and Assign Students"),
-            //        extensions: ".csv",
-            //        url: this.context.uploadCohortsCsvUrl,
-            //        successNotification: function (file, event, data) {
-            //            var message = interpolate_text(gettext(
-            //                "Your file '{file}' has been uploaded. Allow a few minutes for processing."
-            //            ), {file: file});
-            //            return new NotificationModel({
-            //                type: "confirmation",
-            //                title: message
-            //            });
-            //        }
-            //    }).render();
-            //}
+        },
+        getCohortState: function() {
+            return this.$('.discussion-topic').prop('checked');
         }
-
     });
 }).call(this, $, _, Backbone, gettext, interpolate_text);
