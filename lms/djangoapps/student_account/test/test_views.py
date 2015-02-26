7dc0598b2527d338ca74358048f40f52bd265c14
cahrens <christina@edx.org>
2015-02-26 15:18:50 -0500

    Delete ENABLE_NEW_DASHBOARD code.

--- lms/djangoapps/student_account/test/test_views.py
+++ lms/djangoapps/student_account/test/test_views.py
@@ -59,7 +59,6 @@ class StudentAccountUpdateTest(UrlResetMixin, TestCase):
 
     INVALID_KEY = u"123abc"
 
-    @mock.patch.dict(settings.FEATURES, {'ENABLE_NEW_DASHBOARD': True})
     def setUp(self):
         super(StudentAccountUpdateTest, self).setUp("student_account.urls")
 
@@ -70,141 +69,6 @@ class StudentAccountUpdateTest(UrlResetMixin, TestCase):
         # Login
         result = self.client.login(username=self.USERNAME, password=self.OLD_PASSWORD)
         self.assertTrue(result)
-
-    def test_index(self):
-        response = self.client.get(reverse('account_index'))
-        self.assertContains(response, "Student Account")
-
-    def test_change_email(self):
-        response = self._change_email(self.NEW_EMAIL, self.OLD_PASSWORD)
-        self.assertEquals(response.status_code, 200)
-
-        # Verify that the email associated with the account remains unchanged
-        profile_info = profile_api.profile_info(self.USERNAME)
-        self.assertEquals(profile_info['email'], self.OLD_EMAIL)
-
-        # Check that an email was sent with the activation key
-        self.assertEqual(len(mail.outbox), 1)
-        self._assert_email(
-            mail.outbox[0],
-            [self.NEW_EMAIL],
-            u"Email Change Request",
-            u"There was recently a request to change the email address"
-        )
-
-        # Retrieve the activation key from the email
-        email_body = mail.outbox[0].body
-        result = re.search('/email/confirmation/([^ \n]+)', email_body)
-        self.assertIsNot(result, None)
-        activation_key = result.group(1)
-
-        # Attempt to activate the email
-        response = self.client.get(reverse('email_change_confirm', kwargs={'key': activation_key}))
-        self.assertEqual(response.status_code, 200)
-
-        # Verify that the email was changed
-        profile_info = profile_api.profile_info(self.USERNAME)
-        self.assertEquals(profile_info['email'], self.NEW_EMAIL)
-
-        # Verify that notification emails were sent
-        self.assertEqual(len(mail.outbox), 2)
-        self._assert_email(
-            mail.outbox[1],
-            [self.OLD_EMAIL, self.NEW_EMAIL],
-            u"Email Change Successful",
-            u"You successfully changed the email address"
-        )
-
-    def test_email_change_wrong_password(self):
-        response = self._change_email(self.NEW_EMAIL, "wrong password")
-        self.assertEqual(response.status_code, 401)
-
-    def test_email_change_request_no_user(self):
-        # Patch account API to raise an internal error when an email change is requested
-        with mock.patch('student_account.views.account_api.request_email_change') as mock_call:
-            mock_call.side_effect = account_api.AccountUserNotFound
-            response = self._change_email(self.NEW_EMAIL, self.OLD_PASSWORD)
-
-        self.assertEquals(response.status_code, 400)
-
-    def test_email_change_request_email_taken_by_active_account(self):
-        # Create/activate a second user with the new email
-        activation_key = account_api.create_account(self.ALTERNATE_USERNAME, self.OLD_PASSWORD, self.NEW_EMAIL)
-        account_api.activate_account(activation_key)
-
-        # Request to change the original user's email to the email now used by the second user
-        response = self._change_email(self.NEW_EMAIL, self.OLD_PASSWORD)
-        self.assertEquals(response.status_code, 409)
-
-    def test_email_change_request_email_taken_by_inactive_account(self):
-        # Create a second user with the new email, but don't active them
-        account_api.create_account(self.ALTERNATE_USERNAME, self.OLD_PASSWORD, self.NEW_EMAIL)
-
-        # Request to change the original user's email to the email used by the inactive user
-        response = self._change_email(self.NEW_EMAIL, self.OLD_PASSWORD)
-        self.assertEquals(response.status_code, 200)
-
-    @ddt.data(*INVALID_EMAILS)
-    def test_email_change_request_email_invalid(self, invalid_email):
-        # Request to change the user's email to an invalid address
-        response = self._change_email(invalid_email, self.OLD_PASSWORD)
-        self.assertEquals(response.status_code, 400)
-
-    def test_email_change_confirmation(self):
-        # Get an email change activation key
-        activation_key = account_api.request_email_change(self.USERNAME, self.NEW_EMAIL, self.OLD_PASSWORD)
-
-        # Follow the link sent in the confirmation email
-        response = self.client.get(reverse('email_change_confirm', kwargs={'key': activation_key}))
-        self.assertContains(response, "Email change successful")
-
-        # Verify that the email associated with the account has changed
-        profile_info = profile_api.profile_info(self.USERNAME)
-        self.assertEquals(profile_info['email'], self.NEW_EMAIL)
-
-    def test_email_change_confirmation_invalid_key(self):
-        # Visit the confirmation page with an invalid key
-        response = self.client.get(reverse('email_change_confirm', kwargs={'key': self.INVALID_KEY}))
-        self.assertContains(response, "Something went wrong")
-
-        # Verify that the email associated with the account has not changed
-        profile_info = profile_api.profile_info(self.USERNAME)
-        self.assertEquals(profile_info['email'], self.OLD_EMAIL)
-
-    def test_email_change_confirmation_email_already_exists(self):
-        # Get an email change activation key
-        email_activation_key = account_api.request_email_change(self.USERNAME, self.NEW_EMAIL, self.OLD_PASSWORD)
-
-        # Create/activate a second user with the new email
-        account_activation_key = account_api.create_account(self.ALTERNATE_USERNAME, self.OLD_PASSWORD, self.NEW_EMAIL)
-        account_api.activate_account(account_activation_key)
-
-        # Follow the link sent to the original user
-        response = self.client.get(reverse('email_change_confirm', kwargs={'key': email_activation_key}))
-        self.assertContains(response, "address you wanted to use is already used")
-
-        # Verify that the email associated with the original account has not changed
-        profile_info = profile_api.profile_info(self.USERNAME)
-        self.assertEquals(profile_info['email'], self.OLD_EMAIL)
-
-    def test_email_change_confirmation_internal_error(self):
-        # Get an email change activation key
-        activation_key = account_api.request_email_change(self.USERNAME, self.NEW_EMAIL, self.OLD_PASSWORD)
-
-        # Patch account API to return an internal error
-        with mock.patch('student_account.views.account_api.confirm_email_change') as mock_call:
-            mock_call.side_effect = account_api.AccountInternalError
-            response = self.client.get(reverse('email_change_confirm', kwargs={'key': activation_key}))
-
-        self.assertContains(response, "Something went wrong")
-
-    def test_email_change_request_missing_email_param(self):
-        response = self._change_email(None, self.OLD_PASSWORD)
-        self.assertEqual(response.status_code, 400)
-
-    def test_email_change_request_missing_password_param(self):
-        response = self._change_email(self.OLD_EMAIL, None)
-        self.assertEqual(response.status_code, 400)
 
     @skipUnless(settings.ROOT_URLCONF == 'lms.urls', 'Test only valid in LMS')
     def test_password_change(self):
@@ -316,25 +180,6 @@ class StudentAccountUpdateTest(UrlResetMixin, TestCase):
         self.assertEqual(response.status_code, 403)
 
     @ddt.data(
-        ('get', 'account_index', []),
-        ('post', 'email_change_request', []),
-        ('get', 'email_change_confirm', [123])
-    )
-    @ddt.unpack
-    def test_require_login(self, method, url_name, args):
-        # Access the page while logged out
-        self.client.logout()
-        url = reverse(url_name, args=args)
-        response = getattr(self.client, method)(url, follow=True)
-
-        # Should have been redirected to the login page
-        self.assertEqual(len(response.redirect_chain), 1)
-        self.assertIn('accounts/login?next=', response.redirect_chain[0][0])
-
-    @ddt.data(
-        ('get', 'account_index', []),
-        ('post', 'email_change_request', []),
-        ('get', 'email_change_confirm', [123]),
         ('post', 'password_change_request', []),
     )
     @ddt.unpack
@@ -345,24 +190,6 @@ class StudentAccountUpdateTest(UrlResetMixin, TestCase):
         for method in wrong_methods:
             response = getattr(self.client, method)(url)
             self.assertEqual(response.status_code, 405)
-
-    def _assert_email(self, email, expected_to, expected_subject, expected_body):
-        """Check whether an email has the correct properties. """
-        self.assertEqual(email.to, expected_to)
-        self.assertIn(expected_subject, email.subject)
-        self.assertIn(expected_body, email.body)
-
-    def _change_email(self, new_email, password):
-        """Request to change the user's email. """
-        data = {}
-
-        if new_email is not None:
-            data['email'] = new_email
-        if password is not None:
-            # We can't pass a Unicode object to urlencode, so we encode the Unicode object
-            data['password'] = password.encode('utf-8')
-
-        return self.client.post(path=reverse('email_change_request'), data=data)
 
     def _change_password(self, email=None):
         """Request to change the user's password. """
