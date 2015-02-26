e9ad4b9d2ffd249d1df69914ff9797ee00b6cc16
Brian Talbot <btalbot@edx.org>
2015-02-26 15:09:11 -0500

    Bourbon: resolving xmodule's reference to deprecated button mixin (for the short future)

--- common/lib/xmodule/xmodule/static_content.py
+++ common/lib/xmodule/xmodule/static_content.py
@@ -98,6 +98,7 @@ def _write_styles(selector, output_root, classes):
     module_styles_lines = []
     module_styles_lines.append("@import '../../../sass/vendor/bourbon/bourbon';")
     module_styles_lines.append("@import '../../../sass/vendor/bourbon/bourbon-deprecated-upcoming';")
+short future)
     module_styles_lines.append("@import 'assets/anims';")
     for class_, fragment_names in css_imports.items():
         module_styles_lines.append("""{selector}.xmodule_{class_} {{""".format(
