66a03ac6a38bbfce3508f91842d277428076a144
cahrens <christina@edx.org>
2015-02-26 15:18:50 -0500

    Delete ENABLE_NEW_DASHBOARD code.

--- lms/djangoapps/student_account/urls.py
+++ lms/djangoapps/student_account/urls.py
@@ -11,11 +11,3 @@ if settings.FEATURES.get('ENABLE_COMBINED_LOGIN_REGISTRATION'):
         url(r'^register/$', 'login_and_registration_form', {'initial_mode': 'register'}, name='account_register'),
         url(r'^password$', 'password_change_request_handler', name='password_change_request'),
     )
-
-if settings.FEATURES.get('ENABLE_NEW_DASHBOARD'):
-    urlpatterns += patterns(
-        'student_account.views',
-        url(r'^$', 'index', name='account_index'),
-        url(r'^email$', 'email_change_request_handler', name='email_change_request'),
-        url(r'^email/confirmation/(?P<key>[^/]*)$', 'email_change_confirmation_handler', name='email_change_confirm'),
-    )
