8462b2e1f80540a24a0decb3486cfa88174e113c
Brian Talbot <btalbot@edx.org>
2015-02-26 10:45:11 -0500

    adding studio-styles config

--- gulp/config.js
+++ gulp/config.js
@@ -1,11 +1,11 @@
-var studio_dest = '';
-var studio_src = '';
-var lms_dest = '';
-var lms_src = '';
+var studio_src = './cms';
+var studio_dest = './cms';
+var lms_src = './lms';
+var lms_dest = './lms';
 
 module.exports = {
-  studio_styles: {
-    src: '',
-    dest: ''
-  }
+    studio_styles: {
+        src: studio_src + '/static/sass/**/*.scss',
+        dest: studio_dest + '/static/sass/',
+    }
 };
