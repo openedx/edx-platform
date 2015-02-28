015053cb9b8c3e6368ad8bc9385ea4ddf2b7df7f
stv <stv@stanford.edu>
2015-02-28 00:51:25 -0800

    Remove antiquated LMS envs files
    These all date back to 2012 and are hard-coded for specific course runs.

--- lms/envs/devgroups/m_6002.py
+++
@@ -1,8 +0,0 @@
-
-# We intentionally define lots of variables that aren't used, and
-# want to import all variables from base settings files
-# pylint: disable=wildcard-import, unused-wildcard-import
-
-from .courses import *
-
-DATABASES = course_db_for('MITx/6.002x/2012_Fall')
