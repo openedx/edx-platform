4493b216574e7a795fc05c6e9da8d70c1a1fff11
aamir-khan <aamir@edx.org>
2015-03-04 15:09:57 +0500

    ECOM-1124: change the create_order method

--- common/djangoapps/course_modes/models.py
+++ common/djangoapps/course_modes/models.py
@@ -395,7 +395,9 @@ class CourseMode(models.Model):
 
     @classmethod
     def is_professional_mode(cls, course_mode_tuple):
-        return course_mode_tuple.slug == 'professional' or course_mode_tuple.slug == cls.NO_ID_PROFESSIONAL_MODES[0]
+        if course_mode_tuple:
+            return course_mode_tuple.slug == 'professional' or course_mode_tuple.slug == cls.NO_ID_PROFESSIONAL_MODES[0]
+        return False
 
     def to_tuple(self):
         """
