f6b6b6b0c86a55605cc6aef5d0b374d912c49976
Usman Khalid <2200617@gmail.com>
2015-03-06 19:45:01 +0500

    Basic structure of fields.

--- lms/static/js/student_account/views/AccountSettingsView.js
+++ lms/static/js/student_account/views/AccountSettingsView.js
@@ -17,15 +17,13 @@ var edx = edx || {};
         },
 
         render: function() {
-            console.log(this);
             this.$el.html(this.template({
                 sections: this.options.sections,
             }));
+
             var view = this;
             _.each(this.$('.account-settings-section-body'), function(section_el, index) {
-                console.log(section_el);
                 _.each(view.options.sections[index].fields, function(field, index) {
-                    console.log(field);
                     $(section_el).append(field.view.el);
                 });
             });
@@ -33,22 +31,99 @@ var edx = edx || {};
         }
     });
 
-    edx.student.account.ReadonlyFieldView = Backbone.View.extend({
+    edx.student.account.FieldView = Backbone.View.extend({
+
+        className: "account-settings-field",
+        tagName: "div",
+
+    });
+
+    edx.student.account.ReadonlyFieldView = edx.student.account.FieldView.extend({
 
         template: _.template($('#field_readonly-tpl').text()),
 
         events: {
+
         },
 
         initialize: function(options) {
+            this.listenTo(this.model, "change", this.render);
         },
 
         render: function() {
             this.$el.html(this.template({
+                title: this.options.title,
+                value: this.model[this.options.attribute],
+                message: this.options.message,
             }));
             return this;
         }
     });
 
+    edx.student.account.TextFieldView = edx.student.account.FieldView.extend({
+
+        template: _.template($('#field_text-tpl').text()),
+
+        events: {
+
+        },
+
+        initialize: function(options) {
+            this.listenTo(this.model, "change", this.render);
+        },
+
+        render: function() {
+            this.$el.html(this.template({
+                title: this.options.title,
+                value: this.model[this.options.attribute],
+                message: this.options.message,
+            }));
+            return this;
+        }
+    });
+
+    edx.student.account.LinkFieldView = edx.student.account.FieldView.extend({
+
+        template: _.template($('#field_link-tpl').text()),
+
+        events: {
+
+        },
+
+        initialize: function(options) {
+            this.listenTo(this.model, "change", this.render);
+        },
+
+        render: function() {
+            this.$el.html(this.template({
+                title: this.options.title,
+                link_title: this.options.link_title,
+                link_href: this.options.link_href,
+                message: this.options.message,
+            }));
+            return this;
+        }
+    });
+
+    edx.student.account.DropdownFieldView = edx.student.account.FieldView.extend({
+
+        template: _.template($('#field_dropdown-tpl').text()),
+
+        events: {
+
+        },
+
+        initialize: function(options) {
+            this.listenTo(this.model, "change", this.render);
+        },
+
+        render: function() {
+            this.$el.html(this.template({
+                title: this.options.title,
+                message: this.options.message,
+            }));
+            return this;
+        }
+    });
 
 }).call(this, $, _, Backbone, gettext);
