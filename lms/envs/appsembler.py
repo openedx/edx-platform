1889b05a96b82e2585975eb9efdf62b1120419bf
TJ Keemon <keems21@gmail.com>
2015-03-10 13:27:35 -0400

    include intercom vars in appsembler.py

--- lms/envs/appsembler.py
+++ lms/envs/appsembler.py
@@ -1,4 +1,22 @@
 import os
 
-GOOGLE_TAG_MANAGER_ID = os.environ.get("GOOGLE_TAG_MANAGER_ID", None)
-#intercom stuff too
+from path import path
+
+from .common import *
+
+#### from aws.py 
+SERVICE_VARIANT = os.environ.get('SERVICE_VARIANT', None)
+CONFIG_ROOT = path(os.environ.get('CONFIG_ROOT', ENV_ROOT))
+CONFIG_PREFIX = SERVICE_VARIANT + "." if SERVICE_VARIANT else ""
+with open(CONFIG_ROOT / CONFIG_PREFIX + "env.json") as env_file:
+    ENV_TOKENS = json.load(env_file)
+
+#### begin Appsembler specific changes
+# GOOGLE_TAG_MANAGER_ID = os.environ.get("GOOGLE_TAG_MANAGER_ID", None)
+GOOGLE_TAG_MANAGER_ID = ENV_TOKENS.get('GOOGLE_TAG_MANAGER_ID', os.environ.get("GOOGLE_TAG_MANAGER_ID", None))
+
+# intercom.Intercom.app_id = ENV_TOKENS.get('INTERCOM_APP_ID', os.environ.get("INTERCOM_APP_ID", ""))
+# intercom.Intercom.api_key = ENV_TOKENS.get('INTERCOM_API_KEY', os.environ.get("INTERCOM_API_KEY", ""))
+INTERCOM_APP_ID = ENV_TOKENS.get('INTERCOM_APP_ID', os.environ.get("INTERCOM_APP_ID", ""))
+INTERCOM_API_KEY = ENV_TOKENS.get('INTERCOM_API_KEY', os.environ.get("INTERCOM_API_KEY", ""))
+INTERCOM_USER_EMAIL = ENV_TOKENS.get('INTERCOM_USER_EMAIL', os.environ.get("INTERCOM_USER_EMAIL", ""))
