484be554866558d7bf8500f735ce839391feb90f
David Ormsbee <dave@edx.org>
2015-03-10 11:53:10 -0400

    Cache UserStanding access to lower database load.

--- common/djangoapps/student/middleware.py
+++ common/djangoapps/student/middleware.py
@@ -14,23 +14,15 @@ class UserStandingMiddleware(object):
     status is 'disabled'.
     """
     def process_request(self, request):
-        user = request.user
-        try:
-            user_account = UserStanding.objects.get(user=user.id)
-            # because user is a unique field in UserStanding, there will either be
-            # one or zero user_accounts associated with a UserStanding
-        except UserStanding.DoesNotExist:
-            pass
-        else:
-            if user_account.account_status == UserStanding.ACCOUNT_DISABLED:
-                msg = _(
-                    'Your account has been disabled. If you believe '
-                    'this was done in error, please contact us at '
-                    '{support_email}'
-                ).format(
-                    support_email=u'<a href="mailto:{address}?subject={subject_line}">{address}</a>'.format(
-                        address=settings.DEFAULT_FEEDBACK_EMAIL,
-                        subject_line=_('Disabled Account'),
-                    ),
-                )
-                return HttpResponseForbidden(msg)
+        if UserStanding.is_user_disabled(request.user):
+            msg = _(
+                'Your account has been disabled. If you believe '
+                'this was done in error, please contact us at '
+                '{support_email}'
+            ).format(
+                support_email=u'<a href="mailto:{address}?subject={subject_line}">{address}</a>'.format(
+                    address=settings.DEFAULT_FEEDBACK_EMAIL,
+                    subject_line=_('Disabled Account'),
+                ),
+            )
+            return HttpResponseForbidden(msg)
