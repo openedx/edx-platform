f27df4e433c3f3760b77ee0493373f74be1649e4
Waqas Khalid <waqas.khalid@arbisoft.com>
2015-03-11 16:35:24 +0500

    User was not able to login on edge
    user was not able to login on edge becuase of multiple
    entries of loginfailure that was caused by the race
    condition when user tried to login with wrong credentials.
    
    TNL-1368

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -586,6 +586,7 @@ class LoginFailures(models.Model):
             return False
 
     @classmethod
+    @transaction.commit_on_success
     def increment_lockout_counter(cls, user):
         """
         Ticks the failed attempt counter
