4269ea549c258eccf0c1f4ab35175a32394a5b3d
TJ Keemon <keems21@gmail.com>
2015-03-11 19:36:10 -0400

    pull in all appsembler changes from APPSEMBLER_FEATURES, which is set in {l,c{ms.env.json

--- lms/envs/appsembler.py
+++ lms/envs/appsembler.py
@@ -1,22 +1,19 @@
 import os
+import json
 
 from path import path
 
-from .common import *
-
-#### from aws.py 
 SERVICE_VARIANT = os.environ.get('SERVICE_VARIANT', None)
-CONFIG_ROOT = path(os.environ.get('CONFIG_ROOT', ENV_ROOT))
+CONFIG_ROOT = path('/edx/app/edxapp/') #don't hardcode this in the future
 CONFIG_PREFIX = SERVICE_VARIANT + "." if SERVICE_VARIANT else ""
-with open(CONFIG_ROOT / CONFIG_PREFIX + "env.json") as env_file:
+with open(CONFIG_ROOT / CONFIG_PREFIX + 'env.json') as env_file:
     ENV_TOKENS = json.load(env_file)
 
-#### begin Appsembler specific changes
-# GOOGLE_TAG_MANAGER_ID = os.environ.get("GOOGLE_TAG_MANAGER_ID", None)
-GOOGLE_TAG_MANAGER_ID = ENV_TOKENS.get('GOOGLE_TAG_MANAGER_ID', os.environ.get("GOOGLE_TAG_MANAGER_ID", None))
+APPSEMBLER_FEATURES = ENV_TOKENS.get('APPSEMBLER_FEATURES',None)
 
-# intercom.Intercom.app_id = ENV_TOKENS.get('INTERCOM_APP_ID', os.environ.get("INTERCOM_APP_ID", ""))
-# intercom.Intercom.api_key = ENV_TOKENS.get('INTERCOM_API_KEY', os.environ.get("INTERCOM_API_KEY", ""))
-INTERCOM_APP_ID = ENV_TOKENS.get('INTERCOM_APP_ID', os.environ.get("INTERCOM_APP_ID", ""))
-INTERCOM_API_KEY = ENV_TOKENS.get('INTERCOM_API_KEY', os.environ.get("INTERCOM_API_KEY", ""))
-INTERCOM_USER_EMAIL = ENV_TOKENS.get('INTERCOM_USER_EMAIL', os.environ.get("INTERCOM_USER_EMAIL", ""))
+# search APPSEMBLER_FEATURES first, env variables second, fallback to None
+GOOGLE_TAG_MANAGER_ID = APPSEMBLER_FEATURES.get('GOOGLE_TAG_MANAGER_ID','')
+
+INTERCOM_APP_ID = APPSEMBLER_FEATURES.get('INTERCOM_APP_ID', os.environ.get("INTERCOM_APP_ID", ""))
+INTERCOM_API_KEY = APPSEMBLER_FEATURES.get('INTERCOM_API_KEY', os.environ.get("INTERCOM_API_KEY", ""))
+INTERCOM_USER_EMAIL = APPSEMBLER_FEATURES.get('INTERCOM_USER_EMAIL', os.environ.get("INTERCOM_USER_EMAIL", ""))
