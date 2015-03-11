11961a064439c9b472beb9c8c683cc728600b60b
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-03-11 19:13:12 +0500

    fixing the failed bok_choy tests.
    TNL-1256

--- common/test/acceptance/tests/discussion/helpers.py
+++ common/test/acceptance/tests/discussion/helpers.py
@@ -59,7 +59,7 @@ class CohortTestMixin(object):
         """
         url = LMS_BASE_URL + "/courses/" + course_fixture._course_key + '/cohorts/settings'  # pylint: disable=protected-access
         data = json.dumps({'is_cohorted': False})
-        response = course_fixture.session.post(url, data=data, headers=course_fixture.headers)
+        response = course_fixture.session.patch(url, data=data, headers=course_fixture.headers)
         self.assertTrue(response.ok, "Failed to disable cohorts")
 
 
