20f46c8f22de69f7ab3356e8afb58f9097dc33a8
Usman Khalid <2200617@gmail.com>
2015-03-13 16:29:14 +0500

    Account settings page.
    TNL-1499

--- lms/djangoapps/student_account/urls.py
+++ lms/djangoapps/student_account/urls.py
@@ -11,3 +11,8 @@ if settings.FEATURES.get('ENABLE_COMBINED_LOGIN_REGISTRATION'):
         url(r'^register/$', 'login_and_registration_form', {'initial_mode': 'register'}, name='account_register'),
         url(r'^password$', 'password_change_request_handler', name='password_change_request'),
     )
+
+urlpatterns += patterns(
+    'student_account.views',
+    url(r'^settings$', 'account_settings', name='account_settings'),
+)
