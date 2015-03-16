c37dc913a003048611bcc8d1725ea515422afc3b
Will Daly <will@edx.org>
2015-03-16 14:34:41 -0400

    Copy changes to the activation email

--- openedx/core/djangoapps/user_api/tests/test_views.py
+++ openedx/core/djangoapps/user_api/tests/test_views.py
@@ -1306,7 +1306,10 @@ class RegistrationViewTest(ApiTestCase):
         sent_email = mail.outbox[0]
         self.assertEqual(sent_email.to, [self.EMAIL])
         self.assertEqual(sent_email.subject, "Activate Your edX Account")
-        self.assertIn("activate your account", sent_email.body)
+        self.assertIn(
+            u"activating your {platform} account".format(platform=settings.PLATFORM_NAME),
+            sent_email.body
+        )
 
     @ddt.data(
         {"email": ""},
