80b78860a7417642b97460b33721083984a481d0
Calen Pennington <cale@edx.org>
2015-03-23 14:05:12 -0400

    Verticals renders are identified differently; change tests to match

--- lms/djangoapps/courseware/tests/test_module_render.py
+++ lms/djangoapps/courseware/tests/test_module_render.py
@@ -786,7 +786,7 @@ class MongoViewInStudioTest(ViewInStudioTest):
         # Render the parent vertical, then check that there is only a single "View Unit in Studio" link.
         result_fragment = self.module.render(STUDENT_VIEW)
         # The single "View Unit in Studio" link should appear before the first xmodule vertical definition.
-        parts = result_fragment.content.split('xmodule_VerticalBlock')
+        parts = result_fragment.content.split('data-block-type="vertical"')
         self.assertEqual(3, len(parts), "Did not find two vertical blocks")
         self.assertIn('View Unit in Studio', parts[0])
         self.assertNotIn('View Unit in Studio', parts[1])
