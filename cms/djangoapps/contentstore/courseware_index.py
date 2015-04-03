11618e1313e66f2b63a14a2b12a7581d100e6e76
Martyn James <mjames@edx.org>
2015-04-03 16:47:31 -0400

    Index course information alongside courseware
    Conflicts:
    	cms/djangoapps/contentstore/tests/test_courseware_index.py

--- cms/djangoapps/contentstore/courseware_index.py
+++ cms/djangoapps/contentstore/courseware_index.py
@@ -555,6 +555,7 @@ class CourseAboutSearchIndexer(object):
             log.exception(
                 "Course discovery indexing error encountered, course discovery index may be out of date %s",
                 course_id,
+                exc_info=True,
             )
             raise
 
