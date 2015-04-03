dd4bf2ee7c550dfcc5f15cdca45ab141699ed6f0
Martyn James <mjames@edx.org>
2015-04-03 16:47:31 -0400

    Course Discovery feature using edx-search

---
+++ lms/static/js/discovery/filter_view.js
@@ -0,0 +1,37 @@
+;(function (define) {
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'gettext',
+], function ($, _, Backbone, gettext) {
+    'use strict';
+
+    return Backbone.View.extend({
+
+        tagName: 'li',
+        templateId: '#filter-tpl',
+        className: 'active-filter',
+
+        initialize: function () {
+            this.tpl = _.template($(this.templateId).html());
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+
+        render: function () {
+            this.className = this.model.get('type');
+            this.$el.html(this.tpl(this.model.attributes));
+            return this;
+        },
+
+        remove: function() {
+            this.stopListening();
+            this.$el.remove();
+        }
+
+    });
+
+});
+
+})(define || RequireJS.define);
