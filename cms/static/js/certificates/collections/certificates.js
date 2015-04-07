73327a8be70be124fbbbaaf55f86bd851e101655
Matt Drayer <mattdrayer@edx.org>
2015-04-07 10:04:39 -0400

    Add certificates mini-app to Studio
    - added all sorts of new/repurposed code
    - mini app now works, needs styling, tests
    - Sass/CSS fixed, still needs tests
    - updated spec files, but needs refactoring

---
+++ cms/static/js/certificates/collections/certificates.js
@@ -0,0 +1,39 @@
+define([
+    'backbone', 'js/certificates/models/certificate'
+],
+function(Backbone, Certificate) {
+    'use strict';
+    console.log("certificate_collection.start");
+    var CertificateCollection = Backbone.Collection.extend({
+        model: Certificate,
+        url: '/certificates/edX/DemoX/Demo_Course',
+
+        //Parse the JSON into Certificate models
+        parse: function (certificatesJson) {
+
+            console.log("certificates_collection.parse.start");
+            console.log(certificatesJson);
+
+            //Parse the provided JSON and create models in the collection
+            var modelArray = JSON.parse(certificatesJson);
+            for (var i in modelArray) {
+                console.log(modelArray[i]);
+                this.push(modelArray[i]);
+                console.log('Parsed! ');
+
+            }
+
+            console.log(this.toJSON());
+
+            //return models
+            return this.models;
+
+        }
+
+
+    });
+    console.log("certificate_collection.CertificateCollection");
+    console.log(CertificateCollection);
+    console.log("certificate_collection.return")
+    return CertificateCollection;
+});
