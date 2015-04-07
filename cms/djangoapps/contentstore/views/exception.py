e9a186551cc90ec392b37fe549eae4d4cb4cc451
Matt Drayer <mattdrayer@edx.org>
2015-04-07 10:04:39 -0400

    Add certificates mini-app to Studio
    - added all sorts of new/repurposed code
    - mini app now works, needs styling, tests
    - Sass/CSS fixed, still needs tests
    - updated spec files, but needs refactoring
    - SOL-715 adding unit tests for certificates
    - SOL-715 adding some more unit tests for certificates and try to resolve maximum pylnt and pep8 issues
    - SOL-715 clean the certificate tests from group configuration + pep8
    - SOL-718 Close button is working properly
    - SOL-716 Certificates jasmine tests
    - SOL-716 Certificates jasmine tests
    - SOL-801 Backbone Signatories Modeling
    - SOL-803 Underscore template: Editor (Add)
    - SOL-802 Signatories: Underscore template - Details
    - SOL-802 Signatories: Underscore template - Details
    - SOL-804 Signatories: Underscore template: Editor (Edit)
    - Add signatory delete Django view
    - Addressed pylint violations
    - SOL-805 Signatory editor (Delete)
    - Add bok choy tests for SOL-465
    - Fixed broken unit tests
    - Added comments, fixed formatting
    - Add backbone-relational mappings to spec configs
    - Add Coffeescript router
    - SOL-716 Jasmine Tests
    - Addressed PR feedback
    - Fixed all sorts of reported issues
    - Added missing minified JS library
    - client side validation of signatory fields
    - fixed broken bok choy tests and optimised signatory js views
    - Fixed broken Jasmine tests
    - Add certificate_data property
    - Remove onBeforeUpload handler
    - Remove awful bok choy test that burned many hours
    - Fix invalid property impl
    - SOL-390 signatories names
    - cleaned up rebase issue, added in certificates styling to build for studio sass compilation
    - Remove obsolete extends Sass files
    - partial refactor of styling, additional comments and notes for styling code, added in input maxlength limiting for signatory information as well

---
+++ cms/djangoapps/contentstore/views/exception.py
@@ -0,0 +1,18 @@
+"""
+A common module for managing exceptions. Helps to avoid circular references
+"""
+
+
+# Certificates Exceptions
+class CertificateException(Exception):
+    """
+    Base exception for Certificates workflows
+    """
+    pass
+
+
+class CertificateValidationError(CertificateException):
+    """
+    An exception raised when certificate information is invalid.
+    """
+    pass
