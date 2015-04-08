b1286d4c3f92339cbe49f57d547efe5c5a100a5a
Maxim Starodubcev <maxim@trambroid.com>
2015-04-08 17:20:58 +0300

    Add oauth2_provider

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -724,6 +724,8 @@ INSTALLED_APPS = (
     'edx_jsme',    # Molecular Structure
 
     'openedx.core.djangoapps.content.course_structures',
+    'provider',
+    'provider.oauth2',
     'oauth2_provider'
 )
 
