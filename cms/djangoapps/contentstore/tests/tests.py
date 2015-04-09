a87c881024aaf2416b695dd23d2e9a2404a53d60
Nimisha Asthagiri <nasthagiri@edx.org>
2015-04-09 15:13:43 -0400

    MA-612 Mobile Push Notification Studio Django admin setting

--- cms/djangoapps/contentstore/tests/tests.py
+++ cms/djangoapps/contentstore/tests/tests.py
@@ -6,12 +6,14 @@ import mock
 import unittest
 from ddt import ddt, data, unpack
 
+from django.test import TestCase
 from django.test.utils import override_settings
 from django.core.cache import cache
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+from contentstore.models import PushNotificationConfig
 from contentstore.tests.utils import parse_json, user, registration, AjaxEnabledTestClient
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from contentstore.tests.test_course_settings import CourseTestCase
@@ -349,3 +351,15 @@ class CourseKeyVerificationTestCase(CourseTestCase):
         )
         resp = self.client.get_html(url)
         self.assertEqual(resp.status_code, status_code)
+
+
+class PushNotificationConfigTestCase(TestCase):
+    """
+    Tests PushNotificationConfig.
+    """
+    def test_notifications_defaults(self):
+        self.assertFalse(PushNotificationConfig.is_enabled())
+
+    def test_notifications_enabled(self):
+        PushNotificationConfig(enabled=True).save()
+        self.assertTrue(PushNotificationConfig.is_enabled())
