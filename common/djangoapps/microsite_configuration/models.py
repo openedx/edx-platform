c102396d337d72fcaf346603b56a4c119b6f313a
Felipe Montoya <felipe.montoya@edunext.co>
2015-04-16 18:44:57 -0500

    Adding the validator to be sure we only store valid json strings

--- common/djangoapps/microsite_configuration/models.py
+++ common/djangoapps/microsite_configuration/models.py
@@ -1,12 +1,22 @@
+import json
+
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_json(values):
+    try:
+        json.loads(values)
+    except ValueError, e:
+        raise ValidationError("The values field must be a valid json.")
 
 
 class Microsite(models.Model):
     key = models.CharField(max_length=63, db_index=True)
     subdomain = models.CharField(max_length=127, db_index=True)
-    values = models.TextField(null=False, blank=True)
+    values = models.TextField(null=False, blank=True, validators=[validate_json])
 
-    # TODO: must add a is_active flag
+    # TODO: an is_active flag would be useful
 
     def __str__( self):
       return self.key
