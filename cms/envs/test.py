ebdff305aeb0324f01628a20bc35bfa07f7ef60a
Zia Fazal <zia.fazal@arbisoft.com>
2015-04-16 17:33:53 +0500

    fixing issues to make sure all tests pass on cmd

--- cms/envs/test.py
+++ cms/envs/test.py
@@ -181,6 +181,9 @@ INSTALLED_APPS += ('external_auth', )
 # Add milestones to Installed apps for testing
 INSTALLED_APPS += ('milestones', 'openedx.core.djangoapps.call_stack_manager')
 
+# Add projects, organizations,'api_manager', 'progress', 'gradebook to Installed apps for testing
+INSTALLED_APPS += ('projects', 'organizations', 'api_manager', 'progress', 'gradebook', )
+
 # hide ratelimit warnings while running tests
 filterwarnings('ignore', message='No request passed to the backend, unable to rate-limit')
 
