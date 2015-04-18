a455d9d50ac093b8122a1f7efe2d9f6f9f16d2fa
Davorin Sego <davorin.sego@gmail.com>
2015-04-18 14:52:42 +0200

    SOL-495 Cohort-Aware content search

--- lms/lib/courseware_search/lms_filter_generator.py
+++ lms/lib/courseware_search/lms_filter_generator.py
@@ -3,13 +3,43 @@ This file contains implementation override of SearchFilterGenerator which will a
     * Filter by all courses in which the user is enrolled in
 """
 from microsite_configuration import microsite
+
 from student.models import CourseEnrollment
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import CourseKey
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
 
 from search.filter_generator import SearchFilterGenerator
+from openedx.core.djangoapps.course_groups.partition_scheme import get_cohorted_user_partition
+from courseware.access import get_user_role
 
 
 class LmsSearchFilterGenerator(SearchFilterGenerator):
     """ SearchFilterGenerator for LMS Search """
+
+    def filter_dictionary(self, **kwargs):
+        """ base implementation which filters via start_date """
+        filter_dictionary = super(LmsSearchFilterGenerator, self).filter_dictionary(**kwargs)
+        if 'user' in kwargs and 'course_id' in kwargs and kwargs['course_id']:
+            user = kwargs['user']
+            try:
+                course_key = CourseKey.from_string(kwargs['course_id'])
+            except InvalidKeyError:
+                course_key = SlashSeparatedCourseKey.from_deprecated_string(kwargs['course_id'])
+
+            # Staff user looking at course as staff user
+            if get_user_role(user, course_key) == 'staff':
+                return filter_dictionary
+
+            cohorted_user_partition = get_cohorted_user_partition(course_key)
+            if cohorted_user_partition:
+                partition_group = cohorted_user_partition.scheme.get_group_for_user(
+                    course_key,
+                    user,
+                    cohorted_user_partition,
+                )
+                filter_dictionary['content_groups'] = unicode(partition_group.id) if partition_group else None
+        return filter_dictionary
 
     def field_dictionary(self, **kwargs):
         """ add course if provided otherwise add courses in which the user is enrolled in """
