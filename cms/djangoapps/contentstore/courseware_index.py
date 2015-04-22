c1264fdb67c8f20b1ef8bcbe9e0bac14d93eb7ac
Martyn James <mjames@edx.org>
2015-04-22 13:55:15 -0400

    Collect course modes as part of indexed course info

--- cms/djangoapps/contentstore/courseware_index.py
+++ cms/djangoapps/contentstore/courseware_index.py
@@ -520,7 +520,6 @@ class CourseAboutSearchIndexer(object):
             log.exception(
                 "Course discovery indexing error encountered, course discovery index may be out of date %s",
                 course_id,
-                exc_info=True,
             )
             raise
 
