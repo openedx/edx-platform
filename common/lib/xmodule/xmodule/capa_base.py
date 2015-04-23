3f11df3f348cf4357a1114079fe5d6b419522a28
applecool <kmitharsha@gmail.com>
2015-04-22 20:45:44 -0500

    Fix to LOC-66

--- common/lib/xmodule/xmodule/capa_base.py
+++ common/lib/xmodule/xmodule/capa_base.py
@@ -1127,25 +1127,25 @@ class CapaMixin(CapaFields):
         Returns time duration nicely formated, e.g. "3 minutes 4 seconds"
         """
         # Here _ is the N variant ungettext that does pluralization with a 3-arg call
-        _ = self.runtime.service(self, "i18n").ungettext
+        ungettext = self.runtime.service(self, "i18n").ungettext
         hours = num_seconds // 3600
         sub_hour = num_seconds % 3600
         minutes = sub_hour // 60
         seconds = sub_hour % 60
         display = ""
         if hours > 0:
-            display += _("{num_hour} hour", "{num_hour} hours", hours).format(num_hour=hours)
+            display += ungettext("{num_hour} hour", "{num_hour} hours", hours).format(num_hour=hours)
         if minutes > 0:
             if display != "":
                 display += " "
             # translators: "minute" refers to a minute of time
-            display += _("{num_minute} minute", "{num_minute} minutes", minutes).format(num_minute=minutes)
+            display += ungettext("{num_minute} minute", "{num_minute} minutes", minutes).format(num_minute=minutes)
         # Taking care to make "0 seconds" instead of "" for 0 time
         if seconds > 0 or (hours == 0 and minutes == 0):
             if display != "":
                 display += " "
             # translators: "second" refers to a second of time
-            display += _("{num_second} second", "{num_second} seconds", seconds).format(num_second=seconds)
+            display += ungettext("{num_second} second", "{num_second} seconds", seconds).format(num_second=seconds)
         return display
 
     def get_submission_metadata_safe(self, answers, correct_map):
