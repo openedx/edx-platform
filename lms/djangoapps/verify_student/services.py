05068f1b949bbb9f87860e4c718d25ff72c9f9b9
zubair-arbi <zubair.afzal@arbisoft.com>
2015-04-23 12:16:26 +0500

    fix reverification link in lms ECOM-1437

--- lms/djangoapps/verify_student/services.py
+++ lms/djangoapps/verify_student/services.py
@@ -1,6 +1,7 @@
 """
 Implement the Reverification XBlock "reverification" server
 """
+
 from opaque_keys.edx.keys import CourseKey
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
@@ -12,13 +13,13 @@ class ReverificationService(object):
 
     """
 
-    def get_status(self, user_id, course_id, checkpoint_name):
+    def get_status(self, user_id, course_id, related_assessment):
         """ Check if the user has any verification attempt for this checkpoint and course_id
 
         Args:
             user_id(str): User Id string
             course_id(str): A string of course_id
-            checkpoint_name(str): Verification checkpoint name
+            related_assessment(str): Verification checkpoint name
 
         Returns:
             Verification Status string if any attempt submitted by user else None
@@ -28,29 +29,29 @@ class ReverificationService(object):
             checkpoint_status = VerificationStatus.objects.filter(
                 user_id=user_id,
                 checkpoint__course_id=course_key,
-                checkpoint__checkpoint_name=checkpoint_name
+                checkpoint__checkpoint_name=related_assessment
             ).latest()
             return checkpoint_status.status
         except ObjectDoesNotExist:
             return None
 
-    def start_verification(self, course_id, checkpoint_name, item_id):
+    def start_verification(self, course_id, related_assessment, item_id):
         """ Get or create the verification checkpoint and return the re-verification link
 
         Args:
             course_id(str): A string of course_id
-            checkpoint_name(str): Verification checkpoint name
+            related_assessment(str): Verification checkpoint name
 
         Returns:
             Re-verification link
         """
         course_key = CourseKey.from_string(course_id)
-        VerificationCheckpoint.objects.get_or_create(course_id=course_key, checkpoint_name=checkpoint_name)
+        VerificationCheckpoint.objects.get_or_create(course_id=course_key, checkpoint_name=related_assessment)
         re_verification_link = reverse(
             'verify_student_incourse_reverify',
             args=(
                 unicode(course_key),
-                unicode(checkpoint_name),
+                unicode(related_assessment),
                 unicode(item_id)
             )
         )
