67ee646989ff517d42f3545908f6c5144ea0bacf
Christine Lytwynec <clytwynec@edx.org>
2015-04-30 16:27:51 -0400

    fix typo in jenkins-report.sh

--- scripts/jenkins-report.sh
+++ scripts/jenkins-report.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source scripts/jenkins-common.sh
 
-# Run combine the data files that were generated using -p
+# Combine the data files that were generated using -p
 paver combine_coverage
 
 # Get the diff coverage and html reports for unit tests
