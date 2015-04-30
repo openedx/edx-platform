ca0d9ea267d27d5ddab571c04188252e1246a29c
Jonathan Piacenti <kelketek@gmail.com>
2015-04-30 14:40:09 -0500

    Implement OpenBadge Generation upon Certificate generation through Badgr API

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -392,6 +392,10 @@ FEATURES = {
 
     # Teams feature
     'ENABLE_TEAMS': False,
+
+    # Enable OpenBadge support. See the BADGR_* settings later in this file.
+    'ENABLE_OPENBADGES': False,
+
 }
 
 # Ignore static asset files on import which match this pattern
@@ -1994,6 +1998,13 @@ REGISTRATION_EXTRA_FIELDS = {
 ########################## CERTIFICATE NAME ########################
 CERT_NAME_SHORT = "Certificate"
 CERT_NAME_LONG = "Certificate of Achievement"
+
+#################### Badgr OpenBadges generation #######################
+# Be sure to set up images for course modes using the BadgeImageConfiguration model in the certificates app.
+BADGR_API_TOKEN = None
+# Do not add the trailing slash here.
+BADGR_BASE_URL = "http://localhost:8005"
+BADGR_ISSUER_SLUG = "example-issuer"
 
 ###################### Grade Downloads ######################
 GRADES_DOWNLOAD_ROUTING_KEY = HIGH_MEM_QUEUE
