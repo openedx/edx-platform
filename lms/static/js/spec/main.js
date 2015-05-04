97a9384ce7819f10b3d15baad8a0552a3cc86625
muhammad-ammar <mammar@gmail.com>
2015-05-04 15:03:32 +0500

    Unit Bookmarks List View
    TNL-1958

--- lms/static/js/spec/main.js
+++ lms/static/js/spec/main.js
@@ -57,6 +57,7 @@
             'capa/display': 'xmodule_js/src/capa/display',
             'string_utils': 'xmodule_js/common_static/js/src/string_utils',
             'logger': 'xmodule_js/common_static/js/src/logger',
+            'moment': 'xmodule_js/common_static/js/src/moment',
 
             // Manually specify LMS files that are not converted to RequireJS
             'history': 'js/vendor/history',
@@ -95,6 +96,12 @@
             'js/student_profile/views/learner_profile_factory': 'js/student_profile/views/learner_profile_factory',
             'js/student_profile/views/learner_profile_view': 'js/student_profile/views/learner_profile_view',
             'js/ccx/schedule': 'js/ccx/schedule',
+
+            'js/bookmarks/collections/bookmarks': 'js/bookmarks/collections/bookmarks',
+            'js/bookmarks/models/bookmark': 'js/bookmarks/models/bookmark',
+            'js/bookmarks/views/bookmarks_button': 'js/bookmarks/views/bookmarks_button',
+            'js/bookmarks/views/bookmarks_list': 'js/bookmarks/views/bookmarks_list',
+            'js/views/message': 'js/views/message',
 
             // edxnotes
             'annotator_1.2.9': 'xmodule_js/common_static/js/vendor/edxnotes/annotator-full.min',
@@ -639,7 +646,8 @@
         'lms/include/js/spec/edxnotes/collections/notes_spec.js',
         'lms/include/js/spec/search/search_spec.js',
         'lms/include/js/spec/discovery/discovery_spec.js',
-        'lms/include/js/spec/ccx/schedule_spec.js'
+        'lms/include/js/spec/ccx/schedule_spec.js',
+        'lms/include/js/spec/bookmarks/bookmarks_spec.js'
     ]);
 
 }).call(this, requirejs, define);
