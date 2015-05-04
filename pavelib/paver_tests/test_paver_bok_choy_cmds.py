52c4aacad5cf8eabcad01b4b078d8fb291b2c389
Christine Lytwynec <clytwynec@edx.org>
2015-05-04 10:58:25 -0400

    Revert "Clytwynec/unique dirs for test results"

--- pavelib/paver_tests/test_paver_bok_choy_cmds.py
+++ pavelib/paver_tests/test_paver_bok_choy_cmds.py
@@ -14,21 +14,19 @@ class TestPaverBokChoyCmd(unittest.TestCase):
     def _expected_command(self, expected_text_append, expected_default_store=None):
         if expected_text_append:
             expected_text_append = "/" + expected_text_append
-        shard = os.environ.get('SHARD')
         expected_statement = (
             "DEFAULT_STORE={default_store} "
-            "SCREENSHOT_DIR='{repo_dir}/test_root/log{shard_str}' "
-            "BOK_CHOY_HAR_DIR='{repo_dir}/test_root/log{shard_str}/hars' "
-            "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log{shard_str}' "
+            "SCREENSHOT_DIR='{repo_dir}/test_root/log' "
+            "BOK_CHOY_HAR_DIR='{repo_dir}/test_root/log/hars' "
+            "SELENIUM_DRIVER_LOG_DIR='{repo_dir}/test_root/log' "
             "nosetests {repo_dir}/common/test/acceptance/tests{exp_text} "
             "--with-xunit "
-            "--xunit-file={repo_dir}/reports/bok_choy{shard_str}/xunit.xml "
-            "--verbosity=2 "
+            "--xunit-file={repo_dir}/reports/bok_choy/xunit.xml "
+            "--verbosity=2"
         ).format(
             default_store=expected_default_store,
             repo_dir=REPO_DIR,
             exp_text=expected_text_append,
-            shard_str='/shard_' + shard if shard else '',
         )
         return expected_statement.strip()
 
