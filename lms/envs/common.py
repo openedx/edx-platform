01a08ec5ccadae8d21a8d78bc1855ff5e8b85c3a
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-05-13 14:16:55 +0500

    Bookmarks API.
    TNL-2180

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -592,6 +592,7 @@ USAGE_KEY_PATTERN = r'(?P<usage_key_string>(?:i4x://?[^/]+/[^/]+/[^/]+/[^@]+(?:@
 ASSET_KEY_PATTERN = r'(?P<asset_key_string>(?:/?c4x(:/)?/[^/]+/[^/]+/[^/]+/[^@]+(?:@[^/]+)?)|(?:[^/]+))'
 USAGE_ID_PATTERN = r'(?P<usage_id>(?:i4x://?[^/]+/[^/]+/[^/]+/[^@]+(?:@[^/]+)?)|(?:[^/]+))'
 
+USERNAME_PATTERN = r'(?P<username>[\w.@+-]+)'
 
 ############################## EVENT TRACKING #################################
 
@@ -1863,6 +1864,9 @@ INSTALLED_APPS = (
 
     # Course teams
     'teams',
+
+    # Bookmarks
+    'bookmarks',
 )
 
 ######################### CSRF #########################################
--- openedx/core/djangoapps/profile_images/urls.py
+++ openedx/core/djangoapps/profile_images/urls.py
@@ -4,18 +4,18 @@ Defines the URL routes for this app.
 from .views import ProfileImageUploadView, ProfileImageRemoveView
 
 from django.conf.urls import patterns, url
+from django.conf import settings
 
-USERNAME_PATTERN = r'(?P<username>[\w.+-]+)'
 
 urlpatterns = patterns(
     '',
     url(
-        r'^v1/' + USERNAME_PATTERN + '/upload$',
+        r'^v1/' + settings.USERNAME_PATTERN + '/upload$',
         ProfileImageUploadView.as_view(),
         name="profile_image_upload"
     ),
     url(
-        r'^v1/' + USERNAME_PATTERN + '/remove$',
+        r'^v1/' + settings.USERNAME_PATTERN + '/remove$',
         ProfileImageRemoveView.as_view(),
         name="profile_image_remove"
     ),
