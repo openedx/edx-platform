998d957d99f78507eeb242467abb697f24d486c5
Chris Rodriguez <crodriguez@edx.org>
2015-05-20 14:24:22 -0400

    Adding third DOWN key test

--- lms/static/js/spec/utils/dropdown_spec.js
+++ lms/static/js/spec/utils/dropdown_spec.js
@@ -1,28 +1,27 @@
-define([
-    'jquery', 'logger'
-], function($, Logger) {
+define(['jquery', 'js/utils/dropdown'], function($) {
     'use strict';
 
     describe('Dropdown Menus', function() {
+        var wrapper, button, menu, menu_item, menu_action;
+
+        beforeEach(function() {
+            loadFixtures('js/fixtures/utils/dropdown.html');
+            wrapper = $('.wrapper-more-actions');
+            button = wrapper.children('.button-more.has-dropdown');
+            menu = wrapper.children('.dropdown-menu');
+            menu_item = menu.children('.dropdown-item');
+            menu_action = menu_item.children('.action');
+            spyOn($.fn, 'focus').andCallThrough();
+            edx.util.dropdown.init();
+        });
 
         describe('always', function() {
-            var wrapper, button, menu, menu_item, menu_action;
-
-            beforeEach(function() {
-                loadFixtures('js/fixtures/utils/dropdown.html');
-                wrapper = $('.wrapper-more-actions');
-                button = wrapper.children('.button-more.has-dropdown');
-                menu = wrapper.children('.dropdown-menu');
-                menu_item = menu.children('.dropdown-item');
-                menu_action = menu_item.children('.action');
-                spyOn($.fn, 'focus').andCallThrough();
-            });
 
             it('adds the dropdown menus', function() {
-                expect(button.length).toBe(1);
                 expect(wrapper.length).toBe(button.length);
+                expect(button.length).toBe(1);
                 expect(menu.length).toBe(wrapper.length);
-                expect(menu_item.length).toBe(1);
+                expect(menu_item.length).toBeGreaterThan(1);
                 expect(menu_action.length).toBe(menu_item.length);
             });
 
@@ -44,9 +43,9 @@ define([
                 return $.Event('keydown', { keyCode: key });
             }
 
-            var wrapper, button, menu, menu_item, menu_action, KEY = $.ui.keyCode,
-
             it('opens the menu on button click', function() {
+                // console.log(button.length);
+                button.focus();
                 button.click();
                 expect(button).toHaveClass('is-active');
                 expect(button).toHaveAttr({
@@ -66,7 +65,8 @@ define([
             });
 
             it('opens the menu on ENTER kepress', function() {
-                button.trigger(keyPressEvent(KEY.ENTER));
+                button.focus();
+                button.trigger(keyPressEvent(13)); // Enter
                 expect(button).toHaveClass('is-active');
                 expect(button).toHaveAttr({
                     'aria-expanded': 'true'
@@ -76,42 +76,42 @@ define([
             });
 
             it('opens the menu on DOWN keypress', function() {
-                button.trigger(keyPressEvent(KEY.DOWN));
+                button.focus();
+                button.trigger(keyPressEvent(40)); // Down arrow
                 expect(button).toHaveClass('is-active');
                 expect(button).toHaveAttr({
                     'aria-expanded': 'true'
                 });
                 expect(menu).toHaveClass('is-visible');
                 expect(menu.focus).toHaveBeenCalled();
+            });
+
+            it('focuses on the first item after menu is opened', function() {
+                button.focus();
+                button.trigger(keyPressEvent(40)); // Down arrow
+                expect(menu.focus).toHaveBeenCalled();
+                menu.trigger(keyPressEvent(40)); // Down arrow
+                expect(menu_item.eq(0).focus).toHaveBeenCalled();
             });
 
             it('moves between menu items on UP or DOWN', function() {
                 var last_item = menu_item.length - 1, i;
-                container.trigger(keyPressEvent(KEY.ENTER));
+                menu.focus();
+                menu.trigger(keyPressEvent(13)); // Enter
 
                 for (i = last_item; i >= 0; i--) {
-                    menu_item.eq(i).trigger(keyPressEvent(KEY.UP));
+                    menu_item.eq(i).trigger(keyPressEvent(38)); // Up arrow
                     expect(menu_item.eq(i).focus).toHaveBeenCalled();
                 }
 
                 for (i = 0; i <= last_item; i++) {
-                    menu_item.eq(i).trigger(keyPressEvent(KEY.DOWN));
+                    menu_item.eq(i).trigger(keyPressEvent(40)); // Down arrow
                     expect(menu_item.eq(i).focus).toHaveBeenCalled();
                 }
             });
 
-            it('opens the menu on SPACE kepress', function() {
-                button.trigger(keyPressEvent(KEY.SPACE));
-                expect(button).toHaveClass('is-active');
-                expect(button).toHaveAttr({
-                    'aria-expanded': 'true'
-                });
-                expect(menu).toHaveClass('is-visible');
-                expect(menu.focus).toHaveBeenCalled();
-            });
-
             it('closes the menu on ESC keypress', function() {
-                $(window).trigger(keyPressEvent(KEY.ESC));
+                $(window).trigger(keyPressEvent(49)); // Esc
                 expect(button).not.toHaveClass('is-active');
                 expect(button).toHaveAttr({
                     'aria-expanded': 'false'
@@ -121,4 +121,4 @@ define([
             });
         });
     });
-}).call(this);
+});
--- lms/static/js/utils/dropdown.js
+++ lms/static/js/utils/dropdown.js
@@ -1,22 +1,10 @@
-    var edx = edx || {},
-
+var edx = edx || {},
     Dropdown = (function() {
 
         var dropdown = {
 
-            opts: {
-                page: $(document),
-                midpoint: ($(window).width() / 2),
-                button: $('.button-more.has-dropdown'),
-                button_active: 'is-active',
-                menu: $('.dropdown-menu'),
-                menu_inactive: 'is-hidden',
-                menu_active: 'is-visible',
-                menu_align: 'align-',
-                text_align: 'control-text-align'
-            },
-
             init: function(parent) {
+                dropdown.optsSet();
 
                 if (dropdown.opts.button.length && dropdown.opts.menu.length) {
 
@@ -29,8 +17,21 @@
                 }
             },
 
-            listenForClick: function() {
+            optsSet: function() {
+                dropdown.opts = {
+                    page: $(document),
+                    midpoint: ($(window).width() / 2),
+                    button: $('.button-more.has-dropdown'),
+                    button_active: 'is-active',
+                    menu: $('.dropdown-menu'),
+                    menu_inactive: 'is-hidden',
+                    menu_active: 'is-visible',
+                    menu_align: 'align-',
+                    text_align: 'control-text-align'
+                };
+            },
 
+            listenForClick: function() {
                 dropdown.opts.button.on('click', function() {
                     dropdown.closeDropdownMenus(); // close any open menus
                     dropdown.openDropdownMenu($(this)); // then open the chosen menu
@@ -41,7 +42,7 @@
                 });
             },
 
-            handlerIsAction: function(key, menu) {
+            handlerIsAction: function(key, menu, focused) {
                 if (key === 38) { // UP
                     dropdown.previousMenuItemLink(focused, menu);
                 } else if (key === 40) { // DOWN
@@ -62,7 +63,6 @@
             },
 
             listenForKeypress: function() {
-
                 dropdown.opts.page.on('keydown', function(e) {
                     var keyCode = e.keyCode,
                         focused = $(e.currentTarget.activeElement),
@@ -76,7 +76,7 @@
                     if (focused.is('.action')) {
                         // Key handlers for when a menu item has focus
                         menu = focused.closest('.dropdown-menu');
-                        dropdown.handlerIsAction(keyCode, menu);
+                        dropdown.handlerIsAction(keyCode, menu, focused);
 
                     } else if (focused.is('.has-dropdown')) {
                         // Key handlers for when the button that opens the menu has focus
