eab32e668a39a56b71f561986556c385836827bd
Sven Marnach <sven@marnach.net>
2015-05-20 18:30:50 +0200

    Move test for discussion tab visibility into a separate function.

--- common/lib/xmodule/xmodule/tests/test_tabs.py
+++ common/lib/xmodule/xmodule/tests/test_tabs.py
@@ -694,7 +694,6 @@ class DiscussionLinkTestCase(TabTestCase):
         discussion_link_in_course="",
         is_staff=True,
         is_enrolled=True,
-        is_hideable=False,
     ):
         """Helper function to verify whether the discussion tab exists and can be displayed"""
         self.course.tabs = tab_list
@@ -708,14 +707,6 @@ class DiscussionLinkTestCase(TabTestCase):
             ),
             expected_can_display_value
         )
-
-        if is_hideable:
-            # Test that the discussion tab can be hidden
-            self.assertEquals(discussion.is_hideable, True)
-            self.assertEquals(discussion['is_hidden'], False)
-            discussion.is_hidden = True
-            self.assertEquals(discussion['is_hidden'], True)
-            discussion.is_hidden = False
 
     def test_explicit_discussion_link(self):
         """Test that setting discussion_link overrides everything else"""
@@ -744,7 +735,6 @@ class DiscussionLinkTestCase(TabTestCase):
             tab_list=self.tabs_with_discussion,
             expected_discussion_link="default_discussion_link",
             expected_can_display_value=True,
-            is_hideable=True,
         )
 
     def test_tabs_without_discussion(self):
@@ -777,3 +767,15 @@ class DiscussionLinkTestCase(TabTestCase):
             is_enrolled=is_enrolled,
             is_staff=is_staff
         )
+
+    def test_discussion_visibility(self):
+        """Test that the discussion tab can be hidden."""
+        self.settings.FEATURES['ENABLE_DISCUSSION_SERVICE'] = True
+        self.course.tabs = self.tabs_with_discussion
+        self.course.discussion_link = ''
+        discussion = tabs.CourseTabList.get_discussion(self.course)
+        self.assertTrue(discussion.is_hideable)
+        self.assertFalse(discussion['is_hidden'])
+        discussion.is_hidden = True
+        self.assertTrue(discussion['is_hidden'])
+        discussion.is_hidden = False
