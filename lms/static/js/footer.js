f6b4ac3d3cd659dbfa0bbca78cdec7ec414c9ff9
AlasdairSwan <alasdair_swan@hotmail.com>
2015-05-21 13:21:16 -0400

    Updated Scss to set ltr/rtl variables to fix issue with span-columns defaulting to float left. Cleaned up JS to remove duplication, minimize polution of global namespace and use jQuery variables name instead of $ for wider support (i.e. to work on Drupal).

--- lms/static/js/footer.js
+++ lms/static/js/footer.js
@@ -8,7 +8,7 @@ var edx = edx || {};
             el: '#edx-branding-footer',
 
             init: function() {
-                _fn.$el = _fn.$el || $( _fn.el );
+                _fn.$el = $( _fn.el );
 
                 /**
                  *  Only continue if the expected element
@@ -20,18 +20,6 @@ var edx = edx || {};
             },
 
             analytics: {
-                init: function() {
-                    _fn.$el = _fn.$el || $( _fn.el );
-
-                    /**
-                     *  Only continue if the expected element
-                     *  to add footer to is in the DOM
-                     */
-                    if ( _fn.$el.length > 0 ) {
-                        _fn.analytics.eventListener();
-                    }
-                },
-
                 eventListener: function() {
                     if ( window.analytics ) {
                         _fn.$el.on( 'click', 'a', _fn.analytics.track );
@@ -55,7 +43,7 @@ var edx = edx || {};
                 get: function() {
                     var url = _fn.$el.data('base-url') || 'https://courses.edx.org',
                         language = _fn.$el.data('language') || false,
-                        showOpenEdXLogo = Boolean(_fn.$el.data('show-openedx-logo')) || false,
+                        showOpenEdXLogo = !!_fn.$el.data('show-openedx-logo'),
                         params = [];
 
                     if (showOpenEdXLogo) {
@@ -66,10 +54,10 @@ var edx = edx || {};
                         params.push('language=' + language);
                     }
 
-                    url = url + '/api/v1/branding/footer.html';
+                    url += '/api/v1/branding/footer.html';
 
                     if (params) {
-                        url = url + '?' + params.join('&');
+                        url += '?' + params.join('&');
                     }
 
                     $.ajax({
@@ -84,16 +72,15 @@ var edx = edx || {};
 
                 render: function( html ) {
                     $(html).replaceAll(_fn.$el);
+                    _fn.analytics.eventListener();
                 }
             }
         };
 
         return {
-            load: _fn.init,
-            analytics: _fn.analytics.init
+            load: _fn.init
         };
     })();
 
     edx.footer.load();
-    edx.footer.analytics();
 })(jQuery);
