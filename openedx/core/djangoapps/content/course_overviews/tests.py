aa1b9cd26a48269b77887843ad59c9d89c66dfc3
Kyle McCormick <kylemccor@gmail.com>
2015-05-27 11:08:47 -0400

    add one silly test (doesn't actually work atm)

--- openedx/core/djangoapps/content/course_overviews/tests.py
+++ openedx/core/djangoapps/content/course_overviews/tests.py
@@ -1,1 +1,17 @@
-# TODO me: write tests
+
+from django.test import TestCase
+
+from opaque_keys.edx.locator import CourseKey
+
+from . import get_course_overview
+
+class CourseOverviewTests(TestCase):
+
+    # TODO me: write more comprehensive tests
+
+    def test_model(self):
+        course_ids = ['edX+DemoX+split-rerun']
+        for course_id in course_ids:
+            # TODO me: figure out why this doesn't work
+            overview = get_course_overview(CourseKey.from_string(course_id))
+            print overview
