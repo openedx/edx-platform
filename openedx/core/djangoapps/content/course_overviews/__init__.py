9acb5ba4a7e41525228a9d965a27033a69b75a4b
Kyle McCormick <kylemccor@gmail.com>
2015-05-27 10:29:35 -0400

    make get_course_overview handle case in which get_course returns None

--- openedx/core/djangoapps/content/course_overviews/__init__.py
+++ openedx/core/djangoapps/content/course_overviews/__init__.py
@@ -4,10 +4,12 @@ from xmodule.modulestore.django import modulestore
 from .models import CourseOverviewDescriptor
 
 def get_course_overview(course_id):
+    course_overview = None
     try:
         course_overview = CourseOverviewDescriptor.objects.get(id=course_id)
     except CourseOverviewDescriptor.DoesNotExist:
         course = modulestore().get_course(course_id)
-        course_overview = CourseOverviewDescriptor.create_from_course(course)
-        course_overview.save()
+        if course:
+            course_overview = CourseOverviewDescriptor.create_from_course(course)
+            course_overview.save()
     return course_overview
