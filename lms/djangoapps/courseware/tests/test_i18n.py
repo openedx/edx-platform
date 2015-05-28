eb3b8c5ce484ce965e93568d8b7cf616ae843e8a
Ned Batchelder <ned@edx.org>
2015-05-28 11:08:14 -0400

    TNL-2269 Compute language direction every time.
    The old code would compute the language direction once when the template
    was loaded.
    
    TNL-2269

--- lms/djangoapps/courseware/tests/test_i18n.py
+++ lms/djangoapps/courseware/tests/test_i18n.py
@@ -9,19 +9,40 @@ from django.test.utils import override_settings
 
 
 @attr('shard_1')
-@override_settings(LANGUAGES=(('eo', 'Esperanto'),))
+@override_settings(LANGUAGES=[('eo', 'Esperanto'), ('ar', 'Arabic')])
 class I18nTestCase(TestCase):
     """
     Tests for i18n
     """
+    def assert_tag_has_attr(self, content, tag, attname, value):
+        """Assert that a tag in `content` has a certain value in a certain attribute."""
+        regex = r"""<{tag} [^>]*\b{attname}=['"]([\w\d ]+)['"][^>]*>""".format(tag=tag, attname=attname)
+        match = re.search(regex, content)
+        self.assertTrue(match, "Couldn't find desired tag in %r" % content)
+        attvalues = match.group(1).split()
+        self.assertIn(value, attvalues)
+
     def test_default_is_en(self):
         response = self.client.get('/')
-        self.assertIn('<html lang="en">', response.content)
+        self.assert_tag_has_attr(response.content, "html", "lang", "en")
         self.assertEqual(response['Content-Language'], 'en')
-        self.assertTrue(re.search('<body.*class=".*lang_en">', response.content))
+        self.assert_tag_has_attr(response.content, "body", "class", "lang_en")
 
     def test_esperanto(self):
         response = self.client.get('/', HTTP_ACCEPT_LANGUAGE='eo')
-        self.assertIn('<html lang="eo">', response.content)
+        self.assert_tag_has_attr(response.content, "html", "lang", "eo")
         self.assertEqual(response['Content-Language'], 'eo')
-        self.assertTrue(re.search('<body.*class=".*lang_eo">', response.content))
+        self.assert_tag_has_attr(response.content, "body", "class", "lang_eo")
+
+    def test_switching_languages_bidi(self):
+        response = self.client.get('/')
+        self.assert_tag_has_attr(response.content, "html", "lang", "en")
+        self.assertEqual(response['Content-Language'], 'en')
+        self.assert_tag_has_attr(response.content, "body", "class", "lang_en")
+        self.assert_tag_has_attr(response.content, "body", "class", "ltr")
+
+        response = self.client.get('/', HTTP_ACCEPT_LANGUAGE='ar')
+        self.assert_tag_has_attr(response.content, "html", "lang", "ar")
+        self.assertEqual(response['Content-Language'], 'ar')
+        self.assert_tag_has_attr(response.content, "body", "class", "lang_ar")
+        self.assert_tag_has_attr(response.content, "body", "class", "rtl")
