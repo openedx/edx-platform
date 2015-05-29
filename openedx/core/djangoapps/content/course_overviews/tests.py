082cf095b2dc4ea5bbc6f541864ecf0c1d3c6396
Kyle McCormick <kylemccor@gmail.com>
2015-05-29 12:55:43 -0400

    fix course_image_url testing; fix CourseOverviewDescriptor testing

--- openedx/core/djangoapps/content/course_overviews/tests.py
+++ openedx/core/djangoapps/content/course_overviews/tests.py
@@ -1,41 +1,42 @@
 """Tests for content.course_overviews package"""
 
 import ddt
+import itertools
+import datetime
 
 from django.test import TestCase
-from opaque_keys.edx.locator import CourseKey
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+from xmodule.modulestore.tests.factories import CourseFactory
+from xmodule.modulestore import ModuleStoreEnum
 
 from . import get_course_overview
-from .models import CourseOverviewDescriptor
-from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
-
+from courseware.tests.tests import LoginEnrollmentTestCase
 
 # TODO me: finish this
 
-class CourseOverviewDescriptorTests(ModuleStoreTestCase):
+class CourseOverviewDescriptorTests(ModuleStoreTestCase, LoginEnrollmentTestCase):
     """Tests for CourseOverviewDescriptor model"""
 
-    @ddt.data(
+    TODAY = datetime.datetime.now()
+    YESTERDAY = TODAY - datetime.timedelta(days=1)
+    TOMORROW = TODAY + datetime.timedleta(days=1)
+    NEXT_MONTH = TODAY + datetime.timedleta(days=30)
 
-    )
-    def test_course_equals_course_overview(self, course_id):
+    def check_course_equals_course_overview(self, course):
         """Tests that a CourseDescriptor and its corresponding CourseOverviewDescriptor behave the same.
 
-        Specifically, given a course ID, test that all important attributes and methods return the same value when
+        Specifically, given a course, test that all important attributes and methods return the same value when
         called on each of the following:
-         - a CourseDescriptor
+         - the CourseDescriptor itself
          - a CourseOverviewDescriptor that was newly created (the result of a cache miss)
          - a CourseOverviewDescriptor that was loaded from the MySQL database (the result of cache hit)
         """
 
-        # Load up our course
-        course = modulestore().get_course(course_id)
-
-        # Delete this course from the CourseOverviewDescriptor cache, and then try to load it from the cache twice
-        # (the first one will be a miss, the second a hit)
-        CourseOverviewDescriptor.objects.filter(id=course_id).delete()
-        course_overview_cache_miss = get_course_overview(course_id)
-        course_overview_cache_hit = get_course_overview(course_id)
+        # Load the CourseOverviewDescriptor from the cache twice. The first load will be a cache miss (because the cache
+        # is empty) so the course will be newly created with CourseOverviewDescriptor.create_from_course. The second
+        # load will be a cache hit, so the course will be loaded from the cache.
+        course_overview_cache_miss = get_course_overview(course.id)
+        course_overview_cache_hit = get_course_overview(course.id)
 
         # Test if all fields (other than a couple special ones) are equal between the three descriptors
         fields_to_test = [
@@ -77,8 +78,8 @@ class CourseOverviewDescriptorTests(ModuleStoreTestCase):
         ]
         for attribute_name in fields_to_test:
             course_value = getattr(course, attribute_name)
-            cache_miss_value = getattr(course, attribute_name)
-            cache_hit_value = getattr(course, attribute_name)
+            cache_miss_value = getattr(course_overview_cache_miss, attribute_name)
+            cache_hit_value = getattr(course_overview_cache_hit, attribute_name)
             self.assertEqual(course_value, cache_miss_value)
             self.assertEqual(cache_miss_value, cache_hit_value)
 
@@ -94,10 +95,37 @@ class CourseOverviewDescriptorTests(ModuleStoreTestCase):
         ]
         for method_name in methods_to_test:
             course_value = getattr(course, method_name)()
-            cache_miss_value = getattr(course, method_name)()
-            cache_hit_value = getattr(course, method_name)()
+            cache_miss_value = getattr(course_overview_cache_miss, method_name)()
+            cache_hit_value = getattr(course_overview_cache_hit, method_name)()
             self.assertEqual(course_value, cache_miss_value)
             self.assertEqual(cache_miss_value, cache_hit_value)
+
+    @ddt.data(
+        itertools.product(
+            [
+                {
+                    "static_asset_path": "/my/cool/path",
+                    "display_name": "Test Course",
+                    "start": YESTERDAY,
+                    "end": TOMORROW
+                },
+                {}
+            ],
+            [ModuleStoreEnum.Type.mongo, ModuleStoreEnum.Type.split, ModuleStoreEnum.Type.xml],
+            [True, False]
+        )
+    )
+    def test_course_overvewiew_behavior(self, course_kwargs, modulestore_type, is_user_enrolled):
+        course = CourseFactory.create(
+            course="test_course",
+            org="edX",
+            default_store=modulestore_type,
+            *course_kwargs
+        )
+        self.setup_user()
+        if is_user_enrolled:
+            self.enroll(course, verify=True)
+        self.check_course_equals_course_overview(course)
 
 class MobileApiCourseOverviewTests(TestCase):
     """Tests to make sure the parts of the mobile API that have been changed to use this package still work right"""
