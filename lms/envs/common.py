052368df041778ba13cdb5f68c376e6858ac9205
David Adams <dcadams@stanford.edu>
2015-06-03 14:23:29 -0700

    Fix of course tiles after upstream merge.
    Basically fixes a merge issue, not really any new logic.
    Sets to False ENABLE_COURSE_SORTING_BY_START_DATE seting.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -332,7 +332,7 @@ FEATURES = {
     # When a user goes to the homepage ('/') the user see the
     # courses listed in the announcement dates order - this is default Open edX behavior.
     # Set to True to change the course sorting behavior by their start dates, latest first.
-    'ENABLE_COURSE_SORTING_BY_START_DATE': True,
+    'ENABLE_COURSE_SORTING_BY_START_DATE': False,
 
     # Expose Mobile REST API. Note that if you use this, you must also set
     # ENABLE_OAUTH2_PROVIDER to True
