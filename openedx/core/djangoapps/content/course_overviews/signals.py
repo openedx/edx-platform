abbe7489dc53a3d3226668a6a0ba07e9d60a00b2
Kyle McCormick <kylemccor@gmail.com>
2015-06-03 16:53:02 -0400

    improve documentation and code style

--- openedx/core/djangoapps/content/course_overviews/signals.py
+++ openedx/core/djangoapps/content/course_overviews/signals.py
@@ -1,11 +1,17 @@
+"""
+Signal handler for invalidating cached course overviews
+"""
+
 from django.dispatch.dispatcher import receiver
 
 from xmodule.modulestore.django import SignalHandler
 from .models import CourseOverviewDescriptor
 
 @receiver(SignalHandler.course_published)
-def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=unused-argument
-    """Invalidates the CourseOverviewDescriptor cache entry for a course whenever it is modified."""
-
+def listen_for_course_publish(_sender, course_key, **_kwargs):  # pylint: disable=unused-argument
+    """
+    Catches the signal that a course has been published in Studio and invalidates
+    the corresponding CourseOverviewDescriptor cache entry if one exists.
+    """
     # TODO me: confirm that all cache invalidations will be hit by this signal
     CourseOverviewDescriptor.objects.filter(id=course_key).delete()
