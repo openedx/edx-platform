26a68d38d281cbbb9bbf656e5d1ba7d6cc5d731b
Chris Dodge <cdodge@edx.org>
2015-06-04 15:43:40 -0400

    PHX-31. Add new feature and course flags.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -403,6 +403,9 @@ FEATURES = {
     # How many seconds to show the bumper again, default is 7 days:
     'SHOW_BUMPER_PERIODICITY': 7 * 24 * 3600,
 
+    # Timed Proctored Exams
+    'ENABLE_PROCTORED_EXAMS': False,
+
     # Enable OpenBadge support. See the BADGR_* settings later in this file.
     'ENABLE_OPENBADGES': False,
 
