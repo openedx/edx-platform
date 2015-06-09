0c7623d530201882d994b20877a571afdfdcaea3
Phil McGachey <phil_mcgachey@harvard.edu>
2015-06-08 21:38:34 -0400

    [LTI Provider] Fix bug preventing unenrolled users from accessing content
    Change https://github.com/edx/edx-platform/pull/8240 refactored the LTI
    provider template rendering code and introduced an issue where a user was
    required to be enrolled in a course before that course's content could be
    accessed over LTI. According to the LTI design spec:
        https://docs.google.com/document/d/185hdPvIxcKtiDOLjb4sTGovA_WYXWz5Cd79gCzQwBms
    we delegate access control over LTI content to the LTI consumer, rather
    than requiring that users enroll in edX courses explicitly (and that admins
    keep edX and LTI provider enrollemnts consistent when students add or drop
    courses).
    
    This change fixes the immediate issue, which is disrupting the LTI Provider
    pilot currently running at Harvard.

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -1389,7 +1389,7 @@ def _track_successful_certificate_generation(user_id, course_id):  # pylint: dis
 
 
 @require_http_methods(["GET", "POST"])
-def render_xblock(request, usage_key_string):
+def render_xblock(request, usage_key_string, check_if_enrolled=True):
     """
     Returns an HttpResponse with HTML content for the xBlock with the given usage_key.
     The returned HTML is a chromeless rendering of the xBlock (excluding content of the containing courseware).
@@ -1400,7 +1400,7 @@ def render_xblock(request, usage_key_string):
 
     with modulestore().bulk_operations(course_key):
         # verify the user has access to the course, including enrollment check
-        course = get_course_with_access(request.user, 'load', course_key, check_if_enrolled=True)
+        course = get_course_with_access(request.user, 'load', course_key, check_if_enrolled=check_if_enrolled)
 
         # get the block, which verifies whether the user has access to the block.
         block, _ = get_module_by_usage_id(
