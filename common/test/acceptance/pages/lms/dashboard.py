c35ebb765823fc58a4e887b2a5aac7a4907b5f99
Frances Botsford <frances@edx.org>
2015-06-10 21:57:41 -0400

    pr feedback

--- common/test/acceptance/pages/lms/dashboard.py
+++ common/test/acceptance/pages/lms/dashboard.py
@@ -174,14 +174,14 @@ class DashboardPage(PageObject):
         """
         return self.q(css='.dropdown-menu li a').text
 
+        def click_my_profile_link(self):
+            """
+            Click on `Profile` link.
+            """
+            self.q(css='.dropdown-menu li a').nth(1).click()
+
     def click_account_settings_link(self):
         """
         Click on `Account` link.
-        """
-        self.q(css='.dropdown-menu li a').nth(1).click()
-
-    def click_my_profile_link(self):
-        """
-        Click on `Profile` link.
         """
         self.q(css='.dropdown-menu li a').nth(2).click()
