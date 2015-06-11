4281f97724708049c3aa7c59e412181b248b6f24
Adam Palay <adam@edx.org>
2015-06-11 11:52:07 -0400

    add datadog metrics for compressed course structure sizes

--- common/lib/xmodule/xmodule/modulestore/split_mongo/mongo_connection.py
+++ common/lib/xmodule/xmodule/modulestore/split_mongo/mongo_connection.py
@@ -16,6 +16,7 @@ from time import time
 # Import this just to export it
 from pymongo.errors import DuplicateKeyError  # pylint: disable=unused-import
 from django.core.cache import get_cache
+import dogstats_wrapper as dog_stats_api
 
 from contracts import check, new_contract
 from mongodb_proxy import autoretry_read, MongoProxy
@@ -228,6 +229,13 @@ class CourseStructureCache(object):
         pickled_data = pickle.dumps(structure, pickle.HIGHEST_PROTOCOL)
         # 1 = Fastest (slightly larger results)
         compressed_pickled_data = zlib.compress(pickled_data, 1)
+
+        # record compressed course structure sizes
+        dog_stats_api.histogram(
+            'compressed_course_structure.size',
+            len(compressed_pickled_data),
+            tags=[structure_cache_key]
+        )
         # Stuctures are immutable, so we set a timeout of "never"
         self.cache.set(structure_cache_key, compressed_pickled_data, None)
 
