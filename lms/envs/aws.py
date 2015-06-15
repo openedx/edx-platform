77797ebd1b347f5738907cc487d4afc1a65b1be2
Braden MacDonald <braden@opencraft.com>
2015-06-15 11:58:41 -0700

    Make the SAML fetch period configurable

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -539,10 +539,11 @@ if FEATURES.get('ENABLE_THIRD_PARTY_AUTH'):
     # third_party_auth config moved to ConfigurationModels. This is for data migration only:
     THIRD_PARTY_AUTH_OLD_CONFIG = AUTH_TOKENS.get('THIRD_PARTY_AUTH', None)
 
-    CELERYBEAT_SCHEDULE['refresh-saml-metadata'] = {
-        'task': 'third_party_auth.fetch_saml_metadata',
-        'schedule': datetime.timedelta(hours=24),
-    }
+    if ENV_TOKENS.get('THIRD_PARTY_AUTH_SAML_FETCH_PERIOD_HOURS', 24) is not None:
+        CELERYBEAT_SCHEDULE['refresh-saml-metadata'] = {
+            'task': 'third_party_auth.fetch_saml_metadata',
+            'schedule': datetime.timedelta(hours=ENV_TOKENS.get('THIRD_PARTY_AUTH_SAML_FETCH_PERIOD_HOURS', 24)),
+        }
 
 ##### OAUTH2 Provider ##############
 if FEATURES.get('ENABLE_OAUTH2_PROVIDER'):
