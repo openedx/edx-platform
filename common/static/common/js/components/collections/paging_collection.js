804bd6d57dd8fc1af8efee04db4f00f7092e1700
Ben McMorran <ben.mcmorran@gmail.com>
2015-06-22 15:32:23 +0000

    Improve generic paging collection

--- common/static/common/js/components/collections/paging_collection.js
+++ common/static/common/js/components/collections/paging_collection.js
@@ -4,11 +4,49 @@
         var PagingCollection = BackbonePaginator.requestPager.extend({
             paginator_core: {
                 type: 'GET',
-                accepts: 'application/json',
                 dataType: 'json',
                 url: function () { return this.url }
             },
 
+            paginator_ui: {
+                firstPage: function () { return this.isZeroIndexed ? 0 : 1; },
+                currentPage: function () { return this.isZeroIndexed ? 0 : 1; },
+                perPage: function () { return this.perPage }
+            },
+
+            server_api: {
+                'page': function () { return this.currentPage; },
+                'page_size': function () { return this.perPage; }
+            },
+
+            parse: function (response) {
+                this.totalCount = response.count;
+                this.currentPage = response.current_page;
+                this.totalPages = response.num_pages;
+                this.start = response.start;
+                return response.results;
+            },
+
+            sortField: '',
+            sortDirection: 'desc',
+            sortableFields: {},
+
+            filterField: '',
+            filterableFields: {},
+
+            /**
+             * Returns the current page number as if numbering starts on page one, regardless of the indexing of the
+             * underlying server API.
+             */
+            currentOneIndexPage: function () {
+                return this.currentPage + (this.isZeroIndexed ? 1 : 0);
+            },
+
+            /**
+             * Sets the current page of the collection. Page is assumed to be one indexed, regardless of the indexing
+             * of the underlying server API.
+             * @param page one-indexed page to change to
+             */
             setPage: function (page) {
                 var oldPage = this.currentPage,
                     self = this;
@@ -23,25 +61,100 @@
                 })
             },
 
+            /**
+             * Returns true if the collection has a next page, false otherwise.
+             */
+            hasNextPage: function () {
+                return this.currentOneIndexPage() + 1 <= this.totalPages;
+            },
+
+            /**
+             * Returns true if the collection has a previous page, false otherwise.
+             */
+            hasPreviousPage: function () {
+                return this.currentOneIndexPage() - 1 >= 1;
+            },
+
+            /**
+             * Moves the collection to the next page, if it exists.
+             */
             nextPage: function () {
-                if (this.currentPage < this.totalPages - 1) {
-                    this.setPage(this.currentPage + 1);
+                if (this.hasNextPage()) {
+                    this.setPage(this.currentOneIndexPage() + 1);
                 }
             },
 
+            /**
+             * Moves the collection to the previous page, if it exists.
+             */
             previousPage: function () {
-                if (this.currentPage > 0) {
-                    this.setPage(this.currentPage - 1)
+                if (this.hasPreviousPage()) {
+                    this.setPage(this.currentOneIndexPage() - 1);
                 }
             },
 
-            sortDisplayName: function () {
-                return '';
+            /**
+             * Adds the given field to the list of fields that can be sorted on.
+             * @param fieldName name of the field for the server API
+             * @param displayName name of the field to display to the user
+             */
+            registerSortableField: function (fieldName, displayName) {
+                this.addField(this.sortableFields, fieldName, displayName);
             },
 
+            /**
+             * Adds the given field to the list of fields that can be filtered on.
+             * @param fieldName name of the field for the server API
+             * @param displayName name of the field to display to the user
+             */
+            registerFilterableField: function (fieldName, displayName) {
+                this.addField(this.filterableFields, fieldName, displayName);
+            },
+
+            /**
+             * For internal use only. Adds the given field to the given collection of fields.
+             * @param fields object of existing fields
+             * @param fieldName name of the field for the server API
+             * @param displayName name of the field to display to the user
+             */
+            addField: function (fields, fieldName, displayName) {
+                fields[fieldName] = {
+                    displayName: displayName
+                };
+            },
+
+            /**
+             * Returns the display name of the field that the collection is currently sorted on.
+             */
+            sortDisplayName: function () {
+                return this.sortableFields[this.sortField].displayName;
+            },
+
+            /**
+             * Returns the display name of the field that the collection is currently filtered on.
+             */
             filterDisplayName: function () {
-                return '';
+                return this.filterableFields[this.filterField].displayName;
+            },
+
+            /**
+             * Sets the field to sort on.
+             * @param fieldName name of the field to sort on
+             */
+            setSortField: function (fieldName) {
+                this.sortField = fieldName;
+                this.setPage(1);
+            },
+
+            /**
+             * Sets the field to filter on.
+             * @param fieldName name of the field to filter on
+             */
+            setFilterField: function (fieldName) {
+                this.filterField = fieldName;
+                this.setPage(1);
             }
-        })
+        });
+        return PagingCollection;
     });
 }).call(this, define || RequireJS.define);
--- lms/djangoapps/teams/static/teams/js/collections/topic.js
+++ lms/djangoapps/teams/static/teams/js/collections/topic.js
@@ -1,77 +1,23 @@
 ;(function (define) {
     'use strict';
-    define(['backbone.paginator', 'teams/js/models/topic'], function(BackbonePaginator, TopicModel) {
-        var TopicCollection = BackbonePaginator.requestPager.extend({
-            initialize: function(topics, options) {
-                BackbonePaginator.requestPager.prototype.initialize.call(this);
-                this.course_id = options.course_id;
-                this.perPage = topics.results.length;
-            },
-            model: TopicModel,
-            paginator_core: {
-                type: 'GET',
-                //accepts: 'application/json', // TODO: this argument just doesn't work when enabled (passes "undefined"). DRF does JSON by default. Is that good enough?
-                dataType: 'json',
-                url: function() {return this.url;}
-            },
-            paginator_ui: { // TODO: what is the significance of these values?
-                firstPage: 1,
-                currentPage: 1,
-                perPage: function() {return this.perPage;}
-            },
-            sort_field: 'name',
-            sortDisplayName: function() {return this.sort_field;},
-            server_api: {
-                'course_id': function() {return this.course_id;},
-                'order_by': function() {return this.sort_field;},
-                'page_size': function() {return this.perPage;},
-                'page': function() {return this.currentPage;}
-            },
+    define(['common/js/components/collections/paging_collection', 'teams/js/models/topic', 'gettext'],
+        function(PagingCollection, TopicModel, gettext) {
+            var TopicCollection = PagingCollection.extend({
+                initialize: function(topics, options) {
+                    PagingCollection.prototype.initialize.call(this);
+                    this.isZeroIndexed = false;
+                    this.course_id = options.course_id;
+                    this.perPage = topics.results.length;
+                    this.server_api['course_id'] = function () { return this.course_id; };
+                    this.server_api['order_by'] = function () { return this.sortField; };
 
-            parse: function(response) {
-                this.totalCount = response.count;
-                this.currentPage = response.current_page;
-                this.totalPages = response.num_pages;
-                this.start = response.start;
-                return response.results;
-            },
+                    this.registerSortableField('name', gettext('name'));
+                    this.registerSortableField('team_count', gettext('team count'));
+                    this.setSortField('name');
+                },
 
-            hasPreviousPage: function () {
-                return this.firstPage < this.currentPage;
-            },
-
-            hasNextPage: function () {
-                return this.currentPage + (1 - this.firstPage) < this.totalPages;
-            },
-
-            setPage: function (page) {
-                var oldPage = this.currentPage,
-                    self = this;
-                this.goTo(page, {
-                    reset: true,
-                    success: function () {
-                        self.trigger('page_changed');
-                        self.trigger('reset');
-                        self.trigger('sync');
-                    },
-                    error: function () {
-                        self.currentPage = oldPage;
-                    }
-                });
-            },
-
-            nextPage: function () {
-                if (this.hasNextPage()) {
-                    this.setPage(this.currentPage + 1);
-                }
-            },
-
-            previousPage: function () {
-                if (this.hasPreviousPage()) {
-                    this.setPage(this.currentPage - 1);
-                }
-            }
-        });
-        return TopicCollection;
+                model: TopicModel
+            });
+            return TopicCollection;
     });
 }).call(this, define || RequireJS.define);
