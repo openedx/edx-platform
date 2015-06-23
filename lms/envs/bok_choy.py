2ecd41aa8220bfbf32aadfa38111503e6f31b8ca
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-06-23 14:23:15 +0500

    Upgrade YouTube API from v2 to v3
    Cherry-picked from cafbe74d439d1a5ea762d0802da6644e6492fa29
    
    Using youtube api (v3) instead of v2 to get the video duration .
    
    TNL-2413
    
    Conflicts:
    	cms/envs/aws.py
    	cms/envs/common.py
    	common/lib/xmodule/xmodule/js/src/video/01_initialize.js
    	common/lib/xmodule/xmodule/tests/test_video.py
    	common/lib/xmodule/xmodule/video_module/video_module.py
    	lms/djangoapps/courseware/tests/test_video_mongo.py
    	lms/envs/aws.py

--- lms/envs/bok_choy.py
+++ lms/envs/bok_choy.py
@@ -103,8 +103,8 @@ FEATURES['ENTRANCE_EXAMS'] = True
 
 # Point the URL used to test YouTube availability to our stub YouTube server
 YOUTUBE_PORT = 9080
-YOUTUBE['API'] = "127.0.0.1:{0}/get_youtube_api/".format(YOUTUBE_PORT)
-YOUTUBE['TEST_URL'] = "127.0.0.1:{0}/test_youtube/".format(YOUTUBE_PORT)
+YOUTUBE['API'] = "http://127.0.0.1:{0}/get_youtube_api/".format(YOUTUBE_PORT)
+YOUTUBE['METADATA_URL'] = "http://127.0.0.1:{0}/test_youtube/".format(YOUTUBE_PORT)
 YOUTUBE['TEXT_API']['url'] = "127.0.0.1:{0}/test_transcripts_youtube/".format(YOUTUBE_PORT)
 
 ############################# SECURITY SETTINGS ################################
