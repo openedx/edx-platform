7747a7236f7072ad49182dab90cc9b89292e728b
Peter Fogg <pfogg@edx.org>
2015-06-23 17:39:21 -0400

    Add Bok Choy tests.

--- common/test/acceptance/pages/lms/teams.py
+++ common/test/acceptance/pages/lms/teams.py
@@ -3,7 +3,16 @@
 Teams page.
 """
 
+from selenium.webdriver.common.keys import Keys
 from .course_page import CoursePage
+
+
+BROWSE_BUTTON_CSS = 'a.nav-item[data-index="1"]'
+TOPIC_CARD_CSS = 'div.card-core-wrapper'
+PAGE_NUMBER_INPUT_CSS = 'input#page-number-input'
+NEXT_PAGE_BUTTON_CSS = 'a.next-page-link'
+PAGINATION_TEXT_CSS = 'div.search-tools'
+CURRENT_PAGE_TEXT_CSS = 'span.current-page'
 
 
 class TeamsPage(CoursePage):
@@ -24,3 +33,27 @@ class TeamsPage(CoursePage):
             description="Body text is present"
         )
         return self.q(css=main_page_content_css).text[0]
+
+    def browse_topics(self):
+        """ View the Browse tab of the Teams page. """
+        self.q(css=BROWSE_BUTTON_CSS).click()
+
+    def get_topic_cards(self):
+        """Return a list of the topic cards present on the page."""
+        return self.q(css=TOPIC_CARD_CSS).results
+
+    def get_pagination_text(self):
+        """Return the text showing which topics the user is currently viewing."""
+        return self.q(css=PAGINATION_TEXT_CSS).text[0]
+
+    def get_current_page_text(self):
+        """Return the text showing the current page."""
+        return self.q(css=CURRENT_PAGE_TEXT_CSS).text[0]
+
+    def go_to_topics_list_page(self, page_number):
+        """Go to the given page_number in the topics list results."""
+        self.q(css=PAGE_NUMBER_INPUT_CSS).results[0].send_keys(unicode(page_number), Keys.ENTER)
+
+    def press_next_page_button(self):
+        """Press the next page button in the topics list results."""
+        self.q(css=NEXT_PAGE_BUTTON_CSS).click()
