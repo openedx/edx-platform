fc32820fd02ee3192cae4e1f26f1abcabec765a2
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-06-23 14:23:15 +0500

    Using youtube api (v3) instead of v2 to get the video duration .
    TNL-2413

--- cms/envs/acceptance.py
+++ cms/envs/acceptance.py
@@ -118,8 +118,8 @@ except ImportError:
     pass
 
 # Point the URL used to test YouTube availability to our stub YouTube server
-YOUTUBE['API'] = "127.0.0.1:{0}/get_youtube_api/".format(YOUTUBE_PORT)
-YOUTUBE['TEST_URL'] = "127.0.0.1:{0}/test_youtube/".format(YOUTUBE_PORT)
+YOUTUBE['API'] = "http://127.0.0.1:{0}/get_youtube_api/".format(YOUTUBE_PORT)
+YOUTUBE['METADATA_URL'] = "http://127.0.0.1:{0}/test_youtube/".format(YOUTUBE_PORT)
 YOUTUBE['TEXT_API']['url'] = "127.0.0.1:{0}/test_transcripts_youtube/".format(YOUTUBE_PORT)
 
 # Generate a random UUID so that different runs of acceptance tests don't break each other
