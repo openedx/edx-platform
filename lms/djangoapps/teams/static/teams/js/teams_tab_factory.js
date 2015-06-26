dcac35fc481573d579809fc993f72cb315538e09
cahrens <christina@edx.org>
2015-06-26 09:52:55 -0400

    Restore RequireJS wrapper around teams_tab_factory.

--- lms/djangoapps/teams/static/teams/js/teams_tab_factory.js
+++ lms/djangoapps/teams/static/teams/js/teams_tab_factory.js
@@ -1,10 +1,13 @@
-define(['jquery','teams/js/views/teams_tab'],
-   function ($, TeamsTabView) {
-        'use strict';
-        return function () {
-            var view = new TeamsTabView({
-                el: $('.teams-content')
-            });
-            view.render();
-        };
-    });
+;(function (define) {
+    'use strict';
+
+    define(['jquery','teams/js/views/teams_tab'],
+        function ($, TeamsTabView) {
+            return function () {
+                var view = new TeamsTabView({
+                    el: $('.teams-content')
+                });
+                view.render();
+            };
+        });
+}).call(this, define || RequireJS.define);
