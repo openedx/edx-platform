a127912b58cfacfa6fd70c82816a98fe7b805986
Chris Dodge <cdodge@edx.org>
2015-07-06 10:43:12 -0400

    Proctoring backbone has been moved to edx-proctoring

--- lms/static/js/courseware/proctored_exam/proctored_app.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var proctored_exam_view = new edx.coursware.proctored_exam.ProctoredExamView({
-        el: $(".proctored_exam_status"),
-        proctored_template: '#proctored-exam-status-tpl',
-        model: new ProctoredExamModel()
-    });
-    proctored_exam_view.render();
-});
