91236219cb52005ddd97e21087ba6670afc49040
Calen Pennington <cale@edx.org>
2015-07-10 13:19:00 -0400

    Make CcxFactory fill in a coach if one isn't supplied

--- lms/djangoapps/ccx/tests/factories.py
+++ lms/djangoapps/ccx/tests/factories.py
@@ -1,7 +1,9 @@
 """
 Dummy factories for tests
 """
+from factory import SubFactory
 from factory.django import DjangoModelFactory
+from student.tests.factories import UserFactory
 from ccx.models import CustomCourseForEdX  # pylint: disable=import-error
 from ccx.models import CcxMembership  # pylint: disable=import-error
 from ccx.models import CcxFutureMembership  # pylint: disable=import-error
@@ -11,6 +13,7 @@ class CcxFactory(DjangoModelFactory):  # pylint: disable=missing-docstring
     FACTORY_FOR = CustomCourseForEdX
     display_name = "Test CCX"
     id = None  # pylint: disable=redefined-builtin, invalid-name
+    coach = SubFactory(UserFactory)
 
 
 class CcxMembershipFactory(DjangoModelFactory):  # pylint: disable=missing-docstring
