07baaec61d84cb1da75815bd65788073557cfba6
Jonathan Piacenti <kelketek@gmail.com>
2015-07-14 16:04:56 +0000

    Fix inline discussion cohort display when no longer cohorted.

--- lms/djangoapps/django_comment_client/forum/views.py
+++ lms/djangoapps/django_comment_client/forum/views.py
@@ -153,12 +153,12 @@ def get_threads(request, course_key, discussion_id=None, per_page=THREADS_PER_PA
     return threads, query_params
 
 
-def _set_group_names(course_id, threads):
+def _set_group_names(course, threads):
     """ Adds group name if the thread has a group id"""
 
     for thread in threads:
-        if thread.get('group_id'):
-            thread['group_name'] = get_cohort_by_id(course_id, thread.get('group_id')).name
+        if thread.get('group_id') and is_course_cohorted(course):
+            thread['group_name'] = get_cohort_by_id(course, thread.get('group_id')).name
             thread['group_string'] = "This post visible only to Group %s." % (thread['group_name'])
         else:
             thread['group_name'] = ""
@@ -496,7 +496,7 @@ def followed_threads(request, course_key, user_id):
             query_params['group_id'] = group_id
 
         threads, page, num_pages = profiled_user.subscribed_threads(query_params)
-        threads = _set_group_names(course_key, threads)
+        threads = _set_group_names(course, threads)
         query_params['page'] = page
         query_params['num_pages'] = num_pages
         user_info = cc.User.from_django_user(request.user).to_dict()
