0c2d45a46534f3f8100f79f85cb7b0c7259cdb9b
Chris Dodge <cdodge@edx.org>
2015-07-19 17:21:18 -0400

    Add proctored exams to the list of credit requirements

--- cms/djangoapps/contentstore/signals.py
+++ cms/djangoapps/contentstore/signals.py
@@ -9,114 +9,33 @@ from django.dispatch import receiver
 
 from xmodule.modulestore.django import SignalHandler, modulestore
 from contentstore.courseware_index import CoursewareSearchIndexer, LibrarySearchIndexer
-
-from edx_proctoring.api import (
-    get_exam_by_content_id,
-    update_exam,
-    create_exam,
-    get_all_exams_for_course,
-)
-from edx_proctoring.exceptions import (
-    ProctoredExamNotFoundException
-)
-
-log = logging.getLogger(__name__)
+from contentstore.proctoring import register_timed_and_proctored_exams
+from openedx.core.djangoapps.credit.signals import on_course_publish
 
 
 @receiver(SignalHandler.course_published)
 def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=unused-argument
     """
-    Receives signal and kicks off celery task to update search index
+    Receives publishing signal and performs publishing related workflows, such as
+    registering proctored exams, building up credit requirements, and performing
+    search indexing
     """
+
+    # first is to registered exams, the credit subsystem will assume that
+    # all proctored exams have already been registered, so we have to do that first
+    register_timed_and_proctored_exams(course_key)
+
+    # then call into the credit subsystem (in /openedx/djangoapps/credit)
+    # to perform any 'on_publish' workflow
+    on_course_publish(course_key)
+
+    # Finally call into the course search subsystem
+    # to kick off an indexing action
+
     # import here, because signal is registered at startup, but items in tasks are not yet able to be loaded
     from .tasks import update_search_index
     if CoursewareSearchIndexer.indexing_is_enabled():
         update_search_index.delay(unicode(course_key), datetime.now(UTC).isoformat())
-
-
-@receiver(SignalHandler.course_published)
-def look_for_timed_exam_publishing(sender, course_key, **kwargs):  # pylint: disable=unused-argument
-    """
-    Receives a course published signal an examines the course for sequences
-    that are marked as timed exams
-    """
-
-    if not settings.FEATURES.get('ENABLE_PROCTORED_EXAMS'):
-        # if feature is not enabled then do a quick exit
-        return
-
-    course = modulestore().get_course(course_key)
-    if not course.enable_proctored_exams:
-        # likewise if course does not have this feature turned on
-        return
-
-    # get all sequences, since they can be marked as timed/proctored exams
-    timed_exams = modulestore().get_items(
-        course_key,
-        qualifiers={
-            'category': 'sequential',
-        },
-        settings={
-            'is_time_limited': True,
-        }
-    )
-
-    # enumerate over list of sequences which are time-limited and
-    # add/update any exam entries in edx-proctoring
-    for timed_exam in timed_exams:
-        msg = (
-            'Found {location} as a timed-exam in course structure. Inspecting...'.format(
-                location=unicode(timed_exam.location)
-            )
-        )
-        log.info(msg)
-
-        try:
-            exam = get_exam_by_content_id(unicode(course_key), unicode(timed_exam.location))
-            # update case, make sure everything is synced
-            update_exam(
-                exam_id=exam['id'],
-                exam_name=timed_exam.display_name,
-                time_limit_mins=timed_exam.default_time_limit_mins,
-                is_proctored=timed_exam.is_proctored_enabled,
-                is_active=True
-            )
-            msg = 'Updated timed exam {exam_id}'.format(exam_id=exam['id'])
-            log.info(msg)
-        except ProctoredExamNotFoundException:
-            exam_id = create_exam(
-                course_id=unicode(course_key),
-                content_id=unicode(timed_exam.location),
-                exam_name=timed_exam.display_name,
-                time_limit_mins=timed_exam.default_time_limit_mins,
-                is_proctored=timed_exam.is_proctored_enabled,
-                is_active=True
-            )
-            msg = 'Created new timed exam {exam_id}'.format(exam_id=exam_id)
-            log.info(msg)
-
-    # then see which exams we have in edx-proctoring that are not in
-    # our current list. That means the the user has disabled it
-
-    exams = get_all_exams_for_course(course_key)
-
-    for exam in exams:
-        if exam['is_active']:
-            # try to look up the content_id in the sequences location
-
-            search = [
-                timed_exam for timed_exam in timed_exams if
-                unicode(timed_exam.location) == exam['content_id']
-            ]
-            if not search:
-                # This means it was turned off in Studio, we need to mark
-                # the exam as inactive (we don't delete!)
-                msg = 'Disabling timed exam {exam_id}'.format(exam_id=exam['id'])
-                log.info(msg)
-                update_exam(
-                    exam_id=exam['id'],
-                    is_active=False,
-                )
 
 
 @receiver(SignalHandler.library_updated)
