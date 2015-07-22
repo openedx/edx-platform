60290cca790941dfcc3a9e79fd47725da8f7941e
Andy Armstrong <andya@edx.org>
2015-07-22 18:56:19 -0400

    Add skeleton pages

--- lms/djangoapps/onboarding/urls.py
+++ lms/djangoapps/onboarding/urls.py
@@ -1,11 +1,17 @@
 """Defines the URL routes for this app."""
 
+from django.conf import settings
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
 
-from .views import HomePageView
+from .views import (
+    HomePageView, LoginView, RegisterView, CourseDiscoveryView, CourseAboutView,
+)
 
 urlpatterns = patterns(
     'onboarding.views',
-    url(r'^home', HomePageView.as_view(), name='home_page'),
+    url(r'^home$', HomePageView.as_view(), name='home_page'),
+    url(r'^login$', LoginView.as_view(), name='login_page'),
+    url(r'^register$', RegisterView.as_view(), name='registration_page'),
+    url(r'^course_discovery$', CourseDiscoveryView.as_view(), name='course_discovery_page'),
+    url(r'^course/{}$'.format(settings.COURSE_KEY_PATTERN), CourseAboutView.as_view(), name='course_about_page'),
 )
--- lms/djangoapps/onboarding/views.py
+++ lms/djangoapps/onboarding/views.py
@@ -17,3 +17,57 @@ class HomePageView(View):
         """
         context = {}
         return render_to_response("onboarding/index.html", context)
+
+
+class LoginView(View):
+    """
+    View methods related to the login page.
+    """
+
+    def get(self, request):
+        """
+        Renders the login page.
+        """
+        context = {}
+        return render_to_response("onboarding/login.html", context)
+
+
+class RegisterView(View):
+    """
+    View methods related to the registration page.
+    """
+
+    def get(self, request):
+        """
+        Renders the registration page.
+        """
+        context = {}
+        return render_to_response("onboarding/register.html", context)
+
+
+class CourseDiscoveryView(View):
+    """
+    View methods related to the course discovery page.
+    """
+
+    def get(self, request):
+        """
+        Renders the course discovery page.
+        """
+        context = {}
+        return render_to_response("onboarding/course_discovery.html", context)
+
+
+class CourseAboutView(View):
+    """
+    View methods related to the course about page.
+    """
+
+    def get(self, request, course_key_string):
+        """
+        Renders the course about page.
+        """
+        context = {
+            "course_key_string": course_key_string,
+        }
+        return render_to_response("onboarding/course_about.html", context)
