47b81c927496513b884a4c3a978fff94f89990c6
Will Daly <will@edx.org>
2015-07-24 09:59:23 -0700

    Allow users to submit initial verification at in-course checkpoints.
    * Combine verification and reverification photo submission end-points.
    * Combine verification and reverification Backbone models.
    * Allow deletion of verification status in Django admin.
    * Initial verification from a checkpoint is associated with the checkpoint.
    * Fix critical bug in which an invalid link was sent to Software Secure
      for the photo ID image of reverification attempts.

--- lms/static/js/verify_student/pay_and_verify.js
+++ lms/static/js/verify_student/pay_and_verify.js
@@ -34,6 +34,8 @@ var edx = edx || {};
         errorModel: errorView.model,
         displaySteps: el.data('display-steps'),
         currentStep: el.data('current-step'),
+        courseKey: el.data('course-key'),
+        checkpointLocation: el.data('checkpoint-location'),
         stepInfo: {
             'intro-step': {
                 courseName: el.data('course-name'),
