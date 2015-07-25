0811754c69addcf3e5bba6d204ebda3e23d952e9
Chris Dodge <cdodge@edx.org>
2015-07-24 20:22:38 -0400

    Integration of edx_proctoring into the LMS

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -173,6 +173,9 @@ FEATURES = {
     # Show video bumper in Studio
     'ENABLE_VIDEO_BUMPER': False,
 
+    # Timed Proctored Exams
+    'ENABLE_PROCTORED_EXAMS': False,
+
     # How many seconds to show the bumper again, default is 7 days:
     'SHOW_BUMPER_PERIODICITY': 7 * 24 * 3600,
 
@@ -775,6 +778,9 @@ INSTALLED_APPS = (
     'openedx.core.djangoapps.credit',
 
     'xblock_django',
+
+    # edX Proctoring
+    'edx_proctoring',
 )
 
 
@@ -1039,7 +1045,6 @@ CREDIT_PROVIDER_TIMESTAMP_EXPIRATION = 15 * 60
 ################################ Deprecated Blocks Info ################################
 
 DEPRECATED_BLOCK_TYPES = ['peergrading', 'combinedopenended']
-
 
 #### PROCTORING CONFIGURATION DEFAULTS
 
