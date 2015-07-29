a62ef9ffa9aaa7cde2818f9fd972ad38c4227630
Davorin Sego <davorin.sego@gmail.com>
2015-07-29 21:24:38 +0200

    quality

--- common/lib/xmodule/xmodule/util/null_handler.py
+++ common/lib/xmodule/xmodule/util/null_handler.py
@@ -1,10 +1,11 @@
 # pylint: disable=missing-docstring
 
+
 class NullHandler(object):
     """
     Responds to an any method call.
     """
     def __getattr__(self, name):
-        def method(*args, **kwargs): # pylint: disable=unused-argument
+        def method(*args, **kwargs):  # pylint: disable=unused-argument
             pass
         return method
