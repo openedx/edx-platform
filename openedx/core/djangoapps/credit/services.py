3bad96134a7abdebee947d34d7836f63f9ef7f66
Chris Dodge <cdodge@edx.org>
2015-07-29 21:26:11 -0400

    add CreditService as a runtime injection into edx_proctoring, also add profile_fullname to result set

--- openedx/core/djangoapps/credit/services.py
+++ openedx/core/djangoapps/credit/services.py
@@ -7,9 +7,9 @@ import logging
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
-#from opaque_keys.edx.keys import CourseKey
+from opaque_keys.edx.keys import CourseKey
 
-from student.models import CourseEnrollment
+from student.models import CourseEnrollment, UserProfile
 
 log = logging.getLogger(__name__)
 
@@ -19,7 +19,7 @@ class CreditService(object):
     Course Credit XBlock service
     """
 
-    def get_credit_state(self, user_id, course_key):
+    def get_credit_state(self, user_id, course_key_or_id):
         """
         Return all information about the user's credit state inside of a given
         course.
@@ -33,6 +33,7 @@ class CreditService(object):
             - or -
             {
                 'enrollment_mode': the mode that the user is enrolled in the course
+                'profile_fullname': the name that the student registered under, used for verification
                 'credit_requirement_status': the user's status in fulfilling those requirements
             }
         """
@@ -52,6 +53,25 @@ class CreditService(object):
             # bad user_id
             return None
 
+        # since we have to do name matching during various
+        # verifications, User must have a UserProfile
+        try:
+            profile = UserProfile.objects.get(user_id=user_id)
+        except ObjectDoesNotExist:
+            # this shouldn't happen under normal circumstances
+            return None
+
+        course_key = (
+            CourseKey.from_string(course_key_or_id)
+            if isinstance(course_key_or_id, basestring)
+            else course_key_or_id
+        )
+
+        #if isinstance(course_key_or_id, basestring):
+        #    course_key = CourseKey.from_string(course_key_or_id)
+        #else:
+        #    course_key = course_key_or_id
+
         enrollment = CourseEnrollment.get_enrollment(user, course_key)
         if not enrollment or not enrollment.is_active:
             # not enrolled
@@ -62,10 +82,11 @@ class CreditService(object):
 
         return {
             'enrollment_mode': enrollment.mode,
+            'profile_fullname': profile.name,
             'credit_requirement_status': get_credit_requirement_status(course_key, user.username)
         }
 
-    def set_credit_requirement_status(self, user_id, course_key, req_namespace,
+    def set_credit_requirement_status(self, user_id, course_key_or_id, req_namespace,
                                       req_name, status="satisfied", reason=None):
         """
         A simple wrapper around the method of the same name in api.eligibility.py. The only difference is
@@ -76,6 +97,12 @@ class CreditService(object):
 
         # need to get user_name
         user = User.objects.get(id=user_id)
+
+        course_key = (
+            CourseKey.from_string(course_key_or_id)
+            if isinstance(course_key_or_id, basestring)
+            else course_key_or_id
+        )
 
         # This seems to need to be here otherwise we get
         # circular references when starting up the app
