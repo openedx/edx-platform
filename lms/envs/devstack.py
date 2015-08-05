befdb985bef1ef4a82318ea9eaa574430ab19ab0
Jonathan Piacenti <kelketek@gmail.com>
2015-08-05 04:34:24 +0000

    Remove redundant Django Debug Toolbar config.

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -109,41 +109,6 @@ DEBUG_TOOLBAR_PANELS += (
     'debug_toolbar_mongo.panel.MongoDebugPanel',
 )
 
-################################ DEBUG TOOLBAR ################################
-FEATURES['DEBUG_TOOLBAR'] = True
-if FEATURES.get('DEBUG_TOOLBAR'):
-    INSTALLED_APPS += ('debug_toolbar',)
-    MIDDLEWARE_CLASSES += ('django_comment_client.utils.QueryCountDebugMiddleware',
-                           'debug_toolbar.middleware.DebugToolbarMiddleware',
-                           )
-    INTERNAL_IPS = ('127.0.0.1',)
-
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.versions.VersionsPanel',
-        'debug_toolbar.panels.timer.TimerPanel',
-        'debug_toolbar.panels.settings.SettingsPanel',
-        'debug_toolbar.panels.headers.HeadersPanel',
-        'debug_toolbar.panels.request.RequestPanel',
-        'debug_toolbar.panels.sql.SQLPanel',
-        'debug_toolbar.panels.signals.SignalsPanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
-        #  Enabling the profiler has a weird bug as of django-debug-toolbar==0.9.4 and
-        #  Django=1.3.1/1.4 where requests to views get duplicated (your method gets
-        #  hit twice). So you can uncomment when you need to diagnose performance
-        #  problems, but you shouldn't leave it on.
-        #  'debug_toolbar.panels.profiling.ProfilingPanel',
-    )
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-        'SHOW_TOOLBAR_CALLBACK': lambda _: True,
-    }
-
-    INSTALLED_APPS += (
-        # Mongo perf stats
-        'debug_toolbar_mongo',
-    )
-
 
 ########################### PIPELINE #################################
 
