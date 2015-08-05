3304c056ba1a9beb0fd65f33ccdce1e4912908e6
Jonathan Piacenti <kelketek@gmail.com>
2015-08-05 03:04:59 +0000

    More resiliant score update signaling.

--- lms/djangoapps/courseware/tests/test_module_render.py
+++ lms/djangoapps/courseware/tests/test_module_render.py
@@ -1286,7 +1286,7 @@ class TestModuleTrackingContext(ModuleStoreTestCase):
                 break
             self.fail('Event type "problem_check" not found in call list.')
 
-        return call_data['context']['module']['display_name']
+        return call_data['context']['module']
 
     def test_missing_display_name(self, mock_tracker):
         actual_display_name = self.handle_callback_and_get_module_info(mock_tracker)['display_name']
@@ -1358,6 +1358,7 @@ class TestXmoduleRuntimeEvent(TestSubmittingProblems):
         self.assertIsNone(student_module.max_grade)
 
     @patch('courseware.module_render.SCORE_CHANGED.send')
+    @patch.dict(settings.FEATURES, {'MARK_PROGRESS_ON_GRADING_EVENT': True})
     def test_score_change_signal(self, send_mock):
         """Test that a Django signal is generated when a score changes"""
         self.set_module_grade_using_publish(self.grade_dict)
