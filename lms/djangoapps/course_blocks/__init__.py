264f7aec2e363e7edb85756368ea2c2221f0b88c
Nimisha Asthagiri <nasthagiri@edx.org>
2015-08-09 20:03:23 -0400

    LMS Blocks Transformers: Visibility, Start Date, User Partitions

---
+++ lms/djangoapps/course_blocks/__init__.py
@@ -0,0 +1,7 @@
+"""
+...
+"""
+# TODO 8874: Finish docstrings for all of lms_course_cache/
+
+# Importing signals is necessary to active the course publish/delete signal handlers.
+import signals  # pylint: disable=unused-import
---
+++ lms/djangoapps/course_blocks/tests.py
@@ -0,0 +1,5 @@
+"""
+Tests for the LMS side of the course cache.
+"""
+
+# TODO 8874: Write tests to cover api.py and signals.py.
