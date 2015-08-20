0ce260e42ea22256e4e8761f64496a5f1d676e36
Marko Jevtic <marjevtic@gmail.com>
2015-08-20 10:55:10 +0000

    (YONK-120) Fix for 500 error on progress page

--- lms/djangoapps/gradebook/management/commands/tests/test_generate_gradebook_entries.py
+++ lms/djangoapps/gradebook/management/commands/tests/test_generate_gradebook_entries.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django.test.utils import override_settings
 from mock import MagicMock, patch
 import uuid
+import json
 
 from django.conf import settings
 
@@ -15,6 +16,8 @@ from courseware.model_data import FieldDataCache
 from gradebook.management.commands import generate_gradebook_entries
 from gradebook.models import StudentGradebook, StudentGradebookHistory
 from student.tests.factories import UserFactory, CourseEnrollmentFactory
+from xmodule.modulestore import EdxJSONEncoder
+from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase, mixed_store_config
 from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
 
@@ -60,6 +63,22 @@ class GenerateGradebookEntriesTests(ModuleStoreTestCase):
                 display_name="homework problem 1",
                 metadata={'rerandomize': 'always', 'graded': True, 'format': "Homework"}
             )
+            self.problem_progress_summary = {
+                "url_name": "homework_problem_1",
+                "display_name": "homework problem 1",
+                "graded": True,
+                "format": "Homework",
+                "section_total": [0.15, 1.0, False, "homework problem 1", None, None],
+                "due": "2014-05-16T14:30:00+00:00",
+                "scores": [[0.15, 1.0, True, "homework problem 1",
+                            unicode(self.problem.location), "2014-05-16T14:30:00+00:00"]]
+            }
+            self.problem_grade_summary = {
+                "category": "Homework",
+                "percent": 0.15,
+                "detail": "Homework 1 - homework problem 1 - 15% (0.15/1)",
+                "label": "HW 01"
+            }
             self.problem2 = ItemFactory.create(
                 parent_location=chapter2.location,
                 category='problem',
@@ -88,7 +107,33 @@ class GenerateGradebookEntriesTests(ModuleStoreTestCase):
                 display_name="final problem 2",
                 metadata={'rerandomize': 'always', 'graded': True, 'format': "Final Exam"}
             )
-
+            self.grading_policy = {
+                "GRADER": [{
+                    "short_label": "HW",
+                    "min_count": 12,
+                    "type": "Homework",
+                    "drop_count": 2,
+                    "weight": 0.15
+                }, {
+                    "min_count": 12,
+                    "type": "Lab",
+                    "drop_count": 2,
+                    "weight": 0.15
+                }, {
+                    "short_label": "Midterm",
+                    "min_count": 1,
+                    "type": "Midterm Exam",
+                    "drop_count": 0,
+                    "weight": 0.3
+                }, {
+                    "short_label": "Final",
+                    "min_count": 1,
+                    "type": "Final Exam",
+                    "drop_count": 0,
+                    "weight": 0.4
+                }],
+                "GRADE_CUTOFFS": {"Pass": 0.5}
+            }
             # Create some users and enroll them
             self.users = [UserFactory.create(username="testuser" + str(__), profile='test') for __ in xrange(2)]
             self.users.insert(0, self.user)
@@ -134,12 +179,10 @@ class GenerateGradebookEntriesTests(ModuleStoreTestCase):
             field_data_cache,
         )._xmodule
 
-    @patch.dict(
-        settings.FEATURES, {
-            'ALLOW_STUDENT_STATE_UPDATES_ON_CLOSED_COURSE': False,
-            'SIGNAL_ON_SCORE_CHANGED': False
-        }
-    )
+    @patch.dict(settings.FEATURES, {
+        'ALLOW_STUDENT_STATE_UPDATES_ON_CLOSED_COURSE': False,
+        'SIGNAL_ON_SCORE_CHANGED': False
+    })
     def test_generate_gradebook_entries(self):
         """
         Test the gradebook entry generator
@@ -164,7 +207,7 @@ class GenerateGradebookEntriesTests(ModuleStoreTestCase):
         self.assertEqual(user0_entry.grade, 0.24)
         self.assertEqual(user0_entry.proforma_grade, 0.28575)
 
-        # Enable the signalling mechanism
+        # Enable the signaling mechanism
         settings._wrapped.default_settings.FEATURES['SIGNAL_ON_SCORE_CHANGED'] = True
 
         # Change the score of the final exam for that user
@@ -205,3 +248,39 @@ class GenerateGradebookEntriesTests(ModuleStoreTestCase):
         user2_entry = StudentGradebook.objects.get(user=self.users[2])
         self.assertEqual(user2_entry.grade, 0.72)
         self.assertEqual(user2_entry.proforma_grade, 0.85725)
+
+        # Alter the user's progress and grade summaries to ensure we trigger the update logic
+        user0_entry = StudentGradebook.objects.get(user=self.users[0])
+        self.assertIn(json.dumps(self.problem_progress_summary), user0_entry.progress_summary)
+        self.assertIn(json.dumps(self.problem_grade_summary), user0_entry.grade_summary)
+        self.assertEqual(self.grading_policy, json.loads(user0_entry.grading_policy))
+        user0_entry.progress_summary = ""
+        user0_entry.grade_summary = ""
+        user0_entry.grading_policy = ""
+        user0_entry.save()
+
+        # Run the command across all users, but just for the specified course
+        generate_gradebook_entries.Command().handle(course_ids=course_ids)
+
+        # Confirm that the gradebook has been properly updated
+        current_entries = StudentGradebook.objects.all()
+        self.assertEqual("", user0_entry.progress_summary)
+        self.assertEqual("", user0_entry.grade_summary)
+        self.assertEqual("", user0_entry.grading_policy)
+
+        # Alter the user's empty progress and grade summaries to ensure we trigger the update logic
+        user0_entry = StudentGradebook.objects.get(user=self.users[0])
+        self.assertIn(json.dumps(self.problem_progress_summary), user0_entry.progress_summary)
+        user0_entry.progress_summary = json.dumps({"progress": "summary"}, cls=EdxJSONEncoder)
+        user0_entry.grade_summary = json.dumps({"grade": "summary"}, cls=EdxJSONEncoder)
+        user0_entry.grading_policy = json.dumps({"grading": "policy"}, cls=EdxJSONEncoder)
+        user0_entry.save()
+
+        # Run the command across all users, but just for the specified course
+        generate_gradebook_entries.Command().handle(course_ids=course_ids)
+
+        # Confirm that the gradebook has been properly updated
+        current_entries = StudentGradebook.objects.all()
+        self.assertEqual(json.dumps({'progress': 'summary'}), user0_entry.progress_summary)
+        self.assertEqual(json.dumps({"grade": "summary"}), user0_entry.grade_summary)
+        self.assertEqual(json.dumps({'grading': "policy"}), user0_entry.grading_policy)
