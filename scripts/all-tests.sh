c64f4525befce8df2f6bff4bfedb88cbf372a1b3
Jonathan Piacenti <kelketek@gmail.com>
2015-08-25 15:18:20 +0000

    Update pylint violation threshhold.

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -11,7 +11,7 @@ set -e
 ###############################################################################
 
 # Violations thresholds for failing the build
-export PYLINT_THRESHOLD=6200
+export PYLINT_THRESHOLD=8061
 export JSHINT_THRESHOLD=3700
 
 doCheckVars() {
