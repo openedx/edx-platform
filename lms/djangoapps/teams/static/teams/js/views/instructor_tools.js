f6a9749fe517b04650c63e0d4df92bed78ebe0ca
Peter Fogg <pfogg@edx.org>
2015-08-31 13:10:55 -0400

    Allow deleting teams in the UI.
    TNL-3163

--- lms/djangoapps/teams/static/teams/js/views/instructor_tools.js
+++ lms/djangoapps/teams/static/teams/js/views/instructor_tools.js
@@ -5,8 +5,9 @@
             'underscore',
             'gettext',
             'teams/js/views/team_utils',
+            'common/js/components/views/feedback_prompt',
             'text!teams/templates/instructor-tools.underscore'],
-        function (Backbone, _, gettext, TeamUtils, instructorToolbarTemplate) {
+        function (Backbone, _, gettext, TeamUtils, PromptView, instructorToolbarTemplate) {
             return Backbone.View.extend({
 
                 events: {
@@ -16,6 +17,9 @@
 
                 initialize: function(options) {
                     this.template = _.template(instructorToolbarTemplate);
+                    this.team = options.team;
+                    this.teamEvents = options.teamEvents;
+                    this.router = options.router;
                 },
 
                 render: function() {
@@ -25,14 +29,46 @@
 
                 deleteTeam: function (event) {
                     event.preventDefault();
-                    alert("You clicked the button!");
-                    //placeholder; will route to delete team page
+                    var self = this;
+                    var msg = new PromptView.Warning({
+                        title: gettext('Delete this team?'),
+                        message: gettext('Deleting a team removes it from the team listing view, and removes members from the team as well.'),
+                        actions: {
+                            primary: {
+                                text: gettext('Delete'),
+                                class: 'action-delete',
+                                click: function () {
+                                    msg.hide();
+                                    self.handleDelete();
+                                }
+                            },
+                            secondary: {
+                                text: gettext('Close'),
+                                class: 'action-cancel',
+                                click: function () {
+                                    msg.hide();
+                                }
+                            }
+                        }
+                    });
+                    msg.show()
                 },
 
                 editMembership: function (event) {
                     event.preventDefault();
                     alert("You clicked the button!");
                     //placeholder; will route to remove team member page
+                },
+
+                handleDelete: function () {
+                    var self = this;
+                    this.team.destroy().then(function () {
+                        self.teamEvents.trigger('teams:update', {
+                            action: 'delete',
+                            team: self.team
+                        });
+                        self.router.navigate('topics/' + self.team.get('topic_id'), {trigger: true});
+                    });
                 }
             });
         });
--- lms/djangoapps/teams/static/teams/js/views/teams_tab.js
+++ lms/djangoapps/teams/static/teams/js/views/teams_tab.js
@@ -249,7 +249,11 @@
                                 },
                                 model: team
                             });
-                            var instructorToolsView = new InstructorToolsView();
+                            var instructorToolsHeader = new InstructorToolsView({
+                                team: team,
+                                teamEvents: self.teamEvents,
+                                router: self.router
+                            });
                             editViewWithHeader = self.createViewWithHeader({
                                     mainView: view,
                                     subject: {
