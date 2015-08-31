854efad2599533d8c34c285eb278414f18bc5b18
Bryan Wilson <bryanlandia@gmail.com>
2015-08-31 22:54:23 +0000

    Set upload chunk size to 20Mb same as max asset upload size. The chunking into smaller sizes wasn't working though I don't knokw why, causing incomplete downloads.

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -957,7 +957,8 @@ SHIBBOLETH_DOMAIN_PREFIX = 'shib:'
 OPENID_DOMAIN_PREFIX = 'openid:'
 
 ### Size of chunks into which asset uploads will be divided
-UPLOAD_CHUNK_SIZE_IN_MB = 10
+#UPLOAD_CHUNK_SIZE_IN_MB = 10
+UPLOAD_CHUNK_SIZE_IN_MB = 20
 
 ### Max size of asset uploads to GridFS
 #MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = 10
