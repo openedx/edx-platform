e2c78003f99803b6c8962cfdf8a1d6eec230c3e0
Jonathan Piacenti <kelketek@gmail.com>
2015-09-08 16:46:50 +0000

    Update solutions patches to use get_course_cohort_settings

--- lms/djangoapps/django_comment_client/base/tests.py
+++ lms/djangoapps/django_comment_client/base/tests.py
@@ -391,7 +391,7 @@ class ViewsTestCase(UrlResetMixin, ModuleStoreTestCase, MockRequestSetupMixin):
 
         self.course.cohort_config = {
             'cohorted': True,
-            'inline_discussions_cohorting_default': True
+            'always_cohort_inline_discussions': True
         }
         self.store.update_item(self.course, self.student.id)
 
