c0f8645d3c5bcdbf1c0c2cb57310332c980a768a
Jonathan Piacenti <kelketek@gmail.com>
2015-09-15 19:04:57 +0000

    Merge course discussion IDs for cohort config conversion.

--- openedx/core/djangoapps/course_groups/management/commands/convert_cohort_format.py
+++ openedx/core/djangoapps/course_groups/management/commands/convert_cohort_format.py
@@ -20,10 +20,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         store = modulestore()
         for course in store.get_courses():
-            if course.cohort_config and 'inline_discussions_cohorting_default' in course.cohort_config:
+            if course.cohort_config:
                 print "Updating affected course: %s" % unicode(course.location)
-                course.cohort_config['always_cohort_inline_discussions'] = course.cohort_config[
-                    'inline_discussions_cohorting_default'
-                ]
+                if 'inline_discussions_cohorting_default' in course.cohort_config:
+                    course.cohort_config['always_cohort_inline_discussions'] = course.cohort_config[
+                        'inline_discussions_cohorting_default'
+                    ]
+                inlines = course.cohort_config.get('cohorted_inline_discussions', [])
+                course_discussions = course.cohort_config.get('cohorted_course_wide_discussions', [])
+                if inlines or course_discussions:
+                    course.cohort_config['cohorted_discussions'] = inlines + course_discussions
                 store.update_item(course, None)
                 print "%s updated." % unicode(course.location)
