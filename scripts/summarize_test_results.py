6a452702bdf54f9a09aef8559b7af028c8919205
Ned Batchelder <ned@edx.org>
2015-09-29 19:31:14 -0400

    summarize_test_results.py can now show test names for each error message

--- scripts/summarize_test_results.py
+++ scripts/summarize_test_results.py
@@ -1,10 +1,58 @@
-"""Summarize the results of running all the tests."""
+#!/usr/bin/env python
+"""Summarize the results of running all the tests.
+
+See the report_all_files docstring for details, or run this with --help.
+
+"""
 
 import collections
 import os
 
 import click
 from lxml import etree
+
+
+@click.command()
+@click.option(
+    "--errors/--no-errors",
+    help="Show details of errors and failures",
+)
+@click.option(
+    "--names/--no-names",
+    help=(
+        "Show test names"
+        " (with --errors, categorized by error message)"
+    ),
+)
+@click.option(
+    "--outcomes/--no-outcomes",
+    help="Show pass/fail/error with names",
+)
+@click.argument(
+    "start",
+    default="reports",
+)
+def report_all_files(errors, names, outcomes, start):
+    """Find all the nosetests.xml files, and report on them.
+
+    For every nosetests.xml file found, prints a summary of the number of
+    tests, fails, errors, etc.  If --details is used, then the error messages
+    from all of the fails and errors will be shown, most frequent first, with a
+    count of how many tests failed for that reason.
+
+    """
+    totals = TestResults()
+    for dirpath, _, filenames in os.walk(start):
+        if "nosetests.xml" in filenames:
+            results = report_file(
+                os.path.join(dirpath, "nosetests.xml"),
+                errors=errors,
+                names=names,
+                outcomes=outcomes,
+            )
+            totals += results
+
+    print "\nTotals:\n{}".format(totals)
 
 
 class Summable(object):
@@ -47,7 +95,15 @@ def error_line_from_error_element(element):
     return element.get("message").splitlines()[0]
 
 
-def report_file(path, details):
+def testcase_name(testcase):
+    """Given a <testcase> element, return the name of the test."""
+    return "{classname}.{name}".format(
+        classname=testcase.get("classname"),
+        name=testcase.get("name"),
+    )
+
+
+def report_file(path, errors, names, outcomes):
     """Report on one nosetests.xml file."""
     print "\n{}".format(path)
     with open(path) as xml_file:
@@ -57,31 +113,40 @@ def report_file(path, details):
     results = TestResults.from_element(suite)
     print results
 
-    if details:
-        errors = collections.Counter()
+    if errors:
+        errors = collections.defaultdict(list)
         for error_element in tree.xpath(".//error|.//failure"):
-            errors[error_line_from_error_element(error_element)] += 1
+            error_line = error_line_from_error_element(error_element)
+            testcases = error_element.xpath("..")
+            if testcases:
+                testcase = testcases[0]
+                errors[error_line].append(testcase_name(testcase))
 
         if errors:
             print ""
-            for error_message, number in errors.most_common():
-                print "{0:4d}: {1}".format(number, error_message)
+            errors = sorted(errors.items(), key=lambda kv: len(kv[1]), reverse=True)
+            for error_message, test_names in errors:
+                print "{0:4d}: {1}".format(len(test_names), error_message)
+                if names:
+                    for test_name in test_names:
+                        print "        {}".format(test_name)
+
+    elif names:
+        for testcase in tree.xpath(".//testcase"):
+            if outcomes:
+                result = testcase.xpath("error|failure|skipped")
+                if result:
+                    outcome = result[0].tag[0].upper()
+                else:
+                    outcome = "."
+            else:
+                outcome = ""
+            print "  {outcome} {testname}".format(
+                outcome=outcome,
+                testname=testcase_name(testcase),
+            )
 
     return results
-
-
-@click.command()
-@click.option("--details/--no-details", help="Show details of errors")
-@click.argument("start", default="reports")
-def report_all_files(details, start):
-    """Find all the nosetests.xml files, and report on them."""
-    totals = TestResults()
-    for dirpath, _, filenames in os.walk(start):
-        if "nosetests.xml" in filenames:
-            results = report_file(os.path.join(dirpath, "nosetests.xml"), details=details)
-            totals += results
-
-    print "\nTotals:\n{}".format(totals)
 
 
 if __name__ == "__main__":
