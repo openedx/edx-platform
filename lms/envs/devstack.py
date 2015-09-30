c385ba2eb3f91ccc998cede46d2963b0cd2ae382
David Ormsbee <dave@edx.org>
2015-09-30 15:48:57 -0400

    Fix SplitMongoModuleStore.make_course_usage_key to work for CCX.
    CCX courses usage keys are of type CCXBlockUsageLocator, not
    BlockUsageLocator like everything else.

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -215,6 +215,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = ()
 CORS_ORIGIN_ALLOW_ALL = True
 
+################################ CCX ###############################
+FEATURES['CUSTOM_COURSES_EDX'] = True
 
 #####################################################################
 # See if the developer has any local overrides.
