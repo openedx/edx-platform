cc11f80e9bc2ce58f222d75b75b55753ae6fb20f
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-10-01 13:33:10 +0500

    Setting default=False for Boolean Fields.
    TNL-3470

--- openedx/core/djangoapps/content/course_overviews/models.py
+++ openedx/core/djangoapps/content/course_overviews/models.py
@@ -54,9 +54,9 @@ class CourseOverview(TimeStampedModel):
 
     # Certification data
     certificates_display_behavior = TextField(null=True)
-    certificates_show_before_end = BooleanField()
-    cert_html_view_enabled = BooleanField()
-    has_any_active_web_certificate = BooleanField()
+    certificates_show_before_end = BooleanField(default=False)
+    cert_html_view_enabled = BooleanField(default=False)
+    has_any_active_web_certificate = BooleanField(default=False)
     cert_name_short = TextField()
     cert_name_long = TextField()
 
@@ -65,8 +65,8 @@ class CourseOverview(TimeStampedModel):
 
     # Access parameters
     days_early_for_beta = FloatField(null=True)
-    mobile_available = BooleanField()
-    visible_to_staff_only = BooleanField()
+    mobile_available = BooleanField(default=False)
+    visible_to_staff_only = BooleanField(default=False)
     _pre_requisite_courses_json = TextField()  # JSON representation of list of CourseKey strings
 
     # Enrollment details
