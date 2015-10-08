895f4a36b25783ec297838e18f4f8a1345c86db1
openedx-newbie <xavier+democonf@antoviaque.org>
2015-10-08 15:27:53 +0200

    Fix enrollment terminology in about page tests

--- lms/djangoapps/courseware/tests/test_microsites.py
+++ lms/djangoapps/courseware/tests/test_microsites.py
@@ -212,14 +212,14 @@ class TestMicrosites(ModuleStoreTestCase, LoginEnrollmentTestCase):
         url = reverse('about_course', args=[self.course_with_visibility.id.to_deprecated_string()])
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
-        self.assertIn("Register for {}".format(self.course_with_visibility.id.course), resp.content)
+        self.assertIn("Enroll in {}".format(self.course_with_visibility.id.course), resp.content)
         self.assertNotIn("Add {} to Cart ($10)".format(self.course_with_visibility.id.course), resp.content)
 
         # now try on the microsite
         url = reverse('about_course', args=[self.course_with_visibility.id.to_deprecated_string()])
         resp = self.client.get(url, HTTP_HOST=settings.MICROSITE_TEST_HOSTNAME)
         self.assertEqual(resp.status_code, 200)
-        self.assertNotIn("Register for {}".format(self.course_with_visibility.id.course), resp.content)
+        self.assertNotIn("Enroll in {}".format(self.course_with_visibility.id.course), resp.content)
         self.assertIn("Add {} to Cart <span>($10 USD)</span>".format(
             self.course_with_visibility.id.course
         ), resp.content)
