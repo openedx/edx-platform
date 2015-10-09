347180ea3ef91aff9d856615ee0ce6ddbd271b2b
Ben Patterson <bpatterson@edx.org>
2015-10-09 10:33:23 -0400

    Make tests more independent.

--- common/test/acceptance/tests/lms/test_lms_cohorted_courseware_search.py
+++ common/test/acceptance/tests/lms/test_lms_cohorted_courseware_search.py
@@ -3,6 +3,7 @@ Test courseware search
 """
 import os
 import json
+import uuid
 
 from ..helpers import remove_file
 from ...pages.common.logout import LogoutPage
@@ -57,14 +58,14 @@ class CoursewareSearchCohortTest(ContainerBase):
         self.content_group_b = "Content Group B"
 
         # Create a student who will be in "Cohort A"
-        self.cohort_a_student_username = "cohort_a_student"
+        self.cohort_a_student_username = "cohort_a_student" + "_" + str(uuid.uuid4().hex)[:5]
         self.cohort_a_student_email = "cohort_a_student@example.com"
         StudioAutoAuthPage(
             self.browser, username=self.cohort_a_student_username, email=self.cohort_a_student_email, no_login=True
         ).visit()
 
         # Create a student who will be in "Cohort B"
-        self.cohort_b_student_username = "cohort_b_student"
+        self.cohort_b_student_username = "cohort_b_student" + "_" + str(uuid.uuid4().hex)[:5]
         self.cohort_b_student_email = "cohort_b_student@example.com"
         StudioAutoAuthPage(
             self.browser, username=self.cohort_b_student_username, email=self.cohort_b_student_email, no_login=True
