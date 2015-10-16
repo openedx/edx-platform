ced099ca32fb8b98c37fc329403ae7a86aaaf837
Andy Armstrong <andya@edx.org>
2015-10-15 22:22:59 -0400

    Don't kill collectstatic for missing files
    TNL-726

--- openedx/core/lib/django_require/staticstorage.py
+++ openedx/core/lib/django_require/staticstorage.py
@@ -2,12 +2,29 @@
 :class:`~django_require.staticstorage.OptimizedCachedRequireJsStorage`
 """
 
+import logging
+
 from pipeline.storage import PipelineCachedStorage
 from require.storage import OptimizedFilesMixin
+
+log = logging.getLogger(__name__)
 
 
 class OptimizedCachedRequireJsStorage(OptimizedFilesMixin, PipelineCachedStorage):
     """
     Custom storage backend that is used by Django-require.
     """
-    pass
+    def hashed_name(self, name, content=None):
+        """
+        Returns the MD5 hashed version of a file name.
+
+        Note: this overrides the default implementation to catch ValueErrors
+        which indicate that the file doesn't exist so that the collectstatic
+        process doesn't abort. Instead, a warning is logged and then the
+        original file name is returned.
+        """
+        try:
+            return super(OptimizedCachedRequireJsStorage, self).hashed_name(name, content)
+        except ValueError:
+            log.warn("File not found in OptimizedCachedRequireJsStorage: %r", name)
+            return name
