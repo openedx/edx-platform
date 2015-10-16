38090677be920d95000b43e5468a369aae673491
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-10-16 16:03:38 +0500

    Fixing remining tests for user apis.
    TNL-3609

--- openedx/core/djangoapps/user_api/accounts/tests/test_views.py
+++ openedx/core/djangoapps/user_api/accounts/tests/test_views.py
@@ -364,14 +364,14 @@ class TestAccountAPI(UserAPITestCase):
         ("level_of_education", "none", u"ȻħȺɍłɇs", u'"ȻħȺɍłɇs" is not a valid choice.'),
         ("country", "GB", "XY", u'"XY" is not a valid choice.'),
         ("year_of_birth", 2009, "not_an_int", u"A valid integer is required."),
-        ("name", "bob", "z" * 256, u"Ensure this field has no more than 255 characters."),
-        ("name", u"ȻħȺɍłɇs", "z   ", u"The name field must be at least 2 characters long."),
+        ("name", "bob", "z" * 256, u"Ensure this value has at most 255 characters (it has 256)."),
+        ("name", u"ȻħȺɍłɇs", "z   ", "The name field must be at least 2 characters long."),
         ("goals", "Smell the roses"),
         ("mailing_address", "Sesame Street"),
         # Note that we store the raw data, so it is up to client to escape the HTML.
         (
             "bio", u"<html>Lacrosse-playing superhero 壓是進界推日不復女</html>",
-            "z" * 3001, u"Ensure this field has no more than 3000 characters."
+            "z" * 3001, u"Ensure this value has at most 3000 characters (it has 3001)."
         ),
         # Note that email is tested below, as it is not immediately updated.
         # Note that language_proficiencies is tested below as there are multiple error and success conditions.
