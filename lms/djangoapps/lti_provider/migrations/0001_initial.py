aac34ae76e1d21e911b86f3eec738c88af199a48
Ned Batchelder <ned@edx.org>
2015-10-19 12:41:46 -0400

    One last tweak to migrations due to one-to-one fields

--- lms/djangoapps/lti_provider/migrations/0001_initial.py
+++ lms/djangoapps/lti_provider/migrations/0001_initial.py
@@ -39,7 +39,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('lti_user_id', models.CharField(max_length=255)),
-                ('edx_user', models.ForeignKey(to=settings.AUTH_USER_MODEL, unique=True)),
+                ('edx_user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
                 ('lti_consumer', models.ForeignKey(to='lti_provider.LtiConsumer')),
             ],
         ),
