db9697cdcae076630ff2c3fd169b80cf2bfe1de4
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-10-20 19:38:27 +0500

    Temporary fix for "(1215, 'Cannot add foreign key constraint')"
    TNL-3648

--- scripts/reset-test-db.sh
+++ scripts/reset-test-db.sh
@@ -51,6 +51,7 @@ else
     ./manage.py lms --settings bok_choy makemigrations --traceback --noinput
     ./manage.py cms --settings bok_choy makemigrations --traceback --noinput
     ./manage.py lms --settings bok_choy migrate --traceback --noinput
+    ./manage.py lms --settings bok_choy migrate --traceback --noinput
     ./manage.py cms --settings bok_choy migrate --traceback --noinput
 
     # Dump the schema and data to the cache
