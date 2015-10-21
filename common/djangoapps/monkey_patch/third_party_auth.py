57dae3fc1810072022caf302cd2e80aa6ec62341
John Eskew <jeskew@edx.org>
2015-10-21 11:40:53 -0400

    Better comments about the monkey patch.

--- common/djangoapps/monkey_patch/third_party_auth.py
+++ common/djangoapps/monkey_patch/third_party_auth.py
@@ -14,6 +14,11 @@ def patch():
     def create_social_auth_wrapper(wrapped_func):
         wrapped_func = wrapped_func.__func__
         def _w(*args, **kwargs):
+            # The entire reason for this monkey-patch is to wrap the create_social_auth call
+            # in an atomic transaction. The call can sometime raise an IntegrityError, which is
+            # properly caught and dealt with by python_social_auth. However, in Django 1.8, unless
+            # the error is raised in an atomic transaction, the transaction becomes unusable
+            # after the IntegrityError exception is raised.
             with transaction.atomic():
                 return wrapped_func(*args, **kwargs)
         return classmethod(_w)
