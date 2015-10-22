9a9a8d1bfdad04ee8bdfe32edecff035ca651b18
Chris Dodge <cdodge@edx.org>
2015-10-22 14:12:40 -0400

    make context optional and also try to determine a org_id

--- common/djangoapps/track/service.py
+++ common/djangoapps/track/service.py
@@ -4,6 +4,9 @@ Implementation of "Analytics" service
 
 from eventtracking import tracker
 
+from opaque_keys.edx.keys import CourseKey
+from opaque_keys import InvalidKeyError
+
 
 class AnalyticsService(object):
     """
@@ -15,5 +18,18 @@ class AnalyticsService(object):
         Emit an event annotated with the UTC time when this function was called.
         """
 
-        with tracker.get_tracker().context(name, context):
+        if context:
+            # try to parse out the org_id from the course_id
+            if 'course_id' in context:
+                try:
+                    course_key = CourseKey.from_string(context['course_id'])
+                    context['org_id'] = course_key.org
+                except InvalidKeyError:
+                    # leave org_id blank
+                    pass
+
+            with tracker.get_tracker().context(name, context):
+                tracker.emit(name, data)
+        else:
+            # if None is passed in then we don't construct the 'with' context stack
             tracker.emit(name, data)
