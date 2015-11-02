659e940ea82f9540d210e7375a5f4e5b23ae3b24
Filip Jukic <filip@jukic.me>
2015-11-02 18:23:42 +0100

    dummy view with a serializer and permission
    # Conflicts:
    #	requirements/edx/appsembler.txt

---
+++ lms/djangoapps/appsembler/permissions.py
@@ -0,0 +1,22 @@
+import logging
+from django.conf import settings
+from rest_framework import permissions
+
+
+logger = logging.getLogger(__name__)
+
+
+class SecretKeyPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "GET":
+            return True
+
+        request_secret_key = request.POST.get('SecretKey', '')
+        secret_key = settings.FEATURES.get('APPSEMBLER_SECRET_KEY', '')
+        if not secret_key:
+            logger.warning("APPSEMBLER_SECRET_KEY is not set")
+            return False
+        if request_secret_key != secret_key:
+            logger.warning("Wrong secret key.")
+            return False
+        return True
--- lms/djangoapps/appsembler/urls.py
+++ lms/djangoapps/appsembler/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('appsembler.views',
-    url(r'^user$','user_signup_endpoint', name='user_signup_endpoint'),
+    url(r'^user$', 'user_signup_endpoint', name='user_signup_endpoint'),
+    url(r'^user_new$', 'user_signup_endpoint_new', name='user_signup_endpoint_new'),
 )
