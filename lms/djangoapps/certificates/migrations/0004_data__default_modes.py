abb9216543a876b32893142c6d17833107e70449
Ned Batchelder <ned@edx.org>
2015-11-06 13:48:01 -0500

    Even better names for the data migrations we're keeping

--- lms/djangoapps/certificates/migrations/0004_data__default_modes.py
+++ lms/djangoapps/certificates/migrations/0004_data__default_modes.py
@@ -25,7 +25,7 @@ def forwards(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('certificates', '0003_certificatehtmlviewconfiguration_data'),
+        ('certificates', '0003_data__certificatehtmlviewconfiguration_data'),
     ]
 
     operations = [
