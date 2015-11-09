561726ce6e3fd4da9aa42d315eaf81d31a185fca
Peter Fogg <pfogg@edx.org>
2015-11-09 17:15:11 -0500

    Styling and template changes for the Course Home page.
    ECOM-2810

--- lms/djangoapps/courseware/tests/test_views.py
+++ lms/djangoapps/courseware/tests/test_views.py
@@ -818,7 +818,7 @@ class ProgressPageTests(ModuleStoreTestCase):
         self.assertContains(resp, u"Download Your Certificate")
 
     @ddt.data(
-        *itertools.product(((18, 4, True), (18, 4, False)), (True, False))
+        *itertools.product(((19, 4, True), (19, 4, False)), (True, False))
     )
     @ddt.unpack
     def test_query_counts(self, (sql_calls, mongo_calls, self_paced), self_paced_enabled):
