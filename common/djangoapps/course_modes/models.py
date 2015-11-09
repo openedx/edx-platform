alexthered/kienhoc-platform

Overrides for verification and upgrade deadlines

common/djangoapps/course_modes/models.py
@@ -1,16 +1,18 @@
 """
 Add and create new modes for running courses on this particular LMS
 """
+from datetime import datetime, timedelta
 import pytz
-from datetime import datetime
 
+from collections import namedtuple, defaultdict
+from config_models.models import ConfigurationModel
 from django.core.exceptions import ValidationError
 from django.db import models
-from collections import namedtuple, defaultdict
-from django.utils.translation import ugettext_lazy as _
 from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _
 
 from xmodule_django.models import CourseKeyField
+from xmodule.modulestore.django import modulestore
 
 Mode = namedtuple('Mode',
                   [
@@ -54,13 +56,14 @@ class CourseMode(models.Model):
     # For example, if there is a verified mode that expires on 1/1/2015,
     # then users will be able to upgrade into the verified mode before that date.
     # Once the date passes, users will no longer be able to enroll as verified.
-    expiration_datetime = models.DateTimeField(
+    _expiration_datetime = models.DateTimeField(
         default=None, null=True, blank=True,
         verbose_name=_(u"Upgrade Deadline"),
         help_text=_(
             u"OPTIONAL: After this date/time, users will no longer be able to enroll in this mode. "
             u"Leave this blank if users can enroll in this mode until enrollment closes for the course."
         ),
+        db_column='expiration_datetime'
     )
 
     # DEPRECATED: the `expiration_date` field has been replaced by `expiration_datetime`
@@ -137,6 +140,20 @@ class CourseMode(models.Model):
         with a property named slug instead of mode_slug.
         """
         return self.mode_slug
+
+    @property
+    def expiration_datetime(self):
+        """ Return stored expiration_datetime or override."""
+        if self._expiration_datetime is not None:
+            return self._expiration_datetime
+        else:
+            course = modulestore().get_course(self.course_id)
+            return course.end - CourseModeAutoExpirationConfiguration.current().time_from_course_end
+
+    @expiration_datetime.setter
+    def expiration_datetime(self, value):
+        """ Set expiration datetime"""
+        self._expiration_datetime = value
 
     @classmethod
     def all_modes_for_courses(cls, course_id_list):
@@ -692,3 +709,34 @@ class CourseModesArchive(models.Model):
     expiration_date = models.DateField(default=None, null=True, blank=True)
 
     expiration_datetime = models.DateTimeField(default=None, null=True, blank=True)
+
+
+class CourseModeAutoExpirationConfiguration(ConfigurationModel):
+    """
+    Configuration for time period from end of course to auto-expire a course mode.
+    """
+
+    # TODO: Django 1.8 introduces a DurationField
+    # (https://docs.djangoproject.com/en/1.8/ref/models/fields/#durationfield)
+    # for storing timedeltas which uses MySQL's bigint for backing
+    # storage. After we've completed the Django upgrade we should be
+    # able to replace this field with a DurationField named
+    # `verification_window` without having to run a migration or change
+    # other code.
+    verification_window_microseconds = models.BigIntegerField(
+        # default to 10 days
+        default=864000000000,
+        help_text=_(
+            "The time period before a course ends in which a course mode will expire"
+        )
+    )
+
+    @property
+    def verification_window(self):
+        """Return the configured course mode expiration time period as a `datetime.timedelta`."""
+        return timedelta(microseconds=self.verification_window_microseconds)
+
+    @verification_window.setter
+    def verification_window(self, verification_window):
+        """Set the current course mode expiration time period to the given timedelta."""
+        self.verification_window_microseconds = int(verification_window.total_seconds() * 1000000)

openedx/edx-platform

common/djangoapps/course_modes/models.py
@@ -1,16 +1,18 @@
 """
 Add and create new modes for running courses on this particular LMS
 """
+from datetime import datetime, timedelta
 import pytz
-from datetime import datetime
 
+from collections import namedtuple, defaultdict
+from config_models.models import ConfigurationModel
 from django.core.exceptions import ValidationError
 from django.db import models
-from collections import namedtuple, defaultdict
-from django.utils.translation import ugettext_lazy as _
 from django.db.models import Q
+from django.utils.translation import ugettext_lazy as _
 
 from xmodule_django.models import CourseKeyField
+from xmodule.modulestore.django import modulestore
 
 Mode = namedtuple('Mode',
                   [
@@ -54,13 +56,14 @@ class CourseMode(models.Model):
     # For example, if there is a verified mode that expires on 1/1/2015,
     # then users will be able to upgrade into the verified mode before that date.
     # Once the date passes, users will no longer be able to enroll as verified.
-    expiration_datetime = models.DateTimeField(
+    _expiration_datetime = models.DateTimeField(
         default=None, null=True, blank=True,
         verbose_name=_(u"Upgrade Deadline"),
         help_text=_(
             u"OPTIONAL: After this date/time, users will no longer be able to enroll in this mode. "
             u"Leave this blank if users can enroll in this mode until enrollment closes for the course."
         ),
+        db_column='expiration_datetime'
     )
 
     # DEPRECATED: the `expiration_date` field has been replaced by `expiration_datetime`
@@ -137,6 +140,20 @@ class CourseMode(models.Model):
         with a property named slug instead of mode_slug.
         """
         return self.mode_slug
+
+    @property
+    def expiration_datetime(self):
+        """ Return stored expiration_datetime or override."""
+        if self._expiration_datetime is not None:
+            return self._expiration_datetime
+        else:
+            course = modulestore().get_course(self.course_id)
+            return course.end - CourseModeAutoExpirationConfiguration.current().verification_window
+
+    @expiration_datetime.setter
+    def expiration_datetime(self, value):
+        """ Set expiration datetime"""
+        self._expiration_datetime = value
 
     @classmethod
     def all_modes_for_courses(cls, course_id_list):
@@ -692,3 +709,34 @@ class CourseModesArchive(models.Model):
     expiration_date = models.DateField(default=None, null=True, blank=True)
 
     expiration_datetime = models.DateTimeField(default=None, null=True, blank=True)
+
+
+class CourseModeAutoExpirationConfiguration(ConfigurationModel):
+    """
+    Configuration for time period from end of course to auto-expire a course mode.
+    """
+
+    # TODO: Django 1.8 introduces a DurationField
+    # (https://docs.djangoproject.com/en/1.8/ref/models/fields/#durationfield)
+    # for storing timedeltas which uses MySQL's bigint for backing
+    # storage. After we've completed the Django upgrade we should be
+    # able to replace this field with a DurationField named
+    # `verification_window` without having to run a migration or change
+    # other code.
+    _verification_window = models.BigIntegerField(
+        # default to 10 days
+        default=864000000000,
+        help_text=_(
+            "The time period before a course ends in which a course mode will expire"
+        )
+    )
+
+    @property
+    def verification_window(self):
+        """Return the configured course mode expiration time period as a `datetime.timedelta`."""
+        return timedelta(microseconds=self._verification_window)
+
+    @verification_window.setter
+    def verification_window(self, verification_window):
+        """Set the current course mode expiration time period to the given timedelta."""
+        self._verification_window = int(verification_window.total_seconds() * 1000000)