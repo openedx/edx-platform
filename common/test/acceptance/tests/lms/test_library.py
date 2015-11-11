8566634a35871f87f9797bb83a50ac3f2a6e2383
Adnan Ghaffar <adnangahffar_308@yahoo.com>
2015-11-11 13:28:23 +0000

    Udated ddt data

--- common/test/acceptance/tests/lms/test_library.py
+++ common/test/acceptance/tests/lms/test_library.py
@@ -156,9 +156,10 @@ class LibraryContentTest(LibraryContentTestBase):
             XBlockFixtureDesc("html", "Html1", data='html1'),
             XBlockFixtureDesc("html", "Html2", data='html2'),
             XBlockFixtureDesc("html", "Html3", data='html3'),
+
         )
 
-    @ddt.data(2, 3, 4)
+    @ddt.data(2, 2, 3)
     def test_shows_random_xblocks_from_configured(self, count):
         """
         Scenario: Ensures that library content shows {count} random xblocks from library in LMS
