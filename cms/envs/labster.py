897f28da0c9cf76adc405e4aa4941b00dc7bbc1c
Anton Stupak <s2pak.anton@gmail.com>
2015-11-11 18:59:12 +0200

    Update settings.

--- cms/envs/labster.py
+++ cms/envs/labster.py
@@ -1,12 +1,15 @@
 from .aws import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
 
+LABSTER_SETTINGS = ENV_TOKENS.get('LABSTER_SETTINGS', {})
+LABSTER_AUTH = AUTH_TOKENS.get('LABSTER_AUTH', {})
+
 FEATURES['CUSTOM_COURSES_EDX'] = True
 LABSTER_FEATURES = {
     "ENABLE_WIKI": True,
 }
 
-ENV_LABSTER_FEATURES = ENV_TOKENS.get('LABSTER_FEATURES', LABSTER_FEATURES)
+ENV_LABSTER_FEATURES = LABSTER_SETTINGS.get('LABSTER_FEATURES', LABSTER_FEATURES)
 for feature, value in ENV_LABSTER_FEATURES.items():
     FEATURES[feature] = value
 
@@ -15,4 +18,4 @@ INSTALLED_APPS += (
     'labster',
 )
 
-LABSTER_WIKI_LINK = ENV_TOKENS.get('LABSTER_WIKI_LINK', 'http://theory.labster.com/')
+LABSTER_WIKI_LINK = LABSTER_SETTINGS.get('LABSTER_WIKI_LINK', 'https://theory.labster.com/')
