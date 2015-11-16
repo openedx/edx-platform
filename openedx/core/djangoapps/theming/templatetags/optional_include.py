728b0be5bbf96e501f1c4a976960436d920219f6
David Baumgold <david@davidbaumgold.com>
2015-11-16 16:20:55 -0500

    Compatibility changes for Django 1.8

--- openedx/core/djangoapps/theming/templatetags/optional_include.py
+++ openedx/core/djangoapps/theming/templatetags/optional_include.py
@@ -1,6 +1,6 @@
 """
 The functions in this module are based on the contents of
-https://github.com/django/django/blob/1.4.22/django/template/loader_tags.py --
+https://github.com/django/django/blob/1.8.5/django/template/loader_tags.py --
 specifically, the do_include function. It has been modified as little as
 possible, in order to match the behavior of the {% include %} template tag,
 except for making it optional.
@@ -9,63 +9,27 @@ except for making it optional.
 # possible, we should disable pylint so it doesn't complain about the violations
 # that are already in that file
 # pylint: skip-file
-from django.conf import settings
 from django.template.base import (
     TemplateSyntaxError, Library, token_kwargs, TemplateDoesNotExist
 )
-from django.template.loader import get_template
-from django.template.loader_tags import IncludeNode, ConstantIncludeNode
+from django.template.loader_tags import IncludeNode
 
 register = Library()
-
-
-class OptionalConstantIncludeNode(ConstantIncludeNode):
-    def __init__(self, template_path, *args, **kwargs):
-        # Note that this is calling super() on ConstantIncludeNode,
-        # so it's calling the __init__() method of BaseIncludeNode.
-        # It is *not* calling the __init__() method of ConstantIncludeNode itself.
-        super(ConstantIncludeNode, self).__init__(*args, **kwargs)
-        # The logic in the __init__() method of ConstantIncludeNode is
-        # reproduced here, slightly altered.
-        try:
-            t = get_template(template_path)
-            self.template = t
-        except TemplateDoesNotExist:  # this line is new
-            self.template = None      # this line is new
-        except:
-            if settings.TEMPLATE_DEBUG:
-                raise
-            self.template = None
-
-    def render(self, context):
-        if not self.template:
-            return ''
-        try:
-            return self.render_template(self.template, context)
-        except TemplateDoesNotExist:
-            return ''
 
 
 class OptionalIncludeNode(IncludeNode):
     def render(self, context):
         try:
-            template_name = self.template_name.resolve(context)
-            template = get_template(template_name)
-            return self.render_template(template, context)
-        except TemplateDoesNotExist:  # this line is new
-            return ''                 # this line is new
-        except:
-            if settings.TEMPLATE_DEBUG:
-                raise
+            super(OptionalIncludeNode, self).render(context)
+        except TemplateDoesNotExist:
             return ''
 
 
 @register.tag('optional_include')
 def do_include(parser, token):
     """
-    Loads a template and renders it with the current context, but don't throw
-    an error if the template doesn't exist. You can pass additional context
-    using keyword arguments.
+    Loads a template and renders it with the current context, if it exists.
+    You can pass additional context using keyword arguments.
 
     Example::
 
@@ -105,12 +69,9 @@ def do_include(parser, token):
         options[option] = value
     isolated_context = options.get('only', False)
     namemap = options.get('with', {})
-    path = bits[1]
-    if path[0] in ('"', "'") and path[-1] == path[0]:
-        return OptionalConstantIncludeNode(
-            path[1:-1], extra_context=namemap, isolated_context=isolated_context,
-        )
-    return OptionalIncludeNode(
-        parser.compile_filter(bits[1]), extra_context=namemap,
+    node = OptionalIncludeNode(
+        parser.compile_filter(bits[1]),
+        extra_context=namemap,
         isolated_context=isolated_context,
     )
+    return node
