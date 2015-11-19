9c0313a4adc78b0570915cc22cb5ad357575bd0a
Dan Powell <dan@abakas.com>
2015-11-19 17:10:53 -0500

    Adding lti-users with optional personal data (email, name) to edx gradebooks

--- lms/djangoapps/lti_provider/users.py
+++ lms/djangoapps/lti_provider/users.py
@@ -15,8 +15,13 @@ from django.db import IntegrityError, transaction
 from lti_provider.models import LtiUser
 from student.models import UserProfile
 
+USERNAME_DB_FIELD_SIZE = 30
+FIRST_NAME_DB_FIELD_SIZE = 30
+LAST_NAME_DB_FIELD_SIZE = 30
+EMAIL_DB_FIELD_SIZE = 75
 
-def authenticate_lti_user(request, lti_user_id, lti_consumer):
+
+def authenticate_lti_user(request, lti_user_id, lti_consumer, lti_params=None):
     """
     Determine whether the user specified by the LTI launch has an existing
     account. If not, create a new Django User model and associate it with an
@@ -32,7 +37,7 @@ def authenticate_lti_user(request, lti_user_id, lti_consumer):
         )
     except LtiUser.DoesNotExist:
         # This is the first time that the user has been here. Create an account.
-        lti_user = create_lti_user(lti_user_id, lti_consumer)
+        lti_user = create_lti_user(lti_user_id, lti_consumer, lti_params)
 
     if not (request.user.is_authenticated() and
             request.user == lti_user.edx_user):
@@ -41,24 +46,56 @@ def authenticate_lti_user(request, lti_user_id, lti_consumer):
         switch_user(request, lti_user, lti_consumer)
 
 
-def create_lti_user(lti_user_id, lti_consumer):
+def cut_to_max_len(text, max_len):
+    """
+    Cut a passed string to a max length and return it,
+    if the string is less than the max length then it is retured unchanged
+    """
+    if text is None or len(text) < max_len:
+        return text
+    else:
+        return text[:max_len]
+
+
+def create_lti_user(lti_user_id, lti_consumer, lti_params=None):
     """
     Generate a new user on the edX platform with a random username and password,
     and associates that account with the LTI identity.
     """
+    if lti_params is None:
+        lti_params = {}
     edx_password = str(uuid.uuid4())
 
     created = False
     while not created:
         try:
-            edx_user_id = generate_random_edx_username()
-            edx_email = "{}@{}".format(edx_user_id, settings.LTI_USER_EMAIL_DOMAIN)
+            if 'email' in lti_params:
+                edx_email = cut_to_max_len(lti_params['email'], EMAIL_DB_FIELD_SIZE)
+                edx_username = cut_to_max_len(lti_params['email'], USERNAME_DB_FIELD_SIZE)
+                try:
+                    #if this username already exists then use the random username
+                    edx_user = User.objects.get(username=edx_username)
+                    edx_username = generate_random_edx_username()
+                except User.DoesNotExist:
+                    pass
+            else:
+                edx_username = generate_random_edx_username()
+                edx_email = "{}@{}".format(edx_username, settings.LTI_USER_EMAIL_DOMAIN)
+
             with transaction.atomic():
                 edx_user = User.objects.create_user(
-                    username=edx_user_id,
+                    username=edx_username,
                     password=edx_password,
                     email=edx_email,
                 )
+
+                if edx_user is not None:
+                    if 'first_name' in lti_params:
+                        edx_user.first_name = cut_to_max_len(lti_params['first_name'], FIRST_NAME_DB_FIELD_SIZE)
+                    if 'last_name' in lti_params:
+                        edx_user.last_name = cut_to_max_len(lti_params['last_name'], LAST_NAME_DB_FIELD_SIZE)
+                        edx_user.save()
+
                 # A profile is required if PREVENT_CONCURRENT_LOGINS flag is set.
                 # TODO: We could populate user information from the LTI launch here,
                 # but it's not necessary for our current uses.
@@ -104,7 +141,7 @@ def generate_random_edx_username():
     """
     allowable_chars = string.ascii_letters + string.digits
     username = ''
-    for _index in range(30):
+    for _index in range(USERNAME_DB_FIELD_SIZE):
         username = username + random.SystemRandom().choice(allowable_chars)
     return username
 
