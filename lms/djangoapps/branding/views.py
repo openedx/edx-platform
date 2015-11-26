e91ff643d48b843bffebc37e3137d035c0190aa5
Anton Stupak <s2pak.anton@gmail.com>
2015-11-26 15:09:37 +0200

    Use internal /courses.

--- lms/djangoapps/branding/views.py
+++ lms/djangoapps/branding/views.py
@@ -104,8 +104,8 @@ def courses(request):
         settings.FEATURES.get('ENABLE_MKTG_SITE', False)
     )
 
-    if enable_mktg_site:
-        return redirect(marketing_link('COURSES'), permanent=True)
+    # if enable_mktg_site:
+    #     return redirect(marketing_link('COURSES'), permanent=True)
 
     if not settings.FEATURES.get('COURSES_ARE_BROWSABLE'):
         raise Http404
