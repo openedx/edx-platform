a47b61ee085058e8e2a02bc168592a6d74c43272
Filip Jukic <filip@jukic.me>
2015-12-03 16:33:27 +0100

    remove intercom stuff from CMS

--- cms/envs/devstack_appsembler.py
+++ cms/envs/devstack_appsembler.py
@@ -1,10 +1,4 @@
-# devstack_appsembler.py
-
 from .devstack import *
-from .appsembler import *
-
-INSTALLED_APPS += ('appsembler',)
-TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
 
 FEATURES['APPSEMBLER_SECRET_KEY'] = "secret_key"
 OAUTH_ENFORCE_SECURE = False
