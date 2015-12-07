ad16f088241f5dfbf10344e67ffad21dc1b88f86
Anna Callahan <annacallahan@gmail.com>
2015-12-07 15:35:07 -0500

    sending first login to AMC

--- lms/djangoapps/appsembler_lms/receivers.py
+++ lms/djangoapps/appsembler_lms/receivers.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.conf import settings
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
@@ -18,9 +20,33 @@ def send_login_to_AMC(sender, instance, update_fields, **kwargs):
     if update_fields and 'last_login' in update_fields:
         changed = get_changed_fields_dict(instance, sender)
         old_last = changed['last_login']
+        logger.info("old last login: {}".format(old_last))
         if not old_last:
-            logger.info("First time login for user {}".format(instance))
             # ping AMC
+            url_base = settings.FEATURES.get('APPSEMBLER_AMC_API_BASE', '')
+            url_suffix = settings.FEATURES.get('APPSEMBLER_FIRST_LOGIN_API', '')
+            if not url_base:
+                logger.warning("APPSEMBLER_AMC_API_BASE is not set")
+            if not url_suffix:
+                logger.warning("APPSEMBLER_FIRST_LOGIN_API is not set")
+            url = url_base + url_suffix
+            datetime_to_send = instance.last_login.isoformat()
+            payload = {
+                'email':instance.email,
+                'first_logged_into_edx': datetime_to_send,
+                'secret_key': settings.FEATURES['APPSEMBLER_SECRET_KEY']
+            }
+            try:
+                response = requests.post(url, data=payload)
+                if response.status_code == 200:
+                    logger.info("User {} first login sent to AMC.".format(instance.email))
+                else:
+                    logger.warning("User first login NOT received by AMC for user {}".format(instance))
+
+            except requests.exceptions.RequestException as e:
+                logger.info(e.strerror)
+                logger.info(e.message)
+                logger.warning("Could not connect to AMC; first login for user {}".format(instance))
 
         else:
             logger.info("user logged in: {0}, last_login {1}".format(instance, old_last))
--- lms/envs/devstack_appsembler.py
+++ lms/envs/devstack_appsembler.py
@@ -1,6 +1,8 @@
 from .devstack import *
 
 FEATURES['APPSEMBLER_SECRET_KEY'] = "secret_key"
+FEATURES['APPSEMBLER_AMC_API_BASE'] = 'http://localhost:8080/api'
+FEATURES['APPSEMBLER_FIRST_LOGIN_API'] = '/logged_into_edx'
 OAUTH_ENFORCE_SECURE = False
 
 # disable caching in dev environment
