46d231a82b03bae403fb2546c6c3dd40dd0bb1e4
Awais <awais.qureshi@arbisoft.com>
2015-12-07 16:01:50 +0500

    cherry pick the changes. ECOM-2999

---
+++ openedx/core/djangoapps/credentials_service/admin.py
@@ -0,0 +1,19 @@
+"""
+Django admin page for credential
+"""
+from django.contrib import admin
+
+from openedx.core.djangoapps.credentials_service.models import (
+    AbstractCredential, AbstractCertificate, CertificateTemplate ,
+    CourseCertificate, CertificateTemplateAsset, ProgramCertificate,
+    Signatory, UserCredential
+)
+
+admin.site.register(AbstractCredential)
+admin.site.register(AbstractCertificate)
+admin.site.register(CertificateTemplateAsset)
+admin.site.register(CertificateTemplate)
+admin.site.register(CourseCertificate)
+admin.site.register(ProgramCertificate)
+admin.site.register(Signatory)
+admin.site.register(UserCredential)
