596e93a2931012904cdaba372acaac7f20b408c3
Sarina Canelake <sarina@edx.org>
2015-12-08 11:16:02 -0500

    Add __unicode__ method on StudentModuleHistory

--- lms/djangoapps/courseware/models.py
+++ lms/djangoapps/courseware/models.py
@@ -185,6 +185,9 @@ class StudentModuleHistory(models.Model):
                                                  max_grade=instance.max_grade)
             history_entry.save()
 
+    def __unicode__(self):
+        return unicode(repr(self))
+
 
 class XBlockFieldBase(models.Model):
     """
