a7790fbbaca85ba2260d5aa6bb22743d11cc1ed5
Feanil Patel <feanil@edx.org>
2015-12-09 16:59:26 -0500

    Fix the management command.

--- cms/djangoapps/contentstore/management/commands/delete_course.py
+++ cms/djangoapps/contentstore/management/commands/delete_course.py
@@ -24,32 +24,19 @@ class Command(BaseCommand):
     """
     help = '''Delete a MongoDB backed course'''
 
-    def handle(self, *args, **options):
-        if len(args) == 0:
-            raise CommandError("Arguments missing: 'org/number/run commit'")
+    def add_arguments(self, parser):
+        parser.add_argument('course_key', help="ID of the course to delete.")
 
-        if len(args) == 1:
-            if args[0] == 'commit':
-                raise CommandError("Delete_course requires a course_key <org/number/run> argument.")
-            else:
-                raise CommandError("Delete_course requires a commit argument at the end")
-        elif len(args) == 2:
-            try:
-                course_key = CourseKey.from_string(args[0])
-            except InvalidKeyError:
-                try:
-                    course_key = SlashSeparatedCourseKey.from_deprecated_string(args[0])
-                except InvalidKeyError:
-                    raise CommandError("Invalid course_key: '%s'. Proper syntax: 'org/number/run commit' " % args[0])
-            if args[1] != 'commit':
-                raise CommandError("Delete_course requires a commit argument at the end")
-        elif len(args) > 2:
-            raise CommandError("Too many arguments! Expected <course_key> <commit>")
+    def handle(self, *args, **options):
+        try:
+            course_key = CourseKey.from_string(options['course_key'])
+        except InvalidKeyError:
+            raise CommandError("Invalid course_key: '%s'. Proper syntax: 'org/number/run' " % options['course_key'])
 
         if not modulestore().get_course(course_key):
-            raise CommandError("Course with '%s' key not found." % args[0])
+            raise CommandError("Course with '%s' key not found." % options['course_key'])
 
-        print 'Actually going to delete the %s course from DB....' % args[0]
+        print 'Going to delete the %s course from DB....' % options['course_key']
         if query_yes_no("Deleting course {0}. Confirm?".format(course_key), default="no"):
             if query_yes_no("Are you sure. This action cannot be undone!", default="no"):
                 delete_course_and_groups(course_key, ModuleStoreEnum.UserID.mgmt_command)
