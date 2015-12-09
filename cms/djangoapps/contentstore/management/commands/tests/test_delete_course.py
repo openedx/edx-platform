07f015bb570ef5d0cad5cafc15353f3843bf4523
Feanil Patel <feanil@edx.org>
2015-12-09 16:59:26 -0500

    Fix the management command.

--- cms/djangoapps/contentstore/management/commands/tests/test_delete_course.py
+++ cms/djangoapps/contentstore/management/commands/tests/test_delete_course.py
@@ -6,70 +6,10 @@ import unittest
 import mock
 
 from opaque_keys.edx.locations import SlashSeparatedCourseKey
-from django.core.management import CommandError
-from contentstore.management.commands.delete_course import Command
+from django.core.management import call_command, CommandError
 from contentstore.tests.utils import CourseTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
 from xmodule.modulestore.django import modulestore
-
-
-class TestArgParsing(unittest.TestCase):
-    """
-    Tests for parsing arguments for the 'delete_course'  management command
-    """
-
-    def setUp(self):
-        super(TestArgParsing, self).setUp()
-
-        self.command = Command()
-
-    def test_no_args(self):
-        """
-        Testing 'delete_course' command with no arguments provided
-        """
-        errstring = "Arguments missing: 'org/number/run commit'"
-        with self.assertRaisesRegexp(CommandError, errstring):
-            self.command.handle()
-
-    def test_no_course_key(self):
-        """
-        Testing 'delete_course' command with no course key provided
-        """
-        errstring = "Delete_course requires a course_key <org/number/run> argument."
-        with self.assertRaisesRegexp(CommandError, errstring):
-            self.command.handle("commit")
-
-    def test_commit_argument(self):
-        """
-        Testing 'delete_course' command without 'commit' argument
-        """
-        errstring = "Delete_course requires a commit argument at the end"
-        with self.assertRaisesRegexp(CommandError, errstring):
-            self.command.handle("TestX/TS01/run")
-
-    def test_invalid_course_key(self):
-        """
-        Testing 'delete_course' command with an invalid course key argument
-        """
-        errstring = "Invalid course_key: 'TestX/TS01'. Proper syntax: 'org/number/run commit' "
-        with self.assertRaisesRegexp(CommandError, errstring):
-            self.command.handle("TestX/TS01", "commit")
-
-    def test_missing_commit_argument(self):
-        """
-        Testing 'delete_course' command with misspelled 'commit' argument
-        """
-        errstring = "Delete_course requires a commit argument at the end"
-        with self.assertRaisesRegexp(CommandError, errstring):
-            self.command.handle("TestX/TS01/run", "comit")
-
-    def test_too_many_arguments(self):
-        """
-        Testing 'delete_course' command with more than 2 arguments
-        """
-        errstring = "Too many arguments! Expected <course_key> <commit>"
-        with self.assertRaisesRegexp(CommandError, errstring):
-            self.command.handle("TestX/TS01/run", "commit", "invalid")
 
 
 class DeleteCourseTest(CourseTestCase):
@@ -81,8 +21,6 @@ class DeleteCourseTest(CourseTestCase):
 
     def setUp(self):
         super(DeleteCourseTest, self).setUp()
-
-        self.command = Command()
 
         org = 'TestX'
         course_number = 'TS01'
@@ -99,9 +37,9 @@ class DeleteCourseTest(CourseTestCase):
         """
         Test for when a non-existing course key is entered
         """
-        errstring = "Course with 'TestX/TS01/2015_Q7' key not found."
+        errstring = "Invalid course_key: 'TestX/TS01/2015_Q7'. Proper syntax: 'org/number/run' "
         with self.assertRaisesRegexp(CommandError, errstring):
-            self.command.handle('TestX/TS01/2015_Q7', "commit")
+            call_command('delete_course','TestX/TS01/2015_Q7')
 
     def test_course_deleted(self):
         """
@@ -113,5 +51,5 @@ class DeleteCourseTest(CourseTestCase):
 
         with mock.patch(self.YESNO_PATCH_LOCATION) as patched_yes_no:
             patched_yes_no.return_value = True
-            self.command.handle('TestX/TS01/2015_Q1', "commit")
+            call_command('delete_course','TestX/TS01/2015_Q1', "commit")
             self.assertIsNone(modulestore().get_course(SlashSeparatedCourseKey("TestX", "TS01", "2015_Q1")))
