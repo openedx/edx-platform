47e6eae164350c961dd892720e5df34c8e91e00a
Awais <awais.qureshi@arbisoft.com>
2015-12-10 15:33:30 +0500

    Adding the basic api functionality for the credentials. ECOM-2999

--- openedx/core/djangoapps/credentials_service/admin.py
+++ openedx/core/djangoapps/credentials_service/admin.py
@@ -3,17 +3,17 @@ Django admin page for credential
 """
 from django.contrib import admin
 
-from openedx.core.djangoapps.credentials_service.models import (
-    AbstractCredential, AbstractCertificate, CertificateTemplate ,
+from openedx.core.djangoapps.credentials_service.models import ( CertificateTemplate ,
     CourseCertificate, CertificateTemplateAsset, ProgramCertificate,
-    Signatory, UserCredential
-)
+    Signatory, UserCredential,
+    UserCredentialAttribute)
 
-admin.site.register(AbstractCredential)
-admin.site.register(AbstractCertificate)
+
 admin.site.register(CertificateTemplateAsset)
 admin.site.register(CertificateTemplate)
 admin.site.register(CourseCertificate)
 admin.site.register(ProgramCertificate)
 admin.site.register(Signatory)
 admin.site.register(UserCredential)
+
+admin.site.register(UserCredentialAttribute)
--- openedx/core/djangoapps/credentials_service/serializers.py
+++ openedx/core/djangoapps/credentials_service/serializers.py
@@ -3,10 +3,52 @@ Serializers for data manipulated by the credentials service APIs.
 """
 from rest_framework import serializers
 from rest_framework.exceptions import PermissionDenied
-from openedx.core.djangoapps.credentials_service.models import UserCredential
+from openedx.core.djangoapps.credentials_service.models import UserCredential, AbstractCredential, \
+    UserCredentialAttribute, ProgramCertificate, CourseCertificate
+
+
+class AbstractCredentialSerializer(serializers.ModelSerializer):
+    """ Abstract Credential Serializer """
+    class Meta(object):
+        model = AbstractCredential
+
+
+class CredentialRelatedFieldRelatedField(serializers.RelatedField):
+    """
+    A custom field to use for the user credential generic relationship.
+    """
+    def to_representation(self, value):
+        """
+        Serialize objects to a simple textual representation.
+        """
+        if isinstance(value, ProgramCertificate):
+            return {
+                'credential_type': 'program-certificate',
+                'program-certificate': value.program_id
+            }
+        elif isinstance(value, CourseCertificate):
+            return {
+                'credential_type': 'course-certificate',
+                'certificate_type': value.certificate_type,
+                'course-certificate': value.course_id,
+            }
+        raise Exception('Unexpected type of tagged object')
+
+
+class UserCredentialAttributeSerializer(serializers.ModelSerializer):
+    """ Abstract Credential Serializer """
+    class Meta(object):
+        model = UserCredentialAttribute
+        fields = ('user_credential', 'namespace', 'name', 'value')
 
 
 class UserCredentialSerializer(serializers.ModelSerializer):
-    """ CreditProvider """
+    """ User Credential Serializer """
+
+    attributes = UserCredentialAttributeSerializer(many=True, read_only=True)
+    credential = CredentialRelatedFieldRelatedField(read_only='True')
+
     class Meta(object):
         model = UserCredential
+        fields = ('username', 'credential_id', 'credential', 'status', 'download_url', 'uuid'
+                  , 'attributes')
