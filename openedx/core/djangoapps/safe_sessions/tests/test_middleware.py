6ad825db78c3072b07cc5d0c4079b59b98153c7e
Nimisha Asthagiri <nasthagiri@edx.org>
2015-12-09 22:10:39 -0500

    Specially handle login redirect for mobile apps

--- openedx/core/djangoapps/safe_sessions/tests/test_middleware.py
+++ openedx/core/djangoapps/safe_sessions/tests/test_middleware.py
@@ -10,12 +10,24 @@ from django.contrib.auth.models import AnonymousUser
 from django.http import HttpResponse, HttpResponseRedirect, SimpleCookie
 from django.test import TestCase
 from django.test.client import RequestFactory
+from django.test.utils import override_settings
 from mock import patch
 
 from student.tests.factories import UserFactory
 
 from ..middleware import SafeSessionMiddleware, SafeCookieData
 from .test_utils import TestSafeSessionsLogMixin
+
+
+def create_mock_request():
+    """
+    Creates and returns a mock request object for testing.
+    """
+    request = RequestFactory()
+    request.COOKIES = {}
+    request.META = {}
+    request.path = '/'
+    return request
 
 
 class TestSafeSessionProcessRequest(TestSafeSessionsLogMixin, TestCase):
@@ -25,9 +37,7 @@ class TestSafeSessionProcessRequest(TestSafeSessionsLogMixin, TestCase):
     def setUp(self):
         super(TestSafeSessionProcessRequest, self).setUp()
         self.user = UserFactory.create()
-        self.request = RequestFactory()
-        self.request.COOKIES = {}
-        self.request.path = '/'
+        self.request = create_mock_request()
 
     def assert_response(self, safe_cookie_data=None, success=True):
         """
@@ -128,9 +138,7 @@ class TestSafeSessionProcessResponse(TestSafeSessionsLogMixin, TestCase):
     def setUp(self):
         super(TestSafeSessionProcessResponse, self).setUp()
         self.user = UserFactory.create()
-        self.request = RequestFactory()
-        self.request.COOKIES = {}
-        self.request.path = '/'
+        self.request = create_mock_request()
         self.request.session = {}
         self.client.response = HttpResponse()
         self.client.response.cookies = SimpleCookie()
@@ -230,9 +238,7 @@ class TestSafeSessionMiddleware(TestSafeSessionsLogMixin, TestCase):
     def setUp(self):
         super(TestSafeSessionMiddleware, self).setUp()
         self.user = UserFactory.create()
-        self.request = RequestFactory()
-        self.request.COOKIES = {}
-        self.request.path = '/'
+        self.request = create_mock_request()
         self.client.response = HttpResponse()
         self.client.response.cookies = SimpleCookie()
 
@@ -246,7 +252,10 @@ class TestSafeSessionMiddleware(TestSafeSessionsLogMixin, TestCase):
                 settings.SESSION_COOKIE_NAME
             ]
 
-    def test_success(self):
+    def verify_success(self):
+        """
+        Verifies success path.
+        """
         self.client.login(username=self.user.username, password='test')
         self.request.user = self.user
 
@@ -265,11 +274,27 @@ class TestSafeSessionMiddleware(TestSafeSessionsLogMixin, TestCase):
             response = SafeSessionMiddleware().process_response(self.request, self.client.response)
         self.assertEquals(response.status_code, 200)
 
-    def test_error(self):
+    def test_success(self):
+        self.verify_success()
+
+    def test_success_from_mobile_web_view(self):
+        self.request.path = '/xblock/block-v1:org+course+run+type@html+block@block_id'
+        self.verify_success()
+
+    @override_settings(MOBILE_APP_USER_AGENT_REGEXES=[r'open edX Mobile App'])
+    def test_success_from_mobile_app(self):
+        self.request.META = {'HTTP_USER_AGENT': 'open edX Mobile App Version 2.1'}
+        self.verify_success()
+
+    def verify_error(self, expected_response_status):
+        """
+        Verifies error path.
+        """
         self.request.COOKIES[settings.SESSION_COOKIE_NAME] = 'not-a-safe-cookie'
 
         with self.assert_parse_error():
-            SafeSessionMiddleware().process_request(self.request)
+            request_response = SafeSessionMiddleware().process_request(self.request)
+            self.assertEquals(request_response.status_code, expected_response_status)
 
         self.assertTrue(self.request.need_to_delete_cookie)
         self.cookies_from_request_to_response()
@@ -277,3 +302,15 @@ class TestSafeSessionMiddleware(TestSafeSessionsLogMixin, TestCase):
         with patch('django.http.HttpResponse.set_cookie') as mock_delete_cookie:
             SafeSessionMiddleware().process_response(self.request, self.client.response)
             self.assertTrue(mock_delete_cookie.called)
+
+    def test_error(self):
+        self.verify_error(302)
+
+    def test_error_from_mobile_web_view(self):
+        self.request.path = '/xblock/block-v1:org+course+run+type@html+block@block_id'
+        self.verify_error(401)
+
+    @override_settings(MOBILE_APP_USER_AGENT_REGEXES=[r'open edX Mobile App'])
+    def test_error_from_mobile_app(self):
+        self.request.META = {'HTTP_USER_AGENT': 'open edX Mobile App Version 2.1'}
+        self.verify_error(401)
