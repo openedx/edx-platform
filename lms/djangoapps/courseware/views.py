da16b3da22e105112f0502d652d5725ae7a41aee
Renzo Lucioni <renzo@renzolucioni.com>
2015-12-11 14:44:40 -0500

    Remove dead code
    Deletes unused code from an old implementation of microsites.

--- lms/djangoapps/courseware/views.py
+++ lms/djangoapps/courseware/views.py
@@ -136,7 +136,7 @@ def courses(request):
     courses_list = []
     course_discovery_meanings = getattr(settings, 'COURSE_DISCOVERY_MEANINGS', {})
     if not settings.FEATURES.get('ENABLE_COURSE_DISCOVERY'):
-        courses_list = get_courses(request.user, request.META.get('HTTP_HOST'))
+        courses_list = get_courses(request.user)
 
         if microsite.get_value("ENABLE_COURSE_SORTING_BY_START_DATE",
                                settings.FEATURES["ENABLE_COURSE_SORTING_BY_START_DATE"]):
