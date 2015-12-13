a5f03f1b22d0454ac0761ef08bcdd88095e1fa2a
Awais <awais.qureshi@arbisoft.com>
2015-12-13 23:54:03 +0500

    Adding the basic api functionality for the credentials. ECOM-2999

--- openedx/core/djangoapps/credentials_service/tests/test_serializers.py
+++ openedx/core/djangoapps/credentials_service/tests/test_serializers.py
@@ -4,7 +4,6 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-# from openedx.core.djangoapps.credentials_service.factories import SignatoryFactory
 
 from openedx.core.djangoapps.credentials_service import serializers
 from openedx.core.djangoapps.credentials_service.tests.factories import (
@@ -22,26 +21,6 @@ class CredentialSerializerTests(TestCase):
         self.user_credenential = UserCredentialFactory.create(credential_object=self.program_cert)
         self.attr = UserCredentialAttributeFactory.create(user_credential=self.user_credenential)
         self.maxDiff = None
-
-
-    def test_usercredentialbaseserializer(self):
-        # Base serializer will not contain any thing related with generic foreign relation
-        serialize_data = serializers.UserCredentialSerializer(self.user_credenential)
-        expected = {
-            "username": self.user_credenential.username,
-            "status": self.user_credenential.status,
-            "download_url": self.user_credenential.download_url,
-            "uuid": self.user_credenential.uuid,
-            "attributes": [
-                {
-                    "user_credential": self.attr.id,
-                    "namespace": self.attr.namespace,
-                    "name": self.attr.name,
-                    "value": self.attr.value
-                }
-            ]
-        }
-        self.assertDictEqual(serialize_data.data, expected)
 
 
     def test_usercredentialserializer(self):
@@ -65,12 +44,46 @@ class CredentialSerializerTests(TestCase):
         self.assertDictEqual(serialize_data.data, expected)
 
     def test_usercredentialattributeSerializer(self):
-        serialize_data = serializers.UserCredentialSerializer(self.user_credenential)
+        serialize_data = serializers.UserCredentialAttributeSerializer(self.attr)
         expected = {
             "user_credential": self.attr.id,
             "namespace": self.attr.namespace,
             "name": self.attr.name,
             "value": self.attr.value
+        }
+
+        self.assertDictEqual(serialize_data.data, expected)
+
+    def test_programcertificatebaseserializer(self):
+        serialize_data = serializers.ProgramCertificateBaseSerializer(self.program_cert)
+        expected = {
+            "program_id": self.program_cert.program_id
+        }
+
+        self.assertDictEqual(serialize_data.data, expected)
+
+    def test_programcertificateserializer(self):
+
+        # user_cred_2 = UserCredentialFactory.create(credential_object=self.program_cert)
+        serialize_data = serializers.ProgramCertificateSerializer(self.program_cert)
+        expected = {
+            'program_id': '100',
+            'user_credential': [{
+                "object_id": self.user_credenential.object_id,
+                "username": self.user_credenential.username,
+                "status": self.user_credenential.status,
+                "download_url": self.user_credenential.download_url,
+                "uuid": self.user_credenential.uuid,
+                "credential_object": self.program_id,
+                "attributes": [
+                    {
+                        "user_credential": self.attr.id,
+                        "namespace": self.attr.namespace,
+                        "name": self.attr.name,
+                        "value": self.attr.value
+                    }
+                ]
+            }]
         }
 
         self.assertDictEqual(serialize_data.data, expected)
