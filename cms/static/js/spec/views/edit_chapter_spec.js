4bb7713113b602e530c0f846aaa14efc32c16554
vkaracic <karacicvedran@gmail.com>
2015-12-14 15:25:07 +0000

    Update

--- cms/static/js/spec/views/edit_chapter_spec.js
+++ cms/static/js/spec/views/edit_chapter_spec.js
@@ -1,27 +1,29 @@
-define(['jquery', 'underscore', 'js/views/edit_chapter', 'js/models/course'],
-    function ($, _, EditChapter, Course) {
+define(['jquery', 'underscore', 'js/views/edit_chapter', 'js/models/course', 'js/collections/chapter', 'js/models/chapter', "js/spec_helpers/modal_helpers"],
+    function ($, _, EditChapter, Course, ChapterCollection, ChapterModel, ModelHelpers) {
     'use strict';
+    describe('EditChapter', function () {
 
-        describe('EditChapter', function () {
+        describe('openUploadDialog', function() {
+            var edit_chapter_fixture = readFixtures('edit-chapter.underscore');
 
-                beforeEach(function() {
-                    window.course = new Course({
-                        id: '1',
-                        name: '&amp;lt;Vedran&#39;s course&amp;gt;',
-                        url_name: 'course_name',
-                        org: 'course_org',
-                        num: 'course_num',
-                        revision: 'course_rev'
-                    });
+            beforeEach(function() {
+                var course = new Course({
+                    id: '1',
+                    name: '&amp;lt;Vedran&#39;s course&amp;gt;',
+                    url_name: 'course_name',
+                    org: 'course_org',
+                    num: 'course_num',
+                    revision: 'course_rev'
                 });
-
-            describe('openUploadDialog', function() {
-
-                it('displays the encoded name of the course', function () {
-                    var title = _.template(('Upload a new PDF to "<%= name %>"'),
-                        {name: course.get('name')});
-                    expect(title).toBe('Upload a new PDF to "&amp;lt;Vedran&#39;s course&amp;gt;"');
-                });
+                this.model = new ChapterModel({name: 'test-model'});
+                this.collection = new ChapterCollection(this.model);
+            });
+            it('displays the encoded name of the course', function () {
+                setFixtures(edit_chapter_fixture);
+                var test = new EditChapter({model: this.model});
+                $('.action-upload').click();
+                expect($('#modal-window-title').text()).toBe(2);
             });
         });
+    });
 });
