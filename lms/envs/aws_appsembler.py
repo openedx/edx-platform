04ecd3bb703bd089b0997f8fcdc80259f41daf80
TJ Keemon <keems21@gmail.com>
2015-12-15 19:53:17 -0500

    allow course visibility feature flags to be set in studio

--- lms/envs/aws_appsembler.py
+++ lms/envs/aws_appsembler.py
@@ -7,3 +7,7 @@ INSTALLED_APPS += ('appsembler',)
 TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
 
 SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = True
+
+#enable course visibility feature flags
+COURSE_CATALOG_VISIBILITY_PERMISSION = 'see_in_catalog'
+COURSE_ABOUT_VISIBILITY_PERMISSION = 'see_about_page'
