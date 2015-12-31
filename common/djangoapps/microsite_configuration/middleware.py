d0c320a1d9f9c3d62666e9f0c801b18e064bda8d
Zia Fazal <zia.fazal@arbisoft.com>
2015-12-31 20:07:05 +0500

    putting middleware changes back

--- common/djangoapps/microsite_configuration/middleware.py
+++ common/djangoapps/microsite_configuration/middleware.py
@@ -55,31 +55,31 @@ class MicrositeSessionCookieDomainMiddleware(object):
 
         # See if we are running in a Microsite *AND* we have a custom SESSION_COOKIE_DOMAIN defined
         # in configuration
-        # if microsite.has_override_value('SESSION_COOKIE_DOMAIN'):
+        if microsite.has_override_value('SESSION_COOKIE_DOMAIN'):
 
-        # define wrapper function for the standard set_cookie()
-        def _set_cookie_wrapper(key, value='', max_age=None, expires=None, path='/', domain=None, secure=None, httponly=False):
+            # define wrapper function for the standard set_cookie()
+            def _set_cookie_wrapper(key, value='', max_age=None, expires=None, path='/', domain=None, secure=None, httponly=False):
 
-            # only override if we are setting the cookie name to be the one the Django Session Middleware uses
-            # as defined in settings.SESSION_COOKIE_NAME
-            if key == settings.SESSION_COOKIE_NAME:
-                domain = microsite.get_value('SESSION_COOKIE_DOMAIN', domain)
+                # only override if we are setting the cookie name to be the one the Django Session Middleware uses
+                # as defined in settings.SESSION_COOKIE_NAME
+                if key == settings.SESSION_COOKIE_NAME:
+                    domain = microsite.get_value('SESSION_COOKIE_DOMAIN', domain)
 
-            # then call down into the normal Django set_cookie method
-            return response.set_cookie_wrapped_func(
-                key,
-                value,
-                max_age=max_age,
-                expires=expires,
-                path=path,
-                domain=domain,
-                secure=secure,
-                httponly=httponly
-            )
+                # then call down into the normal Django set_cookie method
+                return response.set_cookie_wrapped_func(
+                    key,
+                    value,
+                    max_age=max_age,
+                    expires=expires,
+                    path=path,
+                    domain=domain,
+                    secure=secure,
+                    httponly=httponly
+                )
 
-        # then point the HttpResponse.set_cookie to point to the wrapper and keep
-        # the original around
-        response.set_cookie_wrapped_func = response.set_cookie
-        response.set_cookie = _set_cookie_wrapper
+            # then point the HttpResponse.set_cookie to point to the wrapper and keep
+            # the original around
+            response.set_cookie_wrapped_func = response.set_cookie
+            response.set_cookie = _set_cookie_wrapper
 
         return response
