b7d7e4d588c0ef5608ca8c4f1dc8bf9211ccbdf5
Hasnain <hasnain.naveed123@gmail.com>
2016-01-05 14:14:22 +0500

    remove the flaky decorator.

--- common/test/acceptance/tests/lms/test_lms_instructor_dashboard.py
+++ common/test/acceptance/tests/lms/test_lms_instructor_dashboard.py
@@ -254,7 +254,6 @@ class ProctoredExamsTest(BaseInstructorDashboardTest):
         # Stop the timed exam.
         self.courseware_page.stop_timed_exam()
 
-    @flaky  # TODO fix this SOL-1182
     def test_can_add_remove_allowance(self):
         """
         Make sure that allowances can be added and removed.
@@ -272,7 +271,6 @@ class ProctoredExamsTest(BaseInstructorDashboardTest):
         # Then I can add Allowance to that exam for a student
         self.assertTrue(allowance_section.is_add_allowance_button_visible)
 
-    @flaky  # TODO fix this SOL-1182
     def test_can_reset_attempts(self):
         """
         Make sure that Exam attempts are visible and can be reset.
