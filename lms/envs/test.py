5d28244c3f7a47ad0c6984312776c77026a63ce2
Ahsan Ulhaq <ahsan.haq@arbisoft.com>
2016-01-07 16:20:28 +0500

    Add credentials service user

--- lms/envs/test.py
+++ lms/envs/test.py
@@ -190,6 +190,7 @@ DATABASES = {
 # This hack disables migrations during tests. We want to create tables directly from the models for speed.
 # See https://groups.google.com/d/msg/django-developers/PWPj3etj3-U/kCl6pMsQYYoJ.
 MIGRATION_MODULES = {app: "app.migrations_not_used_in_tests" for app in INSTALLED_APPS}
+MIGRATION_MODULES["credentials"] = "app.migrations_not_used_in_tests"
 
 CACHES = {
     # This is the cache used for most things.
