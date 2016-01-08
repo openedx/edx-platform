17a69a30e4012136356ed9336cf85661b7fd2592
Peter Fogg <pfogg@edx.org>
2016-01-08 09:47:39 -0500

    Revert "Add options trailing slashes to the Enrollment API."
    This reverts commit 3149aaa319620c2e39434fea081968cf7040ef6d.

--- common/djangoapps/enrollment/urls.py
+++ common/djangoapps/enrollment/urls.py
@@ -15,20 +15,20 @@ from .views import (
 urlpatterns = patterns(
     'enrollment.views',
     url(
-        r'^enrollment/{username},{course_key}/$'.format(
+        r'^enrollment/{username},{course_key}$'.format(
             username=settings.USERNAME_PATTERN, course_key=settings.COURSE_ID_PATTERN
         ),
         EnrollmentView.as_view(),
         name='courseenrollment'
     ),
     url(
-        r'^enrollment/{course_key}/$'.format(course_key=settings.COURSE_ID_PATTERN),
+        r'^enrollment/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentView.as_view(),
         name='courseenrollment'
     ),
     url(r'^enrollment$', EnrollmentListView.as_view(), name='courseenrollments'),
     url(
-        r'^course/{course_key}/$'.format(course_key=settings.COURSE_ID_PATTERN),
+        r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentCourseDetailView.as_view(),
         name='courseenrollmentdetails'
     ),
