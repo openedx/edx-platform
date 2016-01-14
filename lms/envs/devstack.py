dc4521fd8ed4d84e59e34a62dad74a52938e8ef6
Matjaz Gregoric <mtyaka@gmail.com>
2016-01-14 09:00:17 +0100

    Remove ThemingAware storage mixins and ComprehensiveThemeFinder.
    Collectstatic failed in production when comprehensive theme contained custom css files.
    This patch fixes that problem by removing ComprehensiveThemeFinder from STATICFILES_FINDERS
    and ComprehensiveThemingAware mixin from STATICFILES_STORAGE.
    
    Comprehensive theme static dirs are added to the top of the STATICFILES_DIRS entry,
    which means that the default django FilesystemFinder will find theme static files,
    and since the theme folder is at the top of STATICFILES_DIRS, theme files will take
    precedence over default LMS/CMS static files.
    
    This change means that theme static file URLs are no longer prefixed with themes/<theme-name>/,
    but since we currently only support one comprehensive theme at a time, that shouldn't be a problem.
    If/when we want to make the choice of a theme dynamic per-request (microsites?), we will have to
    bring custom theme finders and storage mixins back, but for now, we don't need them.

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -91,7 +91,6 @@ STATICFILES_STORAGE = 'openedx.core.storage.DevelopmentStorage'
 
 # Revert to the default set of finders as we don't want the production pipeline
 STATICFILES_FINDERS = [
-    'openedx.core.djangoapps.theming.finders.ComprehensiveThemeFinder',
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
