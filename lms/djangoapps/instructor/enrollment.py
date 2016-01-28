de7fa9097c47fe99e8fbe5b09283996c65e82d2f
Eric Fischer <efischer@edx.org>
2016-01-28 16:42:03 -0500

    Allow xblocks to clear state
    My original task was to fix clear state specifically for ORA2 problems.
    As I dug in, I noticed that the reset_student_attempts code was aware
    of edx-submissions, but not openassessment.
    
    Rather than adding another IDA for edx-platform to concern itself with,
    I elected to make a way for *any* xblock to clear its state as a part
    of this method, as that seems like a general problem.
    
    As a nice side bonus, this edx-platform code no longer needs to know
    about anything in edx-submissions, as openassessment will pass through
    to get any submissions data removed as part of its clear method.

--- lms/djangoapps/instructor/enrollment.py
+++ lms/djangoapps/instructor/enrollment.py
@@ -18,7 +18,6 @@ from courseware.models import StudentModule
 from edxmako.shortcuts import render_to_string
 from lang_pref import LANGUAGE_KEY
 
-from submissions import api as sub_api  # installed from the edx-submissions repository
 from student.models import anonymous_id_for_user
 from openedx.core.djangoapps.user_api.models import UserPreference
 
@@ -219,6 +218,7 @@ def reset_student_attempts(course_id, student, module_state_key, delete_module=F
         submissions.SubmissionError: unexpected error occurred while resetting the score in the submissions API.
 
     """
+    user_id = anonymous_id_for_user(student, course_id)
     try:
         # A block may have children. Clear state on children first.
         block = modulestore().get_item(module_state_key)
@@ -229,30 +229,30 @@ def reset_student_attempts(course_id, student, module_state_key, delete_module=F
                 except StudentModule.DoesNotExist:
                     # If a particular child doesn't have any state, no big deal, as long as the parent does.
                     pass
+        # Allow the block to reset itself, if it has defined such a method
+        # We trust blocks to clean dependencies as well, so openassessment will clear out any relevant submissions
+        block_reset = getattr(block, "clear_state", None)
+        if callable(block_reset):
+            block_reset(user_id=user_id, course_id=course_id, item_id=module_state_key, delete=delete_module)
     except ItemNotFoundError:
         log.warning("Could not find %s in modulestore when attempting to reset attempts.", module_state_key)
 
-    # Reset the student's score in the submissions API
-    # Currently this is used only by open assessment (ORA 2)
-    # We need to do this *before* retrieving the `StudentModule` model,
-    # because it's possible for a score to exist even if no student module exists.
-    if delete_module:
-        sub_api.reset_score(
-            anonymous_id_for_user(student, course_id),
-            course_id.to_deprecated_string(),
-            module_state_key.to_deprecated_string(),
+    try:
+        module_to_reset = StudentModule.objects.get(
+            student_id=student.id,
+            course_id=course_id,
+            module_state_key=module_state_key
         )
-
-    module_to_reset = StudentModule.objects.get(
-        student_id=student.id,
-        course_id=course_id,
-        module_state_key=module_state_key
-    )
-
-    if delete_module:
-        module_to_reset.delete()
-    else:
-        _reset_module_attempts(module_to_reset)
+        if delete_module:
+            module_to_reset.delete()
+        else:
+            _reset_module_attempts(module_to_reset)
+    except StudentModule.DoesNotExist:
+        msg = (
+            u"No record found when deleting module {module_state_key}"
+            u"in course {course_id} for student {student_id}."
+        ).format(module_state_key=module_state_key, course_id=course_id, student_id=student_id)
+        logger.exception(msg)
 
 
 def _reset_module_attempts(studentmodule):
