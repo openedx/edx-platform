4e5792cfc050004e99d701685e17573c74451e5c
cahrens <christina@edx.org>
2016-02-04 09:28:19 -0500

    Updates after initial spike.

--- common/test/acceptance/tests/studio/base_studio_test.py
+++ common/test/acceptance/tests/studio/base_studio_test.py
@@ -16,11 +16,12 @@ class StudioCourseTest(UniqueCourseTest):
     Base class for all Studio course tests.
     """
 
-    def setUp(self, is_staff=False):
+    def setUp(self, is_staff=False, test_xss=True):  # pylint: disable=arguments-differ
         """
         Install a course with no content using a fixture.
         """
         super(StudioCourseTest, self).setUp()
+        self.test_xss = test_xss
         self.install_course_fixture(is_staff)
 
     def install_course_fixture(self, is_staff=False):
@@ -33,15 +34,19 @@ class StudioCourseTest(UniqueCourseTest):
             self.course_info['run'],
             self.course_info['display_name'],
         )
-        test_improper_escaping = {u"value": XSS_INJECTION}
-        self.course_fixture.add_advanced_settings({
-            "advertised_start": test_improper_escaping,
-            "info_sidebar_name": test_improper_escaping,
-            "cert_name_short": test_improper_escaping,
-            "cert_name_long": test_improper_escaping,
-            "display_organization": test_improper_escaping,
-            "display_coursenumber": test_improper_escaping,
-        })
+        if self.test_xss:
+            xss_injected_unique_id = XSS_INJECTION + self.unique_id
+            test_improper_escaping = {u"value": xss_injected_unique_id}
+            self.course_fixture.add_advanced_settings({
+                "advertised_start": test_improper_escaping,
+                "info_sidebar_name": test_improper_escaping,
+                "cert_name_short": test_improper_escaping,
+                "cert_name_long": test_improper_escaping,
+                "display_organization": test_improper_escaping,
+                "display_coursenumber": test_improper_escaping,
+            })
+            self.course_info['display_organization'] = xss_injected_unique_id
+            self.course_info['display_coursenumber'] = xss_injected_unique_id
         self.populate_course_fixture(self.course_fixture)
         self.course_fixture.install()
         self.user = self.course_fixture.user
