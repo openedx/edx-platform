08379205367af75dfb98a8acf187dcc3da1bccfb
Muddasser <muddasser1991@yahoo.com>
2016-02-29 12:39:04 +0500

    Improved readability of code.

--- common/test/acceptance/tests/lms/test_oauth2.py
+++ common/test/acceptance/tests/lms/test_oauth2.py
@@ -54,8 +54,12 @@ class OAuth2PermissionDelegationTests(WebAppTest):
         self._auth()
         assert self.oauth_page.visit()
         self.oauth_page.confirm()
-
-        # This redirects to an invalid URI.
         self.oauth_page.wait_for_element_absence('input[name=authorize]', 'Authorization button is not present')
-        query = self._qs(self.browser.current_url) or self._qs(self.browser.title)
+
+        # This redirects to an invalid URI based on the selected browser.
+        if self.browser.name == 'chrome':
+            query = self._qs(self.browser.title)
+        else:
+            query = self._qs(self.browser.current_url)
+
         self.assertIn('code', query)
