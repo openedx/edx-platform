3d28277388a203ef04d6a0bcaff7886f35258608
Eric Fischer <efischer@edx.org>
2016-02-29 16:23:28 -0500

    Functional fullscreen button

---
+++ lms/static/js/fullscreen/views/fullscreen_button.js
@@ -0,0 +1,62 @@
+;(function (define, undefined) {
+    'use strict';
+    define(['gettext', 'jquery', 'underscore', 'backbone'],
+        function (gettext, $, _, Backbone) {
+
+        return Backbone.View.extend({
+            srFullscreenText: gettext('Click to toggle distraction-free mode'),
+
+            events: {
+                'click': 'toggleFullscreen'
+            },
+
+            initialize: function (options) {
+                this.in_fullscreen=false;
+            },
+
+            toggleFullscreen: function(event) {
+                event.preventDefault();
+
+                if (this.$el.hasClass('in_fullscreen')) {
+                    this.$el.removeClass('in_fullscreen');
+                    this.in_fullscreen=false;
+                    this.exitFullscreen();
+                } else {
+                    this.$el.addClass('in_fullscreen');
+                    this.in_fullscreen=true;
+                    this.enterFullscreen();
+                }
+            },
+
+            launchIntoFullscreen: function(element) {
+                if(element.requestFullscreen) {
+                    element.requestFullscreen();
+                } else if(element.mozRequestFullScreen) {
+                    element.mozRequestFullScreen();
+                } else if(element.webkitRequestFullscreen) {
+                    element.webkitRequestFullscreen();
+                } else if(element.msRequestFullscreen) {
+                    element.msRequestFullscreen();
+                }
+            },
+
+            enterFullscreen: function() {
+                var $fullscreenElement = $('.xblock-student_view-sequential');
+                $fullscreenElement.addClass('fullscreen-element');
+                this.launchIntoFullscreen($fullscreenElement[0]);
+            },
+
+            exitFullscreen: function() {
+                var $fullscreenElement = $('.xblock-student_view-sequential');
+                $fullscreenElement.removeClass('fullscreen-element');
+                if(document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if(document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if(document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                }
+            }
+        });
+    });
+}).call(this, define || RequireJS.define);
