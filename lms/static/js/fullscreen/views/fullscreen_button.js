7ab3cf81bb15c8ac86fa5509329e106688509033
Eric Fischer <efischer@edx.org>
2016-03-01 14:58:59 -0500

    Handle ESC to exit fullscreen

--- lms/static/js/fullscreen/views/fullscreen_button.js
+++ lms/static/js/fullscreen/views/fullscreen_button.js
@@ -14,6 +14,13 @@
                 this.in_fullscreen=false;
                 var $fullscreenElement = $('.xblock-student_view-sequential');
                 $(document).focusin(this.exitIfNothingFocused.bind(this));
+                $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', this.changeHandler.bind(this));
+            },
+
+            changeHandler: function(event) {
+                if (!(document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement)) {
+                    this.exitFullscreenHandler();
+                }
             },
 
             toggleFullscreen: function(event) {
@@ -51,23 +58,17 @@
             },
 
             enterFullscreen: function() {
+                var $fullscreenElement = $('.xblock-student_view-sequential');
+                this.launchIntoFullscreen($fullscreenElement[0]);
                 this.$el.addClass('in_fullscreen');
                 this.in_fullscreen=true;
-                var $fullscreenElement = $('.xblock-student_view-sequential');
                 $fullscreenElement.addClass('fullscreen-element');
                 var $fullscreenBreadcrumb = $('.course-wrapper .course-content .sequence .path');
                 $fullscreenBreadcrumb.addClass('in_fullscreen');
-                this.launchIntoFullscreen($fullscreenElement[0]);
                 Logger.log('fullscreen_opened', "event information here about entering fullscreen" );
             },
 
             exitFullscreen: function() {
-                this.$el.removeClass('in_fullscreen');
-                this.in_fullscreen=false;
-                var $fullscreenElement = $('.xblock-student_view-sequential');
-                $fullscreenElement.removeClass('fullscreen-element');
-                var $fullscreenBreadcrumb = $('.course-wrapper .course-content .sequence .path');
-                $fullscreenBreadcrumb.removeClass('in_fullscreen');
                 if(document.exitFullscreen) {
                     document.exitFullscreen();
                 } else if(document.mozCancelFullScreen) {
@@ -76,6 +77,16 @@
                     document.webkitExitFullscreen();
                 }
                 Logger.log('fullscreen_closed', "evenr information here about exiting fullscreen" );
+                this.exitFullscreenHandler();
+            },
+
+            exitFullscreenHandler: function() {
+                this.$el.removeClass('in_fullscreen');
+                this.in_fullscreen=false;
+                var $fullscreenElement = $('.xblock-student_view-sequential');
+                $fullscreenElement.removeClass('fullscreen-element');
+                var $fullscreenBreadcrumb = $('.course-wrapper .course-content .sequence .path');
+                $fullscreenBreadcrumb.removeClass('in_fullscreen');
             }
         });
     });
