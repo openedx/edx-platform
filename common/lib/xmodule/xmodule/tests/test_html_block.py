c92d3a2a7bb128b6f71bd6b23684c27384b3572c
Brian Beggs <bbeggs@edx.org>
2016-03-01 11:46:25 -0500

    Updated XBlock tests

--- common/lib/xmodule/xmodule/tests/test_html_block.py
+++ common/lib/xmodule/xmodule/tests/test_html_block.py
@@ -31,7 +31,7 @@ class HtmlModuleSubstitutionTestCase(unittest.TestCase):
         field_data = DictFieldData({'data': sample_xml})
         module_system = get_test_system()
         block = HtmlBlock(module_system, field_data, Mock())
-        self.assertEqual(block.student_view().body_html(), str(module_system.anonymous_student_id))
+        self.assertEqual(block.student_view({}).body_html(), str(module_system.anonymous_student_id))
 
     def test_substitution_without_magic_string(self):
         sample_xml = '''
@@ -42,7 +42,7 @@ class HtmlModuleSubstitutionTestCase(unittest.TestCase):
         field_data = DictFieldData({'data': sample_xml})
         module_system = get_test_system()
         block = HtmlBlock(module_system, field_data, Mock())
-        self.assertEqual(block.student_view().body_html(), sample_xml)
+        self.assertEqual(block.student_view({}).body_html(), sample_xml)
 
     def test_substitution_without_anonymous_student_id(self):
         sample_xml = '''%%USER_ID%%'''
@@ -50,7 +50,7 @@ class HtmlModuleSubstitutionTestCase(unittest.TestCase):
         module_system = get_test_system()
         module_system.anonymous_student_id = None
         block = HtmlBlock(module_system, field_data, Mock())
-        self.assertEqual(block.student_view().body_html(), sample_xml)
+        self.assertEqual(block.student_view({}).body_html(), sample_xml)
 
 
 class HtmlDescriptorIndexingTestCase(unittest.TestCase):
