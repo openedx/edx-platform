21c578d3108da0e60110778d1d83dfcec4c4847d
TJ Keemon <keems21@gmail.com>
2016-03-17 15:55:28 -0400

    update intercom context_processor to work with django 1.8

--- cms/envs/devstack_appsembler.py
+++ cms/envs/devstack_appsembler.py
@@ -4,4 +4,4 @@ from .devstack import *
 from .appsembler import *
 
 INSTALLED_APPS += ('appsembler',)
-TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
+DEFAULT_TEMPLATE_ENGINE['OPTIONS']['context_processors'] += ('appsembler.context_processors.intercom',)
