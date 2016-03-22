e854690a99290a94b2b218a76dcb7d34361ad4b5
Andy Armstrong <andya@edx.org>
2016-03-22 10:44:19 -0400

    Revert "Upgrade underscore.string.min.js."
    This reverts commit e5c7fdda038e176f82ab78a0a77e210be6677420.
    
    Unfortunately this change broke bundling on sandboxes, so reverting
    it so that master isn't left in a bad state.

--- lms/static/lms/js/require-config.js
+++ lms/static/lms/js/require-config.js
@@ -18,11 +18,12 @@
             }
         };
         defineDependency("jQuery", "jquery");
-        defineDependency("s", "underscore.string");
-        // Underscore.string no longer installs itself directly on "_". For compatibility with existing
-        // code, add it to "_" with its previous name.
-        if (window._ && window.s) {
-            window._.str = window.s;
+        defineDependency("_", "underscore");
+        if (window._ && window._.str) {
+            define("underscore.string", [], function () {return window._.str;});
+        }
+        else {
+            console.error("Expected _.str (underscore.string) to be on the window object, but not found.");
         }
         defineDependency("gettext", "gettext");
         defineDependency("Logger", "logger");
