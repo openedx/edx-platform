59b452a4b3d2b50f67aac625ea4a3d4695b951a1
Usman Khalid <2200617@gmail.com>
2016-03-24 02:46:54 +0500

    Quality fixes.

--- lms/djangoapps/courseware/tests/test_access.py
+++ lms/djangoapps/courseware/tests/test_access.py
@@ -272,7 +272,9 @@ class AccessTestCase(LoginEnrollmentTestCase, ModuleStoreTestCase, MilestonesTes
         """
         self.assertTrue(bool(access.has_access(self.global_staff, 'staff', self.course, course_key=self.course.id)))
         self.assertTrue(bool(access.has_access(self.course_staff, 'staff', self.course, course_key=self.course.id)))
-        self.assertTrue(bool(access.has_access(self.course_instructor, 'staff', self.course, course_key=self.course.id)))
+        self.assertTrue(bool(access.has_access(
+            self.course_instructor, 'staff', self.course, course_key=self.course.id
+        )))
         self.assertFalse(bool(access.has_access(self.student, 'staff', self.course, course_key=self.course.id)))
         self.assertFalse(bool(access.has_access(self.student, 'load', self.course, course_key=self.course.id)))
 
