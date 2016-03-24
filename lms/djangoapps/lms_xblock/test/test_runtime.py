JCBarahona/edX

Add XBlock Badging Service.

lms/djangoapps/lms_xblock/test/test_runtime.py
@@ -5,10 +5,15 @@ Tests of the LMS XBlock Runtime and associated utilities
 from django.contrib.auth.models import User
 from django.conf import settings
 from ddt import ddt, data
-from mock import Mock
-from unittest import TestCase
+from django.test import TestCase
+from mock import Mock, patch
 from urlparse import urlparse
+
+from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locations import BlockUsageLocator, CourseLocator, SlashSeparatedCourseKey
+
+from badges.tests.factories import BadgeClassFactory
+from badges.tests.test_models import ImageFetchingMixin
 from lms.djangoapps.lms_xblock.runtime import quote_slashes, unquote_slashes, LmsModuleSystem
 from xblock.fields import ScopeIds
 
@@ -181,3 +186,63 @@ class TestUserServiceAPI(TestCase):
         # Try to get tag in wrong scope
         with self.assertRaises(ValueError):
             self.runtime.service(self.mock_block, 'user_tags').get_tag('fake_scope', self.key)
+
+
+class TestBadgingService(TestCase, ImageFetchingMixin):
+    """Test the badging service interface"""
+
+    def setUp(self):
+        super(TestBadgingService, self).setUp()
+        self.course_id = CourseKey.from_string('course-v1:org+course+run')
+
+        self.user = User(username='test_robot', email='test_robot@edx.org', password='test', first_name='Test')
+        self.user.save()
+
+        self.mock_block = Mock()
+        self.mock_block.service_declaration.return_value = 'needs'
+
+    def create_runtime(self):
+        """
+        Create the testing runtime.
+        """
+        def mock_get_real_user(_anon_id):
+            """Just returns the test user"""
+            return self.user
+
+        return LmsModuleSystem(
+            static_url='/static',
+            track_function=Mock(),
+            get_module=Mock(),
+            render_template=Mock(),
+            replace_urls=str,
+            course_id=self.course_id,
+            get_real_user=mock_get_real_user,
+            descriptor_runtime=Mock(),
+        )
+
+    @patch.dict(settings.FEATURES, {'ENABLE_OPENBADGES': True})
+    def test_service_rendered(self):
+        runtime = self.create_runtime()
+        self.assertTrue(runtime.service(self.mock_block, 'badging'))
+
+    @patch.dict(settings.FEATURES, {'ENABLE_OPENBADGES': False})
+    def test_no_service_rendered(self):
+        runtime = self.create_runtime()
+        self.assertFalse(runtime.service(self.mock_block, 'badging'))
+
+    @patch.dict(settings.FEATURES, {'ENABLE_OPENBADGES': True})
+    def test_get_badge_class(self):
+        runtime = self.create_runtime()
+        badge_service = runtime.service(self.mock_block, 'badging')
+        premade_badge_class = BadgeClassFactory.create()
+        # Ignore additional parameters. This class already exists.
+        badge_class = badge_service.get_badge_class(
+            slug='test_slug', issuing_component='test_component', description='Attempted override',
+            criteria='test', display_name='Testola', image_file_handle=self.get_image('good')
+        )
+        # These defaults are set on the factory.
+        self.assertEqual(badge_class.criteria, 'https://example.com/syllabus')
+        self.assertEqual(badge_class.display_name, 'Test Badge')
+        self.assertEqual(badge_class.description, "Yay! It's a test badge.")
+        # File name won't always be the same.
+        self.assertEqual(badge_class.image.path, premade_badge_class.image.path)

openedx/edx-platform

lms/djangoapps/lms_xblock/test/test_runtime.py
@@ -5,15 +5,22 @@ Tests of the LMS XBlock Runtime and associated utilities
 from django.contrib.auth.models import User
 from django.conf import settings
 from ddt import ddt, data
-from mock import Mock
-from unittest import TestCase
+from django.test import TestCase
+from mock import Mock, patch
 from urlparse import urlparse
+
+from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locations import BlockUsageLocator, CourseLocator, SlashSeparatedCourseKey
+
+from badges.tests.factories import BadgeClassFactory
+from badges.tests.test_models import get_image
 from lms.djangoapps.lms_xblock.runtime import quote_slashes, unquote_slashes, LmsModuleSystem
 from xblock.fields import ScopeIds
 from xmodule.modulestore.django import ModuleI18nService
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xblock.exceptions import NoSuchServiceError
+
+from student.tests.factories import UserFactory
 
 TEST_STRINGS = [
     '',
@@ -141,9 +148,7 @@ class TestUserServiceAPI(TestCase):
     def setUp(self):
         super(TestUserServiceAPI, self).setUp()
         self.course_id = SlashSeparatedCourseKey("org", "course", "run")
-
-        self.user = User(username='runtime_robot', email='runtime_robot@edx.org', password='test', first_name='Robot')
-        self.user.save()
+        self.user = UserFactory.create()
 
         def mock_get_real_user(_anon_id):
             """Just returns the test user"""
@@ -184,6 +189,67 @@ class TestUserServiceAPI(TestCase):
         # Try to get tag in wrong scope
         with self.assertRaises(ValueError):
             self.runtime.service(self.mock_block, 'user_tags').get_tag('fake_scope', self.key)
+
+
+class TestBadgingService(TestCase):
+    """Test the badging service interface"""
+
+    def setUp(self):
+        super(TestBadgingService, self).setUp()
+        self.course_id = CourseKey.from_string('course-v1:org+course+run')
+
+        self.user = User(username='test_robot', email='test_robot@edx.org', password='test', first_name='Test')
+        self.user.save()
+
+        self.mock_block = Mock()
+        self.mock_block.service_declaration.return_value = 'needs'
+
+    def create_runtime(self):
+        """
+        Create the testing runtime.
+        """
+        def mock_get_real_user(_anon_id):
+            """Just returns the test user"""
+            return self.user
+
+        return LmsModuleSystem(
+            static_url='/static',
+            track_function=Mock(),
+            get_module=Mock(),
+            render_template=Mock(),
+            replace_urls=str,
+            course_id=self.course_id,
+            get_real_user=mock_get_real_user,
+            descriptor_runtime=Mock(),
+        )
+
+    @patch.dict(settings.FEATURES, {'ENABLE_OPENBADGES': True})
+    def test_service_rendered(self):
+        runtime = self.create_runtime()
+        self.assertTrue(runtime.service(self.mock_block, 'badging'))
+
+    @patch.dict(settings.FEATURES, {'ENABLE_OPENBADGES': False})
+    def test_no_service_rendered(self):
+        runtime = self.create_runtime()
+        self.assertFalse(runtime.service(self.mock_block, 'badging'))
+
+    @patch.dict(settings.FEATURES, {'ENABLE_OPENBADGES': True})
+    def test_get_badge_class(self):
+        runtime = self.create_runtime()
+        badge_service = runtime.service(self.mock_block, 'badging')
+        premade_badge_class = BadgeClassFactory.create()
+        # Ignore additional parameters. This class already exists.
+        # We should get back the first class we created, rather than a new one.
+        badge_class = badge_service.get_badge_class(
+            slug='test_slug', issuing_component='test_component', description='Attempted override',
+            criteria='test', display_name='Testola', image_file_handle=get_image('good')
+        )
+        # These defaults are set on the factory.
+        self.assertEqual(badge_class.criteria, 'https://example.com/syllabus')
+        self.assertEqual(badge_class.display_name, 'Test Badge')
+        self.assertEqual(badge_class.description, "Yay! It's a test badge.")
+        # File name won't always be the same.
+        self.assertEqual(badge_class.image.path, premade_badge_class.image.path)
 
 
 class TestI18nService(ModuleStoreTestCase):