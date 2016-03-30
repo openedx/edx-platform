7eb9b8cacbfe8a56b2713bd6dd10d8f4c27922f0
Zia Fazal <zia.fazal@arbisoft.com>
2016-03-30 11:41:47 +0500

    merging 6b7d37a...disconnect signals to improve tests time
    add feature to disable course_published signal
    
    fixed broken tests
    
    fix for test failures in test_crud
    
    disabled course indexing for test crud
    
    fixed quality violations

--- openedx/core/djangoapps/content/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Setup the signals on startup.
-"""
-import openedx.core.djangoapps.content.course_structures.signals
-import openedx.core.djangoapps.content.block_structure.signals
-import openedx.core.djangoapps.content.course_metadata.signals
