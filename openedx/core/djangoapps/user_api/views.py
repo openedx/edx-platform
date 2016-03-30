aa075825f8bfa458d364e448d74f4809076c2cee
Luiz Aoqui <lgfa29@gmail.com>
2016-03-30 00:15:52 -0400

    fix typo

--- openedx/core/djangoapps/user_api/views.py
+++ openedx/core/djangoapps/user_api/views.py
@@ -363,7 +363,7 @@ class RegistrationView(APIView):
 
         # Translators: These instructions appear on the registration form, immediately
         # below a field meant to hold the user's full name.
-        name_in structions = _(u"Your legal name, used for any certificates you earn.")
+        name_instructions = _(u"Your legal name, used for any certificates you earn.")
 
         form_desc.add_field(
             "name",
