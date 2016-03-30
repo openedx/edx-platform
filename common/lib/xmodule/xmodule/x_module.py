7ae40dced8fe16087a891a4e86dd486f1828f41d
asadiqbal <aiqbal@edx.org>
2016-03-30 17:12:14 +0500

    asadiqbal08/WL-388: ModuleI18nService for Descriptor System
    mattdrayer: Default to XBlock class if no unmixed class

--- common/lib/xmodule/xmodule/x_module.py
+++ common/lib/xmodule/xmodule/x_module.py
@@ -1494,6 +1494,28 @@ class DescriptorSystem(MetricsMixin, ConfigurableFragmentWrapper, Runtime):
         # A stub publish method that doesn't emit any events from XModuleDescriptors.
         pass
 
+    def service(self, block, service_name):
+        """
+        Runtime-specific override for the XBlock service manager.  If a service is not currently
+        instantiated and is declared as a critical requirement, an attempt is made to load the
+        module.
+
+        Arguments:
+            block (an XBlock): this block's class will be examined for service
+                decorators.
+            service_name (string): the name of the service requested.
+
+        Returns:
+            An object implementing the requested service, or None.
+        """
+        # getting the service from parent module. making sure of block service declarations.
+        service = super(DescriptorSystem, self).service(block=block, service_name=service_name)
+        # Passing the block to service if it is callable e.g. ModuleI18nService. It is the responsibility of calling
+        # service to handle the passing argument.
+        if callable(service):
+            return service(block)
+        return service
+
 
 new_contract('DescriptorSystem', DescriptorSystem)
 
