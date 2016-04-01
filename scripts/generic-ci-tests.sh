ccc0bb1883024921318a640d50778441184fb650
Ben Patterson <bpatterson@edx.org>
2016-04-01 09:03:43 -0400

    Revert "Use system path for firefox location."
    This reverts commit bae84bb98425202b35b58fcb7a74b3506cbb8fb1.

--- scripts/generic-ci-tests.sh
+++ scripts/generic-ci-tests.sh
@@ -160,9 +160,10 @@ case "$TEST_SUITE" in
     "bok-choy")
 
         # Back compatibility support for firefox upgrade:
-        # set path for bok-choy to use. This will be removed
-        # later when the default firefox will be updated and used.
-        export SELENIUM_FIREFOX_PATH=$HOME/firefox/firefox
+        # Copy newer firefox version to project root,
+        # set that as the path for bok-choy to use.
+        cp -R $HOME/firefox/ firefox/
+        export SELENIUM_FIREFOX_PATH=firefox/firefox
 
         case "$SHARD" in
 
