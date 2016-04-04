870c6187f204b99ecd0ce7faa886821e7ee6c8c8
Filip Jukić <filip@jukic.me>
2016-04-04 13:31:53 +0200

    minor refactoring of SASS serializer field

--- common/djangoapps/microsite_configuration/serializers.py
+++ common/djangoapps/microsite_configuration/serializers.py
@@ -3,17 +3,19 @@ from .models import Microsite
 from .utils import sass_to_dict, json_to_sass
 
 
-class SASSJSONSerializerField(serializers.DictField):
+class SASSDictField(serializers.DictField):
     child = serializers.CharField()
-    """ Serializer for JSONField -- required to make field writable"""
+
     def to_internal_value(self, data):
         return json_to_sass(data)
+
     def to_representation(self, value):
         return sass_to_dict(value)
 
 
 class MicrositeSerializer(serializers.ModelSerializer):
-    sass_variables = SASSJSONSerializerField()
+    values = serializers.DictField()
+    sass_variables = SASSDictField()
 
     class Meta:
         model = Microsite
