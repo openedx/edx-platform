158e78ed28627259133782ed351b58d531cd6699
Filip Jukić <filip@jukic.me>
2016-04-04 14:01:53 +0200

    use a full, read/write viewset

--- common/djangoapps/microsite_configuration/api.py
+++ common/djangoapps/microsite_configuration/api.py
@@ -3,6 +3,6 @@ from .models import Microsite
 from .serializers import MicrositeSerializer
 
 
-class MicrositeViewSet(viewsets.ReadOnlyModelViewSet):
+class MicrositeViewSet(viewsets.ModelViewSet):
     queryset = Microsite.objects.all()
     serializer_class = MicrositeSerializer
