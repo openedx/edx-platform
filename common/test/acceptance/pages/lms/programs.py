182e34f893481ba578276da0542f4d1db55d0990
Renzo Lucioni <renzo@renzolucioni.com>
2016-04-07 15:40:26 -0400

    Acceptance tests for program listing page
    Includes a11y coverage. ECOM-3980.

---
+++ common/test/acceptance/pages/lms/programs.py
@@ -0,0 +1,22 @@
+"""LMS-hosted Programs pages"""
+from bok_choy.page_object import PageObject
+
+from . import BASE_URL
+
+
+class ProgramListingPage(PageObject):
+    """Program listing page."""
+    url = BASE_URL + '/dashboard/programs/'
+
+    def is_browser_on_page(self):
+        return self.q(css='.program-list-wrapper').present
+
+    @property
+    def are_cards_present(self):
+        """Check whether program cards are present."""
+        return self.q(css='.program-card').present
+
+    @property
+    def is_sidebar_present(self):
+        """Check whether sidebar is present."""
+        return self.q(css='.sidebar').present
