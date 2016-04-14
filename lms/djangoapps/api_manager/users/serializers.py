d88516d6da6fb495183fc0c22e26d6de14d56aa6
Saqib <msaqib52@gmail.com>
2016-04-14 20:49:29 +0500

    Add new fields to users list API and add organizaton, name and course enrolment filters

--- lms/djangoapps/api_manager/users/serializers.py
+++ lms/djangoapps/api_manager/users/serializers.py
@@ -2,7 +2,7 @@
 
 from rest_framework import serializers
 
-from api_manager.models import APIUser
+from api_manager.models import APIUser, GroupProfile
 from organizations.serializers import BasicOrganizationSerializer
 from student.models import UserProfile
 
@@ -27,6 +27,15 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
                 self.fields.pop(field_name)
 
 
+class GroupProfileSerializer(serializers.ModelSerializer):
+    """ Serializer for GroupProfile model interactions """
+
+    class Meta(object):
+        """ Serializer/field specification """
+        model = GroupProfile
+        fields = ('id', 'name', )
+
+
 class UserSerializer(DynamicFieldsModelSerializer):
 
     """ Serializer for User model interactions """
@@ -37,11 +46,45 @@ class UserSerializer(DynamicFieldsModelSerializer):
     title = serializers.CharField(source='profile.title')
     country = serializers.CharField(source='profile.country')
     full_name = serializers.CharField(source='profile.name')
+    courses_enrolled = serializers.SerializerMethodField('get_courses_enrolled')
+    roles = serializers.SerializerMethodField('get_permission_group_type_roles')
 
-    class Meta:
+    def get_courses_enrolled(self, user):
+        """ Serialize user enrolled courses """
+        if hasattr(user, 'courses_enrolled'):
+            return user.courses_enrolled
+
+        return user.courseenrollment_set.count
+
+    def get_permission_group_type_roles(self, user):
+        """ Serialize GroupProfile for permission group type """
+        queryset = GroupProfile.objects.filter(group__user=user, group_type='permission')
+        serializer = GroupProfileSerializer(queryset, many=True)
+
+        return serializer.data
+
+    class Meta(object):
         """ Serializer/field specification """
         model = APIUser
-        fields = ("id", "email", "username", "first_name", "last_name", "created", "is_active", "organizations", "avatar_url", "city", "title", "country", "full_name", "is_staff")
+        fields = (
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "created",
+            "is_active",
+            "organizations",
+            "avatar_url",
+            "city",
+            "title",
+            "country",
+            "full_name",
+            "is_staff",
+            "last_login",
+            "courses_enrolled",
+            "roles"
+        )
         read_only_fields = ("id", "email", "username")
 
 
