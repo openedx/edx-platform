88e39711b99d7b0f7ace4279bb444bbe6d80ec05
Usman Khalid <2200617@gmail.com>
2016-04-15 13:19:28 +0500

    Increase Karma's browserNoActivityTimeout to prevent timeouts on Jenkins.

--- common/static/common/js/karma.common.conf.js
+++ common/static/common/js/karma.common.conf.js
@@ -206,7 +206,9 @@ var getConfig = function (config, useRequireJs) {
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
+        concurrency: Infinity,
+
+        browserNoActivityTimeout: 50000
     };
 };
 
