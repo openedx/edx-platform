d57aa04179e30891c20cf5aa58361a4b57b07b95
Robert Raposa <rraposa@edx.org>
2016-04-25 14:37:54 -0400

    Fix review comments

--- pavelib/paver_tests/test_safelint.py
+++ pavelib/paver_tests/test_safelint.py
@@ -31,20 +31,20 @@ class TestPaverSafeLint(unittest.TestCase):
     @patch.object(pavelib.quality, '_write_metric')
     @patch.object(pavelib.quality, '_prepare_report_dir')
     @patch.object(pavelib.quality, '_get_count_from_last_line')
-    def test_safelint_violation_number_not_found(self, mock_count, mock_report_dir, mock_write_metric):  # pylint: disable=unused-argument
+    def test_safelint_violation_number_not_found(self, _mock_count, _mock_report_dir, _mock_write_metric):
         """
         run_safelint encounters an error parsing the safelint output log
         """
-        mock_count.return_value = None
+        _mock_count.return_value = None
         with self.assertRaises(BuildFailure):
             pavelib.quality.run_safelint("")
 
     @patch.object(pavelib.quality, '_write_metric')
     @patch.object(pavelib.quality, '_prepare_report_dir')
     @patch.object(pavelib.quality, '_get_count_from_last_line')
-    def test_jshint_vanilla(self, mock_count, mock_report_dir, mock_write_metric):  # pylint: disable=unused-argument
+    def test_safelint_vanilla(self, _mock_count, _mock_report_dir, _mock_write_metric):
         """
         safelint finds violations, but a limit was not set
         """
-        mock_count.return_value = 1
+        _mock_count.return_value = 1
         pavelib.quality.run_safelint("")
