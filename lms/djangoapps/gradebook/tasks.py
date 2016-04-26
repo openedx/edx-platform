6f0130f3bb6a8410c15ab507d1182675274ad86b
Zia Fazal <zia.fazal@arbisoft.com>
2016-04-26 18:11:45 +0500

    fixed issue with user grades not being computed

--- lms/djangoapps/gradebook/tasks.py
+++ lms/djangoapps/gradebook/tasks.py
@@ -9,6 +9,8 @@ from celery.task import task  # pylint: disable=import-error,no-name-in-module
 from courseware import grades
 from xmodule.modulestore import EdxJSONEncoder
 from util.request import RequestMockWithoutMiddleware
+from django.contrib.auth.models import User
+from opaque_keys.edx.keys import CourseKey
 
 from gradebook.models import StudentGradebook
 
@@ -16,11 +18,16 @@ log = logging.getLogger('edx.celery.task')
 
 
 @task(name=u'lms.djangoapps.gradebook.tasks.update_user_gradebook')
-def update_user_gradebook(course_key, user):
+def update_user_gradebook(course_key, user_id):
     """
     Taks to recalculate user's gradebook entry
     """
+    if not isinstance(course_key, basestring):
+        raise ValueError('course_key must be a string. {} is not acceptable.'.format(type(course_key)))
+
+    course_key = CourseKey.from_string(course_key)
     try:
+        user = User.objects.get(id=user_id)
         _generate_user_gradebook(course_key, user)
     except Exception as ex:
         log.exception('An error occurred while generating gradebook: %s', ex.message)
