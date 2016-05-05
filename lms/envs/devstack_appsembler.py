08dea8f773b08f27e09497aa106072a06a7a0337
Filip Jukić <filip@jukic.me>
2016-05-05 13:13:59 +0200

    disable ALL the caches!

--- lms/envs/devstack_appsembler.py
+++ lms/envs/devstack_appsembler.py
@@ -13,8 +13,9 @@ FEATURES["SHOW_ONLY_APPSEMBLER_AND_OWNED_COURSES"] = False
 OAUTH_ENFORCE_SECURE = False
 
 # disable caching in dev environment
-CACHES['general']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
-CACHES['default']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
+for cache_key in CACHES.keys():
+    CACHES[cache_key]['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
+
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 MICROSITE_BACKEND = 'microsite_configuration.backends.database.DatabaseMicrositeBackend'
