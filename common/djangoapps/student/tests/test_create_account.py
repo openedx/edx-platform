7a1ca186d954c3bad5763b85e0faaa465674ff92
Elena Komendantova <elena.komendantova@ontarget-group.com>
2016-05-05 16:27:30 +0300

    removed password_copy

--- common/djangoapps/student/tests/test_create_account.py
+++ common/djangoapps/student/tests/test_create_account.py
@@ -54,7 +54,6 @@ class TestCreateAccount(TestCase):
             "name": "Test User",
             "honor_code": "true",
             "terms_of_service": "true",
-            "password_copy": "testpass",
         }
 
     @ddt.data("en", "eo")
@@ -295,7 +294,6 @@ class TestCreateAccountValidation(TestCase):
             "name": "Test Name",
             "honor_code": "true",
             "terms_of_service": "true",
-            "password_copy": "test_password",
         }
 
     def assert_success(self, params):
@@ -383,7 +381,6 @@ class TestCreateAccountValidation(TestCase):
     )
     def test_email_really_exists(self):
         params = dict(self.minimal_params)
-
         field = "email"
         params[field] = "abracadabra@abracadabra.com"
         self.assert_error(params, field, "Email domain 'abracadabra.com' doesn't exist")
@@ -417,7 +414,7 @@ class TestCreateAccountValidation(TestCase):
             self.assert_success(params)
         else:
             self.assert_error(params, "email", "Unauthorized email address.")
-
+		
     def test_password(self):
         params = dict(self.minimal_params)
 
@@ -442,26 +439,6 @@ class TestCreateAccountValidation(TestCase):
         # Matching username
         params["username"] = params["password"] = "test_username_and_password"
         assert_password_error("Username and password fields cannot match")
-
-    def test_password_copy(self):
-        params = dict(self.minimal_params)
-
-        field = "password_copy"
-        error = "A copy of password is required"
-        # missing
-        del params[field]
-        self.assert_error(params, field, error)
-
-        #empty
-        params[field] = ""
-        self.assert_error(params, field, error)
-
-        # too short
-        params[field] = "a"
-        self.assert_error(params, field, error)
-
-        params[field] = "abracadabra"
-        self.assert_error(params, field, "Passwords don't match")
 
     def test_name(self):
         params = dict(self.minimal_params)
@@ -588,7 +565,6 @@ class TestCreateCommentsServiceUser(TransactionTestCase):
             "name": "Test User",
             "honor_code": "true",
             "terms_of_service": "true",
-            "password_copy": "testpass",
         }
 
     def test_cs_user_created(self, request):
