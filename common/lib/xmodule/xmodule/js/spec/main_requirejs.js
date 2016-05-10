0590a6da11f4fc0aa53cb0398d26ad9f36d672e1
Chris Rodriguez <github@chrisrodriguez.me>
2016-05-10 14:58:02 -0400

    AC-454 adding visual clarify for selected menu options

--- common/lib/xmodule/xmodule/js/spec/main_requirejs.js
+++ common/lib/xmodule/xmodule/js/spec/main_requirejs.js
@@ -1,4 +1,36 @@
-(function(requirejs) {
+(function(requirejs, define) {
+    'use strict';
+    // We do not wish to bundle common libraries (that may also be used by non-RequireJS code on the page
+    // into the optimized files. Therefore load these libraries through script tags and explicitly define them.
+    // Note that when the optimizer executes this code, window will not be defined.
+    if (window) {
+        var defineDependency = function (globalName, name, noShim) {
+            var getGlobalValue = function(name) {
+                var globalNamePath = name.split('.'),
+                    result = window,
+                    i;
+                for (i = 0; i < globalNamePath.length; i++) {
+                    result = result[globalNamePath[i]];
+                }
+                return result;
+            },
+                globalValue = getGlobalValue(globalName);
+            if (globalValue) {
+                if (noShim) {
+                    define(name, {});
+                }
+                else {
+                    define(name, [], function() { return globalValue; });
+                }
+            }
+            else {
+                console.error("Expected library to be included on page, but not found on window object: " + name);
+            }
+        };
+        defineDependency("jQuery", "jquery");
+        defineDependency("jQuery", "jquery-migrate");
+        defineDependency("_", "underscore");
+    }
     requirejs.config({
         baseUrl: '/base/',
         paths: {
@@ -6,7 +38,8 @@
             "modernizr": "common_static/edx-pattern-library/js/modernizr-custom",
             "afontgarde": "common_static/edx-pattern-library/js/afontgarde",
             "edxicons": "common_static/edx-pattern-library/js/edx-icons",
-            "draggabilly": "common_static/js/vendor/draggabilly"
+            "draggabilly": "common_static/js/vendor/draggabilly",
+            'edx-ui-toolkit': 'common_static/edx-ui-toolkit'
         },
         "moment": {
             exports: "moment"
@@ -18,5 +51,4 @@
             exports: "AFontGarde"
         }
     });
-
-}).call(this, RequireJS.requirejs);
+}).call(this, RequireJS.requirejs, RequireJS.define);
