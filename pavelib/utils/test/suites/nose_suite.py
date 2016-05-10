226da7d364041e534c017ee4606088d5951ddab1
Calen Pennington <cale@edx.org>
2016-05-10 15:05:46 -0400

    Make LMS tests run concurrently

--- pavelib/utils/test/suites/nose_suite.py
+++ pavelib/utils/test/suites/nose_suite.py
@@ -120,18 +120,29 @@ class SystemTestSuite(NoseTestSuite):
     def cmd(self):
         cmd = (
             './manage.py {system} test --verbosity={verbosity} '
-            '{test_id} {test_opts} --settings=test {extra} '
+            '{test_id} {test_opts} --settings=test {system_opts} {extra} '
             '--with-xunitmp --xunitmp-file={xunit_report}'.format(
                 system=self.root,
                 verbosity=self.verbosity,
                 test_id=self.test_id,
                 test_opts=self.test_options_flags,
+                system_opts=self._system_options,
                 extra=self.extra_args,
                 xunit_report=self.report_dir / "nosetests.xml",
             )
         )
 
         return self._under_coverage_cmd(cmd)
+
+    @property
+    def _system_options(self):
+        """
+        Test arguments that are only enabled for specific systems.
+        """
+        if self.root == 'lms':
+            return '--with-randomly --with-database-isolation --processes=-1'
+
+        return ''
 
     @property
     def _default_test_id(self):
