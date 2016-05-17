3a37a87907a273002240358624890181b6912095
J. Cliff Dyer <jcd@sdf.org>
2016-05-17 13:13:53 -0400

    fixup: added tests

--- lms/djangoapps/courseware/tests/test_transformers.py
+++ lms/djangoapps/courseware/tests/test_transformers.py
@@ -2,11 +2,13 @@
 Test the behavior of the GradesTransformer
 """
 
-from student.tests.factories import UserFactory
+import datetime
+import pytz
 
+from student.tests.factories import UserFactory
 from lms.djangoapps.course_blocks.api import get_course_blocks
-from ..transformers import GradesBlockTransformer
 from lms.djangoapps.course_blocks.transformers.tests.helpers import CourseStructureTestCase
+from ..transformers import GradesBlockTransformer
 
 
 class GradesBlockTransformerTestCase(CourseStructureTestCase):
@@ -14,88 +16,129 @@ class GradesBlockTransformerTestCase(CourseStructureTestCase):
     Verify behavior of the GradesBlockTransformer
     """
 
-    course_dict = [
-        {
-            u'org': u'GradesBlockTestOrg',
-            u'course': u'GB101',
-            u'run': u'cannonball',
-            u'#type': u'course',
-            u'#ref': u'course',
-            u'#children': [
-                {
-                    u'metadata': {
-                        u'graded': True,
-                        u'weight': 1,
-                        u'has_score': True,
-                        u'due': u'2099-03-15T12:30:00+00:00',
-                    },
-                    u'#type': u'problem',
-                    u'#ref': u'problem_1',
-                    u'data': u'''
-                        <problem>
-                            <numericalresponse answer="2">
-                                <textline label="1+1" trailing_text="%" />
-                            </numericalresponse>
-                        </problem>''',
-                },
-                {
-                    u'metadata': {
-                        u'graded': False,
-                        u'weight': 2,
-                        u'has_score': True,
-                        u'due': u'2099-10-31T23:59:59+00:00',
-                        u'visible_to_staff_only': True,
-                    },
-                    u'#type': u'problem',
-                    u'#ref': u'problem_2',
-                    u'data': u'''
-                        <problem>
-                            <numericalresponse answer="27">
-                                <textline label="3^3" />
-                            </numericalresponse>
-                            <numericalresponse answer="13.5">
-                                <textline label="and then half of that?" />
-                            </numericalresponse>
-                        </problem>''',
-                },
-            ],
-        },
-    ]
-
-    expected_max_score = {
-        u'problem_1': 1,
-        u'problem_2': 2,
-    }
     TRANSFORMER_CLASS_TO_TEST = GradesBlockTransformer
+
+    problem_metadata = {
+        u'graded': True,
+        u'weight': 1,
+        u'due': datetime.datetime(2099, 03, 15, 12, 30, 00, tzinfo=pytz.utc),
+    }
 
     def setUp(self):
         super(GradesBlockTransformerTestCase, self).setUp()
-        self.blocks = self.build_course(self.course_dict)
-        self.course = self.blocks[u'course']
-
-    def test_grades_collected(self):
         password = u'test'
-        student = UserFactory.create(is_staff=False, username=u'test_student', password=password)
-        self.client.login(username=student.username, password=password)
-        block_structure = get_course_blocks(student, self.course.location, self.transformers)
+        self.student = UserFactory.create(is_staff=False, username=u'test_student', password=password)
+        self.client.login(username=self.student.username, password=password)
 
-        problem_expectations = self.course_dict[0][u'#children']
-        for problem, expected_result in [
-                (self.blocks[u'problem_1'], problem_expectations[0]),
-                (self.blocks[u'problem_2'], problem_expectations[1]),
-        ]:
-            for field in [u'weight', u'graded', u'has_score']:
-                self.assertEqual(
-                    expected_result[u'metadata'].get(field),
-                    block_structure.get_xblock_field(problem.location, field),
-                )
+    def assert_collected_xblock_fields(self, block_structure, usage_key, **expectations):
+        self.assertGreater(len(expectations), 0)
+        for field in expectations:
             self.assertEqual(
-                expected_result[u'metadata'].get(u'due'),
-                block_structure.get_xblock_field(problem.location, u'due').isoformat(),
+                expectations[field],
+                block_structure.get_xblock_field(usage_key, field),
             )
-            max_score = block_structure.get_transformer_block_field(
-                problem.location,
-                GradesBlockTransformer,
-                'max_score'
+
+    def assert_collected_transformer_block_fields(self, block_structure, usage_key, transformer_class, **expectations):
+        self.assertGreater(len(expectations), 0)
+        for field in expectations:
+            self.assertEqual(
+                expectations[field],
+                block_structure.get_transformer_block_field(usage_key, transformer_class, field),
             )
-            self.assertEqual(self.expected_max_score[expected_result[u'#ref']], max_score)
+
+    def build_course_with_problems(self, data='<problem></problem>', metadata=None):
+        metadata = metadata or self.problem_metadata
+        return self.build_course([
+            {
+                u'org': u'GradesBlockTestOrg',
+                u'course': u'GB101',
+                u'run': u'cannonball',
+                u'#type': u'course',
+                u'#ref': u'course',
+                u'#children': [
+                    {
+                        u'metadata': metadata,
+                        u'#type': u'problem',
+                        u'#ref': u'problem',
+                        u'data': data,
+                    }
+                ]
+            }
+        ])
+
+    def test_grades_collected_basic(self):
+
+        expected_max_score = 1
+
+        blocks = self.build_course_with_problems()
+        block_structure = get_course_blocks(self.student, blocks[u'course'].location, self.transformers)
+
+        self.assert_collected_xblock_fields(
+            block_structure, 
+            blocks['problem'].location,
+            weight=self.problem_metadata[u'weight'],
+            graded=self.problem_metadata[u'graded'],
+            has_score=True,
+            due=self.problem_metadata[u'due'],
+        )
+
+    def test_collecting_staff_only_problem(self):
+        problem_metadata = {
+            u'graded': True,
+            u'weight': 1,
+            u'due': datetime.datetime(2016, 10, 16, 00, 04, 00, tzinfo=pytz.utc),
+            u'visible_to_staff_only': True,
+        }
+
+        blocks = self.build_course_with_problems(metadata=problem_metadata)
+        block_structure = get_course_blocks(self.student, blocks[u'course'].location, self.transformers)
+
+        self.assert_collected_xblock_fields(
+            block_structure, 
+            blocks['problem'].location,
+            weight=problem_metadata[u'weight'],
+            graded=problem_metadata[u'graded'],
+            has_score=True,
+            due=problem_metadata[u'due'],
+        )
+
+    def test_max_score_collection(self):
+        problem_data = u'''
+            <problem>
+                <numericalresponse answer="2">
+                    <textline label="1+1" trailing_text="%" />
+                </numericalresponse>
+            </problem>
+        '''
+
+        blocks = self.build_course_with_problems(data=problem_data)
+        block_structure = get_course_blocks(self.student, blocks[u'course'].location, self.transformers)
+
+        self.assert_collected_transformer_block_fields(
+            block_structure,
+            blocks[u'problem'].location,
+            self.TRANSFORMER_CLASS_TO_TEST,
+            max_score=1,
+        )
+
+    def test_max_score_for_multiresponse_problem(self):
+        problem_data = u'''
+            <problem>
+                <numericalresponse answer="27">
+                    <textline label="3^3" />
+                </numericalresponse>
+                <numericalresponse answer="13.5">
+                    <textline label="and then half of that?" />
+                </numericalresponse>
+            </problem>
+        '''
+
+        blocks = self.build_course_with_problems(problem_data)
+        block_structure = get_course_blocks(self.student, blocks[u'course'].location, self.transformers)
+
+        self.assert_collected_transformer_block_fields(
+            block_structure,
+            blocks[u'problem'].location,
+            self.TRANSFORMER_CLASS_TO_TEST,
+            max_score=2,
+        )
