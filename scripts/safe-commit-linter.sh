a7e754dd9a30a5c9772c35035286922e188bc36b
Andy Armstrong <andya@edx.org>
2016-05-18 12:52:21 -0400

    Exclude deleted files from safe template linting

--- scripts/safe-commit-linter.sh
+++ scripts/safe-commit-linter.sh
@@ -67,7 +67,7 @@ fi
 
 merge_base_command="git merge-base $current_branch_hash $MAIN_COMMIT"
 merge_base=$(${merge_base_command})
-diff_command="git diff --name-only --diff-filter=ACM $current_branch_hash $merge_base"
+diff_command="git diff --name-only --diff-filter=ACM $merge_base $current_branch_hash"
 diff_files=$(${diff_command})
 
 if [ "$diff_files" = "" ]; then
