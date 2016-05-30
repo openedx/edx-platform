88d0615558ddbd95601f4e4f474c46481e50e12e
Filip Jukic <filip@jukic.me>
2016-05-30 19:50:20 +0200

    add initial page contents method

--- common/djangoapps/microsite_configuration/utils.py
+++ common/djangoapps/microsite_configuration/utils.py
@@ -17,6 +17,26 @@ def get_initial_sass_variables():
         return f.read()
 
 
+def get_initial_page_contents():
+    return [
+        [
+            "hero-element",
+            "static-blocks/_hero-section-01.html",
+            {
+                "hero_title": "Welcome to this test site, buddy!", #leave value empty if you don't want it displayed.
+                "hero_subtitle": "Bringing you mobile optimized courses about Open edX. We're Appsembler. Awesome!", # leave value empty if you don't want it displayed.
+                "cta_link": "http://www.appsembler.com",  ## leave value empty if you don't want a CTA button displayed.
+                "cta_link_text": "Awesome link!",  ## leave value empty if you don't want a CTA button displayed.
+                "popup_video_url": "MZrctLnsF4M", ## YouTube video ID - leave value empty if you don't want a video button and modal displayed.
+                "popup_video_text": "Watch our video", ## leave value empty if you don't want a video button and modal displayed.
+                "popup_video_id": "HeroVideo", ## leave value empty if you don't want a video button and modal displayed.
+                "bg_image_url": "/static/themes/amc-beta/images/hero-image.jpg", ## leave empty if you want just a full color background
+                "extra_css": ""  ## add custom css to be added inline to the wrapper element
+            }
+       ]
+    ]
+
+
 def sass_to_dict(sass_input):
     sass_vars = []
     lines = (line for line in sass_input.splitlines() if line and not line.startswith('//'))
@@ -36,6 +56,7 @@ def sass_to_json_string(sass_input):
 def dict_to_sass(dict_input):
     sass_text = '\n'.join("{}: {};".format(key, val) for (key, val) in dict_input)
     return sass_text
+
 
 def json_to_sass(json_input):
     sass_dict = json.loads(json_input)
