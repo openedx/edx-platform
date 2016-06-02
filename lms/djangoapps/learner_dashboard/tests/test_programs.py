a35686a48b31ef74a6026f7eba8ef456926ebb35
Renzo Lucioni <renzo@renzolucioni.com>
2016-06-02 13:08:42 -0400

    Revert "Supplement program data with course and enrollment data"
    This reverts commit a8150a51d2173502825618acf4f597b9790359b5.

--- lms/djangoapps/learner_dashboard/tests/test_programs.py
+++ lms/djangoapps/learner_dashboard/tests/test_programs.py
@@ -24,7 +24,7 @@ from openedx.core.djangoapps.programs.tests.mixins import (
     ProgramsDataMixin)
 from student.models import CourseEnrollment
 from student.tests.factories import UserFactory
-from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase, SharedModuleStoreTestCase
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
 
 
@@ -231,17 +231,12 @@ class TestProgramListing(
 @httpretty.activate
 @override_settings(MKTG_URLS={'ROOT': 'http://edx.org'})
 @unittest.skipUnless(settings.ROOT_URLCONF == 'lms.urls', 'Test only valid in lms')
-class TestProgramDetails(ProgramsApiConfigMixin, SharedModuleStoreTestCase):
+class TestProgramDetails(ProgramsApiConfigMixin, TestCase):
     """
     Unit tests for the program details page
     """
     program_id = 123
     password = 'test'
-
-    @classmethod
-    def setUpClass(cls):
-        super(TestProgramDetails, cls).setUpClass()
-        cls.course = CourseFactory()
 
     def setUp(self):
         super(TestProgramDetails, self).setUp()
@@ -253,12 +248,11 @@ class TestProgramDetails(ProgramsApiConfigMixin, SharedModuleStoreTestCase):
 
         ClientFactory(name=ProgramsApiConfig.OAUTH2_CLIENT_NAME, client_type=CONFIDENTIAL)
 
-        self.organization = factories.Organization()
-        self.run_mode = factories.RunMode(course_key=unicode(self.course.id))  # pylint: disable=no-member
-        self.course_code = factories.CourseCode(run_modes=[self.run_mode])
         self.data = factories.Program(
-            organizations=[self.organization],
-            course_codes=[self.course_code]
+            organizations=[factories.Organization()],
+            course_codes=[
+                factories.CourseCode(run_modes=[factories.RunMode()]),
+            ]
         )
 
     def _mock_programs_api(self):
