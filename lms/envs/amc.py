459d5964ad7bcad19ea3c0ca33116a1259a604c9
Filip Jukić <filip@jukic.me>
2016-06-06 22:35:29 +0200

    add required settings for staging

--- lms/envs/amc.py
+++ lms/envs/amc.py
@@ -14,3 +14,18 @@ MAILGUN_ACCESS_KEY = AUTH_TOKENS.get("MAILGUN_ACCESS_KEY")
 MAILGUN_SERVER_NAME = AUTH_TOKENS.get("MAILGUN_SERVER_NAME")
 
 INSTALLED_APPS += ('appsembler_lms',)
+
+MICROSITE_BACKEND = 'microsite_configuration.backends.database.DatabaseMicrositeBackend'
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_HEADERS = (
+    'x-requested-with',
+    'content-type',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken',
+    'cache-control'
+)
