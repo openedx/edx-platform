7e7cb5bd4dc7235f307513f5a2e29d0aa6ca0ec8
Andy Armstrong <andya@edx.org>
2016-06-06 18:21:27 -0400

    Add a content header to the discussion board

--- lms/djangoapps/discussion/static/discussion/js/discussion_profile_page_factory.js
+++ lms/djangoapps/discussion/static/discussion/js/discussion_profile_page_factory.js
@@ -1,25 +1,25 @@
-;(function(define) {
+(function(define) {
     'use strict';
 
-    define(['jquery', 'DiscussionUserProfileView'],
+    define(['jquery', 'discussion/js/views/discussion_user_profile_view'],
         function($, DiscussionUserProfileView) {
             return function(options) {
-                var element = options.el,
-                    threads = element.data('threads'),
-                    userInfo = element.data('user-info'),
-                    page = element.data('page'),
-                    numPages = element.data('num-pages');
+                var $element = options.$el,
+                    threads = options.threads,
+                    userInfo = options.userInfo,
+                    page = options.page,
+                    numPages = options.numPages;
                 // Roles are not included in user profile page, but they are not used for anything
                 window.DiscussionUtil.loadRoles({
                     'Moderator': [],
                     'Administrator': [],
                     'Community TA': []
                 });
-                window.$$course_id = element.data('course-id');
+                window.$$course_id = options.courseId;
                 window.user = new window.DiscussionUser(userInfo);
                 // jshint nonew:false
                 new DiscussionUserProfileView({
-                    el: element,
+                    el: $element,
                     collection: threads,
                     page: page,
                     numPages: numPages
