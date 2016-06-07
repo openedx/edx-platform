0b74ef374a734d303280f1ae30b99bfd68ed6a08
Jacek Bzdak <jacek@opencraft.com>
2016-06-07 16:11:47 +0200

    Added unittests for discussion_xblock.

--- common/lib/xblock/xblock_discussion/xblock_discussion.py
+++ common/lib/xblock/xblock_discussion/xblock_discussion.py
@@ -16,29 +16,39 @@ log = logging.getLogger(__name__)
 loader = ResourceLoader(__name__)  # pylint: disable=invalid-name
 
 
+def _(text):
+    """A noop underscore function that marks strings for extraction."""
+    return text
+
+
 @XBlock.needs('user')
+@XBlock.needs('i18n')
 class DiscussionXBlock(XBlock, StudioEditableXBlockMixin):
-    """ Provides functionality similar to discussion XModule in inline mode """
+    """Provides a discussion forum that is inline with other content in the courseware."""
 
     discussion_id = String(scope=Scope.settings, default=UNIQUE_ID, force_export=True)
     display_name = String(
-        display_name="Display Name",
-        help="Display name for this module",
+        display_name=_("Display Name"),
+        help=_("Display name for this module"),
         default="Discussion",
         scope=Scope.settings
     )
     discussion_category = String(
-        display_name="Category",
-        default="Week 1",
-        help="A category name for the discussion. "
-             "This name appears in the left pane of the discussion forum for the course.",
+        display_name=_("Category"),
+        default=_("Week 1"),
+        help=_(
+            "A category name for the discussion. "
+            "This name appears in the left pane of the discussion forum for the course."
+        ),
         scope=Scope.settings
     )
     discussion_target = String(
-        display_name="Subcategory",
+        display_name=_("Subcategory"),
         default="Topic-Level Student-Visible Label",
-        help="A subcategory name for the discussion. "
-             "This name appears in the left pane of the discussion forum for the course.",
+        help=_(
+            "A subcategory name for the discussion. "
+            "This name appears in the left pane of the discussion forum for the course."
+        ),
         scope=Scope.settings
     )
     sort_key = String(scope=Scope.settings)
@@ -52,39 +62,56 @@ class DiscussionXBlock(XBlock, StudioEditableXBlockMixin):
         """
         return getattr(self.scope_ids.usage_id, 'course_key', None)
 
+    @property
+    def django_user(self):
+        """
+        Returns django user associated with user currently interacting
+        with the XBlock.
+        """
+        user_service = self.runtime.service(self, 'user')
+        if not user_service:
+            return None
+        return user_service._django_user  # pylint: disable=protected-access
+
+    def has_permission(self, permission):
+        """
+
+        Encapsulates lms specific functionality, as `has_permission` is not
+        importable outside of lms context, namely in tests.
+
+        :param user:
+        :param str permission: Permission
+        :rtype: bool
+        """
+        # normal import causes the xmodule_assets command to fail due to circular import - hence importing locally
+        from django_comment_client.permissions import has_permission  # pylint: disable=import-error
+
+        return has_permission(self.django_user, permission, self.course_key)
+
     def student_view(self, context=None):  # pylint: disable=unused-argument
         """ Renders student view for LMS and Studio """
         # pylint: disable=no-member
         if hasattr(self, 'xmodule_runtime') and getattr(self.xmodule_runtime, 'is_author_mode', False):
-            fragment = self._student_view_studio()
+            return self.student_view_studio()
         else:
-            fragment = self._student_view_lms()
+            return self.student_view_lms()
 
-        return fragment
-
-    def _student_view_lms(self):
+    def student_view_lms(self):
         """ Renders student view for LMS """
+
         fragment = Fragment()
-
-        # normal import causes the xmodule_assets command to fail due to circular import - hence importing locally
-        from django_comment_client.permissions import has_permission  # pylint: disable=import-error
-
-        user = None
-        user_service = self.runtime.service(self, 'user')
-        if user_service:
-            user = user_service._django_user  # pylint: disable=protected-access
 
         course = self.runtime.modulestore.get_course(self.course_key)
 
         context = {
             'discussion_id': self.discussion_id,
-            'user': user,
+            'user': self.django_user,
             'course': course,
             # used as template condition: must be boolean
-            'can_create_thread': has_permission(user, "create_thread", self.course_key),
+            'can_create_thread': self.has_permission("create_thread"),
             # used as html data: must be JSON
-            'can_create_comment': json.dumps(has_permission(user, "create_comment", self.course_key)),
-            'can_create_subcomment': json.dumps(has_permission(user, "create_subcomment", self.course_key)),
+            'can_create_comment': json.dumps(self.has_permission("create_comment")),
+            'can_create_subcomment': json.dumps(self.has_permission("create_subcomment")),
         }
 
         fragment.add_content(self.runtime.render_template('discussion/_discussion_inline.html', context))
@@ -94,7 +121,7 @@ class DiscussionXBlock(XBlock, StudioEditableXBlockMixin):
 
         return fragment
 
-    def _student_view_studio(self):
+    def student_view_studio(self):
         """ Renders student view for Studio """
         fragment = Fragment()
         fragment.add_content(self.runtime.render_template(
