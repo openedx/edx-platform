1d433f6fbc8f7fa261cfd8e509e7339a73ecf1da
Jacek Bzdak <jacek@opencraft.com>
2016-06-07 16:11:47 +0200

    Added unittests for discussion_xblock.

--- common/lib/xblock/xblock_discussion/xblock_discussion.py
+++ common/lib/xblock/xblock_discussion/xblock_discussion.py
@@ -52,39 +52,52 @@ class DiscussionXBlock(XBlock, StudioEditableXBlockMixin):
         """
         return getattr(self.scope_ids.usage_id, 'course_key', None)
 
+    @property
+    def django_user(self):
+        user_service = self.runtime.service(self, 'user')
+        if not user_service:
+            return None
+        return user_service._django_user  # pylint: disable=protected-access
+
+    def has_permission(self, permission):
+        """
+
+        Encapsulates lms specific functionality, as `has_permission` is not
+        importable outside of lms context, namely in tests.
+
+        :param user:
+        :param str permission: Permission
+        :rtype: bool
+        """
+        # normal import causes the xmodule_assets command to fail due to circular import - hence importing locally
+        from django_comment_client.permissions import has_permission  # pylint: disable=import-error
+
+        return has_permission(self.django_user, permission, self.course_key)
+
     def student_view(self, context=None):  # pylint: disable=unused-argument
         """ Renders student view for LMS and Studio """
         # pylint: disable=no-member
         if hasattr(self, 'xmodule_runtime') and getattr(self.xmodule_runtime, 'is_author_mode', False):
-            fragment = self._student_view_studio()
+            return self._student_view_studio()
         else:
-            fragment = self._student_view_lms()
-
-        return fragment
+            return self._student_view_lms()
 
     def _student_view_lms(self):
         """ Renders student view for LMS """
+
         fragment = Fragment()
-
-        # normal import causes the xmodule_assets command to fail due to circular import - hence importing locally
-        from django_comment_client.permissions import has_permission  # pylint: disable=import-error
-
-        user = None
-        user_service = self.runtime.service(self, 'user')
-        if user_service:
-            user = user_service._django_user  # pylint: disable=protected-access
 
         course = self.runtime.modulestore.get_course(self.course_key)
 
         context = {
             'discussion_id': self.discussion_id,
-            'user': user,
+            'user': self.django_user,
             'course': course,
             # used as template condition: must be boolean
-            'can_create_thread': has_permission(user, "create_thread", self.course_key),
+            'can_create_thread': self.has_permission("create_thread"),
             # used as html data: must be JSON
-            'can_create_comment': json.dumps(has_permission(user, "create_comment", self.course_key)),
-            'can_create_subcomment': json.dumps(has_permission(user, "create_subcomment", self.course_key)),
+            'can_create_comment': json.dumps(self.has_permission("create_comment")),
+            'can_create_subcomment': json.dumps(self.has_permission("create_subcomment")),
         }
 
         fragment.add_content(self.runtime.render_template('discussion/_discussion_inline.html', context))
