6e33ea090ab6e39664a8ec3fdf455d980df9b4fc
Filip Jukić <filip@jukic.me>
2016-06-08 21:21:59 +0200

    show all courses

--- lms/envs/amc.py
+++ lms/envs/amc.py
@@ -7,7 +7,7 @@ APPSEMBLER_FIRST_LOGIN_API = '/logged_into_edx'
 
 # needed to show only users and appsembler courses
 #FEATURES["ENABLE_COURSE_DISCOVERY"] = False
-FEATURES["SHOW_ONLY_APPSEMBLER_AND_OWNED_COURSES"] = True
+FEATURES["SHOW_ONLY_APPSEMBLER_AND_OWNED_COURSES"] = False
 
 EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 MAILGUN_ACCESS_KEY = AUTH_TOKENS.get("MAILGUN_ACCESS_KEY")
