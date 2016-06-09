998c4d8154fded86976a2e032772bcca4fe3ec2b
PaulWattenberger <paul@baadlands.com>
2016-06-08 22:57:31 -0400

    Revert "Pwattenberger/sailthru"

--- lms/djangoapps/email_marketing/admin.py
+++
@@ -1,8 +0,0 @@
-""" Admin site bindings for email marketing """
-
-from django.contrib import admin
-
-from email_marketing.models import EmailMarketingConfiguration
-from config_models.admin import ConfigurationModelAdmin
-
-admin.site.register(EmailMarketingConfiguration, ConfigurationModelAdmin)
