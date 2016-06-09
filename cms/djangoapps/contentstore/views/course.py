edf6f7c7185c5175a8c73af1bb933e54c63de4f1
cahrens <christina@edx.org>
2016-06-09 14:23:50 -0400

    WIP

--- cms/djangoapps/contentstore/views/course.py
+++ cms/djangoapps/contentstore/views/course.py
@@ -99,7 +99,7 @@ from xmodule.modulestore import EdxJSONEncoder
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError, DuplicateCourseError
 from xmodule.tabs import CourseTab, CourseTabList, InvalidTabsException
-
+from xblock_django.api import deprecated_xblocks
 
 log = logging.getLogger(__name__)
 
@@ -599,7 +599,8 @@ def course_index(request, course_key):
         except (ItemNotFoundError, CourseActionStateItemNotFoundError):
             current_action = None
 
-        deprecated_blocks_info = _deprecated_blocks_info(course_module, settings.DEPRECATED_BLOCK_TYPES)
+        deprecated_block_names = [block.name for block in deprecated_xblocks()]
+        deprecated_blocks_info = _deprecated_blocks_info(course_module, deprecated_block_names)
 
         return render_to_response('course_outline.html', {
             'context_course': course_module,
