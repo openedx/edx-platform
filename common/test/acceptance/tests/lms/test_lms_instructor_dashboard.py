b0ede3f6f5b43f3df5d3b1389df623e09be6fa1b
Chris Rodriguez <github@chrisrodriguez.me>
2016-06-29 14:52:09 -0400

    AC-519 - list - test_lms_instructor_dashboard.py

--- common/test/acceptance/tests/lms/test_lms_instructor_dashboard.py
+++ common/test/acceptance/tests/lms/test_lms_instructor_dashboard.py
@@ -96,7 +96,6 @@ class BulkEmailTest(BaseInstructorDashboardTest):
         self.send_email_page.a11y_audit.config.set_rules({
             "ignore": [
                 'button-name',  # TODO: AC-491
-                'list',  # TODO: AC-491,
                 'color-contrast',  # TODO: AC-491
             ]
         })
