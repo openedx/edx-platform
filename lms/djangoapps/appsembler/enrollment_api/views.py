da9793e732c243994b5b1b83ff361213557c1835
Filip Jukić <filip@jukic.me>
2016-07-04 21:23:39 +0200

    add API endpoint to cancel/restore an enrollment code

--- lms/djangoapps/appsembler/enrollment_api/views.py
+++ lms/djangoapps/appsembler/enrollment_api/views.py
@@ -2,7 +2,12 @@ import logging
 
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.http import Http404
 from opaque_keys.edx.keys import CourseKey
+from openedx.core.lib.api.authentication import (
+    OAuth2AuthenticationAllowInactiveUser,
+)
+from openedx.core.lib.api.permissions import IsStaffOrOwner
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,16 +16,12 @@ from course_modes.models import CourseMode
 from courseware.courses import get_course_by_id
 from enrollment.views import EnrollmentCrossDomainSessionAuth
 from instructor.views.api import save_registration_code
-from openedx.core.lib.api.authentication import (
-    OAuth2AuthenticationAllowInactiveUser,
-)
-from openedx.core.lib.api.permissions import IsStaffOrOwner
 from shoppingcart.exceptions import (
     RedemptionCodeError
 )
 from shoppingcart.models import (
-    RegistrationCodeRedemption
-)
+    RegistrationCodeRedemption,
+    CourseRegistrationCode)
 from shoppingcart.views import get_reg_code_validity
 from student.models import CourseEnrollment, EnrollmentClosedError, CourseFullError, \
     AlreadyEnrolledError
@@ -41,7 +42,7 @@ class GenerateRegistrationCodesView(APIView):
         except ValueError:
             course_code_number = int(float(request.data.get('total_registration_codes')))
 
-        course_mode = 'honor'
+        course_mode = 'audit'
 
         registration_codes = []
         for __ in range(course_code_number):
@@ -67,15 +68,19 @@ class EnrollUserWithEnrollmentCodeView(APIView):
         user = User.objects.get(email=request.data.get('email'))
         enrollment_code = request.data.get('enrollment_code')
         limiter = BadRequestRateLimiter()
-        reg_code_is_valid, reg_code_already_redeemed, course_registration = get_reg_code_validity(
-            enrollment_code,
-            request,
-            limiter
-        )
-        course = get_course_by_id(course_registration.course_id, depth=0)
         error_reason = ""
+        try:
+            reg_code_is_valid, reg_code_already_redeemed, course_registration = get_reg_code_validity(
+                enrollment_code,
+                request,
+                limiter
+            )
+        except Http404:
+            reg_code_is_valid = False
+            reg_code_already_redeemed = False
+            error_reason = "Enrollment code not found"
         if reg_code_is_valid and not reg_code_already_redeemed:
-            # now redeem the reg code.
+            course = get_course_by_id(course_registration.course_id, depth=0)
             redemption = RegistrationCodeRedemption.create_invoice_generated_registration_redemption(
                 course_registration,
                 user)
@@ -92,7 +97,7 @@ class EnrollUserWithEnrollmentCodeView(APIView):
                     'success': True,
                 })
             except RedemptionCodeError:
-                error_reason = "Redemption code error"
+                error_reason = "Enrollment code error"
             except EnrollmentClosedError:
                 error_reason = "Enrollment closed"
             except CourseFullError:
@@ -108,51 +113,37 @@ class EnrollUserWithEnrollmentCodeView(APIView):
         )
 
 
-class CancelEnrollmentCodeView(APIView):
+class EnrollmentCodeStatusView(APIView):
     authentication_classes = OAuth2AuthenticationAllowInactiveUser, EnrollmentCrossDomainSessionAuth
     permission_classes = IsStaffOrOwner,
 
     def post(self, request):
-        user = User.objects.get(email=request.data.get('email'))
-        enrollment_code = request.data.get('enrollment_code')
-        limiter = BadRequestRateLimiter()
-        reg_code_is_valid, reg_code_already_redeemed, course_registration = get_reg_code_validity(
-            enrollment_code,
-            request,
-            limiter
-        )
-        course = get_course_by_id(course_registration.course_id, depth=0)
-        error_reason = ""
-        if reg_code_is_valid and not reg_code_already_redeemed:
-            # now redeem the reg code.
-            redemption = RegistrationCodeRedemption.create_invoice_generated_registration_redemption(
-                course_registration,
-                user)
-            try:
-                kwargs = {}
-                if course_registration.mode_slug is not None:
-                    if CourseMode.mode_for_course(course.id, course_registration.mode_slug):
-                        kwargs['mode'] = course_registration.mode_slug
-                    else:
-                        raise RedemptionCodeError()
-                redemption.course_enrollment = CourseEnrollment.enroll(user, course.id, **kwargs)
-                redemption.save()
-                return Response(data={
-                    'success': True,
-                })
-            except RedemptionCodeError:
-                error_reason = "Redemption code error"
-            except EnrollmentClosedError:
-                error_reason = "Enrollment closed"
-            except CourseFullError:
-                error_reason = "Course full"
-            except AlreadyEnrolledError:
-                error_reason = "Already enrolled"
-        return Response(
-            data={
-                'success': False,
-                'reason': error_reason,
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        code = request.data.get('enrollment_code')
+        action = request.data.get('action')
+        try:
+            registration_code = CourseRegistrationCode.objects.get(code=code)
+        except CourseRegistrationCode.DoesNotExist:
+            return Response(
+                data={
+                    'reason': 'The enrollment code ({code}) was not found'.format(code=code),
+                    'success': False},
+                status=400
+            )
 
+        if action == 'cancel':
+            if not RegistrationCodeRedemption.is_registration_code_redeemed(code):
+                registration_code.is_valid = False
+                registration_code.save()
+            else:
+                return Response(
+                    data={
+                        'reason': 'Code already used',
+                        'success': False
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        if action == 'restore':
+            registration_code.is_valid = True
+            registration_code.save()
+        return Response(data={'success': True})
