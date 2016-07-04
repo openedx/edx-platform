e555aa77de4fe76966093de021a3f04d9994a370
Filip Jukić <filip@jukic.me>
2016-07-04 19:49:41 +0200

    remove unnecessary comments

--- lms/djangoapps/appsembler/enrollment_api/views.py
+++ lms/djangoapps/appsembler/enrollment_api/views.py
@@ -34,12 +34,8 @@ class GenerateRegistrationCodesView(APIView):
     permission_classes = IsStaffOrOwner,
 
     def post(self, request):
-        """
-            Respond with csv which contains a summary of all Generated Codes.
-            """
         course_id = CourseKey.from_string(request.data.get('course_id'))
 
-        # covert the course registration code number into integer
         try:
             course_code_number = int(request.data.get('total_registration_codes'))
         except ValueError:
@@ -110,3 +106,53 @@ class EnrollUserWithEnrollmentCodeView(APIView):
             },
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class CancelEnrollmentCodeView(APIView):
+    authentication_classes = OAuth2AuthenticationAllowInactiveUser, EnrollmentCrossDomainSessionAuth
+    permission_classes = IsStaffOrOwner,
+
+    def post(self, request):
+        user = User.objects.get(email=request.data.get('email'))
+        enrollment_code = request.data.get('enrollment_code')
+        limiter = BadRequestRateLimiter()
+        reg_code_is_valid, reg_code_already_redeemed, course_registration = get_reg_code_validity(
+            enrollment_code,
+            request,
+            limiter
+        )
+        course = get_course_by_id(course_registration.course_id, depth=0)
+        error_reason = ""
+        if reg_code_is_valid and not reg_code_already_redeemed:
+            # now redeem the reg code.
+            redemption = RegistrationCodeRedemption.create_invoice_generated_registration_redemption(
+                course_registration,
+                user)
+            try:
+                kwargs = {}
+                if course_registration.mode_slug is not None:
+                    if CourseMode.mode_for_course(course.id, course_registration.mode_slug):
+                        kwargs['mode'] = course_registration.mode_slug
+                    else:
+                        raise RedemptionCodeError()
+                redemption.course_enrollment = CourseEnrollment.enroll(user, course.id, **kwargs)
+                redemption.save()
+                return Response(data={
+                    'success': True,
+                })
+            except RedemptionCodeError:
+                error_reason = "Redemption code error"
+            except EnrollmentClosedError:
+                error_reason = "Enrollment closed"
+            except CourseFullError:
+                error_reason = "Course full"
+            except AlreadyEnrolledError:
+                error_reason = "Already enrolled"
+        return Response(
+            data={
+                'success': False,
+                'reason': error_reason,
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
