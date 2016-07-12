3647c17911a5cec77711ad4962fe1b6c2741466c
Jesse Zoldak <zoldak@edx.org>
2016-07-12 16:43:50 -0400

    Make the temp dir for instructor dashboard local files uniquely named. EV-56

--- lms/envs/test.py
+++ lms/envs/test.py
@@ -70,10 +70,6 @@ FEATURES['ENABLE_VERIFIED_CERTIFICATES'] = True
 FEATURES['ENABLE_GRADE_DOWNLOADS'] = True
 FEATURES['ALLOW_COURSE_STAFF_GRADE_DOWNLOADS'] = True
 
-GRADES_DOWNLOAD['ROOT_PATH'] += "-{}".format(os.getpid())
-FINANCIAL_REPORTS['ROOT_PATH'] += "-{}".format(os.getpid())
-
-
 # Toggles embargo on for testing
 FEATURES['EMBARGO'] = True
 
