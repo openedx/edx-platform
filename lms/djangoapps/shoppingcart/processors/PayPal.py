a44b1c7fb160e8e3a29c9e6c85d64cd540dfab4b
Rene Sorel <rene@proversity.org>
2016-08-19 12:02:53 +0200

    adding PayPal payment processor to avoid init error

---
+++ lms/djangoapps/shoppingcart/processors/PayPal.py
@@ -0,0 +1,18 @@
+"""
+Implementation of the PayPal processor
+"""
+
+def render_purchase_form_html(cart, **kwargs):
+    return ''
+
+
+def process_postpay_callback(params, **kwargs):
+    return {}
+
+
+def get_purchase_endpoint():
+    return ''
+
+
+def get_signed_purchase_params(cart, **kwargs):
+    return {}
