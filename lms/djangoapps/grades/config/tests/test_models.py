660bc8f447d03bdb562dce388d178e9f144961b9
Nimisha Asthagiri <nasthagiri@edx.org>
2016-09-09 04:32:04 -0400

    Enable Persistent Grades in unit tests

--- lms/djangoapps/grades/config/tests/test_models.py
+++ lms/djangoapps/grades/config/tests/test_models.py
@@ -3,6 +3,9 @@ Tests for the models that control the
 persistent grading feature.
 """
 import ddt
+from django.conf import settings
+import itertools
+from mock import patch
 
 from django.test import TestCase
 from opaque_keys.edx.locator import CourseLocator
@@ -10,6 +13,7 @@ from lms.djangoapps.grades.config.models import PersistentGradesEnabledFlag
 from lms.djangoapps.grades.config.tests.utils import persistent_grades_feature_flags
 
 
+@patch.dict(settings.FEATURES, {'PERSISTENT_GRADES_ENABLED_FOR_ALL_TESTS': False})
 @ddt.ddt
 class PersistentGradesFeatureFlagTests(TestCase):
     """
@@ -21,16 +25,11 @@ class PersistentGradesFeatureFlagTests(TestCase):
         self.course_id_1 = CourseLocator(org="edx", course="course", run="run")
         self.course_id_2 = CourseLocator(org="edx", course="course2", run="run")
 
-    @ddt.data(
-        (True, True, True),
-        (True, True, False),
-        (True, False, True),
-        (True, False, False),
-        (False, True, True),
-        (False, False, True),
-        (False, True, False),
-        (False, False, False),
-    )
+    @ddt.data(*itertools.product(
+        (True, False),
+        (True, False),
+        (True, False),
+    ))
     @ddt.unpack
     def test_persistent_grades_feature_flags(self, global_flag, enabled_for_all_courses, enabled_for_course_1):
         with persistent_grades_feature_flags(
