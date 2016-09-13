13f3a8b02505bb593696c4b0b6c8289ef6d6cfeb
Elena Komendantova <lenacom@rambler.ru>
2016-09-13 11:16:09 +0000

    fixed check

--- common/djangoapps/student/tests/test_login.py
+++ common/djangoapps/student/tests/test_login.py
@@ -89,7 +89,9 @@ class LoginTest(CacheIsolationTestCase):
 
     @patch.dict('django.conf.settings.FEATURES', {'ENABLE_LOGIN_BY_EMAIL_OR_USERNAME': True})
     def test_login_success_by_username(self):
-        response, mock_audit_log = self._login_response('test', 'test_password', patched_audit_log='student.models.AUDIT_LOG')
+        response, mock_audit_log = \
+            self._login_response('test', 'test_password',
+                                 patched_audit_log='student.models.AUDIT_LOG')
         self._assert_response(response, success=True)
         self._assert_audit_log(mock_audit_log, 'info', [u'Login success', u'test@edx.org'])
 
