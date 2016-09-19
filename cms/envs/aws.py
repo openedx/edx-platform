60b53ebde5690eb3b46e36a275e1a943ad93b0df
Rene Sorel <rene@proversity.org>
2016-09-19 15:11:30 +0200

    add problem-builder to installed apps

--- cms/envs/aws.py
+++ cms/envs/aws.py
@@ -459,3 +459,6 @@ AFFILIATE_COOKIE_NAME = ENV_TOKENS.get('AFFILIATE_COOKIE_NAME', AFFILIATE_COOKIE
 ############## Settings for Studio Context Sensitive Help ##############
 
 DOC_LINK_BASE_URL = ENV_TOKENS.get('DOC_LINK_BASE_URL', DOC_LINK_BASE_URL)
+
+############### External XBlocks ##########
+INSTALLED_APPS += ('problem_builder',)
