12ccdb804e62d220c655ef01387c6b0bfd14489c
stu <stucontact@gmail.com>
2016-09-22 17:58:08 -0400

    change jenkins-report to use codecov

--- scripts/jenkins-report.sh
+++ scripts/jenkins-report.sh
@@ -2,16 +2,25 @@
 
 # This script is used by the edx-platform-unit-coverage jenkins job.
 
+if [ $# -eq 1 ]; then
+    COMMIT=$1
+else
+    echo "Incorrect number of arguments passed to this script!"
+    echo "Please supply a git hash to specify the commit being tested"
+    exit 1
+fi
+
 source scripts/jenkins-common.sh
 
 # Get the diff coverage and html reports for unit tests
 paver coverage
 
-# Send the coverage data to coveralls. Setting 'TRAVIS_BRANCH' allows the
-# data to be sorted by branch in the coveralls UI. The branch is passed as
-# a param to the coverage job on jenkins.
-pip install coveralls==1.0
-COVERALLS_REPO_TOKEN=$1 TRAVIS_BRANCH=$2 coveralls
+# Send the coverage data to codecov. Setting the 'COMMMIT' allows the data
+# to be sorted by a branch in the codecov UI. The branch is passed as a
+# param to the coverage job on Jenkins. The 'CODE_COV_TOKEN' should be
+# available as an environment variable.
+pip install codecov==2.0.5
+codecov --token=$CODE_COV_TOKEN --commit=$COMMIT
 
 # Get coverage reports for bok choy
 # paver bokchoy_coverage
