6704e17afdfc235c47e166c377ff27fb65aee922
Toby Lawrence <tlawrence@edx.org>
2016-09-22 14:21:52 -0400

    [TNL-5632] Optimize the single_thread view.
    Firstly, we're now explicitly instructing the comments service to not
    return thread responses/comments if the request isn't AJAX.  So, if you
    load the URL for a single discussion thread in your browser, this would
    be a non-AJAX call and we'll avoid loading the responses for the entire
    thread behind-the-scenes.  Big win here for large threads.
    
    Next, we removed a redundant "get threads" call which was also happening
    behind-the-scenes.  This call was redundant as the front-end JS also
    grabs the thread list when a topic is chosen, so we were making an
    extranenous call for no benefit.  Poof, gone!
    
    Finally, we added some caching of database queries that are required to
    drive a lot of the permissions/cohorts machinery around discussion.
    This will have a minimal effect but introduced a cleaner way to apply
    general memoization at the per-request level which will let us further
    cache things as we identify them as issues.

--- lms/djangoapps/django_comment_client/permissions.py
+++ lms/djangoapps/django_comment_client/permissions.py
@@ -5,7 +5,7 @@ Module for checking permissions with the comment_client backend
 import logging
 from types import NoneType
 
-from request_cache.middleware import RequestCache
+from request_cache.middleware import RequestCache, request_cached
 from lms.lib.comment_client import Thread
 from opaque_keys.edx.keys import CourseKey
 
@@ -31,19 +31,14 @@ def has_permission(user, permission, course_id=None):
 CONDITIONS = ['is_open', 'is_author', 'is_question_author', 'is_team_member_if_applicable']
 
 
+@request_cached
 def get_team(commentable_id):
     """ Returns the team that the commentable_id belongs to if it exists. Returns None otherwise. """
-    request_cache_dict = RequestCache.get_request_cache().data
-    cache_key = "django_comment_client.team_commentable.{}".format(commentable_id)
-    if cache_key in request_cache_dict:
-        return request_cache_dict[cache_key]
-
     try:
         team = CourseTeam.objects.get(discussion_topic_id=commentable_id)
     except CourseTeam.DoesNotExist:
         team = None
 
-    request_cache_dict[cache_key] = team
     return team
 
 
