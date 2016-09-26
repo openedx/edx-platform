10f6bd1703e71e0cff156d432730923ffb83a554
Awais Jibran <awaisdar001@gmail.com>
2016-09-26 15:01:09 +0500

    Celery need to serialize the arguments for a task. Here, user is not serializable,replace it with ID. ECOM-5784

--- common/djangoapps/student/tests/test_tasks.py
+++ common/djangoapps/student/tests/test_tasks.py
@@ -9,29 +9,36 @@ from django.conf import settings
 from student.tasks import send_activation_email
 from boto.exception import NoAuthHandlerFound
 
+from lms.djangoapps.courseware.tests.factories import UserFactory
+
 
 class SendActivationEmailTestCase(TestCase):
     """
     Test for send activation email to user
     """
+    def setUp(self):
+        """ Setup components used by each test."""
+        super(SendActivationEmailTestCase, self).setUp()
+        self.student = UserFactory()
+
     @mock.patch('time.sleep', mock.Mock(return_value=None))
     @mock.patch('student.tasks.log')
-    @mock.patch('django.contrib.auth.models.User')
-    def test_send_email(self, mock_user, mock_log):
+    @mock.patch('django.contrib.auth.models.User.email_user', mock.Mock(side_effect=NoAuthHandlerFound))
+    def test_send_email(self, mock_log):
         """
         Tests retries when the activation email doesn't send
         """
         from_address = 'task_testing@edX.com'
-        mock_user.email_user.side_effect = NoAuthHandlerFound
         email_max_attempts = settings.RETRY_ACTIVATION_EMAIL_MAX_ATTEMPTS + 1
 
-        send_activation_email.delay(mock_user, 'Task_test', 'Task_test_message', from_address)
+        # pylint: disable=no-member
+        send_activation_email.delay(self.student.id, 'Task_test', 'Task_test_message', from_address)
 
         # Asserts sending email retry logging.
         for attempt in xrange(1, email_max_attempts):
             mock_log.info.assert_any_call(
                 'Retrying sending email to user {dest_addr}, attempt # {attempt} of {max_attempts}'.format(
-                    dest_addr=mock_user.email,
+                    dest_addr=self.student.email,
                     attempt=attempt,
                     max_attempts=email_max_attempts
                 ))
@@ -41,7 +48,7 @@ class SendActivationEmailTestCase(TestCase):
         mock_log.error.assert_called_with(
             'Unable to send activation email to user from "%s" to "%s"',
             from_address,
-            mock_user.email,
+            self.student.email,
             exc_info=True
         )
         self.assertEquals(mock_log.error.call_count, 1)
