eddfba6405da335568642e0c5eb9a65d52272d1c
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2016-09-27 15:36:43 +0500

    Adding response id for sr question title.

--- common/lib/capa/capa/responsetypes.py
+++ common/lib/capa/capa/responsetypes.py
@@ -264,8 +264,9 @@ class LoncapaResponse(object):
         tree.set('tabindex', '-1')
         # tree.set('aria-labelledby', '{id}-problem-title question-title'.format(id=self.xml.get('id')))
 
+        # question title for screen readers
         section_heading = etree.SubElement(tree, 'h4')
-        section_heading.set('id', 'question-title')
+        section_heading.set('id', '{id}-question-title'.format(id=self.xml.get('id')))
         section_heading.set('class', 'sr')
         section_heading.set('value', response_label)
 
