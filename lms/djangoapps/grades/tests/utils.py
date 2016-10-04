62042188f544d6f4b57cbf079f42ad5e580ce6b3
J. Cliff Dyer <cdyer@edx.org>
2016-10-04 17:19:30 -0400

    Revert "Fix Gating to use grades API, instead of its own calculations"
    This reverts commit 318121411a04e1fe4465b921cae06344ade1261f.

--- lms/djangoapps/grades/tests/utils.py
+++ lms/djangoapps/grades/tests/utils.py
@@ -3,8 +3,6 @@ Utilities for grades related tests
 """
 from contextlib import contextmanager
 from mock import patch
-from courseware.module_render import get_module
-from courseware.model_data import FieldDataCache
 from xmodule.graders import ProblemScore
 
 
@@ -26,32 +24,3 @@ def mock_get_score(earned=0, possible=1):
     with patch('lms.djangoapps.grades.new.subsection_grade.get_score') as mock_score:
         mock_score.return_value = ProblemScore(earned, possible, earned, possible, 1, True, None, None)
         yield mock_score
-
-
-def answer_problem(course, request, problem, score=1, max_value=1):
-    """
-    Records an answer for the given problem.
-
-    Arguments:
-        course (Course): Course object, the course the required problem is in
-        request (Request): request Object
-        problem (xblock): xblock object, the problem to be answered
-        score (float): The new score for the problem
-        max_value (float): The new maximum score for the problem
-    """
-
-    user = request.user
-    grade_dict = {'value': score, 'max_value': max_value, 'user_id': user.id}
-    field_data_cache = FieldDataCache.cache_for_descriptor_descendents(
-        course.id,
-        user,
-        course,
-        depth=2
-    )
-    module = get_module(
-        user,
-        request,
-        problem.location,
-        field_data_cache,
-    )
-    module.runtime.publish(problem, 'grade', grade_dict)
