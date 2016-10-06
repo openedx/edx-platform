f3b6c14ece4c2d424f3927f8e18e10b82fcc3a52
Nimisha Asthagiri <nasthagiri@edx.org>
2016-10-05 20:14:48 -0400

    Add support for custom documentation base URLs
    DOC-3315

--- common/djangoapps/util/help_context_processor.py
+++ common/djangoapps/util/help_context_processor.py
@@ -67,7 +67,7 @@ def common_doc_url(request, config_file_object):  # pylint: disable=unused-argum
             # Read an optional configuration property that sets the base
             # URL of documentation links. By default, DOC_LINK_BASE_URL
             # is null, this test determines whether it is set to a non-null
-            # value. If it is set, this funtion will use its string value
+            # value. If it is set, this function will use its string value
             # as the base of documentation link URLs. If it is not set, the
             # function reads the base of the documentation link URLs from
             # the .ini configuration file, lms_config.ini or cms_config.ini.
