53a6edd0329699f5ccb3c0330c1a49f194a2755d
cahrens <christina@edx.org>
2016-10-06 16:00:19 -0400

    Revert "single form element per capa problem"
    This reverts commit 943a3a5eb1e03ee3f29e244fe6515bdc2af951d0.

--- common/lib/xmodule/xmodule/tests/test_capa_module.py
+++ common/lib/xmodule/xmodule/tests/test_capa_module.py
@@ -2600,7 +2600,6 @@ class TestProblemCheckTracking(unittest.TestCase):
                 'response_type': 'optionresponse',
                 'input_type': 'optioninput',
                 'correct': True,
-                'group_label': '',
                 'variant': '',
             },
             factory.answer_key(3): {
@@ -2609,7 +2608,6 @@ class TestProblemCheckTracking(unittest.TestCase):
                 'response_type': 'multiplechoiceresponse',
                 'input_type': 'choicegroup',
                 'correct': False,
-                'group_label': '',
                 'variant': '',
             },
             factory.answer_key(4): {
@@ -2618,7 +2616,6 @@ class TestProblemCheckTracking(unittest.TestCase):
                 'response_type': 'choiceresponse',
                 'input_type': 'checkboxgroup',
                 'correct': False,
-                'group_label': '',
                 'variant': '',
             },
         })
@@ -2659,7 +2656,6 @@ class TestProblemCheckTracking(unittest.TestCase):
                 'response_type': 'numericalresponse',
                 'input_type': 'textline',
                 'correct': True,
-                'group_label': '',
                 'variant': '',
             }
         })
@@ -2791,7 +2787,6 @@ class TestProblemCheckTracking(unittest.TestCase):
                 'response_type': 'numericalresponse',
                 'input_type': 'textline',
                 'correct': True,
-                'group_label': '',
                 'variant': module.seed,
             }
         })
@@ -2824,13 +2819,11 @@ class TestProblemCheckTracking(unittest.TestCase):
                 'response_type': 'coderesponse',
                 'input_type': 'filesubmission',
                 'correct': False,
-                'group_label': '',
                 'variant': '',
             },
             factory.answer_key(3): {
                 'answer': 'None',
                 'correct': True,
-                'group_label': '',
                 'question': '',
                 'response_type': 'customresponse',
                 'input_type': 'textline',
