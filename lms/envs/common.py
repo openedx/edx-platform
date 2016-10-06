27e4029f8dc68205fad9852adfb8553ad08297c7
Sanford Student <sanfordstudent@C02QJ0RCG8WM.tld>
2016-10-06 13:31:11 -0400

    common.py

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1906,7 +1906,7 @@ INSTALLED_APPS = (
     'util',
     'certificates',
     'dashboard',
-    'instructor',
+    'lms.djangoapps.instructor',
     'instructor_task',
     'openedx.core.djangoapps.course_groups',
     'bulk_email',
