15620fb1fbb8e0f4baab3d86c4decaa2cd4f3dae
Jesse Shapiro <jesse@opencraft.com>
2016-10-10 09:20:29 -0400

    Test improvements

--- common/djangoapps/third_party_auth/tests/specs/test_testshib.py
+++ common/djangoapps/third_party_auth/tests/specs/test_testshib.py
@@ -103,6 +103,48 @@ class TestShibIntegrationTest(IntegrationTestMixin, testutil.SAMLTestCase):
         self._configure_testshib_provider(require_data_sharing_consent=True)
         super(TestShibIntegrationTest, self).test_register(data_sharing_consent=True)
 
+    def test_registration_form_has_data_sharing_consent_when_optional(self):
+        """
+        Configure TestShib to request data sharing consent, and then check
+        the form to make sure that the option to enable it is present.
+        """
+        self._configure_testshib_provider(request_data_sharing_consent=True)
+        # The user goes to the register page, and sees a button to register with the provider:
+        provider_register_url = self._check_register_page()
+        # The user clicks on the Dummy button:
+        try_login_response = self.client.get(provider_register_url)
+        # The user should be redirected to the provider's login page:
+        self.assertEqual(try_login_response.status_code, 302)
+        provider_response = self.do_provider_login(try_login_response['Location'])
+        # We should be redirected to the register screen since this account is not linked to an edX account:
+        self.assertEqual(provider_response.status_code, 302)
+        self.assertEqual(provider_response['Location'], self.url_prefix + self.register_page_url)
+        register_response = self.client.get(self.register_page_url)
+        tpa_context = register_response.context["data"]["third_party_auth"]  # pylint: disable=no-member
+        self.assertEqual(tpa_context["errorMessage"], None)
+        # Check that the "You've successfully signed into [PROVIDER_NAME]" message is shown.
+        self.assertEqual(tpa_context["currentProvider"], self.PROVIDER_NAME)
+        # Check that the data (e.g. email) from the provider is displayed in the form:
+        form_data = register_response.context['data']['registration_form_desc']  # pylint: disable=no-member
+        form_fields = {field['name']: field for field in form_data['fields']}
+        # Verify that the data sharing consent checkbox is present
+        self.assertIn('data_sharing_consent', form_fields)
+        # Verify that the data sharing consent checkbox is not required
+        self.assertFalse(form_fields['data_sharing_consent']['required'])
+        registration_values = {
+            'email': 'email-edited@tpa-test.none',
+            'name': 'My Customized Name',
+            'username': 'new_username',
+            'honor_code': True,
+            'data_sharing_consent': False
+        }
+        # Now complete the form:
+        ajax_register_response = self.client.post(
+            reverse('user_api_registration'),
+            registration_values
+        )
+        self.assertEqual(ajax_register_response.status_code, 200)
+
     def test_registration_not_allowed_without_data_sharing_consent(self):
         """
         Configure TestShib to require data sharing consent, but don't provide
