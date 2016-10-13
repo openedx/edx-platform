64b9f085c219c3beb887869af7415ba7843649fe
Eric Fischer <efischer@edx.org>
2016-10-13 15:30:25 -0400

    NOMERGE, tryna fail on jenkins

--- common/test/acceptance/tests/lms/test_lms_instructor_dashboard.py
+++ common/test/acceptance/tests/lms/test_lms_instructor_dashboard.py
@@ -5,8 +5,9 @@ End-to-end tests for the LMS Instructor Dashboard.
 
 import ddt
 
-from nose.plugins.attrib import attr
 from bok_choy.promise import EmptyPromise
+from flaky import flaky
+from nose.plugins.attrib import attr
 
 from common.test.acceptance.tests.helpers import UniqueCourseTest, get_modal_alert, EventsTestMixin
 from common.test.acceptance.pages.common.logout import LogoutPage
@@ -377,6 +378,7 @@ class ProctoredExamsTest(BaseInstructorDashboardTest):
         # Then, the added record should be visible
         self.assertTrue(allowance_section.is_allowance_record_visible)
 
+    @flaky(max_runs=20, min_passes=20)
     def test_can_reset_attempts(self):
         """
         Make sure that Exam attempts are visible and can be reset.
