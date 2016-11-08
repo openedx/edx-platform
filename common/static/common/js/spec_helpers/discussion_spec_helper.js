7806b8a4827a819ca934bc63799b7c1b396ad972
Brian Jacobel <bjacobel@gmail.com>
2016-11-08 16:11:29 -0500

    Out-of-band cleanup on this test fixture because it horrified me so much

--- common/static/common/js/spec_helpers/discussion_spec_helper.js
+++ common/static/common/js/spec_helpers/discussion_spec_helper.js
@@ -113,10 +113,113 @@
                 }).text(templateFixture));
             }
 
-            // suppressing Line is too long (4272 characters!)
-            /* jshint -W101 */
             appendSetFixtures(
-                "<script type=\'text/template\' id=\'thread-list-template\'>\n    <div class=\'forum-nav-header\'>\n        <button type=\'button\' class=\'forum-nav-browse\' id=\'forum-nav-browse\' aria-haspopup=\'true\'>\n            <span class=\'icon fa fa-bars\' aria-hidden=\'true\'></span>\n            <span class=\'sr\'>Discussion topics; currently listing: </span>\n            <span class=\'forum-nav-browse-current\'>All Discussions</span>\n            ▾\n        </button>\n        <form class=\'forum-nav-search\'>\n            <label>\n                <span class=\'sr\'>Search all posts</span>\n                <input class=\'forum-nav-search-input\' id=\'forum-nav-search\' type=\'text\' placeholder=\'Search all posts\'>\n                <span class=\'icon fa fa-search\' aria-hidden=\'true\'></span>\n            </label>\n        </form>\n    </div>\n    <div class=\'forum-nav-browse-menu-wrapper\' style=\'display: none\'>\n        <form class=\'forum-nav-browse-filter\'>\n            <label>\n                <span class=\'sr\'>Filter Topics</span>\n                <input type=\'text\' class=\'forum-nav-browse-filter-input\' placeholder=\'filter topics\'>\n            </label>\n        </form>\n        <ul class=\'forum-nav-browse-menu\'>\n            <li class=\'forum-nav-browse-menu-item forum-nav-browse-menu-all\'>\n                <a href=\'#\' class=\'forum-nav-browse-title\'>All Discussions</a>\n            </li>\n            <li class=\'forum-nav-browse-menu-item forum-nav-browse-menu-following\'>\n                <a href=\'#\' class=\'forum-nav-browse-title\'><span class=\'icon fa fa-star\' aria-hidden=\'true\'></span>Posts I'm Following</a>\n            </li>\n            <li class=\'forum-nav-browse-menu-item\'>\n                <a href=\'#\' class=\'forum-nav-browse-title\'>Parent</a>\n                <ul class=\'forum-nav-browse-submenu\'>\n                    <li class=\'forum-nav-browse-menu-item\'>\n                        <a href=\'#\' class=\'forum-nav-browse-title\'>Target</a>\n                        <ul class=\'forum-nav-browse-submenu\'>\n                            <li\n                                class=\'forum-nav-browse-menu-item\'\n                                data-discussion-id=\'child\'\n                                data-cohorted=\'false\'\n                            >\n                                <a href=\'#\' class=\'forum-nav-browse-title\'>Child</a>\n                            </li>\n                        </ul>\n                    <li\n                        class=\'forum-nav-browse-menu-item\'\n                        data-discussion-id=\'sibling\'\n                        data-cohorted=\'false\'\n                    >\n                        <a href=\'#\' class=\'forum-nav-browse-title\'>Sibling</a>\n                    </li>\n                </ul>\n            </li>\n            <li\n                class=\'forum-nav-browse-menu-item\'\n                data-discussion-id=\'other\'\n                data-cohorted=\'true\'\n            >\n                <a href=\'#\' class=\'forum-nav-browse-title\'>Other Category</a>\n            </li>\n        </ul>\n    </div>\n    <div class=\'forum-nav-thread-list-wrapper\' id=\'sort-filter-wrapper\' tabindex=\'-1\'>\n        <div class=\'forum-nav-refine-bar\'>\n            <label class=\'forum-nav-filter-main\'>\n                <select class=\'forum-nav-filter-main-control\'>\n                    <option value=\'all\'>Show all</option>\n                    <option value=\'unread\'>Unread</option>\n                    <option value=\'unanswered\'>Unanswered</option>\n                    <option value=\'flagged\'>Flagged</option>\n                </select>\n            </label>\n            <% if (isCohorted && isPrivilegedUser) { %>\n            <label class=\'forum-nav-filter-cohort\'>\n                <span class=\'sr\'>Cohort:</span>\n                <select class=\'forum-nav-filter-cohort-control\'>\n                    <option value=\'\'>in all cohorts</option>\n                    <option value=\'1\'>Cohort1</option>\n                    <option value=\'2\'>Cohort2</option>\n                </select>\n            </label>\n            <% } %>\n            <label class=\'forum-nav-sort\'>\n                <select class=\'forum-nav-sort-control\'>\n                    <option value=\'activity\'>by recent activity</option>\n                    <option value=\'comments\'>by most activity</option>\n                    <option value=\'votes\'>by most votes</option>\n                </select>\n            </label>\n        </div>\n    </div>\n    <div class=\'search-alerts\'></div>\n    <ul class=\'forum-nav-thread-list\'></ul>\n</script>");  // eslint-disable-line max-len, no-useless-escape
+                '<script type="text/template" id="thread-list-template">' +
+                '    <div class="forum-nav-header">' +
+                '        <button type="button" class="forum-nav-browse" id="forum-nav-browse" aria-haspopup="true">' +
+                '            <span class="icon fa fa-bars" aria-hidden="true"></span>' +
+                '            <span class="sr">Discussion topics; currently listing: </span>' +
+                '            <span class="forum-nav-browse-current">All Discussions</span>' +
+                '        </button>' +
+                '        <form class="forum-nav-search">' +
+                '            <label>' +
+                '                <span class="sr">Search all posts</span>' +
+                '                <input' +
+                '                    class="forum-nav-search-input"' +
+                '                    id="forum-nav-search"' +
+                '                    type="text"' +
+                '                    placeholder="Search all posts"' +
+                '                >' +
+                '                <span class="icon fa fa-search" aria-hidden="true"></span>' +
+                '            </label>' +
+                '        </form>' +
+                '    </div>' +
+                '    <div class="forum-nav-browse-menu-wrapper" style="display: none">' +
+                '        <form class="forum-nav-browse-filter">' +
+                '            <label>' +
+                '                <span class="sr">Filter Topics</span>' +
+                '                <input' +
+                '                    type="text"' +
+                '                    class="forum-nav-browse-filter-input"' +
+                '                    placeholder="filter topics"' +
+                '                >' +
+                '            </label>' +
+                '        </form>' +
+                '        <ul class="forum-nav-browse-menu">' +
+                '            <li class="forum-nav-browse-menu-item forum-nav-browse-menu-all">' +
+                '                <a href="#" class="forum-nav-browse-title">All Discussions</a>' +
+                '            </li>' +
+                '            <li class="forum-nav-browse-menu-item forum-nav-browse-menu-following">' +
+                '                <a href="#" class="forum-nav-browse-title">' +
+                '                    <span class="icon fa fa-star" aria-hidden="true"></span>' +
+                '                    Posts I\'m Following' +
+                '                </a>' +
+                '            </li>' +
+                '            <li class="forum-nav-browse-menu-item">' +
+                '                <a href="#" class="forum-nav-browse-title">Parent</a>' +
+                '                <ul class="forum-nav-browse-submenu">' +
+                '                    <li class="forum-nav-browse-menu-item">' +
+                '                        <a href="#" class="forum-nav-browse-title">Target</a>' +
+                '                        <ul class="forum-nav-browse-submenu">' +
+                '                            <li' +
+                '                                class="forum-nav-browse-menu-item"' +
+                '                                data-discussion-id="child"' +
+                '                                data-cohorted="false"' +
+                '                            >' +
+                '                                <a href="#" class="forum-nav-browse-title">Child</a>' +
+                '                            </li>' +
+                '                        </ul>' +
+                '                    <li' +
+                '                        class="forum-nav-browse-menu-item"' +
+                '                        data-discussion-id="sibling"' +
+                '                        data-cohorted="false"' +
+                '                    >' +
+                '                        <a href="#" class="forum-nav-browse-title">Sibling</a>' +
+                '                    </li>' +
+                '                </ul>' +
+                '            </li>' +
+                '            <li' +
+                '                class="forum-nav-browse-menu-item"' +
+                '                data-discussion-id="other"' +
+                '                data-cohorted="true"' +
+                '            >' +
+                '                <a href="#" class="forum-nav-browse-title">Other Category</a>' +
+                '            </li>' +
+                '        </ul>' +
+                '    </div>' +
+                '    <div class="forum-nav-thread-list-wrapper" id="sort-filter-wrapper" tabindex="-1">' +
+                '        <div class="forum-nav-refine-bar">' +
+                '            <label class="forum-nav-filter-main">' +
+                '                <select class="forum-nav-filter-main-control">' +
+                '                    <option value="all">Show all</option>' +
+                '                    <option value="unread">Unread</option>' +
+                '                    <option value="unanswered">Unanswered</option>' +
+                '                    <option value="flagged">Flagged</option>' +
+                '                </select>' +
+                '            </label>' +
+                '            <% if (isCohorted && isPrivilegedUser) { %>' +
+                '            <label class="forum-nav-filter-cohort">' +
+                '                <span class="sr">Cohort:</span>' +
+                '                <select class="forum-nav-filter-cohort-control">' +
+                '                    <option value="">in all cohorts</option>' +
+                '                    <option value="1">Cohort1</option>' +
+                '                    <option value="2">Cohort2</option>' +
+                '                </select>' +
+                '            </label>' +
+                '            <% } %>' +
+                '            <label class="forum-nav-sort">' +
+                '                <select class="forum-nav-sort-control">' +
+                '                    <option value="activity">by recent activity</option>' +
+                '                    <option value="comments">by most activity</option>' +
+                '                    <option value="votes">by most votes</option>' +
+                '                </select>' +
+                '            </label>' +
+                '        </div>' +
+                '    </div>' +
+                '    <div class="search-alerts"></div>' +
+                '    <ul class="forum-nav-thread-list"></ul>' +
+                '</script>'
+            );
 
             appendSetFixtures(
                 '<div id=\'fixture-element\'></div>\n' +
