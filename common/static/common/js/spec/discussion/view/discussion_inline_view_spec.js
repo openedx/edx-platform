5e693fe7b73b12a2df0265ee1bf70bb4685baf0f
Brian Jacobel <bjacobel@gmail.com>
2016-11-18 11:17:34 -0500

    Stub for jasmine tests for DiscussionInlineView

---
+++ common/static/common/js/spec/discussion/view/discussion_inline_view_spec.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+    describe('DiscussionInlineView', function() {
+        describe('open/close behavior', function() {
+
+        });
+
+        describe('new post form', function() {
+
+        });
+
+        describe('thread listing', function() {
+            it('builds a view that has the right number of threads', function() {
+
+            });
+
+            it('calls DiscussionThreadListView internally', function() {
+
+            });
+        });
+
+        describe('thread post drilldown', function() {
+
+        });
+    });
+}());
