afc598ddabef64f123da19d0d3c608450daec85b
Andy Armstrong <andya@edx.org>
2016-11-21 15:37:30 -0500

    Add more inline discussion tests

--- common/static/common/js/spec/discussion/view/discussion_inline_view_spec.js
+++ common/static/common/js/spec/discussion/view/discussion_inline_view_spec.js
@@ -5,7 +5,8 @@
 (function() {
     'use strict';
     describe('DiscussionInlineView', function() {
-        var createTestView, setNextAjaxResult;
+        var createTestView, showDiscussion, setNextAjaxResult,
+            TEST_THREAD_TITLE = 'Test thread title';
 
         beforeEach(function() {
             DiscussionSpecHelper.setUpGlobals();
@@ -26,15 +27,8 @@
                 '</div>'
             );
             DiscussionSpecHelper.setUnderscoreFixtures();
-            this.courseSettings = DiscussionSpecHelper.createTestCourseSettings({
-
-            });
-            this.threadData = DiscussionViewSpecHelper.makeThreadWithProps({
-                commentable_id: 'test-topic',
-                title: 'Test thread title'
-            });
             this.ajaxSpy = spyOn($, 'ajax');
-            spyOn(DiscussionUtil, 'makeWmdEditor');
+            spyOn(DiscussionUtil, 'makeWmdEditor');  // Don't attempt to render the markdown editor
         });
 
         createTestView = function() {
@@ -43,6 +37,21 @@
             });
             testView.render();
             return testView;
+        };
+
+        showDiscussion = function(test, testView) {
+            setNextAjaxResult(test, {
+                user_info: DiscussionSpecHelper.getTestUserInfo(),
+                roles: DiscussionSpecHelper.getTestRoleInfo(),
+                course_settings: DiscussionSpecHelper.createTestCourseSettings().attributes,
+                discussion_data: DiscussionViewSpecHelper.makeThreadWithProps({
+                    commentable_id: 'test-topic',
+                    title: TEST_THREAD_TITLE
+                }),
+                page: 1,
+                num_pages: 1
+            });
+            testView.$('.discussion-show').click();
         };
 
         setNextAjaxResult = function(test, result) {
@@ -54,41 +63,74 @@
             });
         };
 
-        describe('open/close behavior', function() {
+        describe('inline discussion', function() {
+            it('is shown after "Show Discussion" is clicked', function() {
+                var testView = createTestView(this),
+                    showButton = testView.$('.discussion-show');
+                showDiscussion(this, testView);
 
+                // Verify that the discussion is now shown
+                expect(showButton).toHaveClass('shown');
+                expect(showButton.text().trim()).toEqual('Hide Discussion');
+                expect(testView.$('.inline-discussion:visible')).not.toHaveClass('is-hidden');
+            });
+
+            it('is hidden after "Hide Discussion" is clicked', function() {
+                var testView = createTestView(this),
+                    showButton = testView.$('.discussion-show');
+                showDiscussion(this, testView);
+
+                // Hide the discussion by clicking the toggle button again
+                testView.$('.discussion-show').click();
+
+                // Verify that the discussion is now hidden
+                expect(showButton).not.toHaveClass('shown');
+                expect(showButton.text().trim()).toEqual('Show Discussion');
+                expect(testView.$('.inline-discussion:visible')).toHaveClass('is-hidden');
+            });
         });
 
         describe('new post form', function() {
+            it('should not be visible when the discussion is first shown', function() {
+                var testView = createTestView(this);
+                showDiscussion(this, testView);
+                expect(testView.$('.new-post-article')).toHaveClass('is-hidden');
+            });
 
+            it('should be shown when the "Add a Post" button is clicked', function() {
+                var testView = createTestView(this);
+                showDiscussion(this, testView);
+                testView.$('.new-post-btn').click();
+                expect(testView.$('.new-post-article')).not.toHaveClass('is-hidden');
+            });
+
+            it('should be hidden when the "Cancel" button is clicked', function() {
+                var testView = createTestView(this);
+                showDiscussion(this, testView);
+                testView.$('.new-post-btn').click();
+                testView.$('.forum-new-post-form .cancel').click();
+                expect(testView.$('.new-post-article')).toHaveClass('is-hidden');
+            });
         });
 
         describe('thread listing', function() {
-            it('builds a view that has the right number of threads', function() {
-
-            });
-
-            it('calls DiscussionThreadListView internally', function() {
-
+            it('builds a view that lists the threads', function() {
+                var testView = createTestView(this);
+                showDiscussion(this, testView);
+                expect(testView.$('.forum-nav-thread-title').text()).toBe(TEST_THREAD_TITLE);
             });
         });
 
         describe('thread post drill down', function() {
             it('can drill down to a thread', function() {
-                var testView = createTestView();
-                setNextAjaxResult(this, {
-                    user_info: DiscussionSpecHelper.getTestUserInfo(),
-                    roles: DiscussionSpecHelper.getTestRoleInfo(),
-                    course_settings: this.courseSettings.attributes,
-                    discussion_data: this.threadData,
-                    page: 1,
-                    num_pages: 1
-                });
-                testView.$('.discussion-show').click();
-                expect(testView).toBeTruthy();
+                var testView = createTestView(this);
+                showDiscussion(this, testView);
+                expect(testView.$('.forum-nav-thread-title').text()).toBe(TEST_THREAD_TITLE);
             });
 
             it('can go back to the list of threads', function() {
-                var testView = createTestView();
+                var testView = createTestView(this);
+                showDiscussion(this, testView);
                 expect(testView).toBeTruthy();
             });
         });
