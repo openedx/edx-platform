a399faba9bb5164c7d78450ec4e29068938867e8
Anton Stupak <s2pak.anton@gmail.com>
2016-11-24 17:25:36 +0200

    Fix pylint.

--- lms/envs/labster_test.py
+++ lms/envs/labster_test.py
@@ -1,4 +1,4 @@
-from.labster import *
+from.labster import *  # pylint: disable=wildcard-import, unused-wildcard-import
 from .test import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
 LABSTER_WIKI_LINK = 'https://theory.example.com/'
