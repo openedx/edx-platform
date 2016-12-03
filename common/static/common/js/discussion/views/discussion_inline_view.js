08f7cc7984ba81bcfb48dfc56e12f1fd64a0ce0d
Andy Armstrong <andya@edx.org>
2016-12-02 19:28:41 -0500

    Add back Brian's change that got lost in a rebase...

--- common/static/common/js/discussion/views/discussion_inline_view.js
+++ common/static/common/js/discussion/views/discussion_inline_view.js
@@ -145,6 +145,7 @@
                 course_settings: this.course_settings
             });
             this.threadView.render();
+            this.listenTo(this.threadView.showView, 'thread:_delete', this.navigateToAllPosts);
             this.threadListView.$el.addClass('is-hidden');
             this.$('.inline-thread').removeClass('is-hidden');
         },
