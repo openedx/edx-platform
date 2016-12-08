5c2214eeacc0b511a1db47d4835d14c008693fb8
cahrens <christina@edx.org>
2016-12-08 14:55:25 -0500

    Support web fragments for tabs.

--- lms/djangoapps/courseware/views/views.py
+++ lms/djangoapps/courseware/views/views.py
@@ -13,6 +13,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, AnonymousUser
 from django.core.exceptions import PermissionDenied
+
 from django.core.urlresolvers import reverse
 from django.core.context_processors import csrf
 from django.db import transaction
@@ -97,6 +98,9 @@ from xmodule.tabs import CourseTabList
 from xmodule.x_module import STUDENT_VIEW
 from ..entrance_exams import user_must_complete_entrance_exam
 from ..module_render import get_module_for_descriptor, get_module, get_module_by_usage_id
+
+from web_fragments.views import FragmentView
+from web_fragments.fragment import Fragment
 
 log = logging.getLogger("edx.courseware")
 
@@ -456,18 +460,45 @@ def static_tab(request, course_id, tab_slug):
     if tab is None:
         raise Http404
 
-    contents = get_static_tab_contents(
+    fragment = get_static_tab_fragment(
         request,
         course,
         tab
     )
-    if contents is None:
-        raise Http404
 
     return render_to_response('courseware/static_tab.html', {
         'course': course,
+        'active_page': 'static_tab_{0}'.format(tab['url_slug']),
         'tab': tab,
-        'tab_contents': contents,
+        'fragment': fragment,
+        'uses_pattern_library': False,
+        'disable_courseware_js': True
+    })
+
+
+@ensure_csrf_cookie
+@ensure_valid_course_key
+def content_tab(request, course_id, tab_type):
+    """
+    Display a content tab based on type name.
+
+    Assumes the course_id is in a valid format.
+    """
+
+    course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
+    course = get_course_with_access(request.user, 'load', course_key)
+
+    content_tab = [tab for tab in course.tabs if tab.type == tab_type][0]
+    fragment = content_tab.render_fragment(request, course)
+
+
+    return render_to_response('courseware/static_tab.html', {
+        'course': course,
+        'active_page': content_tab['type'],
+        'tab': content_tab,
+        'fragment': fragment,
+        'uses_pattern_library': True,
+        'disable_courseware_js': True
     })
 
 
@@ -716,6 +747,65 @@ def course_about(request, course_id):
         inject_coursetalk_keys_into_context(context, course_key)
 
         return render_to_response('courseware/course_about.html', context)
+
+
+class ProgressComponentView(FragmentView):
+    """
+    Component implementation of the discussion board.
+    """
+    def render_fragment(self, request, course_id=None):
+        """
+        Render the component
+        """
+        # nr_transaction = newrelic.agent.current_transaction()
+        #
+        course_key = CourseKey.from_string(course_id)
+        context = _create_progress_context(request, course_key)
+        html = render_to_string('discussion/discussion_board_component.html', context)
+        # # inline_js = render_to_string('discussion/discussion_board_js.template', context)
+        #
+        # fragment = Fragment(html)
+        # # fragment.add_javascript(inline_js)
+        fragment = Fragment()
+        fragment.content = "Hello World"
+        return fragment
+
+
+def _create_progress_context(request, course_key):
+    course = get_course_with_access(request.user, 'load', course_key, depth=None, check_if_enrolled=True)
+    prep_course_for_grading(course, request)
+    staff_access = bool(has_access(request.user, 'staff', course))
+    student = request.user
+
+    # NOTE: To make sure impersonation by instructor works, use
+    # student instead of request.user in the rest of the function.
+
+    # The pre-fetching of groups is done to make auth checks not require an
+    # additional DB lookup (this kills the Progress page in particular).
+    student = User.objects.prefetch_related("groups").get(id=student.id)
+
+    course_grade = CourseGradeFactory().create(student, course)
+    courseware_summary = course_grade.chapter_grades
+    grade_summary = course_grade.summary
+
+    studio_url = get_studio_url(course, 'settings/grading')
+
+    # checking certificate generation configuration
+    enrollment_mode, is_active = CourseEnrollment.enrollment_mode_for_user(student, course_key)
+
+    context = {
+        'course': course,
+        'courseware_summary': courseware_summary,
+        'studio_url': studio_url,
+        'grade_summary': grade_summary,
+        'staff_access': staff_access,
+        'student': student,
+        'passed': is_course_passed(course, grade_summary),
+        'credit_course_requirements': _credit_course_requirements(course_key, student),
+        'certificate_data': _get_cert_data(student, course, course_key, is_active, enrollment_mode)
+    }
+
+    return context
 
 
 @transaction.non_atomic_requests
@@ -1056,9 +1146,9 @@ def submission_history(request, course_id, student_username, location):
     return render_to_response('courseware/submission_history.html', context)
 
 
-def get_static_tab_contents(request, course, tab):
+def get_static_tab_fragment(request, course, tab):
     """
-    Returns the contents for the given static tab
+    Returns the fragment for the given static tab
     """
     loc = course.id.make_usage_key(
         tab.type,
@@ -1073,17 +1163,17 @@ def get_static_tab_contents(request, course, tab):
 
     logging.debug('course_module = %s', tab_module)
 
-    html = ''
+    fragment = Fragment()
     if tab_module is not None:
         try:
-            html = tab_module.render(STUDENT_VIEW).content
+            fragment = tab_module.render(STUDENT_VIEW, {})
         except Exception:  # pylint: disable=broad-except
-            html = render_to_string('courseware/error-message.html', None)
+            fragment.content = render_to_string('courseware/error-message.html', None)
             log.exception(
                 u"Error rendering course=%s, tab=%s", course, tab['url_slug']
             )
 
-    return html
+    return fragment
 
 
 @require_GET
