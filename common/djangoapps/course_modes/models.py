49d7bfbdcc8a55a651372417f50a1bea935ce0a0
Giulio Gratta <giuliog@stanford.edu>
2016-12-16 13:02:42 -0800

    Make HONOR the default mode
    - Need to make PR upstream to make this configurable

--- common/djangoapps/course_modes/models.py
+++ common/djangoapps/course_modes/models.py
@@ -100,7 +100,7 @@ class CourseMode(models.Model):
     NO_ID_PROFESSIONAL_MODE = "no-id-professional"
     CREDIT_MODE = "credit"
 
-    DEFAULT_MODE = Mode(AUDIT, _('Audit'), 0, '', 'usd', None, None, None)
+    DEFAULT_MODE = Mode(HONOR, _('Honor'), 0, '', 'usd', None, None, None)
     DEFAULT_MODE_SLUG = AUDIT
 
     # Modes that allow a student to pursue a verified certificate
