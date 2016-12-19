b95a5416188f9ae289dfb49deec0deba0376784d
David Ormsbee <dave@edx.org>
2016-12-19 10:30:27 -0500

    Disable Datadog metrics gathering

--- openedx/core/djangoapps/monitoring/signals.py
+++ openedx/core/djangoapps/monitoring/signals.py
@@ -48,88 +48,89 @@ def _model_tags(kwargs, key):
     return tags
 
 
-@receiver(post_init, dispatch_uid='edxapp.monitoring.post_init_metrics')
-def post_init_metrics(sender, **kwargs):
-    """
-    Record the number of times that django models are instantiated.
-
-    Args:
-        sender (Model): The model class sending the signals.
-        using (str): The name of the database being used for this initialization (optional).
-        instance (Model instance): The instance being initialized (optional).
-    """
-    tags = _database_tags('initialized', sender, kwargs)
-
-    dog_stats_api.increment('edxapp.db.model', tags=tags)
-
-
-@receiver(post_save, dispatch_uid='edxapp.monitoring.post_save_metrics')
-def post_save_metrics(sender, **kwargs):
-    """
-    Record the number of times that django models are saved (created or updated).
-
-    Args:
-        sender (Model): The model class sending the signals.
-        using (str): The name of the database being used for this update (optional).
-        instance (Model instance): The instance being updated (optional).
-    """
-    action = 'created' if kwargs.pop('created', False) else 'updated'
-
-    tags = _database_tags(action, sender, kwargs)
-    dog_stats_api.increment('edxapp.db.model', tags=tags)
-
-
-@receiver(post_delete, dispatch_uid='edxapp.monitoring.post_delete_metrics')
-def post_delete_metrics(sender, **kwargs):
-    """
-    Record the number of times that django models are deleted.
-
-    Args:
-        sender (Model): The model class sending the signals.
-        using (str): The name of the database being used for this deletion (optional).
-        instance (Model instance): The instance being deleted (optional).
-    """
-    tags = _database_tags('deleted', sender, kwargs)
-
-    dog_stats_api.increment('edxapp.db.model', tags=tags)
-
-
-@receiver(m2m_changed, dispatch_uid='edxapp.monitoring.m2m_changed_metrics')
-def m2m_changed_metrics(sender, **kwargs):
-    """
-    Record the number of times that Many2Many fields are updated. This is separated
-    from post_save and post_delete, because it's signaled by the database model in
-    the middle of the Many2Many relationship, rather than either of the models
-    that are the relationship participants.
-
-    Args:
-        sender (Model): The model class in the middle of the Many2Many relationship.
-        action (str): The action being taken on this Many2Many relationship.
-        using (str): The name of the database being used for this deletion (optional).
-        instance (Model instance): The instance whose many-to-many relation is being modified.
-        model (Model class): The model of the class being added/removed/cleared from the relation.
-    """
-    if 'action' not in kwargs:
-        return
-
-    action = {
-        'post_add': 'm2m.added',
-        'post_remove': 'm2m.removed',
-        'post_clear': 'm2m.cleared',
-    }.get(kwargs['action'])
-
-    if not action:
-        return
-
-    tags = _database_tags(action, sender, kwargs)
-
-    if 'model' in kwargs:
-        tags.append('target_class:{}'.format(kwargs['model'].__name__))
-
-    pk_set = kwargs.get('pk_set', []) or []
-
-    dog_stats_api.increment(
-        'edxapp.db.model',
-        value=len(pk_set),
-        tags=tags
-    )
+# @receiver(post_init, dispatch_uid='edxapp.monitoring.post_init_metrics')
+# def post_init_metrics(sender, **kwargs):
+#     """
+#     Record the number of times that django models are instantiated.
+#
+#     Args:
+#         sender (Model): The model class sending the signals.
+#         using (str): The name of the database being used for this initialization (optional).
+#         instance (Model instance): The instance being initialized (optional).
+#     """
+#     tags = _database_tags('initialized', sender, kwargs)
+#
+#     dog_stats_api.increment('edxapp.db.model', tags=tags)
+#
+#
+# @receiver(post_save, dispatch_uid='edxapp.monitoring.post_save_metrics')
+# def post_save_metrics(sender, **kwargs):
+#     """
+#     Record the number of times that django models are saved (created or updated).
+#
+#     Args:
+#         sender (Model): The model class sending the signals.
+#         using (str): The name of the database being used for this update (optional).
+#         instance (Model instance): The instance being updated (optional).
+#     """
+#     action = 'created' if kwargs.pop('created', False) else 'updated'
+#
+#     tags = _database_tags(action, sender, kwargs)
+#     dog_stats_api.increment('edxapp.db.model', tags=tags)
+#
+#
+# @receiver(post_delete, dispatch_uid='edxapp.monitoring.post_delete_metrics')
+# def post_delete_metrics(sender, **kwargs):
+#     """
+#     Record the number of times that django models are deleted.
+#
+#     Args:
+#         sender (Model): The model class sending the signals.
+#         using (str): The name of the database being used for this deletion (optional).
+#         instance (Model instance): The instance being deleted (optional).
+#     """
+#     tags = _database_tags('deleted', sender, kwargs)
+#
+#     dog_stats_api.increment('edxapp.db.model', tags=tags)
+#
+#
+# @receiver(m2m_changed, dispatch_uid='edxapp.monitoring.m2m_changed_metrics')
+# def m2m_changed_metrics(sender, **kwargs):
+#     """
+#     Record the number of times that Many2Many fields are updated. This is separated
+#     from post_save and post_delete, because it's signaled by the database model in
+#     the middle of the Many2Many relationship, rather than either of the models
+#     that are the relationship participants.
+#
+#     Args:
+#         sender (Model): The model class in the middle of the Many2Many relationship.
+#         action (str): The action being taken on this Many2Many relationship.
+#         using (str): The name of the database being used for this deletion (optional).
+#         instance (Model instance): The instance whose many-to-many relation is being modified.
+#         model (Model class): The model of the class being added/removed/cleared from the relation.
+#     """
+#     if 'action' not in kwargs:
+#         return
+#
+#     action = {
+#         'post_add': 'm2m.added',
+#         'post_remove': 'm2m.removed',
+#         'post_clear': 'm2m.cleared',
+#     }.get(kwargs['action'])
+#
+#     if not action:
+#         return
+#
+#     tags = _database_tags(action, sender, kwargs)
+#
+#     if 'model' in kwargs:
+#         tags.append('target_class:{}'.format(kwargs['model'].__name__))
+#
+#     pk_set = kwargs.get('pk_set', []) or []
+#
+#     dog_stats_api.increment(
+#         'edxapp.db.model',
+#         value=len(pk_set),
+#         tags=tags
+#     )
+#
