4efdd0f5be8723092d97047a16a30f61e509822b
stv <stv@stanford.edu>
2016-12-19 14:24:26 -0800

    Share mimetype startup between CMS and LMS

--- openedx/core/lib/django_startup.py
+++ openedx/core/lib/django_startup.py
@@ -1,9 +1,21 @@
 """
-Automatic execution of startup modules in Django apps.
+Extend startup behavior shared between LMS and CMS
 """
 
 from importlib import import_module
 from django.conf import settings
+
+
+def add_mimetypes():
+    """
+    Add extra mimetype mappings
+    """
+    import mimetypes
+
+    mimetypes.add_type('application/vnd.ms-fontobject', '.eot')
+    mimetypes.add_type('application/x-font-opentype', '.otf')
+    mimetypes.add_type('application/x-font-ttf', '.ttf')
+    mimetypes.add_type('application/font-woff', '.woff')
 
 
 def autostartup():
