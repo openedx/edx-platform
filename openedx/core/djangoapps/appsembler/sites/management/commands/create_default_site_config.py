f91c29dc344b9d53a714e5061c5d58745aa7aa6f
Deni Bertovic <deni@denibertovic.com>
2016-12-19 18:31:34 +0100

    Adds default site configuration
    This is used as a management command that can be run either directly
    but is usually invoked by the playbook during deployment.

---
+++ openedx/core/djangoapps/appsembler/sites/management/commands/create_default_site_config.py
@@ -0,0 +1,29 @@
+import sys
+
+from django.contrib.sites.models import Site
+from django.conf import settings
+from django.core.management.base import NoArgsCommand
+
+
+from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
+
+
+class Command(NoArgsCommand):
+    help = 'Create default SiteConfiguration for the default SITE_ID.'
+
+    def handle_noargs(self, **options):
+        s = Site.objects.get(id=settings.SITE_ID)
+
+        try:
+            if SiteConfiguration.objects.get(site=s).exists():
+                print("Default SiteConfiguration already exists. Doing nothing.")
+            else:
+                sc = SiteConfiguration(
+                    site=s,
+                    enabled=True)
+                sc.save()
+                print("Created default SiteConfiguration.")
+        except Exception as e:
+            print("Failed to create default configuration. Error: {0}".format(str(e)))
+            sys.exit(-1)
+
