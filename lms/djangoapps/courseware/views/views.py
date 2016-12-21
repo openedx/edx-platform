b32f30d879101f5239412d833b20e97923385e5a
Afzal Wali <afzal@edx.org>
2016-12-21 15:30:05 +0500

    Program details page

--- lms/djangoapps/courseware/views/views.py
+++ lms/djangoapps/courseware/views/views.py
@@ -671,6 +671,17 @@ def course_about(request, course_id):
         return render_to_response('courseware/course_about.html', context)
 
 
+@ensure_csrf_cookie
+@cache_if_anonymous()
+def program_detail(request, program_id):
+    """
+    Display the program's detail page.
+
+    Assumes the program_id is in a valid format.
+    """
+    return render_to_response('courseware/program_details.html')
+
+
 @transaction.non_atomic_requests
 @login_required
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
--- lms/urls.py
+++ lms/urls.py
@@ -426,6 +426,14 @@ urlpatterns += (
         name='student_progress',
     ),
 
+    url(
+        r'^programs/{}/about$'.format(
+            settings.PROGRAM_ID_PATTERN,
+        ),
+        'courseware.views.views.program_detail',
+        name='about_program',
+     ),
+
     # rest api for grades
     url(
         r'^api/grades/',
