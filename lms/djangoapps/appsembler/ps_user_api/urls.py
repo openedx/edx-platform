bf32a9ccbc2937e75c35f3beab981e5ac1b489bf
melvinsoft <melvinsoft@gmail.com>
2016-12-27 15:49:04 -0300

    delete old APIs

--- lms/djangoapps/appsembler/ps_user_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from appsembler.ps_user_api import views
-
-urlpatterns = patterns(
-    '',
-    url(r'^v1/accounts/create', views.CreateUserAccountView.as_view(), name="create_user_account_api"),
-    url(r'^v1/accounts/(?P<username>[\w.+-]+)', views.GetUserAccountView.as_view(), name="get_user_account_api"),
-)
