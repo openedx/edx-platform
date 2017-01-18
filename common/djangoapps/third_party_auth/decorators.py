0fa5147a15ba5c86712031fe6bebeb59caa31984
Zia Fazal <zia.fazal@arbisoft.com>
2017-01-18 13:50:54 +0500

    refined xframe decorator to strip trailing slash in url

--- common/djangoapps/third_party_auth/decorators.py
+++ common/djangoapps/third_party_auth/decorators.py
@@ -27,9 +27,12 @@ def allow_frame_from_whitelisted_url(view_func):  # pylint: disable=invalid-name
             referer = request.META.get('HTTP_REFERER')
             if referer is not None:
                 parsed_url = urlparse(referer)
-                # reconstruct a referer url without querystring
-                referer_url = urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, '', '', ''))
+                # reconstruct a referer url without querystring and trailing slash
+                referer_url = urlunparse(
+                    (parsed_url.scheme, parsed_url.netloc, parsed_url.path.rstrip('/'), '', '', '')
+                )
                 sso_urls = SAMLProviderData.objects.values_list('sso_url', flat=True)
+                sso_urls = map(lambda u: u.rstrip('/'), sso_urls)
                 if referer_url in sso_urls:
                     allowed_urls = ' '.join(settings.THIRD_PARTY_AUTH_FRAME_ALLOWED_FROM_URL)
                     x_frame_option = 'ALLOW-FROM {}'.format(allowed_urls)
