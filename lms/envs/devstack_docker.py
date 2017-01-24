e7c0ed77e521beaceac615b4206241af3fccfbc2
Clinton Blackburn <cblackburn@edx.org>
2017-01-23 20:39:29 -0500

    Added settings for Docker-based devstack
    ECOM-6634

---
+++ lms/envs/devstack_docker.py
@@ -0,0 +1,24 @@
+""" Overrides for Docker-based devstack. """
+
+from .devstack import *  # pylint: disable=wildcard-import, unused-wildcard-import
+
+# Docker does not support the syslog socket at /dev/log. Rely on the console.
+LOGGING['handlers']['local'] = LOGGING['handlers']['tracking'] = {
+    'class': 'logging.NullHandler',
+}
+
+LOGGING['loggers']['tracking']['handlers'] = ['console']
+
+HOST = 'edx.devstack.edxapp:18000'
+SITE_NAME = HOST
+LMS_ROOT_URL = 'http://{}:18000'.format(HOST)
+
+ECOMMERCE_PUBLIC_URL_ROOT = "http://edx.devstack.ecommerce:18130"
+
+OAUTH_OIDC_ISSUER = '{}/oauth2'.format(LMS_ROOT_URL)
+
+JWT_AUTH.update({
+    'JWT_SECRET_KEY': 'lms-secret',
+    'JWT_ISSUER': OAUTH_OIDC_ISSUER,
+    'JWT_AUDIENCE': 'lms-key',
+})
