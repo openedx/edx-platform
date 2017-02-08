452a1f1fbbae1d6982f6369c6487f948ff0ec35d
Ben Patterson <bpatterson@edx.org>
2017-02-08 13:44:47 -0500

    Refactor click since it was resulting in timeout errors; use correct id for one of the LTI stub's buttons.

--- lms/djangoapps/courseware/features/lti.py
+++ lms/djangoapps/courseware/features/lti.py
@@ -359,8 +359,9 @@ def click_grade(_step, version):
     location = world.scenario_dict['LTI'].location.html_id()
     iframe_name = 'ltiFrame-' + location
     with world.browser.get_iframe(iframe_name) as iframe:
-        world.wait_for_visible('#' + version_map[version]['selector'])
-        iframe.find_by_name(version_map[version]['selector']).first.click()
+        css_loc = '#' + version_map[version]['selector']
+        world.wait_for_visible(css_loc)
+        world.css_click(css_loc)
         assert iframe.is_text_present(version_map[version]['expected_text'])
 
 
