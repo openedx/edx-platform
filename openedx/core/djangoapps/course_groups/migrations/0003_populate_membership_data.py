79e7ee68c9b3be105ae63758af675d1f9ebdf71e
Fred Smith <derf@edx.org>
2017-02-10 12:31:01 -0500

    double import typo

--- openedx/core/djangoapps/course_groups/migrations/0003_populate_membership_data.py
+++ openedx/core/djangoapps/course_groups/migrations/0003_populate_membership_data.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import logging
 from django.db import migrations
-from django.db import import transaction
+from django.db import transaction
 
 
 log = logging.getLogger(__name__)
