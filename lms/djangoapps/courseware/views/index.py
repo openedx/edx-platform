bd8248cb2f1c7a95d79aa05dc24e34578c564afc
Brian Jacobel <bjacobel@gmail.com>
2017-02-14 14:46:58 -0500

    Link outline subsections into the course

--- lms/djangoapps/courseware/views/index.py
+++ lms/djangoapps/courseware/views/index.py
@@ -28,6 +28,7 @@ except ImportError:
     newrelic = None  # pylint: disable=invalid-name
 
 import urllib
+import waffle
 
 from lms.djangoapps.gating.api import get_entrance_exam_score_ratio, get_entrance_exam_usage_key
 from lms.djangoapps.grades.new.course_grade import CourseGradeFactory
@@ -35,6 +36,7 @@ from opaque_keys.edx.keys import CourseKey
 from openedx.core.djangoapps.lang_pref import LANGUAGE_KEY
 from openedx.core.djangoapps.user_api.preferences.api import get_user_preference
 from openedx.core.djangoapps.crawlers.models import CrawlersConfig
+from request_cache.middleware import RequestCache
 from shoppingcart.models import CourseRegistrationCode
 from student.models import CourseEnrollment
 from student.views import is_course_blocked
@@ -402,6 +404,7 @@ class CoursewareIndex(View):
         Returns and creates the rendering context for the courseware.
         Also returns the table of contents for the courseware.
         """
+        request = RequestCache.get_current_request()
         courseware_context = {
             'csrf': csrf(self.request)['csrf_token'],
             'COURSE_TITLE': self.course.display_name_with_default_escaped,
@@ -417,7 +420,8 @@ class CoursewareIndex(View):
             'language_preference': self._get_language_preference(),
             'disable_optimizely': True,
             'section_title': None,
-            'sequence_title': None
+            'sequence_title': None,
+            'disable_accordion': waffle.flag_is_active(request, 'unified_course_view')
         }
         table_of_contents = toc_for_course(
             self.effective_user,
