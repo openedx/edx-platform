d1b45f5349690a889796f42369055156acc3ddbf
ms-hukoyun <hukoyun@microsoft.com>
2017-02-14 18:28:49 -0800

    This was originally checked in by chenriksson.
    -Fix blob does not exist error on uploading logs
    -Add option to delete uploaded files
    -Fix remove call to match import

--- common/djangoapps/track/management/commands/ship_tracking_logs.py
+++ common/djangoapps/track/management/commands/ship_tracking_logs.py
@@ -1,4 +1,4 @@
-from os import listdir, stat
+from os import listdir, remove, stat
 from os.path import isfile, join
 
 from optparse import make_option
@@ -9,27 +9,62 @@ from openedx.core.storage import get_storage
 
 
 class Command(BaseCommand):
-    help = """
-    This command creates and registers a user in a given course
-    as "audit", "verified" or "honor".
-
-    example:
-        # Enroll a user test@example.com into the demo course
-        # The username and name will default to "test"
-        manage.py ... create_user -e test@example.com -p insecure -c edX/Open_DemoX/edx_demo_course -m verified
     """
+    Django Management command utility to copy local tracking logs to
+    a remote storage environment. While this utility was written with Azure
+    Blob Storage in mind, since it uses the Django-Storages abstraction layer
+    it should be easily modified - if at all - to work with other Django-Storages
+    providers such as S3
 
+    To use this command, be sure to run this utility with an identity that has
+    read access to the tracking logs.
+
+    Arguments:
+
+        -p <local-tracking-file-directory-path>
+           The path where the tracking files can be found
+           DEFAULT: /edx/var/log/tracking
+
+        -c <container-name>
+           The name of the remote container/bucket to use
+           DEFAULT: 'tracking-logs'
+
+        -f <folder-name>
+           The name of the folder in the container/bukcet to 
+           put these files. This can be used to segregate tracking logs
+           from multiple Open edX, in the case that it is a shared storage
+           repository. The Open edX hostname might be a good choice of folder.
+           NOTE: This is a required parameter with no default.
+
+        -s <storage-name>
+           The Open edX Django-Storages provider to use
+           DEFAULT: 'openedx.core.storage.AzureStorageExtended'
+
+        -o <overwrite-existing>   (True/False)
+            A Boolean flag whether to overwrite existing files in the
+            targest destination
+            DEFAULT: False (don't overwrite)
+
+        -d <delete_local>   (True/False)
+            A Boolean flag whether to delete local log files after successful upload
+            DEFAULT: False (don't delete)
+
+    To periodically ship off tracking logs, one can trigger an execution of
+    this command to some recurring task (like cron). Be sure to configure your
+    environments logrotate so that tracking files are rotated on a periodic
+    basis, e.g. daily, hourly, etc. After the system managed logrotation, this
+    django command can be trigger to upload rotated out files to remote storage.
+    """
     option_list = BaseCommand.option_list + (
         make_option('-p', '--path',
-            metavar='PATH',
-            dest='path',
-            default='/edx/var/log/tracking',
-            help='where to find the tracking logs',
-        ),
+                    metavar='PATH',
+                    dest='path',
+                    default='/edx/var/log/tracking',
+                    help='where to find the tracking logs'),
         make_option('-c', '--container',
                     metavar='CONTAINER',
                     dest='container',
-                    default=None,
+                    default='tracking-logs',
                     help='Which container/bucket to use'),
         make_option('-f', '--folder',
                     metavar='FOLDER',
@@ -43,16 +78,15 @@ class Command(BaseCommand):
                     default='openedx.core.storage.AzureStorageExtended',
                     help='Which storage class to use'),
         make_option('-o', '--overwrite',
-            metavar='OVERWRITE',
-            dest='overwrite',
-            default=False,
-            help='Overwrite existing files in remote storage'),
-        make_option('-d', '--delete',
-            metavar='DELETE',
-            dest='delete',
-            default=False,
-            help='delete files already uploaded',
-        )
+                    metavar='OVERWRITE',
+                    dest='overwrite',
+                    default=False,
+                    help='Overwrite existing files in remote storage'),
+        make_option('-d', '--delete_local',
+                    metavar='DELETE',
+                    dest='delete_local',
+                    default=False,
+                    help='Delete local files after successful upload')
     )
 
     def handle(self, *args, **options):
@@ -61,26 +95,27 @@ class Command(BaseCommand):
         container = options['container']
         path = options['path']
         overwrite = options['overwrite']
+        delete_local = options['delete_local']
 
         if not storage_class or not folder or not container or not path:
             print 'You must specify the -c, -f, -p, and -s parameters for this command'
 
         storage = get_storage(storage_class, container=container)
 
-        files = [f for f in listdir(path) if isfile(join(path, f))]
+        files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.gz')]
 
         for file in files:
             print 'Inspecting {} ....'.format(file)
             local_path = join(path, file)
-            with open(local_path,'r') as f:
-                try:
+            try:
+                with open(local_path,'r') as f:
                     dest_fn = '{}/{}'.format(folder, file)
                     exists = storage.exists(dest_fn)
                     # does it already exist? Don't overwrite
                     if not exists or overwrite:
                         # even if we overwrite, don't do so if filesize has not
                         # changed
-                        if overwrite:
+                        if exists and overwrite:
                             remote_size = storage.size(dest_fn)
                             local_size = stat(local_path).st_size
                             print '{} {}'.format(remote_size, local_size)
@@ -94,9 +129,11 @@ class Command(BaseCommand):
 
                         print 'Shipping {} to remote storage...'.format(file)
                         storage.save(dest_fn, f)
+                        
+                        if delete_local:
+                          print 'Deleting {} from disk...'.format(file)
+                          remove(local_path)
                     else:
                         print 'File {} already exists in remote storage. Skipping...'.format(file)
-                except Exception, ex:
-                    print 'Exception: {}'.format(str(ex))
-
-
+            except Exception, ex:
+                print 'Exception: {}'.format(str(ex))
