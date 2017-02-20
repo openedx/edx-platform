8423ecb1c870f368b4fa511b70d4f255677d0f9d
David Ormsbee <dave@edx.org>
2017-02-20 11:27:50 -0500

    Student Dashboard CourseOverviews with one query.
    Pre-load the course overviews attached to CourseEnrollments on the
    Student Dashboard, if possible. This will only grab the CourseOverviews
    that already exist, and will not generate new ones. Any missing
    CourseOverviews fall back to the lazily-created one-at-a-time behavior
    they've always had. That's mostly because I wanted to optimize for the
    common case in the least invasive way possible, and I don't want to get
    caught up in locking issues.

--- openedx/core/djangoapps/content/course_overviews/tests.py
+++ openedx/core/djangoapps/content/course_overviews/tests.py
@@ -515,6 +515,35 @@ class CourseOverviewTestCase(ModuleStoreTestCase):
                 "testing CourseOverview.get_all_courses with filter_={}".format(filter_),
             )
 
+    def test_get_from_ids_if_exists(self):
+        course_with_overview_1 = CourseFactory.create(emit_signals=True)
+        course_with_overview_2 = CourseFactory.create(emit_signals=True)
+        course_without_overview = CourseFactory.create(emit_signals=False)
+
+        courses = [course_with_overview_1, course_with_overview_2, course_without_overview]
+        course_ids_to_overviews = CourseOverview.get_from_ids_if_exists(
+            course.id for course in courses
+        )
+
+        # We should see the ones that have published CourseOverviews
+        # (when the signals were emitted), but not the one that didn't issue
+        # a publish signal.
+        self.assertEqual(len(course_ids_to_overviews), 2)
+        self.assertIn(course_with_overview_1.id, course_ids_to_overviews)
+        self.assertIn(course_with_overview_2.id, course_ids_to_overviews)
+        self.assertNotIn(course_without_overview.id, course_ids_to_overviews)
+
+        # But if we set a CourseOverview to be an old version, it shouldn't be
+        # returned by get_from_ids_if_exists()
+        overview_2 = course_ids_to_overviews[course_with_overview_2.id]
+        overview_2.version = CourseOverview.VERSION - 1
+        overview_2.save()
+        course_ids_to_overviews = CourseOverview.get_from_ids_if_exists(
+            course.id for course in courses
+        )
+        self.assertEqual(len(course_ids_to_overviews), 1)
+        self.assertIn(course_with_overview_1.id, course_ids_to_overviews)
+
 
 @attr(shard=3)
 @ddt.ddt
