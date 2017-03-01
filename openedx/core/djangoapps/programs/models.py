06a6d8525a475ad65928985c252ce9e4f2030e3f
Mike Dikan <mike.dikan@gmail.com>
2017-03-01 17:50:43 -0500

    Removing unused fields on ProgramApiConfig model
    ECOM-7196
    
    Updating model to only include the non-deprecated fields.  This work is a follow on to ECOM-7195 and SHOULD NOT be merged until that PR is merged.

--- openedx/core/djangoapps/programs/models.py
+++ openedx/core/djangoapps/programs/models.py
@@ -9,18 +9,10 @@ from config_models.models import ConfigurationModel
 
 class ProgramsApiConfig(ConfigurationModel):
     """
-    DEPRECATED. To be removed as part of ECOM-5136.
+    This model no longer fronts an API, but now sets a few config-related values for the idea of programs in general.
 
-    Manages configuration for connecting to the Programs service and using its API.
+    A rename to ProgramsConfig would be more accurate, but costly in terms of developer time.
     """
-    OAUTH2_CLIENT_NAME = 'programs'
-    CACHE_KEY = 'programs.api.data'
-    API_NAME = 'programs'
-
-    api_version_number = models.IntegerField(verbose_name=_("API Version"))
-
-    internal_service_url = models.URLField(verbose_name=_("Internal Service URL"), blank=True)
-    public_service_url = models.URLField(verbose_name=_("Public Service URL"), blank=True)
 
     marketing_path = models.CharField(
         max_length=255,
@@ -28,69 +20,4 @@ class ProgramsApiConfig(ConfigurationModel):
         help_text=_(
             'Path used to construct URLs to programs marketing pages (e.g., "/foo").'
         )
-    )
-
-    authoring_app_js_path = models.CharField(
-        verbose_name=_("Path to authoring app's JS"),
-        max_length=255,
-        blank=True,
-        help_text=_(
-            "This value is required in order to enable the Studio authoring interface."
-        )
-    )
-
-    authoring_app_css_path = models.CharField(
-        verbose_name=_("Path to authoring app's CSS"),
-        max_length=255,
-        blank=True,
-        help_text=_(
-            "This value is required in order to enable the Studio authoring interface."
-        )
-    )
-
-    cache_ttl = models.PositiveIntegerField(
-        verbose_name=_("Cache Time To Live"),
-        default=0,
-        help_text=_(
-            "Specified in seconds. Enable caching by setting this to a value greater than 0."
-        )
-    )
-
-    enable_student_dashboard = models.BooleanField(
-        verbose_name=_("Enable Student Dashboard Displays"),
-        default=False
-    )
-
-    enable_studio_tab = models.BooleanField(
-        verbose_name=_("Enable Studio Authoring Interface"),
-        default=False
-    )
-
-    enable_certification = models.BooleanField(
-        verbose_name=_("Enable Program Certificate Generation"),
-        default=False
-    )
-
-    max_retries = models.PositiveIntegerField(
-        verbose_name=_("Maximum Certification Retries"),
-        default=11,  # This gives about 30 minutes wait before the final attempt
-        help_text=_(
-            "When making requests to award certificates, make at most this many attempts "
-            "to retry a failing request."
-        )
-    )
-
-    xseries_ad_enabled = models.BooleanField(
-        verbose_name=_("Do we want to show xseries program advertising"),
-        default=False
-    )
-
-    program_listing_enabled = models.BooleanField(
-        verbose_name=_("Do we want to show program listing page"),
-        default=False
-    )
-
-    program_details_enabled = models.BooleanField(
-        verbose_name=_("Do we want to show program details pages"),
-        default=False
     )
