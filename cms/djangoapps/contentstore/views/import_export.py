e34ee5efee717211361d9369744b85b3fe76e0a9
Jeremy Bowman <jbowman@edx.org>
2017-03-01 15:42:34 -0500

    PLAT-1121 Export courses asynchronously

--- cms/djangoapps/contentstore/views/import_export.py
+++ cms/djangoapps/contentstore/views/import_export.py
@@ -3,13 +3,12 @@ These views handle all actions in Studio related to import and exporting of
 courses
 """
 import base64
+import json
 import logging
 import os
 import re
 import shutil
-import tarfile
 from path import Path as path
-from tempfile import mkdtemp
 
 from six import text_type
 
@@ -17,7 +16,6 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.core.files import File
-from django.core.files.temp import NamedTemporaryFile
 from django.core.servers.basehttp import FileWrapper
 from django.db import transaction
 from django.http import HttpResponse, HttpResponseNotFound, Http404
@@ -26,28 +24,26 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_http_methods, require_GET
 
 from edxmako.shortcuts import render_to_response
-from xmodule.contentstore.django import contentstore
 from xmodule.exceptions import SerializationError
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locator import LibraryLocator
 from user_tasks.conf import settings as user_tasks_settings
-from user_tasks.models import UserTaskStatus
-from xmodule.modulestore.xml_exporter import export_course_to_xml, export_library_to_xml
+from user_tasks.models import UserTaskArtifact, UserTaskStatus
 
 from student.auth import has_course_author_access
 
 from util.json_request import JsonResponse
 from util.views import ensure_valid_course_key
 from contentstore.storage import course_import_export_storage
-from contentstore.tasks import CourseImportTask, import_olx
+from contentstore.tasks import CourseExportTask, CourseImportTask, create_export_tarball, export_olx, import_olx
 
-from contentstore.utils import reverse_course_url, reverse_usage_url, reverse_library_url
+from contentstore.utils import reverse_course_url, reverse_library_url
 
 
 __all__ = [
     'import_handler', 'import_status_handler',
-    'export_handler',
+    'export_handler', 'export_output_handler', 'export_status_handler',
 ]
 
 
@@ -279,64 +275,6 @@ def import_status_handler(request, course_key_string, filename=None):
     return JsonResponse({"ImportStatus": status})
 
 
-def create_export_tarball(course_module, course_key, context):
-    """
-    Generates the export tarball, or returns None if there was an error.
-
-    Updates the context with any error information if applicable.
-    """
-    name = course_module.url_name
-    export_file = NamedTemporaryFile(prefix=name + '.', suffix=".tar.gz")
-    root_dir = path(mkdtemp())
-
-    try:
-        if isinstance(course_key, LibraryLocator):
-            export_library_to_xml(modulestore(), contentstore(), course_key, root_dir, name)
-        else:
-            export_course_to_xml(modulestore(), contentstore(), course_module.id, root_dir, name)
-
-        logging.debug(u'tar file being generated at %s', export_file.name)
-        with tarfile.open(name=export_file.name, mode='w:gz') as tar_file:
-            tar_file.add(root_dir / name, arcname=name)
-
-    except SerializationError as exc:
-        log.exception(u'There was an error exporting %s', course_key)
-        unit = None
-        failed_item = None
-        parent = None
-        try:
-            failed_item = modulestore().get_item(exc.location)
-            parent_loc = modulestore().get_parent_location(failed_item.location)
-
-            if parent_loc is not None:
-                parent = modulestore().get_item(parent_loc)
-                if parent.location.category == 'vertical':
-                    unit = parent
-        except:  # pylint: disable=bare-except
-            # if we have a nested exception, then we'll show the more generic error message
-            pass
-
-        context.update({
-            'in_err': True,
-            'raw_err_msg': str(exc),
-            'failed_module': failed_item,
-            'unit': unit,
-            'edit_unit_url': reverse_usage_url("container_handler", parent.location) if parent else "",
-        })
-        raise
-    except Exception as exc:
-        log.exception('There was an error exporting %s', course_key)
-        context.update({
-            'in_err': True,
-            'unit': None,
-            'raw_err_msg': str(exc)})
-        raise
-    finally:
-        shutil.rmtree(root_dir / name)
-
-    return export_file
-
-
 def send_tarball(tarball):
     """
     Renders a tarball to response, for use when sending a tar.gz file to the user.
@@ -351,7 +289,7 @@ def send_tarball(tarball):
 @transaction.non_atomic_requests
 @ensure_csrf_cookie
 @login_required
-@require_http_methods(("GET",))
+@require_http_methods(('GET', 'POST'))
 @ensure_valid_course_key
 def export_handler(request, course_key_string):
     """
@@ -361,15 +299,21 @@ def export_handler(request, course_key_string):
         html: return html page for import page
         application/x-tgz: return tar.gz file containing exported course
         json: not supported
+    POST
+        Start a Celery task to export the course
 
-    Note that there are 2 ways to request the tar.gz file. The request header can specify
-    application/x-tgz via HTTP_ACCEPT, or a query parameter can be used (?_accept=application/x-tgz).
+    Note that there are 3 ways to request the tar.gz file.  The Studio UI uses
+    a POST request to start the export asynchronously, with a link appearing
+    on the page once it's ready.  Additionally, for backwards compatibility
+    reasons the request header can specify application/x-tgz via HTTP_ACCEPT,
+    or a query parameter can be used (?_accept=application/x-tgz); this will
+    export the course synchronously and return the resulting file (unless the
+    request times out for a large course).
 
-    If the tar.gz file has been requested but the export operation fails, an HTML page will be returned
-    which describes the error.
+    If the tar.gz file has been requested but the export operation fails, the
+    import page will be returned including a description of the error.
     """
     course_key = CourseKey.from_string(course_key_string)
-    export_url = reverse_course_url('export_handler', course_key)
     if not has_course_author_access(request.user, course_key):
         raise PermissionDenied()
 
@@ -389,22 +333,134 @@ def export_handler(request, course_key_string):
             'courselike_home_url': reverse_course_url("course_handler", course_key),
             'library': False
         }
-
-    context['export_url'] = export_url + '?_accept=application/x-tgz'
+    context['status_url'] = reverse_course_url('export_status_handler', course_key)
 
     # an _accept URL parameter will be preferred over HTTP_ACCEPT in the header.
     requested_format = request.GET.get('_accept', request.META.get('HTTP_ACCEPT', 'text/html'))
 
-    if 'application/x-tgz' in requested_format:
+    if request.method == 'POST':
+        export_olx.delay(request.user.id, course_key_string, request.LANGUAGE_CODE)
+        return JsonResponse({'ExportStatus': 1})
+    elif 'application/x-tgz' in requested_format:
         try:
             tarball = create_export_tarball(courselike_module, course_key, context)
+            return send_tarball(tarball)
         except SerializationError:
             return render_to_response('export.html', context)
-        return send_tarball(tarball)
-
     elif 'text/html' in requested_format:
         return render_to_response('export.html', context)
-
     else:
         # Only HTML or x-tgz request formats are supported (no JSON).
         return HttpResponse(status=406)
+
+
+@transaction.non_atomic_requests
+@require_GET
+@ensure_csrf_cookie
+@login_required
+@ensure_valid_course_key
+def export_status_handler(request, course_key_string):
+    """
+    Returns an integer corresponding to the status of a file export. These are:
+
+        -X : Export unsuccessful due to some error with X as stage [0-3]
+        0 : No status info found (export done or task not yet created)
+        1 : Exporting
+        2 : Compressing
+        3 : Export successful
+
+    If the export was successful, a URL for the generated .tar.gz file is also
+    returned.
+    """
+    course_key = CourseKey.from_string(course_key_string)
+    if not has_course_author_access(request.user, course_key):
+        raise PermissionDenied()
+
+    # The task status record is authoritative once it's been created
+    task_status = _latest_task_status(request, course_key_string, export_status_handler)
+    output_url = None
+    error = None
+    if task_status is None:
+        # The task hasn't been initialized yet; did we store info in the session already?
+        try:
+            session_status = request.session["export_status"]
+            status = session_status[course_key_string]
+        except KeyError:
+            status = 0
+    elif task_status.state == UserTaskStatus.SUCCEEDED:
+        status = 3
+        artifact = UserTaskArtifact.objects.get(status=task_status, name='Output')
+        if hasattr(artifact.file.storage, 'bucket'):
+            filename = os.path.basename(artifact.file.name).encode('utf-8')
+            disposition = 'attachment; filename="{}"'.format(filename)
+            output_url = artifact.file.storage.url(artifact.file.name, response_headers={
+                'response-content-disposition': disposition,
+                'response-content-encoding': 'application/octet-stream',
+                'response-content-type': 'application/x-tgz'
+            })
+        else:
+            # local file, serve from the authorization wrapper view
+            output_url = reverse_course_url('export_output_handler', course_key)
+    elif task_status.state in (UserTaskStatus.FAILED, UserTaskStatus.CANCELED):
+        status = max(-(task_status.completed_steps + 1), -2)
+        errors = UserTaskArtifact.objects.filter(status=task_status, name='Error')
+        if len(errors):
+            error = errors[0].text
+            try:
+                error = json.loads(error)
+            except ValueError:
+                # Wasn't JSON, just use the value as a string
+                pass
+    else:
+        status = min(task_status.completed_steps + 1, 2)
+
+    response = {"ExportStatus": status}
+    if output_url:
+        response['ExportOutput'] = output_url
+    elif error:
+        response['ExportError'] = error
+    return JsonResponse(response)
+
+
+@transaction.non_atomic_requests
+@require_GET
+@ensure_csrf_cookie
+@login_required
+@ensure_valid_course_key
+def export_output_handler(request, course_key_string):
+    """
+    Returns the OLX .tar.gz produced by a file export.  Only used in
+    environments such as devstack where the output is stored in a local
+    filesystem instead of an external service like S3.
+    """
+    course_key = CourseKey.from_string(course_key_string)
+    if not has_course_author_access(request.user, course_key):
+        raise PermissionDenied()
+
+    task_status = _latest_task_status(request, course_key_string, export_output_handler)
+    if task_status and task_status.state == UserTaskStatus.SUCCEEDED:
+        artifact = None
+        try:
+            artifact = UserTaskArtifact.objects.get(status=task_status, name='Output')
+            tarball = course_import_export_storage.open(artifact.file.name)
+            return send_tarball(tarball)
+        except UserTaskArtifact.DoesNotExist:
+            raise Http404
+        finally:
+            if artifact:
+                artifact.file.close()
+    else:
+        raise Http404
+
+
+def _latest_task_status(request, course_key_string, view_func=None):
+    """
+    Get the most recent export status update for the specified course/library
+    key.
+    """
+    args = {u'course_key_string': course_key_string}
+    name = CourseExportTask.generate_name(args)
+    task_status = UserTaskStatus.objects.filter(name=name)
+    for status_filter in STATUS_FILTERS:
+        task_status = status_filter().filter_queryset(request, task_status, view_func)
+    return task_status.order_by(u'-created').first()
