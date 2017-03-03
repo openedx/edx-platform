fa74e0bf1939be84e3b5e820e9928596add73b18
Michael Youngstrom <myoungstrom@edx.org>
2017-03-03 18:52:52 +0000

    Revert Django renderer change
    Force run on PR
    
    Remove force run on PR

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2188,9 +2188,6 @@ CSRF_COOKIE_SECURE = False
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'openedx.core.lib.api.paginators.DefaultPagination',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
     'PAGE_SIZE': 10,
     'URL_FORMAT_OVERRIDE': None,
     'DEFAULT_THROTTLE_RATES': {
