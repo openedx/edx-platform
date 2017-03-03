f9a8dd47730dff8dfceba9b00e3d080911c21af9
Diana Huang <dkh@edx.org>
2017-03-03 10:03:21 -0500

    Update query counts to account for Waffle flag.

--- lms/djangoapps/courseware/tests/test_course_outline_views.py
+++ lms/djangoapps/courseware/tests/test_course_outline_views.py
@@ -1,14 +1,13 @@
 """
 Tests for the Course Outline view and supporting views.
 """
-from django.conf import settings
 from django.core.urlresolvers import reverse
 
 from student.models import CourseEnrollment
 from student.tests.factories import UserFactory
 
 from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
-from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory, check_mongo_calls
+from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
 
 
 class TestCourseOutlinePage(SharedModuleStoreTestCase):
@@ -21,32 +20,53 @@ class TestCourseOutlinePage(SharedModuleStoreTestCase):
         # setUpClassAndTestData() already calls setUpClass on SharedModuleStoreTestCase
         # pylint: disable=super-method-not-called
         with super(TestCourseOutlinePage, cls).setUpClassAndTestData():
-            cls.course = CourseFactory.create()
-            with cls.store.bulk_operations(cls.course.id):
-                cls.chapter = ItemFactory.create(category='chapter', parent_location=cls.course.location)
-                cls.section = ItemFactory.create(category='sequential', parent_location=cls.chapter.location)
-                cls.vertical = ItemFactory.create(category='vertical', parent_location=cls.section.location)
+            cls.courses = []
+            course = CourseFactory.create()
+            with cls.store.bulk_operations(course.id):
+                chapter = ItemFactory.create(category='chapter', parent_location=course.location)
+                section = ItemFactory.create(category='sequential', parent_location=chapter.location)
+                ItemFactory.create(category='vertical', parent_location=section.location)
+
+            cls.courses.append(course)
+
+            course = CourseFactory.create()
+            with cls.store.bulk_operations(course.id):
+                chapter = ItemFactory.create(category='chapter', parent_location=course.location)
+                section = ItemFactory.create(category='sequential', parent_location=chapter.location)
+                section2 = ItemFactory.create(category='sequential', parent_location=chapter.location)
+                ItemFactory.create(category='vertical', parent_location=section.location)
+                ItemFactory.create(category='vertical', parent_location=section2.location)
 
     @classmethod
     def setUpTestData(cls):
         """Set up and enroll our fake user in the course."""
         cls.password = 'test'
         cls.user = UserFactory(password=cls.password)
-        CourseEnrollment.enroll(cls.user, cls.course.id)
+        for course in cls.courses:
+            CourseEnrollment.enroll(cls.user, course.id)
 
     def setUp(self):
+        """
+        Set up for the tests.
+        """
+        super(TestCourseOutlinePage, self).setUp()
         self.client.login(username=self.user.username, password=self.password)
 
     def test_render(self):
-        url = reverse(
-            'unified_course_view',
-            kwargs={
-                'course_id': unicode(self.course.id),
-            }
-        )
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        response_content = response.content.decode("utf-8")
-        self.assertIn(self.chapter.display_name, response_content)
-        self.assertIn(self.section.display_name, response_content)
-        self.assertNotIn(self.vertical.display_name, response_content)
+        for course in self.courses:
+            url = reverse(
+                'unified_course_view',
+                kwargs={
+                    'course_id': unicode(course.id),
+                }
+            )
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            response_content = response.content.decode("utf-8")
+
+            for chapter in course.children:
+                self.assertIn(chapter.display_name, response_content)
+                for section in chapter.children:
+                    self.assertIn(section.display_name, response_content)
+                    for vertical in section.children:
+                        self.assertNotIn(vertical.display_name, response_content)
