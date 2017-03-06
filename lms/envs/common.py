9f73ce69ef3a85c23fc9d483b5a6427b26b0c86b
Andy Armstrong <andya@edx.org>
2017-03-06 17:32:52 -0500

    Make course_experience a feature

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1736,7 +1736,6 @@ REQUIRE_JS_PATH_OVERRIDES = {
     'js/student_account/logistration_factory': 'js/student_account/logistration_factory.js',
     'js/student_profile/views/learner_profile_factory': 'js/student_profile/views/learner_profile_factory.js',
     'js/courseware/courseware_factory': 'js/courseware/courseware_factory.js',
-    'js/courseware/course_outline_factory': 'js/courseware/course_outline_factory.js',
     'js/groups/views/cohorts_dashboard_factory': 'js/groups/views/cohorts_dashboard_factory.js',
     'draggabilly': 'js/vendor/draggabilly.js'
 }
@@ -2174,6 +2173,9 @@ INSTALLED_APPS = (
 
     # Unusual migrations
     'database_fixups',
+
+    # Features
+    'openedx.features.course_experience',
 )
 
 ######################### CSRF #########################################
