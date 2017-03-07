6f2b9e87e65df081debb354fae0022ffa41b38ba
Filip Jukic <filip@jukic.me>
2017-03-07 12:35:37 +0100

    run circle tests with appsembler settings

--- scripts/circle-ci-tests.sh
+++ scripts/circle-ci-tests.sh
@@ -75,11 +75,11 @@ else
             ;;
 
         1)  # run all of the lms unit tests
-            paver test_system -s lms --with-flaky --cov-args="-p" --with-xunitmp
+            paver test_system -s lms --with-flaky --cov-args="-p" --with-xunitmp --settings=test_appsembler
             ;;
 
         2)  # run all of the cms unit tests
-            paver test_system -s cms --with-flaky --cov-args="-p" --with-xunitmp
+            paver test_system -s cms --with-flaky --cov-args="-p" --with-xunitmp --settings=test_appsembler
             ;;
 
         3)  # run the commonlib unit tests
