748a00c36c73ea634a9993ea5cb3b35a48347915
Zia Fazal <zia.fazal@arbisoft.com>
2017-03-09 12:15:45 +0500

    removed test sqlite related stuff

--- test_root/pysqlite-2.8.3/doc/includes/sqlite3/shared_cache.py
+++
@@ -1,6 +0,0 @@
-from pysqlite2 import dbapi2 as sqlite3
-
-# The shared cache is only available in SQLite versions 3.3.3 or later
-# See the SQLite documentaton for details.
-
-sqlite3.enable_shared_cache(True)
