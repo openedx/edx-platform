c53e0cafcb9d61b9b2e900c08798575df784f4c7
Brian Jacobel <bjacobel@gmail.com>
2017-03-13 18:21:03 -0400

    Webpack in LMS

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1750,6 +1750,16 @@ REQUIRE_JS_PATH_OVERRIDES = {
     'hls': 'common/js/vendor/hls.js'
 }
 
+########################## DJANGO WEBPACK LOADER ##############################
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(REPO_ROOT, 'webpack-stats.json'),
+    }
+}
+
+
 ########################## DJANGO DEBUG TOOLBAR ###############################
 
 # We don't enable Django Debug Toolbar universally, but whenever we do, we want
@@ -1948,6 +1958,7 @@ INSTALLED_APPS = (
     'edxmako',
     'pipeline',
     'static_replace',
+    'webpack_loader',
 
     # For user interface plugins
     'web_fragments',
