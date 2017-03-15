86bdd5b6e1f44abd8eb83ae1238980f8e4049788
Zia Fazal <zia.fazal@arbisoft.com>
2017-03-15 20:18:44 +0500

    merging 7707087...refactored discussion_course xblock to make it compatible with ficus
    modified discussion router to change root url as per apros

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1385,10 +1385,6 @@ discussion_js = (
     sorted(rooted_glob(COMMON_ROOT / 'static', 'common/js/discussion/**/*.js'))
 )
 
-discussion_forum_js = [
-    'js/discussion_forum.js',
-]
-
 discussion_vendor_js = [
     'js/vendor/edx_ui_toolkit_shim.js',
     'js/vendor/i18n_shim.js',
@@ -1634,7 +1630,7 @@ PIPELINE_CSS = {
 }
 
 
-separately_bundled_js = set(courseware_js + discussion_js + discussion_forum_js + notes_js + instructor_dash_js)
+separately_bundled_js = set(courseware_js + discussion_js + notes_js + instructor_dash_js)
 common_js = sorted(set(rooted_glob(COMMON_ROOT / 'static', 'coffee/src/**/*.js')) - separately_bundled_js)
 xblock_runtime_js = [
     'common/js/xblock/core.js',
@@ -1698,10 +1694,6 @@ PIPELINE_JS = {
     'discussion_vendor': {
         'source_filenames': discussion_vendor_js,
         'output_filename': 'js/discussion_vendor.js',
-    },
-    'discussion_forum': {
-        'source_filenames': discussion_forum_js,
-        'output_filename': 'js/discussion_forum.js',
     },
     'notes': {
         'source_filenames': notes_js,
--- openedx/core/lib/xblock_builtin/xblock_discussion_course/static/js/discussion_course.js
+++ openedx/core/lib/xblock_builtin/xblock_discussion_course/static/js/discussion_course.js
@@ -1,16 +1,85 @@
 /* globals window, $$course_id, DiscussionUtil */
 function DiscussionCourseBlock(runtime, element) {
     'use strict';
-    var hasPushState = window.history && window.history.pushState ? true : false;
+    var $discussionContainer = $("#discussion-container");
     DiscussionUtil.force_async = true;
 
-    // Restart the Backbone router to use the discussion root when
-    // links inside this component are clicked.
+    // stop history if it is already started.
     if (Backbone.History.started) {
         Backbone.history.stop();
     }
-    Backbone.history.start({
-        pushState: hasPushState,
-        root: "/courses/" + $$course_id + "/discussion/"
-    });
+    function defineDiscussionClasses(define) {
+        'use strict';
+        var discussionClasses = [
+            ['Content', 'common/js/discussion/content'],
+            ['Discussion', 'common/js/discussion/discussion'],
+            ['DiscussionModuleView', 'common/js/discussion/discussion_module_view'],
+            ['DiscussionThreadView', 'common/js/discussion/views/discussion_thread_view'],
+            ['DiscussionThreadListView', 'common/js/discussion/views/discussion_thread_list_view'],
+            ['DiscussionThreadProfileView', 'common/js/discussion/views/discussion_thread_profile_view'],
+            ['DiscussionTopicMenuView', 'common/js/discussion/views/discussion_topic_menu_view'],
+            ['DiscussionUtil', 'common/js/discussion/utils'],
+            ['DiscussionCourseSettings', 'common/js/discussion/models/discussion_course_settings'],
+            ['DiscussionUser', 'common/js/discussion/models/discussion_user'],
+            ['NewPostView', 'common/js/discussion/views/new_post_view']
+        ];
+
+        discussionClasses.forEach(function(discussionClass) {
+            define(
+                discussionClass[1],
+                [],
+                function() {
+                    return window[discussionClass[0]];
+                }
+            );
+        });
+    };
+
+    function initializeDiscussion() {
+
+        if (typeof RequireJS === 'undefined') {
+            var vendorScript = document.createElement("script");
+            vendorScript.onload = function() {
+                require.config({
+                    baseUrl: "/static/",
+                });
+
+                var requireConfigScript = document.createElement("script");
+                requireConfigScript.onload = function() {
+                    defineDiscussionClasses(define);
+                    initializeDiscussionBoardFactory(require)
+                };
+                requireConfigScript.src = "/static/lms/js/require-config.js";
+                document.body.appendChild(requireConfigScript);
+            };
+            vendorScript.src = "/static/common/js/vendor/require.js";
+            document.body.appendChild(vendorScript);
+
+        } else {
+            defineDiscussionClasses(RequireJS.define);
+            initializeDiscussionBoardFactory(RequireJS.require);
+        }
+    };
+
+    function initializeDiscussionBoardFactory(require) {
+        require(
+            ['discussion/js/discussion_board_factory'],
+            function (DiscussionBoardFactory) {
+                DiscussionBoardFactory({
+                    courseId: $discussionContainer.data('courseId'),
+                    course_name: $discussionContainer.data('course-name'),
+                    $el: $discussionContainer,
+                    user_info: $discussionContainer.data('userInfo'),
+                    roles: $discussionContainer.data('roles'),
+                    sort_preference: $discussionContainer.data('sortPreference'),
+                    threads: $discussionContainer.data('threads'),
+                    thread_pages: $discussionContainer.data('threadPages'),
+                    content_info: $discussionContainer.data('contentInfo'),
+                    course_settings: $discussionContainer.data('courseSettings')
+                });
+            }
+        )
+    };
+
+    initializeDiscussion();
 }
