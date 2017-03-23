825ff19d284dc29fd0835f57a4bfa4c14aaefab3
Eric Fischer <efischer@edx.org>
2017-03-23 16:43:31 -0400

    eslint fixes

--- lms/static/js/instructor_dashboard/open_response_assessment.js
+++ lms/static/js/instructor_dashboard/open_response_assessment.js
@@ -3,25 +3,26 @@
 (function(_) {
     'use strict';
 
-    if (typeof window.setup_debug === 'undefined') {
-        window.setup_debug = function(element_id, edit_link, staff_context) {
-            // stub function
-        }
-    }
-
     var OpenResponseAssessment = (function() {
-        function OpenResponseAssessment($section) {
+        function OpenResponseAssessmentBlock($section) {
             this.$section = $section;
             this.$section.data('wrapper', this);
         }
 
-        OpenResponseAssessment.prototype.onClickTitle = function() {
+        OpenResponseAssessmentBlock.prototype.onClickTitle = function() {
             var block = this.$section.find('.open-response-assessment');
             XBlock.initializeBlock($(block).find('.xblock')[0]);
         };
 
-        return OpenResponseAssessment;
+        return OpenResponseAssessmentBlock;
     }());
+
+    if (typeof window.setup_debug === 'undefined') {
+        // eslint-disable-next-line no-unused-vars, camelcase
+        window.setup_debug = function(element_id, edit_link, staff_context) {
+            // stub function
+        };
+    }
 
     _.defaults(window, {
         InstructorDashboard: {}
