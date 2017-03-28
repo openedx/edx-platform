c29dddc87dabaaa9b2eb3e7eea657e2547a4f516
Matt Drayer <mattdrayer@edx.org>
2017-03-28 16:00:26 -0400

    Add SAML metadata refresh control flag
    mattdrayer: Change model fieldname, revise code, fix bad tests.

--- common/djangoapps/third_party_auth/tests/specs/test_testshib.py
+++ common/djangoapps/third_party_auth/tests/specs/test_testshib.py
@@ -149,11 +149,13 @@ class TestShibIntegrationTest(IntegrationTestMixin, testutil.SAMLTestCase):
         kwargs.setdefault('attr_email', 'urn:oid:1.3.6.1.4.1.5923.1.1.1.6')  # eduPersonPrincipalName
         self.configure_saml_provider(**kwargs)
         self.assertTrue(httpretty.is_enabled())
-        num_changed, num_failed, num_total, failure_messages = fetch_saml_metadata()
+        num_total, num_skipped, num_attempted, num_updated, num_failed, failure_messages = fetch_saml_metadata()
+        self.assertEqual(num_total, 1)
+        self.assertEqual(num_skipped, 0)
+        self.assertEqual(num_attempted, 1)
+        self.assertEqual(num_updated, 1)
         self.assertEqual(num_failed, 0)
         self.assertEqual(len(failure_messages), 0)
-        self.assertEqual(num_changed, 1)
-        self.assertEqual(num_total, 1)
 
     def _freeze_time(self, timestamp):
         """ Mock the current time for SAML, so we can replay canned requests/responses """
@@ -177,12 +179,14 @@ class TestShibIntegrationTest(IntegrationTestMixin, testutil.SAMLTestCase):
 
         if fetch_metadata:
             self.assertTrue(httpretty.is_enabled())
-            num_changed, num_failed, num_total, failure_messages = fetch_saml_metadata()
+            num_total, num_skipped, num_attempted, num_updated, num_failed, failure_messages = fetch_saml_metadata()
             if assert_metadata_updates:
+                self.assertEqual(num_total, 1)
+                self.assertEqual(num_skipped, 0)
+                self.assertEqual(num_attempted, 1)
+                self.assertEqual(num_updated, 1)
                 self.assertEqual(num_failed, 0)
                 self.assertEqual(len(failure_messages), 0)
-                self.assertEqual(num_changed, 1)
-                self.assertEqual(num_total, 1)
 
     def do_provider_login(self, provider_redirect_url):
         """ Mocked: the user logs in to TestShib and then gets redirected back """
