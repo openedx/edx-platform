78b2978c3e0e56c4c75a3a6b532e02c995ca69ed
Miro Dojkic <mdojkic@gmail.com>
2017-03-28 12:57:54 +0200

    Remove unused import and redundant comment

--- openedx/core/djangoapps/user_api/permissions/views.py
+++ openedx/core/djangoapps/user_api/permissions/views.py
@@ -1,24 +1,12 @@
-"""
-NOTE: this API is WIP and has not yet been approved. Do not use this API
-without talking to Christina or Andy.
-
-For more information, see:
-https://openedx.atlassian.net/wiki/display/TNL/User+API
-"""
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import permissions
-
-from django.db import transaction
-from django.utils.translation import ugettext as _
 
 from openedx.core.lib.api.authentication import (
     SessionAuthenticationAllowInactiveUser,
     OAuth2AuthenticationAllowInactiveUser,
 )
 from openedx.core.lib.api.parsers import MergePatchParser
-from openedx.core.lib.api.permissions import IsUserInUrlOrStaff
 from ..errors import UserNotFound, UserNotAuthorized
 
 
