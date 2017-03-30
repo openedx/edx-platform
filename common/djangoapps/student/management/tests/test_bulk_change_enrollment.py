55174a0ed49b8fb72d615d1fb24602ccc55537c2
Afzal Wali <afzal@edx.org>
2017-03-30 20:01:42 +0500

    WL-1016 | Added org argument for bulk_change_enrollment management command.

--- common/djangoapps/student/management/tests/test_bulk_change_enrollment.py
+++ common/djangoapps/student/management/tests/test_bulk_change_enrollment.py
@@ -9,6 +9,8 @@ from student.models import CourseEnrollment, EVENT_NAME_ENROLLMENT_MODE_CHANGED
 from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
 
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+
 
 @ddt.ddt
 class BulkChangeEnrollmentTests(SharedModuleStoreTestCase):
@@ -16,8 +18,10 @@ class BulkChangeEnrollmentTests(SharedModuleStoreTestCase):
 
     def setUp(self):
         super(BulkChangeEnrollmentTests, self).setUp()
-        self.course = CourseFactory.create()
+        self.org = 'testX'
+        self.course = CourseFactory.create(org=self.org)
         self.users = UserFactory.create_batch(5)
+        CourseOverview.load_from_module_store(self.course.id)
 
     @patch('student.models.tracker')
     @ddt.data(('audit', 'honor'), ('honor', 'audit'))
@@ -51,6 +55,69 @@ class BulkChangeEnrollmentTests(SharedModuleStoreTestCase):
                         {'course_id': unicode(self.course.id), 'user_id': user.id, 'mode': to_mode}
                     ),
                 ]
+            )
+
+    @patch('student.models.tracker')
+    @ddt.data(('audit', 'no-id-professional'), ('no-id-professional', 'audit'))
+    @ddt.unpack
+    def test_bulk_convert_with_org(self, from_mode, to_mode, mock_tracker):
+        """Verify that enrollments are changed correctly when org was given."""
+        self._enroll_users(from_mode)
+        CourseModeFactory(course_id=self.course.id, mode_slug=to_mode)
+
+        # Verify that no users are in the `from` mode yet.
+        self.assertEqual(len(CourseEnrollment.objects.filter(mode=to_mode, course_id=self.course.id)), 0)
+
+        call_command(
+            'bulk_change_enrollment',
+            org=self.org,
+            from_mode=from_mode,
+            to_mode=to_mode,
+            commit=True,
+        )
+
+        # Verify that all users have been moved -- if not, this will
+        # raise CourseEnrollment.DoesNotExist
+        for user in self.users:
+            CourseEnrollment.objects.get(mode=to_mode, course_id=self.course.id, user=user)
+
+            # Confirm the analytics event was emitted.
+            mock_tracker.emit.assert_has_calls(  # pylint: disable=maybe-no-member
+                [
+                    call(
+                        EVENT_NAME_ENROLLMENT_MODE_CHANGED,
+                        {'course_id': unicode(self.course.id), 'user_id': user.id, 'mode': to_mode}
+                    ),
+                ]
+            )
+
+    def test_with_org_and_course_key(self):
+        """Verify that command raises CommandError when `org` and `course_key` both are given."""
+        self._enroll_users('audit')
+        CourseModeFactory(course_id=self.course.id, mode_slug='no-id-professional')
+
+        with self.assertRaises(CommandError):
+            call_command(
+                'bulk_change_enrollment',
+                org=self.org,
+                course=unicode(self.course.id),
+                from_mode='audit',
+                to_mode='no-id-professional',
+                commit=True,
+            )
+
+    def test_with_invalid_org(self):
+        """Verify that command raises CommandError when invalid `org` is given."""
+        self._enroll_users('audit')
+        CourseModeFactory(course_id=self.course.id, mode_slug='no-id-professional')
+
+        with self.assertRaises(CommandError):
+            call_command(
+                'bulk_change_enrollment',
+                org='fakeX',
+                from_mode='audit',
+                to_mode='no-id-professional',
+                commit=True,
             )
 
     def test_without_commit(self):
