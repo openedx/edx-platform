cf4bd15a55a87f99c0827c58da6421d3ed36d965
Clinton Blackburn <cblackburn@edx.org>
2017-04-04 17:24:23 -0400

    Updated program filter for learner dashboard
    Learners will now see both active and retired programs on their
    dashboards.
    
    ECOM-7625

--- openedx/core/djangoapps/catalog/utils.py
+++ openedx/core/djangoapps/catalog/utils.py
@@ -2,6 +2,7 @@
 import copy
 import logging
 
+import waffle
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from edx_rest_api_client.client import EdxRestApiClient
@@ -54,11 +55,18 @@ def get_programs(uuid=None, types=None):  # pylint: disable=redefined-builtin
         )
 
         querystring = {
-            'marketable': 1,
             'exclude_utm': 1,
         }
+
+        # TODO ECOM-7650: Remove this after https://github.com/edx/course-discovery/pull/805 is merged and released.
+        if waffle.switch_is_active('display_retired_programs_on_learner_dashboard'):
+            querystring['status'] = ('active', 'retired',)
+        else:
+            querystring['marketable'] = 1
+
         if uuid:
             querystring['use_full_course_serializer'] = 1
+
         if types_param:
             querystring['types'] = types_param
 
