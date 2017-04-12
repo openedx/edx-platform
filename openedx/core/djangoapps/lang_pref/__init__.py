2bd0bc12fcd79d1769d03039ad05a3f74e0add9b
Calen Pennington <cale@edx.org>
2017-04-12 11:14:14 -0400

    Store Learner language preferences into a cookie rather than the session
    This modifies the lang-pref django app to:
    a) Use the current value of the 'edx-language-preference' cookie to set the
       users Accept-Language header on an incoming request.
    b) At the end of the request, update the 'edx-language-preference' cookie
       to reflect the users current Language Preference choice, if any.
    
    [LEARNER-542]

--- openedx/core/djangoapps/lang_pref/__init__.py
+++ openedx/core/djangoapps/lang_pref/__init__.py
@@ -4,3 +4,9 @@ Useful information for setting the language preference
 
 # this is the UserPreference key for the user's preferred language
 LANGUAGE_KEY = 'pref-lang'
+
+LANGUAGE_COOKIE = 'edx-language-preference'
+
+LANGUAGE_HEADER = 'HTTP_ACCEPT_LANGUAGE'
+
+COOKIE_DURATION = 14 * 24 * 60 * 60  # 14 days in seconds
--- openedx/core/djangoapps/lang_pref/middleware.py
+++ openedx/core/djangoapps/lang_pref/middleware.py
@@ -2,12 +2,17 @@
 Middleware for Language Preferences
 """
 
+from django.conf import settings
 from django.utils.translation import LANGUAGE_SESSION_KEY
 from django.utils.translation.trans_real import parse_accept_lang_header
 
-from openedx.core.djangoapps.lang_pref import LANGUAGE_KEY
-from openedx.core.djangoapps.lang_pref.api import released_languages
-from openedx.core.djangoapps.user_api.preferences.api import get_user_preference, delete_user_preference
+from openedx.core.djangoapps.lang_pref import (
+    LANGUAGE_KEY, LANGUAGE_COOKIE, LANGUAGE_HEADER, COOKIE_DURATION
+)
+from openedx.core.djangoapps.user_api.preferences.api import (
+    get_user_preference, delete_user_preference, set_user_preference
+)
+from openedx.core.djangoapps.user_api.errors import UserAPIInternalError, UserAPIRequestError
 
 
 class LanguagePreferenceMiddleware(object):
@@ -21,17 +26,46 @@ class LanguagePreferenceMiddleware(object):
     def process_request(self, request):
         """
         If a user's UserPreference contains a language preference, use the user's preference.
+        Save the current language preference cookie as the user's preferred language.
         """
-        languages = released_languages()
-        system_released_languages = [seq[0] for seq in languages]
+        cookie_lang = request.COOKIES.get(LANGUAGE_COOKIE, None)
+        if cookie_lang:
+            if request.user.is_authenticated():
+                set_user_preference(request.user, LANGUAGE_KEY, cookie_lang)
 
+            accept_header = request.META.get(LANGUAGE_HEADER, None)
+            if accept_header:
+                current_langs = parse_accept_lang_header(accept_header)
+                # Promote the cookie_lang over any language currently in the accept header
+                current_langs = [(lang, qvalue) for (lang, qvalue) in current_langs if lang != cookie_lang]
+                current_langs.insert(0, (cookie_lang, 1))
+                accept_header = ",".join("{};q={}".format(lang, qvalue) for (lang, qvalue) in current_langs)
+            else:
+                accept_header = cookie_lang
+            request.META[LANGUAGE_HEADER] = accept_header
+
+    def process_response(self, request, response):
         # If the user is logged in, check for their language preference
-        if request.user.is_authenticated():
+        if getattr(request, 'user', None) and request.user.is_authenticated():
             # Get the user's language preference
-            user_pref = get_user_preference(request.user, LANGUAGE_KEY)
-            # Set it to the LANGUAGE_SESSION_KEY (Django-specific session setting governing language pref)
-            if user_pref:
-                if user_pref in system_released_languages:
-                    request.session[LANGUAGE_SESSION_KEY] = user_pref
+            try:
+                user_pref = get_user_preference(request.user, LANGUAGE_KEY)
+            except (UserAPIRequestError, UserAPIInternalError):
+                # If we can't find the user preferences, then don't modify the cookie
+                pass
+            else:
+                # Set it in the LANGUAGE_COOKIE
+                if user_pref:
+                    response.set_cookie(
+                        LANGUAGE_COOKIE,
+                        value=user_pref,
+                        domain=settings.SESSION_COOKIE_DOMAIN,
+                        max_age=COOKIE_DURATION,
+                    )
                 else:
-                    delete_user_preference(request.user, LANGUAGE_KEY)
+                    response.delete_cookie(
+                        LANGUAGE_COOKIE,
+                        domain=settings.SESSION_COOKIE_DOMAIN
+                    )
+
+        return response
