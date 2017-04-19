3aa061d4ebf526d993bcc06eb6e1ab9efc683687
John Eskew <jeskew@edx.org>
2017-04-19 15:15:54 -0400

    Add model migration files for crawlers/programs.

---
+++ openedx/core/djangoapps/crawlers/migrations/0002_auto_20170419_0018.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crawlers', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='crawlersconfig',
+            options={},
+        ),
+    ]
