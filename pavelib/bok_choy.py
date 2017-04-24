7c4d167639cdf0c9176a8f0dce8dbd14f6785356
Ari Rizzitano <arizzitano@edx.org>
2017-04-23 23:33:36 -0400

    blahhh

--- pavelib/bok_choy.py
+++ pavelib/bok_choy.py
@@ -124,7 +124,9 @@ def pa11ycrawler(options, passthrough_options):
         'should_fetch_course',
         not options.get('fasttest')
     )
+    print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     print options
+    print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     options.pa11ycrawler.course_key = getattr(options, 'course-key', "course-v1:edX+Test101+course")
     test_suite = Pa11yCrawler('pa11ycrawler', passthrough_options=passthrough_options, **options.pa11ycrawler)
     test_suite.run()
