1fbac8d9e3a275e8b6711d431d7d7773373c83f3
Filip Jukic <filip@jukic.me>
2017-04-25 14:57:48 +0200

    skip sass compilation for default site

--- openedx/core/djangoapps/appsembler/sites/management/commands/recompile_site_sass.py
+++ openedx/core/djangoapps/appsembler/sites/management/commands/recompile_site_sass.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
@@ -7,5 +8,5 @@ class Command(BaseCommand):
     help = "Recompiles SASS for all microsites"
 
     def handle(self, *args, **options):
-        for site in SiteConfiguration.objects.all():
+        for site in SiteConfiguration.objects.exclude(site__id=settings.SITE_ID):
             site.compile_microsite_sass()
