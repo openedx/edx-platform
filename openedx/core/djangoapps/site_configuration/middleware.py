2d67a6c2a2cd67040b88bb52b1d76819bdd4e45d
Kabir Khan <kakh@microsoft.com>
2017-05-08 19:04:41 +0000

    Added middleware that can be configured through Site Configuration to restrict a particular sites pages to logged in users.

--- openedx/core/djangoapps/site_configuration/middleware.py
+++ openedx/core/djangoapps/site_configuration/middleware.py
@@ -4,6 +4,9 @@ This file contains Django middleware related to the site_configuration app.
 
 from django.conf import settings
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from re import compile
 
 
 class SessionCookieDomainOverrideMiddleware(object):
@@ -53,3 +56,48 @@ class SessionCookieDomainOverrideMiddleware(object):
             response.set_cookie = _set_cookie_wrapper
 
         return response
+
+class LoginRequiredMiddleware:
+    """
+    Middleware that requires a user to be authenticated to view any page other
+    than LOGIN_URL. Exemptions to this requirement can optionally be specified
+    in settings via a list of regular expressions in LOGIN_EXEMPT_URLS (which
+    you can copy from any urls.py).
+    """
+
+    def __init__(self):
+        self.LOGIN_URL = settings.LOGIN_URL or '/login/'
+        self.LOGIN_EXEMPT_URLS = list()
+        self.RESTRICT_SITE_TO_LOGGED_IN_USERS = None
+        self.EXEMPT_URLS = [compile(self.LOGIN_URL.lstrip('/')), compile(r'^user_api/v1/account/.*$')]
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+
+        '''
+        If the site is configured to restrict not logged in users to the LOGIN_EXEMPT_URLS
+        from accessing pages, wrap the next view with the django login_required middleware
+        '''
+
+        if request.user.is_authenticated():
+            return None
+
+        self.RESTRICT_SITE_TO_LOGGED_IN_USERS = self.RESTRICT_SITE_TO_LOGGED_IN_USERS or configuration_helpers.get_value(
+            'RESTRICT_SITE_TO_LOGGED_IN_USERS',
+            settings.FEATURES.get('RESTRICT_SITE_TO_LOGGED_IN_USERS', False)
+        )
+
+        if self.RESTRICT_SITE_TO_LOGGED_IN_USERS:
+            self.LOGIN_EXEMPT_URLS = self.LOGIN_EXEMPT_URLS or configuration_helpers.get_value(
+                'LOGIN_EXEMPT_URLS',
+                getattr(settings, 'LOGIN_EXEMPT_URLS', None)
+            )
+
+            if self.LOGIN_EXEMPT_URLS:
+                if type(self.LOGIN_EXEMPT_URLS) is str or type(self.LOGIN_EXEMPT_URLS) is unicode:
+                    self.LOGIN_EXEMPT_URLS = [self.LOGIN_EXEMPT_URLS]
+                self.EXEMPT_URLS += [compile(expr) for expr in self.LOGIN_EXEMPT_URLS]
+            path = request.path_info.lstrip('/')
+            if not any(m.match(path) for m in self.EXEMPT_URLS):
+                return login_required(view_func)(request, view_args, view_kwargs)
+
+        return None
