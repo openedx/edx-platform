7268beba6823268f9dbf4562d3726de858b62fc7
Aamish Baloch <aamish.baloch@gmail.com>
2017-05-15 12:24:57 +0500

    YONK-643: Hyperlink is showing as text for learners is fixed

--- common/static/common/js/discussion/views/discussion_thread_list_view.js
+++ common/static/common/js/discussion/views/discussion_thread_list_view.js
@@ -215,6 +215,10 @@
                 return this;
             };
 
+            DiscussionThreadListView.prototype.convertMath = function() {
+                DiscussionUtil.convertMath(this.$('.thread-preview-body'));
+            };
+
             DiscussionThreadListView.prototype.renderThreads = function() {
                 var $content, thread, i, len;
                 this.$('.forum-nav-thread-list').empty();
@@ -222,7 +226,9 @@
                     thread = this.displayedCollection.models[i];
                     $content = this.renderThread(thread);
                     this.$('.forum-nav-thread-list').append($content);
+                    this.convertMath();
                 }
+
                 this.showMetadataAccordingToSort();
                 this.renderMorePages();
                 this.trigger('threads:rendered');
