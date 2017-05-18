64502fc2e2f06584cacbb5007b79f8ecbdc428cc
aamishbaloch <aamish.baloch@gmail.com>
2017-05-18 16:27:43 +0500

    Hyperlink issue fixed while reloading the content after replied post.

--- common/static/common/js/discussion/views/discussion_thread_list_view.js
+++ common/static/common/js/discussion/views/discussion_thread_list_view.js
@@ -165,6 +165,7 @@
                 $currentElement = this.$('.forum-nav-thread[data-id=' + threadId + ']');
                 active = $currentElement.has('.forum-nav-thread-link.is-active').length !== 0;
                 $currentElement.replaceWith($content);
+                this.convertMath($content);
                 this.showMetadataAccordingToSort();
                 if (this.supportsActiveThread && active) {
                     this.setActiveThread(threadId);
