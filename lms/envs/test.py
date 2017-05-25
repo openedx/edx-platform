a2124660d7015430649f37b8f8070fa130b953f5
Uman Shahzad <uman@opencraft.com>
2017-05-25 19:35:05 -0400

    Allow configurable password reset support link.
    Defaults to SUPPORT_SITE_LINK intentionally.
    Also fixed surrounding quality tests, and overall
    quality improvements.

--- lms/envs/test.py
+++ lms/envs/test.py
@@ -361,6 +361,7 @@ MKTG_URL_LINK_MAP = {
 }
 
 SUPPORT_SITE_LINK = 'https://support.example.com'
+PASSWORD_RESET_SUPPORT_LINK = 'https://support.example.com/password-reset-help.html'
 ACTIVATION_EMAIL_SUPPORT_LINK = 'https://support.example.com/activation-email-help.html'
 
 ############################ STATIC FILES #############################
