5fdb2a6789f9087beb224cc27e81a518a6c1b442
rabiaiftikhar <rabia.iftikhar@arbisoft.com>
2017-06-01 15:04:42 +0500

    get the course from CourseOverview table instead of Modulestore in certificates/signals.py.

--- lms/djangoapps/certificates/signals.py
+++ lms/djangoapps/certificates/signals.py
@@ -5,13 +5,13 @@ from celery.task import task
 from django.dispatch.dispatcher import receiver
 
 from certificates.models import CertificateGenerationCourseSetting
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview, COURSE_OVERVIEW_UPDATED
 from opaque_keys.edx.keys import CourseKey
-from xmodule.modulestore.django import modulestore, SignalHandler
 
 
-@receiver(SignalHandler.course_published)
+@receiver(COURSE_OVERVIEW_UPDATED)
 def _listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=unused-argument
-    """ Catches the signal that a course has been published in Studio and
+    """ Catches the signal that a course overview table has been updated in database and
     enable/disable the self-generated certificates according to course-pacing.
     """
     set_self_generated_certs.delay(unicode(course_key))
@@ -23,5 +23,5 @@ def set_self_generated_certs(course_key):
     Enable or disable self-generated certificates for a course according to pacing.
     """
     course_key = CourseKey.from_string(course_key)
-    course = modulestore().get_course(course_key)
+    course = CourseOverview.get_from_id(course_key)
     CertificateGenerationCourseSetting.set_enabled_for_course(course_key, course.self_paced)
