45c5ab409496611112aa1b20d9b5a21b0130881b
Mushtaq Ali <mushtaque@edx.org>
2017-06-08 17:32:09 +0500

    Add video image settings to aws settings so that lms can pull values from lms.env.json

--- cms/djangoapps/contentstore/views/videos.py
+++ cms/djangoapps/contentstore/views/videos.py
@@ -186,14 +186,14 @@ def validate_video_image(image_file):
         except TypeError:
             return _('This image file is corrupted.')
         image_file_aspect_ratio = abs(image_file_width / float(image_file_height) - settings.VIDEO_IMAGE_ASPECT_RATIO)
-        if image_file_aspect_ratio > settings.VIDEO_IMAGE_ASPECT_RATIO_ERROR_MARGIN:
-            error = _('This image file must have an aspect ratio of {video_image_aspect_ratio_text}.').format(
-                video_image_aspect_ratio_text=settings.VIDEO_IMAGE_ASPECT_RATIO_TEXT
-            )
-        elif image_file_width < settings.VIDEO_IMAGE_MIN_WIDTH or image_file_height < settings.VIDEO_IMAGE_MIN_HEIGHT:
+        if image_file_width < settings.VIDEO_IMAGE_MIN_WIDTH or image_file_height < settings.VIDEO_IMAGE_MIN_HEIGHT:
             error = _('The minimum allowed image resolution is {image_file_min_width}x{image_file_min_height}.').format(
                 image_file_min_width=settings.VIDEO_IMAGE_MIN_WIDTH,
                 image_file_min_height=settings.VIDEO_IMAGE_MIN_HEIGHT
+            )
+        elif image_file_aspect_ratio > settings.VIDEO_IMAGE_ASPECT_RATIO_ERROR_MARGIN:
+            error = _('This image file must have an aspect ratio of {video_image_aspect_ratio_text}.').format(
+                video_image_aspect_ratio_text=settings.VIDEO_IMAGE_ASPECT_RATIO_TEXT
             )
         else:
             try:
