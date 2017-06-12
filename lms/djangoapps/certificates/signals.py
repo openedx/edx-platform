b8b6fc52ebfb568d5179b508824909781007d66f
rabia23 <riftikhar@edx.org>
2017-06-12 11:19:17 +0000

    fix coursekey error

--- lms/djangoapps/certificates/signals.py
+++ lms/djangoapps/certificates/signals.py
@@ -3,6 +3,7 @@ Signal handler for enabling/disabling self-generated certificates based on the c
 """
 from celery.task import task
 from django.dispatch import receiver
+from opaque_keys.edx.keys import CourseKey
 
 from certificates.models import CertificateGenerationCourseSetting
 from openedx.core.djangoapps.models.course_details import COURSE_PACING_CHANGE
@@ -14,7 +15,7 @@ def _listen_for_course_publish(sender, course_key, course_self_paced, **kwargs):
     Catches the signal that course pacing has changed and enable/disable
     the self-generated certificates according to course-pacing.
     """
-    enable_self_generated_certs.delay(course_key, course_self_paced)
+    enable_self_generated_certs.delay(unicode(course_key), course_self_paced)
 
 
 @task()
@@ -22,4 +23,5 @@ def enable_self_generated_certs(course_key, course_self_paced):
     """
     Enable or disable self-generated certificates for a course according to pacing.
     """
+    course_key = CourseKey.from_string(course_key)
     CertificateGenerationCourseSetting.set_enabled_for_course(course_key, course_self_paced)
