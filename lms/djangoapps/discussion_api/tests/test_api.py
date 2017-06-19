75a8e5f9c0c890105acfce4e4d45734eff973701
Kyle McCormick <kmccormick@edx.org>
2017-06-19 12:54:17 -0400

    Truncate title in thread created event to 1000 chars
    EDUCATOR-341

--- lms/djangoapps/discussion_api/tests/test_api.py
+++ lms/djangoapps/discussion_api/tests/test_api.py
@@ -1427,6 +1427,45 @@ class CreateThreadTest(
         SharedModuleStoreTestCase,
         MockSignalHandlerMixin
 ):
+    LONG_TITLE = (
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. '
+        'Aenean commodo ligula eget dolor. Aenean massa. Cum sociis '
+        'natoque penatibus et magnis dis parturient montes, nascetur '
+        'ridiculus mus. Donec quam felis, ultricies nec, '
+        'pellentesque eu, pretium quis, sem. Nulla consequat massa '
+        'quis enim. Donec pede justo, fringilla vel, aliquet nec, '
+        'vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet '
+        'a, venenatis vitae, justo. Nullam dictum felis eu pede '
+        'mollis pretium. Integer tincidunt. Cras dapibus. Vivamus '
+        'elementum semper nisi. Aenean vulputate eleifend tellus. '
+        'Aenean leo ligula, porttitor eu, consequat vitae, eleifend '
+        'ac, enim. Aliquam lorem ante, dapibus in, viverra quis, '
+        'feugiat a, tellus. Phasellus viverra nulla ut metus varius '
+        'laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies '
+        'nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam '
+        'eget dui. Etiam rhoncus. Maecenas tempus, tellus eget '
+        'condimentum rhoncus, sem quam semper libero, sit amet '
+        'adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, '
+        'luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et '
+        'ante tincidunt tempus. Donec vitae sapien ut libero '
+        'venenatis faucibus. Nullam quis ante. Etiam sit amet orci '
+        'eget eros faucibus tincidunt. Duis leo. Sed fringilla '
+        'mauris sit amet nibh. Donec sodales sagittis magna. Sed '
+        'consequat, leo eget bibendum sodales, augue velit cursus '
+        'nunc, quis gravida magna mi a libero. Fusce vulputate '
+        'eleifend sapien. Vestibulum purus quam, scelerisque ut, '
+        'mollis sed, nonummy id, metus. Nullam accumsan lorem in '
+        'dui. Cras ultricies mi eu turpis hendrerit fringilla. '
+        'Vestibulum ante ipsum primis in faucibus orci luctus et '
+        'ultrices posuere cubilia Curae; In ac dui quis mi '
+        'consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu '
+        'tortor, suscipit eget, imperdiet nec, imperdiet iaculis, '
+        'ipsum. Sed aliquam ultrices mauris. Integer ante arcu, '
+        'accumsan a, consectetuer eget, posuere ut, mauris. Praesent '
+        'adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc '
+        'nonummy metus.'
+    )
+
     """Tests for create_thread"""
     @classmethod
     def setUpClass(cls):
@@ -1489,6 +1528,42 @@ class CreateThreadTest(
                 "group_id": None,
                 "thread_type": "discussion",
                 "title": "Test Title",
+                "title_truncated": False,
+                "anonymous": False,
+                "anonymous_to_peers": False,
+                "options": {"followed": False},
+                "id": "test_id",
+                "truncated": False,
+                "body": "Test body",
+                "url": "",
+                "user_forums_roles": [FORUM_ROLE_STUDENT],
+                "user_course_roles": [],
+            }
+        )
+
+    @mock.patch("eventtracking.tracker.emit")
+    def test_title_truncation(self, mock_emit):
+        data = self.minimal_data.copy()
+        data['title'] = self.LONG_TITLE
+
+        cs_thread = make_minimal_cs_thread({
+            "id": "test_id",
+            "username": self.user.username,
+            "read": True,
+        })
+        self.register_post_thread_response(cs_thread)
+        with self.assert_signal_sent(api, 'thread_created', sender=None, user=self.user, exclude_args=('post',)):
+            actual = create_thread(self.request, data)
+        event_name, event_data = mock_emit.call_args[0]
+        self.assertEqual(event_name, "edx.forum.thread.created")
+        self.assertEqual(
+            event_data,
+            {
+                "commentable_id": "test_topic",
+                "group_id": None,
+                "thread_type": "discussion",
+                "title": self.LONG_TITLE[:1000],
+                "title_truncated": True,
                 "anonymous": False,
                 "anonymous_to_peers": False,
                 "options": {"followed": False},
