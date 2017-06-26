501d62d0882a9689fad3f2e28f516250efa80a0b
Ari Rizzitano <arizzitano@edx.org>
2017-06-26 14:58:23 -0400

    remove some dead code

--- cms/static/js/features/header/UserMenu.js
+++ cms/static/js/features/header/UserMenu.js
@@ -2,32 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Dropdown from 'paragon/dist/Dropdown.js';
 
-class UserMenu extends React.Component {
-  static insertIf (property, entry) {
-    return property ? entry : [];
-  }
-
-  render() {
-    return (
-      <Dropdown
-        title={this.props.username}
-        menuItems={[
-          { label: gettext(this.props.studio_name), href: '/' },
-          ...UserMenu.insertIf(
-            this.props.is_global_staff,
-            { label: gettext('Maintenance'), href: this.props.maintenance_url }
-          ),
-          { label: gettext('Sign Out'), href: this.props.logout_url },
-        ]}
-      />
-    );
-  }
-}
-
 export class UserMenuRenderer {
   constructor({selector, context}) {
+    let menuItems = [
+      { label: gettext(context.studio_name), href: '/' },
+      { label: gettext('Sign Out'), href: context.logout_url },
+    ];
+
+    if (context.is_global_staff) {
+      menuItems.splice(1, 0,
+        { label: gettext('Maintenance'), href: context.maintenance_url }
+      );
+    }
+
     ReactDOM.render(
-      <UserMenu {...context} />,
+      <Dropdown {...context}
+        title={context.username}
+        menuItems={menuItems}
+      />,
       document.querySelector(selector)
     );
   }
