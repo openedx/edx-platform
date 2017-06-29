4f72905c77745cba7a92cccda32c7e0ba4e47d01
Ned Batchelder <ned@edx.org>
2017-06-29 07:10:05 -0400

    Correct spelling of 'Open edX', and pedantic typography in the copyright footer

--- lms/djangoapps/branding/api.py
+++ lms/djangoapps/branding/api.py
@@ -118,8 +118,8 @@ def _footer_copyright():
         # Translators: 'EdX', 'edX', and 'Open edX' are trademarks of 'edX Inc.'.
         # Please do not translate any of these trademarks and company names.
         u"\u00A9 {org_name}.  All rights reserved except where noted.  "
-        u"EdX, Open edX and the edX and Open EdX logos are registered trademarks "
-        u"or trademarks of edX Inc."
+        u"EdX, Open edX and their respective logos are trademarks "
+        u"or registered trademarks of edX Inc."
     ).format(org_name=configuration_helpers.get_value('PLATFORM_NAME', settings.PLATFORM_NAME))
 
 
