705a68b212e5a70399822cbd3a1805c83d83fb19
bmedx <bmesick@edx.org>
2017-07-05 10:14:59 -0400

    Upgrade django-ratelimit-backend to 1.1.1

--- openedx/core/djangoapps/verified_track_content/admin.py
+++ openedx/core/djangoapps/verified_track_content/admin.py
@@ -2,13 +2,15 @@
 Django admin page for verified track configuration
 """
 
-from django.contrib import admin
+from ratelimitbackend import admin
 
 from openedx.core.djangoapps.verified_track_content.forms import VerifiedTrackCourseForm
 from openedx.core.djangoapps.verified_track_content.models import VerifiedTrackCohortedCourse
 
 
-@admin.register(VerifiedTrackCohortedCourse)
 class VerifiedTrackCohortedCourseAdmin(admin.ModelAdmin):
     """Admin for enabling verified track cohorting. """
     form = VerifiedTrackCourseForm
+
+
+admin.site.register(VerifiedTrackCohortedCourse, VerifiedTrackCohortedCourseAdmin)
