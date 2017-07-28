bb33a3aa2559f33b2476d8ee1ca442f74e8661d7
Hasnain <hasnain.naveed123@gmail.com>
2017-07-28 19:04:07 +0500

    LEARNER-1146 | Removed the flag and their corresponding code.

--- openedx/core/djangoapps/catalog/cache.py
+++ openedx/core/djangoapps/catalog/cache.py
@@ -1,10 +1,5 @@
 # Template used to create cache keys for individual programs.
 PROGRAM_CACHE_KEY_TPL = 'program-{uuid}'
 
-# Cache key used to locate an item containing a list of all program UUIDs.
-# This has to be deleted when removing the waffle flags populate-multitenant-programs and get-multitenant-programs
-# For more, see LEARNER-1146
-PROGRAM_UUIDS_CACHE_KEY = 'program-uuids'
-
 # Cache key used to locate an item containing a list of all program UUIDs for a site.
 SITE_PROGRAM_UUIDS_CACHE_KEY_TPL = 'program-uuids-{domain}'
