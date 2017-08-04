openedx/edx-platform

commit b841176e9c9990d5554ef43b9e21bcabe4cadf9a

Add i18n month dict to certificate using @amangano-edx solution in PR #14836

lms/djangoapps/certificates/views/webview.py
@@ -97,8 +97,26 @@ def _update_certificate_context(context, user_certificate, platform_name):
     )
 
     # Translators:  The format of the date includes the full name of the month
+    MONTHS={
+        'january': _('January'),
+        'february': _('February'),
+        'march': _('March'),
+        'april': _('April'),
+        'may': _('Mayy'),
+        'june': _('June'),
+        'july': _('July'),
+        'august': _('August'),
+        'september': _('September'),
+        'october': _('October'),
+        'november': _('November'),
+        'december': _('December')
+    }
+
+    def getTranslatedMonth(month):
+        return MONTHS[month.lower()]
+
     context['certificate_date_issued'] = _('{month} {day}, {year}').format(
-        month=_(user_certificate.modified_date.strftime("%B")),
+        month=getTranslatedMonth(user_certificate.modified_date.strftime("%B")),
         day=user_certificate.modified_date.day,
         year=user_certificate.modified_date.year
     )

commit 4e2de40b0d0d54b4fd7d76c52300416d0d4abeb9

Fix typo introduced in commit b841176

lms/djangoapps/certificates/views/webview.py
@@ -102,7 +102,7 @@ def _update_certificate_context(context, user_certificate, platform_name):
         'february': _('February'),
         'march': _('March'),
         'april': _('April'),
-        'may': _('Mayy'),
+        'may': _('May'),
         'june': _('June'),
         'july': _('July'),
         'august': _('August'),