60eeaae5531e5d48d63efa6afedb8093a3f07b2c
jawadtahir <ranajawadtahir@gmail.com>
2017-08-07 18:50:40 +0500

    Added support for push notification through UA API

--- common/djangoapps/student/tasks.py
+++ common/djangoapps/student/tasks.py
@@ -10,7 +10,8 @@ from celery.exceptions import MaxRetriesExceededError
 from boto.exception import NoAuthHandlerFound
 
 from student.models import CourseEnrollment
-from edx_notifications.lib.publisher import bulk_publish_notification_to_users
+from edx_notifications.lib.publisher import bulk_publish_notification_to_users, \
+    publish_notification_to_tag
 
 log = logging.getLogger('edx.celery.task')
 
@@ -67,6 +68,23 @@ def publish_course_notifications_task(course_id, notification_msg, exclude_user_
 
     try:
         bulk_publish_notification_to_users(user_ids, notification_msg, exclude_user_ids=exclude_user_ids)
+    except Exception, ex:
+        # Notifications are never critical, so we don't want to disrupt any
+        # other logic processing. So log and continue.
+        log.exception(ex)
+
+@task()
+def publish_course_notifications_to_tag_task(course_id, notification_msg,
+                                       exclude_user_ids=None):  # pylint: disable=invalid-name
+    """
+    This function will call the edx_notifications api method
+    "bulk_publish_notification_to_tag"
+    and run as a new Celery task.
+    """
+
+    try:
+        publish_notification_to_tag(notification_msg, 'enrolments', course_id,
+                                    exclude_user_ids=exclude_user_ids)
     except Exception, ex:
         # Notifications are never critical, so we don't want to disrupt any
         # other logic processing. So log and continue.
