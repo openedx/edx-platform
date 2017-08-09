430261d453e6a813353af7581123d3199cf15870
Tyler Hallada <thallada@edx.org>
2017-08-09 11:19:31 -0400

    Comment out actual override and log instead

--- lms/djangoapps/grades/models.py
+++ lms/djangoapps/grades/models.py
@@ -418,14 +418,27 @@ class PersistentSubsectionGrade(DeleteGradesMixin, TimeStampedModel):
                 grade__course_id=usage_key.course_key,
                 grade__usage_key=usage_key,
             )
-            if override.earned_all_override is not None:
-                params['earned_all'] = override.earned_all_override
-            if override.possible_all_override is not None:
-                params['possible_all'] = override.possible_all_override
-            if override.earned_graded_override is not None:
-                params['earned_graded'] = override.earned_graded_override
-            if override.possible_graded_override is not None:
-                params['possible_graded'] = override.possible_graded_override
+            # EDUCTATOR-1127: no-op and log until this behavior is verified in production
+            #  if override.earned_all_override is not None:
+            #      params['earned_all'] = override.earned_all_override
+            #  if override.possible_all_override is not None:
+            #      params['possible_all'] = override.possible_all_override
+            #  if override.earned_graded_override is not None:
+            #      params['earned_graded'] = override.earned_graded_override
+            #  if override.possible_graded_override is not None:
+            #      params['possible_graded'] = override.possible_graded_override
+            log.info(u"EDUCATOR-1127: Subsection grade for user {user_id} on subsection {usage_key} in course "
+                     u"{course_key} would be overridden with params: {params}".format(
+                user_id=unicode(user_id),
+                usage_key=unicode(usage_key),
+                course_key=unicode(usage_key.course_key),
+                params=unicode({
+                    'earned_all': override.earned_all_override,
+                    'possible_all': override.possible_all_override,
+                    'earned_graded': override.earned_graded_override,
+                    'possible_graded': override.possible_graded_override
+                }))
+            )
         except PersistentSubsectionGradeOverride.DoesNotExist:
             pass
 
