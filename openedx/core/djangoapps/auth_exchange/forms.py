c754a9763a094c393353384542afed16d31a82c8
uzairr <uzairr@yahoo.com>
2017-08-16 14:55:08 +0500

    Add logs for third-party auth form

--- openedx/core/djangoapps/auth_exchange/forms.py
+++ openedx/core/djangoapps/auth_exchange/forms.py
@@ -1,6 +1,8 @@
 """
 Forms to support third-party to first-party OAuth 2.0 access token exchange
 """
+import logging
+
 import provider.constants
 from django.contrib.auth.models import User
 from django.forms import CharField
@@ -15,6 +17,7 @@ from social_core.exceptions import AuthException
 
 from third_party_auth import pipeline
 
+log = logging.getLogger(__name__)
 
 class AccessTokenExchangeForm(ScopeMixin, OAuthForm):
     """Form for access token exchange endpoint"""
@@ -55,10 +58,14 @@ class AccessTokenExchangeForm(ScopeMixin, OAuthForm):
 
     def clean(self):
         if self._errors:
+            log.info("inside clean 1st if: self._error: [ %s ]", self._errors)
             return {}
 
         backend = self.request.backend
+        log.info("inside clean: backend: [ %s ]", backend)
         if not isinstance(backend, social_oauth.BaseOAuth2):
+            log.info("inside clean: 2nd if: social_oauth.BaseOAuth2 [ %s ], backend.name: [ %s ]",
+                     social_oauth.BaseOAuth2, backend.name)
             raise OAuthValidationError(
                 {
                     "error": "invalid_request",
@@ -67,10 +74,11 @@ class AccessTokenExchangeForm(ScopeMixin, OAuthForm):
             )
 
         self.request.session[pipeline.AUTH_ENTRY_KEY] = pipeline.AUTH_ENTRY_LOGIN_API
-
+        log.info("inside clean: AUTH_ENTRY_KEY [ %s ]", self.request.session[pipeline.AUTH_ENTRY_KEY])
         client_id = self.cleaned_data["client_id"]
         try:
             client = self.oauth2_adapter.get_client(client_id=client_id)
+            log.info("inside clean: 1st try block: client: [ %s ], client_id [ %s ]", client, client_id )
         except (Client.DoesNotExist, Application.DoesNotExist):
             raise OAuthValidationError(
                 {
@@ -93,18 +101,22 @@ class AccessTokenExchangeForm(ScopeMixin, OAuthForm):
         access_token = self.cleaned_data.get("access_token")
         try:
             user = backend.do_auth(access_token, allow_inactive_user=True)
+            log.info("inside clean: 2nd try block: user: [ %s ]", user)
         except (HTTPError, AuthException):
+            log.info("inside clean: exception passed")
             pass
         if user and isinstance(user, User):
             self.cleaned_data["user"] = user
+            log.info("inside clean last if: user and isinstance(user,User): [ %s ] ", user, isinstance(user, User))
         else:
             # Ensure user does not re-enter the pipeline
             self.request.social_strategy.clean_partial_pipeline(access_token)
+            log.info("inside clean: last line exception")
             raise OAuthValidationError(
                 {
                     "error": "invalid_grant",
                     "error_description": "access_token is not valid",
                 }
             )
-
+        log.info("inside clean: self.cleaned_data: [ %s ]", self.cleaned_data)
         return self.cleaned_data
