71b7b39daa31b9337121c167ef55d7d121a35f92
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2017-08-17 12:40:14 +0500

    testing management command.

--- cms/djangoapps/contentstore/management/commands/test_transcript.py
+++ cms/djangoapps/contentstore/management/commands/test_transcript.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from edxval.api import create_update_video_transcript
+from contextlib import closing
 
 
 class Command(BaseCommand):
@@ -13,5 +14,11 @@ class Command(BaseCommand):
         video_id = options['video_id']
         file_data = options['file_data']
         from nose.tools import set_trace; set_trace()
-        transcript_url = create_update_video_transcript(video_id=video_id, language='en', file_data=file_data)
+        with closing(file_data):
+            transcript_url = create_update_video_transcript(
+                video_id=video_id,
+                language='en',
+                file_name=file_data.name,
+                file_data=file_data
+            )
         print transcript_url
