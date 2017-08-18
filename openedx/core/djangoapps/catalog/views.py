b15dc4bdccbc5bbcdc938f5b30ca2b48268453ae
Hasnain <hasnain.naveed123@gmail.com>
2017-08-18 16:29:11 +0500

    fixing.

--- openedx/core/djangoapps/catalog/views.py
+++ openedx/core/djangoapps/catalog/views.py
@@ -10,20 +10,6 @@ logger = logging.getLogger(__name__)
 @require_GET
 def cache_programs(request):
 
-    logger.error("cache ----------------")
-    from common.test.acceptance.fixtures import CATALOG_STUB_URL
-    COURSE_CATALOG_API_URL = "{catalog_url}/api/v1/".format(catalog_url=CATALOG_STUB_URL)
-    site = request.site
-    logger.error(site)
-    logger.error(COURSE_CATALOG_API_URL)
-    site_config = getattr(site, 'configuration', None)
-
-    if site_config is None:
-        SiteConfiguration.objects.create(
-            site=request.site,
-            enabled=True,
-            values={"COURSE_CATALOG_API_URL": COURSE_CATALOG_API_URL}
-        )
     if settings.FEATURES.get('EXPOSE_CACHE_PROGRAMS_ENDPOINT'):
         call_command('cache_programs')
 
@@ -42,19 +28,20 @@ def cache_programs_test(request):
     API the management command attempts to call should be stubbed out first.
     """
 
-    try:
-        from common.test.acceptance.fixtures import CATALOG_STUB_URL
-        COURSE_CATALOG_API_URL = "{catalog_url}/api/v1/".format(catalog_url=CATALOG_STUB_URL)
-        site = request.site
-        logger.error(site)
-    except Exception as e:
-        logger.error("view exception -asdf")
-        logger.error(e)
-    SiteConfiguration.objects.create(
-        site=request.site,
-        enabled=True,
-        values={"COURSE_CATALOG_API_URL": COURSE_CATALOG_API_URL}
-    )
+    logger.error("cache ----------------")
+    from common.test.acceptance.fixtures import CATALOG_STUB_URL
+    COURSE_CATALOG_API_URL = "{catalog_url}/api/v1/".format(catalog_url=CATALOG_STUB_URL)
+    site = request.site
+    logger.error(site)
+    logger.error(COURSE_CATALOG_API_URL)
+    site_config = getattr(site, 'configuration', None)
+
+    if site_config is None:
+        SiteConfiguration.objects.create(
+            site=request.site,
+            enabled=True,
+            values={"COURSE_CATALOG_API_URL": COURSE_CATALOG_API_URL}
+        )
     if settings.FEATURES.get('EXPOSE_CACHE_PROGRAMS_ENDPOINT'):
         call_command('cache_programs')
 
