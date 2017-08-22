086e15c9b6738f830471f746856c7cc775dde9a2
Diana Huang <dkh@edx.org>
2017-08-22 11:40:17 -0400

    Update event naming for course sock.
    Only show course sock to English readers.

--- lms/static/js/courseware/course_home_events.js
+++ lms/static/js/courseware/course_home_events.js
@@ -9,10 +9,10 @@
                 });
             });
             $('.date-summary-verified-upgrade-deadline .date-summary-link').on('click', function() {
-                Logger.log('edx.course.home.upgrade_verified.clicked', {location: 'sidebar'});
+                Logger.log('edx.course.enrollment.upgrade.clicked', {location: 'sidebar'});
             });
             $('.upgrade-banner-button').on('click', function() {
-                Logger.log('edx.course.home.upgrade_verified.clicked', {location: 'notification'});
+                Logger.log('edx.course.enrollment.upgrade.clicked', {location: 'notification'});
             });
             $('.view-verified-info').on('click', function() {
                 Logger.log('edx.course.home.learn_about_verified.clicked', {location: 'notification'});
