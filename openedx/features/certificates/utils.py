6e40c9897c5e7d06add927cb4ae1869743d0a890
Alex Dusenbery <adusenbery@edx.org>
2017-08-30 14:35:52 -0400

    small checkpoint

--- openedx/features/certificates/utils.py
+++ openedx/features/certificates/utils.py
@@ -2,16 +2,9 @@
 Functions that encapsulate business logic about the
 generation and viewing of certificates and certificate fields.
 """
+from datetime import datetime
+
 from config.waffle import auto_generated_certificates_enabled
-
-
-def can_generate_certificate(student, course):
-    if auto_generated_certificates_enabled():
-        # do one thing
-        pass
-    else:
-        # do another thing
-        pass
 
 
 def can_show_generate_certificate_button(student, course):
@@ -43,17 +36,18 @@ def can_show_course_certificate_in_program(student, course):
 
 def can_show_certificate_available_date_field(course):
     if auto_generated_certificates_enabled():
-        # do one thing
-        pass
-    else:
-        # do another thing
-        pass
+        return not course.self_paced
+    return False
 
 
-def certificate_display_date(student, course):
+def certificate_display_date(course, generated_certificate):
     if auto_generated_certificates_enabled():
-        # do one thing
-        pass
+        if course.self_paced:
+            return generated_certificate.modified_date
+        else:
+            available_date = course.certificate_available_date
+            if (not available_date) or (available_date > datetime.utcnow()):
+                return generated_certificate.modified_date
+            return available_date
     else:
-        # do another thing
-        pass
+        return generated_certificate.modified_date
