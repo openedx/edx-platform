daa28f93a85ef1b41d1d9b54581c5533f4874345
ElakkiyaMohanRajan <elakkiya.mohu@gmail.com>
2017-08-30 16:06:51 -0700

    Platform changes for common cookie URL call  (#89)
    * add cookie api call
    
    * changed to cookies_api
    
    * changed to cookies_api
    
    * Added the comments
    
    * Updated the indentation for the comment
    
    * Updated the comment
    
    * Used the parser library for building the URL
    
    * Update views.py
    
    * aligning settings name with edx-configuration (#92)
    
    * Added the try except block
    
    * add cookie test

--- lms/urls.py
+++ lms/urls.py
@@ -577,6 +577,12 @@ urlpatterns += (
         include('student_account.urls')
     ),
 
+   # Get_Cookies
+    url(
+        r'^cookies/',
+        include('student_account.urls')
+    ),
+
     # Student profile
     url(
         r'^u/(?P<username>[\w.@+-]+)$',
