0a602621e3dca062cbac8b2948849d1a15d7c93a
Clinton Blackburn <cblackburn@edx.org>
2017-09-02 22:41:53 -0400

    Added Studio API endpoint to re-run course runs
    LEARNER-2470

--- cms/djangoapps/api/v1/views/course_runs.py
+++ cms/djangoapps/api/v1/views/course_runs.py
@@ -4,10 +4,11 @@ from edx_rest_framework_extensions.authentication import JwtAuthentication
 from opaque_keys.edx.keys import CourseKey
 from rest_framework import permissions, status, viewsets
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 
 from contentstore.views.course import _accessible_courses_iter, get_course_and_check_access
-from ..serializers.course_runs import CourseRunCreateSerializer, CourseRunSerializer
+from ..serializers.course_runs import CourseRunCreateSerializer, CourseRunRerunSerializer, CourseRunSerializer
 
 
 class CourseRunViewSet(viewsets.ViewSet):
@@ -63,4 +64,15 @@ class CourseRunViewSet(viewsets.ViewSet):
         serializer = CourseRunCreateSerializer(data=request.data, context=self.get_serializer_context())
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @detail_route(methods=['post'])
+    def rerun(self, request, pk=None):
+        course_run_key = CourseKey.from_string(pk)
+        user = request.user
+        course_run = self.get_course_run_or_raise_404(course_run_key, user)
+        serializer = CourseRunRerunSerializer(course_run, data=request.data, context=self.get_serializer_context())
+        serializer.is_valid(raise_exception=True)
+        new_course_run = serializer.save()
+        serializer = self.get_serializer(new_course_run)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
