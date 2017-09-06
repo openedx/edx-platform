8295e33eb5a5035d2f8b64d6031b0eb6f4283e84
Shahbaz Nazir <shahbaznazir1992@gmail.com>
2017-09-06 16:29:07 +0500

    Add different dashboards for learner and POC
    Signed-off-by: Shahbaz Nazir <shahbaz.nazir@arbisoft.com>

---
+++ common/djangoapps/student/migrations/0009_auto_20170830_1048.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('student', '0008_auto_20161117_1209'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='userprofile',
+            name='is_poc',
+            field=models.BooleanField(default=False),
+            preserve_default=False
+        )
+    ]
--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -289,6 +289,7 @@ class UserProfile(models.Model):
     country = CountryField(blank=True, null=True)
     goals = models.TextField(blank=True, null=True)
     allow_certificate = models.BooleanField(default=1)
+    is_poc = models.BooleanField(default=0)
     bio = models.CharField(blank=True, null=True, max_length=3000, db_index=False)
     profile_image_uploaded_at = models.DateTimeField(null=True, blank=True)
 
