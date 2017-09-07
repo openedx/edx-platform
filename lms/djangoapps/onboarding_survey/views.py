78cadeb54c3b00edb2cdcebbbcc84e098024842b
Muddasser <muddasser1991@yahoo.com>
2017-09-07 11:34:36 +0000

    Completed the first form

--- lms/djangoapps/onboarding_survey/views.py
+++ lms/djangoapps/onboarding_survey/views.py
@@ -1,5 +1,6 @@
 import json
 import os
+import datetime
 
 from path import Path as path
 
@@ -9,15 +10,56 @@ from django_countries import countries
 from django.http import HttpResponse
 
 from onboarding_survey import forms
+from lms.djangoapps.onboarding_survey.models import (
+    RoleInsideOrg,
+    OrgSector,
+    OperationLevel,
+    FocusArea,
+    TotalEmployee,
+    TotalVolunteer,
+    PartnerNetwork,
+    OrganizationSurvey,
+    OrganizationalCapacityArea,
+    CommunityTypeInterest,
+    InclusionInCommunityChoice,
+    PersonalGoal,
+    InterestsSurvey,
+    EducationLevel,
+    EnglishProficiency,
+    LearnerSurvey
+)
 
 
 def tell_us_more(request):
 
     if request.method == 'POST':
         form = forms.TellUsMoreForm(request.POST)
-        from nose.tools import set_trace; set_trace()
         if form.is_valid():
-            pass
+            cleaned_data = form.cleaned_data
+            english_proficiency = cleaned_data['english_language_proficiency']
+            english_proficiency = dict(form.fields['english_language_proficiency'].choices)[int(english_proficiency)]
+
+            education_level = cleaned_data['level_of_education']
+            education_level = dict(form.fields['level_of_education'].choices)[int(education_level)]
+
+            user_info = LearnerSurvey()
+
+            user_info.dob = datetime.datetime.strptime(cleaned_data['birth_date'], "%m/%d/%Y")
+
+            user_info.level_of_education = EducationLevel.objects.filter(level=education_level).first()
+
+            user_info.language = cleaned_data['native_language']
+            user_info.english_prof = EnglishProficiency.objects.filter(proficiency=english_proficiency).first()
+
+            user_info.country_of_residence = cleaned_data['country']
+            user_info.city_of_residence = cleaned_data['city']
+
+            user_info.is_country_or_city_different = bool(cleaned_data['is_city_or_country_of_employment_diff'])
+
+            user_info.country_of_employment = cleaned_data['country_of_employment']
+            user_info.city_of_employment = cleaned_data['city_of_employment']
+
+            user_info.save()
 
     else:
         form = forms.TellUsMoreForm()
@@ -38,8 +80,15 @@ def organization(request):
 @csrf_exempt
 def get_country_names(request):
     if request.is_ajax():
-        all_counties = dict(countries).values()
-        data = json.dumps(all_counties)
+        file_path = path(os.path.join(
+            'lms', 'djangoapps', 'onboarding_survey', 'data', 'world_countries.json'
+        )).abspath()
+        with open(file_path) as json_data:
+            q = request.GET.get('term', '')
+            all_countries = json.load(json_data)
+            filtered_countries = [country for country in all_countries if q.lower() in country.lower()]
+
+        data = json.dumps(filtered_countries)
 
     else:
         data = 'fail'
@@ -58,7 +107,7 @@ def get_languages(request):
         with open(file_path) as json_data:
             q = request.GET.get('term', '')
             all_languages = json.load(json_data)
-            filtered_languages = [language for language in all_languages if q in language]
+            filtered_languages = [language for language in all_languages if q.lower() in language.lower()]
 
         data = json.dumps(filtered_languages)
 
