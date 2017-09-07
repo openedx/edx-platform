830da5f5dcf5082f74388964776f92aebfa422f0
Michael LoTurco <mikeloturco13@gmail.com>
2017-09-07 14:29:52 -0400

    migration fix

--- lms/djangoapps/certificates/migrations/0011_certificategenerationcoursesetting_can_self_generate.py
+++ lms/djangoapps/certificates/migrations/0011_certificategenerationcoursesetting_can_self_generate.py
@@ -5,12 +5,12 @@ from django.apps import apps
 from django.db import migrations, models
 from django.db.models import F
 
-def copy_field():
+def copy_field(apps, schema_editor):
     MyModel = apps.get_model('certificates', 'certificategenerationcoursesetting')
     MyModel.objects.all().update(can_self_generate=F('enabled'))
 
 class Migration(migrations.Migration):
-
+    
     dependencies = [
         ('certificates', '0010_certificate_language_course_setting'),
     ]
