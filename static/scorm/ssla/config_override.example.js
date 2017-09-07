c9056ac918b56650cacdbe67cfa0798b51ab9c00
wajeeha-khalid <wajeeha.khalid@arbisoft.com>
2017-09-07 15:08:28 +0500

    Scorm Xblock and SSLA player

---
+++ static/scorm/ssla/config_override.example.js
@@ -0,0 +1,7 @@
+var sslaConfig = {
+    openContentIn: "inline",
+    autoLaunchFirstSco: true,
+    singleScoView: "",
+    setDataUrl: "../install/examples/php/set.php",
+    getDataUrl: "../install/examples/php/get.php"
+};
---
+++ static/scorm/ssla/config_override.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var sslaConfig = {
+    openContentIn: "inline",
+    autoLaunchFirstSco: true,
+    setDataUrl: "/trackAllInOne/set",
+    getDataUrl: "/trackAllInOne/get",
+    returnToLastScoOnLaunch: false,
+//    singleScoView: "HIDE_ALL",
+
+    scoreAllowReduce: false,
+
+//     statusAllowChangeAfterCompleted: false,
+//     statusAllowChangeAfterFailed: false,
+//     statusAllowChangeAfterPassed: false,
+//
+//     pluginLoadDetection: false,
+//     pluginMode: "scorm",
+//     scormManifestUseRawXml: true,
+//     scormManifestRawXml: '<manifest identifier="CQFaceism" version="1.0" xmlns="http://www.imsproject.org/xsd/imscp_rootv1p1p2" xmlns:adlcp="http://www.adlnet.org/xsd/adlcp_rootv1p2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd\
+//     http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd\
+//     http://www.adlnet.org/xsd/adlcp_rootv1p2 adlcp_rootv1p2.xsd">\
+// <metadata>\
+// <schema>ADL SCORM</schema>\
+// <schemaversion>1.2</schemaversion>\
+// </metadata>\
+// <organizations default="Tester">\
+//     <organization identifier="Tester">\
+//     <title>Tester</title>\
+//     <item identifier="quiz1" identifierref="testerf" isvisible="true">\
+//     <title>Tester</title>\
+//     </item>\
+// </organization>\
+// </organizations>\
+// <resources>\
+// <resource adlcp:scormtype="sco" href="index3.htm" identifier="testerf" type="webcontent">\
+//     </resource>\
+//     </resources>\
+//     </manifest>\
+//     ',
+};
