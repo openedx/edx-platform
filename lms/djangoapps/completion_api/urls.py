04555dff3bfe13b6e6c7de607a5c032e150679fc
Brad Melin <melinbrad@gmail.com>
2017-09-13 00:47:48 +0200

    added simple endpoint to set completion for any xblock
    simpler view
    
    URL arguments adhere to setting names
    
    ensure student is enrolled in course
    
    ensure that content is part of given course
    
    check for completion in post data
    
    fixed style errors and started adding tests
    
    remove studentprogress create

--- lms/djangoapps/completion_api/urls.py
+++ lms/djangoapps/completion_api/urls.py
@@ -2,10 +2,15 @@
 URLs for the completion API
 """
 
+from django.conf import settings
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^course/$', views.CompletionListView.as_view()),
-    url(r'^course/(?P<course_key>.*)/$', views.CompletionDetailView.as_view()),
+    url(r'^course/{}/$'.format(settings.COURSE_ID_PATTERN), views.CompletionDetailView.as_view()),
+    url(r'^course/{}/blocks/{}/$'.format(
+        settings.COURSE_ID_PATTERN,
+        settings.USAGE_ID_PATTERN
+    ), views.CompletionBlockUpdateView.as_view()),
 ]
