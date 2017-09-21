c4aa7bad1d7fc78a919f176df4f902912d98cbac
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-09-21 12:27:35 +0000

    url removed to deleted view

--- lms/djangoapps/faq/urls.py
+++ lms/djangoapps/faq/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-from lms.djangoapps.faq.views import get_faq, get_faq_title
+from lms.djangoapps.faq.views import get_faq
 
 urlpatterns = [
-    url(r'^title', get_faq_title, name='title_faq'),
     url(r'', get_faq, name='custom_faq'),
 ]
