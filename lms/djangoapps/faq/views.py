bae8793c91e238c748ceb5c660d2e88bf7dd3faf
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-09-21 10:28:22 +0000

    Default title and content added for FAQ page

--- lms/djangoapps/faq/views.py
+++ lms/djangoapps/faq/views.py
@@ -1,6 +1,5 @@
 import re
 
-from django.http import HttpResponseNotFound
 from django.http import JsonResponse
 from django.utils.safestring import mark_safe
 from django.views.decorators.csrf import csrf_exempt
@@ -15,15 +14,20 @@ def get_faq(request):
     """
 
     faq_page = Faq.objects.filter(is_active=True).last()
+
     if not faq_page:
-        return HttpResponseNotFound(render_to_string('static_templates/404.html', {}, request=request))
+        context = {
+            'title': 'FAQ',
+            'body': '** Please add content for FAQ page',
+        }
+    else:
+        body = re.search('<body>(.*)</body>', faq_page.content, re.S)
 
-    body = re.search('<body>(.*)</body>', faq_page.content, re.S)
+        context = {
+            'title': faq_page.title,
+            'body': mark_safe(body.group(1)) if body else 'No Content',
+        }
 
-    context = {
-        'title': faq_page.title,
-        'body': mark_safe(body.group(1)) if body else 'No Content',
-    }
     return render_to_response("faq/custom_faq.html", context)
 
 
