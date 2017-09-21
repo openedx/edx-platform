b3d74bfade54f37b2300c7206d36e06dfcfd3dd1
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-09-21 12:25:18 +0000

    unused views removed, get_faq view updated

--- lms/djangoapps/faq/views.py
+++ lms/djangoapps/faq/views.py
@@ -1,9 +1,5 @@
-import re
-
-from django.http import JsonResponse
 from django.utils.safestring import mark_safe
-from django.views.decorators.csrf import csrf_exempt
-from edxmako.shortcuts import render_to_response, render_to_string
+from edxmako.shortcuts import render_to_response
 
 from lms.djangoapps.faq.models import Faq
 
@@ -14,36 +10,9 @@ def get_faq(request):
     """
 
     faq_page = Faq.objects.filter(is_active=True).last()
-
-    if not faq_page:
-        context = {
-            'title': 'FAQ',
-            'body': '** Please add content for FAQ page',
-        }
-    else:
-        body = re.search('<body>(.*)</body>', faq_page.content, re.S)
-
-        context = {
-            'title': faq_page.title,
-            'body': mark_safe(body.group(1)) if body else 'No Content',
-        }
+    context = {
+        'title': faq_page.title if faq_page else 'FAQ',
+        'body': mark_safe(faq_page.content) if faq_page else '** Please add content for FAQ page',
+    }
 
     return render_to_response("faq/custom_faq.html", context)
-
-
-@csrf_exempt
-def get_faq_title(request):
-    """
-    Get the the Dynamic FAQ Page Title
-    """
-
-    # Default Title is FAQ
-    data = {'page_title': 'FAQ'}
-
-    if request.is_ajax():
-        faq_page = Faq.objects.filter(is_active=True).last()
-
-        if faq_page:
-            data = {"page_title": faq_page.title}
-
-    return JsonResponse(data)
