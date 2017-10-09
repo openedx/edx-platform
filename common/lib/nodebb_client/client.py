b629ebc687d9b93554a68204df5f9bfca941dc73
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-10-09 13:26:56 +0000

    Logging added for testing purpose

--- common/lib/nodebb_client/client.py
+++ common/lib/nodebb_client/client.py
@@ -9,6 +9,9 @@ from pynodebb.http_client import HttpClient
 
 from common.lib.nodebb_client.categories import ForumCategory
 from common.lib.nodebb_client.users import ForumUser
+from logging import getLogger
+
+log = getLogger(__name__)
 
 
 class NodeBBClient(Client):
@@ -24,6 +27,10 @@ class NodeBBClient(Client):
                        admin_uid=admin_uid)
 
         self.http_client = HttpClient()
+
+        log.info("Node BB client")
+        log.info("Http client end point %s" % self.http_client.endpoint)
+        log.info("Http client end point %s" % self.http_client.headers)
 
         self.users = ForumUser(self.http_client)
         self.topics = Topic(self.http_client)
