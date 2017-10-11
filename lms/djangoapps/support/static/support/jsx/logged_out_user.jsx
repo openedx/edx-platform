d839d177141541a4d131a28eca087b29844dbed7
tasawernawaz <tasawer.nawaz@arbisoft.com>
2017-10-11 13:16:11 +0500

    update css and html/ add basics for react LEARNER-2804

---
+++ lms/djangoapps/support/static/support/jsx/logged_out_user.jsx
@@ -0,0 +1,48 @@
+/* global gettext */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function LoggedOutUser({ loginUrl }) {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm-12">
+          <p>{gettext('Sign in to edX so we can help you better.')}</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-12">
+          <a href={loginUrl} className="btn btn-primary btn-signin">{gettext('Sign in')}</a>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="form-group">
+            <label htmlFor="email">{gettext('Your Email Address')}</label>
+            <input type="text" className="form-control" id="email" />
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="form-group">
+            <label
+              htmlFor="course"
+            >{gettext('Course Name')}<span> {gettext('(Optional)')}</span></label>
+            <input type="text" className="form-control" id="course" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+LoggedOutUser.propTypes = {
+  loginUrl: PropTypes.string.isRequired,
+};
+
+export default LoggedOutUser;
