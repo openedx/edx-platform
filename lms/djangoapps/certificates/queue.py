91d931bf276e9f2caae99c41b71816e193a00049
bmedx <bmesick@edx.org>
2017-10-16 16:26:10 -0400

    LMS - certificates urls

--- lms/djangoapps/certificates/queue.py
+++ lms/djangoapps/certificates/queue.py
@@ -510,7 +510,7 @@ class XQueueCertInterface(object):
         # for other certificates.  Although both tasks use the same queue,
         # we can distinguish whether the certificate was an example cert based
         # on which end-point XQueue uses once the task completes.
-        callback_url_path = reverse('certificates.views.update_example_certificate')
+        callback_url_path = reverse('update_example_certificate')
 
         try:
             self._send_to_xqueue(
