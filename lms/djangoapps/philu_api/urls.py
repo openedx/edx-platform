331681151f3df0c851bf374a4d435dda7656db9d
Muhammad Nadee <muhammad.nadeem@arbisoft.com>
2017-10-17 18:05:36 +0500

    LP-208 Update NodeBB profile sync API to django-rest-framework

--- lms/djangoapps/philu_api/urls.py
+++ lms/djangoapps/philu_api/urls.py
@@ -3,7 +3,9 @@
 """
 from django.conf.urls import url, patterns
 
+from lms.djangoapps.philu_api.views import UpdateCommunityProfile
+
 urlpatterns = patterns(
     'philu_api.views',
-    url(r'profile/update/', 'update_community_profile_update', name='update_community_profile_update'),
+    url(r'profile/update/', UpdateCommunityProfile.as_view(), name='update_community_profile_update'),
 )
--- lms/djangoapps/philu_api/views.py
+++ lms/djangoapps/philu_api/views.py
@@ -1,62 +1,59 @@
 """
 restAPI Views
 """
-import json
-from django.views.decorators.http import require_POST
 import logging
 
-from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from rest_framework.authentication import SessionAuthentication
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from student.models import User
 
-
-THREADS_PER_PAGE = 20
-INLINE_THREADS_PER_PAGE = 20
-PAGES_NEARBY_DELTA = 2
 log = logging.getLogger("edx.philu_api")
 
 
+class UpdateCommunityProfile(APIView):
+    """ Retrieve order details. """
 
-@require_POST
-@login_required
-def update_community_profile_update(request):
-    """
-    Update provided information in openEdx received from nodeBB client
-    """
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-    user = User.objects.get(username=request.GET.get('username'))
-    data = json.loads(request.body)
-    extended_profile = user.extended_profile
-    user_info_survey = user.user_info_survey
+    def post(self, request):
+        """ Update provided information in openEdx received from nodeBB client """
 
-    try:
-        first_name = data.get('first_name', extended_profile.first_name)
-        last_name = data.get('last_name', extended_profile.last_name)
-        bio = data.get('bio', user.profile.bio)
+        try:
+            user = User.objects.get(username=request.GET.get('username'))
+        except User.DoesNotExist:
+            return JsonResponse({'message': "User does not exist for provided username"}, status=status.HTTP_400_BAD_REQUEST)
 
-        city = data.get('city', user_info_survey.city_of_residence)
-        country = data.get('country', user_info_survey.country_of_residence)
-        dob = data.get('dob', user_info_survey.dob)
+        extended_profile = user.extended_profile
+        user_info_survey = user.user_info_survey
 
-        extended_profile.firs_name = first_name
-        extended_profile.firs_name = last_name
-        user.profile.bio = bio
+        data = request.data
 
-        user_info_survey.city_of_residence = city
-        user_info_survey.country_of_residence = country
+        try:
+            first_name = data.get('first_name', extended_profile.first_name)
+            last_name = data.get('last_name', extended_profile.last_name)
+            bio = data.get('bio', user.profile.bio)
 
-        user_info_survey.dob = dob
+            city = data.get('city', user_info_survey.city_of_residence)
+            country = data.get('country', user_info_survey.country_of_residence)
+            dob = data.get('dob', user_info_survey.dob)
 
-        extended_profile.save()
-        user_info_survey.save()
+            extended_profile.firs_name = first_name
+            extended_profile.firs_name = last_name
+            user.profile.bio = bio
 
-        return JsonResponse({
-            "message": "user info updated successfully",
-            "success": True
-        })
+            user_info_survey.city_of_residence = city
+            user_info_survey.country_of_residence = country
 
-    except Exception as ex:
-        return JsonResponse({
-            "message": str(ex.args),
-            "success": False
-        })
+            user_info_survey.dob = dob
+
+            extended_profile.save()
+            user_info_survey.save()
+
+            return JsonResponse({"message": "user info updated successfully"}, status=status.HTTP_200_OK)
+        except Exception as ex:
+            return JsonResponse({"message": str(ex.args)}, status=status.HTTP_400_BAD_REQUEST)
+
