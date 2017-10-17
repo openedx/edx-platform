14cd27081ac9a7bbd710f29358d5a85eb134c4d0
Muhammad Nadee <muhammad.nadeem@arbisoft.com>
2017-10-17 15:17:21 +0500

    LP-308 Update API to update user info at OpenEdx side

--- lms/djangoapps/philu_api/views.py
+++ lms/djangoapps/philu_api/views.py
@@ -1,8 +1,8 @@
 """
 restAPI Views
 """
-
-from functools import wraps
+import json
+from django.views.decorators.http import require_POST
 import logging
 
 from django.contrib.auth.decorators import login_required
@@ -17,39 +17,35 @@ log = logging.getLogger("edx.philu_api")
 
 
 
-# @require_POST
-# @login_required
+@require_POST
+@login_required
 def update_community_profile_update(request):
     """
     Update provided information in openEdx received from nodeBB client
     """
 
     user = User.objects.get(username=request.GET.get('username'))
-    data = request.POST
+    data = json.loads(request.body)
     extended_profile = user.extended_profile
     user_info_survey = user.user_info_survey
 
     try:
-        first_name = data.get('firs_name')
-        last_name = data.get('last_name')
-        bio = data.get('bio')
+        first_name = data.get('first_name', extended_profile.first_name)
+        last_name = data.get('last_name', extended_profile.last_name)
+        bio = data.get('bio', user.profile.bio)
 
-        city = data.get('city')
-        country = data.get('country')
-        dob = data.get('dob')
+        city = data.get('city', user_info_survey.city_of_residence)
+        country = data.get('country', user_info_survey.country_of_residence)
+        dob = data.get('dob', user_info_survey.dob)
 
-        if first_name:
-            extended_profile.firs_name = data.first_name
-        if last_name:
-            extended_profile.firs_name = data.last_name
-        if bio:
-            user.profile.bio = bio
+        extended_profile.firs_name = first_name
+        extended_profile.firs_name = last_name
+        user.profile.bio = bio
 
-        if city and country:
-            user_info_survey.location = "{city}, {country}".format(city=city, country=country)
+        user_info_survey.city_of_residence = city
+        user_info_survey.country_of_residence = country
 
-        if dob:
-            user_info_survey.dob = dob
+        user_info_survey.dob = dob
 
         extended_profile.save()
         user_info_survey.save()
@@ -64,4 +60,3 @@ def update_community_profile_update(request):
             "message": str(ex.args),
             "success": False
         })
-
