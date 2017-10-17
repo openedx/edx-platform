ddd9118f88c38c4f9b2bbd222be73b25d751766e
Muhammad Nadee <muhammad.nadeem@arbisoft.com>
2017-10-17 20:13:39 +0500

    LP-243 Create DiscussionCommunity object
    Create DiscussionCommunity object in create_category_on_nodebb receiver

--- lms/djangoapps/discussion_nodebb/views.py
+++ lms/djangoapps/discussion_nodebb/views.py
@@ -5,6 +5,7 @@ Views handling read (GET) requests for the Discussion tab and inline discussions
 import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from opaque_keys.edx.keys import CourseKey
 
 from lms.djangoapps.discussion_nodebb.models import DiscussionCommunity
 
@@ -16,11 +17,12 @@ def nodebb_form_discussion(request, course_id):
     """
     Redirect user to nodeBB forum page that is loaded into our template using iframe
     """
-    # course_id = CourseKeyField()
-    course_community = DiscussionCommunity.objects.filter(course_id="course-v1:edX+DemoX+Demo_Course").first()
+    course = CourseKey.from_string(course_id)
+    course_community = DiscussionCommunity.objects.filter(course_id=course).first()
     context = {
         "course_id": course_id,
-        "course_community": course_community
+        "course_name": course.run,
+        "community_url": course_community.community_url
     }
 
     return render(request, 'discussion_nodebb/discussion_board.html', context)
