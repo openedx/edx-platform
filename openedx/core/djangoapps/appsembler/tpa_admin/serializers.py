77c62d0584b24866835099692b21d65e5131a65a
melvinsoft <melvinsoft@gmail.com>
2017-10-19 11:03:01 -0300

    improve code style in serializer

--- openedx/core/djangoapps/appsembler/tpa_admin/serializers.py
+++ openedx/core/djangoapps/appsembler/tpa_admin/serializers.py
@@ -6,7 +6,9 @@ class SAMLConfigurationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SAMLConfiguration
-        fields = ('id', 'site', 'enabled','entity_id', 'private_key', 'public_key', 'org_info_str', 'other_config_str')
+        fields = (
+            'id', 'site', 'enabled','entity_id', 'private_key', 'public_key', 'org_info_str', 'other_config_str'
+        )
 
 
 class SAMLProviderConfigSerializer(serializers.ModelSerializer):
@@ -14,6 +16,7 @@ class SAMLProviderConfigSerializer(serializers.ModelSerializer):
     class Meta:
         model = SAMLProviderConfig
         fields = (
-        'id', 'site', 'enabled', 'name', 'icon_class', 'icon_image', 'secondary', 'skip_registration_form', 'visible',
-        'skip_email_verification', 'idp_slug', 'entity_id', 'metadata_source', 'attr_user_permanent_id',
-        'attr_full_name', 'attr_first_name', 'attr_last_name', 'attr_username', 'attr_email', 'other_settings')
+            'id', 'site', 'enabled', 'name', 'icon_class', 'icon_image', 'secondary', 'skip_registration_form',
+            'visible', 'skip_email_verification', 'idp_slug', 'entity_id', 'metadata_source', 'attr_user_permanent_id',
+            'attr_full_name', 'attr_first_name', 'attr_last_name', 'attr_username', 'attr_email', 'other_settings'
+        )
