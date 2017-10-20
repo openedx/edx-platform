24d12acb6b23b1f9dcf8a7ea0ea54a9004ddd87a
Eric Fischer <efischer@edx.org>
2017-10-20 10:45:40 -0400

    Revert "Merge pull request #16260 from edx/mulby/dynamic-deadline-upgrade-messaging"
    This reverts commit a13595352fc3d0a169138ee007327b4921038344, reversing
    changes made to 68d5fe892280c5aa7aa95be4ad07baee3b9038dc.

--- openedx/features/course_experience/views/course_sock.py
+++ openedx/features/course_experience/views/course_sock.py
@@ -3,10 +3,12 @@ Fragment for rendering the course's sock and associated toggle button.
 """
 from django.template.loader import render_to_string
 from django.utils.translation import get_language
+from opaque_keys.edx.keys import CourseKey
 from web_fragments.fragment import Fragment
 
-from course_modes.models import get_cosmetic_verified_display_price
-from courseware.date_summary import verified_upgrade_deadline_link, verified_upgrade_link_is_valid
+from commerce.utils import EcommerceService
+from course_modes.models import CourseMode, get_cosmetic_verified_display_price
+from courseware.date_summary import VerifiedUpgradeDeadlineDate
 from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
 from student.models import CourseEnrollment
 
@@ -23,12 +25,29 @@ class CourseSockFragmentView(EdxFragmentView):
         html = render_to_string('course_experience/course-sock-fragment.html', context)
         return Fragment(html)
 
-    @staticmethod
-    def get_verification_context(request, course):
-        enrollment = CourseEnrollment.get_enrollment(request.user, course.id)
-        show_course_sock = verified_upgrade_link_is_valid(enrollment) and get_language() == 'en'
-        upgrade_url = verified_upgrade_deadline_link(request.user, course=course)
+    def get_verification_context(self, request, course):
+        course_key = CourseKey.from_string(unicode(course.id))
+
+        # Establish whether the course has a verified mode
+        available_modes = CourseMode.modes_for_course_dict(unicode(course.id))
+        has_verified_mode = CourseMode.has_verified_mode(available_modes)
+
+        # Establish whether the user is already enrolled
+        is_already_verified = CourseEnrollment.is_enrolled_as_verified(request.user, course_key)
+
+        # Establish whether the verification deadline has already passed
+        verification_deadline = VerifiedUpgradeDeadlineDate(course, request.user)
+        deadline_has_passed = verification_deadline.deadline_has_passed()
+
+        # If this proves its worth, we can internationalize and display for more than English speakers.
+        show_course_sock = (
+            has_verified_mode and not is_already_verified and
+            not deadline_has_passed and get_language() == 'en'
+        )
+
+        # Get information about the upgrade
         course_price = get_cosmetic_verified_display_price(course)
+        upgrade_url = EcommerceService().upgrade_url(request.user, course_key)
 
         context = {
             'show_course_sock': show_course_sock,
