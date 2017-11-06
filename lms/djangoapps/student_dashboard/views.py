2777aae50fed7b6fabcb6027e1e4fb3fb69d74b1
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-11-06 21:41:20 +0000

    function added to get recommened communities

--- lms/djangoapps/student_dashboard/views.py
+++ lms/djangoapps/student_dashboard/views.py
@@ -8,6 +8,7 @@ from courseware.courses import get_courses
 from custom_settings.models import CustomSettings
 from lms.djangoapps.onboarding_survey.models import InterestsSurvey
 from common.lib.nodebb_client.client import NodeBBClient
+from xmodule.modulestore.django import modulestore
 
 
 def get_recommended_courses(user):
@@ -40,8 +41,28 @@ def get_enrolled_past_courses(course_enrollments):
     return enrolled, past
 
 
+def get_recommended_xmodule_courses(user):
+    recommended_courses = []
+    all_courses = get_courses(user)
+    try:
+        user_interests = InterestsSurvey.objects.get(user=user).capacity_areas.all().values_list('label', flat=True)
+        for course in all_courses:
+            try:
+                tags = CustomSettings.objects.filter(id=course.id).first().tags
+                tags = tags.split(',')
+                matched_interests = set(user_interests) & set(tags)
+                if matched_interests:
+                    detailed_course = modulestore().get_course(course.id)
+                    detailed_course.short_description = course.short_description
+                    detailed_course.interests = ', '.join(list(matched_interests))
+                    recommended_courses.append(detailed_course)
+            except AttributeError:
+                pass
+    except ObjectDoesNotExist:
+        pass
+    return recommended_courses
+
+
 def get_recommended_communities(user):
-    status, categories = NodeBBClient().users.recommended_communities(user)
-    if status in [400, 499]:
-        return []
-    return categories.get('recommended')
+    status, categories = NodeBBClient().categories.featured()
+    return categories if status == 200 else []
