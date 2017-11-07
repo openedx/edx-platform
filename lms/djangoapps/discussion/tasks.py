8eb1900299561e392b80bc9d160548f194f352b4
Alex Dusenbery <adusenbery@edx.org>
2017-11-07 13:59:31 -0500

    EDUCATOR-1571 | Send discussion notification on first response; add a CourseWaffleFlag for discussion notifications.

--- lms/djangoapps/discussion/tasks.py
+++ lms/djangoapps/discussion/tasks.py
@@ -13,13 +13,13 @@ from django.contrib.sites.models import Site
 
 from celery_utils.logged_task import LoggedTask
 from edx_ace import ace
+from edx_ace.utils import date
 from edx_ace.message import MessageType
 from edx_ace.recipient import Recipient
 from opaque_keys.edx.keys import CourseKey
 from openedx.core.djangoapps.site_configuration.helpers import get_value
 from lms.djangoapps.django_comment_client.utils import permalink
 import lms.lib.comment_client as cc
-from lms.lib.comment_client.utils import merge_dict
 
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from openedx.core.djangoapps.schedules.template_context import get_base_template_context
@@ -56,7 +56,22 @@ def send_ace_message(context):
 
 def _should_send_message(context):
     cc_thread_author = cc.User(id=context['thread_author_id'], course_id=context['course_id'])
-    return _is_user_subscribed_to_thread(cc_thread_author, context['thread_id'])
+    return (
+        _is_user_subscribed_to_thread(cc_thread_author, context['thread_id']) and
+        _is_not_subcomment(context['comment_id']) and
+        _is_first_comment(context['comment_id'], context['thread_id'])
+    )
+
+
+def _is_not_subcomment(comment_id):
+    comment = cc.Comment.find(id=comment_id).retrieve()
+    return not getattr(comment, 'parent_id', None)
+
+
+def _is_first_comment(comment_id, thread_id):
+    thread = cc.Thread.find(id=thread_id).retrieve(with_responses=True)
+    first_comment = thread.children[0]
+    return first_comment.get('id') == comment_id
 
 
 def _is_user_subscribed_to_thread(cc_user, thread_id):
@@ -78,11 +93,17 @@ def _get_course_language(course_id):
 
 
 def _build_message_context(context):
-    message_context = get_base_template_context(context['site'])
-    message_context.update(context)
+    message_context = get_base_template_context(Site.objects.get(id=context['site_id']))
+    message_context.update(_deserialize_context_dates(context))
     message_context['post_link'] = _get_thread_url(context)
     message_context['ga_tracking_pixel_url'] = _generate_ga_pixel_url(context)
     return message_context
+
+
+def _deserialize_context_dates(context):
+    context['comment_created_at'] = date.deserialize(context['comment_created_at'])
+    context['thread_created_at'] = date.deserialize(context['thread_created_at'])
+    return context
 
 
 def _get_thread_url(context):
