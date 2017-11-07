fa6a5f99a593c4d380dbb0f8d18cf057affd19ea
uzairr <uzairr@yahoo.com>
2017-11-07 10:42:19 +0000

    Create program detail fragment for mobile app
    To extract the program detail information edX mobile team has to
    parse all the un-necassary details which takes time that ultimately
    slows down app.To avoid it,a detail fragment is created that will
    be used in the mobile app as well as on the web.
    
    LEARNER-2981

--- lms/djangoapps/learner_dashboard/views.py
+++ lms/djangoapps/learner_dashboard/views.py
@@ -1,22 +1,11 @@
 """Learner dashboard views"""
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
-from django.http import Http404
 from django.views.decorators.http import require_GET
 
 from edxmako.shortcuts import render_to_response
-from lms.djangoapps.commerce.utils import EcommerceService
 
-from lms.djangoapps.learner_dashboard.programs import ProgramsFragmentView
-from lms.djangoapps.learner_dashboard.utils import FAKE_COURSE_KEY, strip_course_id
+from lms.djangoapps.learner_dashboard.programs import ProgramsFragmentView, ProgramDetailsFragmentView
 from openedx.core.djangoapps.programs.models import ProgramsApiConfig
-from openedx.core.djangoapps.programs.utils import (
-    ProgramDataExtender,
-    ProgramProgressMeter,
-    get_certificates,
-    get_program_marketing_url
-)
-from openedx.core.djangoapps.user_api.preferences.api import get_user_preferences
 
 
 @login_required
@@ -43,43 +32,17 @@ def program_listing(request):
 def program_details(request, program_uuid):
     """View details about a specific program."""
     programs_config = ProgramsApiConfig.current()
-    if not programs_config.enabled:
-        raise Http404
-
-    meter = ProgramProgressMeter(request.site, request.user, uuid=program_uuid)
-    program_data = meter.programs[0]
-
-    if not program_data:
-        raise Http404
-
-    program_data = ProgramDataExtender(program_data, request.user).extend()
-    course_data = meter.progress(programs=[program_data], count_only=False)[0]
-    certificate_data = get_certificates(request.user, program_data)
-
-    program_data.pop('courses')
-    skus = program_data.get('skus')
-    ecommerce_service = EcommerceService()
-
-    urls = {
-        'program_listing_url': reverse('program_listing_view'),
-        'track_selection_url': strip_course_id(
-            reverse('course_modes_choose', kwargs={'course_id': FAKE_COURSE_KEY})
-        ),
-        'commerce_api_url': reverse('commerce_api:v0:baskets:create'),
-        'buy_button_url': ecommerce_service.get_checkout_page_url(*skus)
-    }
+    program_fragment = ProgramDetailsFragmentView().render_to_fragment(
+        request, program_uuid, programs_config=programs_config
+    )
 
     context = {
-        'urls': urls,
+        'program_fragment': program_fragment,
         'show_program_listing': programs_config.enabled,
         'show_dashboard_tabs': True,
         'nav_hidden': True,
         'disable_courseware_js': True,
         'uses_pattern_library': True,
-        'user_preferences': get_user_preferences(request.user),
-        'program_data': program_data,
-        'course_data': course_data,
-        'certificate_data': certificate_data
     }
 
     return render_to_response('learner_dashboard/program_details.html', context)
