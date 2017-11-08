dfc38bc19e98589adb8c0f7db60781e9fc512949
melvinsoft <melvinsoft@gmail.com>
2017-11-08 18:45:59 -0300

    add custom validators to transform field values

--- openedx/core/djangoapps/appsembler/tpa_admin/serializers.py
+++ openedx/core/djangoapps/appsembler/tpa_admin/serializers.py
@@ -1,27 +1,31 @@
 import json
 
 from third_party_auth.models import SAMLConfiguration, SAMLProviderConfig, SAMLProviderData
+from third_party_auth.models import clean_json
 
 from rest_framework import serializers
 
 
-class JSONSerializerField(serializers.Field):
-    """ Serializer for JSONField -- required to make field writable"""
-    def to_internal_value(self, data):
-        return json.dumps(data)
-
-    def to_representation(self, value):
-        return value
-
-
 class SAMLConfigurationSerializer(serializers.ModelSerializer):
-    other_config_str = JSONSerializerField()
 
     class Meta:
         model = SAMLConfiguration
         fields = (
             'id', 'site', 'enabled','entity_id', 'private_key', 'public_key', 'org_info_str', 'other_config_str'
         )
+
+    def validate_private_key(self, value):
+        return value.replace("-----BEGIN RSA PRIVATE KEY-----", "").replace("-----BEGIN PRIVATE KEY-----", "").replace(
+            "-----END RSA PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "").strip()
+
+    def validate_public_key(self, value):
+        return value.replace("-----BEGIN CERTIFICATE-----", "").replace("-----END CERTIFICATE-----", "").strip()
+
+    def validate_org_info_str(self, value):
+        return clean_json(value, dict)
+
+    def validate_other_config_str(self, value):
+        return clean_json(value, dict)
 
 
 class SAMLProviderConfigSerializer(serializers.ModelSerializer):
@@ -35,6 +39,9 @@ class SAMLProviderConfigSerializer(serializers.ModelSerializer):
             'attr_full_name', 'attr_first_name', 'attr_last_name', 'attr_username', 'attr_email', 'other_settings',
             'metadata_ready'
         )
+
+    def validate_other_settings(self, value):
+        return clean_json(value, dict)
 
     def get_metadata_ready(self, obj):
         """ Do we have cached metadata for this SAML provider? """
