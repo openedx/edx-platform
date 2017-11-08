cc77d4f4da8beb566245861b124c9e12fcdf790e
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-11-08 09:05:19 +0000

    code optimized, used if else instead of exceptions

--- lms/djangoapps/student_dashboard/views.py
+++ lms/djangoapps/student_dashboard/views.py
@@ -45,23 +45,29 @@ def get_enrolled_past_courses(course_enrollments):
 def get_recommended_xmodule_courses(user):
     recommended_courses = []
     all_courses = get_courses(user)
-    try:
-        user_interests = InterestsSurvey.objects.get(user=user).capacity_areas.all().values_list('label', flat=True)
-        for course in all_courses:
-            try:
-                tags = CustomSettings.objects.filter(id=course.id).first().tags
-                tags = tags.split('|')
-                tags = [tag.strip() for tag in tags]
-                matched_interests = set(user_interests) & set(tags)
-                if matched_interests:
-                    detailed_course = modulestore().get_course(course.id)
-                    detailed_course.short_description = course.short_description
-                    detailed_course.interests = '/ '.join(list(matched_interests))
-                    recommended_courses.append(detailed_course)
-            except AttributeError:
-                pass
-    except ObjectDoesNotExist:
-        pass
+    user_interests = InterestsSurvey.objects.get(user=user)
+    if not user_interests:
+        return []
+
+    user_interests = user_interests.capacity_areas.all().values_list('label', flat=True)
+    for course in all_courses:
+        settings = CustomSettings.objects.filter(id=course.id).first()
+        if not settings:
+            continue
+
+        tags = settings.tags
+        if not tags:
+            continue
+
+        tags = tags.split('|')
+        tags = [tag.strip() for tag in tags]
+        matched_interests = set(user_interests) & set(tags)
+        if matched_interests:
+            detailed_course = modulestore().get_course(course.id)
+            detailed_course.short_description = course.short_description
+            detailed_course.interests = '/ '.join(list(matched_interests))
+            recommended_courses.append(detailed_course)
+
     return recommended_courses
 
 
