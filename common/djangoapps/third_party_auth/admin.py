d3bbb86fe306c7b67ec53494586e8fc291ec154d
bmedx <bmesick@edx.org>
2017-11-09 13:34:28 -0500

    Shims to fix test collection errors

--- common/djangoapps/third_party_auth/admin.py
+++ common/djangoapps/third_party_auth/admin.py
@@ -116,8 +116,9 @@ class SAMLProviderDataAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
-            return self.model._meta.get_all_field_names()  # pylint: disable=protected-access
+            return [field.name for field in self.model._meta.get_fields()]  # pylint: disable=protected-access
         return self.readonly_fields
+
 
 admin.site.register(SAMLProviderData, SAMLProviderDataAdmin)
 
