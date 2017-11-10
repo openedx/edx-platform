5cd9b3896e2f14464a46e5f28cbf0b29efb9cc63
Muhammad Nadee <muhammad.nadeem@arbisoft.com>
2017-11-10 17:10:42 +0500

    Add Master Token authentication class for NodeBB requests

--- common/djangoapps/edx_notifications/server/api/api_utils.py
+++ common/djangoapps/edx_notifications/server/api/api_utils.py
@@ -3,15 +3,46 @@ Helpers for the HTTP APIs
 """
 
 from rest_framework.views import APIView
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, BaseAuthentication
 from rest_framework.permissions import IsAuthenticated
+
+from student.admin import User
+from django.conf import settings
+
+
+class CustomTokenSessionAuthentication(BaseAuthentication):
+    """
+    Custom class to authenticate user via custom Token.
+    """
+
+    def authenticate(self, request):
+        """
+        Returns a `User` if the client sending correct master token & username.
+        Otherwise returns `None`.
+        """
+
+        username = request.GET.get('username')
+        token = request.GET.get("token")
+
+        if not (token or username):
+            return None
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
+
+        if not token == settings.NODEBB_MASTER_TOKEN or not user.is_active:
+            return None
+
+        return (user, None)
 
 
 class AuthenticatedAPIView(APIView):
     """
     Returns the number of notifications for the logged in user
     """
-    authentication_classes = (SessionAuthentication,)
+    authentication_classes = (SessionAuthentication, CustomTokenSessionAuthentication)
     permission_classes = (IsAuthenticated,)
 
     _allowed_post_parameters = {}
