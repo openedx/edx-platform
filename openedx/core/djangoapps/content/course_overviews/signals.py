f234f7b55bfb195083f509dc156dc678824b4ddf
Gabe Mulley <gabe@edx.org>
2017-11-17 13:34:15 -0500

    log changes to the start date

--- openedx/core/djangoapps/content/course_overviews/signals.py
+++ openedx/core/djangoapps/content/course_overviews/signals.py
@@ -1,11 +1,15 @@
 """
 Signal handler for invalidating cached course overviews
 """
+import logging
+
 from django.dispatch.dispatcher import receiver
 
 from .models import CourseOverview
 from openedx.core.djangoapps.signals.signals import COURSE_PACING_CHANGED, COURSE_START_DATE_CHANGED
 from xmodule.modulestore.django import SignalHandler
+
+LOG = logging.getLogger(__name__)
 
 
 @receiver(SignalHandler.course_published)
@@ -40,11 +44,25 @@ def _check_for_course_changes(previous_course_overview, updated_course_overview)
 
 def _check_for_course_date_changes(previous_course_overview, updated_course_overview):
     if previous_course_overview.start != updated_course_overview.start:
+        _log_start_date_change(previous_course_overview, updated_course_overview)
         COURSE_START_DATE_CHANGED.send(
             sender=None,
             updated_course_overview=updated_course_overview,
             previous_start_date=previous_course_overview.start,
         )
+
+
+def _log_start_date_change(previous_course_overview, updated_course_overview):
+    previous_start_str = 'None'
+    if previous_course_overview.start is not None:
+        previous_start_str = previous_course_overview.start.isoformat()
+    new_start_str = 'None'
+    if updated_course_overview.start is not None:
+        new_start_str = updated_course_overview.start.isoformat()
+    LOG.info('Course start date changed: previous={0} new={1}'.format(
+        previous_start_str,
+        new_start_str,
+    ))
 
 
 def _check_for_pacing_changes(previous_course_overview, updated_course_overview):
