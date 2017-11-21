8ca0fe9dfc4f017b884d62049af20059477d3a14
Ari Rizzitano <arizzitano@edx.org>
2017-11-21 10:12:20 -0500

    introduce ReactRenderer module and renderReact mako def [FEDX-453]
    [extreme wip] mako/react bridge code [FEDX-453]
    
    more attempts
    
    split out entry points into separate file
    
    this works!
    
    kill dynamic import
    
    error handling
    
    didn't need webpack_static
    
    handle passing props
    
    cleanup django-template-rendering defs
    
    pytest monkeypatch fix
    
    cleanup
    
    add id arg to renderReact def
    
    more cleanup
    
    oops
    
    quality xss fixes
    
    unittest fix
    
    kill HelloWorld

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
 """
 Default unit test configuration and fixtures.
 """
-
 from __future__ import absolute_import, unicode_literals
+import pytest
 
 # Import hooks and fixture overrides from the cms package to
 # avoid duplicating the implementation
 
 from cms.conftest import _django_clear_site_cache, pytest_configure  # pylint: disable=unused-import
+
+
+@pytest.fixture(autouse=True)
+def no_webpack_loader(monkeypatch):
+    monkeypatch.setattr(
+        "webpack_loader.templatetags.webpack_loader.render_bundle",
+        lambda x: ''
+    )
