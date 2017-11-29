8c6a30e0f3089f5683f86e57e3e25af9531b7fda
omairshamshir <omairshamshir@gmail.com>
2017-11-30 01:55:35 +0500

    refactored oef code

--- lms/djangoapps/oef/views.py
+++ lms/djangoapps/oef/views.py
@@ -1,13 +1,11 @@
-import datetime
 import json
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from rest_framework import status
 
-from lms.djangoapps.oef.models import OefSurvey, TopicQuestion, UserOefSurvey, UserAnswers, OptionPriority
+from lms.djangoapps.oef.helpers import *
 
 
 def oef_dashboard(request):
@@ -57,108 +55,6 @@ def fetch_survey(request):
                                                    "priorities": priorities,
                                                    'is_completed': uos.status == 'completed',
                                                    })
-
-
-def get_user_survey_status(user, create_new_survey=True):
-    error = ''
-    is_eligible = True
-    survey = None
-    try:
-        uos = UserOefSurvey.objects.filter(user=user).latest('start_date')
-    except UserOefSurvey.DoesNotExist:
-        if create_new_survey:
-            survey = OefSurvey.objects.filter(is_enabled=True).latest('created')
-
-        return {
-            'error': error,
-            'is_eligible': is_eligible,
-            'survey': survey
-        }
-
-    if uos.status == 'in-progress':
-        error = 'You have a pending survey'
-        is_eligible = False
-        survey = uos.oef_survey
-    else:
-        limit = settings.OEF_RENEWAL_DAYS
-        if (datetime.date.today() - uos.start_date).days < limit:
-            is_eligible = False
-            error = 'You can request a new OEF survey only after %s days of last survey' % limit
-        elif create_new_survey:
-            survey = OefSurvey.objects.filter(is_enabled=True).latest('created')
-
-    return {
-        'error': error,
-        'is_eligible': is_eligible,
-        'survey': survey
-    }
-
-
-def get_user_survey(user, latest_survey):
-    try:
-        uos = UserOefSurvey.objects.get(user_id=user.id, status='in-progress')
-    except UserOefSurvey.DoesNotExist:
-        uos = UserOefSurvey()
-        uos.oef_survey = latest_survey
-        uos.user = user
-        uos.survey_date = datetime.date.today()
-        uos.start_date = datetime.date.today()
-        uos.save()
-
-    return uos
-
-
-def is_answered(uos, question_id):
-    return UserAnswers.objects.filter(user_survey_id=uos.id).filter(question_id=question_id).exists()
-
-
-def get_survey_topics(uos, survey_id):
-    topics = TopicQuestion.objects.filter(survey_id=survey_id)
-    parsed_topics = []
-    for index, topic in enumerate(topics):
-        options = list(topic.options.all())
-        options.sort(key=lambda x: x.priority.value, reverse=False)
-        answer = get_answer(uos, topic.id)
-        parsed_topics.append({
-            'title': topic.title,
-            'description': topic.description,
-            'index': index + 1,
-            'id': topic.id,
-            'options': options,
-            'answer': answer.selected_option.value if answer else None
-        })
-    return parsed_topics
-
-
-def get_option_priorities():
-    priorities = list(OptionPriority.objects.all())
-    priorities.sort(key=lambda x: x.value, reverse=False)
-    return priorities
-
-
-def get_answer(uos, question_id):
-    try:
-        return UserAnswers.objects.get(user_survey_id=uos.id, question_id=question_id)
-    except UserAnswers.DoesNotExist:
-        return None
-
-
-def create_answer(uos, data):
-    answer = UserAnswers()
-    answer.user_survey = uos
-    answer.question_id = int(data['topic_id'])
-    return answer
-
-
-def get_option(option_value):
-    return OptionPriority.objects.get(value=option_value)
-
-
-def check_if_complete(uos, answers_count):
-    if uos.oef_survey.topics.count() == answers_count:
-        uos.status = 'completed'
-        uos.completed_date = datetime.date.today()
-        uos.save()
 
 
 def save_answer(request):
