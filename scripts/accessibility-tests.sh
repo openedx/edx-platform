5ceb2537d44458a73dcd22b2a54e9d251c0104e0
Michael Youngstrom <myoungstrom@edx.org>
2017-12-04 11:14:55 -0500

    try with n=10

--- scripts/accessibility-tests.sh
+++ scripts/accessibility-tests.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-export NUMBER_OF_BOKCHOY_THREADS=10
-
 echo "Setting up for accessibility tests..."
 source scripts/jenkins-common.sh
 
 echo "Running explicit accessibility tests..."
-SELENIUM_BROWSER=phantomjs paver test_a11y
+SELENIUM_BROWSER=phantomjs paver test_a11y -n=10
 
 # The settings that we use are installed with the pa11ycrawler module
 export SCRAPY_SETTINGS_MODULE='pa11ycrawler.settings'
 
 echo "Running pa11ycrawler against test course..."
-paver pa11ycrawler --fasttest --skip-clean --fetch-course --with-html
+paver pa11ycrawler --fasttest --skip-clean --fetch-course --with-html -n=10
