b3d3c74fe9b1e2a33f627798bed2454d75bf6c36
Saleem Latif <saleem_ee@hotmail.com>
2017-12-11 14:42:29 +0500

    fix test failures

--- common/djangoapps/third_party_auth/pipeline.py
+++ common/djangoapps/third_party_auth/pipeline.py
@@ -567,9 +567,10 @@ def ensure_user_information(strategy, auth_entry, backend=None, user=None, socia
                 (current_provider.skip_email_verification or current_provider.send_to_registration_first))
 
     if not user:
+        details = kwargs.get('details', {})
         # pylint: disable=invalid-name
-        qs = {'email': kwargs['details']['email']} if kwargs['details'].get('email') else \
-            {'username': kwargs['details'].get('username')}
+        qs = {'email': details['email']} if details.get('email') else \
+            {'username': details.get('username')}
         if User.objects.filter(**qs).exists():
             return dispatch_to_login()
 
