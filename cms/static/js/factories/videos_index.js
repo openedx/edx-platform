e601767329005b39f37fb8abba6e6147ac3f53bc
Mushtaq Ali <mushtaque@edx.org>
2017-12-13 15:24:56 +0500

    Show transcripts on video uploads page - EDUCATOR-1662

--- cms/static/js/factories/videos_index.js
+++ cms/static/js/factories/videos_index.js
@@ -18,7 +18,8 @@ define([
         transcriptOrganizationCredentials,
         videoTranscriptSettings,
         isVideoTranscriptEnabled,
-        videoImageSettings
+        videoImageSettings,
+        transcriptAvailableLanguages
     ) {
         var activeView = new ActiveVideoUploadListView({
                 postUrl: videoHandlerUrl,
@@ -51,7 +52,11 @@ define([
                                 videoHandlerUrl: videoHandlerUrl,
                                 collection: updatedCollection,
                                 encodingsDownloadUrl: encodingsDownloadUrl,
-                                videoImageSettings: videoImageSettings
+                                videoImageSettings: videoImageSettings,
+                                videoTranscriptSettings: videoTranscriptSettings,
+                                transcriptAvailableLanguages: transcriptAvailableLanguages,
+                                videoSupportedFileFormats: videoSupportedFileFormats,
+                                isVideoTranscriptEnabled: isVideoTranscriptEnabled
                             });
                         $contentWrapper.find('.wrapper-assets').replaceWith(updatedView.render().$el);
                     });
@@ -63,7 +68,11 @@ define([
                 videoHandlerUrl: videoHandlerUrl,
                 collection: new Backbone.Collection(previousUploads),
                 encodingsDownloadUrl: encodingsDownloadUrl,
-                videoImageSettings: videoImageSettings
+                videoImageSettings: videoImageSettings,
+                videoTranscriptSettings: videoTranscriptSettings,
+                transcriptAvailableLanguages: transcriptAvailableLanguages,
+                videoSupportedFileFormats: videoSupportedFileFormats,
+                isVideoTranscriptEnabled: isVideoTranscriptEnabled
             });
         $contentWrapper.append(activeView.render().$el);
         $contentWrapper.append(previousView.render().$el);
