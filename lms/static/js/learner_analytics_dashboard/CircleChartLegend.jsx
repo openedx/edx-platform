dc6330b2303172fbe7a58d977797061dcd633620
AlasdairSwan <aswan@edx.org>
2017-12-14 16:03:44 -0500

    Adding in React components

--- lms/static/js/learner_analytics_dashboard/CircleChartLegend.jsx
+++ lms/static/js/learner_analytics_dashboard/CircleChartLegend.jsx
@@ -21,14 +21,38 @@ class CircleChartLegend extends React.Component {
                style={style}
                aria-hidden="true"></div>
           <span className="label">{label}</span>
-          <span className="percentage">{value}%</span>
+          <span className="percentage">{this.getPercentage(value)}</span>
         </li>
       );
     });
   }
 
+  getPercentage(value) {
+    const num = value * 100;
+
+    return `${num}%`;
+  }
+
+  renderList() {
+    return (
+      <ul className="legend-list">
+        {this.getList()}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="legend">
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+
 CircleChartLegend.propTypes = {
   data: PropTypes.array.isRequired
-};
+}
 
 export default CircleChartLegend;
