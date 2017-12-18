82051d0312313eb31153274e2916fb3c5dd5c2ff
Tasawer Nawaz <tasawer.nawaz@arbisoft.com>
2017-12-18 16:14:13 +0500

    zendesk proxy and front end of support form updated + filuploading is disbaled LEARNER-3594

--- openedx/core/djangoapps/zendesk_proxy/tests/test_v1_views.py
+++ openedx/core/djangoapps/zendesk_proxy/tests/test_v1_views.py
@@ -1,14 +1,14 @@
 """Tests for zendesk_proxy views."""
-from copy import deepcopy
-import ddt
 import json
-from mock import MagicMock, patch
+from copy import deepcopy
 
+import ddt
 from django.core.urlresolvers import reverse
 from django.test.utils import override_settings
+from mock import MagicMock, patch
 
+from openedx.core.djangoapps.zendesk_proxy.v1.views import ZendeskProxyThrottle
 from openedx.core.lib.api.test_utils import ApiTestCase
-from openedx.core.djangoapps.zendesk_proxy.v0.views import ZENDESK_REQUESTS_PER_HOUR
 
 
 @ddt.ddt
@@ -20,15 +20,23 @@ class ZendeskProxyTestCase(ApiTestCase):
     """Tests for zendesk_proxy views."""
 
     def setUp(self):
-        self.url = reverse('zendesk_proxy_v0')
+        self.url = reverse('zendesk_proxy_v1')
         self.request_data = {
-            'name': 'John Q. Student',
+            'requester': {
+                'email': 'JohnQStudent@example.com',
+                'name': 'John Q. Student'
+            },
+            'subject': 'Python Unit Test Help Request',
+            'comment': {
+                'body': "Help! I'm trapped in a unit test factory and I can't get out!",
+            },
             'tags': ['python_unit_test'],
-            'email': {
-                'from': 'JohnQStudent@example.com',
-                'subject': 'Python Unit Test Help Request',
-                'message': "Help! I'm trapped in a unit test factory and I can't get out!",
-            }
+            'custom_fields': [
+                {
+                    'id': '001',
+                    'value': 'demo-course'
+                }
+            ],
         }
         return super(ZendeskProxyTestCase, self).setUp()
 
@@ -50,11 +58,11 @@ class ZendeskProxyTestCase(ApiTestCase):
                         'content-type': 'application/json',
                         'Authorization': 'Bearer abcdefghijklmnopqrstuvwxyz1234567890'
                     },
-                    'data': '{"ticket": {"comment": {"body": "Help! I\'m trapped in a unit test factory and I can\'t get out!"}, "subject": "Python Unit Test Help Request", "tags": ["python_unit_test"], "requester": {"name": "John Q. Student", "email": "JohnQStudent@example.com"}}}'  # pylint: disable=line-too-long
+                    'data': '{"ticket": {"comment": {"body": "Help! I\'m trapped in a unit test factory and I can\'t get out!", "uploads": null}, "tags": ["python_unit_test"], "subject": "Python Unit Test Help Request", "custom_fields": [{"id": "001", "value": "demo-course"}], "requester": {"name": "John Q. Student", "email": "JohnQStudent@example.com"}}}'  # pylint: disable=line-too-long
                 }
             )
 
-    @ddt.data('name', 'tags', 'email')
+    @ddt.data('requester', 'tags')
     def test_bad_request(self, key_to_delete):
         test_data = deepcopy(self.request_data)
         _ = test_data.pop(key_to_delete)
@@ -66,40 +74,6 @@ class ZendeskProxyTestCase(ApiTestCase):
             content_type='application/json'
         )
         self.assertHttpBadRequest(response)
-
-    @override_settings(
-        ZENDESK_URL=None,
-        ZENDESK_OAUTH_ACCESS_TOKEN=None
-    )
-    def test_missing_settings(self):
-        response = self.request_without_auth(
-            'post',
-            self.url,
-            data=json.dumps(self.request_data),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, 503)
-
-    @ddt.data(201, 400, 401, 403, 404, 500)
-    def test_zendesk_status_codes(self, mock_code):
-        with patch('requests.post', return_value=MagicMock(status_code=mock_code)):
-            response = self.request_without_auth(
-                'post',
-                self.url,
-                data=json.dumps(self.request_data),
-                content_type='application/json'
-            )
-            self.assertEqual(response.status_code, mock_code)
-
-    def test_unexpected_error_pinging_zendesk(self):
-        with patch('requests.post', side_effect=Exception("WHAMMY")):
-            response = self.request_without_auth(
-                'post',
-                self.url,
-                data=json.dumps(self.request_data),
-                content_type='application/json'
-            )
-            self.assertEqual(response.status_code, 500)
 
     @override_settings(
         CACHES={
@@ -114,7 +88,8 @@ class ZendeskProxyTestCase(ApiTestCase):
         Confirm rate limits work as expected. Note that drf's rate limiting makes use of the default cache to enforce
         limits; that's why this test needs a "real" default cache (as opposed to the usual-for-tests DummyCache)
         """
-        for _ in range(ZENDESK_REQUESTS_PER_HOUR):
+
+        for _ in range(ZendeskProxyThrottle().num_requests):
             self.request_without_auth('post', self.url)
         response = self.request_without_auth('post', self.url)
         self.assertEqual(response.status_code, 429)
