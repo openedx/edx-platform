99cfb897b60a51d2a2c1d79589b285d22a9f56d4
Jesse Zoldak <zoldak@edx.org>
2017-12-19 11:02:36 -0500

    Update paver to current master hash

--- pavelib/paver_tests/test_database.py
+++ pavelib/paver_tests/test_database.py
@@ -1,33 +1,30 @@
 """
 Tests for the Paver commands for updating test databases
 """
-import unittest
 import os
+from unittest import TestCase
 
 import boto
-from moto import mock_s3
 from mock import patch
 
+from common.test.utils import MockS3Mixin
 from pavelib.database import verify_fingerprint_in_bucket
 
-class TestPaverDatabaseTasks(unittest.TestCase):
 
-
+class TestPaverDatabaseTasks(MockS3Mixin, TestCase):
+    """Tests for the Database cache file manipulation."""
     def setUp(self):
+        super(TestPaverDatabaseTasks, self).setUp()
         self.conn = boto.connect_s3()
-        conn.create_bucket('moto_test_bucket')
-        self.bucket = conn.get_bucket('moto_test_bucket')
+        self.conn.create_bucket('moto_test_bucket')
+        self.bucket = self.conn.get_bucket('moto_test_bucket')
 
-
-    @mock_s3
     @patch.dict(os.environ, {'DB_CACHE_S3_BUCKET': 'moto_test_bucket'})
     def test_fingerprint_in_bucket(self):
         key = boto.s3.key.Key(bucket=self.bucket, name='testfile.zip')
         key.set_contents_from_string('this is a test')
         self.assertTrue(verify_fingerprint_in_bucket('testfile'))
 
-
-    @mock_s3
     @patch.dict(os.environ, {'DB_CACHE_S3_BUCKET': 'moto_test_bucket'})
     def test_fingerprint_not_in_bucket(self):
         key = boto.s3.key.Key(bucket=self.bucket, name='testfile.zip')
