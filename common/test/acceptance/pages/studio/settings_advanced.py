097c0665fca26c703169f835679d1950b32e2787
Jillian Vogel <jill@opencraft.com>
2017-12-23 00:34:06 +1030

    Hides Video Auto Advance from Advanced Settings if disabled
    and fixes affected tests.

--- common/test/acceptance/pages/studio/settings_advanced.py
+++ common/test/acceptance/pages/studio/settings_advanced.py
@@ -178,7 +178,6 @@ class AdvancedSettingsPage(CoursePage):
             'course_image',
             'banner_image',
             'video_thumbnail_image',
-            'video_auto_advance',
             'cosmetic_display_price',
             'advertised_start',
             'announcement',
