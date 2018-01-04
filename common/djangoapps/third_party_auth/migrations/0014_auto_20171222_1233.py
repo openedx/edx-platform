9071117ea2cd931591d10333f989381db4aacec6
Douglas Hall <douglas_hall@icloud.com>
2018-01-04 16:57:52 -0500

    Revert "Revert "ENT-779 Remove ProviderConfig.drop_existing_session field from DB.""
    This reverts commit 23e17b3c35d96853c35be17cf8589d885c1dc6b2.

---
+++ common/djangoapps/third_party_auth/migrations/0014_auto_20171222_1233.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('third_party_auth', '0013_sync_learner_profile_data'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='ltiproviderconfig',
+            name='drop_existing_session',
+        ),
+        migrations.RemoveField(
+            model_name='oauth2providerconfig',
+            name='drop_existing_session',
+        ),
+        migrations.RemoveField(
+            model_name='samlproviderconfig',
+            name='drop_existing_session',
+        ),
+    ]
