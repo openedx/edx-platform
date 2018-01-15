5430b3633f6bcfca25e44ed7bc869518647c8eff
Nimisha Asthagiri <nasthagiri@edx.org>
2018-01-15 12:20:27 -0500

    Move COURSE_START_DATE_CHANGED signal

--- openedx/core/djangoapps/content/course_overviews/signals.py
+++ openedx/core/djangoapps/content/course_overviews/signals.py
@@ -3,13 +3,17 @@ Signal handler for invalidating cached course overviews
 """
 import logging
 
+from django.dispatch import Signal
 from django.dispatch.dispatcher import receiver
 
 from .models import CourseOverview
-from openedx.core.djangoapps.signals.signals import COURSE_PACING_CHANGED, COURSE_START_DATE_CHANGED
+from openedx.core.djangoapps.signals.signals import COURSE_PACING_CHANGED
 from xmodule.modulestore.django import SignalHandler
 
 LOG = logging.getLogger(__name__)
+
+
+COURSE_START_DATE_CHANGED = Signal(providing_args=["updated_course_overview", "previous_start_date"])
 
 
 @receiver(SignalHandler.course_published)
