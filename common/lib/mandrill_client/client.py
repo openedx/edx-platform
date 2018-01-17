951a0df656f5d8bfecc2520b6c258fdf9d9874e6
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2018-01-17 21:08:10 +0500

    Improve: add course enrollment email

--- common/lib/mandrill_client/client.py
+++ common/lib/mandrill_client/client.py
@@ -50,4 +50,25 @@ class MandrillClient(object):
             # A mandrill error occurred: <class 'mandrill.UnknownSubaccountError'> - No subaccount exists with the id 'customer-123'
             raise
 
+    def send_course_notification_email(self, user_email, context):
+        try:
+            result = self.mandrill_client.messages.send_template(
+                template_name='enrollment-confirmation',
+                template_content=[],
+                message={
+                    'from_email': settings.NOTIFICATION_FROM_EMAIL,
+                    'to': [{ 'email': user_email }],
+                    'global_merge_vars': [
+                        {'name': 'course_name', 'content': context['course_name']},
+                        {'name': 'course_link', 'content': context['course_link']},
+                    ]
+                },
+            )
+            log.info(result)
+        except mandrill.Error, e:
+            # Mandrill errors are thrown as exceptions
+            log.error('A mandrill error occurred: %s - %s' % (e.__class__, e))
+            # A mandrill error occurred: <class 'mandrill.UnknownSubaccountError'> - No subaccount exists with the id 'customer-123'
+            raise
+
 
--- openedx/core/djangoapps/timed_notification/notifications.py
+++ openedx/core/djangoapps/timed_notification/notifications.py
@@ -4,6 +4,7 @@ from xmodule.modulestore.django import modulestore
 from core import send_course_notification_email, get_course_link
 
 from django.dispatch import receiver
+from common.lib.mandrill_client.client import MandrillClient
 
 
 @receiver(ENROLL_STATUS_CHANGE)
@@ -14,7 +15,4 @@ def enrollment_confirmation(sender, event=None, user=None, **kwargs):
             'course_name': course.display_name,
             'course_link': get_course_link(course_id=course.id)
         }
-        send_course_notification_email(course=course,
-                                       mako_template_path='timed_notification/enrollment_confirmation.txt',
-                                       context=context,
-                                       to_list=[user])
+        MandrillClient().send_course_notification_email(user.email, context)
