d9ce5fa0c4b4789c57820a122faaed1f6208fbb8
sandroroux <aroux@edx.org>
2018-01-22 14:38:58 -0500

    fixup

--- lms/djangoapps/completion/management/commands/ComputeCompletion.py
+++ lms/djangoapps/completion/management/commands/ComputeCompletion.py
@@ -18,14 +18,13 @@ class CompletionTraverser:
 
 
     def traverse(self):
-        self._compute_course_completion(self._root_name, self._course_blocks)
+        self._compute_course_completion(self._root_name)
 
 
-    def _compute_course_completion(self, name_of_root_block, course_blocks):
-        # print "Number of course_blocks: %d" % (len(course_blocks)) 
+    def _compute_course_completion(self, name_of_root_block):
         course_completion = self._compute_completion_at_block(name_of_root_block)
 
-        # print "Number of completed blocks: %f" % (course_completion)
+        print "Number of completed blocks: %f" % (course_completion)
 
 
     def _compute_completion_at_block(self, current_block_name):
@@ -49,14 +48,14 @@ class CompletionTraverser:
 
 
     def _compute_childrens_completion(self, names_of_children):
-        completion_of_children = 0.0
+        childrens_completion = 0.0
         for name_of_child in names_of_children:
                 childs_completion = self._compute_completion_at_block(
                     name_of_child 
                 )
-                completion_of_children += childs_completion
+                childrens_completion += childs_completion
 
-        return completion_of_children
+        return childrens_completion
 
 
     def _get_block(self, name_of_block):
@@ -77,11 +76,12 @@ class Command(BaseCommand):
         # Course has same contents as MIT's 6.001x:
         # https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x-11
         course_name = 'course-v1:wert+qw4tqw4t+qertqert'
+        course_name = 'course-v1:MITx+8.Mech+Spring2018'
         course_usage_key = self._create_course_usage_key(course_name)
 
         user = UserFactory.create(
-            username="staff",
-            email="staff@example.com"
+            username="edx",
+            email="edx@example.com"
             )
         site = SiteFactory.create()
 
@@ -99,35 +99,9 @@ class Command(BaseCommand):
             course_blocks = api_raw_course_data['blocks']
 
             traverser = CompletionTraverser(name_of_course_root, course_blocks)
-
-            # print "== get_blocks =="
-            # cProfile.runctx(
-            #     'get_blocks(request, course_usage_key, user=user, requested_fields=[\'completion\', \'children\'])', 
-            #     globals(),
-            #     locals()
-            # )
-
-            # print "== Calculate Completion DFS =="
-            # cProfile.runctx('traverser.traverse()', globals(), locals())
             traverser.traverse()
 
 
     def _create_course_usage_key(self, course_name):
         course_key = CourseKey.from_string(course_name)
         return modulestore().make_course_usage_key(course_key)
-
-
-    def _bfsTraverseBlocks(self, course_blocks):
-        root_name = course_blocks['root']
-        course_nodes = course_blocks['blocks']
-
-        traversal_queue = deque()
-        traversal_queue.appendleft(course_nodes[root_name])
-
-        current_block = None
-        while len(traversal_queue) > 0:
-            current_block = traversal_queue.pop()
-            current_block_completion = current_block.get('completion', None)
-
-            for child_name in current_block.get('children', []):
-                traversal_queue.appendleft(course_nodes[child_name])
