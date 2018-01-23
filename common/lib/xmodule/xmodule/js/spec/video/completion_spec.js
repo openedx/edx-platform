078ed719a05ba3f4fd15eb76de4d6ba092ed60c7
Sofiya Semenova <ssemenova@edx.org>
2018-01-23 14:23:15 -0500

    Test that video is only completed upon being watched

--- common/lib/xmodule/xmodule/js/spec/video/completion_spec.js
+++ common/lib/xmodule/xmodule/js/spec/video/completion_spec.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     describe('VideoPlayer completion', function() {
-        var state, oldOTBD;
+        var state, oldOTBD, completionAjaxCall;
 
         beforeEach(function() {
             oldOTBD = window.onTouchBasedDevice;
@@ -13,9 +13,18 @@
                 recordedYoutubeIsAvailable: true,
                 completionEnabled: true,
                 publishCompletionUrl: 'https://example.com/publish_completion_url'
-
             });
             state.completionHandler.completeAfterTime = 20;
+
+            completionAjaxCall = {
+                url: state.config.publishCompletionUrl,
+                type: 'POST',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify({completion: 1.0}),
+                success: jasmine.any(Function),
+                error: jasmine.any(Function)
+            };
         });
 
         afterEach(function() {
@@ -29,15 +38,7 @@
 
         it('calls the completion api when marking an object complete', function() {
             state.completionHandler.markCompletion(Date.now());
-            expect($.ajax).toHaveBeenCalledWith({
-                url: state.config.publishCompletionUrl,
-                type: 'POST',
-                contentType: 'application/json',
-                dataType: 'json',
-                data: JSON.stringify({completion: 1.0}),
-                success: jasmine.any(Function),
-                error: jasmine.any(Function)
-            });
+            expect($.ajax).toHaveBeenCalledWith(completionAjaxCall);
             expect(state.completionHandler.isComplete).toEqual(true);
         });
 
@@ -49,6 +50,16 @@
             // But the handler is not called again after the block is completed.
             state.el.trigger('timeupdate', 30.0);
             expect(state.completionHandler.markCompletion).not.toHaveBeenCalled();
+        });
+
+        it('does not call the completion api on the LMS when the video is loaded but not seen', function() {
+            spyOn(window, 'VerticalStudentView').and.callThrough();
+            // The VerticalStudentView object is created to kick off the function that checks if
+            // each vertical is completable by viewing, and, if so, sends an ajax call to mark completion
+            // eslint-disable-next-line no-new
+            new window.VerticalStudentView(null, '#video_example');
+            expect(window.VerticalStudentView).toHaveBeenCalled();
+            expect($.ajax).not.toHaveBeenCalledWith(completionAjaxCall);
         });
 
         it('calls the completion api on the LMS when the video ends', function() {
