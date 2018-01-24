970bc6a50d8d8e1d479c7b7b9117cf486500efea
Calen Pennington <cale@edx.org>
2018-01-24 16:31:20 -0500

    Move the context course and sock into require_page compatible forms

--- cms/static/js/pages/course.js
+++ cms/static/js/pages/course.js
@@ -1,6 +1,10 @@
-define(
+(function(define) {
+    'use strict';
+
+    define(
         ['js/models/course'],
         function(ContextCourse) {
             window.course = new ContextCourse(window.pageFactoryArguments.ContextCourse[0]);
         }
     );
+}).call(this, define || RequireJS.define);
