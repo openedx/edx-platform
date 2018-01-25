58c765bc317876a72cf8d1b46f03125eb0f1e24d
Jeremy Bowman <jbowman@edx.org>
2018-01-25 18:13:14 -0500

    PLAT-1834 Support running a11y tests in tox

--- scripts/accessibility-tests.sh
+++ scripts/accessibility-tests.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
+###############################################################################
+#
+#   accessibility-tests.sh
+#
+#   Execute the accessibility tests for edx-platform.
+#
+#   If the optional `DJANGO_VERSION` environment variable is defined, it
+#   specifies which version of Django should be installed when running the
+#   tests inside a `tox` virtualenv.  If undefined, the tests are run using
+#   the currently active Python environment.
+#
+###############################################################################
+
+if [[ $DJANGO_VERSION == '1.11' ]]; then
+    TOX="tox -e py27-django111 --"
+elif [[ $DJANGO_VERSION == '1.10' ]]; then
+    TOX="tox -e py27-django110 --"
+elif [[ $DJANGO_VERSION == '1.9' ]]; then
+    TOX="tox -e py27-django19 --"
+else
+    TOX=""
+fi
+
 
 echo "Setting up for accessibility tests..."
 source scripts/jenkins-common.sh
 
 echo "Running explicit accessibility tests..."
-SELENIUM_BROWSER=phantomjs paver test_a11y
+SELENIUM_BROWSER=phantomjs $TOX paver test_a11y
 
 # The settings that we use are installed with the pa11ycrawler module
 export SCRAPY_SETTINGS_MODULE='pa11ycrawler.settings'
 
 echo "Running pa11ycrawler against test course..."
-paver pa11ycrawler --fasttest --skip-clean --fetch-course --with-html
+$TOX paver pa11ycrawler --fasttest --skip-clean --fetch-course --with-html
