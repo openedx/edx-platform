265aff02a2934599db14bb6a6bfdb524800e69b9
Rosheen Pervaiz <rosheen.pervaiz@arbisoft.com>
2018-01-29 16:22:54 +0500

    Review management commands to start celery tasks and schedule in jenkins (#226)
    * management command added to start celery task 1: get_third_party_surveys

--- lms/djangoapps/third_party_surveys/tasks.py
+++ lms/djangoapps/third_party_surveys/tasks.py
@@ -1,26 +1,20 @@
 from datetime import datetime
+from logging import getLogger
 
 import pytz
-from celery.schedules import crontab
-from celery.task import periodic_task
 from django.db import IntegrityError
-from logging import getLogger
 
 from common.lib.surveygizmo_client.client import SurveyGizmoClient
 from lms.djangoapps.third_party_surveys.models import ThirdPartySurvey
 
-from django.core.mail import EmailMultiAlternatives, get_connection
-from django.conf import settings
-
-
 log = getLogger(__name__)
 
 
-@periodic_task(run_every=crontab(minute=1, hour=12))
 def get_third_party_surveys():
     """
     Periodic Task that will run on daily basis and will sync the response data
     of all surveys thorough Survey Gizmo APIs
+    We are scheduling this task through Jenkins instead of celery beat.
     """
     try:
         last_survey = ThirdPartySurvey.objects.latest('request_date')
