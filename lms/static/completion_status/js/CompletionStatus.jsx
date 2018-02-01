e81dc69ac133cc27643294f8b2d2e9f5b1e4b640
George Babey <gbabey@edx.org>
2018-01-31 20:57:53 -0500

    CompletionStatus component with 100% more confetti

--- lms/static/completion_status/js/CompletionStatus.jsx
+++ lms/static/completion_status/js/CompletionStatus.jsx
@@ -4,14 +4,17 @@ Wrapper for React/Paragon accessible status bar
 
 import React from 'react';
 import Websocket from 'react-websocket'
+import Confetti from 'react-confetti'
+import windowDimensions from 'react-window-dimensions';
 
-export class CompletionStatus extends React.Component {
+
+class CompletionStatus extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             course_id: '',
-            percent_complete: 0,
+            percent_complete: 100,
         };
 
         // TODO: do we need to send anything?
@@ -28,6 +31,9 @@ export class CompletionStatus extends React.Component {
         //receives messages from the connected websocket
         let result = JSON.parse(data);
 
+        if (result.percent_complete === 100) {
+        }
+
         // we've received an updated completion status event
         this.setState(result);
     }
@@ -40,16 +46,17 @@ export class CompletionStatus extends React.Component {
 
     render() {
         const socketUrl = `ws://${window.location.host}/${this.props.socketPath}/`;
-        const barStyle = {
-            width: this.state.percent_complete
-        };
+
         return (
-            <div className="row">
+            <div>
                 <Websocket ref="socket" url={socketUrl}
                     onMessage={this.handleData.bind(this)} reconnect={true}/>
-                <div className="progress">
-                    <div className="progress-bar" role="progressbar" style={barStyle} aria-valuenow="{this.state.percent_complete}" aria-valuemin="0" aria-valuemax="100">{this.state.percent_complete}%</div>
-                </div>
+                <label>Completion: </label><progress max="100" value={this.state.percent_complete}></progress><span>{this.state.percent_complete}%</span>
+                {this.state.percent_complete === 100 &&
+                    <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}}>
+                        <Confetti width={this.props.width} height={this.props.height}/>
+                    </div>
+                }
             </div>
         )
     }
@@ -58,3 +65,8 @@ export class CompletionStatus extends React.Component {
 CompletionStatus.propTypes = {
     socketPath: React.PropTypes.string
 };
+
+// windowSize is a HOC that sets props.windowHeight and props.windowWidth
+const sizeAwareCompletionStatus = windowDimensions()(CompletionStatus);
+
+export { sizeAwareCompletionStatus as CompletionStatus };
