2ffa14dbd13fec458203e050ce99ec2bee761c8d
George Babey <gbabey@edx.org>
2018-02-01 10:24:12 -0500

    Added modal upon completion

--- lms/static/completion_status/js/CompletionStatus.jsx
+++ lms/static/completion_status/js/CompletionStatus.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 import Websocket from 'react-websocket'
 import Confetti from 'react-confetti'
 import windowDimensions from 'react-window-dimensions';
+import Modal from 'react-modal';
 
 
 class CompletionStatus extends React.Component {
@@ -15,10 +16,12 @@ class CompletionStatus extends React.Component {
         this.state = {
             course_id: '',
             percent_complete: 0,
+            modalIsOpen: false,
         };
 
-        // TODO: do we need to send anything?
         this.sendSocketMessage = this.sendSocketMessage.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     componentDidMount() {}
@@ -31,6 +34,10 @@ class CompletionStatus extends React.Component {
         //receives messages from the connected websocket
         let result = JSON.parse(data);
 
+        if (result.percent_complete === 100) {
+            this.openModal();
+        }
+
         // we've received an updated completion status event
         this.setState(result);
     }
@@ -41,19 +48,68 @@ class CompletionStatus extends React.Component {
        socket.state.ws.send(JSON.stringify(message))
     }
 
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
     render() {
         const socketUrl = `ws://${window.location.host}/${this.props.socketPath}/`;
+        const isComplete = this.state.percent_complete === 100;
 
         return (
             <div>
                 <Websocket ref="socket" url={socketUrl}
                     onMessage={this.handleData.bind(this)} reconnect={true}/>
                 <label>Completion: </label><progress max="100" value={this.state.percent_complete}></progress><span>{this.state.percent_complete}%</span>
-                {this.state.percent_complete === 100 &&
+                {isComplete &&
                     <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}}>
                         <Confetti width={this.props.width} height={this.props.height}/>
                     </div>
                 }
+                <Modal isOpen={this.state.modalIsOpen}
+                            style={{
+                                overlay: {
+                                  position: 'fixed',
+                                  top: 0,
+                                  left: 0,
+                                  right: 0,
+                                  bottom: 0,
+                                  backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                                },
+                                content: {
+                                    position: 'absolute',
+                                    top: '25%',
+                                    bottom: '-25%',
+                                    left: '25%',
+                                    right: '-25%',
+                                    maxHeight: '300px',
+                                    maxWidth: '600px',
+                                    border: '1px solid #ccc',
+                                    background: '#fff',
+                                    overflow: 'auto',
+                                    WebkitOverflowScrolling: 'touch',
+                                    borderRadius: '4px',
+                                    outline: 'none',
+                                    padding: '20px'
+                                }
+
+                            }}
+                >
+                    <h2>Congratulations!</h2>
+                    <p>You've done it! You've finished the course.</p>
+                    <p>Continue your lifetime of learning by exploring new courses.
+                    Here are a few we think you might like:</p>
+                    <ul>
+                        <li><a href="#">Linear Algebra - Foundations to Frontiers</a></li>
+                        <li><a href="#">Essential Mathematics for Artificial Intelligence</a></li>
+                        <li><a href="#">Foundations of Data Structures</a></li>
+                    </ul>
+                    <button onClick={this.closeModal} style={{float: 'right'}}>OK</button>
+                </Modal>
             </div>
         )
     }
