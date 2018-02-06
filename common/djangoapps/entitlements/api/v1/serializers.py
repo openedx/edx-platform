449c6903a67aedfeddfcabb6152af0e784ac9bce
Michael LoTurco <mikeloturco13@gmail.com>
2018-02-06 15:06:20 -0500

    Add Support endpoint for Course Entitlements

--- common/djangoapps/entitlements/api/v1/serializers.py
+++ common/djangoapps/entitlements/api/v1/serializers.py
@@ -1,10 +1,15 @@
+"""
+Serializers for all Course Entitlement related return objects.
+"""
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from entitlements.models import CourseEntitlement
+from entitlements.models import CourseEntitlement, CourseEntitlementSupportDetail
+from openedx.core.lib.api.serializers import CourseKeyField
 
 
 class CourseEntitlementSerializer(serializers.ModelSerializer):
+    """ Serialize a learner's course entitlement and related information. """
     user = serializers.SlugRelatedField(slug_field='username', queryset=get_user_model().objects.all())
     enrollment_course_run = serializers.CharField(
         source='enrollment_course_run.course_id',
@@ -24,3 +29,40 @@ class CourseEntitlementSerializer(serializers.ModelSerializer):
             'mode',
             'order_number'
         )
+
+
+class CourseEntitlementSupportDetailSerializer(serializers.ModelSerializer):
+    """ Serialize the details of a support team interaction with a learner's course entitlement. """
+    support_user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    unenrolled_run = CourseKeyField('unenrolled_run.id')
+
+    class Meta:
+        model = CourseEntitlementSupportDetail
+        fields = (
+            'support_user',
+            'reason',
+            'comments',
+            'unenrolled_run'
+        )
+
+
+class SupportCourseEntitlementSerializer(CourseEntitlementSerializer):
+    """
+    Serialize a learner's course entitlement with details from all support team interactions with that entitlement.
+    """
+    support_details = serializers.SerializerMethodField()
+
+    def get_support_details(self, model):
+        """
+        Returns a serialized set of all support interactions with the course entitlement
+        """
+        qset = CourseEntitlementSupportDetail.objects.filter(entitlement=model).order_by('-created')
+        return CourseEntitlementSupportDetailSerializer(qset, many=True).data
+
+    class Meta:
+        model = CourseEntitlement
+        fields = CourseEntitlementSerializer.Meta.fields + ('support_details', )
