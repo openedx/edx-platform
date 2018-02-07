7079802e18666fce7bb7e50bf4e2867719efb78d
melvinsoft <melvinsoft@gmail.com>
2018-02-07 14:00:46 -0300

    set tiers getter

--- lms/envs/devstack_appsembler.py
+++ lms/envs/devstack_appsembler.py
@@ -81,6 +81,7 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 if FEATURES.get("ENABLE_TIERS_APP", True):
     TIERS_ORGANIZATION_MODEL = 'organizations.Organization'
     TIERS_EXPIRED_REDIRECT_URL = None
+    TIERS_ORGANIZATION_TIER_GETTER_NAME = 'get_tier_for_org'
 
     TIERS_DATABASE_URL = AUTH_TOKENS.get('TIERS_DATABASE_URL')
     DATABASES['tiers'] = dj_database_url.parse(TIERS_DATABASE_URL)
