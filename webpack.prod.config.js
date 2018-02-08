24794edd8b976c5efa8404cdfad80a5c723cb57b
Tyler Hallada <thallada@edx.org>
2018-02-08 11:23:35 -0500

    Update package-lock and remove unused webpack conf

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,13 +3,7 @@
 'use strict';
 
 var Merge = require('webpack-merge');
-var path = require('path');
 var webpack = require('webpack');
-
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var extractSass = new ExtractTextPlugin({
-    filename: 'css/[name].[contenthash].css'
-});
 
 var commonConfig = require('./webpack.common.config.js');
 
@@ -26,35 +20,5 @@ module.exports = Merge.smart(commonConfig, {
             minimize: true
         }),
         new webpack.optimize.UglifyJsPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /(.scss|.css)$/,
-                include: [
-                    /paragon/,
-                    /font-awesome/
-                ],
-                use: extractSass.extract({
-                    use: [{
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localIdentName: '[name]__[local]___[hash:base64:5]'
-                        }
-                    }, {
-                        loader: 'sass-loader',
-                        options: {
-                            data: '$base-rem-size: 0.625; @import "paragon-reset";',
-                            includePaths: [
-                                path.join(__dirname, './node_modules/@edx/paragon/src/utils'),
-                                path.join(__dirname, './node_modules/')
-                            ]
-                        }
-                    }],
-                    fallback: 'style-loader'
-                })
-            }
-        ]
-    }
+    ]
 });
