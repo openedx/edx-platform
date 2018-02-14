292ebf4e6ce905ff63cf7dca495bebc21def8e93
Bessie Steinberg <bsteinberg@edx.org>
2018-02-14 10:28:17 -0500

    add new url for about page

--- common/djangoapps/digital_book/api/urls.py
+++ common/djangoapps/digital_book/api/urls.py
@@ -1,11 +1,19 @@
+from django.conf import settings
 from django.conf.urls import url
 
 from .views import DigitalBookViewSet
 
 urlpatterns = [
-    url(r'^v1/digital_books',
+    url(r'^v1/digital_books$',
         DigitalBookViewSet.as_view({
             'post': 'create',
         }),
         name='digitalbook'),
+    url(
+        r'^v1/digital_books/{}/about$'.format(
+            settings.COURSE_ID_PATTERN, #TODO: digitalbook pattern?
+        ),
+        DigitalBookViewSet.digital_book_about,
+        name='about_digital_book',
+    ),
 ]
