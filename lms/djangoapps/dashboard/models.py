8b76c264a6d9ce721a4233fc5037153cf0b4eb48
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2018-02-16 15:47:47 +0500

    Fixed issue where git logs aren't functioning
    Use the course key as the primary key for storing coure import logs in
    mongo, instead of the deprecated course key (this only applies to
    courses imported via git_import.py).
    
    No effort is made to migrate existing logs, since they are ephemeral.

--- lms/djangoapps/dashboard/models.py
+++ lms/djangoapps/dashboard/models.py
@@ -13,6 +13,9 @@ class CourseImportLog(mongoengine.Document):
     import_log = mongoengine.StringField(max_length=20 * 65535)
     git_log = mongoengine.StringField(max_length=65535)
     repo_dir = mongoengine.StringField(max_length=128)
+    commit = mongoengine.StringField(max_length=40, null=True)
+    author = mongoengine.StringField(max_length=500, null=True)
+    date = mongoengine.DateTimeField()
     created = mongoengine.DateTimeField()
     meta = {'indexes': ['course_id', 'created'],
             'allow_inheritance': False}
