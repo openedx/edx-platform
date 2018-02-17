0508f421674eec41aab32b61a0961443c974ce08
Mohammad Nadeem <muhammad.nadeem@arbisoft.com>
2018-02-18 02:14:47 +0500

    Fix 404 more stable fix (#295) (#296)

--- lms/djangoapps/philu_overrides/views.py
+++ lms/djangoapps/philu_overrides/views.py
@@ -211,4 +211,4 @@ def render_404(request):
     try:
         return HttpResponseNotFound(render_to_string('custom_static_templates/404.html', {}, request=request))
     except:
-        return redirect("404")
+        return redirect("404/")
