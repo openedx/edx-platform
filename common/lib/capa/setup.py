c2ce6bcee028985b6309e025aab82883e9fd1755
Sven Marnach <sven@marnach.net>
2018-02-26 10:16:05 +0100

    Move requirements of local packages to the global requirements.
    Moving all dependencies to the global requirements ensuires
    consistency and simplifies installation. In particular, it speeds
    up Docker image build for development as all requirements can be
    installed in an early layer of the Dockerfile which is not invalidated
    each time we change code anywhere in the project.

--- common/lib/capa/setup.py
+++ common/lib/capa/setup.py
@@ -4,9 +4,4 @@ setup(
     name="capa",
     version="0.1",
     packages=find_packages(exclude=["tests"]),
-    install_requires=[
-        "setuptools",
-        "lxml",
-        "pytz"
-    ],
 )
--- common/lib/symmath/setup.py
+++ common/lib/symmath/setup.py
@@ -4,7 +4,4 @@ setup(
     name="symmath",
     version="0.2",
     packages=["symmath"],
-    install_requires=[
-        "sympy==0.7.1",
-    ],
 )
