4452bc508adab8bb7f409272a7fe3a60c91c16db
melvinsoft <melvinsoft@gmail.com>
2018-02-27 13:44:41 -0300

    change course creation for get_or_create

--- openedx/core/djangoapps/appsembler/html_certificates/signals.py
+++ openedx/core/djangoapps/appsembler/html_certificates/signals.py
@@ -15,15 +15,8 @@ def set_default_mode_on_course_publish(sender, course_key, **kwargs): # pylint: 
     Catches the signal that a course has been published in Studio and
     creates a CourseMode in the default mode
     """
-    try:
-        default_mode = CourseMode.objects.get(
-            course_id=course_key,
-            mode_slug=settings.DEFAULT_COURSE_MODE_SLUG
-        )
-    except ObjectDoesNotExist:
-        default_mode = CourseMode(
-            course_id=course_key,
-            mode_slug=settings.DEFAULT_COURSE_MODE_SLUG,
-            mode_display_name=settings.DEFAULT_MODE_NAME_FROM_SLUG
-        )
-    default_mode.save()
+    default_mode, created = CourseMode.objects.get_or_create(
+        course_id=course_key,
+        mode_slug=settings.DEFAULT_COURSE_MODE_SLUG,
+        mode_display_name=settings.DEFAULT_MODE_NAME_FROM_SLUG
+    )
