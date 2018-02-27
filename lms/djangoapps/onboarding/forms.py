f86ad6ae2557393284d615d5d5a6ee2f8ad5584e
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2018-02-27 19:06:12 +0500

    Add back-end validation for date inputs (#320)
    * Improve: add back-end validation for effective date
    
    * Improve: add back-end validation for start_month_year
    
    * Fix: correct error message
    
    * Fix: modify error messages

--- lms/djangoapps/onboarding/forms.py
+++ lms/djangoapps/onboarding/forms.py
@@ -174,6 +174,16 @@ class UserInfoModelForm(BaseOnboardingModelForm):
 
         raise forms.ValidationError(ugettext_noop('Please select language.'))
 
+    def clean_start_month_year(self):
+        start_month_year = datetime.strptime(
+            self.cleaned_data['start_month_year'],
+            '%m/%Y')
+
+        if start_month_year > datetime.now():
+            raise forms.ValidationError(ugettext_noop("Please enter a valid start month/year"))
+
+        return self.cleaned_data['start_month_year']
+
     class Meta:
         """
         The meta class used to customize the default behaviour of form fields
@@ -861,6 +871,9 @@ class OrganizationMetricModelForm(BaseOnboardingModelForm):
         if can_provide_info and not last_fiscal_year_end_date:
             raise forms.ValidationError(ugettext_noop(EMPTY_FIELD_ERROR.format("End date for Last Fiscal Year")))
 
+        if last_fiscal_year_end_date > datetime.now().date():
+            raise forms.ValidationError(ugettext_noop("Please enter a valid End date for Last Fiscal Year"))
+
         return last_fiscal_year_end_date
 
     def clean_total_clients(self):
@@ -1062,6 +1075,9 @@ class OrganizationMetricModelUpdateForm(OrganizationMetricModelForm):
 
         if not last_fiscal_year_end_date:
             raise forms.ValidationError(ugettext_noop(EMPTY_FIELD_ERROR.format("End date for Last Fiscal Year")))
+
+        if last_fiscal_year_end_date > datetime.now().date():
+            raise forms.ValidationError(ugettext_noop("Please enter a valid End date for Last Fiscal Year"))
 
         return last_fiscal_year_end_date
 
