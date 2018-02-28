3e6960aeb53e6e2715b952793639a4a1a1ff0f56
Michael LoTurco <mikeloturco13@gmail.com>
2018-02-28 14:24:06 -0500

    Adds EntitlementSupportView, Template, and base react element

---
+++ lms/djangoapps/support/static/support/jsx/entitlements/entitlements.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+export class EntitlementSupportPage extends React.Component {
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return	(
+			<div>
+				Base Entitlement Support Page
+			</div>
+		)
+	}
+}
