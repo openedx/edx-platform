a843495f552d1f6e227098982af0044ef1564443
Mohammad Nadeem <muhammad.nadeem@arbisoft.com>
2018-03-01 21:43:50 +0500

    Update site config API for node bb & add site related support link (#344)
    * username replacing firstname in head banner. (#342)
    * Send site ralted support link & oef setting to nodebb (#343)

--- lms/djangoapps/oef/decorators.py
+++ lms/djangoapps/oef/decorators.py
@@ -7,8 +7,7 @@ def can_take_oef(function):
     def wrap(request, *args, **kwargs):
         user_extended_profile = request.user.extended_profile
 
-        if user_extended_profile.organization and Organization.is_non_profit(user_extended_profile) and  \
-                (user_extended_profile.is_organization_admin or oef_eligible_first_learner(user_extended_profile)):
+        if eligible_for_oef(user_extended_profile):
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied
@@ -17,3 +16,10 @@ def can_take_oef(function):
     wrap.__name__ = function.__name__
     return wrap
 
+
+def eligible_for_oef(user_extended_profile):
+    """
+    :return: True if user can take OEF
+    """
+    return user_extended_profile.organization and Organization.is_non_profit(user_extended_profile) and \
+           (user_extended_profile.is_organization_admin or oef_eligible_first_learner(user_extended_profile))
--- lms/djangoapps/philu_api/urls.py
+++ lms/djangoapps/philu_api/urls.py
@@ -3,12 +3,12 @@
 """
 from django.conf.urls import url, patterns
 
-from lms.djangoapps.philu_api.views import UpdateCommunityProfile, get_user_chat, mark_user_chat_read
+from lms.djangoapps.philu_api.views import PlatformSyncService, get_user_chat, mark_user_chat_read
 
 urlpatterns = patterns(
     'philu_api.views',
-    url(r'profile/status/', UpdateCommunityProfile.as_view(), name='get_user_profile_status'),
-    url(r'profile/update/', UpdateCommunityProfile.as_view(), name='update_community_profile_update'),
+    url(r'platform/sync/service/', PlatformSyncService.as_view(), name='get_shared_data'),
+    url(r'profile/update/', PlatformSyncService.as_view(), name='update_community_profile_update'),
     url(r'profile/chats/?$', get_user_chat, name='get_user_chat'),
     url(r'profile/chats/mark/?$', mark_user_chat_read, name='mark_user_chat_read'),
 )
