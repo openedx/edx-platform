34a96fb58d867e8279c8a88b360d7be3c783747c
Farhanah Sheets <fsheets@edx.org>
2018-03-02 18:30:23 -0500

    Remove NewAssets config model & create migration file

--- cms/djangoapps/contentstore/admin.py
+++ cms/djangoapps/contentstore/admin.py
@@ -2,30 +2,11 @@
 Admin site bindings for contentstore
 """
 
-from config_models.admin import ConfigurationModelAdmin, KeyedConfigurationModelAdmin
+from config_models.admin import ConfigurationModelAdmin
 from django.contrib import admin
 
-from contentstore.config.forms import CourseNewAssetsPageAdminForm
-from contentstore.config.models import NewAssetsPageFlag, CourseNewAssetsPageFlag
 from contentstore.models import PushNotificationConfig, VideoUploadConfig
 
-
-class CourseNewAssetsPageAdmin(KeyedConfigurationModelAdmin):
-    """
-    Admin for enabling new asset page on a course-by-course basis.
-    Allows searching by course id.
-    """
-    form = CourseNewAssetsPageAdminForm
-    search_fields = ['course_id']
-    fieldsets = (
-        (None, {
-            'fields': ('course_id', 'enabled'),
-            'description': 'Enter a valid course id. If it is invalid, an error message will display.'
-        }),
-    )
-
-admin.site.register(NewAssetsPageFlag, ConfigurationModelAdmin)
-admin.site.register(CourseNewAssetsPageFlag, CourseNewAssetsPageAdmin)
 
 admin.site.register(VideoUploadConfig, ConfigurationModelAdmin)
 admin.site.register(PushNotificationConfig, ConfigurationModelAdmin)
