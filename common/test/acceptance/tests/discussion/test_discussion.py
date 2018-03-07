2cd32749f997f40eb4c00cbe42c3a06d50bbfe9a
Michael Youngstrom <myoungstrom@edx.org>
2018-03-07 17:44:37 -0500

    Fix mathjax failing test

--- common/test/acceptance/tests/discussion/test_discussion.py
+++ common/test/acceptance/tests/discussion/test_discussion.py
@@ -991,8 +991,7 @@ class DiscussionEditorPreviewTest(UniqueCourseTest):
             'Text line 2 \n'
             '$$e[n]=d_2$$'
         )
-
-        self.assertEqual(self.page.get_new_post_preview_text(), 'Text line 1\nText line 2')
+        self.assertEqual(self.page.get_new_post_preview_text(), 'Text line 1\ne[n]=d1\nText line 2\ne[n]=d2')
 
     def test_mathjax_not_rendered_after_post_cancel(self):
         """
