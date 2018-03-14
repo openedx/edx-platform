cea33b3a6f429bef5f0ea42dae2f107054e8583e
Ari Rizzitano <arizzitano@edx.org>
2018-03-14 11:19:19 -0400

    forgot base in textbooksfactory

--- cms/static/js/pages/textbooks.js
+++ cms/static/js/pages/textbooks.js
@@ -1,7 +1,10 @@
-define(
-    ['js/factories/textbooks', 'common/js/utils/page_factory', 'js/pages/course'],
-    function(TextbooksFactory, invokePageFactory) {
-        'use strict';
-        invokePageFactory('TextbooksFactory', TextbooksFactory);
-    }
+define([
+    'js/factories/textbooks',
+    'common/js/utils/page_factory',
+    'js/factories/base',
+    'js/pages/course'
+], function(TextbooksFactory, invokePageFactory) {
+    'use strict';
+    invokePageFactory('TextbooksFactory', TextbooksFactory);
+}
 );
