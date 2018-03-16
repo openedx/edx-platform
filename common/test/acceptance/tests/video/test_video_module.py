afc2463871670ffb24eb7be0c0e7fc0d2ff5cde3
Michael Youngstrom <myoungstrom@edx.org>
2018-03-16 13:47:24 -0400

    try scrolling to element with difference css selector

--- common/test/acceptance/tests/video/test_video_module.py
+++ common/test/acceptance/tests/video/test_video_module.py
@@ -1138,7 +1138,8 @@ class DragAndDropTest(VideoBaseTest):
         self.video.show_closed_captions()
         self.video.wait_for_closed_captions()
         self.assertTrue(self.video.is_closed_captions_visible)
-        self.video.scroll_to_element('.closed-captions')
+        # Scroll to video with different id
+        self.video.scroll_to_element(self.get_element_selector(CSS_CLASS_NAMES['closed_captions']))
         action = ActionChains(self.browser)
         captions = self.browser.find_element(By.CLASS_NAME, 'closed-captions')
 
