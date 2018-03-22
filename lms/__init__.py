577667b61d10be6f2914ee92fed3d5b712fc9952
Rocio Aramberri <rocio@opencraft.com>
2018-03-22 18:28:40 -0300

    Fix for migrations running slow on Django 1.8

--- lms/__init__.py
+++ lms/__init__.py
@@ -15,6 +15,10 @@ from __future__ import absolute_import
 import kombu.utils
 kombu.utils.entrypoints = lambda namespace: iter([])
 
+# Monkey Patch for Django migrations running slow, remove after upgrading to 1.9+
+# taken from: https://github.com/cfpb/cfgov-refresh/commit/7616c6bb3ec310e72b1c9538d9176ba61a73ebd3#diff-dffeeb550eca65d51cda43001ff38d4d
+import lms.monkey_patch
+
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import APP as CELERY_APP
