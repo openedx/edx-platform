b26d7913f5476bcb39b7871fd7c123f17eacc632
bmedx <bmesick@edx.org>
2018-03-26 15:29:30 -0400

    Add some common functionality for retiring users
    - Methods for getting hashed retired users and usernames
    - A DRF permission for retiring users

--- openedx/core/djangoapps/user_api/accounts/permissions.py
+++ openedx/core/djangoapps/user_api/accounts/permissions.py
@@ -3,6 +3,7 @@ Permissions classes for User accounts API views.
 """
 from __future__ import unicode_literals
 
+from django.conf import settings
 from rest_framework import permissions
 
 
@@ -13,3 +14,16 @@ class CanDeactivateUser(permissions.BasePermission):
     """
     def has_permission(self, request, view):
         return request.user.has_perm('student.can_deactivate_users')
+
+
+class CanRetireUser(permissions.BasePermission):
+    """
+    Grants access to the various retirement API endpoints if the requesting user is
+    a superuser, the RETIREMENT_SERVICE_USERNAME, or has the explicit permission to
+    retire a User account.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.user.username == settings.RETIREMENT_SERVICE_WORKER_USERNAME or
+            request.user.is_superuser
+        )
