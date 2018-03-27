022001e53de2072bb72486aff39fa253a9bba75e
Eric Fischer <efischer@edx.org>
2018-03-27 12:00:33 -0400

    EDUCATOR-2584

--- common/djangoapps/pipeline_mako/helpers/studiofrontend.py
+++ common/djangoapps/pipeline_mako/helpers/studiofrontend.py
@@ -2,8 +2,12 @@
 Contains code that gets run inside our mako template
 Debugging python-in-mako is terrible, so we've moved the actual code out to its own file
 """
+import logging
+
 from django.conf import settings
 from django.utils.translation import to_locale
+
+log = logging.getLogger(__name__)
 
 
 def load_sfe_i18n_messages(language):
@@ -12,13 +16,16 @@ def load_sfe_i18n_messages(language):
     """
     messages = "{}"
 
-    if language != 'en':
-        # because en is the default, studio-frontend will have it loaded by default
-        messages_path = "{base}/studio-frontend/dist/i18n/messages/{locale}.json".format(
-            base=settings.STATIC_ROOT_BASE,
-            locale=to_locale(language)
-        )
-        with open(messages_path) as inputfile:
-            messages = inputfile.read()
+    try:
+        if language != 'en':
+            # because en is the default, studio-frontend will have it loaded by default
+            messages_path = "{base}/studio-frontend/dist/i18n/messages/{locale}.json".format(
+                base=settings.STATIC_ROOT_BASE,
+                locale=to_locale(language)
+            )
+            with open(messages_path) as inputfile:
+                messages = inputfile.read()
+    except:  # pylint: disable=bare-except
+        log.error("Error loading studiofrontend language files", exc_info=True)
 
     return messages
