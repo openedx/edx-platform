c231d4e3d475d865a88833c7253ae8d912f77b56
sandroroux <aroux@edx.org>
2018-04-02 15:20:19 -0400

    Removes the new course outline waffle flag.

--- openedx/features/course_experience/views/course_outline.py
+++ openedx/features/course_experience/views/course_outline.py
@@ -56,23 +56,14 @@ class CourseOutlineFragmentView(EdxFragmentView):
             if not resume_block:
                 self.mark_first_unit_to_resume(course_block_tree)
 
-            xblock_display_names = self.create_xblock_id_and_name_dict(course_block_tree)
-            gated_content = self.get_content_milestones(request, course_key)
+        xblock_display_names = self.create_xblock_id_and_name_dict(course_block_tree)
+        gated_content = self.get_content_milestones(request, course_key)
 
-            context['gated_content'] = gated_content
-            context['xblock_display_names'] = xblock_display_names
+        context['gated_content'] = gated_content
+        context['xblock_display_names'] = xblock_display_names
 
-            # TODO: EDUCATOR-2283 Rename this file to course-outline-fragment.html
-            html = render_to_string('course_experience/course-outline-fragment-new.html', context)
-            return Fragment(html)
-        else:
-            content_milestones = self.get_content_milestones_old(request, course_key)
-
-            context['gated_content'] = content_milestones
-
-            # TODO: EDUCATOR-2283 Remove this file
-            html = render_to_string('course_experience/course-outline-fragment-old.html', context)
-            return Fragment(html)
+        html = render_to_string('course_experience/course-outline-fragment.html', context)
+        return Fragment(html)
 
     def create_xblock_id_and_name_dict(self, course_block_tree, xblock_display_names=None):
         """
@@ -113,28 +104,6 @@ class CourseOutlineFragmentView(EdxFragmentView):
         }
 
         return gated_content
-
-    # TODO: EDUCATOR-2283 Remove this function when the visual progress waffle flag is turned on in production
-    def get_content_milestones_old(self, request, course_key):
-        """
-        Returns dict of subsections with prerequisites and whether the prerequisite has been completed or not
-        """
-
-        all_course_prereqs = get_course_content_milestones(course_key)
-
-        content_ids_of_unfulfilled_prereqs = {
-            milestone['content_id']
-            for milestone in get_course_content_milestones(course_key, user_id=request.user.id)
-        }
-
-        course_content_milestones = {
-            milestone['content_id']: {
-                'completed_prereqs': milestone['content_id'] not in content_ids_of_unfulfilled_prereqs
-            }
-            for milestone in all_course_prereqs
-        }
-
-        return course_content_milestones
 
     def user_enrolled_after_completion_collection(self, user, course_key):
         """
