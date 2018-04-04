20e7a15ac696f828cb33cf6b40975a5a1aae8c72
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2018-04-04 12:18:10 +0500

    Fix enabling debug allows api access without authentication.
    LEARNER-1220

--- openedx/core/lib/api/permissions.py
+++ openedx/core/lib/api/permissions.py
@@ -21,17 +21,13 @@ class ApiKeyHeaderPermission(permissions.BasePermission):
         """
         Check for permissions by matching the configured API key and header
 
-        If settings.DEBUG is True and settings.EDX_API_KEY is not set or None,
-        then allow the request. Otherwise, allow the request if and only if
-        settings.EDX_API_KEY is set and the X-Edx-Api-Key HTTP header is
-        present in the request and matches the setting.
+        Allow the request if and only if settings.EDX_API_KEY is set and
+        the X-Edx-Api-Key HTTP header is present in the request and
+        matches the setting.
         """
         api_key = getattr(settings, "EDX_API_KEY", None)
 
-        if settings.DEBUG and api_key is None:
-            return True
-
-        elif api_key is not None and request.META.get("HTTP_X_EDX_API_KEY") == api_key:
+        if api_key is not None and request.META.get("HTTP_X_EDX_API_KEY") == api_key:
             audit_log("ApiKeyHeaderPermission used",
                       path=request.path,
                       ip=request.META.get("REMOTE_ADDR"))
