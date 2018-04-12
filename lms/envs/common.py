8a7be9cdae56b8cf0aee4464d98e7cf8fbb6acda
Bill Filler <bfiller@edx.org>
2018-04-11 22:19:35 -0400

    Add journal support
    Enable the journal app in LMS. Add support to display Journals on index
    and courseware pages.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -404,6 +404,8 @@ FEATURES = {
 
     # Whether to display the account deletion section the account settings page
     'ENABLE_ACCOUNT_DELETION': True,
+    # Set to enable Journal integration
+    'ENABLE_JOURNAL_INTEGRATION': False,
 }
 
 # Settings for the course reviews tool template and identification key, set either to None to disable course reviews
@@ -2303,6 +2305,7 @@ INSTALLED_APPS = [
     'openedx.features.learner_profile',
     'openedx.features.learner_analytics',
     'openedx.features.portfolio_project',
+    'openedx.features.journals.apps.JournalsConfig',
 
     'experiments',
 
@@ -2907,6 +2910,9 @@ OPTIONAL_APPS = [
 
     # Required by the Enterprise App
     ('django_object_actions', None),  # https://github.com/crccheck/django-object-actions
+
+    # Journals
+    ('journals', None)
 ]
 
 for app_name, insert_before in OPTIONAL_APPS:
@@ -3461,6 +3467,9 @@ FERNET_KEYS = [
 # Maximum number of rows to fetch in XBlockUserStateClient calls. Adjust for performance
 USER_STATE_BATCH_SIZE = 5000
 
+############## Settings for Journals ######################
+JOURNALS_API_URL = 'http://journals.app:18606/api/v1/'
+JOURNALS_ROOT_URL = 'http://localhost:18606'
 
 ############## Plugin Django Apps #########################
 
---
+++ openedx/features/journals/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class JournalsConfig(AppConfig):
+    name = 'openedx.features.journals'
