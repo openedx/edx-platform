40c9a8e133dd0c16e2693e81e0b4f9b34e993cdf
Ari Rizzitano <arizzitano@edx.org>
2018-05-01 22:53:58 -0400

    install from git url

--- lms/static/js/dashboard-v2/index.jsx
+++ lms/static/js/dashboard-v2/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import WrappedDashboardPage from '@arizzitano/graphql-dashboard/src/containers/WrappedDashboardPage';
 
-export function DashboardV2({apiUri}) {
-  return <WrappedDashboardPage apiUri={apiUri} />;
+export function DashboardV2({apiUri, username}) {
+  return (
+    <WrappedDashboardPage
+      apiUri={apiUri}
+      username={username}
+    />
+  );
 };
