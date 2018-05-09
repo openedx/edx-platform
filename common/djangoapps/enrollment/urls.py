01ebe1e26e26ae2dc5b3d64ef7d993ec985752fb
bmedx <bmesick@edx.org>
2018-05-09 10:40:00 -0400

    Fix issues with retirement state population and unenrollment
    - Prevent RetirementState state_execution_order collisions when updating
    - Add slash to end of unenroll API endpoint to match other retirement endpoints

--- common/djangoapps/enrollment/urls.py
+++ common/djangoapps/enrollment/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     url(r'^enrollment$', EnrollmentListView.as_view(), name='courseenrollments'),
     url(r'^course/{course_key}$'.format(course_key=settings.COURSE_ID_PATTERN),
         EnrollmentCourseDetailView.as_view(), name='courseenrollmentdetails'),
-    url(r'^unenroll$', UnenrollmentView.as_view(), name='unenrollment'),
+    url(r'^unenroll/$', UnenrollmentView.as_view(), name='unenrollment'),
 ]
