b2c9b50de1b8b594b386fc080d865294e026fc85
Diana Huang <dkh@edx.org>
2018-05-15 11:25:00 -0400

    Add a warning message when there are social auth links connected.

--- lms/static/js/student_account/components/StudentAccountDeletion.jsx
+++ lms/static/js/student_account/components/StudentAccountDeletion.jsx
@@ -1,7 +1,7 @@
 /* globals gettext */
 /* eslint-disable react/no-danger, import/prefer-default-export */
 import React from 'react';
-import { Button } from '@edx/paragon/static';
+import { Button, Icon, StatusAlert } from '@edx/paragon/static';
 import StringUtils from 'edx-ui-toolkit/js/utils/string-utils';
 import StudentAccountDeletionModal from './StudentAccountDeletionModal';
 
@@ -10,7 +10,10 @@ export class StudentAccountDeletion extends React.Component {
     super(props);
     this.closeDeletionModal = this.closeDeletionModal.bind(this);
     this.loadDeletionModal = this.loadDeletionModal.bind(this);
-    this.state = { deletionModalOpen: false };
+    this.state = {
+      deletionModalOpen: false,
+      socialAuthConnected: props.socialAccountLinks.providers.reduce((acc, value) => acc || value.connected, false)
+    };
   }
 
   closeDeletionModal() {
@@ -23,13 +26,21 @@ export class StudentAccountDeletion extends React.Component {
   }
 
   render() {
-    const { deletionModalOpen } = this.state;
+    const { deletionModalOpen, socialAuthConnected } = this.state
     const loseAccessText = StringUtils.interpolate(
       gettext('You may also lose access to verified certificates and other program credentials like MicroMasters certificates. If you want to make a copy of these for your records before proceeding with deletion, follow the instructions for {htmlStart}printing or downloading a certificate{htmlEnd}.'),
       {
         htmlStart: '<a href="http://edx.readthedocs.io/projects/edx-guide-for-students/en/latest/SFD_certificates.html#printing-a-certificate" target="_blank">',
         htmlEnd: '</a>',
       },
+    );
+
+    const socialAuthError = StringUtils.interpolate(
+      gettext('Before proceeding, please {htmlStart}unlink all social media accounts{htmlEnd}.'),
+      {
+        htmlStart: '<a href="https://support.edx.org/hc/en-us/articles/207206067" target="_blank">',
+        htmlEnd: '</a>',
+      }
     );
 
     return (
@@ -45,10 +56,26 @@ export class StudentAccountDeletion extends React.Component {
         <Button
           id="delete-account-btn"
           className={['btn-outline-primary']}
+          disabled={socialAuthConnected}
           label={gettext('Delete My Account')}
           inputRef={(input) => { this.modalTrigger = input; }}
           onClick={this.loadDeletionModal}
         />
+        {socialAuthConnected && <StatusAlert
+                  dialog={(
+                    <div className="modal-alert">
+                      <div className="icon-wrapper">
+                        <Icon id="delete-confirmation-body-error-icon" className={['fa', 'fa-exclamation-circle']} />
+                      </div>
+                      <div className="alert-content">
+                        <p dangerouslySetInnerHTML={{ __html: socialAuthError }}/>
+                      </div>
+                    </div>
+                  )}
+                  alertType="danger"
+                  dismissible={false}
+                  open
+              />}
         {deletionModalOpen && <StudentAccountDeletionModal onClose={this.closeDeletionModal} />}
       </div>
     );
