f2ea2ca360a87b11b0b54f88deee56bb4eb5286c
Zia Fazal <zia.fazal@arbisoft.com>
2018-05-21 13:44:22 +0500

    Added a base MessageType for ace
    added unit tests
    
    updated edx-ace version
    
    fixed quality violations
    
    Fixed quality violations
    
    Changed ACEMessageType to BaseMessageType

--- common/djangoapps/student/message_types.py
+++ common/djangoapps/student/message_types.py
@@ -1,17 +1,12 @@
 """
 ACE message types for the student module.
 """
-from django.conf import settings
 
-from edx_ace.message import MessageType
-from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+from openedx.core.djangoapps.ace_common.message import BaseMessageType
 
 
-class PasswordReset(MessageType):
+class PasswordReset(BaseMessageType):
     def __init__(self, *args, **kwargs):
         super(PasswordReset, self).__init__(*args, **kwargs)
 
         self.options['transactional'] = True
-        self.options['from_address'] = configuration_helpers.get_value(
-            'email_from_address', settings.DEFAULT_FROM_EMAIL
-        )
--- openedx/core/djangoapps/schedules/message_types.py
+++ openedx/core/djangoapps/schedules/message_types.py
@@ -1,11 +1,10 @@
 import logging
 
-from edx_ace.message import MessageType
-
+from openedx.core.djangoapps.ace_common.message import BaseMessageType
 from openedx.core.djangoapps.schedules.config import DEBUG_MESSAGE_WAFFLE_FLAG
 
 
-class ScheduleMessageType(MessageType):
+class ScheduleMessageType(BaseMessageType):
     def __init__(self, *args, **kwargs):
         super(ScheduleMessageType, self).__init__(*args, **kwargs)
         self.log_level = logging.DEBUG if DEBUG_MESSAGE_WAFFLE_FLAG.is_enabled() else None
