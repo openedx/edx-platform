1e00c706900f134ade978cf79e04cd348cf71e41
Muhammad Nadeem <muhammad.nadeem@arbisoft.com>
2018-05-31 15:54:47 +0500

    Remove experimental code

--- common/djangoapps/mailchimp_pipeline/helpers.py
+++ common/djangoapps/mailchimp_pipeline/helpers.py
@@ -9,14 +9,19 @@ def is_active_enrollment(course_end_date):
 
 
 def get_org_data_for_mandrill(organization):
-    focus_areas = FocusArea.get_map()
-    org_sectors = OrgSector.get_map()
-    org_label = organization.label if organization else ""
-
+    org_label = ""
     org_type = ""
-    if organization.org_type:
-        org_type = org_sectors.get(organization.org_type, "")
+    focus_area = ""
 
-    focus_area = str(focus_areas.get(organization.focus_area, "")) if organization else ""
+    if organization:
+        focus_areas = FocusArea.get_map()
+        org_sectors = OrgSector.get_map()
+        org_label = organization.label if organization else ""
+
+        org_type = ""
+        if organization.org_type:
+            org_type = org_sectors.get(organization.org_type, "")
+
+        focus_area = str(focus_areas.get(organization.focus_area, "")) if organization else ""
 
     return org_label, org_type, focus_area
