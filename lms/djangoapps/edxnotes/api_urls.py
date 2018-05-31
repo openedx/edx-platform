d33cf1d231e2a0e96479724c22a8193846da4d02
Troy Sankey <sankeytms@gmail.com>
2018-05-31 17:21:58 -0400

    Notes retirement endpoint should require trailing slash
    This is because we already require trailing slashes on most of the rest
    of the retirement endpoints, which are called via slumber which appends
    the trailing slash by default.

--- lms/djangoapps/edxnotes/api_urls.py
+++ lms/djangoapps/edxnotes/api_urls.py
@@ -6,5 +6,5 @@ from django.conf.urls import url
 from edxnotes import views
 
 urlpatterns = [
-    url(r"^retire_user$", views.RetireUserView.as_view(), name="edxnotes_retire_user"),
+    url(r"^retire_user/$", views.RetireUserView.as_view(), name="edxnotes_retire_user"),
 ]
