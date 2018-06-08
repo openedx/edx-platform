5599e5e814f441e2dbd0359a85c55ee5d675f4a5
Imran Bashir <imran.bashir@arbisoft.com>
2018-06-08 16:42:46 +0500

    log errors

--- lms/djangoapps/third_party_surveys/tasks.py
+++ lms/djangoapps/third_party_surveys/tasks.py
@@ -21,33 +21,39 @@ def get_third_party_surveys():
         filters = [('datesubmitted', '>', last_survey.request_date)]
     except ThirdPartySurvey.DoesNotExist:
         filters = []
-
+    filters += [('status', '=', 'Completed')]
     survey_responses = SurveyGizmoClient().get_filtered_survey_responses(survey_filters=filters)
     save_responses(survey_responses)
 
 
 def save_responses(survey_responses):
     surveys_to_create = []
+
+    log.info("survey logs")
     for response in survey_responses:
-        if not response.get('[url("edx_uid")]') \
+        if response.get('[url("edx_uid")]') \
                 or response.get('[url("edx_uid")]') == 'undefined' \
                 or response.get('[url("status")]') == 'Deleted':
             continue
 
+        log.info(response.get('[url("app")]', '') + ", " + response.get('[url("edx_uid")]'))
         date = datetime.strptime(response['datesubmitted'], "%Y-%m-%d %H:%M:%S")
         try:
             surveys_to_create.append(ThirdPartySurvey(
                 response=response,
-                user_id=int(response['[url("edx_uid")]']),
+                user_id=response.get('[url("edx_uid")]'),
                 request_date=date,
                 survey_type=response.get('[url("app")]', '')
                 )
             )
         except (IntegrityError, ValueError) as exc:
+            log.info(str(exc))
             log.error(exc)
 
     # Pass the exception if the user=edx_uid doesn't exist in the Database
     try:
+        log.info("bulk insert")
+        log.info(surveys_to_create)
         ThirdPartySurvey.objects.bulk_create(surveys_to_create)
     except (IntegrityError, ValueError) as exc:
         log.error(exc)
