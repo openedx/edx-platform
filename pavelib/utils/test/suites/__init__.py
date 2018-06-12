b7d16b90f8d3d9f3a00855e3241f61a697c3cbbe
Eric Fischer <efischer@edx.org>
2018-06-12 16:37:39 -0400

    Add jest snapshot testing

--- pavelib/utils/test/suites/__init__.py
+++ pavelib/utils/test/suites/__init__.py
@@ -4,6 +4,6 @@ TestSuite class and subclasses
 from .suite import TestSuite
 from .pytest_suite import PytestSuite, SystemTestSuite, LibTestSuite
 from .python_suite import PythonTestSuite
-from .js_suite import JsTestSuite
+from .js_suite import JsTestSuite, JestSnapshotTestSuite
 from .acceptance_suite import AcceptanceTestSuite
 from .bokchoy_suite import BokChoyTestSuite, Pa11yCrawler
