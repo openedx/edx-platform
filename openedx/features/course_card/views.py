a4620e15aa8e487fbc7af1b4dbfc48c6fe561620
Imran Bashir <imran.bashir@arbisoft.com>
2018-07-02 18:37:58 +0500

    refactor variable name

--- openedx/features/course_card/views.py
+++ openedx/features/course_card/views.py
@@ -1,18 +1,12 @@
 from datetime import datetime
 
 import pytz
-from common.djangoapps.student.views import get_course_related_keys
-from django.core.urlresolvers import reverse
-from lms.djangoapps.courseware.access import has_access
-from lms.djangoapps.courseware.courses import get_course_by_id
-from lms.djangoapps.courseware.views.views import get_last_accessed_courseware
-from opaque_keys.edx.locations import SlashSeparatedCourseKey
+from openedx.core.djangoapps.timed_notification.core import get_course_first_chapter_link
 from student.models import CourseEnrollment
 from edxmako.shortcuts import render_to_response
 from openedx.features.course_card.models import CourseCard
 from django.views.decorators.csrf import csrf_exempt
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from xmodule.modulestore.django import modulestore
 
 
 @csrf_exempt
@@ -22,18 +16,16 @@ def get_course_cards(request):
     :return: list of active cards
     """
 
-    user = request.user
-
     course_card_ids = [cc.course_id for cc in CourseCard.objects.filter(is_enabled=True)]
     courses_list = CourseOverview.objects.filter(id__in=course_card_ids)
-    datetime_now = datetime.now()
+    current_time = datetime.now()
 
     ids_filter = ",".join(["'" + cl.id.__str__() + "'" for cl in courses_list])
 
     qry = "select *, min(co.start) as start_date from course_overviews_courseoverview co " \
           "inner join course_action_state_coursererunstate crs on co.id = crs.course_key " \
           "where crs.source_course_key in (" + ids_filter + ") " \
-          "and co.end >= " + datetime_now.strftime('%Y-%m-%d') + " group by co.id"
+          "and co.end >= " + current_time.strftime('%Y-%m-%d') + " group by co.id"
 
     course_rerun_views = CourseOverview.objects.raw(qry)
 
@@ -60,7 +52,7 @@ def get_course_cards(request):
         course.start_date = None if not start_date else start_date.strftime('%b %-d, %Y')
         is_enrolled = CourseEnrollment.is_enrolled(request.user, current_course.id)
         course.is_enrolled = is_enrolled
-        course_target = get_course_link(current_course, request)
+        course_target = get_course_first_chapter_link(current_course, request)
         course.course_target = course_target
     return render_to_response(
         "course_card/courses.html",
@@ -69,34 +61,3 @@ def get_course_cards(request):
         }
     )
 
-
-def get_course_link(course, request):
-    """
-    create course link for students for the rerun he/she has enrolled for
-    :param course:
-    :param request:
-    :return:
-    """
-    course_target = ''
-    course_key = SlashSeparatedCourseKey.from_deprecated_string(
-        course.id.to_deprecated_string())
-    with modulestore().bulk_operations(course_key):
-        if has_access(request.user, 'load', course):
-            access_link = get_last_accessed_courseware(
-                get_course_by_id(course_key, 0),
-                request,
-                request.user
-            )
-
-            first_chapter_url, first_section = get_course_related_keys(
-                request, get_course_by_id(course_key, 0))
-            first_target = reverse('courseware_section', args=[
-                course.id.to_deprecated_string(),
-                first_chapter_url,
-                first_section
-            ])
-
-            course_target = access_link if access_link != None else first_target
-        else:
-            course_target = '/courses/' + course.id.to_deprecated_string()
-    return course_target
