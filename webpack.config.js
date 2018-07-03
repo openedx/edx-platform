2014f07bb4879703bb653d16741e77afdf1b294f
Naeem Ilyas <naeem-ilyas@live.com>
2018-07-03 19:33:03 +0000

    Revert "Implement completion-on-viewing, based on edx/master"
    This reverts commit 8fa9b49e71e93fb82b4e26d3ab85825202dd4354.

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,7 @@ var wpconfig = {
         CourseOutline: './openedx/features/course_experience/static/course_experience/js/CourseOutline.js',
         CourseSock: './openedx/features/course_experience/static/course_experience/js/CourseSock.js',
         WelcomeMessage: './openedx/features/course_experience/static/course_experience/js/WelcomeMessage.js',
-        Import: './cms/static/js/features/import/factories/import.js',
-        CompletionOnViewService: './lms/static/completion/js/CompletionOnViewService.js'
+        Import: './cms/static/js/features/import/factories/import.js'
     },
 
     output: {
