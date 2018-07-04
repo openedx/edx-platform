c198bd70ec0b4ea9fc3dd369a193995439bba7bf
Naeem Ilyas <naeem-ilyas@live.com>
2018-07-04 13:51:55 +0500

    Revert "Backport completion-by-viewing logic"

--- common/lib/xmodule/xmodule/assets/vertical/public/js/vertical_student_view.js
+++ common/lib/xmodule/xmodule/assets/vertical/public/js/vertical_student_view.js
@@ -1,15 +1,9 @@
 /* JavaScript for Vertical Student View. */
-
-/* global markBlocksCompletedOnViewIfNeeded:false */
-
 window.VerticalStudentView = function(runtime, element) {
     'use strict';
-
-    markBlocksCompletedOnViewIfNeeded(runtime, element);
-
     if (typeof RequireJS === 'undefined') {
         // eslint-disable-next-line no-console
-        console.warn('Cannot initialize bookmarks for VerticalStudentView. RequireJS is not defined.');
+        console.log('Cannot initialize VerticalStudentView. RequireJS is not defined.');
         return;
     }
     RequireJS.require(['js/bookmarks/views/bookmark_button'], function(BookmarkButton) {
