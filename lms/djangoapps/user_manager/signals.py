9eca27a9a6a90449297f9baaf97c2ac1d268443f
Kshitij Sobti <kshitij@sobti.in>
2018-07-21 22:56:37 +0530

    Add signal to upgrade manager

--- lms/djangoapps/user_manager/signals.py
+++ lms/djangoapps/user_manager/signals.py
@@ -1,3 +1,24 @@
 """
 Signals for User Manager app
 """
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import UserManagerRole
+
+
+@receiver(post_save, sender=User)
+def upgrade_manager_role_entry(sender, **kwargs):  # pylint: disable=unused-argument
+    """
+    """
+    created = kwargs.get('created')
+    user = kwargs.get('instance')
+
+    if created and user:
+        UserManagerRole.objects.filter(
+            unregistered_manager_email=user.email
+        ).update(
+            unregistered_manager_email=None,
+            manager_user=user,
+        )
+
