d8226ee2ee37884db7bd222681e9eb20e61a1d88
Anders Pearson <anders@thraxil.org>
2018-07-23 15:07:27 +0100

    backport the chemical equations patch to ficus

--- common/lib/chem/chem/chemcalc.py
+++ common/lib/chem/chem/chemcalc.py
@@ -32,6 +32,8 @@ tokenizer = OneOrMore(tokens) + StringEnd()
 # other applications.
 # See LEARNER-5853 for more details.
 Text = markupsafe.escape                        # pylint: disable=invalid-name
+
+
 def HTML(html):                                 # pylint: disable=invalid-name
     return markupsafe.Markup(html)
 
