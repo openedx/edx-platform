76dab7d2b5912a9a711a6f274aa05419de1df460
Imran Bashir <imran.bashir@arbisoft.com>
2018-07-24 18:56:46 +0500

    fix rerun of rerun

--- cms/djangoapps/contentstore/views/course.py
+++ cms/djangoapps/contentstore/views/course.py
@@ -21,7 +21,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locations import Location
-from openedx.features.course_card.helpers import get_related_card
+from openedx.features.course_card.helpers import get_related_card_id
 
 from .component import (
     ADVANCED_COMPONENT_TYPES,
@@ -853,7 +853,7 @@ def _rerun_course(request, org, number, run, fields):
     # so the user can see the updated status for that course
     add_instructor(destination_course_key, request.user, request.user)
 
-    parent_course_key = get_parent_course_key(source_course_key)
+    parent_course_key = get_related_card_id(source_course_key)
 
     # Mark the action as initiated
     CourseRerunState.objects.initiated(parent_course_key, destination_course_key, request.user, fields['display_name'])
@@ -870,15 +870,6 @@ def _rerun_course(request, org, number, run, fields):
         'url': reverse_url('course_handler'),
         'destination_course_key': unicode(destination_course_key)
     })
-
-
-def get_parent_course_key(source_course_key):
-
-    course_rerun = CourseRerunState.objects.filter(course_key=source_course_key).first()
-    if course_rerun:
-        return course_rerun.source_course_key
-
-    return source_course_key
 
 
 # pylint: disable=unused-argument
