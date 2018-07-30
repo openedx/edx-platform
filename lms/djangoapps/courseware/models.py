054ed8ba6b92d54d160613df0220bfedeb8d84a0
Joseph Mulloy <jmulloy@edx.org>
2018-07-30 12:17:34 -0400

    Add nullable column

--- lms/djangoapps/courseware/models.py
+++ lms/djangoapps/courseware/models.py
@@ -116,6 +116,8 @@ class StudentModule(models.Model):
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     modified = models.DateTimeField(auto_now=True, db_index=True)
 
+    test_field_nullable = models.IntegerField(null=True);
+
     @classmethod
     def all_submitted_problems_read_only(cls, course_id):
         """
