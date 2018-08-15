332b71e00aee236ee501bfc06a4dbe2cdcb2043f
Muhammad Nadeem <muhammad.nadeem@arbisoft.com>
2018-08-15 20:49:01 +0500

    Update hash for ora2 modifications

--- lms/djangoapps/philu_api/helpers.py
+++ lms/djangoapps/philu_api/helpers.py
@@ -1,5 +1,6 @@
 import jwt
 from custom_settings.models import CustomSettings
+from opaque_keys.edx.keys import CourseKey
 
 
 def get_encoded_token(username, email, id):
@@ -8,4 +9,7 @@ def get_encoded_token(username, email, id):
 
 def get_course_custom_settings(course_key):
     """ Return course custom settings object """
+    if isinstance(course_key, str) or isinstance(course_key, unicode):
+        course_key = CourseKey.from_string(course_key)
+
     return CustomSettings.objects.filter(id=course_key).first()
