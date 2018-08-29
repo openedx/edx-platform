52d45affce44469663dde91625ba522355d63c63
Douglas Hall <dhall@edx.org>
2018-08-29 13:49:52 -0400

    Set JWT cookie auth related cookies on login.

--- openedx/core/lib/token_utils.py
+++ openedx/core/lib/token_utils.py
@@ -1,5 +1,6 @@
 """Utilities for working with ID tokens."""
 import json
+import logging
 from time import time
 
 from django.conf import settings
@@ -9,6 +10,9 @@ from jwkest.jws import JWS
 
 from edx_django_utils.monitoring import set_custom_metric
 from student.models import UserProfile, anonymous_id_for_user
+
+
+log = logging.getLogger(__name__)
 
 
 class JwtBuilder(object):
@@ -109,6 +113,9 @@ class JwtBuilder(object):
         """Encode the provided payload."""
         set_custom_metric('jwt_asymmetric', self.asymmetric)
         keys = jwk.KEYS()
+
+        monitoring_utils.set_custom_metric('jwt_asymmetric', self.asymmetric)
+        log.info("Using Asymmetric JWT: %s", self.asymmetric)
 
         if self.asymmetric:
             serialized_keypair = json.loads(self.jwt_auth['JWT_PRIVATE_SIGNING_JWK'])
