d267110b70bd951546bb4cf70b3451b6667f1e38
Imran Bashir <imran.bashir@arbisoft.com>
2018-08-30 20:56:08 +0500

    fix data for possible choices for current poll's xblock selected choices

---
+++ lms/djangoapps/philu_overrides/courseware/tasks.py
@@ -0,0 +1,35 @@
+import json
+import logging
+
+from courseware.models import StudentModule
+
+log = logging.getLogger('edx.celery.task')
+
+
+def task_correct_polls_data():
+    """
+    This task method converts possible choices data from list to string
+    :return:
+    """
+    log.info('Getting student modules')
+
+    student_modules = StudentModule.objects.filter(state__icontains='possible_choices')
+    for module in student_modules:
+        user_state = module.state
+        try:
+            json_state = json.loads(user_state)
+            json_possible_choices = json_state["possible_choices"]
+
+            if type(json_possible_choices) is list:
+
+                possible_choices = json.dumps(json_possible_choices)
+                json_state.update({"possible_choices": possible_choices})
+
+                module.state = json.dumps(json_state)
+                module.save()
+
+                log.info('Module changed with id ' + str(module.id))
+        except Exception as ex:
+            log.error('Code failed for ' + str(module.id) + ' and error is ' + str(ex.message))
+
+
