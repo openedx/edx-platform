34b2ff6a871292580b3cd98b28a154fe4d3d9b69
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2018-09-06 14:56:16 +0500

    Improve: modify tasks to use status codes instead of ConnectionError

--- common/djangoapps/nodebb/tasks.py
+++ common/djangoapps/nodebb/tasks.py
@@ -4,7 +4,6 @@ Tasks to synchronize users with NodeBB
 from celery.utils.log import get_task_logger
 
 from django.conf import settings
-from requests.exceptions import ConnectionError
 from celery.task import task
 
 from common.lib.nodebb_client.client import NodeBBClient
@@ -17,27 +16,28 @@ RETRY_DELAY = settings.NODEBB_RETRY_DELAY  # seconds
 # settings.CELERY_ALWAYS_EAGER = False
 # RETRY_DELAY = 20
 
-def log_response(task_name, status_code, response, username):
+
+def handle_response(caller, task_name, status_code, response, username):
     """
     Logs the response of the specific NodeBB API call
     """
-    if status_code == 200:
-        LOGGER.debug('Success: {} task for user: {}'.format(task_name, username))
-    else:
-        LOGGER.error('Failure: {} task for user: {}, status_code: {}, response: {}'
-                     .format(task_name, username, status_code, response))
+    if status_code >= 500:
+        print('Retrying: {} task for user: {}'.format(task_name, username))
+        caller.retry()
+    elif status_code >= 400:
+        print('Failure: {} task for user: {}, status_code: {}, response: {}'
+              .format(task_name, username, status_code, response))
+    elif status_code >= 200 and status_code < 300:
+        print('Success: {} task for user: {}'.format(task_name, username))
+
 
 @task(default_retry_delay=RETRY_DELAY, max_retries=None)
 def task_create_user_on_nodebb(username, user_data):
     """
     Celery task to create user on NodeBB
     """
-    try:
-        status_code, response = NodeBBClient().users.create(username=username, user_data=user_data)
-        log_response('User creation', status_code, response, username)
-    except ConnectionError as exc:
-        LOGGER.debug('Retrying: User creation task for user: {}'.format(username))
-        task_create_user_on_nodebb.retry(exc=exc)
+    status_code, response = NodeBBClient().users.create(username=username, user_data=user_data)
+    handle_response(task_create_user_on_nodebb, 'User creation', status_code, response, username)
 
 
 @task(default_retry_delay=RETRY_DELAY, max_retries=None)
@@ -45,12 +45,8 @@ def task_update_user_profile_on_nodebb(username, profile_data):
     """
     Celery task to update user profile info on NodeBB
     """
-    try:
-        status_code, response = NodeBBClient().users.update_profile(username=username, profile_data=profile_data)
-        log_response('Update user profile', status_code, response, username)
-    except ConnectionError as exc:
-        LOGGER.debug('Retrying: Update user profile task for user: {}'.format(username))
-        task_update_user_profile_on_nodebb.retry(exc=exc)
+    status_code, response = NodeBBClient().users.update_profile(username=username, profile_data=profile_data)
+    handle_response(task_update_user_profile_on_nodebb, 'Update user profile', status_code, response, username)
 
 
 @task(default_retry_delay=RETRY_DELAY, max_retries=None)
@@ -58,12 +54,8 @@ def task_delete_user_on_nodebb(username):
     """
     Celery task to delete user on NodeBB
     """
-    try:
-        status_code, response = NodeBBClient().users.delete_user(username)
-        log_response('Delete user', status_code, response, username)
-    except ConnectionError as exc:
-        LOGGER.debug('Retrying: Delete user task for user: {}'.format(username))
-        task_delete_user_on_nodebb.retry(exc=exc)
+    status_code, response = NodeBBClient().users.delete_user(username)
+    handle_response(task_delete_user_on_nodebb, 'Delete user', status_code, response, username)
 
 
 @task(default_retry_delay=RETRY_DELAY, max_retries=None)
@@ -71,12 +63,8 @@ def task_activate_user_on_nodebb(username, active):
     """
     Celery task to activate user on NodeBB
     """
-    try:
-        status_code, response = NodeBBClient().users.activate(username=username, active=active)
-        log_response('Activate user', status_code, response, username)
-    except ConnectionError as exc:
-        LOGGER.debug('Retrying: Activate user task for user: {}'.format(username))
-        task_activate_user_on_nodebb.retry(exc=exc)
+    status_code, response = NodeBBClient().users.activate(username=username, active=active)
+    handle_response(task_activate_user_on_nodebb, 'Activate user', status_code, response, username)
 
 
 @task(default_retry_delay=RETRY_DELAY, max_retries=None)
@@ -84,12 +72,8 @@ def task_join_group_on_nodebb(group_name, username):
     """
     Celery task to join user to a community on NodeBB
     """
-    try:
-        status_code, response = NodeBBClient().users.join(group_name=group_name, username=username)
-        log_response('Join user in {}'.format(group_name), status_code, response, username)
-    except ConnectionError as exc:
-        LOGGER.debug('Retrying: Join user task for user: {} and community/group: {}'.format(username, group_name))
-        task_join_group_on_nodebb.retry(exc=exc)
+    status_code, response = NodeBBClient().users.join(group_name=group_name, username=username)
+    handle_response(task_join_group_on_nodebb, 'Join user in {}'.format(group_name), status_code, response, username)
 
 
 @task(default_retry_delay=RETRY_DELAY, max_retries=None)
@@ -97,9 +81,5 @@ def task_update_onboarding_surveys_status(username):
     """
     Celery task to update survey status for username on NodeBB
     """
-    try:
-        status_code, response = NodeBBClient().users.update_onboarding_surveys_status(username=username)
-        log_response('Update Onboarding Survery', status_code, response, username)
-    except ConnectionError as exc:
-        LOGGER.debug('Retrying: Update Onboarding Survey task for user: {}'.format(username))
-        task_update_onboarding_surveys_status.retry(exc=exc)
+    status_code, response = NodeBBClient().users.update_onboarding_surveys_status(username=username)
+    handle_response(task_update_onboarding_surveys_status, 'Update Onboarding Survery', status_code, response, username)
