cc96a7e446d4e5602661034bb0b2b4144055831d
Michael Youngstrom <youngstrom.m@husky.neu.edu>
2018-09-13 09:46:18 -0400

    wow.. fix typo

--- pavelib/paver_tests/test_extract_and_generate.py
+++ pavelib/paver_tests/test_extract_and_generate.py
@@ -12,7 +12,7 @@ from unittest import TestCase
 
 from i18n import config, dummy, extract, generate
 from polib import pofile
-from pytz import UTC'
+from pytz import UTC
 from pavelib.paver_tests.utils import PaverTestCase
 
 
