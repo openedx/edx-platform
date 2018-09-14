287e2d346ea6f522274e19003428bdb8227f9544
Imran Bashir <imran.bashir@arbisoft.com>
2018-09-14 12:21:37 +0500

    add meta tags to course about page

--- common/djangoapps/custom_settings/models.py
+++ common/djangoapps/custom_settings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from openedx.core.djangoapps.xmodule_django.models import CourseKeyField
+import json
 
 
 class CustomSettings(models.Model):
@@ -11,6 +12,28 @@ class CustomSettings(models.Model):
     is_featured = models.BooleanField(default=False)
     show_grades = models.BooleanField(default=True)
     tags = models.CharField(max_length=255, null=True, blank=True)
+    seo_tags = models.TextField(null=True, blank=True)
 
     def __unicode__(self):
         return '{} | {}'.format(self.id, self.is_featured)
+
+    def get_course_meta_tags(self):
+        """
+        :return:
+            get seo tags for course
+        """
+        if self.seo_tags:
+            _json_tags = json.loads(self.seo_tags)
+            title = _json_tags.get("title", "")
+            description = _json_tags.get("description", "")
+            keywords = _json_tags.get("keywords", "")
+            robots = _json_tags.get("robots", "")
+
+            return {
+                "title": title,
+                "description": description,
+                "keywords": keywords,
+                "robots": robots
+            }
+        else:
+            return None
