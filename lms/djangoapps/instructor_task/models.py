667bf488d96af3f557047e1cde28e2cd282315ef
rabiaiftikhar <rabia.iftikhar@arbisoft.com>
2018-10-02 15:06:13 +0500

    EDUCATOR-3514 increase the length of task_input while creating instance of InstructorTask

--- lms/djangoapps/instructor_task/models.py
+++ lms/djangoapps/instructor_task/models.py
@@ -98,8 +98,8 @@ class InstructorTask(models.Model):
         json_task_input = json.dumps(task_input)
 
         # check length of task_input, and return an exception if it's too long:
-        if len(json_task_input) > 255:
-            fmt = 'Task input longer than 255: "{input}" for "{task}" of "{course}"'
+        if len(json_task_input) > 265:
+            fmt = 'Task input longer than 265: "{input}" for "{task}" of "{course}"'
             msg = fmt.format(input=json_task_input, task=task_type, course=course_id)
             raise ValueError(msg)
 
