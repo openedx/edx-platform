c57170dc48b7c47a9336adbe7df945c14eaa2654
Calen Pennington <cale@edx.org>
2018-10-10 12:00:46 -0400

    Add a deprecation warning for when courseware is imported incorrectly

---
+++ lms/djangoapps/courseware/__init__.py
@@ -0,0 +1,5 @@
+#pylint: disable=missing-docstring
+import warnings
+
+if __name__ == 'courseware':
+    warnings.warn("Importing 'lms.djangoapps.courseware' as 'courseware' is no longer supported", DeprecationWarning)
