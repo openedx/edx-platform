89eb73220eb49ca6d93de94a71c6399a6b846ea4
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2018-10-10 18:35:57 +0500

    Refactor: nodebb and mailchimp pipeline

--- common/djangoapps/mailchimp_pipeline/signals/handlers.py
+++ common/djangoapps/mailchimp_pipeline/signals/handlers.py
@@ -1,64 +1,87 @@
+"""
+Handlers and signals for Mailchimp pipeline
+"""
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from lms.djangoapps.certificates import api as certificate_api
+from lms.djangoapps.onboarding.models import (
+    UserExtendedProfile, Organization, EmailPreference,)
 from mailchimp_pipeline.client import ChimpClient, MailChimpException
 from mailchimp_pipeline.helpers import get_org_data_for_mandrill, get_user_active_enrollements, \
     get_enrollements_course_short_ids
 from mailchimp_pipeline.tasks import update_org_details_at_mailchimp
-from lms.djangoapps.onboarding.models import (UserExtendedProfile, Organization, EmailPreference,)
-from lms.djangoapps.certificates import api as certificate_api
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from student.models import (UserProfile, CourseEnrollment, )
-from django.conf import settings
 
 from logging import getLogger
 log = getLogger(__name__)
 
 
-def send_user_profile_info_to_mailchimp(sender, instance, kwargs):  # pylint: disable=unused-argument, invalid-name
-    """ Create user account at nodeBB when user created at edx Platform """
-    user_json = None
-    if sender == UserProfile:
-        profile = instance
-        user_json = {
-            "merge_fields": {
-                "LANG": profile.language if profile.language else "",
-                "COUNTRY": profile.country.name.format() if profile.country else "",
-                "CITY": profile.city if profile.city else "",
-            }
-        }
-    elif sender == UserExtendedProfile:
-        extended_profile = instance
-        org_label, org_type, work_area = get_org_data_for_mandrill(extended_profile.organization)
-        user_json = {
-            "merge_fields": {
-                "ORG": org_label,
-                "ORGTYPE": org_type,
-                "WORKAREA": work_area
-            }
-        }
-    elif sender == EmailPreference:
-        email_preferences = instance
+def update_mailchimp(email, data):
+    try:
+        response = ChimpClient().add_update_member_to_list(
+            settings.MAILCHIMP_LEARNERS_LIST_ID, email, data)
+        log.info(response)
+    except MailChimpException as ex:
+        log.exception(ex)
 
-        if email_preferences.opt_in == 'yes':
-            opt_in = 'TRUE'
-        elif email_preferences.opt_in == 'no':
-            opt_in = 'FALSE'
-        else:
-            opt_in = ''
-        user_json = {
-            "merge_fields": {
-                "OPTIN": opt_in
-            }
+
+@receiver(post_save, sender=EmailPreference)
+def sync_email_preference_with_mailchimp(sender, instance, **kwargs):
+    email_preferences = instance
+
+    if email_preferences.opt_in == 'yes':
+        opt_in = 'TRUE'
+    elif email_preferences.opt_in == 'no':
+        opt_in = 'FALSE'
+    else:
+        opt_in = ''
+    user_json = {
+        "merge_fields": {
+            "OPTIN": opt_in
         }
+    }
 
-    elif sender == Organization:
-        org_label, org_type, work_area = get_org_data_for_mandrill(instance)
-        update_org_details_at_mailchimp.delay(org_label, org_type, work_area, settings.MAILCHIMP_LEARNERS_LIST_ID)
+    update_mailchimp(instance.user.email, user_json)
 
-    if user_json and not sender == Organization:
-        try:
-            response = ChimpClient().add_update_member_to_list(settings.MAILCHIMP_LEARNERS_LIST_ID, instance.user.email, user_json)
-            log.info(response)
-        except MailChimpException as ex:
-            log.exception(ex)
+
+@receiver(post_save, sender=UserProfile)
+def sync_user_profile_with_mailchimp(sender, instance, **kwargs):
+    profile = instance
+    user_json = {
+        "merge_fields": {
+            "LANG": profile.language if profile.language else "",
+            "COUNTRY": profile.country.name.format() if profile.country else "",
+            "CITY": profile.city if profile.city else "",
+        }
+    }
+
+    update_mailchimp(instance.user.email, user_json)
+
+
+@receiver(post_save, sender=UserExtendedProfile)
+def sync_extended_profile_with_mailchimp(sender, instance, **kwargs):
+    extended_profile = instance
+    org_label, org_type, work_area = get_org_data_for_mandrill(
+        extended_profile.organization)
+    user_json = {
+        "merge_fields": {
+            "ORG": org_label,
+            "ORGTYPE": org_type,
+            "WORKAREA": work_area
+        }
+    }
+
+    update_mailchimp(instance.user.email, user_json)
+
+
+@receiver(post_save, sender=Organization)
+def sync_organization_with_mailchimp(sender, instance, **kwargs):
+    org_label, org_type, work_area = get_org_data_for_mandrill(instance)
+    update_org_details_at_mailchimp.delay(
+        org_label, org_type, work_area, settings.MAILCHIMP_LEARNERS_LIST_ID)
 
 
 def send_user_info_to_mailchimp(sender, user, created, kwargs):
@@ -72,16 +95,14 @@ def send_user_info_to_mailchimp(sender, user, created, kwargs):
     }
 
     if created:
-        user_json["merge_fields"].update({"DATEREGIS": str(user.date_joined.strftime("%m/%d/%Y"))})
+        user_json["merge_fields"].update(
+            {"DATEREGIS": str(user.date_joined.strftime("%m/%d/%Y"))})
         user_json.update({
             "email_address": user.email,
             "status_if_new": "subscribed"
         })
-    try:
-        response = ChimpClient().add_update_member_to_list(settings.MAILCHIMP_LEARNERS_LIST_ID, user.email, user_json)
-        log.info(response)
-    except MailChimpException as ex:
-        log.exception(ex)
+
+    update_mailchimp(user.email, user_json)
 
 
 def send_user_enrollments_to_mailchimp(sender, instance, created, kwargs):
@@ -91,12 +112,8 @@ def send_user_enrollments_to_mailchimp(sender, instance, created, kwargs):
             "ENROLL_IDS": get_enrollements_course_short_ids(instance.user.username)
         }
     }
-    try:
-        response = ChimpClient().add_update_member_to_list(settings.MAILCHIMP_LEARNERS_LIST_ID, instance.user.email,
-                                                           user_json)
-        log.info(response)
-    except MailChimpException as ex:
-        log.exception(ex)
+
+    update_mailchimp(instance.user.email, user_json)
 
 
 def send_user_course_completions_to_mailchimp(sender, user, course_key, kwargs):
@@ -108,14 +125,11 @@ def send_user_course_completions_to_mailchimp(sender, user, course_key, kwargs):
 
     completed_course_keys = [cert.get('course_key', '') for cert in all_certs
                              if certificate_api.is_passing_status(cert['status'])]
-    completed_courses = CourseOverview.objects.filter(id__in=completed_course_keys)
+    completed_courses = CourseOverview.objects.filter(
+        id__in=completed_course_keys)
     user_json = {
         "merge_fields": {
             "COMPLETES": ", ".join([course.display_name for course in completed_courses]),
         }
     }
-    try:
-        response = ChimpClient().add_update_member_to_list(settings.MAILCHIMP_LEARNERS_LIST_ID, user.email, user_json)
-        log.info(response)
-    except MailChimpException as ex:
-        log.exception(ex)
+    update_mailchimp(user.email, user_json)
--- common/djangoapps/nodebb/signals/handlers.py
+++ common/djangoapps/nodebb/signals/handlers.py
@@ -11,9 +11,10 @@ from common.djangoapps.nodebb.tasks import (task_create_user_on_nodebb, task_upd
 from common.lib.nodebb_client.client import NodeBBClient
 from lms.djangoapps.onboarding.helpers import COUNTRIES
 from certificates.models import GeneratedCertificate
-from lms.djangoapps.onboarding.models import (UserExtendedProfile, Organization, FocusArea, EmailPreference, )
+from lms.djangoapps.onboarding.models import (
+    UserExtendedProfile, Organization, FocusArea, EmailPreference, )
 from lms.djangoapps.teams.models import CourseTeam, CourseTeamMembership
-from mailchimp_pipeline.signals.handlers import send_user_info_to_mailchimp, send_user_profile_info_to_mailchimp, \
+from mailchimp_pipeline.signals.handlers import send_user_info_to_mailchimp, \
     send_user_enrollments_to_mailchimp, send_user_course_completions_to_mailchimp
 from nodebb.models import DiscussionCommunity, TeamGroupChat
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
@@ -28,9 +29,12 @@ log = getLogger(__name__)
 
 def log_action_response(user, status_code, response_body):
     if status_code != 200:
-        log.error("Error: Can not update user(%s) on nodebb due to %s" % (user.username, response_body))
+        log.error("Error: Can not update user(%s) on nodebb due to %s" %
+                  (user.username, response_body))
     else:
-        log.info('Success: User(%s) has been updated on nodebb' % user.username)
+        log.info('Success: User(%s) has been updated on nodebb' %
+                 user.username)
+
 
 @receiver(post_save, sender=CourseEnrollment)
 def sync_enrolments_to_mailchimp(sender, instance, created, **kwargs):
@@ -43,47 +47,57 @@ def handle_course_cert_awarded(sender, user, course_key, **kwargs):  # pylint: d
 
 
 @receiver(post_save, sender=UserProfile)
+def sync_user_profile_info_with_nodebb(sender, instance, **kwargs):
+    user = instance.user
+    data_to_sync = {
+        "city_of_residence": instance.city,
+        "country_of_residence": COUNTRIES.get(instance.country.code, ''),
+        "birthday": "01/01/%s" % instance.year_of_birth,
+        "language": instance.language,
+    }
+    task_update_user_profile_on_nodebb.delay(
+        username=user.username, profile_data=data_to_sync)
+
+
 @receiver(post_save, sender=UserExtendedProfile)
-@receiver(post_save, sender=EmailPreference)
+def sync_extended_profile_info_with_nodebb(sender, instance, **kwargs):
+    user = instance.user
+    data_to_sync = {
+        "country_of_employment": COUNTRIES.get(instance.country_of_employment, ''),
+        "city_of_employment": instance.city_of_employment,
+        "interests": instance.get_user_selected_interests(),
+        "self_prioritize_areas": instance.get_user_selected_functions()
+    }
+
+    if instance.organization:
+        data_to_sync["organization"] = instance.organization.label
+
+    task_update_user_profile_on_nodebb.delay(
+        username=user.username, profile_data=data_to_sync)
+
+
 @receiver(post_save, sender=Organization)
-def sync_user_info_with_nodebb(sender, instance, created, **kwargs):  # pylint: disable=unused-argument, invalid-name
+def sync_organization_info_with_nodebb(sender, instance, created, **kwargs):  # pylint: disable=unused-argument, invalid-name
     """
     Sync information b/w NodeBB User Profile and Edx User Profile
     """
-
     request = get_current_request()
+
+    # To prevent unnecessary updation in case Django only updates
+    # last_logged_in or last seen etc.
+    if request:
+        if 'login' in request.path or 'logout' in request.path:
+            return
+
+    data_to_sync = {
+        "focus_area": FocusArea.objects.get(code=instance.focus_area).label if instance.focus_area else ""
+    }
+
     user = request.user
 
-    send_user_profile_info_to_mailchimp(sender, instance, kwargs)
+    task_update_user_profile_on_nodebb.delay(
+        username=user.username, profile_data=data_to_sync)
 
-    if 'login' in request.path or 'logout' in request.path or sender == EmailPreference:
-        return
-
-    if sender == UserProfile:
-        data_to_sync = {
-            "city_of_residence": instance.city,
-            "country_of_residence": COUNTRIES.get(instance.country.code, ''),
-            "birthday": "01/01/%s" % instance.year_of_birth,
-            "language": instance.language,
-        }
-    elif sender == UserExtendedProfile:
-        data_to_sync = {
-            "country_of_employment": COUNTRIES.get(instance.country_of_employment, ''),
-            "city_of_employment": instance.city_of_employment,
-            "interests": instance.get_user_selected_interests(),
-            "self_prioritize_areas": instance.get_user_selected_functions()
-        }
-
-        if instance.organization:
-            data_to_sync["organization"] = instance.organization.label
-    elif sender == Organization:
-        data_to_sync = {
-            "focus_area": FocusArea.objects.get(code=instance.focus_area).label if instance.focus_area else ""
-        }
-    else:
-        data_to_sync = {}
-
-    task_update_user_profile_on_nodebb.delay(username=user.username, profile_data=data_to_sync)
 
 @receiver(post_save, sender=User, dispatch_uid='update_user_profile_on_nodebb')
 def update_user_profile_on_nodebb(sender, instance, created, **kwargs):
@@ -106,14 +120,17 @@ def update_user_profile_on_nodebb(sender, instance, created, **kwargs):
             'date_joined': instance.date_joined.strftime('%d/%m/%Y'),
         }
 
-        task_create_user_on_nodebb.delay(username=instance.username, user_data=data_to_sync)
+        task_create_user_on_nodebb.delay(
+            username=instance.username, user_data=data_to_sync)
     else:
         data_to_sync = {
             'first_name': instance.first_name,
             'last_name': instance.last_name
         }
 
-        task_update_user_profile_on_nodebb.delay(username=instance.username, profile_data=data_to_sync)
+        task_update_user_profile_on_nodebb.delay(
+            username=instance.username, profile_data=data_to_sync)
+
 
 @receiver(post_delete, sender=User)
 def delete_user_from_nodebb(sender, **kwargs):
@@ -124,6 +141,7 @@ def delete_user_from_nodebb(sender, **kwargs):
 
     task_delete_user_on_nodebb.delay(username=instance.username)
 
+
 @receiver(pre_save, sender=User, dispatch_uid='activate_deactivate_user_on_nodebb')
 def activate_deactivate_user_on_nodebb(sender, instance, **kwargs):
     """
@@ -132,7 +150,9 @@ def activate_deactivate_user_on_nodebb(sender, instance, **kwargs):
     current_user_obj = User.objects.filter(pk=instance.pk)
 
     if current_user_obj.first() and current_user_obj[0].is_active != instance.is_active:
-        task_activate_user_on_nodebb.delay(username=instance.username, active=instance.is_active)
+        task_activate_user_on_nodebb.delay(
+            username=instance.username, active=instance.is_active)
+
 
 @receiver(post_save, sender=CourseOverview, dispatch_uid="nodebb.signals.handlers.create_category_on_nodebb")
 def create_category_on_nodebb(sender, instance, created, **kwargs):
@@ -140,8 +160,10 @@ def create_category_on_nodebb(sender, instance, created, **kwargs):
     Create a community on NodeBB whenever a new course is created
     """
     if created:
-        community_name = '%s-%s-%s-%s' % (instance.display_name, instance.id.org, instance.id.course, instance.id.run)
-        status_code, response_body = NodeBBClient().categories.create(name=community_name, label=instance.display_name)
+        community_name = '%s-%s-%s-%s' % (instance.display_name,
+                                          instance.id.org, instance.id.course, instance.id.run)
+        status_code, response_body = NodeBBClient().categories.create(
+            name=community_name, label=instance.display_name)
 
         if status_code != 200:
             log.error(
@@ -166,9 +188,12 @@ def join_group_on_nodebb(sender, event=None, user=None, **kwargs):  # pylint: di
         username = user.username
         course = modulestore().get_course(kwargs.get('course_id'))
 
-        community_name = '%s-%s-%s-%s' % (course.display_name, course.id.org, course.id.course, course.id.run)
+        community_name = '%s-%s-%s-%s' % (course.display_name,
+                                          course.id.org, course.id.course, course.id.run)
 
-        task_join_group_on_nodebb.delay(group_name=community_name, username=username)
+        task_join_group_on_nodebb.delay(
+            group_name=community_name, username=username)
+
 
 @receiver(post_save, sender=CourseTeam, dispatch_uid="nodebb.signals.handlers.create_update_groupchat_on_nodebb")
 def create_update_groupchat_on_nodebb(sender, instance, created, **kwargs):
@@ -190,12 +215,15 @@ def create_update_groupchat_on_nodebb(sender, instance, created, **kwargs):
                 )
             )
         else:
-            TeamGroupChat.objects.create(team_id=instance.id, room_id=response_body['room'])
-            log.info("Successfully created group for course %s" % instance.course_id)
+            TeamGroupChat.objects.create(
+                team_id=instance.id, room_id=response_body['room'])
+            log.info("Successfully created group for course %s" %
+                     instance.course_id)
     else:
         room_id = team_group_chat.room_id
         group_info = _get_group_data(instance, is_created=False)
-        status_code, response_body = NodeBBClient().groups.update(room_id=room_id, **group_info)
+        status_code, response_body = NodeBBClient().groups.update(
+            room_id=room_id, **group_info)
 
         if status_code != 200:
             log.error(
@@ -204,7 +232,8 @@ def create_update_groupchat_on_nodebb(sender, instance, created, **kwargs):
                 )
             )
         else:
-            log.info("Successfully updated group for course %s" % instance.course_id)
+            log.info("Successfully updated group for course %s" %
+                     instance.course_id)
 
 
 def _get_group_data(instance, is_created=True):
@@ -230,7 +259,8 @@ def delete_groupchat_on_nodebb(sender, instance, **kwargs):
     team_group_chat = TeamGroupChat.objects.filter(team_id=instance.id).first()
 
     if team_group_chat:
-        status_code, response_body = NodeBBClient().groups.delete(room_id=team_group_chat.room_id)
+        status_code, response_body = NodeBBClient().groups.delete(
+            room_id=team_group_chat.room_id)
 
         if status_code != 200:
             log.error(
@@ -240,7 +270,8 @@ def delete_groupchat_on_nodebb(sender, instance, **kwargs):
             )
         else:
             team_group_chat.delete()
-            log.info("Successfully deleted group chat for course %s" % instance.course_id)
+            log.info("Successfully deleted group chat for course %s" %
+                     instance.course_id)
 
 
 @receiver(post_save, sender=CourseTeamMembership, dispatch_uid="nodebb.signals.handlers.join_groupchat_on_nodebb")
@@ -248,11 +279,13 @@ def join_groupchat_on_nodebb(sender, instance, created, **kwargs):
     """
     Join group on NodeBB whenever a new member joins a team
     """
-    team_group_chat = TeamGroupChat.objects.filter(team_id=instance.team.id).first()
+    team_group_chat = TeamGroupChat.objects.filter(
+        team_id=instance.team.id).first()
 
     if created and team_group_chat:
         member_info = {"team": [instance.user.username, '']}
-        status_code, response_body = NodeBBClient().groups.update(room_id=team_group_chat.room_id, **member_info)
+        status_code, response_body = NodeBBClient().groups.update(
+            room_id=team_group_chat.room_id, **member_info)
 
         if status_code != 200:
             log.error(
@@ -261,7 +294,8 @@ def join_groupchat_on_nodebb(sender, instance, created, **kwargs):
                 )
             )
         else:
-            log.info('Success: User have joined the group %s successfully' % instance.team.name)
+            log.info('Success: User have joined the group %s successfully' %
+                     instance.team.name)
 
 
 @receiver(post_delete, sender=CourseTeamMembership, dispatch_uid="nodebb.signals.handlers.leave_groupchat_on_nodebb")
@@ -269,11 +303,13 @@ def leave_groupchat_on_nodebb(sender, instance, **kwargs):
     """
     Leave group on NodeBB whenever a member leaves a team
     """
-    team_group_chat = TeamGroupChat.objects.filter(team_id=instance.team.id).first()
+    team_group_chat = TeamGroupChat.objects.filter(
+        team_id=instance.team.id).first()
 
     if team_group_chat:
         member_info = {"team": [instance.user.username, '']}
-        status_code, response_body = NodeBBClient().groups.delete(room_id=team_group_chat.room_id, **member_info)
+        status_code, response_body = NodeBBClient().groups.delete(
+            room_id=team_group_chat.room_id, **member_info)
 
         if status_code != 200:
             log.error(
@@ -282,4 +318,5 @@ def leave_groupchat_on_nodebb(sender, instance, **kwargs):
                 )
             )
         else:
-            log.info('Success: User have unjoined the group %s successfully' % instance.team.name)
+            log.info('Success: User have unjoined the group %s successfully' %
+                     instance.team.name)
