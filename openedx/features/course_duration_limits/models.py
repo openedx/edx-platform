57444001122bd411ba5145128fe17a39d55aab9b
Calen Pennington <cale@edx.org>
2018-10-15 13:17:14 -0400

    Add a StackedConfigurationModel to managed content_duration_limits activation and configuration

--- openedx/features/course_duration_limits/models.py
+++ openedx/features/course_duration_limits/models.py
@@ -3,4 +3,43 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from openedx.core.lib.config_model_utils import (
+    CourseStackedConfigurationModel,
+    GlobalStackedConfigurationModel,
+    OrgStackedConfigurationModel,
+    SiteStackedConfigurationModel,
+    StackedConfigurationModel,
+)
+
+
+class CourseDurationLimitsConfig(models.Model):
+    class Meta(object):
+        abstract = True
+
+    enabled_after = models.DateTimeField()
+    STACKABLE_FIELDS = ['enabled', 'enabled_after']
+
+    @classmethod
+    def is_enabled(cls, enrollment):
+        current_config = CourseCourseDurationLimitsConfig.stacked_current(
+            CourseOverview.objects.get(id=enrollment.course_id)
+        )
+
+        return current_config.enabled and current_config.enabled_after <= enrollment.created
+
+
+
+class GlobalCourseDurationLimitsConfig(CourseDurationLimitsConfig, GlobalStackedConfigurationModel):
+    pass
+
+
+class SiteCourseDurationLimitsConfig(CourseDurationLimitsConfig, SiteStackedConfigurationModel):
+    pass
+
+
+class OrgCourseDurationLimitsConfig(CourseDurationLimitsConfig, OrgStackedConfigurationModel):
+    pass
+
+
+class CourseCourseDurationLimitsConfig(CourseDurationLimitsConfig, CourseStackedConfigurationModel):
+    pass
