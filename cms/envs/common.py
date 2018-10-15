1a0bd8bf719f9154b0ba9e4b379d0e95c01a764a
Calen Pennington <cale@edx.org>
2018-10-15 13:21:06 -0400

    Add a shell app for course_duration_limits

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1163,6 +1163,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
 
     'openedx.features.content_type_gating',
+    'openedx.features.course_duration_limits',
 ]
 
 
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2283,6 +2283,7 @@ INSTALLED_APPS = [
     'openedx.features.learner_analytics',
     'openedx.features.portfolio_project',
     'openedx.features.content_type_gating',
+    'openedx.features.course_duration_limits',
 
     'experiments',
 
