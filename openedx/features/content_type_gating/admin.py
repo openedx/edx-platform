56719f0f458ce5f3592b506458f5cbf7e95e5003
Calen Pennington <cale@edx.org>
2018-10-15 13:16:54 -0400

    Add a StackedConfigurationModel to managed content_type_gating activation and configuration

--- openedx/features/content_type_gating/admin.py
+++ openedx/features/content_type_gating/admin.py
@@ -2,5 +2,30 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from config_models.admin import ConfigurationModelAdmin, KeyedConfigurationModelAdmin
+from openedx.features.content_type_gating.models import (
+    GlobalContentTypeGatingConfig,
+    SiteContentTypeGatingConfig,
+    OrgContentTypeGatingConfig,
+    CourseContentTypeGatingConfig,
+)
+
 
 # Register your models here.
+class GlobalContentTypeGatingConfigAdmin(ConfigurationModelAdmin):
+    pass
+
+class SiteContentTypeGatingConfigAdmin(KeyedConfigurationModelAdmin):
+    pass
+
+class OrgContentTypeGatingConfigAdmin(KeyedConfigurationModelAdmin):
+    pass
+
+class CourseContentTypeGatingConfigAdmin(KeyedConfigurationModelAdmin):
+    pass
+
+
+admin.site.register(GlobalContentTypeGatingConfig, GlobalContentTypeGatingConfigAdmin)
+admin.site.register(SiteContentTypeGatingConfig, SiteContentTypeGatingConfigAdmin)
+admin.site.register(OrgContentTypeGatingConfig, OrgContentTypeGatingConfigAdmin)
+admin.site.register(CourseContentTypeGatingConfig, CourseContentTypeGatingConfigAdmin)
