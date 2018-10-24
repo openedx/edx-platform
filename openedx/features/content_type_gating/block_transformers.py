104bb22ebea9ebe35c6585a8d761b7b371720862
Bessie Steinberg <bsteinberg@edx.org>
2018-10-24 10:29:16 -0400

    Put all content type gating stuff behind a waffle flag

--- openedx/features/content_type_gating/block_transformers.py
+++ openedx/features/content_type_gating/block_transformers.py
@@ -7,39 +7,41 @@ from openedx.core.djangoapps.content.block_structure.transformer import (
     BlockStructureTransformer,
 )
 from openedx.features.content_type_gating.partitions import CONTENT_GATING_PARTITION_ID
+from openedx.features.course_duration_limits.config import CONTENT_TYPE_GATING_FLAG
 
+if CONTENT_TYPE_GATING_FLAG.is_enabled():
 
-class ContentTypeGateTransformer(BlockStructureTransformer):
-    """
-    A transformer that adds a partition condition for all graded content
-    so that the content is only visible to verified users.
-    """
-    WRITE_VERSION = 1
-    READ_VERSION = 1
-
-    @classmethod
-    def name(cls):
+    class ContentTypeGateTransformer(BlockStructureTransformer):
         """
-        Unique identifier for the transformer's class;
-        same identifier used in setup.py.
+        A transformer that adds a partition condition for all graded content
+        so that the content is only visible to verified users.
         """
-        return "content_type_gate"
+        WRITE_VERSION = 1
+        READ_VERSION = 1
 
-    @classmethod
-    def collect(cls, block_structure):
-        """
-        Collects any information that's necessary to execute this
-        transformer's transform method.
-        """
-        block_structure.request_xblock_fields('group_access', 'graded', 'has_score')
+        @classmethod
+        def name(cls):
+            """
+            Unique identifier for the transformer's class;
+            same identifier used in setup.py.
+            """
+            return "content_type_gate"
 
-    def transform(self, usage_info, block_structure):
-        for block_key in block_structure.topological_traversal():
-            graded = block_structure.get_xblock_field(block_key, 'graded')
-            has_score = block_structure.get_xblock_field(block_key, 'has_score')
-            if graded and has_score:
-                current_access = block_structure.get_xblock_field(block_key, 'group_access')
-                if current_access is None:
-                    current_access = {}
-                current_access.setdefault(CONTENT_GATING_PARTITION_ID, [settings.CONTENT_TYPE_GATE_PARTITION_IDS['unlocked']])
-                block_structure.override_xblock_field(block_key, 'group_access', current_access)
+        @classmethod
+        def collect(cls, block_structure):
+            """
+            Collects any information that's necessary to execute this
+            transformer's transform method.
+            """
+            block_structure.request_xblock_fields('group_access', 'graded', 'has_score')
+
+        def transform(self, usage_info, block_structure):
+            for block_key in block_structure.topological_traversal():
+                graded = block_structure.get_xblock_field(block_key, 'graded')
+                has_score = block_structure.get_xblock_field(block_key, 'has_score')
+                if graded and has_score:
+                    current_access = block_structure.get_xblock_field(block_key, 'group_access')
+                    if current_access is None:
+                        current_access = {}
+                    current_access.setdefault(CONTENT_GATING_PARTITION_ID, [settings.CONTENT_TYPE_GATE_PARTITION_IDS['unlocked']])
+                    block_structure.override_xblock_field(block_key, 'group_access', current_access)
--- openedx/features/content_type_gating/field_override.py
+++ openedx/features/content_type_gating/field_override.py
@@ -8,32 +8,34 @@ from django.conf import settings
 
 from lms.djangoapps.courseware.field_overrides import FieldOverrideProvider, disable_overrides
 from openedx.features.content_type_gating.partitions import CONTENT_GATING_PARTITION_ID
+from openedx.features.course_duration_limits.config import CONTENT_TYPE_GATING_FLAG
 
+if CONTENT_TYPE_GATING_FLAG.is_enabled():
 
-class ContentTypeGatingFieldOverride(FieldOverrideProvider):
-    """
-    A concrete implementation of
-    :class:`~courseware.field_overrides.FieldOverrideProvider` which forces
-    graded content to only be accessible to the Unlocked group
-    """
-    def get(self, block, name, default):
-        if name != 'group_access':
-            return default
+    class ContentTypeGatingFieldOverride(FieldOverrideProvider):
+        """
+        A concrete implementation of
+        :class:`~courseware.field_overrides.FieldOverrideProvider` which forces
+        graded content to only be accessible to the Unlocked group
+        """
+        def get(self, block, name, default):
+            if name != 'group_access':
+                return default
 
-        if not (getattr(block, 'graded', False) and block.has_score):
-            return default
+            if not (getattr(block, 'graded', False) and block.has_score):
+                return default
 
-        # Read the group_access from the fallback field-data service
-        with disable_overrides():
-            original_group_access = block.group_access
+            # Read the group_access from the fallback field-data service
+            with disable_overrides():
+                original_group_access = block.group_access
 
-        if original_group_access is None:
-            original_group_access = {}
-        original_group_access.setdefault(CONTENT_GATING_PARTITION_ID, [settings.CONTENT_TYPE_GATE_PARTITION_IDS['unlocked']])
+            if original_group_access is None:
+                original_group_access = {}
+            original_group_access.setdefault(CONTENT_GATING_PARTITION_ID, [settings.CONTENT_TYPE_GATE_PARTITION_IDS['unlocked']])
 
-        return original_group_access
+            return original_group_access
 
-    @classmethod
-    def enabled_for(cls, course):
-        """This simple override provider is always enabled"""
-        return True
+        @classmethod
+        def enabled_for(cls, course):
+            """This simple override provider is always enabled"""
+            return True
