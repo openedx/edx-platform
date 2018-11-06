71c6be6846fb574e42e5da4a3a5dd7ae9dd601e5
Maxim Starodubcev <starodubcevmax@gmail.com>
2018-11-06 15:06:44 +0200

    Fix ajax-error script (#732)

--- lms/static/js/ajax-error.js
+++ lms/static/js/ajax-error.js
@@ -1,15 +1,20 @@
 $(document).ajaxError(function(event, jXHR) {
     if (jXHR.status === 403) {
-        var message = gettext(
-            'You have been logged out of your edX account. ' +
-            'Click Okay to log in again now. ' +
-            'Click Cancel to stay on this page ' +
-            '(you must log in again to save your work).'
-        );
+        $.ajax({
+            url: '/api/user/v1/me',
+            error: function() {
+                var message = gettext(
+                    'You have been logged out of your edX account. ' +
+                    'Click Okay to log in again now. ' +
+                    'Click Cancel to stay on this page ' +
+                    '(you must log in again to save your work).'
+                );
 
-        if (window.confirm(message)) {
-            var currentLocation = window.location.pathname;
-            window.location.href = '/login?next=' + encodeURIComponent(currentLocation);
-        }
+                if (window.confirm(message)) {
+                    var currentLocation = window.location.pathname;
+                    window.location.href = '/login?next=' + encodeURIComponent(currentLocation);
+                }
+            }
+        });
     }
 });
