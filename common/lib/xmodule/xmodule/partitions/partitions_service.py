ecb052125c99ef0cbc5b0e458e528943b12bf39c
Cali Stenson <cstenson@edx.org>
2018-11-16 13:11:17 -0500

    Move get_user_partition_groups to partition_services to call from multiple contexts.

--- common/lib/xmodule/xmodule/partitions/partitions_service.py
+++ common/lib/xmodule/xmodule/partitions/partitions_service.py
@@ -41,6 +41,33 @@ def get_all_partitions_for_course(course, active_only=False):
         all_partitions = [partition for partition in all_partitions if partition.active]
     return all_partitions
 
+def get_user_partition_groups(course_key, user):
+        """
+        Collect group ID for each partition in this course for this user.
+
+        Arguments:
+            course_key (CourseKey)
+            user_partitions (list[UserPartition])
+            user (User)
+
+        Returns:
+            dict[int: Group]: Mapping from user partitions to the group to
+                which the user belongs in each partition. If the user isn't
+                in a group for a particular partition, then that partition's
+                ID will not be in the dict.
+        """
+        user_partitions = get_all_partitions_for_course(course)
+        partition_groups = {}
+        for partition in user_partitions:
+            group = partition.scheme.get_group_for_user(
+                course_key,
+                user,
+                partition,
+            )
+            if group is not None:
+                partition_groups[partition.id] = group
+        return partition_groups
+
 
 def _get_dynamic_partitions(course):
     """
