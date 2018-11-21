39220d90922af56357148ad362f3c6539ce4e664
Nishant Dixit <nishant@thedevelopers.co>
2018-11-21 20:06:30 +0530

    temp tahoe devstack fixes

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -279,3 +279,6 @@ if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
 MODULESTORE = convert_module_store_setting_if_needed(MODULESTORE)
 
 SECRET_KEY = '85920908f28904ed733fe576320db18cabd7b6cd'
+
+#####################################################################
+INSTALLED_APPS += ['openedx.core.djangoapps.appsembler']
