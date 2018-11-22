302b89a3370fbde1b442f06dde5d8bc7ccbed7db
Jillian Vogel <jill@opencraft.com>
2018-11-23 00:54:32 +1030

    Trying to fix eslint quality failure

--- common/static/js/video/time.js
+++ common/static/js/video/time.js
@@ -1,57 +1,58 @@
 (function(define) {
+    'use strict';
     define(
 'js/video/time.js',
-[],
-function() {
-    'use strict';
-/**
- * Provides time formatting and conversion functions.
- */
+['underscore'],
+function(_) {
+    /**
+     * Provides time formatting and conversion functions.
+     */
 
-function format(time, formatFull) {
-    var hours, minutes, seconds;
-
-    if (!_.isFinite(time) || time < 0) {
-        time = 0;
+    function pad(number) {
+        if (number < 10) {
+            return '0' + number;
+        } else {
+            return '' + number;
+        }
     }
 
-    seconds = Math.floor(time);
-    minutes = Math.floor(seconds / 60);
-    hours = Math.floor(minutes / 60);
-    seconds = seconds % 60;
-    minutes = minutes % 60;
+    function format(time, fullFormat) {
+        var hours, minutes, seconds;
 
-    if (formatFull) {
-        return '' + _pad(hours) + ':' + _pad(minutes) + ':' + _pad(seconds % 60);
-    } else if (hours) {
-        return '' + hours + ':' + _pad(minutes) + ':' + _pad(seconds % 60);
-    } else {
-        return '' + minutes + ':' + _pad(seconds % 60);
+        if (!_.isFinite(time) || time < 0) {
+            seconds = Math.floor(0);
+        } else {
+            seconds = Math.floor(time);
+        }
+
+        minutes = Math.floor(seconds / 60);
+        hours = Math.floor(minutes / 60);
+        seconds = seconds % 60;
+        minutes = minutes % 60;
+
+        if (fullFormat) {
+            return '' + pad(hours) + ':' + pad(minutes) + ':' + pad(seconds % 60);
+        } else if (hours) {
+            return '' + hours + ':' + pad(minutes) + ':' + pad(seconds % 60);
+        } else {
+            return '' + minutes + ':' + pad(seconds % 60);
+        }
     }
-}
 
-function formatFull(time) {
-    // The returned value will not be user-facing. So no need for
-    // internationalization.
-    return format(time, true);
-}
-
-function convert(time, oldSpeed, newSpeed) {
-    return (time * oldSpeed / newSpeed).toFixed(3);
-}
-
-function _pad(number) {
-    if (number < 10) {
-        return '0' + number;
-    } else {
-        return '' + number;
+    function formatFull(time) {
+        // The returned value will not be user-facing. So no need for
+        // internationalization.
+        return format(time, true);
     }
-}
+
+    function convert(time, oldSpeed, newSpeed) {
+        return (time * oldSpeed / newSpeed).toFixed(3);
+    }
 
     return {
         format: format,
         formatFull: formatFull,
-        convert: convert,
+        convert: convert
     };
 });
 }(RequireJS.define));
