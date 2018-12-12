394a61fbf96d2c6f3bd254f11be731c552d25966
Dave St.Germain <dstgermain@edx.org>
2018-12-12 11:39:04 -0500

    Enabled edx-proctoring as a Pluggable Django App.

--- lms/envs/test.py
+++ lms/envs/test.py
@@ -124,12 +124,6 @@ XQUEUE_WAITTIME_BETWEEN_REQUESTS = 5  # seconds
 MOCK_STAFF_GRADING = True
 MOCK_PEER_GRADING = True
 
-PROCTORING_BACKENDS = {
-    'DEFAULT': 'mock',
-    'mock': {},
-    'mock_proctoring_without_rules': {},
-}
-
 ############################ STATIC FILES #############################
 
 # TODO (cpennington): We need to figure out how envs/test.py can inject things
