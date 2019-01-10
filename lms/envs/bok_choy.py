143b43ac31b76d4bb498f1f91bf8feec9e557166
Cory Lee <cory@edx.org>
2019-01-10 13:21:32 -0500

    Trying to get bok_choy passing

--- lms/envs/bok_choy.py
+++ lms/envs/bok_choy.py
@@ -27,8 +27,13 @@ TEST_ROOT = CONFIG_ROOT.dirname().dirname() / "test_root"
 # Unlike in prod, we use the JSON files stored in this repo.
 # This is a convenience for ensuring (a) that we can consistently find the files
 # and (b) that the files are the same in Jenkins as in local dev.
-os.environ['SERVICE_VARIANT'] = 'bok_choy_docker' if 'BOK_CHOY_HOSTNAME' in os.environ else 'bok_choy'
-os.environ['CONFIG_ROOT'] = CONFIG_ROOT
+if 'BOK_CHOY_HOSTNAME' in os.environ:
+    os.environ['SERVICE_VARIANT'] = 'bok_choy_docker'
+else:
+    os.environ['SERVICE_VARIANT'] = 'bok_choy'
+    
+os.environ['CONFIG_ROOT'] = path(__file__).abspath().dirname()
+os.environ['LMS_CFG'] = str.format("{}/{}{}", path(__file__).abspath().dirname(), os.environ['SERVICE_VARIANT'], ".yaml")
 
 from .production import *  # pylint: disable=wildcard-import, unused-wildcard-import, wrong-import-position
 
