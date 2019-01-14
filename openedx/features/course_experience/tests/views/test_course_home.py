9d23dfe27020f3ea0c11aee3874a45122fb91a2d
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-01-14 14:32:32 -0500

    REVEM-130 Remove program info from user metadata

--- openedx/features/course_experience/tests/views/test_course_home.py
+++ openedx/features/course_experience/tests/views/test_course_home.py
@@ -204,8 +204,7 @@ class TestCourseHomePage(CourseHomePageTestCase):
 
         # Fetch the view and verify the query counts
         # TODO: decrease query count as part of REVO-28
-        # TODO: decrease query count as part of REVEM-106
-        with self.assertNumQueries(88, table_blacklist=QUERY_COUNT_TABLE_BLACKLIST):
+        with self.assertNumQueries(87, table_blacklist=QUERY_COUNT_TABLE_BLACKLIST):
             with check_mongo_calls(4):
                 url = course_home_url(self.course)
                 self.client.get(url)
