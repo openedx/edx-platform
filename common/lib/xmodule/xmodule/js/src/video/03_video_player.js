2ee631f66601f7db90eb48e13aa3931435bf9355
rabiaiftikhar <rabia.iftikhar@arbisoft.com>
2019-01-23 17:15:25 +0500

    EDUCATOR-3930 fix video player speed adjustments

--- common/lib/xmodule/xmodule/js/src/video/03_video_player.js
+++ common/lib/xmodule/xmodule/js/src/video/03_video_player.js
@@ -421,7 +421,7 @@ function(HTML5Video, HTML5HLSVideo, Resizer, HLS, _) {
             );
         }
 
-        newSpeed = parseFloat(newSpeed).toFixed(2).replace(/\.00$/, '.0');
+        newSpeed = parseFloat(newSpeed);
         this.setSpeed(newSpeed);
         this.videoPlayer.setPlaybackRate(newSpeed);
     }
