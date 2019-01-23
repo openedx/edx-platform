78cf70ab1b0974ddeeee966cf5643d1da48ae90c
rabiaiftikhar <rabia.iftikhar@arbisoft.com>
2019-01-23 17:15:25 +0500

    EDUCATOR-3930 fix video player speed adjustments

--- common/lib/xmodule/xmodule/js/spec/video/video_player_spec.js
+++ common/lib/xmodule/xmodule/js/spec/video/video_player_spec.js
@@ -908,9 +908,9 @@ function(VideoPlayer) {
 
                 it('set video speed to the new speed', function() {
                     VideoPlayer.prototype.onSpeedChange.call(state, '0.75', false);
-                    expect(state.setSpeed).toHaveBeenCalledWith('0.75');
+                    expect(state.setSpeed).toHaveBeenCalledWith(0.75);
                     expect(state.videoPlayer.setPlaybackRate)
-                        .toHaveBeenCalledWith('0.75');
+                        .toHaveBeenCalledWith(0.75);
                 });
             });
         });
