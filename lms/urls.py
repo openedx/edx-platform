4fea2ab3a1f361d0052da3a3f66f773aaf47583b
John Baldwin <jlbaldwin@gmail.com>
2019-01-24 11:10:49 -0500

    Updated Tahoe LMS to support Figures (#306)
    * Added conditional inclusion for Figures in lms/urls.py if Figures is
    in the INSTALLLED_APPS
    * Added Figures settings to lms/envs/amc.py and
    lms/envs/devstack_appsembler.py to support Figures
    * Added WEBPACK_LOADER dict declaration in lms/envs/common.py

--- lms/urls.py
+++ lms/urls.py
@@ -980,3 +980,8 @@ urlpatterns += (
         include('openedx.core.djangoapps.appsembler.api.urls',
         namespace='tahoe-api')),
 )
+
+if 'figures' in settings.INSTALLED_APPS:
+    urlpatterns += (
+        url(r'^figures/', include('figures.urls', namespace='figures')),
+    )
