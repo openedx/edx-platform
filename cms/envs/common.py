a32a3add0b4e73e540a416e9f94c72dc412ee96d
sodaling <502464760@qq.com>
2019-02-18 12:16:41 +0800

    2861

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1250,7 +1250,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "util.password_policy_validators.MinimumLengthValidator",
         "OPTIONS": {
-            "min_length": 2
+            "min_length": 6
         }
     },
     {
