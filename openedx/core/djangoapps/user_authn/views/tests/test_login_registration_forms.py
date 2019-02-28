af9d34edfc3abd3fa627b97e3cdfddaae845a615
Adeel Khan <muhammad.adeel@arbisoft.com>
2019-02-28 19:18:39 +0500

    Fix test + xss-lint violations except mako-missing-default

--- openedx/core/djangoapps/user_authn/views/tests/test_login_registration_forms.py
+++ openedx/core/djangoapps/user_authn/views/tests/test_login_registration_forms.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from django.urls import reverse
 from mock import patch
 
+from openedx.core.djangolib.js_utils import js_escaped_string
 from third_party_auth.tests.testutil import ThirdPartyAuthTestMixin
 from util.testing import UrlResetMixin
 from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
@@ -138,13 +139,12 @@ class LoginFormTest(ThirdPartyAuthTestMixin, UrlResetMixin, SharedModuleStoreTes
             ('email_opt_in', opt_in_value),
             ('next', '/custom/final/destination'),
         ]
-
         # Get the login page
         response = self.client.get(self.url, params, HTTP_ACCEPT="text/html")
 
         # Verify that the parameters are sent on to the next page correctly
         post_login_handler = _finish_auth_url(params)
-        js_success_var = u'var nextUrl = "{}";'.format(post_login_handler)
+        js_success_var = 'var nextUrl = "{}";'.format(js_escaped_string(post_login_handler))
         self.assertContains(response, js_success_var)
 
         # Verify that the login link preserves the querystring params
@@ -220,7 +220,11 @@ class RegisterFormTest(ThirdPartyAuthTestMixin, UrlResetMixin, SharedModuleStore
 
         # Verify that the parameters are sent on to the next page correctly
         post_login_handler = _finish_auth_url(params)
+<<<<<<< HEAD
         js_success_var = u'var nextUrl = "{}";'.format(post_login_handler)
+=======
+        js_success_var = 'var nextUrl = "{}";'.format(js_escaped_string(post_login_handler))
+>>>>>>> da73dab... Fix test + xss-lint violations except mako-missing-default
         self.assertContains(response, js_success_var)
 
         # Verify that the login link preserves the querystring params
