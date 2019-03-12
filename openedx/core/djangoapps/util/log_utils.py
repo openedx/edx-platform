e6dada4b5227d8ccf3276e9d53ccb918fe1b4363
Calen Pennington <cale@edx.org>
2019-03-12 14:13:13 -0400

    fixup! Log current userid in all log messages

--- openedx/core/djangoapps/util/log_utils.py
+++ openedx/core/djangoapps/util/log_utils.py
@@ -2,9 +2,10 @@ from logging import Filter
 from crum import get_current_user
 
 class UserIdFilter(Filter):
-    def filter(record):
+    def filter(self, record):
         user = get_current_user()
         if user and user.pk:
             record.userid = user.pk
         else:
             record.userid = None
+        return True
