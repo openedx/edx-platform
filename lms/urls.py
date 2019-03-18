fa20fe7eb7e1dc1569f64a717ec58c2fffbf00e6
lpfaender <laetitia.pfaender@gmail.com>
2019-03-18 14:13:14 +0800

    header/footer in progress

--- lms/urls.py
+++ lms/urls.py
@@ -64,8 +64,7 @@ if settings.DEBUG or settings.FEATURES.get('ENABLE_DJANGO_ADMIN_SITE'):
 
 
 urlpatterns = [
-    # url(r'^$', branding_views.index, name='root'),   # Main marketing page, or redirect to courseware
-    url(r'^$', student_views.student_dashboard, name="root"),
+    url(r'^$', branding_views.index, name='root'),   # Main marketing page, or redirect to courseware
 
     url(r'', include('student.urls')),
     # TODO: Move lms specific student views out of common code
