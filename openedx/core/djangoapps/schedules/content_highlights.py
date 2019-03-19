4d6facaffe0c19b4f38a57ef293915313a51d4e4
Gabe Mulley <gabe@edx.org>
2019-03-19 10:05:18 -0400

    remove extra space

--- openedx/core/djangoapps/schedules/content_highlights.py
+++ openedx/core/djangoapps/schedules/content_highlights.py
@@ -99,7 +99,7 @@ def _get_course_module(course_descriptor, user):
     field_data_cache = FieldDataCache.cache_for_descriptor_descendents(
         course_descriptor.id, user, course_descriptor, depth=1, read_only=True,
     )
-    module =  get_module_for_descriptor(
+    module = get_module_for_descriptor(
         user, request, course_descriptor, field_data_cache, course_descriptor.id, course=course_descriptor,
     )
     if module is None:
