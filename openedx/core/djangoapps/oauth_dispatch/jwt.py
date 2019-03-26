9981b087e234c3fef1a1153f269b47abc8176ee4
Christopher Pappas <cpappas@edx.org>
2019-03-26 12:14:34 -0400

    ENT-1556 - Adding in roles claim to jwt for use with edx rbac
    Adding logic that adds roles to jwt
    
    Quality fixes

--- openedx/core/djangoapps/oauth_dispatch/jwt.py
+++ openedx/core/djangoapps/oauth_dispatch/jwt.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from jwkest import jwk
 from jwkest.jws import JWS
 
+from edx_rbac.utils import create_role_auth_claim_for_user
+
 from edx_django_utils.monitoring import set_custom_metric
 from openedx.core.djangoapps.oauth_dispatch.toggles import ENFORCE_JWT_SCOPES
 from student.models import UserProfile, anonymous_id_for_user
@@ -119,6 +121,9 @@ def _create_jwt(
     }
     payload.update(additional_claims or {})
     _update_from_additional_handlers(payload, user, scopes)
+    role_claims = create_role_auth_claim_for_user(user)
+    if role_claims:
+        payload['roles'] = role_claims
     return _encode_and_sign(payload, use_asymmetric_key, secret)
 
 
