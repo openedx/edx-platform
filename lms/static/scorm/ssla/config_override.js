b990103cdfb912e9bfd7954eb429cafdc03fb691
Moeez Zahid <moeezzahid1996@gmail.com>
2019-03-29 10:27:42 +0500

    MCKIN-9825 MCKIN-10468 Ssla: Configure popup fail message (#1442)
    * MCKIN-9825 MCKIN-10468 Ssla: configure popup failure message

--- lms/static/scorm/ssla/config_override.js
+++ lms/static/scorm/ssla/config_override.js
@@ -21,9 +21,7 @@ var sslaConfig = {
     popupMainContentMessageAfterOpen: function() {
         return '';
     },
-    popupMainContentMessageFailed: function() {
-        return '<div style="background-color:rgb(250,250,250); width: 100%; height: 100%; display: table;"> <p style="font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; color: #000000; padding-top: 54px;">It looks like your browser settings has pop-ups disabled. <br>The content takes place in a new window.</p> <br><br> <button onclick="parent.ssla.ssla.popupManually();" style="background-color: #3385C7; color: white; padding: 1rem 2rem; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; border-width: 0; font-weight: 700; border-color: #2s6a9f; border-radius: 5px;">Launch pop-up to continue</button></div>';
-    },
+    popupMainContentMessageFailed: getPopupLaunchFailedMessage,
     popupWindowParams: "status=1,toolbar=1,scrollbars=yes,resizable=yes,alwaysRaised=1"
 };
 
@@ -158,4 +156,19 @@ function closePopupMultiSco() {
 
 function handlePopupClosed() {
     parent.document.handleScormPopupClosed()
+}
+
+function getPopupLaunchFailedMessage() {
+  const firstMessageText = parent.gettext('It looks like your browser settings has pop-ups disabled.');
+  const secondMessageText = parent.gettext('The content takes place in a new window.');
+  const buttonTitle = parent.gettext('Launch pop-up to continue');
+  const baseHTML = '<div style="background-color:rgb(250,250,250); width: 100%; height: 100%; display: table;"> <p style="font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; color: #000000; padding-top: 54px;">' + firstMessageText + ' <br>' + secondMessageText + '</p> <br><br> '
+
+  if (parent.$("body").hasClass("new-theme")) {
+    const newUIStyle = '.new-theme.button {\nfont-weight: 600;\nfont-size: 12px;\nheight: 40px;\nborder-radius: 2px;\ntext-transform: uppercase;\nline-height: 1.83;\nletter-spacing: 0.5px;\ntext-align: center;\npadding: 0 20px;\nbackground-color: #1c3bce;\nborder: 1px solid #1c3bce;\ntransition: .35s;\ncolor: white;cursor:pointer\n}\n.button:hover {\nbackground: #0b0886;\ncolor: white;\n}\n}'
+    return '<head><style>' + newUIStyle + '</style></head><body>'+ baseHTML + '<button onclick="parent.ssla.ssla.popupManually();" style="" class="new-theme button">' + buttonTitle + '</button></div></body>';
+  }
+  else {
+    return baseHTML + '<button onclick="parent.ssla.ssla.popupManually();" style="background-color: #3385C7; color: white; padding: 1rem 2rem; font-family: \'Open Sans\', Arial, sans-serif; font-size: 14px; border-width: 0; font-weight: 700; border-color: #2s6a9f; border-radius: 5px;">' + buttonTitle + '</button></div>';
+  }
 }
