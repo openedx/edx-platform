709a7fb8a9adf74683bc714404f6c459194b7693
melvinsoft <melvinsoft@gmail.com>
2019-03-29 14:28:58 -0300

    Create app config for Tahoe registration API

---
+++ openedx/core/djangoapps/appsembler/api/apps.py
@@ -0,0 +1,12 @@
+"""
+Configuration for the openedx.core.djangoapps.appsembler.api Django app.
+"""
+from django.apps import AppConfig
+
+
+class TahoeAPIConfig(AppConfig):
+    """
+    Configuration class for the Tahoe API Django app.
+    """
+    name = 'openedx.core.djangoapps.appsembler.api'
+    label = 'tahoe.api'
