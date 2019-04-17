3deebd7e27128630b004492eecef0040adff2186
weijie <wejie00@foxmail.com>
2019-04-17 17:50:28 +0800

    fix clear cookie by domain

--- lms/static/js/student_account/components/removeLoggedInCookies.js
+++ lms/static/js/student_account/components/removeLoggedInCookies.js
@@ -1,9 +1,9 @@
 import cookie from 'js-cookie';
 
 const removeLoggedInCookies = () => {
-  cookie.remove('edxloggedin');
-  cookie.remove('csrftoken');
-  cookie.remove('edx-user-info');
+  cookie.remove('edxloggedin', window.domainName);
+  cookie.remove('csrftoken', window.domainName);
+  cookie.remove('edx-user-info', window.domainName);
 };
 
 export default removeLoggedInCookies;
