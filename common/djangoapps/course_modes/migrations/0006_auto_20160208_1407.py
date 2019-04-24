f88061a3b3626b8e07c6f531764b5165dd624974
Constanza Abarca <costi.abarca@gmail.com>
2019-04-24 15:14:34 -0400

    ran isort

--- common/djangoapps/course_modes/migrations/0006_auto_20160208_1407.py
+++ common/djangoapps/course_modes/migrations/0006_auto_20160208_1407.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
 from django.db import migrations, models
 
 
