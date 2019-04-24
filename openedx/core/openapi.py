59854804092b52ada61f20014e77d0549d990ec1
Ned Batchelder <ned@edx.org>
2019-04-24 15:46:50 -0400

    Add drf-yasg
    * Install drf-yasg
    
    * Add drf-yasg settings and urls
    
    * Pin drf to make drf-yasg work
    
    * Adjust config-models version to be compatible
    
    * Remove django-rest-swagger (the old way)

---
+++ openedx/core/openapi.py
@@ -0,0 +1,20 @@
+"""
+Open API support.
+"""
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
