e03a96037b0df502dfe8b792b61607def63a50c2
bmedx <bmesick@edx.org>
2019-05-01 13:35:19 -0400

    Mock CourseEnrollment history for test performance

--- pavelib/utils/test/suites/python_suite.py
+++ pavelib/utils/test/suites/python_suite.py
@@ -19,6 +19,7 @@ class PythonTestSuite(TestSuite):
         super(PythonTestSuite, self).__init__(*args, **kwargs)
         self.opts = kwargs
         self.disable_migrations = kwargs.get('disable_migrations', True)
+        self.disable_courseenrollment_history = kwargs.get('disable_courseenrollment_history', True)
         self.fasttest = kwargs.get('fasttest', False)
         self.subsuites = kwargs.get('subsuites', self._default_subsuites)
 
@@ -27,6 +28,9 @@ class PythonTestSuite(TestSuite):
 
         if self.disable_migrations:
             os.environ['DISABLE_MIGRATIONS'] = '1'
+
+        if self.disable_courseenrollment_history:
+            os.environ['DISABLE_COURSEENROLLMENT_HISTORY'] = '1'
 
         if not (self.fasttest or self.skip_clean):
             test_utils.clean_test_files()
