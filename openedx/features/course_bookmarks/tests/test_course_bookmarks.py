b222e0aafa198f4d58e30dc085936fd6da254b0c
root <root@lms.devstack.edx>
2019-05-06 15:02:07 +0000

    INCR-228: run python-modernize against course_bookmarks and portfolio_project

--- openedx/features/course_bookmarks/tests/test_course_bookmarks.py
+++ openedx/features/course_bookmarks/tests/test_course_bookmarks.py
@@ -2,9 +2,11 @@
 Unit tests for the course bookmarks feature.
 """
 
-import ddt
+from __future__ import absolute_import
 
+import ddt
 from django.test import RequestFactory
+
 from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.tests.django_utils import CourseUserType, SharedModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
