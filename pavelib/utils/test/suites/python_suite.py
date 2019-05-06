f554a35d2f996742e8e412771eea669bd25519ef
Michael Youngstrom <youngstrom.m@husky.neu.edu>
2019-05-06 11:24:27 -0400

    silly debugging

--- pavelib/utils/test/suites/python_suite.py
+++ pavelib/utils/test/suites/python_suite.py
@@ -30,8 +30,9 @@ class PythonTestSuite(TestSuite):
             os.environ['DISABLE_MIGRATIONS'] = '1'
 
         if self.disable_courseenrollment_history:
+            print("HELLO MIKE!!!!")
             os.environ['DISABLE_COURSEENROLLMENT_HISTORY'] = '1'
-
+        print(os.envrion['DISABLE_COURSEENROLLMENT_HISTORY'])
         if not (self.fasttest or self.skip_clean):
             test_utils.clean_test_files()
 
