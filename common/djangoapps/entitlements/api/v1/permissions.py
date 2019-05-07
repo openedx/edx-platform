bf2d93d36e2c78bf6a0d2273aec17e75b27e6bd2
Anthony Wee <antxwee@gmail.com>
2019-05-07 11:18:11 -0700

    INCR-131: Run python-modernize on common/djangoapps/entitlements/api

--- common/djangoapps/entitlements/api/v1/permissions.py
+++ common/djangoapps/entitlements/api/v1/permissions.py
@@ -3,6 +3,7 @@ This module provides a custom DRF Permission class for supporting SAFE_METHODS t
 requiring Superuser access for all other Request types on an API endpoint.
 """
 
+from __future__ import absolute_import
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 from courseware.access import has_access
--- common/djangoapps/entitlements/api/v1/urls.py
+++ common/djangoapps/entitlements/api/v1/urls.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
