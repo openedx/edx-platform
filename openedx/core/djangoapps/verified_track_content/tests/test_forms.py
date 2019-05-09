a74cddefee8b8fd864620d74d1d0d2b4e4600a69
Amit <43564590+amitvadhel@users.noreply.github.com>
2019-05-09 22:00:06 +0300

    INCR-160 (#20490)
    * INCR-160: Run python-modernize on openedx/core/djangoapps/verified_track_content
    
    * INCR-160: [FIX] line over length limit, datetime - pytz import

--- openedx/core/djangoapps/verified_track_content/tests/test_forms.py
+++ openedx/core/djangoapps/verified_track_content/tests/test_forms.py
@@ -1,10 +1,12 @@
 """
 Test for forms helpers.
 """
+from __future__ import absolute_import
 from xmodule.modulestore.tests.factories import CourseFactory
 from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
 
 from openedx.core.djangoapps.verified_track_content.forms import VerifiedTrackCourseForm
+import six
 
 
 class TestVerifiedTrackCourseForm(SharedModuleStoreTestCase):
@@ -22,7 +24,7 @@ class TestVerifiedTrackCourseForm(SharedModuleStoreTestCase):
 
     def test_form_validation_success(self):
         form_data = {
-            'course_key': unicode(self.course.id), 'verified_cohort_name': 'Verified Learners', 'enabled': True
+            'course_key': six.text_type(self.course.id), 'verified_cohort_name': 'Verified Learners', 'enabled': True
         }
         form = VerifiedTrackCourseForm(data=form_data)
         self.assertTrue(form.is_valid())
