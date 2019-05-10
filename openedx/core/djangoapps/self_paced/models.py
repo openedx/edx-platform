21feb3d1228bf95558807ef48cb03d08f29c7d05
amitvadhel <amit.vadhel@ilearn.ws>
2019-05-10 11:36:50 +0300

    INCR-149: Groups to absolute_import and unicode_literals

--- openedx/core/djangoapps/self_paced/models.py
+++ openedx/core/djangoapps/self_paced/models.py
@@ -3,6 +3,7 @@ Configuration for self-paced courses.
 """
 
 from __future__ import absolute_import
+
 from config_models.models import ConfigurationModel
 from django.db.models import BooleanField
 from django.utils.translation import ugettext_lazy as _
