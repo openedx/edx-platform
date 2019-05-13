b218e912f55101c033c4aa4b679c5322af392dd8
amitvadhel <amit.vadhel@ilearn.ws>
2019-05-13 18:34:12 +0300

    INCR-249: Remove the duplicate lines for absolute_import

--- openedx/core/djangoapps/content/block_structure/migrations/0003_blockstructuremodel_storage.py
+++ openedx/core/djangoapps/content/block_structure/migrations/0003_blockstructuremodel_storage.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from __future__ import absolute_import
 from django.db import migrations, models
 import openedx.core.djangoapps.content.block_structure.models
 
