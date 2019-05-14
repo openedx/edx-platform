064365fb2d4d71515bf5c4cbb20e683165f63d20
amitvadhel <amit.vadhel@ilearn.ws>
2019-05-14 10:53:56 +0300

    INCR-244: Update module docstring

--- common/test/acceptance/pages/common/__init__.py
+++ common/test/acceptance/pages/common/__init__.py
@@ -1,5 +1,5 @@
 """
-Get the URL of the instance under test
+Package of common page objects for acceptance tests
 """
 from __future__ import absolute_import
 
@@ -9,6 +9,7 @@ HOSTNAME = os.environ.get('BOK_CHOY_HOSTNAME', 'localhost')
 CMS_PORT = os.environ.get('BOK_CHOY_CMS_PORT', 8031)
 LMS_PORT = os.environ.get('BOK_CHOY_LMS_PORT', 8003)
 
+# Get the URL of the instance under test
 BASE_URL = os.environ.get('test_url', 'http://{}:{}'.format(HOSTNAME, LMS_PORT))
 
 # The URL used for user auth in testing
