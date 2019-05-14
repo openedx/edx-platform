7e34d92f24474bbea2b78a254f7f1266a77e86ca
Stuart Young <syoung@edx.org>
2019-05-14 15:26:54 -0400

    run isort

--- lms/djangoapps/instructor_task/management/commands/fail_old_tasks.py
+++ lms/djangoapps/instructor_task/management/commands/fail_old_tasks.py
@@ -1,6 +1,5 @@
-from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, print_function, unicode_literals
 
-from __future__ import absolute_import
 from datetime import datetime
 
 from celery.states import FAILURE
--- lms/djangoapps/instructor_task/management/commands/tests/test_fail_old_tasks.py
+++ lms/djangoapps/instructor_task/management/commands/tests/test_fail_old_tasks.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+
 from datetime import datetime
 
 import ddt
