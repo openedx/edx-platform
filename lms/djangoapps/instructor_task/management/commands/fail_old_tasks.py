9c08ac85f0d2fe621a30472338b209b96ed3aeb6
Stuart Young <syoung@edx.org>
2019-05-14 15:26:49 -0400

    run python modernize

--- lms/djangoapps/instructor_task/management/commands/fail_old_tasks.py
+++ lms/djangoapps/instructor_task/management/commands/fail_old_tasks.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 
+from __future__ import absolute_import
 from datetime import datetime
 
 from celery.states import FAILURE
--- lms/djangoapps/instructor_task/management/commands/tests/test_fail_old_tasks.py
+++ lms/djangoapps/instructor_task/management/commands/tests/test_fail_old_tasks.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from datetime import datetime
 
 import ddt
