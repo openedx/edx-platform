ec388a70b958d454066cf2f2a94c22d4249c2ca0
Anders Pearson <anders@thraxil.org>
2019-05-22 15:05:29 +0100

    add management command to force save all sites
    This is required after an upgrade to fix some SASS issues and update
    S3 bucket URLs, etc.
    
    Originally from Omar in slack:
    
    https://appsembler.slack.com/archives/CCT7XSP9D/p1557906862335700

---
+++ openedx/core/djangoapps/appsembler/sites/management/commands/save_all_sites.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
+
+
+class Command(BaseCommand):
+    help = "Force save on all sites"
+
+    def handle(self, *args, **options):
+        if not settings.ROOT_URLCONF == 'lms.urls':
+            raise CommandError('This command can only be run with the lms settings')
+        for sc in SiteConfiguration.objects.all():
+            print('On:', sc.site.domain)
+            try:
+                sc.save()
+                print('OK')
+            except Exception as e:
+                print('Error')
+                print(e)
