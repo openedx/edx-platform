d896cc9f1cc90b7daf67fa97c2ebc65b5223632c
Ned Batchelder <ned@edx.org>
2019-05-28 13:08:27 -0400

    yasg settings

--- openedx/core/openapi.py
+++ openedx/core/openapi.py
@@ -8,12 +8,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        title="Open edX API",
+        default_version="v1",
+        description="APIs for access to Open edX information",
+        #terms_of_service="https://www.google.com/policies/terms/",         # TODO: Do we have these?
+        contact=openapi.Contact(email="oscm@edx.org"),
+        #license=openapi.License(name="BSD License"),                       # TODO: What does this mean?
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
