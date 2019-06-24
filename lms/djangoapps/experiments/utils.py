b8401beaae9577f78b24802e111f9590d5c32878
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-06-24 08:56:05 -0400

    REVEM-374 Remove course prices from dashboard (#20856)

--- lms/djangoapps/experiments/utils.py
+++ lms/djangoapps/experiments/utils.py
@@ -389,18 +389,6 @@ def get_program_context(course, user_enrollments, audit_enrollments):
 # TODO: clean up as part of REVEM-199 (START)
 
 
-# TODO START: Clean up REVEM-205
-def get_experiment_dashboard_metadata_context(enrollments):
-    """
-    Given a list of enrollments return a dict of course ids with their prices.
-    Utility function for experimental metadata. See experiments/dashboard_metadata.html.
-    :param enrollments:
-    :return: dict of courses: course price for dashboard metadata
-    """
-    return {str(enrollment.course): enrollment.course_price for enrollment in enrollments}
-# TODO END: Clean up REVEM-205
-
-
 def stable_bucketing_hash_group(group_name, group_count, username):
     """
     Return the bucket that a user should be in for a given stable bucketing assignment.
