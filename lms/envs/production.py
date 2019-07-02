c2acf6752d3c00caf421d610a0f4d2b73d505e5f
amitvadhel <amit.vadhel@ilearn.ws>
2019-07-02 19:02:56 +0300

    INCR-483: Fixes useless suppression

--- lms/envs/production.py
+++ lms/envs/production.py
@@ -30,8 +30,8 @@ from django.core.exceptions import ImproperlyConfigured
 from path import Path as path
 
 from openedx.core.djangoapps.plugins import plugin_settings, constants as plugin_constants
-from openedx.core.lib.derived import derive_settings  # pylint: disable=wrong-import-order
-from openedx.core.lib.logsettings import get_logger_config  # pylint: disable=wrong-import-order
+from openedx.core.lib.derived import derive_settings
+from openedx.core.lib.logsettings import get_logger_config
 from xmodule.modulestore.modulestore_settings import convert_module_store_setting_if_needed
 
 from .common import *
