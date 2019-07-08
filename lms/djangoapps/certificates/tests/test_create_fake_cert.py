6585f50f0fb07f0608a53a214bc72d8cf84591f3
amitvadhel <amit.vadhel@ilearn.ws>
2019-07-08 15:03:45 +0300

    INCR-469: Updates on Python 3.x, disable: import-error and fix line over length

--- lms/djangoapps/certificates/tests/test_create_fake_cert.py
+++ lms/djangoapps/certificates/tests/test_create_fake_cert.py
@@ -1,5 +1,7 @@
 """Tests for the create_fake_certs management command. """
 
+from __future__ import absolute_import
+
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.test import TestCase
