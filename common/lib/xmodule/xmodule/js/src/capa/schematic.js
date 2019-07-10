2596b676647f3b6c0a9d71650424931b1e495e09
Edward Zarecor <ed@edx.org>
2019-07-10 10:07:52 -0400

    updating to ensureHTML

--- common/lib/xmodule/xmodule/js/src/capa/schematic.js
+++ common/lib/xmodule/xmodule/js/src/capa/schematic.js
@@ -5103,7 +5103,7 @@ schematic = (function() {
 	    {
 		offset_x: this.offset_x,
 		offset_y: this.offset_y,
-		parent: edx.HtmlUtils.HTML(this.parent.toString())
+		parent: edx.HtmlUtils.ensureHTML(this.parent.toString())
 	    });
 	}
 
