209e055ec81acaf498a47bc0b4c037db269df8c3
Diana Huang <dkh@edx.org>
2019-07-10 10:02:54 -0400

    Exclude cookie test from CMS tests.

--- openedx/core/djangoapps/site_configuration/tests/test_middleware.py
+++ openedx/core/djangoapps/site_configuration/tests/test_middleware.py
@@ -19,6 +19,7 @@ from openedx.core.djangolib.testing.utils import skip_unless_lms
 # NOTE: We set SESSION_SAVE_EVERY_REQUEST to True in order to make sure
 # Sessions are always started on every request
 @override_settings(SESSION_SAVE_EVERY_REQUEST=True)
+@skip_unless_lms
 class SessionCookieDomainTests(TestCase):
     """
     Tests regarding the session cookie management in the middleware for site_configuration
@@ -52,6 +53,60 @@ class SessionCookieDomainTests(TestCase):
         response = self.client.get('/')
         self.assertNotIn('test_site.localhost', str(response.cookies['sessionid']))
         self.assertNotIn('Domain', str(response.cookies['sessionid']))
+
+    @ddt.data(*MICROSITE_BACKENDS)
+    def test_session_cookie_domain_with_microsite_override(self, site_backend):
+        """
+        Makes sure that the cookie being set in a Microsite
+        is the one specially overridden in configuration
+        """
+        with patch('microsite_configuration.microsite.BACKEND',
+                   get_backend(site_backend, BaseMicrositeBackend)):
+            response = self.client.get('/', HTTP_HOST=settings.MICROSITE_TEST_HOSTNAME)
+            self.assertIn('test_site.localhost', str(response.cookies['sessionid']))
+
+    @ddt.data(*MICROSITE_BACKENDS)
+    def test_microsite_none_cookie_domain(self, site_backend):
+        """
+        Tests to make sure that a Microsite that specifies None for 'SESSION_COOKIE_DOMAIN' does not
+        set a domain on the session cookie
+        """
+        with patch('microsite_configuration.microsite.get_value') as mock_get_value:
+            mock_get_value.side_effect = side_effect_for_get_value('SESSION_COOKIE_DOMAIN', None)
+            with patch('microsite_configuration.microsite.BACKEND',
+                       get_backend(site_backend, BaseMicrositeBackend)):
+                response = self.client.get('/', HTTP_HOST=settings.MICROSITE_TEST_HOSTNAME)
+                self.assertNotIn('test_site.localhost', str(response.cookies['sessionid']))
+                self.assertNotIn('Domain', str(response.cookies['sessionid']))
+
+
+# NOTE: We set SESSION_SAVE_EVERY_REQUEST to True in order to make sure
+# Sessions are always started on every request
+@override_settings(SESSION_SAVE_EVERY_REQUEST=True)
+@skip_unless_lms
+class SessionCookieDomainSiteConfigurationOverrideTests(TestCase):
+    """
+    Tests regarding the session cookie management in the middlware for Microsites
+    """
+
+    def setUp(self):
+        super(SessionCookieDomainSiteConfigurationOverrideTests, self).setUp()
+        # Create a test client, and log it in so that it will save some session data.
+        self.user = UserFactory.create()
+        self.user.set_password('password')
+        self.user.save()
+        self.site = SiteFactory.create(
+            domain='testserver.fake',
+            name='testserver.fake'
+        )
+        self.site_configuration = SiteConfigurationFactory.create(
+            site=self.site,
+            values={
+                "SESSION_COOKIE_DOMAIN": self.site.domain,
+            }
+        )
+        self.client = Client()
+        self.client.login(username=self.user.username, password="password")
 
     def test_session_cookie_domain_with_site_configuration_override(self):
         """
