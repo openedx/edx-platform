1f5ee8792e149489f7d48fc7059b88b1400b40d9
Muhammad Zeshan Arif <>
2019-07-10 13:34:32 +0500

    Move core change 46 to 47 in Ironwood

--- lms/djangoapps/instructor/enrollment.py
+++ lms/djangoapps/instructor/enrollment.py
@@ -53,6 +53,8 @@ from track.event_transaction_utils import (
 )
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
+from openedx.features.course_card.helpers import get_related_card_id
+from common.lib.mandrill_client.client import MandrillClient
 
 log = logging.getLogger(__name__)
 
@@ -160,7 +162,7 @@ def enroll_email(course_id, student_email, auto_enroll=False, email_students=Fal
             email_params['message_type'] = 'enrolled_enroll'
             email_params['email_address'] = student_email
             email_params['full_name'] = previous_state.full_name
-            send_mail_to_student(student_email, email_params, language=language)
+            # send_mail_to_student(student_email, email_params, language=language)
 
     elif not is_email_retired(student_email):
         cea, _ = CourseEnrollmentAllowed.objects.get_or_create(course_id=course_id, email=student_email)
@@ -169,7 +171,19 @@ def enroll_email(course_id, student_email, auto_enroll=False, email_students=Fal
         if email_students:
             email_params['message_type'] = 'allowed_enroll'
             email_params['email_address'] = student_email
-            send_mail_to_student(student_email, email_params, language=language)
+
+            email_context = {
+                'course_name': email_params['display_name'],
+                'site_name': email_params['site_name'],
+                'registration_url': email_params['registration_url'],
+                'email_address': student_email,
+            }
+
+            MandrillClient().send_mail(
+                MandrillClient.COURSE_INVITATION_ONLY_REGISTER_TEMPLATE,
+                student_email,
+                email_context
+            )
 
     after_state = EmailEnrollmentState(course_id, student_email)
 
@@ -375,6 +389,11 @@ def get_email_params(course, auto_enroll, secure=True, course_key=None, display_
 
     protocol = 'https' if secure else 'http'
     course_key = course_key or text_type(course.id)
+    if course.invitation_only:
+        course_about_key = get_related_card_id(course.id)
+    else:
+        course_about_key = course_key
+
     display_name = display_name or course.display_name_with_default_escaped
 
     stripped_site_name = configuration_helpers.get_value(
@@ -400,7 +419,7 @@ def get_email_params(course, auto_enroll, secure=True, course_key=None, display_
         course_about_url = u'{proto}://{site}{path}'.format(
             proto=protocol,
             site=stripped_site_name,
-            path=reverse('about_course', kwargs={'course_id': course_key})
+            path=reverse('about_course', kwargs={'course_id': course_about_key})
         )
 
     is_shib_course = uses_shib(course)
