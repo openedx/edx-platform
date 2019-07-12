892ca0611b0de1848a65fa02306d91dea19336e9
Ayub khan <muhammadayubkhan6@gmail.com>
2019-07-12 14:58:11 +0500

    INCR-436 python3 compatibility

--- lms/djangoapps/grades/migrations/0001_initial.py
+++ lms/djangoapps/grades/migrations/0001_initial.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-from django.db import migrations, models
-import coursewarehistoryextended.fields
 import django.utils.timezone
 import model_utils.fields
+from django.db import migrations, models
 from opaque_keys.edx.django.models import CourseKeyField, UsageKeyField
+
+import coursewarehistoryextended.fields
 
 
 class Migration(migrations.Migration):
