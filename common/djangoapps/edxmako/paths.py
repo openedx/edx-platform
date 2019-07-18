9bf860852dc53f4644ad928a1d1be02f11bd635c
Feanil Patel <feanil@edx.org>
2019-07-18 14:53:51 -0400

    md5 needs bytes in python 3 but a sting is fine in python2.

--- common/djangoapps/edxmako/paths.py
+++ common/djangoapps/edxmako/paths.py
@@ -55,7 +55,7 @@ class DynamicTemplateLookup(TemplateLookup):
         # and "foo.html.py" in the module directory has no way to know that.
         # Update the module_directory argument to point to a directory
         # specifically for this lookup path.
-        unique = hashlib.md5(":".join(str(d) for d in self.directories)).hexdigest()
+        unique = hashlib.md5(six.b(":".join(str(d) for d in self.directories))).hexdigest()
         self.template_args['module_directory'] = os.path.join(self.__original_module_directory, unique)
 
         # Also clear the internal caches. Ick.
