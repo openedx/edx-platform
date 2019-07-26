b7d4f3076985ecc8e6d27e7d3cc921531d997b8e
Muhammad Zaid Bamber <42027217+zaidbamber161@users.noreply.github.com>
2019-07-26 11:42:15 +0500

    Revert "Fixed CDN urls to Static urls on edit."

--- common/static/js/spec/utility_spec.js
+++ common/static/js/spec/utility_spec.js
@@ -16,25 +16,3 @@ describe('utility.rewriteStaticLinks', function() {
         ).toBe('<img src="http://www.mysite.org/static/foo.x"/>');
     });
 });
-describe('utility.rewriteCdnLinksToStatic', function() {
-    'use strict';
-    it('does not replace "url" to "static url" if url is not following "cdn url" pattern', function() {
-        expect(
-            rewriteCdnLinksToStatic( // eslint-disable-line no-undef
-            '<img src="/asset-v1:UCentralLah+Cs201+2019_1+type@asset+block/foo.x"/>')
-        ).toBe('<img src="/asset-v1:UCentralLah+Cs201+2019_1+type@asset+block/foo.x"/>');
-        expect(
-            rewriteCdnLinksToStatic('<img src="/assets/foo.x"/>') // eslint-disable-line no-undef
-        ).toBe('<img src="/assets/foo.x"/>');
-    });
-    it('does a replace of "cdn url" to "static url" if url is part of absolute url', function() {
-        expect(
-            rewriteCdnLinksToStatic( // eslint-disable-line no-undef
-              '<img src="//prod-edxapp.edx-cdn.org/assets/foo.x"/>')
-        ).toBe('<img src="/static/foo.x"/>');
-        expect(
-          rewriteCdnLinksToStatic( // eslint-disable-line no-undef
-            '<img src="https://prod-edxapp.edx-cdn.org/assets/foo.x"/>')
-        ).toBe('<img src="/static/foo.x"/>');
-    });
-});
