213eafd822507a38d1b425d42c355723605d7e8e
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-08-01 12:16:51 -0400

    REVMI-354 REVMI-356 Revert more masquerade code

--- lms/djangoapps/courseware/permissions.py
+++ lms/djangoapps/courseware/permissions.py
@@ -3,12 +3,10 @@ Permission definitions for the courseware djangoapp
 """
 
 from bridgekeeper import perms
-from .rules import HasAccessRule, HasStaffAccessToContent
+from .rules import HasAccessRule
 
-MASQUERADE_AS_STUDENT = 'courseware.masquerade_as_student'
 VIEW_COURSE_HOME = 'courseware.view_course_home'
 VIEW_COURSEWARE = 'courseware.view_courseware'
 
-perms[MASQUERADE_AS_STUDENT] = HasStaffAccessToContent()
 perms[VIEW_COURSE_HOME] = HasAccessRule('load')
 perms[VIEW_COURSEWARE] = HasAccessRule('load')
