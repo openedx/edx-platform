c146f82e8440622e7584910a93d614fa1bdb1c85
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-08-01 13:17:52 -0400

    REVMI-354 REVMI-356 Revert more masquerade code (#21278)
    * REVMI-354 REVMI-356 Revert more masquerade code
    
    * Fix quality

--- lms/djangoapps/courseware/rules.py
+++ lms/djangoapps/courseware/rules.py
@@ -3,26 +3,15 @@ django-rules and Bridgekeeper rules for courseware related features
 """
 from __future__ import absolute_import
 
-import logging
-
-import laboratory
-import rules
-from bridgekeeper.rules import Rule, EMPTY
-from course_modes.models import CourseMode
-from django.conf import settings
+from bridgekeeper.rules import Rule
 from django.db.models import Q
-from opaque_keys.edx.keys import CourseKey, UsageKey
-from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from student.models import CourseEnrollment, CourseAccessRole
-from xblock.core import XBlock
-from xmodule.course_module import CourseDescriptor
-from xmodule.error_module import ErrorDescriptor
-from xmodule.x_module import XModule
+from opaque_keys.edx.keys import CourseKey
+
+import rules
+from course_modes.models import CourseMode
+from student.models import CourseEnrollment
 
 from .access import has_access
-
-
-LOG = logging.getLogger(__name__)
 
 
 @rules.predicate
@@ -57,92 +46,3 @@ class HasAccessRule(Rule):
         # that is used to determine if the rule should allow a user
         # into django admin
         return Q(pk__in=[])
-
-
-class StaffAccessExperiment(laboratory.Experiment):
-    def compare(self, control, candidate):
-        return bool(control) == candidate
-
-    def publish(self, result):
-        if not result.match:
-            LOG.warning(
-                u"StaffAccessExperiment: control=%r, candidate=%r",
-                result.control,
-                result.candidates[0],
-                exc_info=True
-            )
-
-
-class HasStaffAccessToContent(Rule):
-    """
-    Check whether a user has `staff` access in a course.
-
-    Expects to be used to filter a CourseOverview queryset
-    """
-    def check(self, user, instance=None):
-        """
-        Return True if the supplied user has staff-level access to the supplied content.
-        """
-        staff_sql_experiment = StaffAccessExperiment(
-            raise_on_mismatch=settings.DEBUG,
-            context={'userid': user.id, 'instance': repr(instance)}
-        )
-        staff_sql_experiment.control(self._check_with_has_access, args=(user, instance))
-        staff_sql_experiment.candidate(self._check_with_query, args=(user, instance))
-        return staff_sql_experiment.conduct()
-
-    def _check_with_has_access(self, user, instance=None):
-        return has_access(user, 'staff', instance)
-
-    def _check_with_query(self, user, instance=None):
-        """
-        Use the query method to check whether a single user has access to the supplied object.
-        """
-        # delegate the work to type-specific functions.
-        # (start with more specific types, then get more general)
-        if isinstance(instance, (CourseDescriptor, CourseOverview)):
-            course_key = instance.id
-        elif isinstance(instance, (ErrorDescriptor, XModule, XBlock)):
-            course_key = instance.scope_ids.usage_id.course_key
-        elif isinstance(instance, CourseKey):
-            course_key = instance
-        elif isinstance(instance, UsageKey):
-            course_key = instance.course_key
-        elif isinstance(instance, basestring):
-            course_key = CourseKey.from_string(instance)
-
-        return self.filter(user, CourseOverview.objects.filter(id=course_key)).exists()
-
-    def query(self, user):
-        """
-        Returns a Q object that expects to be used to filter CourseOverview queries.
-        """
-        if not user.is_authenticated:
-            return EMPTY
-
-        masq_settings = getattr(user, 'masquerade_settings', {})
-        masq_as_student = [
-            course_key for
-            (course_key, masq_setting) in masq_settings.items()
-            if masq_setting.role == 'student'
-        ]
-
-        not_masquerading_as_student = ~Q(id__in=masq_as_student)
-
-        is_global_staff = user.is_staff
-        course_staff_or_instructor_courses = CourseAccessRole.objects.filter(
-            user=user,
-            role__in=('staff', 'instructor'),
-            course_id__isnull=False
-        ).values('course_id')
-        org_staff_or_instructor_courses = CourseAccessRole.objects.filter(
-            user=user,
-            role__in=('staff', 'instructor'),
-            course_key__isnull=True,
-            org__isnull=False
-        ).values('org')
-
-        query = not_masquerading_as_student
-        if not is_global_staff:
-            query &= Q(id__in=course_staff_or_instructor_courses) | Q(org__in=org_staff_or_instructor_courses)
-        return query
