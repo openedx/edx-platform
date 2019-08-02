beb9de58beff8b74cb033af68b2e57afaff64f59
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2019-08-02 17:51:05 +0500

    ADD: add setting to common.py and CMS

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -634,6 +634,7 @@ DEBUG = False
 SESSION_COOKIE_SECURE = False
 SESSION_SAVE_EVERY_REQUEST = False
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+SESSION_COOKIE_AGE = 86400
 
 
 # Site info
