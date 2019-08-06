379874d4a79a1223051985a7e16c9a5bbe365e7f
Alex Dusenbery <adusenbery@edx.org>
2019-08-06 07:38:32 -0400

    Add an index from program type to program UUID in the catalog cache.

--- openedx/core/djangoapps/catalog/cache.py
+++ openedx/core/djangoapps/catalog/cache.py
@@ -12,3 +12,9 @@ SITE_PATHWAY_IDS_CACHE_KEY_TPL = 'pathway-ids-{domain}'
 
 # Template used to create cache keys for individual courses to program uuids.
 COURSE_PROGRAMS_CACHE_KEY_TPL = 'course-programs-{course_run_id}'
+
+# Site-aware cache key template used to locate an item containing
+# a list of all program UUIDs with a certain program type (the Site is required
+# because program_type values are likely to be shared between different sites
+# that live in the same environment).
+PROGRAMS_BY_TYPE_CACHE_KEY_TPL = 'programs-by-type-{site_id}-{program_type}'
