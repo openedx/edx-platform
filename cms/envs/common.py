adb2c672e4d17cc1c42bdc206a0051e0fa16b5be
Zainab Amir <40633976+zainab-amir@users.noreply.github.com>
2019-08-08 10:50:40 +0500

    Fix Unregistered Task (#21297)
    Fix Unregistered Task
    
    openedx.core.djangoapps.bookmarks.tasks.update_xblock_cache task is not
    getting autodiscovered by celery and needs to be imported explicitly
    
    PROD-284

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1065,7 +1065,10 @@ WEBPACK_CONFIG_PATH = 'webpack.prod.config.js'
 ################################# CELERY ######################################
 
 # Auto discover tasks fails to detect contentstore tasks
-CELERY_IMPORTS = ('cms.djangoapps.contentstore.tasks')
+CELERY_IMPORTS = (
+    'cms.djangoapps.contentstore.tasks',
+    'openedx.core.djangoapps.bookmarks.tasks'
+)
 
 # Message configuration
 
