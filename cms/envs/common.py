6b781070814aa92669d20b9760def1dc82723df9
Zainab Amir <40633976+zainab-amir@users.noreply.github.com>
2019-08-08 11:32:17 +0500

    Fix unregistered celery task (#21305)
    openedx.core.djangoapps.ccxcon.tasks.update_ccxcon is not get auto
    discovered by celery. Adding it to CELERY_IMPORTS to be explicitly
    added.
    
    PROD-283

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1067,7 +1067,8 @@ WEBPACK_CONFIG_PATH = 'webpack.prod.config.js'
 # Auto discover tasks fails to detect contentstore tasks
 CELERY_IMPORTS = (
     'cms.djangoapps.contentstore.tasks',
-    'openedx.core.djangoapps.bookmarks.tasks'
+    'openedx.core.djangoapps.bookmarks.tasks',
+    'openedx.core.djangoapps.ccxcon.tasks',
 )
 
 # Message configuration
