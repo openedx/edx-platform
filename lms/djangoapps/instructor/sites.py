0cc3f6930e78776923bd099b9234e2da269c4dc8
melvinsoft <melvinsoft@gmail.com>
2019-08-13 20:24:54 +0200

    fix PEP8

--- lms/djangoapps/instructor/sites.py
+++ lms/djangoapps/instructor/sites.py
@@ -1,8 +1,10 @@
 """
+Heplers for site/org isolations functions.
 """
 from openedx.core.djangoapps.theming.helpers import get_current_site
 
 from organizations.models import Organization, UserOrganizationMapping
+
 
 def user_exists_in_organization(user_email, organization):
     """
@@ -10,11 +12,13 @@ def user_exists_in_organization(user_email, organization):
     """
     return organization.userorganizationmapping_set.filter(user__email=user_email).exists()
 
+
 def get_organization_for_site(site):
     """
     TODO
     """
     return get_current_site().organizations.first()
+
 
 def get_user_in_organization_by_email(user_email, organization):
     """
