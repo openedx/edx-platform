48c17803c2ffe9ee20cb27defa7ed2c05fcb8f0b
Emma Green <emma.green.software@gmail.com>
2019-08-15 17:02:36 -0400

    add endpoint that takes a user id to allow us to correctly get the discount after checkout to correctly create order

--- openedx/features/discounts/urls.py
+++ openedx/features/discounts/urls.py
@@ -6,8 +6,11 @@ from __future__ import absolute_import
 from django.conf import settings
 from django.conf.urls import url
 
-from .views import CourseUserDiscount
+from .views import CourseUserDiscount, CourseUserDiscountWithUserParam
 
 urlpatterns = [
     url(r'^course/{}'.format(settings.COURSE_KEY_PATTERN), CourseUserDiscount.as_view(), name='course_user_discount'),
+    url(r'^user/(?P<user_id>[^/]*)/course/{}'.format(settings.COURSE_KEY_PATTERN),
+        CourseUserDiscountWithUserParam.as_view(),
+        name='course_user_discount_with_param'),
 ]
