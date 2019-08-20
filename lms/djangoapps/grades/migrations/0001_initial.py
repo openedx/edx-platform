8d931b84413361d0ce2f921b2bc5d2cfe55bd64b
Cory Lee <cory@edx.org>
2019-08-20 11:25:42 -0400

    bump

--- lms/djangoapps/grades/migrations/0001_initial.py
+++ lms/djangoapps/grades/migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
                 ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
-                ('id', coursewarehistoryextended.fields.UnsignedBigIntAutoField(serialize=False, primary_key=True)),
+                ('id', UnsignedBigIntAutoField(serialize=False, primary_key=True)),
                 ('user_id', models.IntegerField()),
                 ('course_id', CourseKeyField(max_length=255)),
                 ('usage_key', UsageKeyField(max_length=255)),
