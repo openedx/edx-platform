a6d4ffbec9596a74414ac6110e19246bc36a9787
Muhammad Zeshan Arif <>
2019-09-04 17:23:34 +0500

    Create migration to add custom history columns in edx django history tables.

---
+++ lms/djangoapps/philu_overrides/migrations/0002_add_custom_columns_in_edx_django_history_tables.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, connection
+
+#TODO: find a more better way of handling history of builtin packages
+class Migration(migrations.Migration):
+    dependencies = [
+        ('enterprise', '0001_initial'),
+    ]
+
+    def add_history_columns(apps, schema_editor):
+        cursor = connection.cursor()
+        tables = [
+            'enterprise_historicalenterprisecustomerentitlement',
+            'enterprise_historicalenterprisecourseenrollment',
+            'enterprise_historicalenterprisecustomercatalog',
+            'enterprise_historicalenrollmentnotificationemailtemplate',
+            'consent_historicaldatasharingconsent',
+            'degreed_historicaldegreedenterprisecustomerconfiguration'
+        ]
+        for t in tables:
+            query = 'ALTER TABLE %s ADD start_date DATETIME NULL, ADD end_date DATETIME NULL;' % t
+            cursor.execute(query)
+
+    operations = [
+        migrations.RunPython(add_history_columns),
+    ]
