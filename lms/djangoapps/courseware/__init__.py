83406db629abd389da85666dc79925f8e03a22f4
Ned Batchelder <ned@edx.org>
2019-09-05 12:30:05 -0400

    Add more diagnostics to the courseware import warning

--- lms/djangoapps/courseware/__init__.py
+++ lms/djangoapps/courseware/__init__.py
@@ -1,7 +1,12 @@
 #pylint: disable=missing-docstring
 from __future__ import absolute_import
 
+import inspect
 import warnings
 
 if __name__ == 'courseware':
-    warnings.warn("Importing 'lms.djangoapps.courseware' as 'courseware' is no longer supported", DeprecationWarning)
+    # pylint: disable=unicode-format-string
+    # Show the call stack that imported us wrong.
+    stack = "\n".join("%30s : %s:%d" % (t[3], t[1], t[2]) for t in inspect.stack()[:0:-1])
+    msg = "Importing 'lms.djangoapps.courseware' as 'courseware' is no longer supported:\n" + stack
+    warnings.warn(msg, DeprecationWarning)
