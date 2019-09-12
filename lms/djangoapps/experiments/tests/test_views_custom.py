a284f2b88428801a66cd8a6d73adde399f65478b
Ben Holt <bholt+github@edx.org>
2019-09-12 15:55:47 -0400

    A few basic tests

--- lms/djangoapps/experiments/tests/test_views_custom.py
+++ lms/djangoapps/experiments/tests/test_views_custom.py
@@ -9,24 +9,59 @@ from rest_framework.test import APITestCase
 from lms.djangoapps.course_blocks.transformers.tests.helpers import ModuleStoreTestCase
 from student.tests.factories import UserFactory
 
+from openedx.core.djangoapps.waffle_utils.testutils import override_waffle_flag
+from xmodule.modulestore.tests.factories import CourseFactory
+
+from lms.djangoapps.experiments.views_custom import MOBILE_UPSELL_FLAG
+
 CROSS_DOMAIN_REFERER = 'https://ecommerce.edx.org'
 
 
-class Rev934Tests(APITestCase, ModuleStoreTestCase):
-    def test_logged_in(self):
+class Rev934LoggedOutTests(APITestCase):
+    def test_not_logged_in(self):
         """Test mobile app upsell API"""
         url = reverse('api_experiments:rev_934')
-        user = UserFactory()
 
         # Not-logged-in returns 401
         response = self.client.get(url)
         self.assertEqual(response.status_code, 401)
 
-        # No-course-id returns show_upsell false
+
+class Rev934Tests(APITestCase, ModuleStoreTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(Rev934Tests, cls).setUpClass()
+        cls.url = reverse('api_experiments:rev_934')
+
+    def setUp(self):
+        super(Rev934Tests, self).setUp()
+        user = UserFactory()
         self.client.login(
             username=user.username,
             password=UserFactory._DEFAULT_PASSWORD,  # pylint: disable=protected-access
         )
-        response = self.client.get(url)
+
+    @override_waffle_flag(MOBILE_UPSELL_FLAG, active=False)
+    def test_flag_off(self):
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['show_upsell'], False)
+        self.assertEqual(response.data['upsell_flag'], False)
+
+    @override_waffle_flag(MOBILE_UPSELL_FLAG, active=True)
+    def test_no_course_id(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 400)
+
+    @override_waffle_flag(MOBILE_UPSELL_FLAG, active=True)
+    def test_bad_course_id(self):
+        response = self.client.get(self.url + "?course_id=junk")
+        self.assertEqual(response.status_code, 400)
+
+    @override_waffle_flag(MOBILE_UPSELL_FLAG, active=True)
+    def test_simple_course(self):
+        course = CourseFactory.create()
+        response = self.client.get(self.url, {'course_id': course.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['show_upsell'], True)
