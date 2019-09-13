3464681b75a075ce12878d9fcbfbff6c4f0dbb11
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2019-09-13 14:59:16 +0000

    Adding scaled course grade to the grade report

--- lms/djangoapps/instructor_task/tasks_helper/grades.py
+++ lms/djangoapps/instructor_task/tasks_helper/grades.py
@@ -585,7 +585,14 @@ class CourseGradeReport(object):
             if assignment_average is not None:
                 grade_results.append([assignment_average])
 
-        return [course_grade.percent] + _flatten(grade_results)
+        return [course_grade.percent, self.grade_percent_scaled(course_grade, context)] + _flatten(grade_results)
+
+    def grade_percent_scaled(self, course_grade, context):
+        grade_cutoff = min(context.course.grade_cutoffs.values())
+        if course_grade.percent < grade_cutoff:
+            return round(10. * (3. / grade_cutoff * course_grade.percent + 1.)) / 10.
+        return round((3. / (1. - grade_cutoff) * course_grade.percent + (7. - (3. / (1. - grade_cutoff)))) * 10.) / 10.
+
 
     def _user_subsection_grades(self, course_grade, subsection_headers):
         """
