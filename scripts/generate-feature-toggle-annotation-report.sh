13bbf85257b78ea025d4c1ff0f7d61a03841612c
Stu Young <estute@users.noreply.github.com>
2019-09-17 11:11:20 -0400

    clean up toggle annotations (#21673)
    clean up toggle annotations and lint when running feature toggle report

--- scripts/generate-feature-toggle-annotation-report.sh
+++ scripts/generate-feature-toggle-annotation-report.sh
@@ -7,6 +7,6 @@
 
 pip install -r requirements/edx/paver.txt -r requirements/edx/testing.txt
 rm -Rf reports/*
-code_annotations static_find_annotations --config_file=../$CODE_ANNOTATION_CONFIG_PATH --no_lint
+code_annotations static_find_annotations --config_file=../$CODE_ANNOTATION_CONFIG_PATH
 mkdir -p ../$CODE_ANNOTATION_OUTPUT_PATH
 cp reports/* ../$CODE_ANNOTATION_OUTPUT_PATH/lms-annotations.yml
