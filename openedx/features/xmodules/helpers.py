5906592d076ee265bdf97d8401f29f020de9feb6
Muhammad Zeshan Arif <>
2019-09-20 12:33:36 +0500

    Integrate changes from 04 april in ironwood files from 103 to 105

---
+++ openedx/features/xmodules/helpers.py
@@ -0,0 +1,16 @@
+from .constants import XBLOCKS_EXCEPT_PROBLEM
+
+
+def get_due_date_for_problem_xblock(due, module_category):
+    """
+    :param due: It's due date of sub section
+    :param module_category: category of the xblock
+    :return: formatted due date for xblocks that are problem type
+    and for other xblocks it's None
+    """
+    due_date = None
+    if module_category not in XBLOCKS_EXCEPT_PROBLEM:
+        if due:
+            due_date = due.strftime("%b %d, %Y %Z")
+
+    return due_date
