ce0dbd974830cb7c1783c5b9f476b68cec2ae10f
Ben Holt <bholt+github@edx.org>
2019-09-24 15:53:26 -0400

    Remove unused import

--- lms/djangoapps/experiments/tests/test_views_custom.py
+++ lms/djangoapps/experiments/tests/test_views_custom.py
@@ -3,7 +3,7 @@ Tests for experimentation views
 """
 from __future__ import absolute_import
 
-from datetime import datetime, timedelta
+from datetime import timedelta
 from uuid import uuid4
 import six
 
