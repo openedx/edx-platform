b335f2a2ec1cb51358f99b7b8cb5d6d1844e4cb9
Calen Pennington <cale@edx.org>
2019-09-26 10:06:26 -0400

    Log all celery task failures

--- lms/celery.py
+++ lms/celery.py
@@ -6,12 +6,16 @@ Taken from: https://celery.readthedocs.org/en/latest/django/first-steps-with-dja
 """
 from __future__ import absolute_import
 
+import logging
 import os
 
-from celery import Celery
+from celery import Celery, signals
 from django.conf import settings
 
 from openedx.core.lib.celery.routers import AlternateEnvironmentRouter
+
+LOG = logging.getLogger(__name__)
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
@@ -35,3 +39,37 @@ class Router(AlternateEnvironmentRouter):
         Defines alternate environment tasks, as a dict of form { task_name: alternate_queue }
         """
         return {}
+
+
+@signals.task_retry.connect
+@signals.task_failure.connect
+@signals.task_revoked.connect
+def on_task_failure(**kwargs):
+    """
+    Log all exceptions that happen inside celery tasks.
+    """
+    # celery exceptions will not be published to `sys.excepthook`. therefore we have to create another handler here.
+    from traceback import format_list, extract_tb
+
+    traceback = None
+    exception = None
+
+    if 'einfo' in kwargs:
+        traceback = kwargs['einfo'].tb
+        exception = kwargs['einfo'].exception
+
+    traceback = kwargs.get('traceback', traceback)
+    exception = kwargs.get('exception', exception)
+
+    if traceback:
+        stack_frames = extract_tb(traceback)
+    else:
+        stack_frames = []
+
+    LOG.error(
+        '[task:%s:%s]\n%s\n%s',
+        kwargs.get('task_id'),
+        kwargs['sender'].request.correlation_id,
+        ''.join(format_list(stack_frames)),
+        exception
+    )
