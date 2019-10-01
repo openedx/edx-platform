d3a1ca9f6976f420eacdf78e9ef95ded1c061209
Ned Batchelder <ned@edx.org>
2019-10-01 18:00:10 -0400

    See if this fancy stack trace was stopping newrelic

--- lms/djangoapps/courseware/__init__.py
+++ lms/djangoapps/courseware/__init__.py
@@ -1,12 +1,10 @@
 #pylint: disable=missing-docstring
 from __future__ import absolute_import
 
-import inspect
 import warnings
 
 if __name__ == 'courseware':
     # pylint: disable=unicode-format-string
     # Show the call stack that imported us wrong.
-    stack = "\n".join("%30s : %s:%d" % (t[3], t[1], t[2]) for t in inspect.stack()[:0:-1])
-    msg = "Importing 'lms.djangoapps.courseware' as 'courseware' is no longer supported:\n" + stack
+    msg = "Importing 'lms.djangoapps.courseware' as 'courseware' is no longer supported"
     warnings.warn(msg, DeprecationWarning)
