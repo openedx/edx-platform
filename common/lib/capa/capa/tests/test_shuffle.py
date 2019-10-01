5c673711a0280646bf67a9bf4b107f949bd3ba8b
Ayub khan <muhammadayubkhan6@gmail.com>
2019-10-01 13:10:21 +0500

    BOM-796 python3 compatibility Shuffling logic produces different results in python3 as compared to python2.

--- common/lib/capa/capa/tests/test_shuffle.py
+++ common/lib/capa/capa/tests/test_shuffle.py
@@ -199,10 +199,11 @@ class CapaShuffleTest(unittest.TestCase):
         """)
         problem = new_loncapa_problem(xml_str, seed=0)
         the_html = problem.get_html()
-        self.assertRegexpMatches(
-            the_html,
-            r"<div>.*\[.*'Alpha'.*'Beta'.*'B'.*'A'.*'C'.*'D'.*'Psi'.*'Omega'.*\].*</div>"
-        )
+        if six.PY2:
+            regex = r"<div>.*\[.*'Alpha'.*'Beta'.*'B'.*'A'.*'C'.*'D'.*'Psi'.*'Omega'.*\].*</div>"
+        else:
+            regex = r"<div>.*\[.*'Alpha'.*'Beta'.*'C'.*'A'.*'B'.*'D'.*'Psi'.*'Omega'.*\].*</div>"
+        self.assertRegexpMatches(the_html, regex)
 
     def test_shuffle_fixed_both_ends_thin(self):
         xml_str = textwrap.dedent("""
