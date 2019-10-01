ff795347dd97ae5d30c29a4a9d79f461846dd5f1
Diana Huang <dkh@edx.org>
2019-10-01 15:28:08 -0400

    Annotate a few of the course_experience waffle flags.

--- openedx/features/course_experience/waffle.py
+++ openedx/features/course_experience/waffle.py
@@ -9,6 +9,17 @@ from openedx.core.djangoapps.waffle_utils import WaffleSwitchNamespace
 WAFFLE_NAMESPACE = u'course_experience'
 
 # Switches
+# .. toggle_name: course_experience.enable_about_sidebar_html
+# .. toggle_implementation: CourseWaffleFlag
+# .. toggle_default: False
+# .. toggle_description: Used to determine whether to show custom HTML in the sidebar on the internal course about page.
+# .. toggle_category: course-experience
+# .. toggle_use_cases: open_edx
+# .. toggle_creation_date: 2018-01-26
+# .. toggle_expiration_date: ???
+# .. toggle_warnings: N/A
+# .. toggle_tickets: N/A
+# .. toggle_status: supported
 ENABLE_COURSE_ABOUT_SIDEBAR_HTML = u'enable_about_sidebar_html'
 
 
