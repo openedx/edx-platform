66382961a782dfc1cd33cd6f15c75b600f7fb314
Feanil Patel <feanil@edx.org>
2019-10-02 16:32:41 -0400

    Fix all transcript related tests.
    Treat transcript content as unicode strings and convert them at any edge
    where we encounter them.  One decision made here was to not update
    edx-val it treats transcripts as byte but is much closer to the actual
    files so it makes more sense over there. But within the platform they
    are generally passed around as serialized json and so it's much better
    for them to be unicode.

--- cms/djangoapps/contentstore/views/tests/test_transcript_settings.py
+++ cms/djangoapps/contentstore/views/tests/test_transcript_settings.py
@@ -307,7 +307,7 @@ class TranscriptUploadTest(CourseTestCase):
         """
         Tests that transcript upload handler works as expected.
         """
-        transcript_file_stream = BytesIO('0\n00:00:00,010 --> 00:00:00,100\nПривіт, edX вітає вас.\n\n')
+        transcript_file_stream = six.StringIO('0\n00:00:00,010 --> 00:00:00,100\nПривіт, edX вітає вас.\n\n')
         # Make request to transcript upload handler
         response = self.client.post(
             self.view_url,
@@ -422,7 +422,7 @@ class TranscriptUploadTest(CourseTestCase):
         """
         Tests the transcript upload handler with an invalid transcript file.
         """
-        transcript_file_stream = BytesIO('An invalid transcript SubRip file content')
+        transcript_file_stream = six.StringIO('An invalid transcript SubRip file content')
         # Make request to transcript upload handler
         response = self.client.post(
             self.view_url,
