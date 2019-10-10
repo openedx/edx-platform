91e7d8a7be17e36688f156d5efac6fb9a72ee8ca
Awais Qureshi <awais.qureshi@arbisoft.com>
2019-10-10 17:11:23 +0500

    BOM-913 reduce functions moves into functools in python3. Fixed the import. Fixing bytes issue.

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 import datetime
 import logging
 import uuid
+from functools import reduce  # pylint: disable=redefined-builtin
 
 import pytz
 import six
@@ -32,8 +33,6 @@ from xblock.fields import ScopeIds
 from bulk_email.api import is_bulk_email_feature_enabled
 from class_dashboard.dashboard_data import get_array_section_has_problem, get_section_display_name
 from course_modes.models import CourseMode, CourseModesArchive
-from lms.djangoapps.courseware.access import has_access
-from lms.djangoapps.courseware.courses import get_course_by_id, get_studio_url
 from edxmako.shortcuts import render_to_response
 from lms.djangoapps.certificates import api as certs_api
 from lms.djangoapps.certificates.models import (
@@ -44,6 +43,8 @@ from lms.djangoapps.certificates.models import (
     CertificateWhitelist,
     GeneratedCertificate
 )
+from lms.djangoapps.courseware.access import has_access
+from lms.djangoapps.courseware.courses import get_course_by_id, get_studio_url
 from lms.djangoapps.courseware.module_render import get_module_by_usage_id
 from lms.djangoapps.discussion.django_comment_client.utils import available_division_schemes, has_forum_access
 from lms.djangoapps.grades.api import is_writable_gradebook_enabled
