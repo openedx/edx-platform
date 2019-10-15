b15fe202fb5b6716c5939d9690d345e6788f7b5b
Imran Bashir <imran.bashir@arbisoft.com>
2019-10-15 19:40:57 +0500

    fix unique together issue

--- openedx/features/ondemand_email_preferences/models.py
+++ openedx/features/ondemand_email_preferences/models.py
@@ -7,3 +7,7 @@ class OnDemandEmailPreferences(models.Model):
     user = models.ForeignKey(User, db_index=True, related_name='on_demand_email_preferences_user', on_delete=models.CASCADE)
     course_id = CourseKeyField(db_index=True, max_length=255, null=False)
     is_enabled = models.BooleanField(default=True, null=False)
+
+    class Meta(object):
+        """ Meta class to make user and course unique together """
+        unique_together = ('course_id', 'user')
