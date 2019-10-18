ea30aba6fcf5a4a5c995b7884cd604c35a2df5f7
Ned Batchelder <ned@edx.org>
2019-10-18 15:29:02 -0400

    Revert pull request #22042
    Revert "Fix code quality test failures"
    
    This reverts commit 8c55e11d1f69172b1c25558a14906a19dc03d003.
    
    Revert "Fix celery send_activation_email task failure"
    
    This reverts commit 810eea0e51199e28463365df22a0c91b31b760db.
    
    Revert "Convert Account Activation Emails to edx-ACE"
    
    This reverts commit 7984c37a4f45a24db8837a67fc9c4c781f647e2f.

--- common/djangoapps/student/tasks.py
+++ common/djangoapps/student/tasks.py
@@ -5,43 +5,30 @@ from __future__ import absolute_import
 
 import logging
 
+from boto.exception import NoAuthHandlerFound
 from celery.exceptions import MaxRetriesExceededError
 from celery.task import task  # pylint: disable=no-name-in-module, import-error
 from django.conf import settings
-from edx_ace import ace
-from edx_ace.errors import RecoverableChannelDeliveryError
-from edx_ace.message import Message
-from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+from django.core import mail
 
 log = logging.getLogger('edx.celery.task')
 
 
 @task(bind=True)
-def send_activation_email(self, msg_string, from_address=None):
+def send_activation_email(self, subject, message, from_address, dest_addr):
     """
     Sending an activation email to the user.
     """
-    msg = Message.from_string(msg_string)
-
     max_retries = settings.RETRY_ACTIVATION_EMAIL_MAX_ATTEMPTS
     retries = self.request.retries
-
-    if from_address is None:
-        from_address = configuration_helpers.get_value('ACTIVATION_EMAIL_FROM_ADDRESS') or (
-            configuration_helpers.get_value('email_from_address', settings.DEFAULT_FROM_EMAIL)
-        )
-    msg.options['from_address'] = from_address
-
-    dest_addr = msg.recipient.email_address
-
     try:
-        ace.send(msg)
+        mail.send_mail(subject, message, from_address, [dest_addr], fail_silently=False)
         # Log that the Activation Email has been sent to user without an exception
         log.info("Activation Email has been sent to User {user_email}".format(
             user_email=dest_addr
         ))
-    except RecoverableChannelDeliveryError:
-        log.info('Retrying sending email to user {dest_addr}, attempt # {attempt} of {max_attempts}'.format(
+    except NoAuthHandlerFound:  # pylint: disable=broad-except
+        log.info('Retrying sending email to user {dest_addr}, attempt # {attempt} of {max_attempts}'. format(
             dest_addr=dest_addr,
             attempt=retries,
             max_attempts=max_retries
