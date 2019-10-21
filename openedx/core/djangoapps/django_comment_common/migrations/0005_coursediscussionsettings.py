13a73fd7762e0e176fc8d6d51016cfbd982f05bf
Feanil Patel <feanil@edx.org>
2019-10-21 11:40:54 -0400

    Fix type mismatches in the django_comment_common migrations.
    Mixing byte and unicode strings causes migrations to fail.

--- openedx/core/djangoapps/django_comment_common/migrations/0005_coursediscussionsettings.py
+++ openedx/core/djangoapps/django_comment_common/migrations/0005_coursediscussionsettings.py
@@ -16,10 +16,10 @@ class Migration(migrations.Migration):
             name='CourseDiscussionSettings',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('course_id', CourseKeyField(help_text=b'Which course are these settings associated with?', unique=True, max_length=255, db_index=True)),
+                ('course_id', CourseKeyField(help_text='Which course are these settings associated with?', unique=True, max_length=255, db_index=True)),
                 ('always_divide_inline_discussions', models.BooleanField(default=False)),
-                ('_divided_discussions', models.TextField(null=True, db_column=b'divided_discussions', blank=True)),
-                ('division_scheme', models.CharField(default=b'none', max_length=20, choices=[(b'none', b'None'), (b'cohort', b'Cohort'), (b'enrollment_track', b'Enrollment Track')])),
+                ('_divided_discussions', models.TextField(null=True, db_column='divided_discussions', blank=True)),
+                ('division_scheme', models.CharField(default='none', max_length=20, choices=[('none', 'None'), ('cohort', 'Cohort'), ('enrollment_track', 'Enrollment Track')])),
             ],
             options={
                 'db_table': 'django_comment_common_coursediscussionsettings',
