a29abcf3fa5e036941e31a2b31ed72d262c83ee3
jinder1s <msingh@edx.org>
2019-10-22 16:27:03 -0400

    fix type mismatch in dark_lang migrations

--- openedx/core/djangoapps/dark_lang/migrations/0001_initial.py
+++ openedx/core/djangoapps/dark_lang/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('change_date', models.DateTimeField(auto_now_add=True, verbose_name='Change date')),
                 ('enabled', models.BooleanField(default=False, verbose_name='Enabled')),
-                ('released_languages', models.TextField(help_text=b'A comma-separated list of language codes to release to the public.', blank=True)),
+                ('released_languages', models.TextField(help_text=u'A comma-separated list of language codes to release to the public.', blank=True)),
                 ('changed_by', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, editable=False, to=settings.AUTH_USER_MODEL, null=True, verbose_name='Changed by')),
             ],
             options={
