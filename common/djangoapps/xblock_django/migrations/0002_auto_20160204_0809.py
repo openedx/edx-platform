dccfd38c4df8a81ec15db985e4296fe4192f1fbb
jinder1s <msingh@edx.org>
2019-10-23 13:32:05 -0400

    fix type mismatch in xblock_django  migrations

--- common/djangoapps/xblock_django/migrations/0002_auto_20160204_0809.py
+++ common/djangoapps/xblock_django/migrations/0002_auto_20160204_0809.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='xblockdisableconfig',
             name='disabled_create_blocks',
-            field=models.TextField(default=b'', help_text='Space-separated list of XBlock types whose creation to disable in Studio.', blank=True),
+            field=models.TextField(default=u'', help_text='Space-separated list of XBlock types whose creation to disable in Studio.', blank=True),
         ),
     ]
