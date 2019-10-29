31d7cb1a1f754c5b5204737375418c8918deab7a
uzairr <uzairr@yahoo.com>
2019-10-29 18:13:36 +0500

    disable lang cookie for mobile

--- openedx/core/djangoapps/lang_pref/views.py
+++ openedx/core/djangoapps/lang_pref/views.py
@@ -13,6 +13,7 @@ from django.http import HttpResponse
 from django.utils.translation import LANGUAGE_SESSION_KEY
 from django.views.decorators.csrf import ensure_csrf_cookie
 from openedx.core.djangoapps.lang_pref import COOKIE_DURATION, LANGUAGE_KEY
+from openedx.core.lib.mobile_utils import is_request_from_mobile_app
 
 
 @ensure_csrf_cookie
@@ -26,10 +27,11 @@ def update_session_language(request):
         language = data.get(LANGUAGE_KEY, settings.LANGUAGE_CODE)
         if request.session.get(LANGUAGE_SESSION_KEY, None) != language:
             request.session[LANGUAGE_SESSION_KEY] = six.text_type(language)
-        response.set_cookie(
-            settings.LANGUAGE_COOKIE,
-            language,
-            domain=settings.SESSION_COOKIE_DOMAIN,
-            max_age=COOKIE_DURATION
-        )
+        if is_request_from_mobile_app(request):
+            response.set_cookie(
+                settings.LANGUAGE_COOKIE,
+                language,
+                domain=settings.SESSION_COOKIE_DOMAIN,
+                max_age=COOKIE_DURATION
+            )
     return response
