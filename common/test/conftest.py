9842ab5e7b4837947e5ddf2a1e1ef8e43f609c6a
jinder1s <msingh@edx.org>
2019-11-05 10:28:25 -0500

    Added pytest-json-report plugin 	- modifying app-opts in setup.cfg 	- adding hook to all conftest.py files in repo 	- setting report to be saved to test_root/log/warnings.json This was created to allow us to easily parse through test warnings in jenkins

--- common/test/conftest.py
+++ common/test/conftest.py
@@ -3,6 +3,21 @@
 # Patch the xml libs before anything else.
 from __future__ import absolute_import
 
+import openedx.core.tests.pytest_hooks as pytest_hooks
 from safe_lxml import defuse_xml_libs
 
 defuse_xml_libs()
+
+
+class DeferPlugin(object):
+    """Simple plugin to defer pytest-xdist hook functions."""
+
+    def pytest_json_modifyreport(self, json_report):
+        """standard xdist hook function.
+        """
+        return pytest_hooks.pytest_json_modifyreport(json_report)
+
+
+def pytest_configure(config):
+    if config.pluginmanager.hasplugin("json-report"):
+        config.pluginmanager.register(DeferPlugin())
