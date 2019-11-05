c83b18ba80840daca17dfd2bfad9391125193a5f
Imran Bashir <imran.bashir@arbisoft.com>
2019-11-05 17:54:56 +0500

    add partner model and add it to django admin

--- openedx/features/partners/admin.py
+++ openedx/features/partners/admin.py
@@ -1,2 +1,22 @@
+from django.contrib import admin
+from django.urls import reverse
 
+from .models import Partner
+
+
+class PartnerAdmin(admin.ModelAdmin):
+    """
+    Django admin customizations for Partner model
+    """
+    list_display = ('id', 'label', 'slug', 'partner_url')
+    readonly_fields = ('partner_url',)
+
+    def partner_url(self, obj):
+        if not hasattr(Partner, 'slug'):
+            return
+
+        return reverse('partner_url', kwargs={'slug': self.slug})
+
+
+admin.site.register(Partner, PartnerAdmin)
 
---
+++ openedx/features/partners/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Partner(models.Model):
+    """
+    This model represents white-labelled partners.
+    """
+    label = models.CharField(max_length=100, default=None, null=False)
+    main_logo = models.CharField(max_length=255, default=None, null=True)
+    small_logo = models.CharField(max_length=255, default=None, null=True)
+    slug = models.CharField(max_length=100, default=None, null=False, unique=True)
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '{}'.format(self.label)
+
+    class Meta:
+        verbose_name = "Partner"
+        verbose_name_plural = "Partners"
+
+
+class PartnerUser(models.Model):
+    """
+    This model represents all the users that are associated to a partner.
+    """
+    user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE, related_name="partner_user")
+    partner = models.ForeignKey(Partner, db_index=True, on_delete=models.CASCADE, related_name="partner")
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '{}'.format(self.partner.name)
+
+    class Meta:
+        unique_together = ('user', 'partner')
+
