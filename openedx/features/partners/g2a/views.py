b29f3d2b37a23b2605b236c4370f23508bc9d74d
Ahmed Faraz <ahmed.faraz@arbisoft.com>
2019-11-05 17:13:43 +0500

    LP-1633_implement_g2a_recommnedations_page https://philanthropyu.atlassian.net/browse/LP-1633 - Implmenet recommendation logic

--- openedx/features/partners/g2a/views.py
+++ openedx/features/partners/g2a/views.py
@@ -1,5 +1,9 @@
 from edxmako.shortcuts import render_to_response
 
+from openedx.features.partners.helpers import get_partner_recommended_courses
+
 
 def g2a_dashboard(request):
-    return render_to_response('partners/g2a/dashboard.html')
+    # TODO: The argument must be dynamic after integration of LP-1632
+    courses = get_partner_recommended_courses('give2asia')
+    return render_to_response('partners/g2a/dashboard.html', {'recommended_courses': courses})
