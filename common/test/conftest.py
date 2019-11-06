a915343ab006b8d8cccd205ffca49a81688f224d
jinder1s <msingh@edx.org>
2019-11-06 16:13:20 -0500

    Fixing import error

--- common/test/conftest.py
+++ common/test/conftest.py
@@ -3,7 +3,7 @@
 # Patch the xml libs before anything else.
 from __future__ import absolute_import
 
-import openedx.core.tests.pytest_hooks.pytest_json_modifyreport as custom_hook # pylint: disable=unused-import
+import openedx.core.tests.pytest_hooks as pytest_hooks # pylint: disable=unused-import
 
 from safe_lxml import defuse_xml_libs
 
@@ -16,7 +16,7 @@ class DeferPlugin:
     def pytest_json_modifyreport(self, json_report):
         """standard xdist hook function.
         """
-        return custom_hook(json_report)
+        return pytest_hooks.pytest_json_modifyreport(json_report)
 
 
 def pytest_configure(config):
