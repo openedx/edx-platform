f38f30074ca7762fadfe126ec47042567b8fdd5b
Robert Raposa <rraposa@edx.org>
2019-11-07 11:14:34 -0500

    upgrade edx-drf-extensions to 2.4.5
    - Upgrade edx-drf-extensions to 2.4.5
    - Removed constraint to 2.4.0, because 2.4.2 introduces a workaround for
    ARCH-1210 by putting the problematic code behind a django setting.
    - Remove unused JWT_AUTH_REFRESH_COOKIE setting.
    
    ARCH-418, ARCH-1269, ARCH-1044
    
    fix broken toggle

--- openedx/core/djangoapps/oauth_dispatch/toggles.py
+++ openedx/core/djangoapps/oauth_dispatch/toggles.py
@@ -4,10 +4,9 @@ Feature toggle code for oauth_dispatch.
 
 from __future__ import absolute_import
 
-from edx_rest_framework_extensions.config import SWITCH_ENFORCE_JWT_SCOPES
+from edx_rest_framework_extensions.config import OAUTH_TOGGLE_NAMESPACE, SWITCH_ENFORCE_JWT_SCOPES
 
 from openedx.core.djangoapps.waffle_utils import WaffleSwitch, WaffleSwitchNamespace
 
-WAFFLE_NAMESPACE = 'oauth2'
-OAUTH2_SWITCHES = WaffleSwitchNamespace(name=WAFFLE_NAMESPACE)
-ENFORCE_JWT_SCOPES = WaffleSwitch(OAUTH2_SWITCHES, SWITCH_ENFORCE_JWT_SCOPES)
+
+ENFORCE_JWT_SCOPES = WaffleSwitch(WaffleSwitchNamespace(name=OAUTH_TOGGLE_NAMESPACE), SWITCH_ENFORCE_JWT_SCOPES)
