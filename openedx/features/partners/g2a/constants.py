c7a7ee26616a5bf203371e78b9bd18d0bf115953
Zahid <zahid.ali@arbisoft.com>
2019-11-11 20:53:10 +0500

    LP-1637 Creating bridge table between user and partner table

--- openedx/features/partners/g2a/constants.py
+++ openedx/features/partners/g2a/constants.py
@@ -1,16 +1,15 @@
-ORGANIZATION_NAME='organization_name'
-ENGLISH_PROFICIENCY='english_proficiency'
-START_MONTH_YEAR='start_month_year'
-IS_INTERESTS_DATA_SUBMITTED='is_interests_data_submitted'
-FIRST_NAME='first_name'
-LAST_NAME='last_name'
+ORGANIZATION_NAME_KEY = 'organization_name'
+ENGLISH_PROFICIENCY_KEY = 'english_proficiency'
+START_MONTH_YEAR_KEY = 'start_month_year'
+IS_INTERESTS_DATA_SUBMITTED_KEY = 'is_interests_data_submitted'
+FIRST_NAME_KEY = 'first_name'
+LAST_NAME_KEY = 'last_name'
+YEAR_OF_BIRTH_KEY = 'year_of_birth'
+LEVEL_OF_EDUCATION_KEY = 'level_of_education'
 GIVE2ASIA_DEFAULT_DATA = {
-    'year_of_birth': 2000,
-    'level_of_education': 'IWRNS',
-    ENGLISH_PROFICIENCY: 'IWRNS',
-    START_MONTH_YEAR: '11/2019',
-    IS_INTERESTS_DATA_SUBMITTED: True,
-    ORGANIZATION_NAME: None,
-    FIRST_NAME: None,
-    LAST_NAME: None,
+    YEAR_OF_BIRTH_KEY: 2000,
+    LEVEL_OF_EDUCATION_KEY: 'IWRNS',
+    ENGLISH_PROFICIENCY_KEY: 'IWRNS',
+    START_MONTH_YEAR_KEY: '11/2019',
+    IS_INTERESTS_DATA_SUBMITTED_KEY: True,
 }
