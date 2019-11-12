35a888e7e47883bf105f953a190f908b00b2a20f
Zahid <zahid.ali@arbisoft.com>
2019-11-12 18:05:23 +0500

    LP-1637 Adding comments for default data

--- openedx/features/partners/give2asia/constants.py
+++ openedx/features/partners/give2asia/constants.py
@@ -7,11 +7,12 @@ LAST_NAME_KEY = 'last_name'
 YEAR_OF_BIRTH_KEY = 'year_of_birth'
 LEVEL_OF_EDUCATION_KEY = 'level_of_education'
 OPT_IN_KEY = 'opt_in'
+# These are minimum required fields to complete registration
+# These are the default values for Give2Asia partner
 GIVE2ASIA_DEFAULT_DATA = {
-    YEAR_OF_BIRTH_KEY: 2000,
-    LEVEL_OF_EDUCATION_KEY: 'IWRNS',
-    ENGLISH_PROFICIENCY_KEY: 'IWRNS',
+    LEVEL_OF_EDUCATION_KEY: 'IWRNS',  # set I'd rather not say for education level
+    ENGLISH_PROFICIENCY_KEY: 'IWRNS',  # set I'd rather not say for english proficiency
     START_MONTH_YEAR_KEY: '11/2019',
-    IS_INTERESTS_DATA_SUBMITTED_KEY: True,
-    OPT_IN_KEY: 'no'
+    IS_INTERESTS_DATA_SUBMITTED_KEY: True,  # Flag required for by-passing interests form
+    OPT_IN_KEY: 'no'  # By default user opt out of emails by MailChimp
 }
