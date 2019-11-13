c3fb4fb92ee6f99f41b5be6297d8065f6e4026e8
jinder1s <msingh@edx.org>
2019-11-13 10:39:18 -0500

    adding pip sync to jenkins-common

--- scripts/jenkins-common.sh
+++ scripts/jenkins-common.sh
@@ -43,6 +43,9 @@ fi
 # Activate the Python virtualenv
 source $HOME/edx-venv/bin/activate
 
+pip install requirements/edx/pip-tools.txt
+pip-sync requirements/edx/development.txt 
+
 # add the node packages dir to PATH
 PATH=$PATH:node_modules/.bin
 
