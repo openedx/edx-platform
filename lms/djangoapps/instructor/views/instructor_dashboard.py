5e1f0c5b9ffc8a77ad4682601af2db880e78b0eb
Dave St.Germain <dstgermain@edx.org>
2019-11-15 10:41:36 -0500

    Added new role for course data researcher.

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -56,7 +56,10 @@ from openedx.core.lib.url_utils import quote_slashes
 from openedx.core.lib.xblock_utils import wrap_xblock
 from shoppingcart.models import Coupon, CourseRegCodeItem, PaidCourseRegistration
 from student.models import CourseEnrollment
-from student.roles import CourseFinanceAdminRole, CourseInstructorRole, CourseSalesAdminRole, CourseStaffRole
+from student.roles import (
+    CourseDataResearcherRole, CourseFinanceAdminRole, CourseInstructorRole,
+    CourseSalesAdminRole, CourseStaffRole
+)
 from util.json_request import JsonResponse
 from xmodule.html_module import HtmlBlock
 from xmodule.modulestore.django import modulestore
@@ -118,6 +121,7 @@ def instructor_dashboard_2(request, course_id):
         'sales_admin': CourseSalesAdminRole(course_key).has_user(request.user),
         'staff': bool(has_access(request.user, 'staff', course)),
         'forum_admin': has_forum_access(request.user, course_key, FORUM_ROLE_ADMINISTRATOR),
+        'data_researcher': CourseDataResearcherRole(course_key).has_user(request.user),
     }
 
     if not access['staff']:
@@ -709,6 +713,8 @@ def _section_data_download(course, access):
         ),
         'export_ora2_data_url': reverse('export_ora2_data', kwargs={'course_id': six.text_type(course_key)}),
     }
+    if not (access.get('data_researcher') or access.get('admin')):
+        section_data['is_hidden'] = True
     return section_data
 
 
