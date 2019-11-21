5422fb86030cec9600b320ff178cb2268678e212
Imran Bashir <imran.bashir@arbisoft.com>
2019-11-21 14:10:11 +0500

    add login functionality for g2a

--- openedx/features/partners/urls.py
+++ openedx/features/partners/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import dashboard, register_user
+from .views import dashboard, register_user, login_user
 
 PARTNERS_SLUG_PARAM = '(?P<slug>[0-9a-z_-]+)'
 
 urlpatterns = [
     url(r"^partners/{}/$".format(PARTNERS_SLUG_PARAM),  dashboard, name="partner_url"),
     url(r'^partners/{}/register/$'.format(PARTNERS_SLUG_PARAM), register_user, name="partner_register"),
+    url(r'^partners/{}/login/$'.format(PARTNERS_SLUG_PARAM), login_user, name="partner_login"),
 ]
--- openedx/features/partners/views.py
+++ openedx/features/partners/views.py
@@ -38,3 +38,22 @@ def register_user(request, slug):
         return views.Give2AsiaRegistrationView.as_view()(request, partner=partner)
     except ImportError:
         raise Http404('Your partner is not properly registered')
+
+
+@require_http_methods(["POST"])
+@sensitive_post_parameters('password')
+def login_user(request, slug):
+    """
+    This is general login view, for users of all partners
+    :param request.
+    :param slug: partner slug
+    :return: JsonResponse object with success/error message
+    """
+    partner = get_object_or_404(Partner, slug=slug)
+
+    try:
+        views = import_module(PARTNERS_VIEW_FRMT.format(slug=partner.slug))
+        return views.LoginSessionViewG2A.as_view()(request, partner=partner)
+    except ImportError:
+        raise Http404('Your partner is not properly registered')
+
