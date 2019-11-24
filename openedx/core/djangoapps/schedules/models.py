545e0f14e24fabb60be9e4dd82028f5713726b3d
hunytalk <ahannan@edx.org>
2019-11-25 02:22:16 +0500

    Remove old field (start) from Schedule Model

--- openedx/core/djangoapps/schedules/models.py
+++ openedx/core/djangoapps/schedules/models.py
@@ -18,13 +18,7 @@ class Schedule(TimeStampedModel):
         default=True,
         help_text=_('Indicates if this schedule is actively used')
     )
-    # TODO Delete this field during last stage of rolling out field renames
-    start = models.DateTimeField(
-        db_index=True,
-        help_text=_('Date this schedule went into effect')
-    )
     start_date = models.DateTimeField(
-        # TODO remove blank=True and null=True once data migration is complete in column rename release.
         db_index=True,
         help_text=_('Date this schedule went into effect')
     )
