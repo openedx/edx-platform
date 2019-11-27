f42d2edd29d420672a963a6d3fd4f1041d8667b0
Alex Wang <awang@edx.org>
2019-11-26 20:15:33 -0500

    Fix xss warnings (#22408)

--- lms/djangoapps/teams/static/teams/js/views/topic_card.js
+++ lms/djangoapps/teams/static/teams/js/views/topic_card.js
@@ -3,8 +3,14 @@
  */
 (function(define) {
     'use strict';
-    define(['backbone', 'underscore', 'gettext', 'js/components/card/views/card'],
-        function(Backbone, _, gettext, CardView) {
+    define([
+        'backbone',
+        'underscore',
+        'gettext',
+        'js/components/card/views/card',
+        'edx-ui-toolkit/js/utils/html-utils',
+        'edx-ui-toolkit/js/utils/string-utils'],
+        function(Backbone, _, gettext, CardView, HtmlUtils, StringUtils) {
             var TeamCountDetailView = Backbone.View.extend({
                 tagName: 'p',
                 className: 'team-count',
@@ -15,12 +21,15 @@
 
                 render: function() {
                     var team_count = this.model.get('team_count'); // eslint-disable-line camelcase
-                    // eslint-disable-next-line no-undef, max-len
-                    this.$el.html(_.escape(interpolate( // xss-lint: disable=javascript-jquery-html,javascript-interpolate
-                        ngettext('%(team_count)s Team', '%(team_count)s Teams', team_count),
-                        {team_count: team_count},
-                        true
-                    )));
+                    HtmlUtils.setHtml(
+                        this.$el,
+                        HtmlUtils.HTML(_.escape(StringUtils.interpolate(
+                            ngettext('{team_count} Team', '{team_count} Teams', team_count),
+                            {team_count: team_count},
+                            true
+                        )))
+                    );
+
                     return this;
                 }
             });
@@ -43,9 +52,8 @@
                 details: function() { return this.detailViews; },
                 actionClass: 'action-view',
                 actionContent: function() {
-                    // eslint-disable-next-line no-undef
-                    var screenReaderText = _.escape(interpolate( // xss-lint: disable=javascript-interpolate
-                        gettext('View Teams in the %(topic_name)s Topic'),
+                    var screenReaderText = _.escape(StringUtils.interpolate(
+                        gettext('View Teams in the {topic_name} Topic'),
                         {topic_name: this.model.get('name')}, true
                     ));
                     // eslint-disable-next-line max-len
