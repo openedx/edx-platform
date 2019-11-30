c5191960224bbe0e6074ddee9486c3eaf517703e
David Moreno <david.moreno@edunext.co>
2019-11-30 09:19:11 -0500

    @require_certificate_permission parameters added

--- lms/djangoapps/certificates/permissions.py
+++ lms/djangoapps/certificates/permissions.py
@@ -9,3 +9,5 @@ PREVIEW_CERTIFICATES = 'certificates.preview_certificates'
 perms[PREVIEW_CERTIFICATES] = HasAccessRule('staff')
 VIEW_ALL_CERTIFICATES = 'certificates.view_all_certificates'
 perms[VIEW_ALL_CERTIFICATES] = HasAccessRule('certificates')
+GENERATE_ALL_CERTIFICATES = 'certificates.generate_all_certificates'
+perms[GENERATE_ALL_CERTIFICATES] = HasAccessRule('certificates')
