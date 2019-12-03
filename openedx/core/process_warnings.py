b0a9d16f35f25b5b9aebd0bf24d4c7c12ad07ba4
jinder1s <msingh@edx.org>
2019-12-03 09:00:09 -0500

    Change underlying structure for better processing
    made script python 2 and 3 compliant.

--- openedx/core/process_warnings.py
+++ openedx/core/process_warnings.py
@@ -1,10 +1,17 @@
 import json
 import os
+import io
 import pprint
 import re
 import pdb
 import argparse
+import pandas as pd
 from write_to_html import HtmlOutlineWriter
+from collections import Counter
+
+columns =  ['message', 'category', 'filename', 'lineno', 'high_location', 'label', 'num','deprecated']
+columns_index_dict = {key: index for index, key in enumerate(columns)}
+
 
 pp = pprint.PrettyPrinter(indent=4, depth=2)
 
@@ -24,29 +31,36 @@ def seperate_warnings_by_location(warnings_data):
         ".*/edx-platform/common/.*\.py": "common",
     }
 
-    # create datatypes to hold location: dictionary with values that are lists(to store warning objects)
-    warnings_by_location = {}
-    for location in warnings_locations.values():
-        warnings_by_location[location] = []
-    # creating list for any warnings not covered by above locations
-    warnings_by_location["other"] = []
-
     """
     seperate into locations
     flow:
      iterate through each wanring_object, see if its filename matches any regex in warning locations.
-     If so, add it to appropriate list in warnings_by_location
+     If so, change high_location index on warnings_object to location name
     """
     for warnings_object in warnings_data:
         warning_origin_located = False
         for key in warnings_locations:
-            if re.search(key, warnings_object["filename"]) != None:
-                warnings_by_location[warnings_locations[key]].append(warnings_object)
+            if re.search(key, warnings_object[columns_index_dict["filename"]]) != None:
+                warnings_object[columns_index_dict["high_location"]] = warnings_locations[key]
                 warning_origin_located = True
                 break
         if not warning_origin_located:
-            warnings_by_location["other"].append(warnings_object)
-    return warnings_by_location
+            warnings_object[columns_index_dict["high_location"]] = "other"
+    return warnings_data
+
+def convert_warning_dict_to_list(warning_dict):
+    #namedtuple('message', 'category', 'filename', 'lineno', 'high_location', 'label', 'num')
+    output = []
+    for index, column in enumerate(columns):
+        if column in warning_dict:
+            if column == 'message' and "unclosed" in warning_dict[column]:
+                None
+                # pdb.set_trace()
+            output.append(warning_dict[column])
+        else:
+            output.append(None)
+    output[columns_index_dict["num"]] = 1
+    return output
 
 
 def read_warning_data(dir_path):
@@ -77,52 +91,20 @@ def read_warning_data(dir_path):
         with open(os.path.expanduser(dir_path + "/" + file), "r") as read_file:
             json_input = json.load(read_file)
             if "warnings" in json_input:
-                data = json_input["warnings"]
+                data =[convert_warning_dict_to_list(warning_dict) for warning_dict in json_input["warnings"]]
                 warnings_data.extend(data)
             else:
                 print(file)
     return warnings_data
 
-
-def compress_similar_warnings(warnings):
-    """
-        During pytest run, multiple instances of warnings are output
-        This function creates set of unique warnings(based on both warning text and filename)
-        and outputs dict : {warning text:[filename:warning_object]}
-        It also adds num(number of same warnings found) to keys in warning object
-    """
-
-    # first create additional data sturts to make iterating through warning objects easier
-    # list of just warning texts and index of given warning
-    warnings_text = []
-    # list of all unique warning texts
-    warnings_text_set = set()
-    for index, dict_obj in enumerate(warnings):
-        warnings_text.append((index, dict_obj["message"]))
-        warnings_text_set.add(dict_obj["message"])
-    """
-    For each unique warning text, find all occurences of it in warnings. Seperate by filename
-    and count number of times warning occurs
-    POSSIBLE ERROR: Currently this assumes warning is only output in one line in file
-    """
-    output = {}
-    for warning_text in warnings_text_set:
-        output[warning_text] = []
-        for index, warning_message in warnings_text:
-            if warning_message == warning_text:
-                output[warning_text].append(warnings[index])
-        # before removing duplcates, count number of times they occur
-        count = {}
-        for d in output[warning_text]:
-            if d["filename"] in count:
-                count[d["filename"]]["num"] += 1
-            else:
-                count[d["filename"]] = {"num": 1, "warning": d}
-        for d in count:
-            count[d]["warning"]["num"] = count[d]["num"]
-        output[warning_text] = [count[d]["warning"] for d in count]
-        # pdb.set_trace()
+def compress_similar_warnings(warnings_data):
+    tupled_data = [tuple(data) for data in warnings_data]
+    test_counter = Counter(tupled_data)
+    output = [list(value) for value in test_counter.keys()]
+    for data_object in output:
+        data_object[columns_index_dict["num"]] = test_counter[tuple(data_object)]
     return output
+
 
 
 def process_warnings_json(dir_path):
@@ -143,98 +125,50 @@ def process_warnings_json(dir_path):
         non-deprecated warnings
     """
     warnings_data = read_warning_data(dir_path)
-    print(len(warnings_data))
-    deprecated_warnings = []
-    non_deprecated_warnings = []
     for warnings_object in warnings_data:
-        if "deprecated" in warnings_object["message"]:
-            deprecated_warnings.append(warnings_object)
+        if "deprecated" in warnings_object[columns_index_dict["message"]]:
+            warnings_object[columns_index_dict["deprecated"]] = True
         else:
-            non_deprecated_warnings.append(warnings_object)
-    deprecated_output_json = seperate_warnings_by_location(deprecated_warnings)
-    output = {}
-    for k, v in deprecated_output_json.items():
-        output[k] = compress_similar_warnings(v)
-    return output
+            warnings_object[columns_index_dict["deprecated"]] = False
+    warnings_data = seperate_warnings_by_location(warnings_data)
+    compressed_warnings_data = compress_similar_warnings(warnings_data)
+    return compressed_warnings_data
 
 
-def sort_by_count(warnings_object):
-    temp_location_list = []
-    for location in warnings_object:
-        temp_warning_text_list = []
-        for warning_text in warnings_object[location]:
-            sorted_warnings = sorted(warnings_object[location][warning_text], key = lambda warning: -1*warning["num"])
-            num = sum([warning["num"] for warning in sorted_warnings])
-            temp_warning_text_list.append( (warning_text, sorted_warnings, num))
-        sorted_warning_texts = sorted(temp_warning_text_list, key = lambda warning_text_object: -1 * warning_text_object[-1])
-        num = sum([warning_text_object[-1] for warning_text_object in sorted_warning_texts])
-        temp_location_list.append([location, sorted_warning_texts, num])
-    sorted_locations = sorted(temp_location_list, key= lambda location_object: -1 * location_object[-1])          
-    return sorted_locations
 
+def group_and_sort_by_sumof(dataframe, group, sort_by):
+    groups_by = dataframe.groupby(group)
+    temp_list_to_sort = [(key,value,value[sort_by].sum()) for key, value in groups_by]
+        #sort by count
+    return sorted(temp_list_to_sort, key = lambda x: -x[2])
 
-def write_html_report(warnings_object, html_path):
-    """warning_object structured like:
-        {locations: { warning_texts:[warnings objects]}
-        }
+def write_html_report(warnings_dataframe, html_path):
+    """
     """
     html_path = os.path.expanduser(html_path)
-    with open(html_path, "w") as fout:
+    with io.open(html_path, "w") as fout:
         html_writer = HtmlOutlineWriter(fout)
-        for location in warnings_object:
-            html = u'<span class="count">{location}</span> '.format(location=location)
-            html_writer.start_section(html, klass="location")
-            for warning_text in warnings_object[location]:
-                html = u'<span class="count">{warning_text}</span> '.format(
-                    warning_text=warning_text
-                )
-                html_writer.start_section(html, klass="warning_text")
-                # warnings_object[location][warning_text] is a list
-                for warning in warnings_object[location][warning_text]:
-                    html = u'<span class="count">{warning_file_path}</span> '.format(
-                        warning_file_path=warning["filename"]
-                    )
-                    html_writer.start_section(html, klass="warning")
+        locations_sorted_by_count = group_and_sort_by_sumof(warnings_dataframe, "high_location", "num")
 
-                    html = u'<p class="lineno">lineno: {lineno}</p> '.format(
-                        lineno=warning["lineno"]
-                    )
-                    html_writer.write(html)
-                    html = u'<p class="num">num_occur: {num}</p> '.format(
-                        num=warning["num"]
-                    )
-                    html_writer.write(html)
-
-                    html_writer.end_section()
-                html_writer.end_section()
-            html_writer.end_section()
-
-
-def write_html_report_sorted_warnings(sorted_warnings_object, html_path):
-    """warning_object structured like:
-        {locations: { warning_texts:[warnings objects]}
-        }
-    """
-    html_path = os.path.expanduser(html_path)
-    with open(html_path, "w") as fout:
-        html_writer = HtmlOutlineWriter(fout)
-        for location in sorted_warnings_object:
+        for location, group_in_location, location_count in locations_sorted_by_count:
             # pp.pprint(location)
-            html = u'<span class="count">{location}, count: {count}</span> '.format(location=location[0],count=location[-1])
-            html_writer.start_section(html, klass="location")
-            for warning_text in location[1]:
+            html = u'<span class="count">{location}, count: {count}</span> '.format(location=location,count=location_count)
+            html_writer.start_section(html, klass=u"location")
+            message_group_sorted_by_count = group_and_sort_by_sumof(warnings_dataframe, "message", "num")
+            # pdb.set_trace()
+            for message, message_group, message_count in message_group_sorted_by_count:
                 # pp.pprint(warning_text)
                 html = u'<span class="count">{warning_text}, count: {count}</span> '.format(
-                    warning_text=warning_text[0], count=warning_text[-1]
+                    warning_text=message, count=message_count
                 )
-                html_writer.start_section(html, klass="warning_text")
+                html_writer.start_section(html, klass=u"warning_text")
                 # warnings_object[location][warning_text] is a list
-                for warning in warning_text[1]:
+                for index, warning in message_group.iterrows():
                     # pp.pprint(warning)
                     html = u'<span class="count">{warning_file_path}</span> '.format(
                         warning_file_path=warning["filename"]
                     )
-                    html_writer.start_section(html, klass="warning")
+                    html_writer.start_section(html, klass=u"warning")
 
                     html = u'<p class="lineno">lineno: {lineno}</p> '.format(
                         lineno=warning["lineno"]
@@ -248,7 +182,6 @@ def write_html_report_sorted_warnings(sorted_warnings_object, html_path):
                     html_writer.end_section()
                 html_writer.end_section()
             html_writer.end_section()
-
 
 parser = argparse.ArgumentParser(
     description="Process and categorize pytest warnings and output html report."
@@ -257,6 +190,6 @@ parser.add_argument("--dir_path", default="test_root/log")
 parser.add_argument("--html_path", default="test_html.html")
 args = parser.parse_args()
 output = process_warnings_json(args.dir_path)
-sorted_output = sort_by_count(output)
-write_html_report_sorted_warnings(sorted_output, args.html_path)
-# write_html_report(output, args.html_path)
+warnings_dataframe = pd.DataFrame(data = output, columns=columns)
+write_html_report(warnings_dataframe, args.html_path)
+
