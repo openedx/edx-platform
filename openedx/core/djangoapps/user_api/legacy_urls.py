461b11650e1db58f778ffb37dd9b64f1a5fc377d
Diana Huang <dkh@edx.org>
2019-12-04 13:13:07 -0500

    Move account_settings into user_api.

--- openedx/core/djangoapps/user_api/legacy_urls.py
+++ openedx/core/djangoapps/user_api/legacy_urls.py
@@ -9,6 +9,7 @@ from django.conf.urls import include, url
 from rest_framework import routers
 
 from . import views as user_api_views
+from .accounts.settings_views import account_settings
 from .models import UserPreference
 
 USER_API_ROUTER = routers.DefaultRouter()
@@ -16,23 +17,24 @@ USER_API_ROUTER.register(r'users', user_api_views.UserViewSet)
 USER_API_ROUTER.register(r'user_prefs', user_api_views.UserPreferenceViewSet)
 
 urlpatterns = [
-    url(r'^v1/', include(USER_API_ROUTER.urls)),
+    url(r'^account/settings$', account_settings, name='account_settings'),
+    url(r'^user_api/v1/', include(USER_API_ROUTER.urls)),
     url(
-        r'^v1/preferences/(?P<pref_key>{})/users/$'.format(UserPreference.KEY_REGEX),
+        r'^user_api/v1/preferences/(?P<pref_key>{})/users/$'.format(UserPreference.KEY_REGEX),
         user_api_views.PreferenceUsersListView.as_view()
     ),
     url(
-        r'^v1/forum_roles/(?P<name>[a-zA-Z]+)/users/$',
+        r'^user_api/v1/forum_roles/(?P<name>[a-zA-Z]+)/users/$',
         user_api_views.ForumRoleUsersListView.as_view()
     ),
 
     url(
-        r'^v1/preferences/email_opt_in/$',
+        r'^user_api/v1/preferences/email_opt_in/$',
         user_api_views.UpdateEmailOptInPreference.as_view(),
         name="preferences_email_opt_in"
     ),
     url(
-        r'^v1/preferences/time_zones/$',
+        r'^user_api/v1/preferences/time_zones/$',
         user_api_views.CountryTimeZoneListView.as_view(),
     ),
 ]
