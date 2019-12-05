684f254a7764f5bdc8de122232483a75c81ab7ac
Feanil Patel <feanil@edx.org>
2019-12-05 14:37:18 -0500

    Add error handling in course structure cache.
    When going between python 2 and python 3.5 we can get pickeld course
    structires that are incompatible no matter what we do do to the bug
    linked in the comment.  In this case, handle the error and delete the
    corrupt data from the cache.
    
    Making this fairly generic because if we have any bad data in cache we
    don't want it to blow up the whole process.  Just delete the bad data
    and try again.

--- common/lib/xmodule/xmodule/modulestore/tests/test_split_modulestore.py
+++ common/lib/xmodule/xmodule/modulestore/tests/test_split_modulestore.py
@@ -977,6 +977,13 @@ class TestCourseStructureCache(SplitModuleTest):
         # now make sure that you get the same structure
         self.assertEqual(cached_structure, not_cached_structure)
 
+        # If data is corrupted, get it from mongo again.
+        cache_key = self.new_course.id.version_guid
+        self.cache.set(cache_key, b"bad_data")
+        with check_mongo_calls(1):
+            not_cached_structure = self._get_structure(self.new_course)
+
+
     @patch('xmodule.modulestore.split_mongo.mongo_connection.get_cache')
     def test_course_structure_cache_no_cache_configured(self, mock_get_cache):
         mock_get_cache.side_effect = InvalidCacheBackendError
