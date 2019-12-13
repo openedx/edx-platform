f0344fd287048db5ac112e53fe9c78f6afcfbf29
jansenk <jkantor@edx.org>
2019-12-13 13:37:17 -0500

    add team detail url function

--- lms/djangoapps/teams/services.py
+++ lms/djangoapps/teams/services.py
@@ -1,8 +1,19 @@
 """ Services to expose the Teams API to XBlocks """
 from __future__ import absolute_import
 
+from django.urls import reverse
+
 
 class TeamsService(object):
     def get_team(self, user, course_id):
         from . import api
         return api.get_team_for_user_and_course(user, course_id)
+
+    def get_team_detail_url(self, team):
+        teams_dashboard_url = reverse('teams_dashboard', kwargs={'course_id': team.course_id})
+        # Unfortunately required since this URL resolution is done in a Backbone view
+        return "{teams_dashboard_url}#teams/{topic_id}/{team_id}".format(
+            teams_dashboard_url=teams_dashboard_url,
+            topic_id=team.topic_id,
+            team_id=team.team_id,
+        )
