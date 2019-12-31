c610231670a95670993c1c8a79697a80d763f575
Omar Al-Ithawi <i@omardo.com>
2019-12-31 20:32:33 +0300

    Sort AccessControlBackends.SUPPORTED_ACTIONS
    Avoid git merge conflict

--- lms/lib/access_control_backends.py
+++ lms/lib/access_control_backends.py
@@ -18,14 +18,14 @@ class AccessControlBackends(object):
     Meant to be instantiated by this module, so use the `access_control_backends` object.
     """
     SUPPORTED_ACTIONS = {
+        'course.enroll',
+        'course.instructor',
         'course.load',
         'course.load_mobile',
-        'course.enroll',
-        'course.see_exists',
-        'course.staff',
-        'course.instructor',
-        'course.see_in_catalog',
         'course.see_about_page',
+        'course.see_exists',
+        'course.see_in_catalog',
+        'course.staff',
     }
     UNSUPPORTED_ERROR_FMT = '`AccessControlBackends` does not support the action `{action}` yet'.format
 
