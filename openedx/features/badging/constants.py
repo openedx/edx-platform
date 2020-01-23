561f56e18b1480744256336999dc15e86407612c
Usman Khan <usman.khan@arbisoft.com>
2020-01-23 14:28:21 +0500

    Resolve issues suggested in PR review

--- openedx/features/badging/constants.py
+++ openedx/features/badging/constants.py
@@ -2,11 +2,11 @@ CONVERSATIONALIST = ('conversationalist', 'Conversationalist')
 TEAM_PLAYER = ('team', 'Team player')
 
 BADGES_KEY = 'badges'
-TEAM_ID_KEY = 'team_id'
-TEAM_ROOM_ID_KEY = 'team__room_id'
 BADGE_NOT_FOUND_ERROR = 'There exists no badge with id {badge_id}'
 BADGE_TYPE_ERROR = 'Cannot assign badge {badge_id} of unknown type {badge_type}'
 FILTER_BADGES_ERROR = 'Unable to get badges for team {team_id}'
 INVALID_COMMUNITY_ERROR = 'Cannot assign badge {badge_id} for invalid community {community_id}'
 INVALID_TEAM_ERROR = 'Cannot assign badge {badge_id} for invalid team {community_id}'
+TEAM_ID_KEY = 'team_id'
+TEAM_ROOM_ID_KEY = 'team__room_id'
 UNKNOWN_COURSE_ERROR = 'Cannot assign badge {badge_id} for team {community_id} in unknown course'
