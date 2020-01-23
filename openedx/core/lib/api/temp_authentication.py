5d98dcb1c58f6b0561fb5446fa91c1d21d6cef6b
jinder1s <msingh@edx.org>
2020-01-23 14:31:59 -0500

    Added new authentication class
    And testing it in credit

---
+++ openedx/core/lib/api/temp_authentication.py
@@ -0,0 +1,100 @@
+""" Common Authentication Handlers used across projects. """
+
+
+import logging
+
+import django.utils.timezone
+from oauth2_provider import models as dot_models
+from provider.oauth2 import models as dop_models
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework_oauth.authentication import OAuth2Authentication
+
+
+OAUTH2_TOKEN_ERROR = u'token_error'
+OAUTH2_TOKEN_ERROR_EXPIRED = u'token_expired'
+OAUTH2_TOKEN_ERROR_MALFORMED = u'token_malformed'
+OAUTH2_TOKEN_ERROR_NONEXISTENT = u'token_nonexistent'
+OAUTH2_TOKEN_ERROR_NOT_PROVIDED = u'token_not_provided'
+
+
+log = logging.getLogger(__name__)
+
+
+class EdxOAuth2Authentication(BaseAuthentication):
+    """
+    This is created to be a drop in replacement for django-rest-framework-oauth oauth2Authentication class.
+    This is based on NOAuth2AuthenticationAllowINactiveUsers
+    """
+
+    def authenticate(self, request):
+        """
+        Returns two-tuple of (user, token) if access token authentication
+        succeeds, raises an AuthenticationFailed (HTTP 401) if authentication
+        fails or None if the user did not try to authenticate using an access
+        token.
+        """
+
+        auth = get_authorization_header(request).split()
+        if not auth or auth[0].lower() != b'bearer':
+            logger.warning("auth is empty")
+            return None
+
+        if len(auth) == 1:
+            raise exceptions.AuthenticationFailed(
+                u'error_code': OAUTH2_TOKEN_ERROR_NOT_PROVIDED,
+                u'developer_message': 'Invalid token header. No credentials provided.')
+        elif len(auth) > 2:
+            raise exceptions.AuthenticationFailed(
+                u'error_code':  OAUTH2_TOKEN_ERROR_MALFORMED,
+                u'developer_message': 'Invalid token header. Token string should not contain spaces.')
+        return self.authenticate_credentials(request, auth[1].decode('utf8'))
+
+    def authenticate_credentials(self, request, access_token):
+        """
+        Authenticate the request, given the access token.
+
+        Overrides base class implementation to discard failure if user is
+        inactive.
+        """
+
+        token = self.get_access_token(access_token)
+        if not token:
+            raise AuthenticationFailed({
+                u'error_code': OAUTH2_TOKEN_ERROR_NONEXISTENT,
+                u'developer_message': u'The provided access token does not match any valid tokens.'
+            })
+        elif token.expires < django.utils.timezone.now():
+            raise AuthenticationFailed({
+                u'error_code': OAUTH2_TOKEN_ERROR_EXPIRED,
+                u'developer_message': u'The provided access token has expired and is no longer valid.',
+            })
+        else:
+            user = token.user
+            if not user.is_active:
+                msg = 'User inactive or deleted: %s' % user.get_username()
+                raise exceptions.AuthenticationFailed(msg)
+            return user, token
+
+    def get_access_token(self, access_token):
+        """
+        Return a valid access token that exists in one of our OAuth2 libraries,
+        or None if no matching token is found.
+        """
+        return self._get_dot_token(access_token) or self._get_dop_token(access_token)
+
+    def _get_dop_token(self, access_token):
+        """
+        Return a valid access token stored by django-oauth2-provider (DOP), or
+        None if no matching token is found.
+        """
+        token_query = dop_models.AccessToken.objects.select_related('user')
+        return token_query.filter(token=access_token).first()
+
+    def _get_dot_token(self, access_token):
+        """
+        Return a valid access token stored by django-oauth-toolkit (DOT), or
+        None if no matching token is found.
+        """
+        token_query = dot_models.AccessToken.objects.select_related('user')
+        return token_query.filter(token=access_token).first()
