d4f6b42a7586afec3ef40c9bad5514dc3388a9ae
jinder1s <msingh@edx.org>
2020-01-24 15:19:06 -0500

    fix authenticate header

--- openedx/core/lib/api/temp_authentication.py
+++ openedx/core/lib/api/temp_authentication.py
@@ -25,7 +25,7 @@ class OAuth2Authentication(BaseAuthentication):
     This is created to be a drop in replacement for django-rest-framework-oauth oauth2Authentication class.
     This is based on NOAuth2AuthenticationAllowINactiveUsers
     """
-
+    www_authenticate_realm = 'api'
     def authenticate(self, request):
         """
         Returns two-tuple of (user, token) if access token authentication
@@ -97,3 +97,9 @@ class OAuth2Authentication(BaseAuthentication):
         """
         token_query = dot_models.AccessToken.objects.select_related('user')
         return token_query.filter(token=access_token).first()
+    def authenticate_header(self, request):
+        """
+        Bearer is the only finalized type currently
+        Check details on the `OAuth2Authentication.authenticate` method
+        """
+        return 'Bearer realm="%s"' % self.www_authenticate_realm
