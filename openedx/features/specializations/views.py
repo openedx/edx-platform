4f52e69a8a7456f970ca0ae82d361711be701222
Hamza Jamal <hamzajamal@A006-00254.local>
2020-01-24 19:49:16 +0500

    removing unnecessary comment

--- openedx/features/specializations/views.py
+++ openedx/features/specializations/views.py
@@ -2,9 +2,4 @@ from edxmako.shortcuts import render_to_response
 
 
 def list_specializations(request):
-
-    """
-    :param request:
-    :return: list of active cards
-    """
     return render_to_response('features/specializations/list.html', {})
