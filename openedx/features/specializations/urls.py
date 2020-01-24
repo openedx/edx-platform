4321686c0527b2cb26ca277d0f37ce2ac3b11eff
Hamza Jamal <hamzajamal@A006-00254.local>
2020-01-24 19:36:20 +0500

    removed unnecessary constant

--- openedx/features/specializations/urls.py
+++ openedx/features/specializations/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 
 from .views import list_specializations
 
-PARTNERS_SLUG_PARAM = '(?P<slug>[0-9a-z_-]+)'
-
 urlpatterns = [
     # Please keep the `partners/reset_password/` on top
     url(r'^specializations/$', list_specializations, name='list_specializations'),
