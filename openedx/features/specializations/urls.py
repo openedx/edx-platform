833d87f00b0065f6e1abccb652fa4b65ad182f58
Hamza Jamal <hamzajamal@A006-00254.local>
2020-01-24 19:45:41 +0500

    removing the comment

--- openedx/features/specializations/urls.py
+++ openedx/features/specializations/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from .views import list_specializations
 
 urlpatterns = [
-    # Please keep the `partners/reset_password/` on top
     url(r'^specializations/$', list_specializations, name='list_specializations'),
 ]
