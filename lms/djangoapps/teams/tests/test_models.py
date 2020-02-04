782a4c42e9fe0bcbe6730fdbf229a06cc9ae20eb
jansenk <jkantor@edx.org>
2020-02-04 15:57:06 -0500

    remove duplicate enrollment check function

--- lms/djangoapps/teams/tests/test_models.py
+++ lms/djangoapps/teams/tests/test_models.py
@@ -180,10 +180,10 @@ class TeamMembershipTest(SharedModuleStoreTestCase):
         ('user2', COURSE_KEY2, TEAMSET_1_ID, False),
     )
     @ddt.unpack
-    def test_user_in_team_set_for_course(self, username, course_id, teamset_id, expected_value):
+    def test_user_in_team_for_course_teamset(self, username, course_id, teamset_id, expected_value):
         user = getattr(self, username)
         self.assertEqual(
-            CourseTeamMembership.user_in_team_set_for_course(user, course_id, teamset_id),
+            CourseTeamMembership.user_in_team_for_course(user, course_id, teamset_id),
             expected_value
         )
 
