a5b74d882620efff6f4db14231b238a4c6b4299a
Alex Dusenbery <adusenbery@edx.org>
2020-02-11 16:47:21 -0500

    stub out a little meetings_api.py file.

---
+++ lms/djangoapps/teams/meetings_api.py
@@ -0,0 +1,11 @@
+"""
+A module to do all of the interactions with Chime, like:
+- get/list meetings under our account.
+- create new meetings
+- list attendees
+- create attendees
+
+Any of the creation actions above should result in
+an associated creation of either CourseTeamMeetings
+or CourseTeamMeetingAttendees.
+"""
