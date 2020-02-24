b22beab74f0a603b14ba294028739b1b78f034f4
Muhammad Nofal Fayyaz <54840954+monofal@users.noreply.github.com>
2020-02-24 17:39:46 +0500

    Partner feature unit tests (#1389)
    * Adding unit tests for g2a partner.

---
+++ openedx/features/partners/give2asia/tests/test_views.py
@@ -0,0 +1,174 @@
+from ddt import data, ddt
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from lms.djangoapps.onboarding.models import Organization, UserExtendedProfile
+from openedx.core.lib.api.test_utils import ApiTestCase
+from openedx.features.partners.tests.factories import FocusAreaFactory, OrganizationFactory, PartnerFactory
+
+
+@ddt
+class G2ARegistrationViewTest(ApiTestCase):
+    """
+    Includes test cases for g2a registration
+    """
+
+    NAME = 'bob james'
+    COUNTRY = 'Pakistan'
+    ORGANIZATION = 'arbisoft'
+    EMAIL = 'bob@example.com'
+    USERNAME = 'bob'
+    PASSWORD = 'Test@12345'
+    TERMS_OF_SERVICE = True
+
+    PARTNER = 'give2asia'
+
+    def setUp(self):
+        self.partner = PartnerFactory.create(slug=self.PARTNER, label=self.ORGANIZATION)
+        self.registration_url = reverse('partner_register', args=[self.PARTNER])
+
+    def test_put_not_allowed(self):
+        """
+        API should not accept http put method
+        :return : None
+        """
+        response = self.client.put(self.registration_url)
+        self.assertHttpMethodNotAllowed(response)
+
+    def test_delete_not_allowed(self):
+        """
+        API should not accept http delete method
+        :return : None
+        """
+        response = self.client.delete(self.registration_url)
+        self.assertHttpMethodNotAllowed(response)
+
+    def test_create_new_partner_user_with_organization(self):
+        """
+        Test user is not first learner if organization already exists
+        Create an organization and then try to register.
+        :return : None
+        """
+        OrganizationFactory(label=self.ORGANIZATION)
+
+        response = self.client.post(self.registration_url, {
+            'name': self.NAME,
+            'organization_name': self.ORGANIZATION,
+            'country': self.COUNTRY,
+            'email': self.EMAIL,
+            'username': self.USERNAME,
+            'password': self.PASSWORD,
+            'terms_of_service': self.TERMS_OF_SERVICE
+        })
+        self.assertHttpOK(response)
+
+        # get inserted data from the database
+        user = User.objects.filter(username=self.USERNAME).first()
+        organization = Organization.objects.filter(label__iexact=self.ORGANIZATION).first()
+        extended_profile = UserExtendedProfile.objects.filter(user=user).first()
+
+        # verify if user is registered and data is stored in the database
+        self.assertIsNotNone(user)
+        self.assertEqual(self.USERNAME, user.username)
+        self.assertEqual(self.EMAIL, user.email)
+        self.assertEqual(self.NAME.split(' ', 1)[0], user.first_name)
+        self.assertEqual(self.NAME.split(' ', 1)[1], user.last_name)
+        self.assertTrue(user.is_active)
+        self.assertEqual(organization.label, organization.label)
+        # in case organization already exists make sure that user is not first learner
+        self.assertFalse(extended_profile.is_first_learner)
+        self.assertHttpOK(response)
+
+    def test_create_new_partner_user_with_new_organization(self):
+        """
+        Test user is first learner if new organization is added
+        If organization does not exists system will create a new
+        one and add user to it and mark the user as a new learner.
+        :return : None
+        """
+        # create focus area with default values
+        FocusAreaFactory()
+
+        response = self.client.post(self.registration_url, {
+            'name': self.NAME,
+            'organization_name': self.ORGANIZATION,
+            'country': self.COUNTRY,
+            'email': self.EMAIL,
+            'username': self.USERNAME,
+            'password': self.PASSWORD,
+            'terms_of_service': self.TERMS_OF_SERVICE
+        })
+        self.assertHttpOK(response)
+
+        # get inserted data from the database
+        user = User.objects.filter(username=self.USERNAME).first()
+        organization = Organization.objects.filter(label__iexact=self.ORGANIZATION).first()
+        extended_profile = UserExtendedProfile.objects.filter(user=user).first()
+
+        # verify if user is registered and data is stored in the database
+        self.assertIsNotNone(user)
+        self.assertEqual(self.USERNAME, user.username)
+        self.assertEqual(self.EMAIL, user.email)
+        self.assertEqual(self.NAME.split(' ', 1)[0], user.first_name)
+        self.assertEqual(self.NAME.split(' ', 1)[1], user.last_name)
+        self.assertTrue(user.is_active)
+        self.assertEqual(organization.label, organization.label)
+        # in case of new organization make sure that user is created as first learner
+        self.assertTrue(extended_profile.is_first_learner)
+        self.assertHttpOK(response)
+
+    @data(
+        {'name': ''},
+        {'country': ''},
+        {'email': ''},
+        {'username': ''},
+        {'password': 'invalid'},
+        {'terms_of_service': ''}
+    )
+    def test_register_invalid_input(self, invalid_fields):
+        """
+        Test registration is failed if invalid user data is provided
+        :param invalid_fields: field to be override with invalid data
+        :return: None
+        """
+        # Initially, the field values are all valid
+        form_data = {
+            'name': self.NAME,
+            'organization_name': self.ORGANIZATION,
+            'country': self.COUNTRY,
+            'email': self.EMAIL,
+            'username': self.USERNAME,
+            'password': self.PASSWORD,
+            'terms_of_service': self.TERMS_OF_SERVICE
+        }
+        # Override the valid fields, making the input invalid
+        form_data.update(invalid_fields)
+
+        # Attempt to create the partner user, expecting an error response
+        response = self.client.post(self.registration_url, form_data)
+        self.assertHttpBadRequest(response)
+
+    @data('username', 'country', 'password', 'email', 'terms_of_service')
+    def test_register_missing_required_field(self, missing_field):
+        """
+        Test registration is failed if one of required field is missing
+        :param missing_field: field to be deleted from input data
+        :return: None
+        """
+        form_data = {
+            'username': self.USERNAME,
+            'password': self.PASSWORD,
+            'email': self.EMAIL,
+            'name': self.NAME,
+            'country': self.COUNTRY,
+            'organization_name': self.ORGANIZATION,
+            'terms_of_service': self.TERMS_OF_SERVICE
+        }
+
+        # delete required field
+        if missing_field in form_data:
+            del form_data[missing_field]
+
+        # Send a request with missing field
+        response = self.client.post(self.registration_url, form_data)
+        self.assertHttpBadRequest(response)
---
+++ openedx/features/partners/tests/factories.py
@@ -0,0 +1,91 @@
+import factory
+from factory.django import DjangoModelFactory
+from faker.providers import internet
+
+from custom_settings.models import CustomSettings
+from lms.djangoapps.onboarding.models import FocusArea, Organization
+from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
+from openedx.features.course_card.models import CourseCard
+from openedx.features.partners.models import Partner, PartnerCommunity, PartnerUser
+from student.tests.factories import UserFactory
+
+factory.Faker.add_provider(internet)
+
+
+class PartnerFactory(DjangoModelFactory):
+    class Meta:
+        model = Partner
+
+    performance_url = factory.Faker('url')
+    label = factory.Faker('name')
+    slug = factory.Faker('slug')
+    main_logo = 'dummy'
+    small_logo = 'dummy'
+
+
+class PartnerUserFactory(DjangoModelFactory):
+    class Meta:
+        model = PartnerUser
+
+    partner = factory.SubFactory(PartnerFactory)
+    user = factory.SubFactory(UserFactory)
+
+
+class CustomSettingsFactory(DjangoModelFactory):
+    class Meta:
+        model = CustomSettings
+
+    is_featured = True
+    show_grades = False
+    enable_enrollment_email = True
+    auto_enroll = True
+    tags = factory.Faker('word')
+    course_short_id = factory.Faker('random_int')
+    seo_tags = factory.Faker('word')
+    course_open_date = factory.Faker('date_time')
+
+
+class PartnerCourseOverviewFactory(CourseOverviewFactory):
+    display_name = factory.Faker('word')
+    # enrollment start date is less than current date
+    enrollment_start = factory.Faker('date_time_between', start_date='-10d', end_date='now')
+    # enrollment end date greater than current date
+    enrollment_end = factory.Faker('date_time_between', start_date='+1d', end_date='+10d')
+
+
+class CourseCardFactory(DjangoModelFactory):
+    class Meta:
+        model = CourseCard
+
+    is_enabled = True
+    course_name = factory.Faker('word')
+
+
+class PartnerCommunityFactory(DjangoModelFactory):
+    class Meta:
+        model = PartnerCommunity
+
+    community_id = factory.Sequence(lambda n: n)
+
+
+class FocusAreaFactory(DjangoModelFactory):
+    class Meta:
+        model = FocusArea
+
+    order = factory.Sequence(lambda n: n)
+    code = 'IWRNS'
+    label = factory.Faker('word')
+
+
+class OrganizationFactory(DjangoModelFactory):
+    class Meta(object):
+        model = Organization
+        django_get_or_create = ('label',)
+
+    label = factory.Sequence(u'Organization{0}'.format)
+    country = factory.Faker('country')
+    city = factory.Faker('city')
+    founding_year = factory.Faker('random_int')
+    org_type = 'IWRNS'
+    level_of_operation = 'dummy'
+    total_employees = 'dummy'
