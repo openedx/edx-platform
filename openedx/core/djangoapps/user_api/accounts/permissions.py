5e8b4faa0518d5358da66018ae0e18c40f0473e2
Julia Eskew <jeskew@edx.org>
2020-03-03 12:00:12 -0500

    Fix docstring to reflect the current way authz works in the method.

--- openedx/core/djangoapps/user_api/accounts/permissions.py
+++ openedx/core/djangoapps/user_api/accounts/permissions.py
@@ -18,9 +18,8 @@ class CanDeactivateUser(permissions.BasePermission):
 
 class CanRetireUser(permissions.BasePermission):
     """
-    Grants access to the various retirement API endpoints if the requesting user is
-    a superuser, the RETIREMENT_SERVICE_USERNAME, or has the explicit permission to
-    retire a User account.
+    Grants access to the various retirement API endpoints if the requesting user
+    has the explicit permission to retire a User account.
     """
     def has_permission(self, request, view):
         return request.user.has_perm('accounts.can_retire_user')
