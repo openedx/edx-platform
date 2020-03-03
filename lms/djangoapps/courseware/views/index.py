2a90c45120b0518697dac7cbf0f27a5a5028d752
Dave St.Germain <dstgermain@edx.org>
2020-03-03 13:57:53 -0500

    When we create links based on the activate_unit_id request variable, we should make sure that it's actually a unit id. Otherwise, the link to the new courseware MFE might erroneously load a sequence inside a sequence.

--- lms/djangoapps/courseware/views/index.py
+++ lms/djangoapps/courseware/views/index.py
@@ -482,7 +482,12 @@ class CoursewareIndex(View):
                 table_of_contents['previous_of_active_section'],
                 table_of_contents['next_of_active_section'],
             )
-            courseware_context['unit'] = section_context.get('activate_block_id', '')
+            possible_unit = section_context.get('activate_block_id', '') or ''
+            # technically, this could be any block id (typically, the id of the current subsection),
+            # so we want to be sure to only pass through a unit because it'll be used to create a link
+            # to the new courseware MFE unit
+            if 'type@vertical' in possible_unit:
+                courseware_context['unit'] = possible_unit
             courseware_context['fragment'] = self.section.render(self.view, section_context)
 
             if self.section.position and self.section.has_children:
