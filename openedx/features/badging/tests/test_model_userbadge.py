9acbe71a22c36fee0436a4d3ba9c2c7e0ed78ccc
Zahid <31304505+zahid-arbisoft@users.noreply.github.com>
2020-03-04 13:42:58 +0500

    LP-1602 Unit test for sending email when user earn badge (#1397)

--- openedx/features/badging/tests/test_model_userbadge.py
+++ openedx/features/badging/tests/test_model_userbadge.py
@@ -1,20 +1,16 @@
 import factory
 import mock
-
 from django.db.models import signals
 from django.db.utils import IntegrityError
-from unittest import TestCase
+from django.test import TestCase
+from opaque_keys.edx.keys import CourseKey
 
 from lms.djangoapps.teams.tests.factories import CourseTeamFactory, CourseTeamMembershipFactory
-from nodebb.constants import (
-    TEAM_PLAYER_ENTRY_INDEX,
-    CONVERSATIONALIST_ENTRY_INDEX
-)
-from opaque_keys.edx.keys import CourseKey
-from openedx.features.teams.tests.factories import TeamGroupChatFactory
-from openedx.features.badging.constants import CONVERSATIONALIST, TEAM_PLAYER
+from nodebb.constants import CONVERSATIONALIST_ENTRY_INDEX, TEAM_PLAYER_ENTRY_INDEX
 from openedx.core.djangoapps.xmodule_django.models import CourseKeyField
+from openedx.features.badging.constants import CONVERSATIONALIST, TEAM_PLAYER
 from openedx.features.badging.models import UserBadge
+from openedx.features.teams.tests.factories import TeamGroupChatFactory
 from student.tests.factories import UserFactory
 
 from .. import constants as badge_constants
@@ -27,34 +23,32 @@ class UserBadgeModelTestCases(TestCase):
         self.type_conversationalist = CONVERSATIONALIST[CONVERSATIONALIST_ENTRY_INDEX]
         self.type_team = TEAM_PLAYER[TEAM_PLAYER_ENTRY_INDEX]
         self.team_badge = BadgeFactory(type=self.type_team)
+        self.user = UserFactory()
 
-    def test_model_userbadge(self):
-        """
-        Trying to save a UserBadge model with expected arguments
-        """
+    def test_model_user_badge(self):
+        """Save UserBadge model with expected arguments"""
         course_key = CourseKey.from_string('abc/123/course')
-        userbadge_factory = UserBadgeFactory(course_id=course_key)
-        userbadge_query = UserBadge.objects.get(course_id=course_key)
 
-        self.assertEqual(userbadge_query.badge, userbadge_factory.badge)
-        self.assertEqual(userbadge_query.community_id, 1)
-        self.assertEqual(userbadge_query.course_id, course_key)
-        self.assertEqual(userbadge_query.user, userbadge_factory.user)
+        user_badge_factory = UserBadgeFactory(course_id=course_key)
 
-    def test_save_duplicate_badge(self):
+        user_badge_query = UserBadge.objects.get(course_id=course_key)
+        self.assertEqual(user_badge_query.badge, user_badge_factory.badge)
+        self.assertEqual(user_badge_query.community_id, 1)
+        self.assertEqual(user_badge_query.course_id, course_key)
+        self.assertEqual(user_badge_query.user, user_badge_factory.user)
+
+    def test_model_user_badge_by_saving_duplicate_badge(self):
         """
         Trying to save a duplicate UserBadge object with expected arguments
         Raises IntegrityError upon trying to save the second object with
         the same arguments
         """
-        user = UserFactory.create()
-
-        UserBadgeFactory(user=user, badge=self.team_badge)
+        UserBadgeFactory(user=self.user, badge=self.team_badge)
 
         with self.assertRaises(IntegrityError):
-            UserBadgeFactory(user=user, badge=self.team_badge)
+            UserBadgeFactory(user=self.user, badge=self.team_badge)
 
-    def test_community_id_None(self):
+    def test_model_user_badge_with_community_id_none(self):
         """
         Trying to save UserBadge object with community_id as None
         """
@@ -71,7 +65,7 @@ class UserBadgeModelTestCases(TestCase):
         self.assertEqual(str(error.exception), badge_constants.BADGE_NOT_FOUND_ERROR.format(badge_id=-1))
 
     @mock.patch('openedx.features.badging.models.get_course_id_by_community_id')
-    def test_assign_conversationalist_badge_with_invalid_community_id(self, mock_get_community_id):
+    def test_assign_badge_conversationalist_with_invalid_community_id(self, mock_get_community_id):
         """
         Assign conversationalist badge for community which do not exist
         """
@@ -79,33 +73,33 @@ class UserBadgeModelTestCases(TestCase):
         badge = BadgeFactory(type=self.type_conversationalist)
 
         with self.assertRaises(Exception) as error:
-            UserBadge.assign_badge(user_id=1, badge_id=badge.id, community_id=-1)
+            UserBadge.assign_badge(user_id=1, badge_id=badge.id, community_id=999)
 
         self.assertEqual(
             str(error.exception),
-            badge_constants.INVALID_COMMUNITY_ERROR.format(badge_id=badge.id, community_id=-1)
+            badge_constants.INVALID_COMMUNITY_ERROR.format(badge_id=badge.id, community_id=999)
         )
 
-    def test_assign_team_badge_with_invalid_community_id_and_no_team_group_chat(self):
+    def test_assign_badge_team_with_invalid_community_id_and_no_team_group_chat(self):
         """
         Assign team badge for team group chat which do not exist
         """
         with self.assertRaises(Exception) as error:
-            UserBadge.assign_badge(user_id=1, badge_id=self.team_badge.id, community_id=-1)
+            UserBadge.assign_badge(user_id=self.user.id, badge_id=self.team_badge.id, community_id=999)
 
         self.assertEqual(
             str(error.exception),
-            badge_constants.INVALID_TEAM_ERROR.format(badge_id=self.team_badge.id, community_id=-1)
+            badge_constants.INVALID_TEAM_ERROR.format(badge_id=self.team_badge.id, community_id=999)
         )
 
-    def test_unknown_badge_type(self):
+    def test_assign_badge_unknown_badge_type(self):
         """
         Assign badge for type which does not exists
         """
         badge = BadgeFactory(type='custom_type')
 
         with self.assertRaises(Exception) as error:
-            UserBadge.assign_badge(user_id=1, badge_id=badge.id, community_id=-1)
+            UserBadge.assign_badge(user_id=self.user.id, badge_id=badge.id, community_id=999)
 
         self.assertEqual(
             str(error.exception),
@@ -113,15 +107,15 @@ class UserBadgeModelTestCases(TestCase):
         )
 
     @factory.django.mute_signals(signals.pre_save, signals.post_save)
-    def test_assign_team_badge_invalid_course_id(self):
+    def test_assign_badge_for_team_with_invalid_course_id(self):
         """
-        Assign team badge with invalid course id
+        Assign team badge with invalid course id, and assert that corresponding error is raised
         """
         course_team = CourseTeamFactory()
         team_group_chat = TeamGroupChatFactory(team=course_team, room_id=100)
 
         with self.assertRaises(Exception) as error:
-            UserBadge.assign_badge(user_id=UserFactory(), badge_id=self.team_badge.id,
+            UserBadge.assign_badge(user_id=self.user.id, badge_id=self.team_badge.id,
                                    community_id=team_group_chat.room_id)
 
         self.assertEqual(
@@ -130,48 +124,99 @@ class UserBadgeModelTestCases(TestCase):
                                                         community_id=team_group_chat.room_id)
         )
 
-    @factory.django.mute_signals(signals.pre_save, signals.post_save)
-    def test_assign_team_badge_successfully(self):
-        """
-        Trying to save a UserBadge object assigning a team badge successfully
-        """
-        user = UserFactory()
-        course_key = CourseKey.from_string('test/course/123')
-
-        course_team = CourseTeamFactory(course_id=course_key, team_id='team1')
-
-        # Add two users to team, in course
-        CourseTeamMembershipFactory(user=user, team=course_team)
-        CourseTeamMembershipFactory(user=UserFactory(), team=course_team)
-
-        team_group_chat = TeamGroupChatFactory(team=course_team, room_id=200)
-
-        UserBadge.assign_badge(user_id=user.id, badge_id=self.team_badge.id, community_id=team_group_chat.room_id)
-
-        assigned_user_badges = UserBadge.objects.filter(
-            badge_id=self.team_badge.id,
-            course_id=course_key,
-            community_id=team_group_chat.room_id
-        )
-
-        self.assertEqual(len(assigned_user_badges), 2)
-
+    @mock.patch('openedx.features.badging.models.task_user_badge_notify')
     @mock.patch('openedx.features.badging.models.get_course_id_by_community_id')
-    def test_assign_conversationalist_badge_successfully(self, mock_course_id_by_community_id):
+    def test_assign_badge_conversationalist_to_user_successfully(self, mock_course_id_by_community_id,
+                                                                 mock_task_user_badge_notify):
         """
-        Trying to save a UserBadge object assigning a conversationalist badge successfully
+        Assign UserBadge for conversationalist badge successfully, and assert that email and notification
+        handler called
         """
         course_key = CourseKey.from_string('abc/xyz/123')
         mock_course_id_by_community_id.return_value = course_key
-
         badge = BadgeFactory(type=self.type_conversationalist)
-        user = UserFactory()
+        user = self.user
 
-        UserBadge.assign_badge(user_id=user.id, badge_id=badge.id, community_id=1)
+        assigned = UserBadge.assign_badge(user_id=user.id, badge_id=badge.id, community_id=100)
 
         user_badge = UserBadge.objects.get(user_id=user.id, badge_id=badge.id)
-
         self.assertEqual(user_badge.badge, badge)
-        self.assertEqual(user_badge.community_id, 1)
+        self.assertEqual(user_badge.community_id, 100)
         self.assertEqual(user_badge.course_id, course_key)
         self.assertEqual(user_badge.user, user)
+        self.assertTrue(assigned)
+        mock_task_user_badge_notify.assert_has_calls([
+            mock.call(user, course_key, badge.name)
+        ])
+
+
+class AssignBadgeToTeamTestCases(TestCase):
+
+    @factory.django.mute_signals(signals.pre_save, signals.post_save)
+    def setUp(self):
+        self.team_badge = BadgeFactory(type=TEAM_PLAYER[TEAM_PLAYER_ENTRY_INDEX])
+
+        self.user_1 = UserFactory()
+        self.user_2 = UserFactory()
+
+        self.course_key = CourseKey.from_string(u'test/course/123')
+        self.course_team = CourseTeamFactory(course_id=self.course_key, team_id='team1')
+
+        # Add two users to team, in course
+        CourseTeamMembershipFactory(user=self.user_1, team=self.course_team)
+        CourseTeamMembershipFactory(user=self.user_2, team=self.course_team)
+        self.team_group_chat = TeamGroupChatFactory(team=self.course_team, room_id=200)
+
+    @mock.patch('openedx.features.badging.models.task_user_badge_notify')
+    def test_assign_badge_to_team_successfully(self, mock_task_user_badge_notify):
+        """
+        Assign team badge to all team members successfully and assert that
+        email and notification task called for each member
+        """
+        assigned_to_all = UserBadge.assign_badge(
+            user_id=self.user_1.id,
+            badge_id=self.team_badge.id,
+            community_id=self.team_group_chat.room_id
+        )
+
+        assigned_user_badges = UserBadge.objects.filter(
+            badge_id=self.team_badge.id,
+            course_id=self.course_key,
+            community_id=self.team_group_chat.room_id
+        )
+        self.assertEqual(len(assigned_user_badges), 2)
+        self.assertTrue(assigned_to_all)
+        mock_task_user_badge_notify.assert_has_calls([
+            mock.call(self.user_1, self.course_key, self.team_badge.name),
+            mock.call(self.user_2, self.course_key, self.team_badge.name),
+        ], any_order=True)
+
+    @mock.patch('openedx.features.badging.models.task_user_badge_notify')
+    def test_assign_badge_when_one_team_member_already_have_badge(self, mock_task_user_badge_notify):
+        """
+        Assign team badge to all team members successfully, except one member who has already earned
+        team badge. Assert that email and notification task called for only those members who earned
+        badge for the first time
+        """
+        UserBadgeFactory(
+            user=self.user_1, course_id=self.course_key,
+            community_id=self.team_group_chat.room_id,
+            badge=self.team_badge
+        )
+
+        assigned_to_all = UserBadge.assign_badge(
+            user_id=self.user_1.id,
+            badge_id=self.team_badge.id,
+            community_id=self.team_group_chat.room_id
+        )
+
+        assigned_user_badges = UserBadge.objects.filter(
+            badge_id=self.team_badge.id,
+            course_id=self.course_key,
+            community_id=self.team_group_chat.room_id
+        )
+        self.assertEqual(len(assigned_user_badges), 2)
+        self.assertFalse(assigned_to_all)
+        mock_task_user_badge_notify.assert_has_calls([
+            mock.call(self.user_2, self.course_key, self.team_badge.name),
+        ])
---
+++ openedx/features/badging/tests/test_tasks.py
@@ -0,0 +1,33 @@
+import mock
+from django.conf import settings
+from django.test import TestCase
+from django.urls import reverse
+
+from common.lib.mandrill_client.client import MandrillClient
+from student.tests.factories import UserFactory
+
+from .. import tasks as badging_tasks
+from ..constants import COURSE_ID_KEY, MY_BADGES_URL_NAME
+
+
+class BadgeViewsTestCases(TestCase):
+
+    @mock.patch('openedx.features.badging.helpers.send_user_badge_notification')
+    @mock.patch('openedx.features.badging.tasks.MandrillClient.send_mail')
+    def test_task_user_badge_notify_successfully(self, mock_send_mail, mock_send_user_badge_notification):
+        """
+        Test for notifying user that he has earned new badges by asserting data params of relevant functions from
+        mandrill client and badge handler
+        """
+        user = UserFactory()
+        context = {
+            'my_badge_url': u'{host}{path}'.format(
+                host=settings.LMS_ROOT_URL,
+                path=reverse(MY_BADGES_URL_NAME, kwargs={COURSE_ID_KEY: 'abc/123/xyz'})
+            )
+        }
+
+        badging_tasks.task_user_badge_notify(user, 'abc/123/xyz', 'team')
+
+        mock_send_mail.assert_called_once_with(MandrillClient.USER_BADGE_EMAIL_TEMPLATE, user.email, context)
+        mock_send_user_badge_notification.assert_called_once_with(user, mock.ANY, 'team')
