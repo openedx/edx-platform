f236f49854fca76333bb3b73a201f5e587b8a4b3
Maria Andrade <mdandrad@ucsd.edu>
2020-03-10 15:51:24 -0700

    content update
    Update sentence

--- common/djangoapps/student/views/dashboard.py
+++ common/djangoapps/student/views/dashboard.py
@@ -623,7 +623,7 @@ def student_dashboard(request):
     if not user.is_active:
         activate_account_message = Text(_(
             "Check your {email_start}{email}{email_end} inbox for an account activation link from {platform_name}. "
-            "If you need help, contact {link_start}{platform_name} Support{link_end}."
+            "If you need help, visit the {link_start}{platform_name} Center{link_end}."
         )).format(
             platform_name=platform_name,
             email_start=HTML("<strong>"),
