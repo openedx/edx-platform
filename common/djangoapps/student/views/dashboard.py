a97a2dcfa30026d3b921eb11682105b6adca6ac0
Maria Andrade <mdandrad@ucsd.edu>
2020-03-16 15:20:52 -0700

    Remove link over ride
    Remove hardcode variable link override

--- common/djangoapps/student/views/dashboard.py
+++ common/djangoapps/student/views/dashboard.py
@@ -619,7 +619,6 @@ def student_dashboard(request):
 
     # Display activation message
     activate_account_message = ''
-    activation_email_support_link = 'http://edtech.ucsd.edu/uc-san-diego-online-help'
     if not user.is_active:
         activate_account_message = Text(_(
             "Check your {email_start}{email}{email_end} inbox for an account activation link from {platform_name}. "
