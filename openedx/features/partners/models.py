d75fd225dccd5a2fd532318c44072c3f8b1b86b7
Abdullah Zafar <mabdullahzafar08@gmail.com>
2020-03-19 16:36:05 +0500

    Add required fields in Partner and UserPartner model

--- openedx/features/partners/models.py
+++ openedx/features/partners/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from jsonfield.fields import JSONField
+from model_utils import Choices
 from model_utils.models import TimeStampedModel
 
+from .constants import PARTNER_USER_STATUS_WAITING, PARTNER_USER_STATUS_APPROVED
 
 class Partner(TimeStampedModel):
     """
@@ -12,6 +15,7 @@ class Partner(TimeStampedModel):
     main_logo = models.CharField(max_length=255)
     small_logo = models.CharField(max_length=255)
     slug = models.CharField(max_length=100, unique=True)
+    configuration = JSONField(null=False, blank=False, default='{}')
 
     def __unicode__(self):
         return '{}'.format(self.label)
@@ -25,8 +29,12 @@ class PartnerUser(TimeStampedModel):
     """
     This model represents all the users that are associated to a partner.
     """
+
+    USER_STATUS = Choices(PARTNER_USER_STATUS_WAITING, PARTNER_USER_STATUS_APPROVED)
+
     user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE, related_name="partner_user")
     partner = models.ForeignKey(Partner, db_index=True, on_delete=models.CASCADE, related_name="partner")
+    status = models.CharField(choices=USER_STATUS)
 
     def __unicode__(self):
         return '{partner}-{user}'.format(partner=self.partner.label, user=self.user.username)
