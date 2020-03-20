1b9e919de8120f1bff545e7b0a6cbb13d6ec69a7
Asad Manzoor <asad33manzoor@gmail.com>
2020-03-20 17:37:54 +0500

    EDLY Services Cookie Configurations Added In Site Configurations (#50)

--- openedx/features/edly/context_processor.py
+++ openedx/features/edly/context_processor.py
@@ -1,11 +1,11 @@
 from math import floor
 
 from django.core.cache import cache
-
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 CACHE_NAME = 'context_processor.dynamic_theming'
 EDLY_CACHE_NAME = 'context_processor.edly_app'
+DEFAULT_SERVICES_COOKIE_EXPIRY = 180
 CACHE_TIMEOUT = 60
 DEFAULT_COLOR_DICT = {
     'primary': '#3E99D4',
@@ -22,6 +22,7 @@ DEFAULT_BRANDING_DICT = {
 }
 
 DEFAULT_SERVICES_NOTIFICATIONS_URL = 'https://staging.staging.panel.backend.edly.io/api/v1/all_services_notifications/'
+DEFAULT_SERVICES_NOTIFICATIONS_COOKIE_DOMAIN = '.edly.io'
 
 
 def dynamic_theming_context(request):  # pylint: disable=unused-argument
@@ -53,8 +54,27 @@ def edly_app_context(request):  # pylint: disable=unused-argument
     if not edly_app_context:
         edly_app_context = {}
         edly_app_context.update(
-            {'services_notifications_url': configuration_helpers.get_value('SERVICES_NOTIFICATIONS_URL',
-                                                                           DEFAULT_SERVICES_NOTIFICATIONS_URL)}
+            {
+                'services_notifications_url': configuration_helpers.get_value(
+                    'SERVICES_NOTIFICATIONS_URL', DEFAULT_SERVICES_NOTIFICATIONS_URL
+                )
+            }
+        )
+
+        edly_app_context.update(
+            {
+                'services_notifications_cookie_domain': configuration_helpers.get_value(
+                    'SERVICES_NOTIFICATIONS_COOKIE_DOMAIN', DEFAULT_SERVICES_NOTIFICATIONS_COOKIE_DOMAIN
+                )
+            }
+        )
+
+        edly_app_context.update(
+            {
+                'services_cookie_expiry': configuration_helpers.get_value(
+                    'SERVICES_COOKIE_EXPIRY', DEFAULT_SERVICES_COOKIE_EXPIRY
+                )
+            }
         )
         cache.set(EDLY_CACHE_NAME, edly_app_context, CACHE_TIMEOUT)
 
