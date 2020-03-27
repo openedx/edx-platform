97c9e7f5651073de9aadffa67f364d7411f378c3
Omar Al-Ithawi <i@omardo.com>
2020-03-27 11:36:59 +0300

    Review changes and tests for course import on site creation

---
+++ cms/djangoapps/appsembler/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Tests for the Appsembler modifications for CMS.
+
+These tests may test modules in `openedx.djangoapps` which is intentional to overcome
+a limitation when such modules depend on `cms` and `pytest` cannot figure that out.
+
+This is likely a sign that there's a need to refactor those modules with cross-dependency, however
+it's not clear how to address that issue.
+"""
--- openedx/core/djangoapps/appsembler/sites/tasks.py
+++ openedx/core/djangoapps/appsembler/sites/tasks.py
@@ -1,16 +1,14 @@
 """
 This file contains celery tasks for contentstore views
 """
-import os
 import logging
 import requests
-import uuid
 import os
 import tarfile
-import shutil
 import datetime
 
-from backports import tempfile
+from backports.tempfile import TemporaryDirectory
+from tempfile import NamedTemporaryFile
 
 from celery.task import task
 from celery.utils.log import get_task_logger
@@ -19,23 +17,20 @@ from django.conf import settings
 
 from student.roles import CourseAccessRole
 
-from django.contrib.auth.models import User
 from opaque_keys.edx.keys import CourseKey
 
-from student.roles import CourseInstructorRole, CourseStaffRole
 from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.django import modulestore
-from xmodule.modulestore.exceptions import DuplicateCourseError, ItemNotFoundError
+from xmodule.modulestore.exceptions import ItemNotFoundError
 from xmodule.modulestore.xml_importer import import_course_from_xml
 
 LOGGER = get_task_logger(__name__)
-FULL_COURSE_REINDEX_THRESHOLD = 1
 
 
 @task()
 def import_course_on_site_creation(organization):
     """
-    Reruns a course in a new celery task.
+    Celery task to copy the template course for new sites.
     """
     try:
         course_name = settings.TAHOE_DEFAULT_COURSE_NAME
@@ -43,58 +38,58 @@ def import_course_on_site_creation(organization):
         course_github_name = settings.TAHOE_DEFAULT_COURSE_GITHUB_NAME
         course_version = settings.TAHOE_DEFAULT_COURSE_VERSION
 
-        # build the github download URL from settings to download the course
-        # in tar.gz format from the github releases
+        # Create the course key object with the current created rganization, the course
+        # name from the settings and the current year
+        course_target_id = CourseKey.from_string(
+            'course-v1:{}+{}+{}'.format(
+                organization.short_name,
+                course_name,
+                datetime.datetime.now().year,
+            )
+        )
+
+        # Build the GitHub download URL from settings to download the course in tar.gz format
+        # from the GitHub releases
         course_download_url = 'https://github.com/{}/{}/archive/{}.tar.gz'.format(
             course_github_org,
             course_github_name,
-            course_version
+            course_version,
         )
+    except Exception as exc:  # pylint: disable=broad-except
+        logging.exception(u'Course Clone Error')
+        return u'exception: ' + unicode(exc)
 
-        # download the course in tar.gz format from github in /tmp using a
-        # random file name
-        with tempfile.TemporaryDirectory() as dir:
-            r = requests.get(course_download_url)
-            course_file_name = str(uuid.uuid4()) + ".tar.gz"
-            file_path = os.path.join("/", "tmp", course_file_name)
+    try:
+        # Download the course in tar.gz format from github in a temp. file using a random file name
+        with TemporaryDirectory() as dir_name, NamedTemporaryFile() as temp_file:
+            response = requests.get(course_download_url)
+            file_path = temp_file.name
             with open(file_path, 'wb') as fd:
-                for chunk in r.iter_content(chunk_size=128):
+                for chunk in response.iter_content(chunk_size=128):
                     fd.write(chunk)
 
-            # untar the downloaded file in the /tmp/random_dir just created
+            # untar the downloaded file in the temp. directory just created
             with tarfile.open(file_path) as tar:
                 extracted_course_folder = tar.getnames()[0]
-                tar.extractall(path=dir)
+                tar.extractall(path=dir_name)
 
-            # create the course key object with the current created
-            # organization, the course name from the settings and the current
-            # year
-            course_target_id = CourseKey.from_string(
-                'course-v1:{}+{}+{}'.format(
-                    organization.short_name,
-                    course_name,
-                    str(datetime.datetime.now().year)
-                )
-            )
-
-            # import the course
-            mstore = modulestore()
-            course_items = import_course_from_xml(
-                mstore,
-                ModuleStoreEnum.UserID.mgmt_command,
-                os.path.join(dir, extracted_course_folder),
-                source_dirs=os.path.join(dir, extracted_course_folder, 'course'),
+            # Import the course
+            m_store = modulestore()
+            import_course_from_xml(
+                m_store,
+                ModuleStoreEnum.UserID.system,
+                os.path.join(dir_name, extracted_course_folder),
+                source_dirs=[os.path.join(dir_name, extracted_course_folder, 'course')],
                 target_id=course_target_id,
                 create_if_not_present=True,
             )
 
-            # remove the course file
-            os.remove(file_path)
-
-            # add the new registered user as admin in the course
-            access_role, created = CourseAccessRole.objects.get_or_create(
+            # TODO: Remove this line once we implement OrgStaffRole in Tahoe.
+            # Add the new registered user as admin in the course
+            CourseAccessRole.objects.get_or_create(
+                # TODO: Ensure only an admin get this course (`is_amc_admin`).
                 user=organization.users.first(),
-                role="instructor",
+                role='instructor',  # This is called "Course Admin" in Studio.
                 course_id=course_target_id,
                 org=organization.short_name
             )
@@ -106,9 +101,9 @@ def import_course_on_site_creation(organization):
 
         try:
             # cleanup any remnants of the course
-            modulestore().delete_course(course_target_id, organization.users.first())
+            modulestore().delete_course(course_target_id, ModuleStoreEnum.UserID.system)
         except ItemNotFoundError:
             # it's possible there was an error even before the course module was created
             pass
 
-        return "exception: " + unicode(exc)
+        return u'exception: ' + unicode(exc)
