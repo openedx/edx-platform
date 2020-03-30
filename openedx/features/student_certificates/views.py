0ff4a6da502148a44cabb51076dede50be44c36c
AbdullahShahid <abdullah.shahid@arbisoft.com>
2020-03-30 17:16:51 +0500

    LP-1741

--- openedx/features/student_certificates/views.py
+++ openedx/features/student_certificates/views.py
@@ -131,7 +131,7 @@ def student_certificates(request):
                 'course_name': program_name[0],
                 'course_title': program_name[0],
                 'social_sharing_urls': {},
-                'certificate_url': credential.get('certificate_url'),
+                'certificate_url': '',
                 'course_start': '',
                 'completion_date': date_from_str(credential.get('created')).strftime('%b %d, %Y'),
                 'is_program_cert': True,
