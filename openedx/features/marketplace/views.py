020a8dec400b354e187c1e269786fa7d3b7497e5
qasimgulzar <qasim.gulzar@arbisoft.com>
2020-04-03 17:50:46 +0500

    LP-1950 Create Idea and Marketplace App and add all possible pages and their URLs [CHANGE] move templates in lms features folder.

--- openedx/features/marketplace/views.py
+++ openedx/features/marketplace/views.py
@@ -5,12 +5,12 @@ from django.views.generic import TemplateView
 
 
 class MarketplaceListingView(TemplateView):
-    template_name = 'philu/features/templates/marketplace/marketplace-listing-template.html'
+    template_name = 'philu/lms/templates/features/marketplace/marketplace-listing-template.html'
 
 
 class MarketplaceCreateRequestView(TemplateView):
-    template_name = 'philu/features/templates/marketplace/markertplace-request-form-template.html'
+    template_name = 'philu/lms/templates/features/marketplace/markertplace-request-form-template.html'
 
 
 class MarketplaceRequestDetailView(TemplateView):
-    template_name = 'philu/features/templates/marketplace/marketplace-details-template.html'
+    template_name = 'philu/lms/templates/features/marketplace/marketplace-details-template.html'
