6c3f2840183c5d7fb12af8c9548acf371f8c3c46
AhmedFaraz <ahmedfaraz@Arbisofts-MacBook-Pro.local>
2020-04-06 22:34:27 +0500

    Replace custom add job view with  django CreateView

--- openedx/features/job_board/urls.py
+++ openedx/features/job_board/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from .views import add_job, JobDetailView, JobListView
+from .views import JobCreateView, JobDetailView, JobListView
 
 urlpatterns = [
-    url(r'$', JobListView.as_view(), name='job_list'),
-    url(r'add/$', add_job, name='add_job'),
-    url(r'(?P<pk>[0-9]+)/$', JobDetailView.as_view(), name='job_detail'),
+    url(r'^$', JobListView.as_view(), name='job_list'),
+    url(r'^create/$', JobCreateView.as_view(), name='create_job'),
+    url(r'^(?P<pk>[0-9]+)/$', JobDetailView.as_view(), name='job_detail'),
 ]
--- openedx/features/job_board/views.py
+++ openedx/features/job_board/views.py
@@ -1,17 +1,13 @@
 from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
-from edxmako.shortcuts import render_to_response
 
+from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-
 from models import Job
-from forms import JobForm
 
 
 class JobListView(ListView):
-
     model = Job
     context_object_name = 'job_list'
     paginate_by = 10
@@ -26,17 +22,10 @@ class JobDetailView(DetailView):
     template_name = 'features/job_board/job_detail.html'
 
 
-def add_job(request):
-    add_job_template = 'features/job_board/add_job.html'
+class JobCreateView(CreateView):
+    model = Job
+    fields = '__all__'
+    template_name = 'features/job_board/create_job_form.html'
 
-    if request.method == 'GET':
-        return render_to_response(add_job_template, {'job_form': JobForm()})
-    elif request.method == 'POST':
-        submitted_job_form = JobForm(request.POST)
-
-        if submitted_job_form.is_valid():
-            submitted_job_form.save()
-            return redirect(reverse('job_list'))
-
-        return render_to_response(add_job_template, {'job_form': submitted_job_form})
-
+    def get_success_url(self):
+        return reverse('job_list')
