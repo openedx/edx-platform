31f8eef33a20c8dc6846d38ea64061a87a735777
AbdullahShahid <abdullah.shahid@arbisoft.com>
2020-04-06 12:51:39 +0500

    LP-1944 Populated job detail page

--- openedx/features/job_board/urls.py
+++ openedx/features/job_board/urls.py
@@ -4,5 +4,5 @@ from .views import JobListView, show_job_detail
 
 urlpatterns = [
     url(r'^jobs/$', JobListView.as_view(), name='job_list'),
-    url(r'^jobs/(?P<job_id>[0-9])+$', show_job_detail, name='job_detail'),
+    url(r'^jobs/(?P<job_id>[0-9]+)$', show_job_detail, name='job_detail'),
 ]
