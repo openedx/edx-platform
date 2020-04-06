2925335588dcfe1c9b52da7e44ee1abd65c13530
qasimgulzar <qasim.gulzar@arbisoft.com>
2020-04-06 16:35:28 +0500

    LP-1950 Create Idea and Marketplace App and add all possible pages and their URLs [FIX] mako templates not working.

--- openedx/features/idea/views.py
+++ openedx/features/idea/views.py
@@ -1,20 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic import TemplateView
+from django.views import View
+from edxmako.shortcuts import render_to_response
 
 
-class ChallengeLandingView(TemplateView):
+class ChallengeLandingView(View):
     template_name = 'features/idea/challenges_landing.html'
 
+    def get(self, request, *args, **kwargs):
+        return render_to_response(self.template_name, {})
 
-class IdeaListingView(TemplateView):
+
+class IdeaListingView(View):
     template_name = 'features/idea/idea_listing.html'
 
+    def get(self, request, *args, **kwargs):
+        return render_to_response(self.template_name, {})
 
-class IdeaCreateView(TemplateView):
+
+class IdeaCreateView(View):
     template_name = 'features/idea/idea_form.html'
 
+    def get(self, request, *args, **kwargs):
+        return render_to_response(self.template_name, {})
 
-class IdeaDetailView(TemplateView):
+
+class IdeaDetailView(View):
     template_name = 'features/idea/idea_details.html'
+
+    def get(self, request, *args, **kwargs):
+        return render_to_response(self.template_name, {})
