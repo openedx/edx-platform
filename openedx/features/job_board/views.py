573e3919d20eb17d39bef359ad24cab1c3873c83
AbdullahShahid <abdullah.shahid@arbisoft.com>
2020-04-06 12:16:52 +0500

    LP-1944 Populated job detail page

--- openedx/features/job_board/views.py
+++ openedx/features/job_board/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
 
 from edxmako.shortcuts import render_to_response
@@ -15,6 +16,21 @@ class JobListView(ListView):
     template_engine = 'mako'
 
 
-def show_job_detail(request):
+def show_job_detail(request, job_id):
+    job = get_object_or_404(
+        Job,
+        pk=job_id,
+    )
+    context = {
+        'title': job.title,
+        'company': job.company,
+        'type': job.type,
+        'location': job.location,
+        'hours': job.hours,
+        'published': job.created.strftime('%B %d, %Y'),
+        'description': job.description,
+        'function': job.function,
+        'responsibilities': job.responsibilities
+    }
 
-    return render_to_response('features/job_board/job_detail.html', {})
+    return render_to_response('features/job_board/job_detail.html', context)
