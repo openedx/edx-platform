ce6cc3a129da09944e9f7c66c16c07aabc5105b2
Zahid <zahid.ali@arbisoft.com>
2020-04-08 13:39:13 +0500

    LP-1951 Idea admin customization

--- openedx/features/idea/admin.py
+++ openedx/features/idea/admin.py
@@ -6,4 +6,15 @@ from django.contrib import admin
 from .models import Idea
 
 
-admin.site.register(Idea)
+class IdeaAdmin(admin.ModelAdmin):
+    """Django admin customizations for Idea model."""
+
+    list_display = ('title', 'user', 'user_email', 'organization', 'city', 'country')
+    search_fields = ('user__username', 'user__email', 'organization__label')
+
+    def user_email(self, obj):
+        """Returning email address of user."""
+        return obj.user.email
+
+
+admin.site.register(Idea, IdeaAdmin)
