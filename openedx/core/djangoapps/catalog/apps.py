211dcee2178cf9749b716b659ff5c5274f26bc02
Robert Raposa <rraposa@edx.org>
2020-04-11 15:27:31 -0400

    add AppConfig for catalog
    ARCHBOM-1105

---
+++ openedx/core/djangoapps/catalog/apps.py
@@ -0,0 +1,9 @@
+"""
+Configuration for Catalog
+"""
+from django.apps import AppConfig
+
+
+class CatalogConfig(AppConfig):
+    name = 'openedx.core.djangoapps.catalog'
+    verbose_name = "Catalog"
