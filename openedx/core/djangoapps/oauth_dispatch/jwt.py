f14659499ec11e77772b4abe147239cd3267db9f
Nimisha Asthagiri <nasthagiri@edx.org>
2020-04-16 19:36:40 -0400

    fixup! public_username -> display_name

--- openedx/core/djangoapps/oauth_dispatch/jwt.py
+++ openedx/core/djangoapps/oauth_dispatch/jwt.py
@@ -182,7 +182,7 @@ def _attach_profile_claim(payload, user):
         # Some users (e.g., service users) may not have user profiles.
         user_profile = UserProfile.objects.get(user=user)
         name = user_profile.name
-        public_username = user_profile.display_name
+        display_name = user_profile.display_name
     except UserProfile.DoesNotExist:
         name = None
         display_name = None
