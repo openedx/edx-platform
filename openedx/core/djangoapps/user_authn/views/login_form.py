f9c9fa7357ce33a8b173fc622463478729ec4cc6
jinder1s <msingh@edx.org>
2020-04-16 10:15:32 -0400

    Changing email placeholder
    The current placeholder is confusing to users and results in many trying
    to login with their username. The change in this PR hopefully decreases that
    confusion.

--- openedx/core/djangoapps/user_authn/views/login_form.py
+++ openedx/core/djangoapps/user_authn/views/login_form.py
@@ -89,15 +89,15 @@ def get_login_session_form(request):
 
     # Translators: This label appears above a field on the login form
     # meant to hold the user's email address.
-    email_label = _(u"Email")
+    email_label = _("Email")
 
     # Translators: This example email address is used as a placeholder in
     # a field on the login form meant to hold the user's email address.
-    email_placeholder = _(u"username@domain.com")
+    email_placeholder = _("example@domain.com")
 
     # Translators: These instructions appear on the login form, immediately
     # below a field meant to hold the user's email address.
-    email_instructions = _(u"The email address you used to register with {platform_name}").format(
+    email_instructions = _("The email address you used to register with {platform_name}").format(
         platform_name=configuration_helpers.get_value('PLATFORM_NAME', settings.PLATFORM_NAME)
     )
 
