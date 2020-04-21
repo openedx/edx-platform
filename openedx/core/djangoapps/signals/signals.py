6a7dc067dac9d72c9762be6f9b0e64f9cfd476da
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2020-04-21 15:10:24 +0500

    Add revoke program certificate task.
    Upon invalidating course certificate, revoke related program certificates
    as well.
    
    PROD-1271

--- openedx/core/djangoapps/signals/signals.py
+++ openedx/core/djangoapps/signals/signals.py
@@ -13,6 +13,7 @@ COURSE_GRADE_CHANGED = Signal(providing_args=["user", "course_grade", "course_ke
 # rather than a User object; however, this will require changes to the milestones and badges APIs
 COURSE_CERT_CHANGED = Signal(providing_args=["user", "course_key", "mode", "status"])
 COURSE_CERT_AWARDED = Signal(providing_args=["user", "course_key", "mode", "status"])
+COURSE_CERT_REVOKED = Signal(providing_args=["user", "course_key", "mode", "status"])
 
 # Signal that indicates that a user has passed a course.
 COURSE_GRADE_NOW_PASSED = Signal(
