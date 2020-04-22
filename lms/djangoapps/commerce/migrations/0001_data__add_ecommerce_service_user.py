eb4df5ad069b4d1c17023fa9ba6e9552bfd262a0
Robert Raposa <rraposa@edx.org>
2020-04-22 15:22:23 -0400

    fix migrations

--- lms/djangoapps/commerce/migrations/0001_data__add_ecommerce_service_user.py
+++ lms/djangoapps/commerce/migrations/0001_data__add_ecommerce_service_user.py
@@ -2,6 +2,7 @@
 
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import migrations, models
 
@@ -10,6 +11,7 @@ EMAIL = USERNAME + '@fake.email'
 
 def forwards(apps, schema_editor):
     """Add the service user."""
+    User = get_user_model()
     user, created = User.objects.get_or_create(username=USERNAME, email=EMAIL)
     if created:
         user.set_unusable_password()
@@ -20,6 +22,11 @@ def backwards(apps, schema_editor):
     User.objects.get(username=USERNAME, email=EMAIL).delete()
 
 class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('user_api', '0002_retirementstate_userretirementstatus'),
+    ]
 
     operations = [
         migrations.RunPython(forwards, backwards),
