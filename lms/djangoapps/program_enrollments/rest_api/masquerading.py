232d9deaf7b82695ae385cf3fffe8c8f05e5553c
Kyle McCormick <kmccormick@edx.org>
2020-04-28 08:39:15 -0400

    Add MasqueradingMixin to program_enrollments API
    wip: Needs testing.

---
+++ lms/djangoapps/program_enrollments/rest_api/masquerading.py
@@ -0,0 +1,73 @@
+"""
+TODO
+"""
+import logging
+
+from django.contrib.auth import get_user_model
+from django.core.exceptions import PermissionDenied
+from django.utils.functional import cached_property
+
+log = logging.getLogger(__name__)
+
+
+class MasqueradableMixin:
+    """
+    Mixin for views that may be able to support masquerading.
+
+    This mixin does not actually implement masquerading -- that would be done
+    in MST-109 and MST-209. However, it allows views to write code that is easily
+    adaptable to masquerading by providing a 'target_user' property, which is calculated
+    based on the value of the 'target_username_kwarg' URL parameter.
+
+    Instead of referencing `self.request.user`, views implementing this mixin should
+    base its operations on `self.target_user`. When masquerading is active,
+    `self.target_user` would refer to the user we are masquerading as.
+
+    Currently, PermissionDenied is raised if `self.target_user` is not identical to
+    `self.request.user`.
+    """
+    target_username_kwarg = "username"
+
+    @cached_property
+    def target_user(self):
+        """
+        TODO
+
+        Raises: PermssionDenied, if:
+            1. target user does not exist,
+            2. requesting user is *not* global staff, or
+            3. target user *is* global staff.
+        Scenarios 1-3 are not differentiated to the requesting user (to avoid
+        information leak) but are exposed in the logs.
+        """
+        _User = get_user_model()
+        requester = self.request.user
+        target_username = self.request.kwargs[self.target_username_kwarg]
+        cannot_masquerade_message = (
+            "User {requester.username} "
+            "cannot perform operations on behalf of {target_username}"
+        ).format(requester=requester, target_username=target_username)
+        try:
+            target = _User.objects.get(username=target_username)
+        except _User.DoesNotExist:
+            log.info(
+                "%s, as %s does not exist.",
+                cannot_masquerade_message,
+                target_username,
+            )
+            raise PermissionDenied(cannot_masquerade_message)
+        if not requester.is_staff:
+            log.info(
+                "%s, as %s is not global staff.",
+                cannot_masquerade_message,
+                requester.username,
+            )
+            raise PermissionDenied(cannot_masquerade_message)
+        if target.is_staff:
+            log.info(
+                "%s, as %s is global staff.",
+                cannot_masquerade_message,
+                target_username,
+            )
+            raise PermissionDenied(cannot_masquerade_message)
+        return target
