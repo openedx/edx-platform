1f1ed6d05df051cc93a3eb7dbb40459791fdc7ee
Muhammad Nofal Fayyaz <54840954+monofal@users.noreply.github.com>
2020-04-29 14:34:53 +0500

    LP-1981 Assign randomized unique names to files uploaded by user (#1520)
    * lp-1981 allow user to upload logo files with name upto 500 chars long
    * remove extra comments
    * lp-1981 feedback changes
    * lp-1981 remove pk from image name and use only random hex
    * lp-1981 removed use of private private member of instance

--- common/djangoapps/util/philu_utils.py
+++ common/djangoapps/util/philu_utils.py
@@ -1,3 +1,10 @@
+import os
+from uuid import uuid4
+
+from django.db.models import ImageField
+from django.utils.deconstruct import deconstructible
+
+
 def extract_utm_params(input_dict):
     """
     This method returns a subset of the input dictionary that only contains the utm params found
@@ -17,3 +24,22 @@ def extract_utm_params(input_dict):
     ]
 
     return {key: value for key, value in input_dict.items() if key in utm_keys}
+
+
+@deconstructible
+class UploadToPathAndRename(object):
+    """
+    Rename file uploaded by user.
+    """
+
+    def __init__(self, path, name_prefix='file'):
+        self.sub_path = path
+        self.name_prefix = name_prefix
+
+    def __call__(self, instance, filename):
+        file_extension = filename.split('.')[-1] if filename else ''
+
+        filename = '{}_{}.{}'.format(self.name_prefix, uuid4().hex, file_extension)
+
+        # return the whole path to the file
+        return os.path.join(self.sub_path, filename)
