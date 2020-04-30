f041934eafcc9f69bb0bed98968d7dbe2b3a35e6
Robert Raposa <rraposa@edx.org>
2020-04-30 10:26:18 -0400

    add additional options

--- scripts/xdist/find_order_dependent_test_failures.py
+++ scripts/xdist/find_order_dependent_test_failures.py
@@ -26,6 +26,9 @@ import tempfile
 import click
 
 OUTPUT_FOLDER_NAME = "worker_list_files"
+test_suite_option = None
+fast_option = None
+verbose_option = None
 
 
 @click.command()
@@ -42,7 +45,24 @@ OUTPUT_FOLDER_NAME = "worker_list_files"
     type=click.Choice(['lms-unit', 'cms-unit', 'commonlib-unit']),
     required=True
 )
-def main(log_file, test_suite):
+@click.option(
+    '--fast/--slow',
+    help="Fast looks for issues in setup/teardown by running one test per class or file.",
+    default=True
+)
+@click.option(
+    '--verbose/--quiet',
+    help="Verbose includes the test output.",
+    default=None
+)
+def main(log_file, test_suite, fast, verbose):
+    global test_suite_option
+    global fast_option
+    global verbose_option
+    test_suite_option = test_suite
+    fast_option = fast
+    verbose_option = verbose
+
     _clean_output_folder()
 
     failing_test_list = _strip_console_for_tests_with_failure(log_file, test_suite)
@@ -51,7 +71,7 @@ def main(log_file, test_suite):
         print('No failures found in log file.')
         return
 
-    if _get_pytest_command_if_failures(failing_test_list[-1:], 'SINGLE'):
+    if _create_and_check_test_files_for_failures(failing_test_list[-1:], 'SINGLE'):
         print("Single test failed. Failures not dependent on order.")
         return
 
@@ -59,6 +79,10 @@ def main(log_file, test_suite):
     if test_list_with_failures:
         print('Found failures running {} tests.'.format(len(test_list_with_failures)))
         print('Use: {}'.format(pytest_command))
+        return
+
+    if fast_option:
+        print('No tests failed locally with --fast option.  Try --slow.')
         return
 
     print('No tests failed locally.')
@@ -74,24 +98,31 @@ def _strip_console_for_tests_with_failure(log_file, test_suite):
     """
     Returns list of tests ending with a failing test, or None if no failures found.
     """
+    global fast_option
     worker_test_dict = {}
+    test_base_included = {}
     failing_worker_num = None
     with io.open(log_file, 'r') as console_file:
         for line in console_file:
-            regex_search = re.search(r'\[gw(\d+)] (PASSED|FAILED|SKIPPED|ERROR)'.format(test_suite), line)
+            regex_search = re.search(r'\[gw(\d+)] (PASSED|FAILED|SKIPPED|ERROR) (\S+)'.format(test_suite), line)
             if regex_search:
                 worker_num_string = regex_search.group(1)
                 pass_fail_string = regex_search.group(2)
                 if worker_num_string not in worker_test_dict:
                     worker_test_dict[worker_num_string] = []
-                line_parts = line.split()
-                pass_fail_index = line_parts.index(pass_fail_string)
-                # test is not always found at the same index in the list
-                test = line_parts[pass_fail_index + 1]
+                test = regex_search.group(3)
                 if test_suite == "commonlib-unit":
                     if "pavelib" not in test and not test.startswith('scripts'):
                         test = u"common/lib/{}".format(test)
-                worker_test_dict[worker_num_string].append(test)
+                if fast_option and pass_fail_string == 'PASSED':
+                    # fast option will only take one test per class or module, in case
+                    # the failure is a setup/teardown failure.
+                    test_base = '::'.join(test.split('::')[:-1])
+                    if not test_base in test_base_included:
+                        worker_test_dict[worker_num_string].append(test)
+                        test_base_included[test_base] = True
+                elif (not fast_option or (fast_option and pass_fail_string == 'FAILED')):
+                    worker_test_dict[worker_num_string].append(test)
                 if pass_fail_string == 'FAILED':
                     failing_worker_num = worker_num_string
                     break
@@ -110,23 +141,32 @@ def _run_tests_and_check_for_failures(output_file_name):
     """
     Runs tests and returns True if failures are found.
     """
+    global verbose_option
     pytest_command = _get_pytest_command(output_file_name)
     test_output = os.popen(pytest_command).read()
+    if verbose_option:
+        print(test_output)
     failures_search = re.search(r'=== (\d+) failed', test_output)
     return bool(failures_search) and int(failures_search.group(1)) > 0
 
 
-def _get_pytest_command_if_failures(test_list, test_type):
+def _create_and_check_test_files_for_failures(test_list, test_type):
     """
     Run the test list to see if there are any failures.
+
+    Keeps around any test files that produced a failure, and deletes
+    the passing files.
 
     Returns the pytest command to run if failures are found.
     """
     print("Testing {}, includes {} test(s)...".format(test_type, len(test_list)))
-    output_file_name = "{}/failing_test_list_{}_{}.txt".format(
-        OUTPUT_FOLDER_NAME, test_type, len(test_list)
+    global test_suite_option
+    output_file_name = "{}/{}_failing_test_list_{}_{}.txt".format(
+        OUTPUT_FOLDER_NAME, test_suite_option, test_type, len(test_list)
     )
     dirname, basename = os.path.split(output_file_name)
+    # Note: We don't really need a temporary file, and could just output the tests directly
+    # to the command line, but this keeps the verbose  output cleaner.
     temp_file = tempfile.NamedTemporaryFile(prefix=basename, dir=dirname, delete=False)
 
     with io.open(temp_file.name, 'w') as output_file:
@@ -140,7 +180,7 @@ def _get_pytest_command_if_failures(test_list, test_type):
         return _get_pytest_command(output_file_name)
 
     os.remove(temp_file.name)
-    print('- all tests passed.')
+    print('- no failures found.')
     return None
 
 
@@ -155,7 +195,7 @@ def _find_fewest_tests_with_failures(test_list, test_type):
     if len(test_list) <= 1:
         return None, None
 
-    pytest_command = _get_pytest_command_if_failures(test_list, test_type)
+    pytest_command = _create_and_check_test_files_for_failures(test_list, test_type)
     if not pytest_command:
         return None, None
 
@@ -179,4 +219,3 @@ def _find_fewest_tests_with_failures(test_list, test_type):
 
 if __name__ == "__main__":
     main()
-
