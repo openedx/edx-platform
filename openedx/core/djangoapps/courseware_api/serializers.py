51803d33fff19d8318c2f7c79dff03e88671f01e
stvn <stvn@mit.edu>
2020-05-12 11:15:08 -0700

    Check if user can upgrade prior to showing sock upgrade

--- openedx/core/djangoapps/courseware_api/serializers.py
+++ openedx/core/djangoapps/courseware_api/serializers.py
@@ -61,6 +61,7 @@ class CourseInfoSerializer(serializers.Serializer):  # pylint: disable=abstract-
     Compare this with CourseDetailSerializer.
     """
 
+    can_show_upgrade_sock = serializers.BooleanField()
     content_type_gating_enabled = serializers.BooleanField()
     course_expired_message = serializers.CharField()
     effort = serializers.CharField()
