4c1bc4342efc2a09d2c3e157cdd3da1723b0ca0a
asadazam93 <asadazam93@gmail.com>
2020-05-13 16:49:33 +0500

    added help links lms

--- lms/static/js/courseware/courseware_factory.js
+++ lms/static/js/courseware/courseware_factory.js
@@ -21,3 +21,33 @@
         }
     );
 }).call(this, define || RequireJS.define);
+
+$(document).ready(function() {
+    'use strict';
+
+    $('.toggle-links-dropdown, .toggle-links-dropdown span').click(function(e) {
+        var $dropdownMenu = $('.nav-item .dropdown-links-menu');
+        var $userDropdown = $('.toggle-links-dropdown');
+        if ($dropdownMenu.is(':visible')) {
+            $dropdownMenu.addClass('hidden');
+            $userDropdown.attr('aria-expanded', 'false');
+        } else {
+            $dropdownMenu.removeClass('hidden');
+            $dropdownMenu.find('.dropdown-item')[0].focus();
+            $userDropdown.attr('aria-expanded', 'true');
+        }
+        $('.toggle-links-dropdown').toggleClass('open');
+        e.stopPropagation();
+    });
+    // Hide links dropdown on click away
+    if ($('.nav-item .dropdown-links-menu').length) {
+        $(window).click(function(e) {
+            var $dropdownMenu = $('.nav-item .dropdown-links-menu');
+            var $userDropdown = $('.toggle-links-dropdown');
+            if ($userDropdown.is(':visible') && !$(e.target).is('.dropdown-item, .toggle-links-dropdown')) {
+                $dropdownMenu.addClass('hidden');
+                $userDropdown.attr('aria-expanded', 'false');
+            }
+        });
+    }
+});
