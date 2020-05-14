2d4687bbf6e322bedfe34a51781f107ea1a4c2f0
harrisirfan <harris.irfan@arbisoft.com>
2020-05-14 16:22:13 +0500

    LP-2025 Update class name in apps.py

--- openedx/features/smart_referral/apps.py
+++ openedx/features/smart_referral/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class MarketplaceConfig(AppConfig):
+class SmartReferralConfig(AppConfig):
     name = 'openedx.features.smart_referral'
