e0a9c4346dceed57e5200f24324f42a5df6b6e51
harrisirfan <harris.irfan@arbisoft.com>
2020-05-20 15:12:34 +0500

    LP-2039 Add new fields UserExtendedProfile model
    Add a new file 'choices.py'

---
+++ lms/djangoapps/onboarding/choices.py
@@ -0,0 +1,50 @@
+FUNCTIONS_LABELS = (
+    ('strategy_planning', 'Strategy and planning'),
+    ('leadership_governance', 'Leadership and governance'),
+    ('program_design', 'Program design and development'),
+    ('measurement_eval', 'Measurement evaluation and learning'),
+    ('stakeholder_engagement', 'External relations and partnerships'),
+    ('human_resource', 'Human resource management'),
+    ('financial_management', 'Financial management'),
+    ('fundraising', 'Fundraising and resource mobilization'),
+    ('marketing_communication', 'Marketing communications and PR'),
+    ('system_tools', 'Systems tools and processes'),
+)
+
+INTERESTS_LABELS = (
+    ('strategy_planning', 'Strategy and planning'),
+    ('leadership_governance', 'Leadership and governance'),
+    ('program_design', 'Program design and development'),
+    ('measurement_eval', 'Measurement, evaluation, and learning'),
+    ('stakeholder_engagement', 'External relations and partnerships'),
+    ('human_resource', 'Human resource management'),
+    ('financial_management', 'Financial management'),
+    ('fundraising', 'Fundraising and resource mobilization'),
+    ('marketing_communication', 'Marketing, communications, and PR'),
+    ('system_tools', 'Systems, tools, and processes'),
+)
+
+INTERESTED_LEARNERS_LABELS = (
+    ('same_region', 'Learners from my region or country'),
+    ('similar_oe_interest', 'Learners interested in same areas of organization effectiveness'),
+    ('similar_org', 'Learners working for similar organizations'),
+    ('diff_who_are_different', 'Learners who are different from me')
+)
+
+GOALS_LABELS = (
+    ('contribute_to_org', 'Help improve my organization'),
+    ('gain_new_skill', 'Develop new skills'),
+    ('improve_job_prospect', 'Get a job'),
+    ('relation_with_other', 'Build relationships with other nonprofit leaders')
+)
+
+HEAR_ABOUT_PHILANTHROPY_LABELS = (
+    ('philanthropy_partner', 'A Philanthropy University Partner (Global Giving, +Acumen or another'),
+    ('colleague_same_organization', 'A Colleague From My Organization'),
+    ('friend_new_organization', 'A Friend Or Colleague (Not From My Organization)'),
+    ('interest_search', 'An Internet Search'),
+    ('linkedIn_advertisement', 'A LinkedIn Advertisement'),
+    ('facebook_advertisement', 'A Facebook Advertisement'),
+    ('twitter_not_colleague', 'Twitter (Not From A Colleague'),
+    ('other', 'Other')
+)
