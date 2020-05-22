c1c9bf506c2f3ea87cbe574cd20b77887bd02343
Bryan Wilson <bryanlandia@gmail.com>
2020-05-22 20:30:10 +0000

    pep8

--- openedx/core/djangoapps/appsembler/eventtracking/exceptions.py
+++ openedx/core/djangoapps/appsembler/eventtracking/exceptions.py
@@ -2,8 +2,9 @@
 Custom exceptions for this package.
 """
 
+
 class EventProcessingError(Exception):
     """
-    Encapsulating error class for exceptions raised 
+    Encapsulating error class for exceptions raised
     trying to process and event for eventtracking.
     """
