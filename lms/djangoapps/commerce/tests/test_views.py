4af179cafee4ac53f8701b38780b7c27efd988a5
Jeremy Bowman <jbowman@edx.org>
2020-05-26 14:08:05 -0400

    Restore UserMixin, delete associated templates and JS

---
+++ lms/djangoapps/commerce/tests/test_views.py
@@ -0,0 +1,15 @@
+""" Tests for commerce views. """
+
+from student.tests.factories import UserFactory
+
+
+class UserMixin(object):
+    """ Mixin for tests involving users. """
+
+    def setUp(self):
+        super(UserMixin, self).setUp()
+        self.user = UserFactory()
+
+    def _login(self):
+        """ Log into LMS. """
+        self.client.login(username=self.user.username, password='test')
