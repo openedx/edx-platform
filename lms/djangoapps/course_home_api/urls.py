277e34f3845f72c6f64d3ae38c6a88ce8ca844e2
Carla Duarte <cduarte@edx.org>
2020-05-26 14:02:45 -0400

    AA-120: Course Tools API
    Created an API to be called by the Outline Tab
    in the Course Home MFE. This API currently only
    returns Course Tool data. Preemptively using the
    `course_home_api/outline/v1/...` naming scheme to
    make way for more Outline Tab data within this API.

--- lms/djangoapps/course_home_api/urls.py
+++ lms/djangoapps/course_home_api/urls.py
@@ -8,6 +8,7 @@ from django.urls import re_path
 
 from lms.djangoapps.course_home_api.dates.v1.views import DatesTabView
 from lms.djangoapps.course_home_api.course_metadata.v1.views import CourseHomeMetadataView
+from lms.djangoapps.course_home_api.outline.v1.views import OutlineTabView
 
 urlpatterns = []
 
@@ -26,5 +27,14 @@ urlpatterns += [
         r'v1/dates/{}'.format(settings.COURSE_KEY_PATTERN),
         DatesTabView.as_view(),
         name='course-home-dates-tab'
+    ),
+]
+
+# Outline Tab URLs
+urlpatterns += [
+    re_path(
+        r'v1/outline/{}'.format(settings.COURSE_KEY_PATTERN),
+        OutlineTabView.as_view(),
+        name='course-home-outline-tab'
     ),
 ]
