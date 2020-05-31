d0fe00d05a4248c1bcc7d50acf131cc037c12c4a
Robert Raposa <rraposa@edx.org>
2020-05-31 14:20:13 -0400

    simplify. make processing less defensive.

--- lms/djangoapps/monitoring/middleware.py
+++ lms/djangoapps/monitoring/middleware.py
@@ -17,10 +17,6 @@ class CodeOwnerMetricMiddleware:
 
     Custom metrics set:
     - code_owner: The owning team mapped to the current view.
-    - code_owner_mapping_config_load_errors: If there are errors loading config. Even
-        though this isn't done as part of the transaction, we want to ensure that any
-        config issue is addressed immediately because it could affect monitoring and
-        alerts.
     - code_owner_mapping_error: If there are any errors when trying to perform the mapping.
     - view_func_module: The __module__ of the view_func, which can be used to
         find missing mappings.
@@ -37,9 +33,6 @@ class CodeOwnerMetricMiddleware:
         """
         Set custom metric for the code_owner of the view if configured to do so.
         """
-        if _CODE_OWNER_MAPPINGS_CONFIG_LOAD_ERRORS:
-            set_custom_metric('code_owner_mapping_config_load_errors', _CODE_OWNER_MAPPINGS_CONFIG_LOAD_ERRORS)
-
         if not _PATH_TO_CODE_OWNER_MAPPINGS:
             return
 
@@ -62,6 +55,9 @@ class CodeOwnerMetricMiddleware:
         'openedx.features', because the former is more specific.
 
         """
+        if _PATH_TO_CODE_OWNER_MAPPINGS is _INVALID_CODE_OWNER_MAPPING:
+            raise Exception('CODE_OWNER_MAPPINGS django setting set with invalid configuration. See logs for details.')
+
         view_func_module_parts = view_func_module.split('.')
         # To match the most specific, tests the most number of parts first
         for number_of_parts in range(len(view_func_module_parts), 0, -1):
@@ -72,15 +68,17 @@ class CodeOwnerMetricMiddleware:
         return None
 
 
-def _load_path_to_code_owner_mappings():
+def _process_code_owner_mappings():
     """
-    Takes the CODE_OWNER_MAPPINGS Django Setting, and re-organizes and loads
-    into _PATH_TO_CODE_OWNER_MAPPINGS. Sets to None if there are no mappings.
+    Processes the CODE_OWNER_MAPPINGS Django Setting and returns a dict optimized
+    for efficient lookup by path.
 
-    Returns
-        tuple: (mappings, load_errors)
+    Returns:
+         (dict): optimized dict for success processing, None if there are no
+            configured mappings, or _INVALID_CODE_OWNER_MAPPING if there is an
+            error processing the setting.
 
-    Example CODE_OWNER_MAPPINGS Django Setting:
+    Example CODE_OWNER_MAPPINGS Django Setting::
 
         CODE_OWNER_MAPPINGS = {
             'team-red': [
@@ -100,52 +98,26 @@ def _load_path_to_code_owner_mappings():
             'badges': 'team-blue',
         }
 
-    Note: If the Django Setting has invalid data, we load as much as possible
-        and logs errors for bad data. Also will add errors as custom metric on
-        all transactions to increase visibility of any config issue.
-
     """
     if not hasattr(settings, 'CODE_OWNER_MAPPINGS') or not settings.CODE_OWNER_MAPPINGS:
-        return (None, None)
+        return None
 
-    path_to_code_owner_mappings = {}
-    errors = []
     try:
-        if isinstance(settings.CODE_OWNER_MAPPINGS, dict):
-            for code_owner in settings.CODE_OWNER_MAPPINGS:
-                if not isinstance(code_owner, str):
-                    errors.append(
-                        'CODE_OWNER_MAPPINGS keys should be code owner as string, but found {}'.format(code_owner)
-                    )
-                path_list = settings.CODE_OWNER_MAPPINGS[code_owner]
-                if isinstance(path_list, Iterable):
-                    for path in path_list:
-                        if isinstance(path, str):
-                            path_to_code_owner_mappings[path] = code_owner
-                        else:
-                            errors.append(
-                                'CODE_OWNER_MAPPINGS[{}] list should contain path strings, but found {}'.format(
-                                    code_owner, path
-                                )
-                            )
-                else:
-                    errors.append(
-                        'CODE_OWNER_MAPPINGS[{}] should be a list of paths, but found {}'.format(code_owner, path_list)
-                    )
-        else:
-            errors.append(
-                'CODE_OWNER_MAPPINGS should be a dict, but found {}'.format(type(settings.CODE_OWNER_MAPPINGS))
-            )
+        path_to_code_owner_mappings = {}
+        for code_owner in settings.CODE_OWNER_MAPPINGS:
+            path_list = settings.CODE_OWNER_MAPPINGS[code_owner]
+            for path in path_list:
+                path_to_code_owner_mappings[path] = code_owner
+        return path_to_code_owner_mappings
     except Exception as e:
-        errors.append('Unknown error loading from settings.CODE_OWNER_MAPPINGS: {}'.format(e))
-    if errors:
-        log.error('Errors loading path_to_code_owner_mappings. {}'.format(errors))
-    return path_to_code_owner_mappings, errors
+        log.exception('Error processing code_owner_mappings. {}'.format(e))
+        # errors should be unlikely due do scripting the setting values
+        # if this rare event occurs, this should cause this to be noticed quickly
+        return _INVALID_CODE_OWNER_MAPPING
 
-_mappings_and_errors = _load_path_to_code_owner_mappings()
-# Stores lookup table for code owner given a module path
-_PATH_TO_CODE_OWNER_MAPPINGS = _mappings_and_errors[0]
-_CODE_OWNER_MAPPINGS_CONFIG_LOAD_ERRORS = _mappings_and_errors[1]
+_INVALID_CODE_OWNER_MAPPING = 'invalid-code-owner-mapping'
+# lookup table for code owner given a module path
+_PATH_TO_CODE_OWNER_MAPPINGS = _process_code_owner_mappings()
 
 
 def _get_view_func_module(view_func):
--- lms/djangoapps/monitoring/tests/test_middleware.py
+++ lms/djangoapps/monitoring/tests/test_middleware.py
@@ -8,7 +8,7 @@ from mock import call, patch, Mock
 from unittest import TestCase
 from unittest.mock import ANY
 
-from lms.djangoapps.monitoring.middleware import _load_path_to_code_owner_mappings, CodeOwnerMetricMiddleware
+from lms.djangoapps.monitoring.middleware import _process_code_owner_mappings, CodeOwnerMetricMiddleware
 
 
 def _mock_get_view_func_module(view_func):
@@ -63,8 +63,7 @@ class CodeOwnerMetricMiddlewareTests(TestCase):
     def test_code_owner_mapping_hits_and_misses(
         self, view_func_module, expected_owner, mock_get_view_func_module, mock_set_custom_metric
     ):
-        path_to_code_owner_mappings, _ = _load_path_to_code_owner_mappings()
-        with patch('lms.djangoapps.monitoring.middleware._PATH_TO_CODE_OWNER_MAPPINGS', path_to_code_owner_mappings):
+        with patch('lms.djangoapps.monitoring.middleware._PATH_TO_CODE_OWNER_MAPPINGS', _process_code_owner_mappings()):
             mock_view_func = self._create_view_func_mock(view_func_module)
             self.middleware.process_view(None, mock_view_func, None, None)
             self._assert_code_owner_custom_metrics(
@@ -79,64 +78,46 @@ class CodeOwnerMetricMiddlewareTests(TestCase):
 
     @override_settings(CODE_OWNER_MAPPINGS=['invalid_setting_as_list'])
     @patch('lms.djangoapps.monitoring.middleware.set_custom_metric')
-    def test_load_config_with_invalid_dict(self, mock_set_custom_metric):
-        path_to_code_owner_mappings, config_load_errors = _load_path_to_code_owner_mappings()
-        with patch(
-            'lms.djangoapps.monitoring.middleware._PATH_TO_CODE_OWNER_MAPPINGS',
-            path_to_code_owner_mappings
-        ):
-            with patch(
-                'lms.djangoapps.monitoring.middleware._CODE_OWNER_MAPPINGS_CONFIG_LOAD_ERRORS',
-                config_load_errors
-            ):
-                mock_view_func = self._create_view_func_mock('xblock')
-                self.middleware.process_view(None, mock_view_func, None, None)
-                mock_set_custom_metric.assert_called_once()
-                self.assertEqual(mock_set_custom_metric.mock_calls[0].args[0], 'code_owner_mapping_config_load_errors')
-                errors_arg = mock_set_custom_metric.mock_calls[0].args[1]
-                self.assertTrue(any('should be a dict' in error for error in errors_arg))
-
-    @override_settings(CODE_OWNER_MAPPINGS=['invalid_mapping_list'])
-    @patch('lms.djangoapps.monitoring.middleware.set_custom_metric')
-    def test_load_config_with_invalid_settings(self, mock_set_custom_metric):
-        assert False, 'TODO: Add DDT for different settings.'
-        path_to_code_owner_mappings, config_load_errors = _load_path_to_code_owner_mappings()
-        with patch(
-            'lms.djangoapps.monitoring.middleware._PATH_TO_CODE_OWNER_MAPPINGS',
-            path_to_code_owner_mappings
-        ):
-            with patch(
-                'lms.djangoapps.monitoring.middleware._CODE_OWNER_MAPPINGS_CONFIG_LOAD_ERRORS',
-                config_load_errors
-            ):
-                mock_view_func = self._create_view_func_mock('xblock')
-                self.middleware.process_view(None, mock_view_func, None, None)
-                self._assert_code_owner_custom_metrics(
-                    'xblock', mock_set_custom_metric, has_error=True, load_error='should be a dict'
-                )
+    @patch('lms.djangoapps.monitoring.middleware._get_view_func_module', side_effect=_mock_get_view_func_module)
+    def test_load_config_with_invalid_dict(self, mock_get_view_func_module, mock_set_custom_metric):
+        with patch('lms.djangoapps.monitoring.middleware._PATH_TO_CODE_OWNER_MAPPINGS', _process_code_owner_mappings()):
+            mock_view_func = self._create_view_func_mock('xblock')
+            self.middleware.process_view(None, mock_view_func, None, None)
+            self._assert_code_owner_custom_metrics(
+                'xblock', mock_set_custom_metric, has_error=True
+            )
 
     @patch('lms.djangoapps.monitoring.middleware.set_custom_metric')
     @patch('lms.djangoapps.monitoring.middleware._get_view_func_module', side_effect=_mock_get_view_func_module)
     def test_mapping_performance(self, mock_get_view_func_module, mock_set_custom_metric):
-        code_owner_mappings = []
+        code_owner_mappings = {
+            'team-red': []
+        }
         # create a long list of mappings that are nearly identical
         for n in range(1, 200):
             path = 'openedx.core.djangoapps.{}'.format(n)
-            code_owner_mappings.append([path, 'team-red'])
-        # test a module that matches nearly to the end, but doesn't actually match
-        mock_view_func = self._create_view_func_mock('openedx.core.djangoapps.XXX.views')
+            code_owner_mappings['team-red'].append(path)
         with override_settings(CODE_OWNER_MAPPINGS=code_owner_mappings):
-            time = timeit.timeit(lambda: self.middleware.process_view(None, mock_view_func, None, None), number=100)
-        average_time = time / 100
-        self.assertTrue(average_time < 0.005, 'Mapping takes {}s which is too slow.'.format(average_time))
-        self._assert_code_owner_custom_metrics(mock_view_func, mock_set_custom_metric)
+            with patch(
+                'lms.djangoapps.monitoring.middleware._PATH_TO_CODE_OWNER_MAPPINGS', _process_code_owner_mappings()
+            ):
+                # test a module that matches nearly to the end, but doesn't actually match
+                mock_view_func = self._create_view_func_mock('openedx.core.djangoapps.XXX.views')
+                call_iterations = 100
+                time = timeit.timeit(
+                    lambda: self.middleware.process_view(None, mock_view_func, None, None), number=call_iterations
+                )
+                average_time = time / call_iterations
+                self.assertTrue(average_time < 0.0005, 'Mapping takes {}s which is too slow.'.format(average_time))
+                expected_calls = []
+                for n in range(1, call_iterations):
+                    expected_calls.append(call('view_func_module', 'openedx.core.djangoapps.XXX.views'))
+                mock_set_custom_metric.assert_has_calls(expected_calls)
 
     def _assert_code_owner_custom_metrics(
-        self, view_func_module, mock_set_custom_metric, expected_code_owner=None, has_error=False, load_error=None
+        self, view_func_module, mock_set_custom_metric, expected_code_owner=None, has_error=False,
     ):
         call_list = []
-        if load_error:
-            call_list.append(call('code_owner_mapping_config_load_errors', ANY))
         call_list.append(call('view_func_module', view_func_module))
         if expected_code_owner:
             call_list.append(call('code_owner', expected_code_owner))
