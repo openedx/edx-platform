210aede791cdf23df8e05afa3805d0ef68f02ae1
Burhan Nasir <burhandodhy@gmail.com>
2020-06-03 18:11:31 +0500

    Fix: Loggedin cookie not deleted when user loggedout via middleware (#94)

--- openedx/features/edly/middleware.py
+++ openedx/features/edly/middleware.py
@@ -29,8 +29,8 @@ class EdlyOrganizationAccessMiddleware(object):
         user_is_superuser = request.user.is_superuser
         if user_is_authenticated and not user_is_superuser and not user_has_edly_organization_access(request):
             logger.exception('Edly user %s has no access for site %s.' % (request.user.email, request.site))
-            logout(request)
-            return HttpResponseRedirect(reverse('logout'))
+            if request.path != '/logout':
+                return HttpResponseRedirect(reverse('logout'))
 
 
 class SettingsOverrideMiddleware(object):
