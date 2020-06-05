4f8ce2db1714b54f6964ccfde76c1ed8f1744a16
Tobias Macey <tmacey@mit.edu>
2020-06-05 10:45:11 -0400

    Fixing disconnect call for mongoengine

--- lms/djangoapps/dashboard/git_import.py
+++ lms/djangoapps/dashboard/git_import.py
@@ -365,7 +365,6 @@ def add_repo(repo, rdir_in, branch=None):
             log.debug(u'saved CourseImportLog for %s', cil.course_id)
         except mongoengine.errors.ValidationError as e:
             log.exception('Unable to save the course import log for %s.', cil.course_id)
-        mdb.disconnect()
+        mongoengine.disconnect()
     else:
         log.error('Unable to save CourseImportLog because of an error in building mongo connection')
-
