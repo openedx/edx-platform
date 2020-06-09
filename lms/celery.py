faab2599da0e9e26943c2d19b0324752a76c63d2
Anders Pearson <anders@thraxil.org>
2020-06-09 09:50:19 +0100

    honeycomb setup (celery)
    this pulls in the settings (will require
    https://github.com/appsembler/edx-configs/pull/875).
    
    It enables basic honeycomb tracing on celery tasks.
    
    It does not configure the django middleware. I still need to do some
    work to make a version of that that will work with
    `MIDDLEWARE_CLASSES`. So we don't have tracing on gunicorn/HTTP
    requests, but I realized that we could still get tracing on the celery
    tasks, which still has value.

--- lms/celery.py
+++ lms/celery.py
@@ -6,9 +6,12 @@ Taken from: http://celery.readthedocs.org/en/latest/django/first-steps-with-djan
 """
 from __future__ import absolute_import
 
+import beeline
+import logging
 import os
 
 from celery import Celery
+from celery.signals import worker_process_init, task_prerun, task_postrun
 from django.conf import settings
 
 from openedx.core.lib.celery.routers import AlternateEnvironmentRouter
@@ -35,3 +38,37 @@ class Router(AlternateEnvironmentRouter):
         Defines alternate environment tasks, as a dict of form { task_name: alternate_queue }
         """
         return {}
+
+
+# honeycomb setup
+@worker_process_init.connect
+def initialize_honeycomb(**kwargs):
+    if settings.HONEYCOMB_WRITEKEY and settings.HONEYCOMB_DATASET:
+        logging.info('beeline initialization in process pid {}'.format(os.getpid()))
+        beeline.init(
+            writekey=settings.HONEYCOMB_WRITEKEY,
+            dataset=settings.HONEYCOMB_DATASET,
+            service_name='lms-celery'
+        )
+
+
+@task_prerun.connect
+def start_celery_trace(task_id, task, args, kwargs, **rest_args):
+    queue_name = task.request.delivery_info.get("exchange", None)
+    task.request.trace = beeline.start_trace(
+        context={
+            "name": "celery",
+            "celery.task_id": task_id,
+            "celery.args": args,
+            "celery.kwargs": kwargs,
+            "celery.task_name": task.name,
+            "celery.queue": queue_name,
+        }
+    )
+
+
+# optional: finish and send the trace at the end of each task
+@task_postrun.connect
+def end_celery_trace(task, state, **kwargs):
+    beeline.add_field("celery.status", state)
+    beeline.finish_trace(task.request.trace)
