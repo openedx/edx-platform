1aafc82362ade9b7dad230bc35f0b7b9c2d5fdff
Robert Raposa <rraposa@edx.org>
2020-06-11 16:58:11 -0400

    remove temp_view_func_compare custom metric
    In an earlier PR, we moved logic from process_view to process
    request, so mapping would happen earlier in the middleware
    lifecycle, and the code_owner metric would be set for requests
    that never made it to process_view.
    
    The temp_view_func_compare custom metric was added temporarily to
    ensure this earlier refactor did not introduce any unaccounted for
    differences. It did not, so we are removing this temporary metric.
    
    ARCHBOM-1263

--- lms/djangoapps/monitoring/tests/test_middleware.py
+++ lms/djangoapps/monitoring/tests/test_middleware.py
@@ -5,7 +5,6 @@ import ddt
 from django.conf.urls import url
 from django.test import override_settings, RequestFactory
 from django.urls import resolve
-from edx_django_utils.cache import RequestCache
 from mock import call, patch, Mock
 from rest_framework.views import APIView
 from unittest import TestCase
@@ -35,7 +34,6 @@ class CodeOwnerMetricMiddlewareTests(TestCase):
 
     def setUp(self):
         super().setUp()
-        RequestCache.clear_all_namespaces()
         self.mock_get_response = Mock()
         self.middleware = CodeOwnerMetricMiddleware(self.mock_get_response)
 
@@ -69,7 +67,6 @@ class CodeOwnerMetricMiddlewareTests(TestCase):
             request = RequestFactory().get(request_path)
             self.middleware(request)
             view_func, _, _ = resolve(request_path)
-            self.middleware.process_view(None, view_func, None, None)
             expected_view_func_module = self._REQUEST_PATH_TO_MODULE_PATH[request_path]
             self._assert_code_owner_custom_metrics(
                 expected_view_func_module, mock_set_custom_metric, expected_code_owner=expected_owner
@@ -107,41 +104,6 @@ class CodeOwnerMetricMiddlewareTests(TestCase):
                 expected_view_func_module, mock_set_custom_metric, has_error=True
             )
 
-    @override_settings(
-        CODE_OWNER_MAPPINGS={'team-red': ['lms.djangoapps.monitoring.tests.mock_views']},
-        ROOT_URLCONF=__name__,
-    )
-    @patch('lms.djangoapps.monitoring.middleware.set_custom_metric')
-    def test_failed_compare_in_process_view(self, mock_set_custom_metric):
-        """
-        Tests that the process_view gets the same function.  This is to conservatively
-        test that the resolve method results in all the same metrics.
-        """
-        class MockWithMockModule():
-            """
-            Added inside function to enable quick clean-up when this is removed.
-            """
-            def __init__(self, mocked_module_name):
-                self._mocked_module_name = mocked_module_name
-
-            def __getattribute__(self, name):
-                if name == '__module__':
-                    return self._mocked_module_name
-                return super().__getattribute__(name)
-
-        with patch('lms.djangoapps.monitoring.utils._PATH_TO_CODE_OWNER_MAPPINGS', _process_code_owner_mappings()):
-            request_path = '/test/'
-            expected_owner = 'team-red'
-            request = RequestFactory().get(request_path)
-            self.middleware(request)
-            expected_view_func_module = self._REQUEST_PATH_TO_MODULE_PATH[request_path]
-            mock_view_func = MockWithMockModule('different.module')
-            self.middleware.process_view(None, mock_view_func, None, None)
-            self._assert_code_owner_custom_metrics(
-                expected_view_func_module, mock_set_custom_metric, expected_code_owner=expected_owner,
-                process_view_func=mock_view_func,
-            )
-
     def _assert_code_owner_custom_metrics(
         self, view_func_module, mock_set_custom_metric, expected_code_owner=None, has_error=False,
         process_view_func=None,
@@ -153,11 +115,6 @@ class CodeOwnerMetricMiddlewareTests(TestCase):
             call_list.append(call('code_owner', expected_code_owner))
         if has_error:
             call_list.append(call('code_owner_mapping_error', ANY))
-        if view_func_module and not has_error:
-            if process_view_func:
-                call_list.append(call('temp_view_func_compare', process_view_func.__module__))
-            else:
-                call_list.append(call('temp_view_func_compare', 'success'))
         mock_set_custom_metric.assert_has_calls(call_list)
         self.assertEqual(
             len(mock_set_custom_metric.call_args_list), len(call_list),
