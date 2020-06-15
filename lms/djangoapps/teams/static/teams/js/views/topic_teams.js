9d7e6604fb54c3221085d58bf74affc517673fbc
Ben Warzeski <benwarzeski@edX-C02CD0HCLVDM.cable.rcn.com>
2020-06-15 15:16:41 -0400

    Update topic teams page to query ajax endpoint
    The previous code relied on a backbone collection for membership on
    teams across the course, while the new logic requires access to teams
    only within the teamset.
    Was unable to find an easy way to do this with the provided backbone
    collections, but a suitable ajax query exists in the API.
    
    In the process of getting this working, I updated much of the local
    syntax/formatting to ES6 in an attempt to make the code more
    accessible/readable.

--- lms/djangoapps/teams/static/teams/js/views/topic_teams.js
+++ lms/djangoapps/teams/static/teams/js/views/topic_teams.js
@@ -10,7 +10,32 @@
         'text!teams/templates/team-actions.underscore',
         'teams/js/views/team_utils'
     ], function(_, Backbone, gettext, HtmlUtils, TeamsView, PagingHeader, teamActionsTemplate, TeamUtils) {
-        var TopicTeamsView = TeamsView.extend({
+        // Translators: this string is shown at the bottom of the teams page
+        // to find a team to join or else to create a new one. There are three
+        // links that need to be included in the message:
+        // 1. Browse teams in other topics
+        // 2. search teams
+        // 3. create a new team
+        // Be careful to start each link with the appropriate start indicator
+        // (e.g. {browse_span_start} for #1) and finish it with {span_end}.
+        const actionsMessage = interpolate_text( // eslint-disable-line no-undef
+            _.escape(
+                gettext(
+                    '{browse_span_start}Browse teams in other ' +
+                    'topics{span_end} or {search_span_start}search teams{span_end} ' +
+                    'in this topic. If you still can\'t find a team to join, ' +
+                    '{create_span_start}create a new team in this topic{span_end}.'
+                )
+            ),
+            {
+                browse_span_start: '<a class="browse-teams" href="">',
+                search_span_start: '<a class="search-teams" href="">',
+                create_span_start: '<a class="create-team" href="">',
+                span_end: '</a>'
+            }
+        );
+
+        const TopicTeamsView = TeamsView.extend({
             events: {
                 'click a.browse-teams': 'browseTeams',
                 'click a.search-teams': 'searchTeams',
@@ -24,52 +49,65 @@
                 TeamsView.prototype.initialize.call(this, options);
             },
 
-            canUserCreateTeam: function() {
-                    // Note: non-staff and non-privileged users are automatically added to any team
-                    // that they create. This means that if multiple team membership is
-                    // disabled that they cannot create a new team when they already
-                    // belong to one.
-                return this.context.userInfo.staff
-                    || this.context.userInfo.privileged
-                    || (!TeamUtils.isInstructorManagedTopic(this.model.attributes.type)
-                        && this.options.myTopicTeamsCollection.length === 0);
+            /**
+             * Send an AJAX query checking on whether the user is in a team in the given
+             * teamset.
+             * If the response comes back with a failure, do not show actions.
+             */
+            checkIfOnTeamInTeamset: async function() {
+                return new Promise((resolve) => {
+                    $.ajax({
+                        type: 'GET',
+                        url: this.context.teamMembershipsUrl,
+                        data: {
+                            username: this.context.userInfo.username,
+                            course_id: this.context.courseID,
+                            teamset_id: this.model.get('id'),
+                        }
+                    }).done(
+                        data => resolve(data.count === 0)
+                    ).fail(
+                        //TODO: should we throw an exception here?
+                        () => resolve(false)
+                    );
+                });
+            },
+
+            /**
+             * Append the actionsMessage string to the element into the `message` key in the django template.
+             */
+            drawActions: function() {
+                HtmlUtils.append(
+                    this.$el,
+                    HtmlUtils.template(teamActionsTemplate)({ message: actionsMessage })
+                );
+            },
+
+
+            /**
+             * Try to render the actions message.
+             * Show actions if student is staff/priviledged or if the team is not instructor-managed and
+             * the student is not on a team in the teamset.
+             */
+            tryRenderActions: function() {
+                //
+                const { staff, priviledged } = this.context.userInfo;
+                if (staff || priviledged) {
+                    return this.drawActions();
+                }
+                if (TeamUtils.isInstructorManagedTopic(this.model.attributes.type)) {
+                    return null;
+                }
+                return this.checkIfOnTeamInTeamset().then(
+                    onTeamInTeamset => !onTeamInTeamset && this.drawActions()
+                );
             },
 
             render: function() {
-                var self = this;
-                this.collection.refresh().done(function() {
-                    var message;
-                    TeamsView.prototype.render.call(self);
-                    if (self.canUserCreateTeam()) {
-                        message = interpolate_text(  // eslint-disable-line no-undef
-                                // Translators: this string is shown at the bottom of the teams page
-                                // to find a team to join or else to create a new one. There are three
-                                // links that need to be included in the message:
-                                // 1. Browse teams in other topics
-                                // 2. search teams
-                                // 3. create a new team
-                                // Be careful to start each link with the appropriate start indicator
-                                // (e.g. {browse_span_start} for #1) and finish it with {span_end}.
-                                _.escape(gettext(
-                                    '{browse_span_start}Browse teams in other ' +
-                                    'topics{span_end} or {search_span_start}search teams{span_end} ' +
-                                    'in this topic. If you still can\'t find a team to join, ' +
-                                    '{create_span_start}create a new team in this topic{span_end}.'
-                                )),
-                            {
-                                browse_span_start: '<a class="browse-teams" href="">',
-                                search_span_start: '<a class="search-teams" href="">',
-                                create_span_start: '<a class="create-team" href="">',
-                                span_end: '</a>'
-                            }
-                            );
-                        HtmlUtils.append(
-                            self.$el,
-                            HtmlUtils.template(teamActionsTemplate)({message: message})
-                        );
-                    }
+                this.collection.refresh().done(() => {
+                    TeamsView.prototype.render.call(this);
+                    this.tryRenderActions();
                 });
-                return this;
             },
 
             browseTeams: function(event) {
@@ -82,17 +120,15 @@
                 event.preventDefault();
                 $searchField.focus();
                 $searchField.select();
-                $('html, body').animate({
-                    scrollTop: 0
-                }, 500);
+                $('html, body').animate({ scrollTop: 0 }, 500);
             },
 
             showCreateTeamForm: function(event) {
                 event.preventDefault();
                 Backbone.history.navigate(
-                        'topics/' + this.model.id + '/create-team',
-                        {trigger: true}
-                    );
+                    'topics/' + this.model.id + '/create-team',
+                    { trigger: true }
+                );
             },
 
             createHeaderView: function() {
