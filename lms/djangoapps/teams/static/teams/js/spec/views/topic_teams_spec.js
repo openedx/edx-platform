c93e065bbe82df520537e5e99c0ae30f787f5f67
Ben Warzeski <benwarzeski@edX-C02CD0HCLVDM.cable.rcn.com>
2020-06-15 15:26:06 -0400

    Update Topic Teams test
    The new ajax query in the render function required a bit of a refactor
    of the tests.
    Note: these are integration, not unit tests, so I did not pursue adding
    additional granularity for now.
    
    Updates here include making createTopicTeamsView an async function and
    cleaning up options/arguments usage.
    
    Updated code locally to use es6 syntax where appropriate.

--- lms/djangoapps/teams/static/teams/js/spec/views/topic_teams_spec.js
+++ lms/djangoapps/teams/static/teams/js/spec/views/topic_teams_spec.js
@@ -1,34 +1,76 @@
 define([
     'backbone',
     'underscore',
+    'edx-ui-toolkit/js/utils/spec-helpers/ajax-helpers',
     'teams/js/views/topic_teams',
     'teams/js/spec_helpers/team_spec_helpers',
     'common/js/spec_helpers/page_helpers'
-], function(Backbone, _, TopicTeamsView, TeamSpecHelpers, PageHelpers) {
+], function(Backbone, _, AjaxHelpers, TopicTeamsView, TeamSpecHelpers, PageHelpers) {
     'use strict';
     describe('Topic Teams View', function() {
-        var createTopicTeamsView = function(options, isInstructorManagedTopic) {
-            options = options || {}; // eslint-disable-line no-param-reassign
-            return new TopicTeamsView({
-                el: '.teams-container',
-                model: isInstructorManagedTopic ?
-                    TeamSpecHelpers.createMockInstructorManagedTopic() : TeamSpecHelpers.createMockTopic(),
-                collection: options.teams || TeamSpecHelpers.createMockTeams({results: []}),
-                myTopicTeamsCollection: (
-                  options.myTopicTeamsCollection || TeamSpecHelpers.createMockTeams({results: []})
-                ),
-                context: _.extend({}, TeamSpecHelpers.testContext, options)
-            }).render();
-        };
 
-        var verifyActions = function(teamsView, options) {
-            var expectedTitle = 'Are you having trouble finding a team to join?',
+        let requests, view;
+
+        const verifyTeamsetTeamsRequest = (hasTeams) => {
+            const {
+                testUser: username,
+                testCourseID: course_id,
+                testTopicID: teamset_id,
+            } = TeamSpecHelpers;
+            AjaxHelpers.expectRequestURL(
+                requests,
+                TeamSpecHelpers.testContext.teamMembershipsUrl,
+                { username, course_id, teamset_id },
+            );
+            AjaxHelpers.respondWithJson(
+                requests,
+                JSON.stringify({ count: hasTeams ? 1 : 0 }),
+            )
+        }
+
+        /**
+         * Initialize the AjaxHelpers requests and render the TopicTeamsView.
+         * 
+         */
+        const createTopicTeamsView = async ({
+            teams,
+            isInstructorManagedTopic,
+            onTeamInTeamset,
+            options,
+        }) => new Promise((resolve) => {
+
+            requests = AjaxHelpers.requests();
+            const model = isInstructorManaged
+                ? TeamSpecHelpers.createMockInstructorManagedTopic()
+                : TeamSpecHelpers.createMockTopic();
+
+            view = new TopicTeamsView({
+                el: '.teams-container',
+                model,
+                collection: teams || TeamSpecHelpers.createMockTeams({results: []}),
+                context: _.extend({}, TeamSpecHelpers.testContext, options)
+            });
+
+            view.render().then(() => {
+                verifyTeamsetTeamsRequest(onTeamInTeamset);
+                resolve(view);
+            });
+        });
+
+
+        /**
+         * Verify whether the actions message is displayed in the element.
+         * @param {TopicTeamsView} teamsView - parent element
+         * @param {bool=true} showActions - should the actions be shown?
+         */
+        const verifyActions = function(teamsView, showActions) {
+            const expectedTitle = 'Are you having trouble finding a team to join?',
                 expectedMessage = 'Browse teams in other topics or search teams in this topic. ' +
                     'If you still can\'t find a team to join, create a new team in this topic.',
                 title = teamsView.$('.title').text().trim(),
                 message = teamsView.$('.copy').text().trim();
-            options = options || {showActions: true}; // eslint-disable-line no-param-reassign
-            if (options.showActions) {
+
+            if (showActions === false) {
                 expect(title).toBe(expectedTitle);
                 expect(message).toBe(expectedMessage);
             } else {
@@ -37,84 +79,99 @@ define([
             }
         };
 
+
         beforeEach(function() {
             setFixtures('<div class="teams-container"></div>');
             PageHelpers.preventBackboneChangingUrl();
         });
 
-        it('can render itself', function() {
-            var testTeamData = TeamSpecHelpers.createMockTeamData(1, 5);
-            var options = {
-                teams: TeamSpecHelpers.createMockTeams({
-                    results: testTeamData
-                })
-            };
-            var teamsView = createTopicTeamsView(options);
-            var footerEl = teamsView.$('.teams-paging-footer');
-            expect(teamsView.$('.teams-paging-header').text()).toMatch('Showing 1-5 out of 6 total');
-            expect(footerEl.text()).toMatch('1\\s+out of\\s+\/\\s+2'); // eslint-disable-line no-useless-escape
-            expect(footerEl).not.toHaveClass('hidden');
 
-            TeamSpecHelpers.verifyCards(teamsView, testTeamData);
-            verifyActions(teamsView);
+        it('can render itself', function() {
+            // Unpriviledged non-staff user, not on any teams in teamset.
+            // Team is not instructor managed.
+            const testTeamData = TeamSpecHelpers.createMockTeamData(1, 5);
+
+            createTopicTeamsView({
+                teams: TeamSpecHelpers.createMockTeams({ results: testTeamData }),
+            }).then((teamsView) => {
+
+                const footerEl = teamsView.$('.teams-paging-footer');
+
+                expect(teamsView.$('.teams-paging-header').text()).toMatch('Showing 1-5 out of 6 total');
+                expect(footerEl.text()).toMatch('1\\s+out of\\s+\/\\s+2'); // eslint-disable-line no-useless-escape
+                expect(footerEl).not.toHaveClass('hidden');
+
+                TeamSpecHelpers.verifyCards(teamsView, testTeamData);
+                verifyActions(teamsView);
+            });
         });
 
         it('can browse all teams', function() {
-            var teamsView = createTopicTeamsView();
-            spyOn(Backbone.history, 'navigate');
-            teamsView.$('.browse-teams').click();
-            expect(Backbone.history.navigate.calls.mostRecent().args[0]).toBe('browse');
+            createTopicTeamsView().then((teamsView) => {
+                spyOn(Backbone.history, 'navigate');
+                teamsView.$('.browse-teams').click();
+                expect(Backbone.history.navigate.calls.mostRecent().args[0]).toBe('browse');
+            });
         });
 
         it('gives the search field focus when clicking on the search teams link', function() {
-            var teamsView = createTopicTeamsView();
-            spyOn($.fn, 'focus').and.callThrough();
-            teamsView.$('.search-teams').click();
-            expect(teamsView.$('.search-field').first().focus).toHaveBeenCalled();
+            createTopicTeamsView().then((teamsView) => {
+                spyOn($.fn, 'focus').and.callThrough();
+                teamsView.$('.search-teams').click();
+                expect(teamsView.$('.search-field').first().focus).toHaveBeenCalled();
+            });
         });
 
         it('can show the create team modal', function() {
-            var teamsView = createTopicTeamsView();
-            spyOn(Backbone.history, 'navigate');
-            teamsView.$('a.create-team').click();
-            expect(Backbone.history.navigate.calls.mostRecent().args[0]).toBe(
-                'topics/' + TeamSpecHelpers.testTopicID + '/create-team'
-            );
+            createTopicTeamsView().then((teamsView) => {
+                spyOn(Backbone.history, 'navigate');
+                teamsView.$('a.create-team').click();
+                expect(Backbone.history.navigate.calls.mostRecent().args[0]).toBe(
+                    'topics/' + TeamSpecHelpers.testTopicID + '/create-team'
+                );
+            });
         });
 
         it('does not show actions for a user already in a team in the teamset', function() {
-            var options = {myTopicTeamsCollection: TeamSpecHelpers.createMockTeams()};
-            var teamsView = createTopicTeamsView(options);
-            verifyActions(teamsView, {showActions: false});
+            createTopicTeamsView({ onTeamInTeamset: true }).then((teamsView) => {
+                verifyActions(teamsView, false);
+            });
         });
 
         it('does not show actions for a student in an instructor managed topic', function() {
-            var teamsView = createTopicTeamsView({}, true);
-            verifyActions(teamsView, {showActions: false});
+            createTopicTeamsView().then((teamsView) => {
+                verifyActions(teamsView, false);
+            });
         });
 
         it('shows actions for a privileged user already in a team', function() {
-            var options = {
+            const options = {
                 userInfo: {
                     privileged: true,
                     staff: false
                 },
-                myTopicTeamsCollection: TeamSpecHelpers.createMockTeams()
             };
-            var teamsView = createTopicTeamsView(options);
-            verifyActions(teamsView, {showActions: true});
+            createTopicTeamsView({
+                options,
+                onTeamInTeamset: true,
+            }).then((teamsView) => {
+                verifyActions(teamsView);
+            });
         });
 
         it('shows actions for a staff user already in a team', function() {
-            var options = {
+            const options = {
                 userInfo: {
                     privileged: false,
-                    staff: true
+                    staff: true,
                 },
-                myTopicTeamsCollection: TeamSpecHelpers.createMockTeams()
             };
-            var teamsView = createTopicTeamsView(options);
-            verifyActions(teamsView, {showActions: true});
+            createTopicTeamsView({
+                options,
+                onTeamInTeamset: true,
+            }).then((teamsView) => {
+                verifyActions(teamsView);
+            });
         });
 
         /*
@@ -123,7 +180,7 @@ define([
             var requests = AjaxHelpers.requests(this),
                 teamMemberships = TeamSpecHelpers.createMockTeamMemberships([]),
                 teamsView = createTopicTeamsView({ teamMemberships: teamMemberships });
-            verifyActions(teamsView, {showActions: true});
+            verifyActions(teamsView);
             teamMemberships.teamEvents.trigger('teams:update', { action: 'create' });
             teamsView.render();
             AjaxHelpers.expectRequestURL(
@@ -138,7 +195,7 @@ define([
                 }
             );
             AjaxHelpers.respondWithJson(requests, {});
-            verifyActions(teamsView, {showActions: false});
+            verifyActions(teamsView);
         });
         */
     });
