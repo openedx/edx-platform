857331e648621f8a8dde67d11bc4ff22042569f8
aliassad <aliassad2@hotmail.com>
2020-06-16 18:13:22 +0500

    added get_connect_url in helpers and changed the connect_url logic

--- openedx/features/marketplace/helpers.py
+++ openedx/features/marketplace/helpers.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 
-def get_connect_url(marketplace_request, request):
+def get_connect_url(marketplace_request, request_user):
     """
     Connecting user with Org Admin/First Learner via NodeBB Chat
         1: If Organization admin is not present then connect with the first learner.
@@ -10,12 +10,11 @@ def get_connect_url(marketplace_request, request):
         3: In case of requester is the admin/first learner of organization
            button should be grayed out and disabled.
     :param marketplace_request: marketplace request item
-    :param request: user request object
     """
     chat_url = '{nodebb_end_point}/chat/{username}'
     responsible_user = marketplace_request.organization.admin or marketplace_request.organization.first_learner \
                        or marketplace_request.user
-    if responsible_user.username != request.user.username:
+    if responsible_user.username != request_user.username:
         connect_url = chat_url.format(nodebb_end_point=settings.NODEBB_ENDPOINT, username=responsible_user.username)
     else:
         connect_url = None
