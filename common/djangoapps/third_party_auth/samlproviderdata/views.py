8f0875d28053db6bb852526f6c37c8444d453b67
binodpant <bpant@edx.org>
2020-06-23 10:43:26 -0400

    ENT-3007 auth/saml/v0/saml/providerdata and auth/saml/v0/saml/providerconfig endpoints
    Move code to subfolder for samlproviderconfig
    
    extra comma
    
    undo accidental remove of import
    
    GET works for a single config now
    
    Use ModelViewSet to get all CRUD method. Test still fails
    
    Add auth/saml/v0/providerdata endpoints
    
    fixup reverse and test issue, remove leading caret
    
    just triggering run, why is it failing in CI?
    
    pycodelint fixes
    
    Skip auth tests unless feature is on
    
    Tests for post/put for samlproviderdata
    
    move urls to their own folders
    
    api tests for post samlprovierconfig
    
    create 1 providerconfig test case
    
    lint fixes
    
    lint
    
    lint
    
    cleanup code local urls /samlproviderconfig works
    
    note needed right now
    
    Fix import errors
    
    lint
    
    unused import
    
    wip: first attempt at rbac auth and jwt cookie in test
    
    round 2 with enterprise uuid as url param for samlproviderconfig
    
    improve tests, still dont pass
    
    fix test by using system role, wip other test
    
    fix create test
    
    add get/post tests for providerdata
    
    isort fixes
    
    string lint fix
    
    Cleanup based on feedback round1
    
    move utils to tests package
    
    Move util fn to openedx.feature area
    
    lint
    
    ENT-3007 : Round 2 of work on auth/saml/v0/providerconfig and auth/saml/v0/providerdata endpoints

---
+++ common/djangoapps/third_party_auth/samlproviderdata/views.py
@@ -0,0 +1,54 @@
+"""
+    Viewset for auth/saml/v0/samlproviderdata
+"""
+
+from edx_rbac.mixins import PermissionRequiredMixin
+from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
+from enterprise.models import EnterpriseCustomerIdentityProvider
+from rest_framework import permissions, viewsets
+from rest_framework.authentication import SessionAuthentication
+
+from openedx.features.enterprise_support.utils import fetch_enterprise_customer_by_id
+
+from ..models import SAMLProviderConfig, SAMLProviderData
+from .serializers import SAMLProviderDataSerializer
+
+
+class SAMLProviderDataMixin(object):
+    authentication_classes = [JwtAuthentication, SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = SAMLProviderDataSerializer
+
+
+class SAMLProviderDataViewSet(PermissionRequiredMixin, SAMLProviderDataMixin, viewsets.ModelViewSet):
+    """
+    A View to handle SAMLProviderData CRUD.
+    Uses the edx-rbac mixin PermissionRequiredMixin to apply enterprise authorization
+
+    Usage:
+        [HttpVerb] /auth/saml/v0/providerdata/
+    """
+    permission_required = 'enterprise.can_access_admin_dashboard'
+
+    def get_queryset(self):
+        """
+        Find and return the matching providerid for the given enterprise uuid
+        Note: There is no direct association between samlproviderdata and enterprisecustomer.
+        So we make that association in code via samlproviderdata > samlproviderconfig ( via entity_id )
+        then, we fetch enterprisecustomer via samlproviderconfig > enterprisecustomer ( via association table )
+        """
+        enterprise_customer_idp = EnterpriseCustomerIdentityProvider.objects.get(
+            enterprise_customer__uuid=self.requested_enterprise_uuid
+        )
+        saml_provider = SAMLProviderConfig.objects.get(pk=enterprise_customer_idp.provider_id)
+        return SAMLProviderData.objects.filter(entity_id=saml_provider.entity_id)
+
+    @property
+    def requested_enterprise_uuid(self):
+        return self.request.query_params.get('enterprise_customer_uuid')
+
+    def get_permission_object(self):
+        """
+        Retrive an EnterpriseCustomer to do auth against
+        """
+        return fetch_enterprise_customer_by_id(self.requested_enterprise_uuid)
