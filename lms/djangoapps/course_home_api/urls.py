76a2523c6eb174607b1d797f55c822de84b53f9c
Daphne Li-Chen <dli-chen@edx.org>
2020-06-24 19:23:37 -0400

    AA-203: created API framework for Progress tab, sending user information

--- lms/djangoapps/course_home_api/urls.py
+++ lms/djangoapps/course_home_api/urls.py
@@ -9,6 +9,7 @@ from django.urls import re_path
 from lms.djangoapps.course_home_api.dates.v1.views import DatesTabView
 from lms.djangoapps.course_home_api.course_metadata.v1.views import CourseHomeMetadataView
 from lms.djangoapps.course_home_api.outline.v1.views import OutlineTabView
+from lms.djangoapps.course_home_api.progress.v1.views import ProgressTabView
 
 urlpatterns = []
 
@@ -36,5 +37,14 @@ urlpatterns += [
         r'v1/outline/{}'.format(settings.COURSE_KEY_PATTERN),
         OutlineTabView.as_view(),
         name='course-home-outline-tab'
+    ),
+]
+
+# Progress Tab URLs
+urlpatterns += [
+    re_path(
+        r'v1/progress/{}'.format(settings.COURSE_KEY_PATTERN),
+        ProgressTabView.as_view(),
+        name='course-home-progress-tab'
     ),
 ]
