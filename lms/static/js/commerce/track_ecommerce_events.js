edbafa1c723957023e704768760e3182b6064c14
Jie Han <jhan@edx.org>
2020-06-24 10:35:24 -0400

    Add JS function to track upsell events.

---
+++ lms/static/js/commerce/track_ecommerce_events.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * A library of helper functions to track ecommerce related events. 
+ *
+ */
+
+(function(define) {
+    'use strict';
+
+    define([
+        'jquery'
+    ], function($) {
+        var trackUpsellClick = function(linkName, optionalAttrs) {
+            if (!window.analytics) {
+                return;
+            }
+    
+            var eventAttrs = { "linkName": linkName };
+
+            if (
+                typeof optionalAttrs !== 'undefined' &&
+                optionalAttrs !== null &&
+                Object.keys(optionalAttrs).length > 0
+            ) {
+                var allowedAttrs = ["courseId", "pageName"];
+                allowedAttrs.map(
+                    allowedAttr => eventAttrs[allowedAttr] = optionalAttrs[allowedAttr]
+                );
+            }
+    
+            window.analytics.track("edx.bi.ecommerce.upsell_links_clicked", eventAttrs);
+        };
+        
+        var TrackECommerceEvents = {
+            trackUpsellClick: trackUpsellClick,
+        };
+
+        return TrackECommerceEvents;
+    });
+}).call(this, define || RequireJS.define);
