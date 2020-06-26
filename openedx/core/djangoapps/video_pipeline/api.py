0b80987ba06f6e4b81a405650b38f24b87da16a4
Zainab Amir <zainab.amir@arbisoft.com>
2020-06-26 14:30:42 +0500

    Add vem_enabled_courses_percentage (#24294)
    Add a field to VEM config model that will decide the percentage of
    courses allowed to go to VEM pipeline. The courses that don't meet the
    criteria will go to VEDA.
    
    PROD-1722

--- openedx/core/djangoapps/video_pipeline/api.py
+++ openedx/core/djangoapps/video_pipeline/api.py
@@ -16,9 +16,49 @@ from openedx.core.djangoapps.video_pipeline.utils import create_video_pipeline_a
 log = logging.getLogger(__name__)
 
 
+def send_transcript_credentials(pipeline_integration, credentials_payload):
+    """
+    Sends transcript credentials to video pipeline
+    """
+    error_response, is_updated = {}, False
+    try:
+        oauth_client = Application.objects.get(name=pipeline_integration.client_name)
+    except ObjectDoesNotExist:
+        return error_response, is_updated
+
+    client = create_video_pipeline_api_client(
+        oauth_client.client_id,
+        oauth_client.client_secret
+    )
+    error_message = "Unable to update transcript credentials -- org={}, provider={}, response={}"
+    try:
+        response = client.request("POST", pipeline_integration.api_url, json=credentials_payload)
+        if response.ok:
+            is_updated = True
+        else:
+            is_updated = False
+            error_response = json.loads(response.text)
+            log.error(error_message.format(
+                credentials_payload.get('org'),
+                credentials_payload.get('provider'),
+                response.text
+            ))
+    except HttpClientError as ex:
+        is_updated = False
+        log.exception(error_message.format(
+            credentials_payload.get('org'),
+            credentials_payload.get('provider'),
+            ex.content
+        ))
+        error_response = json.loads(ex.content)
+
+    return error_response, is_updated
+
+
 def update_3rd_party_transcription_service_credentials(**credentials_payload):
     """
-    Updates the 3rd party transcription service's credentials.
+    Updates the 3rd party transcription service's credentials. Credentials are updated
+    for all the enabled pipelines.
 
     Arguments:
         credentials_payload(dict): A payload containing org, provider and its credentials.
@@ -26,45 +66,29 @@ def update_3rd_party_transcription_service_credentials(**credentials_payload):
     Returns:
         A Boolean specifying whether the credentials were updated or not
         and an error response received from pipeline.
+
+    Note: If one of the pipeline fails to update the credentials, False is returned, meaning
+    that credentials were not updated and both calls should be tried again.
     """
     error_response, is_updated = {}, False
-    course_key = credentials_payload.pop('course_key', None)
 
-    if course_key and waffle_flags()[ENABLE_VEM_PIPELINE].is_enabled(course_key):
-        pipeline_integration = VEMPipelineIntegration.current()
-    else:
-        pipeline_integration = VideoPipelineIntegration.current()
+    vem_pipeline_integration = VEMPipelineIntegration.current()
+    veda_pipeline_integration = VideoPipelineIntegration.current()
 
-    if pipeline_integration.enabled:
-        try:
-            oauth_client = Application.objects.get(name=pipeline_integration.client_name)
-        except ObjectDoesNotExist:
+    if vem_pipeline_integration.enabled:
+        log.info('Sending transcript credentials to VEM for org: {} and provider: {}'.format(
+            credentials_payload.get('org'), credentials_payload.get('provider')
+        ))
+        error_response, is_updated = send_transcript_credentials(vem_pipeline_integration, credentials_payload)
+        # If credentials update fail for VEM, return proper error message and discontinue to
+        # send credentials to VEDA because we are going to try it next time anyway
+        if not is_updated:
             return error_response, is_updated
 
-        client = create_video_pipeline_api_client(
-            oauth_client.client_id,
-            oauth_client.client_secret
-        )
-        error_message = "Unable to update transcript credentials -- org={}, provider={}, response={}"
-        try:
-            response = client.request("POST", pipeline_integration.api_url, json=credentials_payload)
-            if response.ok:
-                is_updated = True
-            else:
-                is_updated = False
-                error_response = json.loads(response.text)
-                log.error(error_message.format(
-                    credentials_payload.get('org'),
-                    credentials_payload.get('provider'),
-                    response.text
-                ))
-        except HttpClientError as ex:
-            is_updated = False
-            log.exception(error_message.format(
-                credentials_payload.get('org'),
-                credentials_payload.get('provider'),
-                ex.content
-            ))
-            error_response = json.loads(ex.content)
+    if veda_pipeline_integration.enabled:
+        log.info('Sending transcript credentials to VEDA for org: {} and provider: {}'.format(
+            credentials_payload.get('org'), credentials_payload.get('provider')
+        ))
+        error_response, is_updated = send_transcript_credentials(veda_pipeline_integration, credentials_payload)
 
     return error_response, is_updated
