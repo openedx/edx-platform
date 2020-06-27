90c29940b395258324405f4865c4bf3d0b5ea9da
Braden MacDonald <braden@opencraft.com>
2020-06-27 14:43:06 -0700

    Recursively handle children

--- common/lib/xmodule/xmodule/library_tools.py
+++ common/lib/xmodule/xmodule/library_tools.py
@@ -11,6 +11,7 @@ from opaque_keys.edx.locator import LibraryLocator, LibraryUsageLocator, BlockUs
 from openedx.core.djangoapps.xblock.api import load_block
 from search.search_engine_base import SearchEngine
 from student.auth import has_studio_write_access
+from xblock.fields import Scope
 from xmodule.capa_module import ProblemBlock
 from xmodule.library_content_module import ANY_CAPA_TYPE_VALUE
 from xmodule.modulestore import ModuleStoreEnum
@@ -219,36 +220,60 @@ class LibraryToolsService(object):
         orig_blocks = [load_block(UsageKey.from_string(key), user) for key in blockstore_block_ids]
 
         with self.store.bulk_operations(dest_course_key):
-            for block in orig_blocks:
-                orig_key = block.scope_ids.usage_id
+            # As we go, build a set of the IDs of the new children,
+            # so we can delete any existing children that aren't updated.
+            child_ids_updated = set()
+
+            def do_import(source_block, dest_parent_key):
+                """ Recursively import a blockstore block and its children """
+                source_key = source_block.scope_ids.usage_id
                 # Deterministically generate a new ID for this block 
-                new_block_id = dest_key.block_id[:10] + '-' + hashlib.sha1(str(orig_key).encode('utf-8')).hexdigest()[:10]
-                new_block_key = dest_key.context_key.make_usage_key(orig_key.block_type, new_block_id)
+                new_block_id = (
+                    dest_parent_key.block_id[:10] + '-' + hashlib.sha1(str(source_key).encode('utf-8')).hexdigest()[:10]
+                )
+                new_block_key = dest_parent_key.context_key.make_usage_key(source_key.block_type, new_block_id)
 
                 try:
                     new_block = self.store.get_item(new_block_key)
-                    if new_block.parent != dest_key:
+                    if new_block.parent != dest_parent_key:
                         raise ValueError(
-                            "Expected existing block {} to be a child of {}".format(new_block_key, dest_key)
+                            "Expected existing block {} to be a child of {}".format(new_block_key, dest_parent_key)
                         )
                 except ItemNotFoundError:
                     new_block = self.store.create_child(
                         user_id=self.user_id,
-                        parent_usage_key=dest_key,
-                        block_type=orig_key.block_type,
+                        parent_usage_key=dest_parent_key,
+                        block_type=source_key.block_type,
                         block_id=new_block_id,
                     )
-                for field_name, field in block.fields.items():
-                    if field_name in ('children', 'parent', 'content'):
-                        continue
-                    if field.is_set_on(block) or field.is_set_on(new_block):
-                        setattr(new_block, field_name, getattr(block, field_name))
-
-                # TODO: handle children recursively.
+                for field_name, field in source_block.fields.items():
+                    if field.scope not in (Scope.settings, Scope.content):
+                        continue  # Only copy authored field data
+                    if field.is_set_on(source_block) or field.is_set_on(new_block):
+                        setattr(new_block, field_name, getattr(source_block, field_name))
+                new_block.save()
+                self.store.update_item(new_block, self.user_id)
 
                 # TODO: handle static assets
 
-                new_block.save()
-                self.store.update_item(new_block, user_id=self.user_id)
+                # Recursively import children
+                if new_block.has_children:
+                    # Delete any children that the new block has since we'll be replacing them all anyways, and want to
+                    # Ensure that if a grandchild block was deleted from the source library it will get deleted from the
+                    # destination course during this update.
+                    for existing_child_key in new_block.children:
+                        self.store.delete_item(existing_child_key, self.user_id)
+                    # Now import the children
+                    for child in source_block.get_children():
+                        do_import(child, new_block_key)
 
-            # TODO: delete any out of date children
+                return new_block_key
+
+            # Now actually do the import, making each block in 'orig_blocks' become a child of dest_block
+            for block in orig_blocks:
+                new_block_id = do_import(block, dest_key)
+                child_ids_updated.add(new_block_id)
+            # Remove any existing children that are no longer wanted
+            existing_children_to_delete = set(dest_block.children) - child_ids_updated
+            for old_child_id in existing_children_to_delete:
+                self.store.delete_item(old_child_id, self.user_id)
