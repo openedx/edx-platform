8a010b345015dc048d0b16eb324cca389439402f
Alie Langston <alangsto@wellesley.edu>
2020-07-08 09:11:10 -0400

    updates for requested changes

--- cms/djangoapps/contentstore/views/tests/test_exam_settings_view.py
+++ cms/djangoapps/contentstore/views/tests/test_exam_settings_view.py
@@ -17,10 +17,10 @@ FEATURES_WITH_CERTS_ENABLED = settings.FEATURES.copy()
 FEATURES_WITH_CERTS_ENABLED['CERTIFICATES_HTML_VIEW'] = True
 
 FEATURES_WITH_EXAM_SETTINGS_ENABLED = FEATURES_WITH_CERTS_ENABLED.copy()
-FEATURES_WITH_EXAM_SETTINGS_ENABLED['EXAM_SETTINGS_HTML_VIEW'] = True
+FEATURES_WITH_EXAM_SETTINGS_ENABLED['ENABLE_EXAM_SETTINGS_HTML_VIEW'] = True
 
 FEATURES_WITH_EXAM_SETTINGS_DISABLED = FEATURES_WITH_CERTS_ENABLED.copy()
-FEATURES_WITH_EXAM_SETTINGS_DISABLED['EXAM_SETTINGS_HTML_VIEW'] = False
+FEATURES_WITH_EXAM_SETTINGS_DISABLED['ENABLE_EXAM_SETTINGS_HTML_VIEW'] = False
 
 
 @ddt.ddt
@@ -51,7 +51,7 @@ class TestExamSettingsView(CourseTestCase, UrlResetMixin):
         outline_url = reverse_course_url(handler, self.course.id)
         resp = self.client.get(outline_url, HTTP_ACCEPT='text/html')
         self.assertEqual(resp.status_code, 200)
-        self.assertNotContains(resp, 'Exam Settings')
+        self.assertNotContains(resp, 'Proctored Exam Settings')
 
     @override_settings(FEATURES=FEATURES_WITH_EXAM_SETTINGS_ENABLED)
     @ddt.data(
@@ -69,4 +69,4 @@ class TestExamSettingsView(CourseTestCase, UrlResetMixin):
         outline_url = reverse_course_url(handler, self.course.id)
         resp = self.client.get(outline_url, HTTP_ACCEPT='text/html')
         self.assertEqual(resp.status_code, 200)
-        self.assertContains(resp, 'Exam Settings')
+        self.assertContains(resp, 'Proctored Exam Settings')
