ae88726ec92878bb78f74e3de61e28eb109bcbee
Jie Han <jhan@edx.org>
2020-07-08 15:47:13 -0400

    remove deprecated waffle flag param

--- lms/djangoapps/experiments/utils.py
+++ lms/djangoapps/experiments/utils.py
@@ -48,7 +48,6 @@ experiments_namespace = WaffleFlagNamespace(name=u'experiments')
 PROGRAM_INFO_FLAG = WaffleFlag(
     waffle_namespace=experiments_namespace,
     flag_name=u'add_programs',
-    flag_undefined_default=False
 )
 
 # .. toggle_name: experiments.add_dashboard_info
@@ -62,9 +61,7 @@ PROGRAM_INFO_FLAG = WaffleFlag(
 # .. toggle_warnings: None
 # .. toggle_tickets: REVEM-118
 # .. toggle_status: supported
-DASHBOARD_INFO_FLAG = WaffleFlag(experiments_namespace,
-                                 u'add_dashboard_info',
-                                 flag_undefined_default=False)
+DASHBOARD_INFO_FLAG = WaffleFlag(experiments_namespace, u'add_dashboard_info')
 # TODO END: clean up as part of REVEM-199 (End)
 
 # TODO: Clean up as part of REV-1205 (START)
@@ -81,8 +78,7 @@ DASHBOARD_INFO_FLAG = WaffleFlag(experiments_namespace,
 # .. toggle_status: supported
 UPSELL_TRACKING_FLAG = WaffleFlag(
     waffle_namespace=experiments_namespace,
-    flag_name=u'add_upsell_tracking',
-    flag_undefined_default=False
+    flag_name=u'add_upsell_tracking'
 )
 # TODO END: Clean up as part of REV-1205 (End)
 
