96ddcdc28870d1d2fa9d85fdad6e5306356f4d1c
Binod Pant <bpant@edx.org>
2020-07-10 11:23:13 -0400

    ENT-3007 : Add auth/saml/v0/providerconfig|data CRUD endpoints for use in admin portal (#24298)
    * Add auth/samlproviderconfig CRUD endpoints for use in admin portal
    
    Fixes: ENT-3007
    
    * Move code to subfolder for samlproviderconfig
    
    * extra comma
    
    * undo accidental remove of import
    
    * GET works for a single config now
    
    * Use ModelViewSet to get all CRUD method. Test still fails
    
    * Add auth/saml/v0/providerdata endpoints
    
    * fixup reverse and test issue, remove leading caret
    
    * just triggering run, why is it failing in CI?
    
    * pycodelint fixes
    
    * Skip auth tests unless feature is on
    
    * Tests for post/put for samlproviderdata
    
    * move urls to their own folders
    
    * api tests for post samlprovierconfig
    
    * create 1 providerconfig test case
    
    * lint fixes
    
    * lint
    
    * lint
    
    * cleanup code local urls /samlproviderconfig works
    
    * note needed right now
    
    * Fix import errors
    
    * lint
    
    * unused import
    
    * wip: first attempt at rbac auth and jwt cookie in test
    
    * round 2 with enterprise uuid as url param for samlproviderconfig
    
    * improve tests, still dont pass
    
    * fix test by using system role, wip other test
    
    * fix create test
    
    * add get/post tests for providerdata
    
    * isort fixes
    
    * string lint fix
    
    * Cleanup based on feedback round1
    
    * move utils to tests package
    
    * Move util fn to openedx.feature area
    
    * lint
    
    * lint fix
    
    * remove unused import

---
+++ common/djangoapps/third_party_auth/samlproviderconfig/serializers.py
@@ -0,0 +1,13 @@
+"""
+Serializer for SAMLProviderConfig
+"""
+
+from rest_framework import serializers
+
+from third_party_auth.models import SAMLProviderConfig
+
+
+class SAMLProviderConfigSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SAMLProviderConfig
+        fields = '__all__'
