d943b646f9823b3e4ff1947dfcf8ebe4b2cadd8f
Adolfo R. Brandes <adolfo@opencraft.com>
2020-07-10 19:59:32 -0300

    Clean up CORS-related settings

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2778,12 +2778,11 @@ if FEATURES.get('ENABLE_CORS_HEADERS'):
     CORS_ALLOW_CREDENTIALS = True
     CORS_ORIGIN_WHITELIST = ()
     CORS_ORIGIN_ALLOW_ALL = False
+    CORS_ALLOW_INSECURE = False
     CORS_ALLOW_HEADERS = corsheaders_default_headers + (
         'use-jwt-cookie',
     )
 
-CORS_ORIGIN_WHITELIST = []
-CORS_ORIGIN_ALLOW_ALL = False
 # Default cache expiration for the cross-domain proxy HTML page.
 # This is a static page that can be iframed into an external page
 # to simulate cross-domain requests.
