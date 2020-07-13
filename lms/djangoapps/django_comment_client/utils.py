9597a070786f60643430e64631f735d9633db508
Asad Manzoor <asad33manzoor@gmail.com>
2020-07-13 16:03:47 +0500

    Enable Forum Notifications (#116)

--- lms/djangoapps/django_comment_client/utils.py
+++ lms/djangoapps/django_comment_client/utils.py
@@ -677,10 +677,10 @@ def permalink(content):
     else:
         course_id = content['course_id']
     if content['type'] == 'thread':
-        return reverse('discussion.views.single_thread',
+        return reverse('single_thread',
                        args=[course_id, content['commentable_id'], content['id']])
     else:
-        return reverse('discussion.views.single_thread',
+        return reverse('single_thread',
                        args=[course_id, content['commentable_id'], content['thread_id']]) + '#' + content['id']
 
 
