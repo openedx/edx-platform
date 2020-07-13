7ea9714a3385123253304da340c23bfad97e2cd2
alangsto <46360176+alangsto@users.noreply.github.com>
2020-07-13 16:10:48 -0400

    Updated URLs to course-authoring MFE (#24468)
    * updated urls to course-authoring MFE
    
    * updates for requested changes
    
    * updated for pylint errors

--- cms/djangoapps/contentstore/views/certificates.py
+++ cms/djangoapps/contentstore/views/certificates.py
@@ -39,7 +39,11 @@ from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import AssetKey, CourseKey
 from six import text_type
 
-from contentstore.utils import get_lms_link_for_certificate_web_view, reverse_course_url
+from contentstore.utils import (
+    get_lms_link_for_certificate_web_view,
+    reverse_course_url,
+    get_proctored_exam_settings_url
+)
 from contentstore.views.assets import delete_asset
 from contentstore.views.exception import AssetNotFoundException
 from course_modes.models import CourseMode
@@ -416,6 +420,8 @@ def certificates_list_handler(request, course_key_string):
 
             is_active, certificates = CertificateManager.is_activated(course)
 
+            course_authoring_microfrontend_url = get_proctored_exam_settings_url(course)
+
             return render_to_response('certificates.html', {
                 'context_course': course,
                 'certificate_url': certificate_url,
@@ -427,7 +433,8 @@ def certificates_list_handler(request, course_key_string):
                 'certificate_web_view_url': certificate_web_view_url,
                 'is_active': is_active,
                 'is_global_staff': GlobalStaff().has_user(request.user),
-                'certificate_activation_handler_url': activation_handler_url
+                'certificate_activation_handler_url': activation_handler_url,
+                'course_authoring_microfrontend_url': course_authoring_microfrontend_url,
             })
         elif "application/json" in request.META.get('HTTP_ACCEPT'):
             # Retrieve the list of certificates for the specified course
