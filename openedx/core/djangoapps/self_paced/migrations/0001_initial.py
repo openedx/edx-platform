400a5305d2ee32f8f5a1296831f4a18c2809db73
Bill Currie <bill@opencraft.com>
2020-07-13 14:59:08 +0900

    Undo accidental change to migration

--- openedx/core/djangoapps/self_paced/migrations/0001_initial.py
+++ openedx/core/djangoapps/self_paced/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('change_date', models.DateTimeField(auto_now_add=True, verbose_name='Change date')),
-                ('enabled', models.BooleanField(default=False, verbose_name='Enabled')),
+                ('enable_course_home_improvements', models.BooleanField(default=False, verbose_name='Enable course home page improvements.')),
                 ('changed_by', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, editable=False, to=settings.AUTH_USER_MODEL, null=True, verbose_name='Changed by')),
             ],
             options={
