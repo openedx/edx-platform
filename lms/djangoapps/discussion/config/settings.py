4053bfdfbd5b64d0930c2afcb45009b281b03478
Robert Raposa <rraposa@edx.org>
2020-07-15 14:48:05 -0400

    move ENABLE_FORUM_DAILY_DIGEST default (#24483)
    The default for ENABLE_FORUM_DAILY_DIGEST continues
    to be True, but this moves the default from
    common.py to code.

--- lms/djangoapps/discussion/config/settings.py
+++ lms/djangoapps/discussion/config/settings.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # .. toggle_name: FEATURES['ENABLE_FORUM_DAILY_DIGEST']
 # .. toggle_implementation: DjangoSetting
 # .. toggle_default: True
-# .. toggle_description: Set to True to enable forum notification features.
+# .. toggle_description: Settings for forums/discussions to on/off daily digest feature. Set this to True if you want to enable users to subscribe and unsubscribe for daily digest. This setting enables deprecation of daily digest.
 # .. toggle_category: discussion
 # .. toggle_use_cases: open_edx
 # .. toggle_creation_date: 2020-03-09
@@ -19,4 +19,4 @@ ENABLE_FORUM_DAILY_DIGEST = 'enable_forum_daily_digest'
 
 def is_forum_daily_digest_enabled():
     """Returns whether forum notification features should be visible"""
-    return settings.FEATURES.get('ENABLE_FORUM_DAILY_DIGEST')
+    return settings.FEATURES.get('ENABLE_FORUM_DAILY_DIGEST', True)
