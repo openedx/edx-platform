7d72210f0fa1f58dfc11704626f0ebfbcead0de7
Aarif <mraarif@outlook.com>
2020-07-29 14:21:21 +0500

    fixes 3

--- common/lib/xmodule/xmodule/modulestore/tests/django_utils.py
+++ common/lib/xmodule/xmodule/modulestore/tests/django_utils.py
@@ -486,7 +486,7 @@ class ModuleStoreTestCase(
     CREATE_USER = True
 
     # Tell Django to clean out all databases, not just default
-    multi_db = True
+    databases = '__all__'
 
     @classmethod
     def setUpClass(cls):
