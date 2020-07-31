5b4527097bec380f94811db738589b5822ff02d2
Michael Roytman <mroytman@edx.org>
2020-07-31 08:57:07 -0400

    change proctoring_escalation_email serializer to not be required and to allow null to better align with the underlying data model and use case

--- cms/djangoapps/contentstore/rest_api/v1/serializers.py
+++ cms/djangoapps/contentstore/rest_api/v1/serializers.py
@@ -12,7 +12,7 @@ class ProctoredExamSettingsSerializer(serializers.Serializer):
     enable_proctored_exams = serializers.BooleanField()
     allow_proctoring_opt_out = serializers.BooleanField()
     proctoring_provider = serializers.CharField()
-    proctoring_escalation_email = serializers.CharField(allow_blank=True)
+    proctoring_escalation_email = serializers.CharField(required=False, allow_null=True)
     create_zendesk_tickets = serializers.BooleanField()
 
 
@@ -21,4 +21,3 @@ class ProctoredExamConfigurationSerializer(serializers.Serializer):
     proctored_exam_settings = ProctoredExamSettingsSerializer()
     available_proctoring_providers = serializers.ChoiceField(get_available_providers())
     course_start_date = serializers.DateTimeField()
-    is_staff = serializers.BooleanField()
--- cms/djangoapps/contentstore/rest_api/v1/views.py
+++ cms/djangoapps/contentstore/rest_api/v1/views.py
@@ -60,7 +60,6 @@ class ProctoredExamSettingsView(APIView):
                 "proctortrack"
             ],
             "course_start_date": "2013-02-05T05:00:00Z",
-            "is_staff": true
         }
 
     ------------------------------------------------------------------------------------
@@ -100,7 +99,6 @@ class ProctoredExamSettingsView(APIView):
             data['proctored_exam_settings'] = proctored_exam_settings
             data['available_proctoring_providers'] = get_available_providers()
             data['course_start_date'] = course_metadata['start'].get('value')
-            data['is_staff'] = request.user.is_staff
 
             serializer = ProctoredExamConfigurationSerializer(data)
 
