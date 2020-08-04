ae0a294e6226a73225eef7ff4f21d293f2241081
harrisirfan <harris.irfan@arbisoft.com>
2020-08-04 22:04:03 +0500

    LP-2179 Implement 2nd review changes

--- lms/djangoapps/homepage/custom_context_processors.py
+++ lms/djangoapps/homepage/custom_context_processors.py
@@ -3,11 +3,11 @@ from edx_notifications.server.web.utils import get_notifications_widget_context
 from .constants import CONFIGS
 
 
-def notifications_context(request):
+def notifications_configs(request):
     """
-    Context processor to set global context for edx-notifications
+    Context processor to set global configs for edx-notifications
     :param request:
-    :return: edx-notifications global context
+    :return: edx-notifications global configs
     """
 
     return get_notifications_widget_context(CONFIGS)
