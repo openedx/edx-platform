dbe40dae1a8b50fea0954e85f76ebf244129186e
Manjinder Singh <49171515+jinder1s@users.noreply.github.com>
2020-08-12 12:43:07 -0400

    exposing constants to be backwards compatible (#24765)
    * exposing constants to be backwards compatible

--- openedx/core/djangoapps/plugins/constants.py
+++ openedx/core/djangoapps/plugins/constants.py
@@ -1,3 +1,15 @@
+"""
+Constants used by django app plugins
+"""
+
+# expose constants from edx-django-utils so plugins using these continue to work
+from edx_django_utils.plugins import (
+    PluginSettings,  # pylint: disable=unused-import
+    PluginURLs,  # pylint: disable=unused-import
+    PluginSignals,  # pylint: disable=unused-import
+    PluginContexts,  # pylint: disable=unused-import
+)
+
 
 class ProjectType():
     """
