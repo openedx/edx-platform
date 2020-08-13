c7fb8916009e4e3d85e458f5ccd5293a78f42bfe
Robert Raposa <rraposa@edx.org>
2020-08-13 11:33:07 -0400

    add flags and switches to toggle state
    Adds basic waffle flag and waffle switch data
    to the toggle state endpoint.
    
    ARCHBOM-1427

--- openedx/core/djangoapps/waffle_utils/tests/test_views.py
+++ openedx/core/djangoapps/waffle_utils/tests/test_views.py
@@ -3,10 +3,16 @@ Tests for waffle utils views.
 """
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
+from waffle.testutils import override_switch
 
 from student.tests.factories import UserFactory
 
+from .. import WaffleFlag, WaffleFlagNamespace, WaffleSwitch, WaffleSwitchNamespace
 from ..views import ToggleStateView
+from ..testutils import override_waffle_flag
+
+TEST_WAFFLE_FLAG_NAMESPACE = WaffleFlagNamespace('test')
+TEST_WAFFLE_FLAG = WaffleFlag(TEST_WAFFLE_FLAG_NAMESPACE, 'flag')
 
 
 class ToggleStateViewTests(TestCase):
@@ -19,6 +25,20 @@ class ToggleStateViewTests(TestCase):
     def test_failure_for_non_staff(self):
         response = self._get_toggle_state_response(is_staff=False)
         self.assertEqual(response.status_code, 403)
+
+    @override_waffle_flag(TEST_WAFFLE_FLAG, True)
+    def test_response_with_waffle_flag(self):
+        response = self._get_toggle_state_response(is_staff=True)
+        self.assertIn('waffle_flags', response.data)
+        self.assertTrue(response.data['waffle_flags'])
+        self.assertEqual(response.data['waffle_flags'][0]['name'], 'test.flag')
+
+    @override_switch('test.switch', True)
+    def test_response_with_waffle_switch(self):
+        response = self._get_toggle_state_response(is_staff=True)
+        self.assertIn('waffle_switches', response.data)
+        self.assertTrue(response.data['waffle_switches'])
+        self.assertEqual(response.data['waffle_switches'][0]['name'], 'test.switch')
 
     def _get_toggle_state_response(self, is_staff=True):
         request = APIRequestFactory().get('/api/toggles/state/')
