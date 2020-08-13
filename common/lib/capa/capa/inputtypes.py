openedx/edx-platform

commit 07df64eb8cf0e5ab5a63c8ab338838284c22f593

Fix the rest of these.

common/lib/capa/capa/inputtypes.py
@@ -258,7 +258,7 @@ class InputTypeBase(object):
             msg = u"Error in xml '{x}': {err} ".format(
                 x=etree.tostring(xml), err=text_type(err))
             msg = Exception(msg)
-            six.reraise(Exception, msg, sys.exc_info()[2])
+            six.reraise(Exception, Exception(msg), sys.exc_info()[2])
 
     @classmethod
     def get_attributes(cls):

common/lib/xmodule/xmodule/capa_base.py
@@ -294,7 +294,7 @@ class CapaMixin(ScorableXBlockMixin, CapaFields):
         except Exception as err:  # pylint: disable=broad-except
             msg = u'cannot create LoncapaProblem {loc}: {err}'.format(
                 loc=text_type(self.location), err=err)
-            six.reraise(Exception(msg), None, sys.exc_info()[2])
+            six.reraise(Exception, Exception(msg), sys.exc_info()[2])
 
         if self.score is None:
             self.set_score(self.score_from_lcp(lcp))

common/lib/xmodule/xmodule/capa_module.py
@@ -200,7 +200,7 @@ class ProblemBlock(
                 self.scope_ids.user_id
             )
             _, _, traceback_obj = sys.exc_info()  # pylint: disable=redefined-outer-name
-            six.reraise(ProcessingError(generic_error_message), None, traceback_obj)
+            six.reraise(ProcessingError, ProcessingError(generic_error_message), traceback_obj)
 
         after = self.get_progress()
         after_attempts = self.attempts

common/lib/xmodule/xmodule/graders.py
@@ -193,7 +193,7 @@ def grader_from_conf(conf):
             msg = ("Unable to parse grader configuration:\n    " +
                    str(subgraderconf) +
                    "\n    Error was:\n    " + str(error))
-            six.reraise(ValueError(msg), None, sys.exc_info()[2])
+            six.reraise(ValueError, ValueError(msg), sys.exc_info()[2])
 
     return WeightedSubsectionsGrader(subgraders)
 

common/lib/xmodule/xmodule/html_module.py
@@ -291,7 +291,7 @@ class HtmlBlock(
                 msg = 'Unable to load file contents at path {0}: {1} '.format(
                     filepath, err)
                 # add more info and re-raise
-                six.reraise(Exception(msg), None, sys.exc_info()[2])
+                six.reraise(Exception, Exception(msg), sys.exc_info()[2])
 
     @classmethod
     def parse_xml_new_runtime(cls, node, runtime, keys):

commit b2a32bbb69135042f88f45f132765d9b31842c23

Fix double-wrapped exception in capa/inputtypes.py
(accidentally introduced in 07df64eb8cf0e5ab5a63c8ab338838284c22f593 last year)

common/lib/capa/capa/inputtypes.py
@@ -258,7 +258,6 @@ class InputTypeBase(object):
             # Something went wrong: add xml to message, but keep the traceback
             msg = u"Error in xml '{x}': {err} ".format(
                 x=etree.tostring(xml), err=text_type(err))
-            msg = Exception(msg)
             six.reraise(Exception, Exception(msg), sys.exc_info()[2])
 
     @classmethod