3f70f7c0f55421113ceed20e525193579abbf510
Ahtisham Shahid <ahtisham300@gmail.com>
2020-08-19 16:43:39 +0500

    Fixed linter issues

--- lms/static/js/spec/instructor_dashboard/data_download_spec.js
+++ lms/static/js/spec/instructor_dashboard/data_download_spec.js
@@ -6,13 +6,13 @@ define(['jquery',
     function($, DataDownload, AjaxHelpers) {
         'use strict';
         describe('edx.instructor_dashboard.data_download.DataDownload_Certificate', function() {
-            var url, data_download_certificate;
+            var url, dataDownloadCertificates;
 
             beforeEach(function() {
                 loadFixtures('js/fixtures/instructor_dashboard/data_download.html');
-                data_download_certificate = new window.DataDownload_Certificate($('.issued_certificates'));
+                dataDownloadCertificates = new window.DataDownload_Certificate($('.issued_certificates'));
                 url = '/courses/PU/FSc/2014_T4/instructor/api/get_issued_certificates';
-                data_download_certificate.$list_issued_certificate_table_btn.data('endpoint', url);
+                dataDownloadCertificates.$list_issued_certificate_table_btn.data('endpoint', url);
             });
 
             it('show data on success callback', function() {
@@ -24,18 +24,18 @@ define(['jquery',
                     feature_names: {course_id: 'Course ID', mode: ' Mode'}
                 };
 
-                data_download_certificate.$list_issued_certificate_table_btn.click();
+                dataDownloadCertificates.$list_issued_certificate_table_btn.click();
                 AjaxHelpers.expectJsonRequest(requests, 'POST', url);
 
                 // Simulate a success response from the server
                 AjaxHelpers.respondWithJson(requests, data);
-                expect(data_download_certificate.$certificate_display_table.html()
+                expect(dataDownloadCertificates.$certificate_display_table.html()
                     .indexOf('Course ID') !== -1).toBe(true);
-                expect(data_download_certificate.$certificate_display_table.html()
+                expect(dataDownloadCertificates.$certificate_display_table.html()
                     .indexOf('Mode') !== -1).toBe(true);
-                expect(data_download_certificate.$certificate_display_table.html()
+                expect(dataDownloadCertificates.$certificate_display_table.html()
                     .indexOf('xyz_test') !== -1).toBe(true);
-                expect(data_download_certificate.$certificate_display_table.html()
+                expect(dataDownloadCertificates.$certificate_display_table.html()
                     .indexOf('honor') !== -1).toBe(true);
             });
 
@@ -43,27 +43,27 @@ define(['jquery',
                 // Spy on AJAX requests
                 var requests = AjaxHelpers.requests(this);
 
-                data_download_certificate.$list_issued_certificate_table_btn.click();
+                dataDownloadCertificates.$list_issued_certificate_table_btn.click();
                 // Simulate a error response from the server
                 AjaxHelpers.respondWithError(requests);
-                expect(data_download_certificate.$certificates_request_response_error.text())
+                expect(dataDownloadCertificates.$certificates_request_response_error.text())
                     .toEqual('Error getting issued certificates list.');
             });
 
             it('error should be clear from UI on success callback', function() {
                 var requests = AjaxHelpers.requests(this);
-                data_download_certificate.$list_issued_certificate_table_btn.click();
+                dataDownloadCertificates.$list_issued_certificate_table_btn.click();
 
                 // Simulate a error response from the server
                 AjaxHelpers.respondWithError(requests);
-                expect(data_download_certificate.$certificates_request_response_error.text())
+                expect(dataDownloadCertificates.$certificates_request_response_error.text())
                     .toEqual('Error getting issued certificates list.');
 
                 // Simulate a success response from the server
-                data_download_certificate.$list_issued_certificate_table_btn.click();
+                dataDownloadCertificates.$list_issued_certificate_table_btn.click();
                 AjaxHelpers.expectJsonRequest(requests, 'POST', url);
 
-                expect(data_download_certificate.$certificates_request_response_error.text())
+                expect(dataDownloadCertificates.$certificates_request_response_error.text())
                     .not.toEqual('Error getting issued certificates list');
             });
         });
