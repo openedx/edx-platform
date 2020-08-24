23e78bbd8a8cb518f16865060614e076d0382bb3
Simon Chen <schen@edx.org>
2020-08-24 15:37:24 -0400

    MST-348 Add the API to provide detailed ID Verification information

--- openedx/core/djangoapps/user_api/verification_api/views.py
+++ openedx/core/djangoapps/user_api/verification_api/views.py
@@ -1,19 +1,21 @@
 """ Verification API v1 views. """
 
-
+from django.contrib.auth import get_user_model
 from django.http import Http404
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.generics import RetrieveAPIView
-from openedx.core.lib.api.authentication import BearerAuthentication
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from lms.djangoapps.verify_student.models import ManualVerification, SoftwareSecurePhotoVerification, SSOVerification
+from lms.djangoapps.verify_student.services import IDVerificationService
 from lms.djangoapps.verify_student.utils import most_recent_verification
 from openedx.core.djangoapps.user_api.serializers import (
+    IDVerificationDetailsSerializer,
     ManualVerificationSerializer,
     SoftwareSecurePhotoVerificationSerializer,
     SSOVerificationSerializer
 )
+from openedx.core.lib.api.authentication import BearerAuthentication
 from openedx.core.lib.api.permissions import IsStaffOrOwner
 
 
@@ -53,3 +55,27 @@ class IDVerificationStatusView(RetrieveAPIView):
             return verification
 
         raise Http404
+
+
+class IDVerificationStatusDetailsView(ListAPIView):
+    """ IDVerificationStatusDeetails endpoint to retrieve more details about ID Verification status """
+    authentication_classes = (JwtAuthentication, BearerAuthentication, SessionAuthentication,)
+    permission_classes = (IsStaffOrOwner,)
+    pagination_class = None  # No need for pagination for this yet
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['context'] = self.get_serializer_context()
+        return IDVerificationDetailsSerializer(*args, **kwargs)
+
+    def get_queryset(self):
+        username = self.kwargs['username']
+        User = get_user_model()
+        try:
+            user = User.objects.get(username=username)
+            verifications = IDVerificationService.verifications_for_user(user)
+            if not verifications:
+                raise Http404
+
+            return sorted(verifications, key=lambda x: x.updated_at, reverse=True)
+        except User.DoesNotExist:
+            raise Http404
