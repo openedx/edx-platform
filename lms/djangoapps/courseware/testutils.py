2a157ae8543c4a2116a8c323e593167ebd376a20
Calen Pennington <cale@edx.org>
2020-08-26 10:38:42 -0400

    Lower query counts

--- lms/djangoapps/courseware/testutils.py
+++ lms/djangoapps/courseware/testutils.py
@@ -155,9 +155,9 @@ class RenderXBlockTestMixin(six.with_metaclass(ABCMeta, object)):
         return response
 
     @ddt.data(
-        ('vertical_block', ModuleStoreEnum.Type.mongo, 13),
+        ('vertical_block', ModuleStoreEnum.Type.mongo, 12),
         ('vertical_block', ModuleStoreEnum.Type.split, 6),
-        ('html_block', ModuleStoreEnum.Type.mongo, 14),
+        ('html_block', ModuleStoreEnum.Type.mongo, 13),
         ('html_block', ModuleStoreEnum.Type.split, 6),
     )
     @ddt.unpack
@@ -214,7 +214,7 @@ class RenderXBlockTestMixin(six.with_metaclass(ABCMeta, object)):
         Helper method used by test_success_enrolled_staff because one test
         class using this mixin has an increased number of mongo (only) queries.
         """
-        return 9
+        return 8
 
     def test_success_unenrolled_staff(self):
         self.setup_course()
