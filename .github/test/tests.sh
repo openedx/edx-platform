8ff7f7f23b63704cb6e2fa5e1b6a3ee6e4b7cf05
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-08-27 13:28:29 +0000

    eol: Run tests separately

--- .github/test/tests.sh
+++ .github/test/tests.sh
@@ -11,12 +11,8 @@ pip3 install -e git+https://github.com/edx/edx-ora2.git@2.7.6#egg=ora2==2.7.6
 
 # Task Helper modifications for uchileedxlogin package
 ## Test before installing the package
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest \
-	lms/djangoapps/instructor_task/tests/test_tasks_helper.py \
-	lms/djangoapps/instructor_analytics/tests/test_basic.py \
-	lms/djangoapps/certificates/tests/test_webview_views.py \
-	lms/djangoapps/bulk_email/tests/ \
-	lms/djangoapps/instructor/tests/
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest $1
+
 #DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest lms/djangoapps/bulk_email/tests/ lms/djangoapps/instructor/tests/
 #DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest lms/djangoapps/bulk_email/tests/test_models_eol.py lms/djangoapps/instructor/tests/test_api_eol.py
 ## Test after installing the package
