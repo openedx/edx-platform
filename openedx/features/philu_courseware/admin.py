d707dddb8b3c73793083b54e5fc120f3cfe40b09
Saad Waqar <=>
2020-09-03 14:52:02 +0500

    LP-2264: Added new model CourseEnrollmentMeta for storing program_uuid

--- openedx/features/philu_courseware/admin.py
+++ openedx/features/philu_courseware/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import CompetencyAssessmentRecord
+from .models import CompetencyAssessmentRecord, CourseEnrollmentMeta
 
 
 class CompetencyAssessmentRecordModelAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
 
 
+class CourseEnrollmentMetaModelAdmin(admin.ModelAdmin):
+    list_display = ('course_enrollment', 'program_uuid')
+    raw_id_fields = ('course_enrollment',)
+
+
 admin.site.register(CompetencyAssessmentRecord, CompetencyAssessmentRecordModelAdmin)
+admin.site.register(CourseEnrollmentMeta, CourseEnrollmentMetaModelAdmin)
