96ea20600865a20cade840efb0979e1c6acc50e0
Shimul Chowdhury <shimul@opencraft.com>
2020-09-10 17:15:54 +0600

    3rd party imports should be on top

--- cms/djangoapps/api/v1/views/discussion_settings.py
+++ cms/djangoapps/api/v1/views/discussion_settings.py
@@ -4,13 +4,13 @@
 from django.conf import settings
 from django.http import Http404
 from opaque_keys.edx.keys import CourseKey
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 from cms.djangoapps.contentstore.api.views.utils import course_author_access_required
 from lms.djangoapps.courseware.courses import get_course_by_id
 from models.settings.course_metadata import CourseMetadata
 from openedx.core.lib.api.view_utils import DeveloperErrorViewMixin, view_auth_classes
-from rest_framework import viewsets
-from rest_framework.response import Response
 
 from ..serializers.discussion_settings import DiscussionSettingsSerializer
 
