30632f87cc71dcb3f3ba0c52ebeefa1995409ba0
Farhaan Bukhsh <farhaan@opencraft.com>
2020-09-18 02:53:05 +0530

    Added Open response button to the component tray
    Author needs to have a quick access to ORA menu in order
    to get proceed with assesment design. This introduces ORA
    button to the tray.
    
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- cms/djangoapps/contentstore/views/component.py
+++ cms/djangoapps/contentstore/views/component.py
@@ -41,7 +41,7 @@ __all__ = [
 log = logging.getLogger(__name__)
 
 # NOTE: This list is disjoint from ADVANCED_COMPONENT_TYPES
-COMPONENT_TYPES = ['discussion', 'html', 'problem', 'video']
+COMPONENT_TYPES = ['discussion', 'html', 'problem', 'video', 'openassessment']
 
 ADVANCED_COMPONENT_TYPES = sorted(set(name for name, class_ in XBlock.load_classes()) - set(COMPONENT_TYPES))
 
@@ -260,7 +260,8 @@ def get_component_templates(courselike, library=False):
         'discussion': _("Discussion"),
         'html': _("HTML"),
         'problem': _("Problem"),
-        'video': _("Video")
+        'video': _("Video"),
+        'openassessment': _("Open Response")
     }
 
     component_templates = []
