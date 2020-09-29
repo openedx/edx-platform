601e236d992d1c0d8fcaa2485177b397f4f256b6
Soban Javed <iamsobanjaved@gmail.com>
2020-09-29 16:05:26 +0500

    Upadte celery settings and namespace

--- lms/envs/devstack_with_worker.py
+++ lms/envs/devstack_with_worker.py
@@ -20,8 +20,8 @@ import os
 from lms.envs.devstack import *
 
 # Require a separate celery worker
-CELERY_ALWAYS_EAGER = False
-BROKER_URL = 'redis://:password@edx.devstack.redis:6379/'
+CELERY_TASK_ALWAYS_EAGER = False
+CELERY_BROKER_URL = 'redis://:password@edx.devstack.redis:6379/'
 # Disable transaction management because we are using a worker. Views
 # that request a task and wait for the result will deadlock otherwise.
 for database_name in DATABASES:
