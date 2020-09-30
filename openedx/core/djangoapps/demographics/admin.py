200b423412163447c9d3e7e14e6e65172ef14be6
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-09-30 19:05:33 -0400

    Added search to User Demographics Admin page

--- openedx/core/djangoapps/demographics/admin.py
+++ openedx/core/djangoapps/demographics/admin.py
@@ -11,8 +11,9 @@ class UserDemographicsAdmin(admin.ModelAdmin):
     """
     Admin for UserDemographics Model
     """
-    list_display = ('user', 'show_call_to_action')
+    list_display = ('id', 'user', 'show_call_to_action')
     readonly_fields = ('user',)
+    search_fields = ('id', 'user__username')
 
     class Meta(object):
         model = UserDemographics
