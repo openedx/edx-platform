cb777b3b44c33c646c13e48fcf537483d3b55fbd
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-10-05 12:48:52 +0500

    celery name space.

--- lms/envs/bok_choy.py
+++ lms/envs/bok_choy.py
@@ -32,7 +32,7 @@ TEST_ROOT = CONFIG_ROOT.dirname().dirname() / "test_root"
 # Unlike in prod, we use the JSON files stored in this repo.
 # This is a convenience for ensuring (a) that we can consistently find the files
 # and (b) that the files are the same in Jenkins as in local dev.
-os.environ['SERVICE_VARIANT'] = 'bok_choy_docker' if 'BOK_CHOY_HOSTNAME' in os.environ else 'bok_choy'
+os.environ['SERVICE_VARIANT'] = 'bok_choy_docker' if 'BOK_CHOY_HOSTNAME' in os.environ else 'bok_choy1'
 os.environ['LMS_CFG'] = str.format("{config_root}/{service_variant}.yml",
                                    config_root=CONFIG_ROOT, service_variant=os.environ['SERVICE_VARIANT'])
 os.environ['REVISION_CFG'] = "{config_root}/revisions.yml".format(config_root=CONFIG_ROOT)
