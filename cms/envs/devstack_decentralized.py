f7578162db61da03f6bebf154e58b95980d84c13
Soban Javed <iamsobanjaved@gmail.com>
2020-10-05 19:31:40 +0500

    Upadte celery settings and namespace

--- cms/envs/devstack_decentralized.py
+++ cms/envs/devstack_decentralized.py
@@ -73,7 +73,7 @@ DJFS = {
 ################################# CELERY ######################################
 
 # By default don't use a worker, execute tasks as if they were local functions
-CELERY_ALWAYS_EAGER = True
+CELERY_TASK_ALWAYS_EAGER = True
 
 
 def should_show_debug_toolbar(request):
