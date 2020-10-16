d0f3ece5b7e4785a31882e40598d18b1bd5b11a2
Omar Al-Ithawi <i@omardo.com>
2020-10-16 11:46:22 +0300

    Revert "fix variable name"
    This reverts commit 156dbce36bad303a25385365047e66e8215f0ae8.

--- cms/djangoapps/contentstore/views/import_export.py
+++ cms/djangoapps/contentstore/views/import_export.py
@@ -374,11 +374,11 @@ def export_status_handler(request, course_key_string):
         if isinstance(artifact.file.storage, FileSystemStorage):
             output_url = reverse_course_url('export_output_handler', course_key)
         elif isinstance(artifact.file.storage, S3Boto3Storage):
-            disposition = 'attachment; filename="{}"'.format(artifact.file.name)
+            disposition = 'attachment; filename="{}"'.format(filename)
             params = {
-                'ResponseContentDisposition': disposition,
+                "ResponseContentDisposition": disposition,
                 'ResponseContentEncoding': 'application/octet-stream',
-                'ResponseContentType': 'application/x-tgz',
+                "ResponseContentType": "application/x-tgz",
             }
             output_url = artifact.file.storage.url(
                 artifact.file.name,
