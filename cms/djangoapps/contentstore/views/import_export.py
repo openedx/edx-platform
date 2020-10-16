153bb21ba98515398d2295650bdcdac3cab3a1e9
Omar Al-Ithawi <i@omardo.com>
2020-10-16 11:46:22 +0300

    Revert "add proper header for export course download"
    This reverts commit 8077909dd45253f4af5237dad320168f589dadce.

--- cms/djangoapps/contentstore/views/import_export.py
+++ cms/djangoapps/contentstore/views/import_export.py
@@ -374,16 +374,7 @@ def export_status_handler(request, course_key_string):
         if isinstance(artifact.file.storage, FileSystemStorage):
             output_url = reverse_course_url('export_output_handler', course_key)
         elif isinstance(artifact.file.storage, S3Boto3Storage):
-            disposition = 'attachment; filename="{}"'.format(filename)
-            params = {
-                "ResponseContentDisposition": disposition,
-                'ResponseContentEncoding': 'application/octet-stream',
-                "ResponseContentType": "application/x-tgz",
-            }
-            output_url = artifact.file.storage.url(
-                artifact.file.name,
-                parameters=params
-            )
+            output_url = artifact.file.storage.url(artifact.file.name)
         else:
             output_url = artifact.file.storage.url(artifact.file.name)
     elif task_status.state in (UserTaskStatus.FAILED, UserTaskStatus.CANCELED):
