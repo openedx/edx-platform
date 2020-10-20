58049b38a2a47c43957b8a64f7e7beb6eda81702
Kyle McCormick <kmccormick@edx.org>
2020-10-20 17:17:47 -0400

    Move XModule's setup.py and dependencies to root

--- setup.py
+++ setup.py
@@ -4,6 +4,51 @@ Setup script for the Open edX package.
 
 from setuptools import setup
 
+XMODULES = [
+    "book = xmodule.backcompat_module:TranslateCustomTagDescriptor",
+    "chapter = xmodule.seq_module:SectionDescriptor",
+    "course = xmodule.course_module:CourseDescriptor",
+    "customtag = xmodule.template_module:CustomTagDescriptor",
+    "discuss = xmodule.backcompat_module:TranslateCustomTagDescriptor",
+    "image = xmodule.backcompat_module:TranslateCustomTagDescriptor",
+    "error = xmodule.error_module:ErrorDescriptor",
+    "nonstaff_error = xmodule.error_module:NonStaffErrorDescriptor",
+    "poll_question = xmodule.poll_module:PollDescriptor",
+    "problemset = xmodule.seq_module:SequenceDescriptor",
+    "split_test = xmodule.split_test_module:SplitTestDescriptor",
+    "section = xmodule.backcompat_module:SemanticSectionDescriptor",
+    "sequential = xmodule.seq_module:SequenceDescriptor",
+    "slides = xmodule.backcompat_module:TranslateCustomTagDescriptor",
+    "videodev = xmodule.backcompat_module:TranslateCustomTagDescriptor",
+    "videosequence = xmodule.seq_module:SequenceDescriptor",
+    "custom_tag_template = xmodule.raw_module:RawDescriptor",
+    "annotatable = xmodule.annotatable_module:AnnotatableDescriptor",
+    "hidden = xmodule.hidden_module:HiddenDescriptor",
+    "raw = xmodule.raw_module:RawDescriptor",
+    "lti = xmodule.lti_module:LTIDescriptor",
+]
+XBLOCKS = [
+    "about = xmodule.html_module:AboutBlock",
+    "conditional = xmodule.conditional_module:ConditionalBlock",
+    "course_info = xmodule.html_module:CourseInfoBlock",
+    "html = xmodule.html_module:HtmlBlock",
+    "library = xmodule.library_root_xblock:LibraryRoot",
+    "library_content = xmodule.library_content_module:LibraryContentBlock",
+    "library_sourced = xmodule.library_sourced_block:LibrarySourcedBlock",
+    "problem = xmodule.capa_module:ProblemBlock",
+    "randomize = xmodule.randomize_module:RandomizeBlock",
+    "static_tab = xmodule.html_module:StaticTabBlock",
+    "unit = xmodule.unit_block:UnitBlock",
+    "vertical = xmodule.vertical_block:VerticalBlock",
+    "video = xmodule.video_module:VideoBlock",
+    "videoalpha = xmodule.video_module:VideoBlock",
+    "word_cloud = xmodule.word_cloud_module:WordCloudBlock",
+    "wrapper = xmodule.wrapper_module:WrapperBlock",
+]
+XBLOCKS_ASIDES = [
+    'tagging_aside = cms.lib.xblock.tagging:StructuredTagsAside',
+]
+
 setup(
     name="Open edX",
     version="0.12",
@@ -15,6 +60,7 @@ setup(
         "cms",
         "lms",
         "openedx",
+        "xmodule",
     ],
     entry_points={
         "openedx.course_tab": [
@@ -116,6 +162,12 @@ setup(
         'openedx.dynamic_partition_generator': [
             'enrollment_track = common.lib.xmodule.xmodule.partitions.enrollment_track_partition_generator:create_enrollment_track_partition',
             'content_type_gating = openedx.features.content_type_gating.partitions:create_content_gating_partition'
+        ],
+        'xblock.v1': XMODULES + XBLOCKS,
+        'xmodule.v1': XMODULES,
+        'xblock_asides.v1': XBLOCKS_ASIDES,
+        'console_scripts': [
+            'xmodule_assets = xmodule.static_content:main',
         ],
     }
 )
