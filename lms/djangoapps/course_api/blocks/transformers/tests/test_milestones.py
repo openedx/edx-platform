437cff2cd0ae8e5711919f400482202ac92bce0f
Kyle McCormick <kmccormick@edx.org>
2020-10-21 14:26:16 -0400

    Import from lms.djangoapps.gating instead of gating

--- lms/djangoapps/course_api/blocks/transformers/tests/test_milestones.py
+++ lms/djangoapps/course_api/blocks/transformers/tests/test_milestones.py
@@ -8,7 +8,7 @@ import six
 from milestones.tests.utils import MilestonesTestCaseMixin
 from mock import Mock, patch
 
-from gating import api as lms_gating_api
+from lms.djangoapps.gating import api as lms_gating_api
 from lms.djangoapps.course_blocks.api import get_course_blocks
 from lms.djangoapps.course_blocks.transformers.tests.helpers import CourseStructureTestCase
 from openedx.core.djangoapps.content.block_structure.transformers import BlockStructureTransformers
