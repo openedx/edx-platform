473299a78092f9cd9166f11ad001ba668f6e72ce
Kyle McCormick <kmccormick@edx.org>
2020-10-21 13:55:35 -0400

    Import from lms.djangoapps.course_wiki instead of course_wiki

--- lms/djangoapps/course_wiki/settings.py
+++ lms/djangoapps/course_wiki/settings.py
@@ -4,7 +4,7 @@ a user has on an article.
 """
 
 
-from course_wiki.utils import user_is_article_course_staff
+from lms.djangoapps.course_wiki.utils import user_is_article_course_staff
 
 
 def CAN_DELETE(article, user):  # pylint: disable=invalid-name
