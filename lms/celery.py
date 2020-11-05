5a2ea1f95486f36df70615b52ba26502767bb6b5
Muhammad Soban Javed <58461728+iamsobanjaved@users.noreply.github.com>
2020-11-06 02:05:48 +0500

    Revert "Update routing config"

--- lms/celery.py
+++ lms/celery.py
@@ -5,11 +5,12 @@ and auto discover tasks in all installed django apps.
 Taken from: https://celery.readthedocs.org/en/latest/django/first-steps-with-django.html
 """
 
+
 import os
 
 from celery import Celery
 
-from openedx.core.lib.celery.routers import route_task_queue
+from openedx.core.lib.celery.routers import AlternateEnvironmentRouter
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lms.envs.production')
@@ -22,16 +23,15 @@ APP.conf.task_protocol = 1
 APP.config_from_object('django.conf:settings')
 APP.autodiscover_tasks()
 
-# Import after autodiscovery has had a chance to connect to the import_module signal
-# so celery doesn't miss any apps getting installed.
-from django.conf import settings  # pylint: disable=wrong-import-position,wrong-import-order
 
-
-def route_task(name, args, kwargs, options, task=None, **kw):  # pylint: disable=unused-argument
+class Router(AlternateEnvironmentRouter):
     """
-    Celery-defined method allowing for custom routing logic.
-
-    If None is returned from this method, default routing logic is used.
+    An implementation of AlternateEnvironmentRouter, for routing tasks to non-cms queues.
     """
 
-    return route_task_queue(name, settings.EXPLICIT_QUEUES, settings.ALTERNATE_ENV_TASKS)
+    @property
+    def alternate_env_tasks(self):
+        """
+        Defines alternate environment tasks, as a dict of form { task_name: alternate_queue }
+        """
+        return {}
