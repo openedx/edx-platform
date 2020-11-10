dd29906fa0ae3a1e2ed6dc7a2e5e69ed08fea65a
Kshitij Sobti <kshitij@sobti.in>
2020-11-10 21:31:45 +0530

    Fix quality issues

--- openedx/features/discussion_provider_piazza/views.py
+++ openedx/features/discussion_provider_piazza/views.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 from typing import Dict
 from urllib.parse import quote
 
+from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpRequest
 from lti_consumer.lti_1p1.contrib.django import lti_embed
@@ -14,6 +15,7 @@ from web_fragments.fragment import Fragment
 from lms.djangoapps.courseware.access import get_user_role
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from openedx.core.djangoapps.discussions.api.config import get_course_discussion_config
+from openedx.core.djangoapps.discussions.api.data import CourseDiscussionConfigData
 from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
 from openedx.core.djangolib.markup import HTML
 from student.models import anonymous_id_for_user
@@ -22,7 +24,11 @@ from student.models import anonymous_id_for_user
 LtiCredentials = namedtuple('LtiCredentials', ('oauth_key', 'oauth_secret'))
 
 
-class LtiLaunchMixin:
+class LtiCourseLaunchMixin:
+    """
+    Mixin that encapsulates all LTI-related functionality from the View
+    """
+
     LTI_LAUNCH_URL = None
     ROLE_MAP = {
         'student': u'Student',
@@ -36,21 +42,21 @@ class LtiLaunchMixin:
         return {}
 
     @staticmethod
-    def get_user_id(user, course_key):
+    def get_user_id(user: User, course_key: CourseKey):
         return quote(anonymous_id_for_user(user, course_key))
 
-    def get_lti_roles(self, user, course_key) -> str:
+    def get_lti_roles(self, user: User, course_key: CourseKey) -> str:
         return self.ROLE_MAP.get(
             get_user_role(user, course_key),
             self.DEFAULT_ROLE,
         )
 
     @staticmethod
-    def get_context_id(course_key) -> str:
+    def get_context_id(course_key: CourseKey) -> str:
         return quote(str(course_key))
 
     @staticmethod
-    def get_resource_link_id(course_key, request) -> str:
+    def get_resource_link_id(course_key: CourseKey, request: HttpRequest) -> str:
         site = get_current_site(request)
         return quote('{}-{}'.format(
             site.domain,
@@ -58,7 +64,7 @@ class LtiLaunchMixin:
         ))
 
     @staticmethod
-    def get_result_sourcedid(context_id, resource_link_id, user_id) -> str:
+    def get_result_sourcedid(context_id: str, resource_link_id: str, user_id: str) -> str:
         return "{context}:{resource_link}:{user_id}".format(
             context=context_id,
             resource_link=resource_link_id,
@@ -66,7 +72,7 @@ class LtiLaunchMixin:
         )
 
     @staticmethod
-    def get_context_title(course_key):
+    def get_context_title(course_key: CourseKey) -> str:
         course = CourseOverview.get_from_id(course_key)
         return "{} - {}".format(
             course.display_name_with_default,
@@ -74,10 +80,20 @@ class LtiLaunchMixin:
         )
 
     @staticmethod
-    def get_oauth_credentials(course_config) -> LtiCredentials:
+    def get_oauth_credentials(config: CourseDiscussionConfigData) -> LtiCredentials:
         raise NotImplementedError
 
-    def get_lti_embed_code(self, course_key, request):
+    def get_lti_embed_code(self, course_key: CourseKey, request: HttpRequest) -> str:
+        """
+        Returns the LTI embed code for embedding in the current course context.
+        Args:
+            course_key (CourseKey): Course key for course in which to embed LTI.
+            request (HttpRequest): Request object for view in which LTI will be embedded.
+
+        Returns:
+            HTML code to embed LTI in course page.
+
+        """
         discussion_config = get_course_discussion_config(course_key)
         lti_creds = self.get_oauth_credentials(discussion_config)
         user_id = self.get_user_id(request.user, course_key)
@@ -103,7 +119,17 @@ class LtiLaunchMixin:
             **additional_params,
         )
 
-    def render_to_fragment(self, request, course_id=None):  # pylint: disable=arguments-differ
+    def render_to_fragment(self, request: HttpRequest, course_id: str) -> Fragment:
+        """
+        Returns a fragment view for the LTI launch.
+        Args:
+            request (HttpRequest): request object
+            course_id (str): A string course id
+
+        Returns:
+            A Fragment that embeds LTI in a course page.
+
+        """
         course_key = CourseKey.from_string(course_id)
         lti_embed_html = self.get_lti_embed_code(course_key, request)
 
@@ -130,7 +156,10 @@ class LtiLaunchMixin:
         return fragment
 
 
-class PiazzaCourseTabView(LtiLaunchMixin, EdxFragmentView):
+class PiazzaCourseTabView(LtiCourseLaunchMixin, EdxFragmentView):
+    """
+    Course tab view for Piazza discusion provider.
+    """
     LTI_LAUNCH_URL = "https://piazza.com/connect"
 
     @staticmethod
@@ -139,5 +168,3 @@ class PiazzaCourseTabView(LtiLaunchMixin, EdxFragmentView):
             config.config["consumer_key"],
             config.config["consumer_secret"],
         )
-
-
