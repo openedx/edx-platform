d6de26ba5149df4f921df5b656f110c5f7bf0b25
Kyle McCormick <kmccormick@edx.org>
2020-11-24 12:28:29 -0500

    Regenerate import_shims (for Koa)
    Command:
    $ import_shims/regenerate_current_shims.sh

--- import_shims/lms/discussion/notifier_api/tests.py
+++
@@ -1,5 +0,0 @@
-from import_shims.warn import warn_deprecated_import
-
-warn_deprecated_import('discussion.notifier_api.tests', 'lms.djangoapps.discussion.notifier_api.tests')
-
-from lms.djangoapps.discussion.notifier_api.tests import *
