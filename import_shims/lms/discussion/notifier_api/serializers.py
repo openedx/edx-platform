04594dbb3c424c2854fc15ac3da44033875c7d7a
Kyle McCormick <kmccormick@edx.org>
2020-11-24 12:59:12 -0500

    Revert "Fix linting as it related to import_shims, and vice versa (#25632)"
    This reverts commit 390d1b2d5caf8969b18591cdc22393b072fe4efd.
    
    It may be causing "Unregistered Celery task" errors on production.

---
+++ import_shims/lms/discussion/notifier_api/serializers.py
@@ -0,0 +1,5 @@
+from import_shims.warn import warn_deprecated_import
+
+warn_deprecated_import('discussion.notifier_api.serializers', 'lms.djangoapps.discussion.notifier_api.serializers')
+
+from lms.djangoapps.discussion.notifier_api.serializers import *
---
+++ import_shims/lms/discussion/notifier_api/views.py
@@ -0,0 +1,5 @@
+from import_shims.warn import warn_deprecated_import
+
+warn_deprecated_import('discussion.notifier_api.views', 'lms.djangoapps.discussion.notifier_api.views')
+
+from lms.djangoapps.discussion.notifier_api.views import *
