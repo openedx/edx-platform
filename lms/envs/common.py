3a738e6c02f3b5b93975512e35e9b2f3141eab5f
Zach Hancock <zhancock@edx.org>
2020-12-12 13:41:13 -0500

    Create runtime api to return active enrollments by course
    Co-authored-by: Bianca Severino <biancasev@gmail.com>

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2574,7 +2574,7 @@ INSTALLED_APPS = [
     'common.djangoapps.course_modes.apps.CourseModesConfig',
 
     # Enrollment API
-    'openedx.core.djangoapps.enrollments',
+    'openedx.core.djangoapps.enrollments.apps.EnrollmentsConfig',
 
     # Entitlement API
     'common.djangoapps.entitlements.apps.EntitlementsConfig',
