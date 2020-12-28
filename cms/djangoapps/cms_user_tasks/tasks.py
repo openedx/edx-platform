dac00e1da3df05c952f59632f0c1424156c6aca5
Taimoor Ahmed <68893403+taimoor-ahmed-1@users.noreply.github.com>
2020-12-28 12:49:20 +0500

    Fix export course email not recieving EDLY-2252 (#189)

--- cms/djangoapps/cms_user_tasks/tasks.py
+++ cms/djangoapps/cms_user_tasks/tasks.py
@@ -26,6 +26,8 @@ def send_task_complete_email(self, task_name, task_state_text, dest_addr, detail
         'DISABLE_CMS_TASK_EMAILS',
         settings.FEATURES.get('DISABLE_CMS_TASK_EMAILS', True)
     )
+    disable_emails = True if disable_emails == 'true' else False if disable_emails == 'false' else disable_emails
+
     if disable_emails:
         LOGGER.info(
             'Studio task emails are disabled. To enable them, \
