7d2cefbffdfe1015f48d3bfdabddad11eb8d4bbc
Kyle McCormick <kmccormick@edx.org>
2021-01-06 09:55:29 -0500

    refactor: remove superfluous courseware/__init__.py warning

--- lms/djangoapps/courseware/__init__.py
+++
@@ -1,7 +0,0 @@
-# lint-amnesty, pylint: disable=missing-module-docstring
-import warnings
-
-if __name__ == 'courseware':
-    # Show the call stack that imported us wrong.
-    msg = "Importing 'lms.djangoapps.courseware' as 'courseware' is no longer supported"
-    warnings.warn(msg, DeprecationWarning)
