285ec771bf8bc4e7e1414de6625db2336e1bc9eb
Peter Pinch <pdpinch+github@hawklake.com>
2021-01-06 08:15:35 -0500

    fix: Remove CAS settings (#25330)
    part of DEPR-6

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1422,10 +1422,6 @@ STATICFILES_DIRS = [
 FAVICON_PATH = 'images/favicon.ico'
 DEFAULT_COURSE_ABOUT_IMAGE_URL = 'images/pencils.jpg'
 
-CAS_SERVER_URL = ""
-CAS_EXTRA_LOGIN_PARAMS = ""
-CAS_ATTRIBUTE_CALLBACK = ""
-
 # User-uploaded content
 MEDIA_ROOT = '/edx/var/edxapp/media/'
 MEDIA_URL = '/media/'
