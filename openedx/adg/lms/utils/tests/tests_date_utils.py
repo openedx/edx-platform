55873599f643c229dc3629dc0aa59e2f5fe7c88e
Asad Ali <asad.ali@arbisoft.com>
2021-01-07 21:06:56 +0500

    LP-2498 fixed tests for utils

--- openedx/adg/lms/utils/tests/tests_date_utils.py
+++ openedx/adg/lms/utils/tests/tests_date_utils.py
@@ -1,8 +1,6 @@
 """
 Unit test for date utilities
 """
-from datetime import datetime
-
 import pytest
 from mock import patch
 
@@ -26,25 +24,23 @@ def test_month_choices(default_title):
 
 
 @pytest.mark.parametrize('default_title', [None, 'Year'])
-@patch('openedx.adg.lms.utils.date_utils.datetime')
-def test_year_choices(mock_current_year, default_title):
+def test_year_choices(default_title):
     """
     Test year choice list between the range of 2017 to 2019.
     """
-    mock_current_year.today.return_value = datetime(2019, 1, 1)
     choices = [(2019, 2019), (2018, 2018), (2017, 2017)]
 
     if default_title:
         choices.insert(0, (None, default_title))
 
-    assert choices == year_choices(from_year=2017, default_title=default_title)
+    with patch('openedx.adg.lms.utils.date_utils.MAXIMUM_YEAR_OPTION', 2019):
+        assert choices == year_choices(from_year=2017, default_title=default_title)
 
 
-@patch('openedx.adg.lms.utils.date_utils.datetime')
-def test_year_choices_raise_value_error(mock_current_year):
+def test_year_choices_raise_value_error():
     """
     Test year choice list and assert that exception is raised if from year is greater than current year.
     """
-    mock_current_year.today.return_value = datetime(2019, 1, 1)
-    with pytest.raises(ValueError):
-        year_choices(2020)
+    with patch('openedx.adg.lms.utils.date_utils.MAXIMUM_YEAR_OPTION', 2019):
+        with pytest.raises(ValueError):
+            year_choices(2020)
