dd8ad4f767544f8372cc61385767f28f5ced0721
Gábor Boros <gabor.brs@gmail.com>
2021-01-27 22:57:22 +0100

    docs: add annotations to instructor plugin settings

--- lms/djangoapps/instructor/settings/common.py
+++ lms/djangoapps/instructor/settings/common.py
@@ -10,35 +10,87 @@ def plugin_settings(settings):
     settings.ANALYTICS_DASHBOARD_URL = ""
     settings.ANALYTICS_DASHBOARD_NAME = _('Your Platform Insights')
     settings.FEATURES.update({
-        # Enable display of enrollment counts in instructor dash, analytics section
+        # .. toggle_name: FEATURES['DISPLAY_ANALYTICS_ENROLLMENTS']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: True
+        # .. toggle_description: Enable display of enrollment counts in instructor dash, analytics section.
+        # .. toggle_use_cases: opt_out
+        # .. toggle_creation_date: 2014-11-12
+        # .. toggle_tickets: https://github.com/edx/edx-platform/pull/5838
         'DISPLAY_ANALYTICS_ENROLLMENTS': True,
 
-        # Display the 'Analytics' tab in the instructor dashboard for CCX courses.
-        # Note: This has no effect unless ANALYTICS_DASHBOARD_URL is already set,
-        #       because without that setting, the tab does not show up for any courses.
+        # .. toggle_name: FEATURES['ENABLE_CCX_ANALYTICS_DASHBOARD_URL']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: False
+        # .. toggle_description: Display the 'Analytics' tab in the instructor dashboard for CCX courses.
+        #   Note: This has no effect unless ANALYTICS_DASHBOARD_URL is already set, because without that
+        #   setting, the tab does not show up for any courses.
+        # .. toggle_use_cases: opt_in
+        # .. toggle_creation_date: 2016-10-07
+        # .. toggle_tickets: https://github.com/edx/edx-platform/pull/13196
         'ENABLE_CCX_ANALYTICS_DASHBOARD_URL': False,
 
-        # Disable instructor dash buttons for downloading course data
-        # when enrollment exceeds this number
+        # .. toggle_name: FEATURES['MAX_ENROLLMENT_INSTR_BUTTONS']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: 200
+        # .. toggle_description: Disable instructor dashboard buttons for downloading course data
+        #   when enrollment exceeds this number. The number indicates the maximum allowed enrollments
+        #   until a course considered "small". Courses exceeding the upper limit of "small" courses
+        #   will have disabled buttons at the instructor dashboard.
+        # .. toggle_use_cases: open_edx
+        # .. toggle_creation_date: 2020-07-06
+        # .. toggle_tickets: https://openedx.atlassian.net/browse/PROD-1740
         'MAX_ENROLLMENT_INSTR_BUTTONS': 200,
 
-        # Grade calculation started from the instructor dashboard will write grades
-        # CSV files to the configured storage backend and give links for downloads.
+        # .. toggle_name: FEATURES['ENABLE_GRADE_DOWNLOADS']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: False
+        # .. toggle_description: Grade calculation started from the instructor dashboard will write
+        #   grades CSV files to the configured storage backend and give links for downloads.
+        # .. toggle_use_cases: opt_in
+        # .. toggle_creation_date: 2016-07-06
+        # .. toggle_tickets: https://github.com/edx/edx-platform/pull/11286
         'ENABLE_GRADE_DOWNLOADS': False,
 
-        # Give course staff unrestricted access to grade downloads (if set to False,
-        # only edX superusers can perform the downloads)
+        # .. toggle_name: FEATURES['ALLOW_COURSE_STAFF_GRADE_DOWNLOADS']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: False
+        # .. toggle_description: Give course staff unrestricted access to grade downloads;
+        #   if set to False, only edX superusers can perform the downloads.
+        # .. toggle_use_cases: opt_in
+        # .. toggle_creation_date: 2018-03-26
+        # .. toggle_tickets: https://github.com/edx/edx-platform/pull/17385
         'ALLOW_COURSE_STAFF_GRADE_DOWNLOADS': False,
 
-        # Show a section in the membership tab of the instructor dashboard
-        # to allow an upload of a CSV file that contains a list of new accounts to create
-        # and register for course.
+        # .. toggle_name: FEATURES['ALLOW_AUTOMATED_SIGNUPS']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: False
+        # .. toggle_description: Show a section in the membership tab of the instructor dashboard
+        #   to allow an upload of a CSV file that contains a list of new accounts to create and
+        #   register for course.
+        # .. toggle_use_cases: opt_in
+        # .. toggle_creation_date: 2014-10-21
+        # .. toggle_tickets: https://github.com/edx/edx-platform/pull/5670
         'ALLOW_AUTOMATED_SIGNUPS': False,
 
-        # Batch-Generated Certificates from Instructor Dashboard
+        # .. toggle_name: FEATURES['CERTIFICATES_INSTRUCTOR_GENERATION']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: False
+        # .. toggle_description: Allow batch generation of certificates from the instructor dashboard.
+        #   In case of self-paced courses, the certificate generation button is hidden if certificate
+        #   generation is not explicitly enabled globally or for the specific corse.
+        # .. toggle_use_cases: opt_in
+        # .. toggle_creation_date: 2015-07-17
+        # .. toggle_tickets: https://github.com/edx/edx-platform/pull/8967
         'CERTIFICATES_INSTRUCTOR_GENERATION': False,
 
-        # Whether to check the "Notify users by email" checkbox in the batch enrollment form
-        # in the instructor dashboard.
+        # .. toggle_name: FEATURES['BATCH_ENROLLMENT_NOTIFY_USERS_DEFAULT']
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: False
+        # .. toggle_description: Controls if the "Notify users by email" checkbox in the batch
+        #   enrollment form on the instructor dashboard is already checked on page load or not.
+        # .. toggle_use_cases: opt_out
+        # .. toggle_creation_date: 2017-07-05
+        # .. toggle_tickets: https://github.com/edx/edx-platform/pull/15392
         'BATCH_ENROLLMENT_NOTIFY_USERS_DEFAULT': True,
     })
