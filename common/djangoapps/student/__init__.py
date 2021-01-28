a1664d2fd8cff5b255bcbd2da003058b2d28e8f6
Luis Moreno <luis.moreno@edunext.co>
2021-01-28 15:23:06 -0400

    feat: Move student.courseenrollment_admin from LegacyWaffleSwitch t a WaffleSwitch and add annotations

--- common/djangoapps/student/__init__.py
+++ common/djangoapps/student/__init__.py
@@ -1,9 +1,3 @@
 """
 Student app helpers and settings
 """
-
-
-from edx_toggles.toggles import LegacyWaffleSwitchNamespace
-
-# Namespace for student app waffle switches
-STUDENT_WAFFLE_NAMESPACE = LegacyWaffleSwitchNamespace(name='student')
