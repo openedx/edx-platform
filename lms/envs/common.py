40a2ab41e42c4a38d224c17794d8a90ca75f2eae
Gonzalo <gonzalo.romero@edunext.co>
2021-01-29 12:27:59 -0600

    add more fixes on WIKI toggles

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -434,17 +434,6 @@ FEATURES = {
     'ENABLE_FOOTER_MOBILE_APP_LINKS': False,
 
     # Let students save and manage their annotations
-    # .. toggle_name: FEATURES['ENABLE_EDXNOTES']
-    # .. toggle_implementation: SettingToggle
-    # .. toggle_default: False
-    # .. toggle_description: This toggle enables the students to save and manage their annotations in the
-    #   course using the notes service. The bulk of the actual work in storing the notes is done by
-    #   by a separate service(see the edx-notes-api repo).
-    # .. toggle_warnings: Requires the edx-notes-api service properly running and to have configured the django settings
-    #   EDXNOTES_INTERNAL_API and EDXNOTES_PUBLIC_API
-    # .. toggle_use_cases: open_edx
-    # .. toggle_creation_date: None
-    # .. toggle_tickets: None
     'ENABLE_EDXNOTES': False,
 
     # Toggle to enable coordination with the Publisher tool (keep in sync with cms/envs/common.py)
@@ -1075,14 +1064,12 @@ DJFS = {
 ENABLE_MULTICOURSE = False  # set to False to disable multicourse display (see lib.util.views.edXhome)
 
 # .. toggle_name: WIKI_ENABLED
-# .. toggle_implementation: Django setting
+# .. toggle_implementation: DjangoSetting
 # .. toggle_default: True
 # .. toggle_description: This setting allows us to have a collaborative tool to contribute or
 #   modify content of course related materials.
-# .. toggle_warnings: In some cases you may want to disable it in deployment envs.
 # .. toggle_use_cases: open_edx
-# .. toggle_creation_date: None
-# .. toggle_tickets: None
+# .. toggle_creation_date: 2012-07-13
 WIKI_ENABLED = True
 
 ###
@@ -1599,39 +1586,40 @@ SIMPLE_WIKI_REQUIRE_LOGIN_VIEW = False
 from lms.djangoapps.course_wiki import settings as course_wiki_settings  # pylint: disable=wrong-import-position
 
 # .. toggle_name: WIKI_ACCOUNT_HANDLING
-# .. toggle_implementation: Django setting
-# .. toggle_default: True
-# .. toggle_description: This setting allows the access to Sign up, login and logout views.
+# .. toggle_implementation: DjangoSetting
+# .. toggle_default: False
+# .. toggle_description: We recommend you leave this as 'False' for an Open edX installation
+#   to get the proper behavior for register, login and logout. For the original docs see:
+#   https://github.com/edx/django-wiki/blob/edx_release/wiki/conf/settings.py
 # .. toggle_warnings: Turn to False only if you have your own account handling
 # .. toggle_use_cases: open_edx
 # .. toggle_creation_date: None
-# .. toggle_tickets: None
 WIKI_ACCOUNT_HANDLING = False
 WIKI_EDITOR = 'lms.djangoapps.course_wiki.editors.CodeMirror'
 WIKI_SHOW_MAX_CHILDREN = 0  # We don't use the little menu that shows children of an article in the breadcrumb
 # .. toggle_name: WIKI_ANONYMOUS
-# .. toggle_implementation: Django setting
-# .. toggle_default: True
-# .. toggle_description: This setting treats none logged in users as the ¨other¨ user group.
-# .. toggle_warnings: In False it doesn´t allow to access anonymous users.
+# .. toggle_implementation: DjangoSetting
+# .. toggle_default: False
+# .. toggle_description: Enabling this allows access to anonymous users.
+#   For the original docs, see:
+#   https://github.com/edx/django-wiki/blob/edx_release/wiki/conf/settings.py
+# .. toggle_warnings: Setting allow anonymous access to `True` may have styling issues.
 # .. toggle_use_cases: open_edx
 # .. toggle_creation_date: None
-# .. toggle_tickets: None
-WIKI_ANONYMOUS = False  # Don't allow anonymous access until the styling is figured out
+WIKI_ANONYMOUS = False
 
 WIKI_CAN_DELETE = course_wiki_settings.CAN_DELETE
 WIKI_CAN_MODERATE = course_wiki_settings.CAN_MODERATE
 WIKI_CAN_CHANGE_PERMISSIONS = course_wiki_settings.CAN_CHANGE_PERMISSIONS
 WIKI_CAN_ASSIGN = course_wiki_settings.CAN_ASSIGN
 # .. toggle_name: WIKI_USE_BOOTSTRAP_SELECT_WIDGET
-# .. toggle_implementation: Django setting
-# .. toggle_default: True
-# .. toggle_description: This setting allows you to use bootstrap and you can switch off
-#   if you are not using it.
-# .. toggle_warnings: None
+# .. toggle_implementation: DjangoSetting
+# .. toggle_default: False
+# .. toggle_description: Enabling this will use the bootstrap select widget.
+#   For the original docs, see:
+#   https://github.com/edx/django-wiki/blob/edx_release/wiki/conf/settings.py
 # .. toggle_use_cases: open_edx
 # .. toggle_creation_date: None
-# .. toggle_tickets: None
 WIKI_USE_BOOTSTRAP_SELECT_WIDGET = False
 WIKI_LINK_LIVE_LOOKUPS = False
 WIKI_LINK_DEFAULT_LEVEL = 2
