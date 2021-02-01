77f0e6fabf52844249b2049ebc87fe74324f3914
Christie Rice <8483753+crice100@users.noreply.github.com>
2021-02-01 16:38:46 -0500

    MICROBA-918 Check for the allowlist on invalidation and failing grades

--- lms/djangoapps/certificates/services.py
+++ lms/djangoapps/certificates/services.py
@@ -8,6 +8,7 @@ import logging
 from django.core.exceptions import ObjectDoesNotExist
 from opaque_keys.edx.keys import CourseKey
 
+from lms.djangoapps.certificates.generation_handler import is_using_certificate_allowlist_and_is_on_allowlist
 from lms.djangoapps.certificates.models import GeneratedCertificate
 from lms.djangoapps.utils import _get_key
 
@@ -21,9 +22,15 @@ class CertificateService(object):
 
     def invalidate_certificate(self, user_id, course_key_or_id):
         """
-        Invalidate the user certificate in a given course if it exists.
+        Invalidate the user certificate in a given course if it exists and the user is not on the allowlist for this
+        course run.
         """
         course_key = _get_key(course_key_or_id, CourseKey)
+        if is_using_certificate_allowlist_and_is_on_allowlist(user_id, course_key):
+            log.info('{course_key} is using allowlist certificates, and the user {user_id} is on its allowlist. The '
+                     'certificate will not be invalidated.'.format(course_key=course_key, user_id=user_id))
+            return False
+
         try:
             generated_certificate = GeneratedCertificate.objects.get(
                 user=user_id,
@@ -41,3 +48,6 @@ class CertificateService(object):
                 user_id,
                 course_key
             )
+            return False
+
+        return True
