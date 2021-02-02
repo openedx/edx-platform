d1c21db40fcdf8123401ec77d933844c033ce50f
Jawayria <jawayriahashmi@gmail.com>
2021-02-02 21:20:41 +0500

    Resolved error

--- pavelib/utils/test/bokchoy_utils.py
+++ pavelib/utils/test/bokchoy_utils.py
@@ -146,7 +146,7 @@ def is_mysql_running():
     """
     # We need to check whether or not mysql is running as a process
     # even if it is not daemonized.
-    with open(os.devnull, 'w') as os_devnull:  # lint-amnesty, pylint: disable=W6005, bad-option-value
+    with open(os.devnull, 'w') as os_devnull:  # lint-amnesty, pylint: disable=bad-option-value
         #pgrep returns the PID, which we send to /dev/null
         returncode = subprocess.call("pgrep mysqld", stdout=os_devnull, shell=True)
     return returncode == 0
