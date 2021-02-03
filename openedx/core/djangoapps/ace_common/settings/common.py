0b5cadd1764fb01a06eaab3b09865409918dbc81
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-02-03 13:43:47 +0500

    BOM-2315 Apply pylint-amnesty.

--- openedx/core/djangoapps/ace_common/settings/common.py
+++ openedx/core/djangoapps/ace_common/settings/common.py
@@ -1,3 +1,8 @@
+"""
+Settings for ace_common app.
+"""
+
+
 def plugin_settings(settings):  # lint-amnesty, pylint: disable=missing-function-docstring, missing-module-docstring
     settings.ACE_ENABLED_CHANNELS = [
         'django_email'
