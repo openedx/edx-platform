fa98f0837e4ad007897083a79e4ae50d6c2f9ae2
Zaman Afzal <zamanafzal@gmail.com>
2021-02-04 13:21:39 +0500

    ENT-4034 Third party auth waffle switch annotation (#26304)

--- common/djangoapps/third_party_auth/config/waffle.py
+++ common/djangoapps/third_party_auth/config/waffle.py
@@ -3,23 +3,20 @@ Waffle flags and switches for third party auth .
 """
 
 
-from edx_toggles.toggles import LegacyWaffleSwitch, LegacyWaffleSwitchNamespace
+from edx_toggles.toggles import WaffleSwitch
 
-_WAFFLE_NAMESPACE = u'third_party_auth'
-_WAFFLE_SWITCH_NAMESPACE = LegacyWaffleSwitchNamespace(name=_WAFFLE_NAMESPACE, log_prefix=u'ThirdPartyAuth: ')
+WAFFLE_NAMESPACE = 'third_party_auth'
 
 # .. toggle_name: third_party_auth.enable_multiple_sso_accounts_association_to_saml_user
 # .. toggle_implementation: WaffleSwitch
 # .. toggle_default: False
 # .. toggle_description: If enabled than learner should not be prompted for their edX password arriving via SAML
-# and already linked to the enterprise customer linked to the same IdP."
+#   and already linked to the enterprise customer linked to the same IdP."
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2021-01-29
 # .. toggle_target_removal_date: 2021-04-31
-# .. toggle_warnings: None.
 # .. toggle_tickets: ENT-4034
-ENABLE_MULTIPLE_SSO_ACCOUNTS_ASSOCIATION_TO_SAML_USER = LegacyWaffleSwitch(
-    _WAFFLE_SWITCH_NAMESPACE,
-    'enable_multiple_sso_accounts_association_to_saml_user',
+ENABLE_MULTIPLE_SSO_ACCOUNTS_ASSOCIATION_TO_SAML_USER = WaffleSwitch(
+    '{NAMESPACE}.enable_multiple_sso_accounts_association_to_saml_user'.format(NAMESPACE=WAFFLE_NAMESPACE),
     __name__
 )
