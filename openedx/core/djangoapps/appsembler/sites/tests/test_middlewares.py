e603c70e860448295e4d6f2a8312ee26c1adf5cb
Bryan Wilson <bryanlandia@gmail.com>
2021-02-04 00:40:35 -0800

    Tahoe redirect middleware tests updates

--- openedx/core/djangoapps/appsembler/sites/tests/test_middlewares.py
+++ openedx/core/djangoapps/appsembler/sites/tests/test_middlewares.py
@@ -3,7 +3,7 @@ Tests for the sites.middlewares module.
 """
 
 from mock import patch, Mock
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.test.client import RequestFactory
 
 from openedx.core.djangoapps.appsembler.sites.middleware import LmsCurrentOrganizationMiddleware, RedirectMiddleware
@@ -30,36 +30,37 @@ class LmsCurrentOrganizationMiddlewareTests(TestCase):
         assert request.session['organization'] is None
 
 
-@patch('settings.TAHOE_MAIN_SITE_REDIRECT_URL', 'https://foo.bar')
+@override_settings(TAHOE_MAIN_SITE_REDIRECT_URL='https://foo.bar')
+@override_settings(MAIN_SITE_REDIRECT_WHITELIST=['/baz'])
 class RedirectMiddlewareTests(TestCase):
-    def setup(self):
+    def setUp(self):
+        super(RedirectMiddlewareTests, self).setUp()
         self.default_site = SiteFactory.create()
         self.other_site = SiteFactory.create()
-        patcher = patch("settings.SITE_ID", self.default_site.id)
+        patcher = patch("django.conf.settings.SITE_ID", self.default_site.id)
         patcher.start()
         self.addCleanup(patcher.stop)
 
     def test_redirects_default_site(self):
-        request = RequestFactory().get(self.default_site.domain)
+        request = RequestFactory().get('/path', HTTP_HOST=self.default_site.domain)
         request.site = self.default_site
         processed = RedirectMiddleware().process_request(request)
-        self.assertRedirects(processed, 'https://foo.bar')
+        self.assertEquals(processed.url, 'https://foo.bar')  # middleware drops the path
 
     def test_with_no_site_found(self):
-        request = RequestFactory().get("/")
+        request = RequestFactory().get('/')
         request.site = None
         processed = RedirectMiddleware().process_request(request)
-        self.assertEquals(processed, None)
+        self.assertIsNone(processed)
 
     def test_no_redirect_other_site(self):
-        request = RequestFactory().get(self.other_site.domain)
+        request = RequestFactory().get('/', HTTP_HOST=self.other_site.domain)
         request.site = self.other_site
         processed = RedirectMiddleware().process_request(request)
-        self.assertEquals(processed, None)
+        self.assertIsNone(processed)
 
-    @patch('settings.MAIN_SITE_REDIRECT_WHITELIST', ['/baz'])
     def test_url_in_whitelist(self):
-        request = RequestFactory().get('{}/baz'.format(self.default_site.domain))
+        request = RequestFactory().get('/baz', HTTP_HOST=self.default_site.domain)
         request.site = self.default_site
         processed = RedirectMiddleware().process_request(request)
-        self.assertEquals(processed, None)
+        self.assertIsNone(processed)
