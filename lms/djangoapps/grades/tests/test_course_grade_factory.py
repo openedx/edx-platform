8845685fd844a4851b14a854bdcf456a39a7c74a
Christie Rice <8483753+crice100@users.noreply.github.com>
2021-02-04 13:42:33 -0500

    MICROBA-918 Check for the allowlist on invalidation and failing grades (#26356)

--- lms/djangoapps/grades/tests/test_course_grade_factory.py
+++ lms/djangoapps/grades/tests/test_course_grade_factory.py
@@ -121,7 +121,7 @@ class TestCourseGradeFactory(GradeTestBase):
         with self.assertNumQueries(3):
             _assert_read(expected_pass=True, expected_percent=1.0)  # updated to grade of 1.0
 
-        num_queries = 28
+        num_queries = 30
         with self.assertNumQueries(num_queries), mock_get_score(0, 0):  # the subsection now is worth zero
             grade_factory.update(self.request.user, self.course, force_update_subsections=True)
 
