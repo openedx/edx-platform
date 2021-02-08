2b25367789ec3054a2b876e471cc6f6a7d194bd1
DawoudSheraz <dawoud.sheraz@gmail.com>
2021-02-08 17:47:23 +0500

    add migration to create support tools admin role

--- openedx/core/djangoapps/system_wide_roles/constants.py
+++ openedx/core/djangoapps/system_wide_roles/constants.py
@@ -3,3 +3,6 @@ System wide roles application constants.
 """
 
 STUDENT_SUPPORT_ADMIN_ROLE = 'student_support_admin'
+
+# Role needed to access support tools MFE
+SUPPORT_TOOLS_ADMIN_ROLE = "support_tools_admin"
