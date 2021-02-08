7a1fe73a56ba91216e40372e3b7a0f12b6543b5c
Tim McCormack <tmccormack@edx.org>
2021-02-08 15:00:49 +0000

    fix: Add new proctoring key to Studio as well (copy from LMS)
    Turns out it's needed for the Proctoring Settings view.
    
    Also reorder slightly for better clustering.
    
    MST-644

--- cms/envs/devstack.py
+++ cms/envs/devstack.py
@@ -241,5 +241,10 @@ CORS_ALLOW_HEADERS = corsheaders_default_headers + (
 FEATURES['ENABLE_SPECIAL_EXAMS'] = True
 FEATURES['ENABLE_PREREQUISITE_COURSES'] = True
 
+# Used in edx-proctoring for ID generation in lieu of SECRET_KEY - dummy value
+# (ref MST-637)
+PROCTORING_USER_OBFUSCATION_KEY = '85920908f28904ed733fe576320db18cabd7b6cd'
+
+
 # Don't tolerate deprecated edx-platform import usage in devstack.
 ERROR_ON_DEPRECATED_EDX_PLATFORM_IMPORTS = True
