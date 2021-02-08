2bec92939494ab1d96bcfe2702b6713ed53d0ceb
Kyle McCormick <kmccormick@edx.org>
2021-02-08 13:21:51 -0500

    work on actions

--- openedx/core/djangoapps/nexblocks/models.py
+++ openedx/core/djangoapps/nexblocks/models.py
@@ -2,6 +2,8 @@
 Models to support persistence of NexBlock data.
 """
 
+from enum import Enum
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -20,7 +22,7 @@ class NexBlockInstance(models.Model):
     display_name = models.CharField(max_length=255)
 
 
-class NexBlockInstanceDatum(models.Model):
+class NexBlockInstanceData(models.Model):
     """
     An piece of data associated with a NeXBlock instance, keyed by a string.
 
@@ -38,7 +40,7 @@ class NexBlockInstanceDatum(models.Model):
         unique_together = ("instance", "data_key")
 
 
-class NexBlockInstanceLearnerDatum(models.Model):
+class NexBlockLearnerData(models.Model):
     """
     An piece of data associated with a NeXBlock instance and learner, keyed by a string.
 
@@ -59,3 +61,43 @@ class NexBlockInstanceLearnerDatum(models.Model):
 
     class Meta:
         unique_together = ("instance", "learner", "data_key")
+
+
+# Changing this will trigger a migration.
+ACTION_NAME_MAX_LENTH = 32
+
+
+class NexBlockAction(Enum):
+    """
+    TODO
+    """
+
+    SET = "set"
+    INCREMENT = "increment"
+    SCORE_EARNED = "score_earned"
+
+    @classmethod
+    def model_choices(cls):
+        return tuple((action.name, action.name) for action in cls)
+
+
+for action in NexBlockAction:
+    assert (
+        len(action.name) <= ACTION_NAME_MAX_LENTH
+    ), f"NexBlockAction name {action.name} is too long; max length is {ACTION_NAME_MAX_LENTH}"
+
+
+class NexBlockLearnerEvent(models.Model):
+    """
+    TODO
+    """
+
+    instance = models.ForeignKey(
+        to=NexBlockInstance, related_name="learner_events", on_delete=models.CASCADE
+    )
+    learner = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+    action_type = models.CharField(
+        choices=NexBlockAction.model_choices, max_length=ACTION_NAME_MAX_LENTH
+    )
+    action_target = models.CharField(max_length=255)
+    action_args = models.TextField()
