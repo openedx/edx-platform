7b950e13582c059bca5bc6d30a3886122c8e43fa
Nizar Mahmoud <nizarmah@hotmail.com>
2021-02-09 11:59:32 +0300

    Updates tpa email association waffle switch to support edx-toggles=1.1.1
    This change updates the `third_party_auth.always_associate_user_by_email` waffle switch's definition because Koa uses `edx-toggles=1.1.1`, instead of `2.0.0>edx-toggles>=1.2.0`.

--- common/djangoapps/third_party_auth/config/waffle.py
+++ common/djangoapps/third_party_auth/config/waffle.py
@@ -5,7 +5,24 @@ Waffle flags and switches for third party auth .
 
 from edx_toggles.toggles import WaffleSwitch
 
+
 WAFFLE_NAMESPACE = 'third_party_auth'
+
+# .. toggle_name: ALWAYS_ASSOCIATE_USER_BY_EMAIL
+# .. toggle_implementation: WaffleSwitch
+# .. toggle_default: False
+# .. toggle_description: Always associates current social auth user with
+#   the user with the same email address in the database, which verifies
+#   that only a single database user is associated with the email.
+# .. toggle_use_cases: opt_in
+# .. toggle_creation_date: 2020-12-23
+# .. toggle_tickets: https://openedx.atlassian.net/browse/OSPR-5312
+ALWAYS_ASSOCIATE_USER_BY_EMAIL = WaffleSwitch(
+    WAFFLE_NAMESPACE,
+    'always_associate_user_by_email',
+    module_name=__name__,
+)
+
 
 # .. toggle_name: third_party_auth.enable_multiple_sso_accounts_association_to_saml_user
 # .. toggle_implementation: WaffleSwitch
