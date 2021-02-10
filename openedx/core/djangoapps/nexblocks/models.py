e0377fa7b89d808a7dbb5fd607e204bd35e3980b
Kyle McCormick <kmccormick@edx.org>
2021-02-09 22:35:13 -0500

    Radically simplify data model + linting fixes

--- openedx/core/djangoapps/nexblocks/models.py
+++ openedx/core/djangoapps/nexblocks/models.py
@@ -1,62 +1,30 @@
 """
 Models to support persistence of NexBlock data.
 """
-from django.contrib.auth import get_user_model
 from django.db import models
 from opaque_keys.edx.django.models import LearningContextKeyField
-
-User = get_user_model()
-
-
-class NexBlockInstance(models.Model):
-    """
-    An instance of a NeXBlock. Uniquely identified by UUID.
-
-    This model is downstream of the NexWrapperBlock data stored with the course.
-    Changes are pushed here upon course publish.
-    """
-
-    uuid = models.UUIDField(editable=False, unique=True)
-    learning_context_key = LearningContextKeyField(max_length=255)
-    display_name = models.CharField(max_length=255)
-
-
-class NexBlockInstanceData(models.Model):
-    """
-    An piece of data associated with a NeXBlock instance, keyed by a string.
-
-    This model is downstream of the NexWrapperBlock data stored with the course.
-    Changes are pushed here upon course publish.
-    """
-
-    instance = models.ForeignKey(
-        to=NexBlockInstance, related_name="instance_data", on_delete=models.CASCADE
-    )
-    data_key = models.CharField(max_length=255)
-    data_value = models.TextField()
-
-    class Meta:
-        unique_together = ("instance", "data_key")
 
 
 class NexBlockLearnerData(models.Model):
     """
-    An piece of data associated with a NeXBlock instance and learner, keyed by a string.
+    Unstructured JSON data associated with NexBlock type in a learning context.
 
     This model is the authoritative data store for learner data.
     It is written to by the Learner Event API and read from the Learner Data API.
     """
 
-    instance = models.ForeignKey(
-        to=NexBlockInstance, related_name="learner_data", on_delete=models.CASCADE
-    )
-    # NULL learner indicates that the account was deleted or otherwise disassociated
-    # with thie piece of learner data. We keep it in the table for now.
-    # In a production release, we'd want to think about whether or not this data
-    # would need to be permanently deleted since it might contain PII.
-    learner = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
-    data_key = models.CharField(max_length=255)
-    data_value = models.TextField()
+    # The learning context with which this data is associated.
+    # ("learning context" is a more generic way of saying "course run")
+    # Example: course-v1:edX+DemoX+1T2020
+    learning_context_key = LearningContextKeyField(max_length=255)
+
+    # The NexBlock type.
+    # Example: @edx/nexblock-test-announcement
+    block_type = models.CharField(max_length=255)
+
+    # A JSON blob of all learner data associated with this
+    # (learning_context, block_type) pair.
+    value = models.TextField()
 
     class Meta:
-        unique_together = ("instance", "learner", "data_key")
+        unique_together = ("learning_context_key", "block_type")
