d0d259fd64ff087495f24fc1b28df22b31cde66d
usamasadiq <usama.sadiq@arbisoft.com>
2021-02-10 16:17:41 +0500

    Apply lint-amnesty to pavement.py

--- pavement.py
+++ pavement.py
@@ -1,3 +1,4 @@
+# lint-amnesty, pylint: disable=django-not-configured, missing-module-docstring
 import sys
 import os
 
@@ -11,4 +12,4 @@ import os
 # take precedence, leading to some very strange results.
 sys.path.insert(0, os.path.dirname(__file__))
 
-from pavelib import *
+from pavelib import *  # lint-amnesty, pylint: disable=wildcard-import, wrong-import-position
