f5cffb52c5598e634032ab415774744b7bddec46
Asad Ali <asad.ali@arbisoft.com>
2021-02-11 17:31:34 +0500

    LP-2298 updated string formatting

--- lms/djangoapps/onboarding/email_utils.py
+++ lms/djangoapps/onboarding/email_utils.py
@@ -89,8 +89,9 @@ def send_admin_update_confirmation_email(org_name, current_admin, new_admin, con
         MandrillClient().send_mail(MandrillClient.ORG_ADMIN_GET_IN_TOUCH, current_admin.email, {
             "first_name": current_admin.first_name,
             "org_name": org_name,
-            "claimed_by_name": "{first_name} {last_name}".format(first_name=new_admin.first_name,
-                                                                 last_name=new_admin.last_name),
+            "claimed_by_name": "{first_name} {last_name}".format(
+                first_name=new_admin.first_name, last_name=new_admin.last_name
+            ),
             "claimed_by_email": new_admin.email,
         })
         MandrillClient().send_mail(MandrillClient.NEW_ADMIN_GET_IN_TOUCH, new_admin.email, {
