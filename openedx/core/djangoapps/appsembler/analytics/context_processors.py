93b13cb3f742a3fc0e2ce2f7ff7a8e67c4e27dcc
John Baldwin <jlbaldwin@gmail.com>
2021-02-15 19:19:19 +0100

    Appsembler Apps - Analytics - Juniper Upgrade
    - Enforced boolean typ on the 'SHOW_X' (where X is the APP label)
    boolean flags
    - Added test coverage
    - Reorganized tests under 'tests' folder in 'appsembler.analytics'

--- openedx/core/djangoapps/appsembler/analytics/context_processors.py
+++ openedx/core/djangoapps/appsembler/analytics/context_processors.py
@@ -4,13 +4,18 @@ from django.conf import settings
 from student.auth import user_has_role
 from student.roles import CourseCreatorRole
 
-from openedx.core.djangoapps.appsembler.analytics.helpers import should_show_hubspot
+from openedx.core.djangoapps.appsembler.analytics.helpers import (
+    should_show_hubspot
+)
 
 
 def google_analytics(request):
+    app_id = getattr(settings, 'GOOGLE_ANALYTICS_APP_ID', None)
+    show_app = True if app_id else False
+
     data = {
-        'GOOGLE_ANALYTICS_APP_ID': getattr(settings, 'GOOGLE_ANALYTICS_APP_ID', None),
-        'SHOW_GOOGLE_ANALYTICS': getattr(settings, 'GOOGLE_ANALYTICS_APP_ID', None),
+        'GOOGLE_ANALYTICS_APP_ID': app_id,
+        'SHOW_GOOGLE_ANALYTICS': show_app,
     }
 
     user = request.user
@@ -23,17 +28,23 @@ def google_analytics(request):
 
 
 def mixpanel(request):
+    app_id = getattr(settings, 'MIXPANEL_APP_ID', None)
+    show_app = True if app_id else False
     data = {
-        'MIXPANEL_APP_ID': getattr(settings, 'MIXPANEL_APP_ID', None),
-        'SHOW_MIXPANEL': getattr(settings, 'MIXPANEL_APP_ID', None),
+        'MIXPANEL_APP_ID': app_id,
+        'SHOW_MIXPANEL': show_app,
     }
     return data
 
 
 def hubspot(request):
     hubspot_portal_id = getattr(settings, 'HUBSPOT_PORTAL_ID', None)
+    if hubspot_portal_id and should_show_hubspot(request.user):
+        show_app = True
+    else:
+        show_app = False
     data = {
         'HUBSPOT_PORTAL_ID': hubspot_portal_id,
-        'SHOW_HUBSPOT': hubspot_portal_id and should_show_hubspot(request.user),
+        'SHOW_HUBSPOT': show_app,
     }
     return data
