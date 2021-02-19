7d5f9b2016c5739f10bc8815b414da73892d04ee
Aarif <MrAarif@outlook.com>
2021-02-19 16:03:53 +0500

    replaced unittest assertions pytest assertions (#26553)

--- lms/tests.py
+++ lms/tests.py
@@ -19,11 +19,11 @@ class LmsModuleTests(TestCase):
         extensions = ['eot', 'otf', 'ttf', 'woff']
         for extension in extensions:
             mimetype, _ = mimetypes.guess_type('test.' + extension)
-            self.assertIsNotNone(mimetype)
+            assert mimetype is not None
 
     def test_api_docs(self):
         """
         Tests that requests to the `/api-docs/` endpoint do not raise an exception.
         """
         response = self.client.get('/api-docs/')
-        self.assertEqual(200, response.status_code)
+        assert response.status_code == 200
