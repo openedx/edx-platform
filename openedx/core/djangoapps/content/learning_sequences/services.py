1af3b5a68c5ed4915ad20784b174102b699c2a71
Zachary Hancock <zhancock@edx.org>
2021-02-24 17:25:49 -0500

    runtime service exposing learning_sequences api to proctoring (#26692)

---
+++ openedx/core/djangoapps/content/learning_sequences/services.py
@@ -0,0 +1,18 @@
+"""
+Learning Sequences Runtime Service
+"""
+
+
+from .api import get_user_course_outline_details
+
+
+class LearningSequencesRuntimeService:
+    """
+    Provides functions of the public API as a class injected into edx-proctoring
+    """
+
+    def get_user_course_outline_details(self, course_key, user, at_time):
+        """
+        Returns UserCourseOutlineDetailsData
+        """
+        return get_user_course_outline_details(course_key, user, at_time)
