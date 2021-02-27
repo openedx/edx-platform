d75fd04c70dc5fb3b584b507cffc6343a92b9006
Sofiane Bebert <bebertsofiane@gmail.com>
2021-02-27 21:30:42 +0000

    docs: annotate auto_certificate_generation

--- lms/djangoapps/course_home_api/toggles.py
+++ lms/djangoapps/course_home_api/toggles.py
@@ -9,6 +9,16 @@ from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag
 
 WAFFLE_FLAG_NAMESPACE = LegacyWaffleFlagNamespace(name='course_home')
 
+# .. toggle_name: course_home.course_home_mfe
+# .. toggle_implementation: ExperimentWaffleFlag
+# .. toggle_default: False
+# .. toggle_description: This experiment flag enables the use of the learning microfrontend (frontend-app-learning)
+#   as the default course frontend.
+# .. toggle_warnings: For this flag to have an effect, the LEARNING_MICROFRONTEND_URL setting must be configured, too.
+# .. toggle_use_cases: temporary
+# .. toggle_creation_date: 2020-05-29
+# .. toggle_target_removal_date: 2021-10-09
+# .. toggle_tickets: https://openedx.atlassian.net/browse/AA-117
 COURSE_HOME_MICROFRONTEND = ExperimentWaffleFlag(WAFFLE_FLAG_NAMESPACE, 'course_home_mfe', __name__)
 
 COURSE_HOME_MICROFRONTEND_DATES_TAB = CourseWaffleFlag(WAFFLE_FLAG_NAMESPACE, 'course_home_mfe_dates_tab', __name__)
