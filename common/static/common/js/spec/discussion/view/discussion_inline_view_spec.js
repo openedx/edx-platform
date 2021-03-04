b62478c3655ea6e65167cb15567d178168c71940
Paul Aswa <paul@opencraft.com>
2021-03-04 23:46:46 +0300

    fix: hide new post indicator on thread open
    This fixes the removal of the new post indicator in the Discussion XBlock once a thread has been opened.

--- common/static/common/js/spec/discussion/view/discussion_inline_view_spec.js
+++ common/static/common/js/spec/discussion/view/discussion_inline_view_spec.js
@@ -35,6 +35,7 @@
         });
 
         createTestView = function(test) {
+            var testView;
             var courseSettings = DiscussionSpecHelper.createTestCourseSettings({
                 groups: [
                     {
@@ -62,7 +63,7 @@
                     children: []
                 }
             });
-            var testView = new DiscussionInlineView({
+            testView = new DiscussionInlineView({
                 el: $('.discussion-module')
             });
             testView.render();
@@ -233,6 +234,22 @@
 
                 // Verify that the individual thread is no longer shown
                 expect(testView.$('.group-visibility-label').length).toBe(0);
+            });
+
+            it('marks a thread as read once it is opened', function() {
+                var testView = createTestView(this);
+                var thread;
+                showDiscussion(this, testView);
+                thread = testView.$('.forum-nav-thread');
+
+                // The thread is marked as unread.
+                expect(thread).toHaveClass('never-read');
+
+                // Navigate to the thread.
+                thread.find('.forum-nav-thread-link').click();
+
+                // The thread is no longer marked as unread.
+                expect(thread).not.toHaveClass('never-read');
             });
         });
     });
