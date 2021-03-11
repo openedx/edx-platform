ff854280c5aa1ee993f194490a87f6f57b198f89
MunamMubashir-arbisoft <munam.mubashir@arbisoft.com>
2021-03-11 22:06:24 +0500

    Add test for get_extra_course_about_context function
    Signed-off-by: MunamMubashir-arbisoft <munam.mubashir@arbisoft.com>

---
+++ openedx/adg/lms/courseware_override/tests/constants.py
@@ -0,0 +1,5 @@
+"""
+Constants for courseware_override tests
+"""
+DUMMY_IMAGE_URLS = {'small': 'small_dummy_url', 'medium': 'medium_dummy_url', 'large': 'large_dummy_url'}
+EFFORT = '10 hours'
--- openedx/adg/lms/courseware_override/tests/test_helpers.py
+++ openedx/adg/lms/courseware_override/tests/test_helpers.py
@@ -5,11 +5,16 @@ import mock
 import pytest
 
 from common.djangoapps.student.roles import CourseInstructorRole
-from common.djangoapps.student.tests.factories import UserFactory
-from openedx.adg.lms.courseware_override.helpers import get_course_instructors, get_language_names_from_codes
+from common.djangoapps.student.tests.factories import CourseEnrollmentFactory, UserFactory
+from openedx.adg.lms.applications.tests.factories import MultilingualCourseFactory
+from openedx.adg.lms.courseware_override.helpers import (
+    get_course_instructors,
+    get_extra_course_about_context,
+    get_language_names_from_codes
+)
 from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
 
-DUMMY_IMAGE_URLS = {'small': 'small_dummy_url', 'medium': 'medium_dummy_url', 'large': 'large_dummy_url'}
+from .constants import DUMMY_IMAGE_URLS, EFFORT
 
 
 @pytest.mark.django_db
@@ -62,3 +67,35 @@ def test_get_language_name_from_codes(language_codes, language_names):
         expected_output.append((course.id, language_name))
 
     assert get_language_names_from_codes(language_codes_with_course_ids) == expected_output
+
+
+@pytest.mark.django_db
+@mock.patch('openedx.adg.lms.courseware_override.helpers.is_testing_environment')
+@mock.patch('openedx.adg.lms.courseware_override.helpers.get_course_instructors')
+@mock.patch('openedx.adg.lms.courseware_override.helpers.get_language_names_from_codes')
+def test_get_extra_course_about_context(mock_get_lang_names, mock_get_instructors, mock_testing_env):
+    """
+    Test if the context is being returned correctly by the get_extra_course_about_context function
+    """
+    user = UserFactory()
+    course = CourseOverviewFactory(self_paced=True, effort=EFFORT, language='en')
+    CourseInstructorRole(course.id).add_users(user)
+    CourseEnrollmentFactory(user=user, course=course)
+    MultilingualCourseFactory(course=course)
+
+    course_languages_with_ids = [(course.id, 'English')]
+    instructor_profiles_with_image_urls = [{"profile": user.profile, "profile_image_urls": DUMMY_IMAGE_URLS}]
+
+    mock_get_lang_names.return_value = course_languages_with_ids
+    mock_get_instructors.return_value = instructor_profiles_with_image_urls
+    mock_testing_env.return_value = False
+
+    expected_context = {
+        'course_languages': course_languages_with_ids,
+        'instructors': instructor_profiles_with_image_urls,
+        'total_enrollments': 1,
+        'self_paced': True,
+        'effort': EFFORT,
+    }
+
+    assert get_extra_course_about_context(None, course) == expected_context
