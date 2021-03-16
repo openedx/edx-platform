52a92d2b40a040126c70bfddc0b498634027bd0a
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-03-16 14:21:46 +0500

    Pyupgrade in announcements.

--- openedx/features/announcements/apps.py
+++ openedx/features/announcements/apps.py
@@ -4,7 +4,7 @@ Announcements Application Configuration
 
 
 from django.apps import AppConfig
-from edx_django_utils.plugins import PluginSettings, PluginURLs
+from edx_django_utils.plugins import PluginURLs, PluginSettings
 
 from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
 
