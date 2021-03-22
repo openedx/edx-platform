2ec17309d0339faaee18b5910fc313aba675f1f7
Awais Jibran <awaisdar001@gmail.com>
2021-03-22 16:07:06 +0500

    Code Refactor (#27101)

--- common/djangoapps/util/monitoring.py
+++ common/djangoapps/util/monitoring.py
@@ -11,16 +11,17 @@ def monitor_import_failure(course_key, import_step, message=None, exception=None
         message (str): any particular message to add
         exception: Exception object
     """
-    exception_module = getattr(exception, '__module__', '')
-    separator = '.' if exception_module else ''
-    module_and_class = f'{exception_module}{separator}{exception.__class__.__name__}'
-    exc_message = str(exception)
-
     set_custom_attribute('course_import_failure', import_step)
     set_custom_attributes_for_course_key(course_key)
+
     if message:
         set_custom_attribute('course_import_failure_message', message)
 
     if exception is not None:
+        exception_module = getattr(exception, '__module__', '')
+        separator = '.' if exception_module else ''
+        module_and_class = f'{exception_module}{separator}{exception.__class__.__name__}'
+        exc_message = str(exception)
+
         set_custom_attribute('course_import_failure_error_class', module_and_class)
         set_custom_attribute('course_import_failure_error_message', exc_message)
