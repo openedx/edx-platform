openedx/edx-platform

commit dd96a2aa72d9331d4dbc153da3d62b3c29312fe1

Convert Course, Section and Sequence XModules to XBlocks. (#25965)

cms/djangoapps/contentstore/utils.py
@@ -575,7 +575,7 @@ def get_sibling_urls(subsection):
             # won't display a link to a previous unit.
         else:
             try:
-                prev_section = sections[sections.index(section) - 1]
+                prev_section = sections[sections.index(next(s for s in sections if s.location == section.location)) - 1]
                 prev_loc = prev_section.get_children()[-1].get_children()[-1].location
             except IndexError:
                 pass
@@ -589,7 +589,7 @@ def get_sibling_urls(subsection):
             ))
         else:
             try:
-                next_section = sections[sections.index(section) + 1]
+                next_section = sections[sections.index(next(s for s in sections if s.location == section.location)) - 1]
                 next_loc = next_section.get_children()[0].get_children()[0].location
             except IndexError:
                 pass

lms/djangoapps/ccx/utils.py
@@ -38,7 +38,7 @@ def get_ccx_creation_dict(course):
     Return dict of rendering create ccx form.
 
     Arguments:
-        course (CourseDescriptorWithMixins): An edx course
+        course (CourseBlockWithMixins): An edx course
 
     Returns:
         dict: A attribute dict for view rendering
@@ -175,7 +175,7 @@ def get_ccx_by_ccx_id(course, coach, ccx_id):
     Finds a CCX of given coach on given master course.
 
     Arguments:
-        course (CourseDescriptor): Master course
+        course (CourseBlock): Master course
         coach (User): Coach to ccx
         ccx_id (long): Id of ccx
 
@@ -327,7 +327,7 @@ def add_master_course_staff_to_ccx(master_course, ccx_key, display_name, send_em
     Add staff and instructor roles on ccx to all the staff and instructors members of master course.
 
     Arguments:
-        master_course (CourseDescriptorWithMixins): Master course instance.
+        master_course (CourseBlockWithMixins): Master course instance.
         ccx_key (CCXLocator): CCX course key.
         display_name (str): ccx display name for email.
         send_email (bool): flag to switch on or off email to the users on access grant.
@@ -396,7 +396,7 @@ def remove_master_course_staff_from_ccx(master_course, ccx_key, display_name, se
     Remove staff and instructor roles on ccx to all the staff and instructors members of master course.
 
     Arguments:
-        master_course (CourseDescriptorWithMixins): Master course instance.
+        master_course (CourseBlockWithMixins): Master course instance.
         ccx_key (CCXLocator): CCX course key.
         display_name (str): ccx display name for email.
         send_email (bool): flag to switch on or off email to the users on revoke access.

lms/djangoapps/discussion/django_comment_client/tests/utils.py
@@ -92,7 +92,7 @@ def config_course_discussions(
     Set discussions and configure divided discussions for a course.
 
     Arguments:
-        course: CourseDescriptor
+        course: CourseBlock
         discussion_topics (Dict): Discussion topic names. Picks ids and
             sort_keys automatically.
         divided_discussions: Discussion topics to divide. Converts the

lms/djangoapps/survey/utils.py
@@ -26,7 +26,7 @@ def is_survey_required_for_course(course_descriptor):
     Returns whether a Survey is required for this course
     """
 
-    # Check to see that the survey is required in the CourseDescriptor.
+    # Check to see that the survey is required in the CourseBlock.
     if not getattr(course_descriptor, 'course_survey_required', False):
         return SurveyRequiredAccessError()
 

commit 130b9f7bb386a02775e4d5bd67f93095f05c6c6e

fix: contentstore next section navigation (#27112)
Fixes bug introduced by #25965 (commit dd96a2) pointed out in:

  https://github.com/edx/edx-platform/commit/dd96a2#r48570091

The next button redirect at the top of the sequence in Studio's Unit editing view
was not going to the correct location.

cms/djangoapps/contentstore/utils.py
@@ -587,7 +587,7 @@ def get_sibling_urls(subsection):
             ))
         else:
             try:
-                next_section = sections[sections.index(next(s for s in sections if s.location == section.location)) - 1]
+                next_section = sections[sections.index(next(s for s in sections if s.location == section.location)) + 1]
                 next_loc = next_section.get_children()[0].get_children()[0].location
             except IndexError:
                 pass