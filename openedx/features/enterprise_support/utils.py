194f29ca55c2219567782a49bffce11db0a457a2
Zaman Afzal <zamanafzal@gmail.com>
2021-04-06 23:27:20 +0500

    ENT4134 Add the support for multiple IDPs (#27073)
    * ENT4134 Add the support for multiple IDPs

--- openedx/features/enterprise_support/utils.py
+++ openedx/features/enterprise_support/utils.py
@@ -278,16 +278,15 @@ def _user_has_social_auth_record(user, enterprise_customer):
     """
     Return True if a `UserSocialAuth` record exists for `user` False otherwise.
     """
-    if enterprise_customer:
-        identity_provider = third_party_auth.provider.Registry.get(
-            provider_id=enterprise_customer['identity_provider'],
-        )
-        if identity_provider:
-            return UserSocialAuth.objects.select_related('user').filter(
-                provider=identity_provider.backend_name,
-                user=user
-            ).exists()
-
+    provider_backend_names = []
+    if enterprise_customer and enterprise_customer['identity_providers']:
+        for idp in enterprise_customer['identity_providers']:
+            identity_provider = third_party_auth.provider.Registry.get(
+                provider_id=idp['provider_id']
+            )
+            provider_backend_names.append(identity_provider.backend_name)
+        return UserSocialAuth.objects.select_related('user').\
+            filter(provider__in=provider_backend_names, user=user).exists()
     return False
 
 
