c6e779145d0b6ae2e3dff0bbc9b5c5da45fa4f68
Luis Moreno <morenol@users.noreply.github.com>
2021-04-09 20:21:44 -0400

    feat: Define openedx signals in the openedx-events library

--- openedx/core/djangoapps/user_authn/views/register.py
+++ openedx/core/djangoapps/user_authn/views/register.py
@@ -31,6 +31,8 @@ from rest_framework.views import APIView
 from social_core.exceptions import AuthAlreadyAssociated, AuthException
 from social_django import utils as social_utils
 
+from openedx_events.auth.signals.v1 import REGISTER_USER
+
 from common.djangoapps import third_party_auth
 # Note that this lives in LMS, so this dependency should be refactored.
 # TODO Have the discussions code subscribe to the REGISTER_USER signal instead.
@@ -93,7 +95,6 @@ REGISTRATION_UTM_PARAMETERS = {
 }
 REGISTRATION_UTM_CREATED_AT = 'registration_utm_created_at'
 # used to announce a registration
-REGISTER_USER = Signal(providing_args=["user", "registration"])
 
 
 # .. toggle_name: registration.enable_failure_logging
