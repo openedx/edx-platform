301ccd31dc7ccea79dcbaec4ed4e87427901d651
stvn <stvn@mit.edu>
2021-04-13 20:05:56 -0700

    refactor: Remove unused test factory

--- lms/djangoapps/courseware/tests/factories.py
+++ lms/djangoapps/courseware/tests/factories.py
@@ -16,16 +16,11 @@ from lms.djangoapps.courseware.models import (
     XModuleUserStateSummaryField
 )
 from common.djangoapps.student.tests.factories import UserFactory
-from common.djangoapps.student.tests.factories import UserProfileFactory as StudentUserProfileFactory
 
 # TODO fix this (course_id and location are invalid names as constants, and course_id should really be COURSE_KEY)
 # pylint: disable=invalid-name
 course_id = CourseKey.from_string('edX/test_course/test')
 location = partial(course_id.make_usage_key, 'problem')
-
-
-class UserProfileFactory(StudentUserProfileFactory):
-    courseware = 'course.xml'
 
 
 class StudentModuleFactory(DjangoModelFactory):  # lint-amnesty, pylint: disable=missing-class-docstring
