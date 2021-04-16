b1fa2bdae628d7b8195e4069282f3ee76e12136f
Kyle McCormick <kmccormick@edx.org>
2021-04-16 14:00:04 -0400

    refactor: make embargo app go through geoinfo API (#27350)
    Consolidates all usage of geoip database within
    geoinfo app.

--- openedx/core/djangoapps/geoinfo/middleware.py
+++ openedx/core/djangoapps/geoinfo/middleware.py
@@ -10,12 +10,12 @@ decorator `django.utils.decorators.decorator_from_middleware(middleware_class)`
 
 """
 import logging
-import geoip2.database
 
-from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 from ipware.ip import get_client_ip
 from ipware.utils import is_public_ip
+
+from .api import country_code_from_ip
 
 log = logging.getLogger(__name__)
 
@@ -37,14 +37,7 @@ class CountryMiddleware(MiddlewareMixin):
             del request.session['ip_address']
             del request.session['country_code']
         elif new_ip_address != old_ip_address and is_public_ip(new_ip_address):
-            reader = geoip2.database.Reader(settings.GEOIP_PATH)
-            try:
-                response = reader.country(new_ip_address)
-                country_code = response.country.iso_code
-            except geoip2.errors.AddressNotFoundError:
-                country_code = ""
-
+            country_code = country_code_from_ip(new_ip_address)
             request.session['country_code'] = country_code
             request.session['ip_address'] = new_ip_address
             log.debug('Country code for IP: %s is set to %s', new_ip_address, country_code)
-            reader.close()
