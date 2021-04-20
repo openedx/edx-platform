3e74faf99c9b3d52e495df3d36987962ac72eeaf
javeriaejaz-arbisoft <javeria.ejaz@arbisoft.com>
2021-04-20 05:22:22 +0500

    LP 2618 - Remove pycodestyle issues

--- openedx/adg/lms/webinars/tests/test_forms.py
+++ openedx/adg/lms/webinars/tests/test_forms.py
@@ -5,15 +5,16 @@ import pytest
 from django.forms.models import model_to_dict
 
 from openedx.adg.lms.webinars.forms import WebinarForm
-from .constants import VALID_EMAIL_ADDRESSES, INVALID_EMAIL_ADDRESSES
+
+from .constants import INVALID_EMAIL_ADDRESSES, VALID_EMAIL_ADDRESSES
 
 
 @pytest.mark.parametrize(
     'emails , expected_result', [
-            (VALID_EMAIL_ADDRESSES, True),
-            (INVALID_EMAIL_ADDRESSES, False)
-        ]
-    )
+        (VALID_EMAIL_ADDRESSES, True),
+        (INVALID_EMAIL_ADDRESSES, False)
+    ]
+)
 @pytest.mark.django_db
 def test_webinar_form_with_emails(webinar, emails, expected_result):
     """
--- openedx/adg/lms/webinars/tests/test_helpers.py
+++ openedx/adg/lms/webinars/tests/test_helpers.py
@@ -17,16 +17,16 @@ from openedx.adg.lms.webinars.helpers import (
     webinar_emails_for_panelists_co_hosts_and_presenter
 )
 
-from .factories import WebinarFactory, WebinarRegistrationFactory
 from .constants import (
-    VALID_EMAIL_ADDRESSES,
+    CO_HOST_1,
+    CO_HOST_2,
     INVALID_EMAIL_ADDRESSES,
     PANELIST_1,
     PANELIST_2,
-    CO_HOST_1,
-    CO_HOST_2,
-    PRESENTER
+    PRESENTER,
+    VALID_EMAIL_ADDRESSES
 )
+from .factories import WebinarFactory, WebinarRegistrationFactory
 
 
 @pytest.mark.django_db
@@ -137,10 +137,10 @@ def test_send_webinar_registration_email(mocker):
 
 @pytest.mark.parametrize(
     'emails , expected_error', [
-            (VALID_EMAIL_ADDRESSES, False),
-            (INVALID_EMAIL_ADDRESSES, True)
-        ]
-    )
+        (VALID_EMAIL_ADDRESSES, False),
+        (INVALID_EMAIL_ADDRESSES, True)
+    ]
+)
 def test_validate_email_list(emails, expected_error):
     """
     Test if 'validate_email_list' returns error when emails are invalid
@@ -151,18 +151,17 @@ def test_validate_email_list(emails, expected_error):
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     'panelists, co_hosts, presenter, expected_emails', [
-            ([PANELIST_1, PANELIST_2], [], PRESENTER, [PANELIST_1, PANELIST_2, PRESENTER]),
-            ([], [CO_HOST_1, CO_HOST_2], PRESENTER, [CO_HOST_1, CO_HOST_2, PRESENTER]),
-            ([], [], PRESENTER, [PRESENTER]),
-            (
-                [PANELIST_1, PANELIST_2],
-                [CO_HOST_1, CO_HOST_2],
-                PRESENTER,
-                [PANELIST_1, PANELIST_2, CO_HOST_1, CO_HOST_2, PRESENTER]
-            ),
-
-        ]
-    )
+        ([PANELIST_1, PANELIST_2], [], PRESENTER, [PANELIST_1, PANELIST_2, PRESENTER]),
+        ([], [CO_HOST_1, CO_HOST_2], PRESENTER, [CO_HOST_1, CO_HOST_2, PRESENTER]),
+        ([], [], PRESENTER, [PRESENTER]),
+        (
+            [PANELIST_1, PANELIST_2],
+            [CO_HOST_1, CO_HOST_2],
+            PRESENTER,
+            [PANELIST_1, PANELIST_2, CO_HOST_1, CO_HOST_2, PRESENTER]
+        ),
+    ]
+)
 def test_webinar_emails_for_panelists_co_hosts_and_presenter(webinar, panelists, co_hosts, presenter, expected_emails):
     """
     Test if 'webinar_emails_for_panelists_co_hosts_and_presenter' returns the list of webinar related emails
@@ -182,17 +181,17 @@ def test_webinar_emails_for_panelists_co_hosts_and_presenter(webinar, panelists,
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     'emails, reference_emails, expected_emails', [
-            ([], ['t1@eg.com', 't2@eg.com'], []),
-            (['t1@eg.com', 't2@eg.com'], [], ['t1@eg.com', 't2@eg.com']),
-            ([], [], []),
-            (
-                ['t1@eg.com', 't2@eg.com', 't3@eg.com'],
-                ['t1@eg.com', 't2@eg.com', 't4@eg.com'],
-                ['t3@eg.com']
-            ),
+        ([], ['t1@eg.com', 't2@eg.com'], []),
+        (['t1@eg.com', 't2@eg.com'], [], ['t1@eg.com', 't2@eg.com']),
+        ([], [], []),
+        (
+            ['t1@eg.com', 't2@eg.com', 't3@eg.com'],
+            ['t1@eg.com', 't2@eg.com', 't4@eg.com'],
+            ['t3@eg.com']
+        ),
 
-        ]
-    )
+    ]
+)
 def test_remove_emails_duplicate_in_other_list(emails, reference_emails, expected_emails):
     """
     Test that only the list of emails that are not present in reference list of emails are returned
