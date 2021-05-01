f1048ea018157686cf0d358644a6044a05b05dfb
Ahmed Faraz <ahmed.faraz@arbisoft.com>
2021-05-01 09:29:48 +0500

    LP-2674 Add technical documentation of registration_extention app

---
+++ openedx/adg/lms/registration_extension/__init__.py
@@ -0,0 +1,8 @@
+"""
+`registration_extension` app customizes edX out of the box registration. Any kind of customizations
+in registration or UserProfile are made here. In case we add or remove any field(s) in registration form
+we need to update REGISTRATION_EXTRA_FIELDS setting accordingly.
+
+Here is the link to the guide to update the above mentioned setting,
+https://github.com/OmnipreneurshipAcademy/edx-platform/wiki/Settings#registration-page-and-x_frame-settings
+"""
---
+++ openedx/adg/lms/student/__init__.py
@@ -0,0 +1,5 @@
+"""
+This app provides the ability to override core common.django.student app. Any customizations in the core app
+are implemented here and injected back. For example, we have customized email sending functionality in helpers
+of this app and then imported those helpers in the core app.
+"""
