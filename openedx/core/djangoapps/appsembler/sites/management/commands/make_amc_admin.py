7754a708939abe8b18ea4fa102c351caab5cb57f
John Baldwin <jlbaldwin@gmail.com>
2021-05-07 11:35:23 +0200

    Juniper upgrade - Fix "make amc admin" command
    This is part 1 that fixes JSON serialization of the output in the
    "make_amc_admin" Django management command. Fixing this bug reveals
    another bug in the "openedx.core.djangoapps.appsembler.sites.utils"
    code. The specific source for this bug still needs to be identified

--- openedx/core/djangoapps/appsembler/sites/management/commands/make_amc_admin.py
+++ openedx/core/djangoapps/appsembler/sites/management/commands/make_amc_admin.py
@@ -2,8 +2,9 @@ import json
 import logging
 
 from django.db.models.query import Q
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.core.serializers.json import DjangoJSONEncoder
 
 from openedx.core.djangoapps.appsembler.sites.utils import make_amc_admin
 
@@ -30,6 +31,6 @@ class Command(BaseCommand):
                             help='The organization name or short name.')
 
     def handle(self, *args, **options):
-        user = User.objects.get(Q(email=options['user']) | Q(username=options['user']))
+        user = get_user_model().objects.get(Q(email=options['user']) | Q(username=options['user']))
         details = make_amc_admin(user=user, org_name=options['org'])
-        self.stdout.write(json.dumps(details))
+        self.stdout.write(json.dumps(details, cls=DjangoJSONEncoder))
