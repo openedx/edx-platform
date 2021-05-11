fb53de5dc166064951cf068edf25129849174ff2
Diane Kaplan <dkaplan@edx.org>
2021-05-11 13:13:53 -0400

    remove offer_banner_wrapper

--- openedx/features/discounts/utils.py
+++ openedx/features/discounts/utils.py
@@ -7,47 +7,16 @@ from datetime import datetime
 import pytz
 from django.utils.translation import get_language
 from django.utils.translation import ugettext as _
-from edx_django_utils.cache import RequestCache
-from web_fragments.fragment import Fragment
 
 from common.djangoapps.course_modes.models import format_course_price, get_course_prices
-from common.djangoapps.util.date_utils import strftime_localized_html
 from lms.djangoapps.experiments.models import ExperimentData
 from lms.djangoapps.courseware.utils import verified_upgrade_deadline_link
-from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from openedx.core.djangolib.markup import HTML, Text
 from openedx.features.discounts.applicability import (
     REV1008_EXPERIMENT_ID,
     can_receive_discount,
     discount_percentage,
     get_discount_expiration_date
 )
-
-
-def offer_banner_wrapper(user, block, view, frag, context):  # pylint: disable=W0613
-    """
-    A wrapper that prepends the First Purchase Discount banner if
-    the user hasn't upgraded yet.
-    """
-    if block.category != 'vertical':
-        return frag
-
-    offer_banner_fragment = get_first_purchase_offer_banner_fragment_from_key(
-        user, block.course_id
-    )
-
-    if not offer_banner_fragment:
-        return frag
-
-    # Course content must be escaped to render correctly due to the way the
-    # way the XBlock rendering works. Transforming the safe markup to unicode
-    # escapes correctly.
-    offer_banner_fragment.content = str(offer_banner_fragment.content)
-
-    offer_banner_fragment.add_content(frag.content)
-    offer_banner_fragment.add_fragment_resources(frag)
-
-    return offer_banner_fragment
 
 
 def _get_discount_prices(user, course, assume_discount=False):
