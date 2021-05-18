d7a4948b8ee015ad918dac473114b728c65418f8
Carla Duarte <cduarte@edx.org>
2021-05-18 17:18:13 -0400

    feat: add total number of assignments to progress API (AA-816)

--- lms/djangoapps/course_home_api/progress/v1/serializers.py
+++ lms/djangoapps/course_home_api/progress/v1/serializers.py
@@ -56,6 +56,7 @@ class GradingPolicySerializer(serializers.Serializer):
     def get_assignment_policies(self, grading_policy):
         return [{
             'num_droppable': assignment_policy['drop_count'],
+            'num_total': assignment_policy['min_count'],
             'short_label': assignment_policy.get('short_label', ''),
             'type': assignment_policy['type'],
             'weight': assignment_policy['weight'],
