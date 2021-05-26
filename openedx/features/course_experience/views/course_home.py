2243d8a17d9ba229d37c1f6513b1477867ab9c98
Diane Kaplan <dkaplan@edx.org>
2021-05-26 14:01:27 -0400

    feat: remove course_home_welcome upsell link (REV-2131)

--- openedx/features/course_experience/views/course_home.py
+++ openedx/features/course_experience/views/course_home.py
@@ -43,7 +43,6 @@ from openedx.features.course_experience.views.course_outline import CourseOutlin
 from openedx.features.course_experience.views.course_sock import CourseSockFragmentView
 from openedx.features.course_experience.views.latest_update import LatestUpdateFragmentView
 from openedx.features.course_experience.views.welcome_message import WelcomeMessageFragmentView
-from openedx.features.discounts.utils import get_first_purchase_offer_banner_fragment
 from openedx.features.discounts.utils import format_strikeout_price
 from common.djangoapps.student.models import CourseEnrollment
 from common.djangoapps.util.views import ensure_valid_course_key
@@ -137,7 +136,6 @@ class CourseHomeFragmentView(EdxFragmentView):
         outline_fragment = None
         update_message_fragment = None
         course_sock_fragment = None
-        offer_banner_fragment = None
         course_expiration_fragment = None
         has_visited_course = None
         resume_course_url = None
@@ -161,10 +159,6 @@ class CourseHomeFragmentView(EdxFragmentView):
             )
             has_visited_course, resume_course_url = self._get_resume_course_info(request, course_id)
             handouts_html = self._get_course_handouts(request, course)
-            offer_banner_fragment = get_first_purchase_offer_banner_fragment(
-                request.user,
-                course_overview
-            )
             course_expiration_fragment = generate_course_expired_fragment(
                 request.user,
                 course_overview
@@ -227,7 +221,6 @@ class CourseHomeFragmentView(EdxFragmentView):
             'outline_fragment': outline_fragment,
             'handouts_html': handouts_html,
             'course_home_message_fragment': course_home_message_fragment,
-            'offer_banner_fragment': offer_banner_fragment,
             'course_expiration_fragment': course_expiration_fragment,
             'has_visited_course': has_visited_course,
             'resume_course_url': resume_course_url,
