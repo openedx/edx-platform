f62e0d057f13094751e160b81017022eb721c605
WaleedUlHassan <waleedulhassan@A006-00493.local>
2021-05-27 11:25:45 +0500

    Change of function name

--- openedx/features/course_experience/utils.py
+++ openedx/features/course_experience/utils.py
@@ -15,7 +15,7 @@ from xmodule.modulestore.django import modulestore
 
 
 @request_cached()
-def get_course_outline_block_tree(request, course_id, user=None, allow_start_dates_in_future=False):  # lint-amnesty, pylint: disable=too-many-statements
+def get_course_outline_block_tree_accessed(request, course_id, user=None, allow_start_dates_in_future=False):  # lint-amnesty, pylint: disable=too-many-statements
     """
     Returns the root block of the course outline, with children as blocks.
 
@@ -57,6 +57,117 @@ def get_course_outline_block_tree(request, course_id, user=None, allow_start_dat
     )
 
     course_outline_root_block = all_blocks['blocks'].get(all_blocks['root'], None)
+    return course_outline_root_block
+
+
+@request_cached()
+def get_course_outline_block_tree(request, course_id, user=None, allow_start_dates_in_future=False):  # lint-amnesty, pylint: disable=too-many-statements
+    """
+    Returns the root block of the course outline, with children as blocks.
+    allow_start_dates_in_future (bool): When True, will allow blocks to be
+            returned that can bypass the StartDateTransformer's filter to show
+            blocks with start dates in the future.
+    """
+
+    assert user is None or user.is_authenticated
+
+    def populate_children(block, all_blocks):
+        """
+        Replace each child id with the full block for the child.
+        Given a block, replaces each id in its children array with the full
+        representation of that child, which will be looked up by id in the
+        passed all_blocks dict. Recursively do the same replacement for children
+        of those children.
+        """
+        children = block.get('children', [])
+
+        for i in range(len(children)):
+            child_id = block['children'][i]
+            child_detail = populate_children(all_blocks[child_id], all_blocks)
+            block['children'][i] = child_detail
+
+        return block
+
+    def recurse_mark_scored(block):
+        """
+        Mark this block as 'scored' if any of its descendents are 'scored' (that is, 'has_score' and 'weight' > 0).
+        """
+        is_scored = block.get('has_score', False) and block.get('weight', 1) > 0
+        # Use a list comprehension to force the recursion over all children, rather than just stopping
+        # at the first child that is scored.
+        children_scored = any(recurse_mark_scored(child) for child in block.get('children', []))
+        if is_scored or children_scored:
+            block['scored'] = True
+            return True
+        else:
+            block['scored'] = False
+            return False
+
+    def recurse_num_graded_problems(block):
+        """
+        Marks each block with the number of graded and scored leaf blocks below it as 'num_graded_problems'
+        """
+        is_scored = block.get('has_score') and block.get('weight', 1) > 0
+        is_graded = block.get('graded')
+        is_countable = block.get('type') not in ('lti', 'lti_consumer')
+        is_graded_problem = is_scored and is_graded and is_countable
+
+        num_graded_problems = 1 if is_graded_problem else 0
+        num_graded_problems += sum(recurse_num_graded_problems(child) for child in block.get('children', []))
+
+        block['num_graded_problems'] = num_graded_problems
+        return num_graded_problems
+
+    def recurse_mark_auth_denial(block):
+        """
+        Mark this block as 'scored' if any of its descendents are 'scored' (that is, 'has_score' and 'weight' > 0).
+        """
+        own_denial_reason = {block['authorization_denial_reason']} if 'authorization_denial_reason' in block else set()
+        # Use a list comprehension to force the recursion over all children, rather than just stopping
+        # at the first child that is scored.
+        child_denial_reasons = own_denial_reason.union(
+            *(recurse_mark_auth_denial(child) for child in block.get('children', []))
+        )
+        if child_denial_reasons:
+            block['all_denial_reasons'] = child_denial_reasons
+        return child_denial_reasons
+
+    course_key = CourseKey.from_string(course_id)
+    course_usage_key = modulestore().make_course_usage_key(course_key)
+
+    all_blocks = get_blocks(
+        request,
+        course_usage_key,
+        user=user,
+        nav_depth=3,
+        requested_fields=[
+            'children',
+            'contains_gated_content',
+            'display_name',
+            'due',
+            'effort_activities',
+            'effort_time',
+            'format',
+            'graded',
+            'has_score',
+            'show_gated_sections',
+            'special_exam_info',
+            'start',
+            'type',
+            'weight',
+            'completion',
+            'complete',
+            'resume_block',
+        ],
+        allow_start_dates_in_future=allow_start_dates_in_future,
+    )
+
+    course_outline_root_block = all_blocks['blocks'].get(all_blocks['root'], None)
+    if course_outline_root_block:
+        populate_children(course_outline_root_block, all_blocks['blocks'])
+        recurse_mark_scored(course_outline_root_block)
+        recurse_num_graded_problems(course_outline_root_block)
+        recurse_mark_auth_denial(course_outline_root_block)
     return course_outline_root_block
 
 
