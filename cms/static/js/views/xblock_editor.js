4e7160fdd3dc26fd2904348d58707cffc510d06e
João Victor Martins <jvdm@sdf.org>
2021-06-02 23:17:27 -0300

    WIP: Initial installation of visual editor

--- cms/static/js/views/xblock_editor.js
+++ cms/static/js/views/xblock_editor.js
@@ -45,10 +45,21 @@ define(['jquery', 'underscore', 'gettext', 'js/views/baseview', 'js/views/xblock
             },
 
             getDefaultModes: function() {
-                return [
+                var defaultModes = [
                     {id: 'editor', name: gettext('Editor')},
                     {id: 'settings', name: gettext('Settings')}
                 ];
+                if (window.LXCData !== undefined) {
+                    defaultModes.unshift({
+                        id: 'visual-editor', name: gettext('Visual Editor')
+                    });
+                }
+                return defaultModes;
+            },
+
+            getVisualEditor: function() {
+                var editor = this.$('.visual-editor');
+                return editor.length === 1 ? editor : null;
             },
 
             hasCustomTabs: function() {
@@ -157,7 +168,8 @@ define(['jquery', 'underscore', 'gettext', 'js/views/baseview', 'js/views/xblock
             selectMode: function(mode) {
                 var dataEditor = this.getDataEditor(),
                     metadataEditor = this.getMetadataEditor(),
-                    pluginEditor = this.getPluginEditor();
+                    pluginEditor = this.getPluginEditor(),
+                    visualEditor = this.getVisualEditor();
                 if (dataEditor) {
                     this.setEditorActivation(dataEditor, mode === 'editor');
                 }
@@ -166,6 +178,9 @@ define(['jquery', 'underscore', 'gettext', 'js/views/baseview', 'js/views/xblock
                 }
                 if (pluginEditor) {
                     this.setEditorActivation(pluginEditor.$el, mode === 'plugins');
+                }
+                if (visualEditor) {
+                    this.setEditorActivation(visualEditor, mode === 'visual-editor');
                 }
                 this.mode = mode;
             },
