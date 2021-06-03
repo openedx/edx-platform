19248517268f9c43604dd7d513230ebb22724987
Shimul Chowdhury <shimul@opencraft.com>
2021-06-03 19:38:13 +0600

    feat: add Course Membership API
    The get_course_members API returns a dict of users associated with a course.
    This is a potentially expensive operation on a large course, so there is a
    control in place to limit its cost.  If a course has more than
    settings.COURSE_MEMBER_API_ENROLLMENT_LIMIT enrollments, then the function
    raises an OverEnrollmentLimitException.
    
    This API was added to help implement the LTI 1.3 Names and Roles Provisioning
    service.
    
    Jira references: [BD-24] [BB-2726] [TNL-7330]
    Pull request: #25843
    Co-authored-by: Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
    
    (cherry picked from commit 2a8a58ae6b566dcd5262ae08d9c1173890e1c57c)

--- lms/djangoapps/course_api/api.py
+++ lms/djangoapps/course_api/api.py
@@ -2,6 +2,7 @@
 Course API
 """
 import logging
+from collections import defaultdict
 
 import search
 from django.conf import settings
@@ -12,7 +13,7 @@ from edx_when.api import get_dates_for_course
 from opaque_keys.edx.django.models import CourseKeyField
 from rest_framework.exceptions import PermissionDenied
 
-from common.djangoapps.student.models import CourseAccessRole
+from common.djangoapps.student.models import CourseAccessRole, CourseEnrollment
 from common.djangoapps.student.roles import GlobalStaff
 from lms.djangoapps.courseware.access import has_access
 from lms.djangoapps.courseware.courses import (
@@ -25,6 +26,7 @@ from openedx.core.lib.api.view_utils import LazySequence
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
 
+from .exceptions import OverEnrollmentLimitException
 from .permissions import can_view_courses_for_username
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -266,3 +268,101 @@ def get_course_run_url(request, course_id):
     """
     course_run_url = reverse('openedx.course_experience.course_home', args=[course_id])
     return request.build_absolute_uri(course_run_url)
+
+
+def get_course_members(course_key):
+    """
+    Returns a dict containing all users with access to a course through CourseEnrollment
+    and CourseAccessRole models.
+
+    User information includes id, email, username, name, enrollment mode and role list.
+
+    This API is limited and will only work for courses with less than a configurable number
+    of active enrollments (managed through `settings.COURSE_MEMBER_API_ENROLLMENT_LIMIT`,
+    and the default value is 1000). More than that and the method will raise a
+    `OverEnrollmentLimitException` exception.
+
+    This method works by querying the `CourseEnrollment` and `CourseAccessRole` models,
+    prefetching user information and *then joining results in Python using dictionaries*.
+    This approach was choosen to avoid database heavy queries (such as DISTINCT and COUNT) in
+    the `CourseEnrollment` table, which would take too long to complete in a request lifecycle.
+
+    The main concern with this approach is the dataset size and resource usage since this method
+    returns all enrollments without pagination. We're using a conservative number on the
+    `COURSE_MEMBER_API_ENROLLMENT_LIMIT` setting to avoid any issues.
+
+    Examples:
+        - Get all course members:
+            get_course_members(course_key)
+
+    Arguments:
+        course_key (CourseKey): CourseKey to retrieve student data.
+
+    Returns:
+        dict: A dictionary with the following format:
+            {
+                "user_id": {
+                    "id": 12,
+                    "username": "jonh5000",
+                    "email": "jonh@example.com",
+                    "name": "Jonh Doe",
+                    "enrollment_mode": "verified",
+                    "roles": [
+                        "student",
+                        "instructor",
+                        "staff",
+                    ]
+                }
+            }
+    """
+    def make_user_info_dict(user, enrollment_mode=None):
+        """
+        Utility function to extract user information from model.
+        """
+        return {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "name": user.profile.name,
+            "enrollment_mode": enrollment_mode,
+        }
+
+    # Raise error if trying to retrieve user list from a course with more than
+    # settings.COURSE_MEMBER_API_ENROLLMENT_LIMIT active enrollments. The fastest way
+    # to do this is to query for the 1st item after `COURSE_MEMBER_API_ENROLLMENT_LIMIT`.
+    over_limit = CourseEnrollment.get_active_enrollments_in_course(
+        course_key
+    )[settings.COURSE_MEMBER_API_ENROLLMENT_LIMIT:][:1]
+    if over_limit.exists():
+        raise OverEnrollmentLimitException(
+            f"Can't retrieve course members for {course_key} since it has more than "
+            f"{settings.COURSE_MEMBER_API_ENROLLMENT_LIMIT} active enrollments. "
+            f"This limit is stored on `settings.COURSE_MEMBER_API_ENROLLMENT_LIMIT`."
+        )
+
+    # Python dicts where we're going to manually combine the data from the two querysets
+    user_roles = defaultdict(list)
+    user_info = {}
+
+    # Retrieve all active enrollments in course and prefetch user information
+    enrollments = CourseEnrollment.get_active_enrollments_in_course(course_key)
+
+    # Retrieve all course access roles and prefetch user information
+    access_roles = CourseAccessRole.access_roles_in_course(course_key)
+
+    # Evaluates querysets and parses data from the two querysets
+    # into `user_info` and `user_roles` dictionaries.
+    for enrollment in enrollments:
+        user_roles[enrollment.user_id].append('student')
+        user_info[enrollment.user_id] = make_user_info_dict(enrollment.user, enrollment.mode)
+
+    for access_role in access_roles:
+        user_roles[access_role.user_id].append(access_role.role)
+        if access_role.user_id not in user_info:
+            user_info[access_role.user_id] = make_user_info_dict(access_role.user)
+
+    # Merge user role information with `user_info`
+    for user_id in user_info:
+        user_info[user_id]['roles'] = user_roles[user_id]
+
+    return user_info
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3998,6 +3998,17 @@ PROFILE_IMAGE_SIZES_MAP = {
 # If set to None, all courses will be listed on the homepage
 HOMEPAGE_COURSE_MAX = None
 
+# .. setting_name: COURSE_MEMBER_API_ENROLLMENT_LIMIT
+# .. setting_implementation: DjangoSetting
+# .. setting_default: 1000
+# .. setting_description: This limits the response size of the `get_course_members` API, throwing an exception
+#    if the number of Enrolled users is greater than this number. This is needed to limit the dataset size
+#    since the API does most of the calculation in Python to avoid expensive database queries.
+# .. setting_use_cases: open_edx
+# .. setting_creation_date: 2021-05-18
+# .. setting_tickets: https://openedx.atlassian.net/browse/TNL-7330
+COURSE_MEMBER_API_ENROLLMENT_LIMIT = 1000
+
 ################################ Settings for Credit Courses ################################
 # Initial delay used for retrying tasks.
 # Additional retries use longer delays.
