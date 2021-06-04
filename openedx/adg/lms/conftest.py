b66aea5a7fa0667a3f5df0498567c326d6786e8e
Asad Ali <asad.ali@arbisoft.com>
2021-06-04 15:10:47 +0500

    LP-2712 Fix failing test due to fixture

--- openedx/adg/lms/conftest.py
+++ openedx/adg/lms/conftest.py
@@ -1,11 +1,12 @@
 """
 Test configurations for lms
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
 from common.djangoapps.student.tests.factories import UserFactory, UserProfileFactory
+from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
 
 
 @pytest.fixture(name='current_time')
@@ -31,3 +32,18 @@ def user_client_login(request, client):
     user = UserFactory()
     client.login(username=user.username, password='test')
     return user, client
+
+
+@pytest.fixture(name='courses')
+def course_overviews(current_time):
+    """
+    Fixture which return multiple courses
+    """
+    course_start_end_date = {
+        'start_date': current_time - timedelta(days=1),
+        'end_date': current_time + timedelta(days=1),
+    }
+    return {
+        'test_course1': CourseOverviewFactory(language='en', **course_start_end_date),
+        'test_course2': CourseOverviewFactory(language='ar', **course_start_end_date),
+    }
