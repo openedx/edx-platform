b4a1dcce23b52417acc6060b72a3255dc06fef02
Ahmed Faraz <ahmed.faraz@arbisoft.com>
2021-06-08 18:35:57 +0500

    LP-2720 Enable default course intructors - Remove the code get course instructors from course team - Remove the code to pass course instructors in extra context   as the default instructors are being passed OOTB

--- openedx/adg/lms/courseware_override/helpers.py
+++ openedx/adg/lms/courseware_override/helpers.py
@@ -4,11 +4,9 @@ Helpers for courseware app
 from django.utils.translation import get_language_info
 
 from common.djangoapps.student.models import CourseEnrollment
-from common.djangoapps.student.roles import CourseInstructorRole
 from lms.djangoapps.courseware.courses import get_courses as get_courses_core
 from openedx.adg.lms.applications.models import MultilingualCourse, MultilingualCourseGroup
 from openedx.adg.lms.utils.env_utils import is_testing_environment
-from openedx.core.djangoapps.user_api.accounts.image_helpers import get_profile_image_urls_for_user
 
 
 def get_courses(user):
@@ -16,30 +14,6 @@ def get_courses(user):
     Return courses using core method if environment is test environment else uses customized method for courses list.
     """
     return get_courses_core(user) if is_testing_environment() else MultilingualCourseGroup.objects.get_courses(user)
-
-
-def get_course_instructors(course_key, request=None):
-    """
-    Gets all the instructor profiles and image_urls associated for the given course key
-
-    Arguments:
-        request: request object
-        course_key: The key of the Course for which to fetch all the instructor data
-
-    Returns:
-        list: A list of dicts where each dict has a UserProfile object and a dict containing the
-        profile image urls in the {'size':'url'} format for that particular instructor profile.
-    """
-    course_locator = course_key.to_course_locator() if getattr(course_key, 'ccx', None) else course_key
-
-    instructors = CourseInstructorRole(course_locator).users_with_role()
-    instructor_data = []
-
-    for instructor in instructors:
-        profile_image_urls_for_instructor = get_profile_image_urls_for_user(instructor, request=request)
-        instructor_data.append({'profile': instructor.profile, 'profile_image_urls': profile_image_urls_for_instructor})
-
-    return instructor_data
 
 
 def get_language_names_from_codes(language_codes_with_course_id):
@@ -83,12 +57,10 @@ def get_extra_course_about_context(request, course):
         course_language_codes = course_group_courses.open_multilingual_courses().language_codes_with_course_ids()
         course_language_names = get_language_names_from_codes(course_language_codes)
 
-    course_instructors = get_course_instructors(course.id, request=request)
     course_enrollment_count = CourseEnrollment.objects.enrollment_counts(course.id).get('total')
 
     context = {
         'course_languages': course_language_names,
-        'instructors': course_instructors,
         'total_enrollments': course_enrollment_count,
         'self_paced': course.self_paced,
         'effort': course.effort,
--- openedx/adg/lms/courseware_override/tests/test_helpers.py
+++ openedx/adg/lms/courseware_override/tests/test_helpers.py
@@ -4,17 +4,12 @@ Tests for helpers.py file of courseware_override
 import mock
 import pytest
 
-from common.djangoapps.student.roles import CourseInstructorRole
 from common.djangoapps.student.tests.factories import CourseEnrollmentFactory, UserFactory
 from openedx.adg.lms.applications.tests.factories import MultilingualCourseFactory
-from openedx.adg.lms.courseware_override.helpers import (
-    get_course_instructors,
-    get_extra_course_about_context,
-    get_language_names_from_codes
-)
+from openedx.adg.lms.courseware_override.helpers import get_extra_course_about_context, get_language_names_from_codes
 from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
 
-from .constants import DUMMY_IMAGE_URLS, EFFORT
+from .constants import EFFORT
 
 
 @pytest.mark.django_db
@@ -28,26 +23,6 @@ def user_fixture(request):
     """
     users = UserFactory.create_batch(request.param)
     return users
-
-
-@pytest.mark.django_db
-@mock.patch('openedx.adg.lms.courseware_override.helpers.get_profile_image_urls_for_user')
-@pytest.mark.parametrize('users', [0, 1, 2], indirect=True)
-def test_get_course_instructors(profile_image_urls_mock, users):
-    """
-    Tests if all the data related to the course instructors i.e the profile and the image urls are fetched
-    correctly or not
-    """
-    course = CourseOverviewFactory()
-    expected_output = []
-
-    profile_image_urls_mock.return_value = DUMMY_IMAGE_URLS
-
-    for user in users:
-        CourseInstructorRole(course.id).add_users(user)
-        expected_output.append({'profile': user.profile, 'profile_image_urls': DUMMY_IMAGE_URLS})
-
-    assert get_course_instructors(course.id) == expected_output
 
 
 @pytest.mark.django_db
@@ -71,28 +46,23 @@ def test_get_language_name_from_codes(language_codes, language_names):
 
 @pytest.mark.django_db
 @mock.patch('openedx.adg.lms.courseware_override.helpers.is_testing_environment')
-@mock.patch('openedx.adg.lms.courseware_override.helpers.get_course_instructors')
 @mock.patch('openedx.adg.lms.courseware_override.helpers.get_language_names_from_codes')
-def test_get_extra_course_about_context(mock_get_lang_names, mock_get_instructors, mock_testing_env):
+def test_get_extra_course_about_context(mock_get_lang_names, mock_testing_env):
     """
     Test if the context is being returned correctly by the get_extra_course_about_context function
     """
     user = UserFactory()
     course = CourseOverviewFactory(self_paced=True, effort=EFFORT, language='en')
-    CourseInstructorRole(course.id).add_users(user)
     CourseEnrollmentFactory(user=user, course=course)
     MultilingualCourseFactory(course=course)
 
     course_languages_with_ids = [(course.id, 'English')]
-    instructor_profiles_with_image_urls = [{"profile": user.profile, "profile_image_urls": DUMMY_IMAGE_URLS}]
 
     mock_get_lang_names.return_value = course_languages_with_ids
-    mock_get_instructors.return_value = instructor_profiles_with_image_urls
     mock_testing_env.return_value = False
 
     expected_context = {
         'course_languages': course_languages_with_ids,
-        'instructors': instructor_profiles_with_image_urls,
         'total_enrollments': 1,
         'self_paced': True,
         'effort': EFFORT,
