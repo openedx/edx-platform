bebf2524569eeafcced193d8d7d13091ae0908f1
Farhaan Bukhsh <farhaan@opencraft.com>
2021-06-22 15:47:53 +0530

    Fix linting issues
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- cms/djangoapps/contentstore/tests/test_course_create_rerun.py
+++ cms/djangoapps/contentstore/tests/test_course_create_rerun.py
@@ -34,6 +34,7 @@ def mock_render_to_string(template_name, context):
     """Return a string that encodes template_name and context"""
     return str((template_name, context))
 
+
 @ddt.ddt
 class TestCourseListing(ModuleStoreTestCase):
     """
