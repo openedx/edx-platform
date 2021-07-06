a38a54f122d043f5e706ca5f8e3b8d62869bdc76
javeriaejaz-arbisoft <javeria.ejaz@arbisoft.com>
2021-07-06 17:51:05 +0500

    LP-2768 Improvements and Requested Changes in ADG Platform

--- openedx/adg/lms/branding_extension/constants.py
+++ openedx/adg/lms/branding_extension/constants.py
@@ -1,5 +1,9 @@
 """
 Constants for branding extension
 """
+from django.conf import settings
+
 TARGET_BLANK = '_blank'
 TARGET_SELF = '_self'
+
+FAQS_LINK = f'{settings.SUPPORT_LINK}/hc/en-us/sections/360010666551-FAQ'
