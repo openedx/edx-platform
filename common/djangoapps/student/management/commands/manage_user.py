c7f29e2b2511f81331cd096403998f8912d5cd14
Saad Ali <saad.ali@arbisoft.com>
2021-07-08 14:26:07 +0500

    Removed function copied

--- common/djangoapps/student/management/commands/manage_user.py
+++ common/djangoapps/student/management/commands/manage_user.py
@@ -13,20 +13,6 @@ from django.utils.translation import gettext as _
 class Command(BaseCommand):  # lint-amnesty, pylint: disable=missing-class-docstring
     help = 'Creates the specified user, if it does not exist, and sets its groups.'
 
-    # def _maybe_update(self, user, attribute, new_value):
-    #     """
-    #     DRY helper.  If the specified attribute of the user differs from the
-    #     specified value, it will be updated.
-    #     """
-    #     old_value = getattr(user, attribute)
-    #     if new_value != old_value:
-    #         self.stderr.write(
-    #             _('Setting {attribute} for user "{username}" to "{new_value}"').format(
-    #                 attribute=attribute, username=user.username, new_value=new_value
-    #             )
-    #         )
-    #         setattr(user, attribute, new_value)
-
     def add_arguments(self, parser):
         parser.add_argument('username')
         parser.add_argument('email')
