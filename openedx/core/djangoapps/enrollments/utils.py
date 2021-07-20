e7097f4c283bc46de3159882b7ae7917f8b94ae7
binodpant <bpant@edx.org>
2021-07-19 20:09:47 -0400

    docs: docstring minor update

--- openedx/core/djangoapps/enrollments/utils.py
+++ openedx/core/djangoapps/enrollments/utils.py
@@ -9,6 +9,9 @@ logger = logging.getLogger(__name__)
 
 
 def add_user_to_course_cohort(cohort_name, course_id, user):
+    """
+    If cohort_name is provided, adds user to the cohort
+    """
     if cohort_name is not None:
         cohort = get_cohort_by_name(course_id, cohort_name)
         try:
