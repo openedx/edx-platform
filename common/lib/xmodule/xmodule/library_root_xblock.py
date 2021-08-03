d6202880ae43845d1ce6f6027f7e10a5f48a034a
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-08-03 15:42:14 +0500

    chore: Removed python_2_unicode_compatible decorator. (#28342)
    * chore: Removed python_2_unicode_compatible decorator.

--- common/lib/xmodule/xmodule/library_root_xblock.py
+++ common/lib/xmodule/xmodule/library_root_xblock.py
@@ -5,7 +5,6 @@
 
 import logging
 
-from django.utils.encoding import python_2_unicode_compatible
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
 from xblock.fields import Boolean, List, Scope, String
@@ -18,7 +17,6 @@ log = logging.getLogger(__name__)
 _ = lambda text: text
 
 
-@python_2_unicode_compatible
 class LibraryRoot(XBlock):
     """
     The LibraryRoot is the root XBlock of a content library. All other blocks in
