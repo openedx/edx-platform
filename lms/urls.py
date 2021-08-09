79e934b357c8d63391d4a2f2a56530fb0f29cd81
Maneesha Tejani <20100214@lums.edu.pk>
2021-08-09 12:32:32 +0500

    create new repository(package) for wikimedia feature apps

--- lms/urls.py
+++ lms/urls.py
@@ -676,6 +676,12 @@ urlpatterns += [
         r'^u/',
         include('openedx.features.learner_profile.urls'),
     ),
+
+    url(
+        r'^wikimedia_features/',
+        include(('openedx.features.wikimedia_features.urls',
+                 'openedx.features.wikimedia_features'), namespace='wikimedia_features')
+    ),
 ]
 
 if settings.FEATURES.get('ENABLE_TEAMS'):
---
+++ openedx/features/wikimedia_features/admin_dashboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdminDashboardConfig(AppConfig):
+    name = 'openedx.features.wikimedia_features.admin_dashboard'
