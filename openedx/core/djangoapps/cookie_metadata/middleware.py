cfacaaac96fe5c8e38238dc7a06bf6fee2796a46
jinder1s <msingh@edx.org>
2021-08-12 16:37:27 -0400

    squash: review fixes

--- openedx/core/djangoapps/cookie_metadata/middleware.py
+++ openedx/core/djangoapps/cookie_metadata/middleware.py
@@ -7,7 +7,6 @@ class CookieNameChange:
 
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
         """
@@ -15,7 +14,7 @@ class CookieNameChange:
 
         For this middleware to run:
         - set COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE =  True
-        - COOKIE_NAME_CHANGE__EXPAND_INFO is a dict and has following info:
+        - COOKIE_NAME_CHANGE_EXPAND_INFO is a dict and has following info:
             "old_name": Previous name of cookie
             "new_name": New name of cookie
             if you also want to delete the cookie in user's browser, set "old_domain" as well
@@ -26,7 +25,6 @@ class CookieNameChange:
                 + it will not modify cookie with "new_name" if it already exists in request.COOKIES
         """
 
-        # Let students save and manage their annotations
         # .. toggle_name: COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE
         # .. toggle_implementation: SettingToggle
         # .. toggle_default: False
@@ -37,35 +35,25 @@ class CookieNameChange:
         # .. toggle_removal_date: 2021-09-01
         # .. toggle_tickets: TODO
         if getattr(settings, "COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE", False):
-
             old_cookie_in_request = False
+            expand_settings = getattr(settings, "COOKIE_NAME_CHANGE_EXPAND_INFO", None)
+
             if (
-                (
-                    expand_settings := getattr(
-                        settings, "COOKIE_NAME_CHANGE_EXPAND_INFO", None
-                    )
-                )
-                is not None
-                and expand_settings.get("new_name", None) is not None
-                and expand_settings.get("old_name", None) is not None
+                expand_settings is not None
+                and type(expand_settings) is dict
+                and "new_name" in expand_settings
+                and "old_name" in expand_settings
             ):
                 if expand_settings["old_name"] in request.COOKIES:
                     old_cookie_in_request = True
-                    old_cookie_value = request.COOKIES[
-                            expand_settings['old_name']
-                        ]
-                    del request.COOKIES[expand_settings['old_name']]
+                    old_cookie_value = request.COOKIES[expand_settings["old_name"]]
+                    del request.COOKIES[expand_settings["old_name"]]
 
-                if expand_settings["new_name"] not in request.COOKIES and old_cookie_in_request:
-                        request.COOKIES[expand_settings["new_name"]] = old_cookie_value
+                if (
+                    expand_settings["new_name"] not in request.COOKIES
+                    and old_cookie_in_request
+                ):
+                    request.COOKIES[expand_settings["new_name"]] = old_cookie_value
 
         response = self.get_response(request)
-
-        # This was commented out case the current usecase
-        # does not require deleting old cookies cause of short experation date
-        # if old_cookie_in_request:
-        #     response.delete_cookie(
-        #         expand_settings[old_name], domain=expand_settings["old_domain"])
-        #     )
-
         return response
--- openedx/core/djangoapps/cookie_metadata/tests/test_cookie_name_change.py
+++ openedx/core/djangoapps/cookie_metadata/tests/test_cookie_name_change.py
@@ -14,123 +14,100 @@ class TestCookieNameChange(TestCase):
     Test class for CookieNameChange
     """
 
-    def test_cookie_swap(self):
-        """Check to make sure Middleware correctly swaps keys"""
 
-        mock_response = Mock()
-        middleware = CookieNameChange(mock_response)
-        mock_request = Mock()
+    def setUp(self):
+        self.mock_response = Mock()
+        self.cookie_name_change_middleware = CookieNameChange(self.mock_response)
+        self.mock_request = Mock()
 
-        old_value = "." * 100
-        extra_cookies = {
+
+        self.old_value = "." * 100
+        self.old_key = 'a'
+        self.extra_cookies = {
             "_b": "." * 13,
             "_c_": "." * 13,
             "a.b": "." * 10,
         }
-        old_dict = {
-            "a": old_value,
+        self.old_dict = {
+            self.old_key: self.old_value,
         }
-        old_dict.update(extra_cookies)
 
-        mock_request.COOKIES = old_dict.copy()
-        expand_settings = {
-            "old_name": "a",
+        self.expand_settings = {
+            "old_name": self.old_key,
             "new_name": "b",
             "old_domain": "old_domain.com",
         }
 
+    def test_cookie_swap(self):
+        """Check to make sure self.Middleware correctly swaps keys"""
+
+        self.old_dict.update(self.extra_cookies)
+
+        self.mock_request.COOKIES = self.old_dict.copy()
+
         with self.settings(
             COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE=True
-        ), self.settings(COOKIE_NAME_CHANGE_EXPAND_INFO=expand_settings):
-            middleware(mock_request)
+        ), self.settings(COOKIE_NAME_CHANGE_EXPAND_INFO=self.expand_settings):
+            self.cookie_name_change_middleware(self.mock_request)
 
-        assert expand_settings["old_name"] not in mock_request.COOKIES.keys()
-        assert expand_settings["new_name"] in mock_request.COOKIES.keys()
-        assert mock_request.COOKIES[expand_settings["new_name"]] == old_value
-        test_dict = extra_cookies.copy()
-        test_dict[expand_settings['new_name']] = old_value
-        assert mock_request.COOKIES == test_dict
+        assert self.expand_settings["old_name"] not in self.mock_request.COOKIES.keys()
+        assert self.expand_settings["new_name"] in self.mock_request.COOKIES.keys()
+        assert self.mock_request.COOKIES[self.expand_settings["new_name"]] == self.old_value
+        test_dict = self.extra_cookies.copy()
+        test_dict[self.expand_settings['new_name']] = self.old_value
+        assert self.mock_request.COOKIES == test_dict
 
         # make sure response function is called once
-        mock_response.assert_called_once()
+        self.mock_response.assert_called_once()
 
     def test_cookie_no_swap(self):
-        """Make sure middleware does not change cookie if new_name cookie is already present"""
+        """Make sure self.cookie_name_change_middleware does not change cookie if new_name cookie is already present"""
 
-        expand_settings = {
-            "old_name": "a",
-            "new_name": "b",
-            "old_domain": "old_domain.com",
-        }
-
-        mock_response = Mock()
-        middleware = CookieNameChange(mock_response)
-
-        mock_request = Mock()
-
-        old_value = "." * 100
         new_value = "." * 13
         no_change_cookies = {
-            expand_settings['new_name']: new_value,
+            self.expand_settings['new_name']: new_value,
             "_c_": "." * 13,
             "a.b": "." * 10,
         }
-        old_dict = {
-            "a": old_value,
-        }
-        old_dict.update(no_change_cookies)
 
-        mock_request.COOKIES = old_dict.copy()
+        self.old_dict.update(no_change_cookies)
+
+        self.mock_request.COOKIES = self.old_dict.copy()
 
 
         with self.settings(
             COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE=True
-        ), self.settings(COOKIE_NAME_CHANGE_EXPAND_INFO=expand_settings):
-            middleware(mock_request)
+        ), self.settings(COOKIE_NAME_CHANGE_EXPAND_INFO=self.expand_settings):
+            self.cookie_name_change_middleware(self.mock_request)
 
-        assert expand_settings["old_name"] not in mock_request.COOKIES.keys()
-        assert expand_settings["new_name"] in mock_request.COOKIES.keys()
-        assert mock_request.COOKIES[expand_settings["new_name"]] == new_value
-        assert mock_request.COOKIES == no_change_cookies
+        assert self.expand_settings["old_name"] not in self.mock_request.COOKIES.keys()
+        assert self.expand_settings["new_name"] in self.mock_request.COOKIES.keys()
+        assert self.mock_request.COOKIES[self.expand_settings["new_name"]] == new_value
+        assert self.mock_request.COOKIES == no_change_cookies
 
         # make sure response function is called once
-        mock_response.assert_called_once()
+        self.mock_response.assert_called_once()
 
     def test_does_nothing(self):
-        """Make sure turning off toggle turns off middleware"""
+        """Make sure turning off toggle turns off self.cookie_name_change_middleware"""
 
-        expand_settings = {
-            "old_name": "a",
-            "new_name": "b",
-            "old_domain": "old_domain.com",
-        }
-
-        mock_response = Mock()
-        middleware = CookieNameChange(mock_response)
-
-        mock_request = Mock()
-
-        old_value = "." * 100
         new_value = "." * 13
         no_change_cookies = {
-            expand_settings['new_name']: new_value,
+            self.expand_settings['new_name']: new_value,
             "_c_": "." * 13,
             "a.b": "." * 10,
         }
-        old_dict = {
-            "a": old_value,
-        }
-        old_dict.update(no_change_cookies)
+        self.old_dict.update(no_change_cookies)
 
-        mock_request.COOKIES = old_dict.copy()
+        self.mock_request.COOKIES = self.old_dict.copy()
 
 
         with self.settings(
             COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE=False
-        ), self.settings(COOKIE_NAME_CHANGE_EXPAND_INFO=expand_settings):
-            middleware(mock_request)
+        ), self.settings(COOKIE_NAME_CHANGE_EXPAND_INFO=self.expand_settings):
+            self.cookie_name_change_middleware(self.mock_request)
 
-        assert mock_request.COOKIES == old_dict
+        assert self.mock_request.COOKIES == self.old_dict
 
         # make sure response function is called once
-        mock_response.assert_called_once()
+        self.mock_response.assert_called_once()
