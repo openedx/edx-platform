fc8e62f938fb8e4333dbfbe67775d25319f9ef9b
Thomas Tracy <contact@tjtracy.com>
2021-08-13 11:54:31 -0400

    WIP 2: move code to studio

--- cms/djangoapps/contentstore/signals/signals.py
+++ cms/djangoapps/contentstore/signals/signals.py
@@ -3,6 +3,11 @@ Contentstore signals
 """
 
 
+import logging
+from django.dispatch.dispatcher import receiver
+from openedx.core.djangoapps.content.course_overviews.signals import COURSE_PACING_CHANGED
+
+log = logging.getLogger(__name__)
 from django.dispatch import Signal
 
 # Signal that indicates that a course grading policy has been updated.
@@ -14,3 +19,14 @@ GRADING_POLICY_CHANGED = Signal(
         'course_key',  # Unicode string representing the course
     ]
 )
+
+
+
+@receiver(COURSE_PACING_CHANGED, dispatch_uid="update_cert_settings_on_pacing_change")
+def _update_cert_settings_on_pacing_change(sender, updated_course_overview, **kwargs):  # pylint: disable=unused-argument
+    """
+    Catches the signal that course pacing has changed and enable/disable
+    the self-generated certificates according to course-pacing.
+    """
+    log.info('This reciever will now work')
+    import pdb;pdb.set_trace()
