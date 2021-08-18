b7085c2cf9ec10abe29719cbfcf57f0bf91c5230
Justin Hynes <jhynes@edx.org>
2021-08-18 10:43:47 -0400

    refactor!: Remove the `request_certificate` REST endpoint (#28346)
    [MICROBA-1412]
    [DEPR-155]
    
    Remove the request_certificate REST endpoint from the LMS (certificates Django app)

--- lms/djangoapps/certificates/views/__init__.py
+++ lms/djangoapps/certificates/views/__init__.py
@@ -3,4 +3,3 @@ Aggregate all views exposed by the certificates app.
 """
 from lms.djangoapps.certificates.views.support import *
 from lms.djangoapps.certificates.views.webview import *
-from lms.djangoapps.certificates.views.xqueue import *
