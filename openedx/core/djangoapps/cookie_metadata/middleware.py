5d6f163a0b65002b0a75dc478916e238485aa36f
Manjinder Singh <49171515+jinder1s@users.noreply.github.com>
2021-08-25 12:43:21 -0400

    feat: Adding new CookieNameChange middleware (#28404)
    Description: Adds a new middleware to help with cookie name changes. It uses the idea of expand and contract, where after we've changed the name, the middleware allows up to accept either a cookie with new name (given higher priority when both are present) or cookie with old name.
    
    This is also helpful when changing domain of a cookie.
    
    impacts: developers, users(anyone that has cookies)
    Change depends on django setting changes. See CookieNameChange middleware for more info.

---
+++ openedx/core/djangoapps/cookie_metadata/middleware.py
@@ -0,0 +1,58 @@
+"""Middleware to change name of an incoming cookie"""
+from django.conf import settings
+
+
+class CookieNameChange:
+    """Changes name of an incoming cookie"""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """
+        Changes the names of a cookie in request.COOKIES
+
+        For this middleware to run:
+        - set COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE =  True
+        - COOKIE_NAME_CHANGE_EXPAND_INFO is a dict and has following info:
+            "old_name": Previous name of cookie
+            "new_name": New name of cookie
+
+        Actions taken by middleware:
+            - will delete any cookie with "old_name"
+            - if create a new cookie with "new_name" and set its value to value of "old_name" cookie
+                + it will not modify cookie with "new_name" if it already exists in request.COOKIES
+        """
+
+        # .. toggle_name: COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE
+        # .. toggle_implementation: DjangoSetting
+        # .. toggle_default: False
+        # .. toggle_description: Used to enable CookieNameChange middleware which changes a cookie name in request.COOKIES
+        # .. toggle_warnings: This should be set at the same time you set COOKIE_NAME_CHANGE_EXPAND_INFO setting
+        # .. toggle_use_cases: temporary
+        # .. toggle_creation_date: 2021-08-04
+        # .. toggle_target_removal_date: 2021-10-01
+        # .. toggle_tickets: https://openedx.atlassian.net/browse/ARCHBOM-1872
+        if getattr(settings, "COOKIE_NAME_CHANGE_ACTIVATE_EXPAND_PHASE", False):
+            old_cookie_in_request = False
+            expand_settings = getattr(settings, "COOKIE_NAME_CHANGE_EXPAND_INFO", None)
+
+            if (
+                expand_settings is not None
+                and isinstance(expand_settings, dict)
+                and "new_name" in expand_settings
+                and "old_name" in expand_settings
+            ):
+                if expand_settings["old_name"] in request.COOKIES:
+                    old_cookie_in_request = True
+                    old_cookie_value = request.COOKIES[expand_settings["old_name"]]
+                    del request.COOKIES[expand_settings["old_name"]]
+
+                if (
+                    expand_settings["new_name"] not in request.COOKIES
+                    and old_cookie_in_request
+                ):
+                    request.COOKIES[expand_settings["new_name"]] = old_cookie_value
+
+        response = self.get_response(request)
+        return response
