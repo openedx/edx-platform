389bc0d35d4b816cbee31dcf1a80e12af4fdebd6
Jawayria <jawayriahashmi@gmail.com>
2021-09-01 13:53:33 +0500

    chore: Remove django.utils.lru_cache

--- openedx/core/djangoapps/content_libraries/library_bundle.py
+++ openedx/core/djangoapps/content_libraries/library_bundle.py
@@ -5,7 +5,7 @@ Helper code for working with Blockstore bundles that contain OLX
 import dateutil.parser
 import logging  # lint-amnesty, pylint: disable=wrong-import-order
 
-from django.utils.lru_cache import lru_cache
+from functools import lru_cache
 from opaque_keys.edx.locator import BundleDefinitionLocator, LibraryUsageLocatorV2
 from xblock.core import XBlock
 from xblock.plugin import PluginMissingError
