442f497243da4e7ea2214b20277ae839154696a0
Chris Deery <3932645+cdeery@users.noreply.github.com>
2021-09-03 09:52:39 -0400

    fix: [AA-950] Fix gating feature from WaffleFlag
    Extracted enabled flag to variable

--- openedx/core/djangoapps/courseware_api/utils.py
+++ openedx/core/djangoapps/courseware_api/utils.py
@@ -36,8 +36,10 @@ def get_celebrations_dict(user, enrollment, course, browser_timezone):
     if streak_length_to_celebrate:
         # We only want to offer the streak discount
         # if the course has not ended, is upgradeable and the user is not an enterprise learner
-        if can_show_streak_discount_coupon(user, course) and COURSEWARE_MFE_MILESTONES_STREAK_DISCOUNT.is_enabled():
-            celebrations['streak_discount_enabled'] = True
+        discount_enabled = COURSEWARE_MFE_MILESTONES_STREAK_DISCOUNT.is_enabled()
+
+        if can_show_streak_discount_coupon(user, course) and discount_enabled:
+            celebrations['streak_discount_enabled'] = discount_enabled
 
             # Send course streak coupon event
             course_key = str(course.id)
