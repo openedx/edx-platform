c15016483150831275dd5c17d332118a83f0474d
Omar Al-Ithawi <i@omardo.com>
2021-09-04 21:02:52 +0300

    RED-2396: Refactor tokens deletion to filter by AMC app only
    Problem:
    AMC Access tokens are still being removed on password reset.
    
    Solution:
    This pull request simplifies the generic logic into AMC-specific logic
    which makes it easier to test and operate.
    
    Long term:
    This logic should be removed when using an Identity Provider since it
    won't be used.

--- openedx/core/djangoapps/appsembler/auth/oauth.py
+++ openedx/core/djangoapps/appsembler/auth/oauth.py
@@ -1,47 +1,31 @@
 """
-
+Tests for the OAuth helper.
 """
-from django.conf import settings
-from django.db.models import Subquery
 
-from oauth2_provider.models import (get_application_model,
-                                    AccessToken,
-                                    RefreshToken)
-
-from .models import TrustedApplication
+from oauth2_provider.models import (
+    AccessToken,
+    RefreshToken,
+)
 
 
-def destroy_oauth_tokens(user):
+from openedx.core.djangoapps.appsembler.sites.utils import get_amc_oauth_app
+
+
+def destroy_oauth_tokens_excluding_amc_tokens(user):
     """
-    Destroys OAuth access and refresh tokens for the given user
+    Destroys OAuth access and refresh tokens for the given user.
 
     All OAuth access and refresh tokens should be destroyed unless the setting,
-    'KEEP_TRUSTED_CONFIDENTIAL_CLIENT_TOKENS' is `True`. If it is `True` then
-    trusted application tokens should be preserved. These are tracked in the
-    `openedx.core.djangoapps.appsembler.auth.models.TrustedApplication` model
+    'KEEP_AMC_TOKENS_ON_PASSWORD_RESET' is `True`. If it is `True` then
+    AMC Application access and refresh tokens should be preserved.
     """
     dot_access_query = AccessToken.objects.filter(user=user.id)
     dot_refresh_query = RefreshToken.objects.filter(user=user.id)
 
-    Application = get_application_model()
+    dot_access_query = dot_access_query.exclude(
+        application=get_amc_oauth_app())
+    dot_refresh_query = dot_refresh_query.exclude(
+        application=get_amc_oauth_app())
 
-    if settings.FEATURES.get('KEEP_TRUSTED_CONFIDENTIAL_CLIENT_TOKENS', False):
-        # Appsembler: Avoid deleting the trusted confidential applications such
-        # as the Appsembler Management Console
-        trusted_applications = Application.objects.filter(
-            client_type=Application.CLIENT_CONFIDENTIAL,
-            pk__in=Subquery(TrustedApplication.objects.all().values('id')),
-        )
-
-        dot_access_query = dot_access_query.exclude(
-            application__in=trusted_applications)
-        dot_refresh_query = dot_refresh_query.exclude(
-            application__in=trusted_applications)
-
-    # This is a quick hack fix to work around the oauth2_provider migrations
-    # problem where refresh token has a non-nullable foreign key field to the
-    # access token table
-    # For more details see the PR:
-    # https://github.com/appsembler/edx-platform/pull/883
     dot_refresh_query.delete()
     dot_access_query.delete()
--- openedx/core/djangoapps/appsembler/auth/tests/test_oauth.py
+++ openedx/core/djangoapps/appsembler/auth/tests/test_oauth.py
@@ -1,5 +1,5 @@
-"""Test Appsembler Auth 'oauth' module
-
+"""
+Test Appsembler Auth 'oauth' module.
 """
 from mock import patch
 
@@ -12,8 +12,7 @@ from oauth2_provider.models import (get_application_model,
 
 from student.tests.factories import UserFactory
 
-from openedx.core.djangoapps.appsembler.auth.models import TrustedApplication
-from openedx.core.djangoapps.appsembler.auth.oauth import destroy_oauth_tokens
+from openedx.core.djangoapps.oauth_dispatch.api import destroy_oauth_tokens
 
 # there is also the following ApplicationFactory:
 # openeedx.core.djangoapps.api_admin.tests.factories.ApplicationFactory
@@ -26,8 +25,6 @@ from openedx.core.djangoapps.oauth_dispatch.tests.factories import (
     RefreshTokenFactory,
 )
 
-from .factories import TrustedApplicationFactory
-
 
 Application = get_application_model()
 
@@ -35,15 +32,22 @@ Application = get_application_model()
 class TestDestroyOAuthTokensHelper(TestCase):
     def setUp(self):
         super().setUp()
+        self.admin = UserFactory.create()
         self.user = UserFactory.create()
         self.application = ApplicationFactory(
+            user=self.admin,
+            client_id=settings.AMC_APP_OAUTH2_CLIENT_ID,
+        )
+
+        access_token = AccessTokenFactory.create(
             user=self.user,
-            client_type=Application.CLIENT_CONFIDENTIAL)
-        access_token = AccessTokenFactory.create(user=self.user,
-                                                 application=self.application)
-        RefreshTokenFactory.create(user=self.user,
-                                   application=self.application,
-                                   access_token=access_token)
+            application=self.application,
+        )
+        RefreshTokenFactory.create(
+            user=self.user,
+            application=self.application,
+            access_token=access_token,
+        )
 
     def assert_destroy_behaviour(self, should_be_kept, message):
         """
@@ -55,51 +59,32 @@ class TestDestroyOAuthTokensHelper(TestCase):
         assert should_be_kept == AccessToken.objects.count(), message
         assert should_be_kept == RefreshToken.objects.count(), message
 
-    @patch.dict(settings.FEATURES, {'KEEP_TRUSTED_CONFIDENTIAL_CLIENT_TOKENS': False})
-    def test_confidential_trusted_client_feature_disabled(self):
+    @patch.dict(settings.FEATURES, {'KEEP_AMC_TOKENS_ON_PASSWORD_RESET': False})
+    def test_keep_tokens_feature_disabled(self):
         """
         Tokens that have a confidential TrustedClient should be removed if the
         feature is disabled
         """
-        TrustedApplicationFactory.create(application=self.application)
         self.assert_destroy_behaviour(
             should_be_kept=False,
-            message=('Tokens of a trusted confidential client should be '
-                     'deleted if the feature is disabled'),
+            message=('Tokens of a AMC application should be deleted if the feature is disabled'),
         )
 
-    @patch.dict(settings.FEATURES, {'KEEP_TRUSTED_CONFIDENTIAL_CLIENT_TOKENS': True})
-    def test_confidential_trusted_client(self):
+    @patch.dict(settings.FEATURES, {'KEEP_AMC_TOKENS_ON_PASSWORD_RESET': True})
+    def test_keep_tokens_feature_enabled(self):
         """
         Tokens that have a confidential TrustedClient shouldn't be removed.
         """
-        TrustedApplicationFactory.create(application=self.application)
         self.assert_destroy_behaviour(
             should_be_kept=True,
-            message='Tokens of a trusted confidential client should be kept',
+            message='Tokens of a AMC application should be kept',
         )
 
-    @patch.dict(settings.FEATURES, {'KEEP_TRUSTED_CONFIDENTIAL_CLIENT_TOKENS': True})
-    def test_no_trusted_application(self):
+    @patch.dict(settings.FEATURES, {'KEEP_AMC_TOKENS_ON_PASSWORD_RESET': True})
+    def test_no_amc_application_should_raise_exception(self):
         """
-        Only tokens that don't have a TrustedClient should be removed.
+        This feature depends on AMC Application to be configured.
         """
-        # Sanity check, there should not be a client
-        assert not TrustedApplication.objects.count()
-        self.assert_destroy_behaviour(
-            should_be_kept=False,
-            message='Tokens of an untrusted client should be deleted',
-        )
-
-    @patch.dict(settings.FEATURES, {'KEEP_TRUSTED_CONFIDENTIAL_CLIENT_TOKENS': True})
-    def test_public_trusted_client(self):
-        """
-        Tokens for public clients are removed, even if they're trusted.
-        """
-        self.application.client_type = Application.CLIENT_PUBLIC
-        self.application.save()
-        TrustedApplicationFactory.create(application=self.application)
-        self.assert_destroy_behaviour(
-            should_be_kept=False,
-            message='Tokens of a public trusted client should be deleted',
-        )
+        self.application.delete()
+        with self.assertRaises(Application.DoesNotExist):  # The feature expects an AMC app to be configured.
+            destroy_oauth_tokens(self.user)
