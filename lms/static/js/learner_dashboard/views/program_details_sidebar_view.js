915226b211f10ffeaedc8a777ed0e68876fcae56
Ahtisham Shahid <ahtisham300@gmail.com>
2021-09-09 20:01:50 +0500

    fix: updated sidebar code for flag

--- lms/static/js/learner_dashboard/views/program_details_sidebar_view.js
+++ lms/static/js/learner_dashboard/views/program_details_sidebar_view.js
@@ -29,6 +29,7 @@ class ProgramDetailsSidebarView extends Backbone.View {
     this.industryPathways = options.industryPathways;
     this.creditPathways = options.creditPathways;
     this.programModel = options.model;
+    this.programDiscussionEnabled =  options.programDiscussionEnabled;
     this.render();
   }
 
