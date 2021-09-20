918243084feac250daaaa122cf363c400fedf43a
pedrovgp <pedrovgp@gmail.com>
2021-09-20 18:08:17 -0300

    ADD esme custom css as separate file
    (cherry picked from commit f53c64d224c53b68d3443df467016f534b980db2)

--- common/lib/xmodule/xmodule/html_module.py
+++ common/lib/xmodule/xmodule/html_module.py
@@ -144,7 +144,12 @@ class HtmlBlockMixin(  # lint-amnesty, pylint: disable=abstract-method
         ],
         'xmodule_js': resource_string(__name__, 'js/src/xmodule.js'),
     }
-    preview_view_css = {'scss': [resource_string(__name__, 'css/html/display.scss')]}
+    preview_view_css = {
+        'scss': [
+            resource_string(__name__, 'css/html/display.scss'),
+            resource_string(__name__, 'css/html/esme_custom.scss'),
+        ]
+    }
 
     uses_xmodule_styles_setup = True
     requires_per_student_anonymous_id = True
