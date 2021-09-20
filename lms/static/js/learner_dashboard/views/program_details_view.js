991f24488624cd49a7dfd414cf757bbb86e9fee7
Asad <asadazam93@gmail.com>
2021-09-20 13:18:29 +0500

    Fixed conflicts

--- lms/static/js/learner_dashboard/views/program_details_view.js
+++ lms/static/js/learner_dashboard/views/program_details_view.js
@@ -80,12 +80,9 @@ class ProgramDetailsView extends Backbone.View {
       completedCount,
       completeProgramURL: buyButtonUrl,
       programDiscussionEnabled: this.options.programDiscussionEnabled,
-<<<<<<< HEAD
-      industryPathways : this.options.industryPathways,
-      creditPathways : this.options.creditPathways,
-=======
+      industryPathways: this.options.industryPathways,
+      creditPathways: this.options.creditPathways,
       discussionFragment: this.options.discussionFragment,
->>>>>>> e7084d955d... Refactored code
     };
     data = $.extend(data, this.programModel.toJSON());
     HtmlUtils.setHtml(this.$el, this.tpl(data));
