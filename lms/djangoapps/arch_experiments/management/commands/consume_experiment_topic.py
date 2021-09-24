88c2b49e58194dba3d49ee8b40d9ce8aa63f561c
Feanil Patel <feanil@edx.org>
2021-09-24 11:13:46 -0400

    temp: Use a custom schema class for pulsar messages.
    Instead of using the built-in record classes, use a custom schema class
    that let's us have better controll of the avro schema by defining it
    explicitly rather than implicitly via Record and Field declerations.
    
    Using `Record` classes was not super useful because it was not possible
    to create Optional or Required fields that had default values other than
    None.

--- lms/djangoapps/arch_experiments/management/commands/consume_experiment_topic.py
+++ lms/djangoapps/arch_experiments/management/commands/consume_experiment_topic.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from lms.djangoapps.arch_experiments.signals.handlers import ARCH_EXPERIMENTS_TOPIC
+from lms.djangoapps.arch_experiments.signals.handlers import ARCH_EXPERIMENTS_TOPIC, UnenrollSchema
 import pulsar
 
 from lms.djangoapps.arch_experiments.signals.handlers import UnenrollMessage
@@ -9,7 +9,7 @@ ARCH_EXPERIMENTS_CONSUMER = settings.PULSAR_CLIENT.subscribe(
     ARCH_EXPERIMENTS_TOPIC,
     settings.CONSUMER_GROUP_NAME,
     consumer_type=pulsar.ConsumerType.Shared,
-    schema=pulsar.schema.AvroSchema(UnenrollMessage),
+    schema=UnenrollSchema()
 )
 
 
@@ -20,9 +20,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Starting to consume messages...")
         while True:
-            msg = ARCH_EXPERIMENTS_CONSUMER.receive()
+            try:
+                msg = ARCH_EXPERIMENTS_CONSUMER.receive(500)
+            except Exception as e:
+                if (len(e.args) == 1) and ("TimeOut" in e.args[0]):
+                    continue
+                else:
+                    raise
             try:
                 print("Received message '{}' id='{}'".format(msg.data(), msg.message_id()))
+                print(f"Type: '{type(msg.value())}', Content: '{msg.value()}'")
                 # Acknowledge successful processing of the message
                 ARCH_EXPERIMENTS_CONSUMER.acknowledge(msg)
             except:
--- lms/djangoapps/arch_experiments/signals/handlers.py
+++ lms/djangoapps/arch_experiments/signals/handlers.py
@@ -5,7 +5,8 @@ from django.conf import settings
 import json
 from pulsar.schema import *
 import _pulsar
-
+from fastavro import schemaless_reader, schemaless_writer, parse_schema
+import io
 
 ARCH_EXPERIMENTS_TOPIC = "arch_experiment_topic"
 
@@ -17,44 +18,51 @@ class UnenrollMessage(Record):
 
 
 class UnenrollSchema(Schema):
-    json_schema_definition = {
-        "$id": "https://edx.org/unenroll.schema.json",
-        "$schema": "https://json-schema.org/draft/2020-12/schema",
-        "title": "Unenroll",
-        "type": "object",
-        "properties": {
-            "user_id": {"type": "integer", "description": "The numeric id for the impacted user."},
-            "course": {"type": "string", "description": "The course identifier."},
-            "mode": {"type": "string", "description": "The mode they were enrolled in."},
-        },
-    }
+    # Pulsar can't actually consume a JSON schema definition.
+    #    json_schema_definition = {
+    #        "$id": "https://edx.org/unenroll.schema.json",
+    #        "$schema": "https://json-schema.org/draft/2020-12/schema",
+    #        "title": "Unenroll",
+    #        "type": "object",
+    #        "properties": {
+    #            "user_id": {"type": "integer", "description": "The numeric id for the impacted user."},
+    #            "course": {"type": "string", "description": "The course identifier."},
+    #            "mode": {"type": "string", "description": "The mode they were enrolled in."},
+    #        },
+    #    }
 
     avro_schema_definition = {
-        "name": "Unenroll",
+        "name": "UnenrollMsg2",
         "type": "record",
         "fields": [
             {"name": "course", "type": "string"},
             {"name": "mode", "type": "string"},
             {"name": "user_id", "type": "int"},
-#            {"name": "test_required", "type": "int", "default": 9}, # Added afterwards, required with default value.
-#            {"name": "test_required2", "type": "int", "default": 9}, # Added afterwards, required with default value.
-#            {"name": "test_optional", "type": ["int", "null"], "default": 9},
-#            {"name": "test_optional2", "type": ["int", "null"], "default": 9}, # Added after the fact
-#            {"name": "test_optional3", "type": ["int", "null"], "default": 9}, # Added after the fact
+            # {"name": "test_required", "type": "int", "default": 9}, # Added afterwards, required with default value.
+            # {"name": "test_required2", "type": "int", "default": 9}, # Added afterwards, required with default value.
+            # {"name": "test_optional", "type": ["int", "null"], "default": 9},
+            # {"name": "test_optional2", "type": ["int", "null"], "default": 9}, # Added after the fact
+            # {"name": "test_optional3", "type": ["int", "null"], "default": 9}, # Added after the fact
         ],
     }
 
     def __init__(self):
         # super().__init__(None, _pulsar.SchemaType.JSON, self.json_schema_definition, 'JSON')
+        # super().__init__(None, _pulsar.SchemaType.JSON, self.avro_schema_definition, "JSON")
         super().__init__(None, _pulsar.SchemaType.AVRO, self.avro_schema_definition, "AVRO")
+        self.parsed_schema = parse_schema(self.avro_schema_definition)
 
     def encode(self, obj):
-        # TODO: Validate against schema.
-        return json.dumps(obj, indent=True).encode("utf-8")
+        # Validate against schema.
+        bytes_buffer = io.BytesIO()
+        schemaless_writer(bytes_buffer, self.parsed_schema, obj)
+        return bytes_buffer.getvalue()
 
     def decode(self, data):
         # TODO: Validate against schema
-        return json.loads(data)
+        bytes_buffer = io.BytesIO(data)
+        d = schemaless_reader(bytes_buffer, self.parsed_schema)
+        return d
 
     def _validate_object_type(self, obj):
         return isinstance(obj, dict)
@@ -62,8 +70,9 @@ class UnenrollSchema(Schema):
 
 ARCH_EXPERIMENTS_PRODUCER = settings.PULSAR_CLIENT.create_producer(
     # ARCH_EXPERIMENTS_TOPIC, schema=AvroSchema(UnenrollMessage)
-#    ARCH_EXPERIMENTS_TOPIC, schema=JsonSchema(UnenrollMessage)
-    ARCH_EXPERIMENTS_TOPIC, schema=UnenrollSchema(),
+    #    ARCH_EXPERIMENTS_TOPIC, schema=JsonSchema(UnenrollMessage)
+    ARCH_EXPERIMENTS_TOPIC,
+    schema=UnenrollSchema(),
 )
 
 
