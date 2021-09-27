e9e9d30be1f60160dbc21d2ac0ce9803a6ab9c4f
Soban Javed <iamsobanjaved@gmail.com>
2021-09-27 16:27:27 +0500

    fix: stop django 3.2 from picking CoursegraphConfig from coursegraph app
    In Django 3.2, app configuration is automatically selected from apps.py submodule in case of single config class in apps.py module, to disable this feature we need to set `default = False` in AppConfig of that app
    
    https://docs.djangoproject.com/en/3.2/ref/applications/#configuring-applications

--- openedx/core/djangoapps/coursegraph/apps.py
+++ openedx/core/djangoapps/coursegraph/apps.py
@@ -14,4 +14,10 @@ class CoursegraphConfig(AppConfig):
     """
     name = 'openedx.core.djangoapps.coursegraph'
 
+    # In Django 3.2, app configuration is automatically selected from apps.py
+    # submodule in case of single config class in apps.py module, to disable
+    # this feature we need to set `default = False` in AppConfig of that app
+    # https://docs.djangoproject.com/en/3.2/ref/applications/#configuring-applications
+    default = False
+
     from openedx.core.djangoapps.coursegraph import tasks
