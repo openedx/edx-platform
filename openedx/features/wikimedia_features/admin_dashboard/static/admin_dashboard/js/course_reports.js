ecfd7c41e88cc3088977c2a70f5e6177044c9485
arbirali <rahat.ali@arbisoft.com>
2021-09-27 12:27:41 +0500

    Admin dashboard styled and front end implementation

--- openedx/features/wikimedia_features/admin_dashboard/static/admin_dashboard/js/course_reports.js
+++ openedx/features/wikimedia_features/admin_dashboard/static/admin_dashboard/js/course_reports.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    let getCookie, ReportDownloads, AjaxCall, toggleDisplay;
+    let getCookie, ReportDownloads, AjaxCall;
     let course_name = $('.filter-selectbox').children()[0].value;
     let endpoint =  '/courses/' + course_name + '/instructor/api/list_report_downloads';
 
@@ -20,22 +20,22 @@
     };
 
     ReportDownloads = function(){
+        $('.reports-table').show();
         $.ajax({
             type: 'POST',
             dataType: 'json',
             url: endpoint,
             success: function(data) {
+                $('.download-section').show();
                 if (data.downloads.length && $('#report-downloads-table').find('a').length == 0){
                     for (let i = 0; i < data.downloads.length; i++) {
-                        $('#report-downloads-table').append(data.downloads[i]['link']);
-                        $('#report-downloads-table').append('<br>');
+                        $('#report-downloads-table').append('<tr><td>'+data.downloads[i]['link']+'</td></tr>');
                     }
                 }
                 else if (data.downloads.length > $('#report-downloads-table').find('a').length){
                     let newReportsFetched = data.downloads.length - $('#report-downloads-table').find('a').length;
                     for (let i = newReportsFetched; i > 0; i--) {
-                        $('#report-downloads-table').prepend('<br>');
-                        $('#report-downloads-table').prepend(data.downloads[i-1]['link']);
+                        $('#report-downloads-table').prepend('<tr><td>'+data.downloads[i-1]['link']+'</td></tr>');
                     }
                 }
             },
@@ -68,32 +68,27 @@
 
     setInterval(function() {
         ReportDownloads();
-    }, 20000);
-    
-    toggleDisplay = function(list_of_elements, display_type){
-        for (let i = 0; i < list_of_elements.length; i++){
-            list_of_elements[i].style.display = display_type;
-        }
-    }
+    }, 5000);
+
+    $('#select-courses').select2({
+        placeholder: "Browse Courses",
+    });
 
     $('select').change(function (e) {
         e.preventDefault();
-        $('#report-request-response').empty();
-        $('#report-request-response-error').empty();
-        $('#report-downloads-table').empty();
+        $('#report-request-response,#report-request-response-error,#report-downloads-table').empty();
+        $('.reports-table').hide();
         let display_type;
         let list_of_elements = $('.single-course-report');
         if ($(this).val())
         {
             if ($(this).val().length> 1) {
                 course_name = $(this).val().toString();
-                display_type = "none"
-                toggleDisplay(list_of_elements, display_type);                
+                list_of_elements.hide();
             }
             else {
                 course_name = $(this).val()[0];
-                display_type = "block";
-                toggleDisplay(list_of_elements, display_type);
+                list_of_elements.show();
             }
             endpoint = `/courses/${$(this).val()[0]}/instructor/api/list_report_downloads`;
         }
@@ -103,32 +98,32 @@
         }
     });
 
-    $("input[name='list-profiles-csv']").click(function() {
+    $("[name='list-profiles-csv']").click(function() {
         let url_for_list_profiles_csv = '/courses/' + course_name + '/instructor/api/get_students_features' + '/csv';
         AjaxCall(url_for_list_profiles_csv);
     });
 
-    $("input[name='calculate-grades-csv']").click(function() {
+    $("[name='calculate-grades-csv']").click(function() {
         let url_for_calculate_grades = '/courses/' + course_name + '/instructor/api/calculate_grades_csv';
         AjaxCall(url_for_calculate_grades);
     });
 
-    $("input[name='list-anon-ids']").click(function() {
+    $("[name='list-anon-ids']").click(function() {
         let url_for_list_anon_ids = '/courses/' + course_name + '/instructor/api/get_anon_ids';
         AjaxCall(url_for_list_anon_ids);
     });
 
-    $("input[name='problem-grade-report']").click(function() {
+    $("[name='problem-grade-report']").click(function() {
         let url_for_problem_grade_report = '/courses/' + course_name + '/instructor/api/problem_grade_report';
         AjaxCall(url_for_problem_grade_report);
     });
 
-    $("input[name='list-may-enroll-csv']").click(function() {
+    $("[name='list-may-enroll-csv']").click(function() {
         let url_for_list_may_enroll = '/courses/' + course_name + '/instructor/api/get_students_who_may_enroll';
         AjaxCall(url_for_list_may_enroll);
     });
 
-    $("input[name='average-calculate-grades-csv']").click(function() {
+    $("[name='average-calculate-grades-csv']").click(function() {
         let url_for_average_calculate_grades = '/admin_dashboard/average_calculate_grades_csv/' + course_name ;
         AjaxCall(url_for_average_calculate_grades);
     })
