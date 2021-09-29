b587dfa69688ed4b7d76c5230890fd8fce774f58
Nathan Sprenkle <nsprenkle@edx.org>
2021-09-29 14:36:29 -0400

    refactor: move xblock json handler to utils

---
+++ lms/djangoapps/ora_staff_grader/utils.py
@@ -0,0 +1,36 @@
+"""
+Various helpful utilities for ESG
+"""
+import json
+
+from opaque_keys.edx.keys import UsageKey
+from rest_framework.request import clone_request
+
+from lms.djangoapps.courseware.module_render import handle_xblock_callback
+
+
+def call_xblock_json_handler(request, usage_id, handler_name, data={}):
+    """
+    Create an internally-routed XBlock.json_handler request.
+    The internal auth code/param unpacking requires a POST request with payload in the body.
+
+    params:
+        request (HttpRequest): Originating web request, we're going to borrow auth headers/cookies from this
+        usage_id (str): Usage ID of the XBlock for running the handler
+        handler_name (str): the name of the XBlock handler method
+        data (dict): Data to be encoded and sent as the body of the POST request
+    """
+    # XBlock.json_handler operates through a POST request
+    proxy_request = clone_request(request, "POST")
+    proxy_request.META["REQUEST_METHOD"] = "POST"
+
+    # The body is an encoded JSON blob
+    proxy_request.body = json.dumps(data).encode()
+
+    # Course ID can be retrieved from the usage_id
+    usage_key = UsageKey.from_string(usage_id)
+    course_id = str(usage_key.course_key)
+
+    # Send and decode the request
+    response = handle_xblock_callback(proxy_request, course_id, usage_id, handler_name)
+    return json.loads(response.content)
--- lms/djangoapps/ora_staff_grader/views.py
+++ lms/djangoapps/ora_staff_grader/views.py
@@ -1,23 +1,20 @@
 """
 Views for Enhanced Staff Grader
 """
-import json
-
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 from edx_rest_framework_extensions.auth.session.authentication import SessionAuthenticationAllowInactiveUser
 from opaque_keys.edx.keys import UsageKey
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.request import clone_request
 from xmodule.modulestore.django import modulestore
 
-from lms.djangoapps.courseware.module_render import handle_xblock_callback
 from lms.djangoapps.ora_staff_grader.serializers import (
     CourseMetadataSerializer,
     OpenResponseMetadataSerializer,
     SubmissionMetadataSerializer
 )
+from lms.djangoapps.ora_staff_grader.utils import call_xblock_json_handler
 from openedx.core.djangoapps.content.course_overviews.api import get_course_overview_or_none
 from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
 
@@ -77,7 +74,7 @@ class InitializeView(RetrieveAPIView):
         """
         Get a list of submissions from the ORA's 'list_staff_workflows' XBlock.json_handler
         """
-        return self._call_xblock_json_handler(request, usage_id, 'list_staff_workflows')
+        return call_xblock_json_handler(request, usage_id, 'list_staff_workflows')
 
     def get_rubric_config(self, request, usage_id):
         """
@@ -86,30 +83,4 @@ class InitializeView(RetrieveAPIView):
         data = {
             'target_rubric_block_id': usage_id
         }
-        return self._call_xblock_json_handler(request, usage_id, 'get_rubric', data)
-
-    def _call_xblock_json_handler(self, request, usage_id, handler_name, data={}):
-        """
-        Create an internally-routed XBlock.json_handler request.
-        The internal auth code/param unpacking requires a POST request with payload in the body.
-
-        params:
-            request (HttpRequest): Originating web request, we're going to borrow auth headers/cookies from this
-            usage_id (str): Usage ID of the XBlock for running the handler
-            handler_name (str): the name of the XBlock handler method
-            data (dict): Data to be encoded and sent as the body of the POST request
-        """
-        # XBlock.json_handler operates through a POST request
-        proxy_request = clone_request(request, "POST")
-        proxy_request.META["REQUEST_METHOD"] = "POST"
-
-        # The body is an encoded JSON blob
-        proxy_request.body = json.dumps(data).encode()
-
-        # Course ID can be retrieved from the usage_id
-        usage_key = UsageKey.from_string(usage_id)
-        course_id = str(usage_key.course_key)
-
-        # Send and decode the request
-        response = handle_xblock_callback(proxy_request, course_id, usage_id, handler_name)
-        return json.loads(response.content)
+        return call_xblock_json_handler(request, usage_id, 'get_rubric', data)
