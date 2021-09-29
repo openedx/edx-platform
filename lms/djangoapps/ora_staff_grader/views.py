9d2174e02edb7db7dbf8f3c98dd3d70956744c15
Nathan Sprenkle <nsprenkle@edx.org>
2021-09-29 14:29:14 -0400

    feat: add rubric config to init return

--- lms/djangoapps/ora_staff_grader/views.py
+++ lms/djangoapps/ora_staff_grader/views.py
@@ -57,8 +57,11 @@ class InitializeView(RetrieveAPIView):
         course_id = str(ora_usage_key.course_key)
         course_metadata = get_course_overview_or_none(course_id)
 
-        # Get list of submissions for the ORA
-        submissions_metadata = self.get_submissions(request, course_id, ora_location)
+        # Get list of submissions for this ORA
+        submissions_metadata = self.get_submissions(request, ora_location)
+
+        # Get the rubric config for this ORA
+        rubric_config = self.get_rubric_config(request, ora_location)
 
         return Response({
             'courseMetadata': CourseMetadataSerializer(course_metadata).data,
@@ -67,20 +70,46 @@ class InitializeView(RetrieveAPIView):
                 submission_id: SubmissionMetadataSerializer(submission).data
                 for (submission_id, submission) in submissions_metadata.items()
             },
+            'rubricConfig': rubric_config,
         })
 
-    def get_submissions(self, request, course_id, usage_id):
+    def get_submissions(self, request, usage_id):
         """
-        Create an XBlock handler request (routed internally) to get a list of submissions (staff workflows)
+        Get a list of submissions from the ORA's 'list_staff_workflows' XBlock.json_handler
         """
-        # Normally an XBlock.json_handler is routed through a POST request.
-        # We have to pass along a fake POST request to work with the handler auth/routing tooling.
+        return self._call_xblock_json_handler(request, usage_id, 'list_staff_workflows')
+
+    def get_rubric_config(self, request, usage_id):
+        """
+        Get rubric data from the ORA's 'get_rubric' XBlock.json_handler
+        """
+        data = {
+            'target_rubric_block_id': usage_id
+        }
+        return self._call_xblock_json_handler(request, usage_id, 'get_rubric', data)
+
+    def _call_xblock_json_handler(self, request, usage_id, handler_name, data={}):
+        """
+        Create an internally-routed XBlock.json_handler request.
+        The internal auth code/param unpacking requires a POST request with payload in the body.
+
+        params:
+            request (HttpRequest): Originating web request, we're going to borrow auth headers/cookies from this
+            usage_id (str): Usage ID of the XBlock for running the handler
+            handler_name (str): the name of the XBlock handler method
+            data (dict): Data to be encoded and sent as the body of the POST request
+        """
+        # XBlock.json_handler operates through a POST request
         proxy_request = clone_request(request, "POST")
         proxy_request.META["REQUEST_METHOD"] = "POST"
 
-        # Here is where we'd set the body, but it's empty for our use case.
-        proxy_request.body = b'{}'
+        # The body is an encoded JSON blob
+        proxy_request.body = json.dumps(data).encode()
+
+        # Course ID can be retrieved from the usage_id
+        usage_key = UsageKey.from_string(usage_id)
+        course_id = str(usage_key.course_key)
 
         # Send and decode the request
-        response = handle_xblock_callback(proxy_request, course_id, usage_id, 'list_staff_workflows')
+        response = handle_xblock_callback(proxy_request, course_id, usage_id, handler_name)
         return json.loads(response.content)
