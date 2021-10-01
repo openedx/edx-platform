872de99accdac6dc6d0de2da3ff73edefb209407
Awais Jibran <awaisdar001@gmail.com>
2021-10-01 16:26:14 +0500

    fix: textbooks should be enabled if course has any textbooks enabled. (#28887)
    fix: proctoring settings to show pop up modal
    fix: custom pages to be "disabled" by defualt
    fix: only build lagacy link when app sends any link

--- cms/djangoapps/contentstore/utils.py
+++ cms/djangoapps/contentstore/utils.py
@@ -188,13 +188,12 @@ def get_proctored_exam_settings_url(course_locator) -> str:
     proctored_exam_settings_url = ''
     if exam_setting_view_enabled():
         mfe_base_url = get_course_authoring_url(course_locator)
+        course_mfe_url = f'{mfe_base_url}/course/{course_locator}'
         if mfe_base_url:
             if proctoring_settings_modal_view_enabled(course_locator):
-                proctored_exam_settings_url = (
-                    f'{mfe_base_url}/course/{course_locator}/pages-and-resources/proctoring/settings'
-                )
+                proctored_exam_settings_url = f'{course_mfe_url}/pages-and-resources/proctoring/settings'
             else:
-                proctored_exam_settings_url = f'{mfe_base_url}/course/{course_locator}/proctored-exam-settings'
+                proctored_exam_settings_url = f'{course_mfe_url}/proctored-exam-settings'
     return proctored_exam_settings_url
 
 
