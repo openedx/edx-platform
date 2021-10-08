23c94b7ec582f3a914b79f60c76a10960b2afb00
Mubbshar Anwar <78487564+mubbsharanwar@users.noreply.github.com>
2021-10-08 22:39:54 +0500

    fix: add opt in/out event (#28985)
    add opt in/out event for Braze.
    
    VAN-738

--- openedx/core/djangoapps/user_authn/views/register.py
+++ openedx/core/djangoapps/user_authn/views/register.py
@@ -382,6 +382,7 @@ def _track_user_registration(user, profile, params, third_party_provider, regist
             'total_registration_time': round(float(params.get('totalRegistrationTime', '0'))),
             'activation_key': registration.activation_key if registration else None,
         }
+        # VAN-738 - added below properties to experiment marketing emails opt in/out events on Braze.
         if params.get('marketing_emails_opt_in') and settings.MARKETING_EMAILS_OPT_IN:
             properties['marketing_emails_opt_in'] = params.get('marketing_emails_opt_in') == 'true'
             properties['is_active'] = params.get('marketing_emails_opt_in') == 'true'
