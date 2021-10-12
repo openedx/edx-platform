850c6158b3f0ef8c404613f07a1fad4588afd19b
Usama Sadiq <usama.sadiq@arbisoft.com>
2021-10-12 16:31:26 +0500

    fix: try install pip==21.2.4 in jenkins_env

--- scripts/jenkins-common.sh
+++ scripts/jenkins-common.sh
@@ -48,7 +48,7 @@ source $VENV_PATH/edx-venv/bin/activate
 # Hack to fix up egg-link files given that the virtualenv is not relocatable
 sed -i "s|^/home/jenkins/shallow-clone|`pwd`|" -- \
     $VENV_PATH/edx-venv/lib/python*/site-packages/*.egg-link
-pip --version
+pip install pip==21.2.4
 pip install -qr requirements/edx/pip-tools.txt
 pip-sync -q requirements/edx/testing.txt requirements/edx/django.txt
 
