f418886629ed0f112bb1f42101d709db18fc9f37
Nathan Sprenkle <nsprenkle@edx.org>
2021-10-18 17:37:02 -0400

    test: add init happy path

--- lms/djangoapps/ora_staff_grader/tests/test_views.py
+++ lms/djangoapps/ora_staff_grader/tests/test_views.py
@@ -4,24 +4,37 @@ Tests for ESG views
 from django.urls import reverse
 from opaque_keys.edx.keys import CourseKey
 from rest_framework.test import APITestCase
+from unittest.mock import patch
 
 from common.djangoapps.student.tests.factories import StaffFactory
-from openedx.core.djangoapps.catalog.tests.factories import CourseFactory
+from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
+from xmodule.modulestore.tests.django_utils import TEST_DATA_SPLIT_MODULESTORE, SharedModuleStoreTestCase
+from xmodule.modulestore.tests.factories import CourseFactory
+from xmodule.modulestore.tests.factories import ItemFactory
 
 
-class TestInitializeView(APITestCase):
+class TestInitializeView(SharedModuleStoreTestCase, APITestCase):
     """
     Tests for the /initialize view, creating setup data for ESG
     """
     view_name = 'ora-staff-grader:initialize'
     api_url = reverse(view_name)
 
+    MODULESTORE = TEST_DATA_SPLIT_MODULESTORE
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.course_key_str = 'course-v1:Hogwarts+Potions101+S1'
-        cls.course_key = CourseKey.from_string(cls.course_key_str)
-        cls.course = CourseFactory(key=cls.course_key_str)
+
+        cls.course = CourseFactory.create()
+        cls.course_key = cls.course.location.course_key
+
+        cls.ora_block = ItemFactory.create(
+            category='openassessment',
+            parent_location=cls.course.location,
+            display_name='test',
+        )
+        cls.ora_usage_key = str(cls.ora_block.location)
 
         cls.password = 'password'
         cls.staff = StaffFactory(course_key=cls.course_key, password=cls.password)
@@ -45,3 +58,38 @@ class TestInitializeView(APITestCase):
 
         assert response.status_code == 404
         assert response.content.decode() == "Invalid ora_location."
+
+    @patch('lms.djangoapps.ora_staff_grader.views.InitializeView.get_rubric_config')
+    @patch('lms.djangoapps.ora_staff_grader.views.InitializeView.get_submissions')
+    @patch('lms.djangoapps.ora_staff_grader.views.get_course_overview_or_none')
+    def test_init(self, mock_get_course_overview, mock_get_submissions, mock_get_rubric_config):
+        """ A successful call should return course, ORA, submissions, and rubric data """
+        mock_course_overview = CourseOverviewFactory.create()
+        mock_get_course_overview.return_value = mock_course_overview
+
+        mock_get_submissions.return_value = {
+            "a": {
+                "submissionUuid": "a",
+                "username": "foo",
+                "teamName": "",
+                "dateSubmitted": "1969-07-16 13:32:00",
+                "dateGraded": "None",
+                "gradedBy": "",
+                "gradingStatus": "ungraded",
+                "lockStatus": "unlocked",
+                "score": {
+                    "pointsEarned": 0,
+                    "pointsPossible": 10
+                }
+            }
+        }
+
+        # Rubric data is passed through directly, so we can use a toy data payload
+        mock_get_rubric_config.return_value = { "foo": "bar" }
+
+        self.client.login(username=self.staff.username, password=self.password)
+        response = self.client.get(self.api_url, {'ora_location': self.ora_usage_key})
+
+        expected_keys = set(['courseMetadata', 'oraMetadata', 'submissions', 'rubricConfig'])
+        assert response.status_code == 200
+        assert response.data.keys() == expected_keys
