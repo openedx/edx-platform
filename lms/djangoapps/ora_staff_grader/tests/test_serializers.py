0629b93c91a4b6806a6c6172e322066415a9c564
Nathan Sprenkle <nsprenkle@edx.org>
2021-10-20 12:18:11 -0400

    refactor: test data simplifications

--- lms/djangoapps/ora_staff_grader/tests/test_serializers.py
+++ lms/djangoapps/ora_staff_grader/tests/test_serializers.py
@@ -17,29 +17,27 @@ class TestCourseMetadataSerializer(SharedModuleStoreTestCase):
     """
     Tests for CourseMetadataSerializer
     """
-    course_org = "Oxford"
-    course_name = "Introduction to Time Travel"
-    course_number = "TT101"
-    course_run = "2054"
+    course_data = {
+        "org": "Oxford",
+        "display_name": "Introduction to Time Travel",
+        "display_number_with_default": "TT101",
+        "run": "2054",
+    }
+
     course_id = "course-v1:Oxford+TT101+2054"
 
     def setUp(self):
         super().setUp()
 
-        self.course_overview = CourseOverviewFactory.create(
-            org=self.course_org,
-            display_name=self.course_name,
-            display_number_with_default=self.course_number,
-            run=self.course_run,
-        )
+        self.course_overview = CourseOverviewFactory.create(**self.course_data)
 
     def test_course_serialize(self):
         data = CourseMetadataSerializer(self.course_overview).data
 
         assert data == {
-            "title": self.course_name,
-            "org": self.course_org,
-            "number": self.course_number,
+            "title": self.course_data["display_name"],
+            "org": self.course_data["org"],
+            "number": self.course_data["display_number_with_default"],
             "courseId": self.course_id,
         }
 
@@ -48,28 +46,25 @@ class TestOpenResponseMetadataSerializer(TestCase):
     """
     Tests for OpenResponseMetadataSerializer
     """
-    display_name = "Week 1: Time Travel Paradoxes"
-    prompts = ["<p>In your own words, explain a famous time travel paradox</p>"]
-    teams_enabled = False
-
-    mock_ora_instance = None
+    ora_data = {
+        "display_name": "Week 1: Time Travel Paradoxes",
+        "prompts": ["<p>In your own words, explain a famous time travel paradox</p>"],
+        "teams_enabled": False
+    }
 
     def setUp(self):
         super().setUp()
 
-        self.mock_ora_instance = Mock(name='openassessment-block')
-        self.mock_ora_instance.display_name = self.display_name
-        self.mock_ora_instance.prompts = self.prompts
-        self.mock_ora_instance.teams_enabled = self.teams_enabled
+        self.mock_ora_instance = Mock(name='openassessment-block', **self.ora_data)
 
     def test_individual_ora(self):
         # An ORA with teams disabled should have type "individual"
         data = OpenResponseMetadataSerializer(self.mock_ora_instance).data
 
         assert data == {
-            "name": self.display_name,
-            "prompts": self.prompts,
-            "type": "individual"
+            "name": self.ora_data['display_name'],
+            "prompts": self.ora_data['prompts'],
+            "type": "individual",
         }
 
     def test_team_ora(self):
@@ -78,9 +73,9 @@ class TestOpenResponseMetadataSerializer(TestCase):
         data = OpenResponseMetadataSerializer(self.mock_ora_instance).data
 
         assert data == {
-            "name": self.display_name,
-            "prompts": self.prompts,
-            "type": "team"
+            "name": self.ora_data['display_name'],
+            "prompts": self.ora_data['prompts'],
+            "type": "team",
         }
 
 
