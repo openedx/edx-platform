7e3ef93a63db8c0fb95c67a7dbac4e92fadcf18f
Andy Shultz <ashultz@edx.org>
2021-10-21 15:45:20 -0400

    feat: make IDV optional for cert generation
    IDV is on its way to retirement, so it's not going to be necessary for cert
    generation forever.
    
    Introduces a function to combine the honor code flag with IDV to tell
    cert generation if it should care about a missing verification.
    
    Various tests expanded to cover the retired case. The additional calls
    in test_task_helper.py are caused by one call to fetch course
    overrides which finds none, and that forces one check of the
    background flag per student, 71 + 1 + 5 = 77.
    
    MST-854

--- lms/djangoapps/instructor_task/tests/test_tasks_helper.py
+++ lms/djangoapps/instructor_task/tests/test_tasks_helper.py
@@ -2082,7 +2082,7 @@ class TestCertificateGeneration(InstructorTaskModuleTestCase):
             'failed': 0,
             'skipped': 2
         }
-        with self.assertNumQueries(71):
+        with self.assertNumQueries(77):
             self.assertCertificatesGenerated(task_input, expected_results)
 
     @ddt.data(
