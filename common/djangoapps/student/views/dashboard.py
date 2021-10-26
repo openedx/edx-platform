661a7023f11d66a63464f27769346339bd098704
tinumide <tinuade@opencraft.com>
2021-10-26 15:25:33 +0000

    fix: feature improvments

--- common/djangoapps/student/views/dashboard.py
+++ common/djangoapps/student/views/dashboard.py
@@ -516,7 +516,7 @@ def student_dashboard(request):  # lint-amnesty, pylint: disable=too-many-statem
     )
     disable_unenrollment = configuration_helpers.get_value(
         'DISABLE_UNENROLLMENT',
-        settings.FEATURES.get('DISABLE_UNENROLLMENT', False)
+        settings.FEATURES.get('DISABLE_UNENROLLMENT')
     )
 
     disable_course_limit = request and 'course_limit' in request.GET
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -921,12 +921,13 @@ FEATURES = {
     # .. toggle_name: FEATURES['DISABLE_UNENROLLMENT']
     # .. toggle_implementation: DjangoSetting
     # .. toggle_default: False
-    # .. toggle_description: Set to True to enable the disabling of course unenrollment through REST API. 
-    #   This is disabled by default.
+    # .. toggle_description: Set to True to disable self-unenrollments via REST API. 
+    #   This also hides the "Unenroll" button on the learner dashboard.
     # .. toggle_use_cases: open_edx
     # .. toggle_creation_date: 2021-10-11
     # .. toggle_target_removal_date: None
-    # .. toggle_warnings: None
+    # .. toggle_warnings: For consistency in user experience, keep the value in sync with the setting of the same name
+    #   in the LMS and CMS.
     # .. toggle_tickets: 'https://github.com/open-craft/edx-platform/pull/429'
     'DISABLE_UNENROLLMENT': False,
 }
