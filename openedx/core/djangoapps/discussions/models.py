9ba3dcd151397ef8770ae94679e1953cfdd1decd
=Awais Jibran <awaisdar001@gmail.com>
2021-10-26 15:27:30 +0500

    refactor: rename feature variable.

--- openedx/core/djangoapps/discussions/models.py
+++ openedx/core/djangoapps/discussions/models.py
@@ -464,7 +464,6 @@ class DiscussionsConfiguration(TimeStampedModel):
             and discussion_provider.lti_configuration is not None
         )
 
-
 class ProgramDiscussionsConfiguration(TimeStampedModel):
     """
     Associates a program with a discussion provider and configuration
