60da5db4b64d46cef2c9b994609c2a68db188771
Binod Pant <bpant@edx.org>
2021-11-01 21:18:59 -0400

    feat: degreed2 integrated channels (#29183)
    * feat: degreed2 integrated channels
    
    ENT-2789
    
    * feat: :sparkles: New integrated channel via edx-enterprise
    
    * fix: pull in edx-enterprise 3.33.1
    
    fixes db_overrides check failure by renaming field `key` to `client_id`

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1811,6 +1811,7 @@ OPTIONAL_APPS = (
     ('consent', None),
     ('integrated_channels.integrated_channel', None),
     ('integrated_channels.degreed', None),
+    ('integrated_channels.degreed2', None),
     ('integrated_channels.sap_success_factors', None),
     ('integrated_channels.xapi', None),
     ('integrated_channels.cornerstone', None),
