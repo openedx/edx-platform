1003f262adc19196eb621200cd58914b9d8cf19c
M. Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2021-11-02 17:00:46 +0500

    feat: Django codemods on LMS (1) (#28851)

--- lms/djangoapps/certificates/migrations/0012_certificategenerationcoursesetting_include_hours_of_effort.py
+++ lms/djangoapps/certificates/migrations/0012_certificategenerationcoursesetting_include_hours_of_effort.py
@@ -11,6 +11,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='certificategenerationcoursesetting',
             name='include_hours_of_effort',
-            field=models.NullBooleanField(default=None, help_text="Display estimated time to complete the course, which is equal to the maximum hours of effort per week times the length of the course in weeks. This attribute will only be displayed in a certificate when the attributes 'Weeks to complete' and 'Max effort' have been provided for the course run and its certificate template includes Hours of Effort."),
+            field=models.BooleanField(default=None, help_text="Display estimated time to complete the course, which is equal to the maximum hours of effort per week times the length of the course in weeks. This attribute will only be displayed in a certificate when the attributes 'Weeks to complete' and 'Max effort' have been provided for the course run and its certificate template includes Hours of Effort.", null=True),
         ),
     ]
