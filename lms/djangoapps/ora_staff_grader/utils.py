19faf95a88934839637d5192ee0a22c4aad5811f
Nathan Sprenkle <nsprenkle@edx.org>
2021-11-03 18:41:57 -0400

    feat: extend call_xblock_json_handler
    Add decode kwarg. Setting False (non-default) returns the entire
    HttpResponse instead of unpacked JSON body. Helpful if you want to
    preserve the status code in your request or inspect other response info

--- lms/djangoapps/ora_staff_grader/utils.py
+++ lms/djangoapps/ora_staff_grader/utils.py
@@ -39,7 +39,7 @@ def require_params(param_names):
     return decorator
 
 
-def call_xblock_json_handler(request, usage_id, handler_name, data):
+def call_xblock_json_handler(request, usage_id, handler_name, data, decode=True):
     """
     Create an internally-routed XBlock.json_handler request.
     The internal auth code/param unpacking requires a POST request with payload in the body.
@@ -49,6 +49,9 @@ def call_xblock_json_handler(request, usage_id, handler_name, data):
         usage_id (str): Usage ID of the XBlock for running the handler
         handler_name (str): the name of the XBlock handler method
         data (dict): Data to be encoded and sent as the body of the POST request
+        decode (Boolean): Whether to return just the decoded body (True, default) or the original HttpReponse (False)
+    returns:
+        content (Dict) or response (HttpResponse): depending on whether decode is True (Dict) or False (HttpResponse)
     """
     # XBlock.json_handler operates through a POST request
     proxy_request = clone_request(request, "POST")
@@ -61,6 +64,11 @@ def call_xblock_json_handler(request, usage_id, handler_name, data):
     usage_key = UsageKey.from_string(usage_id)
     course_id = str(usage_key.course_key)
 
-    # Send and decode the request
+    # Send the request
     response = handle_xblock_callback(proxy_request, course_id, usage_id, handler_name)
-    return json.loads(response.content)
+
+    # And decode if requested
+    if decode:
+        return json.loads(response.content)
+    else:
+        return response
