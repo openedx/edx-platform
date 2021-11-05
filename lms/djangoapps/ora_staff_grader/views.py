66923f5699ae391a76ced6ffe8940282b2f049ab
Nathan Sprenkle <nsprenkle@edx.org>
2021-11-05 15:42:04 -0400

    feat: add submission lock views

--- lms/djangoapps/ora_staff_grader/views.py
+++ lms/djangoapps/ora_staff_grader/views.py
@@ -1,7 +1,8 @@
 """
 Views for Enhanced Staff Grader
 """
-from django.http.response import HttpResponseBadRequest, HttpResponseNotFound
+import json
+from django.http.response import HttpResponseBadRequest
 from django.utils.translation import ugettext as _
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 from edx_rest_framework_extensions.auth.session.authentication import SessionAuthenticationAllowInactiveUser
@@ -13,7 +14,7 @@ from rest_framework.response import Response
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
 
-from lms.djangoapps.ora_staff_grader.serializers import InitializeSerializer, SubmissionDetailResponseSerializer
+from lms.djangoapps.ora_staff_grader.serializers import InitializeSerializer, LockStatusSerializer, SubmissionDetailResponseSerializer
 from lms.djangoapps.ora_staff_grader.utils import call_xblock_json_handler, require_params
 from openedx.core.djangoapps.content.course_overviews.api import get_course_overview_or_none
 from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
@@ -128,3 +129,80 @@ class SubmissionFetchView(RetrieveAPIView):
             'submission_uuid': submission_uuid
         }
         return call_xblock_json_handler(request, usage_id, 'get_submission_and_assessment_info', data)
+
+
+class SubmissionLockView(RetrieveAPIView):
+    """
+    POST lock a submission for grading course metadata
+
+    Params:
+    - submissionId (UUID): A submission to lock/unlock
+    - value (bool): Whether to add (True, default) or remove (False) a lock from this submission
+
+    Response: {
+        lockStatus
+    }
+
+    Raises:
+    - 400 for bad request or missing query params
+    - 403 for bad auth or contested lock with payload { 'error': '<error-code>'}
+    """
+    authentication_classes = (
+        JwtAuthentication,
+        BearerAuthenticationAllowInactiveUser,
+        SessionAuthenticationAllowInactiveUser,
+    )
+    permission_classes = (IsAuthenticated,)
+
+    @require_params(['ora_location', 'submissionId', 'value'])
+    def post(self, request, ora_location, submission_uuid, value,  *args, **kwargs):
+        # Validate ORA location
+        try:
+            UsageKey.from_string(ora_location)
+        except (InvalidKeyError, ItemNotFoundError):
+            return HttpResponseBadRequest("Invalid ora_location.")
+
+        # Bool-ify the value param
+        value = True if value.lower() == 'true' else False
+
+        # Determine if we want to claim a submission lock (value=True) or delete it (value=False)
+        if value:
+            response = self.claim_submission_lock(request, ora_location, submission_uuid)
+        else:
+            response = self.delete_submission_lock(request, ora_location, submission_uuid)
+
+        # In the case of an error, pass through the error response code directly
+        if response.status_code != 200:
+            return response
+
+        # Success should return serialized lock info
+        response_data = json.loads(response.content)
+        return Response(LockStatusSerializer(response_data).data)
+
+    def claim_submission_lock(self, request, usage_id, submission_uuid):
+        """
+        Attempt to claim a submission lock for grading.
+
+        Returns:
+        - lockStatus (string) - One of ['not-locked', 'locked', 'in-progress']
+        """
+        body = {
+            "submission_id": submission_uuid
+        }
+
+        # running with decode=False to preserve HTTP status codes for failure states
+        return call_xblock_json_handler(request, usage_id, 'claim_submission_lock', body, decode=False)
+
+    def delete_submission_lock(self, request, usage_id, submission_uuid):
+        """
+        Attempt to claim a submission lock for grading.
+
+        Returns:
+        - lockStatus (string) - One of ['not-locked', 'locked', 'in-progress']
+        """
+        body = {
+            "submission_id": submission_uuid
+        }
+
+        # running with decode=False to preserve HTTP status codes for failure states
+        return call_xblock_json_handler(request, usage_id, 'delete_submission_lock', body, decode=False)
