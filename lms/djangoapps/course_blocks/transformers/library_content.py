b9f4b401caeaca6982d322289c302573ba98cc09
Chris Deery <3932645+cdeery@users.noreply.github.com>
2021-11-09 16:50:04 -0500

    feat: [AA-1082] fix 500 error on courses dashboard
    When there are no selected blocks, the dict comprehension to
    compare is throwing an exception. Handle the error more gracefully

--- lms/djangoapps/course_blocks/transformers/library_content.py
+++ lms/djangoapps/course_blocks/transformers/library_content.py
@@ -225,7 +225,10 @@ class ContentLibraryOrderTransformer(BlockStructureTransformer):
             if library_children:
                 state_dict = get_student_module_as_dict(usage_info.user, usage_info.course_key, block_key)
                 current_children_blocks = {block.block_id for block in library_children}
-                current_selected_blocks = {item[1] for item in state_dict['selected']}
+                if 'selected' in state_dict:
+                    current_selected_blocks = {item[1] for item in state_dict['selected']}
+                else:
+                    current_selected_blocks = {}
 
                 # As the selections should have already been made by the ContentLibraryTransformer,
                 # the current children of the library_content block should be the same as the stored
