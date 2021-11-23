ea85ad2db278323317e2e82310dc0189c4537c1b
Jawayria <jawayriahashmi@gmail.com>
2021-11-23 14:03:08 +0500

    chore: Applied lint-amnesty on lms/djangoapps

--- lms/djangoapps/instructor_task/api_helper.py
+++ lms/djangoapps/instructor_task/api_helper.py
@@ -19,7 +19,7 @@ from common.djangoapps.util.db import outer_atomic
 from lms.djangoapps.courseware.courses import get_problems_in_section
 from lms.djangoapps.courseware.module_render import get_xqueue_callback_url_prefix
 from lms.djangoapps.instructor_task.models import PROGRESS, InstructorTask
-from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.django import modulestore  # lint-amnesty, pylint: disable=wrong-import-order
 
 log = logging.getLogger(__name__)
 
