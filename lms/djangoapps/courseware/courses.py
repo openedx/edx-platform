c3b410b995d5d762d700a5bdd3f93b16e2b73a0f
Chris Deery <3932645+cdeery@users.noreply.github.com>
2021-11-23 13:26:34 -0500

    fix: [AA-1106] completed status for unreleased
    Cleaned up logic on verification of complete

--- lms/djangoapps/courseware/courses.py
+++ lms/djangoapps/courseware/courses.py
@@ -571,8 +571,9 @@ def get_course_assignments(course_key, user, include_access=False):  # lint-amne
                 if assignment_released:
                     url = reverse('jump_to', args=[course_key, subsection_key])
 
-                complete = is_block_structure_complete_for_assignments(block_data, subsection_key)
-                if not assignment_released:
+                if assignment_released:
+                    complete = is_block_structure_complete_for_assignments(block_data, subsection_key)
+                else:
                     complete = False
 
                 past_due = not complete and due < now
