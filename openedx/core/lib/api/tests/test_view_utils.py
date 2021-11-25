dba7d78c74ae2abd14050845968b0a81d19fbd2f
Jawayria <jawayriahashmi@gmail.com>
2021-11-25 12:22:09 +0500

    chore: Applied lint-amnesty on openedx/core/lib

--- openedx/core/lib/api/tests/test_view_utils.py
+++ openedx/core/lib/api/tests/test_view_utils.py
@@ -8,9 +8,9 @@ from rest_framework.test import APITestCase
 from rest_framework.views import APIView
 
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from xmodule.modulestore import ModuleStoreEnum
-from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase
-from xmodule.modulestore.tests.factories import CourseFactory
+from xmodule.modulestore import ModuleStoreEnum  # lint-amnesty, pylint: disable=wrong-import-order
+from xmodule.modulestore.tests.django_utils import SharedModuleStoreTestCase  # lint-amnesty, pylint: disable=wrong-import-order
+from xmodule.modulestore.tests.factories import CourseFactory  # lint-amnesty, pylint: disable=wrong-import-order
 
 from ..view_utils import DeveloperErrorViewMixin, verify_course_exists
 
