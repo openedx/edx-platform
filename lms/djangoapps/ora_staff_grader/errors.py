03a5017629b9b0665b1b3d022cfa7c6e1039acc1
Nathan Sprenkle <nsprenkle@edx.org>
2021-12-07 14:09:25 -0500

    docs: add comments for error codes

--- lms/djangoapps/ora_staff_grader/errors.py
+++ lms/djangoapps/ora_staff_grader/errors.py
@@ -1,6 +1,10 @@
 """ Error codes and exceptions for ESG """
 
+# A request is missing a required query param
 ERR_MISSING_PARAM = "ERR_MISSING_PARAM"
+
+# The requested ORA_LOCATION could not be found in the course
 ERR_BAD_ORA_LOCATION = "ERR_BAD_ORA_LOCATION"
 
+# User tried to operate on a submission that they do not have a lock for
 ERR_LOCK_CONTESTED = "ERR_LOCK_CONTESTED"
