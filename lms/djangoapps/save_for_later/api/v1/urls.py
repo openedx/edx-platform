139b46e6114ef4769d8f898bb3e0e54b548b2e1f
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2021-12-21 12:44:50 +0500

    fix: fixed RemovedInDjango40 warnings (#29588)

--- lms/djangoapps/save_for_later/api/v1/urls.py
+++ lms/djangoapps/save_for_later/api/v1/urls.py
@@ -2,11 +2,10 @@
 URLs for save_for_later v1
 """
 
-
-from django.conf.urls import url
+from django.urls import path
 
 from lms.djangoapps.save_for_later.api.v1.views import SaveForLaterApiView
 
 urlpatterns = [
-    url(r'^save/course/', SaveForLaterApiView.as_view(), name='save_course'),
+    path(r'^save/course/', SaveForLaterApiView.as_view(), name='save_course'),
 ]
