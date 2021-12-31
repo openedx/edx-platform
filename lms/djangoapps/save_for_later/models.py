9aa0cc44a26caa19938ace96b100ed012a5ddef3
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2021-12-31 11:31:53 +0500

    chore: handle save-for-later PII (#29696)
    Added PII annotations for email fields in save-for-later models and
    also added user retirement signal receiver to remove related objects.

--- lms/djangoapps/save_for_later/models.py
+++ lms/djangoapps/save_for_later/models.py
@@ -7,14 +7,30 @@ from model_utils.models import TimeStampedModel
 from django.db import models
 from opaque_keys.edx.django.models import CourseKeyField
 
+from openedx.core.djangolib.model_mixins import DeletableByUserValue
 
-class SavedCourse(TimeStampedModel):
+
+class SavedCourse(DeletableByUserValue, TimeStampedModel):
+    """
+    Tracks save course by email.
+
+    .. pii: Stores email address of the User.
+    .. pii_types: email_address
+    .. pii_retirement: local_api
+    """
     user_id = models.IntegerField(null=True, blank=True)
     email = models.EmailField(db_index=True)
     course_id = CourseKeyField(max_length=255, db_index=True)
 
 
-class SavedProgram(TimeStampedModel):
+class SavedProgram(DeletableByUserValue, TimeStampedModel):
+    """
+    Tracks save program by email.
+
+    .. pii: Stores email address of the User.
+    .. pii_types: email_address
+    .. pii_retirement: local_api
+    """
     user_id = models.IntegerField(null=True, blank=True)
     email = models.EmailField(db_index=True)
     program_uuid = models.UUIDField()
