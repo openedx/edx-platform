26b93e7f6642b462223681b5926cd813e65a2ffc
Nathan Sprenkle <nsprenkle@users.noreply.github.com>
2022-01-05 09:56:32 -0500

    refactor: ESG error handling (#29652)
    * feat: add error serializer and unknown error code
    
    * refactor: bad ORA location return new error shape
    
    * refactor: missing param return new error shape
    
    * refactor: update error for submit grade failure
    
    * refactor: move url_with_params
    
    * refactor: update error handling for lock contest
    
    Catch lock contest errors in XBlock handler wrapper, raise new exception
    type and return error code with updated lock status.
    
    * refactor: update errors for contested grade
    
    * refactor: change lock contested to HTTP status 409
    
    * refactor: move constants into a standalone file

--- lms/djangoapps/ora_staff_grader/errors.py
+++ lms/djangoapps/ora_staff_grader/errors.py
@@ -1,10 +1,102 @@
 """ Error codes and exceptions for ESG """
+from rest_framework import serializers
+from rest_framework.response import Response
 
-# A request is missing a required query param
-ERR_MISSING_PARAM = "ERR_MISSING_PARAM"
+from lms.djangoapps.ora_staff_grader.constants import (
+    ERR_BAD_ORA_LOCATION,
+    ERR_GRADE_CONTESTED,
+    ERR_GRADE_SUBMIT,
+    ERR_LOCK_CONTESTED,
+    ERR_MISSING_PARAM,
+    ERR_UNKNOWN,
+)
 
-# The requested ORA_LOCATION could not be found in the course
-ERR_BAD_ORA_LOCATION = "ERR_BAD_ORA_LOCATION"
 
-# User tried to operate on a submission that they do not have a lock for
-ERR_LOCK_CONTESTED = "ERR_LOCK_CONTESTED"
+class LockContestedError(Exception):
+    """Signal for trying to operate on a lock owned by someone else"""
+
+
+class ErrorSerializer(serializers.Serializer):
+    """Returns error code and unpacks additional context, returns unknown error code if not supplied"""
+
+    error = serializers.CharField(default=ERR_UNKNOWN)
+
+    def to_representation(self, instance):
+        """Override to unpack context alongside error code"""
+        output = super().to_representation(instance)
+        for key, value in self.context.items():
+            output[key] = value
+
+        return output
+
+
+class StaffGraderErrorResponse(Response):
+    """An HTTP error response that returns serialized error data with additional provided context"""
+
+    status = 500
+    err_code = ERR_UNKNOWN
+
+    def __init__(self, context={}):
+        # Unpack provided content into error structure
+        content = ErrorSerializer({"error": self.err_code}, context=context).data
+        super().__init__(content, status=self.status)
+
+
+class BadOraLocationResponse(StaffGraderErrorResponse):
+    """
+    Error response for when the requested ORA_LOCATION could not be found in a course.
+    Returns an HTTP 400 with error code.
+    """
+
+    status = 400
+    err_code = ERR_BAD_ORA_LOCATION
+
+
+class MissingParamResponse(StaffGraderErrorResponse):
+    """
+    Error response for when a request is missing a required param/body.
+    Returns an HTTP 400 with error code.
+    """
+
+    status = 400
+    err_code = ERR_MISSING_PARAM
+
+
+class SubmitGradeErrorResponse(StaffGraderErrorResponse):
+    """
+    Error response for errors encountered during grade submission (except for grade contest).
+    Returns an HTTP 500 with error code and error message from ORA.
+    """
+
+    status = 500
+    err_code = ERR_GRADE_SUBMIT
+
+
+class LockContestedResponse(StaffGraderErrorResponse):
+    """
+    Error response for when a user tries to operate on a submission that they do not have a lock for.
+    Returns an HTTP 409 with error code and updated lock status.
+    """
+
+    status = 409
+    err_code = ERR_LOCK_CONTESTED
+
+
+class GradeContestedResponse(StaffGraderErrorResponse):
+    """
+    Error response for when a user tries to operate on a submission that they do not have a lock for.
+    Returns an HTTP 409 with error code and updated submission status.
+    """
+
+    status = 409
+    err_code = ERR_GRADE_CONTESTED
+
+
+class UnknownErrorResponse(StaffGraderErrorResponse):
+    """
+    Generic error response for caught but non-standard exception types.
+    Returns an HTTP 500 with generic error code.
+    """
+
+    status = 500
+    err_code = ERR_UNKNOWN
