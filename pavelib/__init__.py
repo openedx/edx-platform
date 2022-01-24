cfaced1e47e35d99d5df717154f16a7911582044
mraarif <mraarif@outlook.com>
2022-01-25 02:10:17 +0500

    refactor: remove a11y tests setup

--- pavelib/__init__.py
+++ pavelib/__init__.py
@@ -3,4 +3,4 @@ paver commands
 """
 
 
-from . import assets, bok_choy, docs, i18n, js_test, prereqs, quality, servers, tests
+from . import assets, docs, i18n, js_test, prereqs, quality, servers, tests
