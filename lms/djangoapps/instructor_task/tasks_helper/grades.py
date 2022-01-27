c311f8248b82dc0b9bd1d0c5c0c6a3aa2640e2fa
Keith Grootboom <keith@opencraft.com>
2022-01-27 11:39:28 +0200

    fix: problem_grade_report task parent dir being discarded.
    When running the problem_grade_report task the upload_parent_dir
    argument should be used to place the task output in the
    specified directory.

--- lms/djangoapps/instructor_task/tasks_helper/grades.py
+++ lms/djangoapps/instructor_task/tasks_helper/grades.py
@@ -184,9 +184,22 @@ class GradeReportBase:
         Creates and uploads a CSV for the given headers and rows.
         """
         date = datetime.now(UTC)
-        upload_csv_to_report_store(success_rows, context.upload_filename, context.course_id, date)
+        upload_csv_to_report_store(
+            success_rows,
+            context.upload_filename,
+            context.course_id,
+            date,
+            parent_dir=context.upload_parent_dir
+        )
+
         if len(error_rows) > 1:
-            upload_csv_to_report_store(error_rows, context.upload_filename + '_err', context.course_id, date)
+            upload_csv_to_report_store(
+                error_rows,
+                context.upload_filename + '_err',
+                context.course_id,
+                date,
+                parent_dir=context.upload_parent_dir
+            )
 
     def log_additional_info_for_testing(self, context, message):
         """
@@ -317,7 +330,7 @@ class _ProblemGradeReportContext:
         self.report_for_verified_only = problem_grade_report_verified_only(self.course_id)
         self.task_progress = TaskProgress(self.action_name, total=None, start_time=time())
         self.upload_filename = _task_input.get('filename', 'problem_grade_report')
-        self.upload_dir = _task_input.get('upload_parent_dir', '')
+        self.upload_parent_dir = _task_input.get('upload_parent_dir', '')
 
     @lazy
     def course(self):
