7a4ed05279c449c3c4fa1e69f4627e437457f1ba
SaadYousaf <saadyousaf@A006-00314.local>
2022-02-02 13:12:53 +0500

    fix: add additional logs to investigate import issue

--- common/lib/xmodule/xmodule/modulestore/xml.py
+++ common/lib/xmodule/xmodule/modulestore/xml.py
@@ -380,6 +380,8 @@ class XMLModuleStore(ModuleStoreReadBase):
             monitor_import_failure(target_course_id, 'Updating', exception=exc)
             raise exc
         finally:
+            if str(target_course_id) == 'course-v1:ArbiX+CS101+2014_T3':
+                logging.info(f'Investigation Log: {target_course_id} : {course_descriptor}')
             if course_descriptor is None:
                 pass
             elif isinstance(course_descriptor, ErrorBlock):
@@ -487,6 +489,8 @@ class XMLModuleStore(ModuleStoreReadBase):
             course_id = self.get_id(org, course, url_name)
 
             if course_ids is not None and course_id not in course_ids:
+                if str(target_course_id) == 'course-v1:ArbiX+CS101+2014_T3':
+                    logging.info(f'Investigation Log: {target_course_id} : Course ID not in Course IDs (List)')
                 return None
 
             def get_policy(usage_id):
@@ -522,6 +526,8 @@ class XMLModuleStore(ModuleStoreReadBase):
             course_descriptor = system.process_xml(etree.tostring(course_data, encoding='unicode'))
             # If we fail to load the course, then skip the rest of the loading steps
             if isinstance(course_descriptor, ErrorBlock):
+                if str(target_course_id) == 'course-v1:ArbiX+CS101+2014_T3':
+                    logging.info(f'Investigation Log: {target_course_id} : Course Descriptor is instance of ErrorBlock')
                 return course_descriptor
 
             self.content_importers(system, course_descriptor, course_dir, url_name)
