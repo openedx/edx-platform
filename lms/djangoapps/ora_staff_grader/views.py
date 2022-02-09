1cec41ec5df37cf3fa5dd879cc581378149bc503
Nathan Sprenkle <nsprenkle@edx.org>
2022-02-09 15:32:14 -0500

    refactor: log bad ORA locations

--- lms/djangoapps/ora_staff_grader/views.py
+++ lms/djangoapps/ora_staff_grader/views.py
@@ -109,6 +109,7 @@ class InitializeView(StaffGraderBaseView):
 
         # Catch bad ORA location
         except (InvalidKeyError, ItemNotFoundError):
+            log.error(f'Bad ORA location provided: {ora_location}')
             return BadOraLocationResponse()
 
         # Issues with the XBlock handlers
@@ -365,6 +366,7 @@ class SubmissionLockView(StaffGraderBaseView):
 
         # Catch bad ORA location
         except (InvalidKeyError, ItemNotFoundError):
+            log.error(f'Bad ORA location provided: {ora_location}')
             return BadOraLocationResponse()
 
         # Return updated lock info on error
