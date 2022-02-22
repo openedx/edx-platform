4138c2cc16b8fb645c381b20a49de5d6cfd73990
Ahtisham Shahid <ahtisham300@gmail.com>
2022-02-22 13:03:03 +0500

    fix: Updated discussions banner text as per changed requirements (#29961)

--- lms/djangoapps/discussion/tests/test_views.py
+++ lms/djangoapps/discussion/tests/test_views.py
@@ -2296,11 +2296,11 @@ class ForumMFETestCase(ForumsEnableMixin, SharedModuleStoreTestCase):
                 response = self.client.get(reverse("forum_form_discussion", args=[self.course.id]))
                 content = response.content.decode('utf8')
         if mfe_url and toggle_enabled:
-            assert "An educator preview of new discussions experience is available!" in content
+            assert "You are viewing an educator only preview of the new discussions experience!" in content
             assert "legacy experience" in content
             assert "new experience" not in content
         else:
-            assert "An educator preview of new discussions experience is available!" not in content
+            assert "You are viewing an educator only preview of the new discussions experience!" not in content
 
     @override_settings(DISCUSSIONS_MICROFRONTEND_URL="http://test.url")
     @ddt.data(*itertools.product((True, False), ("legacy", "new", None)))
