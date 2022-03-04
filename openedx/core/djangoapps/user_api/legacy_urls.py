a838ab4b018eb6bc07b84e3dbd5b6435d006e5a5
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2022-03-04 15:03:02 +0500

    fix: fixed django4 warnings (#29643)

--- openedx/core/djangoapps/user_api/legacy_urls.py
+++ openedx/core/djangoapps/user_api/legacy_urls.py
@@ -1,7 +1,7 @@
 """
 Defines the URL routes for this app.
 """
-from django.conf.urls import include, url
+from django.urls import path, re_path, include
 from rest_framework import routers
 
 from . import views as user_api_views
@@ -13,24 +13,20 @@ USER_API_ROUTER.register(r'users', user_api_views.UserViewSet)
 USER_API_ROUTER.register(r'user_prefs', user_api_views.UserPreferenceViewSet)
 
 urlpatterns = [
-    url(r'^account/settings$', account_settings, name='account_settings'),
-    url(r'^user_api/v1/', include(USER_API_ROUTER.urls)),
-    url(
+    path('account/settings', account_settings, name='account_settings'),
+    path('user_api/v1/', include(USER_API_ROUTER.urls)),
+    re_path(
         fr'^user_api/v1/preferences/(?P<pref_key>{UserPreference.KEY_REGEX})/users/$',
         user_api_views.PreferenceUsersListView.as_view()
     ),
-    url(
+    re_path(
         r'^user_api/v1/forum_roles/(?P<name>[a-zA-Z]+)/users/$',
         user_api_views.ForumRoleUsersListView.as_view()
     ),
 
-    url(
-        r'^user_api/v1/preferences/email_opt_in/$',
-        user_api_views.UpdateEmailOptInPreference.as_view(),
-        name="preferences_email_opt_in_legacy"
-    ),
-    url(
-        r'^user_api/v1/preferences/time_zones/$',
-        user_api_views.CountryTimeZoneListView.as_view(),
-    ),
+    path('user_api/v1/preferences/email_opt_in/', user_api_views.UpdateEmailOptInPreference.as_view(),
+         name="preferences_email_opt_in_legacy"
+         ),
+    path('user_api/v1/preferences/time_zones/', user_api_views.CountryTimeZoneListView.as_view(),
+         ),
 ]
