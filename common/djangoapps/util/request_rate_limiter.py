467806f691b914fd1929670702d1bd794bb47232
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-03-17 21:56:37 +0500

    test: fixing a warning by closing a image file.

--- common/djangoapps/util/request_rate_limiter.py
+++ common/djangoapps/util/request_rate_limiter.py
@@ -1,6 +1,6 @@
 """
 A utility class which wraps the RateLimitMixin 3rd party class to do bad request counting
-which can be used for rate limiting
+which can be used for rate limiting.ABC
 """
 
 from ratelimitbackend.backends import RateLimitMixin
