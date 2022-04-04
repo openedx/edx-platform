681c2f1d6ca956f6f6eae96d69cc8d877daa9eef
alangsto <46360176+alangsto@users.noreply.github.com>
2022-04-04 08:11:40 -0700

    chore: remove IDV references for proctoring and student/instructor display (#30146)
    Places where IDV is referenced for blocking proctoring or displaying the IDV status to students and instructors should be removed. This includes the use of the ENABLE_INTEGRITY_SIGNATURE setting, which was used in part to deprecate IDV in those specified places. Other areas where ENABLE_INTEGRITY_SIGNATURE is used (such as for showing/hiding the integrity signature, for the verification deadline, or for certs) shall be left as is due to deprecation concerns.

--- common/djangoapps/course_modes/helpers.py
+++ common/djangoapps/course_modes/helpers.py
@@ -10,7 +10,6 @@ from requests.exceptions import ConnectionError, Timeout  # pylint: disable=rede
 from slumber.exceptions import SlumberBaseException
 
 from common.djangoapps.course_modes.models import CourseMode
-from common.djangoapps.student.helpers import VERIFY_STATUS_APPROVED, VERIFY_STATUS_NEED_TO_VERIFY, VERIFY_STATUS_SUBMITTED  # lint-amnesty, pylint: disable=line-too-long
 from openedx.core.djangoapps.commerce.utils import ecommerce_api_client
 
 DISPLAY_VERIFIED = "verified"
@@ -21,7 +20,7 @@ DISPLAY_PROFESSIONAL = "professional"
 LOGGER = logging.getLogger(__name__)
 
 
-def enrollment_mode_display(mode, verification_status, course_id):
+def enrollment_mode_display(mode, course_id):
     """ Select appropriate display strings and CSS classes.
 
         Uses mode and verification status to select appropriate display strings and CSS classes
@@ -38,19 +37,13 @@ def enrollment_mode_display(mode, verification_status, course_id):
     image_alt = ''
     enrollment_title = ''
     enrollment_value = ''
-    display_mode = _enrollment_mode_display(mode, verification_status, course_id)
+    display_mode = _enrollment_mode_display(mode, course_id)
 
     if display_mode == DISPLAY_VERIFIED:
-        if settings.FEATURES.get('ENABLE_INTEGRITY_SIGNATURE') or verification_status == VERIFY_STATUS_APPROVED:
-            enrollment_title = _("You're enrolled as a verified student")
-            enrollment_value = _("Verified")
-            show_image = True
-            image_alt = _("ID Verified Ribbon/Badge")
-        elif verification_status in [VERIFY_STATUS_NEED_TO_VERIFY, VERIFY_STATUS_SUBMITTED]:
-            enrollment_title = _("Your verification is pending")
-            enrollment_value = _("Verified: Pending Verification")
-            show_image = True
-            image_alt = _("ID verification pending")
+        enrollment_title = _("You're enrolled as a verified student")
+        enrollment_value = _("Verified")
+        show_image = True
+        image_alt = _("ID Verified Ribbon/Badge")
     elif display_mode == DISPLAY_HONOR:
         enrollment_title = _("You're enrolled as an honor code student")
         enrollment_value = _("Honor Code")
@@ -67,7 +60,7 @@ def enrollment_mode_display(mode, verification_status, course_id):
     }
 
 
-def _enrollment_mode_display(enrollment_mode, verification_status, course_id):
+def _enrollment_mode_display(enrollment_mode, course_id):
     """Checking enrollment mode and status and returns the display mode
      Args:
         enrollment_mode (str): enrollment mode.
@@ -79,15 +72,9 @@ def _enrollment_mode_display(enrollment_mode, verification_status, course_id):
     course_mode_slugs = [mode.slug for mode in CourseMode.modes_for_course(course_id)]
 
     if enrollment_mode == CourseMode.VERIFIED:
-        if (
-            settings.FEATURES.get('ENABLE_INTEGRITY_SIGNATURE')
-            or verification_status in [VERIFY_STATUS_NEED_TO_VERIFY, VERIFY_STATUS_SUBMITTED, VERIFY_STATUS_APPROVED]
-        ):
-            display_mode = DISPLAY_VERIFIED
-        elif DISPLAY_HONOR in course_mode_slugs:
+        display_mode = DISPLAY_VERIFIED
+        if DISPLAY_HONOR in course_mode_slugs:
             display_mode = DISPLAY_HONOR
-        else:
-            display_mode = DISPLAY_AUDIT
     elif enrollment_mode in [CourseMode.PROFESSIONAL, CourseMode.NO_ID_PROFESSIONAL_MODE]:
         display_mode = DISPLAY_PROFESSIONAL
     else:
