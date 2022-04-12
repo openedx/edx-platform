c57ad6d479493807bc9c59097ee47a8d62e9e02b
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2022-04-12 15:23:41 +0500

    Revert "feat: node16 upgrade (#30158)"
    This reverts commit 5ee25c65eaa1afded9819a6c909034fa855569d3.

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,11 @@ module.exports = {
   "modulePaths": [
     "common/static/common/js/components",
   ],
-  "setupFilesAfterEnv": ["<rootDir>/setupTests.js"],
+  "setupTestFrameworkScriptFile": "<rootDir>/setupTests.js",
   "testMatch": [
     "/**/*.test.jsx",
     "common/static/common/js/components/**/?(*.)+(spec|test).js?(x)",
   ],
-  "testEnvironment": "jsdom",
   "transform": {
     "^.+\\.jsx$": "babel-jest",
     "^.+\\.js$": "babel-jest",
