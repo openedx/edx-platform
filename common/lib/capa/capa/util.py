e654de4101368712ffbc749eef731c31f5a7ef13
UsamaSadiq <usama.sadiq@arbisoft.com>
2022-04-19 14:22:49 +0500

    fix: bump bleach and django-wiki versions

--- common/lib/capa/capa/util.py
+++ common/lib/capa/capa/util.py
@@ -13,6 +13,7 @@ import six
 from calc import evaluator
 from lxml import etree
 
+from bleach.css_sanitizer import CSSSanitizer
 from openedx.core.djangolib.markup import HTML
 
 #-----------------------------------------------------------------------------
@@ -192,7 +193,7 @@ def sanitize_html(html_code):
         html_code,
         protocols=bleach.ALLOWED_PROTOCOLS + ['data'],
         tags=bleach.ALLOWED_TAGS + ['div', 'p', 'audio', 'pre', 'img', 'span'],
-        styles=['white-space'],
+        css_sanitizer=CSSSanitizer(allowed_css_properties=["white-space"]),
         attributes=attributes
     )
     return output
