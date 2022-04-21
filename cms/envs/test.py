openedx/edx-platform

commit ad5ad72273ce36bb3b14b5b939ea5bacac78d88f

[BD-13] Deprecate ModuleSystem.render_template (fixed) (#29354)
* refactor: deprecates ModuleSystem.render_template

in favor of the added MakoSystem render_template method.

Related changes:
* Adds the MakoService to the StudioEditModuleRuntime,
  PreviewModuleSystem, LmsModuleSystem, and XBlockRuntime
* MakoService constructor takes a `namespace_prefix` string, so that the
  CMS PreviewModuleSystem can render to LMS templates, without needing
  the special render_from_lms helper method.
* ModuleSystem.render_template becomes a read-only property, so the
  constructor calls and test module systems are updated accordingly.
* Adds tests for the MakoService and module system shims.

(cherry picked from commit 457f959356bdb0317459290aa34d5df092da8f87)

* refactor: use MakoService.render_template to remove deprecation warnings

from block code.

(cherry picked from commit 8d62d337f5bd14de366cc3f4f315b8482cfe623e)

* refactor: use MakoService.render_template to remove deprecation warnings

from test code.

(cherry picked from commit 26b43465a44b3e784f433c3c9badda16e1a131cb)

* test: Adds a test to verify the bug introduced by the previous changes

The AuthoringMixin is automatically added to all XBlocks (see
settings.XBLOCK_MIXINS), and AuthoringMixin.visibility_view expects the
"mako" service.

This test verifies the bug by testing the PureXBlock, which does not
require the "mako" service, and so fails when the visibility_view is
rendered.

* fix: AuthoringMixin needs mako service

which fixes the visibility_view for XBlocks which don't explicitly
require the mako service.

Also removes the unneeded class property _services_requested from
AuthoringMixin and StudioEditableBlock. This property is better provided
by the XBlockMixin class.

openedx/core/djangoapps/xblock/runtime/runtime.py
@@ -19,6 +19,7 @@ from xblock.field_data import SplitFieldData
 from xblock.fields import Scope
 from xblock.runtime import KvsFieldData, MemoryIdManager, Runtime
 
+from common.djangoapps.edxmako.services import MakoService
 from common.djangoapps.track import contexts as track_contexts
 from common.djangoapps.track import views as track_views
 from common.djangoapps.xblock_django.user_service import DjangoXBlockUserService
@@ -235,6 +236,8 @@ class XBlockRuntime(RuntimeShim, Runtime):
                 user_is_staff=self.user.is_staff,
                 anonymous_user_id=self.anonymous_student_id,
             )
+        elif service_name == "mako":
+            return MakoService()
         elif service_name == "i18n":
             return ModuleI18nService(block=block)
         # Check if the XBlockRuntimeSystem wants to handle this:

lms/djangoapps/lms_xblock/test/test_runtime.py
@@ -63,7 +63,6 @@ class TestHandlerUrl(TestCase):
             static_url='/static',
             track_function=Mock(),
             get_module=Mock(),
-            render_template=Mock(),
             replace_urls=str,
             course_id=self.course_key,
             user=Mock(),
@@ -130,7 +129,6 @@ class TestUserServiceAPI(TestCase):
             static_url='/static',
             track_function=Mock(),
             get_module=Mock(),
-            render_template=Mock(),
             replace_urls=str,
             user=self.user,
             course_id=self.course_id,
@@ -186,7 +184,6 @@ class TestBadgingService(ModuleStoreTestCase):
             static_url='/static',
             track_function=Mock(),
             get_module=Mock(),
-            render_template=Mock(),
             replace_urls=str,
             course_id=self.course_id,
             user=self.user,
@@ -242,7 +239,6 @@ class TestI18nService(ModuleStoreTestCase):
             static_url='/static',
             track_function=Mock(),
             get_module=Mock(),
-            render_template=Mock(),
             replace_urls=str,
             course_id=self.course.id,
             user=Mock(),

lms/envs/test.py
@@ -495,6 +495,20 @@ ENTERPRISE_CONSENT_API_URL = 'http://enterprise.example.com/consent/api/v1/'
 ACTIVATION_EMAIL_FROM_ADDRESS = 'test_activate@edx.org'
 
 TEMPLATES[0]['OPTIONS']['debug'] = True
+TEMPLATES.append(
+    {
+        # This separate copy of the Mako backend is used to test rendering previews in the 'lms.main' namespace
+        'NAME': 'preview',
+        'BACKEND': 'common.djangoapps.edxmako.backend.Mako',
+        'APP_DIRS': False,
+        'DIRS': MAKO_TEMPLATE_DIRS_BASE,
+        'OPTIONS': {
+            'context_processors': CONTEXT_PROCESSORS,
+            'debug': False,
+            'namespace': 'lms.main',
+        }
+    }
+)
 
 ########################## VIDEO TRANSCRIPTS STORAGE ############################
 VIDEO_TRANSCRIPTS_SETTINGS = dict(

cms/djangoapps/contentstore/views/helpers.py
@@ -14,7 +14,6 @@ from xmodule.modulestore.django import modulestore
 from xmodule.tabs import StaticTab
 
 from cms.djangoapps.models.settings.course_grading import CourseGradingModel
-from common.djangoapps.edxmako.shortcuts import render_to_string
 from common.djangoapps.student import auth
 from common.djangoapps.student.roles import CourseCreatorRole, OrgContentCreatorRole
 from openedx.core.toggles import ENTRANCE_EXAMS
@@ -41,13 +40,6 @@ def event(request):
     console logs don't get distracted :-)
     '''
     return HttpResponse(status=204)
-
-
-def render_from_lms(template_name, dictionary, namespace='main'):
-    """
-    Render a template using the LMS Mako templates
-    """
-    return render_to_string(template_name, dictionary, namespace="lms." + namespace)
 
 
 def get_parent_xblock(xblock):

cms/djangoapps/contentstore/views/item.py
@@ -32,6 +32,7 @@ from xblock.fields import Scope
 from cms.djangoapps.contentstore.config.waffle import SHOW_REVIEW_RULES_FLAG
 from cms.djangoapps.models.settings.course_grading import CourseGradingModel
 from cms.lib.xblock.authoring_mixin import VISIBILITY_VIEW
+from common.djangoapps.edxmako.services import MakoService
 from common.djangoapps.edxmako.shortcuts import render_to_string
 from common.djangoapps.static_replace import replace_static_urls
 from common.djangoapps.student.auth import has_studio_read_access, has_studio_write_access
@@ -309,6 +310,8 @@ class StudioEditModuleRuntime:
                 return DjangoXBlockUserService(self._user)
             if service_name == "studio_user_permissions":
                 return StudioPermissionsService(self._user)
+            if service_name == "mako":
+                return MakoService()
             if service_name == "settings":
                 return SettingsService()
             if service_name == "lti-configuration":

cms/djangoapps/contentstore/views/preview.py
@@ -19,6 +19,7 @@ from cms.djangoapps.xblock_config.models import StudioConfig
 from cms.lib.xblock.field_data import CmsFieldData
 from common.djangoapps import static_replace
 from common.djangoapps.edxmako.shortcuts import render_to_string
+from common.djangoapps.edxmako.services import MakoService
 from common.djangoapps.xblock_django.user_service import DjangoXBlockUserService
 from lms.djangoapps.lms_xblock.field_data import LmsFieldData
 from openedx.core.lib.license import wrap_with_license
@@ -43,7 +44,6 @@ from xmodule.x_module import AUTHOR_VIEW, PREVIEW_VIEWS, STUDENT_VIEW, ModuleSys
 
 from ..utils import get_visibility_partition_info
 from .access import get_user_role
-from .helpers import render_from_lms
 from .session_kv_store import SessionKeyValueStore
 
 __all__ = ['preview_handler']
@@ -185,9 +185,10 @@ def _preview_module_system(request, descriptor, field_data):
         )
     ]
 
+    mako_service = MakoService(namespace_prefix='lms.')
     if settings.FEATURES.get("LICENSING", False):
         # stick the license wrapper in front
-        wrappers.insert(0, wrap_with_license)
+        wrappers.insert(0, partial(wrap_with_license, mako_service=mako_service))
 
     return PreviewModuleSystem(
         static_url=settings.STATIC_URL,
@@ -195,7 +196,6 @@ def _preview_module_system(request, descriptor, field_data):
         track_function=lambda event_type, event: None,
         filestore=descriptor.runtime.resources_fs,
         get_module=partial(_load_preview_module, request),
-        render_template=render_from_lms,
         debug=True,
         replace_urls=partial(static_replace.replace_static_urls, data_directory=None, course_id=course_id),
         can_execute_unsafe_code=(lambda: can_execute_unsafe_code(course_id)),
@@ -213,6 +213,7 @@ def _preview_module_system(request, descriptor, field_data):
         services={
             "field-data": field_data,
             "i18n": ModuleI18nService,
+            'mako': mako_service,
             "settings": SettingsService(),
             "user": DjangoXBlockUserService(request.user, anonymous_user_id='student'),
             "partitions": StudioPartitionService(course_id=course_id),

cms/djangoapps/contentstore/views/tests/test_preview.py
@@ -8,6 +8,7 @@ from unittest import mock
 
 import ddt
 from django.test.client import Client, RequestFactory
+from web_fragments.fragment import Fragment
 from xblock.core import XBlock, XBlockAside
 
 from cms.djangoapps.contentstore.utils import reverse_usage_url
@@ -168,13 +169,20 @@ class GetPreviewHtmlTestCase(ModuleStoreTestCase):
 
 @XBlock.needs("field-data")
 @XBlock.needs("i18n")
+@XBlock.needs("mako")
 @XBlock.needs("user")
 @XBlock.needs("teams_configuration")
 class PureXBlock(XBlock):
     """
     Pure XBlock to use in tests.
     """
-    pass  # lint-amnesty, pylint: disable=unnecessary-pass
+    def student_view(self, context):
+        """
+        Renders the output that a student will see.
+        """
+        fragment = Fragment()
+        fragment.add_content(self.runtime.service(self, 'mako').render_template('edxmako.html', context))
+        return fragment
 
 
 @ddt.ddt
@@ -206,3 +214,25 @@ class StudioXBlockServiceBindingTest(ModuleStoreTestCase):
         )
         service = runtime.service(descriptor, expected_service)
         self.assertIsNotNone(service)
+
+
+class CmsModuleSystemShimTest(ModuleStoreTestCase):
+    """
+    Tests that the deprecated attributes in the Module System (XBlock Runtime) return the expected values.
+    """
+    def setUp(self):
+        """
+        Set up the user and other fields that will be used to instantiate the runtime.
+        """
+        super().setUp()
+        self.course = CourseFactory.create()
+        self.user = UserFactory()
+        self.request = RequestFactory().get('/dummy-url')
+        self.request.user = self.user
+        self.request.session = {}
+
+    @XBlock.register_temp_plugin(PureXBlock, identifier='pure')
+    def test_render_template(self):
+        descriptor = ItemFactory(category="pure", parent=self.course)
+        html = get_preview_fragment(self.request, descriptor, {'element_id': 142}).content
+        assert '<div id="142" ns="main">Testing the MakoService</div>' in html

cms/lib/xblock/authoring_mixin.py
@@ -15,14 +15,11 @@ VISIBILITY_VIEW = 'visibility_view'
 
 
 @XBlock.needs("i18n")
+@XBlock.needs("mako")
 class AuthoringMixin(XBlockMixin):
     """
     Mixin class that provides authoring capabilities for XBlocks.
     """
-    _services_requested = {
-        'i18n': 'need',
-    }
-
     def _get_studio_resource_url(self, relative_url):
         """
         Returns the Studio URL to a static resource.
@@ -39,7 +36,7 @@ class AuthoringMixin(XBlockMixin):
         """
         fragment = Fragment()
         from cms.djangoapps.contentstore.utils import reverse_course_url
-        fragment.add_content(self.system.render_template('visibility_editor.html', {
+        fragment.add_content(self.runtime.service(self, 'mako').render_template('visibility_editor.html', {
             'xblock': self,
             'manage_groups_url': reverse_course_url('group_configurations_list_handler', self.location.course_key),
         }))

commit 732d8cb33771047a1c5f396f04c35f3279aafcd4

fix: use mako prefix lms. when XBlock runtime renders in Studio/authoring MFE
When the XBlock runtime is used in Studio/authoring mode, it needs to
use the template path prefix "lms." in order to locate the block
templates. (In LMS view, no prefix is required.)

Fixes bug introduced by https://github.com/openedx/edx-platform/pull/29354
and removes template workaround added by
https://github.com/openedx/edx-platform/pull/29517 (see Author Notes & Concerns)

cms/envs/test.py
@@ -42,7 +42,8 @@ from lms.envs.test import (  # pylint: disable=wrong-import-order
     REGISTRATION_EXTRA_FIELDS,
     GRADES_DOWNLOAD,
     SITE_NAME,
-    WIKI_ENABLED
+    WIKI_ENABLED,
+    XBLOCK_RUNTIME_V2_EPHEMERAL_DATA_CACHE,
 )
 
 
@@ -177,6 +178,12 @@ CACHES = {
     'course_structure_cache': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     },
+    'blockstore': {
+        'KEY_PREFIX': 'blockstore',
+        'KEY_FUNCTION': 'common.djangoapps.util.memcache.safe_key',
+        'LOCATION': 'edx_loc_mem_cache',
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    },
 }
 
 ############################### BLOCKSTORE #####################################
@@ -275,12 +282,6 @@ TEST_ELASTICSEARCH_USE_SSL = os.environ.get(
     'EDXAPP_TEST_ELASTICSEARCH_USE_SSL', 'no').lower() in ('true', 'yes', '1')
 TEST_ELASTICSEARCH_HOST = os.environ.get('EDXAPP_TEST_ELASTICSEARCH_HOST', 'edx.devstack.elasticsearch710')
 TEST_ELASTICSEARCH_PORT = int(os.environ.get('EDXAPP_TEST_ELASTICSEARCH_PORT', '9200'))
-
-############################# TEMPLATE CONFIGURATION #############################
-# Adds mako template dirs for content_libraries tests
-MAKO_TEMPLATE_DIRS_BASE.append(
-    COMMON_ROOT / 'lib' / 'capa' / 'capa' / 'templates'
-)
 
 ########################## AUTHOR PERMISSION #######################
 FEATURES['ENABLE_CREATOR_GROUP'] = False

openedx/core/djangoapps/xblock/runtime/runtime.py
@@ -241,6 +241,8 @@ class XBlockRuntime(RuntimeShim, Runtime):
                 anonymous_user_id=self.anonymous_student_id,
             )
         elif service_name == "mako":
+            if self.system.student_data_mode == XBlockRuntimeSystem.STUDENT_DATA_EPHEMERAL:
+                return MakoService(namespace_prefix='lms.')
             return MakoService()
         elif service_name == "i18n":
             return ModuleI18nService(block=block)

common/test/problem.html
@@ -1,96 +1,0 @@
-<!-- 
-  common/test/problem.html
-  Placeholder template for openedx content_library tests -->
-<%page expression_filter="h"/>
-<%!
-from django.utils.translation import ngettext, gettext as _
-from openedx.core.djangolib.markup import HTML
-%>
-
-<%namespace name='static' file='static_content.html'/>
-<h3 class="hd hd-3 problem-header" id="${ short_id }-problem-title" aria-describedby="${ id }-problem-progress" tabindex="-1">
-  ${ problem['name'] }
-</h3>
-
-<div class="problem-progress" id="${ id }-problem-progress"></div>
-
-<div class="problem">
-  ${ HTML(problem['html']) }
-  <div class="action">
-    <input type="hidden" name="problem_id" value="${ problem['name'] }" />
-
-    <div class="problem-action-buttons-wrapper">
-      % if demand_hint_possible:
-      <span class="problem-action-button-wrapper">
-          <button type="button" class="hint-button problem-action-btn btn-link btn-small" data-value="${_('Hint')}" ${'' if should_enable_next_hint else 'disabled'}>${_('Hint')}</button>
-      </span>
-      % endif
-      % if save_button:
-      <span class="problem-action-button-wrapper">
-          <button type="button" class="save problem-action-btn btn-link btn-small" data-value="${_('Save')}">
-              <span aria-hidden="true">${_('Save')}</span>
-              <span class="sr">${_("Save your answer")}</span>
-          </button>
-      </span>
-      % endif
-      % if reset_button:
-      <span class="problem-action-button-wrapper">
-          <button type="button" class="reset problem-action-btn btn-link btn-small" data-value="${_('Reset')}"><span aria-hidden="true">${_('Reset')}</span><span class="sr">${_("Reset your answer")}</span></button>
-      </span>
-      % endif
-      % if answer_available:
-      <span class="problem-action-button-wrapper">
-          <button type="button" class="show problem-action-btn btn-link btn-small" aria-describedby="${ short_id }-problem-title"><span class="show-label">${_('Show answer')}</span></button>
-      </span>
-      % endif
-    </div>
-    <div class="submit-attempt-container">
-      <button type="button" class="submit btn-brand" data-submitting="${ submit_button_submitting }" data-value="${ submit_button }" data-should-enable-submit-button="${ should_enable_submit_button }" aria-describedby="submission_feedback_${short_id}" ${'' if should_enable_submit_button else 'disabled'}>
-          <span class="submit-label">${ submit_button }</span>
-      </button>
-
-      % if submit_disabled_cta:
-        % if submit_disabled_cta.get('event_data'):
-          <button class="submit-cta-link-button btn-link btn-small" onclick="emit_event(${submit_disabled_cta['event_data']})">
-            ${submit_disabled_cta['link_name']}
-          </button>
-          <span class="submit-cta-description" tabindex="0" role="note" aria-label="description">
-            <span data-tooltip="${submit_disabled_cta['description']}" data-tooltip-show-on-click="true"
-              class="fa fa-info-circle fa-lg" aria-hidden="true">
-            </span>
-          </span>
-          <span class="sr">(${submit_disabled_cta['description']})</span>
-        % else:
-            <form class="submit-cta" method="post" action="${submit_disabled_cta['link']}">
-              <input type="hidden" id="csrf_token" name="csrfmiddlewaretoken" value="${csrf_token}">
-              % for form_name, form_value in submit_disabled_cta['form_values'].items():
-                  <input type="hidden" name="${form_name}" value="${form_value}">
-              % endfor
-              <button class="submit-cta-link-button btn-link btn-small">
-                ${submit_disabled_cta['link_name']}
-              </button>
-              <span class="submit-cta-description" tabindex="0" role="note" aria-label="description">
-                <span data-tooltip="${submit_disabled_cta['description']}" data-tooltip-show-on-click="true"
-                  class="fa fa-info-circle fa-lg" aria-hidden="true">
-                </span>
-              </span>
-              <span class="sr">(${submit_disabled_cta['description']})</span>
-            </form>
-        % endif
-      % endif
-      <div class="submission-feedback ${'cta-enabled' if submit_disabled_cta else ''}" id="submission_feedback_${short_id}">
-        ## When attempts are not 0, the CTA above will contain a message about the number of used attempts
-        % if attempts_allowed and (not submit_disabled_cta or attempts_used == 0):
-          ${ngettext("You have used {num_used} of {num_total} attempt", "You have used {num_used} of {num_total} attempts", attempts_allowed).format(num_used=attempts_used, num_total=attempts_allowed)}
-        % endif
-        <span class="sr">${_("Some problems have options such as save, reset, hints, or show answer. These options follow the Submit button.")}</span>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
-    function emit_event(message) {
-        parent.postMessage(message, '*');
-    }
-</script>

common/test/problem_ajax.html
@@ -1,16 +1,0 @@
-<!-- 
-  common/tests/problem_ajax.html
-  Placeholder template for openedx content_library tests -->
-<div id="problem_${element_id}" class="problems-wrapper" role="group"
-     aria-labelledby="${element_id}-problem-title"
-     data-problem-id="${id}" data-url="${ajax_url}"
-     data-problem-score="${current_score}"
-     data-problem-total-possible="${total_possible}"
-     data-attempts-used="${attempts_used}"
-     data-content="${content | h}"
-     data-graded="${graded}">
-    <p class="loading-spinner">
-        <i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i>
-        <span class="sr">Loading&hellip;</span>
-    </p>
-</div>

common/test/video.html
@@ -1,122 +1,0 @@
-<!-- 
-  common/test/video.html
-  Placeholder template for openedx content_library tests -->
-<%page expression_filter="h"/>
-
-<%!
-from django.utils.translation import ugettext as _
-from openedx.core.djangolib.js_utils import (
-    dump_js_escaped_json, js_escaped_string
-)
-%>
-% if display_name is not UNDEFINED and display_name is not None:
-    <h3 class="hd hd-2">${display_name}</h3>
-% endif
-
-<div
-    id="video_${id}"
-    class="video closed"
-    data-metadata='${metadata}'
-    data-bumper-metadata='${bumper_metadata}'
-    data-autoadvance-enabled="${autoadvance_enabled}"
-    data-poster='${poster}'
-    tabindex="-1"
->
-    <div class="focus_grabber first"></div>
-
-    <div class="tc-wrapper">
-      <div class="video-wrapper">
-          <span tabindex="0" class="spinner" aria-hidden="false" aria-label="${_('Loading video player')}"></span>
-          <span tabindex="-1" class="btn-play fa fa-youtube-play fa-2x is-hidden" aria-hidden="true" aria-label="${_('Play video')}"></span>
-          <div class="video-player-pre"></div>
-          <div class="video-player">
-              <div id="${id}"></div>
-              <h4 class="hd hd-4 video-error is-hidden">${_('No playable video sources found.')}</h4>
-              <h4 class="hd hd-4 video-hls-error is-hidden">
-                  ${_('Your browser does not support this video format. Try using a different browser.')}
-              </h4>
-          </div>
-          <div class="video-player-post"></div>
-          <div class="closed-captions"></div>
-          <div class="video-controls is-hidden">
-              <div>
-                  <div class="vcr"><div class="vidtime">0:00 / 0:00</div></div>
-                  <div class="secondary-controls"></div>
-              </div>
-          </div>
-      </div>
-    </div>
-
-    <div class="focus_grabber last"></div>
-
-    % if download_video_link or track or handout or branding_info:
-    <h3 class="hd hd-4 downloads-heading sr" id="video-download-transcripts_${id}">${_('Downloads and transcripts')}</h3>
-    <div class="wrapper-downloads" role="region" aria-labelledby="video-download-transcripts_${id}">
-        % if download_video_link:
-        <div class="wrapper-download-video">
-            <h4 class="hd hd-5">${_('Video')}</h4>
-            <a class="btn-link video-sources video-download-button" href="${download_video_link}">
-                ${_('Download video file')}
-            </a>
-        </div>
-        % endif
-        % if track:
-        <div class="wrapper-download-transcripts">
-            <h4 class="hd hd-5">${_('Transcripts')}</h4>
-            % if transcript_download_format:
-            <ul class="list-download-transcripts">
-                % for item in transcript_download_formats_list:
-                    <li class="transcript-option">
-                        <% dname = _("Download {file}").format(file=item['display_name']) %>
-                        <a class="btn btn-link" href="${track}" data-value="${item['value']}">${dname}</a>
-                    </li>
-                % endfor
-            </ul>
-            % else:
-            <a class="btn-link external-track" href="${track}">${_('Download transcript')}</a>
-            % endif
-        </div>
-        % endif
-        % if handout:
-        <div class="wrapper-handouts">
-            <h4 class="hd hd-5">${_('Handouts')}</h4>
-            <a class="btn-link" href="${handout}">${_('Download Handout')}</a>
-        </div>
-        % endif
-        % if branding_info:
-        <div class="branding">
-            <span class="host-tag">${branding_info['logo_tag']}</span>
-            <a href="${branding_info['url']}"><img class="brand-logo" src="${branding_info['logo_src']}" alt="${branding_info['logo_tag']}" /></a>
-        </div>
-        % endif
-    </div>
-    % endif
-</div>
-% if cdn_eval:
-<script>
-  //TODO: refactor this js into a separate file.
-  function sendPerformanceBeacon(id, expgroup, value, event_name) {
-    var data = {event: event_name, id: id, expgroup: expgroup, value: value, page: "html5vid"};
-    $.ajax({method: "POST", url: "/performance", data: data});
-  }
-  var cdnStartTime;
-  var salt = Math.floor((1 + Math.random()) * 0x100000).toString(36);
-  var id = "${id | n, js_escaped_string}";
-  function initializeCDNExperiment() {
-    sendPerformanceBeacon(id + "_" + salt, ${cdn_exp_group | n, dump_js_escaped_json}, "", "load");
-    cdnStartTime = Date.now();
-    $.each(['loadstart', 'abort', 'error', 'stalled', 'loadedmetadata',
-                    'loadeddata', 'canplay', 'canplaythrough', 'seeked'],
-                    function(index, eventName) {
-      $("#video_" + id).bind("html5:" + eventName, null, function() {
-        timeElapsed = Date.now() - cdnStartTime;
-        sendPerformanceBeacon(id + "_" + salt, ${cdn_exp_group | n, dump_js_escaped_json}, timeElapsed, eventName);
-      });
-    });
-  }
-  $("#video_" + id).bind("initialize", null, initializeCDNExperiment);
-  if ($("#video_" + id).hasClass("is-initialized")) {
-    initializeCDNExperiment();
-  }
-</script>
-% endif;

openedx/core/djangoapps/content_libraries/tests/test_runtime.py
@@ -6,7 +6,7 @@ from gettext import GNUTranslations
 
 from completion.test_utils import CompletionWaffleTestMixin
 from django.db import connections
-from django.test import LiveServerTestCase, TestCase, override_settings
+from django.test import LiveServerTestCase, TestCase
 from django.utils.text import slugify
 from organizations.models import Organization
 from rest_framework.test import APIClient
@@ -182,8 +182,6 @@ class ContentLibraryRuntimeTestMixin(ContentLibraryContentTestMixin):
 
 
 @requires_blockstore
-# EphemeralKeyValueStore requires a working cache, and the default test cache doesn't work:
-@override_settings(XBLOCK_RUNTIME_V2_EPHEMERAL_DATA_CACHE='blockstore')
 class ContentLibraryRuntimeBServiceTest(ContentLibraryRuntimeTestMixin, TestCase):
     """
     Tests XBlock runtime using XBlocks in a content library using the standalone Blockstore service.
@@ -191,8 +189,6 @@ class ContentLibraryRuntimeBServiceTest(ContentLibraryRuntimeTestMixin, TestCase
 
 
 @requires_blockstore_app
-# EphemeralKeyValueStore requires a working cache, and the default test cache doesn't work:
-@override_settings(XBLOCK_RUNTIME_V2_EPHEMERAL_DATA_CACHE='blockstore')
 class ContentLibraryRuntimeTest(ContentLibraryRuntimeTestMixin, BlockstoreAppTestMixin, LiveServerTestCase):
     """
     Tests XBlock runtime using XBlocks in a content library using the installed Blockstore app.