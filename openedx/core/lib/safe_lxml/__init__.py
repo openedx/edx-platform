e24c40bd586da17425d6d7038d888103a8c68d4a
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-05-19 16:35:49 +0500

    fix: comment out code

--- openedx/core/lib/safe_lxml/__init__.py
+++ openedx/core/lib/safe_lxml/__init__.py
@@ -7,11 +7,12 @@ def defuse_xml_libs():
     """
     Monkey patch and defuse all stdlib xml packages and lxml.
     """
-    from defusedxml import defuse_stdlib
-    defuse_stdlib()
+    pass
+    # from defusedxml import defuse_stdlib
+    # defuse_stdlib()
 
-    import lxml
-    import lxml.etree
-    from . import etree as safe_etree
+    # import lxml
+    # import lxml.etree
+    # from . import etree as safe_etree
 
-    lxml.etree = safe_etree
+    # lxml.etree = safe_etree
