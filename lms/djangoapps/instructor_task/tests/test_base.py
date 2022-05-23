44721cfaffac45a2d9cb3ba39c134e701895387e
M Umar Khan <umar.khan@arbisoft.com>
2022-05-23 12:44:02 +0500

    refactor: fix imports

--- lms/djangoapps/instructor_task/tests/test_base.py
+++ lms/djangoapps/instructor_task/tests/test_base.py
@@ -13,17 +13,17 @@ from unittest.mock import Mock, patch
 from uuid import uuid4
 
 import unicodecsv
+from capa.tests.response_xml_factory import OptionResponseXMLFactory
 from celery.states import FAILURE, SUCCESS
 from django.contrib.auth.models import User  # lint-amnesty, pylint: disable=imported-auth-user
 from django.urls import reverse
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locations import Location
+
 from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.tests.django_utils import TEST_DATA_MONGO_AMNESTY_MODULESTORE, ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
-
-from capa.tests.response_xml_factory import OptionResponseXMLFactory
 from common.djangoapps.student.tests.factories import CourseEnrollmentFactory, UserFactory
 from lms.djangoapps.courseware.model_data import StudentModule
 from lms.djangoapps.courseware.tests.tests import LoginEnrollmentTestCase
