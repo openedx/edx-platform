ca0d59831546edfa08dac50d42904ec221e0f905
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-05-27 19:59:54 +0500

    feat!: common/lib/sandbox-packages folder moved to a new library.

--- common/djangoapps/util/tests/test_codejail_includes.py
+++ common/djangoapps/util/tests/test_codejail_includes.py
@@ -1,4 +1,6 @@
-# lint-amnesty, pylint: disable=missing-module-docstring
+"""
+Tests for codejail-includes package.
+"""
 
 import unittest
 
@@ -12,13 +14,13 @@ class Test_CodeJail_Includes(unittest.TestCase):
 
     def test_loncapa(self):
         random_integer = loncapa.lc_random(2, 60, 5)
-        self.assertLessEqual(random_integer, 60)
-        self.assertGreaterEqual(random_integer, 2)
+        assert random_integer <= 60
+        assert random_integer >= 2
 
     def test_nested_list_and_list1(self):
         assert draganddrop.PositionsCompare([[1, 2], 40]) == draganddrop.PositionsCompare([1, 3])
 
     def test_Eia(self):
         # Test cases. All of these should return True
-        self.assertTrue(eia.iseia(100))  # 100 ohm resistor is EIA
-        self.assertFalse(eia.iseia(101))  # 101 is not
+        assert eia.iseia(100)  # 100 ohm resistor is EIA
+        assert not eia.iseia(101)  # 101 is not
