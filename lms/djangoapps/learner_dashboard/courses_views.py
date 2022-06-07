69c9f63a65355d3111a3f47ae24700e69e5ae912
nsprenkle <nsprenkle@2u.com>
2022-06-07 10:16:48 -0600

    style: fix pylint ignore

--- lms/djangoapps/learner_dashboard/courses_views.py
+++ lms/djangoapps/learner_dashboard/courses_views.py
@@ -9,7 +9,7 @@ from common.djangoapps.util.json_request import JsonResponse
 
 @login_required
 @require_GET
-def course_listing(request):  #pylint: disable=unused-argument
+def course_listing(request):  # pylint: disable=unused-argument
     """ List of courses a user is enrolled in or entitled to """
     course_cards = []
     return JsonResponse(course_cards)
