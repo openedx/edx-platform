fd6b726a687005f8cc55e65a53b5411504587bc3
Alexander Sheehan <asheehan@edx.org>
2022-06-08 14:58:26 -0700

    fix: accounting for non-unique entity ID on idp configs + fixing provider data bulk update bug

--- common/djangoapps/third_party_auth/samlproviderconfig/serializers.py
+++ common/djangoapps/third_party_auth/samlproviderconfig/serializers.py
@@ -14,6 +14,18 @@ class SAMLProviderConfigSerializer(serializers.ModelSerializer):  # lint-amnesty
         model = SAMLProviderConfig
         fields = '__all__'
 
+    def validate(self, data):
+        """
+        Validate that no provider config exists with a different slug and same entity ID
+        """
+        # If there are any existing provider configs that match the payload's entity ID, don't match the slug and
+        # are not archived, raise a validation error. We do this to prevent provider configs from sharing entity ID's
+        # which link a provider config to provider data (SAML certificates). An entity ID therefore, is uniquely linked
+        # to a single slug/provider config (which in the case of enterprise provider slug == customer slug).
+        if SAMLProviderConfig.objects.filter(entity_id=data['entity_id'], archived=False).exclude(slug=data['slug']):
+            raise serializers.ValidationError(f"Entity ID: {data['entity_id']} already taken")
+        return data
+
     def create(self, validated_data):
         """
         Overwriting create in order to get a SAMLConfiguration object from id.
@@ -27,7 +39,6 @@ class SAMLProviderConfigSerializer(serializers.ModelSerializer):  # lint-amnesty
         return SAMLProviderConfig.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-
         if 'saml_config_id' in validated_data:
             saml_configuration = SAMLConfiguration.objects.current_set().get(id=validated_data['saml_config_id'])
             del validated_data['saml_config_id']
