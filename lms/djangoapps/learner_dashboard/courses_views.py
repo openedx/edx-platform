cf6ea15ffffa16df9893ceea161294f08bf9644b
Nathan Sprenkle <nsprenkle@users.noreply.github.com>
2022-06-09 11:22:04 -0400

    feat: add course listing alongside learner dashboard (#30553)
    * refactor: split programs into separate file space
    
    This is in preparation to allow learner dashboard routes/files to live
    alongside.
    
    * feat: add new empty course listing view
    
    * docs: update README and split out programs
    
    Co-authored-by: nsprenkle <nsprenkle@2u.com>

---
+++ lms/djangoapps/learner_dashboard/courses_views.py
@@ -0,0 +1,15 @@
+"""
+Views for the learner dashboard.
+"""
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET
+
+from common.djangoapps.util.json_request import JsonResponse
+
+
+@login_required
+@require_GET
+def course_listing(request):  # pylint: disable=unused-argument
+    """ List of courses a user is enrolled in or entitled to """
+    course_cards = []
+    return JsonResponse(course_cards)
