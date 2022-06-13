205f72b11b748b3b9ee57f095736ad79c6fe91b4
Hasnain Saeed <69143815+hasnain-saeed@users.noreply.github.com>
2022-06-13 19:23:08 +0500

    Add user authentication in api

--- openedx/features/genplus_features/genplus/api/v1/urls.py
+++ openedx/features/genplus_features/genplus/api/v1/urls.py
@@ -3,8 +3,12 @@ URLs for genplus core API v1.
 """
 from django.conf.urls import url
 
+from .views import (
+    UserInfo,
+)
 
 app_name = 'genplus_api_v1'
 
-urlpatterns = (
-)
+urlpatterns = [
+    url(r'^userinfo/$', UserInfo.as_view()),
+]
