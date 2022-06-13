c28e9ffa5aabfa6b43529a84b1e8c031f16525eb
Sagirov Eugeniy <sagirov19@gmail.com>
2022-06-13 16:43:30 +0300

    feat: Take the Library Content xblocks out the Advanced section to the top default level

--- cms/djangoapps/contentstore/views/tests/test_library.py
+++ cms/djangoapps/contentstore/views/tests/test_library.py
@@ -337,6 +337,7 @@ class UnitTestLibraries(CourseTestCase):
         self.assertNotIn('discussion', templates)
         self.assertNotIn('advanced', templates)
         self.assertNotIn('openassessment', templates)
+        self.assertNotIn('library', templates)
 
     def test_advanced_problem_types(self):
         """
