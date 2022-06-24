2c077e89590a8da00fd26dcd7b95072f24f8702e
nsprenkle <nsprenkle@2u.com>
2022-06-24 13:37:46 -0400

    test: add learner dashboard views test

---
+++ lms/djangoapps/learner_dashboard/test_learner_views.py
@@ -0,0 +1,67 @@
+"""Test for learner views"""
+
+import json
+
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from common.djangoapps.student.tests.factories import UserFactory
+from xmodule.modulestore.tests.django_utils import (
+    TEST_DATA_SPLIT_MODULESTORE,
+    SharedModuleStoreTestCase,
+)
+from xmodule.modulestore.tests.factories import CourseFactory
+
+
+class TestDashboardView(SharedModuleStoreTestCase, APITestCase):
+    """Tests for the dashboard view"""
+
+    MODULESTORE = TEST_DATA_SPLIT_MODULESTORE
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # Get view URL
+        cls.view_url = reverse("learner_dash:dashboard_view")
+
+        # Set up a course
+        cls.course = CourseFactory.create()
+        cls.course_key = cls.course.location.course_key
+
+        # Set up a user
+        cls.username = "alan"
+        cls.password = "enigma"
+        cls.user = UserFactory(username=cls.username, password=cls.password)
+
+    def log_in(self):
+        """Log in as a test user"""
+        self.client.login(username=self.username, password=self.password)
+
+    def setUp(self):
+        super().setUp()
+        self.log_in()
+
+    def test_response_structure(self):
+        """Basic test for correct response structure"""
+
+        # Given I am logged in
+        self.log_in()
+
+        # When I request the dashboard
+        response = self.client.get(self.view_url)
+
+        # Then I get the expected success response
+        assert response.status_code == 200
+
+        response_data = json.loads(response.content)
+        expected_keys = set(
+            [
+                "edx",
+                "enrollments",
+                "unfulfilledEntitlements",
+                "suggestedCourses",
+            ]
+        )
+
+        assert expected_keys == response_data.keys()
