b13ea579aff7767304f352273a15a9ccb151c519
nsprenkle <nsprenkle@2u.com>
2022-06-24 13:37:17 -0400

    fix: add missing import

--- lms/djangoapps/learner_dashboard/learner_views.py
+++ lms/djangoapps/learner_dashboard/learner_views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
 
 from common.djangoapps.util.json_request import JsonResponse
-from lms.djangoapps.learner_dashboard.learner_apis import get_platform_settings
+from lms.djangoapps.learner_dashboard.learner_api import get_platform_settings
 from lms.djangoapps.learner_dashboard.serializers import LearnerDashboardSerializer
 
 
