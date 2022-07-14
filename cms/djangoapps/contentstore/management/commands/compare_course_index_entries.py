0d43608a9b848490fda0131922dfe93ff57974f1
connorhaugh <chaugh21@amherst.edu>
2022-07-14 14:46:54 -0400

    feat: make loggin more verbose

--- cms/djangoapps/contentstore/management/commands/compare_course_index_entries.py
+++ cms/djangoapps/contentstore/management/commands/compare_course_index_entries.py
@@ -1,12 +1,18 @@
 import logging
-from xmodule.modulestore.split_mongo.mongo_connection import MongoPersistenceBackend, DjangoFlexPersistenceBackend
-from xmodule.modulestore.django import modulestore
 from django.core.management.base import BaseCommand
-
 from django.conf import settings
+from opaque_keys.edx.locator import CourseLocator
+from common.djangoapps.split_modulestore_django.models import SplitModulestoreCourseIndex
+from xmodule.modulestore.split_mongo.mongo_connection import MongoPersistenceBackend, DjangoFlexPersistenceBackend
+
+
+
 
 logger = logging.getLogger(__name__)
 
+"""
+A Command to determine if the Mongo active_versions and Django course_index tables are out of sync with one another.
+"""
 
 class Command(BaseCommand):
 
@@ -15,9 +21,9 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        module_store = modulestore()
-        courses = module_store.get_courses()
-        course_ids = [x.id for x in courses]
+
+        courses = SplitModulestoreCourseIndex.objects.values('course_id')
+        course_ids = [x['course_id'] for x in courses if isinstance(x['course_id'], CourseLocator)]
         out_of_sync = {}
 
         mongostore = MongoPersistenceBackend(**settings.CONTENTSTORE['DOC_STORE_CONFIG'])
@@ -26,21 +32,32 @@ class Command(BaseCommand):
         def compare_entries(mongo_entry, msql_entry, course_id):
             draft = False
             published = False
-            if(mongo_entry["draft-branch"] != msql_entry["draft-branch"]):
+            if mongo_entry["draft-branch"] != msql_entry["draft-branch"]:
                 draft = True
-            if(mongo_entry["published-branch"] != msql_entry["published-branch"]):
+            if mongo_entry["published-branch"] != msql_entry["published-branch"]:
                 published = True
-            if (draft | published):
-                out_of_sync[course_id._to_string()] = (draft, published)
-            return
+            if draft | published:
+                out_of_sync[str(course_id)] = (
+                    mongo_entry["draft-branch"],
+                    mongo_entry["published-branch"],
+                    msql_entry["draft-branch"],
+                    msql_entry["published-branch"],
+                    mongostore.get_course_index(key=course_id)['last_update'],
+                    msqlstore.get_course_index(key=course_id)['last_update'],
+                )
 
-        for course_id in course_ids:
+        for course_id in sorted(course_ids):
             mongo_entry = mongostore.get_course_index(key=course_id)['versions']
             msql_entry = msqlstore.get_course_index(key=course_id)['versions']
             compare_entries(mongo_entry, msql_entry, course_id)
 
         print(out_of_sync)
-
         for course in out_of_sync:
-            print(f'id: {course} is out of Sync in Draft:{out_of_sync[course][0]} in Published: {out_of_sync[course][1]}')
+            print('*'*8)
+            print(f'{course} is out of sync in course_index tables')
+            print(f'MONGO draft     id: {out_of_sync[course][0]}')
+            print(f'MONGO published id: {out_of_sync[course][1]}')
+            print(f'MYSQL draft     id: {out_of_sync[course][2]}')
+            print(f'MYSQL published id: {out_of_sync[course][3]}')
+        print('*'*8)
         print(f'The number of out-of-sync courses is : {len(out_of_sync)}')
