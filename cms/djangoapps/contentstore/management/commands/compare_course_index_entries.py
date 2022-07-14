0dcef3abb741372e95fba41e16b725e5da7bb202
connorhaugh <chaugh21@amherst.edu>
2022-07-14 12:20:46 -0400

    fix: lint fixes

--- cms/djangoapps/contentstore/management/commands/compare_course_index_entries.py
+++ cms/djangoapps/contentstore/management/commands/compare_course_index_entries.py
@@ -5,10 +5,7 @@ from django.core.management.base import BaseCommand
 
 from django.conf import settings
 
-
 logger = logging.getLogger(__name__)
-
-
 
 class Command(BaseCommand):
 
@@ -25,26 +22,24 @@ class Command(BaseCommand):
         mongostore = MongoPersistenceBackend(**settings.CONTENTSTORE['DOC_STORE_CONFIG'])
         msqlstore = DjangoFlexPersistenceBackend(**settings.CONTENTSTORE['DOC_STORE_CONFIG'])
 
-        def compare_entries(mongo_entry,msql_entry, course_id):
+        def compare_entries(mongo_entry, msql_entry, course_id):
             draft = False
             published = False
             if(mongo_entry["draft-branch"] != msql_entry["draft-branch"]):
                 draft = True
             if(mongo_entry["published-branch"] != msql_entry["published-branch"]):
                 published = True
-            if (draft|published):
-                out_of_sync[course_id._to_string()] = (draft,published)
+            if (draft | published):
+                out_of_sync[course_id._to_string()] = (draft, published)
             return
 
         for course_id in course_ids:
             mongo_entry = mongostore.get_course_index(key=course_id)['versions']
             msql_entry = msqlstore.get_course_index(key=course_id)['versions']
-            compare_entries(mongo_entry,msql_entry,course_id)
+            compare_entries(mongo_entry, msql_entry, course_id)
 
         print(out_of_sync)
 
         for course in out_of_sync:
             print(f'id: {course} is out of Sync in Draft:{out_of_sync[course][0]} in Published: {out_of_sync[course][1]}')
         print(f'The number of out-of-sync courses is : {len(out_of_sync)}')
-
-
