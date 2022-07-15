ac9d34d3d971f5ed6dd346e4a65dab538b88288a
shiv <shivam.sharma@synoriq.in>
2022-07-15 19:11:03 +0530

    live_empt

--- lms/static/js/dashboard/legacy.js
+++ lms/static/js/dashboard/legacy.js
@@ -309,123 +309,51 @@
          });
      };
 
-     $(document).ready(if($("#live_classes_list").is("visible")){
-       {
-            alert("Id is visible");
+  $(document).ready(if($("#live_classes_list").is("visible")){
+         alert("Id is visible");
+//         $.ajax({
+//          type: 'GET',
+//          url: '/api/enrollment/v1/enrollment/live_class/enroll',
+//          data: $(this).serializeArray(),
+//          success: function(response) {
+//          var output = document.getElementById('output');
+//          output.style.display='block';
+//          var response = JSON.parse(JSON.stringify(response.results));
+//          for(let i=0;i<response.length;i++){
+//              if(response[i]['live_class']!=undefined && response[i]['live_class']!=null ){
+//                   var parent = document.createElement("div");
+//                   parent.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+//                   parent.setAttribute("style", "background: #fff; box-shadow: 0 2px 7px 0 rgb(0, 0, 0,.8%); overflow: hidden; border-radius: 8px; margin-bottom: 24px");
+//                   var image = document.createElement("img");
+//                   image.setAttribute('src', 'http://via.placeholder.com/220x120');
+//                   var ele = document.createElement("div");
+//          };
+//        ele.setAttribute("style", "font-size: 1.2em; font-weight: 900; color:#000;");
+//        ele.setAttribute("id","timedrpact"+i);
+//        ele.setAttribute("class","inner");
+//        ele.appendChild(image);
+//        const span = document.createElement("span");
+//        span.innerText = response[i]['live_class']["topic_name"];
+//        span.style.marginLeft = '40px';
+//        ele.appendChild(span);
+//        parent.appendChild(ele);
+//        var ele1= document.createElement("a");
+//        ele1.setAttribute("id","timedrpact1"+i);
+//        ele1.setAttribute("class","button inner-link");
+//        ele1.setAttribute("href",response[i]['live_class']["meeting_link"]);
+//        ele1.setAttribute("target",'_black');
+//        ele1.setAttribute("style", "font-size: 18px; margin-right: 20px; box-shadow: 0px 5px 0px #ee6100; border: none; color:#fff; background: #ff7f27; border-radius: 12px; padding: 12px 20px; background-image: none; text-shadow: 0 0");
+//        ele1.innerHTML='Join Class';
+//        parent.appendChild(ele1);
+//        output.appendChild(parent);
+//
+//
+//          };
+//
+//         }, 500});
 
-            $.ajax({
-                type: 'GET',
-                url: '/api/enrollment/v1/enrollment/live_class/enroll',
-                data: $(this).serializeArray(),
-                success: function(response) {
-                    //alert(JSON.stringify(response))
-                    // setTimeout(
-                    //   function(){
-                        var output = document.getElementById('output');
-                        output.style.display='block';
-                        // var response = JSON.parse(JSON.stringify(response))
-                        var response = JSON.parse(JSON.stringify(response.results))
-
-                        for(let i=0;i<response.length;i++)
-                          {
-                              if(response[i]['live_class']!=undefined && response[i]['live_class']!=null )
-                              {
-                                var parent = document.createElement("div");
-                                parent.classList.add('d-flex', 'justify-content-between', 'align-items-center',)
-                                parent.setAttribute("style", "background: #fff; box-shadow: 0 2px 7px 0 rgb(0, 0, 0,.8%); overflow: hidden; border-radius: 8px; margin-bottom: 24px");
-                                var image = document.createElement("img");
-                                image.setAttribute('src', 'http://via.placeholder.com/220x120')
-                                var ele = document.createElement("div");
-                                ele.setAttribute(
-                                  "style", "font-size: 1.2em; font-weight: 900; color:#000;");
-                                ele.setAttribute("id","timedrpact"+i);
-                                ele.setAttribute("class","inner");
-                                ele.appendChild(image);
-                                const span = document.createElement("span");
-                                span.innerText = response[i]['live_class']["topic_name"];
-                                span.style.marginLeft = '40px';
-                                ele.appendChild(span);
-                                parent.appendChild(ele);
-                                var ele1= document.createElement("a");
-                                ele1.setAttribute("id","timedrpact1"+i);
-                                ele1.setAttribute("class","button inner-link");
-                                ele1.setAttribute("href",response[i]['live_class']["meeting_link"]);
-                                ele1.setAttribute("target",'_black');
-                                ele1.setAttribute(
-                                  "style", "font-size: 18px; margin-right: 20px; box-shadow: 0px 5px 0px #ee6100; border: none; color:#fff; background: #ff7f27; border-radius: 12px; padding: 12px 20px; background-image: none; text-shadow: 0 0");
-                                ele1.innerHTML='Join Class';
-                                parent.appendChild(ele1);
-                                output.appendChild(parent);
-                              }
+  });
 
 
-                          }
 
-                     }, 500); // adding timeout to make spinner animation longer
-                }
-            });
-        }
-     });
-      $("#asde_2").click(function live_class(){
-      $.ajax({
-                type: 'GET',
-                url: '/api/enrollment/v1/enrollment/live_class/enroll',
-                data: $(this).serializeArray(),
-                success: function(response) {
-                    //alert(JSON.stringify(response))
-                    // setTimeout(
-                    //   function(){
-                        var output = document.getElementById('output');
-                        output.style.display='block';
-                        // var response = JSON.parse(JSON.stringify(response))
-                        var response = JSON.parse(JSON.stringify(response.results))
-
-                        for(let i=0;i<response.length;i++)
-                          {
-                              if(response[i]['live_class']!=undefined && response[i]['live_class']!=null )
-                              {
-                                var parent = document.createElement("div");
-                                parent.classList.add('d-flex', 'justify-content-between', 'align-items-center',)
-                                parent.setAttribute("style", "background: #fff; box-shadow: 0 2px 7px 0 rgb(0, 0, 0,.8%); overflow: hidden; border-radius: 8px; margin-bottom: 24px");
-                                var image = document.createElement("img");
-                                image.setAttribute('src', 'http://via.placeholder.com/220x120')
-                                var ele = document.createElement("div");
-                                ele.setAttribute(
-                                  "style", "font-size: 1.2em; font-weight: 900; color:#000;");
-                                ele.setAttribute("id","timedrpact"+i);
-                                ele.setAttribute("class","inner");
-                                ele.appendChild(image);
-                                const span = document.createElement("span");
-                                span.innerText = response[i]['live_class']["topic_name"];
-                                span.style.marginLeft = '40px';
-                                ele.appendChild(span);
-                                parent.appendChild(ele);
-                                var ele1= document.createElement("a");
-                                ele1.setAttribute("id","timedrpact1"+i);
-                                ele1.setAttribute("class","button inner-link");
-                                ele1.setAttribute("href",response[i]['live_class']["meeting_link"]);
-                                ele1.setAttribute("target",'_black');
-                                ele1.setAttribute(
-                                  "style", "font-size: 18px; margin-right: 20px; box-shadow: 0px 5px 0px #ee6100; border: none; color:#fff; background: #ff7f27; border-radius: 12px; padding: 12px 20px; background-image: none; text-shadow: 0 0");
-                                ele1.innerHTML='Join Class';
-                                parent.appendChild(ele1);
-                                output.appendChild(parent);
-                              }
-
-
-                          }
-
-                     }, 500); // adding timeout to make spinner animation longer
-                }
-            });
-      var data = {"name":"shiv"};
-        $.get("", function(data, status){
-            alert("here is data", data);
-            alert('here is status', status);
-            })
-      }
-      );
-     $(".asde").click(function callAlert(){
-     alert('calling alert updated new');
-     })
  }(jQuery, gettext, Logger, accessible_modal, interpolate));
