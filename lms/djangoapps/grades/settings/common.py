73193b069da13f0e393d18556fe6c6c521bab746
jansenk <jkantor@edx.org>
2022-07-25 14:40:21 -0400

    fixup! temp: move recalculate_course_and_subsection_grades_for_user to new queue

--- lms/djangoapps/grades/settings/common.py
+++ lms/djangoapps/grades/settings/common.py
@@ -5,6 +5,6 @@ def plugin_settings(settings):
 
     # Queue to use for updating grades due to grading policy change
     settings.POLICY_CHANGE_GRADES_ROUTING_KEY = settings.DEFAULT_PRIORITY_QUEUE
-    
+
     # Queue to use for individual learner course regrades
     settings.SINGLE_LEARNER_COURSE_REGRADE_ROUTING_KEY = settings.DEFAULT_PRIORITY_QUEUE
