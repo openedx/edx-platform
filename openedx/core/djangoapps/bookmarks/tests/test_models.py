db96c43b607e1dbdca3b08f34f949f36c9812f5d
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-08-02 17:29:46 +0500

    chore: updates

--- openedx/core/djangoapps/bookmarks/tests/test_models.py
+++ openedx/core/djangoapps/bookmarks/tests/test_models.py
@@ -27,7 +27,8 @@ from .factories import BookmarkFactory
 EXAMPLE_USAGE_KEY_1 = 'i4x://org.15/course_15/chapter/Week_1'
 EXAMPLE_USAGE_KEY_2 = 'i4x://org.15/course_15/chapter/Week_2'
 
-noop_contextmanager = contextmanager(lambda x: (yield))  # noqa: E275
+# noqa: E275
+noop_contextmanager = contextmanager(lambda x: (yield))
 
 
 class BookmarksTestsBase(ModuleStoreTestCase):
