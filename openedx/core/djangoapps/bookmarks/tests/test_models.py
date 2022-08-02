07f6292139a3ea28f230b261516c5a522e9a869e
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-08-02 17:20:22 +0500

    fix: fix

--- openedx/core/djangoapps/bookmarks/tests/test_models.py
+++ openedx/core/djangoapps/bookmarks/tests/test_models.py
@@ -27,9 +27,8 @@ from .factories import BookmarkFactory
 EXAMPLE_USAGE_KEY_1 = 'i4x://org.15/course_15/chapter/Week_1'
 EXAMPLE_USAGE_KEY_2 = 'i4x://org.15/course_15/chapter/Week_2'
 
-# noqa: E275
-noop_contextmanager = contextmanager(lambda x: (yield)) # noqa: E275  # pylint: disable=invalid-name
-# noqa: E275
+noop_contextmanager = contextmanager(lambda x: (yield))  # noqa: E275
+
 
 class BookmarksTestsBase(ModuleStoreTestCase):
     """
