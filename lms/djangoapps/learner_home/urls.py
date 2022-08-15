36d0eaf604be7dbae319556631959538e3be6ef2
nsprenkle <nsprenkle@2u.com>
2022-08-15 11:59:04 -0400

    feat: add mock data to mock BFF

--- lms/djangoapps/learner_home/urls.py
+++ lms/djangoapps/learner_home/urls.py
@@ -9,5 +9,5 @@ app_name = "learner_home"
 # Learner Dashboard Routing
 urlpatterns = [
     path("home/", views.dashboard_view, name="dashboard_view"),
-    path("mock/home", mock_views.dashboard_view),
+    path("mock/home/", mock_views.dashboard_view),
 ]
