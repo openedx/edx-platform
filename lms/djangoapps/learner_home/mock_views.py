48e03e74575b6131bf5b75b4ffd005065ec9dc08
Nathan Sprenkle <nsprenkle@users.noreply.github.com>
2022-08-16 12:59:32 -0400

    feat: learner home mock bff (#30858)

---
+++ lms/djangoapps/learner_home/mock_views.py
@@ -0,0 +1,27 @@
+"""
+Mock implementation of the Learner Home.
+Returns statically authored JSON data
+"""
+# pylint: disable=line-too-long
+
+import json
+from os import path
+
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET
+from common.djangoapps.util.json_request import JsonResponse
+
+LEARNER_HOME_DIR = "/edx/app/edxapp/edx-platform/lms/djangoapps/learner_home"
+MOCK_DATA_FILE = "mock_data.json"
+
+
+@login_required
+@require_GET
+def dashboard_view(request):  # pylint: disable=unused-argument
+    """Mock dashboard view, returns static JSON authored in MOCK_DATA_FILE"""
+    with open(path.join(LEARNER_HOME_DIR, MOCK_DATA_FILE), "r") as mock_data_file:
+
+        # Edit me to change response data
+        mock_data = json.load(mock_data_file)
+
+    return JsonResponse(mock_data)
