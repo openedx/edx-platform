c14b087789d58cb21a723e5f66fb517cc08f017d
Ben Warzeski <bwarzeski@edx.org>
2022-08-23 16:22:52 -0400

    fix: update learner home api

--- lms/djangoapps/learner_home/mock_views.py
+++ lms/djangoapps/learner_home/mock_views.py
@@ -7,21 +7,23 @@ Returns statically authored JSON data
 import json
 from os import path
 
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_GET
-from common.djangoapps.util.json_request import JsonResponse
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveAPIView
+
+import logging
+
+log = logging.getLogger(__name__)
 
 LEARNER_HOME_DIR = "/edx/app/edxapp/edx-platform/lms/djangoapps/learner_home"
 MOCK_DATA_FILE = "mock_data.json"
 
 
-@login_required
-@require_GET
-def dashboard_view(request):  # pylint: disable=unused-argument
-    """Mock dashboard view, returns static JSON authored in MOCK_DATA_FILE"""
-    with open(path.join(LEARNER_HOME_DIR, MOCK_DATA_FILE), "r") as mock_data_file:
+class InitializeView(RetrieveAPIView):
+    """Returns static JSON authored in MOCK_DATA_FILE"""
+    def get(self, request):
+        with open(path.join(LEARNER_HOME_DIR, MOCK_DATA_FILE), "r") as mock_data_file:
 
-        # Edit me to change response data
-        mock_data = json.load(mock_data_file)
+            # Edit me to change response data
+            mock_data = json.load(mock_data_file)
+        return Response(mock_data)
 
-    return JsonResponse(mock_data)
--- lms/urls.py
+++ lms/urls.py
@@ -196,7 +196,7 @@ urlpatterns = [
     path('api/dashboard/', include('lms.djangoapps.learner_dashboard.api.urls', namespace='dashboard_api')),
 
     # Learner Home
-    path('learner/', include('lms.djangoapps.learner_home.urls', namespace='learner_home')),
+    path('api/learner/', include('lms.djangoapps.learner_home.urls', namespace='learner_home')),
 
     path(
         'api/experiments/',
