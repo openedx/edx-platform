openedx/edx-platform

commit 46619cce8102fa4ba25a0275de5a98cb9a07165f

Remove UserOrganizationMapping from openedx

openedx/core/djangoapps/user_authn/views/password_reset.py
@@ -25,6 +25,7 @@ from edx_ace import ace
 from edx_ace.recipient import Recipient
 from eventtracking import tracker
 from rest_framework.views import APIView
+from tahoe_sites.api import get_organization_user_by_email
 
 from edxmako.shortcuts import render_to_string
 from openedx.core.djangoapps.ace_common.template_context import get_base_template_context
@@ -47,8 +48,6 @@ from util.password_policy_validators import normalize_password, validate_passwor
 from util.request_rate_limiter import PasswordResetEmailRateLimiter
 
 from openedx.core.djangoapps.appsembler.tahoe_idp import helpers as tahoe_idp_helpers
-
-from organizations.models import UserOrganizationMapping
 
 from tahoe_idp import api as tahoe_idp_api
 
@@ -169,12 +168,13 @@ class PasswordResetFormNoActive(PasswordResetForm):
         if settings.FEATURES.get('APPSEMBLER_MULTI_TENANT_EMAILS', False):
             from openedx.core.djangoapps.appsembler.sites.utils import get_current_organization
             current_org = get_current_organization()
-            found_mappings = UserOrganizationMapping.objects.filter(
-                user__email__iexact=email,
-                organization=current_org,
-                is_active=True,
-            )
-            self.users_cache = [mapping.user for mapping in found_mappings]
+            try:
+                # The following will raise an exception if many records found in the organization for this email
+                one_user = get_organization_user_by_email(email=email, organization=current_org, fail_if_inactive=True)
+            except User.DoesNotExist:
+                self.users_cache = []
+            else:
+                self.users_cache = [one_user]
         else:
             self.users_cache = User.objects.filter(email__iexact=email)
 
@@ -540,7 +540,7 @@ def _get_user_from_email(email):
     """
     if settings.FEATURES.get('APPSEMBLER_MULTI_TENANT_EMAILS', False):
         current_org = get_current_organization()
-        return current_org.userorganizationmapping_set.get(user__email=email).user
+        return get_organization_user_by_email(email=email, organization=current_org)
 
     try:
         return User.objects.get(email=email)

common/djangoapps/student/signals/receivers.py
@@ -61,8 +61,8 @@ def on_user_updated(sender, instance, **kwargs):
                     #
                     # Chicken-and-egg:
                     #   It's impossible to get the organization for a user that we didn't save yet
-                    #   to the database because we don't have `UserOrganizationMapping` yet.
-                    #   We cannot have `UserOrganizationMapping` until we save the user,
+                    #   to the database because we don't have a link between the user and the organization yet.
+                    #   We cannot have that link until we save the user,
                     #   therefore it's not possible to get the organization from the user.
                     check_within_organization=bool(organization),
                     organization=organization,

openedx/core/djangoapps/appsembler/analytics/helpers.py
@@ -2,7 +2,7 @@
 Helpers for the Appsembler Analytics app.
 """
 
-from organizations.models import UserOrganizationMapping
+from tahoe_sites.api import deprecated_get_admin_users_queryset_by_email
 
 
 def should_show_hubspot(user):
@@ -15,8 +15,6 @@ def should_show_hubspot(user):
     if user.is_superuser or user.is_staff:
         return False
 
-    mapping = UserOrganizationMapping.objects.get(user=user)
-    if not (mapping.is_amc_admin and mapping.is_active):
-        return False
+    is_active_admin = user in deprecated_get_admin_users_queryset_by_email(email=user.email)
 
-    return True
+    return is_active_admin

openedx/core/djangoapps/appsembler/api/sites.py
@@ -2,8 +2,13 @@ import beeline
 import logging
 from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
-from organizations.models import Organization, OrganizationCourse
+
 from tahoe_sites.api import get_organization_by_site, get_users_of_organization
+
+from organizations.models import (
+    Organization,
+    OrganizationCourse,
+)
 
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 

openedx/core/djangoapps/appsembler/api/tests/test_enrollment_api.py
@@ -30,8 +30,8 @@ from student.tests.factories import CourseEnrollmentFactory, UserFactory
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
 
-from organizations.models import UserOrganizationMapping
 from tahoe_sites.tests.utils import create_organization_mapping
+from tahoe_sites.api import get_users_of_organization
 
 from openedx.core.djangoapps.appsembler.api.sites import (
     get_enrollments_for_site,
@@ -227,12 +227,10 @@ class EnrollmentApiPostTest(BaseEnrollmentApiTestCase):
             assert not CourseEnrollmentAllowed.objects.filter(email=new_user_email).exists()
 
         before_my_site_ce_count = get_enrollments_for_site(self.my_site).count()
-        before_my_site_user_count = UserOrganizationMapping.objects.filter(
-            organization=self.my_site_org).count()
+        before_my_site_user_count = get_users_of_organization(organization=self.my_site_org).count()
 
         before_other_site_ce_count = get_enrollments_for_site(self.other_site).count()
-        before_other_site_user_count = UserOrganizationMapping.objects.filter(
-            organization=self.other_site_org).count()
+        before_other_site_user_count = get_users_of_organization(organization=self.other_site_org).count()
 
         payload = {
             'action': 'enroll',
@@ -249,12 +247,10 @@ class EnrollmentApiPostTest(BaseEnrollmentApiTestCase):
         })
         results = response.data['results']
         after_my_site_ce_count = get_enrollments_for_site(self.my_site).count()
-        after_my_site_user_count = UserOrganizationMapping.objects.filter(
-            organization=self.my_site_org).count()
+        after_my_site_user_count = get_users_of_organization(organization=self.my_site_org).count()
 
         after_other_site_ce_count = get_enrollments_for_site(self.other_site).count()
-        after_other_site_user_count = UserOrganizationMapping.objects.filter(
-            organization=self.other_site_org).count()
+        after_other_site_user_count = get_users_of_organization(organization=self.other_site_org).count()
 
         assert after_other_site_ce_count == before_other_site_ce_count
         assert after_other_site_user_count == before_other_site_user_count

openedx/core/djangoapps/appsembler/multi_tenant_emails/tests/test_amc_signup.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from organizations.models import UserOrganizationMapping
+from tahoe_sites.api import get_organization_for_user, is_active_admin_on_organization
 
 from .test_utils import lms_multi_tenant_test, with_organization_context
 
@@ -124,23 +124,25 @@ class MultiTenantAMCSignupTest(APITestCase):
     def test_learner_invited_for_existing_organization(self, _mock_add_creator):
         red_site = 'red1'
         learner_email = 'learner@example.com'
-        queryset = UserOrganizationMapping.objects.filter(user__email=learner_email)
-        assert not queryset.exists(), 'Sanity check to ensure correct testing'
 
+        assert not User.objects.filter(email=learner_email).exists()
         with with_organization_context(site_color=red_site):
             self.register_learner(learner_email, 'learner')
 
-        mapping = UserOrganizationMapping.objects.get(user__email=learner_email)
-        assert not mapping.is_amc_admin, 'Should be just a learner'
+        user = User.objects.get(email=learner_email)
+        user.is_active = True
+        user.save()  # Simulate account activation via email
+
+        organization = get_organization_for_user(user=user)
+        assert not is_active_admin_on_organization(user=user, organization=organization), 'Should be just a learner'
 
         # Perform the step after clicking the invite link in the email from the AMC.
         # This is usually implemented in the SetPasswordView
         response = self.trial_step_1_admin_user(
             color=red_site,
             email=learner_email,
-            username=mapping.user.username,
+            username=user.username,
             send_organization=True,
         )
         assert response.status_code == status.HTTP_200_OK, response.content
-        mapping.refresh_from_db()
-        assert mapping.is_amc_admin, 'Should now be a site admin'
+        assert is_active_admin_on_organization(user=user, organization=organization), 'Should now be a site admin'

openedx/core/djangoapps/appsembler/multi_tenant_emails/tests/test_utils.py
@@ -9,8 +9,11 @@ from django.conf import settings
 from unittest import skipUnless
 from openedx.core.djangolib.testing.utils import skip_unless_lms
 
-from organizations.models import Organization, UserOrganizationMapping
 from tahoe_sites.api import create_tahoe_site_by_link
+
+from organizations.models import Organization
+from tahoe_sites.tests.utils import create_organization_mapping
+
 from openedx.core.djangoapps.site_configuration.tests.test_util import with_site_configuration_context
 
 from student.tests.factories import UserFactory
@@ -54,7 +57,7 @@ def create_org_user(organization, is_amc_admin=False, **kwargs):
     Create one user and save it to the database.
     """
     user = UserFactory.create(**kwargs)
-    UserOrganizationMapping.objects.create(user=user, organization=organization, is_amc_admin=is_amc_admin)
+    create_organization_mapping(user=user, organization=organization, is_admin=is_amc_admin)
     return user
 
 

openedx/core/djangoapps/appsembler/sites/admin.py
@@ -9,11 +9,11 @@ from django.utils.html import format_html
 from hijack_admin.admin import HijackUserAdminMixin
 from ratelimitbackend import admin
 from student.admin import UserAdmin
+from organizations.models import Organization
+from tahoe_sites.api import get_organization_for_user
 
 from openedx.core.djangolib.markup import HTML, Text
 from openedx.core.djangoapps.appsembler.sites.models import AlternativeDomain
-from organizations.models import UserOrganizationMapping
-from tahoe_sites.api import get_organization_for_user
 
 from openedx.core.djangoapps.appsembler.sites.forms import MakeAMCAdminForm
 from openedx.core.djangoapps.appsembler.sites.utils import (
@@ -49,7 +49,7 @@ class TahoeUserAdmin(UserAdmin, HijackUserAdminMixin):
         try:
             current_org = get_organization_for_user(user)
             current_org_name = current_org.name
-        except (UserOrganizationMapping.DoesNotExist, MultipleObjectsReturned):
+        except (Organization.DoesNotExist, MultipleObjectsReturned):
             current_org_name = ''
 
         form = MakeAMCAdminForm({'organization_name': current_org_name})

commit 48c0b7da9ebfca6e1c7fb36c522ae36b7b9f15e5

MTE / tahoe-sites: allow sending password reset emails for inactive users
Fixes password email being withheld due to `user.is_active == False`
against the desired use case.

This bug was introduced by the commit: 46619cce8102fa4ba25a0275de5a98cb9a07165f

The tests didn't catch the bug because wasn't enabled in the API tests: APPSEMBLER_MULTI_TENANT_EMAILS

Changes
=======

 - ensured that tests fail properly and catch the error by enabling APPSEMBLER_MULTI_TENANT_EMAILS
 - set `fail_if_inactive=False` to fix the regression bug caused by 46619cce8102fa4ba25a0275de5a98cb9a07165f

openedx/core/djangoapps/user_authn/views/password_reset.py
@@ -170,7 +170,8 @@ class PasswordResetFormNoActive(PasswordResetForm):
             current_org = get_current_organization()
             try:
                 # The following will raise an exception if many records found in the organization for this email
-                one_user = get_organization_user_by_email(email=email, organization=current_org, fail_if_inactive=True)
+                one_user = get_organization_user_by_email(email=email, organization=current_org,
+                                                          fail_if_inactive=False)
             except User.DoesNotExist:
                 self.users_cache = []
             else:

openedx/core/djangoapps/appsembler/api/tests/test_registration_api.py
@@ -24,9 +24,6 @@ APPSEMBLER_API_VIEWS_MODULE = 'openedx.core.djangoapps.appsembler.api.v1.views'
 @patch(APPSEMBLER_API_VIEWS_MODULE + '.RegistrationViewSet.authentication_classes', [])
 @patch(APPSEMBLER_API_VIEWS_MODULE + '.RegistrationViewSet.permission_classes', [AllowAny])
 @patch(APPSEMBLER_API_VIEWS_MODULE + '.RegistrationViewSet.throttle_classes', [])
-@override_settings(APPSEMBLER_FEATURES={
-    'SKIP_LOGIN_AFTER_REGISTRATION': False,
-})
 class RegistrationApiViewTests(TestCase):
     def setUp(self):
 

openedx/core/djangoapps/appsembler/api/tests/test_registration_api_v2.py
@@ -6,28 +6,35 @@ from django.contrib.auth.models import User
 from django.core import mail
 from django.urls import reverse
 from django.test import TestCase
-from django.test.utils import override_settings
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 
 import ddt
 from mock import patch
 
+import tahoe_sites.api
+from openedx.core.djangoapps.appsembler.multi_tenant_emails.tests.test_utils import with_organization_context
 from openedx.core.djangoapps.site_configuration.tests.factories import SiteFactory
+from openedx.core.djangoapps.site_configuration.tests.test_util import with_site_configuration
 
 APPSEMBLER_API_VIEWS_MODULE_V2 = 'openedx.core.djangoapps.appsembler.api.v2.views'
+DUMMY_SITE_DOMAIN = 'blue-org.localhost'
 
 
 @ddt.ddt
 @patch(APPSEMBLER_API_VIEWS_MODULE_V2 + '.RegistrationViewSet.authentication_classes', [])
 @patch(APPSEMBLER_API_VIEWS_MODULE_V2 + '.RegistrationViewSet.permission_classes', [AllowAny])
 @patch(APPSEMBLER_API_VIEWS_MODULE_V2 + '.RegistrationViewSet.throttle_classes', [])
-@override_settings(APPSEMBLER_FEATURES={
-    'SKIP_LOGIN_AFTER_REGISTRATION': False,
-})
+@patch.dict('django.conf.settings.FEATURES', {'APPSEMBLER_MULTI_TENANT_EMAILS': True})
 class RegistrationApiViewTestsV2(TestCase):
     def setUp(self):
-        self.site = SiteFactory()
+        super().setUp()
+        # Emulate Multi-Tenant Emails in production.
+        org_data = tahoe_sites.api.create_tahoe_site(short_name='blue-org', domain=DUMMY_SITE_DOMAIN)
+        self.org_patcher = patch('openedx.core.djangoapps.appsembler.sites.utils._get_current_organization',
+                                 return_value=org_data['organization'])
+        self.org_patcher.start()
+
         # The DRF Router appends '-list' to the base 'registrations' name when
         # registering the endpoint
         self.url = reverse('tahoe-api:v2:registrations-list')
@@ -37,6 +44,10 @@ class RegistrationApiViewTestsV2(TestCase):
             'email': 'mr.robot@example.com',
             'name': 'Mr Robot'
         }
+
+    def tearDown(self):
+        super().tearDown()
+        self.org_patcher.stop()
 
     def test_registration_response(self):
         res = self.client.post(self.url, self.sample_user_data)
@@ -130,10 +141,12 @@ class RegistrationApiViewTestsV2(TestCase):
             'email': 'mr_potato_head@example.com',
             'name': 'Mr Potato Head',
         }
-        with patch('openedx.core.djangoapps.user_authn.views.password_reset.get_current_site',
-                   return_value=self.site):
-            res = self.client.post(self.url, params)
+        res = self.client.post(self.url, params)
         self.assertContains(res, 'user_id', status_code=status.HTTP_200_OK)
+        assert res.json()['password_reset_email_was_sent'], res.json()
+        new_user = User.objects.get(username=params['username'])
+        assert not new_user.is_active
+        assert len(mail.outbox) == 1, res.json()
 
     @ddt.data(
         {'send_activation_email': True},
@@ -157,14 +170,12 @@ class RegistrationApiViewTestsV2(TestCase):
         }
         params.update(activation_email_params)
 
-        with patch('openedx.core.djangoapps.user_authn.views.password_reset.get_current_site',
-                   return_value=self.site):
-            res = self.client.post(self.url, params)
+        res = self.client.post(self.url, params)
 
         self.assertContains(res, 'user_id', status_code=200)
         new_user = User.objects.get(username=params['username'])
         assert not new_user.is_active
-        assert mail.outbox, 'Should send the password reset email'
+        assert mail.outbox, 'Should send the password reset email: {}'.format(res.json())
 
         assert len(mail.outbox) == 1, ('Send NOT password reset email but NOT activation.\n'
                                        'Subjects={subjects}.\n'
@@ -175,6 +186,7 @@ class RegistrationApiViewTestsV2(TestCase):
 
         email = mail.outbox[0]
         assert 'Password reset on' in email.subject
+        assert res.json()['password_reset_email_was_sent'], res.json()
 
     @ddt.unpack
     @ddt.data(

openedx/core/djangoapps/appsembler/api/tests/test_throttling.py
@@ -33,9 +33,6 @@ def make_post_data(val):
 @ddt.ddt
 @patch(APPSEMBLER_API_VIEWS_MODULE + '.RegistrationViewSet.authentication_classes', [])
 @patch(APPSEMBLER_API_VIEWS_MODULE + '.RegistrationViewSet.permission_classes', [AllowAny])
-@override_settings(APPSEMBLER_FEATURES={
-    'SKIP_LOGIN_AFTER_REGISTRATION': False,
-})
 @override_settings(CACHES={
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',