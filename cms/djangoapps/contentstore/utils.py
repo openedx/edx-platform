d111be537519dc78b8604c6eebfd0c4e992c97b2
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2022-09-09 13:57:50 -0400

    feat: add check for new video editor waffle flag

--- cms/djangoapps/contentstore/utils.py
+++ cms/djangoapps/contentstore/utils.py
@@ -26,7 +26,7 @@ from openedx.core.djangoapps.site_configuration import helpers as configuration_
 from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
 from openedx.features.content_type_gating.models import ContentTypeGatingConfig
 from openedx.features.content_type_gating.partitions import CONTENT_TYPE_GATING_SCHEME
-from cms.djangoapps.contentstore.toggles import use_new_text_editor
+from cms.djangoapps.contentstore.toggles import use_new_text_editor, use_new_video_editor
 from xmodule.modulestore import ModuleStoreEnum  # lint-amnesty, pylint: disable=wrong-import-order
 from xmodule.modulestore.django import modulestore  # lint-amnesty, pylint: disable=wrong-import-order
 from xmodule.modulestore.exceptions import ItemNotFoundError  # lint-amnesty, pylint: disable=wrong-import-order
@@ -203,7 +203,7 @@ def get_editor_page_base_url(course_locator) -> str:
     Gets course authoring microfrontend URL for links to the new base editors
     """
     editor_url = None
-    if use_new_text_editor():
+    if use_new_text_editor() or use_new_video_editor():
         mfe_base_url = get_course_authoring_url(course_locator)
         course_mfe_url = f'{mfe_base_url}/course/{course_locator}/editor'
         if mfe_base_url:
