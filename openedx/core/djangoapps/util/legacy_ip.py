c6ad3afcaf8001633dd6e9585b09e600859b88db
Tim McCormack <tmccormack@edx.org>
2022-09-20 20:47:29 +0000

    docs: Update toggle description to account for moved code

--- openedx/core/djangoapps/util/legacy_ip.py
+++ openedx/core/djangoapps/util/legacy_ip.py
@@ -16,16 +16,18 @@ from edx_toggles.toggles import WaffleSwitch
 # .. toggle_name: ip.legacy
 # .. toggle_implementation: WaffleSwitch
 # .. toggle_default: False
-# .. toggle_description: Emergency switch to revert to use the older, less secure method for
-#   IP determination. When enabled, instructs switch's callers to revert to using the *leftmost*
+# .. toggle_description: Emergency switch to revert to use an older, less secure method for
+#   IP determination (instead of the newer, safer code in ``edx_django_utils.ip``).
+#   When enabled, instructs switch's callers to revert to using the *leftmost*
 #   IP from the X-Forwarded-For header. When disabled (the default), callers should use the new
 #   code path for IP determination, which has callers retrieve the entire external chain or pick
 #   the leftmost or rightmost IP from it. The construction of the external chain is configurable
 #   via ``CLOSEST_CLIENT_IP_FROM_HEADERS``.
 #     This toggle, as well as any other legacy IP references, should be deleted (in the off
-#   position) when the new IP code is well-tested and all IP-reliant code has been switched over.
-# .. toggle_warning: This switch does not control the behavior of this module. Callers must
-#   opt into querying this switch, and can call ``get_legacy_ip`` if the switch is enabled.
+#   position) when the new IP code is well-tested and all IP-reliant code has been switched over
+#   to using ``edx_django_utils.ip``.
+# .. toggle_warning: This switch does not globally control handling of IP addresses; it only
+#   affects code that is explicitly querying the switch and using ``get_legacy_ip``.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2022-03-24
 # .. toggle_target_removal_date: 2023-01-01
