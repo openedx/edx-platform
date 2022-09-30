54b5d59fc3264037b78474441bec660590f59598
SaadYousaf <saadyousaf@A006-00314.local>
2022-09-30 14:51:37 +0500

    fix: filter out discussion topics that have not started yet.

--- lms/djangoapps/discussion/rest_api/api.py
+++ lms/djangoapps/discussion/rest_api/api.py
@@ -5,10 +5,13 @@ from __future__ import annotations
 
 import itertools
 from collections import defaultdict
+from datetime import datetime
 
 from enum import Enum
 from typing import Dict, Iterable, List, Literal, Optional, Set, Tuple
 from urllib.parse import urlencode, urlunparse
+from pytz import UTC
+
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
@@ -378,10 +381,13 @@ def get_courseware_topics(
     courseware_topics = []
     existing_topic_ids = set()
 
+    now = datetime.now(UTC)
+
     discussion_xblocks = get_accessible_discussion_xblocks(course, request.user)
     xblocks_by_category = defaultdict(list)
     for xblock in discussion_xblocks:
-        xblocks_by_category[xblock.discussion_category].append(xblock)
+        if xblock.start < now:
+            xblocks_by_category[xblock.discussion_category].append(xblock)
 
     for category in xblocks_by_category.keys():
         children = []
