35c62c3eb0f7f547abf148706b9397d95a07164d
moeez96 <moeezzahid1996@gmail.com>
2022-10-13 16:06:36 +0500

    refactor: Modify annotation description

--- openedx/core/djangoapps/oauth_dispatch/toggles.py
+++ openedx/core/djangoapps/oauth_dispatch/toggles.py
@@ -10,8 +10,11 @@ OAUTH_DISPATCH_WAFFLE_SWITCH_NAMESPACE = 'oauth_dispatch'
 # .. toggle_name: DISABLE_JWT_FOR_MOBILE
 # .. toggle_implementation: WaffleSwitch
 # .. toggle_default: False
-# .. toggle_description: Toggle to control creation of JWT token for mobile
-#   field during development.
+# .. toggle_description: Set toggle to True to ignore mobile requests
+#   for JWTs, and instead provide the legacy opaque Bearer token. This
+#   toggle can be used as a temporary kill switch as the mobile app
+#   attempts to rollout the transition to JWT authentication for the
+#   first time.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2022-10-14
 # .. toggle_target_removal_date: 2022-12-30
