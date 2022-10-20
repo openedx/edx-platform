901581cfa6668b9f5c8129a6e9fd574099e62142
Henrry Pulgarin <39854568+Henrrypg@users.noreply.github.com>
2022-10-20 07:43:20 -0500

    feat: add cli command to generate report (#277)
    Co-authored-by: Alejandro Cardenas <alecar.main@gmail.com>

---
+++ openedx/features/survey_report/application.py
@@ -0,0 +1,6 @@
+"""
+Contains the logic for manage a survey report.
+"""
+
+def generate_report() -> None:
+    """ Generate a report with relevant data."""
---
+++ openedx/features/survey_report/management/commands/tests/test_generate_report.py
@@ -0,0 +1,13 @@
+"""
+Test for generate_report command.
+"""
+
+from io import StringIO
+from django.core.management import call_command
+from django.test import TestCase
+
+class GenerateReportTest(TestCase):
+    def test_command_output(self):
+        out = StringIO()
+        call_command('generate_report', stdout=out)
+        self.assertIn('Survey report has been generated successfully.', out.getvalue())
