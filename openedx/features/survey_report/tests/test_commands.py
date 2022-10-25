40d4435381a6761b6433a3227e19499e54a0a3d7
henrrypg <henrry.pulgarin@edunext.co>
2022-10-25 13:23:04 -0500

    test get unique couirses offered

--- openedx/features/survey_report/tests/test_commands.py
+++ openedx/features/survey_report/tests/test_commands.py
@@ -21,14 +21,6 @@ log = logging.getLogger(__name__)
 
 class TestSurveyReportCommands(ModuleStoreTestCase):
 
-    # def setUp(self):
-    #     super().setUp()
-        # self.user = UserFactory.create(username='test_user', email='test@example.com', password='password')
-        # self.user1 = UserFactory.create(username='test_user1', email='test1@example.com', password='password')
-        # self.user2 = UserFactory.create(username='test_user2', email='test2@example.com', password='password')
-        # self.user3 = UserFactory.create(username='test_user3', email='test3@example.com', password='password')
-        # self.user4 = UserFactory.create(username='test_user4', email='test4@example.com', password='password')
-
     def setUp(self):
         """
         We set up some content here, without publish signals enabled.
@@ -41,15 +33,23 @@ class TestSurveyReportCommands(ModuleStoreTestCase):
         self.second_course = CourseFactory.create(
             org="test", course="course2", display_name="run2", default_store=ModuleStoreEnum.Type.mongo
         )
+        self.user = UserFactory.create(username='test_user', email='test@example.com', password='password')
+        self.user1 = UserFactory.create(username='test_user1', email='test1@example.com', password='password')
+        self.user2 = UserFactory.create(username='test_user2', email='test2@example.com', password='password')
+        self.user3 = UserFactory.create(username='test_user3', email='test3@example.com', password='password')
+        self.user4 = UserFactory.create(username='test_user4', email='test4@example.com', password='password')
 
     def test_get_unique_courses_offered(self):
         """
         Test that get_unique_courses_offered returns the correct number of courses.
         """
-        # course_overview = CourseOverviewFactory.create(id=self.course.id)
-        # CourseEnrollmentFactory.create(user=self.user, course_id=course_overview.id)
-        # with patch('openedx.features.survey_report.queries.datetime') as mock_datetime:
-        #     mock_datetime.now.return_value = datetime.now()
-        #     mock_datetime.now.return_value = mock_datetime.now.return_value - timedelta(days=1)
-        log.critical(get_unique_courses_offered())
-        #     assert get_unique_courses_offered() == 2
+        course_overview = CourseOverviewFactory.create(id=self.first_course.id)
+        CourseEnrollmentFactory.create(user=self.user, course_id=course_overview.id)
+        CourseEnrollmentFactory.create(user=self.user1, course_id=course_overview.id)
+        CourseEnrollmentFactory.create(user=self.user2, course_id=course_overview.id)
+        CourseEnrollmentFactory.create(user=self.user3, course_id=course_overview.id)
+        CourseEnrollmentFactory.create(user=self.user4, course_id=course_overview.id)
+        with patch('openedx.features.survey_report.queries.datetime') as mock_datetime:
+            mock_datetime.now.return_value = datetime.now()
+            mock_datetime.now.return_value = mock_datetime.now.return_value - timedelta(days=1)
+            assert get_unique_courses_offered() == 1
