9fc6cacf7978068cd4eaba887b51501b653f3b61
Alejandro Cardenas <alecar.main@gmail.com>
2022-11-03 15:11:47 -0500

    refactor: remove fields useless

--- openedx/features/survey_report/models.py
+++ openedx/features/survey_report/models.py
@@ -22,9 +22,7 @@ class SurveyReport(models.Model):
         default=dict,
         help_text="Extra information for instance data",
     )
-    request_details = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now=True)
-    sent_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         ordering = ["-created_at"]
