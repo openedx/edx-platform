05c2fc3a664d36819cc6820f22f695c7e9aedcef
Rahat Ali <rahat.ali@edly.io>
2022-11-04 11:31:46 +0500

    Student profile image and fullname added (#95)
    * Student profile image and fullname added
    
    * Full profile url added

--- openedx/features/genplus_features/genplus_assessments/api/v1/serializers.py
+++ openedx/features/genplus_features/genplus_assessments/api/v1/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers
 
 from openedx.features.genplus_features.genplus.models import Student
@@ -25,16 +26,27 @@ class ClassUnitSerializer(serializers.ModelSerializer):
 class ClassStudentSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
     user_id = serializers.CharField(source='user.id')
+    full_name = serializers.SerializerMethodField()
+    profile_pic_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Student
-        fields = ('id', 'user_id', 'username')
+        fields = ('id', 'user_id', 'username', 'full_name', 'profile_pic_url')
+
+    def get_full_name(self, obj):
+        return obj.gen_user.user.get_full_name()
+
+    def get_profile_pic_url(self, obj):
+        if obj.character is not None and obj.character.profile_pic is not None:
+            return f"{settings.LMS_ROOT_URL}{obj.character.profile_pic.url}"
+        else:
+            return None
 
 
 class ClassSerializer(serializers.ModelSerializer):
     students = ClassStudentSerializer(many=True, read_only=True)
     class_units = ClassUnitSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Class
         fields = ('group_id', 'name', 'students', 'class_units')
