e727b282231880e36c67484a915c7ef753768a59
mubbsharanwar <mubbshar.anwar@arbisoft.com>
2022-11-09 16:56:08 +0500

    revert: revert redesign enrollment email - revert changes of redesign course enrollment email PR.
    VAN-1157

--- common/djangoapps/student/views/management.py
+++ common/djangoapps/student/views/management.py
@@ -387,8 +387,7 @@ def change_enrollment(request, check_access=True):
             try:
                 enroll_mode = CourseMode.auto_enroll_mode(course_id, available_modes)
                 if enroll_mode:
-                    CourseEnrollment.enroll(user, course_id, check_access=check_access,
-                                            mode=enroll_mode, request=request)
+                    CourseEnrollment.enroll(user, course_id, check_access=check_access, mode=enroll_mode)
             except Exception:  # pylint: disable=broad-except
                 return HttpResponseBadRequest(_("Could not enroll"))
 
