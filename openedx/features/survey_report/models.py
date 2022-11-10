de046f3bec4f235de2115f5923ca5f329777aed7
Alejandro Cardenas <alecar.main@gmail.com>
2022-11-10 15:39:57 -0500

    docs: add fields descriptions

--- openedx/features/survey_report/models.py
+++ openedx/features/survey_report/models.py
@@ -11,6 +11,14 @@ class SurveyReport(models.Model):
     This model stores information to automate the way of gathering impact data from the openedx project.
 
     .. no_pii:
+
+    fields:
+    - courses_offered: Total number of active unique courses.
+    - learner: Recently active users with login in some weeks.
+    - registered_learners: Total number of users ever registered in the platform.
+    - enrollments: Total number of active enrollments in the platform.
+    - generated_certificates: Total number of generated certificates.
+    - extra_data: Extra information that will be saved in the report, E.g: site_name, openedx-release.
     """
     courses_offered = models.BigIntegerField()
     learners = models.BigIntegerField()
