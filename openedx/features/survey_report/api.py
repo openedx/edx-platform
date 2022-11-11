83835688d483c60a3b67755203f7f9c8d11a7de0
Alejandro Cardenas <alecar.main@gmail.com>
2022-11-11 11:17:25 -0500

    refactor: rename variables and add a constant for weeks

--- openedx/features/survey_report/api.py
+++ openedx/features/survey_report/api.py
@@ -13,12 +13,14 @@ from openedx.features.survey_report.queries import (
     get_unique_courses_offered
 )
 
+MAX_WEEKS_SINCE_LAST_LOGIN: int = 4
+
 
 def generate_report() -> None:
     """ Generate a report with relevant data."""
     courses_offered = get_unique_courses_offered()
-    learners = get_recently_active_users(weeks=3)
-    registered = get_registered_learners()
+    learners = get_recently_active_users(weeks=MAX_WEEKS_SINCE_LAST_LOGIN)
+    registered_learners = get_registered_learners()
     certificates = get_generated_certificates()
     enrollments = get_course_enrollments()
     extra_data = settings.SURVEY_REPORT_EXTRA_DATA
@@ -26,7 +28,7 @@ def generate_report() -> None:
     survey_report = SurveyReport(
         courses_offered=courses_offered,
         learners=learners,
-        registered_learners=registered,
+        registered_learners=registered_learners,
         generated_certificates=certificates,
         enrollments=enrollments,
         extra_data=extra_data,
