eacaeba8d67aa3100d77be3478a542555756a227
Alejandro Cardenas <alecar.main@gmail.com>
2022-11-11 10:17:27 -0500

    docs: add logs for each query

--- openedx/features/survey_report/queries.py
+++ openedx/features/survey_report/queries.py
@@ -2,6 +2,7 @@
 Queries to get data from database.
 """
 
+import logging
 from datetime import datetime, timedelta
 
 from django.contrib.auth.models import User  # lint-amnesty, pylint: disable=imported-auth-user
@@ -12,13 +13,15 @@ from common.djangoapps.student.models import CourseEnrollment
 from lms.djangoapps.grades.models import PersistentCourseGrade
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
+log = logging.getLogger(__name__)
 
 def get_unique_courses_offered() -> int:
     """
     Get total number of unique course that started before today and have an open date,
     or have not finished yet, whose number of enrollments is greater than 5.
     """
-    return CourseOverview.objects.annotate(
+    log.info("Getting the total number of unique courses offered...")
+    total = CourseOverview.objects.annotate(
         count=Subquery(
             CourseEnrollment.objects
                             .filter(course_id=OuterRef('id'))
@@ -31,6 +34,8 @@ def get_unique_courses_offered() -> int:
         .filter(Q(end__isnull=True) | Q(end__gt=datetime.now()))\
         .using(read_replica_or_default())\
         .count()
+    log.info("Getting the total number of unique courses offered... DONE")
+    return total
 
 
 def get_recently_active_users(weeks: int) -> int:
@@ -40,33 +45,42 @@ def get_recently_active_users(weeks: int) -> int:
     Args:
         weeks (int): number of weeks since the last login to considerate as an active learner.
     """
-    return User.objects.filter(last_login__gte=datetime.now() - timedelta(weeks=weeks))\
+    log.info("Getting the total number of recently active users...")
+    total = User.objects.filter(last_login__gte=datetime.now() - timedelta(weeks=weeks))\
         .using(read_replica_or_default())\
         .count()
-
+    log.info("Getting total number of recently active users... DONE")
+    return total
 
 def get_registered_learners() -> int:
     """
     Get total number of active learners registered.
     """
-    return User.objects.filter(is_active=True)\
+    log.info("Getting the total number of ever registered learners...")
+    total = User.objects.filter(is_active=True)\
         .using(read_replica_or_default())\
         .count()
-
+    log.info("Getting the total number of ever registered learners... DONE")
+    return total
 
 def get_generated_certificates() -> int:
     """
     Get total number of generated certificates.
     """
-    return PersistentCourseGrade.objects.filter(passed_timestamp__isnull=False)\
+    log.info("Getting the total number of generated certificates...")
+    total = PersistentCourseGrade.objects.filter(passed_timestamp__isnull=False)\
         .using(read_replica_or_default())\
         .count()
-
+    log.info("Getting the total number of generated certificates... DONE")
+    return total
 
 def get_course_enrollments() -> int:
     """
     Get total number of enrollments from users that aren't staff. Course staff members will be included.
     """
-    return CourseEnrollment.objects.filter(is_active=True, user__is_superuser=False, user__is_staff=False)\
+    log.info("Getting the total number of course enrollments...")
+    total = CourseEnrollment.objects.filter(is_active=True, user__is_superuser=False, user__is_staff=False)\
         .using(read_replica_or_default())\
         .count()
+    log.info("Getting the total number of course enrollments... DONE")
+    return total
