bfd212b6d8d91e4cd03936a39fe4e362bd024e6c
Alejandro Cardenas <alecar.main@gmail.com>
2022-11-15 17:47:21 -0500

    [FC-005] feat: add necessary models for Openedx survey report (#31183)
    * feat: add survey_report djangoapp
    * feat: add survey report cli command and query methods
    * fix: add init file
    * refactor: change model fields
    * refactor: rename application file and rename methods
    * refactor: add is_active to get course enrollments
    * refactor: rename method to get active users
    * refactor: remove fields useless
    * test: rename mocks in command tests
    * test: update test name
    * docs: add README file
    * docs: add selection criteria to get unique courses
    * docs: update README
    * test: remove useless mocks and use default modulestore
    * docs: change command error message
    * docs: add docs decisions
    * docs: Update openedx/features/survey_report/management/commands/generate_report.py
    * docs: add fields descriptions
    * docs: add logs for each query
    * style: add blank lines
    * refactor: rename variables and add a constant for weeks
    * refactor: add constant MIN_ENROLLS_ACTIVE_COURSE
    
    
    Co-authored-by: henrrypg <henrry.pulgarin@edunext.co>
    Co-authored-by: David Ormsbee <dave@tcril.org>
    Co-authored-by: Maria Grimaldi <maria.grimaldi@edunext.co>

---
+++ openedx/features/survey_report/management/commands/generate_report.py
@@ -0,0 +1,31 @@
+"""
+CLI command to generate survey report.
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+
+from openedx.features.survey_report.api import generate_report
+
+
+class Command(BaseCommand):
+    """
+    Management command to generate a new survey report with
+    non-sensitive data.
+    """
+
+    help = """
+        This command will create a new survey report using some
+        models to get:
+        - Total number of courses offered
+        - Currently active learners
+        ...
+        learners ever registered, and generated certificates.
+        """
+
+    def handle(self, *_args, **_options):
+        try:
+            generate_report()
+        except Exception as error:
+            raise CommandError(f'An error has occurred while survey report was generating. {error}') from error
+
+        self.stdout.write(self.style.SUCCESS('Survey report has been generated successfully.'))
