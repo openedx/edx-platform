3f46f5394afb8d066cb64ac9e715e75c767174c7
Alejandro Cardenas <alecar.main@gmail.com>
2022-12-11 11:12:16 -0500

    style: fix style issues

--- openedx/features/survey_report/urls.py
+++ openedx/features/survey_report/urls.py
@@ -6,7 +6,9 @@ from django.urls import path
 from .views import SurveyReportView
 
 urlpatterns = [
-    path('generate_report', SurveyReportView.as_view(),
+    path(
+        'generate_report',
+        SurveyReportView.as_view(),
         name='openedx.generate_survey_report',
     ),
 ]
