469fad1bf0c1dd0cbe67a933c1af2c0d3d9e9c8a
Bryan Wilson <bryanlandia@gmail.com>
2022-12-19 15:56:32 -0800

    Test TahoeUserMetdataProcessor adds metadata getting user id from event, tests refactor

--- openedx/core/djangoapps/appsembler/eventtracking/tests/test_tahoeusermetadata.py
+++ openedx/core/djangoapps/appsembler/eventtracking/tests/test_tahoeusermetadata.py
@@ -1,6 +1,8 @@
 """Test the appsembler.eventtracking.tahoeusermetadata module."""
 
+from copy import deepcopy
 import factory
+import json
 from mock import MagicMock, patch
 import pytest
 
@@ -20,17 +22,28 @@ BASE_EVENT_WITH_CONTEXT = {
     "data": {}
 }
 
+TAHOE_USER_METADATA_CONTEXT = {
+    "tahoe_user_metadata": {
+        "registration_extra": {"custom_reg_field": "value1"}
+    }
+}
+
 
 class UserProfileWithMetadataFactory(UserProfileFactory):
     """Factory for UserProfile sequence with some tahoe_user_metadata."""
-    # TODO: a Sequence is a bit of a silly way to set things up.
-    meta = factory.Sequence(lambda n: {
-        "tahoe_user_metadata": {
-            "some_other_key": "some_other_val",
-            "registration_extra": {"custom_reg_field": "value{n}"}
-        }
-    } if n == 1 else {"tahoe_user_metadata": {}}
-    )
+
+    def _meta_val(n):
+        """Return a JSON meta value for Sequence member"""
+        reg_field_value = "value{}".format(n % 2)
+        meta_dict = {
+            "tahoe_idp_metadata": {
+                "registration_additional": {"custom_reg_field": reg_field_value}
+            }
+        } if n % 2 == 1 else {"tahoe_idp_metadata": {}}
+
+        return json.dumps(meta_dict)
+
+    meta = factory.Sequence(_meta_val)
 
 
 class UserWithTahoeMetadataFactory(UserFactory):
@@ -55,22 +68,33 @@ def processor():
 @pytest.mark.django_db
 def test_for_metadata_no_cache(users, base_event, processor):
     """Test happy path, Processor returns the event with user metadata in `context`."""
+    event_with_metadata = deepcopy(base_event)
+    event_with_metadata.update(context=TAHOE_USER_METADATA_CONTEXT)
+
     with patch(EVENTTRACKING_MODULE + '.tahoeusermetadata.get_current_user', MagicMock()) as mocked:
-        mocked.return_value = users[0]
-        base_event.update(context={
-            "tahoe_user_metadata": {
-                "some_other_key": "some_other_val",
-                "registration_extra": {"custom_reg_field": "value0"}
-            }
-        })
+        mocked.return_value = users[1]
         event = processor(base_event)
-        assert event == base_event
+        assert event == event_with_metadata
 
 
 @pytest.mark.django_db
 def test_no_context_added_if_no_metadata_of_interest(users, base_event, processor):
     """Test happy path, Processor returns the event with user metadata in `context`."""
     with patch(EVENTTRACKING_MODULE + '.tahoeusermetadata.get_current_user', MagicMock()) as mocked:
-        mocked.return_value = users[1]
+        mocked.return_value = users[0]
         event = processor(base_event)
         assert event == base_event
+
+
+@pytest.mark.django_db
+def test_get_user_from_db_when_not_avail_from_request(users, base_event, processor):
+    event_with_metadata = deepcopy(base_event)
+    event_with_metadata.update(context=TAHOE_USER_METADATA_CONTEXT)
+
+    with patch(EVENTTRACKING_MODULE + '.tahoeusermetadata.get_current_user', MagicMock()) as mocked:
+        mocked.return_value = None
+        event_with_user_id = deepcopy(base_event)
+        event_with_user_id.update({'user_id': users[1].id})
+        event_with_metadata.update({'user_id': users[1].id})
+        event = processor(event_with_user_id)
+        assert event == event_with_metadata
