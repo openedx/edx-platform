c5d4e19c7152ce6588d5298fa2143a7b9eacacee
nsprenkle <nsprenkle@2u.com>
2023-01-04 14:31:29 -0500

    refactor: move mocks into separate directory

--- lms/djangoapps/learner_home/urls.py
+++ lms/djangoapps/learner_home/urls.py
@@ -1,17 +1,15 @@
 """Learner home URL routing configuration"""
 
-from django.urls import re_path
+from django.urls import include, re_path
 
-from lms.djangoapps.learner_home import mock_views, views
+from lms.djangoapps.learner_home import views
 
 app_name = "learner_home"
 
 # Learner Dashboard Routing
 urlpatterns = [
     re_path(r"^init/?", views.InitializeView.as_view(), name="initialize"),
-    re_path(
-        r"^mock/init/?", mock_views.InitializeView.as_view(), name="mock_initialize"
-    ),
+    re_path(r"^mock/", include("lms.djangoapps.learner_home.mock.urls")),
     re_path(
         r"^recommendation/courses/$",
         views.CourseRecommendationApiView.as_view(),
