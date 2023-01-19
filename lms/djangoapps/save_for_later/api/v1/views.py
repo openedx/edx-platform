b01cf355a7b9bf395c1e5d5ee6858b179358c4e9
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2023-01-19 16:39:23 +0500

    Unpin django-ratelimit (#31416)
    * fix: fix ratelimit upgrade changes
    
    Co-authored-by: Awais Qureshi <awais.qureshi@arbisoft.com>

--- lms/djangoapps/save_for_later/api/v1/views.py
+++ lms/djangoapps/save_for_later/api/v1/views.py
@@ -6,7 +6,7 @@ import logging
 
 from django.conf import settings
 from django.utils.decorators import method_decorator
-from ratelimit.decorators import ratelimit
+from django_ratelimit.decorators import ratelimit
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.db import transaction
@@ -33,8 +33,12 @@ class CourseSaveForLaterApiView(APIView):
     """
 
     @transaction.atomic
-    @method_decorator(ratelimit(key=POST_EMAIL_KEY, rate=settings.SAVE_FOR_LATER_EMAIL_RATE_LIMIT, method='POST'))
-    @method_decorator(ratelimit(key=REAL_IP_KEY, rate=settings.SAVE_FOR_LATER_IP_RATE_LIMIT, method='POST'))
+    @method_decorator(ratelimit(key=POST_EMAIL_KEY,
+                                rate=settings.SAVE_FOR_LATER_EMAIL_RATE_LIMIT,
+                                method='POST', block=False))
+    @method_decorator(ratelimit(key=REAL_IP_KEY,
+                                rate=settings.SAVE_FOR_LATER_IP_RATE_LIMIT,
+                                method='POST', block=False))
     def post(self, request):
         """
         **Use Case**
@@ -124,8 +128,12 @@ class ProgramSaveForLaterApiView(APIView):
     """
 
     @transaction.atomic
-    @method_decorator(ratelimit(key=POST_EMAIL_KEY, rate=settings.SAVE_FOR_LATER_EMAIL_RATE_LIMIT, method='POST'))
-    @method_decorator(ratelimit(key=REAL_IP_KEY, rate=settings.SAVE_FOR_LATER_IP_RATE_LIMIT, method='POST'))
+    @method_decorator(ratelimit(key=POST_EMAIL_KEY,
+                                rate=settings.SAVE_FOR_LATER_EMAIL_RATE_LIMIT,
+                                method='POST', block=False))
+    @method_decorator(ratelimit(key=REAL_IP_KEY,
+                                rate=settings.SAVE_FOR_LATER_IP_RATE_LIMIT,
+                                method='POST', block=False))
     def post(self, request):
         """
         **Use Case**
