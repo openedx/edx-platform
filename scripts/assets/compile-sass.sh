ccaf14af4381937335530298233123c29e29376f
Kyle McCormick <kdmc@pm.me>
2023-01-25 12:33:06 -0500

    squash: make script work on multiple themes + systems
    currently failing on the red-theme

--- scripts/assets/compile-sass.sh
+++ scripts/assets/compile-sass.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#
-# Compile sass files.
+HELP="Compile SCSS files for LMS and CMS, including SCSS for zero or more themes."
 #
 # Run this from the root of edx-platform, after ... TOOD
 # but before ... TODO
@@ -10,16 +9,19 @@ set -eu
 
 USAGE="\
 USAGE:\n\
-    $0 [OPTIONS] lms [<THEME_DIR>]\n\
-    $0 [OPTIONS] cms [<THEME_DIR>]\n\
+    $0 [OPTIONS]\n\
 \n\
 OPTIONS:\n\
+	-t, --theme <THEME_PATH>                 Path to a custom theme. Can be provided multiple times.\n\
     -n, --node-modules <NODE_MODULES_PATH>   Path to installed node_modules directory.\n\
                                              Defaults to ./node_modules.\n\
-    -w, --watch                              Watch sass directories and compile and recompile whenever changed\n\
+    -w, --watch                              Watch SCSS directories and compile and recompile whenever changed\n\
+    -L, --skip-lms                           Don't compile LMS-specific SCSS.\n\
+    -C, --skip-cms                           Don't compile CMS-specific SCSS.\n\
+    -D, --skip-default-theme                 Don't compile SCSS for the default theme.\n\
     -f, --force                              Remove existing css before generating new css\n\
     -d, --dev                                Dev mode: whether to show source comments in resulting css\n\
-        --dry                                Dry run: don't do anything; just print what _would_ be done.\n\
+    -r, --dry                                Dry run: don't do anything; just print what _would_ be done.\n\
     -h, --help                               Display this.\n\
 "
 
@@ -27,23 +29,25 @@ OPTIONS:\n\
 # In --dry mode, these are overriden to 'echo <command>'
 rm='rm'
 sassc='sassc'
-rtlcss='rtlcss'
+rtlcss='echo rtlcss'  # TODO change
 
 # By default, we look for node_modules in the current directory.
 # Some Open edX distributions may want node_modules to be located somewhere
 # else, so we let this be configured with -n|--node-modules.
 node_modules_path="./node_modules"
 
-# system can be: lms or cms
-# theme_dir can be: a path, or empty
-system=""
-theme_dir=""
+# List of paths to custom themes, newline-separated.
+theme_paths=""
 
 # Flags.
 #  Empty string    (-z) => false
 #  Nonempty string (-n) => true
+skip_lms=""
+skip_cms=""
+skip_default_theme=""
 force=""
 watch=""
+echo_script_lines="T"
 
 # Output style arguments, to be passed to underlying
 # libsass complition command.
@@ -52,10 +56,22 @@ output_options="--output-style=compressed"
 # Parse arguments and options.
 while [ $# -gt 0 ]; do
 	case $1 in
+		-t|--theme)
+			shift
+			if [ $# -eq 0 ] || [ ! -d "$1" ]; then
+				echo "Error: provided theme path is not a directory: ${1:-}"
+				echo
+				echo "$USAGE"
+				exit 1
+			fi 
+			theme_paths="$theme_paths\n$1"
+			shift
+			;;
 		-n|--node-modules)
 			shift
 			if [ $# -eq 0 ]; then
 				echo "Error: Missing value for -n/--node-modules"
+				echo
 				echo "$USAGE"
 				exit 1
 			fi
@@ -66,68 +82,51 @@ while [ $# -gt 0 ]; do
 			watch="T"
 			shift
 			;;
+		-L|--skip-lms)
+			skip_lms="T"
+			shift
+			;;
+		-C|--skip-cms)
+			skip_cms="T"
+			shift
+			;;
+		-D|--skip-default-theme)
+			skip_default_theme="T"
+			shift
+			;;
 		-f|--force)
 			force="T"
 			shift
 			;;
-		-d|--debug)
+		-d|--dev)
 			output_options="--output-style=nested"
-			# TODO: When moving from `sass.compile(...)` to `sassc`, we had to drop
-			#       the " --source-comments" option here because it is not available
-			#       in libsass==0.10. After upgrading to libsass>=0.11, we should
-			#       add back " --source-comments" here.
+			# TODO: When moving from `sass.compile(...)` to `sassc`, we had to stop using
+			#       the " --source-comments" option because it is not available
+			#       in the `sassc` CLI under libsass==0.10. After upgrading to libsass>=0.11,
+			#       we should add back " --source-comments" when in --dev mode.
 			shift
 			;;
-		--dry)
+		-r|-dry)
 			rm="echo rm"
 			sassc="echo sassc"
 			rtlcss="echo rtlcss"
+			echo_script_lines=""
 			shift
 			;;
 		-h|--help)
+			echo "$HELP"
+			echo
 			echo "$USAGE"
 			exit 0
 			;;
-		-*)
+		*)
 			echo "Error: Unrecognized option: $1"
+			echo
 			echo "$USAGE"
 			exit 1
 			;;
-		*)  # Positional arguments. Can be supplied before, after, or between options.
-			# First argument: system
-			if [ -z "$system" ] ; then
-				if [ "$1" = "lms" ] || [ "$1" = "cms" ] ; then
-					system="$1"
-				else
-					echo "Error: expected 'lms' or 'cms' $1"
-					echo "$USAGE"
-					exit 1
-				fi
-			# Second argument: theme_dir
-			elif [ -z "$theme_dir" ] ; then
-				theme_dir="$1"
-			# Three or more arguments is an error.
-			else
-				echo "Error: unexpected argument: $1"
-				echo "$USAGE"
-				exit 1
-			fi
-			shift
-			;;
 	esac
 done
-
-# Input validation
-if [ -z "$system" ]; then
-	echo "Error: must specify 'lms' or 'cms'"
-	echo "$USAGE"
-	exit 1
-fi
-if [ -n "$theme_dir" ] && ! [ -d "$theme_dir" ]; then
-	echo "Error: provided theme directory is not a directory: $theme_dir"
-	echo "$USAGE"
-	exit 1
-fi 
 
 compile_dir ( ) {
 	scss_src="$1"
@@ -138,40 +137,38 @@ compile_dir ( ) {
 		$rm -f "$css_dest/*.css"
 	fi
 
-	# Navigate into sass_src and recursively print out relative paths for all SCSS
+	# Navigate into scss_src and recursively print out relative paths for all SCSS
 	# files, excluding underscore-prefixed ones.
 	for relpath in $(cd "$scss_src" && find . \( -name \*.scss -and \! -name _\* \)) ; do
 
 		# Compile one SCSS file into a CSS file.
-		# Note that sassc's $..._options arguments are not quoted, because they
+		# Note that scssc's $..._options arguments are not quoted, because they
 		# may contain multiple arguments, which we want to split apart rather than
-		# pass as one big argument. Shellcheck is not a fan of this, so:
-		#     shellcheck disable=2086
-		set -x
+		# pass as one big argument. Hence the shellcheck disable directive.
+		# shellcheck disable=2086
 		$sassc $output_options $include_path_options "$scss_src/$relpath" "$css_dest/$relpath"
-		set +x
 
 		# Generate converted RTL css too, if relevant.
 		reldir="$(dirname relpath)"
-		filename_no_ext="$(basename relpath | sed -n 's/.scss$//p')"
+		filename_no_ext="$(basename "$relpath" | sed -n 's/.scss$//p')"
 		case "$filename_no_ext" in
 			*-rtl)
 				# SCSS is already RTL; no need to generate extra RTL file.
 				;;
 			*)
 		 		# shellcheck disable=2086
-				echo TODO $rtlcss "$css_dest/$reldir/${filename_no_ext}.css" "$css_dest/$reldir/{$filename_no_ext}-rtl.css"
+				$rtlcss "$css_dest/$reldir/$filename_no_ext.css" "$css_dest/$reldir/$filename_no_ext-rtl.css"
 				;;
 		esac
 	done
 }
 echo "-------------------------------------------------------------------------"
 if [ -n "$watch" ] ; then
-	echo "Watching $system sass for changes..."
+	echo "Watching SCSS for changes..."
 	echo "ERROR: watching is not yet implemented"
 	exit 1
 else
-	echo "Compiling $system sass..."
+	echo "Compiling SCSS..."
 fi
 echo "  Working directory     : $(pwd)"
 echo "-------------------------------------------------------------------------"
@@ -182,58 +179,98 @@ common_include_paths=\
  --include-path=$node_modules_path\
  --include-path=$node_modules_path/@edx\
 "
-system_include_paths=\
+lms_include_paths=\
 "$common_include_paths\
- --include-path=$system/static/sass\
- --include-path=$system/static/sass/partials\
+ --include-path=lms/static/sass\
  --include-path=lms/static/sass/partials\
 "
-theme_system_include_paths=\
-"$system_include_paths\
- --include-path=$theme_dir/$system/static/sass/partials\
-"
-theme_certificate_include_paths=\
+cms_include_paths=\
 "$common_include_paths\
- --include-path=$theme_dir/lms/static/sass\
- --include-path=$theme_dir/lms/static/sass/partials\
+ --include-path=cms/static/sass\
+ --include-path=cms/static/sass/partials\
+ --include-path=lms/static/sass/partials\
 "
 
-if [ -n "$theme_dir" ] ; then
-	# Compile built-in SCSS into theme's CSS dir.
-	compile_dir \
-		"$system/static/sass" \
-		"$theme_dir/$system/static/css" \
-		"$theme_system_include_paths"
-	# Now override some or all of the built-in CSS by compiling the 
-	# theme's SCSS into its CSS dir.
-	compile_dir \
-		"$theme_dir/$system/static/sass" \
-		"$theme_dir/$system/static/css" \
-		"$theme_system_include_paths"
-	if [ "$system" = "lms" ] ; then
-		# Finally, for LMS only, compile the theme's certificates
-		# SCSS into its certificates CSS dir.
+if [ -n "$echo_script_lines" ] ; then
+	set -x
+fi
+
+if [ -z "$skip_default_theme" ] ; then
+	echo "Compiling SCSS for default theme..."
+	if [ -z "$skip_lms" ] ; then
 		compile_dir \
-			"$theme_dir/lms/static/certificates/sass" \
-			"$theme_dir/lms/static/certificates/css" \
-			"$theme_certificate_include_paths"
-	fi
-else
-	# Compile built-in SCSS into CSS dir.
-	compile_dir \
-		"$system/static/sass" \
-		"$system/static/css" \
-		"$system_include_paths"
-	if [ "$system" = "lms" ] ; then
-		# For LMS only, compile built-in certificate SCSS.
+			"lms/static/sass" \
+			"lms/static/css" \
+			"$lms_include_paths"
 		compile_dir \
 			"lms/static/certificates/sass" \
 			"lms/static/certificates/css" \
-			"$system_include_paths"
+			"$lms_include_paths"
 	fi
+	if [ -z "$skip_cms" ] ; then 
+		compile_dir \
+			"cms/static/sass" \
+			"cms/static/css" \
+			"$cms_include_paths"
+	fi
+	echo "Default theme SCSS compiled."
 fi
+
+echo "$theme_paths" | while read -r theme_path ; do
+
+	if [ -z "$theme_path" ] ; then
+		continue
+	fi
+
+	echo "Compiling SCSS for custom theme at $theme_path..."
+
+	theme_lms_include_paths=\
+"$lms_include_paths\
+ --include-path=$theme_path/lms/static/sass/partials\
+"
+	theme_certificate_include_paths=\
+"$common_include_paths\
+ --include-path=$theme_path/lms/static/sass\
+ --include-path=$theme_path/lms/static/sass/partials\
+"
+	theme_cms_include_paths=\
+"$cms_include_paths\
+ --include-path=$theme_path/cms/static/sass/partials\
+"
+	if [ -z "$skip_lms" ] ; then
+		# First, compile default LMS SCSS into theme's LMS CSS dir.
+		compile_dir \
+			"lms/static/sass"  \
+			"$theme_path/lms/static/css" \
+			"$theme_lms_include_paths"
+		# Then, override some/all default LMS CSS by compiling theme's LMS SCSS.
+		compile_dir \
+			"$theme_path/lms/static/sass" \
+			"$theme_path/lms/static/css" \
+			"$theme_lms_include_paths"
+		# Finally, compile the themed certificate SCSS into certificate CSS dir.
+		compile_dir \
+			"$theme_path/lms/static/certificates/sass" \
+			"$theme_path/lms/static/certificates/css" \
+			"$theme_certificate_include_paths"
+	fi
+	if [ -z "$skip_cms" ] ; then 
+		# Process for CMS is same as LMS, except no certificates.
+		compile_dir \
+			"cms/static/sass"  \
+			"$theme_path/cms/static/css" \
+			"$theme_cms_include_paths"
+		compile_dir \
+			"$theme_path/cms/static/sass" \
+			"$theme_path/cms/static/css" \
+			"$theme_cms_include_paths"
+	fi
+
+	echo "Done compiling SCSS for custom theme at $theme_path."
+done
+
 
 
 echo "-------------------------------------------------------------------------"
-echo "Done compiling $system sass."
+echo "Done compiling SCSS."
 echo "-------------------------------------------------------------------------"
