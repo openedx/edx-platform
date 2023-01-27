b2ca4e4044be8ffb68e3c1a62fa08bdd6baa71b1
Kyle McCormick <kdmc@pm.me>
2023-01-26 20:59:27 -0500

    squash: remove watch from compile-scss

--- scripts/assets/compile-scss.sh
+++ scripts/assets/compile-scss.sh
@@ -12,7 +12,6 @@ USAGE:\n\
     $0 [OPTIONS]\n\
 \n\
 OPTIONS:\n\
-    -w, --watch                              Watch SCSS directories and compile and recompile whenever changed\n\
 	-t, --theme <THEME_PATH>                 Path to a custom theme. Can be provided multiple times.\n\
                                              Defaults to ./node_modules.\n\
     -L, --skip-lms                           Don't compile LMS-specific SCSS.\n\
@@ -37,7 +36,6 @@ node_modules_path="./node_modules"
 # Flags.
 #  Empty string    (-z) => false
 #  Nonempty string (-n) => true
-watch=""
 skip_lms=""
 skip_cms=""
 skip_default_theme=""
@@ -49,10 +47,6 @@ dry=""
 # Parse arguments and options.
 while [ $# -gt 0 ]; do
 	case $1 in
-		-w|--watch)
-			watch="T"
-			shift
-			;;
 		-t|--theme)
 			shift
 			if [ $# -eq 0 ] || [ ! -d "$1" ]; then
@@ -118,7 +112,7 @@ while [ $# -gt 0 ]; do
 	esac
 done
 
-compile_or_watch_dir ( ) {
+compile_dir ( ) {
 
 	scss_src="$1"      # Dir containing SCSS input files.
 	css_dest="$2"      # Target dir for CSS output, to mirror input dir structure.
@@ -140,44 +134,19 @@ compile_or_watch_dir ( ) {
 		compile_scss_dir_command="$compile_scss_dir_command --verbose"
 	fi
 
-	if [ -n "$watch" ] ; then
-
-		echo "Watching directories for compilation: $scss_src -> $css_dest ..."
-		[ -z "$dry" ] &&
-			(echo "$scss_src" && echo "$css_dest" && (echo "$include_paths" | tr ':' '\n')) | \
-			# TODO: watchmedo is part of the Python watchdog library. We should switch to something
-			#       Python-free.
-			xargs \
-				watchmedo shell-command \
-					--patterns=*.scss \
-					--recursive \
-					"--command=$compile_scss_dir_command" &
-		echo " Watchers set up."
-
-	else
-
-		echo "Compiling directory: $scss_src -> $css_dest ..."
-		if [ -z "$watch" ] && [ -n "$force" ] ; then
-			echo " Removing old contents of $css_dest."
-			[ -z "$dry" ] && rm -f "$css_dest/*.css"
-		fi
-		# shellcheck disable=2086
-		[ -z "$dry" ] && \
-			$compile_scss_dir_command
-		echo " Done compiling: $scss_src -> $css_dest."
-
+	echo "Compiling directory: $scss_src -> $css_dest ..."
+	if [ -n "$force" ] ; then
+		echo " Removing old contents of $css_dest."
+		[ -z "$dry" ] && rm -f "$css_dest/*.css"
 	fi
+	# shellcheck disable=2086
+	[ -z "$dry" ] && \
+		$compile_scss_dir_command
+	echo " Done compiling: $scss_src -> $css_dest."
 }
 
-action="Compiling SCSS"
-action_lower="compiling SCSS"
-if [ -n "$watch" ] ; then
-	action="Starting watchers for SCSS"
-	action_lower="starting watchers for SCSS"
-fi
-
 echo "-------------------------------------------------------------------------"
-echo "  $action..."
+echo "  Compiling SCSS..."
 echo
 echo "  Working directory     : $(pwd)"
 echo "-------------------------------------------------------------------------"
@@ -205,24 +174,24 @@ if [ -n "$verbose" ] ; then
 fi
 
 if [ -z "$skip_default_theme" ] ; then
-	echo "$action for default theme..."
+	echo "Compiling SCSS for default theme..."
 	if [ -z "$skip_lms" ] ; then
-		compile_or_watch_dir \
+		compile_dir \
 			"lms/static/sass" \
 			"lms/static/css" \
 			"$lms_include_paths"
-		compile_or_watch_dir \
+		compile_dir \
 			"lms/static/certificates/sass" \
 			"lms/static/certificates/css" \
 			"$lms_include_paths"
 	fi
 	if [ -z "$skip_cms" ] ; then 
-		compile_or_watch_dir \
+		compile_dir \
 			"cms/static/sass" \
 			"cms/static/css" \
 			"$cms_include_paths"
 	fi
-	echo "Done $action_lower for default theme."
+	echo "Done compiling SCSS for default theme."
 fi
 
 echo "$theme_paths" | while read -r theme_path ; do
@@ -231,7 +200,7 @@ echo "$theme_paths" | while read -r theme_path ; do
 		continue
 	fi
 
-	echo "$action for custom theme at $theme_path..."
+	echo "Compiling SCSS for custom theme at $theme_path..."
 
 	theme_lms_include_paths=\
 "$lms_include_paths\
@@ -248,47 +217,37 @@ echo "$theme_paths" | while read -r theme_path ; do
 "
 	if [ -z "$skip_lms" ] ; then
 		# First, compile default LMS SCSS into theme's LMS CSS dir.
-		compile_or_watch_dir \
+		compile_dir \
 			"lms/static/sass"  \
 			"$theme_path/lms/static/css" \
 			"$theme_lms_include_paths"
 		# Then, override some/all default LMS CSS by compiling theme's LMS SCSS.
-		compile_or_watch_dir \
+		compile_dir \
 			"$theme_path/lms/static/sass" \
 			"$theme_path/lms/static/css" \
 			"$theme_lms_include_paths"
 		# Finally, compile the themed certificate SCSS into certificate CSS dir.
-		compile_or_watch_dir \
+		compile_dir \
 			"$theme_path/lms/static/certificates/sass" \
 			"$theme_path/lms/static/certificates/css" \
 			"$theme_certificate_include_paths"
 	fi
 	if [ -z "$skip_cms" ] ; then 
 		# Process for CMS is same as LMS, except no certificates.
-		compile_or_watch_dir \
+		compile_dir \
 			"cms/static/sass"  \
 			"$theme_path/cms/static/css" \
 			"$theme_cms_include_paths"
-		compile_or_watch_dir \
+		compile_dir \
 			"$theme_path/cms/static/sass" \
 			"$theme_path/cms/static/css" \
 			"$theme_cms_include_paths"
 	fi
 
-	echo "Done $action_lower for custom theme at $theme_path."
+	echo "Done compiling SCSS for custom theme at $theme_path."
 done
 
 echo "-------------------------------------------------------------------------"
-echo "  Done $action_lower."
+echo "  Done compiling SCSS."
 echo "-------------------------------------------------------------------------"
-
-if [ -n "$watch" ] ; then
-	# Kill all child processes (the SCSS watchers) upon exit.
-	#trap 'trap - TERM && kill -- -$$' INT TERM EXIT
-	trap "exit" INT TERM
-	trap "kill 0" EXIT
-
-	echo "Use Ctrl+c to stop watchers."
-	sleep infinity
-fi
 
