631cba1b987a8bde021405fb2434339d01dd4b28
Hasnain Saeed <hasnain.saeed@arbisoft.com>
2023-02-07 22:39:37 +0500

    Fix student name bug

--- openedx/features/genplus_features/genplus_learning/api/v1/serializers.py
+++ openedx/features/genplus_features/genplus_learning/api/v1/serializers.py
@@ -170,14 +170,15 @@ class ClassStudentSerializer(serializers.ModelSerializer):
         fields = ('id', 'user_id', 'username', 'has_first_logon', 'profile_pic', 'skills_assessment', 'unit_lesson_completion')
 
     def get_username(self, obj):
-        edx_user = obj.gen_user.user
-        return edx_user.get_full_name() if edx_user else obj.gen_user.email
+        user = obj.gen_user.user
+        if user and user.profile:
+            return user.profile.name
+        return obj.gen_user.email
 
     def get_has_first_logon(self, obj):
         if obj.gen_user.from_private_school:
             return obj.gen_user.user.last_login is not None
         return obj.gen_user.user is not None
-
 
     def get_profile_pic(self, obj):
         profile = obj.character.profile_pic if obj.character else None
