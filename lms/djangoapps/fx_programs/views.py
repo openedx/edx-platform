7cc131d02f41cb26a3b03e04e7936a86d45f90e8
quannguyen <hdragon1501@gmail.com>
2023-02-09 20:16:30 +0700

    FEAD/Add program view to LMS

---
+++ lms/djangoapps/fx_programs/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import RetrieveAPIView
+from common.djangoapps.edxmako.shortcuts import render_to_response
+
+from django.http import HttpResponse
+from django.template import loader
+import requests
+
+def index(request):
+    # if this is a POST request we need to process the form data
+    context = {
+        'status': '200'
+    }
+    response = render_to_response('programs_list.html', context)
+    return response
