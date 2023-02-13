151cbdc04a01726d2c162b587020eb6840422e32
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-02-13 23:56:15 +0500

    feat!: upgrade django-storage version.

--- lms/djangoapps/instructor/tests/test_api.py
+++ lms/djangoapps/instructor/tests/test_api.py
@@ -14,7 +14,7 @@ import dateutil
 import ddt
 import pytest
 import pytz
-from boto.exception import BotoServerError
+from botocore.exception import ClientError
 from django.conf import settings
 from django.contrib.auth.models import User  # lint-amnesty, pylint: disable=imported-auth-user
 from django.core import mail
@@ -2750,7 +2750,7 @@ class TestInstructorAPILevelsDataDump(SharedModuleStoreTestCase, LoginEnrollment
         ex_reason = 'Slow Down'
         url = reverse(endpoint, kwargs={'course_id': str(self.course.id)})
         with patch('storages.backends.s3boto3.S3Boto3Storage.listdir',
-                   side_effect=BotoServerError(ex_status, ex_reason)):
+                   side_effect=ClientError(error_code, error_message)):
             if endpoint in INSTRUCTOR_GET_ENDPOINTS:
                 response = self.client.get(url)
             else:
