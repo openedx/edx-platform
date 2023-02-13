e9d92922b5e3022db8f7dea207c21e8738cbd52f
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-02-14 01:03:08 +0500

    feat!: upgrade django-storage version.

--- cms/djangoapps/cms_user_tasks/tasks.py
+++ cms/djangoapps/cms_user_tasks/tasks.py
@@ -3,7 +3,7 @@ Celery tasks used by cms_user_tasks
 """
 import json
 
-from boto.exception import NoAuthHandlerFound
+import botocore
 from celery import shared_task
 from celery.exceptions import MaxRetriesExceededError
 from celery.utils.log import get_task_logger
@@ -52,7 +52,7 @@ def send_task_complete_email(self, task_name, task_state_text, dest_addr, detail
     try:
         mail.send_mail(subject, message, from_address, [dest_addr], fail_silently=False)
         LOGGER.info("Task complete email has been sent to User %s", dest_addr)
-    except NoAuthHandlerFound:
+    except botocore.exceptions.NoCredentialsError:
         LOGGER.info(
             'Retrying sending email to user %s, attempt # %s of %s',
             dest_addr,
--- cms/djangoapps/cms_user_tasks/tests.py
+++ cms/djangoapps/cms_user_tasks/tests.py
@@ -8,7 +8,7 @@ from unittest.mock import patch
 from uuid import uuid4
 
 import ddt
-from boto.exception import NoAuthHandlerFound
+import botocore
 from django.conf import settings
 from django.core import mail
 from django.test import override_settings
@@ -303,7 +303,7 @@ class TestUserTaskStopped(APITestCase):
         Make sure we can succeed on retries
         """
         with mock.patch('django.core.mail.send_mail') as mock_exception:
-            mock_exception.side_effect = NoAuthHandlerFound()
+            mock_exception.side_effect = botocore.exceptions.NoCredentialsError()
 
             with mock.patch('cms.djangoapps.cms_user_tasks.tasks.send_task_complete_email.retry') as mock_retry:
                 user_task_stopped.send(sender=UserTaskStatus, status=self.status)
@@ -315,7 +315,7 @@ class TestUserTaskStopped(APITestCase):
         logger.addHandler(hdlr)
 
         with mock.patch('cms.djangoapps.cms_user_tasks.tasks.send_task_complete_email.delay') as mock_delay:
-            mock_delay.side_effect = NoAuthHandlerFound()
+            mock_delay.side_effect = botocore.exceptions.NoCredentialsError()
             user_task_stopped.send(sender=UserTaskStatus, status=self.status)
             self.assertTrue(mock_delay.called)
             self.assertEqual(hdlr.messages['error'][0], 'Unable to queue send_task_complete_email')
