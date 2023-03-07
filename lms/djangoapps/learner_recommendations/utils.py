a99826b1ef44720ed92d1270e7f95acfa377e406
Shahbaz Shabbir <32649010+shahbaz-arbisoft@users.noreply.github.com>
2023-03-07 10:32:57 +0500

    revert: remove algolia API endpoint and unit tests from lms (#31868)

--- lms/djangoapps/learner_recommendations/utils.py
+++ lms/djangoapps/learner_recommendations/utils.py
@@ -4,7 +4,6 @@ Additional utilities for Learner Recommendations.
 import logging
 import requests
 
-from algoliasearch.exceptions import RequestException, AlgoliaUnreachableHostException
 from algoliasearch.search_client import SearchClient
 from django.conf import settings
 
@@ -112,48 +111,6 @@ def _get_program_duration(weeks):
 
     else:
         return f'{total_years} years {total_remainder_months} months'
-
-
-def get_algolia_courses_recommendation(course_data):
-    """
-    Get courses recommendation from Algolia search.
-
-    Args:
-        course_data (dict): Course data to create the search query.
-
-    Returns:
-        Response object with courses recommendation from Algolia search.
-    """
-    algolia_client = AlgoliaClient.get_algolia_client()
-
-    search_query = " ".join(course_data["skill_names"])
-    searchable_course_levels = [
-        f"level:{course_level}"
-        for course_level in COURSE_LEVELS
-        if course_level != course_data["level_type"]
-    ]
-    if algolia_client and search_query:
-        algolia_index = algolia_client.init_index(settings.ALGOLIA_COURSES_RECOMMENDATION_INDEX_NAME)
-        try:
-            # Algolia search filter criteria:
-            # - Product type: Course
-            # - Courses are available (enrollable)
-            # - Courses should not have the same course level as the current course
-            # - Exclude current course from the results
-            results = algolia_index.search(
-                search_query,
-                {
-                    "filters": f"NOT active_run_key:'{course_data['key']}'",
-                    "facetFilters": ["availability:Available now", "product:Course", searchable_course_levels],
-                    "optionalWords": f"{search_query}",
-                }
-            )
-
-            return results
-        except (AlgoliaUnreachableHostException, RequestException) as ex:
-            log.warning(f"Unexpected exception while attempting to fetch courses data from Algolia: {str(ex)}")
-
-    return {}
 
 
 def get_amplitude_course_recommendations(user_id, recommendation_id):
