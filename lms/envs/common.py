0f02c7b3d90292a87f7e6ea15b244a2de18d1a52
Sagirov Evgeniy <34642612+UvgenGen@users.noreply.github.com>
2023-03-07 14:41:52 +0200

    FC-0001: Account pages -> micro-frontend (#30336)
    * feat: Account pages. Learner Profile page
    
    * feat: Account pages. Account Settings page
    
    * feat: Account pages. Removed unused styles
    
    * feat: Account pages. Removed unused toggles
    
    * feat: fixed tests and pylint errors
    
    * feat: update redirect to account settings for student_dashboard
    
    * feat: fix pylint errors

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3196,7 +3196,6 @@ INSTALLED_APPS = [
     'openedx.features.course_bookmarks',
     'openedx.features.course_experience',
     'openedx.features.enterprise_support.apps.EnterpriseSupportConfig',
-    'openedx.features.learner_profile',
     'openedx.features.course_duration_limits',
     'openedx.features.content_type_gating',
     'openedx.features.discounts',
