13c850385e5ed6afdb5f3b91d3d9b8aaa24cd82e
Muhammad Abdullah Waheed <42172960+abdullahwaheed@users.noreply.github.com>
2023-03-08 14:41:48 +0500

    Revert "Revert "FC-0001: Account pages -> micro-frontend (#30336)" (#31888)"
    This reverts commit 90c4ca6e47636f57148afe7740a016c9a58d3821.

--- cms/envs/test.py
+++ cms/envs/test.py
@@ -26,7 +26,8 @@ from xmodule.modulestore.modulestore_settings import update_module_store_setting
 from .common import *
 
 # import settings from LMS for consistent behavior with CMS
-from lms.envs.test import (  # pylint: disable=wrong-import-order
+from lms.envs.test import (  # pylint: disable=wrong-import-order, disable=unused-import
+    ACCOUNT_MICROFRONTEND_URL,
     BLOCKSTORE_USE_BLOCKSTORE_APP_API,
     BLOCKSTORE_API_URL,
     COMPREHENSIVE_THEME_DIRS,  # unimport:skip
@@ -37,8 +38,10 @@ from lms.envs.test import (  # pylint: disable=wrong-import-order
     LOGIN_ISSUE_SUPPORT_LINK,
     MEDIA_ROOT,
     MEDIA_URL,
+    ORDER_HISTORY_MICROFRONTEND_URL,
     PLATFORM_DESCRIPTION,
     PLATFORM_NAME,
+    PROFILE_MICROFRONTEND_URL,
     REGISTRATION_EXTRA_FIELDS,
     GRADES_DOWNLOAD,
     SITE_NAME,
