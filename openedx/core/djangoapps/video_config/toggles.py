41d01977c059a4abe06b72f778bef0b2334357fc
jansenk <jkantor@edx.org>
2023-03-15 16:05:07 -0400

    docs: fix toggle annotation

--- openedx/core/djangoapps/video_config/toggles.py
+++ openedx/core/djangoapps/video_config/toggles.py
@@ -1,3 +1,6 @@
+"""
+Video config toggles
+"""
 from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag
 
 WAFFLE_FLAG_NAMESPACE = 'video_config'
@@ -6,7 +9,7 @@ WAFFLE_FLAG_NAMESPACE = 'video_config'
 # .. toggle_implementation: CourseWaffleFlag
 # .. toggle_default: False
 # .. toggle_description: Gates access to the public social sharing video feature.
-# .. toggle_use_cases: beta_testing
+# .. toggle_use_cases: temporary, opt_in
 # .. toggle_creation_date: 2023-02-02
 # .. toggle_target_removal_date: None
 PUBLIC_VIDEO_SHARE = CourseWaffleFlag(
