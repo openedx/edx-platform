02386230809c86fa2fe27f6aebbc5903b047b2aa
Luis Santana <luis.santana101@gmail.com>
2023-03-15 17:32:20 -0300

    add eol_report_analytics on instructor dashboard

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -635,6 +635,13 @@ def _section_data_download(course, access):
         ),
     }
     ######### EOL #############
+     try:
+        from eol_report_analytics import views
+        section_data['has_eol_report_analytics'] = True
+        section_data['eol_report_analytics_url'] = '{}?{}'.format(reverse('eol_report_analytics:data'), urllib.parse.urlencode({'course': str(course_key)}))
+    except ImportError:
+        section_data['has_eol_report_analytics'] = False
+
     try:
         from xblockcompletion import views
         section_data['has_xblockcompletion'] = True
