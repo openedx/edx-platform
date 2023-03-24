7f5320183d082acf9930584914a84ed26d1ee082
Jansen Kantor <jkantor@edx.org>
2023-03-24 13:47:46 -0400

    fix: change video embed url (#31988)
    * fix: change video embed url
    
    * fix: trailing slash

--- lms/urls.py
+++ lms/urls.py
@@ -327,14 +327,14 @@ urlpatterns += [
         name=RENDER_XBLOCK_NAME,
     ),
     re_path(
+        fr'^videos/embed/{settings.USAGE_KEY_PATTERN}$',
+        courseware_views.PublicVideoXBlockEmbedView.as_view(),
+        name=RENDER_VIDEO_XBLOCK_EMBED_NAME,
+    ),
+    re_path(
         fr'^videos/{settings.USAGE_KEY_PATTERN}$',
         courseware_views.PublicVideoXBlockView.as_view(),
         name=RENDER_VIDEO_XBLOCK_NAME,
-    ),
-    re_path(
-        fr'^videos/{settings.USAGE_KEY_PATTERN}/embed$',
-        courseware_views.PublicVideoXBlockEmbedView.as_view(),
-        name=RENDER_VIDEO_XBLOCK_EMBED_NAME,
     ),
 
 
