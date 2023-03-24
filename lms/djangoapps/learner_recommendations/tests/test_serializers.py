amr205/edx-platform-med

refactor: Reduce redundant code
Move recommendations related code to learner_recommendations app.

VAN-1310

lms/djangoapps/learner_recommendations/tests/test_serializers.py
@@ -1,0 +1,83 @@
+"""Tests for serializers for the Learner Recommendations"""
+
+from uuid import uuid4
+
+from django.test import TestCase
+
+from lms.djangoapps.learner_recommendations.serializers import (
+    HomeRecommendationsSerializer,
+)
+
+
+class TestCourseRecommendationSerializer(TestCase):
+    """High-level tests for CourseRecommendationSerializer"""
+
+    @classmethod
+    def mock_recommended_courses(cls, courses_count=2):
+        """Sample course data"""
+
+        recommended_courses = []
+
+        for _ in range(courses_count):
+            recommended_courses.append(
+                {
+                    "course_key": str(uuid4()),
+                    "logo_image_url": "http://edx.org/images/test.png",
+                    "marketing_url": "http://edx.org/courses/AI",
+                    "title": str(uuid4()),
+                },
+            )
+
+        return recommended_courses
+
+    def test_no_recommended_courses(self):
+        """That that data serializes correctly for empty courses list"""
+
+        recommended_courses = self.mock_recommended_courses(courses_count=0)
+
+        output_data = HomeRecommendationsSerializer(
+            {
+                "courses": recommended_courses,
+            }
+        ).data
+
+        self.assertDictEqual(
+            output_data,
+            {
+                "courses": [],
+                "isControl": None,
+            },
+        )
+
+    def test_happy_path(self):
+        """Test that data serializes correctly"""
+
+        recommended_courses = self.mock_recommended_courses()
+
+        output_data = HomeRecommendationsSerializer(
+            {
+                "courses": recommended_courses,
+                "is_control": False,
+            }
+        ).data
+
+        self.assertDictEqual(
+            output_data,
+            {
+                "courses": [
+                    {
+                        "courseKey": recommended_courses[0]["course_key"],
+                        "logoImageUrl": recommended_courses[0]["logo_image_url"],
+                        "marketingUrl": recommended_courses[0]["marketing_url"],
+                        "title": recommended_courses[0]["title"],
+                    },
+                    {
+                        "courseKey": recommended_courses[1]["course_key"],
+                        "logoImageUrl": recommended_courses[1]["logo_image_url"],
+                        "marketingUrl": recommended_courses[1]["marketing_url"],
+                        "title": recommended_courses[1]["title"],
+                    },
+                ],
+                "isControl": False,
+            },
+        )

lms/djangoapps/learner_recommendations/urls.py
@@ -11,6 +11,9 @@ app_name = "learner_recommendations"
 
 urlpatterns = [
     re_path(fr'^amplitude/{settings.COURSE_ID_PATTERN}/$',
-            views.AmplitudeRecommendationsView.as_view(),
+            views.AboutPageRecommendationsView.as_view(),
             name='amplitude_recommendations'),
+    re_path(r"^courses/$",
+            views.HomeRecommendationsApiView.as_view(),
+            name="courses"),
 ]

lms/djangoapps/learner_recommendations/views.py
@@ -9,26 +9,35 @@ from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthenticat
 from edx_rest_framework_extensions.auth.session.authentication import (
     SessionAuthenticationAllowInactiveUser,
 )
+from edx_rest_framework_extensions.permissions import NotJwtRestrictedApplication
 from django.core.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from common.djangoapps.track import segment
+from common.djangoapps.student.toggles import show_fallback_recommendations
 from openedx.core.djangoapps.geoinfo.api import country_code_from_ip
 from openedx.features.enterprise_support.utils import is_enterprise_learner
-from lms.djangoapps.learner_recommendations.toggles import enable_course_about_page_recommendations
+from lms.djangoapps.learner_recommendations.toggles import (
+    enable_course_about_page_recommendations,
+    enable_learner_home_recommendations,
+)
 from lms.djangoapps.learner_recommendations.utils import (
     get_amplitude_course_recommendations,
     filter_recommended_courses,
+    is_user_enrolled_in_ut_austin_masters_program,
 )
-from lms.djangoapps.learner_recommendations.serializers import RecommendationsSerializer
+from lms.djangoapps.learner_recommendations.serializers import (
+    AboutPageRecommendationsSerializer,
+    HomeRecommendationsSerializer,
+)
 
 
 log = logging.getLogger(__name__)
 
 
-class AmplitudeRecommendationsView(APIView):
+class AboutPageRecommendationsView(APIView):
     """
     **Example Request**
 
@@ -105,7 +114,7 @@ class AmplitudeRecommendationsView(APIView):
         )
 
         return Response(
-            RecommendationsSerializer(
+            AboutPageRecommendationsSerializer(
                 {
                     "courses": recommended_courses,
                     "is_control": is_control,
@@ -113,3 +122,99 @@ class AmplitudeRecommendationsView(APIView):
             ).data,
             status=200,
         )
+
+
+class HomeRecommendationsApiView(APIView):
+    """
+    API to get personalized recommendations from Amplitude.
+
+    **Example Request**
+
+    GET /api/learner_recommendations/courses/
+    """
+
+    authentication_classes = (
+        JwtAuthentication,
+        SessionAuthenticationAllowInactiveUser,
+    )
+    permission_classes = (IsAuthenticated, NotJwtRestrictedApplication)
+
+    def get(self, request):
+        """
+        Retrieves course recommendations details.
+        """
+        if not enable_learner_home_recommendations():
+            return Response(status=404)
+
+        user_id = request.user.id
+
+        if is_user_enrolled_in_ut_austin_masters_program(request.user):
+            return self._recommendations_response(user_id, None, [], False)
+
+        fallback_recommendations = settings.GENERAL_RECOMMENDATIONS if show_fallback_recommendations() else []
+
+        try:
+            is_control, has_is_control, course_keys = get_amplitude_course_recommendations(
+                user_id, settings.DASHBOARD_AMPLITUDE_RECOMMENDATION_ID
+            )
+        except Exception as ex:  # pylint: disable=broad-except
+            log.warning(f"Cannot get recommendations from Amplitude: {ex}")
+            return self._recommendations_response(user_id, None, fallback_recommendations, False)
+
+        is_control = is_control if has_is_control else None
+        if is_control or is_control is None or not course_keys:
+            return self._recommendations_response(user_id, is_control, fallback_recommendations, False)
+
+        ip_address = get_client_ip(request)[0]
+        user_country_code = country_code_from_ip(ip_address).upper()
+        filtered_courses = filter_recommended_courses(
+            request.user, course_keys, user_country_code=user_country_code, recommendation_count=5
+        )
+        # If no courses are left after filtering already enrolled courses from
+        # the list of amplitude recommendations, show general recommendations
+        # to the user.
+        if not filtered_courses:
+            return self._recommendations_response(user_id, is_control, fallback_recommendations, False)
+
+        recommended_courses = list(map(self._course_data, filtered_courses))
+        return self._recommendations_response(user_id, is_control, recommended_courses, True)
+
+    def _emit_recommendations_viewed_event(
+        self, user_id, is_control, recommended_courses, amplitude_recommendations=True
+    ):
+        """Emits an event to track Learner Home page visits."""
+        segment.track(
+            user_id,
+            "edx.bi.user.recommendations.viewed",
+            {
+                "is_control": is_control,
+                "amplitude_recommendations": amplitude_recommendations,
+                "course_key_array": [course["course_key"] for course in recommended_courses],
+                "page": "dashboard",
+            },
+        )
+
+    def _recommendations_response(self, user_id, is_control, recommended_courses, amplitude_recommendations):
+        """ Helper method for general recommendations response. """
+        self._emit_recommendations_viewed_event(
+            user_id, is_control, recommended_courses, amplitude_recommendations
+        )
+        return Response(
+            HomeRecommendationsSerializer(
+                {
+                    "courses": recommended_courses,
+                    "is_control": is_control,
+                }
+            ).data,
+            status=200,
+        )
+
+    def _course_data(self, course):
+        """Helper method for personalized recommendation response"""
+        return {
+            "course_key": course.get("key"),
+            "title": course.get("title"),
+            "logo_image_url": course.get("owners")[0]["logo_image_url"] if course.get(
+                "owners") else "",
+            "marketing_url": course.get("marketing_url"),
+        }

openedx/edx-platform

lms/djangoapps/learner_recommendations/tests/test_serializers.py
@@ -1,0 +1,83 @@
+"""Tests for serializers for the Learner Recommendations"""
+
+from uuid import uuid4
+
+from django.test import TestCase
+
+from lms.djangoapps.learner_recommendations.serializers import (
+    DashboardRecommendationsSerializer,
+)
+
+
+class TestDashboardRecommendationsSerializer(TestCase):
+    """High-level tests for DashboardRecommendationsSerializer"""
+
+    @classmethod
+    def mock_recommended_courses(cls, courses_count=2):
+        """Sample course data"""
+
+        recommended_courses = []
+
+        for _ in range(courses_count):
+            recommended_courses.append(
+                {
+                    "course_key": str(uuid4()),
+                    "logo_image_url": "http://edx.org/images/test.png",
+                    "marketing_url": "http://edx.org/courses/AI",
+                    "title": str(uuid4()),
+                },
+            )
+
+        return recommended_courses
+
+    def test_no_recommended_courses(self):
+        """That that data serializes correctly for empty courses list"""
+
+        recommended_courses = self.mock_recommended_courses(courses_count=0)
+
+        output_data = DashboardRecommendationsSerializer(
+            {
+                "courses": recommended_courses,
+            }
+        ).data
+
+        self.assertDictEqual(
+            output_data,
+            {
+                "courses": [],
+                "isControl": None,
+            },
+        )
+
+    def test_happy_path(self):
+        """Test that data serializes correctly"""
+
+        recommended_courses = self.mock_recommended_courses()
+
+        output_data = DashboardRecommendationsSerializer(
+            {
+                "courses": recommended_courses,
+                "is_control": False,
+            }
+        ).data
+
+        self.assertDictEqual(
+            output_data,
+            {
+                "courses": [
+                    {
+                        "courseKey": recommended_courses[0]["course_key"],
+                        "logoImageUrl": recommended_courses[0]["logo_image_url"],
+                        "marketingUrl": recommended_courses[0]["marketing_url"],
+                        "title": recommended_courses[0]["title"],
+                    },
+                    {
+                        "courseKey": recommended_courses[1]["course_key"],
+                        "logoImageUrl": recommended_courses[1]["logo_image_url"],
+                        "marketingUrl": recommended_courses[1]["marketing_url"],
+                        "title": recommended_courses[1]["title"],
+                    },
+                ],
+                "isControl": False,
+            },
+        )

lms/djangoapps/learner_recommendations/urls.py
@@ -11,6 +11,9 @@ app_name = "learner_recommendations"
 
 urlpatterns = [
     re_path(fr'^amplitude/{settings.COURSE_ID_PATTERN}/$',
-            views.AmplitudeRecommendationsView.as_view(),
+            views.AboutPageRecommendationsView.as_view(),
             name='amplitude_recommendations'),
+    re_path(r"^courses/$",
+            views.DashboardRecommendationsApiView.as_view(),
+            name="courses"),
 ]

lms/djangoapps/learner_recommendations/views.py
@@ -9,26 +9,35 @@ from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthenticat
 from edx_rest_framework_extensions.auth.session.authentication import (
     SessionAuthenticationAllowInactiveUser,
 )
+from edx_rest_framework_extensions.permissions import NotJwtRestrictedApplication
 from django.core.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from common.djangoapps.track import segment
+from common.djangoapps.student.toggles import show_fallback_recommendations
 from openedx.core.djangoapps.geoinfo.api import country_code_from_ip
 from openedx.features.enterprise_support.utils import is_enterprise_learner
-from lms.djangoapps.learner_recommendations.toggles import enable_course_about_page_recommendations
+from lms.djangoapps.learner_recommendations.toggles import (
+    enable_course_about_page_recommendations,
+    enable_dashboard_recommendations,
+)
 from lms.djangoapps.learner_recommendations.utils import (
     get_amplitude_course_recommendations,
     filter_recommended_courses,
+    is_user_enrolled_in_ut_austin_masters_program,
 )
-from lms.djangoapps.learner_recommendations.serializers import RecommendationsSerializer
+from lms.djangoapps.learner_recommendations.serializers import (
+    AboutPageRecommendationsSerializer,
+    DashboardRecommendationsSerializer,
+)
 
 
 log = logging.getLogger(__name__)
 
 
-class AmplitudeRecommendationsView(APIView):
+class AboutPageRecommendationsView(APIView):
     """
     **Example Request**
 
@@ -105,7 +114,7 @@ class AmplitudeRecommendationsView(APIView):
         )
 
         return Response(
-            RecommendationsSerializer(
+            AboutPageRecommendationsSerializer(
                 {
                     "courses": recommended_courses,
                     "is_control": is_control,
@@ -113,3 +122,99 @@ class AmplitudeRecommendationsView(APIView):
             ).data,
             status=200,
         )
+
+
+class DashboardRecommendationsApiView(APIView):
+    """
+    API to get personalized recommendations from Amplitude.
+
+    **Example Request**
+
+    GET /api/learner_recommendations/courses/
+    """
+
+    authentication_classes = (
+        JwtAuthentication,
+        SessionAuthenticationAllowInactiveUser,
+    )
+    permission_classes = (IsAuthenticated, NotJwtRestrictedApplication)
+
+    def get(self, request):
+        """
+        Retrieves course recommendations details.
+        """
+        if not enable_dashboard_recommendations():
+            return Response(status=404)
+
+        user_id = request.user.id
+
+        if is_user_enrolled_in_ut_austin_masters_program(request.user):
+            return self._recommendations_response(user_id, None, [], False)
+
+        fallback_recommendations = settings.GENERAL_RECOMMENDATIONS if show_fallback_recommendations() else []
+
+        try:
+            is_control, has_is_control, course_keys = get_amplitude_course_recommendations(
+                user_id, settings.DASHBOARD_AMPLITUDE_RECOMMENDATION_ID
+            )
+        except Exception as ex:  # pylint: disable=broad-except
+            log.warning(f"Cannot get recommendations from Amplitude: {ex}")
+            return self._recommendations_response(user_id, None, fallback_recommendations, False)
+
+        is_control = is_control if has_is_control else None
+        if is_control or is_control is None or not course_keys:
+            return self._recommendations_response(user_id, is_control, fallback_recommendations, False)
+
+        ip_address = get_client_ip(request)[0]
+        user_country_code = country_code_from_ip(ip_address).upper()
+        filtered_courses = filter_recommended_courses(
+            request.user, course_keys, user_country_code=user_country_code, recommendation_count=5
+        )
+        # If no courses are left after filtering already enrolled courses from
+        # the list of amplitude recommendations, show general recommendations
+        # to the user.
+        if not filtered_courses:
+            return self._recommendations_response(user_id, is_control, fallback_recommendations, False)
+
+        recommended_courses = list(map(self._course_data, filtered_courses))
+        return self._recommendations_response(user_id, is_control, recommended_courses, True)
+
+    def _emit_recommendations_viewed_event(
+        self, user_id, is_control, recommended_courses, amplitude_recommendations=True
+    ):
+        """Emits an event to track Learner Home page visits."""
+        segment.track(
+            user_id,
+            "edx.bi.user.recommendations.viewed",
+            {
+                "is_control": is_control,
+                "amplitude_recommendations": amplitude_recommendations,
+                "course_key_array": [course["course_key"] for course in recommended_courses],
+                "page": "dashboard",
+            },
+        )
+
+    def _recommendations_response(self, user_id, is_control, recommended_courses, amplitude_recommendations):
+        """ Helper method for general recommendations response. """
+        self._emit_recommendations_viewed_event(
+            user_id, is_control, recommended_courses, amplitude_recommendations
+        )
+        return Response(
+            DashboardRecommendationsSerializer(
+                {
+                    "courses": recommended_courses,
+                    "is_control": is_control,
+                }
+            ).data,
+            status=200,
+        )
+
+    def _course_data(self, course):
+        """Helper method for personalized recommendation response"""
+        return {
+            "course_key": course.get("key"),
+            "title": course.get("title"),
+            "logo_image_url": course.get("owners")[0]["logo_image_url"] if course.get(
+                "owners") else "",
+            "marketing_url": course.get("marketing_url"),
+        }