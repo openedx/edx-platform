dc63e525f8865e64d0ce23c46a97c9caf4e15a2a
SaadYousaf <saadyousaf@A006-00314.local>
2023-04-04 16:53:11 +0500

    feat: add platform notification app and models

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3265,6 +3265,9 @@ INSTALLED_APPS = [
 
     # MFE API
     'lms.djangoapps.mfe_config_api',
+
+    # Notifications
+    'openedx.core.djangoapps.notifications',
 ]
 
 ######################### CSRF #########################################
