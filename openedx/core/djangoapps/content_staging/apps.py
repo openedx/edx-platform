30de843b4a9a5c653e7c3c76eac06bb279eba048
Braden MacDonald <braden@opencraft.com>
2023-04-11 10:11:18 -0700

    fix: remove unneeded signal handlers

--- openedx/core/djangoapps/content_staging/apps.py
+++ openedx/core/djangoapps/content_staging/apps.py
@@ -24,9 +24,3 @@ class ContentStagingAppConfig(AppConfig):
             },
         },
     }
-
-    def ready(self):
-        """
-        Import signal handler's module to ensure they are registered.
-        """
-        from . import signal_handlers  # pylint: disable=unused-import
