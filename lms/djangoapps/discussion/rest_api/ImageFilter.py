2d92bc77924cd790d95f66be000c2672ac7aa574
AhtishamShahid <ahtisham300@gmail.com>
2023-04-11 16:41:14 +0500

    feat: added eden ai integration in forum

---
+++ lms/djangoapps/discussion/rest_api/ImageFilter.py
@@ -0,0 +1,58 @@
+import logging
+import json
+import requests
+from django.conf import settings
+from bs4 import BeautifulSoup
+
+log = logging.getLogger(__name__)
+TOLERANCE = 1
+
+
+class EdenAIImageExplicitContent:
+    def __init__(self):
+        self.url = "https://api.edenai.run/v2/image/explicit_content"
+        self.headers = {
+            "Authorization": f"Bearer {settings.EDEN_API_KEY}"
+        }
+        self.providers = ",".join(settings.EDEN_PROVIDERS)
+
+    def detect_explicit_content(self, show_original_response, file_url=None, file=None):
+        json_payload = {
+            "providers": self.providers,
+            "show_original_response": show_original_response,
+        }
+        if file_url:
+            json_payload['file_url'] = file_url
+        response = requests.post(self.url, json=json_payload, headers=self.headers)
+        return json.loads(response.text)
+
+    def nsfw_labels(self, nsfw_dict) -> list:
+        labels = []
+        for provider, result in nsfw_dict.items():
+            nsfw_labels = [item['label'] for item in result['items']]
+            labels.append(nsfw_labels)
+        return labels
+
+    def nsfw_likelihood(self, responses) -> int:
+        total = 0
+        for key, value in responses.items():
+            total = total + value['nsfw_likelihood']
+        return total
+
+    def get_nsfw_likelihood(self, file=None, file_url=None, show_original_response=False) -> tuple:
+        responses = self.detect_explicit_content(show_original_response, file=file, file_url=file_url)
+        self.nsfw_labels(responses)
+        return self.nsfw_likelihood(responses), self.nsfw_labels(responses)
+
+    def clean_html(self, html):
+
+        soup = BeautifulSoup(html, 'html.parser')
+        img_tags = soup.find_all('img')
+
+        src_list = [img['src'] for img in img_tags]
+
+        for src in src_list:
+            total, labels = self.get_nsfw_likelihood(file_url=src)
+            log.info(f"This image has a total of {total} nsfw likelihood. Labels: {labels}")
+            if total > TOLERANCE:
+                raise Exception(f"This image {src} does not follow community guidelines. Please upload a different image.")
