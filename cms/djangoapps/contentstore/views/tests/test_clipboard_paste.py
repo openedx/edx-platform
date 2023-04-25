9f0ddede25e3dd159b6453a34a6e450fbbdddf36
Braden MacDonald <braden@opencraft.com>
2023-04-25 10:56:12 -0700

    chore: convert new test suite to pytest assertions

--- cms/djangoapps/contentstore/views/tests/test_clipboard_paste.py
+++ cms/djangoapps/contentstore/views/tests/test_clipboard_paste.py
@@ -35,28 +35,28 @@ class ClipboardPasteTestCase(ModuleStoreTestCase):
         # Check how many blocks are in the vertical currently
         parent_key = course_key.make_usage_key("vertical", "vertical_test")  # This is the vertical that holds the video
         orig_vertical = modulestore().get_item(parent_key)
-        self.assertEqual(len(orig_vertical.children), 4)
+        assert len(orig_vertical.children) == 4
 
         # Copy the video
         video_key = course_key.make_usage_key("video", "sample_video")
         copy_response = client.post(CLIPBOARD_ENDPOINT, {"usage_key": str(video_key)}, format="json")
-        self.assertEqual(copy_response.status_code, 200)
+        assert copy_response.status_code == 200
 
         # Paste the video
         paste_response = client.post(XBLOCK_ENDPOINT, {
             "parent_locator": str(parent_key),
             "staged_content": "clipboard",
         }, format="json")
-        self.assertEqual(paste_response.status_code, 200)
+        assert paste_response.status_code == 200
         new_block_key = UsageKey.from_string(paste_response.json()["locator"])
 
         # Now there should be an extra block in the vertical:
         updated_vertical = modulestore().get_item(parent_key)
-        self.assertEqual(len(updated_vertical.children), 5)
-        self.assertEqual(updated_vertical.children[-1], new_block_key)
+        assert len(updated_vertical.children) == 5
+        assert updated_vertical.children[-1] == new_block_key
         # And it should match the original:
         orig_video = modulestore().get_item(video_key)
         new_video = modulestore().get_item(new_block_key)
-        self.assertEqual(new_video.youtube_id_1_0, orig_video.youtube_id_1_0)
+        assert new_video.youtube_id_1_0 == orig_video.youtube_id_1_0
         # The new block should store a reference to where it was copied from
-        self.assertEqual(new_video.copied_from_block, video_key)
+        assert new_video.copied_from_block == video_key
