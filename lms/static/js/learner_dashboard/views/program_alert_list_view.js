7174da78d0ef19632bc526282e3f9b4cc991bcbd
Nawfal Ahmed <111358247+NawfalAhmed@users.noreply.github.com>
2023-05-16 15:34:42 +0500

    feat: subscription changes and alerts for program dashboard and details (#32217)
    * feat: subscription changes on program dashboard and details (#31909)
    
      - add a h2 programs heading on program dashboard
      - subscription ui changes on program dashboard
      - subscription alerts on both pages
    
    * feat: add subscription upsell, fix responsive layout of program dashboard (#31943)
    
    * test: update tests for subscription changes on program dashboard (#32021)
    
    * feat: subscription api changes on program dashboard (#32085)
    
    * feat: add subscription segment events to program details and dashboard (#32164)
    
    * feat: subscription changes on program dashboard and details pages (#32205)

---
+++ lms/static/js/learner_dashboard/views/program_alert_list_view.js
@@ -0,0 +1,89 @@
+import Backbone from 'backbone';
+
+import HtmlUtils from 'edx-ui-toolkit/js/utils/html-utils';
+import StringUtils from 'edx-ui-toolkit/js/utils/string-utils';
+
+import warningIcon from '../../../images/warning-icon.svg';
+import programAlertTpl from '../../../templates/learner_dashboard/program_alert_list_view.underscore';
+
+class ProgramAlertListView extends Backbone.View {
+    constructor(options) {
+        const defaults = {
+            el: '.js-program-details-alerts',
+        };
+        super(Object.assign({}, defaults, options));
+    }
+
+    initialize({ context }) {
+        this.tpl = HtmlUtils.template(programAlertTpl);
+        this.enrollmentAlerts = context.enrollmentAlerts || [];
+        this.trialEndingAlerts = context.trialEndingAlerts || [];
+        this.pageType = context.pageType;
+        this.render();
+    }
+
+    render() {
+        const data = {
+            alertList: this.getAlertList(),
+            warningIcon,
+        };
+        HtmlUtils.setHtml(this.$el, this.tpl(data));
+    }
+
+    getAlertList() {
+        const alertList = this.enrollmentAlerts.map(
+            ({ title: programName, url }) => ({
+                url,
+                urlText: gettext('View program'),
+                title: StringUtils.interpolate(
+                    gettext('Enroll in a {programName} course'),
+                    { programName }
+                ),
+                message: this.pageType === 'programDetails'
+                    ? StringUtils.interpolate(
+                        gettext('You have an active subscription to the {programName} program but are not enrolled in any courses. Enroll in a remaining course and enjoy verified access.'),
+                        { programName }
+                    )
+                    : HtmlUtils.interpolateHtml(
+                        gettext('According to our records, you are not enrolled in any courses included in your {programName} program subscription. Enroll in a course from the {i_start}Program Details{i_end} page.'),
+                        {
+                            programName,
+                            i_start: HtmlUtils.HTML('<i>'),
+                            i_end: HtmlUtils.HTML('</i>'),
+                        }
+                    ),
+            })
+        );
+        return alertList.concat(this.trialEndingAlerts.map(
+            ({ title: programName, remainingDays, ...data }) => {
+                const title = 'Subscription trial expires in {remainingDays} day';
+                const message = 'Your {programName} trial will expire in {remainingDays} day at {trialEndTime} on {trialEndDate} and the card on file will be charged {subscriptionPrice}/month.';
+
+                return {
+                    title: StringUtils.interpolate(
+                        ngettext(
+                            title,
+                            title.replace(/\bday\b/, 'days'),
+                            remainingDays
+                        ),
+                        { remainingDays }
+                    ),
+                    message: StringUtils.interpolate(
+                        ngettext(
+                            message,
+                            message.replace(/\bday\b/, 'days'),
+                            remainingDays
+                        ),
+                        {
+                            programName,
+                            remainingDays,
+                            ...data,
+                        }
+                    ),
+                };
+            }
+        ));
+    }
+}
+
+export default ProgramAlertListView;
