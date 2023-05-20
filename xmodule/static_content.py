b727142a578be2022d31b4bdf4404ed16ada86a2
Kyle McCormick <kdmc@pm.me>
2023-05-20 09:40:44 -0400

    build: reference original XModule JS instead of copying
    TODO: details
    
    Part of: https://github.com/openedx/edx-platform/issues/31624

--- xmodule/static_content.py
+++ xmodule/static_content.py
@@ -77,14 +77,12 @@ XBLOCK_CLASSES = [
 ]
 
 
-def write_module_js(output_root):
-    """Write all registered XModule js and coffee files to output root."""
-    return _write_js(output_root, XBLOCK_CLASSES, 'get_preview_view_js')
+def write_module_js():
+    return _write_js(XBLOCK_CLASSES, 'get_preview_view_js')
 
 
-def write_descriptor_js(output_root):
-    """Write all registered XModuleDescriptor js and coffee files to output root."""
-    return _write_js(output_root, XBLOCK_CLASSES, 'get_studio_view_js')
+def write_descriptor_js():
+    return _write_js(XBLOCK_CLASSES, 'get_studio_view_js')
 
 
 def _ensure_dir(directory):
@@ -98,89 +96,21 @@ def _ensure_dir(directory):
             raise
 
 
-def _write_js(output_root, classes, js_attribute):
+def _write_js(classes, js_attribute):
     """
-    Write the javascript fragments from all XModules in `classes`
-    into `output_root` as individual files, hashed by the contents to remove
-    duplicates
-
     Returns a dictionary mapping class names to the files that they depend on.
     """
-    file_contents = {}
     file_owners = defaultdict(list)
-
-    fragment_owners = defaultdict(list)
     for class_ in classes:
+        bundle_name = getattr(class_, js_attribute + '_bundle_name')()
         module_js = getattr(class_, js_attribute)()
-        with open(module_js.get('xmodule_js'), 'rb') as xmodule_js_file:
-            xmodule_js_fragment = xmodule_js_file.read()
-        # It will enforce 000 prefix for xmodule.js.
-        fragment_owners[(0, 'js', xmodule_js_fragment)].append(getattr(class_, js_attribute + '_bundle_name')())
-        for filetype in ('coffee', 'js'):
-            for idx, fragment_path in enumerate(module_js.get(filetype, [])):
-                with open(fragment_path, 'rb') as fragment_file:
-                    fragment = fragment_file.read()
-                fragment_owners[(idx + 1, filetype, fragment)].append(getattr(class_, js_attribute + '_bundle_name')())
-
-    for (idx, filetype, fragment), owners in sorted(fragment_owners.items()):
-        filename = "{idx:0=3d}-{hash}.{type}".format(
-            idx=idx,
-            hash=hashlib.md5(fragment).hexdigest(),
-            type=filetype)
-        file_contents[filename] = fragment
-        for owner in owners:
-            file_owners[owner].append(output_root / filename)
-
-    _write_files(output_root, file_contents, {'.coffee': '.js'})
-
+        fragment_paths = [module_js['xmodule_js']] + module_js.get('js', [])
+        for fragment_path in fragment_paths:
+            if "/edx-platform/" not in fragment_path:
+                breakpoint()
+            fragment_rel_path = fragment_path.split("/edx-platform/")[-1]
+            file_owners[bundle_name].append(fragment_rel_path)
     return file_owners
-
-
-def _write_files(output_root, contents, generated_suffix_map=None):
-    """
-    Write file contents to output root.
-
-    Any files not listed in contents that exists in output_root will be deleted,
-    unless it matches one of the patterns in `generated_suffix_map`.
-
-    output_root (path): The root directory to write the file contents in
-    contents (dict): A map from filenames to file contents to be written to the output_root
-    generated_suffix_map (dict): Optional. Maps file suffix to generated file suffix.
-        For any file in contents, if the suffix matches a key in `generated_suffix_map`,
-        then the same filename with the suffix replaced by the value from `generated_suffix_map`
-        will be ignored
-    """
-    _ensure_dir(output_root)
-    to_delete = {file.basename() for file in output_root.files()} - set(contents.keys())
-
-    if generated_suffix_map:
-        for output_file in contents.keys():
-            for suffix, generated_suffix in generated_suffix_map.items():
-                if output_file.endswith(suffix):
-                    to_delete.discard(output_file.replace(suffix, generated_suffix))
-
-    for extra_file in to_delete:
-        (output_root / extra_file).remove_p()
-
-    for filename, file_content in contents.items():
-        output_file = output_root / filename
-
-        not_file = not output_file.isfile()
-
-        # Sometimes content is already unicode and sometimes it's not
-        # so we add this conditional here to make sure that below we're
-        # always working with streams of bytes.
-        if not isinstance(file_content, bytes):
-            file_content = file_content.encode('utf-8')
-
-        # not_file is included to short-circuit this check, because
-        # read_md5 depends on the file already existing
-        write_file = not_file or output_file.read_md5() != hashlib.md5(file_content).digest()
-        if write_file:
-            LOG.debug("Writing %s", output_file)
-            output_file.write_bytes(file_content)
-        else:
-            LOG.debug("%s unchanged, skipping", output_file)
 
 
 def write_webpack(output_file, module_files, descriptor_files):
@@ -195,12 +125,9 @@ def write_webpack(output_file, module_files, descriptor_files):
         'entry': {}
     }
     for (owner, files) in list(module_files.items()) + list(descriptor_files.items()):
-        unique_files = sorted({f'./{file}' for file in files})
-        if len(unique_files) == 1:
-            unique_files = unique_files[0]
-        config['entry'][owner] = unique_files
-    # config['entry']['modules/js/all'] = sorted(set('./{}'.format(file) for file in sum(module_files.values(), [])))
-    # config['entry']['descriptors/js/all'] = sorted(set('./{}'.format(file) for file in sum(descriptor_files.values(), [])))  # lint-amnesty, pylint: disable=line-too-long
+        if len(files) == 1:
+            files = files[0]
+        config['entry'][owner] = files
 
     with output_file.open('w') as outfile:
         outfile.write(
@@ -244,8 +171,8 @@ def main():
     args = docopt(main.__doc__)
     root = path(args['<output_root>'])
 
-    descriptor_files = write_descriptor_js(root / 'descriptors/js')
-    module_files = write_module_js(root / 'modules/js')
+    descriptor_files = write_descriptor_js()
+    module_files = write_module_js()
     write_webpack(root / 'webpack.xmodule.config.js', module_files, descriptor_files)
 
 
