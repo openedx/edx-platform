2985b015f7b6cc4ef092968a34f720484310060b
Jesper Hodge <jhodge@outlook.de>
2023-05-29 21:02:27 +0000

    test: check that xblock_handler receives correct args

--- cms/djangoapps/contentstore/rest_api/v1/views/tests/test_xblock.py
+++ cms/djangoapps/contentstore/rest_api/v1/views/tests/test_xblock.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 from django.urls import reverse
 from path import Path as path
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import SampleCourseFactory, CourseFactory
 from opaque_keys.edx.keys import CourseKey
@@ -91,12 +91,31 @@ class XblockViewPostTest(CourseAPITestMixin, ModuleStoreTestCase, APITestCase):
         'courseKey': 'test-course-key',
     }))
     @patch('cms.djangoapps.contentstore.rest_api.v1.views.xblock.toggles.use_studio_content_api', return_value=True)
-    def make_request(self, mock_use_studio_content_api, mock_handle_xblock, course_id=None, data=None):
+    def make_request(
+        self,
+        mock_use_studio_content_api,
+        mock_handle_xblock,
+        course_id=None,
+        data=None,
+        assert_xblock_handler_call=False
+    ):
         course_id = course_id if course_id else self.course.id
-        url = self.get_url(course_id)
-        data = self.get_test_data(course_id)
+        course_id_string = course_id.html_id()
+        url = self.get_url(course_id_string)
+        data = self.get_test_data(course_id_string)
 
-        return self.client.post(url, data=data)
+        response = self.client.post(url, data=data)
+
+        # test that xblock_handler has been called with the correct request, url, and data
+        if (assert_xblock_handler_call):
+            passed_args = mock_handle_xblock.call_args[0][0]
+
+            assert (mock_handle_xblock.call_count == 1)
+            assert (passed_args.data.get('courseKey') == course_id_string)
+            assert (passed_args.method == 'POST')
+            assert (passed_args.path == url)
+
+        return response
 
     # tests that the api is behind a feature flag
     def test_api_behind_feature_flag(self):
@@ -114,6 +133,11 @@ class XblockViewPostTest(CourseAPITestMixin, ModuleStoreTestCase, APITestCase):
     def test_200_course_instructor(self):
         self.client.login(username=self.course_instructor.username, password=self.password)
         response = self.make_request()
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_xblock_handler_called_with_correct_arguments(self):
+        self.client.login(username=self.course_instructor.username, password=self.password)
+        response = self.make_request(assert_xblock_handler_call=True)
         assert response.status_code == status.HTTP_200_OK
 
     # a test case for the create operation
