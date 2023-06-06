d4214cd3a98a52c53445d4c4a8b050f5b4580b86
Jesper Hodge <jhodge@outlook.de>
2023-06-06 00:54:51 +0000

    test: add all crud tests

--- cms/djangoapps/contentstore/rest_api/v1/views/tests/test_xblock.py
+++ cms/djangoapps/contentstore/rest_api/v1/views/tests/test_xblock.py
@@ -210,3 +210,82 @@ class XblockViewPutTest(XblockViewTestCase, ModuleStoreTestCase, APITestCase):
         data = response.json()
         assert data["locator"] == TEST_LOCATOR
         assert data["courseKey"] == self.get_course_key_string()
+
+
+class XblockViewPatchTest(XblockViewPutTest):
+    """
+    Test PATCH operation on xblocks - update an xblock
+    """
+
+    def get_test_data(self):
+        id = self.get_course_key_string()
+        return {
+            "category": "html",
+            "courseKey": id,
+            "data": "<p>Patched block!</p>",
+            "has_changes": True,
+            "id": TEST_LOCATOR,
+            "metadata": {
+                "display_name": "Text"
+            }
+        }
+
+    def assert_xblock_handler_called(self, *, mock_handle_xblock, response):
+        mock_handle_xblock.assert_called_once()
+        passed_args = mock_handle_xblock.call_args[0][0]
+
+        course_id = self.get_course_key_string()
+
+        assert passed_args.data.get("courseKey") == course_id
+        assert passed_args.data.get("data") == "<p>Patched block!</p>"
+        assert passed_args.data.get("id") == TEST_LOCATOR
+        assert passed_args.method == "PATCH"
+        assert passed_args.path == self.get_url()
+
+    def send_request(self, url, data):
+        return self.client.patch(url, data=data, format="json")
+
+    def test_api_behind_feature_flag(self):
+        # should return 404 if the feature flag is not enabled
+        url = self.get_url()
+
+        response = self.client.patch(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class XblockViewDeleteTest(XblockViewTestCase, ModuleStoreTestCase, APITestCase):
+    """
+    Test DELETE operation on xblocks - delete an xblock
+    """
+
+    def get_test_data(self):
+        return None
+
+    def assert_xblock_handler_called(self, *, mock_handle_xblock, response):
+        mock_handle_xblock.assert_called_once()
+        passed_args = mock_handle_xblock.call_args[0][0]
+
+        assert passed_args.method == "DELETE"
+        assert passed_args.path == self.get_url()
+
+    def send_request(self, url, data):
+        return self.client.delete(url)
+
+    def test_api_behind_feature_flag(self):
+        # should return 404 if the feature flag is not enabled
+        url = self.get_url()
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_xblock_handler_called_with_correct_arguments(self):
+        self.client.login(
+            username=self.course_instructor.username, password=self.password
+        )
+        response = self.make_request(
+            run_assertions=self.assert_xblock_handler_called,
+        )
+        assert response.status_code == status.HTTP_200_OK
+        data = response.json()
+        assert data["locator"] == TEST_LOCATOR
+        assert data["courseKey"] == self.get_course_key_string()
