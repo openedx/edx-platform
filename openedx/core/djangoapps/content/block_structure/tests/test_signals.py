97c79a549e87ff8f99343bf1634fc536b2fc739a
Yagnesh <yagnesh.nayi@manprax.com>
2023-06-08 17:50:28 +0530

    feat: Remove block_structure.invalidate_cache_on_publish waffle flag.

--- openedx/core/djangoapps/content/block_structure/tests/test_signals.py
+++ openedx/core/djangoapps/content/block_structure/tests/test_signals.py
@@ -12,7 +12,6 @@ from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
 
 from ..api import get_block_structure_manager
-from ..config import INVALIDATE_CACHE_ON_PUBLISH
 from ..signals import update_block_structure_on_course_publish
 from .helpers import is_course_in_block_structure_cache
 
@@ -47,15 +46,7 @@ class CourseBlocksSignalTest(ModuleStoreTestCase):
 
     @ddt.data(True, False)
     @patch('openedx.core.djangoapps.content.block_structure.manager.BlockStructureManager.clear')
-    def test_cache_invalidation(self, invalidate_cache_enabled, mock_bs_manager_clear):
-        test_display_name = "Jedi 101"
-
-        with override_waffle_switch(INVALIDATE_CACHE_ON_PUBLISH, active=invalidate_cache_enabled):
-            self.course.display_name = test_display_name
-            self.update_course(self.course, self.user.id)
-
-        assert mock_bs_manager_clear.called == invalidate_cache_enabled
-
+    
     def test_course_delete(self):
         bs_manager = get_block_structure_manager(self.course.id)
         assert bs_manager.get_collected() is not None
