b9ecf9a0a11641a658fe35bf1884a56dd41c1d53
AhtishamShahid <ahtisham300@gmail.com>
2023-06-08 21:12:54 +0500

    feat: added notifications for discussions app

--- openedx/core/djangoapps/notifications/tasks.py
+++ openedx/core/djangoapps/notifications/tasks.py
@@ -70,3 +70,34 @@ def delete_expired_notifications():
         logger.info(f'{delete_count} Notifications deleted in current batch in {time_elapsed} seconds.')
     time_elapsed = datetime.now() - start_time
     logger.info(f'{total_deleted} Notifications deleted in {time_elapsed} seconds.')
+
+
+@shared_task
+@set_code_owner_attribute
+def send_notifications(user_ids, app_name, notification_type, context, content_url):
+    """
+    Send notifications to the users.
+    """
+    from .models import Notification
+    user_ids = list(set(user_ids))
+    # check if what is preferences of user and make decision to send notification or not
+    preferences = CourseNotificationPreference.objects.filter(
+        user_id__in=user_ids,
+        course_id=context.get('course_id', None),
+    )
+    preferences = {pref.user_id: pref for pref in preferences}
+    notifications = []
+
+    for user_id in user_ids:
+        app_pref = preferences.get(user_id, None)
+        # There might be no config for the app, notification_type, so we need to check for it
+        if app_pref and app_pref.get_web_config(app_name, notification_type):
+            notifications.append(Notification(
+                user_id=user_id,
+                app_name=app_name,
+                notification_type=notification_type,
+                content_context=context,
+                content_url=content_url
+            ))
+    # send notification to users but use bulk_create
+    Notification.objects.bulk_create(notifications)
