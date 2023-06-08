b4f301b2f3371eb904612be4a374d0b1093c2d37
mubbsharanwar <mubbshar.anwar@arbisoft.com>
2023-06-08 12:42:39 +0500

    revert: Removing the save_far_later Removing the save_for_later feature after anylising the experiment results. We are not going to make this feature permanent.
    VAN-1451

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1080,12 +1080,6 @@ MARKETING_EMAILS_OPT_IN = False
 # .. toggle_tickets: 'https://openedx.atlassian.net/browse/VAN-622'
 ENABLE_COPPA_COMPLIANCE = False
 
-# VAN-741 - save for later api put behind a flag to make it only available for edX
-ENABLE_SAVE_FOR_LATER = False
-
-# VAN-887 - save for later reminder emails threshold days
-SAVE_FOR_LATER_REMINDER_EMAIL_THRESHOLD = 15
-
 ############################# SET PATH INFORMATION #############################
 PROJECT_ROOT = path(__file__).abspath().dirname().dirname()  # /edx-platform/lms
 REPO_ROOT = PROJECT_ROOT.dirname()
@@ -3280,9 +3274,6 @@ INSTALLED_APPS = [
     # For edx ace template tags
     'edx_ace',
 
-    # For save for later
-    'lms.djangoapps.save_for_later',
-
     # Blockstore
     'blockstore.apps.bundles',
 
@@ -4797,11 +4788,6 @@ OPTIONAL_FIELD_API_RATELIMIT = '10/h'
 ##### PASSWORD RESET RATE LIMIT SETTINGS #####
 PASSWORD_RESET_IP_RATE = '1/m'
 PASSWORD_RESET_EMAIL_RATE = '2/h'
-
-#### SAVE FOR LATER EMAIL RATE LIMIT SETTINGS ####
-SAVE_FOR_LATER_IP_RATE_LIMIT = '100/d'
-SAVE_FOR_LATER_EMAIL_RATE_LIMIT = '5/h'
-
 
 #### BRAZE API SETTINGS ####
 
