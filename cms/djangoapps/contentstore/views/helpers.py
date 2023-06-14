84bf579f91a71419c9f30e03b0da35d1c690413e
Jesper Hodge <jhodge@outlook.de>
2023-06-14 18:57:40 +0000

    fix: lint

--- cms/djangoapps/contentstore/views/helpers.py
+++ cms/djangoapps/contentstore/views/helpers.py
@@ -1,3 +1,8 @@
+"""
+Helpers specific to these specific views.
+Since many helper methods are shared between these views as well as views in the /api
+and /rest_api folders, they live one level up in contentstore/helpers.py
+"""
 from django.http import HttpResponse
 
 
