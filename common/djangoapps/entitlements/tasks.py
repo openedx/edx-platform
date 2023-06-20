4d4f1cd1b57c3f88a2630a6ab574b9e178d20a0c
Muhammad Zubair <syedzubairtahir12@gmail.com>
2023-06-20 18:48:51 +0500

    chore: added a django management command to expire old entitlements and create new one against them

--- common/djangoapps/entitlements/tasks.py
+++ common/djangoapps/entitlements/tasks.py
@@ -1,7 +1,8 @@
 """
 This file contains celery tasks for entitlements-related functionality.
 """
-
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
@@ -9,6 +10,7 @@ from django.conf import settings  # lint-amnesty, pylint: disable=unused-import
 from edx_django_utils.monitoring import set_code_owner_attribute
 
 from common.djangoapps.entitlements.models import CourseEntitlement
+from common.djangoapps.entitlements.rest_api.v1.views import EntitlementViewSet
 
 LOGGER = get_task_logger(__name__)
 
@@ -17,7 +19,15 @@ LOGGER = get_task_logger(__name__)
 # time of 2047 seconds (about 30 minutes). Setting this to None could yield
 # unwanted behavior: infinite retries.
 MAX_RETRIES = 11
-
+#course uuids for which entitlements should be expired after 18 months.
+MIT_SUPPLY_CHAIN_COURSES = [
+    '0d9b47982e3d486aa3189a7035bbda77',
+    '09532745c837467b9078093b8e1265a8',
+    '324970b703a444d7b39e10bbda6f119f',
+    '5f1c55b4354e4155af4a76450953e10d',
+    'ed927a1a4a95415ba865c3d722ac549c',
+    '6513ed9c112a495182ad7036cbe52831',    
+]
 
 @shared_task(bind=True, ignore_result=True)
 @set_code_owner_attribute
@@ -62,3 +72,52 @@ def expire_old_entitlements(self, start, end, logid='...'):
         raise self.retry(exc=exc, countdown=countdown, max_retries=MAX_RETRIES)
 
     LOGGER.info('Successfully completed the task expire_old_entitlements after examining %d entries [%s]', entitlements.count(), logid)  # lint-amnesty, pylint: disable=line-too-long
+
+
+@shared_task(bind=True, ignore_result=True)
+@set_code_owner_attribute
+def expire_and_create_entitlements(self):
+    """
+    This task is designed to be called to process and expire bundle of entitlements
+    that are older than one year on in exceptional case 18 months. 
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    """
+    LOGGER.info('Running task expire_and_create_entitlements')
+
+    current_date = date.today()
+    expiration_period = current_date - relativedelta(years=1)
+    exceptional_expiration_period = current_date - relativedelta(years=1, months=6)
+    normal_entitlements = CourseEntitlement.objects.filter(expired_at__isnull=True, created__lte=expiration_period).exclude(course_uuid__in=MIT_SUPPLY_CHAIN_COURSES)
+    exceptional_entitlements = CourseEntitlement.objects.filter(expired_at__isnull=True, created__lte=exceptional_expiration_period, course_uuid__in=MIT_SUPPLY_CHAIN_COURSES)
+    entitlements = normal_entitlements | exceptional_entitlements
+
+    countdown = 2 ** self.request.retries
+
+
+    try:
+        for entitlement in entitlements:
+            # This property request will update the expiration if necessary as
+            # a side effect. We could manually call update_expired_at(), but
+            # let's use the same API the rest of the LMS does, to mimic normal
+            # usage and allow the update call to be an internal detail.
+            entitlement.expire_entitlement()
+            LOGGER.info('Expired entitlement with id %d ', entitlement.id)
+            entitlement.pk = None
+            entitlement.expired_at = None
+            entitlement.modified = None
+            entitlement.save()
+            LOGGER.info('created new entitlement with id %d ', entitlement.id)
+            
+
+    except Exception as exc:
+        LOGGER.exception('Failed to expire entitlements ',)
+        # The call above is idempotent, so retry at will
+        raise self.retry(exc=exc, countdown=countdown, max_retries=MAX_RETRIES)
+
+    LOGGER.info('Successfully completed the task expire_and_create_entitlements after examining %d entries', entitlements.count())  # lint-amnesty, pylint: disable=line-too-long
