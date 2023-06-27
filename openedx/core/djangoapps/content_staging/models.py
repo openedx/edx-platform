12a8d99824c9440cca60ebb802bad4e082c63eb1
Braden MacDonald <braden@opencraft.com>
2023-06-27 12:06:43 -0700

    feat: Copy/Paste associated static assets along with components (#32346)
    * refactor: improve typing of StaticFile named tuple
    * feat: copy static asset files into the clipboard
    * feat: paste static assets
    * feat: show notification in studio about pasted assets
    * fix: HTML XBlocks would lose the editor="raw" setting when copy-pasted.
    * feat: copy python_lib.zip to the clipboard when it seems to be in use

--- openedx/core/djangoapps/content_staging/models.py
+++ openedx/core/djangoapps/content_staging/models.py
@@ -67,6 +67,25 @@ class StagedContent(models.Model):
         return f'Staged {self.block_type} block "{self.display_name}" ({self.status})'
 
 
+class StagedContentFile(models.Model):
+    """
+    A data file ("Static Asset") associated with some StagedContent.
+
+    These usually come from a course's Files & Uploads page, but can also come
+    from per-xblock file storage (e.g. video transcripts or images used in
+    v2 content libraries).
+    """
+    for_content = models.ForeignKey(StagedContent, on_delete=models.CASCADE, related_name="files")
+    filename = models.CharField(max_length=255, blank=False)
+    # Everything below is optional:
+    data_file = models.FileField(upload_to="staged-content-temp/", blank=True)
+    # If this asset came from Files & Uploads in a course, this is an AssetKey
+    # as a string. If this asset came from an XBlock's filesystem, this is the
+    # UsageKey of the XBlock.
+    source_key_str = models.CharField(max_length=255, blank=True)
+    md5_hash = models.CharField(max_length=32, blank=True)
+
+
 class UserClipboard(models.Model):
     """
     Each user has a clipboard that can hold one item at a time, where an item
