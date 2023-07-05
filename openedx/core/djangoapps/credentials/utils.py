5c32db17cf0b35a14d84882fda4838c645072d90
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2023-07-05 17:13:57 +0500

    fix: fixes

--- openedx/core/djangoapps/credentials/utils.py
+++ openedx/core/djangoapps/credentials/utils.py
@@ -110,14 +110,14 @@ def get_credentials(user, program_uuid=None, credential_type=None):
     )
 
 
-def get_course_completion_status(lms_user_id, course_run_keys):
+def get_course_completion_status(lms_user_id, course_run_ids):
     """
     Given the lms_user_id and course run ids, checks for course completion status
     Arguments:
         lms_user_id (User): The user to authenticate as when requesting credentials.
-        course_run_keys(List): list of course run ids for which we need to check the completion status
+        course_run_ids(List): list of course run ids for which we need to check the completion status
     Returns:
-        list of course_run_keys for which user has completed the course
+        list of course_run_ids for which user has completed the course
     """
     credential_configuration = CredentialsApiConfig.current()
     if not credential_configuration.enabled:
@@ -134,7 +134,7 @@ def get_course_completion_status(lms_user_id, course_run_keys):
             completion_status_url,
             data={
                 'lms_user_id': lms_user_id,
-                'course_runs': course_run_keys,
+                'course_runs': course_run_ids,
             }
         )
         # TODO: will be fixed in https://2u-internal.atlassian.net/browse/APER-2425
@@ -142,18 +142,18 @@ def get_course_completion_status(lms_user_id, course_run_keys):
         # which means user has not completed any course.
         if api_response.status_code == 404:
             log.info("Encountered a 404 while reqeusting course completion statuses "
-                     "for lms_user_id [%s] for course_run_keys [%s]",
+                     "for lms_user_id [%s] for course_run_ids [%s]",
                      lms_user_id,
-                     course_run_keys,
+                     course_run_ids,
                      )
             return []
         api_response.raise_for_status()
         course_completion_response = api_response.json()
     except Exception as exc:  # pylint: disable=broad-except
         log.exception("An unexpected error occurred while reqeusting course completion statuses "
-                      "for lms_user_id [%s] for course_run_keys [%s] with exc [%s]:",
+                      "for lms_user_id [%s] for course_run_ids [%s] with exc [%s]:",
                       lms_user_id,
-                      course_run_keys,
+                      course_run_ids,
                       exc
                       )
         return []
@@ -161,9 +161,8 @@ def get_course_completion_status(lms_user_id, course_run_keys):
         # Yes, This is course_credentials_data. The key is named status but
         # it contains all the courses data from credentials.
         course_credentials_data = course_completion_response.get('status')
-        # Based on the first to-do we may need a change here.
         filtered_records = [course_data for course_data in course_credentials_data if
-                            course_data['course_run']['key'] in course_run_keys and
+                            course_data['course_run']['key'] in course_run_ids and
                             course_data['status'] == 'awarded']
         return filtered_records
     return []
