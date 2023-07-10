7a89f2e5d31a1351f3d91ecae4b9c681ae8cde61
Braden MacDonald <braden@opencraft.com>
2023-07-10 11:16:16 -0700

    fix: remove deprecated django-debug-toolbar Logging Panel (#32553)

--- cms/envs/devstack.py
+++ cms/envs/devstack.py
@@ -104,7 +104,6 @@ DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.request.RequestPanel',
     'debug_toolbar.panels.sql.SQLPanel',
     'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
     'debug_toolbar.panels.profiling.ProfilingPanel',
     'debug_toolbar.panels.history.HistoryPanel',
 )
