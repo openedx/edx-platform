f93b2182f5698e0ccd079e020aa30a251b8a07fd
Taimoor Ahmed <68893403+taimoor-ahmed-1@users.noreply.github.com>
2023-07-11 13:17:19 +0500

    Remove reset password support text line - EDLY-5776 (#392)
    Co-authored-by: Taimoor  Ahmed <taimoor.ahmed@A006-00933.local>

--- lms/static/js/student_account/views/LoginView.js
+++ lms/static/js/student_account/views/LoginView.js
@@ -146,22 +146,14 @@
                     successTitle = gettext('Check Your Email'),
                     successMessageHtml = HtmlUtils.interpolateHtml(
                         gettext('{paragraphStart}You entered {boldStart}{email}{boldEnd}. If this email address is associated with your {platform_name} account, we will send a message with password recovery instructions to this email address.{paragraphEnd}' + // eslint-disable-line max-len
-                        '{paragraphStart}If you do not receive a password reset message after 1 minute, verify that you entered the correct email address, or check your spam folder.{paragraphEnd}' + // eslint-disable-line max-len
-                        '{paragraphStart}If you need further assistance, {anchorStart}contact technical support{anchorEnd}.{paragraphEnd}'), { // eslint-disable-line max-len
+                        '{paragraphStart}If you do not receive a password reset message after 1 minute, verify that you entered the correct email address, or check your spam folder.{paragraphEnd}' // eslint-disable-line max-len
+                        ), { // eslint-disable-line max-len
                             boldStart: HtmlUtils.HTML('<b>'),
                             boldEnd: HtmlUtils.HTML('</b>'),
                             paragraphStart: HtmlUtils.HTML('<p>'),
                             paragraphEnd: HtmlUtils.HTML('</p>'),
                             email: email,
-                            platform_name: this.platformName,
-                            anchorStart: HtmlUtils.HTML(
-                                StringUtils.interpolate(
-                                    '<a href="{passwordResetSupportUrl}">', {
-                                        passwordResetSupportUrl: this.passwordResetSupportUrl
-                                    }
-                                )
-                            ),
-                            anchorEnd: HtmlUtils.HTML('</a>')
+                            platform_name: this.platformName
                         }
                     );
 
