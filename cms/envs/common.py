cb50ec0d12c76e41f50d1bf59ea13c6349a1af7b
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-07-29 00:41:06 +0500

    chore: upgrading deprecated import.

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -2721,3 +2721,7 @@ BRAZE_COURSE_ENROLLMENT_CANVAS_ID = ''
 
 DISCUSSIONS_INCONTEXT_FEEDBACK_URL = ''
 DISCUSSIONS_INCONTEXT_LEARNMORE_URL = ''
+
+#### django-simple-history##
+# disable indexing on date field via django-simple-history.
+SIMPLE_HISTORY_DATE_INDEX = False
