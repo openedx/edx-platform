bc749ff5bd0bbbeecc5260276ec95d4c3ce8c93c
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-07-31 20:28:06 +0500

    feat: upgrading django-history==3.1.1.

--- common/djangoapps/util/tests/test_db.py
+++ common/djangoapps/util/tests/test_db.py
@@ -121,7 +121,7 @@ class MigrationTests(TestCase):
     Tests for migrations.
     """
 
-    @pytest.mark.skip(reason="Upgrading django-simple-history without adding migrations.")  # migrations will be added in followup prs.
+    @pytest.mark.skip(reason="Upgrading django-simple-history without adding migrations.")
     @override_settings(MIGRATION_MODULES={})
     def test_migrations_are_in_sync(self):
         """
