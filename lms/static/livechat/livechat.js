3449a36e5fceabd6ad681a5eea30ae274bec52c5
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-08-02 12:04:36 +0700

    edit script HF

--- lms/static/livechat/livechat.js
+++ lms/static/livechat/livechat.js
@@ -1,9 +1,9 @@
 function getFormHTML() {
 	const url = `${ this.host }/hf40-livechat/form.html`;
-	
+
 	return new Promise((resolve) => {
 		const xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
+		xhttp.onreadystatechange = function () {
 			if (this.readyState === 4 && this.status === 200) {
 				resolve(this.responseText);
 			}
@@ -16,14 +16,13 @@ function getFormHTML() {
 
 
 function callback(resolve) {
-
 	const xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
+	xhttp.onreadystatechange = function () {
 		if (this.readyState === 4) {
 			if (this.status === 200) {
 				const json = JSON.parse(this.responseText);
 				resolve(json[0].email);
-				
+
 			} else {
 				resolve(null);
 			}
@@ -31,29 +30,32 @@ function callback(resolve) {
 	};
 	xhttp.open('GET', '/api/user/v1/accounts', true);
 	xhttp.send();
-	
-}
-function getEmail() {
-	const mails=[]
-	const mail=  new Promise(callback)  ;
-	 mail.then((res)=>mails.push(res))
-	 return mails
+
 }
 
+async function getEmail() {
+	const email = await new Promise(callback);
+	return email;
+}
 
-(  function(w, d, s, u) {
-w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
-var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
-j.async = true; j.src = 'https://test-xseries2.funix.edu.vn/livechat/rocketchat-livechat.min.js?_=201903270000';
-h.parentNode.insertBefore(j, h);
+(function (w, d, s, u) {
+	w.RocketChat = function (c) {
+		w.RocketChat._.push(c)
+	};
+	w.RocketChat._ = [];
+	w.RocketChat.url = u;
+	var h = d.getElementsByTagName(s)[0],
+		j = d.createElement(s);
+	j.async = true;
+	j.src = 'https://test-xseries2.funix.edu.vn/livechat/rocketchat-livechat.min.js?_=201903270000';
+	h.parentNode.insertBefore(j, h);
 })(window, document, 'script', 'https://test-xseries2.funix.edu.vn/livechat');
-RocketChat(async function() {
+RocketChat(async function () {
 	const email = await getEmail()
-	console.log('email',email)
+	console.log('email', email)
 	this.registerGuest({
-	token: email, // The token field is not required. If it is not passed, a new token will be generated
-	name: email,
-	email: email,
-	
-});
+		token: email, // The token field is not required. If it is not passed, a new token will be generated
+		name: email,
+		email: email,
+	});
 });
