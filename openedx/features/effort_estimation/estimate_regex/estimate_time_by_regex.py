a565e68dc974816b373251e343e821b7aa322a06
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-08-08 13:52:47 +0700

    update logic estimate_time

--- openedx/features/effort_estimation/estimate_regex/estimate_time_by_regex.py
+++ openedx/features/effort_estimation/estimate_regex/estimate_time_by_regex.py
@@ -8,6 +8,9 @@ import math
 TIME_HOUR_REGEX = r"([0-9]+\.*[0-9]*) gio"
 TIME_MINUTE_REGEX = r"([0-9]+) phut"
 NUMBER_REGEX = r"([0-9]+\.*[0-9]*)"
+TIME_H_REGEX = r"([0-9]+\.*[0-9]*)h"
+TIME_M_REGEX = r"([0-9]+)p"
+TIME_HOUR_MINUTE_REGEX = r"([0-9]+\.*[0-9]*) gio ([0-9]+) phut"
 
 def _get_hour_time(line):
 	"""
@@ -69,10 +72,63 @@ def estimate_time_by_regex(text):
 	if len(lines) == 0:
 		return None
 
+ 
+ 
 	time_by_regex = None
 	last_line = lines[-1].lower()
 
 	last_line = no_accent_vietnamese(last_line)
+ 
+	for line in lines:
+		line_text = no_accent_vietnamese(line.lower())
+		
+		if 'gio' in line_text and 'phut' in line_text:
+			regex_result = re.findall(NUMBER_REGEX, line_text)
+			if len(regex_result) >= 2:			
+				try : 
+					time_by_regex = float(regex_result[-2]) * 60
+					time_by_regex += float(regex_result[-1])
+				except ValueError :
+					time_by_regex = None
+		elif 'gio' in line_text:
+			regex_result = re.findall(TIME_HOUR_REGEX, line_text)
+			if len(regex_result) == 1:
+				time_by_regex = float(regex_result[-1]) * 60
+			if len(regex_result) >= 2:
+				try : 
+					time_by_regex = float(regex_result[-2]) * 60
+					time_by_regex += float(regex_result[-1])
+				except ValueError :
+					time_by_regex = None	
+		elif 'phut' in line_text:
+			regex_result = re.findall(TIME_MINUTE_REGEX, line_text)
+			if len(regex_result) == 1:
+				time_by_regex = float(regex_result[-1])
+			# print('============', time_by_regex, line_text)
+		
+		elif 'h' in line_text and time_by_regex is None:
+			regex_result = re.findall(TIME_H_REGEX, line_text)
+			if len(regex_result) == 1 :
+				time_by_regex = float(regex_result[-1]) * 60
+			
+			if len(regex_result) >= 2:
+				try : 
+					time_by_regex = float(regex_result[-2]) * 60
+					time_by_regex += float(regex_result[-1])
+				except ValueError :
+					time_by_regex = None	
+		elif 'p' in line_text  and time_by_regex is None:
+			regex_result = re.findall(TIME_M_REGEX, line_text)
+			if len(regex_result) == 0:
+				time_by_regex = None
+			elif len(regex_result) == 1:
+				time_by_regex = float(regex_result[-1])
+	
+		
+    
+    
+
+	
 
 	# Old method: extract minute and hour from last line
 	# hour = _get_hour_time(last_line)
@@ -83,32 +139,35 @@ def estimate_time_by_regex(text):
 	# 	time_by_regex = math.ceil(time_by_regex / 5) * 5
 
 	# New method: extract number from last line
-	regex_result = re.findall(NUMBER_REGEX, last_line)
+	# regex_result = re.findall(NUMBER_REGEX, last_line)
 
-	# Case 1: only one result => it is hours
-	if len(regex_result) == 1:
+	# # Case 1: only one result => it is hours
+	# if len(regex_result) == 1:
 
-		time_by_regex = float(regex_result[-1])
-		# Case 2: two result => it is hours and minutes
-	elif len(regex_result) >= 2:
-		try : 
-			time_by_regex = float(regex_result[-2]) * 60
-			time_by_regex += float(regex_result[-1])
-		except ValueError :
-			time_by_regex = None
-	# Case 3: no result => return None
-	else:
-		time_by_regex = None
+	# 	time_by_regex = float(regex_result[-1])
+	# 	# Case 2: two result => it is hours and minutes
+	# elif len(regex_result) >= 2:
+		# try : 
+		# 	time_by_regex = float(regex_result[-2]) * 60
+		# 	time_by_regex += float(regex_result[-1])
+		# except ValueError :
+		# 	time_by_regex = None
+	# # Case 3: no result => return None
+	# else:
+	# 	time_by_regex = None
 
-	# if time is not None than round it to 5 min
-	if time_by_regex is not None:
-		if 'gio' in last_line or 'phut' in last_line:
-			time_by_regex = math.ceil(time_by_regex / 5) * 5
-	else :
-		for line in lines :
-			line_text = no_accent_vietnamese(line.lower())
-			regex_result = re.findall(NUMBER_REGEX, line_text)
-			if 'thoi gian lam bai du kien' in line_text :
-				time_by_regex = float(regex_result[-1])
+	# # if time is not None than round it to 5 min
+	# if time_by_regex is not None:
+	# 	if 'gio' in last_line or 'phut' in last_line:
+	# 		time_by_regex = math.ceil(time_by_regex / 5) * 5
+	# else :
+		
+	# 		for line in lines :
+	# 			line_text = no_accent_vietnamese(line.lower())
+	# 			regex_result = re.findall(TIME_HOUR_MINUTE_REGEX, line_text)
+	# 			print('========',regex_result ,  line_text)
+				
 
+	# print('======', time_by_regex, last_line)
+ 
 	return time_by_regex
