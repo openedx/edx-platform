29310f581d6757be9b5a7493bacbd4040d180f3e
Usama Sadiq <usama.sadiq@arbisoft.com>
2023-08-16 14:39:49 +0500

    fix: fix django4 deprecation warnings (#32957)

--- cms/djangoapps/contentstore/rest_api/urls.py
+++ cms/djangoapps/contentstore/rest_api/urls.py
@@ -2,7 +2,8 @@
 Contentstore API URLs.
 """
 
-from django.urls import include, re_path
+from django.urls import path
+from django.urls import include
 
 from .v0 import urls as v0_urls
 from .v1 import urls as v1_urls
@@ -10,6 +11,6 @@ from .v1 import urls as v1_urls
 app_name = 'cms.djangoapps.contentstore'
 
 urlpatterns = [
-    re_path(r'^v0/', include(v0_urls)),
-    re_path(r'^v1/', include(v1_urls)),
+    path('v0/', include(v0_urls)),
+    path('v1/', include(v1_urls)),
 ]
