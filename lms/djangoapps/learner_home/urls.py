4dea331486efb800c2ad1217768ca197a9117d27
Usama Sadiq <usama.sadiq@arbisoft.com>
2023-08-16 11:15:17 +0500

    fix: fix django4 deprecation warnings (#32948)

--- lms/djangoapps/learner_home/urls.py
+++ lms/djangoapps/learner_home/urls.py
@@ -2,6 +2,7 @@
 Learner Home URL routing configuration
 """
 
+from django.urls import path
 from django.urls import include, re_path
 
 from lms.djangoapps.learner_home import views
@@ -11,5 +12,5 @@ app_name = "learner_home"
 # Learner Dashboard Routing
 urlpatterns = [
     re_path(r"^init/?", views.InitializeView.as_view(), name="initialize"),
-    re_path(r"^mock/", include("lms.djangoapps.learner_home.mock.urls")),
+    path("mock/", include("lms.djangoapps.learner_home.mock.urls")),
 ]
