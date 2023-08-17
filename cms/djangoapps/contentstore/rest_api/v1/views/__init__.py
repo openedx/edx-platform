9ae98d25f9468659dd67201e083a348401d22d9d
ruzniaievdm <ruzniaievdm@gmail.com>
2023-08-17 16:12:02 +0300

    feat: Create DRF for course team (#32782)

--- cms/djangoapps/contentstore/rest_api/v1/views/__init__.py
+++ cms/djangoapps/contentstore/rest_api/v1/views/__init__.py
@@ -2,6 +2,7 @@
 Views for v1 contentstore API.
 """
 from .course_details import CourseDetailsView
+from .course_team import CourseTeamView
 from .grading import CourseGradingView
 from .proctoring import ProctoredExamSettingsView, ProctoringErrorsView
 from .settings import CourseSettingsView
