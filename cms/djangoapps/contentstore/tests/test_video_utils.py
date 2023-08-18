92ab89287a8432d3ae463459e62bd28c1221ec5c
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-08-18 18:42:33 +0500

    chore: Adding test to verify s3boto3 storages with invalid params.

--- cms/djangoapps/contentstore/tests/test_video_utils.py
+++ cms/djangoapps/contentstore/tests/test_video_utils.py
@@ -368,11 +368,6 @@ class ScrapeVideoThumbnailsTestCase(CourseTestCase):
         video1_image_url = get_course_video_image_url(course_id=course_id, edx_video_id=video1_edx_video_id)
         self.assertIsNone(video1_image_url)
 
-from unittest import mock
-
-from django.core.files.storage import get_storage_class
-from storages.backends.s3boto3 import S3Boto3Storage
-
 
 class S3Boto3TestCase(TestCase):
     """ verify s3boto3 returns valid backend."""
