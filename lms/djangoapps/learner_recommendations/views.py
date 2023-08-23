6eba79af41d45a619d6323395589eda3408d633e
Mubbshar Anwar <78487564+mubbsharanwar@users.noreply.github.com>
2023-08-23 16:53:32 +0500

    fix: update dashboard recommendations endpoint (#33072)
    - Update recommendations endpoint from learner recommendations to edx recommendations.
    - Added docstring to tell contributors that the recommendations related code has been moved to edx-recommendations plugin.
    
    VAN-1596

--- lms/djangoapps/learner_recommendations/views.py
+++ lms/djangoapps/learner_recommendations/views.py
@@ -48,6 +48,9 @@ log = logging.getLogger(__name__)
 
 class AboutPageRecommendationsView(APIView):
     """
+    IMPORTANT: Please do not update or use this API. This code has been moved to edx-recommendations plugin.
+    Please use that plugin for further code changes. This API will be removed as part of VAN-1427.
+
     **Example Request**
 
     GET api/learner_recommendations/amplitude/{course_id}/
