2222da2265fa0ded47f5bb1c6009759d80023a91
John Nagro <jnagro@edx.org>
2023-08-24 18:37:04 +0000

    fix: tweaks to pass tests

--- common/djangoapps/student/handlers.py
+++ common/djangoapps/student/handlers.py
@@ -11,14 +11,14 @@ from openedx_events.learning.signals import (
 
 
 @receiver(COURSE_UNENROLLMENT_COMPLETED)
-def course_unenrollment_receiver(sender, **kwargs):
+def course_unenrollment_receiver(sender, signal, **kwargs):
     """
     Removes user notification preference when user un-enrolls from the course
     """
     if settings.FEATURES.get("ENABLE_SEND_ENROLLMENT_EVENTS_OVER_BUS"):
         get_producer().send(
             signal=COURSE_UNENROLLMENT_COMPLETED,
-            topic=getattr(settings, "EVENT_BUS_ENROLLMENT_LIFECYCLE_TOPIC", "course-authoring-enrollment-lifecycle"),
+            topic=getattr(settings, "EVENT_BUS_ENROLLMENT_LIFECYCLE_TOPIC", "course-unenrollment-lifecycle"),
             event_key_field='enrollment.course.course_key',
             event_data={'enrollment': kwargs.get('enrollment')},
             event_metadata=kwargs.get('metadata')
--- common/djangoapps/student/tests/test_handlers.py
+++ common/djangoapps/student/tests/test_handlers.py
@@ -33,10 +33,10 @@ class UnenrollmentEventBusTests(unittest.TestCase):
         Test to verify that we do not push `CERTIFICATE_CREATED` events to the event bus if the
         `SEND_CERTIFICATE_CREATED_SIGNAL` setting is disabled.
         """
-        course_unenrollment_receiver(None)
+        course_unenrollment_receiver(None, None)
         mock_producer.assert_not_called()
 
-    @override_settings(ENABLE_SEND_ENROLLMENT_EVENTS_OVER_BUS=True)
+    @override_settings(FEATURES={'ENABLE_SEND_ENROLLMENT_EVENTS_OVER_BUS': True})
     @mock.patch('common.djangoapps.student.handlers.get_producer', autospec=True)
     def test_event_enabled(self, mock_producer):
         """
@@ -61,12 +61,12 @@ class UnenrollmentEventBusTests(unittest.TestCase):
             'enrollment': enrollment,
             'metadata': event_metadata
         }
-        course_unenrollment_receiver(None, **event_kwargs)
+        course_unenrollment_receiver(None, COURSE_UNENROLLMENT_COMPLETED, **event_kwargs)
 
         # verify that the data sent to the event bus matches what we expect
         print(mock_producer.return_value)
         print(mock_producer.return_value.send.call_args)
         data = mock_producer.return_value.send.call_args.kwargs
         assert data['event_data']['enrollment'] == enrollment
-        assert data['topic'] == 'course-unenrollment-completed'
+        assert data['topic'] == 'course-unenrollment-lifecycle'
         assert data['event_key_field'] == 'enrollment.course.course_key'
