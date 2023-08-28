30a7ab6fba36b65db574768fb1d4640bea6a8e25
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-08-28 14:22:43 +0500

    Revert "Revert "feat!: upgrading django-storages to 1.10.1 (#32571)" (#33109)"
    This reverts commit 191b825a2ce982cd8c1f3e560f1f69f4cf2e3535.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3801,9 +3801,10 @@ VIDEO_IMAGE_SETTINGS = dict(
     # STORAGE_KWARGS=dict(bucket='video-image-bucket'),
     STORAGE_KWARGS=dict(
         location=MEDIA_ROOT,
-        base_url=MEDIA_URL,
     ),
     DIRECTORY_PREFIX='video-images/',
+    BASE_URL=MEDIA_URL,
+
 )
 
 VIDEO_IMAGE_MAX_AGE = 31536000
@@ -3817,9 +3818,9 @@ VIDEO_TRANSCRIPTS_SETTINGS = dict(
     # STORAGE_KWARGS=dict(bucket='video-transcripts-bucket'),
     STORAGE_KWARGS=dict(
         location=MEDIA_ROOT,
-        base_url=MEDIA_URL,
     ),
     DIRECTORY_PREFIX='video-transcripts/',
+    BASE_URL=MEDIA_URL,
 )
 
 VIDEO_TRANSCRIPTS_MAX_AGE = 31536000
