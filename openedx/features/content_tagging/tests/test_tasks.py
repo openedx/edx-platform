7097dd4100c54b3888c3da4719b882a1323d9b05
Rômulo Penido <romulo@opencraft.com>
2023-08-28 20:00:07 -0300

    test: refactor tests

--- openedx/features/content_tagging/tests/test_tasks.py
+++ openedx/features/content_tagging/tests/test_tasks.py
@@ -1,6 +1,8 @@
 """
 Test for auto-tagging content
 """
+from __future__ import annotations
+
 from unittest.mock import patch
 
 import ddt
@@ -14,8 +16,8 @@ from openedx.core.lib.tests import attr
 from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.tests.test_mixed_modulestore import CommonMixedModuleStoreSetup
 
+from .. import api
 from ..models import ContentLanguageTaxonomy, TaxonomyOrg
-from ..tasks import delete_xblock_tags, update_course_tags, update_xblock_tags
 
 if not settings.configured:
     settings.configure()
@@ -23,22 +25,24 @@ if not settings.configured:
 LANGUAGE_TAXONOMY_ID = -1
 
 
-@ddt.ddt
-@attr("mongo")
-@override_settings(
-    CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-    CELERY_ALWAYS_EAGER=True,
-    BROKER_BACKEND="memory",
-)
 class TestCourseAutoTagging(CommonMixedModuleStoreSetup):
     """
     Test if the handlers are callend and if they call the right tasks
     """
 
-    def _check_tag(self, object_id, taxonomy, value):
-        object_tag = ObjectTag.objects.filter(object_id=object_id, taxonomy=taxonomy).first()
-        assert object_tag, "Tag not found"
-        assert object_tag.value == value, f"Tag value mismatch {object_tag.value} != {value}"
+    def _check_tag(self, object_id: str, taxonomy_id: int, value: str | None):
+        """
+        Check if the ObjectTag exists for the given object_id and taxonomy_id
+
+        If value is None, check if the ObjectTag does not exists
+        """
+        object_tag = ObjectTag.objects.filter(object_id=object_id, taxonomy_id=taxonomy_id).first()
+        if value is None:
+            assert not object_tag, f"Expected no tag for taxonomy_id={taxonomy_id}, but one found with value={value}"
+        else:
+            assert object_tag, f"Tag for taxonomy_id={taxonomy_id} with value={value} with expected, but none found"
+            assert object_tag.value == value, f"Tag value mismatch {object_tag.value} != {value}"
+
         return True
 
     @classmethod
@@ -56,62 +60,61 @@ class TestCourseAutoTagging(CommonMixedModuleStoreSetup):
     def setUp(self):
         super().setUp()
         self.orgA = Organization.objects.create(name="Organization A", short_name="orgA")
+        self.initdb(ModuleStoreEnum.Type.split)
+        self.patcher = patch("openedx.features.content_tagging.tasks.modulestore", return_value=self.store)
+        self.addCleanup(self.patcher.stop)
+        self.patcher.start()
 
-    @ddt.data(
-        ModuleStoreEnum.Type.mongo,
-        ModuleStoreEnum.Type.split,
-    )
-    @patch("openedx.features.content_tagging.tasks.modulestore")
-    def test_create_course_with_xblock(self, default_ms, mock_modulestore):
-        with patch.object(update_course_tags, "delay") as mock_update_course_tags:
-            self.initdb(default_ms)
-            mock_modulestore.return_value = self.store
-            # initdb will create a Course and trigger mock_update_course_tags, so we need to reset it
-            mock_update_course_tags.reset_mock()
+    def tearDown(self):
+        self.patcher.stop()
+        super().tearDown()
 
-            # Create course
-            course = self.store.create_course(
-                self.orgA.short_name, "test_course", "test_run", self.user_id, fields={"language": "pt"}
-            )
-            course_key_str = str(course.id)
-            # Check if task was called
-            mock_update_course_tags.assert_called_with(course_key_str)
-
-        # Make the actual call synchronously
-        assert update_course_tags(course_key_str)
+    def test_create_course(self):
+        # Create course
+        course = self.store.create_course(
+            self.orgA.short_name, "test_course", "test_run", self.user_id, fields={"language": "pt"}
+        )
 
         # Check if the tags are created in the Course
         assert self._check_tag(course.id, LANGUAGE_TAXONOMY_ID, "Portuguese")
 
-        with patch.object(update_xblock_tags, "delay") as mock_update_xblock_tags:
-            # Create XBlock
-            sequential = self.store.create_child(self.user_id, course.location, "sequential", "test_sequential")
-            vertical = self.store.create_child(self.user_id, sequential.location, "vertical", "test_vertical")
+    def test_update_course(self):
+        # Create course
+        course = self.store.create_course(
+            self.orgA.short_name, "test_course", "test_run", self.user_id, fields={"language": "pt"}
+        )
 
-            # publish sequential changes
-            self.store.publish(sequential.location, self.user_id)
+        # Simulates user manually changing a tag
+        lang_taxonomy = Taxonomy.objects.get(pk=LANGUAGE_TAXONOMY_ID)
+        api.tag_content_object(lang_taxonomy, ["Spanish"], course.id)
 
-            usage_key_str = str(vertical.location)
-            # Check if task was called
-            mock_update_xblock_tags.assert_any_call(usage_key_str)
+        # Update course language
+        course.language = "en"
+        self.store.update_item(course, self.user_id)
 
-        # Make the actual call synchronously
-        assert update_xblock_tags(usage_key_str)
+        # Does not automatically update the tag
+        assert self._check_tag(course.id, LANGUAGE_TAXONOMY_ID, "Spanish")
+
+    def test_create_delete_xblock(self):
+        # Create course
+        course = self.store.create_course(
+            self.orgA.short_name, "test_course", "test_run", self.user_id, fields={"language": "pt"}
+        )
+
+        # Create XBlocks
+        sequential = self.store.create_child(self.user_id, course.location, "sequential", "test_sequential")
+        vertical = self.store.create_child(self.user_id, sequential.location, "vertical", "test_vertical")
+
+        # Publish sequential changes
+        self.store.publish(sequential.location, self.user_id)
+
+        usage_key_str = str(vertical.location)
 
         # Check if the tags are created in the XBlock
         assert self._check_tag(usage_key_str, LANGUAGE_TAXONOMY_ID, "Portuguese")
 
-        # Update course language
-        with patch.object(update_course_tags, "delay") as mock_update_course_tags:
-            course.language = "en"
-            self.store.update_item(course, self.user_id)
-            # Check if task was called
-            mock_update_course_tags.assert_called_with(course_key_str)
+        # Delete the XBlock
+        self.store.delete_item(vertical.location, self.user_id)
 
-        # Make the actual call synchronously
-        assert update_course_tags(course_key_str)
-
-        self.store.publish(sequential.location, self.user_id)
-        with patch.object(delete_xblock_tags, "delay") as mock_delete_xblock_tags:
-            self.store.delete_item(vertical.location, self.user_id)
-            mock_delete_xblock_tags.assert_called_with(usage_key_str)
+        # Check if the tags are deleted
+        assert self._check_tag(usage_key_str, LANGUAGE_TAXONOMY_ID, None)
