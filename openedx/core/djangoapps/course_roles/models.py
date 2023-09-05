fd30ddc043ad8ad0f481fd91582781e48ba740c4
Julian Palmerio <julianpalmerio@hotmail.com>
2023-09-05 14:53:50 -0300

    feat: add CourseRolesRole model

--- openedx/core/djangoapps/course_roles/models.py
+++ openedx/core/djangoapps/course_roles/models.py
@@ -4,6 +4,26 @@ Models for course roles schema
 from django.db import models
 
 
+class CourseRolesRole(models.Model):
+    """
+    Model for a course role.
+    """
+    class RoleType(models.TextChoices):
+        """
+        Enum for the role type.
+        """
+        DEFAULT = 'default'
+        CUSTOM = 'custom'
+    name = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255)
+    long_description = models.TextField()
+    type = models.CharField(max_length=64, choices=RoleType.choices, default=RoleType.DEFAULT)
+    service = models.ForeignKey('CourseRolesService', on_delete=models.DO_NOTHING, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class CourseRolesService(models.Model):
     """
     Model for a course roles service.
