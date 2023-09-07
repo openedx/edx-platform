c1dcfa5d4fb13172e46b6a5de650ea139d66e437
Julian Palmerio <julianpalmerio@hotmail.com>
2023-09-07 18:59:05 -0300

    feat: add permission check logic

--- openedx/core/djangoapps/course_roles/helpers.py
+++ openedx/core/djangoapps/course_roles/helpers.py
@@ -2,12 +2,18 @@
 Helpers for the course roles app.
 """
 
+from openedx.core.djangoapps.course_roles.models import CourseRolesUserRole
 from openedx.core.lib.cache_utils import request_cached
 
 
 @request_cached()
-def permission_check():
+def permission_check(user, permission, course_key):
     """
     Check if a user has a permission.
     """
     pass
+    return CourseRolesUserRole.objects.filter(
+        user=user,
+        role__permissions__name=permission,
+        course=course_key,
+    ).exists()
