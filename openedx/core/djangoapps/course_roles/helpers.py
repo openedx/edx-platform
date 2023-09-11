2f14498ae7f20e751a231f2463e8b7b9c10aecad
Julian Palmerio <julianpalmerio@hotmail.com>
2023-09-11 13:38:02 -0300

    feat: split permission_check function

--- openedx/core/djangoapps/course_roles/helpers.py
+++ openedx/core/djangoapps/course_roles/helpers.py
@@ -1,19 +1,27 @@
 """
 Helpers for the course roles app.
 """
-
 from openedx.core.djangoapps.course_roles.models import CourseRolesUserRole
 from openedx.core.lib.cache_utils import request_cached
 
 
 @request_cached()
-def permission_check(user, permission, course_key):
+def course_permission_check(user, permission_name, course_id):
     """
     Check if a user has a permission.
     """
-    pass
     return CourseRolesUserRole.objects.filter(
         user=user,
-        role__permissions__name=permission,
-        course=course_key,
+        role__permissions__name=permission_name,
+        course=course_id,
+    ).exists()
+
+
+@request_cached()
+def organization_permission_check(user, permission_name, organization_name):
+    return CourseRolesUserRole.objects.filter(
+        user=user,
+        role__permissions__name=permission_name,
+        course__isnull=True,
+        org__name=organization_name,
     ).exists()
