c8741b52f738386bcc195b563d49972277112d74
Julian Palmerio <julianpalmerio@hotmail.com>
2023-09-13 10:47:41 -0300

    feat: ROLES-3 Model Setup (#33229)
    * feat: add CourseRolesService model
    
    * feat: add CourseRolesRole model
    
    * feat: add CourseRolesPermission model
    
    * feat: add CourseRolesRolePermissions model
    
    * feat: add CourseRolesUserRole model
    
    * feat: remove wrong unique_together in CourseRolesUserRole
    
    * feat: add coures_roles app to cms and lms common envs
    
    * feat: add migrations
    
    * feat: delete wrong migration
    
    * feat: change many to many relationships to foreign keys
    
    * feat: add many to many relationships between role-user and role-permission
    
    * feat: add initial migration
    
    * docs: add readme file
    
    * docs: update readme file
    
    * chore: add course_roles in quality matrix CI
    
    * feat: remove fields related to custom roles
    
    * docs: update docstrings
    
    * feat: add minimal string representation change
    
    * feat: remove descriptions from role and permission tables
    
    * feat: swich the role to service relationship into a many to many relationship
    
    * docs: update role docstring
    
    * feat: update initial migration
    
    * feat: change on_delete for org and coures on UserRole model to cascade
    
    * feat: update initial migration
    
    * docs: update CourseRolesPermission model docstring

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3312,6 +3312,9 @@ INSTALLED_APPS = [
     # Notifications
     'openedx.core.djangoapps.notifications',
     'openedx_events',
+
+    # Course Roles
+    'openedx.core.djangoapps.course_roles',
 ]
 
 ######################### CSRF #########################################
---
+++ openedx/core/djangoapps/course_roles/models.py
@@ -0,0 +1,103 @@
+"""
+Models for course roles schema
+"""
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from organizations.models import Organization
+
+User = get_user_model()
+
+
+class CourseRolesRole(models.Model):
+    """
+    Model for a course roles role.
+
+    A role is a collection of permissions that can be assigned to a user.
+    The services field defines for which service UI the role is intended, such as CMS and/or LMS.
+
+    """
+    name = models.CharField(max_length=255)
+    services = models.ManyToManyField('CourseRolesService', through='CourseRolesRoleService')
+    permissions = models.ManyToManyField('CourseRolesPermission', through='CourseRolesRolePermissions')
+    users = models.ManyToManyField(User, through='CourseRolesUserRole')
+
+    def __str__(self):
+        return self.name
+
+
+class CourseRolesPermission(models.Model):
+    """
+    Model for a course roles permission.
+
+    A permission represents what a user can do.
+    """
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CourseRolesRolePermissions(models.Model):
+    """
+    Model for a course roles role permission.
+
+    A role permission is a mapping between a role and a permission.
+    """
+    role = models.ForeignKey('CourseRolesRole', on_delete=models.CASCADE)
+    permission = models.ForeignKey('CourseRolesPermission', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.role} - {self.permission}"
+
+
+class CourseRolesUserRole(models.Model):
+    """
+    Model for a course roles user role.
+
+    A user role is a mapping between a user, a role, a course and an organization.
+    If the course is null, the user role is for the organization.
+    If the course and the organization are null the role is for the instance.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.ForeignKey('CourseRolesRole', on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        CourseOverview,
+        db_constraint=False,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    org = models.ForeignKey(Organization, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        unique_together = ('user', 'role', 'course')
+
+    def __str__(self):
+        return f"{self.user} - {self.course} - {self.role} - {self.org}"
+
+
+class CourseRolesService(models.Model):
+    """
+    Model for a course roles service.
+
+    A service is a UI that can be used to assign roles to users.
+    Such as CMS or LMS.
+    """
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CourseRolesRoleService(models.Model):
+    """
+    Model for a course roles role service.
+
+    A role service is a mapping between a role and a service.
+    """
+    role = models.ForeignKey('CourseRolesRole', on_delete=models.CASCADE)
+    service = models.ForeignKey('CourseRolesService', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.role} - {self.service}"
