f4c7f9803460e460f87947d141bd2ff843fa6ac5
Justin Hynes <Jhynes@edx.org>
2023-09-22 17:20:00 +0000

    fix: update toggle description to fix copy/paste issue

--- lms/djangoapps/certificates/config.py
+++ lms/djangoapps/certificates/config.py
@@ -34,8 +34,8 @@ SEND_CERTIFICATE_CREATED_SIGNAL = SettingToggle('SEND_CERTIFICATE_CREATED_SIGNAL
 # .. toggle_implementation: SettingToggle
 # .. toggle_default: False
 # .. toggle_description: When True, the system will publish `CERTIFICATE_REVOKED` signals to the event bus. The
-#   `CERTIFICATE_REVOKED` signal is emit when a certificate has been awarded to a learner and the creation process has
-#   completed.
+#   `CERTIFICATE_REVOKED` signal is emit when a certificate has been revoked from a learner and the revocation process
+#   has completed.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2023-09-15
 # .. toggle_target_removal_date: 2024-01-01
