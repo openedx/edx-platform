8c9232ace9343dd897c31adfa73cdff4ded5db42
Muhammad Adeel Tajamul <77053848+muhammadadeeltajamul@users.noreply.github.com>
2023-09-25 11:14:32 +0500

    feat: updated notification admin (#33320)

--- openedx/core/djangoapps/notifications/admin.py
+++ openedx/core/djangoapps/notifications/admin.py
@@ -3,8 +3,52 @@ Django Admin for Notifications
 """
 
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
+from .base_notification import COURSE_NOTIFICATION_APPS, COURSE_NOTIFICATION_TYPES
 from .models import CourseNotificationPreference, Notification
+
+
+class NotificationAppNameListFilter(admin.SimpleListFilter):
+    """
+    Shows list filter in django admin of notification apps
+    """
+    title = _("Notification App")
+    parameter_name = "app_name"
+
+    def lookups(self, request, model_admin):
+        lookup_list = [
+            (app_name, app_name)
+            for app_name in COURSE_NOTIFICATION_APPS.keys()
+        ]
+        return lookup_list
+
+    def queryset(self, request, queryset):
+        app_name = self.value()
+        if app_name not in COURSE_NOTIFICATION_APPS.keys():
+            return queryset
+        return queryset.filter(app_name=app_name)
+
+
+class NotificationTypeListFilter(admin.SimpleListFilter):
+    """
+    Shows list filter in django admin of notification types
+    """
+    title = _("Notification Type")
+    parameter_name = "notification_type"
+
+    def lookups(self, request, model_admin):
+        lookup_list = [
+            (notification_type, notification_type)
+            for notification_type in COURSE_NOTIFICATION_TYPES.keys()
+        ]
+        return lookup_list
+
+    def queryset(self, request, queryset):
+        notification_type = self.value()
+        if notification_type not in COURSE_NOTIFICATION_TYPES.keys():
+            return queryset
+        return queryset.filter(notification_type=notification_type)
 
 
 class NotificationAdmin(admin.ModelAdmin):
@@ -12,8 +56,8 @@ class NotificationAdmin(admin.ModelAdmin):
     Admin for Notifications
     """
     raw_id_fields = ('user',)
-    search_fields = ('course_id', 'user__username')
-    list_filter = ('app_name',)
+    search_fields = ('course_id', 'app_name', 'notification_type', 'user__username')
+    list_filter = (NotificationAppNameListFilter, NotificationTypeListFilter)
 
 
 class CourseNotificationPreferenceAdmin(admin.ModelAdmin):
