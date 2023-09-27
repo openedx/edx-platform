73c4e110fee4b780588bf68352d5776cacff86e5
salman2013 <salman.nawaz@arbisoft.com>
2023-09-27 11:27:41 +0500

    chore: remove karma

--- pavelib/utils/envs.py
+++ pavelib/utils/envs.py
@@ -86,25 +86,25 @@ class Env:
     TEST_DIR = REPO_ROOT / ".testids"
 
     # Configured browser to use for the js test suites
-    SELENIUM_BROWSER = os.environ.get('SELENIUM_BROWSER', 'firefox')
-    if USING_DOCKER:
-        KARMA_BROWSER = 'ChromeDocker' if SELENIUM_BROWSER == 'chrome' else 'FirefoxDocker'
-    else:
-        KARMA_BROWSER = 'FirefoxNoUpdates'
+    # SELENIUM_BROWSER = os.environ.get('SELENIUM_BROWSER', 'firefox')
+    # if USING_DOCKER:
+    #     KARMA_BROWSER = 'ChromeDocker' if SELENIUM_BROWSER == 'chrome' else 'FirefoxDocker'
+    # else:
+    #     KARMA_BROWSER = 'FirefoxNoUpdates'
 
     # Files used to run each of the js test suites
     # TODO:  Store this as a dict. Order seems to matter for some
     # reason. See issue TE-415.
-    KARMA_CONFIG_FILES = [
-        REPO_ROOT / 'cms/static/karma_cms.conf.js',
-        REPO_ROOT / 'cms/static/karma_cms_squire.conf.js',
-        REPO_ROOT / 'cms/static/karma_cms_webpack.conf.js',
-        REPO_ROOT / 'lms/static/karma_lms.conf.js',
-        REPO_ROOT / 'xmodule/js/karma_xmodule.conf.js',
-        REPO_ROOT / 'xmodule/js/karma_xmodule_webpack.conf.js',
-        REPO_ROOT / 'common/static/karma_common.conf.js',
-        REPO_ROOT / 'common/static/karma_common_requirejs.conf.js',
-    ]
+    # KARMA_CONFIG_FILES = [
+    #     REPO_ROOT / 'cms/static/karma_cms.conf.js',
+    #     REPO_ROOT / 'cms/static/karma_cms_squire.conf.js',
+    #     REPO_ROOT / 'cms/static/karma_cms_webpack.conf.js',
+    #     REPO_ROOT / 'lms/static/karma_lms.conf.js',
+    #     REPO_ROOT / 'xmodule/js/karma_xmodule.conf.js',
+    #     REPO_ROOT / 'xmodule/js/karma_xmodule_webpack.conf.js',
+    #     REPO_ROOT / 'common/static/karma_common.conf.js',
+    #     REPO_ROOT / 'common/static/karma_common_requirejs.conf.js',
+    # ]
 
     JS_TEST_ID_KEYS = [
         'cms',
