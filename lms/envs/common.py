512bc405ccdb6509b4fe72994bdbaa7e1757aa8e
jszewczulak <128841175+jszewczulak@users.noreply.github.com>
2023-09-27 14:53:11 -0400

    feat: added routing keys for ORA workflow batch update (#33136)
    * feat: added routing key for ORA batch update

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3772,6 +3772,7 @@ PASSWORD_POLICY_COMPLIANCE_ROLLOUT_CONFIG = {
 }
 
 ############################ ORA 2 ############################################
+ORA_WORKFLOW_UPDATE_ROUTING_KEY = "edx.lms.core.ora_workflow_update"
 
 # By default, don't use a file prefix
 ORA2_FILE_PREFIX = None
