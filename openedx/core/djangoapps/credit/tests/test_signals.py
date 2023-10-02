b41f4f1e7254bb6905ceabebe727951bf165a76d
Zach Hancock <zhancock@edx.org>
2023-10-02 15:05:35 -0400

    feat: add remianing exam credit state handlers

--- openedx/core/djangoapps/credit/tests/test_signals.py
+++ openedx/core/djangoapps/credit/tests/test_signals.py
@@ -1,7 +1,6 @@
 """
-Tests for minimum grade requirement status
+Tests for minimum grade and credit requirement status
 """
-
 
 from datetime import datetime, timedelta, timezone
 from unittest import mock
@@ -13,14 +12,27 @@ from django.test.client import RequestFactory
 from opaque_keys.edx.keys import UsageKey
 from openedx_events.data import EventsMetadata
 from openedx_events.learning.data import ExamAttemptData, UserData, UserPersonalData
-from openedx_events.learning.signals import EXAM_ATTEMPT_RESET
+from openedx_events.learning.signals import (
+    EXAM_ATTEMPT_ERRORED,
+    EXAM_ATTEMPT_REJECTED,
+    EXAM_ATTEMPT_RESET,
+    EXAM_ATTEMPT_SUBMITTED,
+    EXAM_ATTEMPT_VERIFIED
+)
 
 from common.djangoapps.course_modes.models import CourseMode
 from common.djangoapps.student.models import CourseEnrollment
 from common.djangoapps.student.tests.factories import UserFactory
 from openedx.core.djangoapps.credit.api import get_credit_requirement_status, set_credit_requirements
 from openedx.core.djangoapps.credit.models import CreditCourse, CreditProvider
-from openedx.core.djangoapps.credit.signals import handle_exam_reset, listen_for_grade_calculation
+from openedx.core.djangoapps.credit.signals import (
+    listen_for_exam_errored,
+    listen_for_exam_rejected,
+    listen_for_exam_reset,
+    listen_for_exam_submitted,
+    listen_for_exam_verified,
+    listen_for_grade_calculation
+)
 from openedx.core.djangolib.testing.utils import skip_unless_lms
 from xmodule.modulestore.tests.django_utils import \
     ModuleStoreTestCase  # lint-amnesty, pylint: disable=wrong-import-order
@@ -138,10 +150,18 @@ class TestMinGradedRequirementStatus(ModuleStoreTestCase):
 
 
 @skip_unless_lms
+@ddt.ddt
 class TestExamEvents(ModuleStoreTestCase):
     """
     Test exam events
     """
+    HANDLERS = {
+        EXAM_ATTEMPT_ERRORED: listen_for_exam_errored,
+        EXAM_ATTEMPT_REJECTED: listen_for_exam_rejected,
+        EXAM_ATTEMPT_RESET: listen_for_exam_reset,
+        EXAM_ATTEMPT_SUBMITTED: listen_for_exam_submitted,
+        EXAM_ATTEMPT_VERIFIED: listen_for_exam_verified,
+    }
 
     def setUp(self):
         super().setUp()
@@ -196,37 +216,77 @@ class TestExamEvents(ModuleStoreTestCase):
         event_data = self._get_exam_event_data(self.user, self.course, self.subsection_key)
         event_metadata = self._get_exam_event_metadata(EXAM_ATTEMPT_RESET)
 
-        handle_exam_reset(None, EXAM_ATTEMPT_RESET, event_metadata=event_metadata, exam_attempt=event_data)
+        listen_for_exam_reset(None, EXAM_ATTEMPT_RESET, event_metadata=event_metadata, exam_attempt=event_data)
 
         mock_remove_credit_status.assert_called_once_with(
-            self.user.username, self.course.id, 'special_exam', str(self.subsection_key)
+            self.user.username, self.course.id, 'exam', str(self.subsection_key)
         )
 
-    def test_exam_reset_bad_user(self):
+    @ddt.data(
+        (EXAM_ATTEMPT_ERRORED, 'failed'),
+        (EXAM_ATTEMPT_REJECTED, 'failed'),
+        (EXAM_ATTEMPT_VERIFIED, 'satisfied'),
+        (EXAM_ATTEMPT_SUBMITTED, 'submitted'),
+    )
+    @ddt.unpack
+    @mock.patch('openedx.core.djangoapps.credit.signals.set_credit_requirement_status', autospec=True)
+    def test_exam_update_event(self, event_signal, expected_status, mock_set_credit_status):
         """
-        Test exam reset event with a user that does not exist in the LMS
+        Test exam events that update credit status
+        """
+        event_data = self._get_exam_event_data(self.user, self.course, self.subsection_key)
+        event_metadata = self._get_exam_event_metadata(event_signal)
+
+        handler = self.HANDLERS.get(event_signal)
+        handler(None, event_signal, event_metadata=event_metadata, exam_attempt=event_data)
+
+        mock_set_credit_status.assert_called_once_with(
+            self.user.username, self.course.id, 'exam', str(self.subsection_key), status=expected_status
+        )
+
+    @ddt.data(
+        EXAM_ATTEMPT_RESET,
+        EXAM_ATTEMPT_REJECTED,
+        EXAM_ATTEMPT_ERRORED,
+        EXAM_ATTEMPT_VERIFIED,
+        EXAM_ATTEMPT_SUBMITTED,
+    )
+    def test_exam_event_bad_user(self, event_signal):
+        """
+        Test exam event with a user that does not exist in the LMS
         """
         self.user.id = 999  # don't save to db so user doesn't exist
         event_data = self._get_exam_event_data(self.user, self.course, self.subsection_key)
-        event_metadata = self._get_exam_event_metadata(EXAM_ATTEMPT_RESET)
+        event_metadata = self._get_exam_event_metadata(event_signal)
+        handler = self.HANDLERS.get(event_signal)
 
         with mock.patch('openedx.core.djangoapps.credit.signals.log.error') as mock_log:
-            handle_exam_reset(None, EXAM_ATTEMPT_RESET, event_metadata=event_metadata, exam_attempt=event_data)
+            handler(None, event_signal, event_metadata=event_metadata, exam_attempt=event_data)
             mock_log.assert_called_once_with(
-                'Error occurred while handling EXAM_ATTEMPT_RESET signal for '
+                'Error occurred while handling exam event for '
                 f'{self.user.id} and content_id {self.subsection_key}. '
                 'User does not exist!'
             )
 
+    @ddt.data(
+        EXAM_ATTEMPT_RESET,
+        EXAM_ATTEMPT_REJECTED,
+        EXAM_ATTEMPT_ERRORED,
+        EXAM_ATTEMPT_VERIFIED,
+        EXAM_ATTEMPT_SUBMITTED,
+    )
     @mock.patch('openedx.core.djangoapps.credit.signals.remove_credit_requirement_status', autospec=True)
-    def test_exam_reset_non_credit_course(self, mock_remove_credit_status):
+    @mock.patch('openedx.core.djangoapps.credit.signals.set_credit_requirement_status', autospec=True)
+    def test_exam_event_non_credit_course(self, event_signal, mock_remove_credit_status, mock_set_credit_status):
         """
-        Credit logic should not run on non-credit courses
+        Credit credit logic should not run on non-credit courses
         """
         non_credit_course = CourseFactory.create()
         event_data = self._get_exam_event_data(self.user, non_credit_course, self.subsection_key)
-        event_metadata = self._get_exam_event_metadata(EXAM_ATTEMPT_RESET)
+        event_metadata = self._get_exam_event_metadata(event_signal)
+        handler = self.HANDLERS.get(event_signal)
 
-        handle_exam_reset(None, EXAM_ATTEMPT_RESET, event_metadata=event_metadata, exam_attempt=event_data)
+        handler(None, event_signal, event_metadata=event_metadata, exam_attempt=event_data)
 
         mock_remove_credit_status.assert_not_called()
+        mock_set_credit_status.assert_not_called()
