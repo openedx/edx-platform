23befb3ad837ae61415eb2df9a6635673fdce9b5
Julian Palmerio <julianpalmerio@hotmail.com>
2023-10-04 15:04:53 -0300

    ROLES-23 Create permissions in db table (#33394)
    * feat: add migration to load permissions in the database
    
    * feat: add Permission enum
    
    * feat: change Permission enum name to CourseRolesPermission
    
    * feat: replace permission constants with the CourseRolesPermission enum
    
    * feat: add unique decorator to permissions enum
    
    * feat: add course_roles_permissions dict with names and descriptions (with i18n)
    
    * docs: add CourseRolesPermission docstring
    
    * style: fix pylint errors
    
    * style: fix pylint errors
    
    * docs: add permissions module docstring

--- common/djangoapps/student/api.py
+++ common/djangoapps/student/api.py
@@ -31,6 +31,7 @@ from common.djangoapps.student.roles import (
     REGISTERED_ACCESS_ROLES as _REGISTERED_ACCESS_ROLES,
 )
 from openedx.core.djangoapps.course_roles.helpers import course_permission_check
+from openedx.core.djangoapps.course_roles.permissions import CourseRolesPermission
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 
@@ -131,7 +132,6 @@ def is_user_staff_or_instructor_in_course(user, course_key):
     """
     if not isinstance(course_key, CourseKey):
         course_key = CourseKey.from_string(course_key)
-    MANAGE_STUDENTS_PERMISSION = "manage_students"
     # TODO: course roles: If the course roles feature flag is disabled the course_permission_check call
     #       below will never be true.
     #       Remove the CourseStaffRole and the CourseInstructorRole .has_user() calls below when course
@@ -140,5 +140,5 @@ def is_user_staff_or_instructor_in_course(user, course_key):
         GlobalStaff().has_user(user) or
         CourseStaffRole(course_key).has_user(user) or
         CourseInstructorRole(course_key).has_user(user) or
-        course_permission_check(user, MANAGE_STUDENTS_PERMISSION, course_key)
+        course_permission_check(user, CourseRolesPermission.MANAGE_STUDENTS.value, course_key)
     )
