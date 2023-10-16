dcb5b9406e115d2872d81a57d8b11a426c55eb02
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-10-16 22:27:25 +0500

    feat!: updating default algorithm to sha256.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1735,7 +1735,7 @@ DATABASES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # This will be overridden through LMS config
-DEFAULT_HASHING_ALGORITHM = 'sha1'
+DEFAULT_HASHING_ALGORITHM = 'sha256'
 #################### Python sandbox ############################################
 
 CODE_JAIL = {
