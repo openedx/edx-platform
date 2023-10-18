4617ac1c502f877122c7548eab79d51ac824e0ff
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-10-18 14:21:05 +0700

    change library

--- cms/djangoapps/contentstore/views/import_excel.py
+++ cms/djangoapps/contentstore/views/import_excel.py
@@ -7,14 +7,15 @@ from cms.djangoapps.contentstore.views.certificates import _get_course_and_check
 from common.djangoapps.util.json_request import JsonResponse
 from opaque_keys.edx.keys import  CourseKey
 from common.djangoapps.edxmako.shortcuts import render_to_response
-from cms.djangoapps.contentstore.views.item import create_item_import , _update_with_callback ,_save_xblock
+from cms.djangoapps.contentstore.views.item import create_item_import , _update_with_callback ,_save_xblock 
 
-import tablib
+import openpyxl
 
 @login_required
 @ensure_csrf_cookie
 def viewImportExcel (request, course_id) :
     course_key = CourseKey.from_string(course_id)
+   
     try:
         course = _get_course_and_check_access(course_key, request.user)
     except PermissionDenied:
@@ -30,19 +31,20 @@ def viewImportExcel (request, course_id) :
     parent_locator = f'block-v1:{organization}+{course_number}+{course_run}+type@course+block@course'
     if request.method == "POST" and request.FILES.get("excel_file"):
         excel_file = request.FILES["excel_file"]
-   
+       
         if excel_file.name.endswith('.xlsx'):
-            dataset = tablib.Dataset()  
-            dataImport = dataset.load(excel_file.read(), format='xlsx')    
+            workbook = openpyxl.load_workbook(excel_file, data_only=True)
+            sheets = workbook.sheetnames
+            first_sheet = workbook['2. Details']       
 
             section=create_item_import(request, parent_locator, category='chapter', display_name='Mở đầu')
-        #     # lesson= create_item_import(request, parent_locator=str(section.location) , category='sequential', display_name='Mở đầu') 
-        #     # unit= create_item_import(request, parent_locator= str(lesson.location)  ,category='vertical', display_name='unit')  
-        #     # html = create_item_import(request, parent_locator= str(unit.location)  ,category='html')  
-           
+
             section_block = create_item_import(request ,parent_locator, category='chapter', display_name='Nội dung khoá học')
+           
             list_video = []
-            for data in dataImport:
+            url_ = ''
+            for data in first_sheet.iter_rows(values_only=True):
+                
                 if data[1] is not None :
                     if ':' in data[1] :
                         
@@ -50,19 +52,31 @@ def viewImportExcel (request, course_id) :
                         create_item_import(request, parent_locator=str(lesson_block.location), category='vertical', display_name='Mở đầu')
                         unit_block=create_item_import(request, parent_locator=str(lesson_block.location), category='vertical', display_name='Nội dung bài học')
                         html_block =create_item_import(request, parent_locator= str(unit_block.location) ,category='html')  
+                        
                     if data[3] is not None and 'video' in data[3].lower() and data[5] is not None:
                         videos =[]
                         videos.append(data[5])
-                        list_video.append({'title' : data[2], 'video' : videos})
-                        # _save_xblock(xblock=html_block , user=request.user, data=f'<p><a href="{data[5]}" title={data[2]}>video: {data[2]}</a></p>')
-            url_video = ''
-            for v in list_video :
-      
-                for e in v['video'] :
-                    url_ = f'<p><a href="{e}" target="_blank" title={v["title"]}>Video: {v["title"]}</a></p>'
-                    url_video += url_
-            _save_xblock(xblock=html_block , user=request.user, data=url_video)
+                        list_video.append({'title' : data[2], 'video' : videos , 'unit' : data[1] , 'block': html_block})
 
+            newArr = []  
+            grouped = {} 
+            for item in list_video:
+                block_value = item['block']
+                if block_value not in grouped:
+                    grouped[block_value] = []  
+                grouped[block_value].append({'title': item['title'], 'video' : item['video']})  #   
+                     
+           
+            for block_value, content in grouped.items():
+                newArr.append({'block' : block_value , 'content' : content})
+
+            for block in newArr :
+                url_ =''
+                for a in block['content']:
+                    for v in a['video']:
+                        url_ += f'<p><a href="{v}" target="_blank" title={a["title"]}>Video: {a["title"]}</a></p>'
+                _save_xblock(xblock=block['block'] , user=request.user, data=url_)
+            workbook.close()
            
                   
     return render_to_response('import_excel.html', context)
