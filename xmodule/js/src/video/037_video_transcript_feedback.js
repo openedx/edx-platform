da4aed2fe3f2bbaac0a08f31387ad3dc616bcba8
María Guillermina Véscovo <mvescovo@2u.com>
2023-10-22 19:46:11 -0300

    use url

--- xmodule/js/src/video/037_video_transcript_feedback.js
+++ xmodule/js/src/video/037_video_transcript_feedback.js
@@ -54,8 +54,8 @@
 
                     this.videoId = this.el.data('video-id');
                     this.userId = this.el.data('user-id');
-                    // this.aiTranslationsUrl = this.state.config.getFeedbackUrl;
-                    this.aiTranslationsUrl = 'http://localhost:18760/api/v1';
+                    this.aiTranslationsUrl = this.state.config.getFeedbackUrl;
+                    // this.aiTranslationsUrl = 'http://localhost:18760/api/v1';
 
                     this.thumbsUpButton = this.el.find('.thumbs-up-btn');
                     this.thumbsDownButton = this.el.find('.thumbs-down-btn');
