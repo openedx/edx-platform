19a01bbf782b0b7324f51ccb9db3bed059a5f585
fsologureng <fsologur@uchile.cl>
2023-10-24 13:02:19 -0300

    Added flags to pytest to exit immediately under error and verbosely

--- .github/test/tests.sh
+++ .github/test/tests.sh
@@ -13,7 +13,7 @@ pip3 install ora2==2.11.5.1
 
 # Task Helper modifications for uchileedxlogin package
 ## Test before installing the package
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest \
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest --exitfirst --full-trace --verbose \
 	lms/djangoapps/instructor_task/tests/test_tasks_helper.py \
 	lms/djangoapps/instructor_analytics/tests/test_basic.py \
 	lms/djangoapps/certificates/tests/test_webview_views.py \
