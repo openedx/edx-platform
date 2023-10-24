1ada91252a86eecbd7823085c3b4d3a38cac8ea2
fsologureng <fsologur@uchile.cl>
2023-10-24 10:05:18 -0300

    Revert "eol: Run tests separately"
    This reverts commit 8ff7f7f23b63704cb6e2fa5e1b6a3ee6e4b7cf05.

--- .github/test/tests.sh
+++ .github/test/tests.sh
@@ -7,8 +7,15 @@ cd /openedx/edx-platform
 mkdir -p test_root/log/
 
 # Install required edx-ora2 for tests
-#pip3 install -e git+https://github.com/fdns/edx-ora2.git@810e75fb2028272c10b3b86b645720c3e584a4c6#egg=ora2
 pip3 install ora2==2.11.5.1
 
-# Test given parameter
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest $1
+# Test EOL Modifications
+
+# Task Helper modifications for uchileedxlogin package
+## Test before installing the package
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest \
+	lms/djangoapps/instructor_task/tests/test_tasks_helper.py \
+	lms/djangoapps/instructor_analytics/tests/test_basic.py \
+	lms/djangoapps/certificates/tests/test_webview_views.py \
+	lms/djangoapps/bulk_email/tests/ \
+	lms/djangoapps/instructor/tests/
