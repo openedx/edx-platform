c2861c2dec52afdb792abb6cb62f243d6d1edd78
Max Sokolski <cmltawt0@gmail.com>
2023-10-30 13:01:10 +0200

    Merge pull request #33516 from openedx/quince-django42-upgrade
    Quince django42 upgrade

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1763,7 +1763,8 @@ DATABASES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # This will be overridden through LMS config
-DEFAULT_HASHING_ALGORITHM = 'sha1'
+DEFAULT_HASHING_ALGORITHM = 'sha256'
+
 #################### Python sandbox ############################################
 
 CODE_JAIL = {
@@ -3346,7 +3347,6 @@ CSRF_COOKIE_AGE = 60 * 60 * 24 * 7 * 52
 # end users
 CSRF_COOKIE_SECURE = False
 CSRF_TRUSTED_ORIGINS = []
-CSRF_TRUSTED_ORIGINS_WITH_SCHEME = []
 CROSS_DOMAIN_CSRF_COOKIE_DOMAIN = ''
 CROSS_DOMAIN_CSRF_COOKIE_NAME = ''
 
