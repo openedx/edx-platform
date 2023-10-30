061638d9e5a816548d88c4fe8fdaf98d85a55fc7
qasim.gulzar <qasim.gulzar@arbisoft.com>
2023-10-30 14:43:17 +0500

    fix: class students api crash

--- openedx/features/genplus_features/utils.py
+++ openedx/features/genplus_features/utils.py
@@ -1,7 +1,7 @@
 def get_full_name(instance, default=''):
     name = default
-    if instance.profile and (instance.profile.name or '').strip():
+    if instance and instance.profile and (instance.profile.name or '').strip():
         name = instance.profile.name
-    elif (instance.first_name or '').strip() or (instance.last_name or '').strip():
+    elif instance and ((instance.first_name or '').strip() or (instance.last_name or '').strip()):
         name = f'{instance.first_name} {instance.last_name}'.strip()
     return name
