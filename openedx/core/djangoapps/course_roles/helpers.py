4ac95de571d358016736b8eaf2f0329f90af8a8d
Julian Palmerio <julianpalmerio@hotmail.com>
2023-10-31 11:30:50 -0300

    feat: course_roles mfe-course authoring helper function (#33599)
    * feat: Roles 15 - permission checks back end changes part 1 (#33347)
    
    * test: add test cases for permission list check functions
    
    * test: update tests
    
    * feat: add helper functions to check lists of permissions
    
    * style: improve code style
    
    * feat: add course roles checks in the contentstore app
    
    * feat: add course roles checks in the student app
    
    * feat: add course roles checks in the lms discussion app
    
    * feat: add course roles checks in the lms instructor app
    
    * feat: add course roles checks in the Learning Sequences package
    
    * style: fix code style
    
    * fix: course_permission_check calls
    
    * feat: add validation for AnonymousUser in course permission check helper functions
    
    * fix: disable some pylint warnings
    
    * test: update number of querys asserted in has_course_author_access
    
    * feat: add helper functions to check course or organization permissions
    
    * test: update course_roles tests
    
    * feat: replace course or organization helper functions in auth
    
    * docs: update course_roles docstrings
    
    * feat: ROLES-23 Create permissions in db table (#33394)
    
    * feat: add migration to load permissions in the database
    
    * feat: add Permission enum
    
    * feat: change Permission enum name to CourseRolesPermission
    
    * feat: replace permission constants with the CourseRolesPermission enum
    
    * feat: add unique decorator to permissions enum
    
    * feat: add course_roles_permissions dict with names and descriptions (with i18n)
    
    * docs: add CourseRolesPermission docstring
    
    * style: fix pylint errors
    
    * style: fix pylint errors
    
    * docs: add permissions module docstring
    
    * feat: add helper function to get user permissions for a course
    
    * test: add test for get_all_user_permissions_for_a_course
    
    * feat: add views to coures roles api
    
    * test: add test for course roles views
    
    * feat: add urls for course roles api
    
    * feat: add course roles api urls to lms and cms
    
    * docs: add comment to indicate which urls are from course roles api
    
    * feat: add translation to ValueError exception message
    
    * feat: add translation to ValueError exception message
    
    * feat: raise exeption if course does not exist
    
    * feat: add instance permissions in get_all_user_permissions_for_a_course helper
    
    * feat: improve validations in get_all_user_permissions_for_a_course
    
    * feat: improve validations in UserPermissionsView
    
    * test: update get user permissions tests
    
    * docs: update UserPermissionsView docstring
    
    * feat: change message errors
    
    * docs: update docstrings in test_views
    
    * fix: add missing super method call in a class
    
    * fix: add password to test user
    
    * fix: chain re-raising exceptions

--- openedx/core/djangoapps/course_roles/helpers.py
+++ openedx/core/djangoapps/course_roles/helpers.py
@@ -1,7 +1,8 @@
 """
 Helpers for the course roles app.
 """
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, User  # lint-amnesty, pylint: disable=imported-auth-user
+from django.utils.translation import gettext as _
 
 from openedx.core.djangoapps.course_roles.models import CourseRolesUserRole
 from openedx.core.lib.cache_utils import request_cached
@@ -90,3 +91,41 @@ def course_or_organization_permission_list_check(user, permission_names, course_
         course_or_organization_permission_check(user, permission_name, course_id, organization_name)
         for permission_name in permission_names
     )
+
+
+def get_all_user_permissions_for_a_course(user_id, course_id):
+    """
+    Get all of a user's permissions for a course,
+    including, if applicable, organization-level permissions
+    and instance-level permissions.
+    """
+    if user_id is None or course_id is None:
+        raise ValueError(_('user_id and course_id must not be None'))
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist as exc:
+        raise ValueError(_('user does not exist')) from exc
+    try:
+        course = modulestore().get_course(course_id)
+    except AssertionError as exc:
+        raise ValueError(_('course_id is not valid')) from exc
+    if not course:
+        raise ValueError(_('course does not exist'))
+    course_permissions = set(CourseRolesUserRole.objects.filter(
+        user__id=user_id,
+        course=course_id,
+    ).values_list('role__permissions__name', flat=True))
+    organization_name = course.org
+    organization_permissions = set(CourseRolesUserRole.objects.filter(
+        user__id=user_id,
+        course__isnull=True,
+        org__name=organization_name,
+    ).values_list('role__permissions__name', flat=True))
+    permissions = course_permissions.union(organization_permissions)
+    instance_permissions = set(CourseRolesUserRole.objects.filter(
+        user__id=user_id,
+        course__isnull=True,
+        org__isnull=True,
+    ).values_list('role__permissions__name', flat=True))
+    permissions = permissions.union(instance_permissions)
+    return permissions
