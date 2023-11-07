bea7223e0280ee0074337c441ae49bec6490a39c
sonxauxi2411 <sontnfx18388@funix.edu.vn>
2023-11-07 10:04:14 +0700

    feat : change feedback

--- lms/djangoapps/feedback/views.py
+++ lms/djangoapps/feedback/views.py
@@ -5,6 +5,11 @@ from django.template import loader
 from .models import Feedback
 from .forms import FeedbackForm
 import requests
+from django.views.decorators.csrf import  ensure_csrf_cookie 
+from rest_framework.decorators import api_view
+from django.contrib.auth.decorators import login_required
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+from django.conf import settings
 
 def index(request):
     # if this is a POST request we need to process the form data
@@ -54,3 +59,46 @@ def index(request):
     response = render_to_response('feedback_form.html', context)
     # template = loader.get_template('feedback_form.html')
     return response
+
+
+
+
+@api_view(['POST'])
+def create_feedback (request) :
+    form = FeedbackForm(request.POST, request.FILES)
+    # url_lms = configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL)
+    if form.is_valid():
+        feedback = Feedback() 
+       
+        feedback.email = form.cleaned_data['email']
+        feedback.category_id = form.cleaned_data['category_id']
+        feedback.content = form.cleaned_data['content']
+        feedback.attachment = request.FILES.get('attachment', '')
+        feedback.course_id = form.cleaned_data['course_id']
+        feedback.lesson_url = form.cleaned_data['lesson_url']
+        feedback.save()
+        print('============')
+    #     if feedback.attachment == False:
+    #         attachmentURL = ''
+    #     else:
+    #         attachmentURL = url_lms+ '/media/' + feedback.attachment.name
+    #     print('==============',attachmentURL )
+    #     category = [
+    #         {"id" : 'outdated' , "content": 'Content contains outdated information' } ,
+    #         {'id' : "bad_explain" , "content" : "Content is not explained well"} ,
+    #         {"id" : "insufficient_details" , "content" : "Content needs more detail"},
+    #         {"id" : "broken_resource" , "content" : "Resource is missing or broken (link, dataset, etc)"},
+    #         {"id" : "error_translation" , "content" : "Translation Error in content"}
+    #         ]
+    #     category_id = ''
+    #     for c in category :
+    #         if form.cleaned_data['category_id'] == c['content'] :
+    #             category_id = c['id']
+    #     data = {
+    #             'ticket_title': 'lms_test',
+    #             'ticket_category': category_id,
+    #             'ticket_description': feedback.content ,
+    #             "student_id" : 3
+    #         }
+
+    return HttpResponse('success')
--- lms/static/feedback/add_feedback.js
+++ lms/static/feedback/add_feedback.js
@@ -4,16 +4,16 @@ function dropupError() {
 	const dropupElement = document.getElementById("dropupError")
 	dropupElement.classList.toggle("show");
 
-	if (dropupElement.classList.contains("show")) {
-		const btnSubmit = document.getElementById('submit-feedback')
-		const comment = document.getElementById('comment')
-		btnSubmit.setAttribute('disabled', 'disabled');
-		comment.addEventListener('input', (e)=>{
-			if (e.target.value.length > 0){
-				btnSubmit.removeAttribute('disabled')
-			}
-		})
-	  } 
+	// if (dropupElement.classList.contains("show")) {
+	// 	const btnSubmit = document.getElementById('submit-feedback')
+	// 	const comment = document.getElementById('comment')
+	// 	btnSubmit.setAttribute('disabled', 'disabled');
+	// 	comment.addEventListener('input', (e)=>{
+	// 		if (e.target.value.length > 0){
+	// 			btnSubmit.removeAttribute('disabled')
+	// 		}
+	// 	})
+	//   } 
 }
 
 
@@ -48,15 +48,40 @@ const initFUNiXFeedback = (LMSHost = '', isMFE = false) => {
 
 const handlerSubmit  = (event)=>{
 	event.preventDefault()
-	const categoryFeedback = document.getElementById('feedback-category').value;
-	const comment = document.getElementById('comment').value
-	const fileInput = document.getElementById('fileInput');
-	const selectedFile = fileInput.files[0];
-	console.log(selectedFile)
-	if (selectedFile) {
-		const formData = new FormData();
-		formData.append('file', selectedFile);
-	
-	}
+	url =this.LMSHost + '/api/feedback/create'
+	const lesson_url = window.location.href
+	const csrf_token = document.getElementById('csrf_token').value
+	const feedbackcategory = document.getElementById('feedback-category').value
+	const comment = document.getElementById('comments').value
+	const email = document.getElementById('email').value
+	const formData = new FormData();
+	const parts = lesson_url.split('/');
+	const course_id = parts[4]
+	formData.append('attachment', fileInput.files[0]);
+	formData.append('category_id', feedbackcategory)
+	formData.append('content' , comment)
+	formData.append('email' , email)
+	formData.append('lesson_url' , lesson_url)
+	formData.append('course_id', course_id)
+	fetch(url, {
+		method: 'POST', 
+		headers: {
+		  'X-CSRFToken': csrf_token
+		},
+		body: formData,
+	  })
+		.then((response) => {
+		  if (response.status === 200) {
+			return response.json(); 
+		  } else {
+			throw new Error(`API request failed with status: ${response.status}`);
+		  }
+		})
+		.then((data) => {
+		 console.log('API Response:', data);
+		})
+		.catch((error) => {
+		  console.error('API Request Error:', error);
+		});
 
 }
