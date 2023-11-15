56df5191eaebbf64482679407b8696696c3d2127
Julian Palmerio <julianpalmerio@hotmail.com>
2023-11-15 13:29:41 -0300

    feat: Add limited_staff role validation to course_roles bridgekeeper permissions

--- openedx/core/djangoapps/course_roles/bridgekeeper_permisions.py
+++ openedx/core/djangoapps/course_roles/bridgekeeper_permisions.py
@@ -22,7 +22,7 @@ perms[f'course_roles.{CourseRolesPermission.MANAGE_ADVANCED_SETTINGS.value}'] = 
     | HasPermissionRule(CourseRolesPermission.MANAGE_ADVANCED_SETTINGS.value)
 )
 perms[f'course_roles.{CourseRolesPermission.VIEW_ALL_CONTENT.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.VIEW_ALL_CONTENT.value)
 )
 perms[f'course_roles.{CourseRolesPermission.VIEW_ONLY_LIVE_PUBLISHED_CONTENT.value}'] = (
@@ -34,7 +34,7 @@ perms[f'course_roles.{CourseRolesPermission.VIEW_ALL_PUBLISHED_CONTENT.value}'] 
     HasPermissionRule(CourseRolesPermission.VIEW_ALL_PUBLISHED_CONTENT.value)
 )
 perms[f'course_roles.{CourseRolesPermission.ACCESS_INSTRUCTOR_DASHBOARD.value}'] = (
-    HasRolesRule('staff', 'instructor', 'ccx_coach', 'data_researcher')
+    HasRolesRule('staff', 'instructor', 'ccx_coach', 'data_researcher', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.ACCESS_INSTRUCTOR_DASHBOARD.value)
 )
 perms[f'course_roles.{CourseRolesPermission.ACCESS_DATA_DOWNLOADS.value}'] = (
@@ -42,11 +42,11 @@ perms[f'course_roles.{CourseRolesPermission.ACCESS_DATA_DOWNLOADS.value}'] = (
     | HasPermissionRule(CourseRolesPermission.ACCESS_DATA_DOWNLOADS.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MANAGE_GRADES.value}'] = (
-    HasRolesRule('staff', 'instructor', 'ccx_coach')
+    HasRolesRule('staff', 'instructor', 'ccx_coach', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_GRADES.value)
 )
 perms[f'course_roles.{CourseRolesPermission.VIEW_GRADEBOOK.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.VIEW_GRADEBOOK.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MANAGE_ALL_USERS.value}'] = (
@@ -54,29 +54,29 @@ perms[f'course_roles.{CourseRolesPermission.MANAGE_ALL_USERS.value}'] = (
     | HasPermissionRule(CourseRolesPermission.MANAGE_ALL_USERS.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MANAGE_USERS_EXCEPT_ADMIN_AND_STAFF.value}'] = (
-    HasRolesRule('staff')
+    HasRolesRule('staff', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_USERS_EXCEPT_ADMIN_AND_STAFF.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MANAGE_DISCUSSION_MODERATORS.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasForumsRolesRule('administrator')
     | HasPermissionRule(CourseRolesPermission.MANAGE_DISCUSSION_MODERATORS.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MANAGE_COHORTS.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_COHORTS.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MANAGE_STUDENTS.value}'] = (
-    HasRolesRule('staff', 'instructor', 'ccx_coach')
+    HasRolesRule('staff', 'instructor', 'ccx_coach', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_STUDENTS.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MODERATE_DISCUSSION_FORUMS.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasForumsRolesRule('administrator', 'moderator', 'community_ta')
     | HasPermissionRule(CourseRolesPermission.MODERATE_DISCUSSION_FORUMS.value)
 )
 perms[f'course_roles.{CourseRolesPermission.MODERATE_DISCUSSION_FORUMS_FOR_A_COHORT.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasForumsRolesRule('administrator', 'moderator', 'group_moderator', 'community_ta')
     | HasPermissionRule(CourseRolesPermission.MODERATE_DISCUSSION_FORUMS_FOR_A_COHORT.value)
 )
@@ -88,10 +88,10 @@ perms[f'course_roles.{CourseRolesPermission.MANAGE_LIBRARIES.value}'] = (
     | HasPermissionRule(CourseRolesPermission.MANAGE_LIBRARIES.value)
 )
 perms[f'course_roles.{CourseRolesPermission.GENERAL_MASQUERADING.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.GENERAL_MASQUERADING.value)
 )
 perms[f'course_roles.{CourseRolesPermission.SPECIFIC_MASQUERADING.value}'] = (
-    HasRolesRule('staff', 'instructor')
+    HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.SPECIFIC_MASQUERADING.value)
 )
