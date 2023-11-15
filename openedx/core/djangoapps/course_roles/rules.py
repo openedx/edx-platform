b93195551abc461dada9f434f01074fd56b97204
Julian Palmerio <julianpalmerio@hotmail.com>
2023-11-15 13:24:45 -0300

    feat: Add HasForumsRolesRule bridgekeeper rule to check user roles

--- openedx/core/djangoapps/course_roles/rules.py
+++ openedx/core/djangoapps/course_roles/rules.py
@@ -9,6 +9,7 @@ from xblock.core import XBlock
 from openedx.core.djangoapps.course_roles.helpers import course_or_organization_or_instance_permission_check
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from xmodule.course_block import CourseBlock
+from openedx.core.djangoapps.django_comment_common.models import Role
 from xmodule.error_block import ErrorBlock
 
 
@@ -29,3 +30,22 @@ class HasPermissionRule(Rule):  # lint-amnesty, pylint: disable=abstract-method,
             course_key = CourseKey.from_string(str(instance))
 
         return course_or_organization_or_instance_permission_check(user, self.permission, course_key, course_key.org)
+
+
+class HasForumsRolesRule(Rule):  # lint-amnesty, pylint: disable=abstract-method, missing-class-docstring
+    def __init__(self, role):
+        self.role = role
+
+    def check(self, user, instance=None):
+        if not user.is_authenticated:
+            return False
+        if isinstance(instance, CourseKey):
+            course_key = instance
+        elif isinstance(instance, (CourseBlock, CourseOverview)):
+            course_key = instance.id
+        elif isinstance(instance, (ErrorBlock, XBlock)):
+            course_key = instance.scope_ids.usage_id.course_key
+        else:
+            course_key = CourseKey.from_string(str(instance))
+
+        return Role.user_has_role_for_course(user, course_key, self.role)
