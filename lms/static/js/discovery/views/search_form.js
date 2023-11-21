9c744f2baa7e064246c3037d87d7add1ca12fb69
qtw97 <qiutianw@gmail.com>
2023-11-20 20:26:22 -0500

    Update search_form.js

--- lms/static/js/discovery/views/search_form.js
+++ lms/static/js/discovery/views/search_form.js
@@ -42,25 +42,7 @@
                 this.$loadingIndicator.addClass('hidden');
             },
 
-<<<<<<< HEAD
-            // showFoundMessage: function(count) {
-            //     var msg;
-            //     if (count > 0) {
-            //         msg = ngettext(
-            //             'Found %s course for "%s"',
-            //             'Found %s courses for "%s"',
-            //             count
-            //         );
-            //     } else {
-            //         msg = ngettext(
-            //             'Viewing %s course',
-            //             'Viewing %s courses',
-            //             count
-            //         );
-            //     }
-            //     this.$message.html(interpolate(msg, [count, _.escape(term)]));
-            // },
-=======
+           
             showFoundMessage: function(count) {
                 var msg = ngettext(
                     'Viewing %s course',
@@ -69,16 +51,15 @@
                 );
                 this.$message.html(interpolate(msg, [count]));
             },
->>>>>>> parent of 4a0cae0bfe9 (Update search_form.js)
 
-            // showNotFoundMessage: function(term) {
-            //     var msg = interpolate(
-            //         gettext('We couldn\'t find any results for "%s".'),
-            //         [_.escape(term)]
-            //     );
-            //     this.$message.html(msg);
-            //     this.clearSearch();
-            // },
+            showNotFoundMessage: function(term) {
+                var msg = interpolate(
+                    gettext('We couldn\'t find any results for "%s".'),
+                    [_.escape(term)]
+                );
+                this.$message.html(msg);
+                this.clearSearch();
+            },
 
             showErrorMessage: function(error) {
                 this.$message.text(gettext(error || 'There was an error, try searching again.'));
