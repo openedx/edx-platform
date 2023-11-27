997171d787013cf5a01f55d12b619e000dd3c586
connorhaugh <chaugh@edx.org>
2023-11-27 22:03:19 +0000

    feat: add duplicate_children logging

--- openedx/core/djangoapps/content_libraries/tasks.py
+++ openedx/core/djangoapps/content_libraries/tasks.py
@@ -300,7 +300,13 @@ def duplicate_children(
     # Then, copy over any overridden settings the course author may have applied to the blocks.
     source_block = store.get_item(BlockUsageLocator.from_string(source_block_id))
     with store.bulk_operations(source_block.scope_ids.usage_id.context_key):
-        _copy_overrides(store=store, user_id=user_id, source_block=source_block, dest_block=dest_block)
+        try:
+            TASK_LOGGER.info('Copying Overrides for %s', dest_block.scope_ids.usage_id)
+            _copy_overrides(store=store, user_id=user_id, source_block=source_block, dest_block=dest_block)
+        except Exception as exception:  # pylint: disable=broad-except
+            TASK_LOGGER.exception('Error importing children for %s', dest_block.scope_ids.usage_id, exc_info=True)
+            if task.status.state != UserTaskStatus.FAILED:
+                task.status.fail({'raw_error_msg': str(exception)})
 
 
 def _sync_children(
