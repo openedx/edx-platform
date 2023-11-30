473505c75ef43bc550c6f19f7d5cc6f33e5ff0db
KristinAoki <kaoki@2u.com>
2023-11-30 10:28:26 -0500

    fix: lint errors

--- cms/djangoapps/contentstore/asset_storage_handlers.py
+++ cms/djangoapps/contentstore/asset_storage_handlers.py
@@ -138,7 +138,8 @@ def _get_asset_usage_path(course_key, assets):
                     subsection = unit.get_parent()
                     subsection_display_name = getattr(subsection, 'display_name', '')
                     current_locations = usage_locations[asset_key_string]
-                    usage_dict['display_location'] = f'{subsection_display_name} - {unit_display_name} / {xblock_display_name}'
+                    usage_dict['display_location'] = (f'{subsection_display_name} - '
+                        f'{unit_display_name} / {xblock_display_name}')
                     usage_dict['url'] = f'/container/{unit_location}#{xblock_location}'
                     usage_locations[asset_key_string] = [*current_locations, usage_dict]
             else:
@@ -153,7 +154,8 @@ def _get_asset_usage_path(course_key, assets):
                     subsection = unit.get_parent()
                     subsection_display_name = getattr(subsection, 'display_name', '')
                     current_locations = usage_locations[asset_key_string]
-                    usage_dict['display_location'] = f'{subsection_display_name} - {unit_display_name} / {xblock_display_name}'
+                    usage_dict['display_location'] = (f'{subsection_display_name} - '
+                        f'{unit_display_name} / {xblock_display_name}')
                     usage_dict['url'] = f'/container/{unit_location}#{xblock_location}'
                     usage_locations[asset_key_string] = [*current_locations, usage_dict]
     return usage_locations
