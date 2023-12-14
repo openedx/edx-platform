97eab6989c5cc07cae4931dba2eff44d17381d61
Julian Palmerio <julianpalmerio@hotmail.com>
2023-12-14 15:47:17 -0300

    feat: fix waffle flag toggle name in docstring

--- openedx/core/djangoapps/course_roles/toggles.py
+++ openedx/core/djangoapps/course_roles/toggles.py
@@ -3,7 +3,7 @@ Toggles for Course Roles app.
 """
 from edx_toggles.toggles import WaffleFlag
 
-# .. toggle_name: FLAG_USE_PERMISSION_CHECKS
+# .. toggle_name: course_roles.use_permission_checks
 # .. toggle_implementation: WaffleFlag
 # .. toggle_default: False
 # .. toggle_description: Enabling the toggle will allow the db checks for a users permissions. These are used alongside current
@@ -12,7 +12,7 @@ from edx_toggles.toggles import WaffleFlag
 # .. toggle_creation_date: 2023-10-17
 # .. toggle_target_removal_date: 2023-12-01
 # .. toggle_warning:
-USE_PERMISSION_CHECKS_FLAG = WaffleFlag('course_roles.use_permission_checks', module_name=__name__) or False  # lint-amnesty, pylint: disable=toggle-missing-annotation
+USE_PERMISSION_CHECKS_FLAG = WaffleFlag('course_roles.use_permission_checks', module_name=__name__) or False
 
 
 def use_permission_checks():
