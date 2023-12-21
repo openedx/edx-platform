dcbf7adf23ae0c45b41117234733e1f749685057
hsinkoff <10408711+hsinkoff@users.noreply.github.com>
2023-12-21 21:29:43 +0000

    chore: update permissions to use perm_name attr

--- openedx/core/djangoapps/course_roles/permissions.py
+++ openedx/core/djangoapps/course_roles/permissions.py
@@ -17,89 +17,89 @@ from openedx.core.djangoapps.course_roles.rules import HasPermissionRule, HasFor
 perms['course_roles.is_staff'] = (
     is_staff
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_CONTENT.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_CONTENT.perm_name] = (
     HasRolesRule('staff', 'instructor')
     | HasPermissionRule(CourseRolesPermission.MANAGE_CONTENT)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_COURSE_SETTINGS.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_COURSE_SETTINGS.perm_name] = (
     HasRolesRule('staff', 'instructor')
     | HasPermissionRule(CourseRolesPermission.MANAGE_COURSE_SETTINGS)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_ADVANCED_SETTINGS.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_ADVANCED_SETTINGS.perm_name] = (
     HasRolesRule('staff', 'instructor')
     | HasPermissionRule(CourseRolesPermission.MANAGE_ADVANCED_SETTINGS)
 )
-perms[f'course_roles.{CourseRolesPermission.VIEW_ALL_CONTENT.value.name}'] = (
+perms[CourseRolesPermission.VIEW_ALL_CONTENT.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.VIEW_ALL_CONTENT)
 )
-perms[f'course_roles.{CourseRolesPermission.VIEW_LIVE_PUBLISHED_CONTENT.value.name}'] = (
+perms[CourseRolesPermission.VIEW_LIVE_PUBLISHED_CONTENT.perm_name] = (
     HasRolesRule('beta_testers', 'ccx_coach')
     | HasForumsRolesRule('administrator')
     | HasPermissionRule(CourseRolesPermission.VIEW_LIVE_PUBLISHED_CONTENT)
 )
-perms[f'course_roles.{CourseRolesPermission.VIEW_ALL_PUBLISHED_CONTENT.value.name}'] = (
+perms[CourseRolesPermission.VIEW_ALL_PUBLISHED_CONTENT.perm_name] = (
     HasPermissionRule(CourseRolesPermission.VIEW_ALL_PUBLISHED_CONTENT)
 )
-perms[f'course_roles.{CourseRolesPermission.ACCESS_INSTRUCTOR_DASHBOARD.value.name}'] = (
+perms[CourseRolesPermission.ACCESS_INSTRUCTOR_DASHBOARD.perm_name] = (
     HasRolesRule('staff', 'instructor', 'ccx_coach', 'data_researcher', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.ACCESS_INSTRUCTOR_DASHBOARD)
 )
-perms[f'course_roles.{CourseRolesPermission.ACCESS_DATA_DOWNLOADS.value.name}'] = (
+perms[CourseRolesPermission.ACCESS_DATA_DOWNLOADS.perm_name] = (
     HasRolesRule('data_researcher')
     | HasPermissionRule(CourseRolesPermission.ACCESS_DATA_DOWNLOADS)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_GRADES.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_GRADES.perm_name] = (
     HasRolesRule('staff', 'instructor', 'ccx_coach', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_GRADES)
 )
-perms[f'course_roles.{CourseRolesPermission.VIEW_GRADEBOOK.value.name}'] = (
+perms[CourseRolesPermission.VIEW_GRADEBOOK.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.VIEW_GRADEBOOK)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_ALL_USERS.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_ALL_USERS.perm_name] = (
     HasRolesRule('instructor')
     | HasPermissionRule(CourseRolesPermission.MANAGE_ALL_USERS)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_USERS_EXCEPT_ADMIN_AND_STAFF.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_USERS_EXCEPT_ADMIN_AND_STAFF.perm_name] = (
     HasRolesRule('staff', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_USERS_EXCEPT_ADMIN_AND_STAFF)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_DISCUSSION_MODERATORS.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_DISCUSSION_MODERATORS.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasForumsRolesRule('administrator')
     | HasPermissionRule(CourseRolesPermission.MANAGE_DISCUSSION_MODERATORS)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_COHORTS.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_COHORTS.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_COHORTS)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_STUDENTS.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_STUDENTS.perm_name] = (
     HasRolesRule('staff', 'instructor', 'ccx_coach', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.MANAGE_STUDENTS)
 )
-perms[f'course_roles.{CourseRolesPermission.MODERATE_DISCUSSION_FORUMS.value.name}'] = (
+perms[CourseRolesPermission.MODERATE_DISCUSSION_FORUMS.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasForumsRolesRule('administrator', 'moderator', 'community_ta')
     | HasPermissionRule(CourseRolesPermission.MODERATE_DISCUSSION_FORUMS)
 )
-perms[f'course_roles.{CourseRolesPermission.MODERATE_DISCUSSION_FORUMS_FOR_A_COHORT.value.name}'] = (
+perms[CourseRolesPermission.MODERATE_DISCUSSION_FORUMS_FOR_A_COHORT.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasForumsRolesRule('administrator', 'moderator', 'group_moderator', 'community_ta')
     | HasPermissionRule(CourseRolesPermission.MODERATE_DISCUSSION_FORUMS_FOR_A_COHORT)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_CERTIFICATES.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_CERTIFICATES.perm_name] = (
     HasPermissionRule(CourseRolesPermission.MANAGE_CERTIFICATES)
 )
-perms[f'course_roles.{CourseRolesPermission.MANAGE_LIBRARIES.value.name}'] = (
+perms[CourseRolesPermission.MANAGE_LIBRARIES.perm_name] = (
     HasRolesRule('library_user')
     | HasPermissionRule(CourseRolesPermission.MANAGE_LIBRARIES)
 )
-perms[f'course_roles.{CourseRolesPermission.GENERAL_MASQUERADING.value.name}'] = (
+perms[CourseRolesPermission.GENERAL_MASQUERADING.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.GENERAL_MASQUERADING)
 )
-perms[f'course_roles.{CourseRolesPermission.SPECIFIC_MASQUERADING.value.name}'] = (
+perms[CourseRolesPermission.SPECIFIC_MASQUERADING.perm_name] = (
     HasRolesRule('staff', 'instructor', 'limited_staff')
     | HasPermissionRule(CourseRolesPermission.SPECIFIC_MASQUERADING)
 )
