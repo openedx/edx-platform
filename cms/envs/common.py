042f9ea81d543133855bf79e9fd34e623840de6d
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-12-22 13:57:42 +0500

    chore: Update common.py

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1105,6 +1105,7 @@ DATABASES = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_HASHING_ALGORITHM = 'sha256'
 
 #################### Python sandbox ############################################
 
