38e03ce221eccbb0774ec500c55ccb4e775132cb
Rômulo Penido <romulo@opencraft.com>
2024-02-06 17:58:41 -0300

    fix: types

--- openedx/core/djangoapps/content_tagging/types.py
+++ openedx/core/djangoapps/content_tagging/types.py
@@ -4,5 +4,6 @@ Types used by content tagging API and implementation
 from typing import Union
 
 from opaque_keys.edx.keys import CourseKey, UsageKey
+from opaque_keys.edx.locator import LibraryLocatorV2
 
-ContentKey = Union[CourseKey, UsageKey]
+ContentKey = Union[LibraryLocatorV2, CourseKey, UsageKey]
--- openedx/core/djangoapps/content_tagging/utils.py
+++ openedx/core/djangoapps/content_tagging/utils.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey, UsageKey
+from opaque_keys.edx.locator import LibraryLocatorV2
 
 from .types import ContentKey
 
@@ -13,25 +14,30 @@ def get_content_key_from_string(key_str: str) -> ContentKey:
     """
     Get content key from string
     """
-    # Library locators are also subclasses of CourseKey
     try:
         return CourseKey.from_string(key_str)
     except InvalidKeyError:
         try:
-            return UsageKey.from_string(key_str)
-        except InvalidKeyError as usage_key_error:
-            raise ValueError("object_id must be from a CourseKey or a UsageKey") from usage_key_error
+            return LibraryLocatorV2.from_string(key_str)
+        except InvalidKeyError:
+            try:
+                return UsageKey.from_string(key_str)
+            except InvalidKeyError as usage_key_error:
+                raise ValueError("object_id must be a CourseKey, LibraryLocatorV2 or a UsageKey") from usage_key_error
 
 
-def get_context_key_from_key_string(key_str: str) -> CourseKey:
+def get_context_key_from_key_string(key_str: str) -> CourseKey | LibraryLocatorV2:
     """
     Get context key from an key string
     """
     content_key = get_content_key_from_string(key_str)
-    if isinstance(content_key, CourseKey):
+    if isinstance(content_key, CourseKey) or isinstance(content_key, LibraryLocatorV2):
         return content_key
 
-    if not isinstance(content_key.context_key, CourseKey):
-        raise ValueError("context must be from a CourseKey")
+    context_key = content_key.context_key
 
-    return content_key.context_key
+    if isinstance(context_key, CourseKey) or isinstance(context_key, LibraryLocatorV2):
+        return context_key
+
+    raise ValueError("context must be a CourseKey or a LibraryLocatorV2")
+
