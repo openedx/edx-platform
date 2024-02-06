d28684dbda871e3e5ab3f5b474c32d69cb390de3
Nathan Sprenkle <nsprenkle@users.noreply.github.com>
2024-02-06 15:30:31 -0500

    feat: add ORA MFE to devstack CSRF trusted origins (#34197)

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -544,6 +544,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:2000',  # frontend-app-learning
     'http://localhost:1997',  # frontend-app-account
     'http://localhost:1995',  # frontend-app-profile
+    'http://localhost:1992',  # frontend-app-ora
 ]
 
 
