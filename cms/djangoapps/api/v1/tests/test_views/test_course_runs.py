823ebee5f1633d2fc70da9fbcb2ddec95066b778
Lewis Kabui <lewisemm@users.noreply.github.com>
2024-02-19 11:14:45 +0300

    chore: Fix deprecation warning from cms-1 tests
    Fixes 1 deprecation warning from test runs on the cms-1 shard.

--- cms/djangoapps/api/v1/tests/test_views/test_course_runs.py
+++ cms/djangoapps/api/v1/tests/test_views/test_course_runs.py
@@ -286,7 +286,8 @@ class CourseRunViewSetTests(ModuleStoreTestCase):
         data['team'] = [{'user': 'invalid-username'}]
         response = self.client.post(self.list_url, data, format='json')
         self.assertEqual(response.status_code, 400)
-        self.assertDictContainsSubset({'team': ['Course team user does not exist']}, response.data)
+        invalid_course_team_user = {'team': ['Course team user does not exist']}
+        self.assertTrue(invalid_course_team_user.items() <= response.data.items())
 
     def test_images_upload(self):
         # http://www.django-rest-framework.org/api-guide/parsers/#fileuploadparser
