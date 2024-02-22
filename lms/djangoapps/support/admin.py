ae2fd05b5bd5eda742c2490075fae7eab8a4bd6f
jansenk <jkantor@edx.org>
2024-02-22 09:50:11 -0500

    feat: add db models for course reset feature

---
+++ lms/djangoapps/support/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from lms.djangoapps.support.models import CourseResetCourseOptIn, CourseResetAudit
+
+
+class CourseResetCourseOptInAdmin(admin.ModelAdmin):
+    list_display =    ['course_id', 'active']
+    fields =          ['course_id', 'active', 'created', 'modified']
+    readonly_fields = ['course_id', 'created', 'modified']
+
+
+class CourseResetAuditAdmin(admin.ModelAdmin):
+    list_display = ['course', 'user', 'status', 'created', 'completed_at', 'reset_by']
+    fields = ['created', 'modified', 'status', 'completed_at', 'course', 'user', 'course_enrollment', 'reset_by']
+    readonly_fields = fields
+    
+    @admin.display(description="user")
+    def user(self, obj):
+        return obj.course_enrollment.user
+
+
+admin.site.register(CourseResetCourseOptIn, CourseResetCourseOptInAdmin)
+admin.site.register(CourseResetAudit, CourseResetAuditAdmin)
+
