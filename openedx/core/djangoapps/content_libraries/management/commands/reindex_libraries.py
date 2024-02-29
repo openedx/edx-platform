ea01c7084968c1678f748f6b75df2e58ac19015d
Braden MacDonald <braden@opencraft.com>
2024-02-28 17:55:23 -0800

    refactor: cleanup

--- openedx/core/djangoapps/content_libraries/management/commands/reindex_libraries.py
+++ openedx/core/djangoapps/content_libraries/management/commands/reindex_libraries.py
@@ -20,7 +20,61 @@ log = logging.getLogger(__name__)
 LIBRARIES_INDEX_NAME = "content_libraries"
 
 
-class Command(BaseCommand):
+class MeiliCommandMixin:
+    """
+    Mixin for Django management commands that interact with Meilisearch
+    """
+
+    def get_meilisearch_client(self):
+        """
+        Get the Meiliesearch client
+        """
+        if hasattr(self, "_meili_client"):
+            return self._meili_client
+        # Connect to Meilisearch
+        if not settings.MEILISEARCH_URL:
+            raise CommandError("MEILISEARCH_URL is not set - search functionality disabled.")
+
+        self._meili_client = meilisearch.Client(settings.MEILISEARCH_URL, settings.MEILISEARCH_API_KEY)
+        try:
+            self._meili_client.health()
+        except MeilisearchError as err:
+            self.stderr.write(err.message)  # print this because 'raise...from...' doesn't print the details
+            raise CommandError("Unable to connect to Meilisearch") from err
+        return self._meili_client
+
+    def wait_for_meili_task(self, info: TaskInfo):
+        """
+        Simple helper method to wait for a Meilisearch task to complete
+        """
+        client = self.get_meilisearch_client()
+        current_status = client.get_task(info.task_uid)
+        while current_status.status in ("enqueued", "processing"):
+            self.stdout.write("...")
+            time.sleep(1)
+            current_status = client.get_task(info.task_uid)
+        if current_status.status != "succeeded":
+            self.stderr.write(f"Task has status: {current_status.status}")
+            self.stderr.write(str(current_status.error))
+            try:
+                err_reason = current_status.error['message']
+            except (TypeError, KeyError):
+                err_reason = "Unknown error"
+            raise MeilisearchError(err_reason)
+
+    def index_exists(self, index_name: str) -> bool:
+        """
+        Check if an index exists
+        """
+        client = self.get_meilisearch_client()
+        try:
+            client.get_index(index_name)
+        except MeilisearchError as err:
+            return False
+        return True
+
+
+class Command(MeiliCommandMixin, BaseCommand):
     """
     Build or re-build the search index for content libraries.
     """
@@ -29,17 +83,7 @@ class Command(BaseCommand):
         """
         Build a new search index
         """
-
-        # Connect to Meilisearch
-        if not settings.MEILISEARCH_URL:
-            raise CommandError("MEILISEARCH_URL is not set - search functionality disabled.")
-        # TODO: put this into a helper lib?
-        client = meilisearch.Client(settings.MEILISEARCH_URL, settings.MEILISEARCH_API_KEY)
-        try:
-            client.health()
-        except MeilisearchError as err:
-            self.stderr.write(err.message)  # print this because 'raise...from...' doesn't print the details
-            raise CommandError("Unable to connect to Meilisearch") from err
+        client = self.get_meilisearch_client()
 
         # Get the list of libraries
         self.stdout.write("Counting libraries...")
@@ -58,17 +102,12 @@ class Command(BaseCommand):
         self.stdout.write("Checking index...")
         index_name = settings.MEILISEARCH_INDEX_PREFIX + LIBRARIES_INDEX_NAME
         temp_index_name = index_name + "_new"
-        try:
-            client.get_index(temp_index_name)
-        except MeilisearchError as err:
-            pass
-        else:
+        if self.index_exists(temp_index_name):
             self.stdout.write("Index already exists. Deleting it...")
-            self._wait_for_meili_task(client, client.delete_index(temp_index_name))
+            self.wait_for_meili_task(client.delete_index(temp_index_name))
 
         self.stdout.write("Creating new index...")
-        self._wait_for_meili_task(
-            client,
+        self.wait_for_meili_task(
             client.create_index(temp_index_name, {'primaryKey': 'id'})
         )
 
@@ -81,15 +120,15 @@ class Command(BaseCommand):
                 doc = searchable_doc_for_library_block(metadata)
                 docs.append(doc)
             # Add all the docs in this library at once (usually faster than adding one at a time):
-            self._wait_for_meili_task(client, client.index(temp_index_name).add_documents(docs))
+            self.wait_for_meili_task(client.index(temp_index_name).add_documents(docs))
             num_done += len(docs)
 
         new_index_created = client.get_index(temp_index_name).created_at
-        if not self._index_exists(index_name, client):
+        if not self.index_exists(index_name):
             # We have to create the "target" index before we can successfully swap the new one into it:
-            self.stdout.write(f"Preparing to swap into index (first time)...")
-            self._wait_for_meili_task(client, client.create_index(index_name))
-        self.stdout.write(f"Swapping index...")
+            self.stdout.write("Preparing to swap into index (first time)...")
+            self.wait_for_meili_task(client.create_index(index_name))
+        self.stdout.write("Swapping index...")
         client.swap_indexes([{'indexes': [temp_index_name, index_name]}])
         # If we're using an API key that's restricted to certain index prefix(es), we won't be able to get the status
         # of this request unfortunately. https://github.com/meilisearch/meilisearch/issues/4103
@@ -100,33 +139,6 @@ class Command(BaseCommand):
             else:
                 break
         self.stdout.write("Deleting old index...")
-        self._wait_for_meili_task(client, client.delete_index(temp_index_name))
+        self.wait_for_meili_task(client.delete_index(temp_index_name))
 
         self.stdout.write(f"Done! {num_blocks} blocks indexed.")
-
-    def _wait_for_meili_task(self, client: meilisearch.Client, info: TaskInfo):
-        """
-        Simple helper method to wait for a Meilisearch task to complete
-        """
-        current_status = client.get_task(info.task_uid)
-        while current_status.status in ("enqueued", "processing"):
-            self.stdout.write("...")
-            time.sleep(1)
-            current_status = client.get_task(info.task_uid)
-        if current_status.status != "succeeded":
-            self.stderr.write(f"Task has status: {current_status.status}")
-            self.stderr.write(str(current_status.error))
-            try:
-                raise MeilisearchError(current_status.error['message'])
-            except (TypeError, KeyError):
-                raise MeilisearchError("Unknown error")
-
-    def _index_exists(self, index_name: str, client: meilisearch.Client) -> bool:
-        """
-        Check if an index exists
-        """
-        try:
-            client.get_index(index_name)
-        except MeilisearchError as err:
-            return False
-        return True
