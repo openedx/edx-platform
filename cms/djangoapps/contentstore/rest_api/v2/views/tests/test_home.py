2b54de756ec6a1b3a5b9d80e6c98cda864045e12
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-03-07 14:32:13 -0400

    test: add test suite for home page courses v2

--- cms/djangoapps/contentstore/rest_api/v2/views/tests/test_home.py
+++ cms/djangoapps/contentstore/rest_api/v2/views/tests/test_home.py
@@ -1,7 +1,9 @@
 """
 Unit tests for home page view.
 """
+from datetime import datetime, timedelta
 import ddt
+import pytz
 from django.conf import settings
 from django.urls import reverse
 from collections import OrderedDict
@@ -13,8 +15,7 @@ from rest_framework import status
 from cms.djangoapps.contentstore.tests.utils import CourseTestCase
 from cms.djangoapps.contentstore.views.course import ENABLE_GLOBAL_STAFF_OPTIMIZATION
 from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
-from xmodule.modulestore.tests.factories import CourseFactory
-from cms.djangoapps.contentstore.utils import get_lms_link_for_item, reverse_course_url
+from cms.djangoapps.contentstore.utils import reverse_course_url
 
 
 @ddt.ddt
@@ -26,41 +27,60 @@ class HomePageCoursesViewV2Test(CourseTestCase):
     def setUp(self):
         super().setUp()
         self.url = reverse("cms.djangoapps.contentstore:v2:courses")
-        CourseOverviewFactory.create(
+        self.active_course = CourseOverviewFactory.create(
             id=self.course.id,
             org=self.course.org,
-            run=self.course.id.run,
             display_name=self.course.display_name,
+        )
+        archived_course_key = self.store.make_course_key('demo-org', 'demo-number', 'demo-run')
+        self.archived_course = CourseOverviewFactory.create(
+            display_name="Demo Course (Sample)",
+            id=archived_course_key,
+            org=archived_course_key.org,
+            end=(datetime.now() - timedelta(days=365)).replace(tzinfo=pytz.UTC),
         )
 
     def test_home_page_response(self):
         """Get list of courses available to the logged in user.
 
         Expected result:
-        - A list of courses available to the logged in user.
         - A paginated response.
+        - A list of courses available to the logged in user.
         """
         response = self.client.get(self.url)
         course_id = str(self.course.id)
 
         expected_data = {
             "courses": [
-            OrderedDict([
-                ("course_key", course_id),
-                ("display_name", self.course.display_name),
-                ("lms_link", f'//{settings.LMS_BASE}/courses/{course_id}/jump_to/{self.course.location}'),
-                ("cms_link", f'//{settings.CMS_BASE}{reverse_course_url("course_handler", self.course.id)}'),
-                ("number", self.course.number),
-                ("org", self.course.org),
-                ("rerun_link", f'/course_rerun/{course_id}'),
-                ("run", self.course.id.run),
-                ("url", f'/course/{course_id}'),
-                ("is_active", True),
-            ])],
+                OrderedDict([
+                    ("course_key", course_id),
+                    ("display_name", self.course.display_name),
+                    ("lms_link", f'//{settings.LMS_BASE}/courses/{course_id}/jump_to/{self.course.location}'),
+                    ("cms_link", f'//{settings.CMS_BASE}{reverse_course_url("course_handler", self.course.id)}'),
+                    ("number", self.course.number),
+                    ("org", self.course.org),
+                    ("rerun_link", f'/course_rerun/{course_id}'),
+                    ("run", self.course.id.run),
+                    ("url", f'/course/{course_id}'),
+                    ("is_active", True),
+                ]),
+                OrderedDict([
+                    ("course_key", str(self.archived_course.id)),
+                    ("display_name", self.archived_course.display_name),
+                    ("lms_link", f'//{settings.LMS_BASE}/courses/{str(self.archived_course.id)}/jump_to/{self.archived_course.location}'),
+                    ("cms_link", f'//{settings.CMS_BASE}{reverse_course_url("course_handler", self.archived_course.id)}'),
+                    ("number", self.archived_course.number),
+                    ("org", self.archived_course.org),
+                    ("rerun_link", f'/course_rerun/{str(self.archived_course.id)}'),
+                    ("run", self.archived_course.id.run),
+                    ("url", f'/course/{str(self.archived_course.id)}'),
+                    ("is_active", False),
+                ]),
+            ],
             "in_process_course_actions": [],
         }
         expected_response = OrderedDict([
-            ('count', 1),
+            ('count', 2),
             ('num_pages', 1),
             ('next', None),
             ('previous',None),
@@ -77,9 +97,6 @@ class HomePageCoursesViewV2Test(CourseTestCase):
         Expected result:
         - A list of courses available to the logged in user for the specified org.
         """
-        demo_course_key = self.store.make_course_key('demo-org', 'demo-number', 'demo-run')
-        CourseOverviewFactory.create(id=demo_course_key, org=demo_course_key.org)
-
         response = self.client.get(self.url, {"org": "demo-org"})
 
         self.assertEqual(len(response.data['results']['courses']), 1)
@@ -87,8 +104,106 @@ class HomePageCoursesViewV2Test(CourseTestCase):
 
     @override_waffle_switch(ENABLE_GLOBAL_STAFF_OPTIMIZATION, True)
     def test_org_query_if_empty(self):
-        """Test home page with an empty org query param"""
+        """Get home page with an empty org query param.
+
+        Expected result:
+        - An empty list of courses available to the logged in user.
+        """
         response = self.client.get(self.url)
 
         self.assertEqual(len(response.data['results']['courses']), 0)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_active_only_query_if_passed(self):
+        """Get list of active courses only.
+
+        Expected result:
+        - A list of active courses available to the logged in user.
+        """
+        response = self.client.get(self.url, {"active_only": "true"})
+
+        self.assertEqual(len(response.data["results"]["courses"]), 1)
+        self.assertEqual(response.data["results"]["courses"], [OrderedDict([
+            ("course_key", str(self.course.id)),
+            ("display_name", self.course.display_name),
+            ("lms_link", f'//{settings.LMS_BASE}/courses/{str(self.course.id)}/jump_to/{self.course.location}'),
+            ("cms_link", f'//{settings.CMS_BASE}{reverse_course_url("course_handler", self.course.id)}'),
+            ("number", self.course.number),
+            ("org", self.course.org),
+            ("rerun_link", f'/course_rerun/{str(self.course.id)}'),
+            ("run", self.course.id.run),
+            ("url", f'/course/{str(self.course.id)}'),
+            ("is_active", True),
+        ])])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_archived_only_query_if_passed(self):
+        """Get list of archived courses only.
+
+        Expected result:
+        - A list of archived courses available to the logged in user.
+        """
+        response = self.client.get(self.url, {"archived_only": "true"})
+
+        self.assertEqual(len(response.data["results"]["courses"]), 1)
+        self.assertEqual(response.data["results"]["courses"], [OrderedDict([
+            ("course_key", str(self.archived_course.id)),
+            ("display_name", self.archived_course.display_name),
+            ("lms_link", f'//{settings.LMS_BASE}/courses/{str(self.archived_course.id)}/jump_to/{self.archived_course.location}'),
+            ("cms_link", f'//{settings.CMS_BASE}{reverse_course_url("course_handler", self.archived_course.id)}'),
+            ("number", self.archived_course.number),
+            ("org", self.archived_course.org),
+            ("rerun_link", f'/course_rerun/{str(self.archived_course.id)}'),
+            ("run", self.archived_course.id.run),
+            ("url", f'/course/{str(self.archived_course.id)}'),
+            ("is_active", False),
+        ])])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_search_query_if_passed(self):
+        """Get list of courses when search filter passed as a query param.
+
+        Expected result:
+        - A list of courses (active or inactive) available to the logged in user for the specified search.
+        """
+        response = self.client.get(self.url, {"search": "sample"})
+
+        self.assertEqual(len(response.data["results"]["courses"]), 1)
+        self.assertEqual(response.data["results"]["courses"], [OrderedDict([
+            ("course_key", str(self.archived_course.id)),
+            ("display_name", self.archived_course.display_name),
+            ("lms_link", f'//{settings.LMS_BASE}/courses/{str(self.archived_course.id)}/jump_to/{self.archived_course.location}'),
+            ("cms_link", f'//{settings.CMS_BASE}{reverse_course_url("course_handler", self.archived_course.id)}'),
+            ("number", self.archived_course.number),
+            ("org", self.archived_course.org),
+            ("rerun_link", f'/course_rerun/{str(self.archived_course.id)}'),
+            ("run", self.archived_course.id.run),
+            ("url", f'/course/{str(self.archived_course.id)}'),
+            ("is_active", False),
+        ])])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @ddt.data(("org", "demo-org"), ("-org", "org.4"))
+    @ddt.unpack
+    def test_order_query_if_passed(self, order_query, expected_first_org):
+        """Get list of courses when order filter passed as a query param.
+
+        Expected result:
+        - A list of courses (active or inactive) available to the logged in user for the specified order.
+        """
+        response = self.client.get(self.url, {"order": order_query})
+
+        self.assertEqual(len(response.data["results"]["courses"]), 2)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["results"]["courses"][0]["org"], expected_first_org)
+
+    def test_page_query_if_passed(self):
+        """Get list of courses when page filter passed as a query param.
+
+        Expected result:
+        - A list of courses (active or inactive) available to the logged in user for the specified page.
+        """
+        response = self.client.get(self.url, {"page": 1})
+
+        self.assertEqual(response.data["count"], 2)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
