a58b00a90e2ff8a8419945be08017608e8422287
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-03-08 15:49:22 -0400

    fix: address test failures

--- cms/djangoapps/contentstore/tests/test_course_listing.py
+++ cms/djangoapps/contentstore/tests/test_course_listing.py
@@ -37,7 +37,7 @@ from openedx.core.djangoapps.content.course_overviews.tests.factories import Cou
 from openedx.core.djangoapps.waffle_utils.testutils import WAFFLE_TABLES
 from xmodule.modulestore import ModuleStoreEnum  # lint-amnesty, pylint: disable=wrong-import-order
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase  # lint-amnesty, pylint: disable=wrong-import-order
-from xmodule.modulestore.tests.factories import CourseFactory, check_mongo_calls  # lint-amnesty, pylint: disable=wrong-import-order
+from xmodule.modulestore.tests.factories import CourseFactory  # lint-amnesty, pylint: disable=wrong-import-order
 
 TOTAL_COURSES_COUNT = 10
 USER_COURSES_COUNT = 1
