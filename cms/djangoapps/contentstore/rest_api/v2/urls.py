8e6b1f5e7d42634783056c91700a494b95fcfa0b
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-03-14 23:35:41 -0400

    refactor: return not found when feature not enabled

--- cms/djangoapps/contentstore/rest_api/v2/urls.py
+++ cms/djangoapps/contentstore/rest_api/v2/urls.py
@@ -1,18 +1,15 @@
 """Contenstore API v2 URLs."""
-from django.conf import settings
+
 from django.urls import path
 
 from cms.djangoapps.contentstore.rest_api.v2.views import HomePageCoursesViewV2
 
 app_name = "v2"
 
-if settings.FEATURES.get('ENABLE_HOME_PAGE_COURSE_API_V2', False):
-    urlpatterns = [
-        path(
-            "home/courses",
-            HomePageCoursesViewV2.as_view(),
-            name="courses",
-        ),
-    ]
-else:
-    urlpatterns = []
+urlpatterns = [
+    path(
+        "home/courses",
+        HomePageCoursesViewV2.as_view(),
+        name="courses",
+    ),
+]
