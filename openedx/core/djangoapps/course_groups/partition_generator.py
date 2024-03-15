345ac3f82ca5224b945dd3e0e0077e3e7a983b18
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-03-15 09:23:26 -0400

    refactor: address PR reviews

--- openedx/core/djangoapps/course_groups/partition_generator.py
+++ openedx/core/djangoapps/course_groups/partition_generator.py
@@ -4,7 +4,6 @@ openedx.dynamic_partition plugin.
 """
 import logging
 
-from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from openedx.core.djangoapps.course_groups.flags import CONTENT_GROUPS_FOR_TEAMS
 
