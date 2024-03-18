92943797c7fd801d0d9d00d9a12fab0d5002c66f
shavaiz.hussain <shavaiz.hussain@arbisoft.com>
2024-03-18 18:57:19 +0500

    unnecessary logs remove

--- lms/djangoapps/courseware/views/index.py
+++ lms/djangoapps/courseware/views/index.py
@@ -476,15 +476,10 @@ class CoursewareIndex(View):
             )
             try:
                 gen_user = request.user.gen_user
-                log.info('GP-920 user_email %s', request.user.email)
-                log.info('GP-920 staff_access %s', staff_access)
-                log.info('GP-920 global_staff_access %s', request.user.is_staff)
                 sections = course_block_tree.get('children', [])
-                log.info('GP-920 sections %s', sections)
 
                 if gen_user.is_student:
                     gen_class = gen_user.student.active_class
-                    log.info('GP-920 log for active_class', gen_class)
                     lessons = ClassLesson.objects.filter(class_unit__gen_class=gen_class, course_key=self.course.id)
                     for i, section in enumerate(sections):
                         lesson = lessons.get(usage_key=UsageKey.from_string(section.get('id')))
@@ -621,7 +616,6 @@ def render_accordion(request, course, table_of_contents, active_section, active_
             ('due_date_display_format', course.due_date_display_format),
         ] + list(TEMPLATE_IMPORTS.items())
     )
-    log.info('GP-920 render_accordion context', context)
     return render_to_string('courseware/accordion.html', context)
 
 
