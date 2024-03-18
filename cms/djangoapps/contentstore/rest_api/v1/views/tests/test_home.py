084adfadd83a95dfd4e8fdabfabe09b70c4b62af
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-03-18 12:52:11 -0400

    refactor: turn on feature as default

--- cms/djangoapps/contentstore/rest_api/v1/views/tests/test_home.py
+++ cms/djangoapps/contentstore/rest_api/v1/views/tests/test_home.py
@@ -4,6 +4,7 @@ Unit tests for home page view.
 import ddt
 from collections import OrderedDict
 from django.conf import settings
+from django.test import override_settings
 from django.urls import reverse
 from edx_toggles.toggles.testutils import (
     override_waffle_switch,
@@ -17,6 +18,13 @@ from cms.djangoapps.contentstore.views.course import ENABLE_GLOBAL_STAFF_OPTIMIZ
 from cms.djangoapps.contentstore.toggles import ENABLE_TAGGING_TAXONOMY_LIST_PAGE
 from openedx.core.djangoapps.content.course_overviews.tests.factories import CourseOverviewFactory
 from xmodule.modulestore.tests.factories import CourseFactory
+
+
+FEATURES_WITH_HOME_PAGE_COURSE_V2_API = settings.FEATURES.copy()
+FEATURES_WITH_HOME_PAGE_COURSE_V2_API['ENABLE_HOME_PAGE_COURSE_API_V2'] = True
+
+FEATURES_WITHOUT_HOME_PAGE_COURSE_V2_API = settings.FEATURES.copy()
+FEATURES_WITHOUT_HOME_PAGE_COURSE_V2_API['ENABLE_HOME_PAGE_COURSE_API_V2'] = False
 
 
 @ddt.ddt
@@ -74,7 +82,7 @@ class HomePageViewTest(CourseTestCase):
             f'{settings.COURSE_AUTHORING_MICROFRONTEND_URL}/taxonomies'
         )
 
-
+@override_settings(FEATURES=FEATURES_WITHOUT_HOME_PAGE_COURSE_V2_API)
 @ddt.ddt
 class HomePageCoursesViewTest(CourseTestCase):
     """
@@ -88,6 +96,7 @@ class HomePageCoursesViewTest(CourseTestCase):
             id=self.course.id,
             org=self.course.org,
             display_name=self.course.display_name,
+            display_number_with_default=self.course.number,
         )
 
     def test_home_page_response(self):
@@ -95,6 +104,32 @@ class HomePageCoursesViewTest(CourseTestCase):
         response = self.client.get(self.url)
         course_id = str(self.course.id)
 
+        expected_response = {
+            "archived_courses": [],
+            "courses": [{
+                "course_key": course_id,
+                "display_name": self.course.display_name,
+                "lms_link": f'//{settings.LMS_BASE}/courses/{course_id}/jump_to/{self.course.location}',
+                "number": self.course.number,
+                "org": self.course.org,
+                "rerun_link": f'/course_rerun/{course_id}',
+                "run": self.course.id.run,
+                "url": f'/course/{course_id}',
+            }],
+            "in_process_course_actions": [],
+        }
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        self.assertDictEqual(expected_response, response.data)
+
+    def test_home_page_response_with_api_v2(self):
+        """Check successful response content with api v2 modifications.
+
+        When the feature flag is enabled, the courses are exclusively fetched from the CourseOverview model, so
+        the values in the courses' list are OrderedDicts instead of the default dictionaries.
+        """
+        course_id = str(self.course.id)
         expected_response = {
             "archived_courses": [],
             "courses": [
@@ -112,8 +147,10 @@ class HomePageCoursesViewTest(CourseTestCase):
             "in_process_course_actions": [],
         }
 
+        with override_settings(FEATURES=FEATURES_WITH_HOME_PAGE_COURSE_V2_API):
+            response = self.client.get(self.url)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
         self.assertDictEqual(expected_response, response.data)
 
     @override_waffle_switch(ENABLE_GLOBAL_STAFF_OPTIMIZATION, True)
