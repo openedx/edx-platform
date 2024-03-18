40bfce2841c5ea63df2b39dd35a147f728ea2a88
Agrendalath <piotr@surowiec.it>
2024-03-18 23:34:35 +0100

    fixup! refactor: use single method to make ctas

--- openedx/features/personalized_learner_schedules/call_to_action.py
+++ openedx/features/personalized_learner_schedules/call_to_action.py
@@ -118,18 +118,7 @@ class PersonalizedLearnerScheduleCallToAction:
         from lms.urls import RESET_COURSE_DEADLINES_NAME
 
         if RELATIVE_DATES_DISABLE_RESET_FLAG.is_enabled(course_key):
-            return {
-                "description": Text("{b_open}{header}{b_close} {explanation}").format(
-                    b_open=HTML("<b>"),
-                    b_close=HTML("</b>"),
-                    form_values={},
-                    header=_("It looks like you missed some important deadlines based on our suggested schedule."),
-                    explanation=_(
-                        "Unfortunately you can't update this schedule and shift the past due "
-                        "assignments into the future. Contact your course team for  more information."
-                    ),
-                ),
-            }
+            return {"description": _("The deadline to complete this assignment has passed.")}
 
         course_key = xblock.scope_ids.usage_id.context_key
 
