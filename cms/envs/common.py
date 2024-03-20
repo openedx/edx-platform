45178e0ced90ea43bce2e6dc0ee7b54b40204755
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-03-20 11:31:59 -0400

    feat: add paginated HomePageCoursesV2 view with filtering & ordering (#34173)

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -561,6 +561,16 @@ FEATURES = {
     # .. toggle_creation_date: 2024-02-29
     # .. toggle_tickets: https://github.com/openedx/edx-platform/pull/33952
     'ENABLE_HIDE_FROM_TOC_UI': False,
+
+    # .. toggle_name: FEATURES['ENABLE_HOME_PAGE_COURSE_API_V2']
+    # .. toggle_implementation: DjangoSetting
+    # .. toggle_default: True
+    # .. toggle_description: Enables the new home page course v2 API, which is a new version of the home page course
+    #   API with pagination, filter and ordering capabilities.
+    # .. toggle_use_cases: open_edx
+    # .. toggle_creation_date: 2024-03-14
+    # .. toggle_tickets: https://github.com/openedx/edx-platform/pull/34173
+    'ENABLE_HOME_PAGE_COURSE_API_V2': True,
 }
 
 # .. toggle_name: ENABLE_COPPA_COMPLIANCE
