6052984cda2038517ba2177d2ab3f9746fed0c82
qasim.gulzar <qasim.gulzar@arbisoft.com>
2024-03-25 10:28:24 +0500

    refactor: [DEPR]: Neo4J Support #34342

--- cms/djangoapps/contentstore/signals/handlers.py
+++ cms/djangoapps/contentstore/signals/handlers.py
@@ -123,9 +123,6 @@ def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=
         update_search_index,
         update_special_exams_and_publish
     )
-    from cms.djangoapps.coursegraph.tasks import (
-        dump_course_to_neo4j
-    )
 
     # DEVELOPER README: probably all tasks here should use transaction.on_commit
     # to avoid stale data, but the tasks are owned by many teams and are often
@@ -145,10 +142,6 @@ def listen_for_course_publish(sender, course_key, **kwargs):  # pylint: disable=
     if key_supports_outlines(course_key):
         # Push the course outline to learning_sequences asynchronously.
         update_outline_from_modulestore_task.delay(course_key_str)
-
-    if settings.COURSEGRAPH_DUMP_COURSE_ON_PUBLISH:
-        # Push the course out to CourseGraph asynchronously.
-        dump_course_to_neo4j.delay(course_key_str)
 
     # Kick off a courseware indexing action after the data is ready
     if CoursewareSearchIndexer.indexing_is_enabled() and CourseAboutSearchIndexer.indexing_is_enabled():
