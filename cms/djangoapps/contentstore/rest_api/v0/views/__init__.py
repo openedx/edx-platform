f199264a7120ba3fe53c5fd08fbddc4bb9da1e67
KristinAoki <kaoki@2u.com>
2024-03-27 14:40:09 -0400

    feat: add youtube transcript import functions as drf

--- cms/djangoapps/contentstore/rest_api/v0/views/__init__.py
+++ cms/djangoapps/contentstore/rest_api/v0/views/__init__.py
@@ -3,3 +3,4 @@ Views for v0 contentstore API.
 """
 from .advanced_settings import AdvancedCourseSettingsView
 from .tabs import CourseTabSettingsView, CourseTabListView, CourseTabReorderView
+from .transcripts import TranscriptView, YoutubeTranscriptCheckView, YoutubeTranscriptUploadView
