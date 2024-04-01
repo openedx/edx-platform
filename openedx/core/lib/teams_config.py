d7bbcb69d38034455bebf5881b8d803c95a995e7
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-04-01 13:15:06 -0400

    refactor: address PR review about toggle docs

--- openedx/core/lib/teams_config.py
+++ openedx/core/lib/teams_config.py
@@ -31,7 +31,8 @@ TEAMS_NAMESPACE = "course_teams"
 #    who will see a particular set of course content. When this flag is enabled, course authors can create teams and
 #    assign content to each of them. Then, when a learner joins a team, they will see the content that is assigned to
 #    that team's content group. This flag is only relevant for courses that have teams enabled.
-# .. toggle_use_cases: open_edx
+# .. toggle_use_cases: temporary, opt_in
+# .. toggle_target_removal_date: Teak
 # .. toggle_creation_date: 2024-04-01
 CONTENT_GROUPS_FOR_TEAMS = CourseWaffleFlag(
     f"{TEAMS_NAMESPACE}.content_groups_for_teams", __name__
