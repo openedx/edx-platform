b834d398c727cd7d6c34258942c089a4217f623e
Rômulo Penido <romulo.penido@gmail.com>
2024-04-03 18:55:27 -0300

    feat: add celery retry to search tasks

--- openedx/core/djangoapps/content/search/tasks.py
+++ openedx/core/djangoapps/content/search/tasks.py
@@ -11,102 +11,73 @@ from celery_utils.logged_task import LoggedTask
 from edx_django_utils.monitoring import set_code_owner_attribute
 from opaque_keys.edx.keys import UsageKey
 from opaque_keys.edx.locator import LibraryLocatorV2, LibraryUsageLocatorV2
+from meilisearch.errors import MeilisearchError
 
 from . import api
 
 log = logging.getLogger(__name__)
 
 
-@shared_task(base=LoggedTask)
+@shared_task(base=LoggedTask, autoretry_for=(MeilisearchError, ConnectionError))
 @set_code_owner_attribute
-def upsert_xblock_index_doc(usage_key_str: str, recursive: bool, update_metadata: bool, update_tags: bool) -> bool:
+def upsert_xblock_index_doc(usage_key_str: str, recursive: bool, update_metadata: bool, update_tags: bool) -> None:
     """
     Celery task to update the content index document for an XBlock
     """
-    try:
-        usage_key = UsageKey.from_string(usage_key_str)
+    usage_key = UsageKey.from_string(usage_key_str)
 
-        log.info("Updating content index document for XBlock with id: %s", usage_key)
+    log.info("Updating content index document for XBlock with id: %s", usage_key)
 
-        api.upsert_xblock_index_doc(usage_key, recursive, update_metadata, update_tags)
-
-        return True
-    except Exception as e:  # pragma: no cover  # pylint: disable=broad-except
-        log.error("Error updating content index document for XBlock with id: %s. %s", usage_key_str, e)
-        return False
+    api.upsert_xblock_index_doc(usage_key, recursive, update_metadata, update_tags)
 
 
-@shared_task(base=LoggedTask)
+@shared_task(base=LoggedTask, autoretry_for=(MeilisearchError, ConnectionError))
 @set_code_owner_attribute
-def delete_xblock_index_doc(usage_key_str: str) -> bool:
+def delete_xblock_index_doc(usage_key_str: str) -> None:
     """
     Celery task to delete the content index document for an XBlock
     """
-    try:
-        usage_key = UsageKey.from_string(usage_key_str)
+    usage_key = UsageKey.from_string(usage_key_str)
 
-        log.info("Updating content index document for XBlock with id: %s", usage_key)
+    log.info("Updating content index document for XBlock with id: %s", usage_key)
 
-        api.delete_index_doc(usage_key)
-
-        return True
-    except Exception as e:  # pragma: no cover  # pylint: disable=broad-except
-        log.error("Error deleting content index document for XBlock with id: %s. %s", usage_key_str, e)
-        return False
+    api.delete_index_doc(usage_key)
 
 
-@shared_task(base=LoggedTask)
+@shared_task(base=LoggedTask, autoretry_for=(MeilisearchError, ConnectionError))
 @set_code_owner_attribute
-def upsert_library_block_index_doc(usage_key_str: str, update_metadata: bool, update_tags: bool) -> bool:
+def upsert_library_block_index_doc(usage_key_str: str, update_metadata: bool, update_tags: bool) -> None:
     """
     Celery task to update the content index document for a library block
     """
-    try:
-        usage_key = LibraryUsageLocatorV2.from_string(usage_key_str)
+    usage_key = LibraryUsageLocatorV2.from_string(usage_key_str)
 
-        log.info("Updating content index document for library block with id: %s", usage_key)
+    log.info("Updating content index document for library block with id: %s", usage_key)
 
-        api.upsert_library_block_index_doc(usage_key, update_metadata, update_tags)
-
-        return True
-    except Exception as e:  # pragma: no cover  # pylint: disable=broad-except
-        log.error("Error updating content index document for libray block with id: %s. %s", usage_key_str, e)
-        return False
+    api.upsert_library_block_index_doc(usage_key, update_metadata, update_tags)
 
 
-@shared_task(base=LoggedTask)
+@shared_task(base=LoggedTask, autoretry_for=(MeilisearchError, ConnectionError))
 @set_code_owner_attribute
-def delete_library_block_index_doc(usage_key_str: str) -> bool:
+def delete_library_block_index_doc(usage_key_str: str) -> None:
     """
     Celery task to delete the content index document for a library block
     """
-    try:
-        usage_key = LibraryUsageLocatorV2.from_string(usage_key_str)
+    usage_key = LibraryUsageLocatorV2.from_string(usage_key_str)
 
-        log.info("Deleting content index document for library block with id: %s", usage_key)
+    log.info("Deleting content index document for library block with id: %s", usage_key)
 
-        api.delete_index_doc(usage_key)
-
-        return True
-    except Exception as e:  # pragma: no cover  # pylint: disable=broad-except
-        log.error("Error deleting content index document for library block with id: %s. %s", usage_key_str, e)
-        return False
+    api.delete_index_doc(usage_key)
 
 
-@shared_task(base=LoggedTask)
+@shared_task(base=LoggedTask, autoretry_for=(MeilisearchError, ConnectionError))
 @set_code_owner_attribute
-def update_content_library_index_docs(library_key_str: str) -> bool:
+def update_content_library_index_docs(library_key_str: str) -> None:
     """
     Celery task to update the content index documents for all library blocks in a library
     """
-    try:
-        library_key = LibraryLocatorV2.from_string(library_key_str)
+    library_key = LibraryLocatorV2.from_string(library_key_str)
 
-        log.info("Updating content index documents for library with id: %s", library_key)
+    log.info("Updating content index documents for library with id: %s", library_key)
 
-        api.upsert_content_library_index_docs(library_key)
-
-        return True
-    except Exception as e:  # pragma: no cover  # pylint: disable=broad-except
-        log.error("Error updating content index documents for library with id: %s. %s", library_key, e)
-        return False
+    api.upsert_content_library_index_docs(library_key)
