6112f85c0b61d23016076f2a66353a64495d4ea8
Ben Lu <hilltop16@gamil.com>
2024-04-03 13:22:51 -0500

    fixup! feat: add JWT KID

--- openedx/core/djangoapps/oauth_dispatch/tests/test_jwt.py
+++ openedx/core/djangoapps/oauth_dispatch/tests/test_jwt.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 from unittest.mock import patch
 
 import ddt
-import jwt
 from django.test import TestCase
 from django.test.utils import override_settings
 from django.utils.timezone import now
@@ -82,27 +81,27 @@ class TestCreateJWTs(AccessTokenMixin, TestCase):
 
     def test_kid_not_in_jwt_header_with_symmetric_key_and_kid_disabled(self):
         jwt_token = self._create_jwt_for_token(DOTAdapter(), use_asymmetric_key=False)
-        header = jwt.get_unverified_header(jwt_token)
+        header = jwt_api.jwt.get_unverified_header(jwt_token)
         assert 'kid' not in header
         self._assert_jwt_is_valid(jwt_token, should_be_asymmetric_key=False)
 
     def test_kid_not_in_jwt_header_with_asymmetric_key_and_kid_disabled(self):
         jwt_token = self._create_jwt_for_token(DOTAdapter(), use_asymmetric_key=True)
-        header = jwt.get_unverified_header(jwt_token)
+        header = jwt_api.jwt.get_unverified_header(jwt_token)
         assert 'kid' not in header
         self._assert_jwt_is_valid(jwt_token, should_be_asymmetric_key=True)
 
     @override_settings(JWT_AUTH_ADD_KID_HEADER=True)
     def test_kid_not_in_jwt_header_with_symmetric_key_and_kid_enabled(self):
         jwt_token = self._create_jwt_for_token(DOTAdapter(), use_asymmetric_key=False)
-        header = jwt.get_unverified_header(jwt_token)
+        header = jwt_api.jwt.get_unverified_header(jwt_token)
         assert 'kid' not in header
         self._assert_jwt_is_valid(jwt_token, should_be_asymmetric_key=False)
 
     @override_settings(JWT_AUTH_ADD_KID_HEADER=True)
     def test_kid_in_jwt_header_with_asymmetric_key_and_kid_enabled(self):
         jwt_token = self._create_jwt_for_token(DOTAdapter(), use_asymmetric_key=True)
-        header = jwt.get_unverified_header(jwt_token)
+        header = jwt_api.jwt.get_unverified_header(jwt_token)
         assert 'kid' in header
         assert header['kid'] == 'BTZ9HA6K'
         self._assert_jwt_is_valid(jwt_token, should_be_asymmetric_key=True)
