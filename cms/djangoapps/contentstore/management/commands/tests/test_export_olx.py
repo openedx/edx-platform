bda8da23add07c08ceb9a9dbe4c1f131e9ec2be7
XnpioChV <xnpiochv@gmail.com>
2024-04-04 14:34:28 -0500

    fix: Fix tests

--- cms/djangoapps/contentstore/management/commands/tests/test_export_olx.py
+++ cms/djangoapps/contentstore/management/commands/tests/test_export_olx.py
@@ -17,7 +17,7 @@ from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
 
-from openedx.core.djangoapps.content_tagging.helpers.test_objecttag_export_helpers import TaggedCourseMixin
+from openedx.core.djangoapps.content_tagging.tests.test_objecttag_export_helpers import TaggedCourseMixin
 
 
 class TestArgParsingCourseExportOlx(unittest.TestCase):
