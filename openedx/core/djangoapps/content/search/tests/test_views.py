902e75cfd88feaabb3c7970c2408fc3897ecf5a9
Jillian Vogel <jill@opencraft.com>
2024-04-05 13:02:26 +1030

    refactor: refactors the view tests to make them clearer
    Uses helper methods and decorators to wrap the settings and patches used
    by multiple view tests.

--- openedx/core/djangoapps/content/search/tests/test_views.py
+++ openedx/core/djangoapps/content/search/tests/test_views.py
@@ -1,6 +1,7 @@
 """
 Tests for the Studio content search REST API.
 """
+import functools
 from django.test import override_settings
 from rest_framework.test import APITestCase, APIClient
 from unittest import mock
@@ -10,6 +11,29 @@ from openedx.core.djangolib.testing.utils import skip_unless_cms
 
 STUDIO_SEARCH_ENDPOINT_URL = "/api/content_search/v2/studio/"
 MOCK_API_KEY_UID = "3203d764-370f-4e99-a917-d47ab7f29739"
+
+
+def mock_meilisearch(enabled=True):
+    """
+    Decorator that mocks the required parts of content.search.views to simulate a running Meilisearch instance.
+    """
+    def decorator(func):
+        """
+        Overrides settings and patches to enable view tests.
+        """
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with override_settings(
+                MEILISEARCH_ENABLED=enabled,
+                MEILISEARCH_PUBLIC_URL="http://meilisearch.url",
+            ):
+                with mock.patch(
+                    'openedx.core.djangoapps.content.search.views._get_meili_api_key_uid',
+                    return_value=MOCK_API_KEY_UID,
+                ):
+                    return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 
 @skip_unless_cms
@@ -32,7 +56,7 @@ class StudioSearchViewTest(APITestCase):
         super().setUp()
         self.client = APIClient()
 
-    @override_settings(MEILISEARCH_ENABLED=False)
+    @mock_meilisearch(enabled=False)
     def test_studio_search_unathenticated_disabled(self):
         """
         Whether or not Meilisearch is enabled, the API endpoint requires authentication.
@@ -40,7 +64,7 @@ class StudioSearchViewTest(APITestCase):
         result = self.client.get(STUDIO_SEARCH_ENDPOINT_URL)
         assert result.status_code == 401
 
-    @override_settings(MEILISEARCH_ENABLED=True)
+    @mock_meilisearch(enabled=True)
     def test_studio_search_unathenticated_enabled(self):
         """
         Whether or not Meilisearch is enabled, the API endpoint requires authentication.
@@ -48,7 +72,7 @@ class StudioSearchViewTest(APITestCase):
         result = self.client.get(STUDIO_SEARCH_ENDPOINT_URL)
         assert result.status_code == 401
 
-    @override_settings(MEILISEARCH_ENABLED=False)
+    @mock_meilisearch(enabled=False)
     def test_studio_search_disabled(self):
         """
         When Meilisearch is disabled, the Studio search endpoint gives a 404
@@ -57,34 +81,37 @@ class StudioSearchViewTest(APITestCase):
         result = self.client.get(STUDIO_SEARCH_ENDPOINT_URL)
         assert result.status_code == 404
 
-    @override_settings(MEILISEARCH_ENABLED=True, MEILISEARCH_PUBLIC_URL="http://meilisearch.url")
-    @mock.patch('openedx.core.djangoapps.content.search.views._get_meili_api_key_uid')
-    def test_studio_search_enabled(self, mock_get_api_key_uid):
+    @mock_meilisearch(enabled=True)
+    def test_studio_search_enabled(self):
         """
         We've implement fine-grained permissions on the meilisearch content,
         so any logged-in user can get a restricted API key for Meilisearch using the REST API.
         """
         self.client.login(username='student', password='student_pass')
-        mock_get_api_key_uid.return_value = MOCK_API_KEY_UID
         result = self.client.get(STUDIO_SEARCH_ENDPOINT_URL)
         assert result.status_code == 200
         assert result.data["index_name"] == "studio_content"
         assert result.data["url"] == "http://meilisearch.url"
         assert result.data["api_key"] and isinstance(result.data["api_key"], str)
 
-    @override_settings(MEILISEARCH_ENABLED=True)
-    @mock.patch('openedx.core.djangoapps.content.search.views.meilisearch.Client')
-    @mock.patch('openedx.core.djangoapps.content.search.views._get_meili_api_key_uid')
-    def test_studio_search_student_no_access(self, mock_get_api_key_uid, mock_search_client):
+    def _mock_generate_tenant_token(self, mock_search_client):
         """
-        Users without access to any courses or libraries will have all documents filtered out.
+        Return a mocked meilisearch.Client.generate_tenant_token method.
         """
-        self.client.login(username='student', password='student_pass')
-        mock_get_api_key_uid.return_value = MOCK_API_KEY_UID
         mock_generate_tenant_token = mock.Mock(return_value='restricted_api_key')
         mock_search_client.return_value = mock.Mock(
             generate_tenant_token=mock_generate_tenant_token,
         )
+        return mock_generate_tenant_token
+
+    @mock_meilisearch(enabled=True)
+    @mock.patch('openedx.core.djangoapps.content.search.views.meilisearch.Client')
+    def test_studio_search_student_no_access(self, mock_search_client):
+        """
+        Users without access to any courses or libraries will have all documents filtered out.
+        """
+        self.client.login(username='student', password='student_pass')
+        mock_generate_tenant_token = self._mock_generate_tenant_token(mock_search_client)
         result = self.client.get(STUDIO_SEARCH_ENDPOINT_URL)
         assert result.status_code == 200
         mock_generate_tenant_token.assert_called_once_with(
@@ -97,19 +124,14 @@ class StudioSearchViewTest(APITestCase):
             expires_at=mock.ANY,
         )
 
-    @override_settings(MEILISEARCH_ENABLED=True)
+    @mock_meilisearch(enabled=True)
     @mock.patch('openedx.core.djangoapps.content.search.views.meilisearch.Client')
-    @mock.patch('openedx.core.djangoapps.content.search.views._get_meili_api_key_uid')
-    def test_studio_search_staff(self, mock_get_api_key_uid, mock_search_client):
+    def test_studio_search_staff(self, mock_search_client):
         """
         Users with global staff access can search any document.
         """
         self.client.login(username='staff', password='staff_pass')
-        mock_get_api_key_uid.return_value = MOCK_API_KEY_UID
-        mock_generate_tenant_token = mock.Mock(return_value='restricted_api_key')
-        mock_search_client.return_value = mock.Mock(
-            generate_tenant_token=mock_generate_tenant_token,
-        )
+        mock_generate_tenant_token = self._mock_generate_tenant_token(mock_search_client)
         result = self.client.get(STUDIO_SEARCH_ENDPOINT_URL)
         assert result.status_code == 200
         mock_generate_tenant_token.assert_called_once_with(
@@ -120,22 +142,17 @@ class StudioSearchViewTest(APITestCase):
             expires_at=mock.ANY,
         )
 
-    @override_settings(MEILISEARCH_ENABLED=True)
+    @mock_meilisearch(enabled=True)
     @mock.patch('openedx.core.djangoapps.content.search.views.get_access_ids_for_request')
     @mock.patch('openedx.core.djangoapps.content.search.views.meilisearch.Client')
-    @mock.patch('openedx.core.djangoapps.content.search.views._get_meili_api_key_uid')
-    def test_studio_search_limit(self, mock_get_api_key_uid, mock_search_client, mock_get_access_ids):
+    def test_studio_search_limit_access_ids(self, mock_search_client, mock_get_access_ids):
         """
         Users with access to many courses or libraries will only be able to search content
         from the most recent 1_000 courses/libraries.
         """
         self.client.login(username='student', password='student_pass')
-        mock_get_api_key_uid.return_value = MOCK_API_KEY_UID
+        mock_generate_tenant_token = self._mock_generate_tenant_token(mock_search_client)
         mock_get_access_ids.return_value = list(range(2000))
-        mock_generate_tenant_token = mock.Mock(return_value='restricted_api_key')
-        mock_search_client.return_value = mock.Mock(
-            generate_tenant_token=mock_generate_tenant_token,
-        )
         expected_access_ids = list(range(1000))
 
         result = self.client.get(STUDIO_SEARCH_ENDPOINT_URL)
