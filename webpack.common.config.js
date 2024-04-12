58185d778400072e10e613f4be80e9814cb305b2
Brian Smith <bsmith@axim.org>
2024-04-12 07:00:46 -0400

    fix: use `string-replace-loader` instead of `string-replace-webpack-plugin`

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -5,7 +5,6 @@
 var path = require('path');
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
-var StringReplace = require('string-replace-webpack-plugin');
 var Merge = require('webpack-merge');
 
 var files = require('./webpack-config/file-lists.js');
@@ -168,6 +167,35 @@ module.exports = Merge.smart({
                 /\/sinon\.js|codemirror-compressed\.js|hls\.js|tinymce.js/
             ],
             rules: [
+                {
+                    test: files.namespacedRequire.concat(files.textBangUnderscore, filesWithRequireJSBlocks),
+                    loader: 'string-replace-loader',
+                    options: {
+                        multiple: [
+                            { search: defineHeader, replace: '' },
+                            { search: defineFooter, replace: '' },
+                            { 
+                                search: /(\/\* RequireJS) \*\//g,
+                                replace(match, p1, offset, string) {
+                                    return p1;
+                                }
+                            },
+                            { 
+                                search: /\/\* Webpack/g,
+                                replace(match, p1, offset, string) {
+                                    return match + ' */';
+                                }
+                            },
+                            { 
+                                search: /text!(.*?\.underscore)/g,
+                                replace(match, p1, offset, string) {
+                                    return p1;
+                                }
+                            },
+                            { search: /RequireJS.require/g, replace: 'require' }
+                        ]
+                    }
+                },
                 {
                     test: /\.(js|jsx)$/,
                     exclude: [
