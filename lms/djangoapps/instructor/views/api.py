61b8961cdfecbb2e213c4b43edd7db1c5756878c
Navin Karkera <navin@opencraft.com>
2024-04-26 22:46:32 +0530

    fix: auto enroll users when added as staff (#32561)
    The course team management section under Instructor > Membership tab
    allows users to be added a role even if are not enrolled in the course.
    This is behaviour does not match the help text displayed in the section.
    
    This PR updates modify_access api to enrolls user if they are not enrolled
    after adding them to a role as well as changes the help text to reflect
    actual changes.

--- lms/djangoapps/instructor/views/api.py
+++ lms/djangoapps/instructor/views/api.py
@@ -1005,11 +1005,12 @@ def modify_access(request, course_id):
     course = get_course_with_access(
         request.user, 'instructor', course_id, depth=None
     )
+    unique_student_identifier = request.POST.get('unique_student_identifier')
     try:
-        user = get_student_from_identifier(request.POST.get('unique_student_identifier'))
+        user = get_student_from_identifier(unique_student_identifier)
     except User.DoesNotExist:
         response_payload = {
-            'unique_student_identifier': request.POST.get('unique_student_identifier'),
+            'unique_student_identifier': unique_student_identifier,
             'userDoesNotExist': True,
         }
         return JsonResponse(response_payload)
@@ -1044,6 +1045,8 @@ def modify_access(request, course_id):
 
     if action == 'allow':
         allow_access(course, user, rolename)
+        if not is_user_enrolled_in_course(user, course_id):
+            CourseEnrollment.enroll(user, course_id)
     elif action == 'revoke':
         revoke_access(course, user, rolename)
     else:
