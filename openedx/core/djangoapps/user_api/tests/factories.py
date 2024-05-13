fd805e0cc8b70b757e2878b41e179c22144f9136
Justin Hynes <Jhynes@edx.org>
2024-05-13 18:10:59 +0000

    feat: purge name from certificate records during user retirement
    [APER-3241]
    
    This PR updates the retirement pipeline to purge learners' names from certificate records when their account is being retired.
    
    It also introduces a new management command that can be used by Open edX operators to purge the leftover name data (PII data) from the `certificates_generatedcertificate` table. This is designed as a one-time use data fixup, as the retirement functionality should clean this up now.

--- openedx/core/djangoapps/user_api/tests/factories.py
+++ openedx/core/djangoapps/user_api/tests/factories.py
@@ -1,13 +1,20 @@
 """Provides factories for User API models."""
 
 
-from factory import SubFactory
+from factory import Sequence, SubFactory
 from factory.django import DjangoModelFactory
 from opaque_keys.edx.locator import CourseLocator
 
 from common.djangoapps.student.tests.factories import UserFactory
 
-from ..models import UserCourseTag, UserOrgTag, UserPreference
+from openedx.core.djangoapps.user_api.models import (
+    RetirementState,
+    UserCourseTag,
+    UserOrgTag,
+    UserPreference,
+    UserRetirementRequest,
+    UserRetirementStatus,
+)
 
 
 # Factories are self documenting
@@ -40,3 +47,44 @@ class UserOrgTagFactory(DjangoModelFactory):
     org = 'org'
     key = None
     value = None
+
+
+class RetirementStateFactory(DjangoModelFactory):
+    """
+    Factory class for generating RetirementState instances.
+    """
+    class Meta:
+        model = RetirementState
+
+    state_name = Sequence("STEP_{}".format)
+    state_execution_order = Sequence(lambda n: n * 10)
+    is_dead_end_state = False
+    required = False
+
+
+class UserRetirementStatusFactory(DjangoModelFactory):
+    """
+    Factory class for generating UserRetirementStatus instances.
+    """
+    class Meta:
+        model = UserRetirementStatus
+
+    user = SubFactory(UserFactory)
+    original_username = Sequence('learner_{}'.format)
+    original_email = Sequence("learner{}@email.org".format)
+    original_name = Sequence("Learner{} Shmearner".format)
+    retired_username = Sequence("retired__learner_{}".format)
+    retired_email = Sequence("returned__learner{}@retired.invalid".format)
+    current_state = None
+    last_state = None
+    responses = ""
+
+
+class UserRetirementRequestFactory(DjangoModelFactory):
+    """
+    Factory class for generating UserRetirementRequest instances.
+    """
+    class Meta:
+        model = UserRetirementRequest
+
+    user = SubFactory(UserFactory)
