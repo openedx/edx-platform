b0690163dcaee60158a5f12b9a4d01226ca6303e
ayeshoali <ayeshoali@gmail.com>
2024-05-29 19:09:12 +0500

    fix: fixed creating post based on discussion restrictions settings

--- xmodule/course_block.py
+++ xmodule/course_block.py
@@ -108,6 +108,7 @@ class EmailString(String):
     """
     Parse String with email validation
     """
+
     def from_json(self, value):
         if value:
             validate_email(value)
@@ -226,6 +227,7 @@ class ProctoringProvider(String):
     ProctoringProvider field, which includes validation of the provider
     and default that pulls from edx platform settings.
     """
+
     def from_json(self, value):
         """
         Return ProctoringProvider as full featured Python type. Perform validation on the provider
@@ -1457,15 +1459,18 @@ class CourseBlock(
     @property
     def forum_posts_allowed(self):
         """
-        Return whether forum posts are allowed by the discussion_blackouts
-        setting
+        Return whether forum posts are allowed by the discussion_blackouts setting
+        Checks if posting restrictions are enabled or if there's a currently ongoing blackout period.
         """
+
         blackouts = self.get_discussion_blackout_datetimes()
+        posting_restrictions = self.discussions_settings.get('posting_restrictions', 'disabled')
         now = datetime.now(utc)
-        for blackout in blackouts:
-            if blackout["start"] <= now <= blackout["end"]:
-                return False
-        return True
+
+        if posting_restrictions == 'enabled':
+            return False
+
+        return all(not (blackout["start"] <= now <= blackout["end"]) for blackout in blackouts)
 
     @property
     def number(self):
