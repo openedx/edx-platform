8c5e9e2966d7ae9b0c517262ddf23afe1951cf21
Jade Olivier <jolivier@2u.com>
2024-07-02 16:11:33 +0200

    fix: final quality

--- openedx/core/djangoapps/user_api/accounts/views.py
+++ openedx/core/djangoapps/user_api/accounts/views.py
@@ -636,7 +636,7 @@ class DeactivateLogoutView(APIView):
                 except Exception as exc:  # pylint: disable=broad-except
                     # Catch any possible exceptions from the Commerce Coordinator service to ensure we fail gracefully
                     log.exception(
-                        "Unexpected exception while attempting to retire user in Coordinator, "
+                        "Unexpected exception while attempting to retire user in Commerce Coordinator, "
                         "with user ID [%s]: ",
                         lms_user_id,
                         str(exc)
@@ -644,7 +644,7 @@ class DeactivateLogoutView(APIView):
 
                 log.info("User [%s] successfully sent to Commerce Coordinator for retirement.",
                          lms_user_id
-                        )
+                )
 
                 # Log the user out.
                 logout(request)
