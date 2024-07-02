0ba526387c62b8bda5fdd290981caa1df682d679
salman2013 <salman.nawaz@arbisoft.com>
2024-07-02 13:54:18 +0500

    chore: test code

--- pavelib/quality_test.py
+++ pavelib/quality_test.py
@@ -20,7 +20,7 @@ def _get_pep8_violations(clean=True):
 
     # if not report.exists():
         # sh(f'pycodestyle . | tee {report} -a')
-    subprocess.run(['pycodestyle', '.']
+    subprocess.run(['pycodestyle', '.'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # violations_list = _pep8_violations(report)
 
