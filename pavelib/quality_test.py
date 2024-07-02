872893c9291e2aa2f15fe16dead2b0a255fe4db9
salman2013 <salman.nawaz@arbisoft.com>
2024-07-02 13:46:21 +0500

    chore: test code

--- pavelib/quality_test.py
+++ pavelib/quality_test.py
@@ -9,21 +9,22 @@ def _get_pep8_violations(clean=True):
     where violations_string is a string of all PEP 8 violations found, separated
     by new lines.
     """
-    report_dir = (Env.REPORT_DIR / 'pep8')
-    if clean:
-        report_dir.rmtree(ignore_errors=True)
-    report_dir.makedirs_p()
-    report = report_dir / 'pep8.report'
+    # report_dir = REPORT_DIR / 'pep8'
+    # if clean:
+    #     report_dir.rmtree(ignore_errors=True)
+    # report_dir.makedirs_p()
+    # report = report_dir / 'pep8.report'
 
     # Make sure the metrics subdirectory exists
-    Env.METRICS_DIR.makedirs_p()
+    # METRICS_DIR.makedirs_p()
 
-    if not report.exists():
-        sh(f'pycodestyle . | tee {report} -a')
+    # if not report.exists():
+        # sh(f'pycodestyle . | tee {report} -a')
+        subprocess.run(['pycodestyle', '.']
 
-    violations_list = _pep8_violations(report)
+    # violations_list = _pep8_violations(report)
 
-    return len(violations_list), violations_list
+    #return len(violations_list), violations_list
 
 
 def _pep8_violations(report_file):
@@ -39,26 +40,27 @@ def run_pep8():  # pylint: disable=unused-argument
     Run pycodestyle on system code.
     Fail the task if any violations are found.
     """
-    (count, violations_list) = _get_pep8_violations()
-    violations_list = ''.join(violations_list)
+    _get_pep8_violations()
+    # (count, violations_list) = _get_pep8_violations()
+    # violations_list = ''.join(violations_list)
 
-    # Print number of violations to log
-    violations_count_str = f"Number of PEP 8 violations: {count}"
-    print(violations_count_str)
-    print(violations_list)
+    # # Print number of violations to log
+    # violations_count_str = f"Number of PEP 8 violations: {count}"
+    # print(violations_count_str)
+    # print(violations_list)
 
-    # Also write the number of violations to a file
-    with open(Env.METRICS_DIR / "pep8", "w") as f:
-        f.write(violations_count_str + '\n\n')
-        f.write(violations_list)
+    # # Also write the number of violations to a file
+    # with open(METRICS_DIR / "pep8", "w") as f:
+    #     f.write(violations_count_str + '\n\n')
+    #     f.write(violations_list)
 
-    # Fail if any violations are found
-    if count:
-        failure_string = "FAILURE: Too many PEP 8 violations. " + violations_count_str
-        failure_string += f"\n\nViolations:\n{violations_list}"
-        fail_quality('pep8', failure_string)
-    else:
-        write_junit_xml('pep8')
+    # # Fail if any violations are found
+    # if count:
+    #     failure_string = "FAILURE: Too many PEP 8 violations. " + violations_count_str
+    #     failure_string += f"\n\nViolations:\n{violations_list}"
+    #     fail_quality('pep8', failure_string)
+    # else:
+    #     write_junit_xml('pep8')
 
 
 if __name__ == "__main__":
