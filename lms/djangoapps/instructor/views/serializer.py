6cc0e2892930d36d1a3959549f2579a44d5eb8ac
awais qureshi <awais.qureshi@arbisoft.com>
2024-07-10 16:48:44 +0500

    feat: upgrading simple api to drf compatible.

---
+++ lms/djangoapps/instructor/views/serializer.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User  # lint-amnesty, pylint: disable=imported-auth-user
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+from rest_framework import serializers, status
+
+from lms.djangoapps.instructor.access import ROLES
+
+
+class RoleNameSerializer(serializers.Serializer):  # pylint: disable=abstract-method
+    """
+    Serializer that describes the response of the problem response report generation API.
+    """
+
+    rolename = serializers.CharField(help_text=_("Role name"))
+
+    def validate_rolename(self, value):
+        """
+        Check that the rolename is valid.
+        """
+        if value not in ROLES:
+            raise ValidationError(_("Invalid role name."))
+        return value
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
