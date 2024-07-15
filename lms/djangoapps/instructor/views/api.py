480dc75e127281bf21fe324c8ec3c65b945d1cb0
awais qureshi <awais.qureshi@arbisoft.com>
2024-07-16 01:27:20 +0500

    feat: upgrading simple api to drf compatible.

--- lms/djangoapps/instructor/views/api.py
+++ lms/djangoapps/instructor/views/api.py
@@ -232,6 +232,29 @@ def require_course_permission(permission):
     return decorator
 
 
+def verify_course_permission(permission):
+    """
+        Decorator with argument that requires a specific permission of the requesting
+        user. If the requirement is not satisfied, returns an
+        HttpResponseForbidden (403).
+        Assumes that request is in self.
+        Assumes that course_id is in kwargs['course_id'].
+        """
+    def decorator(func):
+        def wrapped(self, *args, **kwargs):
+            request = self.request
+            course = get_course_by_id(CourseKey.from_string(kwargs['course_id']))
+
+            if request.user.has_perm(permission, course):
+                return func(self, *args, **kwargs)
+            else:
+                return HttpResponseForbidden()
+
+        return wrapped
+
+    return decorator
+
+
 def require_sales_admin(func):
     """
     Decorator for checking sales administrator access before executing an HTTP endpoint. This decorator
@@ -2366,46 +2389,57 @@ def _list_instructor_tasks(request, course_id):
     return JsonResponse(response_payload)
 
 
-@require_POST
-@ensure_csrf_cookie
-@cache_control(no_cache=True, no_store=True, must_revalidate=True)
-@require_course_permission(permissions.SHOW_TASKS)
-def list_entrance_exam_instructor_tasks(request, course_id):
+class ListEntranceExamInstructorTasks(APIView):
     """
     List entrance exam related instructor tasks.
-
-    Takes either of the following query parameters
-        - unique_student_identifier is an email or username
-        - all_students is a boolean
     """
-    course_id = CourseKey.from_string(course_id)
-    course = get_course_by_id(course_id)
-    student = request.POST.get('unique_student_identifier', None)
-    if student is not None:
-        student = get_student_from_identifier(student)
+    authentication_classes = (
+        JwtAuthentication,
+        BearerAuthenticationAllowInactiveUser,
+        SessionAuthenticationAllowInactiveUser,
+    )
+    permission_classes = (IsAuthenticated,)
+    http_method_names = ['post']
 
-    try:
-        entrance_exam_key = UsageKey.from_string(course.entrance_exam_id).map_into_course(course_id)
-    except InvalidKeyError:
-        return HttpResponseBadRequest(_("Course has no valid entrance exam section."))
-    if student:
-        # Specifying for a single student's entrance exam history
-        tasks = task_api.get_entrance_exam_instructor_task_history(
-            course_id,
-            entrance_exam_key,
-            student
-        )
-    else:
-        # Specifying for all student's entrance exam history
-        tasks = task_api.get_entrance_exam_instructor_task_history(
-            course_id,
-            entrance_exam_key
-        )
+    @method_decorator(ensure_csrf_cookie)
+    @verify_course_permission(permissions.SHOW_TASKS)
+    @method_decorator(cache_control(no_cache=True, no_store=True, must_revalidate=True))
+    def post(self, request, course_id):
+        """
+        List entrance exam related instructor tasks.
 
-    response_payload = {
-        'tasks': list(map(extract_task_features, tasks)),
-    }
-    return JsonResponse(response_payload)
+        Takes either of the following query parameters
+            - unique_student_identifier is an email or username
+            - all_students is a boolean
+        """
+        course_id = CourseKey.from_string(course_id)
+        course = get_course_by_id(course_id)
+        student = request.POST.get('unique_student_identifier', None)
+        if student is not None:
+            student = get_student_from_identifier(student)
+
+        try:
+            entrance_exam_key = UsageKey.from_string(course.entrance_exam_id).map_into_course(course_id)
+        except InvalidKeyError:
+            return HttpResponseBadRequest(_("Course has no valid entrance exam section."))
+        if student:
+            # Specifying for a single student's entrance exam history
+            tasks = task_api.get_entrance_exam_instructor_task_history(
+                course_id,
+                entrance_exam_key,
+                student
+            )
+        else:
+            # Specifying for all student's entrance exam history
+            tasks = task_api.get_entrance_exam_instructor_task_history(
+                course_id,
+                entrance_exam_key
+            )
+
+        response_payload = {
+            'tasks': list(map(extract_task_features, tasks)),
+        }
+        return JsonResponse(response_payload)
 
 
 class ReportDownloadSerializer(serializers.Serializer):  # pylint: disable=abstract-method
