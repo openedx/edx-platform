fab0267757ca2851625707aee4c8e8e1e82a310a
Feanil Patel <feanil@axim.org>
2024-07-25 15:52:11 -0400

    feat!: Drop the block_structure.storage_backing_for_cache waffle switch.
    This work is part of DEPR https://github.com/openedx/public-engineering/issues/32
    
    Now that we've removed all uses for this switch remove the decleration
    as well.
    
    BREAKING CHANGE: The `block_structure.storage_backing_for_cache` will no
    longer exist and its value will be ignored. If you have this switch set
    in your instance you can remove it. The backing cache is now always ON.

--- openedx/core/djangoapps/content/block_structure/config/__init__.py
+++ openedx/core/djangoapps/content/block_structure/config/__init__.py
@@ -9,28 +9,6 @@ from openedx.core.lib.cache_utils import request_cached
 
 from .models import BlockStructureConfiguration
 
-# Switches
-# .. toggle_name: block_structure.storage_backing_for_cache
-# .. toggle_implementation: WaffleSwitch
-# .. toggle_default: False
-# .. toggle_description: When enabled, block structures are stored in a more permanent storage,
-#   like a database, which provides an additional backup for cache misses, instead having them
-#   regenerated. The regenration of block structures is a time consuming process. Therefore,
-#   enabling this switch is recommended for Production.
-# .. toggle_warning: Depends on `BLOCK_STRUCTURES_SETTINGS['STORAGE_CLASS']` and
-#   `BLOCK_STRUCTURES_SETTINGS['STORAGE_KWARGS']`.
-#   This switch will likely be deprecated and removed.
-#   The annotation will be updated with the DEPR ticket once that process has started.
-# .. toggle_use_cases: temporary
-# .. toggle_creation_date: 2017-02-23
-# .. toggle_target_removal_date: 2017-05-23
-# .. toggle_tickets: https://github.com/openedx/edx-platform/pull/14512,
-#   https://github.com/openedx/edx-platform/pull/14770,
-#   https://openedx.atlassian.net/browse/DEPR-145
-STORAGE_BACKING_FOR_CACHE = WaffleSwitch(
-    "block_structure.storage_backing_for_cache", __name__
-)
-
 
 @request_cached()
 def num_versions_to_keep():
