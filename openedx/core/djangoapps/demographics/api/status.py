cda2e555ee50b16074053a1d4a07f56cc37e0ddb
Deborah Kaplan <deborahgu@users.noreply.github.com>
2024-07-25 16:35:55 +0000

    feat: removing djangoapp `demographics`, step 1
    This step removes the models, the references to the models, and adds a
    migration to drop both tables  (`HistoricalUserDemographics`  didn't
    have a corresponding model but was still a valid table).
    
    Once this has deployed, this will be removed from `INSTALLED_APPS` and
    completely removed.
    
    No other apps  in the repository currently reference this djangoapp  in
    code or tables.
    
    FIXES: APER-3560

--- openedx/core/djangoapps/demographics/api/status.py
+++ openedx/core/djangoapps/demographics/api/status.py
@@ -2,9 +2,8 @@
 Python API for Demographics Status
 """
 
-from openedx.features.enterprise_support.utils import is_enterprise_learner
 from openedx.core.djangoapps.programs.api import is_user_enrolled_in_program_type
-from openedx.core.djangoapps.demographics.models import UserDemographics
+from openedx.features.enterprise_support.utils import is_enterprise_learner
 
 
 def show_user_demographics(user, enrollments=None, entitlements=None):
@@ -13,10 +12,7 @@ def show_user_demographics(user, enrollments=None, entitlements=None):
     to MicroBachlors Programs' learners who aren't part of an enterprise.
     """
     is_user_in_microbachelors_program = is_user_enrolled_in_program_type(
-        user,
-        "microbachelors",
-        enrollments=enrollments,
-        entitlements=entitlements
+        user, "microbachelors", enrollments=enrollments, entitlements=entitlements
     )
     return is_user_in_microbachelors_program and not is_enterprise_learner(user)
 
@@ -26,7 +22,4 @@ def show_call_to_action_for_user(user):
     Utility method to determine if a user should be shown the Demographics call to
     action.
     """
-    try:
-        return UserDemographics.objects.get(user=user).show_call_to_action
-    except UserDemographics.DoesNotExist:
-        return True
+    return False
