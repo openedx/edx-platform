c0c9a6071d6f2771304b17ab93bfef7ddb13c9c0
Jade Olivier <jolivier@2u.com>
2024-08-06 11:18:52 +0200

    fix: generalize internal services

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -4996,6 +4996,8 @@ RETIREMENT_STATES = [
     'COMPLETE',
 ]
 
+EXTRA_SERVICES_TO_RETIRE_FROM = {}
+
 USERNAME_REPLACEMENT_WORKER = "REPLACE WITH VALID USERNAME"
 
 ############## Settings for Microfrontends  #########################
