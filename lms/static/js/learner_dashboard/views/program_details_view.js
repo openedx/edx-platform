ce0a728be1f6d25712a7cb21672a91aac9b7b592
julianajlk <julianajlk@gmail.com>
2024-08-12 16:50:43 -0400

    fix: Remove B2C Subscriptions from Program Details views

--- lms/static/js/learner_dashboard/views/program_details_view.js
+++ lms/static/js/learner_dashboard/views/program_details_view.js
@@ -12,9 +12,6 @@ import HeaderView from './program_header_view';
 import SidebarView from './program_details_sidebar_view';
 import AlertListView from './program_alert_list_view';
 
-// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
-import SubscriptionModel from '../models/program_subscription_model';
-
 import launchIcon from '../../../images/launch-icon.svg';
 import restartIcon from '../../../images/restart-icon.svg';
 import pageTpl from '../../../templates/learner_dashboard/program_details_view.underscore';
@@ -27,7 +24,6 @@ class ProgramDetailsView extends Backbone.View {
             el: '.js-program-details-wrapper',
             events: {
                 'click .complete-program': 'trackPurchase',
-                'click .js-subscription-cta': 'trackSubscriptionCTA',
             },
         };
         // eslint-disable-next-line prefer-object-spread
@@ -46,9 +42,6 @@ class ProgramDetailsView extends Backbone.View {
         this.certificateCollection = new Backbone.Collection(
             this.options.certificateData,
         );
-        this.subscriptionModel = new SubscriptionModel({
-            context: this.options,
-        });
         this.completedCourseCollection = new CourseCardCollection(
             this.courseData.get('completed') || [],
             this.options.userPreferences,
@@ -61,11 +54,6 @@ class ProgramDetailsView extends Backbone.View {
             this.courseData.get('not_started') || [],
             this.options.userPreferences,
         );
-        this.subscriptionEventParams = {
-            label: this.options.programData.title,
-            program_uuid: this.options.programData.uuid,
-        };
-        this.options.isSubscriptionEligible = this.getIsSubscriptionEligible();
 
         this.render();
 
@@ -76,7 +64,6 @@ class ProgramDetailsView extends Backbone.View {
             pageName: 'program_dashboard',
             linkCategory: 'green_upgrade',
         });
-        this.trackSubscriptionEligibleProgramView();
     }
 
     static getUrl(base, programData) {
@@ -107,7 +94,6 @@ class ProgramDetailsView extends Backbone.View {
             creditPathways: this.options.creditPathways,
             discussionFragment: this.options.discussionFragment,
             live_fragment: this.options.live_fragment,
-            isSubscriptionEligible: this.options.isSubscriptionEligible,
             launchIcon,
             restartIcon,
         };
@@ -115,7 +101,6 @@ class ProgramDetailsView extends Backbone.View {
         data = $.extend(
             data,
             this.programModel.toJSON(),
-            this.subscriptionModel.toJSON(),
         );
         HtmlUtils.setHtml(this.$el, this.tpl(data));
         this.postRender();
@@ -126,18 +111,15 @@ class ProgramDetailsView extends Backbone.View {
             model: new Backbone.Model(this.options),
         });
 
-        if (this.options.isSubscriptionEligible) {
-            const { enrollmentAlerts, trialEndingAlerts } = this.getAlerts();
+        const { enrollmentAlerts } = this.getAlerts();
 
-            if (enrollmentAlerts.length || trialEndingAlerts.length) {
-                this.alertListView = new AlertListView({
-                    context: {
-                        enrollmentAlerts,
-                        trialEndingAlerts,
-                        pageType: 'programDetails',
-                    },
-                });
-            }
+        if (enrollmentAlerts.length) {
+            this.alertListView = new AlertListView({
+                context: {
+                    enrollmentAlerts,
+                    pageType: 'programDetails',
+                },
+            });
         }
 
         if (this.remainingCourseCollection.length > 0) {
@@ -178,12 +160,10 @@ class ProgramDetailsView extends Backbone.View {
             el: '.js-program-sidebar',
             model: this.programModel,
             courseModel: this.courseData,
-            subscriptionModel: this.subscriptionModel,
             certificateCollection: this.certificateCollection,
             industryPathways: this.options.industryPathways,
             creditPathways: this.options.creditPathways,
             programTabViewEnabled: this.options.programTabViewEnabled,
-            isSubscriptionEligible: this.options.isSubscriptionEligible,
             urls: this.options.urls,
         });
         let hasIframe = false;
@@ -197,55 +177,14 @@ class ProgramDetailsView extends Backbone.View {
         }).bind(this);
     }
 
-    getIsSubscriptionEligible() {
-        const courseCollections = [
-            this.completedCourseCollection,
-            this.inProgressCourseCollection,
-        ];
-        const isSomeCoursePurchasable = courseCollections.some((collection) => (
-            collection.some((course) => (
-                course.get('upgrade_url')
-                && !(course.get('expired') === true)
-            ))
-        ));
-        const programPurchasedWithoutSubscription = (
-            this.subscriptionModel.get('subscriptionState') !== 'active'
-            && this.subscriptionModel.get('subscriptionState') !== 'inactive'
-            && !isSomeCoursePurchasable
-            && this.remainingCourseCollection.length === 0
-        );
-
-        const isSubscriptionActiveSunsetting = (
-            this.subscriptionModel.get('subscriptionState') === 'active'
-        )
-
-        return (
-            this.options.isUserB2CSubscriptionsEnabled
-            && isSubscriptionActiveSunsetting
-            && !programPurchasedWithoutSubscription
-        );
-    }
-
     getAlerts() {
         const alerts = {
             enrollmentAlerts: [],
-            trialEndingAlerts: [],
         };
-        if (this.subscriptionModel.get('subscriptionState') === 'active') {
-            if (this.courseData.get('all_unenrolled')) {
-                alerts.enrollmentAlerts.push({
-                    title: this.programModel.get('title'),
-                });
-            }
-            if (
-                this.subscriptionModel.get('remainingDays') <= 7
-                && this.subscriptionModel.get('hasActiveTrial')
-            ) {
-                alerts.trialEndingAlerts.push({
-                    title: this.programModel.get('title'),
-                    ...this.subscriptionModel.toJSON(),
-                });
-            }
+        if (this.courseData.get('all_unenrolled')) {
+            alerts.enrollmentAlerts.push({
+                title: this.programModel.get('title'),
+            });
         }
         return alerts;
     }
@@ -257,37 +196,6 @@ class ProgramDetailsView extends Backbone.View {
             label: data.title,
             uuid: data.uuid,
         });
-    }
-
-    trackSubscriptionCTA() {
-        const state = this.subscriptionModel.get('subscriptionState');
-
-        if (state === 'active') {
-            window.analytics.track(
-                'edx.bi.user.subscription.program-detail-page.manage.clicked',
-                this.subscriptionEventParams,
-            );
-        } else {
-            const isNewSubscription = state !== 'inactive';
-            window.analytics.track(
-                'edx.bi.user.subscription.program-detail-page.subscribe.clicked',
-                {
-                    category: `${this.options.programData.variant} bundle`,
-                    is_new_subscription: isNewSubscription,
-                    is_trial_eligible: isNewSubscription,
-                    ...this.subscriptionEventParams,
-                },
-            );
-        }
-    }
-
-    trackSubscriptionEligibleProgramView() {
-        if (this.options.isSubscriptionEligible) {
-            window.analytics.track(
-                'edx.bi.user.subscription.program-detail-page.viewed',
-                this.subscriptionEventParams,
-            );
-        }
     }
 }
 
