0be8550c955e86e07ff879e647e81dab03a73149
Justin Hynes <jhynes@2u.com>
2024-08-15 16:00:40 +0000

    Revert "fix: quality"
    This reverts commit 77ca0f754af322b918434960f74948665d0847e1.

--- scripts/user_retirement/utils/edx_api.py
+++ scripts/user_retirement/utils/edx_api.py
@@ -491,7 +491,6 @@ class LicenseManagerApi(BaseApiClient):
             LOG.info("No license manager data found for user")
             return True
 
-
 class CommerceCoordinatorApi(BaseApiClient):
     """
     Commerce-Coordinator API client.
