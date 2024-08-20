a89adf4d495b56af4bc299cb95ff6dbabb4a872d
awais qureshi <awais.qureshi@arbisoft.com>
2024-08-20 12:18:31 +0500

    feat: upgrading simple api to drf compatible.

--- lms/djangoapps/instructor/views/serializer.py
+++ lms/djangoapps/instructor/views/serializer.py
@@ -66,6 +66,7 @@ class ShowStudentExtensionSerializer(serializers.Serializer):
     Serializer for validating and processing the student identifier.
     """
     student = serializers.CharField(write_only=True, required=True)
+
     def validate_student(self, value):
         """
         Validate that the student corresponds to an existing user.
@@ -73,10 +74,6 @@ class ShowStudentExtensionSerializer(serializers.Serializer):
         try:
             user = get_student_from_identifier(value)
         except User.DoesNotExist:
-            response_payload = {
-                'student': value,
-                'userDoesNotExist': True,
-            }
-            return response_payload
+            return None
 
         return user
