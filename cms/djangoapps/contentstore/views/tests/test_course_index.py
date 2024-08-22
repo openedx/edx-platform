36a3b0ba8178aa479464b9eccfb9f9b0e5e0240e
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2024-08-22 15:49:21 -0400

    Revert "fix: update course discussion config before course load (#35219)" (#35349)
    This reverts commit 5c0942481ce292a90f86259bd223d66e7ceffe9f.

--- cms/djangoapps/contentstore/views/tests/test_course_index.py
+++ cms/djangoapps/contentstore/views/tests/test_course_index.py
@@ -717,8 +717,8 @@ class TestCourseOutline(CourseTestCase):
         """
         Test to check number of queries made to mysql and mongo
         """
-        with self.assertNumQueries(32, table_ignorelist=WAFFLE_TABLES):
-            with check_mongo_calls(5):
+        with self.assertNumQueries(29, table_ignorelist=WAFFLE_TABLES):
+            with check_mongo_calls(3):
                 self.client.get_html(reverse_course_url('course_handler', self.course.id))
 
 
