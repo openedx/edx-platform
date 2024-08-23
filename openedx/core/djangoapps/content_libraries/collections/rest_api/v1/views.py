b2b38cb940c8885fb0fd7e619b062742a9f69938
Yusuf Musleh <yusuf@opencraft.com>
2024-08-23 13:46:23 +0300

    feat: Verify collection belongs to library

--- openedx/core/djangoapps/content_libraries/collections/rest_api/v1/views.py
+++ openedx/core/djangoapps/content_libraries/collections/rest_api/v1/views.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 
 from django.http import Http404
 
-# from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.status import HTTP_405_METHOD_NOT_ALLOWED
@@ -30,19 +29,37 @@ class LibraryCollectionsView(ModelViewSet):
 
     serializer_class = ContentLibraryCollectionSerializer
 
+    def _verify_and_fetch_library_collection(self, library_key, collection_id, user, permission) -> Collection | None:
+        """
+        Verify that the collection belongs to the library and the user has the correct permissions
+        """
+        try:
+            library_obj = api.require_permission_for_library_key(library_key, user, permission)
+        except api.ContentLibraryNotFound:
+            raise Http404
+
+        collection = None
+        if library_obj.learning_package_id:
+            collection = authoring_api.get_learning_package_collections(
+                library_obj.learning_package_id
+            ).filter(id=collection_id).first()
+        return collection
+
     def retrieve(self, request, lib_key_str, pk=None):
         """
         Retrieve the Content Library Collection
         """
-        try:
-            collection = authoring_api.get_collection(pk)
-        except Collection.DoesNotExist as exc:
-            raise Http404 from exc
+        library_key = LibraryLocatorV2.from_string(lib_key_str)
 
         # Check if user has permissions to view this collection by checking if
         # user has permission to view the Content Library it belongs to
-        library_key = LibraryLocatorV2.from_string(lib_key_str)
-        api.require_permission_for_library_key(library_key, request.user, permissions.CAN_VIEW_THIS_CONTENT_LIBRARY)
+        collection = self._verify_and_fetch_library_collection(
+            library_key, pk, request.user, permissions.CAN_VIEW_THIS_CONTENT_LIBRARY
+        )
+
+        if not collection:
+            raise Http404
+
         serializer = self.get_serializer(collection)
         return Response(serializer.data)
 
@@ -53,8 +70,13 @@ class LibraryCollectionsView(ModelViewSet):
         # Check if user has permissions to view collections by checking if user
         # has permission to view the Content Library they belong to
         library_key = LibraryLocatorV2.from_string(lib_key_str)
-        api.require_permission_for_library_key(library_key, request.user, permissions.CAN_VIEW_THIS_CONTENT_LIBRARY)
-        content_library = api.get_library(library_key)
+        try:
+            content_library = api.require_permission_for_library_key(
+                library_key, request.user, permissions.CAN_VIEW_THIS_CONTENT_LIBRARY
+            )
+        except api.ContentLibraryNotFound:
+            raise Http404
+
         collections = authoring_api.get_learning_package_collections(content_library.learning_package.id)
         serializer = self.get_serializer(collections, many=True)
         return Response(serializer.data)
@@ -66,10 +88,15 @@ class LibraryCollectionsView(ModelViewSet):
         # Check if user has permissions to create a collection in the Content Library
         # by checking if user has permission to edit the Content Library
         library_key = LibraryLocatorV2.from_string(lib_key_str)
-        api.require_permission_for_library_key(library_key, request.user, permissions.CAN_EDIT_THIS_CONTENT_LIBRARY)
+        try:
+            content_library = api.require_permission_for_library_key(
+                library_key, request.user, permissions.CAN_EDIT_THIS_CONTENT_LIBRARY
+            )
+        except api.ContentLibraryNotFound:
+            raise Http404
+
         create_serializer = ContentLibraryCollectionCreateOrUpdateSerializer(data=request.data)
         create_serializer.is_valid(raise_exception=True)
-        content_library = api.get_library(library_key)
         collection = authoring_api.create_collection(
             content_library.learning_package.id,
             create_serializer.validated_data["title"],
@@ -83,15 +110,15 @@ class LibraryCollectionsView(ModelViewSet):
         """
         Update a Collection that belongs to a Content Library
         """
+        library_key = LibraryLocatorV2.from_string(lib_key_str)
         # Check if user has permissions to update a collection in the Content Library
         # by checking if user has permission to edit the Content Library
-        library_key = LibraryLocatorV2.from_string(lib_key_str)
-        api.require_permission_for_library_key(library_key, request.user, permissions.CAN_EDIT_THIS_CONTENT_LIBRARY)
+        collection = self._verify_and_fetch_library_collection(
+            library_key, pk, request.user, permissions.CAN_EDIT_THIS_CONTENT_LIBRARY
+        )
 
-        try:
-            collection = authoring_api.get_collection(pk)
-        except Collection.DoesNotExist as exc:
-            raise Http404 from exc
+        if not collection:
+            raise Http404
 
         update_serializer = ContentLibraryCollectionCreateOrUpdateSerializer(
             collection, data=request.data, partial=True
