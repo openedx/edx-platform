0b274715f5d92a38fef4de4c736dec12c19a4732
awais qureshi <awais.qureshi@arbisoft.com>
2024-08-29 18:40:17 +0500

    feat: upgrading simple api to drf compatible.

--- openedx/core/djangoapps/django_comment_common/admin.py
+++ openedx/core/djangoapps/django_comment_common/admin.py
@@ -5,6 +5,10 @@ Admin for managing the connection to the Forums backend service.
 
 from django.contrib import admin
 
-from .models import ForumsConfig
+from .models import ForumsConfig, Role,DiscussionsIdMapping, User
 
 admin.site.register(ForumsConfig)
+admin.site.register(Role)
+admin.site.register(DiscussionsIdMapping)
+# admin.site.register(User)
+
