50574ed82af85dea9450de410da48240ceb5f70d
Awais Qureshi <awais.qureshi@arbisoft.com>
2024-08-30 11:54:38 +0500

    chore: Update admin.py

--- openedx/core/djangoapps/django_comment_common/admin.py
+++ openedx/core/djangoapps/django_comment_common/admin.py
@@ -5,10 +5,5 @@ Admin for managing the connection to the Forums backend service.
 
 from django.contrib import admin
 
-from .models import ForumsConfig, Role,DiscussionsIdMapping, User
-
+from .models import ForumsConfig
 admin.site.register(ForumsConfig)
-admin.site.register(Role)
-admin.site.register(DiscussionsIdMapping)
-# admin.site.register(User)
-
