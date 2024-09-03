650e8aabd5010d864ee38ad7503bbdc00349e9b7
Braden MacDonald <mail@bradenm.com>
2024-09-03 10:40:35 -0700

    Revert "refactor: remove unused JS requirement in CMS (#34545)"
    This reverts commit 4a12de7851ddc39c371e7ea4fb98316b8fc7d136.

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1449,8 +1449,9 @@ base_vendor_js = [
     'edx-ui-toolkit/js/utils/string-utils.js',
     'edx-ui-toolkit/js/utils/html-utils.js',
 
-    # Here we were loading Bootstrap and supporting libraries, but it no longer seems to be needed for any Studio UI.
-    # 'common/js/vendor/bootstrap.bundle.js',
+    # Load Bootstrap and supporting libraries
+    'common/js/vendor/popper.js',
+    'common/js/vendor/bootstrap.js',
 
     # Finally load RequireJS
     'common/js/vendor/require.js'
