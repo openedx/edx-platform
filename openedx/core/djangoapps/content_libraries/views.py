7665f1354774eaeecb828ce58dfeebd6b4fdee78
Yusuf Musleh <yusuf@opencraft.com>
2024-09-11 21:21:10 +0300

    feat: Add Library Collections REST endpoints [FC-0062] (#35321)
    * feat: Add Library Collections REST endpoints
    
    * test: Add tests for Collections REST APIs
    
    * chore: Add missing __init__ files
    
    * feat: Add events emitting for Collections
    
    * feat: Add REST endpoints to update Components in a Collections (temp) (#674)
    
    * feat: add/remove components to/from a collection
    
    * docs: Add warning about unstable REST APIs
    
    * chore: updates openedx-events==9.14.0
    
    * chore: updates openedx-learning==0.11.4
    
    * fix: assert collection doc have unique id
    
    ---------
    
    Co-authored-by: Jillian <jill@opencraft.com>
    Co-authored-by: Chris Chávez <xnpiochv@gmail.com>
    Co-authored-by: Rômulo Penido <romulo.penido@gmail.com>

--- openedx/core/djangoapps/content_libraries/views.py
+++ openedx/core/djangoapps/content_libraries/views.py
@@ -84,6 +84,7 @@ from pylti1p3.contrib.django import DjangoCacheDataStorage, DjangoDbToolConf, Dj
 from pylti1p3.exception import LtiException, OIDCException
 
 import edx_api_doc_tools as apidocs
+from opaque_keys import InvalidKeyError
 from opaque_keys.edx.locator import LibraryLocatorV2, LibraryUsageLocatorV2
 from organizations.api import ensure_organization
 from organizations.exceptions import InvalidOrganizationException
@@ -136,12 +137,21 @@ def convert_exceptions(fn):
     def wrapped_fn(*args, **kwargs):
         try:
             return fn(*args, **kwargs)
+        except InvalidKeyError as exc:
+            log.exception(str(exc))
+            raise NotFound  # lint-amnesty, pylint: disable=raise-missing-from
         except api.ContentLibraryNotFound:
             log.exception("Content library not found")
             raise NotFound  # lint-amnesty, pylint: disable=raise-missing-from
         except api.ContentLibraryBlockNotFound:
             log.exception("XBlock not found in content library")
             raise NotFound  # lint-amnesty, pylint: disable=raise-missing-from
+        except api.ContentLibraryCollectionNotFound:
+            log.exception("Collection not found in content library")
+            raise NotFound  # lint-amnesty, pylint: disable=raise-missing-from
+        except api.LibraryCollectionAlreadyExists as exc:
+            log.exception(str(exc))
+            raise ValidationError(str(exc))  # lint-amnesty, pylint: disable=raise-missing-from
         except api.LibraryBlockAlreadyExists as exc:
             log.exception(str(exc))
             raise ValidationError(str(exc))  # lint-amnesty, pylint: disable=raise-missing-from
