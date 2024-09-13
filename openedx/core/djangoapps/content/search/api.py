c41fe8991832de7e987368a533d4b0e9b91fefce
Jillian <jill@opencraft.com>
2024-09-13 22:50:54 +0930

    Store content object collections in search index [FC-0062] (#35469)
    * feat: Add Library Collections REST endpoints
    
    * test: Add tests for Collections REST APIs
    
    * chore: Add missing __init__ files
    
    * docs: Add warning about unstable REST APIs
    
    * feat: emit CONTENT_OBJECT_ASSOCIATIONS_CHANGED
    whenever a content object's tags or collections have changed,
    and handle that event in content/search.
    
    The deprecated CONTENT_OBJECT_TAGS_CHANGED event is still emitted when
    tags change; to be removed after Sumac.
    
    * docs: replaces CONTENT_OBJECT_TAGS_CHANGED with CONTENT_OBJECT_ASSOCIATIONS_CHANGED
    
    * chore: updates openedx-events==9.14.0
    
    * chore: updates openedx-learning==0.11.4
    
    Co-authored-by: Yusuf Musleh <yusuf@opencraft.com>
    Co-authored-by: Chris Chávez <xnpiochv@gmail.com>
    Co-authored-by: Rômulo Penido <romulo@thinkdash.dev>

--- openedx/core/djangoapps/content/search/api.py
+++ openedx/core/djangoapps/content/search/api.py
@@ -34,6 +34,7 @@ from .documents import (
     searchable_doc_for_course_block,
     searchable_doc_for_collection,
     searchable_doc_for_library_block,
+    searchable_doc_collections,
     searchable_doc_tags,
 )
 
@@ -322,6 +323,9 @@ def rebuild_index(status_cb: Callable[[str], None] | None = None) -> None:
             Fields.tags + "." + Fields.tags_level1,
             Fields.tags + "." + Fields.tags_level2,
             Fields.tags + "." + Fields.tags_level3,
+            Fields.collections,
+            Fields.collections + "." + Fields.collections_display_name,
+            Fields.collections + "." + Fields.collections_key,
             Fields.type,
             Fields.access_id,
             Fields.last_published,
@@ -333,8 +337,9 @@ def rebuild_index(status_cb: Callable[[str], None] | None = None) -> None:
             Fields.display_name,
             Fields.block_id,
             Fields.content,
-            Fields.tags,
             Fields.description,
+            Fields.tags,
+            Fields.collections,
             # If we don't list the following sub-fields _explicitly_, they're only sometimes searchable - that is, they
             # are searchable only if at least one document in the index has a value. If we didn't list them here and,
             # say, there were no tags.level3 tags in the index, the client would get an error if trying to search for
@@ -344,6 +349,8 @@ def rebuild_index(status_cb: Callable[[str], None] | None = None) -> None:
             Fields.tags + "." + Fields.tags_level1,
             Fields.tags + "." + Fields.tags_level2,
             Fields.tags + "." + Fields.tags_level3,
+            Fields.collections + "." + Fields.collections_display_name,
+            Fields.collections + "." + Fields.collections_key,
         ])
         # Mark which attributes can be used for sorting search results:
         client.index(temp_index_name).update_sortable_attributes([
@@ -375,6 +382,7 @@ def rebuild_index(status_cb: Callable[[str], None] | None = None) -> None:
                     doc = {}
                     doc.update(searchable_doc_for_library_block(metadata))
                     doc.update(searchable_doc_tags(metadata.usage_key))
+                    doc.update(searchable_doc_collections(metadata.usage_key))
                     docs.append(doc)
                 except Exception as err:  # pylint: disable=broad-except
                     status_cb(f"Error indexing library component {component}: {err}")
@@ -549,6 +557,22 @@ def upsert_library_block_index_doc(usage_key: UsageKey) -> None:
     _update_index_docs(docs)
 
 
+def upsert_library_collection_index_doc(library_key: LibraryLocatorV2, collection_key: str) -> None:
+    """
+    Creates or updates the document for the given Library Collection in the search index
+    """
+    content_library = lib_api.ContentLibrary.objects.get_by_key(library_key)
+    collection = authoring_api.get_collection(
+        learning_package_id=content_library.learning_package_id,
+        collection_key=collection_key,
+    )
+    docs = [
+        searchable_doc_for_collection(collection)
+    ]
+
+    _update_index_docs(docs)
+
+
 def upsert_content_library_index_docs(library_key: LibraryLocatorV2) -> None:
     """
     Creates or updates the documents for the given Content Library in the search index
@@ -568,6 +592,15 @@ def upsert_block_tags_index_docs(usage_key: UsageKey):
     """
     doc = {Fields.id: meili_id_from_opaque_key(usage_key)}
     doc.update(searchable_doc_tags(usage_key))
+    _update_index_docs([doc])
+
+
+def upsert_block_collections_index_docs(usage_key: UsageKey):
+    """
+    Updates the collections data in documents for the given Course/Library block
+    """
+    doc = {Fields.id: meili_id_from_opaque_key(usage_key)}
+    doc.update(searchable_doc_collections(usage_key))
     _update_index_docs([doc])
 
 
