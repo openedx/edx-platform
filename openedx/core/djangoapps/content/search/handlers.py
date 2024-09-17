8fe50a7e3678c9bc27b1178935c77e9413451a51
Braden MacDonald <braden@opencraft.com>
2024-09-17 12:37:02 -0700

    feat: when changing a v2 library block, update search index synchronously

--- openedx/core/djangoapps/content/search/handlers.py
+++ openedx/core/djangoapps/content/search/handlers.py
@@ -124,7 +124,9 @@ def library_block_updated_handler(**kwargs) -> None:
         log.error("Received null or incorrect data for event")
         return
 
-    upsert_library_block_index_doc.delay(str(library_block_data.usage_key))
+    # Update content library index synchronously to make sure that search index is updated before
+    # the frontend invalidates/refetches results. This is only a single document update so is very fast.
+    upsert_library_block_index_doc.apply(args=[str(library_block_data.usage_key)])
 
 
 @receiver(LIBRARY_BLOCK_DELETED)
