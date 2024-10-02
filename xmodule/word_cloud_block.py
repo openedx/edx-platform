64cf575486322c570a82ff7141c20078c9ba2dc9
farhan <farhan.khan@arbisoft.com>
2024-10-02 14:31:38 +0500

    chore: Initial commit

--- xmodule/word_cloud_block.py
+++ xmodule/word_cloud_block.py
@@ -6,6 +6,9 @@ If student does not yet answered - `num_inputs` numbers of text inputs.
 If student have answered - words he entered and cloud.
 """
 
+# TODO: Uncomment following once https://github.com/openedx/edx-platform/issues/34840 is done
+# from xblocks_contrib import WordCloudBlock as _ExtractedWordCloudBlock
+
 
 import json
 import logging
@@ -15,6 +18,7 @@ from xblock.core import XBlock
 from xblock.fields import Boolean, Dict, Integer, List, Scope, String
 from xmodule.editing_block import EditingMixin
 from xmodule.raw_block import EmptyDataRawMixin
+from xmodule.toggles import USE_EXTRACTED_WORD_CLOUD_BLOCK
 from xmodule.util.builtin_assets import add_webpack_js_to_fragment, add_sass_to_fragment
 from xmodule.xml_block import XmlMixin
 from xmodule.x_module import (
@@ -41,7 +45,7 @@ def pretty_bool(value):
 
 
 @XBlock.needs('mako')
-class WordCloudBlock(  # pylint: disable=abstract-method
+class _BuiltInWordCloudBlock(  # pylint: disable=abstract-method
     EmptyDataRawMixin,
     XmlMixin,
     EditingMixin,
@@ -52,6 +56,8 @@ class WordCloudBlock(  # pylint: disable=abstract-method
     """
     Word Cloud XBlock.
     """
+
+    is_extracted = False
 
     display_name = String(
         display_name=_("Display Name"),
@@ -308,3 +314,10 @@ class WordCloudBlock(  # pylint: disable=abstract-method
         xblock_body["content_type"] = "Word Cloud"
 
         return xblock_body
+
+
+WordCloudBlock = (
+    # _ExractedWordCloudBLock if USE_EXTRACTED_WORD_CLOUD_BLOCK.is_enabled()
+    _BuiltInWordCloudBlock if USE_EXTRACTED_WORD_CLOUD_BLOCK.is_enabled()
+    else _BuiltInWordCloudBlock
+)
