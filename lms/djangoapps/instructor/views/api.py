f48b474e3ada44380982b0183ee18b7925f7f272
Awais Qureshi <awais.qureshi@arbisoft.com>
2024-10-07 00:27:35 +0500

    chore: Update api.py

--- lms/djangoapps/instructor/views/api.py
+++ lms/djangoapps/instructor/views/api.py
@@ -76,10 +76,7 @@ from common.djangoapps.util.file import (
 from common.djangoapps.util.json_request import JsonResponse, JsonResponseBadRequest
 from common.djangoapps.util.views import require_global_staff  # pylint: disable=unused-import
 from lms.djangoapps.bulk_email.api import is_bulk_email_feature_enabled, create_course_email
-from lms.djangoapps.certificates import api as certs_api
-from lms.djangoapps.certificates.models import (
-    CertificateStatuses
-)
+from lms.djangoapps.certificates import api as certs_api    
 from lms.djangoapps.course_home_api.toggles import course_home_mfe_progress_tab_is_active
 from lms.djangoapps.courseware.access import has_access
 from lms.djangoapps.courseware.courses import get_course_with_access
