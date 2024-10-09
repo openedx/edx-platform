houras989/openLMS

feat: Upstream Sync with Content Library Blocks
Implements:
https://github.com/kdmccormick/edx-platform/blob/kdmccormick/upstream-proto/docs/decisions/0020-upstream-block.rst

cms/djangoapps/contentstore/rest_api/v1/serializers/vertical_block.py
@@ -103,6 +103,17 @@ class ContainerHandlerSerializer(serializers.Serializer):
         return None
 
 
+class UpstreamInfoSerializer(serializers.Serializer):
+    """
+    Serializer holding info for syncing a block with its upstream (eg, a library block).
+    """
+    upstream_ref = serializers.CharField()
+    current_version = serializers.IntegerField(allow_null=True)
+    latest_version = serializers.IntegerField(allow_null=True)
+    warning = serializers.CharField(allow_null=True)
+    can_sync = serializers.BooleanField()
+
+
 class ChildVerticalContainerSerializer(serializers.Serializer):
     """
     Serializer for representing a xblock child of vertical container.
@@ -113,6 +124,7 @@ class ChildVerticalContainerSerializer(serializers.Serializer):
     block_type = serializers.CharField()
     user_partition_info = serializers.DictField()
     user_partitions = serializers.ListField()
+    upstream_info = UpstreamInfoSerializer(allow_null=True)
     actions = serializers.SerializerMethodField()
     validation_messages = MessageValidation(many=True)
     render_error = serializers.CharField()

openedx/edx-platform

cms/djangoapps/contentstore/rest_api/v1/serializers/vertical_block.py
@@ -103,6 +103,18 @@ class ContainerHandlerSerializer(serializers.Serializer):
         return None
 
 
+class UpstreamLinkSerializer(serializers.Serializer):
+    """
+    Serializer holding info for syncing a block with its upstream (eg, a library block).
+    """
+    upstream_ref = serializers.CharField()
+    version_synced = serializers.IntegerField()
+    version_available = serializers.IntegerField(allow_null=True)
+    version_declined = serializers.IntegerField(allow_null=True)
+    error_message = serializers.CharField(allow_null=True)
+    ready_to_sync = serializers.BooleanField()
+
+
 class ChildVerticalContainerSerializer(serializers.Serializer):
     """
     Serializer for representing a xblock child of vertical container.
@@ -113,6 +125,7 @@ class ChildVerticalContainerSerializer(serializers.Serializer):
     block_type = serializers.CharField()
     user_partition_info = serializers.DictField()
     user_partitions = serializers.ListField()
+    upstream_link = UpstreamLinkSerializer(allow_null=True)
     actions = serializers.SerializerMethodField()
     validation_messages = MessageValidation(many=True)
     render_error = serializers.CharField()