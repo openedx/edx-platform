1edbf49d41ffbef8927a33f26ce3e4e2f901b2e1
dattatadepalli <datta.t@talentsprint.com>
2024-10-21 14:23:09 +0530

    timezone issue fix

--- lms/static/js/dateutil_factory.js
+++ lms/static/js/dateutil_factory.js
@@ -13,95 +13,106 @@
  *
  */
 
-(function(define) {
-    'use strict';
+(function (define) {
+  "use strict";
 
-    define([
-        'jquery',
-        'edx-ui-toolkit/js/utils/date-utils',
-        'edx-ui-toolkit/js/utils/string-utils'
-    ], function($, DateUtils, StringUtils) {
-        var DateUtilFactory;
-        var localizedTime;
-        var stringHandler;
-        var displayDatetime;
-        var isValid;
-        var transform;
-        var dueDateFormat;
-        var dateFormat;
+  define([
+    "jquery",
+    "edx-ui-toolkit/js/utils/date-utils",
+    "edx-ui-toolkit/js/utils/string-utils",
+    "moment", // Ensure moment is included
+    "moment-timezone", // Ensure moment-timezone is included
+  ], function ($, DateUtils, StringUtils) {
+    var DateUtilFactory;
+    var localizedTime;
+    var stringHandler;
+    var displayDatetime;
+    var isValid;
+    var transform;
+    var dueDateFormat;
+    var dateFormat;
 
-        dueDateFormat = Object.freeze({
-            '%Y-%d-%m': 'YYYY, D MMM HH[:]mm z', // example: 2018, 01 Jan 15:30 UTC
-            '%m-%d-%Y': 'MMM D, YYYY HH[:]mm z', // example: Jan 01, 2018 15:30 UTC
-            '%d-%m-%Y': 'D MMM YYYY HH[:]mm z', // example: 01 Jan, 2018 15:30 UTC
-            '%Y-%m-%d': 'YYYY, MMM D HH[:]mm z' // example: 2018, Jan 01 15:30 UTC
-        });
-
-        transform = function(iterationKey) {
-            var context;
-            $(iterationKey).each(function() {
-                if (isValid($(this).data('datetime'))) {
-                    dateFormat = DateUtils.dateFormatEnum[$(this).data('format')];
-                    if (typeof dateFormat === 'undefined') {
-                        dateFormat = dueDateFormat[$(this).data('format')];
-                    }
-                    context = {
-                        datetime: $(this).data('datetime'),
-                        timezone: $(this).data('timezone'),
-                        language: $(this).data('language'),
-                        format: dateFormat
-                    };
-                    displayDatetime = stringHandler(
-                        localizedTime(context),
-                        $(this).data('string'),
-                        $(this).data('datetoken')
-                    );
-                    $(this).text(displayDatetime);
-                } else {
-                    displayDatetime = stringHandler(
-                        $(this).data('string')
-                    );
-                    $(this).text(displayDatetime);
-                }
-            });
-        };
-
-        localizedTime = function(context) {
-            return DateUtils.localize(context);
-        };
-
-        stringHandler = function(localTimeString, containerString, token) {
-            var returnString;
-            var interpolateDict = {};
-            var dateToken;
-            if (isValid(token)) {
-                dateToken = token;
-            } else {
-                dateToken = 'date';
-            }
-            interpolateDict[dateToken] = localTimeString;
-
-            if (isValid(containerString)) {
-                returnString = StringUtils.interpolate(
-                    containerString,
-                    interpolateDict
-                );
-            } else {
-                returnString = localTimeString;
-            }
-            return returnString;
-        };
-
-        isValid = function(candidateVariable) {
-            return candidateVariable !== undefined
-                && candidateVariable !== ''
-                && candidateVariable !== 'Invalid date'
-                && candidateVariable !== 'None';
-        };
-        DateUtilFactory = {
-            transform: transform,
-            stringHandler: stringHandler
-        };
-        return DateUtilFactory;
+    dueDateFormat = Object.freeze({
+      "%Y-%d-%m": "YYYY, D MMM HH:mm [GMT]ZZ", // example: 2018, 01 Jan 15:30 GMT+0000
+      "%m-%d-%Y": "MMM D, YYYY, HH:mm [GMT]ZZ", // example: Jan 01, 2018, 15:30 GMT+0000
+      "%d-%m-%Y": "D MMM YYYY, HH:mm [GMT]ZZ", // example: 01 Jan, 2018, 15:30 GMT+0000
+      "%Y-%m-%d": "YYYY, MMM D HH:mm [GMT]ZZ", // example: 2018, Jan 01 15:30 GMT+0000
     });
+
+    transform = function (iterationKey) {
+      var context;
+      $(iterationKey).each(function () {
+        if (isValid($(this).data("datetime"))) {
+          dateFormat = DateUtils.dateFormatEnum[$(this).data("format")];
+          if (typeof dateFormat === "undefined") {
+            dateFormat = dueDateFormat[$(this).data("format")];
+          }
+          context = {
+            datetime: $(this).data("datetime"),
+            timezone: $(this).data("timezone"),
+            language: $(this).data("language"),
+            format: dateFormat,
+          };
+          displayDatetime = stringHandler(localizedTime(context), $(this).data("string"), $(this).data("datetoken"));
+          $(this).text(displayDatetime);
+        } else {
+          displayDatetime = stringHandler($(this).data("string"));
+          $(this).text(displayDatetime);
+        }
+      });
+    };
+
+    localizedTime = function (context) {
+      function formatTimeInTimezone(dateTime, timeZone = "UTC") {
+        const options = {
+          year: "numeric",
+          month: "short", // 'short' gives the abbreviated month name (e.g., "Aug")
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false, // 24-hour format (set to `true` for 12-hour format)
+          timeZone: timeZone, // Specify the desired timezone (e.g., CST)
+          timeZoneName: "short", // Include the short timezone abbreviation (CST, PDT, etc.)
+        };
+
+        const formattedTime = new Intl.DateTimeFormat(context.language, options).format(new Date(dateTime));
+        return formattedTime;
+      }
+      return formatTimeInTimezone(context.datetime, context.timezone);
+      // Output: "Aug 1, 2024, 02:30 CST"
+    };
+
+    stringHandler = function (localTimeString, containerString, token) {
+      var returnString;
+      var interpolateDict = {};
+      var dateToken;
+      if (isValid(token)) {
+        dateToken = token;
+      } else {
+        dateToken = "date";
+      }
+      interpolateDict[dateToken] = localTimeString;
+
+      if (isValid(containerString)) {
+        returnString = StringUtils.interpolate(containerString, interpolateDict);
+      } else {
+        returnString = localTimeString;
+      }
+      return returnString;
+    };
+
+    isValid = function (candidateVariable) {
+      return (
+        candidateVariable !== undefined &&
+        candidateVariable !== "" &&
+        candidateVariable !== "Invalid date" &&
+        candidateVariable !== "None"
+      );
+    };
+    DateUtilFactory = {
+      transform: transform,
+      stringHandler: stringHandler,
+    };
+    return DateUtilFactory;
+  });
 }).call(this, define || RequireJS.define);
