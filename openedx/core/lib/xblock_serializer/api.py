b991a1e88f1c6e62515e17d69086da5a719b0d4a
David Ormsbee <dave@axim.org>
2024-10-20 23:54:21 -0400

    refactor: remove XBlockSerializerForLearningCore
    The XBlockSerializerForLearningCore class existed to account for the
    differences in how Blockstore and later Learning Core stored OLX. This
    involved doing things like:
    
    * removing the url_name attribute (because it's encoded elsewhere)
    * special case conversion from <vertical> tags to <unit>
    * other handling over child definition identifiers
    
    Stripping the url_name was moved to the XBlockSerializer superclass
    because it was useful for copy-paste functionality. The other two are no
    longer relevant because Learning Core won't internally model a Unit as
    an XBlock, and the Learning Core XBlock Runtime doesn't support having
    children.

--- openedx/core/lib/xblock_serializer/api.py
+++ openedx/core/lib/xblock_serializer/api.py
@@ -2,7 +2,7 @@
 Public python API for serializing XBlocks to OLX
 """
 # pylint: disable=unused-import
-from .block_serializer import StaticFile, XBlockSerializer, XBlockSerializerForLearningCore
+from .block_serializer import StaticFile, XBlockSerializer
 
 
 def serialize_xblock_to_olx(block):
@@ -32,4 +32,4 @@ def serialize_modulestore_block_for_learning_core(block):
     we have around how we should rewrite this (e.g. are we going to
     remove <xblock-include>?).
     """
-    return XBlockSerializerForLearningCore(block)
+    return XBlockSerializer(block, write_url_name=False)
