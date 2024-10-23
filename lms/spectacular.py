6fb2424c1f991977eede4aef526a4b998cd1cc32
Awais Qureshi <awais.qureshi@arbisoft.com>
2024-10-23 19:20:21 +0500

    chore: Update spectacular.py

--- lms/spectacular.py
+++ lms/spectacular.py
@@ -3,17 +3,13 @@
 
 def lms_api_filter(endpoints):
     """
-    At the moment, we are only enabling drf-spectacular for the CMS API.
-    Filter out endpoints that are not part of the CMS API.
+    At the moment, we are only enabling drf-spectacular for the LMS INSTRUCTOR APIs.
     """
     filtered = []
     for (path, path_regex, method, callback) in endpoints:
-        # Add only paths to the list that are part of the CMS API
+        # Add only paths to the list that are part of the LMS API
         if (
-            # Don't just replace this with /v1 when switching to a later version of the CMS API.
-            # That would include some unintended endpoints.
             path.startswith("/courses/")
-            # or path.startswith("/verify_student/status/")
         ):
             filtered.append((path, path_regex, method, callback))
     return filtered
