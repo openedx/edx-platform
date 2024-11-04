70bbd68ba4af1c9131505a0ed1bc49c5fa637fdf
qasimgulzar <qasim.gulzar@arbisoft.com>
2024-11-04 13:25:51 +0500

    fix: migrations to make postgresql compatible.

--- lms/djangoapps/commerce/migrations/0001_data__add_ecommerce_service_user.py
+++ lms/djangoapps/commerce/migrations/0001_data__add_ecommerce_service_user.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from django.db import migrations, models
+from django.db import migrations, models, transaction
 
 USERNAME = settings.ECOMMERCE_SERVICE_WORKER_USERNAME
 EMAIL = USERNAME + '@fake.email'
@@ -9,14 +9,16 @@ EMAIL = USERNAME + '@fake.email'
 def forwards(apps, schema_editor):
     """Add the service user."""
     User = get_user_model()
-    user, created = User.objects.get_or_create(username=USERNAME, email=EMAIL)
-    if created:
-        user.set_unusable_password()
-        user.save()
+    with transaction.atomic():
+        user, created = User.objects.get_or_create(username=USERNAME, email=EMAIL)
+        if created:
+            user.set_unusable_password()
+            user.save()
 
 def backwards(apps, schema_editor):
     """Remove the service user."""
-    User.objects.get(username=USERNAME, email=EMAIL).delete()
+    with transaction.atomic():
+        User.objects.get(username=USERNAME, email=EMAIL).delete()
 
 class Migration(migrations.Migration):
 
