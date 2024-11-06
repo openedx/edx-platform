0452408ded2cd268c29ac457d8a178a9fd52d28a
Chandana3008 <74036324+Chandana3008@users.noreply.github.com>
2024-11-06 13:48:13 +0530

    Update urls.py added certificate and transcript urls

--- common/djangoapps/student/urls.py
+++ common/djangoapps/student/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     re_path(r'^extras/get_mettl_report', views.extras_get_mettl_report, name = "extras_get_mettl_report"), 
     re_path(r'^extras/update_lti_grades', views.extras_update_lti_grades, name = "extras_update_lti_grades"),
     re_path(r'^extras/get_peer_profiles', views.extras_get_peer_profiles, name = "extras_get_peer_profiles"),
+    
+    re_path(r'^extras/certificate', views.extras_certificate, name = "extras_certificate"),
+    re_path(r'^extras/transcript', views.extras_transcript, name = "extras_transcript"),
 
     re_path(r'^extras/get_user_enrolled_courses', views.extras_get_user_enrolled_courses, name = "extras_get_user_enrolled_courses"),
     re_path(r'^extras/get_last_login', views.extras_get_last_login, name = "extras_get_last_login"),
