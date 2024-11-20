969a3b8551607a6e7b209b1b767ffd6e5f57fddb
kshitij.sobti <kshitij@sobti.in>
2024-11-20 18:11:37 +0530

    feat: User agreements API for generic agreement records
    This change adds a new kind of generic user agreement that allows plugins or
    even the core platform to record a user's acknowledgement of an agreement.

--- openedx/core/djangoapps/agreements/urls.py
+++ openedx/core/djangoapps/agreements/urls.py
@@ -3,9 +3,9 @@ URLs for the Agreements API
 """
 
 from django.conf import settings
-from django.urls import re_path
+from django.urls import path, re_path
 
-from .views import IntegritySignatureView, LTIPIISignatureView
+from .views import IntegritySignatureView, LTIPIISignatureView, UserAgreementsView
 
 urlpatterns = [
     re_path(r'^integrity_signature/{course_id}$'.format(
@@ -14,4 +14,5 @@ urlpatterns = [
     re_path(r'^lti_pii_signature/{course_id}$'.format(
         course_id=settings.COURSE_ID_PATTERN
     ), LTIPIISignatureView.as_view(), name='lti_pii_signature'),
+    path("agreement/<slug:agreement_type>", UserAgreementsView.as_view(), name="user_agreements"),
 ]
