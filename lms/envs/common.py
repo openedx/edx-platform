41c9712ef2fe4eb1e0ee359b121e9fcfb31d8672
farhan <farhan.khan@arbisoft.com>
2024-12-03 17:30:14 +0500

    chore: test commit to check test cases status after turning flags

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -5575,7 +5575,7 @@ LMS_COMM_DEFAULT_FROM_EMAIL = "no-reply@example.com"
 # .. toggle_warning: Not production-ready until https://github.com/openedx/edx-platform/issues/34840 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_WORD_CLOUD_BLOCK = False
+USE_EXTRACTED_WORD_CLOUD_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_ANNOTATABLE_BLOCK
 # .. toggle_default: False
@@ -5585,7 +5585,7 @@ USE_EXTRACTED_WORD_CLOUD_BLOCK = False
 # .. toggle_warning: Not production-ready until https://github.com/openedx/edx-platform/issues/34841 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_ANNOTATABLE_BLOCK = False
+USE_EXTRACTED_ANNOTATABLE_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_POLL_QUESTION_BLOCK
 # .. toggle_default: False
@@ -5595,7 +5595,7 @@ USE_EXTRACTED_ANNOTATABLE_BLOCK = False
 # .. toggle_warning: Not production-ready until https://github.com/openedx/edx-platform/issues/34839 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_POLL_QUESTION_BLOCK = False
+USE_EXTRACTED_POLL_QUESTION_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_LTI_BLOCK
 # .. toggle_default: False
@@ -5605,7 +5605,7 @@ USE_EXTRACTED_POLL_QUESTION_BLOCK = False
 # .. toggle_warning: Not production-ready until relevant subtask https://github.com/openedx/edx-platform/issues/34827 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_LTI_BLOCK = False
+USE_EXTRACTED_LTI_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_HTML_BLOCK
 # .. toggle_default: False
@@ -5615,7 +5615,7 @@ USE_EXTRACTED_LTI_BLOCK = False
 # .. toggle_warning: Not production-ready until relevant subtask https://github.com/openedx/edx-platform/issues/34827 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_HTML_BLOCK = False
+USE_EXTRACTED_HTML_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_DISCUSSION_BLOCK
 # .. toggle_default: False
@@ -5625,7 +5625,7 @@ USE_EXTRACTED_HTML_BLOCK = False
 # .. toggle_warning: Not production-ready until relevant subtask https://github.com/openedx/edx-platform/issues/34827 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_DISCUSSION_BLOCK = False
+USE_EXTRACTED_DISCUSSION_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_PROBLEM_BLOCK
 # .. toggle_default: False
@@ -5635,7 +5635,7 @@ USE_EXTRACTED_DISCUSSION_BLOCK = False
 # .. toggle_warning: Not production-ready until relevant subtask https://github.com/openedx/edx-platform/issues/34827 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_PROBLEM_BLOCK = False
+USE_EXTRACTED_PROBLEM_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_VIDEO_BLOCK
 # .. toggle_default: False
@@ -5645,4 +5645,4 @@ USE_EXTRACTED_PROBLEM_BLOCK = False
 # .. toggle_warning: Not production-ready until relevant subtask https://github.com/openedx/edx-platform/issues/34827 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_VIDEO_BLOCK = False
+USE_EXTRACTED_VIDEO_BLOCK = True
