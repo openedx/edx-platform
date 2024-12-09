5520613b1149822d9eb8a9765fedbf5a36f5fde1
Régis Behmo <regis@behmo.com>
2024-12-09 16:15:54 +0100

    feat: make it possible to globally disable forum v2 with setting
    We introduce a setting that allows us to bypass any course waffle flag
    check. The advantage of such a setting is that we don't need to find the
    course ID: in some cases, we might not have access to the course ID, and
    we need to look for it... in forum v2.
    
    See discussion here: https://github.com/openedx/forum/issues/137

--- openedx/core/djangoapps/django_comment_common/comment_client/models.py
+++ openedx/core/djangoapps/django_comment_common/comment_client/models.py
@@ -2,10 +2,11 @@
 
 
 import logging
+import typing as t
 
 from .utils import CommentClientRequestError, extract, perform_request, get_course_key
 from forum import api as forum_api
-from openedx.core.djangoapps.discussions.config.waffle import is_forum_v2_enabled
+from openedx.core.djangoapps.discussions.config.waffle import is_forum_v2_enabled, is_forum_v2_disabled_globally
 
 log = logging.getLogger(__name__)
 
@@ -72,13 +73,14 @@ class Model:
 
     def _retrieve(self, *args, **kwargs):
         course_id = self.attributes.get("course_id") or kwargs.get("course_id")
-        if not course_id:
-            course_id = forum_api.get_course_id_by_comment(self.id)
-        course_key = get_course_key(course_id)
+        if course_id:
+            use_forumv2 = is_forum_v2_enabled(course_id)
+        else:
+            use_forumv2, course_id = is_forum_v2_enabled_for_comment(self.id)
         response = None
-        if is_forum_v2_enabled(course_key):
+        if use_forumv2:
             if self.type == "comment":
-                response = forum_api.get_parent_comment(comment_id=self.attributes["id"], course_id=str(course_key))
+                response = forum_api.get_parent_comment(comment_id=self.attributes["id"], course_id=course_id)
             if response is None:
                 raise CommentClientRequestError("Forum v2 API call is missing")
         else:
@@ -369,3 +371,22 @@ class Model:
             context=request_data.get("context", None),
         )
         return response
+
+
+def is_forum_v2_enabled_for_comment(comment_id: str) -> tuple[bool, t.Optional[str]]:
+    """
+    Figure out whether we use forum v2 for a given comment.
+
+    See is_forum_v2_enabled_for_thread.
+
+    Return:
+
+        enabled (bool)
+        course_id (str or None)
+    """
+    if is_forum_v2_disabled_globally():
+        return False, None
+
+    course_id = forum_api.get_course_id_by_comment(comment_id)
+    course_key = get_course_key(course_id)
+    return is_forum_v2_enabled(course_key), course_id
