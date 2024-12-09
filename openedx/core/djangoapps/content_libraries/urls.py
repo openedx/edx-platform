38be2edd72523131127141a4679e1dfa3092e573
Navin Karkera <navin@opencraft.com>
2024-12-09 19:58:28 +0530

    feat: api to restore soft-deleted component

--- openedx/core/djangoapps/content_libraries/urls.py
+++ openedx/core/djangoapps/content_libraries/urls.py
@@ -57,6 +57,7 @@ urlpatterns = [
         path('blocks/<str:usage_key_str>/', include([
             # Get metadata about a specific XBlock in this library, or delete the block:
             path('', views.LibraryBlockView.as_view()),
+            path('restore/', views.LibraryBlockRestore.as_view()),
             # Update collections for a given component
             path('collections/', views.LibraryBlockCollectionsView.as_view(), name='update-collections'),
             # Get the LTI URL of a specific XBlock
