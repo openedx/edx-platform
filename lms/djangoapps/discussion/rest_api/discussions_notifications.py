1391010e4bc688f7fdc4600e14da5472f240dee8
Jawad Khan <jawadkhan444@gmail.com>
2024-12-17 06:09:20 +0500

    fix: Added topic id in notification context

--- lms/djangoapps/discussion/rest_api/discussions_notifications.py
+++ lms/djangoapps/discussion/rest_api/discussions_notifications.py
@@ -68,7 +68,8 @@ class DiscussionNotificationSender:
                 "sender_id": self.creator.id,
                 'thread_id': self.thread.id,
                 'parent_id': self.parent_id,
-                'comment_id': self.comment_id
+                'comment_id': self.comment_id,
+                'topic_id': self.thread.commentable_id,
                 **extra_context,
             },
             notification_type=notification_type,
