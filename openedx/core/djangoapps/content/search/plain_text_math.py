5c5c4102e29b98eef439bca4712ea38aa0890e0c
Navin Karkera <navin@opencraft.com>
2024-12-18 20:43:09 +0530

    feat: show math in plain text in library cards

---
+++ openedx/core/djangoapps/content/search/plain_text_math.py
@@ -0,0 +1,91 @@
+import re
+
+import unicodeit
+
+
+class PlainTextMath:
+    """
+    Converts mathjax equations to plain text using unicodeit and some preprocessing.
+    """
+    equation_pattern = re.compile(
+        r'\[mathjaxinline\](.*?)\[\/mathjaxinline\]|\[mathjax\](.*?)\[\/mathjax\]|\\\((.*?)\\\)|\\\[(.*?)\\\]'
+    )
+    eqn_replacements = (
+        # just remove prefix `\`
+        ("\\sin", "sin"),
+        ("\\cos", "cos"),
+        ("\\tan", "tan"),
+        ("\\arcsin", "arcsin"),
+        ("\\arccos", "arccos"),
+        ("\\arctan", "arctan"),
+        ("\\cot", "cot"),
+        ("\\sec", "sec"),
+        ("\\csc", "csc"),
+        # Is used for matching brackets in mathjax, should be required in plain text.
+        ("\\left", ""),
+        ("\\right", ""),
+    )
+    regex_replacements = (
+        (re.compile(r'\\mathbf{(.*?)}'), r"\1"),
+    )
+
+    def _fraction_handler(self, equation: str) -> str:
+        """
+        Converts `\frac{x}{y}` to `(x/y)` while handling nested `{}`.
+
+        For example: `\frac{2}{\sqrt{1+y}}` is converted to `(2/\sqrt{1+y})`.
+
+        Args:
+            equation: string
+
+        Returns:
+            String with `\frac` replaced by normal `/` symbol.
+        """
+        start_index = equation.find("\\frac{")
+        if start_index == -1:
+            return equation
+        mid_index = equation.find("}{")
+        numerator = equation[start_index + 6:mid_index]
+        open_count = 0
+        for i, char in enumerate(equation[mid_index + 2:]):
+            if char == "{":
+                open_count += 1
+            if char == "}":
+                if open_count == 0:
+                    break
+                open_count -= 1
+        else:
+            # Invalid `\frac` format
+            return equation
+        denominator = equation[mid_index + 2:mid_index + 2 + i]
+        equation = equation[:start_index] + f"({numerator}/{denominator})" + equation[mid_index + 2 + i + 1:]
+        return equation
+
+    def _handle_replacements(self, equation: str) -> str:
+        """
+        Makes a bunch of replacements in equation string.
+        """
+        for q, replacement in self.eqn_replacements:
+            equation = equation.replace(q, replacement)
+        for pattern, replacement in self.regex_replacements:
+            equation = re.sub(pattern, replacement, equation)
+        return equation
+
+    def run(self, eqn_matches: re.Match) -> str:
+        """
+        Takes re.Match object and runs conversion process on each match group.
+        """
+        groups = eqn_matches.groups()
+        for group in groups:
+            if group:
+                group = self._fraction_handler(group)
+                group = self._handle_replacements(group)
+                return unicodeit.replace(group)
+        return None
+
+
+processor = PlainTextMath()
+
+
+def process_mathjax(content: str) -> str:
+    return re.sub(processor.equation_pattern, processor.run, content)
