abb5df50323c3127b4caad67bf7718b02fdbd5f0
Jesper Hodge <jhodge@outlook.de>
2025-01-15 12:20:51 -0500

    test: get view

--- cms/djangoapps/contentstore/rest_api/v0/tests/test_course_optimizer.py
+++ cms/djangoapps/contentstore/rest_api/v0/tests/test_course_optimizer.py
@@ -1,4 +1,5 @@
 from cms.djangoapps.contentstore.tests.test_utils import AuthorizeStaffTestCase
+from common.djangoapps.student.tests.factories import InstructorFactory
 from rest_framework import status
 from django.test import TestCase
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
@@ -13,10 +14,29 @@ class TestCourseOptimizer(AuthorizeStaffTestCase, ModuleStoreTestCase, TestCase)
         pass
 
     def make_request(self, course_id=None, data=None):
-        return self.client.get(self.get_url(course_id), data)
+        url = self.get_url(self.course.id)
+        print('make_request url: ', url)
+        response = self.client.get(url, data)
+        print('make_request response status code: ', response.status_code)
+        print('make_request response content: ', response.content)
+        return response
 
     def get_url(self, course_key):
-        return reverse(
+        url = reverse(
             'cms.djangoapps.contentstore:v0:link_check_status',
-            kwargs={'course_id': 'course-v1:someOrg+someCourse+someRun'}
+            kwargs={'course_id': self.course.id}
         )
+        print('get_url: ', url)
+        return url
+
+    def test_course_instructor(self, expect_status=status.HTTP_200_OK):
+        self.course_instructor = InstructorFactory(
+            username='instructor',
+            password=self.password,
+            course_key=self.course.id,
+        )
+        self.client.login(username=self.course_instructor.username, password=self.password)
+        response = self.make_request()
+        print('test_course_instructor response status code: ', response.status_code)
+        assert response.status_code == expect_status
+        return response
