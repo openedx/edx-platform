175fc8e3df0914c1104b88d6fbaa244211baa984
Bernard Szabo <bszabo@edx.org>
2025-01-15 16:40:29 -0500

    feat: TNL-11812 no iteration

--- cms/djangoapps/contentstore/tests/test_tasks.py
+++ cms/djangoapps/contentstore/tests/test_tasks.py
@@ -304,9 +304,8 @@ class CourseOptimizerTestCase(TestCase):
         course_key = 'course-v1:edX+DemoX+Demo_Course'
         batch_size=2
         results = _validate_urls_access_in_batches(url_list, course_key, batch_size)
-        for result in results:
-            r = asyncio.run(result)
-            print(r)
+        r = asyncio.run(results)
+        print(r)
         assert  15 == 17, "forced fail"
 
     def test_all_links_in_link_list_longer_than_batch_size_are_validated(self):
