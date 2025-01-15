41ff45c1e3fab98dede12069ceefad2f5ccca941
Jesper Hodge <jhodge@outlook.de>
2025-01-15 15:35:59 -0500

    test: add validation tests and document how to test views

--- cms/djangoapps/contentstore/rest_api/v0/tests/test_course_optimizer.py
+++ cms/djangoapps/contentstore/rest_api/v0/tests/test_course_optimizer.py
@@ -1,24 +1,16 @@
 from cms.djangoapps.contentstore.tests.test_utils import AuthorizeStaffTestCase
-from common.djangoapps.student.tests.factories import InstructorFactory
-from rest_framework import status
 from django.test import TestCase
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from django.urls import reverse
 
-class TestCourseOptimizer(AuthorizeStaffTestCase, ModuleStoreTestCase, TestCase):
+class TestGetLinkCheckStatus(AuthorizeStaffTestCase, ModuleStoreTestCase, TestCase):
     '''
-    Tests for CourseOptimizer
+    Authentication and Authorization Tests for CourseOptimizer.
+    For concrete tests that are run, check `AuthorizeStaffTestCase`.
     '''
-    def test_inherited(self):
-        # This method ensures that pytest recognizes this class as containing tests
-        pass
-
-    def make_request(self, course_id=None, data=None):
+    def make_request(self, course_id=None, data=None, **kwargs):
         url = self.get_url(self.course.id)
-        print('make_request url: ', url)
         response = self.client.get(url, data)
-        print('make_request response status code: ', response.status_code)
-        print('make_request response content: ', response.content)
         return response
 
     def get_url(self, course_key):
@@ -26,17 +18,56 @@ class TestCourseOptimizer(AuthorizeStaffTestCase, ModuleStoreTestCase, TestCase)
             'cms.djangoapps.contentstore:v0:link_check_status',
             kwargs={'course_id': self.course.id}
         )
-        print('get_url: ', url)
+        return url
+    
+    def test_produces_4xx_when_invalid_course_id(self):
+        '''
+        Test course_id validation
+        '''
+        response = self.make_request(course_id='invalid_course_id')
+        self.assertIn(response.status_code, range(400, 500))
+    
+    def test_produces_4xx_when_additional_kwargs(self):
+        '''
+        Test additional kwargs validation
+        '''
+        response = self.make_request(course_id=self.course.id, malicious_kwarg='malicious_kwarg')
+        self.assertIn(response.status_code, range(400, 500))
+
+class TestPostLinkCheck(AuthorizeStaffTestCase, ModuleStoreTestCase, TestCase):
+    '''
+    Authentication and Authorization Tests for CourseOptimizer.
+    For concrete tests that are run, check `AuthorizeStaffTestCase`.
+    '''
+    def make_request(self, course_id=None, data=None, **kwargs):
+        url = self.get_url(self.course.id)
+        response = self.client.post(url, data)
+        return response
+
+    def get_url(self, course_key):
+        url = reverse(
+            'cms.djangoapps.contentstore:v0:link_check',
+            kwargs={'course_id': self.course.id}
+        )
         return url
 
-    def test_course_instructor(self, expect_status=status.HTTP_200_OK):
-        self.course_instructor = InstructorFactory(
-            username='instructor',
-            password=self.password,
-            course_key=self.course.id,
-        )
-        self.client.login(username=self.course_instructor.username, password=self.password)
-        response = self.make_request()
-        print('test_course_instructor response status code: ', response.status_code)
-        assert response.status_code == expect_status
-        return response
+    def test_produces_4xx_when_invalid_course_id(self):
+        '''
+        Test course_id validation
+        '''
+        response = self.make_request(course_id='invalid_course_id')
+        self.assertIn(response.status_code, range(400, 500))
+
+    def test_produces_4xx_when_additional_kwargs(self):
+        '''
+        Test additional kwargs validation
+        '''
+        response = self.make_request(course_id=self.course.id, malicious_kwarg='malicious_kwarg')
+        self.assertIn(response.status_code, range(400, 500))
+
+    def test_produces_4xx_when_unexpected_data(self):
+        '''
+        Test validation when request contains unexpected data
+        '''
+        response = self.make_request(course_id=self.course.id, data={'unexpected_data': 'unexpected_data'})
+        self.assertIn(response.status_code, range(400, 500))
