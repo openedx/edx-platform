37d8be61e3d3160fa2742e505bf7e8d26fc9ecce
Jesper Hodge <jhodge@outlook.de>
2025-01-15 15:37:11 -0500

    fix: lint

--- cms/djangoapps/contentstore/rest_api/v0/tests/test_course_optimizer.py
+++ cms/djangoapps/contentstore/rest_api/v0/tests/test_course_optimizer.py
@@ -1,7 +1,8 @@
-from cms.djangoapps.contentstore.tests.test_utils import AuthorizeStaffTestCase
 from django.test import TestCase
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from django.urls import reverse
+
+from cms.djangoapps.contentstore.tests.test_utils import AuthorizeStaffTestCase
 
 class TestGetLinkCheckStatus(AuthorizeStaffTestCase, ModuleStoreTestCase, TestCase):
     '''
@@ -19,14 +20,14 @@ class TestGetLinkCheckStatus(AuthorizeStaffTestCase, ModuleStoreTestCase, TestCa
             kwargs={'course_id': self.course.id}
         )
         return url
-    
+
     def test_produces_4xx_when_invalid_course_id(self):
         '''
         Test course_id validation
         '''
         response = self.make_request(course_id='invalid_course_id')
         self.assertIn(response.status_code, range(400, 500))
-    
+
     def test_produces_4xx_when_additional_kwargs(self):
         '''
         Test additional kwargs validation
