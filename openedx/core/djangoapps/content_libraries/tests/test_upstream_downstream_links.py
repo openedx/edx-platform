06bcbaf21673ed551fbe1888ae49bd716957a9e2
Navin Karkera <navin@opencraft.com>
2025-01-15 19:39:02 +0530

    test: upstream - downstream link tasks

--- openedx/core/djangoapps/content_libraries/tests/test_upstream_downstream_links.py
+++ openedx/core/djangoapps/content_libraries/tests/test_upstream_downstream_links.py
@@ -7,13 +7,16 @@ from unittest.mock import patch
 
 from django.core.management import call_command
 from django.core.management.base import CommandError
+from django.utils import timezone
+from freezegun import freeze_time
 
-from openedx.core.djangolib.testing.utils import CacheIsolationTestCase
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
-from xmodule.modulestore.tests.factories import CourseFactory
+from xmodule.modulestore.tests.factories import BlockFactory, CourseFactory
+
+from ..tasks import create_or_update_upstream_links, create_or_update_xblock_upstream_link
 
 
-class TestRecreateUpstreamLinks(ModuleStoreTestCase, CacheIsolationTestCase):
+class TestRecreateUpstreamLinks(ModuleStoreTestCase):
     """
     Test recreate_upstream_links management command.
     """
@@ -74,3 +77,81 @@ class TestRecreateUpstreamLinks(ModuleStoreTestCase, CacheIsolationTestCase):
         self.call_command('--all')
         mock_task.delay.assert_any_call(str(course_key_1), False)
         mock_task.delay.assert_any_call(str(course_key_2), False)
+
+
+class TestUpstreamLinksTasks(ModuleStoreTestCase):
+    """
+    Test tasks related to managing upstream->downstream links.
+    """
+
+    ENABLED_SIGNALS = ['course_deleted', 'course_published']
+
+    def setUp(self):
+        super().setUp()
+        self.now = timezone.now()
+        freezer = freeze_time(self.now)
+        freezer.start()
+        self.addCleanup(freezer.stop)
+        self.course = course = CourseFactory.create(emit_signals=True)
+        self.course_key = course_key = self.course.id
+        self.upstream_1 = "upstream-block-id-1"
+        self.upstream_2 = "upstream-block-id-2"
+        with self.store.bulk_operations(course_key):
+            self.section = BlockFactory.create(parent=course, category="chapter", display_name="Section")
+            self.sequence = BlockFactory.create(parent=self.section, category="sequential", display_name="Sequence")
+            self.unit = BlockFactory.create(parent=self.sequence, category="vertical", display_name="Unit")
+            self.component_1 = BlockFactory.create(
+                parent=self.unit,
+                category="html",
+                display_name="An HTML Block",
+                upstream=self.upstream_1,
+            )
+            self.component_2 = BlockFactory.create(
+                parent=self.unit,
+                category="html",
+                display_name="Another HTML Block",
+                upstream=self.upstream_2,
+            )
+            self.component_3 = BlockFactory.create(
+                parent=self.unit,
+                category="html",
+                display_name="Another another HTML Block",
+            )
+
+    @patch(
+        'openedx.core.djangoapps.content_libraries.api.create_or_update_xblock_upstream_link'
+    )
+    def test_create_or_update_upstream_links_task(self, mock_api):
+        """
+        Test task create_or_update_upstream_links for a course
+        """
+        create_or_update_upstream_links(str(self.course_key), force=False)
+        expected_calls = [
+            (self.component_1.usage_key, str(self.course_key), self.course.display_name_with_default, self.now),
+            (self.component_2.usage_key, str(self.course_key), self.course.display_name_with_default, self.now),
+        ]
+        assert [(x[0][0].usage_key, x[0][1], x[0][2], x[0][3]) for x in mock_api.call_args_list] == expected_calls
+        mock_api.reset_mock()
+        # call again with same course, it should not be processed again as its CourseLinksStatusChoices = COMPLETED
+        create_or_update_upstream_links(str(self.course_key), force=False)
+        mock_api.assert_not_called()
+        # again with same course but with force=True, it should be processed now
+        create_or_update_upstream_links(str(self.course_key), force=True)
+        assert [(x[0][0].usage_key, x[0][1], x[0][2], x[0][3]) for x in mock_api.call_args_list] == expected_calls
+
+    @patch(
+        'openedx.core.djangoapps.content_libraries.api.create_or_update_xblock_upstream_link'
+    )
+    def test_create_or_update_xblock_upstream_link(self, mock_api):
+        """
+        Test task create_or_update_xblock_upstream_link for a course
+        """
+        create_or_update_xblock_upstream_link(str(self.component_1.usage_key))
+        expected_calls = [
+            (self.component_1.usage_key, str(self.course_key), self.course.display_name_with_default),
+        ]
+        assert [(x[0][0].usage_key, x[0][1], x[0][2]) for x in mock_api.call_args_list] == expected_calls
+        mock_api.reset_mock()
+        # call for xblock with no upstream
+        create_or_update_xblock_upstream_link(str(self.component_3.usage_key))
+        mock_api.assert_not_called()
