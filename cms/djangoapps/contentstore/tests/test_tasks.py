a44b7e60784e0ea207295a8d86235f5d209f213b
Bernard Szabo <bszabo@edx.org>
2025-01-17 07:10:20 -0500

    feat: TNL-11812 splitlines on input text

--- cms/djangoapps/contentstore/tests/test_tasks.py
+++ cms/djangoapps/contentstore/tests/test_tasks.py
@@ -282,8 +282,8 @@ class CourseOptimizerTestCase(TestCase):
         '''
         processed_url_list = _get_urls(url_list)
         pprint.pp(processed_url_list)
-        processed_lines = len(processed_url_list)
-        original_lines = len(url_list)
+        processed_lines = len(processed_url_list)    # regex returns a list
+        original_lines = len(url_list.splitlines())    # use splitline to get a list instead of a bunch of chars
         assert processed_lines == original_lines-2, \
             f'Processed URL list lines = {processed_lines}; expected {original_lines - 2}'
 
