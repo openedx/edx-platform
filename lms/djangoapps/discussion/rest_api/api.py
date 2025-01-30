e3528a7f164f07e619fb44ba6bf23870561dea9a
Usama Sadiq <usama.sadiq@arbisoft.com>
2025-01-30 17:15:33 +0500

    fix: enable pylint warnings (#36195)
    * fix: enable pylint warnings

--- lms/djangoapps/discussion/rest_api/api.py
+++ lms/djangoapps/discussion/rest_api/api.py
@@ -1010,7 +1010,7 @@ def get_thread_list(
         if view in ["unread", "unanswered", "unresponded"]:
             query_params[view] = "true"
         else:
-            ValidationError({
+            raise ValidationError({
                 "view": [f"Invalid value. '{view}' must be 'unread' or 'unanswered'"]
             })
 
