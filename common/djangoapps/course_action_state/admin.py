77f5d7dff29d4ebd103c6693842f8dfb6d995450
Simon Chen <schen@edx.org>
2025-02-04 14:58:32 -0500

    fix: exclude 'course_key' field from the django admin form on course_action_state

--- common/djangoapps/course_action_state/admin.py
+++ common/djangoapps/course_action_state/admin.py
@@ -6,4 +6,16 @@ from django.contrib import admin
 
 from common.djangoapps.course_action_state.models import CourseRerunState
 
-admin.site.register(CourseRerunState)
+
+class CourseRerunStateAdmin(admin.ModelAdmin):
+    """ Django Admin form class for CourseRerunState model """
+    exclude = ["course_key"]
+
+    def get_queryset(self, request):
+        # For any query against this table, remove course_key field
+        # because that field might have bad data in it.
+        qs = CourseRerunState.objects.defer('course_key')
+        return qs
+
+
+admin.site.register(CourseRerunState, CourseRerunStateAdmin)
