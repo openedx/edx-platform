67fdfd526217c9a0c87461e5f428c81d667d672c
jciasenza <jciasenza@aulasneo.com>
2025-02-11 12:07:58 -0300

    fix enzyme testing

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,5 @@
 
 // setupTests.js
-import '@testing-library/jest-dom'; // Importa métodos de aserción personalizados de Jest para interactuar con el DOM
+import '@testing-library/jest-dom';
 
-// Define una función global gettext para internacionalización, si es necesaria
 global.gettext = (text) => text;
