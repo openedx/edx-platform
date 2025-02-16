39ef4d1ff47322cc4c0adc0afbb5e72a25d66cf9
Іван Нєдєльніцев <ivan.niedielnitsev@raccoongang.com>
2025-02-16 18:32:31 +0200

    revert: "refactor: refactor generation triggering"

--- openedx/features/offline_mode/tasks.py
+++ openedx/features/offline_mode/tasks.py
@@ -6,13 +6,11 @@ from edx_django_utils.monitoring import set_code_owner_attribute
 from django.http.response import Http404
 from opaque_keys.edx.keys import CourseKey, UsageKey
 
-from lms.djangoapps.course_blocks.api import get_course_blocks
 from xmodule.modulestore.django import modulestore
 
 from .assets_management import is_modified
 from .constants import MAX_RETRY_ATTEMPTS, OFFLINE_SUPPORTED_XBLOCKS, RETRY_BACKOFF_INITIAL_TIMEOUT
 from .storage_management import OfflineContentGenerator
-from .utils import get_offline_service_user
 
 
 @shared_task
@@ -24,12 +22,12 @@ def generate_offline_content_for_course(course_id):
     Blocks that are closed to responses won't be processed.
     """
     course_key = CourseKey.from_string(course_id)
-    root_block_usage_key = modulestore().make_course_usage_key(course_key)
-    user = get_offline_service_user()
-    blocks = get_course_blocks(user, root_block_usage_key)
-    for block_usage_key in blocks:
-        if block_usage_key.block_type in OFFLINE_SUPPORTED_XBLOCKS:
-            generate_offline_content_for_block.apply_async([str(block_usage_key)])
+
+    for offline_supported_block_type in OFFLINE_SUPPORTED_XBLOCKS:
+        for xblock in modulestore().get_items(course_key, qualifiers={'category': offline_supported_block_type}):
+            is_not_closed = not hasattr(xblock, 'closed') or not xblock.closed()
+            if is_not_closed and is_modified(xblock):
+                generate_offline_content_for_block.apply_async([str(xblock.location)])
 
 
 @shared_task(
