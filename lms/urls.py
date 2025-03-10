b711c5fd5cfce83f3937318e3fc3e823bc5fea11
gabrielC1409 <gcarvajal@aulasneo.com>
2025-03-10 19:45:20 +0000

    feat: Send xqueue submissions to edx-submission
    fix: Restructuring to send course_id and score to edx submission
    
    fix: Refactoring of sending information to sent_to_submission
    
    fix: Elimination of unnecessary functions
    
    fix: Added usage comment to ProblemBlock in XqueueInterface constructor
    
    fix: update doc ADR
    
    fix: setting for Quality Others test (ubuntu-24.04, 3.11, 20)
    
    fix: Deprecation for django-trans-escape-filter-parse-error
    
    test: Add @pytest.mark.django_db decorator to test functions
    
    test: Fix for pylint being disabled
    
    fix: updated changes for pylint disable
    
    fix: update error from docs ADR-0005
    
    update: xmodule/docs/decisions/0005-send-data-to-edx-submission.rst
    
    Co-authored-by: Sarina Canelake <sarina@axim.org>
    
    update: xmodule/docs/decisions/0005-send-data-to-edx-submission.rst
    
    Co-authored-by: Sarina Canelake <sarina@axim.org>
    
    fix: Adjusted correction
    
    fix: update date for docs ADR
    
    Revert "fix: update date for docs ADR"
    
    This reverts commit 0b4229c51c4937f95cb407872645dd448df45418.
    
    fix: replace call created_submission to create_external_grader_detail
    
    fix: update test xqueue_submission
    
    fix: add docstring in test_xqueue_submission
    
    fix: update date doc ADR
    
    fix: update version edx-submission 3.8.6
    
    fix: add @pytest.mark.xfail
    
    fix: add 20 chances in test_capa_block:
    
    fix: increase retry attempts for seed generation in ProblemBlockTest
    
    fix: change version to edx-submission lib
    
    fix: new version edx-submission in testings
    
    fix: replace parameter file to files
    
    fix: update variable grader_file_name and points_possible
    
    fix: Adjustment in the is_flag_active function to always take the last record edited in the waffle
    
    fix: wrap large line of code
    
    fix: update function is_flag_active
    
    fix: code style adjustment
    
    fix: changes for 60 retry

--- lms/urls.py
+++ lms/urls.py
@@ -348,6 +348,14 @@ urlpatterns += [
         name='xqueue_callback',
     ),
 
+    re_path(
+        r'^courses/{}/xqueue/(?P<userid>[^/]*)/(?P<mod_id>.*?)/(?P<dispatch>[^/]*)$'.format(
+            settings.COURSE_ID_PATTERN,
+        ),
+        xqueue_callback,
+        name='callback_submission',
+    ),
+
     # TODO: These views need to be updated before they work
     path('calculate', util_views.calculate),
 
