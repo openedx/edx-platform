88ba74f5603336dfa0e053c33cb88311356b4603
Braden MacDonald <braden@opencraft.com>
2025-03-12 16:49:29 -0700

    feat: use new version of openedx-learning with containers support

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1876,6 +1876,7 @@ INSTALLED_APPS = [
     "openedx_learning.apps.authoring.components",
     "openedx_learning.apps.authoring.contents",
     "openedx_learning.apps.authoring.publishing",
+    "openedx_learning.apps.authoring.units",
 ]
 
 
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3373,6 +3373,7 @@ INSTALLED_APPS = [
     "openedx_learning.apps.authoring.components",
     "openedx_learning.apps.authoring.contents",
     "openedx_learning.apps.authoring.publishing",
+    "openedx_learning.apps.authoring.units",
 ]
 
 
