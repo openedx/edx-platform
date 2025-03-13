bece5922babfff6bb2de2d43797bcb15e6a1f22a
jciasenza <jciasenza@aulasneo.com>
2025-03-13 12:04:31 -0300

    fix: updated existing enzyme tests to use react testing library

--- setupTests.js
+++ setupTests.js
@@ -1,8 +1,4 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import {configure} from 'enzyme';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({adapter: new Adapter()});
+// setupTests.js
+import '@testing-library/jest-dom';
 
 global.gettext = (text) => text;
