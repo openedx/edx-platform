2103a9e60697bf276f5ec8836e67d94762554323
Robert Raposa <rraposa@edx.org>
2025-03-14 02:09:16 -0400

    style: remove eslint with frontend code removal ADR
    - Add ADR for frontend code removal
    - Drop eslint, as explained in the ADR

--- scripts/all-tests.sh
+++ scripts/all-tests.sh
@@ -11,8 +11,6 @@ set -e
 ###############################################################################
 
 # Violations thresholds for failing the build
-source scripts/thresholds.sh
-
 XSSLINT_THRESHOLDS=$(cat scripts/xsslint_thresholds.json)
 export XSSLINT_THRESHOLDS=${XSSLINT_THRESHOLDS//[[:space:]]/}
 
