34538dc089a3660e76664ae78455cabdb4746561
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2025-03-25 01:23:34 +0200

    feat: [AXM-1775] add first component block id field (#2613)

--- lms/djangoapps/mobile_api/course_dates/serializers.py
+++ lms/djangoapps/mobile_api/course_dates/serializers.py
@@ -2,8 +2,11 @@
 Serializers for course dates.
 """
 
+from crum import get_current_user
 from rest_framework import serializers
 
+from lms.djangoapps.course_blocks.api import get_course_blocks
+from lms.djangoapps.courseware.courses import get_first_component_of_block
 from xmodule.modulestore.django import modulestore
 
 
@@ -13,12 +16,13 @@ class ContentDateSerializer(serializers.Serializer):
     """
 
     course_id = serializers.CharField()
-    assignment_block_id = serializers.CharField(source="location")
+    location = serializers.CharField()
     due_date = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S%z")
     assignment_title = serializers.SerializerMethodField()
     learner_has_access = serializers.SerializerMethodField()
     course_name = serializers.CharField()
     relative = serializers.BooleanField()
+    first_component_block_id = serializers.SerializerMethodField()
 
     def get_assignment_title(self, obj):
         return modulestore().get_item(obj.location).display_name
@@ -26,3 +30,13 @@ class ContentDateSerializer(serializers.Serializer):
     def get_learner_has_access(self, obj):
         block = modulestore().get_item(obj.location)
         return not getattr(block, "contains_gated_content", False)
+
+    def get_first_component_block_id(self, obj):
+        user = get_current_user()
+        block_data = get_course_blocks(user, obj.location, allow_start_dates_in_future=True, include_completion=True)
+        first_block_id = get_first_component_of_block(obj.location, block_data)
+
+        if first_block_id == str(obj.location):
+            return None
+
+        return first_block_id
