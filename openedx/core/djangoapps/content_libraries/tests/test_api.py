f16abd9ab2784bfa2b41ac61dcf18b1866627d52
Jillian Vogel <jill@opencraft.com>
2025-04-03 18:33:41 +1030

    fix: container tests not sending signals

--- openedx/core/djangoapps/content_libraries/tests/test_api.py
+++ openedx/core/djangoapps/content_libraries/tests/test_api.py
@@ -754,6 +754,18 @@ class ContentLibraryContainersTest(ContentLibrariesRestApiTest, OpenEdxEventsTes
         LIBRARY_CONTAINER_UPDATED.event_type,
     ]
 
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up class method for the Test class.
+
+        TODO: It's unclear why we need to call start_events_isolation ourselves rather than relying on
+              OpenEdxEventsTestMixin.setUpClass to handle it. It fails it we don't, and many other test cases do it,
+              so we're following a pattern here. But that pattern doesn't really make sense.
+        """
+        super().setUpClass()
+        cls.start_events_isolation()
+
     def setUp(self):
         super().setUp()
 
