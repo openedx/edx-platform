6b703b488cdeb1f9e556da7f07fbf27c62bafb16
andrii-hantkovskyi <131773947+andrii-hantkovskyi@users.noreply.github.com>
2025-04-10 15:03:33 +0300

    fix: [AXM-1877] staged content not creating (#2632)
    Co-authored-by: Andrii <andrii.hantkovskyi@raccoongang.com>

--- cms/djangoapps/import_from_modulestore/api.py
+++ cms/djangoapps/import_from_modulestore/api.py
@@ -1,6 +1,8 @@
 """
 API for course to library import.
 """
+from functools import partial
+
 from django.db import transaction
 
 from .models import Import as _Import
@@ -32,15 +34,13 @@ def create_import(source_key, user_id: int, learning_package_id: int) -> _Import
     """
     Create a new import task to import a course to a library.
     """
-    import_from_modulestore = _Import.objects.create(
-        source_key=source_key,
-        target_id=learning_package_id,
-        user_id=user_id,
-    )
-    transaction.on_commit(
-        lambda: save_legacy_content_to_staged_content_task.apply_async(
-            kwargs={'import_uuid': import_from_modulestore.uuid}
+    with transaction.atomic():
+        import_from_modulestore = _Import.objects.create(
+            source_key=source_key,
+            target_id=learning_package_id,
+            user_id=user_id,
         )
-    )
-
-    return import_from_modulestore
+        transaction.on_commit(
+            partial(save_legacy_content_to_staged_content_task.delay, import_uuid=str(import_from_modulestore.uuid))
+        )
+        return import_from_modulestore
