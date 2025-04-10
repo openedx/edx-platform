78cbf275eec7a0347ae7119843524eb362fd4ce1
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-04-10 22:30:21 +0500

    fix: updated name of waffle flag

--- cms/djangoapps/contentstore/rest_api/v1/views/course_waffle_flags.py
+++ cms/djangoapps/contentstore/rest_api/v1/views/course_waffle_flags.py
@@ -62,7 +62,7 @@ class CourseWaffleFlagsView(APIView):
             "use_new_certificates_page": true,
             "use_new_textbooks_page": true,
             "use_new_group_configurations_page": true
-            "use_markdown_editor": true,
+            "react_markdown_editor": true,
         }
         ```
         """
