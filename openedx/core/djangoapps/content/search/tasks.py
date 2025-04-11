c4329eed4d25b68bacafeb542c39b0b5cb1a326c
Jillian Vogel <jill@opencraft.com>
2025-04-11 11:41:56 +0930

    fix: update_library_container_index_doc task takes a string

--- openedx/core/djangoapps/content/search/tasks.py
+++ openedx/core/djangoapps/content/search/tasks.py
@@ -121,10 +121,11 @@ def update_library_components_collections(collection_key_str: str) -> None:
 
 @shared_task(base=LoggedTask, autoretry_for=(MeilisearchError, ConnectionError))
 @set_code_owner_attribute
-def update_library_container_index_doc(container_key: LibraryContainerLocator) -> None:
+def update_library_container_index_doc(container_key_str: str) -> None:
     """
     Celery task to update the content index document for a library container
     """
+    container_key = LibraryContainerLocator.from_string(container_key_str)
     library_key = container_key.library_key
 
     log.info("Updating content index documents for container %s in library%s", container_key, library_key)
