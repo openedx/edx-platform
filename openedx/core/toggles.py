5a7eb937e924d754c8a4f3036447b554344771a7
Edward Zarecor <ed@tcril.org>
2025-04-24 19:20:14 -0400

    feat: adding WaffleFlag and check method

--- openedx/core/toggles.py
+++ openedx/core/toggles.py
@@ -15,3 +15,24 @@ from edx_toggles.toggles import SettingDictToggle
 ENTRANCE_EXAMS = SettingDictToggle(
     "FEATURES", "ENTRANCE_EXAMS", default=False, module_name=__name__
 )
+
+# .. toggle_name: entrance_exams
+# .. toggle_implementation: WaffleFlag
+# .. toggle_default: False
+# .. toggle_description: Enable entrance exams feature. When enabled, students
+# .. toggle_use_cases: open_edx
+# .. toggle_creation_date: 2015-12-01
+# .. toggle_migration_date: 2025-04-24
+# .. toggle_tickets: https://openedx.atlassian.net/browse/SOL-40
+# .. toggle_warnings: This replaces the FEATURES['ENTRANCE_EXAMS'] setting
+ENTRANCE_EXAMS_FLAG = WaffleFlag('core.entrance_exams', module_name=__name__)
+
+
+def are_entrance_exams_enabled():
+    """
+    Returns whether entrance exams are enabled.
+
+    During the transition period, this checks both the old setting and the new
+    Once the transition is complete, the old setting check can be removed.
+    """
+    return ENTRANCE_EXAMS.is_enabled() or ENTRANCE_EXAMS_FLAG.is_enabled()
