5192c8d6b207cf41dd9bb42a483e8d9813de057c
Jawad Khan <jawadkhan444@gmail.com>
2025-04-24 23:08:37 +0500

    fix: Moved braze logic to edx-ace

--- openedx/core/djangoapps/notifications/migrations/0007_notification_push.py
+++ openedx/core/djangoapps/notifications/migrations/0007_notification_push.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('notifications', '0007_notificationbrazecampaigns'),
+        ('notifications', '0006_notification_group_by_id'),
     ]
 
     operations = [
---
+++ openedx/core/djangoapps/notifications/push/tasks.py
@@ -0,0 +1,45 @@
+""" Tasks for sending notification to ace push channel """
+from celery.utils.log import get_task_logger
+from django.contrib.auth import get_user_model
+from edx_ace import ace
+from edx_ace.channel import ChannelType
+from edx_ace.recipient import Recipient
+
+from .message_type import PushNotificationMessageType
+
+User = get_user_model()
+logger = get_task_logger(__name__)
+
+
+def send_ace_msg_to_braze_push_channel(audience_ids, notification_object, sender_id):
+    """
+    Send mobile notifications using ace braze_push channel.
+    """
+    if not audience_ids:
+        return
+
+    if notification_object.app_name != 'discussion':
+        return
+
+    notification_type = notification_object.notification_type
+
+    post_data = {
+        'notification_type': notification_type,
+        'course_id': str(notification_object.course_id),
+        'content_url': notification_object.content_url,
+        **notification_object.content_context
+    }
+    emails = list(User.objects.filter(id__in=audience_ids).values_list('email', flat=True))
+    context = {'post_data': post_data}
+
+    sender = User.objects.get(id=sender_id)
+    recipient = Recipient(sender.id, sender.email)
+
+    message = PushNotificationMessageType(
+        app_label="notifications", name="braze_push"
+    ).personalize(recipient, 'en', context)
+    message.options['emails'] = emails
+    message.options['braze_campaign'] = notification_type
+
+    ace.send(message, limit_to_channels=[ChannelType.BRAZE_PUSH])
+    logger.info('Sent mobile notification for %s to ace channel', notification_type)
