b71f11acca533eec2ebbbf1562f09bbc69967f1d
Braden MacDonald <braden@opencraft.com>
2025-04-23 20:52:23 -0700

    test: continue building out integration tests

--- cms/djangoapps/contentstore/rest_api/v2/views/tests/test_downstream_sync_integration.py
+++ cms/djangoapps/contentstore/rest_api/v2/views/tests/test_downstream_sync_integration.py
@@ -2,6 +2,7 @@
 Unit and integration tests to ensure that syncing content from libraries to
 courses is working.
 """
+from typing import Any
 from xml.etree import ElementTree
 
 import ddt
@@ -77,6 +78,12 @@ class CourseToLibraryTestCase(ContentLibrariesRestApiTest, ModuleStoreTestCase):
             "parent_locator": parent_usage_key,
             "library_content_key": upstream_key,
         }, expect_response=expect_response)
+    
+    def _update_course_block_fields(self, usage_key: str, fields: dict[str, Any] = None):
+        """ Update fields of an XBlock """
+        return self._api('patch', f"/xblock/{usage_key}", {
+            "metadata": fields,
+        }, expect_response=200)
 
     def assertXmlEqual(self, xml_str_a: str, xml_str_b: str) -> bool:
         """ Assert that the given XML strings are equal, ignoring attribute order and some whitespace variations. """
@@ -102,7 +109,7 @@ class CourseToLibraryTestCase(ContentLibrariesRestApiTest, ModuleStoreTestCase):
         """
         Test that we can sync a problem from a library into a course.
         """
-        # First, create the problem in the course, using the upstream problem as a template:
+        # 1️⃣ First, create the problem in the course, using the upstream problem as a template:
         downstream_problem1 = self._create_block_from_upstream(
             block_category="problem",
             parent_usage_key=str(self.course_subsection.usage_key),
@@ -121,10 +128,11 @@ class CourseToLibraryTestCase(ContentLibrariesRestApiTest, ModuleStoreTestCase):
         assert status["upstream_link"].startswith("http://course-authoring-mfe/library/")
         assert status["upstream_link"].endswith(f"/components?usageKey={self.upstream_problem1['id']}")
 
-        downstream_olx = self._get_course_block_olx(downstream_problem1["locator"])
-        # Notice that per UpstreamSyncMixin.get_customizable_fields(), some fields like
-        # weight and max_attempts are DROPPED entirely from the upstream version when creating the downstream:
-        self.assertXmlEqual(downstream_olx, f"""
+        # Check the OLX of the downstream block. Notice that:
+        # (1) fields display_name and markdown, as well as the 'data' (content/body of the <problem>) are synced.
+        # (2) per UpstreamSyncMixin.get_customizable_fields(), some fields like weight and max_attempts are
+        #     DROPPED entirely from the upstream version when creating the downstream:
+        self.assertXmlEqual(self._get_course_block_olx(downstream_problem1["locator"]), f"""
             <problem
                 display_name="Problem 1 Display Name"
                 markdown="MD 1"
@@ -133,4 +141,63 @@ class CourseToLibraryTestCase(ContentLibrariesRestApiTest, ModuleStoreTestCase):
                 upstream_version="2"
                 {self.standard_capa_attributes}
             >multiple choice...</problem>
+        """)
+
+        # 2️⃣ Now, lets modify the upstream problem AND the downstream problem:
+
+        self._update_course_block_fields(downstream_problem1["locator"], {
+            "display_name": "Custom Display Name",
+            "max_attempts": 3,
+            "markdown": "blow me away, scotty!",  # This change will be lost
+        })
+
+        self._set_library_block_olx(
+            self.upstream_problem1["id"],
+            '<problem display_name="Problem 1 NEW name" markdown="updated">multiple choice v2...</problem>'
+        )
+        self._publish_library_block(self.upstream_problem1["id"])
+
+        # Here's how the downstream OLX looks now, before we sync:
+        self.assertXmlEqual(self._get_course_block_olx(downstream_problem1["locator"]), f"""
+            <problem
+                display_name="Custom Display Name"
+                markdown="blow me away, scotty!"
+                max_attempts="3"
+                upstream="{self.upstream_problem1['id']}"
+                upstream_display_name="Problem 1 Display Name"
+                upstream_version="2"
+                {self.standard_capa_attributes}
+            >multiple choice...</problem>
+        """)
+
+        status = self._get_sync_status(downstream_problem1["locator"])
+        self.assertDictContainsEntries(status, {
+            'upstream_ref': self.upstream_problem1["id"],  # e.g. 'lb:CL-TEST:testlib:problem:prob1'
+            'version_available': 3,  # <--- updated
+            'version_synced': 2,
+            'version_declined': None,
+            'ready_to_sync': True,  # <--- updated
+            'error_message': None,
+        })
+
+        # 3️⃣ Now, sync and check the resulting OLX of the downstream
+
+        self._sync_downstream(downstream_problem1["locator"])
+
+        # Here's how the downstream OLX looks now, after we synced it.
+        # Notice:
+        #   (1) content like "markdown" and the body XML content are synced
+        #   (2) the "display_name" is left alone (customized downstream), but
+        #   (3) "upstream_display_name" is updated.
+        #   (4) The customized "max_attempts" is also still present.
+        self.assertXmlEqual(self._get_course_block_olx(downstream_problem1["locator"]), f"""
+            <problem
+                display_name="Custom Display Name"
+                markdown="updated"
+                max_attempts="3"
+                upstream="{self.upstream_problem1['id']}"
+                upstream_display_name="Problem 1 NEW name"
+                upstream_version="3"
+                {self.standard_capa_attributes}
+            >multiple choice v2...</problem>
         """)
