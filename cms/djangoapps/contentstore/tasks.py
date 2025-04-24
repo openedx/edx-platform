a609134e69b9794e6e128271f94bb9f15d99b807
Navin Karkera <navin@opencraft.com>
2025-04-24 18:41:47 +0000

    feat: support for syncing units from libraries to courses (#36553)
    * feat: library unit sync
    * feat: create component link only for component xblocks
    * feat: container link model
    * feat: update downstream api views
    * feat: delete extra components in container on sync (not working)
    * fix: duplicate definitions of LibraryXBlockMetadata
    * test: add a new integration test suite for syncing
    * feat: partially implement container+child syncing
    * fix: blockserializer wasn't always serializing all HTML block fields
    * feat: handle reorder, addition and deletion of components in sync
    
    Updates children components of unit in course based on upstream unit,
    deletes removed component, adds new ones and updates order as per
    upstream.
    
    * feat: return unit upstreamInfo and disallow edits to units in courses that are sourced from a library (#773)
    * feat: Add upstream_info to unit
    * feat: disallow edits to units in courses that are sourced from a library (#774)
    
    ---------
    
    Co-authored-by: Jillian Vogel <jill@opencraft.com>
    Co-authored-by: Rômulo Penido <romulo.penido@gmail.com>
    
    * docs: capitalization of XBlock
    
    Co-authored-by: David Ormsbee <dave@axim.org>
    
    * refactor: (minor) change python property name to reflect type better
    
    * fix: lots of "Tried to inspect a missing...upstream link" warnings
    
    when viewing a unit in Studio
    
    * docs: mention potential REST API for future refactor
    
    * fix: check if upstream actually exists before making unit read-only
    
    * chore: fix camel-case var
    
    * fix: test failure when mocked XBlock doesn't have UpstreamSyncMixin
    
    ---------
    
    Co-authored-by: Braden MacDonald <braden@opencraft.com>
    Co-authored-by: Chris Chávez <xnpiochv@gmail.com>
    Co-authored-by: Jillian Vogel <jill@opencraft.com>
    Co-authored-by: Rômulo Penido <romulo.penido@gmail.com>
    Co-authored-by: Braden MacDonald <mail@bradenm.com>
    Co-authored-by: David Ormsbee <dave@axim.org>

--- cms/djangoapps/contentstore/tasks.py
+++ cms/djangoapps/contentstore/tasks.py
@@ -84,7 +84,7 @@ from xmodule.modulestore.exceptions import DuplicateCourseError, InvalidProctori
 from xmodule.modulestore.xml_exporter import export_course_to_xml, export_library_to_xml
 from xmodule.modulestore.xml_importer import CourseImportException, import_course_from_xml, import_library_from_xml
 
-from .models import LearningContextLinksStatus, LearningContextLinksStatusChoices, PublishableEntityLink
+from .models import ContainerLink, LearningContextLinksStatus, LearningContextLinksStatusChoices, ComponentLink
 from .outlines import update_outline_from_modulestore
 from .outlines_regenerate import CourseOutlineRegenerate
 from .toggles import bypass_olx_failure_enabled
@@ -1475,7 +1475,8 @@ def create_or_update_upstream_links(
         updated=created,
     )
     if replace:
-        PublishableEntityLink.objects.filter(downstream_context_key=course_key).delete()
+        ComponentLink.objects.filter(downstream_context_key=course_key).delete()
+        ContainerLink.objects.filter(downstream_context_key=course_key).delete()
     try:
         xblocks = store.get_items(course_key, settings={"upstream": lambda x: x is not None})
     except ItemNotFoundError:
@@ -1483,7 +1484,7 @@ def create_or_update_upstream_links(
         course_status.update_status(LearningContextLinksStatusChoices.FAILED)
         return
     for xblock in xblocks:
-        create_or_update_xblock_upstream_link(xblock, course_key_str, created)
+        create_or_update_xblock_upstream_link(xblock, course_key, created)
     course_status.update_status(LearningContextLinksStatusChoices.COMPLETED)
 
 
@@ -1501,7 +1502,11 @@ def handle_unlink_upstream_block(upstream_usage_key_string: str) -> None:
         LOGGER.exception(f'Invalid upstream usage_key: {upstream_usage_key_string}')
         return
 
-    for link in PublishableEntityLink.objects.filter(
+    for link in ComponentLink.objects.filter(
+        upstream_usage_key=upstream_usage_key,
+    ):
+        make_copied_tags_editable(str(link.downstream_usage_key))
+    for link in ContainerLink.objects.filter(
         upstream_usage_key=upstream_usage_key,
     ):
         make_copied_tags_editable(str(link.downstream_usage_key))
