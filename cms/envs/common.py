dc40444df0f47727f4e68bbee801768af31655e2
awais qureshi <awais.qureshi@arbisoft.com>
2025-04-30 16:45:21 +0500

    fix: adding.

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -2892,3 +2892,13 @@ LIBRARY_ENABLED_BLOCKS = [
     'survey',
     'word_cloud',
 ]
+
+
+OPEN_EDX_FILTERS_CONFIG = {
+    "org.edly.templates.fetch.requested.v1": {
+        "pipeline": [
+            "course_import.pipeline.GithubTemplatesPipeline",
+        ],
+        "fail_silently": False,
+    },
+}
