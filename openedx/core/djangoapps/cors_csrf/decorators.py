4aad9178ddd5a9795c0bffacbe14d63e36f680cb
Daniel Wong <danieleduardo.wongfa@wgu.edu>
2025-05-07 12:01:34 -0600

    feat!: Set CSRF cookie only if successful_authenticator is an instance of SessionAuthentication

--- openedx/core/djangoapps/cors_csrf/decorators.py
+++ openedx/core/djangoapps/cors_csrf/decorators.py
@@ -2,17 +2,16 @@
 
 
 from django.views.decorators.csrf import ensure_csrf_cookie
-from edx_rest_framework_extensions.auth.session.authentication import (
-    SessionAuthenticationAllowInactiveUser
-)  # lint-amnesty, pylint: disable=wrong-import-order
+from rest_framework.authentication import SessionAuthentication
 
 
 def ensure_csrf_cookie_cross_domain(func):
     """View decorator for sending a cross-domain CSRF cookie.
 
     This works like Django's `@ensure_csrf_cookie`, but
-    additionally checks request.successful_authenticator for cases where
-    multiple authentication classes are involved in a view.
+    additionally checks request.successful_authenticator to determine if
+    it is derived from rest_framework.authentication.SessionAuthentication
+    If so, the CSRF cookie is added.
 
     Arguments:
         func (function): The view function to decorate.
@@ -21,10 +20,12 @@ def ensure_csrf_cookie_cross_domain(func):
     def _inner(*args, **kwargs):  # pylint: disable=missing-docstring
         if args:
             request = args[0]
+            current_authenticator = getattr(request, 'successful_authenticator', None)
 
-            # if the successful_authenticator is an instance of SessionAuthenticationAllowInactiveUser
+            # if the current_authenticator is an instance or derived from
+            # rest_framework.authentication.SessionAuthentication
             # it's safe to ensure the CSRF cookie is included.
-            if isinstance(request.successful_authenticator, SessionAuthenticationAllowInactiveUser):
+            if current_authenticator and isinstance(current_authenticator, SessionAuthentication):
                 return ensure_csrf_cookie(func)(*args, **kwargs)
         return func(*args, **kwargs)
     return _inner
