f50565c730b68c1ac579807db1a4485f9dd61b77
Rômulo Penido <romulo.penido@gmail.com>
2025-05-09 14:04:39 -0300

    feat: add last_published_at to upstream summary endpoint [FC-0083] (#36656)
    Returns the last_published_at date from the upstream summary endpoint.

--- cms/djangoapps/contentstore/models.py
+++ cms/djangoapps/contentstore/models.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 
 from config_models.models import ConfigurationModel
 from django.db import models
-from django.db.models import Count, F, Q, QuerySet
+from django.db.models import Count, F, Q, QuerySet, Max
 from django.db.models.fields import IntegerField, TextField
 from django.db.models.functions import Coalesce
 from django.db.models.lookups import GreaterThan
@@ -160,7 +160,8 @@ class ComponentLink(EntityLinkBase):
         ready_to_sync = link_filter.pop('ready_to_sync', None)
         result = cls.objects.filter(**link_filter).select_related(
             "upstream_block__publishable_entity__published__version",
-            "upstream_block__publishable_entity__learning_package"
+            "upstream_block__publishable_entity__learning_package",
+            "upstream_block__publishable_entity__published__publish_log_record__publish_log",
         ).annotate(
             ready_to_sync=(
                 GreaterThan(
@@ -186,13 +187,15 @@ class ComponentLink(EntityLinkBase):
                 "upstream_context_title": "CS problems 3",
                 "upstream_context_key": "lib:OpenedX:CSPROB3",
                 "ready_to_sync_count": 11,
-                "total_count": 14
+                "total_count": 14,
+                "last_published_at": "2025-05-02T20:20:44.989042Z"
             },
             {
                 "upstream_context_title": "CS problems 2",
                 "upstream_context_key": "lib:OpenedX:CSPROB2",
                 "ready_to_sync_count": 15,
-                "total_count": 24
+                "total_count": 24,
+                "last_published_at": "2025-05-03T21:20:44.989042Z"
             },
         ]
         """
@@ -201,7 +204,10 @@ class ComponentLink(EntityLinkBase):
             upstream_context_title=F("upstream_block__publishable_entity__learning_package__title"),
         ).annotate(
             ready_to_sync_count=Count("id", Q(ready_to_sync=True)),
-            total_count=Count('id')
+            total_count=Count("id"),
+            last_published_at=Max(
+                "upstream_block__publishable_entity__published__publish_log_record__publish_log__published_at"
+            )
         )
         return result
 
@@ -307,6 +313,7 @@ class ContainerLink(EntityLinkBase):
         result = cls.objects.filter(**link_filter).select_related(
             "upstream_container__publishable_entity__published__version",
             "upstream_container__publishable_entity__learning_package"
+            "upstream_container__publishable_entity__published__publish_log_record__publish_log",
         ).annotate(
             ready_to_sync=(
                 GreaterThan(
@@ -332,13 +339,15 @@ class ContainerLink(EntityLinkBase):
                 "upstream_context_title": "CS problems 3",
                 "upstream_context_key": "lib:OpenedX:CSPROB3",
                 "ready_to_sync_count": 11,
-                "total_count": 14
+                "total_count": 14,
+                "last_published_at": "2025-05-02T20:20:44.989042Z"
             },
             {
                 "upstream_context_title": "CS problems 2",
                 "upstream_context_key": "lib:OpenedX:CSPROB2",
                 "ready_to_sync_count": 15,
-                "total_count": 24
+                "total_count": 24,
+                "last_published_at": "2025-05-03T21:20:44.989042Z"
             },
         ]
         """
@@ -347,7 +356,10 @@ class ContainerLink(EntityLinkBase):
             upstream_context_title=F("upstream_container__publishable_entity__learning_package__title"),
         ).annotate(
             ready_to_sync_count=Count("id", Q(ready_to_sync=True)),
-            total_count=Count('id')
+            total_count=Count('id'),
+            last_published_at=Max(
+                "upstream_container__publishable_entity__published__publish_log_record__publish_log__published_at"
+            )
         )
         return result
 
