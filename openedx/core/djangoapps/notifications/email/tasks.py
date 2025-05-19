408ad490d0d2d8dbb71ede30474e5c8ea3665914
muhammadadeeltajamul <muhammadadeeltajamul@hotmail.com>
2025-05-19 12:52:54 +0500

    feat: added immediate email notifications

--- openedx/core/djangoapps/notifications/email/tasks.py
+++ openedx/core/djangoapps/notifications/email/tasks.py
@@ -1,6 +1,7 @@
 """
 Celery tasks for sending email notifications
 """
+from bs4 import BeautifulSoup
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.contrib.auth import get_user_model
@@ -20,8 +21,11 @@ from .utils import (
     add_headers_to_email_message,
     create_app_notifications_dict,
     create_email_digest_context,
+    create_email_template_context,
     filter_notification_with_email_enabled_preferences,
+    get_course_info,
     get_start_end_date,
+    get_text_for_notification_type,
     get_unique_course_ids,
     is_email_notification_flag_enabled
 )
@@ -105,6 +109,7 @@ def send_digest_email_to_user(user, cadence_type, start_date, end_date, course_l
         app_label="notifications", name="email_digest"
     ).personalize(recipient, course_language, message_context)
     message = add_headers_to_email_message(message, message_context)
+    message.options['skip_disable_user_policy'] = True
     ace.send(message)
     send_user_email_digest_sent_event(user, cadence_type, notifications, message_context)
     logger.info(f'<Email Cadence> Email sent to {user.username} ==Temp Log==')
@@ -123,3 +128,46 @@ def send_digest_email_to_all_users(cadence_type):
     logger.info(f'<Email Cadence> Email Cadence Audience {len(users)}')
     for user in users:
         send_digest_email_to_user(user, cadence_type, start_date, end_date, courses_data=courses_data)
+
+
+def send_immediate_cadence_email(email_notification_mapping, course_key):
+    """
+    Send immediate cadence email to users
+    Parameters:
+        email_notification_mapping: Dictionary of user_id and Notification object
+        course_key: Course key for which the email is sent
+    """
+    if not email_notification_mapping:
+        return
+    user_list = email_notification_mapping.keys()
+    users = User.objects.filter(id__in=user_list)
+    course_name = get_course_info(course_key).get("name", course_key)
+    for user in users.iterator(chunk_size=100):
+        if not user.has_usable_password():
+            logger.info(f'<Immediate Email> User is disabled {user.username}')
+            continue
+        if not is_email_notification_flag_enabled(user):
+            logger.info(f'<Immediate Email> Flag disabled for {user.username}')
+            continue
+        notification = email_notification_mapping.get(user.id, None)
+        if not notification:
+            logger.info(f'<Immediate Email> No notification for {user.username}')
+            continue
+        soup = BeautifulSoup(notification.content, "html.parser")
+        title = "New Course Update" if notification.notification_type == "course_updates" else soup.get_text()
+        message_context = create_email_template_context(user.username)
+        message_context.update({
+            "course_id": course_key,
+            "course_name": course_name,
+            "content_url": notification.content_url,
+            "content_title": title,
+            "footer_email_reason": "You are receiving this email because you are enrolled in the edX course "
+                                   f"{course_name}",
+            "content": notification.content_context.get("email_content", notification.content),
+            "view_text": get_text_for_notification_type(notification.notification_type),
+        })
+        message = EmailNotificationMessageType(
+            app_label="notifications", name="immediate_email"
+        ).personalize(Recipient(user.id, user.email), 'en', message_context)
+        message = add_headers_to_email_message(message, message_context)
+        ace.send(message)
