82ca9b787453a1326d0c0e0594b17a7fa074660a
M Umar Khan <umar.khan@A006-01609.local>
2025-05-20 11:30:19 +0500

    chore: remove csrf with scheme settings from edxapp

--- cms/envs/production.py
+++ cms/envs/production.py
@@ -223,8 +223,7 @@ if 'staticfiles' in CACHES:
 # Once we have migrated to service assets off S3, then we can convert this back to
 # managed by the yaml file contents
 STATICFILES_STORAGE = os.environ.get('STATICFILES_STORAGE', STATICFILES_STORAGE)
-
-CSRF_TRUSTED_ORIGINS = _YAML_TOKENS.get("CSRF_TRUSTED_ORIGINS_WITH_SCHEME", [])
+CSRF_TRUSTED_ORIGINS = _YAML_TOKENS.get("CSRF_TRUSTED_ORIGINS", [])
 
 MKTG_URL_LINK_MAP.update(_YAML_TOKENS.get('MKTG_URL_LINK_MAP', {}))
 
