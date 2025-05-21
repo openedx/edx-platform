2c4021132396f0282868d6d4593aa86d7c99be92
Daniel Wong <danieleduardo.wongfa@wgu.edu>
2025-05-21 13:38:30 -0600

    fix: resolve feedback comments

--- common/djangoapps/util/storage.py
+++ common/djangoapps/util/storage.py
@@ -1,8 +1,7 @@
 """ Utility functions related to django storages """
 
-import django
 from django.conf import settings
-from django.core.files.storage import default_storage
+from django.core.files.storage import default_storage, storages
 from django.utils.module_loading import import_string
 
 
@@ -30,30 +29,23 @@ def resolve_storage_backend(storage_key, options=None):
     if storage_key == "default":
         # Use case 1: Default storage
         # Works consistently across Django 4.2 and 5.x
-        # In Django 4.2, default_storage uses DEFAULT_FILE_STORAGE
-        # In Django 5.x, it uses STORAGES['default']
+        # In Django 4.2 and above, `default_storage` uses
+        # either `DEFAULT_FILE_STORAGE` or `STORAGES['default']`.
         return default_storage
 
-    if django.VERSION >= (5, 0) and storage_key in storages_config:
-        # Use case 2: Django 5+ with STORAGES defined
+    if storage_key in storages_config:
+        # Use case 2: STORAGES is defined
+        # If STORAGES is present, we retrieve it through the storages API
         # settings.py must define STORAGES like:
         # STORAGES = {
         #     "default": {"BACKEND": "...", "OPTIONS": {...}},
         #     "custom": {"BACKEND": "...", "OPTIONS": {...}},
         # }
         # See: https://docs.djangoproject.com/en/5.2/ref/settings/#std-setting-STORAGES
-        from django.core.files.storage import storages
         return storages[storage_key]
 
-    if not storage_path and storage_key in storages_config:
-        # Use case 3: Transitional setup
-        # Running on Django 4.x but using Django 5-style STORAGES config
-        # Manually load the backend and options
-        storage_path = storages_config.get(storage_key, {}).get("BACKEND")
-        options = storages_config.get(storage_key, {}).get("OPTIONS", {})
-
     if not storage_path:
-        # if no specific storage was resolved, use the default storage
+        # If no storage settings are defined anywhere, use the default storage
         return default_storage
 
     # Fallback to import the storage_path manually
