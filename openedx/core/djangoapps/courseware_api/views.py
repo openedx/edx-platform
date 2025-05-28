01fa59396eabb5ef383c45de8b6f2825ad45ec72
Andrii <andrii.hantkovskyi@raccoongang.com>
2025-05-28 16:34:59 +0300

    feat: extend courseware api with new fields

--- openedx/core/djangoapps/courseware_api/views.py
+++ openedx/core/djangoapps/courseware_api/views.py
@@ -25,15 +25,22 @@ from xmodule.modulestore.exceptions import ItemNotFoundError, NoPathToItem
 from xmodule.modulestore.search import path_to_location
 from xmodule.x_module import PUBLIC_VIEW, STUDENT_VIEW
 
-from common.djangoapps.course_modes.models import CourseMode
+from common.djangoapps.course_modes.models import CourseMode, get_course_prices
 from common.djangoapps.util.views import expose_header
 from lms.djangoapps.edxnotes.helpers import is_feature_enabled
 from lms.djangoapps.certificates.api import get_certificate_url
 from lms.djangoapps.certificates.models import GeneratedCertificate
+from lms.djangoapps.commerce.utils import EcommerceService
 from lms.djangoapps.course_api.api import course_detail
 from lms.djangoapps.course_goals.models import UserActivity
 from lms.djangoapps.course_goals.api import get_course_goal
 from lms.djangoapps.courseware.access import has_access
+from lms.djangoapps.courseware.access_utils import check_public_access
+from lms.djangoapps.courseware.courses import (
+    get_course_about_section,
+    get_course_with_access,
+    get_permission_for_course_about,
+)
 
 from lms.djangoapps.courseware.context_processor import user_timezone_locale_prefs
 from lms.djangoapps.courseware.entrance_exams import course_has_entrance_exam, user_has_passed_entrance_exam
@@ -43,19 +50,24 @@ from lms.djangoapps.courseware.masquerade import (
     is_masquerading_as_non_audit_enrollment,
 )
 from lms.djangoapps.courseware.models import LastSeenCoursewareTimezone
+from lms.djangoapps.courseware.permissions import VIEW_COURSEWARE
 from lms.djangoapps.courseware.block_render import get_block_by_usage_id
-from lms.djangoapps.courseware.toggles import course_exit_page_is_active
+from lms.djangoapps.courseware.toggles import course_exit_page_is_active, course_is_invitation_only
 from lms.djangoapps.courseware.views.views import get_cert_data
 from lms.djangoapps.gating.api import get_entrance_exam_score, get_entrance_exam_usage_key
 from lms.djangoapps.grades.api import CourseGradeFactory
+from lms.djangoapps.instructor.enrollment import uses_shib
+from common.djangoapps.util.milestones_helpers import get_prerequisite_courses_display
 from lms.djangoapps.verify_student.services import IDVerificationService
 from openedx.core.djangoapps.agreements.api import get_integrity_signature
 from openedx.core.djangoapps.courseware_api.utils import get_celebrations_dict
+from openedx.core.djangoapps.enrollments.permissions import ENROLL_IN_COURSE
 from openedx.core.djangoapps.programs.utils import ProgramProgressMeter
 from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
 from openedx.core.lib.api.view_utils import DeveloperErrorViewMixin
 from openedx.core.lib.courses import get_course_by_id
 from openedx.features.course_experience import ENABLE_COURSE_GOALS
+from openedx.features.course_experience.waffle import ENABLE_COURSE_ABOUT_SIDEBAR_HTML
 from openedx.features.content_type_gating.models import ContentTypeGatingConfig
 from openedx.features.course_duration_limits.access import get_access_expiration_data
 from openedx.features.discounts.utils import generate_offer_data
@@ -63,6 +75,10 @@ from common.djangoapps.student.models import (
     CourseEnrollment,
     CourseEnrollmentCelebration,
     LinkedInAddToProfileConfiguration
+)
+from xmodule.course_block import (
+    COURSE_VISIBILITY_PUBLIC,
+    COURSE_VISIBILITY_PUBLIC_OUTLINE,
 )
 
 from .serializers import CourseInfoSerializer
@@ -94,6 +110,7 @@ class CoursewareMeta:
         self.overview.bind_course_for_student(self.request)
         self.enrollment_object = CourseEnrollment.get_enrollment(self.effective_user, self.course_key,
                                                                  select_related=['celebration', 'user__celebration'])
+        self.ecomm_service = EcommerceService()
 
     def __getattr__(self, name):
         return getattr(self.overview, name)
@@ -368,6 +385,139 @@ class CoursewareMeta:
         Returns a boolean representing whether the requesting user should have access to the Xpert Learning Assistant.
         """
         return getattr(settings, 'LEARNING_ASSISTANT_AVAILABLE', False)
+
+    @property
+    def show_courseware_link(self):
+        """
+        Returns a boolean representing whether the courseware link should be shown in the course details page.
+        """
+        with modulestore().bulk_operations(self.course_key):
+            permission = get_permission_for_course_about()
+            course_with_access = get_course_with_access(self.request.user, permission, self.course_key)
+            return bool(
+                self.request.user.has_perm(VIEW_COURSEWARE, course_with_access)
+                or settings.FEATURES.get('ENABLE_LMS_MIGRATION')
+            )
+
+    @property
+    def is_course_full(self):
+        """
+        Returns a boolean representing whether the course is full.
+        """
+        return CourseEnrollment.objects.is_course_full(self.course)
+
+    @property
+    def can_enroll(self):
+        """
+        Returns a boolean representing whether the user can enroll in the course.
+        """
+        return bool(self.request.user.has_perm(ENROLL_IN_COURSE, self.course))
+
+    @property
+    def invitation_only(self):
+        """
+        Returns a boolean representing whether the course is invitation only.
+        """
+        return course_is_invitation_only(self.course)
+
+    @property
+    def is_shib_course(self):
+        """
+        Returns a boolean representing whether the course is a Shibboleth course.
+        """
+        return uses_shib(self.course)
+
+    @property
+    def allow_anonymous(self):
+        """
+        Returns a boolean representing whether the course allows anonymous access.
+        """
+        return bool(check_public_access(self.course, [COURSE_VISIBILITY_PUBLIC, COURSE_VISIBILITY_PUBLIC_OUTLINE]))
+
+    @property
+    def ecommerce_checkout(self):
+        """
+        Returns a boolean representing whether the course has an ecommerce checkout.
+        """
+        return self.ecomm_service.is_enabled(self.request.user)
+
+    @property
+    def single_paid_mode(self):
+        """
+        Returns a dict representing the single paid mode for the course, if it exists.
+        """
+        modes = CourseMode.modes_for_course_dict(self.course_key)
+        single_paid_mode = {}
+        if self.ecommerce_checkout:
+            if len(modes) == 1 and list(modes.values())[0].min_price:
+                single_paid_mode = list(modes.values())[0]
+            else:
+                # have professional ignore other modes for historical reasons
+                single_paid_mode = modes.get(CourseMode.PROFESSIONAL)
+        return single_paid_mode
+
+    @property
+    def ecommerce_checkout_link(self):
+        """
+        Returns the ecommerce checkout link for the course.
+        """
+        if self.single_paid_mode and self.single_paid_mode.sku:
+            return self.ecomm_service.get_checkout_page_url(
+                self.single_paid_mode.sku, course_run_keys=[self.course_key]
+            )
+        return None
+
+    @property
+    def course_image_urls(self):
+        """
+        Returns a list of course image URLs.
+        """
+        return self.overview.image_urls
+
+    @property
+    def start_date_is_still_default(self):
+        """
+        Returns a boolean indicating whether the course start date is still the default value.
+        """
+        return self.overview.start_date_is_still_default
+
+    @property
+    def advertised_start(self):
+        """
+        Returns the advertised start date of the course.
+        """
+        return self.overview.advertised_start
+
+    @property
+    def course_price(self):
+        """
+        Returns the course price, formatted with the currency symbol.
+        """
+        _, course_price = get_course_prices(self.course)
+        return course_price
+
+    @property
+    def pre_requisite_courses(self):
+        """
+        Returns a list of pre-requisite courses for the course.
+        """
+        return get_prerequisite_courses_display(self.course)
+
+    @property
+    def sidebar_html_enabled(self):
+        """
+        Returns a boolean indicating whether the sidebar HTML is enabled for the course.
+        """
+        return ENABLE_COURSE_ABOUT_SIDEBAR_HTML.is_enabled()
+
+    @property
+    def course_about_section_html(self):
+        """
+        Returns the HTML content for the course about section.
+        """
+        if ENABLE_COURSE_ABOUT_SIDEBAR_HTML.is_enabled():
+            return get_course_about_section(self.request, self.course, "about_sidebar_html")
+        return None
 
 
 @method_decorator(transaction.non_atomic_requests, name='dispatch')
