f4c3575bb06086dba62213b594b686d17ccb0aee
Robert Raposa <rraposa@edx.org>
2025-06-13 12:57:47 -0400

    Revert "fix(settings): replace DEFAULT_FILE_STORAGE with STORAGES[default]" (#36907)
    This reverts commit 39028b95006c1ec35ade4ee497d4d074c88902e8.

--- cms/envs/openstack.py
+++ cms/envs/openstack.py
@@ -22,11 +22,11 @@ if AUTH_TOKENS.get('SWIFT_REGION_NAME'):
     SWIFT_EXTRA_OPTIONS = {'region_name': AUTH_TOKENS['SWIFT_REGION_NAME']}
 
 if AUTH_TOKENS.get('DEFAULT_FILE_STORAGE'):
-    STORAGES["default"]["BACKEND"] = AUTH_TOKENS.get('DEFAULT_FILE_STORAGE')
+    DEFAULT_FILE_STORAGE = AUTH_TOKENS.get('DEFAULT_FILE_STORAGE')
 elif SWIFT_AUTH_URL and SWIFT_USERNAME and SWIFT_KEY:
-    STORAGES["default"]["BACKEND"] = 'swift.storage.SwiftStorage'
+    DEFAULT_FILE_STORAGE = 'swift.storage.SwiftStorage'
 else:
-    STORAGES["default"]["BACKEND"] = 'django.core.files.storage.FileSystemStorage'
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 # Use default file storage class set above for course import/export
-COURSE_IMPORT_EXPORT_STORAGE = STORAGES["default"]["BACKEND"]
+COURSE_IMPORT_EXPORT_STORAGE = DEFAULT_FILE_STORAGE
