cffa7da472df02bf245558efe807012906aeb44e
Andrii <andrii.hantkovskyi@raccoongang.com>
2025-06-16 15:35:35 +0300

    refactor: remove ContentLibrary & LearningPackage factories

--- cms/djangoapps/modulestore_migrator/tests/factories.py
+++ cms/djangoapps/modulestore_migrator/tests/factories.py
@@ -5,41 +5,8 @@ import uuid
 
 import factory
 from opaque_keys.edx.keys import LearningContextKey
-from openedx_learning.api.authoring_models import LearningPackage
-from organizations.tests.factories import OrganizationFactory
 
-from common.djangoapps.student.tests.factories import UserFactory
-from cms.djangoapps.modulestore_migrator.models import ModulestoreSource, ModulestoreMigration
-from openedx.core.djangoapps.content_libraries.api import ContentLibrary
-
-
-class LearningPackageFactory(factory.django.DjangoModelFactory):
-    """
-    Factory for LearningPackage model.
-    """
-
-    class Meta:
-        model = LearningPackage
-
-    title = factory.Faker('sentence')
-    description = factory.Faker('sentence')
-    uuid = factory.LazyFunction(lambda: str(uuid.uuid4()))
-    created = factory.Faker('date_time')
-    updated = factory.Faker('date_time')
-
-
-class ContentLibraryFactory(factory.django.DjangoModelFactory):
-    """
-    Factory for ContentLibrary model.
-    """
-
-    class Meta:
-        model = ContentLibrary
-
-    org = factory.SubFactory(OrganizationFactory)
-    license = factory.Faker('sentence')
-    slug = factory.Faker('slug')
-    learning_package = factory.SubFactory(LearningPackageFactory)
+from cms.djangoapps.modulestore_migrator.models import ModulestoreSource
 
 
 class ModulestoreSourceFactory(factory.django.DjangoModelFactory):
@@ -53,14 +20,3 @@ class ModulestoreSourceFactory(factory.django.DjangoModelFactory):
     @factory.lazy_attribute
     def key(self):
         return LearningContextKey.from_string(f"course-v1:edX+DemoX+{uuid.uuid4()}")
-
-
-class ModulestoreMigrationFactory(factory.django.DjangoModelFactory):
-    """
-    Factory for creating ModulestoreMigration instances.
-    """
-    class Meta:
-        model = ModulestoreMigration
-
-    source = factory.SubFactory(ModulestoreSourceFactory)
-    user = factory.SubFactory(UserFactory)
