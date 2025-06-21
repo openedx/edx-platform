27f0845f4397a71cff21206e3259f0ac14d985db
Jawad Khan <jawadkhan444@gmail.com>
2025-06-21 08:27:09 +0500

    fix: removed braze metnion in code

--- openedx/core/djangoapps/notifications/config/waffle.py
+++ openedx/core/djangoapps/notifications/config/waffle.py
@@ -66,5 +66,5 @@ ENABLE_NOTIFY_ALL_LEARNERS = CourseWaffleFlag(f'{WAFFLE_NAMESPACE}.enable_post_n
 # .. toggle_description: Waffle flag to enable push Notifications feature on mobile devices
 # .. toggle_use_cases: open_edx
 # .. toggle_creation_date: 2025-05-13
-# .. toggle_warning: When the flag is ON, Notifications will go through using braze on mobile devices.
+# .. toggle_warning: When the flag is ON, Notifications will go through ace push channels.
 ENABLE_PUSH_NOTIFICATIONS = CourseWaffleFlag(f'{WAFFLE_NAMESPACE}.enable_push_notifications', __name__)
