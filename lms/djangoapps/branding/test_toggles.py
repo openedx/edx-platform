e294b6e8107c5be20df9beebff2280b698471f2c
Andrii <andrii.hantkovskyi@raccoongang.com>
2025-06-23 19:08:59 +0300

    refactor: remove per-page waffle flag in favour of global one

--- lms/djangoapps/branding/test_toggles.py
+++ lms/djangoapps/branding/test_toggles.py
@@ -2,11 +2,10 @@
 Tests for toggles, where there is logic beyond enable/disable.
 """
 
-from unittest.mock import patch
 import ddt
-from django.test import TestCase
+from django.test import override_settings, TestCase
 
-from lms.djangoapps.branding.toggles import use_new_catalog_page
+from lms.djangoapps.branding.toggles import use_catalog_mfe
 
 
 @ddt.ddt
@@ -16,15 +15,9 @@ class TestBrandingToggles(TestCase):
     """
 
     @ddt.data(True, False)
-    @patch("lms.djangoapps.branding.toggles.ENABLE_NEW_CATALOG_PAGE")
-    def test_use_new_catalog_page_enabled(
-        self, is_waffle_enabled, mock_enable_new_catalog_page
-    ):
-        # Given legacy catalog feature is / not enabled
-        mock_enable_new_catalog_page.is_enabled.return_value = is_waffle_enabled
-
-        # When I check if the feature is enabled
-        should_use_new_catalog_page = use_new_catalog_page()
-
-        # Then I respects waffle setting.
-        self.assertEqual(should_use_new_catalog_page, is_waffle_enabled)
+    def test_use_catalog_mfe(self, enabled):
+        """
+        Test the use_catalog_mfe toggle.
+        """
+        with override_settings(FEATURES={'ENABLE_CATALOG_MICROFRONTEND': enabled}):
+            assert use_catalog_mfe() == enabled
