2c84170bce76cd39349b22a15df9b9f721da7515
jawad khan <jawad.khan@arbisoft.com>
2025-06-24 19:42:36 +0500

    Add organization logo field in mobile apis (#36940)
    * feat: Add org_logo field in mobile apis
    
    * fix: fixed quality checks

--- lms/djangoapps/mobile_api/utils.py
+++ lms/djangoapps/mobile_api/utils.py
@@ -1,6 +1,7 @@
 """
 Common utility methods for Mobile APIs.
 """
+from organizations import api as organizations_api
 
 API_V05 = 'v0.5'
 API_V1 = 'v1'
@@ -12,3 +13,16 @@ API_V4 = 'v4'
 def parsed_version(version):
     """ Converts string X.X.X.Y to int tuple (X, X, X) """
     return tuple(map(int, (version.split(".")[:3])))
+
+
+def get_course_organization_logo(course_key):
+    """
+    Get organization logo of given course key.
+    """
+    organization_logo = None
+    organizations = organizations_api.get_course_organizations(course_key=course_key)
+    if organizations:
+        organization = organizations[0]
+        organization_logo = organization.get('logo', None)
+
+    return str(organization_logo.url) if organization_logo else ''
