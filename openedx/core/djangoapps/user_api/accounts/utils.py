64054d6b3b75e89669c001fd5afbdd9b2d7555fe
Atiya Ishaq <atiya.ishaq@A006-01676.local>
2025-07-03 16:33:55 +0500

    chore: refactor image_storage method into general method

--- openedx/core/djangoapps/user_api/accounts/utils.py
+++ openedx/core/djangoapps/user_api/accounts/utils.py
@@ -15,6 +15,11 @@ from django.utils.translation import gettext as _
 from edx_django_utils.user import generate_password
 from social_django.models import UserSocialAuth
 
+
+from django.core.files.storage import default_storage, storages
+from django.utils.module_loading import import_string
+
+
 from common.djangoapps.student.models import AccountRecovery, Registration, get_retired_email_by_email
 from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
 from openedx.core.djangoapps.theming.helpers import get_config_value_from_site_or_settings, get_current_site
@@ -255,3 +260,43 @@ def handle_retirement_cancellation(retirement, email_address=None):
     retirement.user.save()
 
     retirement.delete()
+
+
+def get_named_storage(name, legacy_setting_name=None):
+    """
+    Returns an instance of the configured storage backend for the given name.
+
+    This function prioritizes different settings in the following order to determine
+    which storage class to use:
+
+    1. Use the named storage from Django's STORAGES if defined (Django 4.2+).
+    2. If not available, check the legacy setting (if provided).
+    3. If still undefined, fall back to Django's default_storage.
+
+    Args:
+        name (str): The name of the storage as defined in Django's STORAGES setting.
+        legacy_setting_name (str, optional): The name of the legacy setting dict
+            to check for a storage class path and options.
+
+    Returns:
+        An instance of the configured storage backend.
+
+    Raises:
+        ImportError: If the specified storage class cannot be imported.
+    """
+    # 1. Check Django 4.2+ STORAGES dict
+    storages_config = getattr(settings, 'STORAGES', {})
+    if name in storages_config:
+        return storages[name]
+
+    # 2. Check legacy config
+    if legacy_setting_name:
+        legacy_config = getattr(settings, legacy_setting_name, {})
+        storage_class_path = legacy_config.get('class')
+        options = legacy_config.get('options', {})
+        if storage_class_path:
+            storage_class = import_string(storage_class_path)
+            return storage_class(**options)
+
+    # 3. Fallback to Django's default_storage
+    return default_storage
