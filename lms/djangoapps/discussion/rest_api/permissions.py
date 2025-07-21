acc32fee53a9fecb8163a4d6fb52da24189b6ec5
Muhammad Adeel Tajamul <77053848+muhammadadeeltajamul@users.noreply.github.com>
2025-07-21 09:43:55 +0500

    feat: added endpoint for priviledged roles to delete threads of a user (#37030)
    * feat: added endpoint for priviledged roles to delete threads of a user
    
    * chore: moved forum calls to django comment common app
    
    * fix: fixed nits

--- lms/djangoapps/discussion/rest_api/permissions.py
+++ lms/djangoapps/discussion/rest_api/permissions.py
@@ -6,7 +6,7 @@ from typing import Dict, Set, Union
 from opaque_keys.edx.keys import CourseKey
 from rest_framework import permissions
 
-from common.djangoapps.student.models import CourseEnrollment
+from common.djangoapps.student.models import CourseAccessRole, CourseEnrollment
 from common.djangoapps.student.roles import (
     CourseInstructorRole,
     CourseStaffRole,
@@ -19,7 +19,7 @@ from lms.djangoapps.discussion.django_comment_client.utils import (
 from openedx.core.djangoapps.django_comment_common.comment_client.comment import Comment
 from openedx.core.djangoapps.django_comment_common.comment_client.thread import Thread
 from openedx.core.djangoapps.django_comment_common.models import (
-    FORUM_ROLE_ADMINISTRATOR, FORUM_ROLE_COMMUNITY_TA, FORUM_ROLE_MODERATOR
+    Role, FORUM_ROLE_ADMINISTRATOR, FORUM_ROLE_COMMUNITY_TA, FORUM_ROLE_MODERATOR
 )
 
 
@@ -185,3 +185,46 @@ class IsStaffOrAdmin(permissions.BasePermission):
             request.user.is_staff or
             is_user_staff and request.method == "GET"
         )
+
+
+def can_take_action_on_spam(user, course_id):
+    """
+    Returns if the user has access to take action against forum spam posts
+    Parameters:
+        user: User object
+        course_id: CourseKey or string of course_id
+    """
+    if GlobalStaff().has_user(user):
+        return True
+
+    if isinstance(course_id, str):
+        course_id = CourseKey.from_string(course_id)
+    org_id = course_id.org
+    course_ids = CourseEnrollment.objects.filter(user=user).values_list('course_id', flat=True)
+    course_ids = [c_id for c_id in course_ids if c_id.org == org_id]
+    user_roles = set(
+        Role.objects.filter(
+            users=user,
+            course_id__in=course_ids,
+        ).values_list('name', flat=True).distinct()
+    )
+    if bool(user_roles & {FORUM_ROLE_ADMINISTRATOR, FORUM_ROLE_MODERATOR}):
+        return True
+
+    if CourseAccessRole.objects.filter(user=user, course_id__in=course_ids, role__in=["instructor", "staff"]).exists():
+        return True
+    return False
+
+
+class IsAllowedToBulkDelete(permissions.BasePermission):
+    """
+    Permission that checks if the user is staff or an admin.
+    """
+
+    def has_permission(self, request, view):
+        """Returns true if the user can bulk delete posts"""
+        if not request.user.is_authenticated:
+            return False
+
+        course_id = view.kwargs.get("course_id")
+        return can_take_action_on_spam(request.user, course_id)
