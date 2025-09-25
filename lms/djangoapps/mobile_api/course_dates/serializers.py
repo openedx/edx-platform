03771ad714e730abb6fe9aa07bc43514150135f5
Serhii Nanai <serhii.nanai@raccoongang.com>
2025-09-25 11:10:07 +0300

    feat: change learner_has_access to bool on dates serializer

--- lms/djangoapps/mobile_api/course_dates/serializers.py
+++ lms/djangoapps/mobile_api/course_dates/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 
 
 class AllCourseDatesSerializer(serializers.Serializer):
-    learner_has_access = serializers.SerializerMethodField()
+    learner_has_access = serializers.BooleanField()
     course_id = serializers.CharField(source="content_date.course_id")
     due_date = serializers.DateTimeField(source="actual_date", format="%Y-%m-%dT%H:%M:%S%z")
     assignment_title = serializers.CharField(source="content_date.assignment_title")
@@ -13,11 +13,6 @@ class AllCourseDatesSerializer(serializers.Serializer):
     course_name = serializers.CharField(source="content_date.course_name")
     location = serializers.CharField()
     relative = serializers.SerializerMethodField()
-
-    @staticmethod
-    def get_learner_has_access(obj):
-        """Whether the learner is blocked (gated) from this content or not"""
-        return not obj.content_date.contains_gated_content
 
     @staticmethod
     def get_relative(obj):
