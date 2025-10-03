a99b8cb8bfa8a65b252deb6eecd4239464deb462
Serhii Nanai <serhii.nanai@raccoongang.com>
2025-10-03 13:29:10 +0300

    feat: adjust AllCourseDatesSerializer

--- lms/djangoapps/mobile_api/course_dates/serializers.py
+++ lms/djangoapps/mobile_api/course_dates/serializers.py
@@ -1,23 +1,19 @@
 """
 Serializers for course dates.
 """
-
 from rest_framework import serializers
 
 
 class AllCourseDatesSerializer(serializers.Serializer):
-    learner_has_access = serializers.SerializerMethodField()
-    course_id = serializers.CharField()
-    due_date = serializers.DateTimeField(source="date", format="%Y-%m-%dT%H:%M:%S%z")
-    assignment_title = serializers.CharField(source="title")
-    first_component_block_id = serializers.SerializerMethodField()
-    course_name = serializers.CharField(source="course.display_name")
+    learner_has_access = serializers.BooleanField()
+    course_id = serializers.CharField(source="content_date.course_id")
+    due_date = serializers.DateTimeField(source="actual_date", format="%Y-%m-%dT%H:%M:%S%z")
+    assignment_title = serializers.CharField(source="content_date.assignment_title")
+    first_component_block_id = serializers.CharField()
+    course_name = serializers.CharField(source="content_date.course_name")
     location = serializers.CharField()
-    relative = serializers.BooleanField(source="is_relative")
+    relative = serializers.SerializerMethodField()
 
-    def get_learner_has_access(self, block):
-        """Whether the learner is blocked (gated) from this content or not"""
-        return not getattr(block, "contains_gated_content", False)
-
-    def get_first_component_block_id(self, block):
-        return getattr(block, "first_component_block_id", "")
+    @staticmethod
+    def get_relative(obj):
+        return obj.rel_date is not None
