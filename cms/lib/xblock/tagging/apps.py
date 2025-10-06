f9898f167da1d7b05acc5553633b8a49e03a4a3d
farhan <farhan.khan@arbisoft.com>
2025-10-06 18:18:36 +0500

    chore: Adds tagging app fix

---
+++ cms/lib/xblock/tagging/apps.py
@@ -0,0 +1,12 @@
+"""
+Django app configuration for the XBlock tagging app
+"""
+from django.apps import AppConfig
+
+
+class TaggingConfig(AppConfig):
+    """
+    Django app configuration for the XBlock tagging app
+    """
+    name = 'cms.lib.xblock.tagging'
+    verbose_name = 'XBlock Tagging' 
