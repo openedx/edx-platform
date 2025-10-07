d60462dd1c89a67bee9fc6981598cb8968182c52
Feanil Patel <feanil@axim.org>
2025-10-07 16:47:06 -0400

    build: Update and add MFE URLs
    Add MFE urls that match where MFEs run when you call `npm run dev` in
    the MFE repos.

--- lms/envs/development.py
+++ lms/envs/development.py
@@ -1,6 +1,9 @@
 """
 This settings file is optimized for local development.  It should work equally well for bare-metal development and for
 running inside of development environments such as tutor.
+
+This file is currently in development itself and so may not work for everyone out of the box.  More updates, including
+updated documentation will be added as we get closer to removing devstack.py
 """
 
 #Helpers for loading plugins and their settings.
@@ -8,8 +11,9 @@ from edx_django_utils.plugins import add_plugins
 from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
 
 # Use the common file as the starting point.
+# pylint: disable=wildcard-import
 from .common import *
-from openedx.core.lib.derived import Derived, derive_settings
+from openedx.core.lib.derived import derive_settings
 
 DEBUG = True
 
@@ -66,6 +70,7 @@ JWT_AUTH.update({
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     "http://apps.local.openedx.io:1996",
+    "http://apps.local.openedx.io:2000",
 )
 
 # Cookie Related Settings
@@ -73,6 +78,7 @@ SESSION_COOKIE_DOMAIN = '.local.openedx.io'
 
 # MFE Development URLs
 LEARNER_HOME_MICROFRONTEND_URL = 'http://apps.local.openedx.io:1996/learner-dashboard/'
+LEARNING_MICROFRONTEND_URL = "http://apps.local.openedx.io:2000/learning/"
 
 #######################################################################################################################
 #### DERIVE ANY DERIVED SETTINGS
