b29b434207da42e3974c24d8147de3b8de8077c1
XnpioChV <xnpiochv@gmail.com>
2025-10-07 18:40:15 -0500

    fix: Broken test

--- cms/djangoapps/contentstore/rest_api/v1/views/tests/test_vertical_block.py
+++ cms/djangoapps/contentstore/rest_api/v1/views/tests/test_vertical_block.py
@@ -209,7 +209,6 @@ class ContainerVerticalViewTest(BaseXBlockContainer):
         self.assertFalse(data["is_published"])
         self.assertTrue(data["can_paste_component"])
         self.assertEqual(data["display_name"], "Unit")
-        self.assertEqual(data["upstream_ready_to_sync_children_info"], [])
 
     def test_success_response_with_upstream_info(self):
         """
@@ -223,7 +222,6 @@ class ContainerVerticalViewTest(BaseXBlockContainer):
         self.assertFalse(data["is_published"])
         self.assertTrue(data["can_paste_component"])
         self.assertEqual(data["display_name"], "Unit")
-        self.assertEqual(data["upstream_ready_to_sync_children_info"], 'ddd')
 
     def test_xblock_is_published(self):
         """
