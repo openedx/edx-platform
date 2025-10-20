3f5ac6ddbc98dc1a17c86396f1bfcf8c1ab72c58
Chris Chávez <xnpiochv@gmail.com>
2025-10-20 17:02:04 -0500

    fix: Update `on_commit_changes_to` of modulestore to check MySQL transaction [FC-0097] (#37485)
    - `handle_update_xblock_upstream_link` is called asynchronously with celery. In `update_upstream_downstream_link_handler`, the xblock has the updated version, but when calling `handle_update_xblock_upstream_link` inside Celery, the xblock is outdated in a previous version, which is why the error occurs. This happens because `on_commit_changes_to` is executed before the MySQL transaction ends.
    - Added `ImmediateOnCommitMixin` to be used in tests that need to call `on_commit_changes_to`. See https://github.com/openedx/edx-platform/pull/37485#issuecomment-3412979170  for more info

--- openedx/core/djangoapps/content/course_overviews/tests/test_signals.py
+++ openedx/core/djangoapps/content/course_overviews/tests/test_signals.py
@@ -13,7 +13,11 @@ from pytz import UTC
 
 from xmodule.data import CertificatesDisplayBehaviors
 from xmodule.modulestore import ModuleStoreEnum
-from xmodule.modulestore.tests.django_utils import TEST_DATA_ONLY_SPLIT_MODULESTORE_DRAFT_PREFERRED, ModuleStoreTestCase
+from xmodule.modulestore.tests.django_utils import (
+    TEST_DATA_ONLY_SPLIT_MODULESTORE_DRAFT_PREFERRED,
+    ModuleStoreTestCase,
+    ImmediateOnCommitMixin,
+)
 from xmodule.modulestore.tests.factories import CourseFactory, check_mongo_calls
 
 from ..models import CourseOverview
@@ -23,7 +27,7 @@ Change = namedtuple("Change", ["field_name", "initial_value", "changed_value"])
 
 
 @ddt.ddt
-class CourseOverviewSignalsTestCase(ModuleStoreTestCase):
+class CourseOverviewSignalsTestCase(ImmediateOnCommitMixin, ModuleStoreTestCase):
     """
     Tests for CourseOverview signals.
     """
@@ -43,16 +47,14 @@ class CourseOverviewSignalsTestCase(ModuleStoreTestCase):
         )
 
         # changing display name doesn't fire the signal
-        with self.captureOnCommitCallbacks(execute=True) as callbacks:
-            course.display_name = course.display_name + 'changed'
-            course = self.store.update_item(course, ModuleStoreEnum.UserID.test)
+        course.display_name = course.display_name + 'changed'
+        course = self.store.update_item(course, ModuleStoreEnum.UserID.test)
         assert not mock_signal.called
 
         # changing the given field fires the signal
-        with self.captureOnCommitCallbacks(execute=True) as callbacks:
-            for change in changes:
-                setattr(course, change.field_name, change.changed_value)
-            self.store.update_item(course, ModuleStoreEnum.UserID.test)
+        for change in changes:
+            setattr(course, change.field_name, change.changed_value)
+        self.store.update_item(course, ModuleStoreEnum.UserID.test)
         assert mock_signal.called
 
     def test_caching(self):
