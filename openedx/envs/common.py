174d9f4b8540a34c1049cc33d73b8665aae29c49
Feanil Patel <feanil@axim.org>
2025-10-29 15:31:21 -0400

    docs: Update openedx/envs/common.py
    Co-authored-by: Kyle McCormick <kyle@axim.org>

--- openedx/envs/common.py
+++ openedx/envs/common.py
@@ -37,8 +37,8 @@ from openedx.core.djangoapps.theming.helpers_dirs import (
     get_theme_base_dirs_from_settings
 )
 
-# For some reason other parts of the system reference these constants
-# via the settings module.
+# We have legacy components that reference these constants via the settings module.
+# New code should import them directly from `openedx.core.constants` instead.
 from openedx.core.constants import (  # pylint: disable=unused-import
     ASSET_KEY_PATTERN,
     COURSE_KEY_REGEX,
