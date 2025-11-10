951ab382796b39789453058c718cdbbd6c1343fe
XnpioChV <xnpiochv@gmail.com>
2025-11-10 16:09:22 -0500

    style: Fix broken lint

--- cms/djangoapps/modulestore_migrator/rest_api/v1/views.py
+++ cms/djangoapps/modulestore_migrator/rest_api/v1/views.py
@@ -13,7 +13,6 @@ from rest_framework.exceptions import ParseError
 from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from user_tasks.models import UserTaskStatus
