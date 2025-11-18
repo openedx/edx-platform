5c7bd3a417a80c04e945e8777206a329deb4a8ba
Naincy Chourasia <nchourasia-apphelix@2u.com>
2025-11-18 13:48:10 +0530

    fix: team posts now visible by handling standalone context properly (#38)
    https://2u-internal.atlassian.net/jira/software/c/projects/COSMO2/boards/2821?selectedIssue=COSMO2-776

--- lms/djangoapps/discussion/views.py
+++ lms/djangoapps/discussion/views.py
@@ -166,6 +166,7 @@ def get_threads(request, course, user_info, discussion_id=None, per_page=THREADS
                     'flagged',
                     'unread',
                     'unanswered',
+                    'context',
                 ]
             )
         )
