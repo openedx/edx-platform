e662394398ae8955e59f274773ac55c6e7aadc72
farhan <farhan.khan@arbisoft.com>
2025-12-16 18:52:12 +0500

    refactor: Enable word cloud XBlock

--- openedx/envs/common.py
+++ openedx/envs/common.py
@@ -1666,7 +1666,7 @@ XBLOCK_RUNTIME_V2_EPHEMERAL_DATA_CACHE = 'default'
 # .. toggle_warning: Not production-ready until https://github.com/openedx/edx-platform/issues/34840 is done.
 # .. toggle_creation_date: 2024-11-10
 # .. toggle_target_removal_date: 2025-06-01
-USE_EXTRACTED_WORD_CLOUD_BLOCK = False
+USE_EXTRACTED_WORD_CLOUD_BLOCK = True
 
 # .. toggle_name: USE_EXTRACTED_ANNOTATABLE_BLOCK
 # .. toggle_default: False
